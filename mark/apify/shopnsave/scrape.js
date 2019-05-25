11f3fc49224c7bfccc583b4d19038b66ea2440c2
Mark <markswong8@gmail.com>
2019-05-25 06:15:44 -0700

    shopnsave update

---
+++ mark/apify/shopnsave/scrape.js
@@ -0,0 +1,32 @@
+const Apify = require('apify');
+const rp = require('request-promise-native');
+const { xml2json, parseData } = require('./tools');
+
+const storeurl = 'https://www.shopnsavefood.com/DesktopModules/StoreLocator/API/StoreWebAPI.asmx/GetAllStores';
+
+Apify.main(async () => {
+  const requestList = new Apify.RequestList({
+    sources: [
+      { url: storeurl },
+    ],
+  });
+  await requestList.initialize();
+
+  const crawler = new Apify.BasicCrawler({
+    requestList,
+    handleRequestFunction: async ({ request }) => {
+      const html = await rp(request.url);
+      const json = await xml2json(html);
+
+      // The data is nested so define data to this new object
+      const data = json.ArrayOfStore.Store;
+
+      /* eslint-disable no-restricted-syntax */
+      for await (const obj of data) {
+        await Apify.pushData(parseData(obj));
+      }
+    },
+  });
+
+  await crawler.run();
+});
--- mark/apify/shopnsave/tools.js
+++ mark/apify/shopnsave/tools.js
@@ -1,9 +1,5 @@
-const Apify = require('apify');
 const { parseString } = require('xml2js');
-const rp = require('request-promise-native');
 
-// ENV URL: https://www.shopnsavefood.com/DesktopModules/StoreLocator/API/StoreWebAPI.asmx/GetAllStores
-const storeurl = 'https://www.shopnsavefood.com/DesktopModules/StoreLocator/API/StoreWebAPI.asmx/GetAllStores';
 const noDataLabel = 'NO-DATA';
 
 // Converts XML to JSON
@@ -21,11 +17,11 @@ const xml2json = xmlString => new Promise((fullfill, reject) => {
 const formatPhoneNumber = string => string.replace(/\D/g, '');
 
 const validateHours = (string1, string2) => {
-  if (string1 === 'NO-DATA' || string1.length === 0) {
-    return 'NO-DATA';
+  if (string1 === noDataLabel || string1.length === 0) {
+    return noDataLabel;
   }
-  if (string1 !== 'NO-DATA' || string1.length !== 0) {
-    if (string2 === 'NO-DATA' || string2.length === 0) {
+  if (string1 !== noDataLabel || string1.length !== 0) {
+    if (string2 === noDataLabel || string2.length === 0) {
       return string1;
     }
   }
@@ -33,7 +29,7 @@ const validateHours = (string1, string2) => {
 };
 
 // Simply receives data from the scrape, then formats it.
-const parsedShopData = ({
+const parseData = ({
   // If any data points are undefined / null, return 'NO-DATA'
   locatorDomain = 'shopnsavefood.com__api', Name = noDataLabel, Address1 = noDataLabel, City = noDataLabel,
   State = noDataLabel, Zip = noDataLabel, countryCode = 'US', StoreID = noDataLabel,
@@ -51,8 +47,8 @@ const parsedShopData = ({
     country_code: countryCode,
     store_number: StoreID,
     // Although redundant is required due to format Phone Number
-    ...((Phone === 'NO-DATA') && { phone: 'NO-DATA' }),
-    ...((Phone !== 'NO-DATA') && { phone: formatPhoneNumber(Phone) }),
+    ...((Phone === noDataLabel) && { phone: noDataLabel }),
+    ...((Phone !== noDataLabel) && { phone: formatPhoneNumber(Phone) }),
     ...((IsGasStation === 'true') && { location_type: 'Gas Station' }),
     ...((IsGasStation === 'false') && { location_type: 'Store' }),
     naics_code: naics,
@@ -61,29 +57,8 @@ const parsedShopData = ({
     hours_of_operation: validateHours(Hours, Hours2),
   });
 
-Apify.main(async () => {
-  const requestList = new Apify.RequestList({
-    sources: [
-      { url: storeurl },
-    ],
-  });
-  await requestList.initialize();
-
-  const crawler = new Apify.BasicCrawler({
-    requestList,
-    handleRequestFunction: async ({ request }) => {
-      const html = await rp(request.url);
-      const json = await xml2json(html);
-
-      // The data is nested so define data to this new object
-      const data = json.ArrayOfStore.Store;
-
-      /* eslint-disable no-restricted-syntax */
-      for await (const obj of data) {
-        await Apify.pushData(parsedShopData(obj));
-      }
-    },
-  });
-
-  await crawler.run();
-});
+module.exports = {
+  xml2json,
+  formatPhoneNumber,
+  parseData
+}
