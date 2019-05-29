d5432dae86985e53c3b5ce695879c81d2ad6b788
Gary Soeller <gsoeller@eng.ucsd.edu>
2019-05-28 22:42:10 -0700

    Implement marriott and shopnsave

--- gary/marriott/scrape.js
+++ gary/marriott/scrape.js
@@ -152,7 +152,6 @@ async function run(csvFile) {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      //args: ['--no-sandbox', 'disable-setuid-sandbox']
     });
     const page = await browser.newPage();
     
