a7f1833816865750f484fa996e5bb47e67c7b52f
Tenzing Shaw <tenzing@safegraph.com>
2019-06-28 14:50:56 -0700

    add back env var

--- scripts/run_scraper.sh
+++ scripts/run_scraper.sh
@@ -4,4 +4,4 @@ domain=${PWD##*/}
 scraper_name=${domain}-scraper
 docker build -t $scraper_name --no-cache .
 rm -rf apify_docker_storage
-docker run -e "APIFY_TOKEN=''" -e "APIFY_LOCAL_STORAGE_DIR=apify_storage" -v ${base_path}/apify_docker_storage:/apify_storage ${scraper_name}:latest
+docker run -e "APIFY_LOCAL_STORAGE_DIR"="apify_storage" -e "APIFY_TOKEN=''" -v ${base_path}/apify_docker_storage:/apify_storage ${scraper_name}:latest
