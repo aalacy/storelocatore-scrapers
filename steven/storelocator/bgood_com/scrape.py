566e99a7e07c909d6f1fab540db61dec2fc84857
spiscatelli <48687881+spiscatelli@users.noreply.github.com>
2019-07-06 10:25:09 -0400

    Update scrape.py
    Change name of CSV output

--- steven/storelocator/bgood_com/scrape.py
+++ steven/storelocator/bgood_com/scrape.py
@@ -8,7 +8,7 @@ location_url = 'https://www.bgood.com/locations/'
 # output path of CSV
 output_path = os.path.dirname(os.path.realpath(__file__)
 # file name of CSV output
-file_name = 'bgood_extract.csv'
+file_name = 'data.csv'
 
 # Function pull webpage content in soup object
 def pull_content(url):
