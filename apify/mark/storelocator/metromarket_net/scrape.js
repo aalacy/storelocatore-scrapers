4e69d96391f454db6481972b7e688e80f80d5aba
Tenzing Shaw <tenzing@safegraph.com>
2019-07-09 00:11:27 -0700

    use stealth mode

--- apify/mark/storelocator/metromarket_net/scrape.js
+++ apify/mark/storelocator/metromarket_net/scrape.js
@@ -17,7 +17,7 @@ const {
 Apify.main(async () => {
   const requestQueue = await Apify.openRequestQueue();
   await requestQueue.addRequest({
-    url: 'https://www.metromarket.net/storelocator-sitemap.xml',
+		url: 'https://www.metromarket.net/storelocator-sitemap.xml',
     userData: {
       urlType: 'initial',
     },
@@ -25,51 +25,48 @@ Apify.main(async () => {
 
   const crawler = new Apify.PuppeteerCrawler({
     requestQueue,
-    handlePageFunction: async ({ request, page }) => {
+		launchPuppeteerOptions: {
+			headless: false,
+			useChrome: true,
+			stealth: true
+		},
+		handlePageFunction: async ({ request, page }) => {
 			if (request.userData.urlType === 'initial') {
 				await page.waitForSelector('span', { timeout: 0 });
 				const urls = await page.$$eval('span', se => se.map(s => s.innerText));
         const locationUrls = urls.filter(e => e.match(/www.metromarket.net\/stores\/details\//))
 					.map(e => ({ url: e, userData: { urlType: 'detail' } }));
-        await page.waitFor(5000);
 				/* eslint-disable no-restricted-syntax */
-        for await (const url of locationUrls) {
-          await requestQueue.addRequest(url);
+				for (const url of locationUrls) {
+					await requestQueue.addRequest(url);
         }
       }
 			if (request.userData.urlType === 'detail') {
-				if (await page.$(locationObjectSelector) !== null) {
-					await page.waitForSelector(locationObjectSelector, { timeout: 0 });
-					const locationObjectRaw = await page.$eval(locationObjectSelector, s => s.innerText);
-          const locationObject = formatObject(locationObjectRaw);
+				await page.waitForSelector('main', { timeout: 0 });
+				const locationObjectRaw = await page.$eval(locationObjectSelector, s => s.innerText);
+				const locationObject = formatObject(locationObjectRaw);
 
-          const poiData = {
-            locator_domain: 'metromarket.net',
-            location_name: locationObject.name,
-            street_address: locationObject.address.streetAddress,
-            city: locationObject.address.addressLocality,
-            state: locationObject.address.addressRegion,
-            zip: locationObject.address.postalCode,
-            country_code: undefined,
-            store_number: undefined,
-            phone: formatPhoneNumber(locationObject.telephone),
-            location_type: locationObject['@type'],
-            latitude: locationObject.geo.latitude,
-            longitude: locationObject.geo.longitude,
-            hours_of_operation: formatHours(locationObject.openingHours),
-          };
-					const poi = new Poi(poiData);
-          await Apify.pushData(poi);
-          await page.waitFor(5000);
-				} 
-      }
-    },
-    maxRequestsPerCrawl: 100,
-    maxConcurrency: 5,
-    launchPuppeteerOptions: {
-			headless: true,
-			stealth: true
-		}
+				const poiData = {
+					locator_domain: 'metromarket.net',
+					location_name: locationObject.name,
+					street_address: locationObject.address.streetAddress,
+					city: locationObject.address.addressLocality,
+					state: locationObject.address.addressRegion,
+					zip: locationObject.address.postalCode,
+					country_code: undefined,
+					store_number: undefined,
+					phone: formatPhoneNumber(locationObject.telephone),
+					location_type: locationObject['@type'],
+					latitude: locationObject.geo.latitude,
+					longitude: locationObject.geo.longitude,
+					hours_of_operation: formatHours(locationObject.openingHours),
+				};
+				const poi = new Poi(poiData);
+				await Apify.pushData(poi);
+			}
+		},
+		maxRequestsPerCrawl: 100,
+		maxConcurrency: 1,
   });
 
   await crawler.run();
