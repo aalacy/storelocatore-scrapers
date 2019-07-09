042c803ecc85daae91d27b503b25f0f4f22375ab
Tenzing Shaw <tenzing@safegraph.com>
2019-07-08 18:34:37 -0700

    rm option and don't call fetchNextRequest

--- apify/mark/storelocator/metromarket_net/scrape.js
+++ apify/mark/storelocator/metromarket_net/scrape.js
@@ -27,29 +27,20 @@ Apify.main(async () => {
     requestQueue,
     handlePageFunction: async ({ request, page }) => {
 			if (request.userData.urlType === 'initial') {
-				console.log("found initial URL");
-				await page.waitForSelector('span', { waitUntil: 'load', timeout: 0 });
-				console.log("found a span");
+				await page.waitForSelector('span', { timeout: 0 });
 				const urls = await page.$$eval('span', se => se.map(s => s.innerText));
-				console.log("grabbed span text");
         const locationUrls = urls.filter(e => e.match(/www.metromarket.net\/stores\/details\//))
 					.map(e => ({ url: e, userData: { urlType: 'detail' } }));
-				console.log("waitint 5 seconds");
         await page.waitFor(5000);
 				/* eslint-disable no-restricted-syntax */
-				console.log("adding new requests to queue");
         for await (const url of locationUrls) {
           await requestQueue.addRequest(url);
         }
       }
 			if (request.userData.urlType === 'detail') {
-				console.log("found a detail page");
 				if (await page.$(locationObjectSelector) !== null) {
-					console.log("got location object selector");
-					await page.waitForSelector(locationObjectSelector, { waitUntil: 'load', timeout: 0 });
-					console.log("finished waiting for selector");
+					await page.waitForSelector(locationObjectSelector, { timeout: 0 });
 					const locationObjectRaw = await page.$eval(locationObjectSelector, s => s.innerText);
-					console.log("finished getting selector text");
           const locationObject = formatObject(locationObjectRaw);
 
           const poiData = {
@@ -68,13 +59,9 @@ Apify.main(async () => {
             hours_of_operation: formatHours(locationObject.openingHours),
           };
 					const poi = new Poi(poiData);
-					console.log("pushing record");
           await Apify.pushData(poi);
           await page.waitFor(5000);
-				} else {
-					console.log("fetching next request");
-          await requestQueue.fetchNextRequest();
-        }
+				} 
       }
     },
     maxRequestsPerCrawl: 100,
