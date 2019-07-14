2275a5d7da229aa449354e217e39e170d2477f38
Saleem Abdul Hamid <meelash@gmail.com>
2019-07-14 12:04:58 -0400

    added scraping for location hours

--- apify/saleem/storelocator/bluemoonfitness_com/scrape.js
+++ apify/saleem/storelocator/bluemoonfitness_com/scrape.js
@@ -10,19 +10,33 @@ Apify.main(async () => {
 async function scrape() {
 
   // Begin scraper
-  var records = [];
-  await request('https://www.bluemoonfitness.com/gym-locations/', function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
+  const records = [];
+  await request('https://www.bluemoonfitness.com/gym-locations/')
+    .then(async function (html) {
+      let $ = cheerio.load(html);
 
       // location information is in individual list elements
-      $('.loc-list').find('li').each((index, location_element) => {
-
+      const locationList = $('.loc-list').find('li').toArray();
+      for (const location_element of locationList) {
+        // get address
         const rawAddress = $('.info', location_element).text();
         const {groups: addressParts} = rawAddress.match(/\s*(?<street_address>.+)\n\s*(?<city>.+),\s*(?<state>[A-Z]{2})\s*(?<zip>\d+)[\n.\s]*(?<phone>.*?)[\n\s]*$/);
 
+        // get lat and long if available in directions link
         const directionsLink = $('a:contains(Get Directions)', location_element).attr('href');
         const {groups: {lat, long}} = directionsLink.match(/\@(?<lat>[-?\d\.]*)\,(?<long>[-?\d\.]*)/);
+
+        // click on location info and get hours from indivual page
+        const informationLink = $('a:contains(Location Info)', location_element).attr('href');
+        var hours_of_operation;
+        await request(informationLink)
+          .then(function (html) {
+            let $ = cheerio.load(html);
+            hours_of_operation = $('.unit:contains(Hours)').find('.panel').text();
+          })
+          .catch(function () {
+            throw "Couldn't load location information page to get Hours"
+          });
 
         records.push({
           locator_domain: 'bluemoonfitness.com',
@@ -37,14 +51,15 @@ async function scrape() {
           location_type: '<MISSING>',
           latitude: lat,
           longitude: long,
-          hours_of_operation: '<MISSING>',
+          hours_of_operation: hours_of_operation,
         });
-      });
-    }
-    else {
+
+      };
+    })
+    .catch(function () {
       throw 'Invalid response from address.'
-    }
-  });
+    });
+
   return records;
 	// End scraper
 
