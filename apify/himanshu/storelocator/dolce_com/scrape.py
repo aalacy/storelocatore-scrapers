42e5aeaf845a21b81d850df808570db1a4474070
Himanshu347 <hdhameliya@dhameliyaconsultingservice.com>
2019-07-15 10:01:05 -0400

    eartbars

---
+++ apify/himanshu/storelocator/dolce_com/scrape.py
@@ -0,0 +1,121 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import re
+import json 
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    base_url = "https://www.wyndhamhotels.com/bin/propertypages.json?locale=en-us&brandId=DX"
+    r = requests.get(base_url).json()['subcategory']
+    return_main_object = []
+    for i in range(len(r)):
+        country=r[i]['name']
+        if country=="United States":
+            country="US"
+        if country=="Canada":
+            country="CA"
+        if "subcategory" in r[i]:
+            for val in r[i]['subcategory']:
+                for data in val['properties']:
+                    state=data['state']
+                    city=data['city']
+                    link=data['path']
+                    r1 = requests.get(link)
+                    soup = BeautifulSoup(r1.text,"lxml")
+                    mainadd = json.loads(soup.find('script',{"type":"application/ld+json"}).text)
+                    script=soup.find_all('script')
+                    hour=""
+                    for scr in script:
+                        if "var overview_lat" in scr.text:
+                            link1="https://www.wyndhamhotels.com/BWSServices/services/search/property/search?propertyId="+scr.text.split('"')[1]+"&isOverviewNeeded=true&isAmenitiesNeeded=true&channelId=tab&language=en-us"
+                            r1 = requests.get(link1).json();
+                            if "properties" in r1:
+                                for d in r1['properties']:
+                                    hour=d['checkInTime']+'-'+d['checkOutTime']
+                    store = []
+                    store.append("https://www.wyndhamhotels.com")
+                    store.append(mainadd['name'])
+                    store.append(mainadd['address']['streetAddress'])
+                    store.append(city)
+                    store.append(state)
+                    if(country=='US'):
+                        store.append(mainadd['address']['postalCode'].split(' - ')[0].strip().replace(" ","").replace(" ",""))
+                    else:
+                         store.append(mainadd['address']['postalCode'].strip().replace(" ",""))
+                    store.append(country)
+                    store.append("<MISSING>")
+                    try:
+                        store.append(mainadd['telephone'])
+                    except:
+                        store.append("<MISSING>")
+                    store.append("wyndhamhotels")
+                    try:
+                        store.append(mainadd['geo']['latitude'])
+                        store.append(mainadd['geo']['longitude'])
+                    except:
+                        store.append("<MISSING>")
+                        store.append("<MISSING>")
+                    if hour != '':
+                        store.append(hour)
+                    else:
+                        store.append("<MISSING>")
+                    return_main_object.append(store)
+        else:
+            for data in r[i]['properties']:
+                state=data['state']
+                city=data['city']
+                link=data['path']
+                r1 = requests.get(link)
+                soup = BeautifulSoup(r1.text,"lxml")
+                hour=""
+                mainadd = json.loads(soup.find('script',{"type":"application/ld+json"}).text)
+                script=soup.find_all('script')
+                for scr in script:
+                    if "var overview_lat" in scr.text:
+                        link1="https://www.wyndhamhotels.com/BWSServices/services/search/property/search?propertyId="+scr.text.split('"')[1]+"&isOverviewNeeded=true&isAmenitiesNeeded=true&channelId=tab&language=en-us"
+                        r1 = requests.get(link1).json();
+                        if "properties" in r1:
+                            for d in r1['properties']:
+                                hour=d['checkInTime']+'-'+d['checkOutTime']
+                store = []
+                store.append("https://www.wyndhamhotels.com")
+                store.append(mainadd['name'])
+                store.append(mainadd['address']['streetAddress'])
+                store.append(city)
+                store.append(state)
+                store.append(mainadd['address']['postalCode'].strip())
+                store.append(country)
+                store.append("<MISSING>")
+                try:
+                    store.append(mainadd['telephone'])
+                except:
+                    store.append("<MISSING>")
+                store.append("wyndhamhotels")
+                try:
+                    store.append(mainadd['geo']['latitude'])
+                    store.append(mainadd['geo']['longitude'])
+                except:
+                    store.append("<MISSING>")
+                    store.append("<MISSING>")
+                if hour != '':
+                    store.append(hour)
+                else:
+                    store.append("<MISSING>")
+                return_main_object.append(store)
+
+    return return_main_object
+
+def scrape():
+    data = fetch_data()
+    # write_output(data)
+
+scrape()
---
+++ apify/himanshu/storelocator/earthbar_com/scrape.py
@@ -0,0 +1,60 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    base_url = "https://earthbar.com/apps/store-locator"
+    r = requests.get(base_url)
+    soup = BeautifulSoup(r.text ,"lxml")
+    return_main_object = []
+    store_listing = soup.find("div",{"id": "addresses_list"}).find('ul').find_all('li')
+    for lit in store_listing:
+        store_no=lit['onmouseover'].split('(')[-1].split(')')[0].strip()
+        address=lit.find('a').find('span',{"class":"address"}).text.strip()
+        city=lit.find('a').find('span',{"class":"city"}).text.strip()
+        state=lit.find('a').find('span',{"class":"prov_state"}).text.strip()
+        zip=lit.find('a').find('span',{"class":"postal_zip"}).text.strip()
+        country=lit.find('a').find('span',{"class":"country"}).text.strip()
+        try:
+            phone=lit.find('a').find('span',{"class":"phone"}).text.strip()
+        except:
+            phone="<MISSING>"
+        try:
+            store_name=lit.find('a').find('span',{"class":"hours"}).text.split('Store Name:')[-1].strip()
+        except:
+            store_name="<MISSING>"
+        if(country=="United States"):
+            country="US"
+        store = []
+        store.append("https://www.earthbar.com")
+        store.append(store_name)
+        store.append(address)
+        store.append(city)
+        store.append(state)
+        store.append(zip)
+        store.append("US")
+        store.append(store_no)
+        store.append(phone)
+        store.append("Earthbar")
+        store.append("<MISSING>")
+        store.append("<MISSING>")
+        store.append("<MISSING>")
+        return_main_object.append(store)
+    return return_main_object
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
