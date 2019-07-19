de267178bd4713651af8686595b0826f190d174d
Graham Baker <graham.cr.baker@gmail.com>
2019-07-19 18:25:36 -0500

    fixing stuff i broke

---
+++ apify/ggrahambaker/storelocator/jiffytrip_com/scrape.py
@@ -0,0 +1,30 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    # return 2D array to be written to csv
+    return_main_object = []
+    
+
+
+
+    
+    return return_main_object
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
