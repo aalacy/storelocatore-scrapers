7abc2e67cc1be7972c63d4f1ee8cddcc73f68fa9
Noah Yonack <noah.yonack@gmail.com>
2019-07-19 16:53:15 -0700

    remove sgvalidator

--- scripts/sgvalidator/sgvalidator/__init__.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from .sgvalidator import *
-=======
-from .sgvalidator import validate
-<<<<<<< HEAD
->>>>>>> 8a8bd7cbad467e4179c5c9c89d99052ec72fa90d
-=======
->>>>>>> origin/master
->>>>>>> graham_branch
