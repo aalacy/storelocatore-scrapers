33fc2670c7725c79dd0ef74549ebbd86e5940f14
Saleem Abdul Hamid <meelash@gmail.com>
2019-07-21 09:04:32 -0400

    bowlbrunswick passing validation

---
+++ apify/saleem/storelocator/bowlbrunswick_com/scrape.js
@@ -0,0 +1,52 @@
+const Apify = require('apify');
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+
+Apify.main(async () => {
+  const data = await scrape();
+  await Apify.pushData(data);
+});
+
+async function scrape() {
+  // Begin scraper
+  const rootAddress = 'https://www.bowlbrunswick.com/bowlero-location/finder?_format=json';
+  const records = [];
+  await request.get(rootAddress)
+    .then((json) => {
+      return JSON.parse(json).forEach(location => {
+        // Check US vs. Canada. Canada has the province in the city and state is blank
+        if (location.state === "") {
+          // if a Canada location, we need to manipulate a little....
+          [location.state] = location.city.match(/(?<=\,\s).{2}$/) || ["<INACCESSIBLE>"];
+          [location.city] = location.city.match(/.*(?=\,)/);
+          location.country_code = 'CA';
+        }
+        records.push({
+          locator_domain: 'bowlbrunswick.com',
+          location_name: location.name,
+          street_address: location.address1,
+          city: location.city,
+          state: location.state,
+          zip: location.zip,
+          country_code: location.country_code || 'US',
+          store_number: location.centerNumber,
+          phone: location.phone || "<MISSING>",
+          location_type: location.brand,
+          latitude: location.lat,
+          longitude: location.lng,
+          hours_of_operation: (() => {
+            if (!location.hours[0]) {
+              return "<MISSING>";
+            }
+            else {
+              return JSON.stringify(location.hours)
+            }
+          })()
+        });      
+      });
+    })
+  return records;
+    // End scraper
+
+}
