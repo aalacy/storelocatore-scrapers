821e29e9a912981d5b5b7780f28ad56bab771baf
Tenzing Shaw <tenzing@safegraph.com>
2019-07-25 15:02:53 -0700

    update scraper

--- apify/andrew/storelocator/billgrays_com/scrape.py
+++ apify/andrew/storelocator/billgrays_com/scrape.py
@@ -2,12 +2,20 @@ import csv
 import os
 import json
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 MISSING = "<MISSING>"
 INACCESSIBLE = "<INACCESSIBLE>"
 BILL_GRAYS_STORE_LOCATOR_URL = "https://www.billgrays.com/index.cfm?Page=Bill%20Grays%20Locations"
 
-DRIVER = webdriver.Chrome(f'{os.path.dirname(os.path.abspath(__file__))}/chromedriver')
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    return webdriver.Chrome('chromedriver', chrome_options=options)
+
+DRIVER = get_driver()
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
@@ -26,8 +34,7 @@ def fetch_stores():
     stores = script_dict['@graph']
     stores = stores[1:]
     names = [store['name'] for store in stores]
-    import pdb; pdb.set_trace()
-    pass
+    return stores
 
 def fetch_data():
     data = []
