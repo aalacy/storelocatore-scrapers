d039524281cf753635cc207ce8a497b0eac97337
Hank4957! <Himanshu347>
2019-07-28 12:29:59 +0530

    weigels

---
+++ apify/himanshu/storelocator/vivachicken_com/scrape.py
@@ -0,0 +1,51 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    base_url = "https://vivachicken.com"
+    r = requests.get("https://vivachicken.com/wp-admin/admin-ajax.php?action=asl_load_stores&nonce=60058472b8&load_all=1&layout=1").json()
+    return_main_object = []
+    for loc in r:
+        hour=loc['open_hours'].replace('[','').replace(']','').replace('{','').replace('}','')
+        hour=hour.replace('\"','')
+        store=[]
+        store.append(base_url)
+        store.append(loc['title'])
+        store.append(loc['street'])
+        store.append(loc['city'])
+        store.append(loc['state'])
+        store.append(loc['postal_code'])
+        if loc['country']=="United States":
+            store.append("US")
+        else:
+            store.append(loc['country'])
+        store.append(loc['id'])
+        store.append(loc['phone'])
+        store.append("vivachicken")
+        store.append(loc['lat'])
+        store.append(loc['lng'])
+        if hour.strip():
+            store.append(hour.strip())
+        else:
+            store.append("<MISSING>")
+        return_main_object.append(store)
+    return return_main_object
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
---
+++ apify/himanshu/storelocator/weigels_com/scrape.py
@@ -0,0 +1,57 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name","row_street", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    base_url = "https://weigels.com"
+    r = requests.get(base_url+"/location/")
+    soup=BeautifulSoup(r.text,'lxml')
+    return_main_object = []
+    main=soup.find('div',{"class":"location-list"}).find_all('div',{'class':"location"})
+    for loc in main:
+        lat=loc['data-lat']
+        zip="<MISSING>"
+        lng=loc['data-lng']
+        name=loc.find('div',{"class":"locationInfo"}).find('h4').text.strip()
+        storeno=loc.find('div',{"class":"locationInfo"}).find('h4').text.strip().split(' ')[-1]
+        hour=' '.join(list(loc.find('div',{"class":"locationInfo"}).find('div',{'class':"locationHours"}).stripped_strings)).replace('Hours:','').strip()
+        madd=loc.find('div',{"class":"locationInfo"}).find('div',{'class':"locationAddress"}).text.strip()
+        phone=loc.find('div',{"class":"locationInfo"}).find('div',{'class':"locationPhone"}).text.strip()
+        store=[]
+        store.append(base_url)
+        store.append(name)
+        store.append(madd)
+        store.append("<INACCESSIBLE>")
+        store.append("<INACCESSIBLE>")
+        store.append("<INACCESSIBLE>")
+        store.append("<INACCESSIBLE>")
+        store.append("US")
+        store.append(storeno)
+        store.append(phone)
+        store.append("weigels")
+        store.append(lat)
+        store.append(lng)
+        if hour.strip():
+            store.append(hour.strip())
+        else:
+            store.append("<MISSING>")
+        return_main_object.append(store)
+    return return_main_object
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
