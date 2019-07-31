c836e81ecb5048cac67214afdec69e673a9b38c8
Himanshu347 <hdhameliya@dhameliyaconsultingservice.com>
2019-07-31 12:36:49 +0530

    accesshealthdental_com

--- apify/himanshu/storelocator/accesshealthdental_com/scrape.py
+++ apify/himanshu/storelocator/accesshealthdental_com/scrape.py
@@ -3,6 +3,9 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 import re
+import json,urllib
+import time
+import lxml
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
@@ -15,66 +18,92 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
-    base_url = "http://accesshealthdental.com/locations.php"
-    r = requests.get(base_url)
+    base_url = "https://www.accesshealthdental.com/locations/"
+
+    r = requests.get(base_url )
     soup = BeautifulSoup(r.text,"lxml")
-    k = soup.find("table",{"width":"620",'cellspacing':"0",'cellspacing':"0",'border':"0"}).td
+    return_main_object=[]
     store_names = []
-    for name in k.find_all('h3'): 
-        store_names.append(name.text.strip(' \n\t'))
-    store_details = []
-    for i in k.find_all('p'):
-        lat_lgt=''
-        if(i.a != None):
-            a = (i.find_all('a') [len(i.find_all('a')) - 1])
-            k=a['href'].split('ll=')
-            if len(k)==2 or len(k)==3:
-                lat_lgt= lat_lgt + (k[1].split('&')[0])
-            k=a['href'].split('@')
-            if len(k)==2:
-                lat_lgt = lat_lgt+(k[1].split('m')[0])
-            if len(k)==1 and len(a['href'].split('ll='))!=2 and len(a['href'].split('ll='))!=3  :
-                lat_lgt = lat_lgt+"<MISSING>,<MISSING>"
+    store_detail =[]
+    latitude =[]
+    longitude =[]
+
+    k = soup.find("div",{"class":"su-tabs su-tabs-style-default"})
+    name1 =k.find_all("div",{"class":"su-tabs-pane su-clearfix"})
+    for  i in name1:
+        store_names.append(i.h2.text) 
+
+    p = k.find_all('p')
+    lat_log1 = soup.find_all("div",{"class":"video-container"})
+
+    for i in lat_log1:
+        if len(i.iframe.get('src').split('3d')) != 1:
+            latitude.append(i.iframe.get('src').split('3d')[1].split('!2')[0])
+            longitude.append(i.iframe.get('src').split("!2d")[1].split('!3d')[0])
+
+
+
+    for index,i in  enumerate(p):
+        temp_var =[]
+        data = list(i.stripped_strings)
+        if len(data) ==7:
+            street_address = data[5]
+            city_zip = data[6]
+            city = city_zip.split(',')
+            if len(city) ==1:
+                state = city[0].split( )[2]
+                zipcode =  city[0].split( )[3]
+                city = city[0].split( )[0] + ' ' +city[0].split( )[1]        
+            else:
+                state = city[1].split( )[0]
+                zipcode  = city[1].split( )[1]
+                city = city[0]
+
+            phone = data[1]
+
+            temp_var.append(street_address)
+            temp_var.append(city)
+            temp_var.append(state)
+            temp_var.append(zipcode)
+            temp_var.append("US")
+            temp_var.append("<MISSING>")
+            temp_var.append(phone)
+            temp_var.append("accesshealthdental")
+            store_detail.append(temp_var)
+        
         else:
-            lat_lgt = lat_lgt + '<MISSING>,<MISSING>'
-        lat_lgt1=lat_lgt.split(',')
-        temp_var = []
-        temp = list(i.stripped_strings)
-        ds =temp[1].split(',',2)
-        if temp[1].count(",") == 2:
-            store_number = temp[1].split(",")[0]
-            temp[0] = temp[0] + store_number
-            temp[1] = ','.join(temp[1].split(",")[1:])
-        street_address=temp[0]
-        city_zip = temp[1].split(',')
-        city = city_zip[0]
-        state = city_zip[1].split()[0]
-        zipcode  = city_zip[1].split()[1]
-        phone = temp[2].split('p:')[1].replace(" ", "")
-        latitude = lat_lgt1[0]
-        longitude = lat_lgt1[1]
-        temp_var.append(street_address)
-        temp_var.append(city)
-        temp_var.append(state)
-        temp_var.append(zipcode)
-        temp_var.append("US")
-        temp_var.append("<MISSING>")
-        temp_var.append(phone)
-        temp_var.append("<MISSING>")
-        temp_var.append(latitude)
-        temp_var.append(longitude)
-        temp_var.append("<MISSING>")
-        store_details.append(temp_var)
-    main_object = []
+            if len(data) != 4:
+                street_address = data[4]
+                city_zip = data[5].split(',')
+                city =  data[5].split(',')[0]
+                state = data[5].split(',')[1].split( )[0]
+                zipcode =  data[5].split(',')[1].split( )[1]
+                phone = data[1]
 
-    for i in range(len(store_names)):
-        store= list()
-        store.append("http://accesshealthdental.com")
-        store.append(store_names[i])
-        store.extend(store_details[i])
-        main_object.append(store)
+                temp_var.append(street_address)
+                temp_var.append(city)
+                temp_var.append(state)
+                temp_var.append(zipcode)
+                temp_var.append("US")
+                temp_var.append("<MISSING>")
+                temp_var.append(phone)
+                temp_var.append("accesshealthdental")
+                store_detail.append(temp_var)
 
-    return main_object
+
+    for i in  range(len(store_names[2:])):
+        store = list()
+        store.append("https://www.accesshealthdental.com")
+        store.append(store_names[i+2])
+        store.extend(store_detail[i])
+        store.append(latitude[i])
+        store.append(longitude[i])
+        store.append("<MISSING>")
+        return_main_object.append(store)
+    return return_main_object
+    
+
+
 
 def scrape():
     data = fetch_data()
