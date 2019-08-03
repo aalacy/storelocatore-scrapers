c06014e5455b40e76feeea2f24d1a7dfe1c2f881
Daniel Pastushchak <danikpastushchak90@gmail.com>
2019-08-04 00:09:40 +0300

    enhance

--- apify/kranzer/storelocator/gertrudehawkchocolates_com/scrape.py
+++ apify/kranzer/storelocator/gertrudehawkchocolates_com/scrape.py
@@ -1,82 +1,55 @@
 import csv
 import re
-
+from lxml import html
 import requests
-from bs4 import BeautifulSoup
 from w3lib.html import remove_tags
+import base
 
-def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+class Scrape(base.Spider):
 
-        # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
-        # Body
-        for row in data:
-            writer.writerow(row)
-
-def fetch_data():
-    base_url = "https://www.gertrudehawkchocolates.com/find-a-store"
-    cities = list(set([x.get('value') for x in BeautifulSoup(requests.get(base_url).text, 'html.parser').find("select", {"name":"city"}).findAll('option') if x]))
-    data = []
-    for city in cities:
-        city_url = base_url+"?state=&city={}&zip=&distance=5".format(city.replace(' ', '+'))
-        soup = BeautifulSoup(requests.get(city_url).text, 'html.parser')
-        index = 0
-        for location in soup.findAll("div", {"class": "location-details"}):
-            l_data = [remove_tags(s.replace('\r\n','')).rstrip().strip() for s in str(location.p).split('<br/>')]
-            l_ = []
-            l_.append(city_url)
-            if not l_data[0][0].isdigit():
-                l_.append(l_data[0])
+    def crawl(self):
+        base_url = "https://www.gertrudehawkchocolates.com/find-a-store"
+        cities = list(set([x for x in html.fromstring(requests.get(base_url).text).xpath('//select[@name="city"]/option/@value') if x]))
+        for city in cities:
+            city_url = base_url+"?state=&city={}&zip=&distance=5".format(city.replace(' ', '+'))
+            locations = html.fromstring(requests.get(city_url).text)
+            index = 0
+            for location in locations.xpath('//div[@class="location-details"]'):
+                l_data = [remove_tags(s.replace('\r\n','')).rstrip().strip() for s in location.xpath('./p/text()')]
+                item = base.Item(location)
+                item.add_value('locator_domain', city_url)
+                if not l_data[0][0].isdigit():
+                    item.add_value('location_name', l_data[0])
+                    del l_data[0]
+                    item.add_value('street_address', l_data[0])
+                    del l_data[0]
+                else:
+                    item.add_value('street_address', l_data[0])
+                    del l_data[0]
+                if not l_data[0].startswith(city):
+                    del l_data[0]
+                d = re.match(r'(?P<city>.+?)\s,(?P<state>[A-Z][A-Z])(\s(?P<zip>.+))?', l_data[0]).groupdict()
                 del l_data[0]
-                l_.append(l_data[0])
-                del l_data[0]
-            else:
-                l_.append("<MISSING>")
-                l_.append(l_data[0])
-                del l_data[0]
-            if not l_data[0].startswith(city):
-                del l_data[0]
-            d = re.match(r'(?P<city>.+?)\s,(?P<state>[A-Z][A-Z])(\s(?P<zip>.+))?', l_data[0]).groupdict()
-            del l_data[0]
-            if d.get('city'):
-                l_.append(d['city'])
-            else:
-                l_.append("<MISSING>")
-            if d.get('state'):
-                l_.append(d['state'])
-            else:
-                l_.append("<MISSING>")
-            if d.get('zip'):
-                l_.append(d['zip'])
-            else:
-                l_.append("<MISSING>")
-            l_.append('US')
-            l_.append('<MISSING>')
-            if '-' in l_data[0]:
-                l_.append(l_data[0])
-            else:
-                l_.append("<MISSING>")
-            l_.append("<MISSING>")
-            script = ''.join([s.text for s in soup.findAll('script')]).replace('\r','').replace('\n','').replace('\t','')
-            try:
-                lat, lng = re.findall(r'newLatLng = new google\.maps\.LatLng\((?P<lat>.+?),(?P<lng>.+?)\);', script)[index]
-                l_.append(lat)
-                l_.append(lng)
-            except:
-                l_.append("<MISSING>")
-                l_.append("<MISSING>")
-            l_.append("<MISSING>")
-            data.append(l_)
-            index+=1
-    return data
+                if d.get('city'):
+                    item.add_value('city', d['city'])
+                if d.get('state'):
+                    item.add_value('state', d['state'])
+                if d.get('zip'):
+                    item.add_value('zip', d['zip'])
+                item.add_value('country_code', 'US')
+                if '-' in l_data[0]:
+                    item.add_value('phone', l_data[0])
+                script = ''.join(locations.xpath('//script/text()')).replace('\r','').replace('\n','').replace('\t','')
+                try:
+                    lat, lng = re.findall(r'newLatLng = new google\.maps\.LatLng\((?P<lat>.+?),(?P<lng>.+?)\);', script)[index]
+                    item.add_value('latitude', lat)
+                    item.add_value('longitude', lng)
+                except:
+                    pass
+                index+=1
+                yield item
 
 
-
-
-def scrape():
-    data = fetch_data()
-    write_output(data)
-
-scrape()
+if __name__ == '__main__':
+    s = Scrape()
+    s.run()
