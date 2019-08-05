2558344157a95c9b434279ae61a41d0fd753e189
Graham Baker <graham.cr.baker@gmail.com>
2019-08-05 09:45:39 -0500

    little fixes

--- apify/ggrahambaker/storelocator/pioneersupermarkets_com/scrape.py
+++ apify/ggrahambaker/storelocator/pioneersupermarkets_com/scrape.py
@@ -63,8 +63,8 @@ def fetch_data():
         country_code = 'US'
         store_number = '<MISSING>'
         location_type = '<MISSING>'
-        lat = '<MISSING>'
-        longit = '<MISSING>'
+        lat = '<INACCESSIBLE>'
+        longit = '<INACCESSIBLE>'
         
         store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
                      store_number, phone_number, location_type, lat, longit, hours ]
