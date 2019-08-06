40d7d197f24593bf69fc2baa344a4ec764dc3589
coseasonruby <coseasonruby@gmail.com>
2019-08-07 02:23:42 +0700

    validate.py

--- apify/coralisland/storelocator/freebirds_com/scrape.py
+++ apify/coralisland/storelocator/freebirds_com/scrape.py
@@ -12,7 +12,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('../chromedriver', chrome_options=chrome_options)
+driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
 
 
 base_url = 'https://freebirds.com'
