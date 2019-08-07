317231959e2e5901c43fa856724d14fd6e54ed5d
Andrew You <andrew@simplefractal.com>
2019-08-07 17:05:05 -0400

    Completes steinmart

--- apify/andrew/storelocator/steinmart_com/scrape.py
+++ apify/andrew/storelocator/steinmart_com/scrape.py
@@ -1,19 +1,8 @@
 import csv
 import re
 import json
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-
-options = Options() 
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/bin/chromedriver', options=options)
+import requests
+from bs4 import BeautifulSoup
 
 BASE_URL = 'https://www.steinmart.com'
 MISSING = '<MISSING>'
@@ -39,19 +28,19 @@ def fetch_street_address(address):
 
 def fetch_data():
     data = []
-    store_urls = []
-    driver.get('https://www.steinmart.com/store-locator/all-stores.do')
+    res = requests.get('https://www.steinmart.com/store-locator/all-stores.do')
+    soup = BeautifulSoup(res.content, 'html.parser')
     store_urls = [
-        a_tag.get_attribute('href')
-        for a_tag in driver.find_elements_by_css_selector('div.ml-storelocator-item-wrapper a')
+        BASE_URL + a_tag['href']
+        for a_tag in soup.select('div.ml-storelocator-item-wrapper a')
     ]
     for idx, store_url in enumerate(store_urls):
-        print(f'{idx} of {len(store_urls)}')
-        driver.get(store_url)
+        res = requests.get(store_url)
+        soup = BeautifulSoup(res.content, 'html.parser')
         script = [
-            script.get_attribute('innerText')
-            for script in driver.find_elements_by_css_selector('script[type="text/javascript"]')
-            if 'latitude' in script.get_attribute('innerText')
+            script.text
+            for script in soup.select('script[type="text/javascript"]')[-11:]
+            if 'latitude' in script.text
         ][0]
         script = json.loads(re.findall(r'{.*}', script)[0])
         store_data = script['results'][0]
@@ -59,24 +48,29 @@ def fetch_data():
         street_address = fetch_street_address(address)
         hours_of_operation = " | ".join([
             tr.text.strip()
-            for tr in driver.find_elements_by_css_selector('div.ml-storelocator-hours tr')
+            for tr in soup.select('div.ml-storelocator-hours tr')
         ])
+        zipcode = address.get('postalCode')
+        if len(zipcode) == 4:
+            zipcode = '0' + zipcode
+        phone = address.get('phone')
+        if not sum(c.isdigit() for c in phone) == 10:
+            continue
         data.append([
             BASE_URL,
             store_data.get('name'),
             street_address,
             address.get('city'),
             address.get('stateCode'),
-            address.get('postalCode'),
+            zipcode,
             address.get('countryName'),
             store_data.get('id'),
-            address.get('phone'),
+            phone,
             MISSING,
             store_data.get('location', {}).get('latitude'),
             store_data.get('location', {}).get('longitude'),
             hours_of_operation
         ])
-    driver.quit()
     return data
 
 def scrape():
