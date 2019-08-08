c26efa18cbd3cbad2d4d5d4fd7a32d7612619d18
Daniel Pastushchak <danikpastushchak90@gmail.com>
2019-08-08 20:38:53 +0300

    missed some stuff

--- apify/kranzer/storelocator/dontdrivedirty_com/scrape.py
+++ apify/kranzer/storelocator/dontdrivedirty_com/scrape.py
@@ -1,30 +1,41 @@
+import time
 
 import base
+from lxml import html
 import requests, json
 from urllib.parse import urljoin
 
 class Scrape(base.Spider):
 
     def crawl(self):
-        base_url = "https://www.brookdale.com/bin/brookdale/community-search?care_type_category=resident&loc=&finrpt=&state={}"
-        for state in base.us_states_codes:
-            json_body = requests.get(base_url.format(state)).json()
-            for result in json_body:
-                href = result.get('website')
-                if href:
-                    i = base.Item(result)
-                    i.add_value('locator_domain', href)
-                    i.add_value('location_name', result.get('name','').strip())
-                    i.add_value('street_address', result.get('address1','').strip())
-                    i.add_value('store_number', result.get('business_unit_number','').strip())
-                    i.add_value('city', result.get('city','').strip())
-                    i.add_value('state', result.get('state','').strip())
-                    i.add_value('zip', result.get('zip_postal_code','').strip(), lambda x: x[:5]+'-'+x[5:] if len(x) > 5 else x)
-                    i.add_value('phone', result.get('phone_main','').strip())
-                    i.add_value('country_code', result.get('country_code'))
-                    i.add_value('latitude', result.get('latitude',''))
-                    i.add_value('longitude', result.get('longitude',''))
-                    yield i
+
+        body = "action=qq_ajax_map_first_run"
+        base_url = "https://www.dontdrivedirty.com/wp-admin/admin-ajax.php"
+        r = requests.post(base_url, data=body, headers={"origin":"https://www.dontdrivedirty.com",
+                                                        "pragma":"no-cache",
+                                                        "referer":"https://www.dontdrivedirty.com/",
+                                                        "user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+                                                        "x-requested-with":"XMLHttpRequest",
+                                                        "accept":"*/*",
+                                                        "accept-encoding":"gzip, deflate, br",
+                                                        "content-type":"application/x-www-form-urlencoded; charset=UTF-8"})
+
+        for result in html.fromstring(r.text).xpath('//marker'):
+            i = base.Item(result)
+            if result.xpath('./@location_type')[0] == "1":
+                i.add_value('locator_domain', "https://www.dontdrivedirty.com/locationsandpricing/")
+                i.add_xpath('location_name', './@name', base.get_first)
+                i.add_xpath('street_address', './@address', base.get_first)
+                i.add_xpath('store_number', './@id', base.get_first)
+                i.add_xpath('city', './@town', base.get_first)
+                i.add_xpath('state', './@state', base.get_first)
+                i.add_xpath('zip', './@zipcode', base.get_first)
+                i.add_xpath('phone', './@phone', base.get_first)
+                i.add_value('country_code', 'US')
+                i.add_xpath('latitude', './@lat', base.get_first)
+                i.add_xpath('longitude', './@lng', base.get_first)
+                i.add_xpath('hours_of_operation', './@open | ./@close', lambda x: ' - '.join(x))
+                yield i
 
 if __name__ == '__main__':
     s = Scrape()
