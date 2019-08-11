6ce5bf2a7c0444854f40af026975e9fb128b339b
Graham Baker <graham.cr.baker@gmail.com>
2019-08-11 17:16:44 -0500

    last 10

---
+++ apify/ggrahambaker/storelocator/brewsters_ca/scrape.py
@@ -0,0 +1,112 @@
+import csv
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    return webdriver.Chrome('chromedriver', options=options)
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def addy_ext(addy):
+    address = addy.split(',')
+    city = address[0]
+    state_zip = address[1].strip().split(' ')
+    state = state_zip[0]
+    zip_code = state_zip[1] + ' ' + state_zip[2]
+    return city, state, zip_code
+
+
+
+
+def fetch_data():
+    locator_domain = 'https://brewsters.ca/'
+    ext = 'find-us/'
+
+    driver = get_driver()
+    driver.get(locator_domain + ext)
+
+    lis = driver.find_elements_by_css_selector('a.icon-arrow-right')
+    print(len(lis))
+    link_list = []
+    for li in lis:
+        if len(li.get_attribute('href')) > 30:
+            print(li.get_attribute('href'))
+            link_list.append(li.get_attribute('href'))
+
+    all_store_data = []
+    for link in link_list:
+        driver.implicitly_wait(10)
+        driver.get(link)
+        main = driver.find_element_by_css_selector('main#main')
+        # main.find_element_by_css_selector
+        print()
+        print(main.text.split('\n'))
+        content = main.text.split('\n')
+        print(len(content))
+        if len(content) == 17:
+            location_name = content[1]
+            street_address = content[4]
+
+            print(content[4])
+            print(content[5])
+            city, state, zip_code = addy_ext(content[5])
+            phone_number = content[6].replace('Phone:', '').replace('BREW', '2739').replace('HOPS', '4677')[:-7].strip()
+            print(phone_number)
+            print(content[9])
+            hours = ''
+            for h in content[9:-1]:
+                hours += h + ' '
+            hours = hours.strip()
+            print(hours)
+            print()
+        else:
+            location_name = content[1]
+            street_address = content[5]
+
+            print(content[5])
+            print(content[6])
+            city, state, zip_code = addy_ext(content[6])
+            phone_number = content[7].replace('Phone:', '').replace('BREW', '2739').replace('HOPS', '4677')[:-7].strip()
+            print(phone_number)
+            hours = ''
+            for h in content[10:-1]:
+                hours += h + ' '
+            hours = hours.strip()
+            print(hours)
+
+        location_name = '<MISSING>'
+        country_code = 'CA'
+        store_number = '<MISSING>'
+        location_type = '<MISSING>'
+        lat = '<MISSING>'
+        longit = '<MISSING>'
+
+        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
+                      store_number, phone_number, location_type, lat, longit, hours]
+        all_store_data.append(store_data)
+
+        print()
+
+    driver.quit()
+    return all_store_data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
---
+++ apify/ggrahambaker/storelocator/matchboxrestaurants_com/scrape.py
@@ -0,0 +1,106 @@
+import csv
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    return webdriver.Chrome('chromedriver', options=options)
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def addy_ext(addy):
+    address = addy.split(',')
+    city = address[0]
+    state_zip = address[1].strip().split(' ')
+    state = state_zip[0]
+    zip_code = state_zip[1]
+    return city, state, zip_code
+
+
+
+
+def fetch_data():
+    locator_domain = 'https://www.matchboxrestaurants.com/'
+
+    driver = get_driver()
+    driver.get(locator_domain)
+
+    element = driver.find_element_by_css_selector('a.sqs-popup-overlay-close')
+    driver.execute_script("arguments[0].click();", element)
+
+    element_to_hover_over = driver.find_element_by_css_selector("a.Header-nav-folder-title")
+
+    hover = ActionChains(driver).move_to_element(element_to_hover_over)
+    hover.perform()
+
+    hrefs = driver.find_element_by_css_selector('span.Header-nav-folder').find_elements_by_css_selector('a')
+    print(len(hrefs))
+    link_list = []
+    for href in hrefs:
+        link_list.append(href.get_attribute('href'))
+        print(href.get_attribute('href'))
+
+    all_store_data = []
+    for link in link_list:
+        driver.implicitly_wait(10)
+        driver.get(link)
+        print()
+
+        main = driver.find_element_by_css_selector('section.Main-content')
+        content = main.text.split('\n')[:17]
+        print(content)
+        street_address = content[1]
+        city, state, zip_code = addy_ext(content[2])
+        phone_number = content[3].replace('call', '').strip()
+        hours = ''
+        for h in content[11:]:
+            hours += h + ' '
+
+        # print(main.find_elements_by_css_selector('a')[1].get_attribute('href'))
+        if 'ellsworth' in street_address or 'potomac' in street_address:
+            href = main.find_elements_by_css_selector('a')[0].get_attribute('href')
+            print(href)
+        else:
+            href = main.find_elements_by_css_selector('a')[1].get_attribute('href')
+
+        start_idx = href.find('/@')
+        end_idx = href.find('z/data')
+        print(href[start_idx + 2: end_idx].split(','))
+        coords = href[start_idx + 2: end_idx].split(',')
+        lat = coords[0]
+        longit = coords[1]
+        print(hours)
+
+        location_name = '<MISSING>'
+        country_code = 'US'
+        store_number = '<MISSING>'
+        location_type = '<MISSING>'
+
+        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
+                      store_number, phone_number, location_type, lat, longit, hours]
+        all_store_data.append(store_data)
+        print()
+
+    driver.quit()
+    return all_store_data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
