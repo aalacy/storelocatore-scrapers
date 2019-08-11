95217456c9556d4cd5630cbb4a37647dc535c55a
Saleem Abdul Hamid <meelash@gmail.com>
2019-08-11 12:23:33 -0400

    barneysbeanery.com passing validation

--- apify/saleem/storelocator/barneysbeanery_com/scrape.js
+++ apify/saleem/storelocator/barneysbeanery_com/scrape.js
@@ -1,8 +1,16 @@
 const Apify = require('apify');
+const request = require('request-promise');
+const cheerio = require('cheerio');
 
 (async () => {
+  const rootAddress = new URL('https://barneysbeanery.com/locations/');
+  const rootPage = await request.get(rootAddress.href);
+  const $ = cheerio.load(rootPage)
+  const sources = $('.location .entry-thumbnail a').map((_, elem) => {
+    return { url: $(elem).attr('href')}
+  }).get();
   const requestList = new Apify.RequestList({
-    sources: [{ url: 'http://safegraph.com' }],
+    sources
   });
   await requestList.initialize();
 
@@ -10,22 +18,26 @@ const Apify = require('apify');
     requestList,
     handlePageFunction: async ({ request, response, html, $ }) => {
 
-			// Begin scraper
+      // Begin scraper
+      const dataArea = $('.entry-content .content-area');
+      const {street_address, city, state, zip, phone} = $('p', dataArea).first().text().trim().match(
+        /^(?<street_address>[\d\D]*)\n(?<city>.*),?\s(?<state>[A-Z]{2})\s(?<zip>[\d-]*)\n(?<phone>[\d\(\)-\s]*)$/
+      ).groups;
 
 			const poi = {
-        locator_domain: 'safegraph.com',
-        location_name: $('title').text(),
-        street_address: '1543 mission st',
-        city: 'san francisco',
-        state: 'CA',
-        zip: '94107',
+        locator_domain: 'barneysbeanery.com',
+        location_name: $('title').text().trim(),
+        street_address,
+        city,
+        state,
+        zip,
         country_code: 'US',
 				store_number: '<MISSING>',
-				phone: '<MISSING>',
+				phone,
 				location_type: '<MISSING>',
-        latitude: 37.773500,
-        longitude: -122.417774,
-				hours_of_operation: '<MISSING>'
+        latitude: '<MISSING>',
+        longitude: '<MISSING>',
+				hours_of_operation: $('p', dataArea).eq(1).text().trim()+' '+$('p', dataArea).eq(2).text().trim()
 			};
 
 			await Apify.pushData([poi]);
