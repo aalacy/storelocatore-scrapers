00cb8b32c3a873ecd7d316631175380852b3fbb3
nsxdarin <nsxdarin@gmail.com>
2019-08-16 18:40:25 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/borregodeoro_com/scrape.py
+++ apify/nsxdarin/storelocator/borregodeoro_com/scrape.py
@@ -24,10 +24,17 @@ def fetch_data():
     country = 'US'
     lat = '<MISSING>'
     lng = '<MISSING>'
+    hours = '<MISSING>'
     Found = True
     num = 0
     lines = r.iter_lines()
+    maps = []
+    locs = []
     for line in lines:
+        if '<iframe src="https://www.google.com/maps/' in line:
+            maplat = line.split('!2d')[1].split('!')[0]
+            maplng = line.split('!3d')[1].split('!')[0]
+            maps.append(maplat + ',' + maplng)
         if 'Contact</p></div></a>' in line:
             Found = True
         if Found and '<div class="colelem"' in line:
@@ -43,8 +50,17 @@ def fetch_data():
             zc = g.split(',')[1].split('<')[0].rsplit(' ',1)[1]
             next(lines)
             phone = next(lines).split('>')[1].split('<')[0].replace('&#45;','-')
-            hours = '<MISSING>'
-            yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+            locs.append(name + '|' + add + '|' + city + '|' + state + '|' + zc + '|' + phone)
+    for x in range(0, len(locs)):
+        name = locs[x].split('|')[0]
+        add = locs[x].split('|')[1]
+        city = locs[x].split('|')[2]
+        state = locs[x].split('|')[3]
+        zc = locs[x].split('|')[4]
+        phone = locs[x].split('|')[5]
+        lat = maps[x].split(',')[0]
+        lng = maps[x].split(',')[1]
+        yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
