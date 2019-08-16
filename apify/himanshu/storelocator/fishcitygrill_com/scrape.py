7964aee7d5c8535fc0134dfe15ba7733d2ec9ea4
Himanshu347 <Hank4957!>
2019-08-16 19:51:57 +0530

    docker run & push

---
+++ apify/himanshu/storelocator/fishcitygrill_com/scrape.py
@@ -0,0 +1,104 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import re
+import io
+import json
+import time
+
+def write_output(data):
+    with open('fishcitygrill.csv', mode='w', encoding="utf-8") as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
+    }
+    headers1 = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'referrer': 'https://google.com',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Pragma': 'no-cache',
+    }
+    base_url = "https://fishcitygrill.com/locations/"
+    r = requests.get(base_url, headers=headers, timeout=5)
+    soup = BeautifulSoup(r.text, "lxml")
+    return_main_object = []
+    address = []
+    exists = soup.find('div', {'class', 'theme-content'})
+    if exists:
+        for data in exists.findAll('a'):
+            print(data.get('href'))
+            time.sleep(10)
+            detail_page_req = requests.get(data.get('href'), headers=headers1, timeout=5)
+            detail_page_soup = BeautifulSoup(detail_page_req.text, "lxml")
+            if detail_page_soup.find('div', {'class', 'vc_col-sm-6'}).find('h2'):
+                location_name = detail_page_soup.find('div', {'class', 'vc_col-sm-6'}).find('h2').get_text().strip()
+            else:
+                location_name = "<MISSING>"
+            hours_of_operation_val = detail_page_soup.select('#text-block-3')[0].get_text()
+            hours_of_operation = ' '.join(hours_of_operation_val.split(' ')[1:])
+            full_address = detail_page_soup.select('#text-block-4')[0].find('p').get_text().replace('\n', ' ').strip()
+            check_if = full_address.split(' ')
+            if len(check_if) > 3:
+                full_address = detail_page_soup.select('#text-block-4')[0].find('p').get_text().replace('\n',' ').strip()
+                if "(" in full_address:
+                    street_address = ' '.join(full_address.split(' ')[1:][:-5])
+                    city = full_address.split(' ')[1:][-5][:-1]
+                    state = full_address.split(' ')[1:][-4]
+                    zip = full_address.split(' ')[1:][-3]
+                    phone = full_address.split(' ')[1:][-2] + " " + full_address.split(' ')[1:][-1]
+                else:
+                    street_address = ' '.join(full_address.split(' ')[1:][:-4])
+                    city = full_address.split(' ')[1:][-4][:-1]
+                    state = full_address.split(' ')[1:][-3]
+                    zip = full_address.split(' ')[1:][-2]
+                    phone = full_address.split(' ')[1:][-1]
+            else:
+                full_address = detail_page_soup.select('#text-block-4')[0].find('p').find_next('p').get_text().replace('\n',' ').strip()
+                if "(" in full_address:
+                    street_address = ' '.join(full_address.split(' ')[1:][:-5])
+                    city = full_address.split(' ')[1:][-5][:-1]
+                    state = full_address.split(' ')[1:][-4]
+                    zip = full_address.split(' ')[1:][-3]
+                    phone = full_address.split(' ')[1:][-2] + " " + full_address.split(' ')[1:][-1]
+                else:
+                    street_address = ' '.join(full_address.split(' ')[1:][:-4])
+                    city = full_address.split(' ')[1:][-4][:-1]
+                    state = full_address.split(' ')[1:][-3]
+                    zip = full_address.split(' ')[1:][-2]
+                    phone = full_address.split(' ')[1:][-1]
+            store = []
+            store.append(data.get('href'))
+            store.append(location_name)
+            store.append(street_address)
+            store.append(city)
+            store.append(state)
+            store.append(zip)
+            store.append("US")
+            store.append("<MISSING>")
+            store.append(phone)
+            store.append("Fish City Grill")
+            store.append("<INACCESSIBLE>")
+            store.append("<INACCESSIBLE>")
+            store.append(hours_of_operation)
+            return_main_object.append(store)
+        return return_main_object
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+scrape()
