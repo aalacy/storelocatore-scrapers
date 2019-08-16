b9cd741ed0f812e175d0dedfb3fcd3efc48bb30c
Tayyaba Qamar <52938783+TayyabaQ@users.noreply.github.com>
2019-08-17 01:06:09 +0500

    Update scrape.py

--- apify/tayyabaq/storelocator/sturdevants_com/scrape.py
+++ apify/tayyabaq/storelocator/sturdevants_com/scrape.py
@@ -17,6 +17,7 @@ def write_output(data):
 def get_driver():
     options = Options() 
     options.add_argument('--window-size=1920,1080')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     return webdriver.Chrome('chromedriver', chrome_options=options)
