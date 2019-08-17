afe79d779f359ce6752ee141d286015f5033c9f9
nsxdarin <nsxdarin@gmail.com>
2019-08-16 23:04:52 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/metro-dentalcare_com/scrape.py
+++ apify/nsxdarin/storelocator/metro-dentalcare_com/scrape.py
@@ -1,6 +1,7 @@
 import csv
 import urllib2
 import requests
+import json
 
 session = requests.Session()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
@@ -27,41 +28,38 @@ def fetch_data():
     
     r = session.post(url, headers=headers, data=payload)
     website = 'metro-dentalcare.com'
-    store = '<MISSING>'
     typ = 'Office'
     country = 'US'
-    for line in r.iter_lines():
-        if '{"id":"' in line:
-            items = line.split('{"id":"')
-            for item in items:
-                if '"code":"' in item:
-                    phone = item.split('"phone_primary":"')[1].split('"')[0]
-                    name = item.split('"name":"')[1].split('"')[0]
-                    add = item.split('"address1":"')[1].split('"')[0]
-                    add2 = item.split('"address2":"')[1].split('"')[0]
-                    if add2 != '':
-                        add = add + ' ' + add2
-                    city = item.split('"city":"')[1].split('"')[0]
-                    state = item.split('"state":"')[1].split('"')[0]
-                    zc = item.split('"postal_code":"')[1].split('"')[0]
-                    lat = item.split('"lat":"')[1].split('"')[0]
-                    lng = item.split('"lng":"')[1].split('"')[0]
-                    hours = 'Mon: ' + item.split('"Monday":["')[1].split('"]')[0].replace('","','-')
-                    hours = hours + '; ' + 'Tue: ' + item.split('"Tuesday":["')[1].split('"]')[0].replace('","','-')
-                    hours = hours + '; ' + 'Wed: ' + item.split('"Wednesday":["')[1].split('"]')[0].replace('","','-')
-                    hours = hours + '; ' + 'Thu: ' + item.split('"Thursday":["')[1].split('"]')[0].replace('","','-')
-                    hours = hours + '; ' + 'Fri: ' + item.split('"Friday":["')[1].split('"]')[0].replace('","','-')
-                    hours = hours + '; ' + 'Sat: ' + item.split('"Saturday":["')[1].split('"]')[0].replace('","','-')
-                    hours = hours + '; ' + 'Sun: ' + item.split('"Sunday":["')[1].split('"]')[0].replace('","','-')
-                    hours = hours.replace(': -',': Closed')
-                    phone = phone.replace('\\r\\n',';').replace(';;',';').replace(';;',';').replace(';;',';').replace(';;',';').replace(';;',';')
-                    phone = phone.replace('  PHONE','')
-                    if ';' in phone:
-                        try:
-                            phone = phone.split(';')[0].strip().replace(') ',')').rsplit(' ',1)[1].replace(')',') ').strip()
-                        except:
-                            phone = phone.split(';')[0].strip()
-                    yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+    array = json.loads(r.content)
+    for item in array['results']:
+        store = item['id']
+        phone = item['phone_primary']
+        name = item['name']
+        add = item['address1']
+        add2 = item['address2']
+        if add2 != '':
+            add = add + ' ' + add2
+        city = item['city']
+        state = item['state']
+        zc = item['postal_code']
+        lat = item['lat']
+        lng = item['lng']
+        hours = 'Mon: ' + json.dumps(item['hours']['Monday'])
+        hours = hours + '; ' + 'Tue: ' + json.dumps(item['hours']['Tuesday'])
+        hours = hours + '; ' + 'Wed: ' + json.dumps(item['hours']['Wednesday'])
+        hours = hours + '; ' + 'Thu: ' + json.dumps(item['hours']['Thursday'])
+        hours = hours + '; ' + 'Fri: ' + json.dumps(item['hours']['Friday'])
+        hours = hours + '; ' + 'Sat: ' + json.dumps(item['hours']['Saturday'])
+        hours = hours + '; ' + 'Sun: ' + json.dumps(item['hours']['Sunday'])
+        hours = hours.replace('["", ""]','Closed').replace('[','').replace('"','').replace(']','').replace(',',' -')
+        phone = phone.replace('\r\n',';').replace(';;',';').replace(';;',';').replace(';;',';').replace(';;',';').replace(';;',';')
+        phone = phone.replace('  PHONE','')
+        if ';' in phone:
+            try:
+                phone = phone.split(';')[0].strip().replace(') ',')').rsplit(' ',1)[1].replace(')',') ').strip()
+            except:
+                phone = phone.split(';')[0].strip()
+        yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
