8858e0dda4e6d44a200332bcdc572c0120eac02a
nsxdarin <nsxdarin@gmail.com>
2019-08-16 23:22:47 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/mykoolsmiles_com/scrape.py
+++ apify/nsxdarin/storelocator/mykoolsmiles_com/scrape.py
@@ -2,6 +2,7 @@ import csv
 import urllib2
 import requests
 import sgzip
+import json
 
 session = requests.Session()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
@@ -26,37 +27,34 @@ def fetch_data():
                    'page': 'https://www.mykoolsmiles.com/'
                    }
         r = session.post(url, headers=headers, data=payload)
-        for line in r.iter_lines():
-            if '"location_id":"' in line:
-                items = line.split('"location_id":"')
-                for item in items:
-                    if '"brand_id":"' in item:
-                        website = 'mykoolsmiles.com'
-                        typ = 'Office'
-                        name = item.split('"FacilityName":"')[1].split('"')[0]
-                        add = item.split('"Address1":"')[1].split('"')[0]
-                        city = item.split('"City":"')[1].split('"')[0]
-                        state = item.split('"State":"')[1].split('"')[0]
-                        zc = item.split('"ZIP":"')[1].split('"')[0]
-                        phone = item.split('"OfficePhone":"')[1].split('"')[0].strip()
-                        country = 'US'
-                        store = item.split('"')[0]
-                        hours = 'Mon: ' + item.split('Mon_Hours":"')[1].split('"')[0]
-                        hours = hours + '; Tue: ' + item.split('Tue_Hours":"')[1].split('"')[0]
-                        hours = hours + '; Wed: ' + item.split('Wed_Hours":"')[1].split('"')[0]
-                        hours = hours + '; Thu: ' + item.split('Tue_Hours":"')[1].split('"')[0]
-                        hours = hours + '; Fri: ' + item.split('Fri_Hours":"')[1].split('"')[0]
-                        hours = hours + '; Sat: ' + item.split('Sat_Hours":"')[1].split('"')[0]
-                        if 'Sun_Hours":""' not in item:
-                            hours = hours + '; Sun: ' + item.split('Sun_Hours":"')[1].split('"')[0]
-                        else:
-                            hours = hours + '; Sun: Closed'
-                        hours = hours.replace('; Sat: ;','; Sat: Closed;')
-                        lat = item.split('"latitude":"')[1].split('"')[0]
-                        lng = item.split('"longitude":"')[1].split('"')[0]
-                        if store not in ids:
-                            ids.append(store)
-                            yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+        array = json.loads(r.content)
+        for item in array:
+            website = 'mykoolsmiles.com'
+            typ = 'Office'
+            name = item['FacilityName']
+            add = item['Address1']
+            city = item['City']
+            state = item['State']
+            zc = item['ZIP']
+            phone = item['OfficePhone']
+            country = 'US'
+            store = item['location_id']
+            hours = 'Mon: ' + item['Mon_Hours']
+            hours = hours + '; Tue: ' + item['Tue_Hours']
+            hours = hours + '; Wed: ' + item['Wed_Hours']
+            hours = hours + '; Thu: ' + item['Thu_Hours']
+            hours = hours + '; Fri: ' + item['Fri_Hours']
+            hours = hours + '; Sat: ' + item['Sat_Hours']
+            if 'Sun_Hours":""' not in item:
+                hours = hours + '; Sun: ' + item['Sun_Hours']
+            else:
+                hours = hours + '; Sun: Closed'
+            hours = hours.replace('; Sat: ;','; Sat: Closed;')
+            lat = item['latitude']
+            lng = item['longitude']
+            if store not in ids:
+                ids.append(store)
+                yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
