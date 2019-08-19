6f007fae614d061d1cb89d4f996a00a39c2ac8c9
nsxdarin <nsxdarin@gmail.com>
2019-08-18 19:49:06 -0500

    Create scrape.py

---
+++ apify/nsxdarin/storelocator/bekins/scrape.py
@@ -0,0 +1,57 @@
+import csv
+import urllib2
+import requests
+
+session = requests.Session()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    url = 'https://www.bekins.com/agent-sitemap.xml'
+    locs = []
+    r = session.get(url, headers=headers)
+    for line in r.iter_lines():
+        if '<loc>https://www.bekins.com/find-a-local-agent/agents/' in line and '<loc>https://www.bekins.com/find-a-local-agent/agents/<' not in line:
+            lurl = line.split('<loc>')[1].split('<')[0]
+            locs.append(lurl)
+    print('Found %s Locations.' % str(len(locs)))
+    for loc in locs:
+        name = ''
+        add = ''
+        city = ''
+        state = ''
+        zc = ''
+        phone = ''
+        print('Pulling Location %s...' % loc)
+        website = 'bekins.com'
+        typ = 'Agent'
+        r2 = session.get(loc, headers=headers)
+        for line2 in r2.iter_lines():
+            if '<h2>' in line2:
+                name = line2.split('<h2>')[1].split('<')[0].replace('\t','')
+            if '<div class=agent-address>' in line2:
+                add = line2.split('<div class=agent-address>')[1].split('<')[0].strip().replace('\t','')
+                city = line2.split('<div class=agent-address>')[1].split('<br>')[1].strip().split(',')[0].replace('\t','')
+                state = line2.split('<div class=agent-address>')[1].split('<br>')[1].strip().split(',')[1].strip().split('&')[0].replace('\t','')
+                zc = line2.split('<div class=agent-address>')[1].split('&nbsp;&nbsp;')[1].split('<')[0].strip().replace('\t','')
+            if '<span class=agent-phone><' in line2:
+                phone = line2.split('<span class=agent-phone><')[1].split('>')[1].split('<')[0]
+            country = 'US'
+            store = '<MISSING>'
+            hours = '<MISSING>'
+            lat = '<MISSING>'
+            lng = '<MISSING>'
+        yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
