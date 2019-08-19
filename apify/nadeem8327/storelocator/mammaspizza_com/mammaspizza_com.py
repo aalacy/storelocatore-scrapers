1f24340d35f829ddd613938e9118428e244a2d77
nadeem8327 <mnadeem8327@gmail.com>
2019-08-19 16:44:18 +0500

    Update mammaspizza_com.py

--- apify/nadeem8327/storelocator/mammaspizza_com/mammaspizza_com.py
+++ apify/nadeem8327/storelocator/mammaspizza_com/mammaspizza_com.py
@@ -7,9 +7,10 @@ from selenium.webdriver.common.proxy import Proxy,ProxyType
 import time
 import re #for regular expression
 options = webdriver.ChromeOptions()
+options.add_argument("--headless")
 prefs= {"profile.default_content_setting_values.geolocation":2}
 options.add_experimental_option("prefs",prefs)
-driver=webdriver.Chrome('C:\\Users\\Lenovo\\Desktop\\chrome-driver\\chromedriver',options=options)
+driver=webdriver.Chrome('chromedriver',options=options)
 
 url = "https://mammaspizza.com/locations/"
 driver.get(url)
