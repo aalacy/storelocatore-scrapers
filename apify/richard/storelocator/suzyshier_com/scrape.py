31154186829c06d8a66fb9b3a09dd6dcba5b9f16
Richard Chen <rca@fb.com>
2019-08-25 22:58:40 -0700

    add cond

--- apify/richard/storelocator/suzyshier_com/scrape.py
+++ apify/richard/storelocator/suzyshier_com/scrape.py
@@ -57,56 +57,74 @@ def fetch_data():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(CHROME_DRIVER_PATH, options=options)
-    url = 'https://suzyshier.com/apps/api/v1/stores?location%5Blongitude%5D=100.5333&location%5Blatitude%5D=13.6667&location%5Bradius%5D=30000&location%5Bunits%5D=km&_=1566795334972'
+    url = "https://suzyshier.com/apps/api/v1/stores?location%5Blongitude%5D=100.5333&location%5Blatitude%5D=13.6667&location%5Bradius%5D=30000&location%5Bunits%5D=km&_=1566795334972"
     driver.get(url)
-    listings = json.loads(driver.find_element_by_tag_name("pre").text)['stores']
+    listings = json.loads(driver.find_element_by_tag_name("pre").text)["stores"]
 
     for listing in listings:
         # Location code
-        location_id.append(listing['store_code'])
+        location_id.append(listing["store_code"])
 
         # Location title
-        locations_titles.append(listing['address']['name'])
+        locations_titles.append(listing["address"]["name"])
 
         # Address
-        street_addresses.append((listing['address']['line1'] + ' ' + listing['address']['line2'] + ' ' + str(listing['address']['line3'])).strip())
+        street_addresses.append(
+            (
+                listing["address"]["line1"]
+                + " "
+                + (listing["address"]["line2"] if listing["address"]["line2"] else "")
+                + " "
+                + (listing["address"]["line3"] if listing["address"]["line3"] else "")
+            ).strip()
+        )
 
         # City
-        cities.append(listing['address']['city'].strip() if listing['address']['city'].strip() else '<MISSING>')
+        cities.append(
+            listing["address"]["city"].strip()
+            if listing["address"]["city"].strip()
+            else "<MISSING>"
+        )
 
         # State
-        states.append(listing['address']['state_code'].strip() if listing['address']['state_code'] else '<MISSING>')
+        states.append(
+            listing["address"]["state_code"].strip()
+            if listing["address"]["state_code"]
+            else "<MISSING>"
+        )
 
         # Country
-        countries.append(listing['address']['country_code'])
+        countries.append(listing["address"]["country_code"])
 
         # Zip
-        zip_codes.append(listing['address']['zip'])
+        zip_codes.append(listing["address"]["zip"])
 
         # Lat
-        latitude_list.append(listing['address']['latitude'])
+        latitude_list.append(listing["address"]["latitude"])
 
         # lon
-        longitude_list.append(listing['address']['longitude'])
+        longitude_list.append(listing["address"]["longitude"])
 
         # Phone
-        phone_numbers.append(listing['phone'] if listing['phone'] != '' else '<MISSING>')
+        phone_numbers.append(
+            listing["phone"] if listing["phone"] != "" else "<MISSING>"
+        )
 
         # Hours
-        hours.append(listing['open_hours'])
+        hours.append(listing["open_hours"])
 
     for (
-            locations_title,
-            street_address,
-            city,
-            state,
-            zipcode,
-            phone_number,
-            latitude,
-            longitude,
-            hour,
-            id,
-            country
+        locations_title,
+        street_address,
+        city,
+        state,
+        zipcode,
+        phone_number,
+        latitude,
+        longitude,
+        hour,
+        id,
+        country,
     ) in zip(
         locations_titles,
         street_addresses,
@@ -118,7 +136,7 @@ def fetch_data():
         longitude_list,
         hours,
         location_id,
-        countries
+        countries,
     ):
         data.append(
             [
