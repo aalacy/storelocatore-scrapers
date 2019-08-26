0faa16751cdb1c6b3e7d2134f5ed30ea7e130a23
Graham Baker <graham.cr.baker@gmail.com>
2019-08-26 00:00:03 -0500

    pushing 17 more

---
+++ apify/ggrahambaker/storelocator/brigantine_com/scrape.py
@@ -0,0 +1,67 @@
+import csv
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    return webdriver.Chrome('chromedriver', options=options)
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    locator_domain = 'http://www.brigantine.com/'
+
+    driver = get_driver()
+    driver.get(locator_domain)
+
+    locs = driver.find_elements_by_css_selector('div.location')
+    all_store_data = []
+    for loc in locs:
+        cont = loc.text.split('\n')
+        location_name = cont[0].split('(')[0].strip()
+        phone_number = cont[1]
+        street_address = cont[2]
+        city = cont[3]
+        state = cont[4]
+        zip_code = cont[5]
+        hours_div = loc.find_element_by_xpath('//div[@class="list-location"]')
+        hours_html = driver.execute_script("return arguments[0].innerHTML", hours_div)
+        soup = BeautifulSoup(hours_html, 'html.parser')
+
+        hours = soup.text.replace('\n', ' ').strip()
+
+        lat = '<MISSING>'
+        longit = '<MISSING>'
+        country_code = 'US'
+        store_number = '<MISSING>'
+        location_type = '<MISSING>'
+
+
+        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
+                      store_number, phone_number, location_type, lat, longit, hours]
+        all_store_data.append(store_data)
+
+    driver.quit()
+    return all_store_data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
---
+++ apify/ggrahambaker/storelocator/working/redorestaurant_com/scrape.py
@@ -0,0 +1,110 @@
+import csv
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    return webdriver.Chrome('chromedriver', options=options)
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def addy_ext(addy):
+    address = addy.split(',')
+    city = address[0]
+    state_zip = address[1].strip().split(' ')
+    state = state_zip[0]
+    zip_code = state_zip[1]
+    return city, state, zip_code
+
+
+
+
+def fetch_data():
+    locator_domain = 'https://www.redorestaurant.com/'
+    ext = 'locations/'
+
+    driver = get_driver()
+    driver.get(locator_domain + ext)
+
+    locs = driver.find_elements_by_css_selector('div.six.columns')
+    all_store_data = []
+    for loc in locs:
+        cont = loc.text.split('\n')
+        if len(cont) > 30:
+            continue
+        elif len(cont) < 10:
+            continue
+
+        cont = loc.text.split('\n')
+
+
+        href = loc.find_element_by_css_selector('a').get_attribute('href')
+
+        start_idx = href.find('/@')
+        end_idx = href.find('z/d')
+
+        coords = href[start_idx + 2: end_idx].split(',')
+
+        lat = coords[0]
+        longit = coords[1]
+
+        if len(cont) == 22:
+            street_address = cont[2]
+            city, state, zip_code = addy_ext(cont[3])
+            phone_number = cont[4].replace('Tel:', '').strip()
+            hours = ''
+            for h in cont[8:-3]:
+                hours += h + ' '
+
+
+        else:
+            if len(cont) == 19:
+                off = 2
+            else:
+                off = 0
+
+            street_address = cont[1]
+            city, state, zip_code = addy_ext(cont[2])
+            phone_number = cont[3].replace('Tel:', '').strip()
+
+            hours = ''
+
+            for h in cont[off + 6:-3]:
+                hours += h + ' '
+
+
+
+        location_name = city
+        country_code = 'US'
+        store_number = '<MISSING>'
+        location_type = '<MISSING>'
+        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
+                      store_number, phone_number, location_type, lat, longit, hours]
+        all_store_data.append(store_data)
+
+
+
+    driver.quit()
+    return all_store_data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
