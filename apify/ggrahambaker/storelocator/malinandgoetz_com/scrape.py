2839bef74eb2b88ef6df7b1b92ea1854c4c4e3bb
Graham Baker <graham.cr.baker@gmail.com>
2019-08-26 23:39:07 -0500

    fixes for malina

--- apify/ggrahambaker/storelocator/malinandgoetz_com/scrape.py
+++ apify/ggrahambaker/storelocator/malinandgoetz_com/scrape.py
@@ -119,7 +119,8 @@ def fetch_data():
 
         store_number = '<MISSING>'
         location_type = '<MISSING>'
-
+        if location_name == '':
+            location_name = 'Hayes Street'
 
         store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
                       store_number, phone_number, location_type, lat, longit, hours]
