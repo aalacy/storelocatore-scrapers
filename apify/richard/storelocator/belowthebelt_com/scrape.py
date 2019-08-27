439258505f871851edfc353c6964d5908b69b76e
Richard Chen <rca@fb.com>
2019-08-26 21:12:06 -0700

    fix

--- apify/richard/storelocator/belowthebelt_com/scrape.py
+++ apify/richard/storelocator/belowthebelt_com/scrape.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import re
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -7,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-COMPANY_URL = "https://www.belowthebelt.com/"
+COMPANY_URL = "https://www.beggarspizza.com"
 CHROME_DRIVER_PATH = "chromedriver"
 
 
@@ -43,57 +45,102 @@ def write_output(data):
 def fetch_data():
     # store data
     locations_titles = []
+    location_id = []
     street_addresses = []
     cities = []
     states = []
     zip_codes = []
     phone_numbers = []
+    hours = []
+    longitude_list = []
+    latitude_list = []
     data = []
-
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(CHROME_DRIVER_PATH, options=options)
-    driver.get('https://www.belowthebelt.com/pages/stores')
+
+    store_url = "https://www.beggarspizza.com/locations/"
+    driver.get(store_url)
+    names = [
+        name.get_attribute("textContent")
+        for name in driver.find_elements_by_css_selector(
+            "div.wpsl-store-location > p > strong > a"
+        )
+    ]
+    urls = [
+        url.get_attribute("href")
+        for url in driver.find_elements_by_css_selector(
+            "div.wpsl-store-location > p > strong > a"
+        )
+    ]
+    hour_dict = {}
+    for name, url in zip(names, urls):
+        driver.get(url)
+        hour = driver.find_element_by_css_selector(
+            "div.col-sm-12.reset-left > div.col-xs-6.smallWide.reset-left"
+        ).get_attribute("textContent")
+        hour_dict[name.strip()] = hour
+
+    # Fetch store urls from location menu
+    store_url = "https://www.beggarspizza.com/wp-admin/admin-ajax.php?action=store_search&lat=41.878114&lng=-87.629798&max_results=25&search_radius=1000&autoload=1"
+    driver.get(store_url)
 
     # Wait until element appears - 10 secs max
     wait = WebDriverWait(driver, 10)
-    wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, "div.storelocator-panel")))
+    wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, "pre")))
 
-    while True:
-        if len(driver.find_elements_by_css_selector('div.store')) > 0:
-            stores = driver.find_elements_by_css_selector('div.store')
-            break
+    locations = json.loads(driver.find_element_by_css_selector("pre").text)
 
-    for store in stores:
-        # Location title
-        locations_titles.append(store.find_element_by_css_selector('div.title').get_attribute('textContent'))
+    for location in locations:
+        # ID
+        location_id.append(location["id"])
+
+        # Title
+        locations_titles.append(location["store"])
 
         # Street address
-        street_addresses.append(store.find_element_by_css_selector('div.address').get_attribute('innerHTML').split('<br>')[1].replace('<span>', '').replace('</span>', ''))
+        street_addresses.append(location["address"] + " " + location["address2"])
 
         # City
-        cities.append(store.find_element_by_css_selector('div.address').get_attribute('innerHTML').split('<br>')[2].split(',')[0].strip())
+        cities.append(location["city"])
 
-        # Province
-        states.append(store.find_element_by_css_selector('div.address').get_attribute('innerHTML').split('<br>')[2].split(',')[1].strip()[:-7].strip())
+        # State
+        states.append(location["state"])
 
-        # Zip code
-        zip_codes.append(store.find_element_by_css_selector('div.address').get_attribute('innerHTML').split('<br>')[2].split(',')[1].strip()[-7:].strip())
+        # Phone
+        phone_numbers.append(location["phone"])
 
-        # phone
-        phone_numbers.append(store.find_element_by_css_selector('div.phone').get_attribute('textContent'))
+        # Zip
+        zip_codes.append(location["zip"])
 
+        # Latitude
+        latitude_list.append(location["lat"])
 
-    # Store data
+        # Longitude
+        longitude_list.append(location["lng"])
+
+        # Hour  - Use backup to reference
+        hour = " ".join(re.sub("<[^>]*>", ",", location["hours"]).split(","))
+        if hour == "":
+            if location["store"] in hour_dict.keys():
+                hour = hour_dict[location["store"]]
+            else:
+                hour = "<MISSING>"
+        hours.append(hour)
+
     for (
-            locations_title,
-            street_address,
-            city,
-            state,
-            zipcode,
-            phone_number,
+        locations_title,
+        street_address,
+        city,
+        state,
+        zipcode,
+        phone_number,
+        latitude,
+        longitude,
+        hour,
+        id,
     ) in zip(
         locations_titles,
         street_addresses,
@@ -101,6 +148,10 @@ def fetch_data():
         states,
         zip_codes,
         phone_numbers,
+        latitude_list,
+        longitude_list,
+        hours,
+        location_id,
     ):
         data.append(
             [
@@ -110,13 +161,13 @@ def fetch_data():
                 city,
                 state,
                 zipcode,
-                "CA",
-                "<MISSING>",
+                "US",
+                id,
                 phone_number,
                 "<MISSING>",
-                "<MISSING>",
-                "<MISSING>",
-                "<MISSING>",
+                latitude,
+                longitude,
+                hour,
             ]
         )
 
