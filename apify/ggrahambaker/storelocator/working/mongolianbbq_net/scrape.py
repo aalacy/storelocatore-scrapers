e1d50b657138c4f8f44e07828f953b1bcc7e9b20
Graham Baker <graham.cr.baker@gmail.com>
2019-08-28 12:05:58 -0500

    checking in

--- apify/ggrahambaker/storelocator/working/mongolianbbq_net/scrape.py
+++ apify/ggrahambaker/storelocator/working/mongolianbbq_net/scrape.py
@@ -23,26 +23,45 @@ def write_output(data):
         for row in data:
             writer.writerow(row)
 
+
+def addy_ext(addy):
+    address = addy.split(',')
+    city = address[0]
+    state_zip = address[1].strip().split(' ')
+    state = state_zip[0]
+    zip_code = state_zip[1]
+    return city, state, zip_code
+
+
 def fetch_data():
-    locator_domain = 'https://www.malinandgoetz.com/'
-    ext = 'storelocator/'
+    locator_domain = 'http://mongolianbbq.net/'
+    ext = 'locations.html'
 
     driver = get_driver()
     driver.get(locator_domain + ext)
 
-    pop = driver.find_elements_by_xpath('//img[@data-popup-image-type="square_new"]')
-    if len(pop) == 1:
-        pop = driver.find_element_by_xpath('//img[@data-popup-image-type="square_new"]')
-        driver.execute_script("arguments[0].click();", pop)
-
-    driver.execute_script('MapManager.prototype.updateCountLabel = (() => console.log("update count label"))')
-    try: driver.execute_script('window.onload()')
-    except: print("failed")
-    man = driver.execute_script('return mapManager.markers.reduce((accumulator, marker) => ({ ...accumulator, [marker.storelocator_id]: [marker.position.lat(), marker.position.lng()] }),{})')
-    print(man)
-
-
+    hrefs = driver.find_elements_by_xpath("//a[contains(@href, 'mapquest')]")
     all_store_data = []
+    for h in hrefs:
+        cont = h.text.split('\n')
+        street_address = cont[0]
+        city, state, zip_code = addy_ext(cont[1])
+        phone_number = cont[2]
+
+        hours = '11:30am - 9:00pm 7 days a week'
+
+        lat = '<MISSING>'
+        longit = '<MISSING>'
+
+        country_code = 'US'
+        location_type = '<MISSING>'
+        store_number = '<MISSING>'
+        location_name = '<MISSING>'
+
+        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
+                      store_number, phone_number, location_type, lat, longit, hours]
+        all_store_data.append(store_data)
+
     driver.quit()
     return all_store_data
 
---
+++ apify/ggrahambaker/storelocator/working/ramen-yamadaya_com/scrape.py
@@ -0,0 +1,89 @@
+import csv
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import json
+
+
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    return webdriver.Chrome('chromedriver', options=options)
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    locator_domain = 'https://www.ramen-yamadaya.com/'
+
+    driver = get_driver()
+    driver.get(locator_domain)
+
+    hrefs = driver.find_elements_by_xpath("//a[contains(text(),'VISIT STORE')]")
+    hrefs.append(driver.find_element_by_xpath("//a[contains(text(),'visit store')]"))
+    link_list = []
+    for href in hrefs:
+        link_list.append(href.get_attribute('href'))
+
+    all_store_data = []
+    for link in link_list:
+        driver.get(link)
+        driver.implicitly_wait(10)
+
+        map_coord = driver.find_element_by_css_selector('div.sqs-block.map-block.sqs-block-map').get_attribute(
+            'data-block-json')
+        loc_json = json.loads(map_coord)
+
+        lat = loc_json['location']['mapLat']
+        longit = loc_json['location']['mapLng']
+        street_address = loc_json['location']['addressLine1']
+        city, state, zip_code = loc_json['location']['addressLine2'].split(',')
+        state = state.strip()
+        zip_code = zip_code.strip()
+
+        location_name = loc_json['location']['addressTitle']
+
+        divs = driver.find_elements_by_css_selector('div.sqs-block-content')
+        for div in divs:
+            if 'HOURS' in div.text:
+                cont = div.text.split('\n')
+
+                hours = ''
+                for i, h in enumerate(cont):
+                    if 'Phone' in h:
+                        phone_number = cont[i + 1]
+                        break
+                    clean = ' '.join(h.split())
+                    hours += clean + ' '
+
+                hours = hours.replace('HOURS', '').strip()
+
+
+        country_code = 'US'
+        location_type = '<MISSING>'
+        store_number = '<MISSING>'
+
+        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
+                      store_number, phone_number, location_type, lat, longit, hours]
+        all_store_data.append(store_data)
+
+
+    driver.quit()
+    return all_store_data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
