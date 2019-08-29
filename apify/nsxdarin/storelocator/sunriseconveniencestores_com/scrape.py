9df4778c37fd3fe1be4121307b8f97a3cb2f0ce6
nsxdarin <nsxdarin@gmail.com>
2019-08-29 17:19:55 -0500

    Create scrape.py

---
+++ apify/nsxdarin/storelocator/sunriseconveniencestores_com/scrape.py
@@ -0,0 +1,58 @@
+import csv
+import urllib2
+import requests
+import json
+
+session = requests.Session()
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    url = 'https://sunriseconveniencestores.com'
+    locs = []
+    cities = []
+    r = session.get(url, headers=headers)
+    lines = r.iter_lines()
+    for line in lines:
+        if '"title":"' in line:
+            items = line.split('"title":"')
+            for item in items:
+                if '"address":"' in item:
+                    add = item.split('"address":"')[1].split(',')[0]
+                    name = item.split('">')[1].split('<')[0]
+                    website = 'sunriseconveniencestores.com'
+                    if 'BP' in name:
+                        typ = 'BP'
+                    else:
+                        typ = 'Marathon'
+                    store = '<MISSING>'
+                    zc = '<MISSING>'
+                    city = item.split('"address":"')[1].split('","desc')[0].split(',')[1].strip()
+                    try:
+                        state = item.split('"address":"')[1].split('","desc')[0].split(',')[2].split('"')[0].strip()
+                    except:
+                        city = item.split('"address":"')[1].split('","desc')[0].split(',')[1].rsplit(' ',1)[0].strip()
+                        state = item.split('"address":"')[1].split('","desc')[0].split(',')[1].rsplit(' ',1)[1].strip()
+                    country = 'US'
+                    phone = item.split('"tel:+')[1].split('\\')[0]
+                    hours = item.split('"desc":"')[1].split('<\\/a>\\n<p>')[1].split('<')[0]
+                    lat = item.split('"lat":"')[1].split('"')[0]
+                    lng = item.split('"lng":"')[1].split('"')[0]
+                    if '-' in name:
+                        name = city + ' Marathon'
+                    if '\\' in hours:
+                        hours = hours.split('\\')[0]
+                    yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
