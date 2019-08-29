e7213e7520d35862aa845a0cc8550203511cdb86
Tenzing Shaw <tenzing@safegraph.com>
2019-08-29 10:26:28 -0700

    firstwatch use hybrid search

--- apify/nsxdarin/storelocator/firstwatch_com/scrape.py
+++ apify/nsxdarin/storelocator/firstwatch_com/scrape.py
@@ -3,8 +3,11 @@ import urllib2
 import requests
 import json
 import time
-from sgzip import sgzip
+import sgzip 
 import os
+
+search = sgzip.ClosestNSearch()
+search.initialize()
 
 session = requests.Session()
 proxy_password = os.environ["PROXY_PASSWORD"]
@@ -17,6 +20,9 @@ session.proxies = proxies
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
 
+MAX_RESULTS = 15
+MAX_DISTANCE = 100.0
+
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -25,53 +31,57 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
-    ids = []
-    for coord in sgzip.coords_for_radius(50):
+    ids = set()
+    locations = []
+    coord = search.next_coord()
+    while coord:
+        print("remaining zipcodes: " + str(len(search.zipcodes)))
         x = coord[0]
         y = coord[1]
         print('Pulling Lat-Long %s,%s...' % (str(x), str(y)))
-        url = 'https://www.firstwatch.com/api/get_locations.php?latitude=' + x + '&longitude=' + y
-        Found = True
-        while Found:
-            Found = False
-            try:
-                r = session.get(url, headers=headers)
-                if '"city"' in r.content:
-                    array = json.loads(r.content)
-                    for item in array:
-                        store = item['corporate_id']
-                        if store in ids: continue
-                        website = 'firstwatch.com'
-                        hours = '<MISSING>'
-                        name = item['name']
-                        add = item['address'] + ' ' + item['address_extended']
-                        add = add.strip()
-                        phone = item['phone']
-                        slug = item['slug']
-                        surl = 'https://www.firstwatch.com/locations/' + slug
-                        SFound = True
-                        while SFound:
-                            SFound = False
-                            try:
-                                r2 = session.get(surl, headers=headers)
-                                for line2 in r2.iter_lines():
-                                    if '<address>Open' in line2:
-                                        hours = line2.split('<address>')[1].split('<')[0]
-                                typ = 'Restaurant'
-                                city = item['city']
-                                state = item['state']
-                                zc = item['zip']
-                                country = 'US'
-                                store = item['corporate_id']
-                                lat = item['latitude']
-                                lng = item['longitude']
-                                ids.append(store)
-                                print('Pulling Store ID #%s...' % store)
-                                yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
-                            except:
-                                SFound = True
-            except:
-                Found = False
+        url = 'https://www.firstwatch.com/api/get_locations.php?latitude=' + str(x) + '&longitude=' + str(y)
+        r = session.get(url, headers=headers)
+        array = json.loads(r.content)
+        result_coords = []
+        for item in array:
+            lat = item['latitude']
+            lng = item['longitude']
+            result_coords.append((lat, lng))
+            store = item['corporate_id']
+            if store in ids: continue
+            website = 'firstwatch.com'
+            hours = '<MISSING>'
+            name = item['name']
+            add = item['address'] + ' ' + item['address_extended']
+            add = add.strip()
+            phone = item['phone']
+            slug = item['slug']
+            surl = 'https://www.firstwatch.com/locations/' + slug
+            r2 = session.get(surl, headers=headers)
+            for line2 in r2.iter_lines():
+                if '<address>Open' in line2:
+                    hours = line2.split('<address>')[1].split('<')[0]
+            typ = 'Restaurant'
+            city = item['city']
+            state = item['state']
+            zc = item['zip']
+            country = 'US'
+            store = item['corporate_id']
+            ids.add(store)
+            print('Pulling Store ID #%s...' % store)
+            locations.append([website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours])
+        if len(array) < MAX_RESULTS:
+            print("max distance update")
+            search.max_distance_update(MAX_DISTANCE)
+        elif len(array) == MAX_RESULTS:
+            print("max count update")
+            search.max_count_update(result_coords)
+        else:
+            raise Exception("expected at most " + MAX_RESULTS + " results")
+        coord = search.next_coord()
+    for loc in locations:
+        yield loc
+
 def scrape():
     data = fetch_data()
     write_output(data)
