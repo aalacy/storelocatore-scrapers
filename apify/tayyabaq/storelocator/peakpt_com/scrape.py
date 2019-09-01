aa3aa22b6d96f070841e0c267da1ff973e0f98ed
Tayyaba Qamar <52938783+TayyabaQ@users.noreply.github.com>
2019-09-01 18:57:09 +0500

    Create scrape.py

---
+++ apify/tayyabaq/storelocator/peakpt_com/scrape.py
@@ -0,0 +1,58 @@
+import csv
+import os
+import re, time
+import requests
+from bs4 import BeautifulSoup
+
+def write_output(data):
+    with open('data.csv', mode='wb') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            if row:
+                writer.writerow(row)
+def fetch_data():
+    data=[]; location_name=[];address_stores=[]; city=[];street_address=[]; zipcode=[]; state=[]; latitude=[]; longitude=[]; hours_of_operation=[]; phone=[]
+    #Driver
+    url ='http://www.peakpt.com/contact.html'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    stores = soup.findAll("div", {"class": "address-full"})
+    for n in range(0,len(stores)):
+        street_address.append(stores[n].get_text().split(",")[0])
+        state.append(stores[n].get_text().split(",")[1])
+        zipcode.append(stores[n].get_text().split(",")[2])
+    phones = soup.findAll("div", {"class": "address-line-idphone"})
+    for n in range(0,len(phones)):
+        phone.append(phones[n].get_text().split("Phone:")[1])
+    hours = soup.findAll("div", {"class": "address-line-idhours"})
+    for n in range(0,len(street_address)):
+        try:
+            hours_of_operation.append(hours[n].get_text())
+        except:
+            hours_of_operation.append('<MISSING>')
+    for n in range(0,len(street_address)): 
+        data.append([
+            'http://www.peakpt.com',
+            '<MISSING>',
+            street_address[n],
+            '<MISSING>',
+            state[n],
+            zipcode[n],
+            'US',
+            '<MISSING>',
+            phone[n],
+            '<MISSING>',
+            '<MISSING>',
+            '<MISSING>',
+            hours_of_operation[n]
+        ])
+    return data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+   
+scrape()
