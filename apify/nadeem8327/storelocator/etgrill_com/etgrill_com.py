68bfa9600c8048da5be214a3bd024cb84e7a51fe
nadeem8327 <mnadeem8327@gmail.com>
2019-09-03 09:45:53 +0500

    Update etgrill_com.py

--- apify/nadeem8327/storelocator/etgrill_com/etgrill_com.py
+++ apify/nadeem8327/storelocator/etgrill_com/etgrill_com.py
@@ -15,7 +15,7 @@ opts.add_argument("--headless")
 prefs= {"profile.default_content_setting_values.geolocation":2}
 opts.add_experimental_option("prefs",prefs)
 capabilities = webdriver.DesiredCapabilities.CHROME
-driver=webdriver.Chrome("chromedriver",options=opts,desired_capabilities=capabilities)
+driver=webdriver.Chrome("/home/nadeem/Downloads/chromedriver",options=opts,desired_capabilities=capabilities)
 url= "https://www.etgrill.com/contact/brea/"
 locator_domain=url
 driver.get(url)
@@ -24,6 +24,14 @@ soup = BeautifulSoup(html,"html.parser")
  #   location_name=location_name+x.text
 hed=["locator_domain","location_name","street_address","city","state","zip","country_code","store_number","phone","location_type","latitude",
            "longitude","hours_of_operation"]
+lat = soup.find("div",attrs={"class":"gmap"})
+tx = lat["data-url"]
+tx = tx.split("@")
+tx = tx[1]
+tx = tx.split(",")
+latitude = tx[0]
+longitude = tx[1]
+#print(latitude,longitude)
 all_rec = soup.find('section',attrs={"class":"page__content"})
 with open("data.csv",mode="w") as file:
     fl_writer=csv.writer(file,delimiter=',')
@@ -48,8 +56,43 @@ with open("data.csv",mode="w") as file:
 
 
     data=["www_etgrill_com","<MISSING>",street_address,city,state,zip_code,
-    "US","<MISSING>",contact_number,"<MISSING>","<MISSING>","<MISSING>",timing]
+    "US","<MISSING>",contact_number,"<MISSING>",latitude,longitude,timing]
     fl_writer.writerow(data)
-
+##################################################
+    url= "https://www.etgrill.com/contact/irvine/"
+    locator_domain=url
+    driver.get(url)
+    html = driver.execute_script("return document.body.innerHTML")
+    soup = BeautifulSoup(html,"html.parser")
+    lat = soup.find("div",attrs={"class":"gmap"})
+    tx = lat["data-url"]
+    tx = tx.split("@")
+    tx = tx[1]
+    tx = tx.split(",")
+    latitude = tx[0]
+    longitude = tx[1]
+    #print(latitude,longitude)
+    all_rec = soup.find('section',attrs={"class":"page__content"})
+    pp = all_rec.find_all('div',attrs={"class":"grid__item"})
+    dta = pp[0].find_all("p")
+    add = dta[0].text
+    street_address = add.split(",")[0]
+    city=street_address.strip().split(" ")[3]
+    street_address = street_address.strip().split("brea")[0]
+    zip_st = add.split(",")[1]
+    zip_st = zip_st.strip().replace(" ","")
+    state = zip_st[:2]
+    zip_code = zip_st[2:]
+    tim = pp[1].find("p")
+    timing = tim.text
+    contact_number = dta[1].text
+    contact_number = contact_number.replace(".","-")
+    zip_code = zip_code.strip()
+    if zip_code == "92621": #this zip code is invalid
+        zip_code = "92821"
+    data=["www_etgrill_com","<MISSING>",street_address,city,state,zip_code,
+    "US","<MISSING>",contact_number,"<MISSING>",latitude,longitude,timing]
+    fl_writer.writerow(data)
+    #print(data)
 
 driver.quit()
