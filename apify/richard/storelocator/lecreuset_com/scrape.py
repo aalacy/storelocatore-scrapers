a800f87d9e9b31ef88295b9d0db894feb69dbb66
Richard Chen <rca@fb.com>
2019-09-11 05:28:02 -0700

    sixth_batch

---
+++ apify/richard/storelocator/lecreuset_com/scrape.py
@@ -0,0 +1,148 @@
+import json
+
+from Scraper import Scrape
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+URL = "https://www.lecreuset.com"
+
+
+class Scraper(Scrape):
+    def __init__(self, url):
+        Scrape.__init__(self, url)
+        self.data = []
+
+    def fetch_data(self):
+        # store data
+        locations_ids = []
+        locations_titles = []
+        street_addresses = []
+        cities = []
+        states = []
+        zip_codes = []
+        latitude_list = []
+        longitude_list = []
+        phone_numbers = []
+        hours = []
+        countries = []
+        stores = []
+
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        driver = webdriver.Chrome(self.CHROME_DRIVER_PATH, options=options)
+
+        # Fetch stores from location menu
+        location_url = "https://www.lecreuset.com/ustorelocator/location/searchJson/"
+        driver.get(location_url)
+        stores.extend(json.loads(driver.find_element_by_css_selector("pre").text)['markers'])
+
+        for store in stores:
+            print(store)
+            # Store ID
+            location_id = store['location_id']
+
+            # Name
+            location_title = store['title']
+
+            # Street Address
+            street_address = store['address']
+
+            try:
+                # City
+                city = store['address_display'].split('\n')[1].strip().split(',')[0].strip()
+
+                # State
+                state = store['address_display'].split('\n')[1].strip().split(',')[1].strip().split(' ')[0].strip()
+
+                # Zip
+                zip_code = store['address_display'].split('\n')[1].strip().split(',')[1].strip().split(' ')[1].strip()
+
+            except:
+                # City
+                city = store['address_display'].split(' ')[-3]
+
+                # State
+                state = store['address_display'].split(' ')[-2]
+
+                # Zip
+                zip_code = store['address_display'].split(' ')[-1]
+
+            # Hours
+            hour = store['store_hours']
+
+            # Lat
+            lat = store['latitude']
+
+            # Lon
+            lon = store['longitude']
+
+            # Phone
+            phone = store['phone']
+
+            # Country
+            country = store['country']
+
+            # Store data
+            locations_ids.append(location_id)
+            locations_titles.append(location_title)
+            street_addresses.append(street_address)
+            states.append(state)
+            zip_codes.append(zip_code)
+            hours.append(hour)
+            latitude_list.append(lat)
+            longitude_list.append(lon)
+            phone_numbers.append(phone)
+            cities.append(city)
+            countries.append(country)
+
+        for (
+                locations_title,
+                street_address,
+                city,
+                state,
+                zipcode,
+                phone_number,
+                latitude,
+                longitude,
+                hour,
+                location_id,
+                country,
+        ) in zip(
+            locations_titles,
+            street_addresses,
+            cities,
+            states,
+            zip_codes,
+            phone_numbers,
+            latitude_list,
+            longitude_list,
+            hours,
+            locations_ids,
+            countries,
+        ):
+            self.data.append(
+                [
+                    self.url,
+                    locations_title,
+                    street_address,
+                    city,
+                    state,
+                    zipcode,
+                    country,
+                    location_id,
+                    phone_number,
+                    "<MISSING>",
+                    latitude,
+                    longitude,
+                    hour,
+                ]
+            )
+
+        driver.quit()
+
+
+scrape = Scraper(URL)
+scrape.scrape()
