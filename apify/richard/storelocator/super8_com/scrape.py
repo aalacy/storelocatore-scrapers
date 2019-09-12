f97e70f3c0b9bb19042bcaa51181b84b7fdfee17
Richard Chen <rca@fb.com>
2019-09-12 10:51:55 -0700

    seventh_batch

---
+++ apify/richard/storelocator/super8_com/scrape.py
@@ -0,0 +1,152 @@
+import re
+
+from Scraper import Scrape
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+URL = "super8.com"
+
+
+class Scraper(Scrape):
+    def __init__(self, url):
+        Scrape.__init__(self, url)
+        self.data = []
+        self.stores_url = []
+        self.provinces = []
+
+    def fetch_data(self):
+        # store data
+        locations_ids = []
+        locations_type = []
+        locations_titles = []
+        street_addresses = []
+        cities = []
+        states = []
+        zip_codes = []
+        latitude_list = []
+        longitude_list = []
+        phone_numbers = []
+        hours = []
+        stores = []
+        countries = []
+
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        driver = webdriver.Chrome(self.CHROME_DRIVER_PATH, options=options)
+
+        driver.get('https://www.wyndhamhotels.com/super-8/locations')
+        stores.extend([url.get_attribute('href') for url in driver.find_elements_by_css_selector('li.property > a:nth-of-type(1)')])
+
+        for store in stores:
+            try:
+                driver.get(store)
+
+                location_info = driver.find_element_by_css_selector('div.uu-map-address.hidden-xs > p').text.split(',')
+
+                # Location id
+                location_id = '<MISSING>'
+
+                # Location title
+                locations_title = driver.find_element_by_css_selector('span.highlight-wrapper').text
+
+                # Location type
+                location_type = 'Hotel'
+
+                # Street address
+                street_address = location_info[0].strip()
+
+                # City
+                city = location_info[1].strip()
+
+                # State
+                state = location_info[2].strip()
+
+                # Zip code
+                zip_code = location_info[3].strip()
+
+                # Country
+                country = 'US'
+
+                # Store hour
+                hour = 'Always Open'
+
+                # Phone
+                phone_number = driver.find_element_by_css_selector('div.property-phone.hidden-xs > span > a').text
+
+                # Latitude
+                lat = re.search('(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)', driver.find_element_by_css_selector('div.directions-btn.action-btn.col-md-7.col-md-offset-8 > a').get_attribute('href')).group().split(',')[0]
+
+                # Longitude
+                lon = re.search('(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)', driver.find_element_by_css_selector('div.directions-btn.action-btn.col-md-7.col-md-offset-8 > a').get_attribute('href')).group().split(',')[1]
+
+                locations_ids.append(location_id)
+                locations_type.append(location_type)
+                locations_titles.append(locations_title)
+                street_addresses.append(street_address)
+                cities.append(city)
+                states.append(state)
+                zip_codes.append(zip_code)
+                phone_numbers.append(phone_number)
+                hours.append(hour)
+                countries.append(country)
+                latitude_list.append(lat)
+                longitude_list.append(lon)
+            except:
+                break
+
+
+        for (
+                locations_title,
+                street_address,
+                city,
+                state,
+                zipcode,
+                phone_number,
+                latitude,
+                longitude,
+                hour,
+                location_id,
+                country,
+                location_type
+        ) in zip(
+            locations_titles,
+            street_addresses,
+            cities,
+            states,
+            zip_codes,
+            phone_numbers,
+            latitude_list,
+            longitude_list,
+            hours,
+            locations_ids,
+            countries,
+            locations_type
+        ):
+            self.data.append(
+                [
+                    self.url,
+                    locations_title,
+                    street_address,
+                    city,
+                    state,
+                    zipcode,
+                    country,
+                    location_id,
+                    phone_number,
+                    location_type,
+                    latitude,
+                    longitude,
+                    hour,
+                ]
+            )
+
+        driver.quit()
+
+
+scrape = Scraper(URL)
+scrape.scrape()
+
+
