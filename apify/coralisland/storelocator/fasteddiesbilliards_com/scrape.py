25f33eb2a578a2ece134cefb80ccd11b2d5b7eec
coralisland-git <coralisland8327@gmail.com>
2019-09-17 04:51:55 +0700

    add new 10 scrapers for 7th

---
+++ apify/coralisland/storelocator/fasteddiesbilliards_com/scrape.py
@@ -0,0 +1,104 @@
+import csv
+import re
+import pdb
+import requests
+from lxml import etree
+import json
+import usaddress
+
+
+base_url = 'https://fasteddiesbilliards.com'
+
+def validate(item):    
+    if item == None:
+        item = ''
+    if type(item) == int or type(item) == float:
+        item = str(item)
+    if type(item) == list:
+        item = ' '.join(item)
+    return item.replace(u'\u2013', '-').encode('ascii', 'ignore').encode("utf8").strip()
+
+def get_value(item):
+    if item == None :
+        item = '<MISSING>'
+    item = validate(item)
+    if item == '':
+        item = '<MISSING>'    
+    return item
+
+def eliminate_space(items):
+    rets = []
+    for item in items:
+        item = validate(item)
+        if item != '' and item != '|':
+            rets.append(item)
+    return rets
+
+def parse_address(address):
+    address = usaddress.parse(address)
+    street = ''
+    city = ''
+    state = ''
+    zipcode = ''
+    for addr in address:
+        if addr[1] == 'PlaceName':
+            city += addr[0].replace(',', '') + ' '
+        elif addr[1] == 'ZipCode':
+            zipcode = addr[0].replace(',', '')
+        elif addr[1] == 'StateName':
+            state = addr[0].replace(',', '')
+        else:
+            street += addr[0].replace(',', '') + ' '
+    return { 
+        'street': get_value(street), 
+        'city' : get_value(city), 
+        'state' : get_value(state), 
+        'zipcode' : get_value(zipcode)
+    }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    output_list = []
+    url = "https://fasteddiesbilliards.com"
+    session = requests.Session()
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+    source = session.get(url, headers=headers).text
+    response = etree.HTML(source)
+    store_list = response.xpath('//div[@id="locations"]//a/@href')
+    for store_link in store_list[:-1]:
+        store = etree.HTML(session.get(store_link,headers=headers).text)
+        detail = eliminate_space(store.xpath('.//div[@id="info"]//div[@class="wpb_text_column wpb_content_element "]//text()'))
+        output = []
+        output.append(base_url) # url
+        address = detail[-1].split('|')
+        output.append(address[1].split(',')[0]) #location name
+        output.append(address[0]) #address
+        output.append(address[1].split(',')[0]) #city
+        output.append(address[1].split(',')[1]) #state
+        output.append('<MISSING>') #zipcode   
+        output.append('US') #country code
+        output.append("<MISSING>") #store_number
+        output.append(detail[-2]) #phone
+        output.append("") #location type
+        output.append("<MISSING>") #latitude
+        output.append("<MISSING>") #longitude
+        store_hours = validate(detail[:-2]).replace('18 years old and up, ', '').replace('|', '')
+        output.append(get_value(store_hours)) #opening hours
+        output_list.append(output)
+    return output_list
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
