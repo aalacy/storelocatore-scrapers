a6bc9c7dcc2663c8f4c503f9d4d4d8c4353c1087
nsxdarin <nsxdarin@gmail.com>
2019-09-19 22:12:33 -0500

    Create scrape.py

---
+++ apify/nsxdarin/storelocator/lululemon_com/scrape.py
@@ -0,0 +1,51 @@
+import csv
+import urllib2
+import requests
+
+session = requests.Session()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    url = 'https://info.lululemon.com/stores?mnid=ftr;en-US;store-locator'
+    locs = []
+    r = session.get(url, headers=headers)
+    for line in r.iter_lines():
+        if '<h1><a href="/stores/ca/' in line or '<h1><a href="/stores/us/' in line:
+            lurl = 'https://shop.lululemon.com' + line.split('href="')[1].split('"')[0]
+            locs.append(lurl)
+    print('Found %s Locations.' % str(len(locs)))
+    for loc in locs:
+        print('Pulling Location %s...' % loc)
+        website = 'lululemon.com'
+        typ = 'Store'
+        store = '<MISSING>'
+        r2 = session.get(loc, headers=headers)
+        for line2 in r2.iter_lines():
+            if '"openingHours":[' in line2:
+                hours = line2.split('"openingHours":[')[1].split(']')[0].replace('","','; ').replace('"','')
+                phone = line2.split('"telephone":"')[1].split('"')[0]
+                lat = line2.split(',"latitude":"')[1].split('"')[0]
+                lng = line2.split(',"longitude":"')[1].split('"')[0]
+                state = line2.split('"addressRegion":"')[1].split('"')[0]
+                country = line2.split('"addressCountry":"')[1].split('"')[0]
+                add = line2.split('"streetAddress":"')[1].split('"')[0]
+                city = line2.split(',"addressLocality":"')[1].split('"')[0]
+                name = line2.split('"name":"lululemon ')[1].split(' store')[0]
+                zc = line2.split(',"postalCode":"')[1].split('"')[0]
+                if 'null-null' in hours:
+                    hours = '<MISSING>'
+                yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
