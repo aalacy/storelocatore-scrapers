ad69111e3acaf8cfa2602c4e0e8983e0f715ee59
Tenzing Shaw <tenzing@safegraph.com>
2019-09-23 17:31:32 -0700

    import os

--- apify/nsxdarin/storelocator/lululemon_com/scrape.py
+++ apify/nsxdarin/storelocator/lululemon_com/scrape.py
@@ -1,6 +1,7 @@
 import csv
 import urllib2
 import requests
+import os
 
 session = requests.Session()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
