8ecb1de74b2ef5819610921b054ef37beab2537e
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2019-09-27 16:06:49 +0500

    Update scrape.py

--- apify/shumaila/storelocator/mardens_com/scrape.py
+++ apify/shumaila/storelocator/mardens_com/scrape.py
@@ -23,8 +23,8 @@ def get_driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    #return webdriver.Chrome('chromedriver', chrome_options=options)
-    return webdriver.Chrome('/Users/Dell/local/chromedriver', chrome_options=options)
+    return webdriver.Chrome('chromedriver', chrome_options=options)
+    #return webdriver.Chrome('/Users/Dell/local/chromedriver', chrome_options=options)
 
 def fetch_data():
     # Your scraper here
