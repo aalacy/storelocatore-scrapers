50335b3e9febf39b22423ee934446d7c195ac8d1
Heba Abd Elshafi <heba.abdelshafi1@gmail.com>
2019-09-28 16:34:38 +0200

    update scrape.py

--- apify/heba-abdelshafi/storelocator/barresoul_com/scrape.py
+++ apify/heba-abdelshafi/storelocator/barresoul_com/scrape.py
@@ -8,8 +8,8 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-driver=webdriver.Chrome('C:\chromedriver.exe', options=options)
-#driver = webdriver.Chrome("chromedriver", options=options)
+#driver=webdriver.Chrome('C:\chromedriver.exe', options=options)
+driver = webdriver.Chrome("chromedriver", options=options)
 
 
 def write_output(data):
@@ -23,13 +23,13 @@ def fetch_data():
     driver.get('https://www.barresoul.com')
 
     location_data_urls=[i.get_attribute('href') for i in driver.find_elements_by_xpath('//div[@id="headerNav"]//nav[@id="mainNavigation"]/div[@class="folder"][1]//div[@class="collection"]/a')]
-    
+
     data={'locator_domain':[],'location_name':[],'street_address':[],'city':[], 'state':[], 'zip':[], 'country_code':[], 'store_number':[],'phone':[], 'location_type':[], 'latitude':[], 'longitude':[], 'hours_of_operation':[],'page_url':[]}
     loc=[]
     for i in location_data_urls:
         driver.get(i)
         loc.append(driver.find_element_by_xpath('//div[@class="col sqs-col-6 span-6"]/div[contains(@class,"sqs-block")][@data-block-type="2"][contains(@id,"block")][2]').text.split('\n'))
-    
+
     for ind,i in enumerate(loc):
         if len(i)==3:
             data['street_address'].append(i[0])
@@ -37,7 +37,7 @@ def fetch_data():
             data['state'].append(i[1].split(',')[1].split()[0])
             data['zip'].append(i[1].split(',')[1].split()[1])
             data['phone'].append(i[2].split('|')[0])
-            
+
             data['locator_domain'].append('https://www.barresoul.com')
             data['country_code'].append('US')
             data['hours_of_operation'].append('<MISSING>')
@@ -47,8 +47,8 @@ def fetch_data():
             data['longitude'].append('<MISSING>')
             data['page_url'].append(location_data_urls[ind])
             data['location_name'].append(location_data_urls[ind].split('/')[-1])
-    
-    
+
+
         else:
             data['street_address'].append(i[1])
             data['city'].append(i[2].split(',')[0])
@@ -64,8 +64,8 @@ def fetch_data():
             data['longitude'].append('<MISSING>')
             data['page_url'].append(location_data_urls[ind])
             data['location_name'].append(location_data_urls[ind].split('/')[-1])
-    
-            
+
+
             data['street_address'].append(i[3])
             data['city'].append(i[4].split(',')[0])
             data['state'].append(i[4].split(',')[1].split()[0])
@@ -80,7 +80,7 @@ def fetch_data():
             data['longitude'].append('<MISSING>')
             data['page_url'].append(location_data_urls[ind])
             data['location_name'].append(location_data_urls[ind].split('/')[-1])
-    
+
 
 
     driver.close()
