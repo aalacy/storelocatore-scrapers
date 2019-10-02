1d5ffce8e21d21626d8dd49f12f698718cbd26f2
Heba Abd Elshafi <heba.abdelshafi1@gmail.com>
2019-10-02 20:14:56 +0200

    update scrape.py

--- apify/heba-abdelshafi/storelocator/snb_com/scrape.py
+++ apify/heba-abdelshafi/storelocator/snb_com/scrape.py
@@ -9,8 +9,8 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument("user-agent= 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'")
-#driver=webdriver.Chrome('C:\chromedriver.exe', options=options)
-driver = webdriver.Chrome("chromedriver", options=options)
+driver=webdriver.Chrome('C:\chromedriver.exe', options=options)
+#driver = webdriver.Chrome("chromedriver", options=options)
 
 
 def write_output(data):
@@ -20,13 +20,13 @@ def write_output(data):
 
 def fetch_data():
     data={'locator_domain':[],'location_name':[],'street_address':[],'city':[], 'state':[], 'zip':[], 'country_code':[], 'store_number':[],'phone':[], 'location_type':[], 'latitude':[], 'longitude':[], 'hours_of_operation':[],'page_url':[]}
-    driver.get('https://parknationalbank.com/locations/?view=all')    
+    driver.get('https://securitynationalbank.com/locations/?view=all')    
     
     location_data=[i.text.split('\n') for i in driver.find_elements_by_xpath('//div[@class="branch-info-container"]')]
     data['page_url']=[i.get_attribute('href') for i in driver.find_elements_by_xpath('//span[@class="sub-head fw-light"]/a')]
 
     for i in location_data:
-        data['locator_domain'].append('https://parknationalbank.com')[1]
+        data['locator_domain'].append('https://parknationalbank.com')
         data['country_code'].append('US')
         data['store_number'].append('<MISSING>')
         data['latitude'].append('<MISSING>')
