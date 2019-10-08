d7451b5222f58cb30fa5f7538eec384c0803dafc
Richard Chen <richard.chen.1989@gmail.com>
2019-10-07 23:20:00 -0700

    fix

--- apify/richard/storelocator/theshoecompany_ca/scrape.py
+++ apify/richard/storelocator/theshoecompany_ca/scrape.py
@@ -35,19 +35,31 @@ class Scraper(Scrape):
         results = pcdb.get_postalcodes_around_radius(pc, radius)
         postalcodes = [p.postalcode for p in results]
 
-        for postalcode in postalcodes:
-            url = f'https://www.theshoecompany.ca/api/v1/stores/search?address={postalcode}&distance=100&locale=en_CA&pushSite=TSL_SC&sites=TSL_SC'
+        headers = {
+            'sec-fetch-mode': 'cors',
+            'dnt': '1',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+            'accept': 'application/json, text/plain, */*',
+            'referer': 'https://www.theshoecompany.ca/en/ca/',
+            'authority': 'www.theshoecompany.ca',
+            'cookie': '_abck=5138B76242C076D43B0E17E7386E18F6~-1~YAAQJjPFFxJ43mNtAQAAF7hLewKmnC16vvtSHGOOLvrTkX8cCubc7dXzoGActX45X45QD/sR4udIS1NAs0dKJnXtyL1bDQUzvi3rGtz613uUDnRRGPGLXzj9FyINzjvY+xYi4Y9e2HGpd8WYB2aRDKkP8UTEmTB/NDNik5lLjg5oiHToC2L4Ofl+cKnnJ3DCxdp2PKIVRQoxDeb65iH2JFZvs0BKEYKN0bnUDjBExL2UfcKintYs5vZOqcAeperAgGGX+T+smxdpW8pxCO/BhHLOs5pJU3DpETvr7A==~-1~-1~-1; _evga_3157=f86941228417a184.; TIER=GUEST; BVBRANDID=a11874ba-26e4-4cfb-9ac7-8c841285fb35; cto_lwid=e34e3a00-5596-4f6f-b4bd-287466b4220d; _ga=GA1.2.1668866735.1569731626; AMCVS_6020C2D754D583630A4C98C6%40AdobeOrg=1; s_cc=true; _fbp=fb.1.1569731626165.845940868; s_fid=14386CAE18CC6428-2BD68C5034DC8FF8; bm_sz=2C95705200FC902755A3819754322DBE~YAAQ9zLFF2ORGYNtAQAAEuX9qQW5PDIoTvegnPXDxjunsjMcwurYmRQSwczTN6u0AekdV6tEJCB/wTM6Mkni3JyHqPM1ulr+Z3KRUoSyaOulPpFwxBi190Az2Fhb7BRX8uaQp6uGO1drFPaGxtaPtbU0XhldK5c2XNQIcQAo1rqeycTcPICLR0d+6Rf1WJW4Yx+Do7yGaA==; MOUS=9; ak_bmsc=FC098A1A21F0C57FFC7BD23E557FB94F17C532F77A3000006B289C5D50993271~plNDcvw/71OZUV0IN2QTPu9q9vF4f55haggE64JOtBUu4P2ckeTVzJYM4EExbWdre/9Jn8y4qdOLjLoPkwm8RYbDa8lmGoPstHadzfRw/lTMGGt/Is5wB+UCi9tFJ+i8xtsXIC3k7RgVeCnEbxkpdouqxMQVH9EewrP+EZX1JVCU2vRXxLWcV4QsqMc+RT1qoDmFMlFW8yw6MpZJBRB+uSP38Z57dZg6UJiPuFeiM2qGHYysXj7P7kH8xmzPTBZuvvHh9EzgIXLMrfXSaFzFUWMNhwFcEyUKArZcyN5t44+W4=; JSESSIONID=9QDZOHb8Egr8jnwcvdrfig8U.ATGPS02; _dynSessConf=6057608483931769894; BVBRANDSID=867853f7-45f5-4cca-b958-6a033844947b; _gid=GA1.2.412361150.1570515055; AMCV_6020C2D754D583630A4C98C6%40AdobeOrg=281789898%7CMCIDTS%7C18178%7CMCMID%7C08680147445304652151167050752523131749%7CMCAAMLH-1571119855%7C9%7CMCAAMB-1571119855%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1570522255s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C4.1.0; s_vnum=1572591600299%26vn%3D1; s_invisit=true; aa_lv_s=More%20than%207%20days; bm_sv=12EAE1353D7C724157A2024E4F7BF45D~7RwNnnelQzVDI3VDkMWdjeR6TO8wUiDNyNZ+iSjceDZV+l4LIuRVYD6qjxudB1E2CCPIWUtEZQMoYAqArdlL9zQsQePKe9/U5h0HQvzVFlbciELKX3UiBhsWJL9vYCoZbxWSJpAjoa+y7JEsoep9uvLGqLYbFlDwm2stryGz+q4=; utag_main=v_id:016d7b4bc2d8001b5b728918aa9103078002407000bd0$_sn:2$_ss:0$_st:1570517197917$vapi_domain:theshoecompany.ca$ses_id:1570515054907%3Bexp-session$_pn:1%3Bexp-session$_prevpage:tsc%3Bexp-1570518997921; aa_lv=1570515397928; s_getNewRepeat=1570515397929-Repeat; s_sq=%5B%5BB%5D%5D',
+            'sec-fetch-site': 'same-origin',
+        }
 
-            r = requests.get(
-                url=url,
-                headers={
-                    'cookie': 'bm_sz=3BB0478984C66DBCE6D52E5B9EE4ED74~YAAQJjPFFxF43mNtAQAAF7hLewVslpD0jhJmHDB4ihgg51cmdm2EmPuXiIHZX7IAhjFU5p5B4jWkLhKfAgfxGUPrL+io53l7I7WmSdDLdwUoSFcpzncVHe4QeG8HukYgW8+030zOL5ZjI3jxEvZAl2Io9NdY66K8pIwibCleC59zI833KMYzw/EArK5yjpxYD2YJDJ6bOw==; _abck=5138B76242C076D43B0E17E7386E18F6~-1~YAAQJjPFFxJ43mNtAQAAF7hLewKmnC16vvtSHGOOLvrTkX8cCubc7dXzoGActX45X45QD/sR4udIS1NAs0dKJnXtyL1bDQUzvi3rGtz613uUDnRRGPGLXzj9FyINzjvY+xYi4Y9e2HGpd8WYB2aRDKkP8UTEmTB/NDNik5lLjg5oiHToC2L4Ofl+cKnnJ3DCxdp2PKIVRQoxDeb65iH2JFZvs0BKEYKN0bnUDjBExL2UfcKintYs5vZOqcAeperAgGGX+T+smxdpW8pxCO/BhHLOs5pJU3DpETvr7A==~-1~-1~-1; _evga_3157=f86941228417a184.; JSESSIONID=U+7jKbzrYKrIK+wXrLp5OuNg.ATGPS08; TIER=GUEST; MOUS=3; _dynSessConf=5654863123688744674; BVBRANDID=a11874ba-26e4-4cfb-9ac7-8c841285fb35; BVBRANDSID=394a8f6c-e17e-4e29-a4bf-b81ed897f2ed; ak_bmsc=EFDF6D8C644167FB1687EF8B826C6E2E17C533264C2B00002634905DD03BBF51~pl8xGaniYcTpLiCCCrP/mmxw3m7YDhmIMS1o9Jcg7qDvYiJQEb858pHyPJWsalqanQGyhIf0BCzNucRqEtdorA4Zk1y6YT+X3AOZyTJpfcmGdMcsuOnRZ0QjmLrBQaqYvx1AuNPtvlpFr5jbw1h/wHHG6K3NsGacPbHeyktPPAfYOqk+kzE2BFRi3sCe33bjjWzS88dZmV78RgeP7N9v3WJlMOdopTd196caYfl/wivQbfczkMldPjFpmBaLEzofORlz9J4LWmFadCPZ+Q4l6+C0AYX7CKA9rJRiUwZnjyhGk=; cto_lwid=e34e3a00-5596-4f6f-b4bd-287466b4220d; _ga=GA1.2.1668866735.1569731626; _gid=GA1.2.1368967726.1569731626; AMCVS_6020C2D754D583630A4C98C6%40AdobeOrg=1; AMCV_6020C2D754D583630A4C98C6%40AdobeOrg=281789898%7CMCIDTS%7C18169%7CMCMID%7C08680147445304652151167050752523131749%7CMCAAMLH-1570336426%7C9%7CMCAAMB-1570336426%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1569738826s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C4.1.0; s_cc=true; _fbp=fb.1.1569731626165.845940868; s_fid=14386CAE18CC6428-2BD68C5034DC8FF8; s_vnum=1569913200686%26vn%3D1; s_invisit=true; aa_lv_s=First%20Visit; bm_sv=D0D36F4ECD99EA986BD3A619E625A970~HFneFsH+Q6C7kFleLAsnx8qMwhbIeg94nxdbR22uyH9/nUYjPh6rGbnlw/lPINKNJkIsfSNJ/WZLp5bnnI31vCVjeAO7X15lYkyfnumpDplRVQBJ+VNO2WCCycl1WWTeal+NT2g/oQo5+v06smkAkbpfXPk401Ehnt+kg1dFV10=; utag_main=v_id:016d7b4bc2d8001b5b728918aa9103078002407000bd0$_sn:1$_ss:0$_st:1569733455404$ses_id:1569731625689%3Bexp-session$_pn:1%3Bexp-session$_prevpage:tsc%3Bexp-1569735255405$vapi_domain:theshoecompany.ca; aa_lv=1569731655409; s_getNewRepeat=1569731655410-New; s_sq=%5B%5BB%5D%5D'
-                }
+        for postalcode in postalcodes:
+            params = (
+                ('address', postalcode.lower()),
+                ('distance', '100'),
+                ('locale', 'en_CA'),
+                ('pushSite', 'TSL_SC'),
+                ('sites', 'TSL_SC'),
             )
-            try:
-                stores.extend(json.loads(r.content)['Response']['sortedlist'].values())
-            except:
-                pass
+            r = requests.get('https://www.theshoecompany.ca/api/v1/stores/search', headers=headers, params=params)
+            data = json.loads(r.content)['Response']['sortedlist'].values()
+            stores.extend(data)
+            print(f'{len(data)} locations scraped for postal code {postalcode}')
 
 
         for store in stores:
