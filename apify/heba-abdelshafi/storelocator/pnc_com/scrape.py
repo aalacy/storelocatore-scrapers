47274880413937e31571c6c869906f8b15d74c12
Heba Abd Elshafi <heba.abdelshafi1@gmail.com>
2019-10-10 12:36:47 +0200

    fixed

--- apify/heba-abdelshafi/storelocator/pnc_com/scrape.py
+++ apify/heba-abdelshafi/storelocator/pnc_com/scrape.py
@@ -7,8 +7,8 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-driver=webdriver.Chrome('C:\chromedriver.exe')#, options=options)
-#driver = webdriver.Chrome("chromedriver", options=options)
+#driver=webdriver.Chrome('C:\chromedriver.exe', options=options)
+driver = webdriver.Chrome("chromedriver", options=options)
 
 
 def fetch_data():
