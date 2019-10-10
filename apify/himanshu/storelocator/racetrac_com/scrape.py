1cae8da7bd2746826a62c5a06b2ab64379eda245
diapk <Hank4957!>
2019-10-10 19:53:50 +0530

    docker run and push

--- apify/himanshu/storelocator/racetrac_com/scrape.py
+++ apify/himanshu/storelocator/racetrac_com/scrape.py
@@ -9,7 +9,7 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -20,30 +20,33 @@ def fetch_data():
     "Content-Type": "application/json; charset=UTF-8"
     }
     base_url = "https://racetrac.com"
-    data = '{"swLat":19.56371046529199,"swLng":-154.56903,"neLat":41.7744145203491,"neLng":-45.584655,"features":[]}'
+    data = '{"swLat":25.82303640133416,"swLng":-115.6947670532553,"neLat":41.79048379771046,"neLng":-61.202579553255305,"features":[]}'
     r = requests.post("https://racetrac.com/Services.asmx/Locate",headers=headers,data=data)
     return_main_object = []
     location_list = r.json()
+    store_ids = []
     for key in location_list:
         if "Stores" in location_list[key]:
             for store_data in location_list[key]["Stores"]:
-                if store_data['ZipCode'] == '30004': print(store_data) 
                 store = []
                 store.append("https://racetrac.com")
-                store.append(store_data['Address'])
+                store.append("<MISSING>")
                 store.append(store_data["Address"])
                 store.append(store_data['City'])
                 store.append(store_data['State'])
                 store.append(store_data["ZipCode"])
                 store.append("US")
-                store.append(store_data["ID"])
-                store.append(store_data["PhoneNumber"] if store_data["PhoneNumber"] != "" else "<MISSING>")
-                store.append("race trac")
-                store.append(store_data["Latitude"])
-                store.append(store_data["Longitude"])
+                store.append(store_data["StoreNumber"])
+                if store[-1] in store_ids:
+                    continue
+                store_ids.append(store[-1])
+                store.append(store_data["PhoneNumber"].split("x")[0] if store_data["PhoneNumber"] else "<MISSING>")
                 store.append("<MISSING>")
-                return_main_object.append(store)
-    return return_main_object
+                store.append(store_data["Latitude"] if store_data["Latitude"] != 0 else "<MISSING>")
+                store.append(store_data["Longitude"] if store_data["Longitude"] != 0 else "<MISSING>")
+                store.append("<MISSING>")
+                store.append("<MISSING>")
+                yield store
 
 def scrape():
     data = fetch_data()
