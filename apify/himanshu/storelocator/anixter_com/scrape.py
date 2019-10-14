e8bb1a7830cd2fd6a87a5fcbdbdec306f2655e1a
diapk <Hank4957!>
2019-10-14 19:56:49 +0530

    docker run and push

--- apify/himanshu/storelocator/anixter_com/scrape.py
+++ apify/himanshu/storelocator/anixter_com/scrape.py
@@ -1,60 +1,125 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 import re
 import json
+import time
+from random import randrange
 
 def write_output(data):
     with open('data.csv', mode='w',encoding="utf-8") as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","raw_address"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    return webdriver.Firefox(executable_path='./geckodriver', options=options)
 
 def fetch_data():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+    }
+    driver = get_driver()
+    driver.get("https://www.anixter.com")
     base_url = "https://www.anixter.com"
-    r = requests.get("https://fusiontables.googleusercontent.com/embedviz?viz=CARD&q=select+*+from+1vpAly2VF-wmZjM8YJD9GD8hzZKkdkCBVu81lI-pb&tmplt=11&cpr=2")
-    soup = BeautifulSoup(r.text,"lxml")
-    return_main_object = []
-    location_addresses = []
-    for location in soup.find_all("td"):
-        location_details = list(location.find("div",{"style":"height:170px"}).stripped_strings)
-        if location.find("a") == None:
-            geo_location = ""
-        else:
-            geo_location = location.find("a")["href"]
-        store = []
-        store.append("https://www.anixter.com")
-        store.append(location_details[0])
-        store.append("<INACCESSIBLE>")
-        store.append("<INACCESSIBLE>")
-        store.append("<INACCESSIBLE>")
-        store.append("<INACCESSIBLE>")
-        location_address = ""
-        for k in range(1,len(location_details)):
-            if location_details[k] == "Solutions":
-                break
-            location_address = location_address + " " + location_details[k]
-        store.append("US")
-        store.append("<MISSING>")
-        phone = ""
-        for k in range(len(location_details)):
-            if len(location_details[k].split(":")) == 2:
-                phone = location_details[k].split(":")[1]
-        store.append(phone.split("|")[0] if phone != "" and phone != " NA" else "<MISSING>")
-        store.append("anixer")
-        store.append(geo_location.split("/")[-1].split(",")[0] if len(geo_location.split("/")) < 3 and len(geo_location.split("/")[-1].split(",")) > 1 else "<MISSING>")
-        store.append(geo_location.split("/")[-1].split(",")[1] if len(geo_location.split("/")) < 3 and len(geo_location.split("/")[-1].split(",")) > 1 else "<MISSING>")
-        store.append("<MISSING>")
-        if location_address in location_addresses:
-            continue
-        location_addresses.append(location_address)
-        store.append(location_address)
-        return_main_object.append(store)
-    return return_main_object
+    cookies = driver.get_cookies()
+    addresses = []
+    s = requests.Session()
+    for cookie in cookies:
+        s.cookies.set(cookie['name'], cookie['value'])
+    for country in ["usa","canada"]:
+        r = s.get("https://www.anixter.com/bin/locationList?locationPath=/content/anixter/en_us/about-us/contact-us/global-locations-contact-info/" + country,headers=headers)
+        for state in r.json():
+            state_request = s.get(base_url + state["href"],headers=headers)
+            state_soup = BeautifulSoup(state_request.text,"lxml")
+            for location in state_soup.find_all("a",{"title":"More Details"}):
+                # print(base_url + location["href"])
+                location_request = s.get(base_url + location["href"],headers=headers)
+                location_soup = BeautifulSoup(location_request.text,"lxml")
+                iframe_url = repr(location_soup.find_all("iframe")[-1]["src"]).replace('"',"").replace(r"\r","").replace(r"\n","")
+                # print(iframe_url)
+                random_number = randrange(3,6)
+                time.sleep(random_number)
+                if iframe_url[1] == "/":
+                    location_details = list(location_soup.find("table").stripped_strings)
+                    geo_location = location_soup.find("a",{"title":"Get Directions"})
+                    if geo_location:
+                        lat = geo_location["href"].split("/")[-1].split(",")[0]
+                        lng = geo_location["href"].split("/")[-1].split(",")[1].replace("%0A","")
+                else:
+                    driver.get(iframe_url)
+                    iframe_soup = BeautifulSoup(driver.page_source,"lxml")
+                    location_details = list(iframe_soup.find("table").stripped_strings)
+                    geo_location = iframe_soup.find_all("iframe")[-1]["src"]
+                    lat = geo_location.split("q=")[1].split("%20")[0] if geo_location.split("q=")[1].split("%20")[0] else "<MISSING>"
+                    lng = geo_location.split("q=")[1].split("%20")[1].split("&")[0] if geo_location.split("q=")[1].split("%20")[1].split("&")[0] else "<MISSING>"
+                if "," not in location_details[2]:
+                    location_details[1] = location_details[1] + " " + location_details[2]
+                    del location_details[2]
+                if re.findall(r"\b[0-9]{5}(?:-[0-9]{4})?\b",location_details[2]) == []:
+                    if re.findall("([A-Z]{2})",location_details[2]) == []:
+                        location_details[1] = location_details[1] + " " + location_details[2]
+                        del location_details[2]
+                state_split = re.findall("([A-Z]{2})",location_details[2])
+                if state_split:
+                    state = state_split[-1]
+                else:
+                    state = "<MISSING>"
+                if country == "usa":
+                    store_zip_split = re.findall(r"\b[0-9]{5}(?:-[0-9]{4})?\b",location_details[2])
+                    if store_zip_split:
+                        store_zip = store_zip_split[-1]
+                    else:
+                        store_zip = "<MISSING>"
+                else:
+                    store_zip_split = re.findall(r'[A-Z]{1}[0-9]{1}[A-Z]{1}\s*[0-9]{1}[A-Z]{1}[0-9]{1}',location_details[2])
+                    if store_zip_split:
+                        store_zip = store_zip_split[-1]
+                    else:
+                        store_zip = "<MISSING>"
+                phone = ""
+                for detail in location_details:
+                    if re.findall(re.compile(".?(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}).?"),detail):
+                        phone = re.findall(re.compile(".?(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}).?"),detail)[-1]
+                        break
+                hours = ""
+                for i in range(len(location_details)):
+                    if "Operating Hours" in location_details[i]:
+                        for j in range(i+1,len(location_details)):
+                            if "Solutions" in location_details[j]:
+                                break
+                            hours = hours + " "  + location_details[j]
+                        break
+                name = location_soup.find("h1").text.strip()
+                store = []
+                store.append("https://www.anixter.com")
+                store.append(name)
+                store.append(location_details[1])
+                if store[-1] in addresses:
+                    continue
+                addresses.append(store[-1])
+                store.append(location_details[2].split(",")[0])
+                store.append(state)
+                store.append(store_zip)
+                store.append("US" if country == "usa" else "CA")
+                store.append("<MISSING>")
+                store.append(phone if phone else "<MISSING>")
+                store.append("<MISSING>")
+                store.append(lat)
+                store.append(lng)
+                store.append(hours if hours else "<MISSING>")
+                store.append(base_url + location["href"])
+                yield store
 
 def scrape():
     data = fetch_data()
--- apify/himanshu/storelocator/galvanize_com/scrape.py
+++ apify/himanshu/storelocator/galvanize_com/scrape.py
@@ -2,69 +2,94 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 import re
-import io
 import json
+# import sgzip
+# import time
+
 
 def write_output(data):
     with open('data.csv', mode='w', encoding="utf-8") as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(output_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
-    }
-    base_url = "https://www.galvanize.com/"
-    r = requests.get(base_url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
     return_main_object = []
-    exists = soup.find('div', {'class', 'nav-items'})
-    if exists:
-        all_loc = exists.findAll('div')[-2]
-        for data in all_loc.findAll('a'):
-            print("https://www.galvanize.com" + data.get("href"))
-            detail_url = requests.get("https://www.galvanize.com" + data.get("href"), headers=headers)
-            detail_soup = BeautifulSoup(detail_url.text, "lxml")
-            location_name = detail_soup.find('h3').get_text().strip()
-            detail_exists = detail_soup.find('div', {'class', 'description-section'})
-            if detail_exists.findAll('p', {'class', 'callout'}):
-                address = detail_soup.findAll('p', {'class', 'callout'})[2].text.replace('\n', '').replace('        ', ' ').strip().split(' ')
-                city = data.text
-                street_address = ' '.join(address[:-2])
-                state = address[-2]
-                zip = address[-1]
-                phone = detail_soup.findAll('p', {'class', 'callout'})[1].get_text().strip()
-            else:
-                city = "<MISSING>"
-                street_address = "<MISSING>"
-                state = "<MISSING>"
-                zip = "<MISSING>"
-                phone = "<MISSING>"
-            store = []
-            store.append("https://www.galvanize.com" + data.get("href"))
-            store.append(location_name)
-            store.append(street_address)
-            store.append(city)
-            store.append(state)
-            store.append(zip)
-            store.append("US")
-            store.append("<MISSING>")
-            store.append(phone)
-            store.append("Galvanize")
-            store.append("<MISSING>")
-            store.append("<MISSING>")
-            store.append("<MISSING>")
-            return_main_object.append(store)
-        return return_main_object
-    else:
-        pass
+    addresses = []
+
+
+
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+        "accept": "application/json, text/javascript, */*; q=0.01",
+        # "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    }
+
+    # it will used in store data.
+    locator_domain = "https://www.galvanize.com"
+    location_name = ""
+    street_address = "<MISSING>"
+    city = "<MISSING>"
+    state = "<MISSING>"
+    zipp = "<MISSING>"
+    country_code = "US"
+    store_number = "<MISSING>"
+    phone = "<MISSING>"
+    location_type = "<MISSING>"
+    latitude = "<MISSING>"
+    longitude = "<MISSING>"
+    raw_address = ""
+    hours_of_operation = "<MISSING>"
+    page_url = "<MISSING>"
+
+
+
+    r= requests.get('https://www.galvanize.com',headers = headers)
+    soup = BeautifulSoup(r.text,'html.parser')
+    li = soup.find('li',{'data-tag':'Campuses'}).find_all('a')
+    for i in li:
+        r_loc = requests.get(locator_domain + i['href'],headers = headers)
+        soup = BeautifulSoup(r_loc.text,'lxml')
+        loc = soup.find('textarea',{'id':'state'}).text
+        script_text = loc.split(',"location":')[-1].split('}')[0]+"}"
+        json_data = eval(script_text)
+        latitude = json_data['lat']
+        longitude = json_data['lon']
+        zipp = json_data['zip']
+        city = json_data['city']
+        location_name = json_data['name']
+        phone = json_data['phone']
+        state = json_data['state']
+        street_address = json_data['address']
+        page_url = locator_domain + i['href']
+
+
+
+        store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
+                     store_number, phone, location_type, latitude, longitude, hours_of_operation,page_url]
+        store = ["<MISSING>" if x == "" or x == "Blank" else x for x in store]
+
+        # print("data = " + str(store))
+        # print(
+        #     '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+        return_main_object.append(store)
+
+    return return_main_object
+
+
+
 
 def scrape():
     data = fetch_data()
     write_output(data)
+
+
 scrape()
