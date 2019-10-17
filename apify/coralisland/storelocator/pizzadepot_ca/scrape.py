e36a31ff5784c9f8fbbd24405a00e86a60d95d46
coralisland-git <coralisland8327@gmail.com>
2019-10-17 16:21:59 +0700

    update pizzadepot

--- apify/coralisland/storelocator/pizzadepot_ca/scrape.py
+++ apify/coralisland/storelocator/pizzadepot_ca/scrape.py
@@ -39,9 +39,9 @@ def fetch_data():
     output_list = []
     url = "http://pizzadepot.ca/location/"
     session = requests.Session()
+    init_request = session.get('http://pizzadepot.ca/index')
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-        'Cookie': 'PHPSESSID=44fb602268af9b54a98adc8b5f5b7dba',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
     }
