8e8f37e34b8217c8e42f5307e3e3325102972a31
Richard Chen <rca@fb.com>
2019-10-22 14:53:00 -0700

    fix

--- apify/richard/storelocator/53_com/scrape.py
+++ apify/richard/storelocator/53_com/scrape.py
@@ -30,17 +30,17 @@ class Scraper(Scrape):
         )
         data = None
         for iteration in range(1, 4):
-            async with aiohttp.ClientSession(headers=self.headers) as session:
-                async with session.get(url='https://tcjl25l2al.execute-api.us-east-1.amazonaws.com/prod', params=params, verify_ssl=False) as data:
-                    try:
+            try:
+                async with aiohttp.ClientSession(headers=self.headers) as session:
+                    async with session.get(url='https://tcjl25l2al.execute-api.us-east-1.amazonaws.com/prod', params=params, verify_ssl=False) as data:
                         data = await data.json()
                         break
-                    except:
-                        if iteration == 3:
-                            print(f'Last try: Tried getting info {iteration} time(s) for {zipcode}.')
-                        else:
-                            print(f'Tried getting info {iteration} time(s) for {zipcode}.')
-                        pass
+            except:
+                if iteration == 3:
+                    print(f'Last try: Tried getting info {iteration} time(s) for {zipcode}.')
+                else:
+                    print(f'Tried getting info {iteration} time(s) for {zipcode}.')
+                pass
         if data:
             if 'locations' in data.keys():
                 self.stores.extend(data['locations'])
