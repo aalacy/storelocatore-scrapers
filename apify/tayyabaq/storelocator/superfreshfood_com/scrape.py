ed48eea62e56926ad8a36d5024ac00d95c51f5eb
Tayyaba Qamar <52938783+TayyabaQ@users.noreply.github.com>
2019-10-24 22:22:19 +0500

    Update scrape.py

--- apify/tayyabaq/storelocator/superfreshfood_com/scrape.py
+++ apify/tayyabaq/storelocator/superfreshfood_com/scrape.py
@@ -3,6 +3,10 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import re, time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
