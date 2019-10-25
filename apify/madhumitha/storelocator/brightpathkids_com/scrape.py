4ccd84b12ee958436d172cc42a6b50ede5b954ff
Madhumitha <51913980+madhuram17@users.noreply.github.com>
2019-10-25 22:05:50 +0530

    Update scrape.py

--- apify/madhumitha/storelocator/brightpathkids_com/scrape.py
+++ apify/madhumitha/storelocator/brightpathkids_com/scrape.py
@@ -6,7 +6,7 @@ DOMAIN = 'https://brightpathkids.com'
 MISSING = '<MISSING>'
 
 def write_output(data):
-    with open('data.csv', mode='w', encoding='utf-8') as output_file:
+    with open('data.csv', mode='w', encoding='utf-8', newline='') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
