03f720dbc0d552bf1eed49561b751f4538ad2e2d
Tenzing Shaw <tenzing@safegraph.com>
2019-10-25 11:50:26 -0700

    add retries and timeouts

--- apify/himanshu/curatedsources/tpl_org/scrape.js
+++ apify/himanshu/curatedsources/tpl_org/scrape.js
@@ -1,10 +1,16 @@
-const request=require('request');
 const Apify = require('apify');
 const esriUtils = require('@esri/arcgis-to-geojson-utils');
 const epsg = require('epsg');
 const reproject = require('reproject');
 const wicket = require('wicket');
 const polygonCenter = require('geojson-polygon-center')
+const axios = require('axios');
+const {default: PQueue} = require('p-queue');
+const axiosRetry = require('axios-retry');
+
+axiosRetry(axios, { retries: 3, shouldResetTimeout: true, retryCondition: (error) => {
+  return axiosRetry.isNetworkOrIdempotentRequestError(error) || error.code === 'ECONNABORTED';
+}});
 
 function esriJsonEpsg3857ToGeojsonEpsg4326(esriJson) {
 	const geoJson = esriUtils.arcgisToGeoJSON(esriJson);
@@ -21,65 +27,78 @@ function geoJsonToWkt(geoJson) {
 	return parsed.write();
 }
 
-let items=[]
-let stop=false
-let cnt=1
-let req=1
-let errors = 0;
-
-async function scrape(){
-	return new Promise(async (resolve,reject) => {
-		setInterval(()=> {
-			for (i=0;i<500;i++) {
-				if (req<500) {
-					req=req+1;
-					request(`https://server3.tplgis.org/arcgis3/rest/services/ParkServe/ParkServe_Parks/MapServer/0/${cnt}?f=pjson`,(err,res)=>{
-						try {
-							let data=JSON.parse(res.body)
-							if (data.hasOwnProperty("error")) {
-								stop=true
-								clearInterval(this)
-								resolve(items)
-							} else {
-								const esriJson = data['feature']['geometry'];
-								const geoJson = esriJsonEpsg3857ToGeojsonEpsg4326(esriJson);
-								const centroid = geoJsonToCentroid(geoJson);
-								const polygonWkt = geoJsonToWkt(geoJson);
-								const item = {
-									locator_domain: "https://www.tpl.org",
-									location_name:data["feature"]["attributes"]["Park_Name"]?data["feature"]["attributes"]["Park_Name"]:"<MISSING>",
-									street_address: data["feature"]["attributes"]["Park_Address_1"]?data["feature"]["attributes"]["Park_Address_1"]:"<MISSING>",
-									city:data["feature"]["attributes"]["Park_UrbanArea"]?data["feature"]["attributes"]["Park_UrbanArea"]: "<MISSING>",
-									state:data["feature"]["attributes"]["Park_UrbanArea"]?data["feature"]["attributes"]["Park_UrbanArea"]:"<MISSING>",
-									zip:"<MISSING>",
-									country_code:"US",
-									store_number:data["feature"]["attributes"]["ParkID"]?data["feature"]["attributes"]["ParkID"]:"<MISSING>",
-									phone:"<MISSING>",
-									location_type: "<MISSING>",
-									latitude: centroid[1],
-									longitude: centroid[0],
-									hours_of_operation:"<MISSING>",
-									page_url:`https://server3.tplgis.org/arcgis3/rest/services/ParkServe/ParkServe_Parks/MapServer/0/${cnt}?f=pjson`,
-									wkt:polygonWkt
-								}
-								items.push(item);
-								req=req-1
-							}
-						} catch(exce) {
-							console.log(res.body);
-							console.log(exce);
-							errors++;
-							console.log(`error count: ${errors}`);
-						}
-					})
-					cnt=cnt+1;
-				}
-			}
-		},5000)
-	});
+function parseResult(data, itemIndex) {
+	const parkId = data["feature"]["attributes"]["ParkID"]?data["feature"]["attributes"]["ParkID"]:"<MISSING>"
+	const esriJson = data['feature']['geometry'];
+	const geoJson = esriJsonEpsg3857ToGeojsonEpsg4326(esriJson);
+	const centroid = geoJsonToCentroid(geoJson);
+	const polygonWkt = geoJsonToWkt(geoJson);
+	const item = {
+		locator_domain: "https://www.tpl.org",
+		location_name:data["feature"]["attributes"]["Park_Name"]?data["feature"]["attributes"]["Park_Name"]:"<MISSING>",
+		street_address: data["feature"]["attributes"]["Park_Address_1"]?data["feature"]["attributes"]["Park_Address_1"]:"<MISSING>",
+		city:data["feature"]["attributes"]["Park_UrbanArea"]?data["feature"]["attributes"]["Park_UrbanArea"]: "<MISSING>",
+		state:data["feature"]["attributes"]["Park_UrbanArea"]?data["feature"]["attributes"]["Park_UrbanArea"]:"<MISSING>",
+		zip:"<MISSING>",
+		country_code:"US",
+		store_number:parkId,
+		phone:"<MISSING>",
+		location_type: "<MISSING>",
+		latitude: centroid[1],
+		longitude: centroid[0],
+		hours_of_operation:"<MISSING>",
+		page_url:`https://server3.tplgis.org/arcgis3/rest/services/ParkServe/ParkServe_Parks/MapServer/0/${itemIndex}?f=pjson`,
+		wkt:polygonWkt
+	}
+	return item;
 }
 
+const BASE_URL = 'https://server3.tplgis.org/arcgis3/rest/services/ParkServe/ParkServe_Parks/MapServer/0'
+
+let errors = 0;
+async function scrapeItem(itemIndex) {
+	if (itemIndex % 1000 == 0) console.log(itemIndex);
+	try {
+		const url = `${BASE_URL}/${itemIndex}?f=pjson`;
+		const res = await axios.get(url, { timeout: 20000 });
+		const data = res.data;
+		if (!data || data.hasOwnProperty("error")) {
+			return undefined;
+		} else {
+			return parseResult(data, itemIndex);
+		}
+	} catch(exce) {
+		console.log(`itemIndex: ${itemIndex}`);
+		if (typeof res !== 'undefined') console.log(res.body);
+		console.log(exce);
+		errors++;
+		console.log(`error count: ${errors}`);
+	}
+}
+
+async function scrapeBatch(startIndex, size) {
+	const queue = new PQueue({concurrency: 100});
+	let promises = [];
+	for (let i = 0; i < size; i++) {
+		const itemIndex = startIndex + i;
+		promises.push(() => { return scrapeItem(itemIndex) });
+	}
+	const results = await queue.addAll(promises);
+	return results.filter((result) => !!result);
+}
+
+const BATCH_SIZE = 10000;
+
 Apify.main(async () => {
-	const data = await scrape();
-	await Apify.pushData(data);
+	let stop = false;
+	let startIndex = 1;
+	while (!stop) {
+		const currentBatch = await scrapeBatch(startIndex, BATCH_SIZE);
+		if (currentBatch.length < BATCH_SIZE / 2) {
+			stop = true;
+		}
+		startIndex += BATCH_SIZE;
+		console.log(`${currentBatch.length} items retrieved`);
+		await Apify.pushData(data);
+	}
 });
