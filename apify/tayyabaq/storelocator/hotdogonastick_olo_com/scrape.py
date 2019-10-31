71cd0c1474ed93c98920837ea98c57e9cc94e778
Tayyaba Qamar <52938783+TayyabaQ@users.noreply.github.com>
2019-10-31 19:25:15 +0500

    Update scrape.py

--- apify/tayyabaq/storelocator/hotdogonastick_olo_com/scrape.py
+++ apify/tayyabaq/storelocator/hotdogonastick_olo_com/scrape.py
@@ -9,12 +9,16 @@ def write_output(data):
     with open('data.csv', mode='wb') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain","page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             if row:
                 writer.writerow(row)
-                
+def parse_geo(url):
+    lon = re.findall(r'\,(--?[\d\.]*)', url)[0]
+    lat = re.findall(r'\&ll=(--?[\d\.]*)', url)[0]
+    return lat,lon
+                     
 def get_driver():
     options = Options() 
     options.add_argument('--headless')
@@ -23,60 +27,45 @@ def get_driver():
     return webdriver.Chrome('chromedriver', chrome_options=options)
 
 def fetch_data():
-    data=[];store_no=[];country=[];location_name=[];address_stores=[]; city=[];street_address=[]; zipcode=[]; state=[]; latitude=[]; longitude=[]; hours_of_operation=[]; phone=[];location_type=[]
+    data=[]; location_name=[];address_stores=[]; city=[];street_address=[]; zipcode=[]; state=[]; latitude=[]; longitude=[]; hours_of_operation=[]; phone=[]
     driver = get_driver()
-    countries = ['https://www.gucci.com/us/en/store', 'https://www.gucci.com/ca/en/store']
-    for n in range(0,len(countries)):
-        driver.get(countries[n])
+    driver.get('https://hotdogonastick.olo.com/locations')
+    location = driver.find_elements_by_xpath('//a[contains(@href,"/locations/")]')
+    location_href=[location[n].get_attribute('href') for n in range(0,len(location))]
+    for n in range(0,len(location_href)):
+        driver.get(location_href[n])
         time.sleep(3)
-        location = driver.find_elements_by_xpath('//h3[@class="name"]/a')
-        for n in range(0,len(location)):
-            location_name.append(location[n].text)
-        lat_lon = driver.find_elements_by_xpath('//ol[@id="stores"]/li')
-        for n in range(0,len(lat_lon)):
-            latitude.append(lat_lon[n].get_attribute('data-latitude'))
-            longitude.append(lat_lon[n].get_attribute('data-longitude'))
-            store_no.append(lat_lon[n].get_attribute('data-store-code'))
-            location_type.append(lat_lon[n].get_attribute('data-store-type'))
-        address = driver.find_elements_by_class_name('address')
-        for n in range(0,len(address)):
-            street_address.append(address[n].text.split("\n")[0])
-            zipcode.append(address[n].text.split("\n")[1].split(",")[-2])
-            country.append(address[n].text.split("\n")[1].split(",")[-1])
-            city.append(address[n].text.split("\n")[1].split(",")[0])
-        for n in range(0,len(address)):
-            a=address[n].text.split("\n")[1].split(",")[1].strip()
-            if bool(re.match('^[0-9]+$',str(a)))==True:
-                state.append('<MISSING>')
-            else:
-                state.append(a)
-        info = driver.find_elements_by_xpath('//div[@class="store-infos"]')
-        phones = driver.find_elements_by_xpath('//div[@class="store-infos"]/div[1]')
-        for n in range(0,len(info)):
-            if "T:" in info[n].text:
-                try:
-                    phone.append(phones[n].text.split("T:")[1])
-                except:
-                    phone.append('<MISSING>')
-            else:
-                phone.append('<MISSING>')
-        stores = driver.find_elements_by_xpath('//h3[@class="name"]/a')
-        links = [stores[n].get_attribute('href') for n in range(0,len(stores))]
-        for n in range(0,len(links)):
-            driver.get(links[n])
-            hours_of_operation.append(driver.find_element_by_class_name('store-detail-store-hours-hour').text)
+        loc = driver.find_elements_by_xpath('//li[@class="vcard"]/h2/a/span')
+        address =driver.find_elements_by_xpath('//div[contains(@class,"streetaddress")]')
+        cities = driver.find_elements_by_class_name('locality')
+        states = driver.find_elements_by_class_name('region')
+        phones = driver.find_elements_by_class_name('location-tel-number')
+        hours = driver.find_elements_by_class_name('location-hours')
+        lat = driver.find_elements_by_xpath('//span[@class="latitude"]/span')
+        lon = driver.find_elements_by_xpath('//span[@class="longitude"]/span')
+        for n in range(0,len(loc)):
+            location_name.append(loc[n].text)
+            page_url.append(location_href[n])
+            street_address.append(address.split("\n")[0])
+            city.append(cities[n].text)
+            state.append(states[n].text)
+            phone.append(phones[n].text)
+            latitude.append(lat[n].get_attribute('title'))
+            longitude.append(lon[n].get_attribute('title'))
+            hours_of_operation.append(hours[n].text)
     for n in range(0,len(street_address)): 
         data.append([
-            'https://www.gucci.com',
+            'https://hotdogonastick.olo.com/',
+            page_url[n],
             location_name[n],
             street_address[n],
             city[n],
             state[n],
-            zipcode[n],
-            country[n],
-            store_no[n],
+            '<MISSING>',
+            'US',
+            '<MISSING>',
             phone[n],
-            location_type[n],
+            '<MISSING>',
             latitude[n],
             longitude[n],
             hours_of_operation[n]
