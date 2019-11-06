2fab4e1454f465bacda57722368408bd46230604
Tayyaba Qamar <52938783+TayyabaQ@users.noreply.github.com>
2019-11-06 19:06:15 +0500

    Update scrape.py

--- apify/tayyabaq/storelocator/superfreshfood_com/scrape.py
+++ apify/tayyabaq/storelocator/superfreshfood_com/scrape.py
@@ -1,93 +1,62 @@
 import csv
-import os
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import re, time
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+import time
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
+    with open('data.csv', mode='wb') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         # Header
-        writer.writerow(["locator_domain", "page_url","location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain","page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             if row:
                 writer.writerow(row)
-                
-def parse_geo(url):
-    try:
-        lon = re.findall(r'\,(--?[\d\.]*)', url)[0]
-    except:
-        lon="<MISSING>"
-    try:
-        lat = re.findall(r'\=(-?[\d\.]*\,)', url)[0]
-    except:
-        lat="<MISSING>"
-    return lat, lon
 
-def get_driver():
-    options = Options() 
-    #options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    return webdriver.Chrome('chromedriver', chrome_options=options)
 
 def fetch_data():
-    data=[]; location_name=[];address_stores=[]; city=[];street_address=[]; zipcode=[]; state=[]; latitude=[]; longitude=[]; hours_of_operation=[]; phone=[]
-    #Driver
-    driver = get_driver()
-    driver.get('http://keyfood.mywebgrocer.com/StoreLocator.aspx?s=723953668&g=2d6a1d25-bf8f-4056-8736-269d1b9db7cf&uc=581EF7')
-    wait = WebDriverWait(driver, 10)
-    men_menu = wait.until(ec.visibility_of_element_located((By.XPATH, "//select[@name='selStates']/option")))
-    ActionChains(driver).move_to_element(men_menu).perform()
-    state_opt = driver.find_elements_by_xpath("//select[@name='selStates']/option")
-    state_options = [state_opt[n].text for n in range(0,len(state_opt))]
-    for n in range(1,len(state_options)):
-        time.sleep(3)
-        driver.find_element_by_xpath("//select[@name='selStates']/option[text()='%s']"%state_options[n]).click()
-        driver.find_element_by_xpath("//select[@name='selZipCodeRadius']/option[text()='20 Miles']").click()
-        driver.find_element_by_class_name('submitButton').click()
-        location = driver.find_elements_by_class_name('StoreTitle')
-        for n in range(0,len(location)):
-            location_name.append(location[n].text)
-        time.sleep(3)
-        lat_lon = driver.find_elements_by_class_name('SingleItemLinkText')
-        for n in range(0,len(lat_lon)):
-            lat,lon = parse_geo(lat_lon[n].get_attribute('href'))
-            latitude.append(lat)
-            longitude.append(lon)
-        address2=driver.find_elements_by_xpath("//div[@class='StoreAddress']/p[2]")
-        address1 = driver.find_elements_by_xpath("//div[@class='StoreAddress']/p[1]")
-        for n in range(0,len(address1)):
-            street_address.append(address1[n].text)
-        for n in range(0,len(address2)):
-            city.append(address2[n].text.split(",")[0])
-            state.append(address2[n].text.split(",")[1].split()[0])
-            zipcode.append(address2[n].text.split(",")[1].split()[1])
-        hours = driver.find_elements_by_class_name('StoreHours')
-        for n in range(0,len(hours)):
-            hours_of_operation.append(hours[n].text)
-        phones = driver.find_elements_by_xpath("//div[@class='StoreContact']")
-        for n in range(0,len(phones)):
-            try:
-                phone.append(phones[n].text.split("Phone: ")[1].split("\n")[0])
-            except:
-                phone.append("<MISSING>")
-    for n in range(0,len(location_name)):
+    data=[];hours_of_operation=[]; latitude=[];longitude=[];zipcode=[];location_name=[];city=[];street_address=[]; state=[]; phone=[];country=[];store_no=[]
+    headers = {
+        "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+    }
+    page = 0
+    while True:
+        r = requests.get("https://keyfoodstores.keyfood.com/store/keyFood/en/store-locator?q=11756&page=" + str(page) + "&radius=5000000000&all=true",headers=headers)
+        store_data = r.json()["data"]
+        for content in store_data:
+            location_name.append(content["displayName"])
+            if content["line2"]:
+                street_address.append(content["line1"] + " " + content["line2"])
+            else:
+                street_address.append(content["line1"])
+            city.append(content["town"])
+            state.append(content["state"])
+            zipcode.append(content["postalCode"])
+            country.append("US")
+            store_no.append(content["name"])
+            phone.append(content["phone"] if content["phone"] else "<MISSING>")
+            latitude.append(content["latitude"])
+            longitude.append(content["longitude"])
+            hours=""
+            for hour in content["openings"]:
+                hours = hours + " " + hour + " " + content["openings"][hour]
+            hours_of_operation.append(hours if hours else "<MISSING>")
+        if len(data) < 250:
+            break
+        page = page + 1
+    for n in range(0,len(location_name)): 
         data.append([
             'http://superfreshfood.com',
-            'http://keyfood.mywebgrocer.com/StoreLocator.aspx?s=723953668&g=2d6a1d25-bf8f-4056-8736-269d1b9db7cf&uc=581EF7',
+            '<MISSING>',
             location_name[n],
             street_address[n],
             city[n],
             state[n],
             zipcode[n],
             'US',
-            '<MISSING>',
+            store_no[n],
             phone[n],
             '<MISSING>',
             latitude[n],
@@ -99,4 +68,5 @@ def fetch_data():
 def scrape():
     data = fetch_data()
     write_output(data)
+
 scrape()
