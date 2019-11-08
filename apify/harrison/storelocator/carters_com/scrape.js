6e5eed9819b93ce75a20be302cf2c1d9befd1af7
Tenzing Shaw <tenzing@safegraph.com>
2019-11-07 16:42:54 -0800

    save

--- apify/harrison/storelocator/carters_com/scrape.js
+++ apify/harrison/storelocator/carters_com/scrape.js
@@ -1,59 +1,58 @@
-//Harrison Hayes
 const Apify = require('apify');
-const puppeteer = require('puppeteer');
-
-async function scrape() {
-
-    let stores = [];
-
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless'],
-        defaultViewport: { width: 1920, height: 1080 },
-    });
-    const page = await browser.newPage();
-    await page.goto('https://www.carters.com/on/demandware.store/Sites-Carters-Site/default/Stores-GetNearestStores?carters=true&oshkosh=false&skiphop=false&lat=0&lng=0');
-    page.on('response', async response => {
-        try {
-            let data = await response.json();
-            data = data.stores;
-            for(let key in data){
-                if(data[key].isOpen == 'open'){
-                    let store = data[key];
-                    stores.push({
-                        locator_domain: 'https://www.carters.com/',
-                        location_name: store.name,
-                        street_address: store.address1,
-                        city: store.city,
-                        state: store.stateCode,
-                        zip: store.postalCode,
-                        country_code: store.countryCode,
-                        store_number: store.storeid,
-                        phone: store.phone,
-												location_type: '<MISSING>',
-                        latitude: store.latitude,
-                        longitude: store.longitude,
-                        hours_of_operation: {
-                            'Sunday Hours': store.sundayHours,
-                            'Monday Hours': store.mondayHours,
-                            'Tuesday Hours': store.tuesdayHours,
-                            'Wednesday Hours': store.wednesdayHours,
-                            'Thurday Hours': store.thursdayHours,
-                            'Friday Hours': store.fridayHours,
-                            'Saturday Hours': store.saturdayHours,
-                        },
-                    });
-                }
-            }
-        } catch(err) {
-            console.log('Try again later, encountered CAPTCH.');
-        }
-    });
-    return stores;
-}
 
 Apify.main(async () => {
-    const data = await scrape();
-    await Apify.pushData(data);
-});
+	const requestQueue = await Apify.openRequestQueue();
+	await requestQueue.addRequest({ url: 'https://www.carters.com/on/demandware.store/Sites-Carters-Site/default/Stores-GetNearestStores?carters=true&oshkosh=false&skiphop=false&lat=0&lng=0' });
 
+	const useProxy = process.env.USE_PROXY;
+
+	let stores = [];
+
+	const crawler = new Apify.PuppeteerCrawler({
+		requestQueue,
+		handlePageFunction: async ({ request, page }) => {
+
+			try {
+				let data = await response.json();
+				data = data.stores;
+				for(let key in data){
+					if(data[key].isOpen == 'open'){
+						let store = data[key];
+						stores.push({
+							locator_domain: 'https://www.carters.com/',
+							location_name: store.name,
+							street_address: store.address1,
+							city: store.city,
+							state: store.stateCode,
+							zip: store.postalCode,
+							country_code: store.countryCode,
+							store_number: store.storeid,
+							phone: store.phone,
+							location_type: '<MISSING>',
+							latitude: store.latitude,
+							longitude: store.longitude,
+							hours_of_operation: {
+								'Sunday Hours': store.sundayHours,
+								'Monday Hours': store.mondayHours,
+								'Tuesday Hours': store.tuesdayHours,
+								'Wednesday Hours': store.wednesdayHours,
+								'Thurday Hours': store.thursdayHours,
+								'Friday Hours': store.fridayHours,
+								'Saturday Hours': store.saturdayHours,
+							},
+						});
+					}
+				}
+			} catch(err) {
+				console.log('Try again later, encountered CAPTCH.');
+			}
+
+		},
+		maxRequestsPerCrawl: 100,
+		maxConcurrency: 10,
+		launchPuppeteerOptions: {headless: true, stealth: true, useChrome: true, useApifyProxy: !!useProxy},
+	});
+
+	await crawler.run();
+	await Apify.pushData(stores);
+});
