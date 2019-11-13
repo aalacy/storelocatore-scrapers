ecadad81763dfed91e6a860caaf273033f0908fb
diapk <Hank4957!>
2019-11-13 19:51:12 +0530

    docker run and push

--- apify/himanshu/storelocator/lionschoice_com/scrape.py
+++ apify/himanshu/storelocator/lionschoice_com/scrape.py
@@ -10,7 +10,7 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         # Header
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -21,77 +21,50 @@ def fetch_data():
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
     }
 
-    print("soup ===  first")
+    return_main_object = []
 
     base_url = "http://www.lionschoice.com"
-    r = requests.get("http://www.lionschoice.com/locations", headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    return_main_object = []
-    #   data = json.loads(soup.find("div",{"paging_container":re.compile('latlong.push')["paging_container"]}))
-    # for link in soup.find_all('ul',re.compile('content')):
-    #     print(link)
+    r = requests.get("https://lionschoice.com/wp-admin/admin-ajax.php?action=store_search&lat=38.64466&lng=-90.33013&max_results=25&search_radius=10&autoload=1", headers=headers).json()
+    # soup = BeautifulSoup(r.text, "lxml")
 
-    # it will used in store data.
-    locator_domain = base_url
-    location_name = ""
-    street_address = ""
-    city = ""
-    state = ""
-    zipp = ""
-    country_code = ""
-    store_number = "<MISSING>"
-    phone = ""
-    location_type = "lionschoice"
-    latitude = ""
-    longitude = ""
-    hours_of_operation = ""
-
-    for script in soup.find('ul', {'class': 'medium-block-grid-3'}).find_all('li'):
-        list_store_data = list(script.stripped_strings)
-
-        if 'More Details' in list_store_data:
-            list_store_data.remove('More Details')
-
-        if 'Delivery' in list_store_data:
-            list_store_data.remove('Delivery')
-
-        if 'Order Online' in list_store_data:
-            list_store_data.remove('Order Online')
-
-        if 'Download Breakfast Menu' in list_store_data:
-            list_store_data.remove('Download Breakfast Menu')
-
-        if len(list_store_data) > 1:
-            print(str(len(list_store_data)) + ' = list_store_data === ' + str(list_store_data))
-
-            location_name = list_store_data[0]
-            street_address = list_store_data[1]
-            city = ' '.join(list_store_data[2].strip().split(' ')[:-2])
-            state = list_store_data[2].strip().split(' ')[-2]
-            # state = list_store_data[2].strip()[:-5].strip().replace('.',"")
-            zipp = list_store_data[2].strip().split(' ')[-1]
-            phone = list_store_data[3]
-            country_code = 'US'
-
-            if 'Business Hours:' in list_store_data:
-                hours_of_operation = ", ".join(list_store_data[list_store_data.index('Business Hours:'):])
-            elif 'Dining Room:' in list_store_data:
-                hours_of_operation = ", ".join(list_store_data[list_store_data.index('Dining Room:'):])
-            elif 'Dining Room Hours:' in list_store_data:
-                hours_of_operation = ", ".join(list_store_data[list_store_data.index('Dining Room Hours:'):])
-            else:
-                hours_of_operation = ", ".join(list_store_data[list_store_data.index('Open Daily'):])
-
-            latitude = '<MISSING>'
-            longitude = '<MISSING>'
-
-            store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
-                     store_number, phone, location_type, latitude, longitude, hours_of_operation]
-
-            print("data = " + str(store))
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
-            return_main_object.append(store)
+    for i in r:
+        location_name=i['store'].replace('&#8217;',' ')
+        address = i['address']
+        city = i['city']
+        state = i['state']
+        zip= i['zip']
+        hour_tmp =i['description']
+        store_number =i['id']
+        phone = i['phone'].replace('Not Available','<MISSING>')
+        if  phone =='':
+            phone= '<MISSING>'
+        lat =i['lat']
+        lng = i['lng']    
+        hour1 = BeautifulSoup(hour_tmp, "lxml")
+        hour = " ".join(list(hour1.stripped_strings)).replace('Order Online Delivery','')
+        if hour =='':
+            hour='<MISSING>'
+        if 'Only Open for events' in hour:
+            hour = '<MISSING>'
+        # print(location_name)
+        store = []     
+        store.append(base_url if base_url else '<MISSING>')
+        store.append(location_name if location_name else '<MISSING>')
+        store.append(address if address else '<MISSING>')
+        store.append(city if city else '<MISSING>')
+        store.append(state if state else '<MISSING>')
+        store.append(zip if zip else '<MISSING>')
+        store.append('US')
+        store.append(store_number if store_number else '<MISSING>')
+        store.append(phone if phone else '<MISSING>')
+        store.append('<MISSING>')
+        store.append(lat if lat else '<MISSING>')
+        store.append(lng if lng else '<MISSING>')
+        store.append(hour if hour else '<MISSING>')
+        store.append('<MISSING>')
+    
+   
+        return_main_object.append(store)
 
     return return_main_object
 
--- apify/himanshu/storelocator/peoplesnationalbank_com/scrape.py
+++ apify/himanshu/storelocator/peoplesnationalbank_com/scrape.py
@@ -9,7 +9,7 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -23,9 +23,11 @@ def fetch_data():
     soup = BeautifulSoup(r.text,"lxml")
     return_main_object = []
     hours_location = {}
-    for location in soup.find_all("li",{"id":re.compile("location")}):
+  
+    for location in soup.find_all("li",{"class":re.compile("loc")}):
         address = location.find("span",{"class":"street-address"}).text
         hours = " ".join(list(location.find("div",{'class':"hours"}).stripped_strings))
+
         hours_location[address] = hours
     for script in soup.find_all("script"):
         if "var gMapLocations = " in script.text:
@@ -41,10 +43,11 @@ def fetch_data():
                 store.append("US")
                 store.append(store_data["filterClass"].split("location")[1])
                 store.append(store_data["phones"][0].split(": ")[1] if store_data["phones"] != [] else "<MISSING>")
-                store.append("peoples national bank")
+                store.append("<MISSING>")
                 store.append(store_data['latitude'])
-                store.append(store_data['longitude'])
+                store.append(store_data['longitude'])               
                 store.append(hours_location[store_data["address"].replace(str(store_data["zipcode"]),"")[:-1]])
+                store.append("<MISSING>")
                 return_main_object.append(store)
     return return_main_object
 
