dabcbc3af22b57af79e94948fef898b1c2b8f7e5
Graham Baker <graham.cr.baker@gmail.com>
2019-11-12 19:02:40 -0800

    fix comet

--- apify/ggrahambaker/storelocator/cometcleaners_com/scrape.py
+++ apify/ggrahambaker/storelocator/cometcleaners_com/scrape.py
@@ -8,7 +8,7 @@ import time
 
 def get_driver():
     options = Options()
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920,1080')
