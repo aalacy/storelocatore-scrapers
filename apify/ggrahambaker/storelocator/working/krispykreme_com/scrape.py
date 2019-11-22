c63e239b9a4f2798c671f1d3645c4c5efc63a658
Graham Baker <graham.cr.baker@gmail.com>
2019-11-22 10:57:14 -0600

    fixes

--- apify/ggrahambaker/storelocator/working/krispykreme_com/scrape.py
+++ apify/ggrahambaker/storelocator/working/krispykreme_com/scrape.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 
 def get_driver():
     options = Options()
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920,1080')
@@ -53,7 +53,6 @@ def fetch_data():
 
     all_store_data = []
     for link in link_list:
-        print(link)
         driver.get(link)
         driver.implicitly_wait(20)
 
