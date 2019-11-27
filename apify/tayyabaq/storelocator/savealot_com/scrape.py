c8387c88d3b2f516cd48994c4d580112056ad080
odettaAM <52939294+odettaAM@users.noreply.github.com>
2019-11-27 19:55:53 +0530

    Create scrape.py

---
+++ apify/tayyabaq/storelocator/savealot_com/scrape.py
@@ -0,0 +1,199 @@
+from bs4 import BeautifulSoup
+import csv
+import re
+import requests
+import pandas as pd
+import json
+
+url = "https://savealot.com/grocery-stores/"
+base = 'https://savealot.com'
+
+
+def write_output(df1):
+    df1.to_csv('data.csv', index=False)
+
+
+def fetch_data():
+    states = []
+    hours = []
+    zip_codes = []
+    phones = []
+    street_address = []
+    lats = []
+    longts = []
+    location_names = []
+    locator_domains = []
+    page_urls = []
+    country_codes = []
+    store_numbrs = []
+    location_types = []
+
+    try:
+        pattern = re.compile(r'\s+')
+
+        cities = []
+        html = requests.get(url).text
+        soup = BeautifulSoup(html)
+        content = soup.find("div", class_="content")
+        lis = content.findAll('a')
+        for tag in lis:
+            state_loc = (base + tag.get('href'))
+            page = requests.get(state_loc)
+            if page.status_code == 200:
+                bs = BeautifulSoup(page.text, "html.parser")
+                ent = bs.find("div", class_="content")
+                lis = ent.find('ul')
+                lis = lis.findAll('a')
+                for tag in lis:
+                    city_loc = (base + tag.get('href'))
+                    try:
+                        page = requests.get(city_loc)
+                    except:
+                        continue
+                    if page.status_code == 200:
+                        bso = BeautifulSoup(page.text, "html.parser")
+                        ent = bso.find("div", class_="content")
+
+                        lis = ent.find('ul')
+                        lis = lis.findAll('a')
+
+                        for tag in lis:
+                            loc = (tag.get('href'))
+                            try:
+                                response = requests.get(loc, timeout=5)
+                            except Exception as e:
+                                continue
+                            if response.status_code == 200:
+                                page_urls.append(loc)
+                                try:
+                                    city = city_loc.split('/')[-2]
+                                except Exception as e:
+                                    city = "<MISSING>"
+                                print(city)
+                                cities.append(city)
+                                res = response
+                                response.close()
+                                data = res.text
+                                soup = BeautifulSoup(data, 'html.parser')
+
+                                try:
+                                    for wrapper in soup.find('div', {"class": "address"}).findAll('p'):
+                                        add = wrapper.text
+                                        address = re.sub(pattern, " ", add)
+
+                                except Exception as e:
+                                    address = "<MISSING>"
+
+                                try:
+                                    for wrapper in soup.find('div', {"class": "address"}).findAll('p'):
+                                        street = wrapper.contents[0].strip()
+
+                                except Exception as e:
+                                    street = "<MISSING>"
+
+                                #                                 print(street)
+                                street_address.append(street)
+
+                                try:
+                                    st = address.split(',')[1]
+                                    state = st.split()[0]
+
+                                except Exception as e:
+                                    state = "<MISSING>"
+                                print(state)
+                                states.append(state)
+
+                                try:
+                                    zp = address.split()[-1]
+                                    zp_code = re.findall(r"\d{5}", zp)
+                                    zip_code = zp_code[0]
+
+                                except Exception as e:
+                                    zip_code = "<MISSING>"
+                                #                                 print(zip_code)
+                                zip_codes.append(zip_code)
+
+                                try:
+                                    phn = soup.find('div', attrs={'class': 'phone'}).text.strip()
+                                    phone = re.sub(pattern, " ", phn)
+
+                                except Exception as e:
+                                    phone = "<MISSING>"
+                                    #                                 print(phone)
+                                phones.append(phone)
+
+                                try:
+                                    result = soup.findAll('div', class_='day-row')
+                                    hour = ''
+                                    for i in result:
+                                        pat = re.sub(pattern, "", i.text)
+                                        hour = hour + pat + '|'
+                                    if len(hour) < 3:
+                                        hour = "<MISSING>"
+
+                                except Exception as e:
+                                    hour = "<MISSING>"
+
+                                print(hour)
+                                hours.append(hour)
+
+                                try:
+                                    s = soup.find('script', {'type': 'application/ld+json'})
+                                    j_file = json.loads(s.text)
+                                    lat = j_file["geo"]['latitude']
+
+                                except Exception as e:
+                                    lat = "<MISSING>"
+                                lats.append(lat)
+
+                                try:
+                                    s = soup.find('script', {'type': 'application/ld+json'})
+                                    j_file = json.loads(s.text)
+                                    longt = j_file["geo"]['longitude']
+
+                                except Exception as e:
+                                    longt = "<MISSING>"
+                                longts.append(longt)
+
+                                try:
+                                    locn = soup.find('div', attrs={'class': 'ib-text'})
+                                    location_name = locn.h1.text
+
+                                except Exception as e:
+                                    location_name = "<MISSING>"
+                                location_names.append(location_name)
+
+                                country_code = "US"
+                                country_codes.append(country_code)
+                                #         # print(ccode)
+
+                                location_type = "<MISSING>"
+                                location_types.append(location_type)
+                                #         # print(location_type)
+
+                                store_numbr = "<MISSING>"
+                                store_numbrs.append(store_numbr)
+                                #         # print(store_number)
+
+                                locator_domain = "https://www.savealot.com"
+                                locator_domains.append(locator_domain)
+
+        dic = {'locator_domain': locator_domains, 'page_url': page_urls, 'location_name': location_names,
+               'street_address': street_address, 'city': cities, 'state': states,
+               'zip': zip_codes, 'country_code': country_codes, 'phone': phones, 'location_type': location_types,
+               'latitude': lats, 'longitude': longts, 'hours_of_operation': hours, 'store_number': store_numbrs}
+        d = pd.DataFrame(dic)
+        df = d.fillna("<MISSING>")
+        df1 = df.drop_duplicates()
+        return df1
+
+    except Exception as e:
+        print(str(e))
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+scrape()
