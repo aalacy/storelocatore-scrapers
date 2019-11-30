00a77f91b6212a617aa756e4e50e2380ccf2bee7
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2019-12-01 04:32:33 +0500

    Add files via upload

--- apify/Kullaleeni/storelocator/firstmidwest_com/scrape.py
+++ apify/Kullaleeni/storelocator/firstmidwest_com/scrape.py
@@ -1,297 +1,157 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 29 20:33:11 2019
-
-@author: srek
-"""
-import pandas as pd
-from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
-import time
+import csv
+import string
+import re, time
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
 
 
 def fetch_data():
-    url = "https://firstmidwest.com/locations/"
-    locator_domain = url
+    # Your scraper here
+
     data = []
-    driver = webdriver.Chrome()
-    driver.get(url)
-    time.sleep(10)
-    
-    df_zip = pd.read_csv("US_states.csv")
-    
-    states = df_zip.State_Mod.drop_duplicates().tolist()
-    
-    for s in range(len(states)):
+    p = 1
+    links = []
+    pattern = re.compile(r'\s\s+')
+    url = 'https://www.firstmidwest.com/locations/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    maindiv = soup.find('div',{'id':'rls-map'})
+    #print(maindiv)
+    maindiv = str(maindiv)
+    start = 0
+    while True:
+        start = maindiv.find('a href=',start)
+        if start != -1:
+            start =  maindiv.find('"',start)+1
+            end = maindiv.find('"', start)
+            link = maindiv[start:end]
+            if link.find("google") == -1:
+                link = link.replace("/","")
+                link = link.replace('\\', "/")
+                link = link[0:len(link)-1]
+                flag = True
+                i = 0
+                while i < len(links):
+                    if link == links[i]:
+                        flag = False
+                        break
+                    i += 1
+                if flag:
+                    links.append(link)
+                #print(link)
+            start = end + 1
+        else:
+            break
+
+    print(len(links))
+    for link in links:
+        #print(link)
+        page = requests.get(link)
+        soup = BeautifulSoup(page.text, "html.parser")
+        title = soup.find('div',{'id':'rio-locName'}).text
+        address = soup.find('div', {'class': 'rio-addrText'})
+        address = address.findAll('span')
+        if len(address) == 4:
+            street = address[0].text
+            city = address[1].text
+            state = address[2].text
+            pcode = address[3].text
+        elif len(address) == 5:
+            street = address[0].text + " " + address[1].text
+            city = address[2].text
+            state = address[3].text
+            pcode = address[4].text
         try:
-            driver.find_element_by_xpath("//select/option[@value='100']").click()
-            element = driver.find_element_by_xpath('//*[@id="rio-txbCityState"]')
-            element.clear()
-            element.send_keys(states[s])
-            driver.find_element_by_xpath('//*[@id="rio-searchZip"]').click()
-            time.sleep(3)
-            
-            driver.find_element_by_xpath('//*[@id="spid17549"]').click()
-            time.sleep(2)
-            driver.find_element_by_xpath('//*[@id="spid17551"]').click()
-            time.sleep(2)
-            driver.find_element_by_xpath('//*[@id="spid17553"]').click()
-            time.sleep(2)
-            driver.find_element_by_xpath('//*[@id="spid17555"]').click()
-            time.sleep(2)
-            driver.find_element_by_xpath('//*[@id="spid17601"]').click()
-            time.sleep(2)
-            
-            driver.find_element_by_xpath('//*[@id="spid17549"]').click()
-            time.sleep(2)
-            
-            soup_page = BeautifulSoup(driver.page_source)
-            if "We're sorry there are no locations" in soup_page.find("div",attrs={"id":"rio-listWrapper"}).text:
-                driver.find_element_by_xpath('//*[@id="spid17549"]').click()
-                time.sleep(2)
-                pass
-            else:
-                soup_page = BeautifulSoup(driver.page_source)
-            
-                records = soup_page.find_all("div",attrs={"id":"rio-listWrapper"})
-                for r in range(len(records)):
-                    location_name = records[r].find("strong").text + ", " + records[0].find_all("strong")[1].text
-                    street_address = records[r].find("div",attrs={"class":"rio-list-addr"}).text 
-                    add = records[r].find("div",attrs={"class":"rio-list-csz"}).text
-                    try:
-                        city,[state,zipcode] = add.split(",")[0],add.split(",")[1].strip().split(" ")
-                    except:
-                        city,[state,zipcode] = add,[add,add]
-                    country_code = "USA"
-                    location_type = soup_page.find("label",attrs={"for":"spid17601"}).text.split("[")[0].strip()
-                    phone = '<MISSING>'
-                    
-                    data_record = {}
-                    data_record['locator_domain'] = locator_domain.strip()
-                    data_record['location_name'] = location_name.strip()
-                    data_record['street_address'] = street_address.strip()
-                    data_record['city'] = city.strip()
-                    data_record['state'] = state.strip()
-                    data_record['zip'] = zipcode.strip()
-                    data_record['country_code'] = country_code
-                    data_record['store_number'] = '<MISSING>'
-                    data_record['phone'] = phone.strip()
-                    data_record['location_type'] = location_type.strip()
-                    data_record['latitude'] = '<MISSING>'
-                    data_record['longitude'] = '<MISSING>'
-                    data_record['hours_of_operation'] = '<MISSING>'
-                    data_record['page_url'] = '<MISSING>'
-                    data.append(data_record)
-                    
-                driver.find_element_by_xpath('//*[@id="spid17549"]').click()
-                time.sleep(2)
-            
-            driver.find_element_by_xpath('//*[@id="spid17551"]').click()
-            time.sleep(2)
-            soup_page = BeautifulSoup(driver.page_source)
-            if "We're sorry there are no locations" in soup_page.find("div",attrs={"id":"rio-listWrapper"}).text:
-                driver.find_element_by_xpath('//*[@id="spid17551"]').click()
-                time.sleep(2)
-            else:   
-                soup_page = BeautifulSoup(driver.page_source)
-            
-                records = soup_page.find_all("div",attrs={"id":"rio-listWrapper"})
-                for r in range(len(records)):
-                    location_name = records[r].find("strong").text + ", " + records[0].find_all("strong")[1].text
-                    street_address = records[r].find("div",attrs={"class":"rio-list-addr"}).text 
-                    add = records[r].find("div",attrs={"class":"rio-list-csz"}).text
-                    try:
-                        city,[state,zipcode] = add.split(",")[0],add.split(",")[1].strip().split(" ")
-                    except:
-                        city,[state,zipcode] = add,[add,add]
-                    country_code = "USA"
-                    location_type = soup_page.find("label",attrs={"for":"spid17601"}).text.split("[")[0].strip()
-                    phone = '<MISSING>'
-                    
-                    data_record = {}
-                    data_record['locator_domain'] = locator_domain.strip()
-                    data_record['location_name'] = location_name.strip()
-                    data_record['street_address'] = street_address.strip()
-                    data_record['city'] = city.strip()
-                    data_record['state'] = state.strip()
-                    data_record['zip'] = zipcode.strip()
-                    data_record['country_code'] = country_code
-                    data_record['store_number'] = '<MISSING>'
-                    data_record['phone'] = phone.strip()
-                    data_record['location_type'] = location_type.strip()
-                    data_record['latitude'] = '<MISSING>'
-                    data_record['longitude'] = '<MISSING>'
-                    data_record['hours_of_operation'] = '<MISSING>'
-                    data_record['page_url'] = '<MISSING>'
-                    data.append(data_record)
-                    
-                driver.find_element_by_xpath('//*[@id="spid17551"]').click()
-                time.sleep(2)
-           
-            
-            driver.find_element_by_xpath('//*[@id="spid17553"]').click()
-            time.sleep(2)
-            soup_page = BeautifulSoup(driver.page_source)
-            if "We're sorry there are no locations" in soup_page.find("div",attrs={"id":"rio-listWrapper"}).text:
-                driver.find_element_by_xpath('//*[@id="spid17553"]').click()
-                time.sleep(2)
-            else:
-                soup_page = BeautifulSoup(driver.page_source)
-            
-                records = soup_page.find_all("div",attrs={"id":"rio-listWrapper"})
-                for r in range(len(records)):
-                    location_name = records[r].find("strong").text + ", " + records[0].find_all("strong")[1].text
-                    street_address = records[r].find("div",attrs={"class":"rio-list-addr"}).text 
-                    add = records[r].find("div",attrs={"class":"rio-list-csz"}).text
-                    try:
-                        city,[state,zipcode] = add.split(",")[0],add.split(",")[1].strip().split(" ")
-                    except:
-                        city,[state,zipcode] = add,[add,add]
-                    country_code = "USA"
-                    location_type = soup_page.find("label",attrs={"for":"spid17601"}).text.split("[")[0].strip()
-                    phone = '<MISSING>'
-                    
-                    data_record = {}
-                    data_record['locator_domain'] = locator_domain.strip()
-                    data_record['location_name'] = location_name.strip()
-                    data_record['street_address'] = street_address.strip()
-                    data_record['city'] = city.strip()
-                    data_record['state'] = state.strip()
-                    data_record['zip'] = zipcode.strip()
-                    data_record['country_code'] = country_code
-                    data_record['store_number'] = '<MISSING>'
-                    data_record['phone'] = phone.strip()
-                    data_record['location_type'] = location_type.strip()
-                    data_record['latitude'] = '<MISSING>'
-                    data_record['longitude'] = '<MISSING>'
-                    data_record['hours_of_operation'] = '<MISSING>'
-                    data_record['page_url'] = '<MISSING>'
-                    data.append(data_record)
-                    
-                driver.find_element_by_xpath('//*[@id="spid17553"]').click()
-                time.sleep(2)
-            
-            
-            
-            
-            driver.find_element_by_xpath('//*[@id="spid17555"]').click()
-            time.sleep(2)
-            soup_page = BeautifulSoup(driver.page_source)
-            if "We're sorry there are no locations" in soup_page.find("div",attrs={"id":"rio-listWrapper"}).text:
-                driver.find_element_by_xpath('//*[@id="spid17555"]').click()
-                time.sleep(2)
-            else:
-                soup_page = BeautifulSoup(driver.page_source)
-            
-                records = soup_page.find_all("div",attrs={"id":"rio-listWrapper"})
-                for r in range(len(records)):
-                    location_name = records[r].find("strong").text + ", " + records[0].find_all("strong")[1].text
-                    street_address = records[r].find("div",attrs={"class":"rio-list-addr"}).text 
-                    add = records[r].find("div",attrs={"class":"rio-list-csz"}).text
-                    try:
-                        city,[state,zipcode] = add.split(",")[0],add.split(",")[1].strip().split(" ")
-                    except:
-                        city,[state,zipcode] = add,[add,add]
-                    country_code = "USA"
-                    location_type = soup_page.find("label",attrs={"for":"spid17601"}).text.split("[")[0].strip()
-                    phone = '<MISSING>'
-                    
-                    data_record = {}
-                    data_record['locator_domain'] = locator_domain.strip()
-                    data_record['location_name'] = location_name.strip()
-                    data_record['street_address'] = street_address.strip()
-                    data_record['city'] = city.strip()
-                    data_record['state'] = state.strip()
-                    data_record['zip'] = zipcode.strip()
-                    data_record['country_code'] = country_code
-                    data_record['store_number'] = '<MISSING>'
-                    data_record['phone'] = phone.strip()
-                    data_record['location_type'] = location_type.strip()
-                    data_record['latitude'] = '<MISSING>'
-                    data_record['longitude'] = '<MISSING>'
-                    data_record['hours_of_operation'] = '<MISSING>'
-                    data_record['page_url'] = '<MISSING>'
-                    data.append(data_record)
-                    
-                driver.find_element_by_xpath('//*[@id="spid17555"]').click()
-                time.sleep(2)
-             
-            
-            
-            driver.find_element_by_xpath('//*[@id="spid17601"]').click()
-            time.sleep(2)
-            soup_page = BeautifulSoup(driver.page_source)
-            if "We're sorry there are no locations" in soup_page.find("div",attrs={"id":"rio-listWrapper"}).text:
-                driver.find_element_by_xpath('//*[@id="spid17601"]').click()
-                time.sleep(2)
-            else:
-                soup_page = BeautifulSoup(driver.page_source)
-            
-                records = soup_page.find_all("div",attrs={"id":"rio-listWrapper"})
-                for r in range(len(records)):
-                    location_name = records[r].find("strong").text + ", " + records[0].find_all("strong")[1].text
-                    street_address = records[r].find("div",attrs={"class":"rio-list-addr"}).text 
-                    add = records[r].find("div",attrs={"class":"rio-list-csz"}).text
-                    try:
-                        city,[state,zipcode] = add.split(",")[0],add.split(",")[1].strip().split(" ")
-                    except:
-                        city,[state,zipcode] = add,[add,add]
-                    country_code = "USA"
-                    location_type = soup_page.find("label",attrs={"for":"spid17601"}).text.split("[")[0].strip()
-                    phone = '<MISSING>'
-                    
-                    data_record = {}
-                    data_record['locator_domain'] = locator_domain.strip()
-                    data_record['location_name'] = location_name.strip()
-                    data_record['street_address'] = street_address.strip()
-                    data_record['city'] = city.strip()
-                    data_record['state'] = state.strip()
-                    data_record['zip'] = zipcode.strip()
-                    data_record['country_code'] = country_code
-                    data_record['store_number'] = '<MISSING>'
-                    data_record['phone'] = phone.strip()
-                    data_record['location_type'] = location_type.strip()
-                    data_record['latitude'] = '<MISSING>'
-                    data_record['longitude'] = '<MISSING>'
-                    data_record['hours_of_operation'] = '<MISSING>'
-                    data_record['page_url'] = '<MISSING>'
-                    data.append(data_record)     
-    
-                driver.find_element_by_xpath('//*[@id="spid17601"]').click()
-                time.sleep(2)
+            phone = soup.find('span',{'class':'rio-phoneText'}).text
         except:
-            print(s)
-            
-    return data               
-                
-def write_output(data):
-    df_data = pd.DataFrame(columns=['locator_domain','location_name','street_address','city','state','zip','country_code','store_number','phone','location_type','latitude','longitude','hours_of_operation'])
-    
-    for d in range(len(data)):
-        df = pd.DataFrame(list(data[d].values())).transpose()
-        df.columns = list((data[d].keys()))   
-        df_data = df_data.append(df)
-    #df_data = df_data.fillna("<MISSING>")
-    df_data = df_data.replace(r'^\s*$', "<MISSING>", regex=True)
-    df_data = df_data.drop_duplicates(["location_name","street_address"])
-    df_data['zip'] = df_data.zip.astype(str)
-    print(data)
-    df_data.to_csv('./data.csv',index = 0,header=True,columns=['locator_domain','location_name','street_address','city',
-                                                               'state','zip','country_code','store_number','phone','location_type',
-                                                               'latitude','longitude','hours_of_operation','page_url'])
+            phone = "<MISSING>"
+        try:
+            hoursd =  soup.find('div',{'id':'rio-locHoursServices'})
+            hoursd = hoursd.findAll('div',{'class':'hours'})
+            lhours = hoursd[0].text
+            lhours = re.sub(pattern," ",lhours)
+            dhours = hoursd[1].text
+            dhours = re.sub(pattern, " ", dhours)
+            hours = "Lobby Hours : " + lhours + " | Drive Up Hours : " + dhours
+
+
+
+        except:
+            hours= "<MISSING>"
+        try:
+            hoursd = soup.find('div', {'id': 'rio-atmHours'})
+            hoursd = hoursd.find('div', {'class': 'hours'}).text
+            hoursd = re.sub(pattern, " ",hoursd)
+            if hours == "<MISSING>":
+                temp = "ATM Hours : "
+                hours = temp + hoursd
+            else:
+                temp ="|ATM Hours : "
+                hours = hours + temp + hoursd
+
+        except:
+            pass
+        try:
+           services = soup.find('div', {'id': 'rio-locServices'}).text
+           services= services.replace("\n","|")
+           services = services[1:len(services)-2]
+           services = services[services.find("|")+1:len(services)]
+           services = services.replace("ATM|ATM 24 Hr","ATM")
+        except:
+            pass
+        soup = str(soup)
+        start = soup.find('RLS.centerLat')
+        start = soup.find('=',start) + 2
+        end =  soup.find(';',start)
+        lat = soup[start:end]
+        start = soup.find('RLS.centerLng')
+        start = soup.find('=', start) + 2
+        end = soup.find(';', start)
+        longt = soup[start:end]
+
+        start = link.find('banking-')
+        start = link.find('-',start)+1
+        end = link.find('.', start)
+        store = link[start:end]
+        store = store.replace("atm","")
+        data.append([
+            'https://www.firstmidwest.com',
+            link,
+            title,
+            street,
+            city,
+            state,
+            pcode,
+            "US",
+            store,
+            phone,
+            services,
+            lat,
+            longt,
+            hours
+        ])
+        #print(p,",",data[p-1])
+        p += 1
+
+    return data
+
+
+
 
 def scrape():
     data = fetch_data()
     write_output(data)
 
-
 scrape()
 
-    
-    
-    
-    
-    
-    
-    
