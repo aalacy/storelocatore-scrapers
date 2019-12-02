78e265eb3cf5247a9e31ddb9397d3bd853cf7665
odettaAM <52939294+odettaAM@users.noreply.github.com>
2019-12-03 00:43:01 +0530

    Add files via upload

--- apify/tayyabaq/storelocator/marcheami_ca/scrape.py
+++ apify/tayyabaq/storelocator/marcheami_ca/scrape.py
@@ -2,6 +2,7 @@ import csv
 import os
 import re, time
 import requests
+import itertools
 from bs4 import BeautifulSoup
 import json
 import lxml.html
@@ -23,39 +24,84 @@ def get_driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    return webdriver.Chrome('chromedriver', chrome_options=options)
+    return webdriver.Chrome('c:/chromedriver.exe', chrome_options=options)
+    #return webdriver.Chrome('chromedriver', chrome_options=options)
 
 def fetch_data():
     driver = get_driver()
+    c=0
     data=[];page_url=[];hours_of_operation=[]; latitude=[];longitude=[];zipcode=[];location_name=[];city=[];street_address=[]; state=[]; phone=[]
     url = "http://marcheami.ca/siteamienglish/index.php"
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     doc = lxml.html.fromstring(r.content)
-    store = doc.xpath('//ul[@class="rvnavigator"]/li[4]/ul/li/a/@href')
-    states = doc.xpath('//ul[@class="rvnavigator"]/li[4]/ul/li/a/span/text()')
+    store = doc.xpath('//ul[@class="rvnavigator"]/li[5]/ul/li/a/@href')
+    states = doc.xpath('//ul[@class="rvnavigator"]/li[5]/ul/li/a/span/text()')
     for n in range(0,len(store)):
         driver.get(store[n])
         time.sleep(4)
         name = driver.find_elements_by_xpath('//div[@class="store"]/h2')
         address =  driver.find_elements_by_xpath('//div[@class="store"]/ul/li[1]')
+        cities =  driver.find_elements_by_xpath('//div[@class="store"]/ul/li[2]')
         phones =  driver.find_elements_by_xpath('//div[@class="store"]/ul/li[3]')
+        name1 = driver.find_elements_by_xpath('//div[@class="store last"]/h2')
+        address1 =  driver.find_elements_by_xpath('//div[@class="store last"]/ul/li[1]')
+        cities1 =  driver.find_elements_by_xpath('//div[@class="store last"]/ul/li[2]')
+        phones1 =  driver.find_elements_by_xpath('//div[@class="store last"]/ul/li[3]')
+        name2 = driver.find_elements_by_xpath('//div[@class="store alone"]/h2')
+        address2 =  driver.find_elements_by_xpath('//div[@class="store alone"]/ul/li[1]')
+        cities2 =  driver.find_elements_by_xpath('//div[@class="store alone"]/ul/li[2]')
+        phones2 =  driver.find_elements_by_xpath('//div[@class="store alone"]/ul/li[3]')
         for m in range(0,len(address)):
-            location_name.append(name[m].text)
-            street_address.append(address[m].text)
-            state.append(states[n])
-            phone.append(phones[m].text)
+            if name[m]=='ÉPICERIE DE ST-VIANNEY COOP SOLIDARITÉ':
+                c+=1
+                if c<=1:
+                    location_name.append(name[m].text)
+                    street_address.append(address[m].text)
+                    if states[n] in ['Bas St-Laurent','Chaudières Appalaches','Centre du Québec','Outaouais','Saguenay','Côte Nord','Estrie','Gaspésie','Lanaudière','Laurentides','Mauricie','Montérégie','Montréal']:
+                        state.append("Quebec")
+                    elif states[n] in ['Clair / New-Brunswick']:
+                        state.append("New Brunswick")
+                    else:
+                        state.append(states[n])
+                    city.append(cities[m].text)
+                    phone.append(phones[m].text)
+                    page_url.append(store[n])
+        for x in range(0,len(address1)):
+            location_name.append(name1[x].text)
+            street_address.append(address1[x].text)
+            if states[n] in ['Bas St-Laurent','Chaudières Appalaches','Centre du Québec','Saguenay','Outaouais','Côte Nord','Estrie','Gaspésie','Lanaudière','Laurentides','Mauricie','Montérégie','Montréal']:
+                state.append("Quebec")
+            elif states[n] in ['Clair / New-Brunswick']:
+                state.append("New Brunswick")
+            else:
+                state.append(states[n])
+            city.append(cities1[x].text)
+            phone.append(phones1[x].text)
             page_url.append(store[n])
+        for y in range(0,len(address2)):
+            location_name.append(name2[y].text)
+            street_address.append(address2[y].text)
+            if states[n] in ['Bas St-Laurent','Chaudières Appalaches','Centre du Québec','Saguenay','Outaouais','Côte Nord','Estrie','Gaspésie','Lanaudière','Laurentides','Mauricie','Montérégie','Montréal']:
+                state.append("Quebec")
+            elif states[n] in ['Clair / New-Brunswick']:
+                state.append("New Brunswick")
+            else:
+                state.append(states[n])
+            city.append(cities2[y].text)
+            phone.append(phones2[y].text)
+            page_url.append(store[n])
+        
     for n in range(0,len(location_name)): 
         data.append([
             'https://marcheami.ca/',
             page_url[n],
             location_name[n],
             street_address[n],
-            '<MISSING>',
+            city[n],
             state[n],
             '<MISSING>',
-            'US',
+            'CA',
             '<MISSING>',
             phone[n],
             '<MISSING>',
@@ -63,6 +109,7 @@ def fetch_data():
             '<MISSING>',
             '<MISSING>'
         ])
+    
     return data
 
 def scrape():
