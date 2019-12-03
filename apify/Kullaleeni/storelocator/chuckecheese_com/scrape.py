fae1c20a8ccd4b43ff0e221a38261e74bd29d059
Tenzing Shaw <tenzing@safegraph.com>
2019-12-03 12:11:36 -0800

    driver

--- apify/Kullaleeni/storelocator/chuckecheese_com/scrape.py
+++ apify/Kullaleeni/storelocator/chuckecheese_com/scrape.py
@@ -10,13 +10,21 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    return webdriver.Chrome('chromedriver', options=options)
+
 def fetch_data():
     url = "https://www.chuckecheese.com/locations"
     data = []
     
     locator_domain = url
     
-    driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+    driver = get_driver()
     driver.get(url)
     
     
