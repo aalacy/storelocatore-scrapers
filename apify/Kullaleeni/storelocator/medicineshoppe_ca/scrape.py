70f985117d91b76ecd3da0a5a19b120546a06db7
Tenzing Shaw <tenzing@safegraph.com>
2019-12-03 12:04:40 -0800

    driver settings

--- apify/Kullaleeni/storelocator/medicineshoppe_ca/scrape.py
+++ apify/Kullaleeni/storelocator/medicineshoppe_ca/scrape.py
@@ -12,10 +12,17 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-def fetch_data():
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    return webdriver.Chrome('chromedriver', options=options)
 
+def fetch_data():
     #df_ca_list = pd.read_csv("/home/srek/xyz/Odetta/CA_states.csv")
-    driver = webdriver.Chrome()
+    driver = get_driver()
 
     data = []
     
