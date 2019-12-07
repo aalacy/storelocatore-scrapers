080e316015e6056879bd09fff86cbb65e0a6e884
Tenzing Shaw <tenzing@safegraph.com>
2019-12-07 00:17:36 -0800

    fancy state/province stuff

--- apify/harrison/storelocator/maxmara_com/scrape.py
+++ apify/harrison/storelocator/maxmara_com/scrape.py
@@ -2,6 +2,11 @@ import csv
 import requests
 import usaddress
 import us
+from uszipcode import SearchEngine
+from pypostalcode import PostalCodeDatabase
+
+search = SearchEngine(simple_zipcode=True)
+pcdb = PostalCodeDatabase()
 
 API_URL = 'https://world.maxmara.com/store-locator?south=-81.96490562901019&west=-174.52175505623518&north=87.18732610733203&east=39.93136994376482&lat=40.7127431&lng=-74.01337949999999&name=&listJson=true&withoutRadius=false'
 COUNTRY_DICT = { 'United States': 'US', 'Canada': 'CA', 'united states': 'US', 'canada': 'CA' }
@@ -37,13 +42,14 @@ def fetch_data():
         full_address = store['properties']['formattedAddress']
         tagged = usaddress.tag(full_address)[0]
         street_address = tagged['AddressNumber']+" "+tagged['StreetName']+" "+tagged['StreetNamePostType']
-        state_from_address = '<MISSING>'
-        if 'StateName' in tagged and us.states.lookup(tagged['StateName']):
-            state_from_address = tagged['StateName']
-        elif 'PlaceName' in tagged and us.states.lookup(tagged['PlaceName']):
-            state_from_address = tagged['PlaceName']
         prov = store['properties']['prov']
-        state = prov.split('-')[1] if prov else state_from_address
+        state = prov.split('-')[1] if prov else '<MISSING>'
+        zip_lookup = search.by_zipcode(zip_code).to_dict()
+        if country_code == 'US' and 'state' in zip_lookup:
+            state = zip_lookup['state']
+        elif country_code == 'CA':
+            location = pcdb[zip_code[0:3]]
+            state = location.province
         page_url = '<MISSING>'
         location_type = '<MISSING>'
         stores.append([locator_domain, page_url, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
