f13a74c1b1d0fd945d6afe6b7f8c601b30c42f18
nsxdarin <nsxdarin@gmail.com>
2019-12-07 18:31:34 -0600

    Create scrape.py

---
+++ apify/nsxdarin/storelocator/olivegarden_ca/scrape.py
@@ -0,0 +1,51 @@
+import csv
+import urllib2
+import requests
+
+session = requests.Session()
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    locs = []
+    url = 'https://www.olivegarden.ca/ca-locations-sitemap.xml'
+    r = session.get(url, headers=headers)
+    for line in r.iter_lines():
+        if '<loc>' in line:
+            locs.append(line.split('<loc>')[1].split('<')[0])
+    for loc in locs:
+        print('Pulling Location %s...' % loc)
+        r2 = session.get(loc, headers=headers)
+        store = loc.rsplit('/',1)[1]
+        website = 'olivegarden.ca'
+        country = 'CA'
+        typ = 'Restaurant'
+        for line2 in r2.iter_lines():
+            if '<title>' in line2:
+                name = line2.split('<title>')[1].split(' Italian')[0]
+            if 'id="restAddress" value="' in line2:
+                add = line2.split('id="restAddress" value="')[1].split(',')[0]
+                city = line2.split('id="restAddress" value="')[1].split(',')[1]
+                state = line2.split('id="restAddress" value="')[1].split(',')[2]
+                zc = line2.split('id="restAddress" value="')[1].split(',')[3].split('"')[0]
+            if ',"name":"' in line2:
+                lat = line2.split('"latitude":"')[1].split('"')[0]
+                lng = line2.split('"longitude":"')[1].split('"')[0]
+                phone = line2.split('"telephone":"')[1].split('"')[0]
+                hours = line2.split('"openingHours":["')[1].split(']')[0].replace('","','; ').replace('"','')
+                if phone == '':
+                    phone = '<MISSING>'
+                yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
