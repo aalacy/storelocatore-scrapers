8558af65a340beac2e6269646fba797d42e258e4
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2019-12-09 00:35:37 +0500

    Update scrape.py

--- apify/tayyabaq/storelocator/fastsigns_com/scrape.py
+++ apify/tayyabaq/storelocator/fastsigns_com/scrape.py
@@ -51,66 +51,72 @@ def fetch_data():
     stores_text = [stores[n].text for n in range(0, len(stores))]
     country = driver.find_elements_by_xpath('//tbody/tr/td[5]')
     address_type = driver.find_elements_by_xpath('//tbody/tr/td[2]')
-    print(address_type)
+    #print(address_type)
     for n in range(0, len(stores_href)):
         if (('COMING SOON' not in address_type[n].text) and ('COMING SOON!' not in address_type[n].text)) and (
                 ('US' in country[n].text) or ('CA' in country[n].text)):
             links.append(stores_href[n])
-            
-            # print(location_name)
-            
-            #print(state)
 
-            #countries.append(country[n].text)
+            # print(location_name)
+
+            # print(state)
+
+            # countries.append(country[n].text)
     for n in range(0, len(links)):
         print(links[n])
         driver.get(links[n])
-        
+
         time.sleep(5)
-        addr = driver.find_element_by_class_name('address').text.replace(",,",",").split("\n")[0].strip()
-        #address=addr.strip().split(",")
-        if addr.split(",")[0] !="COMING SOON" and addr.split(",")[0]!= "COMING SOON!":
-            z= re.findall(r'[0-9]{5}',addr)
-            if z== []:
-                z=re.findall(r'[A-Z][0-9][A-Z] [0-9][A-Z][0-9]',addr)
-                if z==[]:
-                    z=""
+        addr = driver.find_element_by_class_name('address').text.replace(",,", ",").split("\n")[0].strip()
+        loca=driver.find_element_by_class_name('location-x').text.lower()
+        if  "COMING SOON" not in addr and "COMING SOON!"not in addr and "coming soon" not in loca:
+
+            location_name.append(driver.find_element_by_class_name('title').text.strip())
+            z = re.findall(r'[0-9]{5}', addr)
+            if z == []:
+                z = re.findall(r'[A-Z][0-9][A-Z] [0-9][A-Z][0-9]', addr)
+                if z == []:
+                    z = "<MISSING>"
                 else:
-                    z=z[-1]
-                    addr=addr.replace(z,"")
+                    z = z[-1]
+                    addr = addr.replace(z, "")
             else:
-                z=z[-1]
-                    addr=addr.replace(z,"")
-               
-            s= re.findall(r'[A-Z]{2}',addr)
-            if s==[]:
-                s=""
+                if re.findall(r'[0-9]{5}\-[\d]+', addr) != []:
+                    addr=addr.replace(re.findall(r'[0-9]{5}(\-[\d]+)', addr)[-1],"")
+                z = z[-1]
+                addr = addr.replace(z, "")
+
+            s = re.findall(r'[A-Z]{2}', addr)
+            if s == []:
+                s = "<MISSING>"
             else:
-                s=s[-1]
-                addr=addr.replace(s,"")
+                s = s[-1]
+                addr = addr.replace(s, "")
             state.append(s)
-            print('check',re.findall(r'(.*[a-z0-9])[ ,.]*',addr)[0])
-            c=address[-3].strip()
+            addr= re.findall(r'(.*[a-z0-9])[ ,.]*', addr)[0]
+            c = addr.split(",")[-1]
             city.append(c)
-            
             zipcode.append(z)
-            pages_url.append(links[n])   
-            street_address.append(addr.replace(c,"").replace(s,"").replace(z,"").replace(",",""))
-            print(address)
-            print(city)
-            print(street_address)
-            print(zipcode)
-            if len(z)==5:
+            pages_url.append(links[n])
+            street_address.append(addr.replace(c, "").replace(",",""))
+            print(addr)
+            print(c)
+            #rint(street_address)
+            print(z)
+            if len(z) == 5:
                 countries.append('US')
             else:
                 countries.append('CA')
             ids.append(str(links[n]).split('/')[-1].split('-')[0])
-            print(ids)
+            #rint(ids)
             if driver.find_element_by_class_name('phone').text != "Coming Soon":
                 phone.append(driver.find_element_by_class_name('phone').text)
             else:
                 phone.append("<MISSING>")
-            hours_of_operation.append(driver.find_element_by_xpath('//div[@class="inner"]/div[3]').text+" "+driver.find_element_by_xpath('//div[@class="inner"]/span[1]').text+" "+driver.find_element_by_xpath('//div[@class="inner"]/span[2]').text)
+            hours_of_operation.append(
+                driver.find_element_by_xpath('//div[@class="inner"]/div[3]').text + " " + driver.find_element_by_xpath(
+                    '//div[@class="inner"]/span[1]').text + " " + driver.find_element_by_xpath(
+                    '//div[@class="inner"]/span[2]').text)
             lat_lon = b = driver.find_element_by_xpath(
                 '//a[contains(@href,"https://www.google.com/maps")]').get_attribute('href')
             try:
@@ -121,6 +127,8 @@ def fetch_data():
                 longitude.append(lat_lon.split("n/")[1].split(",")[1])
             except:
                 longitude.append('<MISSING>')
+
+
     data = []
     for i in range(0, len(street_address)):
         row = []
