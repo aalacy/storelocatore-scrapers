61fdaf4b731b1c3d5f919f63d0b6e165d0743d6a
nsxdarin <nsxdarin@gmail.com>
2019-12-10 13:09:51 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/lgstoreswv_com/scrape.py
+++ apify/nsxdarin/storelocator/lgstoreswv_com/scrape.py
@@ -4,54 +4,61 @@ import requests
 import json
 
 session = requests.Session()
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+headers = {'authority': 'lgstoreswv.com',
+           'method': 'POST',
+           'scheme': 'https',
+           'x-requested-with': 'XMLHttpRequest',
+           'x-wp-nonce': '0d62884d5f',
+           'x-wpgmza-action-nonce': '336411b60a',
            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-           'x-requested-with': 'XMLHttpRequest'
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
            }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
 
 def fetch_data():
-    url = 'https://lgstoreswv.com/wp-admin/admin-ajax.php'
-    payload = {'action': 'locate',
-               'address': '',
-               'formatted_address': '',
-               'locatorNonce': '234f4b7d7c',
-               'distance': '500',
-               'latitude': '38',
-               'longitude': '-82',
-               'unit': 'miles',
-               'geolocation': 'false',
-               'allow_empty_address': 'true'
+    url = 'https://lgstoreswv.com/wp-json/wpgmza/v1/marker-listing/'
+    payload = {'phpClass': 'WPGMZA\MarkerListing\Carousel',
+               'map_id': '2'
                }
     r = session.post(url, headers=headers, data=payload)
-    array = json.loads(r.content)['results']
-    for item in array:
-        name = item['title']
-        output = item['output'].replace('\r','').replace('\n','').replace('\t','')
-        address = output.split('Distance')[1].split('/>',1)[1].split('<a href')[0]
-        add = address.split('<')[0]
-        city = address.split('>')[1].split(',')[0]
-        state = 'WV'
-        zc = address.split('>')[1].split('<')[0].rsplit(' ',1)[1]
-        phone = address.split('>')[2].split('<')[0]
-        website = 'lgstoreswv.com'
-        typ = name
-        if '|' in typ:
-            typ = typ.split('|')[1].strip()
-        country = 'US'
-        store = item['id']
-        hours = '<MISSING>'
-        lat = item['latitude']
-        lng = item['longitude']
-        if phone == '':
-            phone = '<MISSING>'
-        yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+    state = 'WV'
+    website = 'lgstoreswv.com'
+    country = 'US'
+    hours = '<MISSING>'
+    for line in r.iter_lines():
+        if '"id":"' in line:
+            items = line.split('"id":"')
+            for item in items:
+                if '"address":"' in item:
+                    if '"title":"' in item:
+                        name = item.split('"title":"')[1].split('"')[0]
+                        store = item.split('STORE#')[1].split('<')[0]
+                    if '<\\/strong><\\/p><p>' in item:
+                        addinfo = item.split('<\\/strong><\\/p><p>')[1].split('<\\/span><\\/')[0]
+                    if '><p class=\\"p1\\"><span class=\\"s1\\">' in item:
+                        addinfo = item.split('><p class=\\"p1\\"><span class=\\"s1\\">')[1].split('<\\/span><\\/')[0]
+                    if '/p><p><strong>' in addinfo:
+                        addinfo = addinfo.split('/p><p><strong>')[0]
+                    addinfo = addinfo.replace('\\/span><span class=\\"s1\\">','')
+                    add = addinfo.split(',')[0]
+                    city = addinfo.split(',')[1].strip()
+                    zc = addinfo.rsplit(' ',1)[1]
+                    if '>304-' in item:
+                        phone = '304-' + item.split('>304-')[1].split(' ')[0]
+                        phone = phone.split('<')[0]
+                    else:
+                        phone = '<MISSING>'
+                    typ = name
+                    lat = item.split('"lat":"')[1].split('"')[0]
+                    lng = item.split('"lng":"')[1].split('"')[0]
+                    purl = '<MISSING>'
+                    yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
