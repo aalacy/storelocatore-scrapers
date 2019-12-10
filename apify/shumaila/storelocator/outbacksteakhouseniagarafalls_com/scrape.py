298ca9c2ac427eaa09c50601573932883727b8b6
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2019-12-10 23:16:19 +0500

    Add files via upload

---
+++ apify/shumaila/storelocator/outbacksteakhouseniagarafalls_com/scrape.py
@@ -0,0 +1,93 @@
+#
+import requests
+from bs4 import BeautifulSoup
+import csv
+import string
+import re, time
+
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(
+            ["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code",
+             "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+
+
+def fetch_data():
+    # Your scraper here
+
+    data = []
+
+    pattern = re.compile(r'\s\s+')
+
+    p = 0
+    url = 'http://outbacksteakhouseniagarafalls.com/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text,"html.parser")
+    repo_list = soup.findAll("div",{'class':'split'})
+    print(len(repo_list))
+    for repo in repo_list:
+        try:
+            coord = repo.find('iframe')
+            coord = coord['src']
+            #print(coord)
+            start = coord.find('!2d')+3
+            end = coord.find('!3d',start)
+            lat = coord[start:end]
+            start = end + 3
+            end = coord.find('!2m',start)
+            longt = coord[start:end]
+            repo = repo.text
+            repo = re.sub(pattern," ",repo)
+            address,hours = repo.split("\n")
+            hours = hours.replace("pm","pm ")
+            hours = hours.replace(" Complimentary Parking on site Make Reservations","")
+            hours = hours.replace(" Street Parking in front of building", "")
+            address = address.lstrip()
+
+            title,street = re.split('\d',address,1)
+            title = "Outback Steakhouse " + title
+            address = address.replace(title,"")
+            street,phone = re.split('•',address,1)
+            phone = phone.lstrip()
+
+
+            data.append([
+                'http://outbacksteakhouseniagarafalls.com/',
+                'http://outbacksteakhouseniagarafalls.com/',
+                title,
+                street,
+                'Niagara Falls',
+                'Ontario',
+                "<MISSING>",
+                "CA",
+                "<MISSING>",
+                phone,
+                "<MISSING>",
+                lat,
+                longt,
+                hours
+            ])
+
+
+
+        except:
+            pass
+    return data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+scrape()
+
