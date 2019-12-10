f84c00603fc36fb437bf2f4eb12340cf37f296f3
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2019-12-10 23:12:19 +0500

    Add files via upload

--- apify/Kullaleeni/storelocator/ymca_net/scrape.py
+++ apify/Kullaleeni/storelocator/ymca_net/scrape.py
@@ -12,7 +12,9 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(
+            ["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code",
+             "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -20,18 +22,17 @@ def write_output(data):
 
 def fetch_data():
     # Your scraper here
-    states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
-  "Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
-  "Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
-  "Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
-  "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York",
-  "North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania",
-  "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
-  "Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
-    
-    
-    MAX_RESULTS=20       #max number of results the website gives
-    MAX_DISTANCE=50.0        #max number of distance from the zip it covers
+    states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
+              "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
+              "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+              "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+              "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York",
+              "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
+              "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+              "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+    MAX_RESULTS = 20  # max number of results the website gives
+    MAX_DISTANCE = 50.0  # max number of distance from the zip it covers
     data = []
     p = 0
     pattern = re.compile(r'\s\s+')
@@ -39,38 +40,38 @@ def fetch_data():
     search.initialize()
     query_coord = search.next_zip()
     print(query_coord)
-    #2:56
+    # 2:56
     while query_coord:
-        count =0
-        result_coords=[]                    #mantain the list of coords of data collected
-    #for s in range(0,len(states)):
+        count = 0
+        result_coords = []  # mantain the list of coords of data collected
+        # for s in range(0,len(states)):
         try:
-            url = 'https://www.ymca.net/find-your-y/?address='+query_coord
+            url = 'https://www.ymca.net/find-your-y/?address=' + query_coord
             print(url)
             page = requests.get(url)
-            soup = BeautifulSoup(page.text,"html.parser")
-            mainul = soup.findAll('ul',{'class':'find-y-page'})
-            #print(len(mainul))
+            soup = BeautifulSoup(page.text, "html.parser")
+            mainul = soup.findAll('ul', {'class': 'find-y-page'})
+            # print(len(mainul))
             for ul in mainul:
                 li_list = ul.findAll('li')
-                count += len(li_list)               #to calculate total # of results
+                count += len(li_list)  # to calculate total # of results
                 for li in li_list:
                     try:
                         lat = li['data-latitude']
                         longt = li['data-longitude']
-                        result_coords.append((lat, longt))         #add coords to list
+                        result_coords.append((lat, longt))  # add coords to list
                         link = li.find('a')
                         link = "https://www.ymca.net" + link['href']
                         page1 = requests.get(link)
                         soup1 = BeautifulSoup(page1.text, "html.parser")
-                        soup1 = soup1.find('div',{'class':'col-md-4'})
+                        soup1 = soup1.find('div', {'class': 'col-md-4'})
                         title = soup1.find('h1').text
                         mainp = soup1.findAll('p')
                         address = mainp[0].text
                         phone = address[address.find("Phone"):len(address)]
-                        phone = phone.replace("Phone: ","")
+                        phone = phone.replace("Phone: ", "")
                         address = address[0: address.find("Phone")]
-                        address = address.replace("\n"," ")
+                        address = address.replace("\n", " ")
                         address = usaddress.parse(address)
                         i = 0
                         street = ""
@@ -81,7 +82,8 @@ def fetch_data():
                             temp = address[i]
                             if temp[1].find("Address") != -1 or temp[1].find("Street") != -1 or temp[1].find(
                                     "Recipient") != -1 or \
-                                    temp[1].find("BuildingName") != -1 or temp[1].find("USPSBoxType") != -1 or temp[1].find(
+                                    temp[1].find("BuildingName") != -1 or temp[1].find("USPSBoxType") != -1 or temp[
+                                1].find(
                                 "USPSBoxID") != -1:
                                 street = street + " " + temp[0]
                             if temp[1].find("PlaceName") != -1:
@@ -95,22 +97,23 @@ def fetch_data():
                         hours = mainp[2].text
                         hours = re.sub(pattern, " ", hours)
                         hours = hours.replace("\n", "")
-                        phone = re.sub(pattern,"",phone)
-                        phone = phone.replace("\n","")
-                        pcode = pcode.replace("\n","")
+                        phone = re.sub(pattern, "", phone)
+                        phone = phone.replace("\n", "")
+                        pcode = pcode.replace("\n", "")
                         pcode = pcode.lstrip()
                         street = street.lstrip()
                         city = city.lstrip()
-                        city = city.replace(",","")
+                        city = city.replace(",", "")
                         state = state.lstrip()
                         hours = hours.replace("Hours of Operation: ", "")
 
-
-                        store = link[link.find("=")+1:len(link)]
+                        store = link[link.find("=") + 1:len(link)]
                         if len(phone) < 3:
                             phone = "<MISSING>"
+                        hours = hours.replace("Hours of Operation:","")
                         if len(hours) < 3:
                             hours = "<MISSING>"
+
 
                         flag = True
                         m = 0
@@ -137,21 +140,21 @@ def fetch_data():
                                 longt,
                                 hours
                             ])
-                            print(p,",",data[p])
+                            print(p, ",", data[p])
                             p += 1
 
                     except:
                         pass
         except:
             pass
-        if count < MAX_RESULTS:                         #check a near zip code
+        if count < MAX_RESULTS:  # check a near zip code
             print("max distance update")
             search.max_distance_update(MAX_DISTANCE)
-        elif len(array) == MAX_RESULTS:                  #check to save lat lngs to find zip that excludes them
+        elif count == MAX_RESULTS:  # check to save lat lngs to find zip that excludes them
             print("max count update")
             search.max_count_update(result_coords)
         else:
-            print("oops! the maxcount should be",count)
+            print("oops! the maxcount should be", count)
             raise Exception("expected at most " + MAX_RESULTS + " results")
 
         query_coord = search.next_zip()
@@ -159,10 +162,9 @@ def fetch_data():
     return data
 
 
-
 def scrape():
     data = fetch_data()
     write_output(data)
 
-scrape()
 
+scrape()
