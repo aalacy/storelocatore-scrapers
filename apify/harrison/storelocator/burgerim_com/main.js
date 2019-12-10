9ddf7910d2cab8988b73bab305965af0061c5e94
Tenzing Shaw <tenzing@safegraph.com>
2019-12-10 15:36:52 -0800

    refactor main

--- apify/harrison/storelocator/burgerim_com/main.js
+++ apify/harrison/storelocator/burgerim_com/main.js
@@ -3,54 +3,64 @@ const child_process = require("child_process");
 const util = require('util');
 const fs = require('fs-extra');
 const csv = util.promisify(require('csv-parse'));  
-const glob = require("glob");
+const glob = require("glob-promise");
+
+const readFileAsync = util.promisify(fs.readFile);
+const exec = util.promisify(child_process.exec);
 
 function fail(message) {
 	console.log(message);
 	process.exit(1);
 }
 
-(async () => {
+async function listDataFiles() {
+	const files = await glob("./apify_storage/datasets/default/*.json");
+	return files;
+}
+
+async function readDataFiles(files) {
+	let pois = [];
+	files.forEach(async function(file) {
+		const data = await readFileAsync(file, {encoding: "utf8"});
+		pois.push(JSON.parse(data));
+	});
+	return pois;
+}
+
+async function pushToCloud() {
+	try {
+		const files = await listDataFiles();
+		const pois = await readDataFiles(files);
+		await Apify.pushData(pois);
+	} catch(error) {
+		console.log(error);
+		fail("error parsing output data!");
+	}
+}
+
+async function runScraper() {
 	console.log("in main.js");
 	console.log("starting scrape.....");
-	const exec = util.promisify(child_process.exec);
+	const environment = { env: {
+		APIFY_LOCAL_STORAGE_DIR: "apify_storage",
+		APIFY_CHROME_EXECUTABLE_PATH: "/usr/bin/google-chrome",
+		NODE_ENV: "production",
+		NODE_PATH: "/usr/local/lib/node_modules/"
+	}};
 	let err, stdout, stderr;
 	try {
-		const environment = { env: {
-			APIFY_LOCAL_STORAGE_DIR: "apify_storage",
-			APIFY_CHROME_EXECUTABLE_PATH: "/usr/bin/google-chrome",
-			NODE_ENV: "production",
-			NODE_PATH: "/usr/local/lib/node_modules/"
-		}};
 		({ err, stdout, stderr } = await exec('node scrape.js', environment));
 	} catch(err) {
 		fail(`error executing scraper: ${err}`);
 	}
 	console.log('stdout:', stdout);
 	console.log('stderr:', stderr);
+}
 
+(async () => {
+	await runScraper();
 	if (!fs.existsSync('./apify_storage/datasets/default')) {
 		fail("scraper did not produce any output data!");
 	}
-
-	let pois = [];
-	try {
-		glob("./apify_storage/datasets/default/*.json", function(err, files) {
-			if(err) {
-				console.log("Cannot read output data directory", err);
-			}
-			files.forEach(function(file) {
-				fs.readFile(file, 'utf8', function (err, data) {
-					if(err) {
-						console.log("Failed to read file: ", err);
-					}
-					let item = JSON.parse(data);
-					await Apify.pushData(item);
-				});
-			});
-		});
-	} catch(error) {
-		console.log(error);
-		fail("error parsing output data!");
-	}
+	await pushToCloud();
 })();
