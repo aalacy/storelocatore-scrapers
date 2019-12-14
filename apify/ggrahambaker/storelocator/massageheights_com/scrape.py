f2bbbf71ccdacfa672deaa1dda39c83b00d08527
Graham Baker <graham.cr.baker@gmail.com>
2019-12-14 15:17:57 -0600

    maintain pushes

--- apify/ggrahambaker/storelocator/massageheights_com/scrape.py
+++ apify/ggrahambaker/storelocator/massageheights_com/scrape.py
@@ -1,26 +1,14 @@
 import csv
 import os
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import requests
 import json
-
-
-
-def get_driver():
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--window-size=1920,1080')
-    return webdriver.Chrome('chromedriver', options=options)
-
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -28,17 +16,17 @@ def write_output(data):
 
 
 def fetch_data():
-    locator_domain = 'https://www.massageheights.com/'
-    ext = 'locations/?CallAjax=GetLocations'
+    locator_domain = 'https://www.massageheights.com'
+    ext = '/locations/?CallAjax=GetLocations'
+    to_scrape = locator_domain + ext
+    page = requests.get(to_scrape)
+    assert page.status_code == 200
 
-    driver = get_driver()
-    driver.get(locator_domain + ext)
-
-    arr = driver.find_element_by_css_selector('body').text
-    json_data = json.loads(arr)
+    locs = json.loads(page.content)
     all_store_data = []
 
-    for a in json_data:
+
+    for a in locs:
         location_name = a['FranchiseLocationName']
         street_address = a['Address1']
         street_address += ' ' + a['Address2']
@@ -48,27 +36,32 @@ def fetch_data():
         phone_number = a['Phone']
         lat = a['Latitude']
         longit = a['Longitude']
-        location_hours = a['LocationHours'][1:-1].split('][')
 
-        hours = ''
-        for a in location_hours:
-            temp = '{' + a + '}'
-            hours_json = json.loads(temp)
-            day = hours_json['Interval']
-            open_start = hours_json['OpenTime']
-            close_start = hours_json['CloseTime']
-            day_info = day + ' ' + open_start + ' - ' + close_start
-            hours += day_info + ' '
+        location_hours = a['LocationHours']
+        page_url = locator_domain + a['Path'] 
+        if location_hours == '':
+            hours = '<MISSING>'
+        else:
+            hours = ''
+            location_hours = location_hours[1:-1].split('][')
+            for a in location_hours:
+                temp = '{' + a + '}'
+                hours_json = json.loads(temp)
+                day = hours_json['Interval']
+                open_start = hours_json['OpenTime']
+                close_start = hours_json['CloseTime']
+                day_info = day + ' ' + open_start + ' - ' + close_start
+                hours += day_info + ' '
 
         country_code = 'US'
         store_number = '<MISSING>'
         location_type = '<MISSING>'
+        
 
         store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
-                      store_number, phone_number, location_type, lat, longit, hours]
+                    store_number, phone_number, location_type, lat, longit, hours, page_url]
         all_store_data.append(store_data)
 
-    driver.quit()
     return all_store_data
 
 def scrape():
---
+++ apify/ggrahambaker/storelocator/working/maintain/scandinaviandesigns_com/scrape.py
@@ -0,0 +1,59 @@
+import csv
+import requests
+import json
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    url = 'https://cdn.shopify.com/s/files/1/1921/1117/t/324/assets/sca.storelocatordata.json'
+    locator_domain = 'https://scandinaviandesigns.com/'
+    page = requests.get(url)
+    assert page.status_code == 200
+    
+    locs = json.loads(page.content)
+
+    all_store_data = []
+    for loc in locs:
+        lat = loc['lat']
+        longit = loc['lng']
+        hours = loc['schedule'].replace('<br>', ' ')
+
+        if 'Soon' in hours:
+            continue
+            
+        location_name = loc['name']
+        phone_number = loc['phone']
+        page_url = loc['web']
+        
+        street_address = loc['address']
+        state = loc['state']
+        city = loc['city']
+        zip_code = loc['postal']
+        
+        
+        country_code = 'US'
+
+        location_type = '<MISSING>'
+        store_number = '<MISSING>'
+        
+        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
+                        store_number, phone_number, location_type, lat, longit, hours, page_url]
+        all_store_data.append(store_data)
+
+
+
+    return all_store_data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
