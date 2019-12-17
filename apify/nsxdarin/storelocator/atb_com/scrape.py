4e86d5989b4119e6f8098daec65c8415e12589b0
nsxdarin <nsxdarin@gmail.com>
2019-12-17 14:33:14 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/atb_com/scrape.py
+++ apify/nsxdarin/storelocator/atb_com/scrape.py
@@ -1,1 +1,66 @@
+import csv
+import urllib2
+import requests
 
+session = requests.Session()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    url = 'https://www.atb.com/resources/find-a-location/'
+    locs = []
+    r = session.get(url, headers=headers)
+    for line in r.iter_lines():
+        if 'window.branchData' in line:
+            items = line.split('"name":"')
+            for item in items:
+                if 'window.branchData' not in item:
+                    lurl = 'https://www.atb.com' + item.split('"url":"')[1].split('"')[0]
+                    name = item.split('"')[0]
+                    website = 'atb.com'
+                    item.split('"')[0]
+                    store = item.split('"Id":"')[1].split('"')[0]
+                    add = item.split('"address":"')[1].split('\\')[0]
+                    city = item.split('"cityName":"')[1].split('"')[0].title()
+                    country = 'CA'
+                    state = item.split('\\r\\n')[1].split(',')[1].strip().split(' ')[0]
+                    zc = item.split('\\r\\n')[1].split(',')[1].strip().split(' ',1)[1].split('"')[0]
+                    lat = item.split('"lat":')[1].split('}')[0]
+                    lng = item.split('"lng":')[1].split(',')[0]
+                    typ = item.split('"type":"')[1].split('"')[0]
+                    hours = ''
+                    phone = ''
+                    r2 = session.get(lurl, headers=headers)
+                    print('Pulling Location %s...' % lurl)
+                    lines = r2.iter_lines()
+                    for line2 in lines:
+                        if '<a href="tel:' in line2:
+                            phone = line2.split('<a href="tel:')[1].split('"')[0]
+                        if '<li class="col-5">' in line2:
+                            day = line2.split('<li class="col-5">')[1].split('<')[0]
+                            g = next(lines)
+                            if '>' not in g:
+                                g = next(lines)
+                            hrs = g.split('>')[1].split('<')[0]
+                            if hours == '':
+                                hours = day + ': ' + hrs
+                            else:
+                                hours = hours + '; ' + day + ': ' + hrs
+                    if phone == '':
+                        phone = '<MISSING>'
+                    if hours == '':
+                        hours = '<MISSING>'
+                    yield [website, lurl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
