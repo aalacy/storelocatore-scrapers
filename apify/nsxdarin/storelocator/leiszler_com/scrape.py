3b80c8bafa7c6faaa0098128b3ef6397b7bf0c63
nsxdarin <nsxdarin@gmail.com>
2019-12-17 17:00:19 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/leiszler_com/scrape.py
+++ apify/nsxdarin/storelocator/leiszler_com/scrape.py
@@ -2,7 +2,6 @@ import csv
 import urllib2
 import requests
 import os
-import usaddress
 
 session = requests.Session()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
@@ -11,57 +10,62 @@ headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "location_name", "raw_address", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
 
 def fetch_data():
-    url = 'http://leiszler.com/locations.aspx'
+    coords = []
+    places = []
+    url = 'https://www.leiszler.com/locations/'
     r = session.get(url, headers=headers)
     lines = r.iter_lines()
     for line in lines:
-        if 'var myLatlng' in line:
-            lat = line.split('(')[1].split(',')[0]
-            lng = line.split(',')[1].split(')')[0]
-            website = 'leiszler.com'
-            typ = 'Store'
-            hours = '<MISSING>'
+        if 'var infowindow = new google.maps' in line:
+            title = line.split('"title">')[1].split('<')[0].replace('&#039;',"'")
             g = next(lines)
-            address = ''
-            city = ''
-            state = ''
-            zc = ''
-            rawadd = g.split('input name="daddr" value="')[1].split('"')[0]
-            try:
-                add = usaddress.tag(rawadd)
-                baseadd = add[0]
-                if 'AddressNumber' not in baseadd:
-                    baseadd['AddressNumber'] = ''
-                if 'StreetName' not in baseadd:
-                    baseadd['StreetName'] = ''
-                if 'StreetNamePostType' not in baseadd:
-                    baseadd['StreetNamePostType'] = ''
-                if 'PlaceName' not in baseadd:
-                    baseadd['PlaceName'] = '<INACCESSIBLE>'
-                if 'StateName' not in baseadd:
-                    baseadd['StateName'] = '<INACCESSIBLE>'
-                if 'ZipCode' not in baseadd:
-                    baseadd['ZipCode'] = '<INACCESSIBLE>'
-                address = add[0]['AddressNumber'] + ' ' + add[0]['StreetName'] + ' ' + add[0]['StreetNamePostType']
-                address = address.encode('utf-8')
-                if address == '':
-                    address = '<INACCESSIBLE>'
-                city = add[0]['PlaceName'].encode('utf-8')
-                state = add[0]['StateName'].encode('utf-8')
-                zc = add[0]['ZipCode'].encode('utf-8')
-            except:
-                pass
-            name = g.split("content: '")[1].split('<')[0]
-            country = 'US'
-            store = g.split('Short Stop #')[1].split('<')[0]
-            phone = g.split('<br />')[2].split('<')[0]
-            yield [website, name, rawadd, address, city, state, zc, country, store, phone, typ, lat, lng, hours]
-
+            latlng = g.split('new google.maps.LatLng(')[1].split(',')[0] + '|' + g.split(', ')[1].split(')')[0]
+            coords.append(title + '|' + latlng)
+        if 'class="img-responsive" /><a href="https://www.leiszler.com/locations/location/' in line:
+            items = line.split('class="img-responsive" /><a href="')
+            for item in items:
+                if '<div class="row location-rows">' not in item:
+                    loc = item.split('"')[0]
+                    name = item.split('<span itemprop="name">')[1].split('<')[0]
+                    try:
+                        phone = item.split('itemprop="telephone">')[1].split('<')[0]
+                    except:
+                        phone = '<MISSING>'
+                    hours = '<MISSING>'
+                    website = 'leiszler.com'
+                    add = item.split('"streetAddress">')[1].split('<')[0]
+                    city = item.split('"addressLocality">')[1].split('<')[0]
+                    zc = item.split('"postalCode">')[1].split('<')[0]
+                    state = item.split('"addressRegion">')[1].split('<')[0]
+                    country = 'US'
+                    store = '<MISSING>'
+                    typ = 'Store'
+                    hours = '<MISSING>'
+                    info = website + '|' + loc + '|' + name + '|' + add + '|' + city + '|' + state + '|' + zc + '|' + country + '|' + store + '|' + phone + '|' + typ + '|' + hours
+                    places.append(info)
+    for item in coords:
+        for place in places:
+            if place.split('|')[2] == item.split('|')[0]:
+                lat = item.split('|')[1]
+                lng = item.split('|')[2]
+                website = place.split('|')[0]
+                loc = place.split('|')[1]
+                name = place.split('|')[2]
+                add = place.split('|')[3]
+                city = place.split('|')[4]
+                state = place.split('|')[5]
+                zc = place.split('|')[6]
+                country = place.split('|')[7]
+                store = place.split('|')[8]
+                phone = place.split('|')[9]
+                typ = place.split('|')[10]
+                hours = place.split('|')[11]
+                yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 def scrape():
     data = fetch_data()
     write_output(data)
