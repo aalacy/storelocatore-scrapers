f849129b6ccd05059cecb1f3e0ca4cef33a5a7e7
nsxdarin <nsxdarin@gmail.com>
2019-12-17 15:45:28 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/redroof_com/scrape.py
+++ apify/nsxdarin/storelocator/redroof_com/scrape.py
@@ -2,6 +2,8 @@ import csv
 import urllib2
 import requests
 
+requests.packages.urllib3.disable_warnings()
+
 session = requests.Session()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
