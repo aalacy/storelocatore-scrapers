1536e42f22b994d7966ea1deed18c1ceda40e611
nsxdarin <nsxdarin@gmail.com>
2019-12-19 10:42:48 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/dickswingsandgrill_com/scrape.py
+++ apify/nsxdarin/storelocator/dickswingsandgrill_com/scrape.py
@@ -1,1 +1,104 @@
+# -*- coding: cp1252 -*-
+import csv
+import urllib2
+import requests
 
+requests.packages.urllib3.disable_warnings()
+
+session = requests.Session()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    url = 'https://dickswingsandgrill.com/'
+    locs = []
+    r = session.get(url, headers=headers, verify=False)
+    Found = False
+    for line in r.iter_lines():
+        if 'LOCATIONS</span></a>' in line:
+            Found = True
+        if Found and '>CONTACT</span>' in line:
+            Found = False
+        if Found and '"><a href="' in line and '#locations' not in line:
+            lurl = line.split('href="')[1].split('"')[0]
+            if 'http' in lurl:
+                locs.append(lurl)
+            else:
+                locs.append('https://dickswingsandgrill.com' + lurl)
+    print('Found %s Locations.' % str(len(locs)))
+    for loc in locs:
+        name = ''
+        add = ''
+        city = ''
+        state = ''
+        store = '<MISSING>'
+        lat = ''
+        lng = ''
+        hours = ''
+        country = 'US'
+        zc = ''
+        phone = ''
+        print('Pulling Location %s...' % loc)
+        website = 'dickswingsandgrill.com'
+        typ = 'Restaurant'
+        r2 = session.get(loc, headers=headers)
+        for line2 in r2.iter_lines():
+            if '<title>' in line2:
+                name = line2.split('<title>')[1].split(' |')[0]
+            if '<h2>' in line2 and '<h2><' not in line2 and 'Weekly' not in line2:
+                add = line2.split('<h2>')[1].split('•')[0]
+                phone = '(' + line2.split('(')[1].split('<')[0].strip()
+            if '!2d-' in line2:
+                lng = line2.split('!2d')[1].split('!')[0]
+                lat = line2.split('!2d-')[1].split('!3d')[1].split('!')[0]
+                city = line2.split('%2C+')[1].replace('+',' ')
+                state = line2.split('%2C+')[2].split('+')[0]
+                zc = line2.split('%2C+')[2].split('+')[1].split('!')[0]
+            if '&#8211;' in line2 and '</span>' not in line2:
+                hrs = line2.replace('\r','').replace('\n','').replace('\t','').replace('<strong>','').replace('</strong>','').replace('&#8211;','-').replace('<p style="text-align: right;">','').strip()
+                if hours == '':
+                    hours = hrs
+                else:
+                    hours = hours + '; ' + hrs
+        if '•' in add:
+            add = add.split('•')[0].strip()
+        if phone == '':
+            phone = '<MISSING>'
+        if zc == '':
+            zc = '<MISSING>'
+        if add == '':
+            add = '<MISSING>'
+        if city == '':
+            city = '<MISSING>'
+        if hours == '':
+            hours = '<MISSING>'
+        if lat == '':
+            lat = '<MISSING>'
+        if lng == '':
+            lng = '<MISSING>'
+        if state == '':
+            state = 'FL'
+        hours = hours.replace('<br />','').replace('</p>','')
+        if 'pm' not in hours:
+            hours = '<MISSING>'
+        if '(' in add:
+            add = add.split('(')[0].strip()
+        if '·' in add:
+            add = add.split('·')[0].strip()
+        add = add.replace('Â â€¢Â','').replace('Â â€¢','').strip()
+        if 'Â' in add:
+            add = add.split('Â')[0].strip()
+        yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
