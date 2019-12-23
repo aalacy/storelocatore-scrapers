0cb34b2439df815e05b4eb569d9a796154c7ce57
Tenzing Shaw <tenzing@safegraph.com>
2019-12-23 11:26:18 -0800

    try proxy

--- apify/himanshu/storelocator/choicehotels_com__ascend/scrape.py
+++ apify/himanshu/storelocator/choicehotels_com__ascend/scrape.py
@@ -5,6 +5,7 @@ import re
 import unicodedata
 import sgzip
 import datetime
+import os
 
 def write_output(data):
     with open('data.csv', mode='w',encoding="utf-8") as output_file:
@@ -15,6 +16,15 @@ def write_output(data):
         # Body
         for row in data:
             writer.writerow(row)
+
+session = requests.Session()
+proxy_password = os.environ["PROXY_PASSWORD"]
+proxy_url = "http://auto:{}@proxy.apify.com:8000/".format(proxy_password)
+proxies = {
+    'http': proxy_url,
+    'https': proxy_url
+}
+session.proxies = proxies
 
 def fetch_data():
     main_url = "https://choicehotels.com/ascend"
@@ -41,7 +51,7 @@ def fetch_data():
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
         }
         data = "adults=1&checkInDate=" + str(today) + "&checkOutDate=" + str(tomorrow) + "&lat=" + str(x) + "&lon=" + str(y) + "&minors=0&optimizeResponse=image_url&platformType=DESKTOP&preferredLocaleCode=en-us&ratePlanCode=RACK&ratePlans=RACK%2CPREPD%2CPROMO%2CFENCD&rateType=LOW_ALL&rooms=1&searchRadius=100&siteName=us&siteOpRelevanceSortMethod=ALGORITHM_B"
-        r = requests.post("https://www.choicehotels.com/webapi/location/hotels",headers=headers,data=data)
+        r = session.post("https://www.choicehotels.com/webapi/location/hotels",headers=headers,data=data)
         
         if "hotels" not in r.json():
             search.max_distance_update(MAX_DISTANCE)
