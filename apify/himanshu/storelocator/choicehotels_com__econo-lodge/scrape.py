422d0e5e3d0464e1c0e008f00eadd3c4836fc311
Tenzing Shaw <tenzing@safegraph.com>
2019-12-23 18:47:01 -0800

    proxy up

--- apify/himanshu/storelocator/choicehotels_com__econo-lodge/scrape.py
+++ apify/himanshu/storelocator/choicehotels_com__econo-lodge/scrape.py
@@ -6,6 +6,39 @@ import unicodedata
 import sgzip
 import datetime
 
+import os
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+requests.packages.urllib3.disable_warnings()
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504)
+):
+    session = requests.Session()
+    proxy_password = os.environ["PROXY_PASSWORD"]
+    proxy_url = "http://groups-RESIDENTIAL,session-s1:{}@proxy.apify.com:8000/".format(proxy_password)
+    proxies = {
+        'http': proxy_url,
+        'https': proxy_url
+    }
+    session.proxies = proxies
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
+session = requests_retry_session()
+
 def write_output(data):
     with open('data.csv', mode='w',encoding="utf-8") as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -42,7 +75,7 @@ def fetch_data():
             "cookie":'',
         }
         data = "adults=1&checkInDate=" + str(today) + "&checkOutDate=" + str(tomorrow) + "&lat=" + str(x) + "&lon=" + str(y) + "&minors=0&optimizeResponse=image_url&platformType=DESKTOP&preferredLocaleCode=en-us&ratePlanCode=RACK&ratePlans=RACK%2CPREPD%2CPROMO%2CFENCD&rateType=LOW_ALL&rooms=1&searchRadius=100&siteName=us&siteOpRelevanceSortMethod=ALGORITHM_B"
-        r = requests.post("https://www.choicehotels.com/webapi/location/hotels",headers=headers,data=data)
+        r = session.post("https://www.choicehotels.com/webapi/location/hotels",headers=headers,data=data)
         if "hotels" not in r.json():
             search.max_distance_update(MAX_DISTANCE)
             coord = search.next_coord()
