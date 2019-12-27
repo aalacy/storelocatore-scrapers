376237a27bf1d1df42a1b288d616535568e7cf0e
nsxdarin <nsxdarin@gmail.com>
2019-12-27 11:27:59 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/pnc_com/scrape.py
+++ apify/nsxdarin/storelocator/pnc_com/scrape.py
@@ -27,71 +27,70 @@ def fetch_data():
     url = 'https://apps.pnc.com/locator-api/locator/api/v1/locator/browse?t=1575479568229'
     r = session.get(url, headers=headers, verify=False)
     locs = []
+    badlist = ['01298040','00142040','00145040','00704040','00074040',
+               '00711070','00408070','00853001','00645020','00771040']
     for line in r.iter_lines():
         if '"externalId" : "' in line:
             lid = line.split('" : "')[1].split('"')[0]
-            if lid != '01298040':
+            if lid not in badlist:
                 locs.append(lid)
+    print('Found %s Locations...' % str(len(locs)))
     for loc in locs:
-        time.sleep(5)
         lurl = 'https://apps.pnc.com/locator-api/locator/api/v2/location/' + loc
-        r2 = session.get(lurl, headers=headers, timeout=3, verify=False)
-        lines = r2.iter_lines()
-        website = 'pnc.com'
-        HFound = False
-        hours = ''
-        TypFound = False
-        PageFound = True
-        while PageFound:
-            try:
-                PageFound = False
-                print('Pulling Location %s...' % loc)
-                for line2 in lines:
-                    if '"locationName" : "' in line2:
-                        name = line2.split('"locationName" : "')[1].split('"')[0]
-                    if '"locationTypeDesc" : "' in line2 and TypFound is False:
-                        TypFound = True
-                        typ = line2.split('"locationTypeDesc" : "')[1].split('"')[0]
-                        store = loc
-                    if '"address1" : "' in line2:
-                        add = line2.split('"address1" : "')[1].split('"')[0]
-                    if '"address2" : "' in line2:
-                        add = add + ' ' + line2.split('"address2" : "')[1].split('"')[0]
-                    if '"city" : "' in line2:
-                        city = line2.split('"city" : "')[1].split('"')[0]
-                    if '"state" : "' in line2:
-                        state = line2.split('"state" : "')[1].split('"')[0]
-                    if '"zip" : "' in line2:
-                        zc = line2.split('"zip" : "')[1].split('"')[0]
-                    if '"latitude" : ' in line2:
-                        lat = line2.split('"latitude" : ')[1].split(',')[0]
-                    if '"longitude" : ' in line2:
-                        lng = line2.split('"longitude" : ')[1].split(',')[0]
-                    if '"contactDescriptionDB" : "External Phone",' in line2:
-                        phone = next(lines).split(' : "')[1].split('"')[0]
-                    if '"serviceNameDB" : "Lobby Hours"' in line2:
-                        HFound = True
-                    if HFound and '"hoursByDayIndex"' in line2:
-                        HFound = False
-                    if HFound and 'day"' in line2:
-                        day = line2.split('"')[1]
-                        g = next(lines)
-                        h = next(lines)
-                        if 'null' in g:
-                            hrs = 'Closed'
-                        else:
-                            hrs = g.split('"')[3] + '-' + h.split('"')[3]
-                        if hours == '':
-                            hours = day + ': ' + hrs
-                        else:
-                            hours = hours + '; ' + day + ': ' + hrs
-                country = 'US'
-                purl = 'https://apps.pnc.com/locator/#/result-details/' + loc
-                if hours == '':
-                    hours = '<MISSING>'
-                yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
-            except:
-                PageFound = True
+        try:
+            r2 = session.get(lurl, headers=headers, timeout=3, verify=False)
+            lines = r2.iter_lines()
+            website = 'pnc.com'
+            HFound = False
+            hours = ''
+            TypFound = False
+            print('Pulling Location %s...' % loc)
+            for line2 in lines:
+                if '"locationName" : "' in line2:
+                    name = line2.split('"locationName" : "')[1].split('"')[0]
+                if '"locationTypeDesc" : "' in line2 and TypFound is False:
+                    TypFound = True
+                    typ = line2.split('"locationTypeDesc" : "')[1].split('"')[0]
+                    store = loc
+                if '"address1" : "' in line2:
+                    add = line2.split('"address1" : "')[1].split('"')[0]
+                if '"address2" : "' in line2:
+                    add = add + ' ' + line2.split('"address2" : "')[1].split('"')[0]
+                if '"city" : "' in line2:
+                    city = line2.split('"city" : "')[1].split('"')[0]
+                if '"state" : "' in line2:
+                    state = line2.split('"state" : "')[1].split('"')[0]
+                if '"zip" : "' in line2:
+                    zc = line2.split('"zip" : "')[1].split('"')[0]
+                if '"latitude" : ' in line2:
+                    lat = line2.split('"latitude" : ')[1].split(',')[0]
+                if '"longitude" : ' in line2:
+                    lng = line2.split('"longitude" : ')[1].split(',')[0]
+                if '"contactDescriptionDB" : "External Phone",' in line2:
+                    phone = next(lines).split(' : "')[1].split('"')[0]
+                if '"serviceNameDB" : "Lobby Hours"' in line2:
+                    HFound = True
+                if HFound and '"hoursByDayIndex"' in line2:
+                    HFound = False
+                if HFound and 'day"' in line2:
+                    day = line2.split('"')[1]
+                    g = next(lines)
+                    h = next(lines)
+                    if 'null' in g:
+                        hrs = 'Closed'
+                    else:
+                        hrs = g.split('"')[3] + '-' + h.split('"')[3]
+                    if hours == '':
+                        hours = day + ': ' + hrs
+                    else:
+                        hours = hours + '; ' + day + ': ' + hrs
+            country = 'US'
+            purl = 'https://apps.pnc.com/locator/#/result-details/' + loc
+            if hours == '':
+                hours = '<MISSING>'
+            yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+        except requests.exceptions.ReadTimeout:
+            pass
 
 def scrape():
     data = fetch_data()
