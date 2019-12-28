1378f8366bd1f6f5154e80ec24d5e032d70c7fc5
Tenzing Shaw <tenzing@safegraph.com>
2019-12-27 16:09:53 -0800

    fix import statement

--- apify/nsxdarin/storelocator/7-eleven_com/scrape.py
+++ apify/nsxdarin/storelocator/7-eleven_com/scrape.py
@@ -1,7 +1,7 @@
 import csv
 import urllib2
 import requests
-import sgrequests
+from sgrequests import SgRequests
 
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
