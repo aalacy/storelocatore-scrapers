51ff8d760a416a9727f6325f7d7645db7aef1241
graham <graham.cr.baker@gmail.com>
2019-12-28 16:47:12 -0600

    fix savealot

--- apify/ggrahambaker/storelocator/savealot_com/scrape.py
+++ apify/ggrahambaker/storelocator/savealot_com/scrape.py
@@ -16,40 +16,59 @@ def write_output(data):
 def fetch_data():
     locator_domain = 'https://savealot.com/'
     day_lookup = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
-    loc_json_url = 'https://savealot.com/grocery-stores/locationfinder/modules/multilocation/?near_lat=47.600392&near_lon=-122.3353602&threshold=4000&services__in=&within_business=true'
-
-    page = requests.get(loc_json_url)
+    
+    init_search_url = 'near_lat=47.600392&near_lon=-122.3353602&threshold=4000&services__in=&within_business=true&limit=2000'
+    base_url = 'https://savealot.com/grocery-stores/locationfinder/modules/multilocation/?'
+    page = requests.get(base_url + init_search_url)
     loc_json = json.loads(page.content)
-
+    rest_url = loc_json['meta']['next']
+    
     all_store_data = []
-    for loc in loc_json['objects']:
-        phone_number = loc['phonemap_e164']['phone']
-        state = loc['state']
-        street_address = loc['street']
-        zip_code = loc['postal_code']
-        page_url = loc['location_url']
-        longit = loc['lon']
-        lat = loc['lat']
-        city = loc['city']
+    
+    while True:
+        for loc in loc_json['objects']:
+            phone_number = loc['phonemap_e164']['phone']
+            state = loc['state']
+            street_address = loc['street']
+            zip_code = loc['postal_code']
+            page_url = loc['location_url']
+            longit = loc['lon']
+            lat = loc['lat']
+            city = loc['city']
 
-        hours_arr = loc['hours_of_operation']
-        hours = ''
-        for i, day in enumerate(hours_arr):
-            day_start = day[0][0]
-            day_end = day[0][1]
-            day_hours = day_lookup[i] + ' ' + day_start + ' : ' + day_end + ' '
-            hours += day_hours
+            hours_arr = loc['hours_of_operation']
+            hours = ''
 
-        hours = hours.strip()
+            for i, day in enumerate(hours_arr):
+                
+                if len(day[0]) == 0:
+                    day_hours = day_lookup[i] + ' Closed '
+                else:
+                    day_start = day[0][0]
+                    day_end = day[0][1]
+                    day_hours = day_lookup[i] + ' ' + day_start + ' : ' + day_end + ' '
+                hours += day_hours
 
-        country_code = 'US'
-        location_name = '<MISSING>'
-        location_type = '<MISSING>'
-        store_number = '<MISSING>'
+            hours = hours.strip()
 
-        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
-                      store_number, phone_number, location_type, lat, longit, hours, page_url]
-        all_store_data.append(store_data)
+            country_code = 'US'
+            location_name = '<MISSING>'
+            location_type = '<MISSING>'
+            store_number = '<MISSING>'
+
+            store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
+                        store_number, phone_number, location_type, lat, longit, hours, page_url]
+            all_store_data.append(store_data)
+        
+        rest_url = loc_json['meta']['next']
+
+        if rest_url != None:
+            rest_url_temp = rest_url.split('?')[1]
+            page = requests.get(base_url + rest_url_temp)
+
+            loc_json = json.loads(page.content)
+        else:
+            break
 
     return all_store_data
 
