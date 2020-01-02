4ad51bed041e074818f07ab326ce93c143452b26
Tenzing Shaw <tenzing@safegraph.com>
2020-01-02 13:35:20 -0800

    queue with retries

--- apify/nsxdarin/storelocator/7-eleven_com/scrape.py
+++ apify/nsxdarin/storelocator/7-eleven_com/scrape.py
@@ -2,6 +2,7 @@ import csv
 import urllib2
 import requests
 from sgrequests import SgRequests
+import collections
 
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
@@ -24,7 +25,10 @@ def fetch_data():
             count = lurl.count('/')
             if count == 6:
                 locs.append(lurl)
-    for loc in locs:
+    q = collections.deque(locs)
+    attempts = {}
+    while q:
+        loc = q.popleft()
         website = '7-eleven.com'
         typ = ''
         name = '7-Eleven'
@@ -38,7 +42,18 @@ def fetch_data():
         lat = ''
         lng = ''
         country = 'US'
-        r2 = session.get(loc, headers=headers)
+        r2 = None
+        try :
+            r2 = session.get(loc, headers=headers)
+        except ConnectionError:
+            print('Failed to connect to ' + loc)
+            if attempts.get(loc, 0) >= 3:
+                print('giving up on ' + loc)
+            else:
+                q.append(loc)
+                attempts[loc] = attempts.get(loc, 0) + 1
+                print('attempts: ' + str(attempts[loc]))
+            continue
         for line2 in r2.iter_lines():
             if '"hours":{"message":"' in line2:
                 hours = line2.split('"hours":{"message":"')[1].split('"')[0]
