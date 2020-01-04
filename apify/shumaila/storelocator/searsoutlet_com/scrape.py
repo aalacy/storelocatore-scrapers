67d5718b4addf2be195bdc8df9c1d72a0ebcf5a5
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-01-04 17:19:54 +0500

    Add files via upload

--- apify/shumaila/storelocator/searsoutlet_com/scrape.py
+++ apify/shumaila/storelocator/searsoutlet_com/scrape.py
@@ -25,137 +25,110 @@ def fetch_data():
     links = []
     p = 1
     pattern = re.compile(r'\s\s+')
-    url = "https://www.sears.com/stores.html"
+    url = "https://www.searsoutlet.com/br/api/stores?_=1578134784196"
     page = requests.get(url)
-
-    soup = BeautifulSoup(page.text, "html.parser")
-    mainul = soup.find('ul',{'id':'stateList'})
-    statelist = mainul.findAll('a')
-    for state in statelist:
-        if state['href'].find('404') == -1:
-            statelink = "https://www.sears.com" + state['href']
-            #print(statelink)
-            flag1 = True
-            while flag1:
-                try:
-                    page1 = requests.get(statelink)
-                    soup1 = BeautifulSoup(page1.text, "html.parser")
-                    maindiv = soup1.find('div', {'id': 'cityList'})
-                    linklist = maindiv.findAll('a',{'itemprop':'url'})
-                    for blinks in linklist:
-                        link = blinks['href']
-                        if link.find("http") == -1 and blinks.text.find("Outlet") > -1 :
-                            #print("enter")
-                            link = "https://www.sears.com" + link
-                            #print(link)
-                            flag = True
-                            while flag:
-                                try:
-                                    page2 = requests.get(link)
-                                    soup2 = BeautifulSoup(page2.text, "html.parser")
-                                    try:
-                                        title = soup2.find('h1').text
-                                    except:
-                                        title = soup2.find('small', {'itemprop': 'name'}).text
-
-                                    title = re.sub(pattern, " ", title)
-                                    start = title.find("#")
-                                    if start != -1:
-                                        start = start + 2
-                                        store = title[start:len(title)]
-                                    else:
-                                        store = "<MISSING>"
-                                    try:
-                                        street = soup2.find('span',{'itemprop':'streetAddress'}).text
-                                        street = street.lstrip()
-                                    except:
-                                        street = "<MISSING>"
-                                    try:
-                                        city = soup2.find('span', {'itemprop': 'addressLocality'}).text
-                                        city = city.lstrip()
-                                    except:
-                                        city = "<MISSING>"
-                                    try:
-                                        state = soup2.find('span', {'itemprop': 'addressRegion'}).text
-                                        state = state.lstrip()
-                                    except:
-                                        state = "<MISSING>"
-                                    try:
-                                        pcode = soup2.find('span', {'itemprop': 'postalCode'}).text
-                                    except:
-                                        pcode = "<MISSING>"
-                                    try:
-                                        phone = soup2.find('span', {'itemprop': 'telephone'}).text
-                                    except:
-                                        phone = "<MISSING>"
-                                    try:
-                                        hourd = soup2.findAll('tr',{'itemprop':'openingHoursSpecification'})
-                                        hours = ""
-                                        for hour in hourd:
-
-                                            hours = hours + hour.text + " "
-                                            hours = re.sub(pattern, " ", hours)
-                                    except:
-                                        hours = "<MISSING>"
-                                    try:
-                                        soup2 = str(soup2)
-                                        start = soup2.find('lat =')
-                                        start = soup2.find('=',start) + 2
-                                        #print(start)
-                                        end = soup2.find(',',start)
-                                        lat = soup2[start:end]
-                                        start = soup2.find('lon =')
-                                        start = soup2.find('=',start) + 2
-                                        end = soup2.find(',', start)
-                                        longt = soup2[start:end]
-                                    except:
-                                        lat =  "<MISSING>"
-                                        longt =  "<MISSING>"
-                                    hours = hours.replace("\n"," ")
-                                    hours = hours.strip()
-                                    title = title.lstrip()
-
-                                    #print(link)
-                                    #print(title)
-                                    #print(store)
-                                    #print(street)
-                                    #print(city)
-                                    #print(state)
-                                    #print(pcode)
-                                    #print(phone)
-                                    #print(hours)
-                                    #print(lat)
-                                    #print(longt)
-
-                                    data.append([
-                                        'https://www.searsoutlet.com/',
-                                        link,
-                                        title,
-                                        street,
-                                        city,
-                                        state,
-                                        pcode,
-                                        'US',
-                                        store,
-                                        phone,
-                                        "<MISSING>",
-                                        lat,
-                                        longt,
-                                        hours
-                                    ])
-                                    flag = False
-                                except:
-                                    pass
-
-                    flag1 = False
-
-                except:
-                    pass
-
-
+    page = page.text
+    flag = True
+    start = 0
+    i = 0
+    while flag:
+        start = page.find('storeName',start)
+        if start == -1:
+            flag = False
+        else:
+            start = page.find(':',start)+1
+            end = page.find(',',start)
+            title = page[start:end]
+            title = title.replace('"',"")            
+            start = end
+            start = page.find('"streetAddr"',start)
+            start = page.find(':',start)+1
+            end = page.find(',',start)
+            street = page[start:end]
+            street = street.replace('"',"")            
+            start = end
+            start = page.find('"city"',start)
+            start = page.find(':',start)+1
+            end = page.find(',',start)
+            city = page[start:end]
+            city = city.replace('"',"")            
+            start = end
+            start = page.find('"state"',start)
+            start = page.find(':',start)+1
+            end = page.find(',',start)
+            state = page[start:end]
+            state = state.replace('"',"")            
+            start = end
+            start = page.find('"zip"',start)
+            start = page.find(':',start)+1
+            end = page.find(',',start)
+            pcode = page[start:end]
+            pcode = pcode.replace('"',"")            
+            start = end
+            start = page.find('"phone"',start)
+            start = page.find(':',start)+1
+            end = page.find(',',start)
+            phone = page[start:end]
+            phone = phone.replace('"',"")            
+            start = end
+            start = page.find('"streetAddr2"',start)
+            start = page.find(',',start)+1
+            end = page.find('"storeIndex"',start)-1
+            hours = page[start:end]
+            hours = hours.replace('"',"")
+            hours = hours.replace("Hours","")
+            start = end
+            start = page.find('"storeIndex"',start)
+            start = page.find(':',start)+1
+            end = page.find(',',start)
+            store = page[start:end]
+            store = store.replace('"',"")            
+            start = end
+            start = page.find('"url"',start)
+            start = page.find(':',start)+1
+            end = page.find(',',start)
+            link = page[start:end]
+            link = link.replace('"',"")
+            link = 'https://www.searsoutlet.com'+link
+            page2 = requests.get(link)
+            page2 = page2.text
+            start1 = page2.find('"latitude"')
+            start1 = page2.find(':',start1)+1
+            end1 = page2.find(',',start1)
+            lat = page2[start1:end1]
+            start1 = page2.find('"longitude"')
+            start1 = page2.find(':',start1)+1
+            end1 = page2.find(',',start1)
+            longt = page2[start1:end1]
+            longt = longt[0:longt.find('\n')]
+            lat = lat.replace('"','')
+            longt = longt.replace('"','')
+            
+            data.append([
+             'https://www.searsoutlet.com/',
+             link,
+              title,
+              street,
+              city,
+              state,
+              pcode,
+              'US',
+              store,
+              phone,
+              '<MISSING>',
+              lat,
+              longt,
+              hours
+            ])
+            #print(data[i])
+            i += 1
+            start = end
+            
+            
+            
+  
 
     return data
-
 
 def scrape():
     data = fetch_data()
