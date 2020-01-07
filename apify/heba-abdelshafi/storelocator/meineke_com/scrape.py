d673861c23b4385943a7ecd581df64b6b995d7f8
Heba Abd Elshafi <heba.abdelshafi1@gmail.com>
2020-01-07 20:04:24 +0200

    create script

---
+++ apify/heba-abdelshafi/storelocator/meineke_com/scrape.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from time import sleep
+import pandas as pd
+import itertools
+
+
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("user-agent= 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'")
+driver = webdriver.Chrome("chromedriver", options=options)
+
+driver=webdriver.Chrome('C:\webdrivers\chromedriver.exe')#, options=options)
+
+
+def write_output(data):
+    df=pd.DataFrame(data)
+    df.to_csv('data.csv', index=False)
+
+
+def fetch_data():
+
+    data={'locator_domain':[],'location_name':[],'street_address':[],'city':[], 'state':[], 'zip':[], 'country_code':[], 'store_number':[],'phone':[], 'location_type':[], 'latitude':[], 'longitude':[], 'hours_of_operation':[],'page_url':[]}
+    driver.get('https://www.meineke.com/locations/')    
+    status_url=[i.get_attribute('href') for i in driver.find_elements_by_xpath('//div[@class="col-md-4 col-sm-5 left-list "]//li/a')]
+    
+    city_urls=[]    
+    for city in status_url:
+        driver.get(city)
+        city_urls.append([i.get_attribute('href') for i in driver.find_elements_by_xpath('//li/a[@class="city-link"]')])    
+    city_urls=list(itertools.chain.from_iterable(city_urls))
+
+    locations=[]
+    for loc in city_urls:
+        driver.get(loc)
+        sleep(3)
+        locations.append([i.get_attribute('data-ng-click')[i.get_attribute('data-ng-click').find("(")+1:i.get_attribute('data-ng-click').find("rawSemCamPhone")] for i in driver.find_elements_by_xpath("//div[contains(@data-ng-click, 'vm.reloadMap')]")])
+        data['hours_of_operation'].append([i.text.replace('Store Hours\n','') for i in driver.find_elements_by_xpath('//div[@class="segment-store"]')])
+        data['page_url'].append([i.get_attribute('href') for i in driver.find_elements_by_xpath('//div[@class="segment-store-info"]/a')])
+    locations=list(itertools.chain.from_iterable(locations))
+    data['hours_of_operation']=list(itertools.chain.from_iterable(data['hours_of_operation']))
+    data['page_url']=list(itertools.chain.from_iterable(data['page_url']))
+
+    for i in locations:
+        data['locator_domain'].append('https://www.meineke.com')
+        data['location_name'].append('Meineke')
+        data['street_address'].append(i.split(',')[5].split(':')[-1])
+        data['city'].append(i.split(',')[3].split(':')[-1])
+        data['state'].append(i.split(',')[7].split(':')[-1])
+        data['zip'].append(int(i.split(',')[8].split(':')[-1]))
+        data['country_code'].append('US')
+        data['store_number'].append(i.split(',')[2])
+        data['phone'].append(i.split(',')[9].split(':')[-1])
+        data['location_type'].append('Car Care Center')
+        data['latitude'].append(int(i.split(',')[0]))
+        data['longitude'].append(int(i.split(',')[1]))
+        
+        
+    driver.close()
+    return data
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+scrape()
