8d4838080953e111259c43ac8af0ba59aa9343dc
odettaAM <52939294+odettaAM@users.noreply.github.com>
2020-01-09 10:56:37 +0530

    Add files via upload

---
+++ apify/tayyabaq/storelocator/sunglasshut_com/scrape.py
@@ -0,0 +1,132 @@
+import csv
+import os
+import requests
+from bs4 import BeautifulSoup
+import re, time
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    data = []
+    store_links =[]
+    clear_links =[]
+    #CA stores
+    urls = ['https://stores.sunglasshut.com/ca.html','https://stores.sunglasshut.com/gb.html','https://stores.sunglasshut.com/us.html']
+    u='https://stores.sunglasshut.com/'
+    for url in urls:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        store=soup.find_all('a',class_='c-directory-list-content-item-link')
+        for i in store:
+            newurl=u+i['href']
+            page = requests.get(newurl)
+            soup = BeautifulSoup(page.content, "html.parser")
+            store=soup.find_all('a',class_='c-directory-list-content-item-link')
+            if len(store)==0:
+                lnk=soup.find_all('h5',class_='c-location-grid-item-title')
+                if len(lnk) == 0:
+                    store_links.append(newurl)
+                else:
+                    for m in lnk:
+                        h=m.find("a")['href']
+                        ul=u+h
+                        ul=ul.replace("../","")
+                        store_links.append(ul)
+            else:
+                for j in store:
+                    nurl=u+j['href']
+                    nurl=nurl.replace("../","")
+                    page = requests.get(nurl)
+                    soup = BeautifulSoup(page.content, "html.parser")
+                    loc=soup.find('div',class_='banner-h1-content h1-large').text
+                    if 'Locationsin ' in loc:
+                        lnk=soup.find_all('h5',class_='c-location-grid-item-title')
+                        for m in lnk:
+                            h=m.find("a")['href']
+                            ul=u+h
+                            ul=ul.replace("../","")
+                            store_links.append(ul)
+                    else:
+                        store_links.append(nurl)
+    for u in store_links:
+        page = requests.get(u)
+        soup = BeautifulSoup(page.content, "html.parser")
+        loc=soup.find('div',class_='banner-h1-content h1-large').text
+        if loc == "":
+            loc='<MISSING>'
+        lat=soup.find('meta',itemprop='latitude')['content']
+        lng=soup.find('meta',itemprop='longitude')['content']
+        try :
+            ctry=soup.find('span',itemprop='addressCountry').text
+        except:
+            ctry = '<MISSING>'                    
+        strt=soup.find_all('span',itemprop='streetAddress')
+        street=""
+        for k in strt:
+            street+=k.text
+            street+=" "
+        if len(strt)== 0:
+            street='<MISSING>'
+        try:
+            cty=soup.find('span',itemprop='addressLocality').text
+        except:
+            cty = '<MISSING>'  
+        cty=cty.replace(",","")
+        try:
+            sts=soup.find('span',itemprop='addressRegion').text
+        except:
+            sts ='<MISSING>'  
+        try:
+            zcode=soup.find('span',itemprop='postalCode').text
+        except:
+            zcode = '<MISSING>'  
+        try :
+            ph=soup.find('span',itemprop='telephone').text
+        except:
+            ph = '<MISSING>'  
+        hr=soup.find_all('meta',itemprop='openingHours')
+        hours =""
+        for l in hr:
+            hours+=l['content']
+            hours+=" "
+        if hours == "":
+            hours='<MISSING>'
+        hours=hours.replace("Mo","MON")
+        hours=hours.replace("Tu","TUE")
+        hours=hours.replace("We","WED")
+        hours=hours.replace("Th","THU")
+        hours=hours.replace("Fr","FRI")
+        hours=hours.replace("Sa","SAT")
+        hours=hours.replace("Su","SUN")
+        data.append([
+             'https://www.sunglasshut.com/',
+              u,
+             loc,
+             street,
+             cty,
+             sts,
+             zcode,
+             ctry,
+             '<MISSING>',
+             ph,
+             '<MISSING>',
+             lat,
+             lng,
+             hours
+             ])
+    return data
+    
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
