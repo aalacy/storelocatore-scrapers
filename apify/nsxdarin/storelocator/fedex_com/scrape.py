35c92271435cb0fba4ed28f59a922a6bae35a550
Tenzing Shaw <tenzing@safegraph.com>
2020-01-13 12:35:04 -0800

    keep alive

--- apify/nsxdarin/storelocator/fedex_com/scrape.py
+++ apify/nsxdarin/storelocator/fedex_com/scrape.py
@@ -4,8 +4,10 @@ from sgrequests import SgRequests
 import json
 
 session = SgRequests()
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
-           }
+headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+        'Connection':'Keep-Alive'
+}
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
