47e1bda758e2ff851d5fc650bf3d32a6761676a3
nsxdarin <nsxdarin@gmail.com>
2020-01-14 09:42:16 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/anytimefitness_com/scrape.py
+++ apify/nsxdarin/storelocator/anytimefitness_com/scrape.py
@@ -1,1 +1,52 @@
+import csv
+import urllib2
+from sgrequests import SgRequests
+import json
 
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    locs = []
+    url = 'https://www.anytimefitness.com/wp-content/uploads/gyms.json'
+    r = session.get(url, headers=headers)
+    for line in r.iter_lines():
+        if '"latitude":"' in line:
+            items = line.split('"latitude":"')
+            for item in items:
+                if '"country":"US"' in item or '"country":"CA"' in item:
+                    lat = item.split('"')[0]
+                    lng = item.split('"longitude":"')[1].split('"')[0]
+                    website = 'anytimefitness.com'
+                    hours = 'Open 24 Hours'
+                    name = item.split('"title":"')[1].split('"')[0]
+                    typ = '<MISSING>'
+                    loc = item.split('"url":"')[1].split('"')[0].replace('\\/','/')
+                    add = item.split('"address":"')[1].split('"')[0]
+                    if '"address2":"' in item:
+                        add = add + ' ' + item.split('"address2":"')[1].split('"')[0]
+                    city = item.split('"city":"')[1].split('"')[0]
+                    state = item.split('"state_abbr":"')[1].split('"')[0]
+                    country = item.split('"country":"')[1].split('"')[0]
+                    zc = item.split('"zip":"')[1].split('"')[0]
+                    try:
+                        phone = item.split('"phone":"')[1].split('"')[0]
+                    except:
+                        phone = '<MISSING>'
+                    store = item.split('"number":"')[1].split('"')[0]
+                    if country == 'US' or country == 'CA':
+                        yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
