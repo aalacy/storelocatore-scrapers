004e6c9424ce7f050b0306d478236c2de21979db
nsxdarin <nsxdarin@gmail.com>
2020-01-22 06:46:18 -0800

    Update scrape.py

--- apify/nsxdarin/storelocator/selectmedical_com/scrape.py
+++ apify/nsxdarin/storelocator/selectmedical_com/scrape.py
@@ -16,16 +16,16 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
-    url = 'https://www.selectmedical.com//sxa/search/results/?s={648F4C3A-C9EA-4FCF-82A3-39ED2AC90A06}&itemid={94793D6A-7CC7-4A8E-AF41-2FB3EC154E1C}&sig=&autoFireSearch=true&v={D2D3D65E-3A18-43DD-890F-1328E992446A}&p=200&e=0'
+    url = 'https://www.selectmedical.com//sxa/search/results/?s={648F4C3A-C9EA-4FCF-82A3-39ED2AC90A06}&itemid={94793D6A-7CC7-4A8E-AF41-2FB3EC154E1C}&sig=&autoFireSearch=true&v={D2D3D65E-3A18-43DD-890F-1328E992446A}&p=50&e=0&g=&o=Distance,Ascending'
     r = session.get(url, headers=headers)
     count = 0
     for line in r.iter_lines():
         if '"Count":' in line:
             count = int(line.split('"Count":')[1].split(',')[0])
     print('Found %s Locations...' % str(count))
-    for x in range(0, count + 16, 8):
+    for x in range(0, count + 75, 50):
         print('Pulling Results %s...' % str(x))
-        url2 = 'https://www.selectmedical.com//sxa/search/results/?s={648F4C3A-C9EA-4FCF-82A3-39ED2AC90A06}&itemid={94793D6A-7CC7-4A8E-AF41-2FB3EC154E1C}&sig=&autoFireSearch=true&v={D2D3D65E-3A18-43DD-890F-1328E992446A}&p=8&e=' + str(x)
+        url2 = 'https://www.selectmedical.com//sxa/search/results/?s={648F4C3A-C9EA-4FCF-82A3-39ED2AC90A06}&itemid={94793D6A-7CC7-4A8E-AF41-2FB3EC154E1C}&sig=&autoFireSearch=true&v={D2D3D65E-3A18-43DD-890F-1328E992446A}&p=50&e=' + str(x) + '&g=&o=Distance,Ascending'
         r2 = session.get(url2, headers=headers)
         for line2 in r2.iter_lines():
             if '"Id":"' in line2:
