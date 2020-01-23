5fa4c37c8401fe6c5d9fde5f2f37dcc76089c67c
diapk <Hank4957!>
2020-01-23 16:00:13 +0530

    docker run and push

--- apify/himanshu/storelocator/ruthschris_com/scrape.py
+++ apify/himanshu/storelocator/ruthschris_com/scrape.py
@@ -24,7 +24,7 @@ def request_wrapper(url,method,headers,data=None):
                 return r
                 break
             except:
-                time.sleep(2)
+                time.sleep(5)
                 request_counter = request_counter + 1
                 if request_counter > 10:
                     return None
