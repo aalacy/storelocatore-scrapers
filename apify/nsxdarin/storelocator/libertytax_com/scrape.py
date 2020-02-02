91922907f6850d9d9cd44d9d6ffd993570b53fe9
nsxdarin <nsxdarin@gmail.com>
2020-02-01 20:15:55 -0800

    Update scrape.py

--- apify/nsxdarin/storelocator/libertytax_com/scrape.py
+++ apify/nsxdarin/storelocator/libertytax_com/scrape.py
@@ -34,54 +34,59 @@ def fetch_data():
                     if 'margin-top' in item:
                         locs.append('https://www.libertytax.com/income' + item.split('"')[0])
         for loc in locs:
-            print('Pulling Location %s...' % loc)
-            website = 'libertyfax.com'
-            typ = '<MISSING>'
-            hours = ''
-            name = ''
-            add = ''
-            city = ''
-            state = ''
-            zc = ''
-            country = 'US'
-            store = loc.rsplit('/',1)[1].split('.')[0]
-            phone = ''
-            lat = ''
-            lng = ''
-            r2 = session.get(loc, headers=headers)
-            hcount = 0
-            for line2 in r2.iter_lines():
-                if '<section class="office-details-body-text">' in line2:
-                    hcount = hcount + 1
-                if '<h1>' in line2:
-                    name = line2.split('<h1>')[1].split('<')[0]
-                if '"streetAddress": "' in line2:
-                    add = line2.split('"streetAddress": "')[1].split('"')[0]
-                if '"addressLocality": "' in line2:
-                    city = line2.split('"addressLocality": "')[1].split('"')[0]
-                if '"addressRegion": "' in line2:    
-                    state = line2.split('"addressRegion": "')[1].split('"')[0]
-                if '"postalCode": "' in line2:
-                    zc = line2.split('"postalCode": "')[1].split('"')[0]
-                if '"telephone": "' in line2:
-                    phone = line2.split('"telephone": "')[1].split('"')[0]
-                if 'Latitude:' in line2:
-                    lat = line2.split('Latitude:')[1].split(',')[0].strip()
-                if 'Longitude:' in line2:
-                    lng = line2.split('Longitude:')[1].split(',')[0].strip()
-                if '<span class="office-day-name">' in line2 and hcount == 0:
-                    day = line2.split('<span class="office-day-name">')[1].split('<')[0]
-                if '<span class="office-day-hours">' in line2 and hcount == 0:
-                    hrs = line2.split('<span class="office-day-hours">')[1].split('<')[0]
+            LFound = True
+            while LFound:
+                try:
+                    LFound = False
+                    website = 'libertyfax.com'
+                    typ = '<MISSING>'
+                    hours = ''
+                    name = ''
+                    add = ''
+                    city = ''
+                    state = ''
+                    zc = ''
+                    country = 'US'
+                    store = loc.rsplit('/',1)[1].split('.')[0]
+                    phone = ''
+                    lat = ''
+                    lng = ''
+                    r2 = session.get(loc, headers=headers)
+                    hcount = 0
+                    for line2 in r2.iter_lines():
+                        if '<section class="office-details-body-text">' in line2:
+                            hcount = hcount + 1
+                        if '<h1>' in line2:
+                            name = line2.split('<h1>')[1].split('<')[0]
+                        if '"streetAddress": "' in line2:
+                            add = line2.split('"streetAddress": "')[1].split('"')[0]
+                        if '"addressLocality": "' in line2:
+                            city = line2.split('"addressLocality": "')[1].split('"')[0]
+                        if '"addressRegion": "' in line2:    
+                            state = line2.split('"addressRegion": "')[1].split('"')[0]
+                        if '"postalCode": "' in line2:
+                            zc = line2.split('"postalCode": "')[1].split('"')[0]
+                        if '"telephone": "' in line2:
+                            phone = line2.split('"telephone": "')[1].split('"')[0]
+                        if 'Latitude:' in line2:
+                            lat = line2.split('Latitude:')[1].split(',')[0].strip()
+                        if 'Longitude:' in line2:
+                            lng = line2.split('Longitude:')[1].split(',')[0].strip()
+                        if '<span class="office-day-name">' in line2 and hcount == 0:
+                            day = line2.split('<span class="office-day-name">')[1].split('<')[0]
+                        if '<span class="office-day-hours">' in line2 and hcount == 0:
+                            hrs = line2.split('<span class="office-day-hours">')[1].split('<')[0]
+                            if hours == '':
+                                hours = day + ': ' + hrs
+                            else:
+                                hours = hours + '; ' + day + ': ' + hrs
                     if hours == '':
-                        hours = day + ': ' + hrs
-                    else:
-                        hours = hours + '; ' + day + ': ' + hrs
-            if hours == '':
-                hours = '<MISSING>'
-            if phone == '':
-                phone = '<MISSING>'
-            yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+                        hours = '<MISSING>'
+                    if phone == '':
+                        phone = '<MISSING>'
+                    yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+                except:
+                    LFound = True
 
 def scrape():
     data = fetch_data()
