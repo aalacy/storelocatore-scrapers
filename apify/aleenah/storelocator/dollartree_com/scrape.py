700f9932d2d4e04642910c034ccf08ee1207e557
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-02-15 16:15:51 +0500

    Update scrape.py

--- apify/aleenah/storelocator/dollartree_com/scrape.py
+++ apify/aleenah/storelocator/dollartree_com/scrape.py
@@ -21,38 +21,66 @@ session = SgRequests()
 
 def fetch_data():
     # Your scraper here
-    locs = []
-    street = []
-    states = []
-    cities = []
-    types = []
-    phones = []
-    zips = []
-    long = []
-    lat = []
-    timing = []
-    ids = []
+    
     page_url = []
-    countries = []
 
     res=session.get("https://www.dollartree.com/locations/")
     soup = BeautifulSoup(res.text, 'html.parser')
     sls = soup.find('div', {'class': 'content_area'}).find_all('a')
     print("sls: ",len(sls))
+    count=0
     for sl in sls:
         res = session.get(sl.get('href'))
-        print(sl.get('href'))
+        #print(sl.get('href'))
         soup = BeautifulSoup(res.text, 'html.parser')
         cls = soup.find('div', {'class': 'content_area'}).find_all('a')
-        print("cls",len(cls))
+        #print("cls",len(cls))
         for cl in cls:
             res = session.get(cl.get('href'))
-            print(cl.get('href'))
+            #print(cl.get('href'))
             soup = BeautifulSoup(res.text, 'html.parser')
             pls = soup.find_all('div', {'class': 'storeinfo_div '})
-            print("pls: ",len(pls))
+            #print("pls: ",len(pls))
             for p in pls:
+                p=p.find('a')
                 page_url.append(p.get('href'))
-                print(p.get('href'))
-            break
-fetch_data()
+                count+=1
+                #print(p.get('href'))
+    print("count: ",count)  
+    all =[]
+    for url in page_url:
+        res = session.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        data = soup.find_all('script', {'type': 'application/ld+json'})[1].contents
+        js=json.loads("".join(data))
+        addr=js["address"]
+        timl=js["openingHoursSpecification"]
+        tim=""
+        for l in timl:
+            tim+= l["dayOfWeek"]+": "+l["opens"]+" - "+l["closes"]+" "
+        
+        all.append([
+        "https://www.dollartree.com/",
+        js["ContainedIn"],
+        addr["streetAddress"],
+        addr["addressLocality"],
+        addr["addressRegion"],
+        addr["postalCode"].split("-")[0],
+        addr["addressCountry"],
+        js["@id"], 
+        js["telephone"],  
+        js["@type"],  
+        js["geo"]["latitude"], 
+        js["geo"]["longitude"], 
+        tim.strip(),  
+        url])
+        
+    return all
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+scrape()
