0746fa0dada18e6396a4c1d555cee738d6195c95
nsxdarin <nsxdarin@gmail.com>
2020-02-18 09:02:13 -0800

    Update scrape.py

--- apify/nsxdarin/storelocator/swgaoil_com__ss-food-stores/scrape.py
+++ apify/nsxdarin/storelocator/swgaoil_com__ss-food-stores/scrape.py
@@ -47,8 +47,28 @@ def fetch_data():
         store = '<MISSING>'
         country = 'US'
         typ = '<MISSING>'
-        hours = '<MISSING>'
-        phone = '<MISSING>'
+        hours = ''
+        phone = ''
+        r2 = session.get(llink, headers=headers)
+        lines = r2.iter_lines()
+        for line2 in lines:
+            if 'Phone' in line2:
+                phone = next(lines).split('<')[0].strip().replace('\t','')
+            if 'Hours:' in line2:
+                Found = True
+            if Found and '</p>' in line2:
+                Found = False
+                hours = hours + '; ' + line2.split('<')[0].strip().replace('\t','')
+            if Found and 'Hours:' not in line2:
+                hrs = line2.split('<')[0].strip().replace('\t','')
+                if hours == '':
+                    hours = hrs
+                else:
+                    hours = hours + '; ' + hrs
+        if hours == '':
+            hours = '<MISSING>'
+        if phone == '':
+            phone = '<MISSING>'
         yield [website, llink, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
