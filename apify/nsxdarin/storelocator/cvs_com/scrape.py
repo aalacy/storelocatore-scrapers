bcb447e1c27c5055abbe9895d357da19fe0d0b2f
nsxdarin <nsxdarin@gmail.com>
2020-02-18 09:16:03 -0800

    Update scrape.py

--- apify/nsxdarin/storelocator/cvs_com/scrape.py
+++ apify/nsxdarin/storelocator/cvs_com/scrape.py
@@ -86,6 +86,7 @@ def fetch_data():
                 hours = '<MISSING>'
             if phone == '':
                 phone = '<MISSING>'
+            hours = hours.replace(':00:00',':00').replace(':30:00',':30')
             yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
