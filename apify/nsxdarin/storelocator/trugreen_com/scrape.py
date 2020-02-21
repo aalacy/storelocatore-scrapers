de6faae7b6d89f9aa5e92b7ae2ff516e403a9a8e
nsxdarin <nsxdarin@gmail.com>
2020-02-21 09:11:47 -0800

    Update scrape.py

--- apify/nsxdarin/storelocator/trugreen_com/scrape.py
+++ apify/nsxdarin/storelocator/trugreen_com/scrape.py
@@ -2,8 +2,6 @@ import csv
 import urllib2
 import requests
 
-requests.packages.urllib3.disable_warnings()
-
 session = requests.Session()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
@@ -16,9 +14,9 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
-    url = 'https://www.trugreen.com/local-sitemap.xml'
+    url = 'https://www.trugreen.com/sitemap.xml'
     locs = []
-    r = session.get(url, headers=headers, verify=False)
+    r = session.get(url, headers=headers)
     for line in r.iter_lines():
         if '<loc>https://www.trugreen.com/local-lawn-care/' in line:
             lurl = line.split('<loc>')[1].split('<')[0]
@@ -34,9 +32,10 @@ def fetch_data():
         zc = ''
         phone = ''
         hours = ''
+        #print('Pulling Location %s...' % loc)
         website = 'trugreen.com'
-        typ = 'Location'
-        r2 = session.get(loc, headers=headers, verify=False)
+        typ = '<MISSING>'
+        r2 = session.get(loc, headers=headers)
         for line2 in r2.iter_lines():
             if '"name": "' in line2:
                 name = line2.split('"name": "')[1].split('"')[0]
@@ -50,8 +49,8 @@ def fetch_data():
                 state = line2.split('"addressRegion": "')[1].split('"')[0]
             if '"postalCode": "' in line2:
                 zc = line2.split('"postalCode": "')[1].split('"')[0]
-            if '"telePhone": "' in line2:
-                phone = line2.split('"telePhone": "')[1].split('"')[0]
+            if '"telephone": "' in line2:
+                phone = line2.split('"telephone": "')[1].split('"')[0]
             if '"latitude": "' in line2:
                 lat = line2.split('"latitude": "')[1].split('"')[0]
             if '"longitude": "' in line2:
@@ -66,6 +65,14 @@ def fetch_data():
                     hours = hours + '; ' + day + ': ' + hrs
         country = 'US'
         store = '<MISSING>'
+        if phone == '':
+            phone = '<MISSING>'
+        if city == '':
+            city = '<MISSING>'
+        if zc == '':
+            zc = '<MISSING>'
+        if state == '':
+            state = '<MISSING>'
         if add != '':
             yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
