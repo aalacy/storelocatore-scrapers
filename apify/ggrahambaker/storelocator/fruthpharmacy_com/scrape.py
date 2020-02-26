8b00eb28ef9b36eef7e12d93228ccc8c71fad258
graham <graham.cr.baker@gmail.com>
2020-02-26 12:45:19 -0800

    fixes

--- apify/ggrahambaker/storelocator/fruthpharmacy_com/scrape.py
+++ apify/ggrahambaker/storelocator/fruthpharmacy_com/scrape.py
@@ -2,6 +2,8 @@ import csv
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+import time
 
 
 def get_driver():
@@ -17,7 +19,7 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -28,46 +30,83 @@ def fetch_data():
 
     driver = get_driver()
     driver.get(locator_domain + ext)
+    driver.implicitly_wait(10)
+    time.sleep(2)
 
-    main = driver.find_element_by_css_selector('div#map_sidebar')
-    locs = main.find_elements_by_css_selector('div.results_entry.location_primary')
+    select = Select(driver.find_element_by_id('radiusSelect'))
+    select.select_by_value('500')
+    time.sleep(2)
+
+
+    states = ['Kentucky', 'virginia', 'west virginia']
     all_store_data = []
-    for loc in locs:
-        location_name = loc.find_element_by_css_selector('span.location_name').text
-        street_address = loc.find_element_by_css_selector('span.slp_result_address.slp_result_street').text
-        address = loc.find_element_by_css_selector('span.slp_result_address.slp_result_citystatezip').text.split(',')
+    dup_tracker = set()
+    for state in states:
 
-        city = address[0]
-        state = address[1].strip()
-        if len(state.split(' ')) == 2:
-            state_zip = state.split(' ')
-            state = state_zip[0]
-            zip_code = state_zip[1]
-        else:
-            zip_code = '<MISSING>'
+        addy_input = driver.find_element_by_css_selector('input#addressInput')
+        addy_input.send_keys(state)
 
-        phone_number = loc.find_element_by_css_selector('span.slp_result_address.slp_result_phone').text
-        hours_div = loc.find_element_by_css_selector('span.slp_result_contact.slp_result_hours.textblock').text.split(
-            '\n')
 
-        if len(hours_div) == 1:
-            hours = '<MISSING>'
-        else:
-            hours = ''
-            for h in hours_div[:3]:
-                hours += h + ' '
+        driver.find_element_by_id('addressSubmit').click()
 
-            hours = hours.replace('Store Hours:', '').strip()
+        time.sleep(2)
 
-        country_code = 'US'
-        store_number = '<MISSING>'
-        location_type = '<MISSING>'
-        lat = '<MISSING>'
-        longit = '<MISSING>'
+        main = driver.find_element_by_css_selector('div#map_sidebar')
+        locs = main.find_elements_by_css_selector('div.results_entry')
 
-        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
-                      store_number, phone_number, location_type, lat, longit, hours]
-        all_store_data.append(store_data)
+        for loc in locs:
+            location_name = loc.find_element_by_css_selector('span.location_name').text
+            if location_name not in dup_tracker:
+                dup_tracker.add(location_name)
+            else:
+                continue
+            street_address = loc.find_element_by_css_selector('span.slp_result_address.slp_result_street').text
+            address = loc.find_element_by_css_selector('span.slp_result_address.slp_result_citystatezip').text.split(',')
+
+            city = address[0]
+            state = address[1].strip()
+            if len(state.split(' ')) == 2:
+                state_zip = state.split(' ')
+                state = state_zip[0]
+                if state_zip[1].isdigit():
+                    zip_code = state_zip[1]
+                else:
+                    state += ' ' + state_zip[1]
+                
+                zip_code == '<MISSING>'
+            elif len(state.split(' ')) == 3:
+                state_zip = state.split(' ')
+                state = state_zip[0] + ' ' + state_zip[1]
+                zip_code = state_zip[2]
+
+            else:
+                zip_code = '<MISSING>'
+
+            phone_number = loc.find_element_by_css_selector('span.slp_result_address.slp_result_phone').text
+            if phone_number == '':
+                phone_number = '<MISSING>'
+
+            hours_div = loc.find_element_by_css_selector('span.slp_result_hours').text.split('\n')
+
+            if len(hours_div) == 1:
+                hours = '<MISSING>'
+            else:
+                hours = ''
+                for h in hours_div[:3]:
+                    hours += h + ' '
+
+                hours = hours.replace('Store Hours:', '').strip()
+
+            country_code = 'US'
+            store_number = '<MISSING>'
+            location_type = '<MISSING>'
+            lat = '<MISSING>'
+            longit = '<MISSING>'
+            page_url = '<MISSING>'
+
+            store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
+                        store_number, phone_number, location_type, lat, longit, hours, page_url]
+            all_store_data.append(store_data)
 
     driver.quit()
     return all_store_data
