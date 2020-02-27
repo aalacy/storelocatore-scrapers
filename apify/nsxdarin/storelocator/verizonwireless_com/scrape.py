eb511b7b24dd0a465f8154bc0f4140cc5604bc62
nsxdarin <nsxdarin@gmail.com>
2020-02-27 10:03:42 -0800

    Update scrape.py

--- apify/nsxdarin/storelocator/verizonwireless_com/scrape.py
+++ apify/nsxdarin/storelocator/verizonwireless_com/scrape.py
@@ -10,7 +10,7 @@ session = requests.Session()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
 
-MAX_RESULTS = 25
+MAX_RESULTS = 250
 MAX_DISTANCE = 5
 
 def write_output(data):
