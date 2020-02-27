5413d5ca0011573f677dd08924f944534973b70c
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-02-28 01:44:30 +0500

    Update scrape.py

--- apify/aleenah/storelocator/awaytravel_com/scrape.py
+++ apify/aleenah/storelocator/awaytravel_com/scrape.py
@@ -67,7 +67,7 @@ def fetch_data():
             "https://www.awaytravel.com",
             loc,
             street,
-            city,
+            city.replace(",","")
             state,
             zip,
             con,
