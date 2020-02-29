64307d5682330e62659de18c8af32904cedebc8e
diapk <Hank4957!>
2020-02-29 18:24:59 +0530

    docker run and push

---
+++ apify/himanshu/storelocator/hollywoodtans_com/scrape.py
@@ -0,0 +1,97 @@
+import csv
+from sgrequests import SgRequests
+from bs4 import BeautifulSoup
+import re
+import json
+session = SgRequests()
+import requests
+
+def write_output(data):
+    with open('data.csv', mode='w', encoding="utf-8") as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    headers = {
+             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36',    
+    }
+
+    base_url = "http://hollywoodtans.com/" 
+    try:
+        r = requests.get("http://hollywoodtans.com/locations/", headers=headers)
+    except:
+        pass
+    soup = BeautifulSoup(r.text, "lxml")
+    json_data = json.loads(soup.find(lambda tag: (tag.name == "script" and "map_options" in tag.text)).text.split(".maps(")[1].split(").data(")[0])['places']
+
+    for data in json_data:
+        if data['location']['country'] != "United States":
+            continue
+        location_name = data['title'].replace("Hollywood Tans Fairfax VA","Hollywood Tans Fairfax , VA")
+        street_address = data['address'].split(',')[0]
+        # print(street_address)
+        city = data['location']['city'].replace("11","Nottingham").replace("3","Severna Park").replace("7","Westminster").split(",")[-1].strip() 
+        state = location_name.split(",")[-1].strip()
+        zipp = data['location']['postal_code']
+        if zipp == "1045":
+            zipp = "19053"
+        country_code = data['location']['country'].replace("United States","US")
+        latitude = data['location']['lat']
+        longitude = data['location']['lng']
+        phone = data['content'].replace("Phone: ","").replace("\r\n","").strip()
+        location_type = "Salon"
+        store_number = data['id']   
+        page_url = data['location']['redirect_custom_link']
+        # print(page_url)
+        r1 = requests.get(page_url, headers=headers)
+        soup1 = BeautifulSoup(r1.text , "lxml")
+        hours = soup1.find_all("div",{"class":"gdlr-item gdlr-content-item"})[-2].find_all("p")
+        if len(hours) == 5:
+            hours = " ".join(list(hours[3].stripped_strings)).replace("Salon Hours*:","").strip()
+        elif len(hours) == 6:
+            hours = " ".join(list(hours[4].stripped_strings)).replace("Salon Hours*:","").strip()
+        elif len(hours) ==4:
+            hours = " ".join(list(hours[2].stripped_strings)).replace("Salon Hours*:","").strip()
+        else:
+            hours = " ".join(list(hours[4].stripped_strings)).replace("Salon Hours*:","").strip() \
+                +" "+ " ".join(list(hours[5].stripped_strings)).replace("Salon Hours*:","").strip()
+                    
+    
+        store = []
+        
+        store.append(base_url)
+        store.append(location_name)
+        store.append(street_address)
+        store.append(city)
+        store.append(state)
+        store.append(zipp)   
+        store.append(country_code)
+        store.append(store_number)
+        store.append(phone)
+        store.append(location_type)
+        store.append(latitude )
+        store.append(longitude )
+        store.append(hours)
+        store.append(page_url)
+        # print("data==="+str(store))
+        # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`")
+
+        yield store
+    
+
+        
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+scrape()
--- apify/himanshu/storelocator/wawa_com/scrape.py
+++ apify/himanshu/storelocator/wawa_com/scrape.py
@@ -1,31 +1,23 @@
 import csv
 from bs4 import BeautifulSoup
 from sgrequests import SgRequests
+import requests
+from random import choice
+import http.client
 import re
 import json
 import sgzip
 import ssl
-import pprint
-import requests
-from selenium import webdriver
 import urllib3
-from selenium.webdriver.support.wait import WebDriverWait
+import pprint
+from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-session = SgRequests()
 import platform
-requests.packages.urllib3.disable_warnings()
 
 system = platform.system()
-# try:
-#     _create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-#     #print("Error##################")
-#     # Legacy Python that doesn't verify HTTPS certificates by default
-#     pass
-# else:
-#     # Handle target environment that doesn't support HTTPS verification
-#     ssl._create_default_https_context = _create_unverified_https_context
-
+session = SgRequests()
+requests.packages.urllib3.disable_warnings() 
+import urllib3
 
 def get_driver():
     options = Options()
@@ -37,7 +29,22 @@ def get_driver():
         return webdriver.Firefox(executable_path='./geckodriver', options=options)        
     else:
         return webdriver.Firefox(executable_path='geckodriver.exe', options=options)
+def get_proxy():
+    url = "https://www.sslproxies.org/"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html5lib")
+    return {'https': (choice(list(map(lambda x:x[0]+':'+x[1],list(zip(map(lambda x:x.text,soup.findAll('td')[::8]),map(lambda x:x.text,soup.findAll('td')[1::8])))))))}
 
+def proxy_request(request_type, url, **kwargs):
+    while 1:
+        try:
+            proxy = get_proxy()
+            # print("Using Proxy {}".format(proxy))
+            r = requests.request(request_type, url, proxies=proxy, timeout=3, **kwargs)
+            break
+        except:
+            pass
+    return r
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',',
@@ -56,14 +63,37 @@ def fetch_data():
     urllib3.disable_warnings()
     
     base_url = "https://www.wawa.com"
+    driver.get(base_url)
+    cookies_list = driver.get_cookies()
+        # print("cookies_list === " + str(cookies_list))
+    cookies_json = {}
+    for cookie in cookies_list:
+        cookies_json[cookie['name']] = cookie['value']
+
+    cookies_string = str(cookies_json).replace("{", "").replace("}", "").replace("'", "").replace(": ", "=").replace(
+    ",", ";")
 
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9,gu;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Cookie': cookies_string,
+        'Host': 'www.wawa.com',
+        'Pragma': 'no-cache',
+        'Referer': 'https://www.wawa.com/',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36',
+    
     }
-    r = session.get("https://www.wawa.com/site-map", verify=False, headers=headers)
+    r = proxy_request("get","https://www.wawa.com/site-map", verify=False, headers=headers)
     soup = BeautifulSoup(r.text, "lxml")
-
+    # print(soup)
     for link in soup.find_all("ul",{"class":"CMSSiteMapList"})[-1].find_all("a",{"class":"CMSSiteMapLink"}):
         store_number = link['href'].split("/")[2]
         page_url = base_url + link['href']
@@ -80,7 +110,8 @@ def fetch_data():
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36',
             'Cookie': cookies_string,
         }
-        r1 = session.get(page_url, headers=r_headers, verify=False)
+        #print(page_url)
+        r1 = proxy_request("get",page_url, headers=r_headers, verify=False)
         soup1 = BeautifulSoup(r1.text, "lxml")
         location_name = soup1.find("span",{"itemprop":"name"}).text.strip()
         street_address = soup1.find("span",{"itemprop":"streetAddress"}).text.strip()
@@ -107,6 +138,8 @@ def fetch_data():
         store.append(longitude)
         store.append(hours)
         store.append(page_url)
+        #print("data ==="+str(store))
+        #print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~````")
         yield store
 
 
