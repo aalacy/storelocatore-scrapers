c5fd95a723b80927fdc1b8e913b147c9529cfa84
diapk <Hank4957!>
2020-03-02 18:18:01 +0530

    docker run and push

---
+++ apify/himanshu/curatedsources/viewfrommyseat_com/scrape.py
@@ -0,0 +1,59 @@
+import csv
+from bs4 import BeautifulSoup
+import json
+import requests
+from sgrequests import SgRequests
+session = SgRequests()
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation",
+                         "page_url"])
+        for row in data:
+            writer.writerow(row)
+def fetch_data():
+    headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+    }
+    addresses = []
+    r =  session.get("https://aviewfrommyseat.com/ajax/map.php?jsoncallback=jQuery34107226692616565376_1582850194718&xmin=7.697665325020192&xmax=%2056.745349387957184&ymin=%20-136.35066085170004&ymax=%20-70.96003585170003&show=venues&zoom=4&_=1582850194729", headers=headers)
+    soup = BeautifulSoup(r.text, "lxml")
+    data = soup.text.split("6_1582850194718(")[1].split(');')[0]
+    json_data = json.loads(data)
+    for mp in json_data:
+        location_name = (mp['name'].replace(' ',"+"))
+        try:
+            zipp1 = ("https://aviewfrommyseat.com/venue/"+str(location_name)+"/about/")
+            r =  session.get(zipp1, headers=headers)
+            soup = BeautifulSoup(r.text, "lxml")
+            data = soup.find("script",{"type":"application/ld+json"}).text
+            json_data = json.loads(data)
+            zipp = (json_data['address']['postalCode'])
+            location_type = json_data['@type']
+        except:
+            zipp = "<MISSING>"   
+        store = []
+        store.append("https://aviewfrommyseat.com/")
+        store.append(mp['name'] if mp['name'] else "<MISSING>" )
+        store.append(mp['address'] if mp['address'] else "<MISSING>")
+        store.append(mp['city'] if mp['city'] else "<MISSING>")
+        store.append(mp['state'] if mp['state'] else "<MISSING>")
+        store.append(zipp if zipp else "<MISSING>")
+        store.append(mp['country'] if mp['country'] else "<MISSING>")
+        store.append(mp['id'] if mp['id'] else "<MISSING>")
+        store.append("<MISSING>")
+        store.append(location_type)
+        store.append(mp['lat'] if mp['lat'] else "<MISSING>")
+        store.append(mp['long'] if mp['long'] else "<MISSING>")
+        store.append("<MISSING>")
+        store.append("https://aviewfrommyseat.com/venue/"+str(location_name)+"/about/")
+        store = [x.encode('ascii', 'ignore').decode('ascii').strip() if type(x) == str else x for x in store]
+        # if store[2] in addresses :
+        #     continue
+        # addresses.append(store[2])
+        yield store    
+def scrape():
+    data = fetch_data()
+    write_output(data)
+scrape()
--- apify/himanshu/storelocator/rosesdiscountstores_com__rosesexpress/scrape.py
+++ apify/himanshu/storelocator/rosesdiscountstores_com__rosesexpress/scrape.py
@@ -3,21 +3,16 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import json
-import sgzip
 
 def write_output(data):
     with open('data.csv', mode='w', encoding="utf-8") as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        # Header
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
-        # Body
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         for row in data:
             writer.writerow(row)
 def fetch_data():
-        zips = sgzip.for_radius(100)
-
-        # print(sgzip.coords_for_radius(50))
+        
         addresses = []
         return_main_object = []
         header = {'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'}
@@ -25,11 +20,8 @@ def fetch_data():
         location_url  = 'https://api.zenlocator.com/v1/apps/app_vfde3mfb/locations/search?northeast=69.659042%2C70.11417&southwest=-77.464808%2C-180'
 
         r = requests.get(location_url, headers=header).json()
-        # soup = BeautifulSoup(r.text, "lxml")
-
         for id,vj in enumerate(r['locations']):
             locator_domain = base_url
-
             location_name = vj['name']
             street_address = vj['address'].split(',')[0].strip()
             city = vj['city']
@@ -37,21 +29,26 @@ def fetch_data():
             zip = vj['postcode']
             store_number = vj['importLogId']
             country_code = vj['countryCode']
-            phone = vj['contacts']['con_wg5rd22k']['text']
-            location_type = 'rosesdiscountstores'
+            try:
+                phone = vj['contacts']['con_wg5rd22k']['text']
+            except:
+                phone = "<MSSING>"
+            location_type = '<MISSING>'
             latitude = vj['lat']
             longitude = vj['lng']
-
-
             if street_address in addresses:
                 continue
             addresses.append(street_address)
-
             if vj['hours'] != "hrs_ywfef43p":
-                hours_of_operation = vj['hours']
+                if vj['hours'] == "hrs_a4db656x":
+                    hours_of_operation = '<MISSING>'
+                else :
+                    if vj['hours']:
+                        hours_of_operation1 = vj['hours']
+                        ki = (hours_of_operation1['hoursOfOperation'])
+                        hours_of_operation = ("sun"+" - "+ki['sun']+","+"mon"+" - "+ki['mon']+","+"tue"+" - "+ki['tue']+","+"wed"+" - "+ki['wed']+","+"thu"+" - "+ki['thu']+","+"fri"+" - "+ki['fri']+","+"sat"+" - "+ki['sat'])
             else:
-                hours_of_operation = ''
-
+                hours_of_operation = '<MISSING>'
             store = []
             store.append(locator_domain if locator_domain else '<MISSING>')
             store.append(location_name if location_name else '<MISSING>')
@@ -65,16 +62,11 @@ def fetch_data():
             store.append(location_type if location_type else '<MISSING>')
             store.append(latitude if latitude else '<MISSING>')
             store.append(longitude if longitude else '<MISSING>')
-
             store.append(hours_of_operation if hours_of_operation else '<MISSING>')
-            print('===', str(store))
-
+            store.append('<MISSING>')
+            # print('===', str(store))
             return_main_object.append(store)
-
         return return_main_object
-
-
-
 def scrape():
     data = fetch_data()
     write_output(data)
