044938760d95467f5832fd9374c1bf90eb3e8fd8
nsxdarin <nsxdarin@gmail.com>
2020-03-05 08:35:17 -0800

    Update scrape.py

--- apify/nsxdarin/storelocator/jiffylube_com/scrape.py
+++ apify/nsxdarin/storelocator/jiffylube_com/scrape.py
@@ -15,64 +15,31 @@ def write_output(data):
 
 def fetch_data():
     locs = []
-    url = 'https://www.jiffylube.com/sitemap.xml'
+    url = 'https://www.jiffylube.com/api/locations?lat=46.34935760498047&lng=-94.19837188720703&radius=9999&state='
     r = session.get(url, headers=headers)
     for line in r.iter_lines():
-        if '<ns1:loc>https://www.jiffylube.com/locations/' in line:
-            lurl = line.split('>')[1].split('<')[0]
-            count = lurl.count('/')
-            if count == 6:
-                locs.append(lurl)
-    for loc in locs:
-        print('Pulling Location %s...' % loc)
-        website = 'jiffylube.com'
-        typ = '<MISSING>'
-        hours = ''
-        name = ''
-        add = ''
-        city = ''
-        state = ''
-        zc = ''
-        country = 'US'
-        store = loc.rsplit('/',1)[1]
-        phone = ''
-        lat = ''
-        lng = ''
-        r2 = session.get(loc, headers=headers)
-        for line2 in r2.iter_lines():
-            if '<meta itemprop="name" content="' in line2:
-                name = line2.split('<meta itemprop="name" content="')[1].split('"')[0]
-                if ' |' in name:
-                    name = name.split(' |')[0]
-            if '<span itemprop="streetAddress">' in line2:
-                add = line2.split('<span itemprop="streetAddress">')[1].split('<')[0]
-            if '<span itemprop="addressLocality">' in line2:
-                city = line2.split('<span itemprop="addressLocality">')[1].split('<')[0]
-            if '<span itemprop="addressRegion">' in line2:
-                state = line2.split('<span itemprop="addressRegion">')[1].split('<')[0]
-            if '<span itemprop="postalCode">' in line2:
-                zc = line2.split('<span itemprop="postalCode">')[1].split('<')[0]
-            if '<span itemprop="telephone" content="' in line2:
-                phone = line2.split('<span itemprop="telephone" content="')[1].split('"')[0].replace('+','')
-            if '<staticmap lat="' in line2:
-                lat = line2.split('<staticmap lat="')[1].split('"')[0]
-                lng = line2.split('lng="')[1].split('"')[0]
-            if '<full-hours hours="' in line2:
-                days = line2.split(';name&quot;:&quot;')
-                for day in days:
-                    if 'time_open&quot;:&quot;' in day:
-                        dname = day.split('&')[0]
-                        oh = day.split('time_open&quot;:&quot;')[1].split('&')[0]
-                        ch = day.split('time_close&quot;:&quot;')[1].split('&')[0]
-                        hrs = dname + ': ' + oh + '-' + ch
-                        if hours == '':
-                            hours = hrs
-                        else:
-                            hours = hours + '; ' + hrs
-        if hours == '':
-            hours = '<MISSING>'
-        if add != '':
-            yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+        if ',"postal_code":"' in line:
+            items = lines.split(',"postal_code":"')
+            for item in items:
+                if '"country":"' in item:
+                    zc = item.split('"')[0]
+                    country = item.split('"country":"')[1].split('"')[0]
+                    state = item.split(',"state":"')[1].split('"')[0]
+                    city = item.split('"city":"')[1].split('"')[0]
+                    add = item.split('"address":"')[1].split('"')[0]
+                    loc = 'https://www.jiffylube.com' + item.split('"_self":"')[1].split('"')[0]
+                    store = loc.rsplit('/',1)[1]
+                    phone = item.split('"phone_main":"')[1].split('"')[0]
+                    name = item.split('"nickname":"')[1].split('"')[0]
+                    website = 'jiffylube.com'
+                    typ = '<MISSING>'
+                    lat = item.split('"latitude":')[1].split(',')[0]
+                    lng = item.split('"longitude":')[1].split('}')[0]
+                    hours = item.split('"hours":["')[1].split(']')[0].replace('","','; ').replace('"','')
+                    if hours == '':
+                        hours = '<MISSING>'
+                    if add != '':
+                        yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
