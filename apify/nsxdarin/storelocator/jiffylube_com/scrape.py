f845202e73a824786552c19dac4e42337daf8e4d
nsxdarin <nsxdarin@gmail.com>
2020-03-05 08:44:07 -0800

    Update scrape.py

--- apify/nsxdarin/storelocator/jiffylube_com/scrape.py
+++ apify/nsxdarin/storelocator/jiffylube_com/scrape.py
@@ -15,31 +15,37 @@ def write_output(data):
 
 def fetch_data():
     locs = []
-    url = 'https://www.jiffylube.com/api/locations?lat=46.34935760498047&lng=-94.19837188720703&radius=9999&state='
-    r = session.get(url, headers=headers)
-    for line in r.iter_lines():
-        if ',"postal_code":"' in line:
-            items = lines.split(',"postal_code":"')
-            for item in items:
-                if '"country":"' in item:
-                    zc = item.split('"')[0]
-                    country = item.split('"country":"')[1].split('"')[0]
-                    state = item.split(',"state":"')[1].split('"')[0]
-                    city = item.split('"city":"')[1].split('"')[0]
-                    add = item.split('"address":"')[1].split('"')[0]
-                    loc = 'https://www.jiffylube.com' + item.split('"_self":"')[1].split('"')[0]
-                    store = loc.rsplit('/',1)[1]
-                    phone = item.split('"phone_main":"')[1].split('"')[0]
-                    name = item.split('"nickname":"')[1].split('"')[0]
-                    website = 'jiffylube.com'
-                    typ = '<MISSING>'
-                    lat = item.split('"latitude":')[1].split(',')[0]
-                    lng = item.split('"longitude":')[1].split('}')[0]
-                    hours = item.split('"hours":["')[1].split(']')[0].replace('","','; ').replace('"','')
-                    if hours == '':
-                        hours = '<MISSING>'
-                    if add != '':
-                        yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+    for x in range(15, 65, 5):
+        for y in range(-65, -175, -5):
+            print('Pulling Coordinates %s-%s...' % (str(x), str(y)))
+            url = 'https://www.jiffylube.com/api/locations?lat=' + str(x) + '&lng=' + str(y) + '&radius=250&state='
+            r = session.get(url, headers=headers)
+            for line in r.iter_lines():
+                if ',"postal_code":"' in line:
+                    items = line.split(',"postal_code":"')
+                    for item in items:
+                        if '"country":"' in item:
+                            zc = item.split('"')[0]
+                            country = item.split('"country":"')[1].split('"')[0]
+                            state = item.split(',"state":"')[1].split('"')[0]
+                            city = item.split('"city":"')[1].split('"')[0]
+                            add = item.split('"address":"')[1].split('"')[0]
+                            loc = 'https://www.jiffylube.com' + item.split('"_self":"')[1].split('"')[0]
+                            store = loc.rsplit('/',1)[1]
+                            phone = item.split('"phone_main":"')[1].split('"')[0]
+                            name = item.split('"nickname":"')[1].split('"')[0]
+                            website = 'jiffylube.com'
+                            typ = '<MISSING>'
+                            lat = item.split('"latitude":')[1].split(',')[0]
+                            lng = item.split('"longitude":')[1].split('}')[0]
+                            try:
+                                hours = item.split('"hours":["')[1].split(']')[0].replace('","','; ').replace('"','')
+                            except:
+                                hours = '<MISSING>'
+                            if hours == '':
+                                hours = '<MISSING>'
+                            if add != '':
+                                yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
