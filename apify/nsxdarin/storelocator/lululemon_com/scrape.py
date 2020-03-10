4243008056041d2ff7ae99f31c168bef2aec72d3
nsxdarin <nsxdarin@gmail.com>
2020-03-10 12:40:53 -0700

    Update scrape.py

--- apify/nsxdarin/storelocator/lululemon_com/scrape.py
+++ apify/nsxdarin/storelocator/lululemon_com/scrape.py
@@ -1,56 +1,56 @@
 import csv
 import urllib2
+import requests
 import time
-from sgrequests import SgRequests
+import json
 
-session = SgRequests()
+requests.packages.urllib3.disable_warnings()
+
+session = requests.Session()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
 
 def fetch_data():
-    url = 'https://shop.lululemon.com/stores/all-lululemon-stores'
+    url = 'https://wae-store-experience-prod.lllapi.com/stores'
     locs = []
-    r = session.get(url, headers=headers)
-    for line in r.iter_lines():
-        if '"" href="https://shop.lululemon.com/stores/' in line:
-            lurl = line.split('href="')[1].split('"')[0]
-            locs.append(lurl)
-    print('Found %s Locations.' % str(len(locs)))
-    for loc in locs:
+    r = session.get(url, headers=headers, verify=False)
+    for item in json.loads(r.content):
         website = 'lululemon.com'
         typ = '<MISSING>'
         hours = ''
         store = '<MISSING>'
-        locurl = loc.split('|')[0]
-        r2 = session.get(locurl, headers=headers)
-        for line2 in r2.iter_lines():
-            if ',"streetAddress":"' in line2:
-                add = line2.split(',"streetAddress":"')[1].split('"')[0]
-                phone = line2.split('"telephone":"')[1].split('"')[0]
-                city = line2.split(',"addressLocality":"')[1].split('"')[0]
-                state = line2.split('"addressRegion":"')[1].split('"')[0]
-                country = line2.split('"addressCountry":"')[1].split('"')[0]
-                lat = line2.split(',"latitude":"')[1].split('"')[0]
-                zc = line2.split('"postalCode":"')[1].split('"')[0]
-                lng = line2.split(',"longitude":"')[1].split('"')[0]
-                name = line2.split('"photo":{')[1].split('"name":"')[1].split('"')[0]
-                hours = line2.split('"openingHours":[')[1].split(']')[0].replace('","','; ').replace('"','')
-                if hours == '':
-                    hours = '<MISSING>'
-                if 'null' in hours:
-                    hours = '<MISSING>'
-                if phone == '':
-                    phone = '<MISSING>'
-                if hours == '':
-                    hours = '<MISSING>'
-                yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+        name = item['name'].encode('utf-8')
+        loc = item['websiteUrl']
+        city = item['city'].encode('utf-8')
+        country = item['country']
+        store = item['storeNumber']
+        addinfo = item['fullAddress'].encode('utf-8')
+        lat = item['latitude']
+        lng = item['longitude']
+        state = item['state']
+        zc = addinfo.rsplit(' ',1)[1]
+        add = addinfo.split(',')[0]
+        phone = item['phone']
+        for day in item['hours']:
+            try:
+                hrs = day['name'] + ': ' + day['openHour'] + '-' + day['closeHour']
+            except:
+                hours = day['name'] + ': Closed'
+            if hours == '':
+                hours = hrs
+            else:
+                hours = hours + '; ' + hrs
+        if country == 'US' or country == 'CA':
+            if len(phone) < 5:
+                phone = '<MISSING>'
+            yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
