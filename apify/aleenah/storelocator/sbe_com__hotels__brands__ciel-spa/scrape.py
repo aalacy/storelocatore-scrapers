a7800df29c17bccf773f911d7a0638df3bb7f961
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-03-22 01:11:40 +0500

    Update scrape.py

--- apify/aleenah/storelocator/sbe_com__hotels__brands__ciel-spa/scrape.py
+++ apify/aleenah/storelocator/sbe_com__hotels__brands__ciel-spa/scrape.py
@@ -25,11 +25,10 @@ def fetch_data():
     soup = BeautifulSoup(res.text, 'html.parser')
     urls = soup.find_all('a', {'class': 'ftr-itm-hrf item-click'})
     divs=soup.find_all('div', {'class': 'address abstop vert-center'})
-    del urls[-1] #kuwait
 
     for url in urls:
         url=url.get('href')
-        if url=="https://www.sbe.com/restaurants/locations/cleo-baha-mar/":
+        if url=="https://www.sbe.com/restaurants/locations/cleo-baha-mar/" or url == "https://www.sbe.com/restaurants/locations/cleo-kuwait/":
             continue
      #   print(url)
         res = session.get(url)
@@ -40,13 +39,15 @@ def fetch_data():
         except:
             continue
         street = soup.find('span', {'class': 'address1'}).text
-        city=soup.find('span', {'class': 'city'}).text
+        city=soup.find('span', {'class': 'city'}).text.replace(',','')
         state=soup.find('span', {'class': 'state'}).text
         zip=soup.find('span', {'class': 'postal_code'}).text.strip()
         if zip=="":
             zip="<MISSING>"
 
         phone=soup.find('li', {'class': 'serif-face cols borderright'}).find('a').text
+        lat=soup.find('div', {'id': 'map_canvas'}).get('data-latitude')
+        long=soup.find('div', {'id': 'map_canvas'}).get('data-longitude')
 
         all.append([
             "https://www.sbe.com/restaurants/brands/cleo",
@@ -59,8 +60,8 @@ def fetch_data():
             "<MISSING>",  # store #
             phone,  # phone
             "<MISSING>",  # type
-            "<MISSING>",  # lat
-            "<MISSING>",  # long
+            lat,  # lat
+            long,  # long
             tim,  # timing
             url])
 
@@ -71,7 +72,7 @@ def fetch_data():
         divas=divs[i].find_all('div')
         street=divas[0].text.strip()
         addr=divas[1].text.strip().split(',')
-        city=addr[0]
+        city=addr[0].replace(',','')
         state=addr[1].strip()
         zip=addr[2].strip()
 
