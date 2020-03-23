28a0fdad99f9b963102abda9ff405a4d483209bb
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-03-24 02:31:36 +0500

    Update scrape.py

--- apify/tayyabaq/storelocator/petsmart_ca/scrape.py
+++ apify/tayyabaq/storelocator/petsmart_ca/scrape.py
@@ -22,8 +22,8 @@ def fetch_data():
     store_links =[]
     clear_links =[]
     #CA stores
-    url = 'https://www.petsmart.ca/store-locator/all/'
-    u='https://www.petsmart.ca/'
+    url = 'https://www.petsmart.com/stores/ca/'
+    u='https://www.petsmart.com/'
     page = session.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     store=soup.find('div',class_='all-states-list container')
@@ -34,57 +34,89 @@ def fetch_data():
         soup = BeautifulSoup(page.content, "html.parser")
         store=soup.find_all('a', class_='store-details-link')
         for j in store:
+
             ul=u+j['href']
+            print(ul)
             page = session.get(ul)
             soup = BeautifulSoup(page.content, "html.parser")
-            loc=soup.find('h1', class_ ='store-name').text
+            div = soup.find('div',class_='store-page-details')
             try:
+              loc = div.find('h1').text
+              if "closed" in loc.lower():
+                continue
+            except:
+               continue
+            ph=div.find('p',class_='store-page-details-phone').text.strip()
+            addr=div.find('p',class_='store-page-details-address').text.strip().split("\n")
+            print(addr)
+            if len(addr) ==2:
+                street=addr[0]
+                addr=addr[1].strip().split(',')
+            elif len(addr)>2:
+                add=addr[-1]
+                del addr[-1]
+                street=" ".join(addr)
+                addr=add.strip().split(',')
+            
+            cty=addr[0]
+            addr=addr[1].strip().split(' ')
+            sts=addr[0]
+            zcode=addr[1]
+            try:
+                hours=soup.find('div',class_='store-page-details-hours-mobile visible-sm visible-md ui-accordion ui-widget ui-helper-reset').text
+            except:
+                hours=soup.find('div',class_='store-page-details-hours-mobile visible-sm visible-md').text
+            hours=hours.strip().replace('\n\n','').replace('\n','')
+            for day in ['MON','TUE','THU','WED','FRI','SAT','SUN']:
+                if day not in hours:
+                    hours=hours.replace('TODAY',day)
+            #print(hours)
+            
+            
+            """try:
+                loc=soup.find('h1', class_ ='store-name').text
+            except:
+                print("closed")
+            else:
                 street = soup.find('div', itemprop='streetAddress').text
-            except:
-                street="<MISSING>"
-            city = soup.find('span',itemprop='addressLocality')
-            cty=city.text
-            sts = city.find_next('span',itemprop='addressLocality').text
-            try:
+                city = soup.find('span',itemprop='addressLocality')
+                cty=city.text
+                sts = city.find_next('span',itemprop='addressLocality').text
                 zcode = soup.find('span',itemprop='postalCode').text
-            except:
-                zcode = "<MISSING>"
-            num=j['id']
-            try:
+                num=j['id']
                 ph=soup.find("a",class_="store-contact-info").text
-            except:
-                ph="<MISSING>"
-            lat=soup.find("meta",itemprop='latitude')['content']
-            lng=soup.find("meta",itemprop='longitude')['content']
-            hr=soup.find_all("span",itemprop="dayOfWeek")  
-            op=soup.find_all("time",itemprop='opens')
-            cl=soup.find_all("time",itemprop='closes')
-            DY=['SUN','MON','TUE','WED','THU','FRI','SAT']
-            hours=""
-            for k in range(0,len(op)) :
-                hours+=hr[k].text+" "
-                if hr[k].text != 'TODAY':
-                    DY.remove(hr[k].text)
-                hours+=op[k]['content']+"-"
-                hours+=cl[k]['content']+" "
-            hours=hours.replace("TODAY",DY[0])
-            hours=hours.replace("-null","")
+                lat=soup.find("meta",itemprop='latitude')['content']
+                lng=soup.find("meta",itemprop='longitude')['content']
+                hr=soup.find_all("span",itemprop="dayOfWeek")  
+                op=soup.find_all("time",itemprop='opens')
+                cl=soup.find_all("time",itemprop='closes')
+                DY=['SUN','MON','TUE','WED','THU','FRI','SAT']
+                hours=""
+                for k in range(0,len(op)) :
+                    hours+=hr[k].text+" "
+                    if hr[k].text != 'TODAY':
+                        DY.remove(hr[k].text)
+                    hours+=op[k]['content']+"-"
+                    hours+=cl[k]['content']+" "
+                hours=hours.replace("TODAY",DY[0])
+                hours=hours.replace("-null","")
+                print(hours)"""
             data.append([
-                'https://www.petmarts.ca/',
-                 ul,
-                loc,
-                street,
-                cty,
-                sts,
-                zcode,
-                'CA',
-                num,
-                ph,
-                '<MISSING>',
-                lat,
-                lng,
-                hours
-                ])
+                    'https://www.petsmart.com/',
+                     ul.replace(u'\u2019',''),
+                    loc.replace(u'\u2019','').strip(),
+                    street.replace(u'\u2019',''),
+                    cty.replace(u'\u2019',''),
+                    sts.replace(u'\u2019',''),
+                    zcode.replace(u'\u2019',''),
+                    'US'.replace(u'\u2019',''),
+                    j['id'].replace(u'\u2019',''),
+                    ph.replace(u'\u2019',''),
+                    '<MISSING>',
+                    '<MISSING>',
+                    '<MISSING>',
+                    hours.replace(u'\u2019','')
+                    ])
          
     return data
     
