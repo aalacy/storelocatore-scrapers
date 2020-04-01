01002b6132987a1ec8e70b2b5dfe7167c9bb6626
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-04-01 21:15:41 +0500

    Update scrape.py

--- apify/aleenah/storelocator/jerrysusa_com/scrape.py
+++ apify/aleenah/storelocator/jerrysusa_com/scrape.py
@@ -23,20 +23,32 @@ def fetch_data():
     soup = BeautifulSoup(res.text, 'html.parser')
 
     divs = soup.find_all('div', {'class': 'g48'})
-    print(len(divs))
-    divs=divs[1::2]
-    print(len(divs))
+    #print(len(divs))
+    divs=divs[0::2]
+    #print(len(divs))
+    del divs[0]
+    del divs[-1]
+    del divs[-1]
+    
     for div in divs:
+        #print(div)
         loc=div.find('h4').text
+        print(loc)
         ps=div.find_all('p')
         addr=ps[0].text.strip().split("\n")
-        street=addr[0]
-        addr=addr[1].strip().split(",")
+        if len(addr)==2:
+              street=addr[0]
+        elif len(addr)==3:
+              street=addr[0]+" "+addr[1]
+        addr=addr[-1].strip().split(",")
         city=addr[0]
         addr=addr[1].strip().split(" ")
         state=addr[0]
         zip=addr[1]
         phone=ps[1].text.strip()
+        if phone=="":
+               phone="<MISSING>"
+
 
 
         all.append([
@@ -63,5 +75,3 @@ def scrape():
 
 
 scrape()
-
-
