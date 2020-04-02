51bb1bf0a9264419f3177eba4f2691fc785c4a5a
diapk <Hank4957!>
2020-04-02 18:59:03 +0530

    docker run and push

---
+++ apify/himanshu/curatedsources/metropcs_com/scrape.py
@@ -0,0 +1,172 @@
+import csv
+import requests
+import http.client
+from bs4 import BeautifulSoup
+import re
+import json
+import sgzip
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    #print("Error##################")
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
+
+def write_output(data):
+    with open('data.csv', mode='w', encoding="utf-8", newline="") as output_file:
+        writer = csv.writer(output_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    addresses = []
+    addresses1 =[]
+    search = sgzip.ClosestNSearch()
+    search.initialize()
+    MAX_RESULTS = 100000
+    MAX_DISTANCE = 1000
+    current_results_len = 0  # need to update with no of count.
+    coord = search.next_coord()
+
+
+    response1 =0
+    while coord:
+        # print("remaining zipcodes: " + str(len(search.zipcodes)))
+        result_coords = []
+        lat = coord[0]
+        lng = coord[1]
+        base_url = "https://www.metropcs.com/"
+        url = "https://www.metrobyt-mobile.com/api/v1/commerce/store-locator"
+        
+        querystring1 = {"address": ""+str(search.current_zip)+"", "store-type": "AuthorizedDealer", "min-latitude": "19.50139",
+                       "max-latitude": ""+str(lat)+"", "min-longitude": "-68.01197", "max-longitude": ""+str(lng)+""}
+        
+       
+        
+
+        headers = {
+            'Content-type': "*/*,multipart/form-data; boundary=--------------------------773008355060723902209115",
+            'User-Agent': "PostmanRuntime/7.20.1",
+            'Accept': "*/*",
+            'Cache-Control': "no-cache",
+            'Host': "www.metrobyt-mobile.com",
+            'Accept-Encoding': "gzip, deflate",
+            'Content-Length': "0",
+            'Connection': "keep-alive",
+            'cache-control': "no-cache"
+        }
+
+        try:
+            response1 = requests.request(
+                "GET", url, headers=headers, params=querystring1).json()
+            current_results_len = len(response1)
+        except:
+            continue
+
+        for loc in response1:
+            locator_domain = base_url
+            country_code = "US"
+            store_number = loc["id"]
+            location_type = loc["type"]
+            try:
+                location_name = loc["name"]
+            except:
+                location_name = "<MISSING>"
+            try:
+                phone = loc["telephone"].replace("910` 745-8286","910 745-8286").replace("405 632-07534","405 632-7534").replace("831 768?1869","831 768-1869").replace("510 510-346-8687","510-346-8687")
+            except:
+                phone = "<MISSING>"
+            try:
+                latitude = loc["location"]["latitude"]
+                longitude = loc["location"]["longitude"]
+            except:
+                latitude = "<MISSING>"
+                longitude = "<MISSING>"
+            # print(loc["address"]["streetAddress"])
+            try:
+                street_address = loc["location"]["address"]["streetAddress"]
+            except:
+                street_address = "<MISSING>"
+            try:
+                city = loc["location"]["address"]["addressLocality"]
+            except:
+                city = "<MISSING>"
+            try:
+                state = loc["location"]["address"]["addressRegion"]
+            except:
+                state = "<MISSING>"
+            try:
+                zipp = loc["location"]["address"]["postalCode"]
+            except:
+                zipp = "<MISSING>"
+            if len(zipp.strip()) == 4:
+                zipp = "0" + zipp
+            try:
+                hours_of_operation = ""
+                for hour in loc["openingHours"]:
+                    hours_of_operation += " " +",".join(hour['days']) +" - "+ hour['time']
+            except:
+                hours_of_operation = "<MISSING>"
+
+            page_url = "https://www.metrobyt-mobile.com/storelocator/"+str(state.lower())+"/"+str(city.replace(" ","-").lower())+"/"+str(street_address.replace(" ","-").lower())
+            if phone== None:
+                phone = "<MISSING>"
+
+            if phone==".":
+                phone = "<MISSING>"
+
+            store = []
+            result_coords.append((latitude, longitude))
+            store.append(locator_domain if locator_domain else '<MISSING>')
+            store.append(location_name if location_name else '<MISSING>')
+            store.append(street_address if street_address else '<MISSING>')
+            store.append(city if city else '<MISSING>')
+            store.append(state if state else '<MISSING>')
+            store.append(zipp if zipp else '<MISSING>')
+            store.append(country_code if country_code else '<MISSING>')
+            store.append(store_number if store_number else '<MISSING>')
+            store.append(phone.encode('ascii', 'ignore').decode('ascii').strip() if phone.encode('ascii', 'ignore').decode('ascii').strip() else '<MISSING>')
+            store.append(location_type if location_type else '<MISSING>')
+            store.append(latitude if latitude else '<MISSING>')
+            store.append(longitude if longitude else '<MISSING>')
+            store.append(hours_of_operation.strip() if hours_of_operation else '<MISSING>')
+            store.append(page_url)
+            if store[2] in addresses:
+                continue
+            addresses.append(store[2])
+            store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+            # print("====", str(store))
+            yield store
+
+       
+
+        if current_results_len < MAX_RESULTS:
+            # print("max distance update")
+            search.max_distance_update(MAX_DISTANCE)
+        elif current_results_len == MAX_RESULTS:
+            # print("max count update")
+            search.max_count_update(result_coords)
+        else:
+            raise Exception("expected at most " +
+                            str(MAX_RESULTS) + " results")
+        coord = search.next_coord()
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+scrape()
--- apify/himanshu/storelocator/gulfoil_com/scrape.py
+++ apify/himanshu/storelocator/gulfoil_com/scrape.py
@@ -5,13 +5,11 @@ import re
 import json
 import sgzip
 import time
-
-
-
 session = SgRequests()
 
+
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
+    with open('data.csv', mode='w', newline='') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
@@ -22,47 +20,10 @@ def write_output(data):
             writer.writerow(row)
 
 
-def request_wrapper(url,method,headers,data=None):
-    request_counter = 0
-    if method == "get":
-        while True:
-            try:
-                r = session.get(url,headers=headers)
-                return r
-                break
-            except:
-                time.sleep(2)
-                request_counter = request_counter + 1
-                if request_counter > 10:
-                    return None
-                    break
-    elif method == "post":
-        while True:
-            try:
-                if data:
-                    r = session.post(url,headers=headers,data=data)
-                else:
-                    r = session.post(url,headers=headers)
-                return r
-                break
-            except:
-                time.sleep(2)
-                request_counter = request_counter + 1
-                if request_counter > 10:
-                    return None
-                    break
-    else:
-        return None
 
 def fetch_data():
 
     addressess = []
-    search = sgzip.ClosestNSearch()
-    search.initialize()
-    MAX_RESULTS = 50
-    MAX_DISTANCE = 10
-    current_results_len = 0     # need to update with no of count.
-    zip_code = search.next_zip()
     base_url = "http://gulfoil.com"
 
     headers = {
@@ -70,91 +31,57 @@ def fetch_data():
     "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
     "Accept": "application/json, text/javascript, */*; q=0.01",
     }
-    while zip_code:
-        location_name = ""
-        street_address1 = ""
-        city = ""
-        state = ""
-        zipp = ""
-        country_code = "US"
-        store_number = ""
-        phone = ""
-        location_type = ""
-        latitude = ""
-        longitude = ""
-        raw_address = ""
-        hours_of_operation = ""
-        page_url = ''
-        #print(zip_code)
-        result_coords = []
-        # q = 0
+    page = 0
+    while True:
+        data="view_name=station_locator_block&view_display_id=block&view_args=&view_path=node%2F21&view_base_path=&view_dom_id=910e3642fde0f7df1040d1f1ac0c6f75&pager_element=0&field_geofield_distance%5Bdistance%5D=10000&field_geofield_distance%5Bunit%5D=3959&field_geofield_distance%5Borigin%5D=AL&page="+str(page)+"&ajax_html_ids%5B%5D=&ajax_html_ids%5B%5D=&ajax_html_ids%5B%5D=custom-bootstrap-menu&ajax_html_ids%5B%5D=block-views-station-locator-block-block&ajax_html_ids%5B%5D=views-exposed-form-station-locator-block-block&ajax_html_ids%5B%5D=edit-field-geofield-distance-wrapper&ajax_html_ids%5B%5D=edit-field-geofield-distance&ajax_html_ids%5B%5D=edit-field-geofield-distance-distance&ajax_html_ids%5B%5D=edit-field-geofield-distance-unit&ajax_html_ids%5B%5D=edit-field-geofield-distance-origin&ajax_html_ids%5B%5D=edit-submit-station-locator-block&ajax_html_ids%5B%5D=mm_sync_back_ground&ajax_page_state%5Btheme%5D=bootstrap_subtheme&ajax_page_state%5Btheme_token%5D=TuVAp0dOK3itPX0nt1xjQl6ISZs3sAtI5dQqvrLpTTw&ajax_page_state%5Bcss%5D%5Bmodules%2Fsystem%2Fsystem.base.css%5D=1&ajax_page_state%5Bcss%5D%5Bmodules%2Ffield%2Ftheme%2Ffield.css%5D=1&ajax_page_state%5Bcss%5D%5Bmodules%2Fnode%2Fnode.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fmodules%2Fviews%2Fcss%2Fviews.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fmodules%2Fckeditor%2Fcss%2Fckeditor.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fmodules%2Fctools%2Fcss%2Fctools.css%5D=1&ajax_page_state%5Bcss%5D%5Bmodules%2Fgeofield%2Fcss%2Fproximity-element.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fmodules%2Fwebform%2Fcss%2Fwebform.css%5D=1&ajax_page_state%5Bcss%5D%5B%2F%2Fcdn.jsdelivr.net%2Fbootstrap%2F3.3.7%2Fcss%2Fbootstrap.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fcss%2F3.3.7%2Foverrides.min.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap_subtheme%2Fcss%2Fstyle.css%5D=1&ajax_page_state%5Bjs%5D%5B0%5D=1&ajax_page_state%5Bjs%5D%5B1%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fjs%2Fbootstrap.js%5D=1&ajax_page_state%5Bjs%5D%5B%2F%2Fajax.googleapis.com%2Fajax%2Flibs%2Fjquery%2F1.10.2%2Fjquery.min.js%5D=1&ajax_page_state%5Bjs%5D%5Bmisc%2Fjquery.once.js%5D=1&ajax_page_state%5Bjs%5D%5Bmisc%2Fdrupal.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fjquery_update%2Freplace%2Fui%2Fexternal%2Fjquery.cookie.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fjquery_update%2Freplace%2Fmisc%2Fjquery.form.min.js%5D=1&ajax_page_state%5Bjs%5D%5Bmisc%2Fajax.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fjquery_update%2Fjs%2Fjquery_update.js%5D=1&ajax_page_state%5Bjs%5D%5B%2F%2Fcdn.jsdelivr.net%2Fbootstrap%2F3.3.7%2Fjs%2Fbootstrap.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fviews%2Fjs%2Fbase.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fjs%2Fmisc%2F_progress.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fwebform%2Fjs%2Fwebform.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fviews%2Fjs%2Fajax_view.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fgoogle_analytics%2Fgoogleanalytics.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fjs%2Fmodules%2Fviews%2Fjs%2Fajax_view.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fjs%2Fmisc%2Fajax.js%5D=1&ajax_page_state%5Bjquery_version%5D=1.10"
+        json_data = session.post("http://gulfoil.com/views/ajax",data=data, headers=headers).json()
+        loc = json_data[-1]
+        soup = BeautifulSoup(loc['data'], "lxml")
+        if soup.find("div",{"class":"view-empty"}):
+            break
+        table = soup.find_all('table',{'class':'table'})
+        for i in range(len(table)):
+            location_name  = table[i].find("span").text
+            address_tage = table[i].find("p")
+            phone_list = re.findall(re.compile(r".?(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}).?"), str(table[i]))
+            if phone_list:
+                phone = phone_list[-1]
+            us_zip_list = re.findall(re.compile(r"\b[0-9]{5}(?:-[0-9]{4})?\b"), str(address_tage.text))
+            if us_zip_list:
+                zipp = us_zip_list[-1]
+                country_code = "US"
+            state_list = re.findall(r' ([A-Z]{2})', str(address_tage.text))
+            if state_list:
+                state = state_list[-1]
+            full_address = list(address_tage.stripped_strings)
+            city = full_address[-1].replace(state,"").replace(zipp,"").replace(",",'').strip().replace("CARR #123  33.2 Adjuntas Adjuntas Municipio","Adjuntas")
+            street_address = " ".join(full_address[:-1]).replace("BO SALTILLO","BO SALTILLO CARR #123")
+            if state in ['KM']:
+                continue
 
-        data="field_geofield_distance%5Bdistance%5D=500&field_geofield_distance%5Bunit%5D=3959&field_geofield_distance%5Borigin%5D="+str(zip_code)+"&view_name=station_locator_block&view_display_id=block&view_args=&view_path=node%2F21&view_base_path=&view_dom_id=999559f25a324da30a53db55ca017515&pager_element=0&ajax_html_ids%5B%5D=custom-bootstrap-menu&ajax_html_ids%5B%5D=block-views-station-locator-block-block&ajax_html_ids%5B%5D=views-exposed-form-station-locator-block-block&ajax_html_ids%5B%5D=edit-field-geofield-distance-wrapper&ajax_html_ids%5B%5D=edit-field-geofield-distance&ajax_html_ids%5B%5D=edit-field-geofield-distance-distance&ajax_html_ids%5B%5D=edit-field-geofield-distance-unit&ajax_html_ids%5B%5D=edit-field-geofield-distance-origin&ajax_html_ids%5B%5D=edit-submit-station-locator-block&ajax_html_ids%5B%5D=&ajax_html_ids%5B%5D=&ajax_html_ids%5B%5D=mm_sync_back_ground&ajax_page_state%5Btheme%5D=bootstrap_subtheme&ajax_page_state%5Btheme_token%5D=VK5PQBNQBSjygw9s9M9JrEaD1NW8gEEQvtSNaB68auA&ajax_page_state%5Bcss%5D%5Bmodules%2Fsystem%2Fsystem.base.css%5D=1&ajax_page_state%5Bcss%5D%5Bmodules%2Ffield%2Ftheme%2Ffield.css%5D=1&ajax_page_state%5Bcss%5D%5Bmodules%2Fnode%2Fnode.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fmodules%2Fviews%2Fcss%2Fviews.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fmodules%2Fckeditor%2Fcss%2Fckeditor.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fmodules%2Fctools%2Fcss%2Fctools.css%5D=1&ajax_page_state%5Bcss%5D%5Bmodules%2Fgeofield%2Fcss%2Fproximity-element.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fmodules%2Fwebform%2Fcss%2Fwebform.css%5D=1&ajax_page_state%5Bcss%5D%5B%2F%2Fcdn.jsdelivr.net%2Fbootstrap%2F3.3.7%2Fcss%2Fbootstrap.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fcss%2F3.3.7%2Foverrides.min.css%5D=1&ajax_page_state%5Bcss%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap_subtheme%2Fcss%2Fstyle.css%5D=1&ajax_page_state%5Bjs%5D%5B0%5D=1&ajax_page_state%5Bjs%5D%5B1%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fjs%2Fbootstrap.js%5D=1&ajax_page_state%5Bjs%5D%5B%2F%2Fajax.googleapis.com%2Fajax%2Flibs%2Fjquery%2F1.10.2%2Fjquery.min.js%5D=1&ajax_page_state%5Bjs%5D%5Bmisc%2Fjquery.once.js%5D=1&ajax_page_state%5Bjs%5D%5Bmisc%2Fdrupal.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fjquery_update%2Freplace%2Fui%2Fexternal%2Fjquery.cookie.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fjquery_update%2Freplace%2Fmisc%2Fjquery.form.min.js%5D=1&ajax_page_state%5Bjs%5D%5Bmisc%2Fajax.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fjquery_update%2Fjs%2Fjquery_update.js%5D=1&ajax_page_state%5Bjs%5D%5B%2F%2Fcdn.jsdelivr.net%2Fbootstrap%2F3.3.7%2Fjs%2Fbootstrap.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fviews%2Fjs%2Fbase.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fjs%2Fmisc%2F_progress.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fwebform%2Fjs%2Fwebform.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fviews%2Fjs%2Fajax_view.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fmodules%2Fgoogle_analytics%2Fgoogleanalytics.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fjs%2Fmodules%2Fviews%2Fjs%2Fajax_view.js%5D=1&ajax_page_state%5Bjs%5D%5Bsites%2Fall%2Fthemes%2Fbootstrap%2Fjs%2Fmisc%2Fajax.js%5D=1&ajax_page_state%5Bjquery_version%5D=1.10"
-        json_data = request_wrapper("http://gulfoil.com/views/ajax" ,"post",data=data, headers=headers).json()
-        for loc in json_data:
-            if "data" in loc:
-                soup = BeautifulSoup(loc['data'], "lxml")
-                table1 = soup.find('table',{'class':'table'})
-                table = soup.find_all('table',{'class':'table'})
-                current_results_len = len(table)
-                for i in range(len(table)):
-                    location_name  = table[i].find("span").text
-                    address_tage = table[i].find("p")
-                    phone_list = re.findall(re.compile(".?(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}).?"), str(table[i]))
-                    if phone_list:
-                        phone = phone_list[-1]
-                    us_zip_list = re.findall(re.compile(r"\b[0-9]{5}(?:-[0-9]{4})?\b"), str(address_tage.text))
-                    if us_zip_list:
-                        zipp = us_zip_list[-1]
-                        country_code = "US"
-                    state_list = re.findall(r' ([A-Z]{2})', str(address_tage.text))
-                    if state_list:
-                        state = state_list[-1]
-                    full_address = list(address_tage.stripped_strings)
-                    indices = [i for i, s in enumerate(full_address) if zipp in s]
-                    city = full_address[indices[0]].replace(state,"").replace(zipp,"").replace(",",'').strip()
-                    if indices and indices[0]>0:
-                        street_address = " ".join(full_address[:indices[0]])
-                    else:
-                        street_address = "<MISSING>"
+            store = []
+            locator_domain =base_url
+            store.append(locator_domain if locator_domain else '<MISSING>')
+            store.append(location_name if location_name else '<MISSING>')
+            store.append(street_address if street_address else '<MISSING>')
+            store.append(city if city else '<MISSING>')
+            store.append(state if state else '<MISSING>')
+            store.append(zipp if zipp else '<MISSING>')
+            store.append(country_code if country_code else '<MISSING>')
+            store.append('<MISSING>')
+            store.append(phone if phone else '<MISSING>')
+            store.append('<MISSING>')
+            store.append('<MISSING>')
+            store.append('<MISSING>')
+            store.append('<MISSING>')
+            store.append("http://gulfoil.com/station-locator")
+            # if store[2] in addressess:
+            #     continue
+            # addressess.append(store[2])
+            # print('---store--'+str(store))
+            yield store
+        page+= 1     
         
-                    # print("==========================full_address=  ",full_address)
-                    # print("==========================city=  ",city)
-                    # print(indices[0],"==========================street_address=  ",street_address)
-                    store = []
-                    locator_domain =base_url
-                    result_coords.append((latitude, longitude))
-                    store.append(locator_domain if locator_domain else '<MISSING>')
-                    store.append(location_name if location_name else '<MISSING>')
-                    store.append(street_address if street_address else '<MISSING>')
-                    store.append(city if city else '<MISSING>')
-                    store.append(state if state else '<MISSING>')
-                    store.append(zipp if zipp else '<MISSING>')
-                    store.append(country_code if country_code else '<MISSING>')
-                    store.append(store_number if store_number else '<MISSING>')
-                    store.append(phone if phone else '<MISSING>')
-                    store.append(location_type if location_type else '<MISSING>')
-                    store.append(latitude if latitude else '<MISSING>')
-                    store.append(longitude if longitude else '<MISSING>')
-                    store.append(hours_of_operation if hours_of_operation else '<MISSING>')
-                    store.append("<MISSING>")
-                    if store[2] in addressess:
-                        continue
-                    addressess.append(store[2])
-                   # print('---store--'+str(store))
-                    yield store
-                
-            # q= q+1
-        if current_results_len < MAX_RESULTS:
-            #print("max distance update")
-            search.max_distance_update(MAX_DISTANCE)
-        elif current_results_len == MAX_RESULTS:
-            #print("max count update")
-            search.max_count_update(result_coords)
-        else:
-            raise Exception("expected at most " + str(MAX_RESULTS) + " results")
-        zip_code = search.next_zip()           
-        # print("===========================================", q)
             
 
 def scrape():
