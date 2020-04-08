fc9e6f6abc1420b08637842f487bc6ce9fd2f71f
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-04-09 00:14:25 +0500

    Add files via upload

---
+++ apify/aleenah/storelocator/talbots_com/scrape.py
@@ -0,0 +1,49 @@
+import csv
+from sgrequests import SgRequests
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation",
+                         "page_url"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+session = SgRequests()
+all=[]
+def fetch_data():
+    # Your scraper here
+    res=session.get("https://www.talbots.com/on/demandware.store/Sites-talbotsus-Site/default/Stores-GetNearestStores?latitude=37.090240&longitude=-95.712891&maxdistance=5000")
+    jso=res.json()["stores"]
+
+    for id,js in jso.items():
+        #print(id)
+        all.append([
+            "https://www.talbots.com",
+            js["name"],
+            js["address2"],
+            js["city"],
+            js["stateCode"],
+            js["postalCode"],
+            js["countryCode"],
+            id,  # store #
+            js["phone"],  # phone
+            "<MISSING>",  # type
+            js["latitude"],  # lat
+            js["longitude"],  # long
+            js["storeHours"].replace("<br>",", "),  # timing
+            "https://www.talbots.com/on/demandware.store/Sites-talbotsus-Site/default/Stores-GetNearestStores?latitude=37.090240&longitude=-95.712891&maxdistance=5000"])
+    return all
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+scrape()
