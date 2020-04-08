b3e587928c2eb3d13d7248c78e2f86006ad820e7
nsxdarin <nsxdarin@gmail.com>
2020-04-08 10:38:08 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/sonicdrivein_com/scrape.py
+++ apify/nsxdarin/storelocator/sonicdrivein_com/scrape.py
@@ -1,6 +1,7 @@
 import csv
 import urllib2
 from sgrequests import SgRequests
+import gzip
 
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
@@ -14,17 +15,29 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
-    url = 'https://locations.sonicdrivein.com/sitemap.xml'
+    url = 'https://locations.sonicdrivein.com/sitemap/sitemap_index.xml'
     locs = []
+    sitemaps = []
     r = session.get(url, headers=headers)
     for line in r.iter_lines():
-        if '<xhtml:link rel="alternate" hreflang="en" href="' in line:
-            lurl = line.split('<xhtml:link rel="alternate" hreflang="en" href="')[1].split('"')[0]
-            if lurl.count('/') == 5 and lurl not in locs:
-                locs.append(lurl)
+        if '<loc>https://locations.sonicdrivein.com/sitemap/sitemap' in line:
+            sitemaps.append(line.split('<loc>')[1].split('<')[0])
+    for sm in sitemaps:
+        print('Pulling Sitemap %s...' % sm)
+        smurl = sm
+        with open('branches.xml.gz','wb') as f:
+            f.write(urllib2.urlopen(smurl).read())
+            f.close()
+            with gzip.open('branches.xml.gz', 'rb') as f:
+                for line in f:
+                    if '<loc>https://locations.sonicdrivein.com/' in line and '.html' in line:
+                        lurl = line.split('<loc>')[1].split('<')[0]
+                        if lurl not in locs:
+                            locs.append(lurl)
+        print(str(len(locs)) + ' Locations Found...')
     print('Found %s Locations.' % str(len(locs)))
     for loc in locs:
-
+        print('Pulling Location %s...' % loc)
         url = loc
         add = ''
         city = ''
@@ -41,41 +54,26 @@ def fetch_data():
         typ = 'Restaurant'
         r2 = session.get(loc, headers=headers)
         for line2 in r2.iter_lines():
-            if '</head><body>' in line2:
-                store = line2.split(',"id":')[1].split(',')[0]
-                lat = line2.split(',"latitude":')[1].split(',')[0]
-                lng = line2.split(',"longitude":')[1].split(',')[0]
-                try:
-                    phone = line2.split('itemprop="telephone" id="telephone">')[1].split('<')[0].strip()
-                except:
-                    pass
-                addinfo = line2.split('itemtype="http://schema.org/PostalAddress"')[1].split('</address>')[0]
-                add = addinfo.split('<span class="c-address-street-1">')[1].split('<')[0].strip()
-                try:
-                    add = add + ' ' + addinfo.split('<span class="c-address-street-2">')[1].split('<')[0].strip()
-                except:
-                    pass
-                city = addinfo.split('itemprop="addressLocality">')[1].split('<')[0].strip()
-                state = addinfo.split('itemprop="addressRegion">')[1].split('<')[0].strip()
-                country = addinfo.split('itemprop="addressCountry">')[1].split('<')[0].strip()
-                zc = addinfo.split('itemprop="postalCode">')[1].split('<')[0].strip()
-                dayinfo = line2.split("data-days='[")[1].split(']}]')[0]
-                days = dayinfo.split('"day":"')
-                for day in days:
-                    if '"intervals":' in day:
-                        if '"end":' not in day:
-                            if hours == '':
-                                hours = day.split('"')[0] + ': Closed'
-                            else:
-                                hours = hours + '; ' + day.split('"')[0] + ': Closed'
-                        else:
-                            if hours == '':
-                                endtime = day.split('"end":')[1].split(',')[0]
-                                if endtime == '0':
-                                    endtime = '0000'
-                                hours = day.split('"')[0] + ': ' + day.split('"start":')[1].split('}')[0] + '-' + endtime
-                            else:
-                                hours = hours + '; ' + day.split('"')[0] + ': ' + day.split('"start":')[1].split('}')[0] + '-' + endtime
+            if '<div class="map-list-item-wrap" data-fid="' in line2:
+                store = line2.split('<div class="map-list-item-wrap" data-fid="')[1].split('"')[0]
+            if '<span class="stores-nearby-text">' in line2:
+                name = line2.split('<span class="stores-nearby-text">')[1].split('<')[0]
+            if '"latitude": "' in line2:
+                lat = line2.split('"latitude": "')[1].split('"')[0]
+            if '"longitude": "' in line2:
+                lng = line2.split('"longitude": "')[1].split('"')[0]
+            if '"streetAddress": "' in line2:
+                add = line2.split('"streetAddress": "')[1].split('"')[0].strip()
+            if '"addressLocality": "' in line2:
+                city = line2.split('"addressLocality": "')[1].split('"')[0]
+            if '"addressRegion": "' in line2:
+                state = line2.split('"addressRegion": "')[1].split('"')[0]
+            if '"postalCode": "' in line2:
+                zc = line2.split('"postalCode": "')[1].split('"')[0]
+            if '"telephone": "' in line2:
+                phone = line2.split('"telephone": "')[1].split('"')[0].strip()
+            if '"openingHours": "' in line2:
+                hours = line2.split('"openingHours": "')[1].split('"')[0].strip()
         if hours == '':
             hours = '<MISSING>'
         if phone == '':
