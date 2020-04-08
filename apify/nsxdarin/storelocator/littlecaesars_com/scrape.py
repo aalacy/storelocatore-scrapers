9a59538a16b382b33321c0796dc048cddc362987
nsxdarin <nsxdarin@gmail.com>
2020-04-08 10:25:31 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/littlecaesars_com/scrape.py
+++ apify/nsxdarin/storelocator/littlecaesars_com/scrape.py
@@ -7,8 +7,6 @@ import sgzip
 search = sgzip.ClosestNSearch()
 search.initialize()
 
-requests.packages.urllib3.disable_warnings()
-
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
