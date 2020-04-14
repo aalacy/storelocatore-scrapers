c45d2bd3b46dd16966575e34b892ad32f862e448
graham <graham.cr.baker@gmail.com>
2020-04-14 15:22:24 -0600

    new

---
+++ apify/ggrahambaker/storelocator/amitahealth_org/scrape.py
@@ -0,0 +1,120 @@
+import csv
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    return webdriver.Chrome('chromedriver', options=options)
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def addy_ext(addy):
+    addy = addy.split(',')
+    city = addy[0]
+    state_zip = addy[1].strip().split(' ')
+    if len(state_zip) == 1:
+        state = '<MISSING>'
+        zip_code = state_zip[0]
+    else:
+        state = state_zip[0]
+        zip_code = state_zip[1]
+    return city, state, zip_code
+
+
+def fetch_data():
+    locator_domain = 'https://www.amitahealth.org/'
+    ext = 'our-locations/'
+
+    driver = get_driver()
+    driver.get(locator_domain + ext)
+
+    past_urls = []
+    link_list = []
+    for i in range(1, 15):
+        url = 'https://www.amitahealth.org/our-locations/?page=' + str(i) + '&count=50'
+        driver.get(url)
+        driver.implicitly_wait(5)
+        time.sleep(3)
+        curr_url = driver.current_url
+        if curr_url not in past_urls:
+            past_urls.append(curr_url)
+        else:
+            break
+            
+            
+        locs = driver.find_elements_by_css_selector('div.stacked-md')
+        for loc in locs:
+            link = loc.find_element_by_css_selector('a').get_attribute('href')
+            if link not in link_list:
+                link_list.append(link)
+            else:
+                continue
+
+    
+    all_store_data = []
+    for link in link_list:
+        if link == 'https://www.amitahealth.org/location/amita-health-medical-group':
+            continue
+        driver.get(link)
+        driver.implicitly_wait(5)
+        
+        location_name = driver.find_element_by_css_selector('h1').text
+        google_link = driver.find_element_by_xpath('//a[contains(@href,"/maps/")]').get_attribute('href')
+        
+        start = google_link.find('query=')
+        coords = google_link[start + 6:].split('%2C')
+        lat = coords[0]
+        longit = coords[1]
+        
+        
+        addy = driver.find_element_by_xpath('//strong[contains(text(),"Address")]').find_element_by_xpath('..').text.split('\n')
+        
+        street_address = addy[1].split('Ste')[0].strip()
+        city, state, zip_code = addy_ext(addy[2])
+
+        phone_number = driver.find_element_by_xpath('//strong[contains(text(),"Phone")]').find_element_by_xpath('..').text
+        phone_number = phone_number.replace('Phone:', '').strip()
+        
+        hours_div = driver.find_elements_by_xpath('//label[contains(text(),"Office Hours")]')#.text
+        if len(hours_div) == 0:
+            hours = '<MISSING>'
+        else:
+            hours = hours_div[0].find_element_by_xpath('..').text.replace('Office Hours', '').replace('\n', ' ').strip()
+        
+        country_code = 'US'
+        store_number = '<MISSING>'
+        location_type = '<MISSING>'
+        page_url = link
+        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code, 
+                    store_number, phone_number, location_type, lat, longit, hours, page_url]
+
+                
+        all_store_data.append(store_data)
+
+
+
+
+
+    driver.quit()
+    return all_store_data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
---
+++ apify/ggrahambaker/storelocator/capitalhealth_org/scrape.py
@@ -0,0 +1,114 @@
+import csv
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    return webdriver.Chrome('chromedriver', options=options)
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    driver = get_driver()
+
+    link_list = []
+
+    locator_domain = 'https://www.capitalhealth.org'
+
+    for i in range(7):
+        url = 'https://www.capitalhealth.org/our-locations/result?field_services_target_id=All&field_search_city_target_id=All&field_location_type_target_id=All&field_location_name_value=&page=' + str(i)
+        driver.get(url)
+        driver.implicitly_wait(5)
+        time.sleep(3)
+        
+        
+        locs = driver.find_elements_by_css_selector('div.view-empty')
+        if len(locs) > 0:
+            break
+            
+        titles = driver.find_elements_by_css_selector('h2.v-listing__title')
+        
+        links = [t.find_element_by_css_selector('a').get_attribute('href') for t in titles]
+        
+        for l in links:
+            link_list.append(l)
+
+    all_store_data = []
+    for link in link_list:
+        if 'multispecialty-care-lower-makefield' in link:
+            continue
+        driver.get(link)
+        driver.implicitly_wait(5)
+        time.sleep(3)
+        
+        
+        location_name = driver.find_element_by_css_selector('h1').text
+        
+        
+        street_address = driver.find_element_by_css_selector('span.address-line1').text
+        city = driver.find_element_by_css_selector('span.locality').text
+        state = driver.find_element_by_css_selector('span.administrative-area').text
+        zip_code = driver.find_element_by_css_selector('span.postal-code').text
+        
+        phone_number = driver.find_element_by_css_selector('div.field--name-field-phone').find_element_by_css_selector('div.field__item').text
+        
+        
+        
+        hours_div = driver.find_elements_by_css_selector('div.field--name-field-office-hours')
+        if len(hours_div) == 0:
+            hours = '<MISSING>'
+        else:
+            hours = hours_div[0].text.replace('\n', ' ')
+
+            
+
+        
+        google_link = driver.find_elements_by_xpath('//a[contains(@href,"/maps/")]')#.get_attribute('href')
+        if len(google_link) == 0:
+            lat, longit = '<MISSING>', '<MISSING>'
+        else:
+            
+            goog = google_link[0].get_attribute('href')
+            start = goog.find('@')
+            coords = goog[start + 1:].split(',')
+            lat = coords[0]
+            longit = coords[1]
+        
+        
+        
+        
+        country_code = 'US'
+        store_number = '<MISSING>'
+        location_type = '<MISSING>'
+        page_url = link
+        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code, 
+                    store_number, phone_number, location_type, lat, longit, hours, page_url]
+
+
+        all_store_data.append(store_data)
+        
+
+    driver.quit()
+    return all_store_data
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
