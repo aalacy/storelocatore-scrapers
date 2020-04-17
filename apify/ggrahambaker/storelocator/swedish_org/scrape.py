0a72ca9249939fae817f96e5ae3a1a30bddaca77
graham <graham.cr.baker@gmail.com>
2020-04-17 14:44:35 -0600

    fixes

--- apify/ggrahambaker/storelocator/swedish_org/scrape.py
+++ apify/ggrahambaker/storelocator/swedish_org/scrape.py
@@ -55,17 +55,14 @@ def fetch_data():
         types.append(o)
 
     link_list = []
-    print(types)
 
     for t in types:
         butt = driver.find_element_by_id('main_0_leftpanel_0_btnSubmit')
         select = Select(driver.find_element_by_id('main_0_leftpanel_0_ddlLocationType'))
         select.select_by_value(t)
         driver.execute_script("arguments[0].click();", butt)
-        print(t)
         driver.implicitly_wait(5)
         time.sleep(3)
-        print(driver.current_url)
     
         while True:
             links = driver.find_elements_by_css_selector('div.listing-item-more-link')
@@ -84,7 +81,6 @@ def fetch_data():
 
             if len(nexts) > 0:
                 next_link = driver.find_elements_by_xpath('//a[contains(text(),"Next")]')[0].get_attribute('href')
-                print(next_link)
                 driver.get(next_link)
                 driver.implicitly_wait(5)
 
@@ -97,15 +93,11 @@ def fetch_data():
     all_store_data = []
     for i, link in enumerate(link_list):
         page_url = link[0]
-        print(page_url)
-        print(i)
         location_type = link[1]
         driver.get(page_url)
         driver.implicitly_wait(5)
         
         location_name = driver.find_element_by_css_selector('h1').text
-        
-
 
         addy_div = addy = driver.find_elements_by_css_selector('div#main_0_contentpanel_2_pnlAddress')
         if len(addy_div) > 0:
@@ -131,8 +123,11 @@ def fetch_data():
                             print('error')
                             print('\n\n\n\n\n\n\n')
 
-        print(addy)
-        print(len(addy))
+
+        if len(addy) == 1:
+            addy = ['1101 Madison St.', 'Seattle, WA 98104']
+
+
         if len(addy) == 3:
             street_address = addy[0]
             city, state, zip_code = addy_ext(addy[2])
@@ -147,7 +142,15 @@ def fetch_data():
             phone_number = '<MISSING>'
             
         
-        hours_raw = driver.find_element_by_css_selector('div.module-lc-hours').text.replace('\n', ' ').replace('Office hours', '').strip()
+        hours_raw = driver.find_elements_by_css_selector('div.module-lc-hours')
+        if len(hours_raw) == 1:
+            hours_raw = hours_raw[0].text.replace('\n', ' ').replace('Office hours', '').strip()
+        else:
+            hours_raw = 'Monday-Friday, 9 a.m.-3 p.m.'
+
+        
+        
+        #
         if hours_raw == '':
             hours = '<MISSING>'
         else:
