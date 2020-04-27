614782ace892453b9891d787b5d07ba5972e17fe
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-04-27 20:48:30 +0500

    Add files via upload

--- apify/shumaila/storelocator/hymiler_com/scrape.py
+++ apify/shumaila/storelocator/hymiler_com/scrape.py
@@ -4,9 +4,11 @@ from bs4 import BeautifulSoup
 import csv
 import string
 import re, time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 
 def write_output(data):
@@ -19,79 +21,65 @@ def write_output(data):
         for row in data:
             writer.writerow(row)
 
-def get_driver():
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    chrome_path = '/Users/Dell/local/chromedriver'
-    #return webdriver.Chrome(chrome_path, chrome_options=options)
-    return webdriver.Chrome('chromedriver', chrome_options=options)
+
 
 
 def fetch_data():
     # Your scraper here
     data = []
-    p = 1
+    p = 0
     url = 'https://hymiler.com/locations/hy-miler-2224-townsend-road'
-    driver = get_driver()
-    driver.get(url)
-    time.sleep(2)
-    mainselect = driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div/div/select')
-    poption = mainselect.find_elements_by_tag_name('option')
+    r = session.get(url, headers=headers, verify=False)
+    time.sleep(3)    
+    soup = BeautifulSoup(r.text,"html.parser")
+    mainselect = soup.find('select')
+    poption = mainselect.findAll('option')
     print(len(poption))
+    
     locs = []
     titles = []
 
     for n in range(1, len(poption)):
-        link = poption[n].get_attribute('value')
+        link = poption[n]['value']
         link = "https://hymiler.com" + link
         locs.append(link)
         titles.append(poption[n].text)
     locs.append('https://hymiler.com/locations/hy-miler-2224-townsend-road')
     titles.append('Hy-Miler #2224, Townsend Road')
 
+    
+    cleanr = re.compile(r'<[^>]+>')
     for n in range(0, len(locs)):
         link = locs[n]
         title = titles[n]
-
-        print(link)
-        driver1 = get_driver()
-        driver1.get(link)
+        
+          
         time.sleep(2)
         start = title.find("#") + 1
         end = title.find(",", start)
         store = title[start:end]
-        maindiv = driver1.find_element_by_class_name('locationaddress')
-        detail = maindiv.text
-        detail = detail.replace("\n","|")
-        print(detail)
-        start = detail.find("|")
-        street = detail[0:start]
-        start = start + 1
-        end = detail.find(",", start)
-        city = detail[start:end]
-        start = end + 1
-        end = detail.find("|", start)
-        state = detail[start:end]
-        start = detail.find("States",end)
-        start = detail.find(" ",start) + 1
-        end = detail.find("|", start)
-        pcode = detail[start:end]
-        start = end + 1
-        phone = detail[start:len(detail)]
+        r = session.get(link, headers=headers, verify=False)
+        soup = BeautifulSoup(r.text,"html.parser")
+        maindiv = soup.find('span',{'class':'locationaddress'})
+        detail = cleanr.sub('\n', str(maindiv))
+        
+        detail = detail.splitlines()
+        street = detail[1]
+        city,state = detail[2].split(',', 1)
+        pcode = detail[3]
+        pcode = pcode.replace('United States ','')
+        
+        phone = detail[5]
 
-        detail = str(driver1.page_source)
+        
+        detail = str(soup)
         start = detail.find('"coordinates"')
         start = detail.find('[', start)+ 1
         end = detail.find(",", start)
         longt = detail[start:end]
         start = end + 1
         end = detail.find(']',start)
-        lat = detail[start:end]
-
-        detail = driver1.find_element_by_xpath('//*[@id="locationsearch"]/div[2]/span[2]')
-        detail = detail.text
+        lat = detail[start:end]               
         if detail.find("Open 24 hours") > -1:
             hours = "Open 24 hours"
         else:
@@ -100,20 +88,9 @@ def fetch_data():
         longt = longt[0:8]
         title = title.replace(",", "")
 
-        print(title)
-        print(store)
-        print(street)
-        print(city)
-        print(state)
-        print(pcode)
-        print(phone)
-        print(lat)
-        print(longt)
-        print(hours)
-        print(p)
-        print(".................")
-        p += 1
-        driver1.quit()
+       
+        
+        
         data.append([
             'https://hymiler.com',
             link,
@@ -130,7 +107,9 @@ def fetch_data():
             longt,
             hours
         ])
-
+        #print(p,data[p])
+        p += 1
+        
     return data
 
 
