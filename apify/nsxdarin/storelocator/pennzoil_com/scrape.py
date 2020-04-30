ffb2a00c4d160976e91167f64c581bdb323b8449
nsxdarin <nsxdarin@gmail.com>
2020-04-30 15:36:00 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/pennzoil_com/scrape.py
+++ apify/nsxdarin/storelocator/pennzoil_com/scrape.py
@@ -1,13 +1,13 @@
 import csv
 import urllib2
-import requests
+from sgrequests import SgRequests
 import sgzip
 import json
 
 search = sgzip.ClosestNSearch()
 search.initialize()
 
-session = requests.Session()
+session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
 
