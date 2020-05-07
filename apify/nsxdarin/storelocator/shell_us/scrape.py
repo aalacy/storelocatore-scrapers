cbc35e5e37f813e8d30bc2876bab4b678ebd8d07
nsxdarin <nsxdarin@gmail.com>
2020-05-07 18:55:35 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/shell_us/scrape.py
+++ apify/nsxdarin/storelocator/shell_us/scrape.py
@@ -16,6 +16,7 @@ def write_output(data):
 def fetch_data():
     ids = []
     urls = ['https://shellgsllocator.geoapp.me/api/v1/locations/within_bounds?sw%5B%5D=7.2&sw%5B%5D=-174.7&ne%5B%5D=25&ne%5B%5D=-148.1&autoload=true&travel_mode=driving&avoid_tolls=false&avoid_highways=false&avoid_ferries=false&corridor_radius=5&driving_distances=false&format=json',
+            'https://shellgsllocator.geoapp.me/api/v1/locations/within_bounds?sw%5B%5D=15.42&sw%5B%5D=-69.88&ne%5B%5D=19.87&ne%5B%5D=-63.23&autoload=true&travel_mode=driving&avoid_tolls=false&avoid_highways=false&avoid_ferries=false&corridor_radius=5&driving_distances=false&format=json',
             'https://shellgsllocator.geoapp.me/api/v1/locations/within_bounds?sw%5B%5D=49.7&sw%5B%5D=175&ne%5B%5D=68.3&ne%5B%5D=-131.8&autoload=true&travel_mode=driving&avoid_tolls=false&avoid_highways=false&avoid_ferries=false&corridor_radius=5&driving_distances=false&format=json'
             ]
     url = ''
@@ -49,6 +50,9 @@ def fetch_data():
                         hours = ''
                         if phone == '':
                             phone = '<MISSING>'
+                        if country == 'PR':
+                            state = 'PR'
+                            country = 'US'
                         if storeinfo not in ids and country == 'US' and loc != '<MISSING>':
                             print('Pulling Store %s...' % name)
                             days = []
@@ -85,13 +89,13 @@ def fetch_data():
                                 ids.append(storeinfo)
 
     url = ''
-    for x in range(24, 50):
-        for y in range(-66, -126, -1):
-            lats = x
-            latn = x + 1
-            lnge = y
-            lngw = y - 1
-            print(str(x) + ',' + str(y))
+    for x in range(240, 500, 5):
+        for y in range(-660, -1260, -5):
+            lats = float(x / 10)
+            latn = float(x / 10) + .5
+            lnge = float(y / 10)
+            lngw = float(y / 10) - .5
+            print(str(lats) + ',' + str(lngw) + ',' + str(latn) + ',' + str(lnge))
             url = 'https://shellgsllocator.geoapp.me/api/v1/locations/within_bounds?sw%5B%5D=' + str(lats) + '&sw%5B%5D=' + str(lngw) + '&ne%5B%5D=' + str(latn) + '&ne%5B%5D=' + str(lnge) + '&selected=&autoload=true&travel_mode=driving&avoid_tolls=false&avoid_highways=false&avoid_ferries=false&corridor_radius=5&driving_distances=false&format=json'
             r = session.get(url, headers=headers)
             for line in r.iter_lines():
