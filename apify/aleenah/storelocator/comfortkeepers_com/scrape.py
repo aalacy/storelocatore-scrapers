78593df061a94c1eb8acdb9337213ec561510b66
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-05-09 05:35:04 +0500

    Update scrape.py

--- apify/aleenah/storelocator/comfortkeepers_com/scrape.py
+++ apify/aleenah/storelocator/comfortkeepers_com/scrape.py
@@ -18,21 +18,8 @@ session = SgRequests()
 def fetch_data():
 
     all=[]
-    headers={
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-'accept-encoding': 'gzip, deflate, br',
-'accept-language': 'en-US,en;q=0.9',
-'cache-control': 'max-age=0',
-'if-none-match': 'W/"1b70fd-YIZuQ3temblmMrTefTuK6Cg4fH0"',
-'referer': 'https://www.comfortkeepers.com/offices',
-'sec-fetch-dest': 'document',
-'sec-fetch-mode': 'navigate',
-'sec-fetch-site': 'same-origin',
-'sec-fetch-user': '?1',
-'upgrade-insecure-requests': '1',
-'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
-    }
-    res= session.get("https://www.comfortkeepers.com/offices",headers=headers)
+
+    res= session.get("https://www.comfortkeepers.com/offices")
     soup = BeautifulSoup(res.text, 'html.parser')
     states = soup.find_all('a', {'class': 'btn btn-flat btn-block ember-view'})  #states
     print(len(states))
