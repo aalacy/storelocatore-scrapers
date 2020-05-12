abf43160ad2547a1c714a19320dfbc4d705634c4
nck <nck@users.noreply.github.com>
2020-05-12 11:00:57 -0400

    uhaul.com (SLC-411): fix whitespace issue

--- apify/nsxdarin/storelocator/uhaul_com/scrape.py
+++ apify/nsxdarin/storelocator/uhaul_com/scrape.py
@@ -17,63 +17,63 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_ALL)
         writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip",
-                        "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+                         "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
 
 
 def random_sleep():
-  time.sleep(random.random()*2)
+    time.sleep(random.random()*2)
 
 
 def get_session():
-  # give each thread its own session object
-  # if using proxy, each thread's session should have a unique IP
-  if not hasattr(thread_local, "session"):
-    thread_local.session = SgRequests()
-  return thread_local.session
+    # give each thread its own session object
+    # if using proxy, each thread's session should have a unique IP
+    if not hasattr(thread_local, "session"):
+        thread_local.session = SgRequests()
+    return thread_local.session
 
 
 def get_state_urls():
-  state_urls = []
-  session = get_session()
-  url = 'https://www.uhaul.com/Locations/US_and_Canada/'
-  r = session.get(url, headers=headers)
-  for line in r.iter_lines(decode_unicode=True):
-      if "<a href='/Locations/" in line:
-          lurl = 'https://www.uhaul.com' + \
-              line.split("href='")[1].split("'")[0]
-          state_urls.append(lurl)
-  return state_urls
+    state_urls = []
+    session = get_session()
+    url = 'https://www.uhaul.com/Locations/US_and_Canada/'
+    r = session.get(url, headers=headers)
+    for line in r.iter_lines(decode_unicode=True):
+        if "<a href='/Locations/" in line:
+            lurl = 'https://www.uhaul.com' + \
+                line.split("href='")[1].split("'")[0]
+            state_urls.append(lurl)
+    return state_urls
 
 
 def get_city_urls(state_urls):
-  cities_all = []
-  with ThreadPoolExecutor(max_workers=8) as executor:
-      futures = [executor.submit(get_cities_in_state, url)
-                                 for url in state_urls]
-      for result in as_completed(futures):
-          cities_all.extend(result.result())
-  return cities_all
+    cities_all = []
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        futures = [executor.submit(get_cities_in_state, url)
+                   for url in state_urls]
+        for result in as_completed(futures):
+            cities_all.extend(result.result())
+    return cities_all
 
 
 def get_cities_in_state(state_url):
-  cities = []
-  session = get_session()
+    cities = []
+    session = get_session()
 
-  #print('Pulling State %s ...' % state_url)
-  random_sleep()
-  r = session.get(state_url, headers=headers)
-  #print('status: ', r.status_code)
+    #print('Pulling State %s ...' % state_url)
+    random_sleep()
+    r = session.get(state_url, headers=headers)
+    #print('status: ', r.status_code)
 
-  for line in r.iter_lines(decode_unicode=True):
-      if "<a href='/Locations/" in line:
-          lurl = 'https://www.uhaul.com' + \
-              line.split("href='")[1].split("'")[0]
-          if lurl not in cities:
-            cities.append(lurl)
+    for line in r.iter_lines(decode_unicode=True):
+        if "<a href='/Locations/" in line:
+            lurl = 'https://www.uhaul.com' + \
+                line.split("href='")[1].split("'")[0]
+            if lurl not in cities:
+                cities.append(lurl)
 
-  return cities
+    return cities
 
 
 def get_location_urls(city_urls):
@@ -81,7 +81,7 @@ def get_location_urls(city_urls):
 
     with ThreadPoolExecutor(max_workers=8) as executor:
         futures = [executor.submit(get_locations_in_city, url)
-                                   for url in city_urls]
+                   for url in city_urls]
         for result in as_completed(futures):
             locations_all.extend(result.result())
 
@@ -89,47 +89,47 @@ def get_location_urls(city_urls):
 
 
 def get_locations_in_city(city_url):
-  locs = []
-  session = get_session()
-  allids = []
-  coords = []
-  alllocs = []
-  #print('Pulling City %s ...' % city_url)
-  r2 = session.get(city_url, headers=headers)
-  #print('status: ', r2.status_code)
-  lines = r2.iter_lines(decode_unicode=True)
-  for line2 in lines:
-         if '"entityNum":"' in line2:
-              items = line2.split('"lat":')
-              for item in items:
-                  if '"entityNum":"' in item:
-                      lat = item.split(',')[0]
-                      lng = item.split('"long":')[1].split(',')[0]
-                      pid = item.split('"entityNum":"')[1].split('"')[0]
-                      coords.append(pid + '|' + lat + '|' + lng)
-          if '<ul class="sub-nav ">' in line2:
-              next(lines)
-              g = next(lines)
-              if 'href="' not in g:
-                  g = next(lines)
-              lurl = g.split('href="')[1].split('/"')[0]
-              if 'http' not in lurl:
-                  lurl = 'https://www.uhaul.com' + lurl
-              enum = lurl.rsplit('/', 1)[1]
-              alllocs.append(lurl + '|' + enum)
-  for location in alllocs:
-      for place in coords:
-          if location.split('|')[1] == place.split('|')[0]:
-              plat = place.split('|')[1]
-              plng = place.split('|')[2]
-              lurl = location.split('|')[0]
-              lid = lurl.rsplit('/', 1)[1]
-              lurl = 'https://www.uhaul.com/Locations/Truck-Rentals/' + lid
-              if lid not in allids:
-                  allids.append(lid)
-                  locs.append(lurl + '|' + plat + '|' + plng)
+    locs = []
+    session = get_session()
+    allids = []
+    coords = []
+    alllocs = []
+    #print('Pulling City %s ...' % city_url)
+    r2 = session.get(city_url, headers=headers)
+    #print('status: ', r2.status_code)
+    lines = r2.iter_lines(decode_unicode=True)
+    for line2 in lines:
+        if '"entityNum":"' in line2:
+            items = line2.split('"lat":')
+            for item in items:
+                if '"entityNum":"' in item:
+                    lat = item.split(',')[0]
+                    lng = item.split('"long":')[1].split(',')[0]
+                    pid = item.split('"entityNum":"')[1].split('"')[0]
+                    coords.append(pid + '|' + lat + '|' + lng)
+        if '<ul class="sub-nav ">' in line2:
+            next(lines)
+            g = next(lines)
+            if 'href="' not in g:
+                g = next(lines)
+            lurl = g.split('href="')[1].split('/"')[0]
+            if 'http' not in lurl:
+                lurl = 'https://www.uhaul.com' + lurl
+            enum = lurl.rsplit('/', 1)[1]
+            alllocs.append(lurl + '|' + enum)
+    for location in alllocs:
+        for place in coords:
+            if location.split('|')[1] == place.split('|')[0]:
+                plat = place.split('|')[1]
+                plng = place.split('|')[2]
+                lurl = location.split('|')[0]
+                lid = lurl.rsplit('/', 1)[1]
+                lurl = 'https://www.uhaul.com/Locations/Truck-Rentals/' + lid
+                if lid not in allids:
+                    allids.append(lid)
+                    locs.append(lurl + '|' + plat + '|' + plng)
 
-  return locs
+    return locs
 
 
 def fetch_data():
@@ -183,8 +183,9 @@ def get_location(loc):
             add = line2.split('"streetAddress":"')[1].split('"')[0]
             phone = line2.split('"telephone":"')[1].split('"')[0]
             hours = line2.split('"openingHours":')[
-                                1].split(',"aggregateRating')[0]
-            hours = hours.replace('[', '').replace(']','').replace('","','; ').replace('"','')
+                1].split(',"aggregateRating')[0]
+            hours = hours.replace('[', '').replace(']', '').replace(
+                '","', '; ').replace('"', '')
     if hours == '':
         hours = '<MISSING>'
     if typ == '':
