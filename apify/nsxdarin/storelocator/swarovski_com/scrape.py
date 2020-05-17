a12cfa0d585334e6afa165a8a8c088fdb377e139
nsxdarin <nsxdarin@gmail.com>
2020-05-17 11:04:48 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/swarovski_com/scrape.py
+++ apify/nsxdarin/storelocator/swarovski_com/scrape.py
@@ -2,7 +2,6 @@ import csv
 import urllib2
 from sgrequests import SgRequests
 import json
-from unidecode import unidecode
 
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
