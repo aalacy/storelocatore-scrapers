8440e9b64cd17759f4d9f7f8e0504d8340673787
diapk <Hank4957!>
2020-05-18 11:10:47 +0530

    docker run and push

--- apify/himanshu/storelocator/aspirus_org/scrape.py
+++ apify/himanshu/storelocator/aspirus_org/scrape.py
@@ -15,16 +15,15 @@ system = platform.system()
 
 session = SgRequests()
 def get_driver():
-	options = Options()
-	options.add_argument('--headless')
-	options.add_argument('--no-sandbox')
-	options.add_argument('--disable-dev-shm-usage')
-	options.add_argument('--window-size=1920,1080')
-	if "linux" in system.lower():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    if "linux" in system.lower():
         return webdriver.Firefox(executable_path='./geckodriver', options=options)        
     else:
         return webdriver.Firefox(executable_path='geckodriver.exe', options=options)
-
 
 def write_output(data):
 	with open('data.csv', mode='w', encoding="utf-8",newline="") as output_file:
