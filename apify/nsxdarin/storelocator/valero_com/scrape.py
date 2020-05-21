c32f5c121972ab22b712b99ab1c232aecfb78e69
nsxdarin <nsxdarin@gmail.com>
2020-05-21 15:32:51 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/valero_com/scrape.py
+++ apify/nsxdarin/storelocator/valero_com/scrape.py
@@ -4,8 +4,10 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import usaddress
+chrome_options = Options()
 
-driver = webdriver.Chrome("chromedriver")
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 session = requests.Session()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
