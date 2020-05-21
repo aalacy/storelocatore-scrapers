8691358b8b9b4879455c27f66431956ba4c77eb0
diapk <Hank4957!>
2020-05-21 10:38:03 +0530

    docker run and push

--- apify/himanshu/storelocator/betfred_com/scrape.py
+++ apify/himanshu/storelocator/betfred_com/scrape.py
@@ -1,85 +1,129 @@
 import csv
-from sgrequests import SgRequests
 from bs4 import BeautifulSoup
+import re
 import json
+import sgzip
+# import datetime
+# from datetime import datetime
 import requests
-session = SgRequests()
+
 
 def write_output(data):
-    with open('data.csv', mode='w', newline="") as output_file:
+    with open('data.csv', mode='w', newline='') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation",
-                         "page_url"])
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
         # Body
         for row in data:
-            
             writer.writerow(row)
 
 
 def fetch_data():
-    addressess123=[]
-    locator_domain='https://www.brotoloc.com/'
-    vurl = "https://fairview.org/coveo/rest/v2/"
-    urls =['https://www.brotoloc.com/eau_claire_group_homes.phtml','https://www.brotoloc.com/western_rivers_group_homes.phtml']
-    r2 = session.get("https://www.brotoloc.com/fox_valley_group_homes.phtml")
-    soup2 = BeautifulSoup(r2.text, "html5lib")
-    for data in soup2.find_all("p",{"class":"nomargin"}):
-        full = list(soup2.find("div",{"id":data.find("span",{"class":"strong-orange"})['id'].replace("_info",'')}).stripped_strings)
-        street_address = full[0]
-        city = full[1].split( )[0]
-        state = full[1].split( )[1].strip().split( )[0]
-        zipp = full[1].split( )[2]
-        phone = full[2]
-        page_url = '<MISSING>'
-        hours1='<MISSING>'
-        location_name = data.text.strip().split("{ ")[0]
-        latitude = '<MISSING>'
-        longitude = '<MISSING>'
-        country_code = 'US'
-        store_number = '<MISSING>'
-        location_type = '<MISSING>'
-        store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
-                    store_number, phone, location_type, latitude, longitude, hours1, page_url]
+    
+    search = sgzip.ClosestNSearch()
+    search.initialize(country_codes = ["UK"])
+    MAX_RESULTS = 500
+    MAX_DISTANCE = 5
+    current_results_len = 0  # need to update with no of count.
+    zip_code = search.next_zip()
+    addresses = []
+    locator_domain = "https://www.betfred.com/"
+    addresses = []
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+    }
+    result_coords = []
+    while zip_code:
+        result_coords = []
+        # print("zip_code === " + str(zip_code))
+        #print("remaining zip =====" + str(len(search.zipcodes)))
         
-        # if store[2]  in addressess123:
-        #     continue
-        # addressess123.append(store[2])
-        # print(store)
-        yield store
-        # print(location_name)
-    for value in urls:
-        r1 = session.get(value)
-        soup1 = BeautifulSoup(r1.text, "html5lib")
-        for data in soup1.find_all("p",{"class":"nomargin"}):
-            full = soup1.find("div",{"id":data.find("span",{"class":"strong-orange"})['id'].replace("_info",'')})
-            street_address = list(full.stripped_strings)[0].split("•")[0]
-            city = list(full.stripped_strings)[0].split("•")[1].split(",")[0]
-            state = list(full.stripped_strings)[0].split("•")[1].split(",")[1].strip().split(" ")[0]
-            zipp = list(full.stripped_strings)[0].split("•")[1].split(",")[1].strip().split(" ")[1]
-            phone = list(full.stripped_strings)[0].split("•")[2].strip()
-            page_url = '<MISSING>'
-            hours1='<MISSING>'
-            location_name =data.text.strip().split("{ ")[0]
-            latitude = '<MISSING>'
-            longitude = '<MISSING>'
-            country_code = 'US'
-            store_number = '<MISSING>'
-            location_type = '<MISSING>'
-            store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
-                        store_number, phone, location_type, latitude, longitude, hours1, page_url]
-            store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
-            
-            # if store[2]  in addressess123:
-            #     continue
-            # addressess123.append(store[2])
-            # print(store)
-            yield store
+
+        url = "https://www.betfred.com/services/gis/searchstores"
+
+        payload = "{\"SearchLocation\":\" "+str(zip_code)+", UK\",\"MaximumShops\":10,\"MaximumDistance\":5}"
+        headers = {
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36",
+            'Accept': "*/*",
+            'Content-Type': "application/json",
+            'Cache-Control': "no-cache",
+            'Postman-Token': "3c1ecc49-46ce-46e0-9b1e-fdbe4cc065f2,fe8dde9f-a9f4-4533-93bc-31dc8ea517fa",
+            'Host': "www.betfred.com",
+            'Accept-Encoding': "gzip, deflate",
+            'Content-Length': "72",
+            # 'Cookie': "visid_incap_2254385=hjSkdKv+RkqMtjWwTAxSi6Vuvl4AAAAAQUIPAAAAAACR3BBQrL8/DL7LLA5GhBNv; nlbi_2254385=5f8jYykSo3o565vYUZa94AAAAACesR1rz6NtVnLS33FEVwCJ; incap_ses_47_2254385=uv/PFOHBhgE1lgwnePqmAJZvvl4AAAAA9y9mBXWwGEC3gUUk6DRG8g==",
+            'Connection': "keep-alive",
+            'cache-control': "no-cache"
+            }
+
+        response = requests.request("POST", url, data=payload, headers=headers).json()
+        
+        if response["Stores"]:
+            current_results_len=len(response["Stores"])
+            for loc in response["Stores"]:
+                store_number = loc["ShopNumber"]
+                location_name = loc["Title"]
+                if len(loc["Address"].split(",")) > 1:
+                    street_address = loc["Address"].split(",")[0].strip()
+                    city = loc["Address"].split(",")[-1].strip()
+                    # raw_address = "<MISSING>"
+                elif len(loc["Address"].split("  ")) > 1:
+                    
+                    # Berryden Retail Park  Berryden
+                    street_address =" ".join(loc["Address"].split("  ")[:-1]).strip()
+                    city = loc["Address"].split("  ")[-1]
+                else:
+                    #print(loc["Address"])
+                    street_address = loc["Address"]
+                    city = "<MISSING>"
+                    
+                # street_address = "<INACCESSIBLE>"
+                # city = "<INACCESSIBLE>"
+                state = "<MISSING>"
+                zipp = loc["Postcode"]
+                country_code = "UK"
+                phone = "<MISSING>"
+                latitude = loc["Location"]["Latitude"]
+                longitude = loc["Location"]["Longitude"]
+                hours_of_operation = loc["OpeningHoursDescription"].replace("\r\n","").strip()
+                location_type = "<MISSING>"
+                page_url = "https://www.betfred.com/shop-locator"
+                # raw_address = loc["Address"]
+                
+                result_coords.append((latitude, longitude))
+                store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
+                         store_number, phone, location_type, latitude, longitude, hours_of_operation, page_url]
+                if store_number not in addresses:
+                    addresses.append(store_number)
+
+                    store = [str(x).encode('ascii','ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+
+                    #print("data = " + str(store))
+                    #print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                    yield store
+                
+                
 
 
-          
+        
+
+        if current_results_len < MAX_RESULTS:
+            # print("max distance update")
+            search.max_distance_update(MAX_DISTANCE)
+        elif current_results_len == MAX_RESULTS:
+            # print("max count update")
+            search.max_count_update(result_coords)
+        else:
+            raise Exception("expected at most " + str(MAX_RESULTS) + " results")
+        zip_code = search.next_zip()
+
+
+    
+                
+       
+
 def scrape():
     data = fetch_data()
     write_output(data)
