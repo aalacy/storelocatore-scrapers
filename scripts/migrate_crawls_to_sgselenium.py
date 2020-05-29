258a6b22740ae67ec3b9e5a3cb18e1b4a6d23e43
Tenzing Shaw <tenzing@safegraph.com>
2020-05-28 23:01:35 -0700

    save script

--- scripts/migrate_crawls_to_sgselenium.py
+++ scripts/migrate_crawls_to_sgselenium.py
@@ -26,7 +26,6 @@ def get_padding(line):
         else:
             return '\t'*tab_count + ' '*space_count
 
-
 def process_get_driver_scraper(base_path):
     with open('{}/scrape.py'.format(base_path)) as oldfile:
         with open('{}/scrape-tmp.py'.format(base_path), 'w') as newfile:
@@ -52,20 +51,52 @@ def process_get_driver_scraper(base_path):
     process_requirements_file(base_path)
     replace_files(base_path)
 
+def process_global_scraper(base_path):
+    with open('{}/scrape.py'.format(base_path)) as oldfile:
+        with open('{}/scrape-tmp.py'.format(base_path), 'w') as newfile:
+            content = oldfile.readlines()
+            in_get_driver_method = False
+            ua_arg = ''
+            for line in content:
+                if 'from selenium import webdriver' in line:
+                    newfile.write(get_padding(line) + 'from sgselenium import SgSelenium\n')
+                elif 'from selenium.webdriver.chrome.options import Options' in line:
+                    continue
+                elif 'options = Options()' in line:
+                    continue
+                elif 'options.add_argument("user-agent' in line:
+                    ua = line.split('user-agent=')[1].split('"')[0]
+                    ua_arg = 'user_agent="{}"'.format(ua)
+                    continue
+                elif 'options.add_argument(' in line:
+                    continue
+                elif line.startswith('#driver'):
+                    continue
+                elif 'driver = webdriver.Chrome' in line:
+                    newfile.write(get_padding(line) + 'driver = SgSelenium().chrome({})\n'.format(ua_arg))
+                else:
+                    newfile.write(line)
+    process_requirements_file(base_path)
+    replace_files(base_path)
+
 def process(base_path):
     (_, _, files) = next(os.walk(base_path))
     if 'scrape.py' not in files or 'requirements.txt' not in files:
         print('skipping {}'.format(base_path))
         return
     with open('{}/scrape.py'.format(base_path)) as f:
-        has_get_driver, has_chrome = False, False
+        has_get_driver, has_chrome, is_global = False, False, False
         content = f.readlines()
         for line in content:
             if 'driver = get_driver()' in line:
                 has_get_driver = True
+            elif 'driver = webdriver.Chrome' in line:
+                is_global = True
             elif 'webdriver.Chrome' in line:
                 has_chrome = True
-        if has_get_driver and has_chrome:
+        if is_global:
+            process_global_scraper(base_path)
+        elif has_get_driver and has_chrome:
             process_get_driver_scraper(base_path)
 
 def run(root):
@@ -76,4 +107,4 @@ def run(root):
     else:
         process(root)
 
-run('/Users/tenzing/code/crawl-service/apify/ggrahambaker/storelocator')
+run('/Users/tenzing/code/crawl-service/apify/aleenah/storelocator')
