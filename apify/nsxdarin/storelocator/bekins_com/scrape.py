e82a3f4ebe38c40c3a09d356390e977181113345
nsxdarin <nsxdarin@gmail.com>
2020-06-03 10:18:52 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/bekins_com/scrape.py
+++ apify/nsxdarin/storelocator/bekins_com/scrape.py
@@ -1,6 +1,6 @@
 import csv
 import urllib2
-import SgRequests
+from sgrequests import SgRequests
 
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
@@ -32,28 +32,114 @@ def fetch_data():
         print('Pulling Location %s...' % loc)
         website = 'bekins.com'
         typ = 'Agent'
-        if 'ams-relocation' not in loc:
+        if 'ams-relocation' not in loc and 'bekins-northwest' not in loc:
             r2 = session.get(loc, headers=headers)
             lines = r2.iter_lines()
             for line2 in lines:
                 if '<h2>' in line2:
                     name = line2.split('<h2>')[1].split('<')[0].replace('\t','')
                 if '<div class="agent-address">' in line2:
-                    g = next(lines)
-                    add = g.split('<')[0].strip().replace('\t','')
-                    g = next(lines)
-                    city = g.split(',')[0].strip().replace('\t','')
-                    state = g.split(',')[1].split('&nbsp')[0].strip().replace('\t','')
-                    zc = g.split('&nbsp;&nbsp;')[1].split('<')[0].strip().replace('\t','')
+                    h = line2
+                    try:
+                        g = next(lines)
+                        add = g.split('<')[0].strip().replace('\t','')
+                        g = next(lines)
+                        city = g.split(',')[0].strip().replace('\t','')
+                        state = g.split(',')[1].split('&nbsp')[0].strip().replace('\t','')
+                        zc = g.split('&nbsp;&nbsp;')[1].split('<')[0].strip().replace('\t','')
+                    except:
+                        add = h.split('>')[1].split(',')[0].rsplit(' ',1)[0]
+                        city = h.split('>')[1].split(',')[0].rsplit(' ',1)[1]
+                        state = h.split(',')[1].strip().split(' ')[0]
+                        zc = h.split('</div>')[0].rsplit(' ',1)[1]
                 if 'Phone:' in line2 and 'href="tel:' in line2:
                     phone = line2.split('href="tel:')[1].split('"')[0].replace('/','')
-        else:
+        if 'ams-relocation' in loc:
             name = 'AMS Relocation, Inc.'
             add = '1873 Rollins Rd'
             city = 'Burlingame'
             state = 'CA'
             zc = '94010'
             phone = '866-798-0342'
+        if 'bekins-northwest-10' in loc:
+            name = 'Bekins Northwest'
+            add = '22647 72nd Ave S'
+            city = 'Kent'
+            state = 'WA'
+            zc = '98032'
+        if 'bekins-northwest-2' in loc:
+            name = 'Bekins Northwest'
+            add = '1100 Columbia Park Trail'
+            city = 'Richland'
+            state = 'WA'
+            zc = '99352'
+        if 'bekins-northwest-3' in loc:
+            name = 'Bekins Northwest'
+            add = '6501 216th Street SW'
+            city = 'Mountlake Terrace'
+            state = 'WA'
+            zc = '98043'
+        if 'bekins-northwest-4' in loc:
+            name = 'Bekins Northwest'
+            add = '1891 N 1st Street'
+            city = 'Yakima'
+            state = 'WA'
+            zc = '98901'
+        if 'bekins-northwest-5' in loc:
+            name = 'Bekins Northwest'
+            add = '1017 South 344th Street, Suite A'
+            city = 'Federal Way'
+            state = 'WA'
+            zc = '98003'
+        if 'bekins-northwest-6' in loc:
+            name = 'Bekins Northwest'
+            add = '10115 E. Knox Avenue'
+            city = 'Spokane'
+            state = 'WA'
+            zc = '99206'
+        if 'bekins-northwest-7' in loc:
+            name = 'Bekins Northwest'
+            add = '940 Poplar St SE'
+            city = 'Olympia'
+            state = 'WA'
+            zc = '98501'
+        if 'bekins-northwest-9' in loc:
+            name = 'Bekins Northwest'
+            add = '841 N 6th Ave'
+            city = 'Walla Walla'
+            state = 'WA'
+            zc = '99362'
+        if 'bekins-northwest' in loc and 'northwest-' not in loc:
+            name = 'Bekins Northwest'
+            add = '7010 150th St SW, Suite 101'
+            city = 'Lakewood'
+            state = 'WA'
+            zc = '98439'
+        if 'bekins-of-south-florida-3' in loc:
+            add = 'SALES OFFICE , 7875 Mandarin Dr'
+            city = 'Boca Raton'
+            state = 'FL'
+            zc = '33433'
+        if 'bekins-transfer-storage-2' in loc:
+            add = '201 Windsor Rd , Limerick Airport Business Center'
+            city = 'Pottstown'
+            state = 'PA'
+            zc = '19464'
+        if 'bekins-moving-solutions-inc-5' in loc:
+            add = '2025 Gillespie Way, Ste B'
+            city = 'El Cajon'
+            state = 'CA'
+            zc = '92020'
+        if 'boyer-rosene-moving-storage-inc' in loc:
+            add = '2638 S Clearbrook Dr'
+            city = 'Arlington Heights'
+            state = 'IL'
+            zc = '60005'
+        if 'bekins-moving-solutions-va' in loc:
+            add = '5827 Curlew Drive, Suite 5'
+            city = 'Norfolk'
+            state = 'VA'
+            zc = '23502'
         country = 'US'
         store = '<MISSING>'
         hours = '<MISSING>'
