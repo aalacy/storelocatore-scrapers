5e90affee0053d35aa07a33629125b1be67bcde4
nsxdarin <nsxdarin@gmail.com>
2020-06-03 09:46:04 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/bekins_com/scrape.py
+++ apify/nsxdarin/storelocator/bekins_com/scrape.py
@@ -1,8 +1,8 @@
 import csv
 import urllib2
-import requests
+import SgRequests
 
-session = requests.Session()
+session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
 
@@ -32,20 +32,28 @@ def fetch_data():
         print('Pulling Location %s...' % loc)
         website = 'bekins.com'
         typ = 'Agent'
-        r2 = session.get(loc, headers=headers)
-        lines = r2.iter_lines()
-        for line2 in lines:
-            if '<h2>' in line2:
-                name = line2.split('<h2>')[1].split('<')[0].replace('\t','')
-            if '<div class="agent-address">' in line2:
-                g = next(lines)
-                add = g.split('<')[0].strip().replace('\t','')
-                g = next(lines)
-                city = g.split(',')[0].strip().replace('\t','')
-                state = g.split(',')[1].split('&nbsp')[0].strip().replace('\t','')
-                zc = g.split('&nbsp;&nbsp;')[1].split('<')[0].strip().replace('\t','')
-            if 'Phone:' in line2 and 'href="tel:' in line2:
-                phone = line2.split('href="tel:')[1].split('"')[0].replace('/','')
+        if 'ams-relocation' not in loc:
+            r2 = session.get(loc, headers=headers)
+            lines = r2.iter_lines()
+            for line2 in lines:
+                if '<h2>' in line2:
+                    name = line2.split('<h2>')[1].split('<')[0].replace('\t','')
+                if '<div class="agent-address">' in line2:
+                    g = next(lines)
+                    add = g.split('<')[0].strip().replace('\t','')
+                    g = next(lines)
+                    city = g.split(',')[0].strip().replace('\t','')
+                    state = g.split(',')[1].split('&nbsp')[0].strip().replace('\t','')
+                    zc = g.split('&nbsp;&nbsp;')[1].split('<')[0].strip().replace('\t','')
+                if 'Phone:' in line2 and 'href="tel:' in line2:
+                    phone = line2.split('href="tel:')[1].split('"')[0].replace('/','')
+        else:
+            name = 'AMS Relocation, Inc.'
+            add = '1873 Rollins Rd'
+            city = 'Burlingame'
+            state = 'CA'
+            zc = '94010'
+            phone = '866-798-0342'
         country = 'US'
         store = '<MISSING>'
         hours = '<MISSING>'
