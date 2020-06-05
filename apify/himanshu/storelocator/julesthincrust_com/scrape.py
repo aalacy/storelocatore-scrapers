ed31c6cb6d3a38a5d70aa62dcb1f180bbfde1f7e
diapk <Hank4957!>
2020-06-05 12:02:39 +0530

    docker run and push

--- apify/himanshu/storelocator/julesthincrust_com/scrape.py
+++ apify/himanshu/storelocator/julesthincrust_com/scrape.py
@@ -2,72 +2,64 @@ import csv
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
-
-
 session = SgRequests()
-
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
-
 def fetch_data():
     header = {'User-agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'}
-    return_main_object = []
+    address = []
     base_url = "http://julesthincrust.com/"
     loacation_url = base_url+'locations/'
     r = session.get(loacation_url, headers = header)
     soup = BeautifulSoup(r.text,"lxml")
     
-    ck = soup.find("ul",{"class": "locations"}).find_all('li',{'class':'col-md-4'})
+    ck = soup.find("section",{"class": "section locations"}).find_all('div',{'class':'location-state'})
+    # print(ck)
     for target_list in ck:
-       locator_domain = base_url
-       location_name = target_list.find('div',{'class':'copy'}).find('h1').text.replace(',','')
-       
-       phone  = target_list.find('div',{'class':'copy'}).find('p',{'class':'address'}).text.split('(')[1].replace(')','') 
-       street_address = target_list.find('div',{'class':'copy'}).find('p',{'class':'address'}).text.split('(')[0]
-       city = target_list.find('div',{'class':'copy'}).find('h1').text.split(',')[0].strip()
-       state = target_list.find('div',{'class':'copy'}).find('h1').text.split(',')[1].strip()
-       store_number = "<MISSING>"
-       hb = target_list.find('div',{'class':'copy'}).find_all('p')[1].text.strip().split('\n')
-       
-       if len(hb) > 2:
-          hours_of_operation = hb[0].strip() + ' , ' + hb[1].strip() + ' , ' + hb[2].strip()
-       else:
-          hours_of_operation = hb[0].strip() + ' , ' + hb[1].strip()
-
-       country_code = 'US'
-       store_number = '<MISSING>'
-       location_type = 'julesthincrust'
-       latitude =  '<MISSING>'
-       longitude =  '<MISSING>'
-
-       zip = '<MISSING>'
-
-       store=[]
-       store.append(locator_domain if locator_domain else '<MISSING>')
-       store.append(location_name if location_name else '<MISSING>')
-       store.append(street_address if street_address else '<MISSING>')
-       store.append(city if city else '<MISSING>')
-       store.append(state if state else '<MISSING>')
-       store.append(zip if zip else '<MISSING>')
-       store.append(country_code if country_code else '<MISSING>')
-       store.append(store_number if store_number else '<MISSING>')
-       store.append(phone if phone else '<MISSING>')
-       store.append(location_type if location_type else '<MISSING>')
-       store.append(latitude if latitude else '<MISSING>')
-       store.append(longitude if longitude else '<MISSING>')
-       store.append(hours_of_operation  if hours_of_operation else '<MISSING>')
-       return_main_object.append(store)  
-    return return_main_object      
-          
-    
-    
+    #    locator_domain = base_url
+       location_name1 = target_list.find_all('div',{'class':'card'})
+       for i in location_name1:
+            location_name = (i.find('h1').text)
+            street_address1 = i.find_all('div',{'class':'details'})
+            for k in street_address1:
+                state = (k.find_all("p")[1].text.split(",")[-1].strip().split(" ")[0])
+                zipp = (k.find_all("p")[1].text.split(",")[-1].strip().split(" ")[-1])
+                city = k.find_all("p")[1].text.split(",")[0]
+                street_address = k.find_all("p")[0].text
+                phone = k.find_all("p")[-1].text
+            hours = i.find_all('div',{'class':'hours'})
+            hours_of_operation=''
+            for t in hours:
+                hours_of_operation =hours_of_operation+ ' '+ " ".join(list(t.stripped_strings)).strip()
+            page_url = ''
+            url = i.find_all('ul',{'class':'ordering'})
+            for h in url:
+                page_url = (h.find("li").find("a")['href'])
+            store = []
+            store.append(base_url if base_url else "<MISSING>")
+            store.append(location_name if location_name else "<MISSING>") 
+            store.append(street_address if street_address else "<MISSING>")
+            store.append(city if city else "<MISSING>")
+            store.append(state if state else "<MISSING>")
+            store.append(zipp if zipp else "<MISSING>")
+            store.append("US")
+            store.append("<MISSING>") 
+            store.append(phone if phone else "<MISSING>")
+            store.append("<MISSING>")
+            store.append("<MISSING>")
+            store.append("<MISSING>")
+            store.append(hours_of_operation if hours_of_operation else "<MISSING>")
+            store.append(page_url if page_url else "<MISSING>")
+            if store[2] in address :
+                continue
+            address.append(store[2])
+            yield store 
 def scrape():
     data = fetch_data()
     write_output(data)
