a9469f01b99126680fdf3248018650e34cfa1f3e
Kevin Collins <nck@users.noreply.github.com>
2020-06-06 21:19:20 -0400

    kfc.co.uk (SLC-1848): use sgrequests

--- apify/nsxdarin/storelocator/kfc_co_uk/scrape.py
+++ apify/nsxdarin/storelocator/kfc_co_uk/scrape.py
@@ -1,8 +1,8 @@
 import csv
-import requests
+from sgrequests import SgRequests
 import json
 
-session = requests.Session()
+session = SgRequests()
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
 days = ['Sunday', 'Monday', 'Tuesday',
