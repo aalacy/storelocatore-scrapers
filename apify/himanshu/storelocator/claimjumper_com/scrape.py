47fc22dd5291b297642781b3c04c08932e9cb13e
diapk <Hank4957!>
2020-06-08 16:01:39 +0530

    docker run and push

---
+++ apify/himanshu/storelocator/claimjumper_com/scrape.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import re
+import csv
+# import requests
+from sgrequests import SgRequests
+session = SgRequests()
+
+def write_output(data):
+    with open('data.csv', mode='w',newline= "",encoding='utf-8') as output_file:
+        writer = csv.writer(output_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    headers = {'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5','Accept':'application/json, text/javascript, */*; q=0.01'}
+    locator_domain = "http://claimjumper.com/"
+    r  = session.get("https://www.claimjumper.com/locations/",headers=headers)
+    soup = BeautifulSoup(r.text,"lxml")
+    for loc in soup.find_all("p",class_="locationsList"):
+        add = list(loc.stripped_strings)
+        street_address = add[1].strip()
+        city = add[2].split(",")[0].strip()
+        location_name = add[0].strip()
+        state = add[2].split(",")[1].split()[0].strip()
+        zipp = add[2].split(",")[1].split()[-1].strip()
+        country_code = "US"
+        phone = add[-1].strip()
+        location_type = "<MISSING>"
+        store_number = "<MISSING>"
+        page_url = "http://claimjumper.com"+loc.find("a",class_="locationLink")["href"].strip()
+        r1 = session.get(page_url,headers=headers)
+        soup1 = BeautifulSoup(r1.text,"lxml")
+        try:
+            location_id = soup1.find("div",{"id":"map"}).find("div",class_="mfcard mf_map_card")["location_id"].strip()
+            card_id = soup1.find("div",{"id":"map"}).find("div",class_="mfcard mf_map_card")["card_id"]
+            token = soup1.find(lambda tag: (tag.name == "script") and "loadWidgets" in tag.text).text.split("('")[1].split("')")[0]
+            headers = {
+                'Accept': '*/*',
+                'token':str(token),
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+                }
+            url = "https://partner-api.momentfeed.com/locations/cards?location_id="+str(location_id)+"&card_id=7"
+            r2= session.get(url,headers=headers)
+            soup2 = BeautifulSoup(r2.text,"lxml")
+            hours_of_operation = " ".join(list(soup2.find("dl",{"itemprop":"openingHours"}).stripped_strings))
+            latitude = soup1.find("span",{"id":"jsLat"}).text.strip()
+            longitude = soup1.find("span",{"id":"jsLong"}).text.strip()
+            
+        except:
+            try:
+                hours_of_operation = " ".join(list(soup1.find("h5",class_="hoursHeader").parent.stripped_strings)).replace("hours","").strip()
+            except:
+                hours_of_operation= "<MISSING>"
+            latitude = "33.378910"
+            longitude = "-111.964780"
+        store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
+        store_number, phone, location_type, latitude, longitude, hours_of_operation,page_url]
+
+        store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+        yield store
+        # print(store)
+        
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+scrape()
---
+++ apify/himanshu/storelocator/winkinglizard_com/scrape.py
@@ -0,0 +1,102 @@
+import csv
+import re
+import pdb
+import requests
+from lxml import etree
+import json
+
+base_url = 'https://winkinglizard.com'
+
+def validate(item):    
+    if type(item) == list:
+        item = ' '.join(item)
+    return item
+
+def get_value(item):
+    item = validate(item)
+    if item == '':
+        item = '<MISSING>'    
+    return item
+
+def eliminate_space(items):
+    rets = []
+    for item in items:
+        item = validate(item)
+        if item != '':
+            rets.append(item)
+    return rets
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    output_list = []
+    url = "https://winkinglizard.com/locations"
+    session = requests.Session()
+    
+    # //*[@id="content"]/section/div/div/div[2]
+    request = session.get(url)
+ 
+ 
+    response = etree.HTML(request.text)
+    store_list = response.xpath('.//div[@class="medium-12 columns"]//a[@class="button"]/@href')
+    for link in store_list:
+        link =   link
+        store = etree.HTML(session.get(link).text)
+        output = []
+        output.append(base_url) # url
+        # print(store.xpath('.//h1//text()')[-1])
+        if store.xpath('.//h1//text()') != []:
+            output.append(get_value(store.xpath('.//h1//text()'))) #location name
+            
+
+            # detail = eliminate_space()
+            detail = list(map(lambda s: s.strip(), store.xpath('.//div[@class="medium-6 columns"]')[0].xpath('.//text()') ))
+            
+            output.append(detail[2]) #address
+            address = detail[3].strip().split(',')
+            
+            output.append(address[0]) #city
+            output.append(address[1].strip().split(' ')[0]) #state
+            output.append(address[1].strip().split(' ')[1]) #zipcode
+            output.append('US') #country code
+            output.append("<MISSING>") #store_number
+            h_temp = ''
+            
+            phone = ''
+            for idx, hour in enumerate(detail[5:]):
+                if 'phone' in hour.lower():
+                    phone = detail[idx+6]
+                    break
+                h_temp += hour + ' '
+            # print(store.xpath('.//div[@class="medium-6 columns"]//a[1]/@href'))
+            latitude = store.xpath('.//div[@class="medium-6 columns"]//a/@href')[0].split("=")[-1].split(",")[0]
+            longitude =store.xpath('.//div[@class="medium-6 columns"]//a/@href')[0].split("=")[-1].split(",")[-1]
+            page_url = store.xpath('.//div[@class="medium-6 columns"]//a/@href')[1]
+            output.append(phone if phone else "<MISSING>") #phone
+            output.append("Winking Lizard Restaurant and Tavern | Winking Lizard") #location type
+            output.append(latitude) #latitude
+            output.append(longitude) #longitude
+            output.append(h_temp.replace("Find Directions  Order Online  Our ","").replace("or until the beer stops flowing (kitchen closes 9:30pm)",'').replace("or until the beer stops flowing (kitchen closes 10pm)",'').replace("or until the beer stops flowing (kitchen closes 9:30pm)",'').replace("or until the beer stops flowing (kitchen closes 10:00pm)",'').replace(" at this time  ",'').replace("or until the beer stops flowing (kitchen closes at 9:30pm)",'').replace("or until the beer stops flowing (kitchen closes at 9:30pm)",'').replace('Hours ','').strip() if h_temp.replace("Find Directions  Order Online  Our ","") else "<MISSING>") #opening hours
+            output.append(page_url)
+            # print(output)
+            yield output
+            
+        # except:
+        #     print(link)
+
+
+
+        # output_list.append(output)
+    #     print(output_list)
+    # return output_list
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
