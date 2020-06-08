d7b042b1311b0672fd1a4ffa4da142257d280e38
nsxdarin <nsxdarin@gmail.com>
2020-06-07 20:30:21 -0500

    Create scrape.py

---
+++ apify/nsxdarin/storelocator/arlohotels_com/scrape.py
@@ -0,0 +1,67 @@
+import csv
+import urllib2
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    locs = []
+    url = 'https://www.arlohotels.com'
+    r = session.get(url, headers=headers)
+    for line in r.iter_lines():
+        if '<li class="location">' in line:
+            items = line.split('<li class="location">')
+            for item in items:
+                if '<div class="hotel-row"><div id="footer-sidebar2"' not in item and '<a href="/' in item:
+                    stub = 'https://www.arlohotels.com/' + item.split('<a href="/')[1].split('"')[0]
+                    if 'com//' not in stub:
+                        locs.append(stub)
+    website = 'arlohotels.com'
+    typ = '<MISSING>'
+    country = 'US'
+    store = '<MISSING>'
+    hours = '<MISSING>'
+    lat = '<MISSING>'
+    lng = '<MISSING>'
+    for loc in locs:
+        print('Pulling Location %s...' % loc)
+        name = ''
+        add = ''
+        city = ''
+        state = ''
+        zc = ''
+        phone = ''
+        r2 = session.get(loc, headers=headers)
+        for line2 in r2.iter_lines():
+            if '<h3 class="address__hotel-name">' in line2:
+                name = line2.split('<h3 class="address__hotel-name">')[1].split('<')[0]
+            if '"streetAddress":"' in line2:
+                addinfo = line2.split('"streetAddress":"')[1].split('"')[0]
+                add = addinfo.split(',')[0]
+                city = addinfo.split(',')[1].strip()
+                state = addinfo.split(',')[2].strip().split(' ')[0]
+                zc = addinfo.split(',')[2].rsplit(' ',1)[1]
+            if 'target="_blank" rel="noopener noreferrer">' in line2:
+                addinfo = line2.split('target="_blank" rel="noopener noreferrer">')[1].split('</a>')[0]
+                add = addinfo.split('<')[0]
+                city = addinfo.split('<br>')[1].strip().split(',')[0]
+                state = addinfo.split('<br>')[1].strip().split(',')[1].strip().split(' ')[0]
+                zc = addinfo.rsplit(' ',1)[1]
+            if '<br><a href="tel:' in line2:
+                phone = line2.split('<br><a href="tel:')[1].split('"')[0]
+        yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
