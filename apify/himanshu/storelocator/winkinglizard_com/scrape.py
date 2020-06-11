6fefdde0adc5b97d683ae4355eb0fe97d2fdd619
diapk <Hank4957!>
2020-06-11 12:56:24 +0530

    docker run and push

--- apify/himanshu/storelocator/winkinglizard_com/scrape.py
+++ apify/himanshu/storelocator/winkinglizard_com/scrape.py
@@ -25,23 +25,17 @@ def eliminate_space(items):
         if item != '':
             rets.append(item)
     return rets
-
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         for row in data:
             writer.writerow(row)
-
 def fetch_data():
     output_list = []
     url = "https://winkinglizard.com/locations"
     session = requests.Session()
-    
-    # //*[@id="content"]/section/div/div/div[2]
     request = session.get(url)
- 
- 
     response = etree.HTML(request.text)
     store_list = response.xpath('.//div[@class="medium-12 columns"]//a[@class="button"]/@href')
     for link in store_list:
@@ -49,31 +43,26 @@ def fetch_data():
         store = etree.HTML(session.get(link).text)
         output = []
         output.append(base_url) # url
-        # print(store.xpath('.//h1//text()')[-1])
         if store.xpath('.//h1//text()') != []:
             output.append(get_value(store.xpath('.//h1//text()'))) #location name
-            
-
-            # detail = eliminate_space()
             detail = list(map(lambda s: s.strip(), store.xpath('.//div[@class="medium-6 columns"]')[0].xpath('.//text()') ))
-            
             output.append(detail[2]) #address
             address = detail[3].strip().split(',')
-            
             output.append(address[0]) #city
             output.append(address[1].strip().split(' ')[0]) #state
-            output.append(address[1].strip().split(' ')[1]) #zipcode
+            zipp = address[1].strip().split(' ')[1]
+            output.append(zipp) #zipcode
             output.append('US') #country code
             output.append("<MISSING>") #store_number
             h_temp = ''
-            
             phone = ''
             for idx, hour in enumerate(detail[5:]):
                 if 'phone' in hour.lower():
                     phone = detail[idx+6]
+                if "44122" in zipp:
+                    phone = "(216) 454-0380"
                     break
                 h_temp += hour + ' '
-            # print(store.xpath('.//div[@class="medium-6 columns"]//a[1]/@href'))
             latitude = store.xpath('.//div[@class="medium-6 columns"]//a/@href')[0].split("=")[-1].split(",")[0]
             longitude =store.xpath('.//div[@class="medium-6 columns"]//a/@href')[0].split("=")[-1].split(",")[-1]
             page_url = store.xpath('.//div[@class="medium-6 columns"]//a/@href')[1]
@@ -83,18 +72,7 @@ def fetch_data():
             output.append(longitude) #longitude
             output.append(h_temp.replace("Find Directions  Order Online  Our ","").replace("or until the beer stops flowing (kitchen closes 9:30pm)",'').replace("or until the beer stops flowing (kitchen closes 10pm)",'').replace("or until the beer stops flowing (kitchen closes 9:30pm)",'').replace("or until the beer stops flowing (kitchen closes 10:00pm)",'').replace(" at this time  ",'').replace("or until the beer stops flowing (kitchen closes at 9:30pm)",'').replace("or until the beer stops flowing (kitchen closes at 9:30pm)",'').replace('Hours ','').strip() if h_temp.replace("Find Directions  Order Online  Our ","") else "<MISSING>") #opening hours
             output.append(page_url)
-            # print(output)
             yield output
-            
-        # except:
-        #     print(link)
-
-
-
-        # output_list.append(output)
-    #     print(output_list)
-    # return output_list
-
 def scrape():
     data = fetch_data()
     write_output(data)
