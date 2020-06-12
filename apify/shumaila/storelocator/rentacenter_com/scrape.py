e743adef2f782e477d8a4ca08b0f253674219206
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-06-12 18:22:43 +0500

    Add files via upload

--- apify/shumaila/storelocator/rentacenter_com/scrape.py
+++ apify/shumaila/storelocator/rentacenter_com/scrape.py
@@ -20,22 +20,24 @@ def write_output(data):
 def fetch_data():
     # Your scraper here
     data = []
-    p = 1
-    url = 'https://locations.rentacenter.com/'
+    p = 0
+    url = 'https://locations.rentacenter.com/#contains-place-toggle'
+    
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
-    mainul = soup.find('ul',{'class':'list-group'})
+    mainul = soup.find('div',{'id':'contains-place'})
     statelinks = mainul.findAll('a')
+    #print(statelinks)
     for states in statelinks:
-        statelink = "https://locations.rentacenter.com" + states['href']
-       # print(statelink)
+        statelink = 'https://locations.rentacenter.com'+ states['href']+'#contains-place-toggle'
+        print(statelink)
         page1 = requests.get(statelink)
         soup1 = BeautifulSoup(page1.text, "html.parser")
-        mainul1 = soup1.find('ul', {'class': 'list-group'})
+        mainul1 = soup1.find('div', {'id': 'contains-place'})
         citylinks = mainul1.findAll('a')
         for cities in citylinks:
-            citylink =  "https://locations.rentacenter.com" + cities['href']
-            #print(citylink)
+            citylink = 'https://locations.rentacenter.com'+ cities['href']
+            print(citylink)
             page2 = requests.get(citylink)
             soup2 = BeautifulSoup(page2.text, "html.parser")
             mainul2 = soup2.find('ul', {'class': 'list-group'})
@@ -47,68 +49,25 @@ def fetch_data():
                     page3 = requests.get(branchlink)
                     soup3 = BeautifulSoup(page3.text, "html.parser")
                     data.append(extract(soup3,branchlink))
+                    print(p,data[p])
+                    p += 1
 
             else:
                 data.append(extract(soup2,citylink))
+                #print(p,data[p])
+                p += 1
+            
+            
     return data
 
 def extract(soup,link):
     try:
-        hourd = soup.find('dl', {'class': 'list-hours'}).text
+        hourd = soup.find('div', {'class': 'hours'}).find('dd').text
         hourd = hourd.replace("\n", " ")
         hourd = hourd.strip()
     except:
         hourd = "<MISSING>"
-    script = soup.find("script",{'type':'application/ld+json'})
-    script = str(script)
-    start = script.find('"addressCountry"')
-    start = script.find(":", start) + 1
-    end = script.find(",", start)
-    ccode = script[start:end]
-    ccode = ccode.replace('"',"")
-    start = script.find('"addressLocality"')
-    start = script.find(":", start) + 1
-    end = script.find(",", start)
-    city = script[start:end]
-    city = city.replace('"', "")
-    start = script.find('"addressRegion"')
-    start = script.find(":", start) + 1
-    end = script.find(",", start)
-    state = script[start:end]
-    state = state.replace('"', "")
-    start = script.find('"postalCode"')
-    start = script.find(":", start) + 1
-    end = script.find(",", start)
-    pcode = script[start:end]
-    pcode = pcode.replace('"', "")
-    start = script.find('"streetAddress"')
-    start = script.find(":", start) + 1
-    end = script.find("}", start)
-    street = script[start:end]
-    street = street.replace('"', "")
-    start = script.find('"branchCode"')
-    start = script.find(":", start) + 1
-    end = script.find(",", start)
-    store = script[start:end]
-    store= store.replace('"', "")
-    start = script.find('"latitude"')
-    start = script.find(":", start) + 1
-    end = script.find(",", start)
-    lat = script[start:end]
-    lat = lat.replace('"', "")
-    start = script.find('"longitude"')
-    start = script.find(":", start) + 1
-    end = script.find("}", start)
-    longt= script[start:end]
-    longt = longt.replace('"', "")
-    start = script.find('"telephone"')
-    if start != -1:
-        start = script.find(":", start) + 1
-        end = script.find(",", start)
-        phone = script[start:end]
-        phone = phone.replace('"', "")
-    else:
-        phone = "<MISSING>"
+    
     title = soup.find('title').text
     start = title.find(",")
     if start != -1:
@@ -117,8 +76,52 @@ def extract(soup,link):
     if start != -1:
         title = title[0:start]
 
-    data = ['https://www.rentacenter.com/', link, title, street, city, state, pcode, ccode, store, phone, '<MISSING>', lat,longt, hourd]
+    try:
+        street = soup.find('div',{'class':'street'}).text.replace('\n','').lstrip().rstrip()
+    except:
+        street = '<MISSING>'
+    try:
+        det = soup.find('div',{'class':'locality'}).text.replace('\n',' ').lstrip()
+        
+        city ,state= det.split(', ')
+        
+        state, pcode = state.lstrip().split(' ',1)
+        pcode = pcode.lstrip().rstrip()
+        state  = state.lstrip().rstrip()
+       
+       
+    except:
+        state= '<MISSING>'
+        city='<MISSING>'
+        pcode ='<MISSING>'
+        
+    try:
+        phone = soup.find('a',{'class':'list-location-phone-number'}).text
+    except:
+        phone = '<MISSING>'
+
+    try:
+        coord = soup.find('a',{'class':'list-location-secondary-anchor'})['href']
+        start = coord.find('+')
+        start = coord.find('/',start)+1
+        end = coord.find(',',start)
+        lat = coord[start:end]
+        start = end +1
+        end = len(coord)
+        longt = coord[start:end]
+    except:
+        lat = '<MISSING>'
+        longt = '<MISSING>'
+    try:    
+        store = soup.find('div',{'id':'location-list'})['data-currentlocation']
+        print(store)
+    except:
+        store ='<MISSING>'
+        
+
+    data = ['https://www.rentacenter.com/', link, title, street, city, state, pcode,'US', store, phone, '<MISSING>', lat ,longt, hourd]
     #print(data)
+    #input()
     return data
 
 
