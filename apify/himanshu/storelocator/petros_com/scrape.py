5171d8f50a90b54584131afe357b2faba7fea03e
diapk <Hank4957!>
2020-06-15 16:35:39 +0530

    docker run and push

--- apify/himanshu/storelocator/petros_com/scrape.py
+++ apify/himanshu/storelocator/petros_com/scrape.py
@@ -3,8 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import json
-
-
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -15,12 +13,11 @@ def write_output(data):
         # Body
         for row in data:
             writer.writerow(row)
-
-
 def fetch_data():
     headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
     }
+    adressessess=[]
     base_url= "https://www.petros.com/locations/"
     r = requests.get(base_url,headers=headers)
     soup= BeautifulSoup(r.text,"lxml")
@@ -28,13 +25,10 @@ def fetch_data():
     # soup1= BeautifulSoup(,"lxml")
     op = str(soup).split('<div class=""><div class="one_third">')[1].split('<div class="one_third last_column">')[0].replace("</div>",'')
     soup1= BeautifulSoup(op,"lxml")
-    # print(soup1)
-
-
+    new_array =[]
     store_name=[]
     store_detail=[]
     return_main_object=[]
-    
     k = soup.find("div",{"dir":"ltr"})
     # for i in k:
     p = k.find_all("p")
@@ -48,19 +42,17 @@ def fetch_data():
                 del full[0]
             name = full[0]
             street_address = full[1]
-            
             phone = list(p1.stripped_strings)[-1]
             city = full[2].split(',')[0]
             state = full[2].split(',')[1].split( )[0]
             zipcode = full[2].split(',')[1].split( )[1]
             full.insert(0,"Hours")
             hours = " ".join(" ".join(full[4:-1]).split("Hours:")[1:]).strip().replace("*Note: Dining room closes one hour earlier than close time",'')
-            
             tem_var.append("https://www.petros.com/")
             tem_var.append(name)
             if "2 Market Square" in street_address:
                 hours ="Sundays-Thursdays: 11 AM – 6 PM Fridays & Saturdays: 11 AM – 8 PM"
-            tem_var.append(street_address)
+            tem_var.append(street_address.replace("Love�s Travel Center",""))
             tem_var.append(city)
             tem_var.append(state.strip())
             tem_var.append(zipcode.strip())
@@ -72,12 +64,9 @@ def fetch_data():
             tem_var.append("<MISSING>")
             tem_var.append(hours if hours else "<MISSING>")
             tem_var.append("<MISSING>")
-            # print(tem_var)
-            yield tem_var
-
+            new_array.append(tem_var)
     k = soup.find("div",{"class":"one_third last_column"})
     p = k.find_all("p")
-
     for p1 in p:
         tem_var =[]
         if "SOUTHERN PINES (Coming Spring 2020)" in list(p1.stripped_strings) or "10725 S US Hwy 15 501 North" in list(p1.stripped_strings) :
@@ -87,15 +76,14 @@ def fetch_data():
             if len(full) != 0:
                 hours = " ".join(full[-3:-1]).replace("Unit A Southern Pines, NC 28387",'<MISSING>')
                 name = full[0]
-                street_address = " ".join(full[1:3]).replace('Love’s Travel Center ','').replace('#236 ','')
+                street_address = " ".join(full[1:3])
                 phone = list(p1.stripped_strings)[-1]
                 city = full[-4].split(',')[0]
                 state = full[-4].split(',')[1].split( )[0]
                 zipcode = full[-4].split(',')[1].split( )[1]
-                
                 tem_var.append("https://www.petros.com/")
                 tem_var.append(name)
-                tem_var.append(street_address)
+                tem_var.append(street_address.replace("Love’s Travel Center ","").replace('#236',''))
                 tem_var.append(city)
                 tem_var.append(state.strip())
                 tem_var.append(zipcode.strip())
@@ -107,11 +95,7 @@ def fetch_data():
                 tem_var.append("<MISSING>")
                 tem_var.append(hours if hours else "<MISSING>")
                 tem_var.append("<MISSING>")
-
-                yield tem_var
-            
-                # print(tem_var)
-            
+                new_array.append(tem_var)       
     p = soup1.find_all("p")
     for p1 in p:
         tem_var =[]
@@ -119,14 +103,12 @@ def fetch_data():
             pass
         else:
             full =list(p1.stripped_strings)
-            
             if full[0]=="TEMPORARILY CLOSED DUE TO COVID:":
                 del full[0]
             street_address=''
             if full[1][0].isdigit():
                 name = full[0]
                 street_address = full[1]
-                
                 # replace("Phone number coming soon","<MISSING>")
                 phone = list(p1.stripped_strings)[-1]
                 city = full[2].split(',')[0]
@@ -153,7 +135,7 @@ def fetch_data():
                 hours ="Mondays – Saturdays: 10 AM – 9 PM Sundays: 11 AM – 9 PM"
             tem_var.append("https://www.petros.com/")
             tem_var.append(name)
-            tem_var.append(street_address)
+            tem_var.append(street_address.replace("Love�s Travel Center",""))
             tem_var.append(city)
             tem_var.append(state.strip())
             tem_var.append(zipcode.strip())
@@ -165,16 +147,15 @@ def fetch_data():
             tem_var.append("<MISSING>")
             tem_var.append(hours if hours else "<MISSING>")
             tem_var.append("<MISSING>")
-            yield tem_var
-
-   
-            
-
+            new_array.append(tem_var)
+    for q in range(len(new_array)):
+        if new_array[q][2] in adressessess :
+            continue
+        adressessess.append(new_array[q][2])
+        yield new_array[q] 
 def scrape():
     data = fetch_data()
     write_output(data)
-
-
 scrape()
 
 
