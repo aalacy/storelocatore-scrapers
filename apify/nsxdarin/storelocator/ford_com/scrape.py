691c3d293af4a1da6f4ef6c3cda5d3ddfd69611c
nsxdarin <nsxdarin@gmail.com>
2020-06-16 10:33:10 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/ford_com/scrape.py
+++ apify/nsxdarin/storelocator/ford_com/scrape.py
@@ -20,48 +20,54 @@ def fetch_data():
     for code in sgzip.for_radius(100):
         print('Pulling Zip Code %s...' % code)
         url = 'https://www.ford.com/services/dealer/Dealers.json?make=Ford&radius=500&filter=&minDealers=1&maxDealers=100&postalCode=' + code
-        r = session.get(url, headers=headers)
-        if '"Dealer":[{' in r.content:
-            for item in json.loads(r.content)['Response']['Dealer']:
-                lng = item['Longitude']
-                lat = item['Latitude']
-                name = item['Name']
-                typ = item['dealerType']
-                website = 'ford.com'
-                purl = item['URL']
-                hours = ''
-                add = item['Address']['Street1'] + ' ' + item['Address']['Street2'] + ' ' + item['Address']['Street3']
-                add = add.strip()
-                city = item['Address']['City']
-                state = item['Address']['State']
-                country = item['Address']['Country'][:2]
-                zc = item['Address']['PostalCode']
-                store = item['SalesCode']
-                phone = item['Phone']
-                daytext = str(item['SalesHours'])
-                daytext = daytext.replace("'",'"')
-                daytext = daytext.replace('u"','"').replace(' {','{')
-                days = daytext.split(',{')
-                for day in days:
-                    if '"name": "' in day:
-                        dname = day.split('"name": "')[1].split('"')[0]
-                        if '"closed": "true"' in day:
-                            hrs = 'Closed'
-                        else:
-                            hrs = day.split('"open": "')[1].split('"')[0] + '-' + day.split('"close": "')[1].split('"')[0]
-                        if hours == '':
-                            hours = dname + ': ' + hrs
-                        else:
-                            hours = hours + '; ' + dname + ': ' + hrs
-                if store not in ids:
-                    ids.append(store)
-                    if hours == '':
-                        hours = '<MISSING>'
-                    if purl == '':
-                        purl = '<MISSING>'
-                    if phone == '':
-                        phone = '<MISSING>'
-                    yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+        LFound = True
+        while LFound:
+            try:
+                LFound = False
+                r = session.get(url, headers=headers, timeout=10)
+                if '"Dealer":[{' in r.content:
+                    for item in json.loads(r.content)['Response']['Dealer']:
+                        lng = item['Longitude']
+                        lat = item['Latitude']
+                        name = item['Name']
+                        typ = item['dealerType']
+                        website = 'ford.com'
+                        purl = item['URL']
+                        hours = ''
+                        add = item['Address']['Street1'] + ' ' + item['Address']['Street2'] + ' ' + item['Address']['Street3']
+                        add = add.strip()
+                        city = item['Address']['City']
+                        state = item['Address']['State']
+                        country = item['Address']['Country'][:2]
+                        zc = item['Address']['PostalCode']
+                        store = item['SalesCode']
+                        phone = item['Phone']
+                        daytext = str(item['SalesHours'])
+                        daytext = daytext.replace("'",'"')
+                        daytext = daytext.replace('u"','"').replace(' {','{')
+                        days = daytext.split(',{')
+                        for day in days:
+                            if '"name": "' in day:
+                                dname = day.split('"name": "')[1].split('"')[0]
+                                if '"closed": "true"' in day:
+                                    hrs = 'Closed'
+                                else:
+                                    hrs = day.split('"open": "')[1].split('"')[0] + '-' + day.split('"close": "')[1].split('"')[0]
+                                if hours == '':
+                                    hours = dname + ': ' + hrs
+                                else:
+                                    hours = hours + '; ' + dname + ': ' + hrs
+                        if store not in ids:
+                            ids.append(store)
+                            if hours == '':
+                                hours = '<MISSING>'
+                            if purl == '':
+                                purl = '<MISSING>'
+                            if phone == '':
+                                phone = '<MISSING>'
+                            yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+            except:
+                LFound = True
 
 def scrape():
     data = fetch_data()
