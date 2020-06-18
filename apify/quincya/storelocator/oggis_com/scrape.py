b731b6378158b734d8e284c62f92a1cda92a1f7c
Quincy <qcalexander@gmail.com>
2020-06-18 05:31:50 -0400

    oggis_com fixed

--- apify/quincya/storelocator/oggis_com/scrape.py
+++ apify/quincya/storelocator/oggis_com/scrape.py
@@ -1,4 +1,4 @@
-import requests
+from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import csv
 
@@ -7,7 +7,7 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -16,17 +16,17 @@ def fetch_data():
     base_link = "https://oggis.com/locations/"
 
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
-    headers = {'User-Agent' : user_agent}
+    HEADERS = {'User-Agent' : user_agent}
 
-
-    req = requests.get(base_link, headers=headers)
+    session = SgRequests()
+    req = session.get(base_link, headers = HEADERS)
 
     try:
         base = BeautifulSoup(req.text,"lxml")
-        print "Got today page"
+        print("Got today page")
     except (BaseException):
-        print '[!] Error Occured. '
-        print '[?] Check whether system is Online.'
+        print('[!] Error Occured. ')
+        print('[?] Check whether system is Online.')
 
     items = base.findAll('div', attrs={'class': 'post-entry post-entry-type-page post-entry-66'})
     items.pop(0)
@@ -61,8 +61,9 @@ def fetch_data():
         location_type = "<MISSING>"
         links = item.find('div', attrs={'class': 'result_links'})
         link = links.findAll('a')[0]['href']
-
-        if no_zip or location_name == "Del Mar":
+        
+        if location_name == "Del Mar":
+            # Set as missing since it is duplicated
             latitude = "<MISSING>"
             longitude = "<MISSING>"
         else:
@@ -73,23 +74,38 @@ def fetch_data():
             if latitude == "":
                 latitude = directions[directions.find("@")+1:comma_point].strip()
                 longitude = directions[directions.find(",")+1:directions.rfind(",",comma_point)].strip()
+                if longitude:
+                    if not latitude:
+                        comma_point = longitude.rfind(",")
+                        latitude = longitude[longitude.find("@")+1:comma_point].strip()
+                        longitude = longitude[comma_point+1:].strip()
         
         phone = item.find('div', attrs={'class': 'result_address'})
         phone = phone.find('a').text
 
-        req = requests.get(link, headers=headers)
+        hours_of_operation = ""
+        check_hours = item.find_all(class_="av_textblock_section")[-1].text
+        if "Operating Hours" in check_hours:
+            hours_of_operation = check_hours.replace("\n"," ").replace("\r","").replace("*","").replace("<strong>","").replace("</strong>","").replace("<br>"," ").strip()
+        else:
+            req = session.get(link, headers = HEADERS)
 
-        try:
-            new_base = BeautifulSoup(req.text,"lxml")
-            print "Got store details page"
-        except (BaseException):
-            print '[!] Error Occured. '
-            print '[?] Check whether system is Online.'
+            try:
+                new_base = BeautifulSoup(req.text,"lxml")
+                print("Got store details page")
+            except (BaseException):
+                print('[!] Error Occured. ')
+                print('[?] Check whether system is Online.')
 
-        section = new_base.findAll('section', attrs={'class': 'av_textblock_section'})[1]
-        hours_of_operation = section.find('div', attrs={'id': 'lbmb_hours'}).text.replace("\n"," ").replace("\r","").replace("*","").replace("<strong>","").replace("</strong>","").replace("<br>"," ").encode('utf-8').strip()
+            try:
+                section = new_base.findAll('section', attrs={'class': 'av_textblock_section'})[1]
+                hours_of_operation = section.find('div', attrs={'id': 'lbmb_hours'}).text.replace("\n"," ").replace("\r","").replace("*","").replace("<strong>","").replace("</strong>","").replace("<br>"," ").encode('utf-8').strip()
+            except:
+                pass
+        if not hours_of_operation:
+            hours_of_operation = "<MISSING>"
 
-        data.append([locator_domain, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+        data.append([locator_domain, base_link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
 
     return data
 
