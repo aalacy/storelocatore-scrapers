51567411136d8267b253689f49d9e8158013ff97
Kevin Collins <nck@users.noreply.github.com>
2020-06-18 17:48:49 -0400

    seat.co.uk (SLC-2477): fix dns host name issues

--- apify/himanshu/storelocator/seat_co_uk/scrape.py
+++ apify/himanshu/storelocator/seat_co_uk/scrape.py
@@ -66,7 +66,16 @@ def fetch_data():
                 headers = {
                     'Origin': str(page_url),
                 }
-                data = session.get("https://api.lakeside.seat-store.co.uk/api/v1/config", headers=headers).json()['content']['translations']['en_GB']['departments']['store']
+                # data = session.get("https://api.lakeside.seat-store.co.uk/api/v1/config", headers=headers).json()['content']['translations']['en_GB']['departments']['store']
+                # **************************************************************************************************
+                # 2020-06-18: When running in Docker, the host name api.lakeside.seat-store.co.uk doesn't resolve. 
+                #   Perhaps related to it pointing to a wildcard subdomain *.seat-prod.motortrak.com.
+                #   But it works if we change it to api.lakeside.seat-prod.motortrak.com !
+                #   Same for the below section that was accessing api.whitecity.seat-store.co.uk ... 
+                #   Had to changer that to api.whitecity.seat-prod.motortrak.com
+                # **************************************************************************************************
+                data = session.get("https://api.lakeside.seat-prod.motortrak.com/api/v1/config", verify=False, 
+                                   headers=headers).json()['content']['translations']['en_GB']['departments']['store']
                 hours = ''
                 for key,value in data['opening'].items():
                     hours+= " "+key.capitalize() +" "+ value['open']+" - "+value['close']+" "
@@ -78,7 +87,9 @@ def fetch_data():
                 headers = {
                     'Origin': str(page_url),
                 }
-                data = session.get("https://api.whitecity.seat-store.co.uk/api/v1/config", headers=headers).json()['content']['translations']['en_GB']['departments']['store']
+                # data = session.get("https://api.whitecity.seat-store.co.uk/api/v1/config", headers=headers).json()['content']['translations']['en_GB']['departments']['store']
+                data = session.get("https://api.whitecity.seat-prod.motortrak.com/api/v1/config", verify=False,
+                                   headers=headers).json()['content']['translations']['en_GB']['departments']['store']
                 hours = ''
                 for key,value in data['opening'].items():
                     hours+= " "+key.capitalize() +" "+ value['open']+" - "+value['close']+" "
