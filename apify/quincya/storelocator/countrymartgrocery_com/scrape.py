5911ec46f6ecfacaa2a0ffd5f4fe8f344a0b3cc9
Quincy <qcalexander@gmail.com>
2020-06-18 06:54:22 -0400

    countrymartgrocery_com fixed

--- apify/quincya/storelocator/countrymartgrocery_com/scrape.py
+++ apify/quincya/storelocator/countrymartgrocery_com/scrape.py
@@ -3,6 +3,12 @@ from bs4 import BeautifulSoup
 import csv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+
 import time
 import re
 
@@ -12,7 +18,6 @@ def get_driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    #return webdriver.Chrome(executable_path='driver/chromedriver', chrome_options=options)
     return webdriver.Chrome('chromedriver', chrome_options=options)
 
 def write_output(data):
@@ -20,90 +25,86 @@ def write_output(data):
 		writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
 		# Header
-		writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+		writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
 		# Body
 		for row in data:
 			writer.writerow(row)
 
 def fetch_data():
 	
-	base_link = "http://www.countrymartgrocery.com/stores.php?view=all"
-
-	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
-	headers = {'User-Agent' : user_agent}
-
-	req = requests.get(base_link, headers=headers)
-
-	try:
-		base = BeautifulSoup(req.text,"lxml")
-	except (BaseException):
-		print ('[!] Error Occured. ')
-		print ('[?] Check whether system is Online.')
-
-	section = base.findAll('table')[-2]
-	items = section.findAll('td')
+	base_link = "https://www.countrymartgrocery.com/my-store/store-locator"
 
 	driver = get_driver()
+	time.sleep(2)
+
+	driver.get(base_link)
+	time.sleep(2)
+
+	wait = WebDriverWait(driver, 20).until(EC.presence_of_element_located(
+                (By.CLASS_NAME, "fp-panel-item-wrapper")))
+
 	data = []
 
-	for item in items:
-		
-		locator_domain = "countrymartgrocery.com"
-		try:
-			link = "http://www.countrymartgrocery.com/" + item.find('a')['href']
-			zip_code = item.find('a').text
-			zip_code = zip_code[zip_code.rfind(",")+1:zip_code.rfind("|")-1].strip()
-		except:
-			continue
-		try:
-			location_type = item.find('span').text.strip()
-			if location_type == "":
-				location_type = "<MISSING>"
-		except:
-			location_type = "<MISSING>"
+	base = BeautifulSoup(driver.page_source,"lxml")
+	items = base.find_all(class_="fp-panel-item-wrapper")
 
-		req = requests.get(link, headers=headers)
+	locator_domain = "countrymartgrocery.com"
 
-		try:
-			new_base = BeautifulSoup(req.text,"lxml")
-		except (BaseException):
-			print ('[!] Error Occured. ')
-			print ('[?] Check whether system is Online.')
-		
-		table = new_base.find('table')
+	for i in range(len(items)):
+		item = items[i]
+		location_name = item.find(class_='fp-link-secondary').text.strip()
+		print(location_name)
 
-		raw_data = new_base.findAll('td')[1]
-		raw_data = raw_data.findAll('div')[1].text.strip()
-		
-		street_address = raw_data[:raw_data.find(',')].strip()
-		city = raw_data[raw_data.find(',')+1:raw_data.rfind(',')].strip()
-		state = raw_data[raw_data.rfind(',')+1:].strip()
-		location_name = new_base.find('u').text.strip() + " " + city.upper()
-		print (location_name)
+		location_type = "<MISSING>"
+
+		raw_data = str(item.find(class_='fp-store-address')).replace("</div>","")
+		raw_data = raw_data[raw_data.find(">")+2:].strip().split("<br/>")
+		street_address = raw_data[0].strip()
+		if len(raw_data) > 2:
+			street_address = street_address + " " + raw_data[1].strip()
+		city = raw_data[-1][:raw_data[-1].find(',')].strip()
+		state = raw_data[-1][raw_data[-1].find(',')+1:raw_data[-1].rfind(' ')].strip()
+		zip_code = raw_data[-1][raw_data[-1].rfind(' '):].strip()
 		country_code = "US"
-		store_number = link[link.rfind("=")+1:]
+		store_number = item['data-store-number']
 		
 		try:
 			phone = re.findall("[[(\d)]{5} [\d]{3}-[\d]{4}", str(item))[0]
 		except:
 			phone = "<MISSING>"
 
-		driver.get(link)
+		directions = driver.find_elements_by_css_selector(".fp-link-secondary.fp-link-directions")[i]
+		# Open in new tab
+		directions.send_keys(Keys.CONTROL + Keys.ENTER)
 		time.sleep(2)
+		new_tab = driver.window_handles[1]
+		driver.switch_to.window(new_tab)
+		time.sleep(5)
 
 		try:
-			maps = driver.find_element_by_class_name('gm-style')
-			raw_gps = maps.find_element_by_tag_name('a').get_attribute('href')
-			latitude = raw_gps[raw_gps.find("=")+1:raw_gps.find(",")].strip()
-			longitude = raw_gps[raw_gps.find(",")+1:raw_gps.find("&")].strip()
+			map_link = driver.current_url
+			at_pos = map_link.rfind("@")
+			latitude = map_link[at_pos+1:map_link.find(",", at_pos)].strip()
+			longitude = map_link[map_link.find(",", at_pos)+1:map_link.find(",", at_pos+15)].strip()
 		except:
 			latitude = "<INACCESSIBLE>"
 			longitude = "<INACCESSIBLE>"
 
-		hours = new_base.find('td', attrs={'class': 'store_detail_box'}).get_text(separator=u' ').replace("\n"," ").replace("\x96","").replace("  "," ").strip()
-		hours_of_operation = re.sub(' +', ' ', hours)
+		driver.close()
+		driver.switch_to.window(driver.window_handles[0])
 
-		data.append([locator_domain, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+		try:
+			hours = item.find(class_='fp-panel-store-hours').get_text(separator=u' ').replace("\n"," ").replace("\x96","").replace("  "," ").strip()
+			hours_of_operation = re.sub(' +', ' ', hours)
+		except:
+			hours_of_operation = "<MISSING>"
+
+		data.append([locator_domain, base_link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+
+	try:
+		driver.close()
+	except:
+		pass
 
 	return data
 
