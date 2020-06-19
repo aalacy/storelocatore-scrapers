78a574411348db3932c48527eae6d027615dc5e7
nsxdarin <nsxdarin@gmail.com>
2020-06-19 09:48:43 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/hilton_com/scrape.py
+++ apify/nsxdarin/storelocator/hilton_com/scrape.py
@@ -21,6 +21,7 @@ def fetch_data():
         if '<loc>https://www3.hilton.com/en/hotels/' in line and 'accommodations/index.html' in line:
             lurl = line.split('<loc>')[1].split('<')[0]
             locs.append(lurl)
+    print(len(locs))
     for loc in locs:
         print('Pulling Location %s...' % loc)
         website = 'hilton.com'
@@ -31,7 +32,7 @@ def fetch_data():
         state = ''
         zc = ''
         country = ''
-        store = ''
+        store = '<MISSING>'
         phone = ''
         lat = ''
         lng = ''
@@ -39,27 +40,28 @@ def fetch_data():
         for line2 in r2.iter_lines():
             if '<meta name="og:title" content="' in line2:
                 name = line2.split('<meta name="og:title" content="')[1].split('"')[0]
-            if '<meta name="geo.position" content="' in line2:
-                lat = line2.split('<meta name="geo.position" content="')[1].split(';')[0]
-                lng = line2.split('<meta name="geo.position" content="')[1].split(';')[1].split('"')[0]
+            if '"latitude": "' in line2:
+                lat = line2.split('"latitude": "')[1].split('"')[0]
+            if '"longitude": "' in line2:
+                lng = line2.split('"longitude": "')[1].split('"')[0]
             if '"productID":"' in line2:
                 store = line2.split('"productID":"')[1].split('"')[0]
-            if '"streetAddress":' in line2:
-                add = line2.split("'")[1].strip()
-            if '"addressLocality":' in line2:
-                city = line2.split("'")[1].strip()
-            if '"addressRegion":' in line2:
-                state = line2.split("'")[1].strip()
-            if '"addressCountry":' in line2:
-                country = line2.split("'")[1].strip()
+            if '"streetAddress": "' in line2:
+                add = line2.split('"streetAddress": "')[1].split('"')[0]
+            if '"addressLocality": "' in line2:
+                city = line2.split('"addressLocality": "')[1].split('"')[0]
+            if '"addressRegion": "' in line2:
+                state = line2.split('"addressRegion": "')[1].split('"')[0]
+            if '"addressCountry": "' in line2:
+                country = line2.split('"addressCountry": "')[1].split('"')[0]
                 if country == 'USA':
                     country = 'US'
                 if country == 'Canada':
                     country = 'CA'
-            if '"postalCode":' in line2:
-                zc = line2.split("'")[1].strip()
-            if '"telephone":' in line2:
-                phone = line2.split("'")[1].strip()
+            if '"postalCode": "' in line2:
+                zc = line2.split('"postalCode": "')[1].split('"')[0]
+            if '"telephone": "' in line2:
+                phone = line2.split('"telephone": "')[1].split('"')[0]
         hours = '<MISSING>'
         if country == 'US' or country == 'CA':
             yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
