5884c0794b9f88a523fb467529e56912022d8c44
Quincy <qcalexander@gmail.com>
2020-06-19 06:48:22 -0400

    Updated to chromedriver 83

--- apify/quincya/storelocator/healthyback_com/scrape.py
+++ apify/quincya/storelocator/healthyback_com/scrape.py
@@ -7,12 +7,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def get_driver():
     options = Options() 
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    #return webdriver.Chrome(executable_path='driver/chromedriver', chrome_options=options)
+    options.add_argument('--window-size=1920,1080')
     return webdriver.Chrome('chromedriver', chrome_options=options)
 
 
--- apify/quincya/storelocator/selectphysicaltherapy_com/scrape.py
+++ apify/quincya/storelocator/selectphysicaltherapy_com/scrape.py
@@ -13,6 +13,7 @@ def get_driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
     return webdriver.Chrome('chromedriver', chrome_options=options)
 
 def write_output(data):
