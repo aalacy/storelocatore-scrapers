3aebee7bfa537fc33dddaaf6484f8a23c546dee7
Quincy <qcalexander@gmail.com>
2020-06-20 15:12:20 -0400

    gqtmovies_com first commit

---
+++ apify/quincya/storelocator/gqtmovies_com/scrape.py
@@ -0,0 +1,76 @@
+from sgrequests import SgRequests
+from bs4 import BeautifulSoup
+import csv
+import time
+from random import randint
+
+def write_output(data):
+	with open('data.csv', mode='w') as output_file:
+		writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+		# Header
+		writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+		# Body
+		for row in data:
+			writer.writerow(row)
+
+def fetch_data():
+	
+	base_link = "https://www.gqtmovies.com/florida/riverview-14-gdx/theater-info"
+
+	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+	HEADERS = {'User-Agent' : user_agent}
+
+	session = SgRequests()
+	req = session.get(base_link, headers = HEADERS)
+	time.sleep(randint(1,2))
+	try:
+		base = BeautifulSoup(req.text,"lxml")
+		print("Got today page")
+	except (BaseException):
+		print('[!] Error Occured. ')
+		print('[?] Check whether system is Online.')
+
+	pages = base.find(class_="select-0").find_all("option")[1:]
+	
+	data = []
+	for page in pages:
+		locator_domain = "gqtmovies.com"
+
+		link = "https://www.gqtmovies.com" + page['value']
+		req = session.get(link, headers = HEADERS)
+		time.sleep(randint(1,2))
+		try:
+			item = BeautifulSoup(req.text,"lxml")
+			print(link)
+		except (BaseException):
+			print('[!] Error Occured. ')
+			print('[?] Check whether system is Online.')
+
+		location_name = page.text.strip()
+		raw_address = item.find(class_="cinAdress").text.split("\n\t") 
+		street_address = raw_address[0].replace(location_name,"").strip()
+		city = raw_address[-3].replace(",","").strip()
+		state = raw_address[-2].replace(",","").strip()
+		zip_code = raw_address[-1].replace(",","").strip()
+		country_code = "US"
+		store_number = "<MISSING>"
+		location_type = "<MISSING>"
+		try:
+			phone = item.find(class_="cinTel").text.replace("Office Phone: ","").strip()
+		except:
+			phone = "<MISSING>"
+		map_link = item.find("a", string="GET DIRECTIONS")["href"]
+		latitude = map_link[map_link.rfind("=")+1:map_link.rfind(",")].strip()
+		longitude = map_link[map_link.rfind(",")+1:].strip()
+		hours_of_operation = "<MISSING>"
+
+		data.append([locator_domain, link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+
+	return data
+
+def scrape():
+	data = fetch_data()
+	write_output(data)
+
+scrape()
