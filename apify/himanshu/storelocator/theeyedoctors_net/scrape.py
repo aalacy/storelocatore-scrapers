fa7fa3bb7b067aeabaca3aa664f7d10144f9b42b
diapk <Hank4957!>
2020-06-22 14:40:05 +0530

    docker run and push

--- apify/himanshu/storelocator/theeyedoctors_net/scrape.py
+++ apify/himanshu/storelocator/theeyedoctors_net/scrape.py
@@ -3,7 +3,6 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
-import sgzip
 
 
 session = SgRequests()
@@ -13,47 +12,73 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
 
 def fetch_data():
-    return_main_object = []
-    addresses = []
-    cords = sgzip.coords_for_radius(50)
-    for cord in cords:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
-        }
-        r = session.get("https://www.theeyedoctors.net/wp-json/352inc/v1/locations/coordinates?lat=" + str(cord[0]) + "&lng=" + str(cord[1]),headers=headers)
-        if r.text == "null":
-            continue
+    
+    addressess = []
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+    }
+
+    base_url = "https://www.theeyedoctors.net/"
+
+    link_soup = BeautifulSoup(session.get("https://www.theeyedoctors.net/our-eye-doctors/").text, "lxml")
+
+    for link in link_soup.find("div",{"class":"doctortabs"}).find_all("a"):
+        if "https:" in  link['href']:
+            page_url = link['href']
         else:
-            data = r.json()
-            for store_data in data:
-                store = []
-                store.append("https://www.theeyedoctors.net")
-                store.append(store_data["name"])
-                store.append(store_data["address1"] + " " + store_data['address2'] + " " + store_data["address3"])
-                if store[-1] in addresses:
-                    continue
-                addresses.append(store[-1])
-                store.append(store_data["city"])
-                store.append(store_data["state"])
-                store.append(store_data["zip_code"])
-                store.append("US")
-                store.append("<MISSING>")
-                store.append(store_data["phone_number"] if store_data["phone_number"] != "" and store_data["phone_number"] != None else "<MISSING>")
-                store.append("the eye doctors")
-                store.append(store_data["lat"])
-                store.append(store_data["lng"])
-                location_request = session.get(store_data["permalink"],headers=headers)
-                location_soup = BeautifulSoup(location_request.text,"lxml")
-                hours = " ".join(list(location_soup.find("div",{"class":"col-lg-4 times"}).stripped_strings))
-                store.append(hours if hours != "" else "<MISSING>")
-                return_main_object.append(store)
-    return return_main_object
+            page_url = "https://www.theeyedoctors.net/" + link['href']
+        
+
+        location_soup = BeautifulSoup(session.get(page_url).text, "lxml")
+        try:
+            location_name = location_soup.find("h1",{"class":"location-name"}).text.strip()
+        except:
+            continue
+        addr = list(location_soup.find("address",{"id":"prgmStoreAddress"}).stripped_strings)
+        
+        street_address = " ".join(addr[:-1]).replace(",","").strip()
+        
+        city = addr[-1].split(",")[0]
+        state = addr[-1].split(",")[1].split()[0].strip()
+        zipp =  addr[-1].split(",")[1].split()[1].strip()
+        
+        phone = location_soup.find("div",{"class":"phone-number"}).text.strip()
+        
+        lat = location_soup.find("div",{"class":"marker"})['data-lat']
+        lng = location_soup.find("div",{"class":"marker"})['data-lng']
+
+        hours = ''
+        for hr in location_soup.find("div",{"hours"}).find_all("div",{"class":"day d-flex"}):
+            hours+= " "+hr.text+" "
+
+
+        store = []
+        store.append(base_url)
+        store.append(location_name)
+        store.append(street_address)
+        store.append(city)
+        store.append(state)
+        store.append(zipp)   
+        store.append("US")
+        store.append("<MISSING>")
+        store.append(phone)
+        store.append("<MISSING>")
+        store.append(lat)
+        store.append(lng)
+        store.append(hours)
+        store.append(page_url)     
+        store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+        if store[2] in addressess:
+            continue
+        addressess.append(store[2])
+        yield store 
+        
 
 def scrape():
     data = fetch_data()
