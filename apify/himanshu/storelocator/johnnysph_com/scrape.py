4a6d7b1dcfdac8d9dc6e8fd51313805d1caaf8ce
diapk <Hank4957!>
2020-06-23 16:36:46 +0530

    docker run and push

---
+++ apify/himanshu/storelocator/johnnysph_com/scrape.py
@@ -0,0 +1,77 @@
+import csv
+import re
+import pdb
+import requests
+#from lxml import etree
+import json
+from bs4 import BeautifulSoup as bs
+
+base_url = 'https://johnnysph.com'
+
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    addressess=[]
+    url =[]
+    for q in range(0,22):
+        base_url = "https://johnnysph.com/locations/3/?post%5B0%5D=jph_store&address%5B0%5D&units=imperial&per_page="+str(q)+"&lat&lng&form=2"
+        soup = bs(requests.get(base_url).text,'lxml')
+        script = soup.find(lambda tag: (tag.name == "script") and "var gmwMapObjects" in tag.text.strip()).text.split("var gmwMapObjects =")[1].split(";\n")[0]
+        data = json.loads(script)
+        for q in data:
+            for tag in data[q]['locations']:
+                soup1 = bs(tag['info_window_content'],'lxml')
+                href = soup1.find("a")['href']
+                name  = soup1.find("a").text
+                if href not in url:
+                    url.append(href)
+                    full = soup1.find("span",{"class":"address"}).text.split(",")
+                    if "USA" in full[-1]:
+                        del full[-1]
+                    state = full[-1].strip().split( )[0]
+                    zipp = full[-1].strip().split( )[1]
+                    city = full[:-1][-1]
+                    street_address = " ".join(full[:-2])
+                    page_url = href
+                    lat = tag['lat']
+                    lng =  tag['lng']
+                    store = []
+                    store.append("https://johnnysph.com/")
+                    store.append(name.encode('ascii', 'ignore').decode('ascii').strip())
+                    store.append(street_address.encode('ascii', 'ignore').decode('ascii').strip())
+                    store.append(city.encode('ascii', 'ignore').decode('ascii').strip())
+                    store.append(state.encode('ascii', 'ignore').decode('ascii').strip())
+                    store.append(zipp)
+                    store.append("US")
+                    store.append("<MISSING>")
+                    store.append( "<MISSING>")
+                    store.append("<MISSING>")
+                    store.append(lat.strip() if lat.strip() else "<MISSING>" )
+                    store.append(lng.strip() if lng.strip() else "<MISSING>")
+                    store.append( "<MISSING>")
+                    store.append(page_url)
+                    if store[2] in addressess:
+                        continue
+                    addressess.append(store[2])
+                    # store = [x.replace("–", "-") if type(x) ==
+                    #          str else x for x in store]
+                    store = [x.encode('ascii', 'ignore').decode(
+                        'ascii').strip() if type(x) == str else x for x in store]
+                    #print("data ===" + str(store))
+                    #print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                    yield store
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
---
+++ apify/himanshu/storelocator/kuehne-nagel_com/scrape.py
@@ -0,0 +1,67 @@
+import csv
+import re
+from  sgrequests import SgRequests
+from bs4 import BeautifulSoup as BS
+import json
+session = SgRequests()
+
+base_url = 'http://kuehne-nagel.com'
+
+
+
+def write_output(data):
+    with open('data.csv', mode='w', newline='') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    
+    for url in ['https://ca.kuehne-nagel.com/en_gb/other-links/our-locations-in-canada/','https://us.kuehne-nagel.com/en_gb/top-links/usa-locations']:
+        soup = BS(session.get(url).text, "lxml")
+        
+        json_data = json.loads(json.loads(soup.find(lambda tag: (tag.name == "script") and "var inlineSettings =" in tag.text).text.split("var inlineSettings =")[1].split("if (typeof")[0].replace("};","}").strip())['locationList'])
+        for data in json_data:
+            location_name = data['locationName']
+
+            street_address = data['buildingNo'] +" "+ data['street'].replace("\r","").replace("\n","")
+
+            if data['addressLine1']:
+                street_address+= " " + data['addressLine1']
+            if data['addressLine2']:
+                street_address+= " " + data['addressLine2']
+            city = data['city']
+            state = data['stateRegion']
+            zipp = data['postalCode']
+            country_code = data['country']
+            store_number = data['uid']
+            phone = data['phoneNumber'].split("or")[0].split("x")[0]
+            location_type = data['locationType']
+            lat = data['latitude']
+            lng = data['longitude']
+            hours = data['openingHours'].replace("\r"," ").replace("\n"," ").replace("\t"," ")
+
+            store = []
+            store.append(base_url)
+            store.append(location_name)
+            store.append(street_address)
+            store.append(city)
+            store.append(state)
+            store.append(zipp)
+            store.append(country_code)
+            store.append(store_number)
+            store.append(phone)
+            store.append(location_type)
+            store.append(lat)
+            store.append(lng)
+            store.append(hours)
+            store.append(url)
+            store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+            yield store
+        
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
