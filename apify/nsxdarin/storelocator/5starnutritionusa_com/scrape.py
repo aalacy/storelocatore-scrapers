6e7a9577533e7a20d8a1ef223ca3945275b19057
nsxdarin <nsxdarin@gmail.com>
2020-06-23 13:35:30 -0500

    Create scrape.py

---
+++ apify/nsxdarin/storelocator/5starnutritionusa_com/scrape.py
@@ -0,0 +1,60 @@
+import csv
+import urllib2
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        for row in data:
+            writer.writerow(row)
+
+def fetch_data():
+    locs = []
+    url = 'https://stockist.co/api/v1/u3959/locations/all.js?callback=_stockistAllStoresCallback'
+    r = session.get(url, headers=headers)
+    website = '5starnutritionusa.com'
+    for line in r.iter_lines():
+        if '"id":' in line:
+            items = line.split('"id":')
+            for item in items:
+                if '"name":"' in item:
+                    name = item.split('"name":"')[1].split('"')[0]
+                    store = item.split(',')[0]
+                    loc = '<MISSING>'
+                    typ = '<MISSING>'
+                    lat = item.split('"latitude":"')[1].split('"')[0]
+                    lng = item.split('"longitude":"')[1].split('"')[0]
+                    city = item.split('"city":"')[1].split('"')[0]
+                    add = item.split('"address_line_1":"')[1].split('"')[0]
+                    if '"address_line_2":"' in item:
+                        add = add + ' ' + item.split('"address_line_2":"')[1].split('"')[0]
+                    try:
+                        zc = item.split('"postal_code":"')[1].split('"')[0]
+                    except:
+                        zc = '<MISSING>'
+                    country = 'US'
+                    try:
+                        state = item.split('"state":"')[1].split('"')[0]
+                    except:
+                        state = '<MISSING>'
+                    if zc == '71292':
+                        state = 'LA'
+                    phone = item.split('"phone":"')[1].split('"')[0]
+                    try:
+                        hours = item.split('"description":"')[1].split('"')[0].replace('day\\t','day: ').replace('\u2013','-').replace('\\n','; ')
+                    except:
+                        hours = '<MISSING>'
+                    if '1800 McFarland Blvd' in add:
+                        zc = '35404'
+                    yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
