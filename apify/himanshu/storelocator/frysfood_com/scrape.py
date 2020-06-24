7807ecfecd5fcb53ef5c8f31eea59e3144e0f50f
Kevin Collins <nck@users.noreply.github.com>
2020-06-24 17:30:09 -0400

    frysfood.com(SLC-1079): add attempts at resetting session / getting new IP on request exception

--- apify/himanshu/storelocator/frysfood_com/scrape.py
+++ apify/himanshu/storelocator/frysfood_com/scrape.py
@@ -1,12 +1,13 @@
 from requests.exceptions import RequestException
 import csv
 from sgrequests import SgRequests
-from bs4 import BeautifulSoup
 import re
 import sgzip
 import json
 
-session = SgRequests()
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
+
 show_logs = False
 
 
@@ -27,6 +28,98 @@ def write_output(data):
             writer.writerow(row)
 
 
+def override_retries():
+    # monkey patch sgrequests in order to set max retries ...
+    # we will control retries in this script in order to reset the session and get a new IP each time
+    import requests
+
+    def new_init(self):
+        requests.packages.urllib3.disable_warnings()
+        self.session = self.requests_retry_session(retries=0)
+    SgRequests.__init__ = new_init
+
+
+def do_search(zip_code, attempts=0):
+    global session
+    headers = {
+        'accept': 'application/json, text/plain, */*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,la;q=0.8',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json;charset=UTF-8',
+        'origin': 'https://www.frysfood.com',
+        'pragma': 'no-cache',
+        'referer': 'https://www.frysfood.com/stores/search',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',
+        'x-dtpc': '7$31030907_447h26vWSKTEEVGLSJVMPJLLDTDQKJHRRRORHMM-0',
+        'x-dtreferer': 'https://www.frysfood.com/stores/search?searchText=64433'
+    }
+
+    data = r'{"query":"\n      query storeSearch($searchText: String!, $filters: [String]!) {\n        storeSearch(searchText: $searchText, filters: $filters) {\n          stores {\n            ...storeSearchResult\n          }\n          fuel {\n            ...storeSearchResult\n          }\n          shouldShowFuelMessage\n        }\n      }\n      \n  fragment storeSearchResult on Store {\n    banner\n    vanityName\n    divisionNumber\n    storeNumber\n    phoneNumber\n    showWeeklyAd\n    showShopThisStoreAndPreferredStoreButtons\n    storeType\n    distance\n    latitude\n    longitude\n    tz\n    ungroupedFormattedHours {\n      displayName\n      displayHours\n      isToday\n    }\n    address {\n      addressLine1\n      addressLine2\n      city\n      countryCode\n      stateCode\n      zip\n    }\n    pharmacy {\n      phoneNumber\n    }\n    departments {\n      code\n    }\n    fulfillmentMethods{\n      hasPickup\n      hasDelivery\n    }\n  }\n","variables":{"searchText":"'+str(zip_code)+'","filters":[]},"operationName":"storeSearch"}'
+
+    max_attempts = 10
+    if attempts > max_attempts:
+        raise SystemExit(
+            f'Exceeded max attempts ({max_attempts}) searching zip code: {zip_code}')
+
+    try:
+        r = session.post(
+            'https://www.frysfood.com/stores/api/graphql', headers=headers, data=data)
+        r.raise_for_status()
+        return r
+    except RequestException as ex:
+        log("--- RequestException -->", ex)
+        log("--- Resetting session ...")
+        # reset the session and try again
+        session = SgRequests()
+        # check IP if logging enabled
+        if show_logs = True:
+            r = session.get('https://jsonip.com/')
+            log(f"--- New IP for session: {r.json()['ip']}")
+        search_page_init()
+        return do_search(zip_code, attempts+1)
+
+
+def search_page_init(attempts=0):
+    global session
+    log("getting initial search page")
+    max_attempts = 10
+    if attempts > max_attempts:
+        raise SystemExit(
+            f'Exceeded max attempts ({max_attempts}) getting initial search page')
+    headers = {
+        'accept': 'application/json, text/plain, */*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,la;q=0.8',
+        'cache-control': 'no-cache',
+        'pragma': 'no-cache',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',
+        'upgrade-insecure-requests': '1'
+    }
+    try:
+        r = session.get(
+            'https://www.frysfood.com/stores/search', headers=headers)
+        r.raise_for_status()
+        log(f'status code for search_page_init(): {r.status_code}')
+    except RequestException as ex:
+        log("--- RequestException -->", ex)
+        log("--- Resetting session ...")
+        # reset the session and try again
+        session = SgRequests()
+        # check IP if logging enabled
+        if show_logs = True:
+            r = session.get('https://jsonip.com/')
+            log(f"--- New IP for session: {r.json()['ip']}")
+        search_page_init(attempts+1)
+
+
 def fetch_data():
     locator_domain = 'https://www.frysfood.com'
     addresses = []
@@ -35,33 +128,23 @@ def fetch_data():
     MAX_RESULTS = 100
     MAX_DISTANCE = 100
     zip_code = search.next_zip()
-    session = SgRequests()
+
+    search_page_init()
+
     while zip_code:
         result_coords = []
-        log("zip_code === " + str(zip_code))
-        log("ramiang zip =====" + str(len(search.zipcodes)))
-        headers = {
-            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36",
-            'content-type': 'application/json;charset=UTF-8'
-        }
-        data = r'{"query":"\n      query storeSearch($searchText: String!, $filters: [String]!) {\n        storeSearch(searchText: $searchText, filters: $filters) {\n          stores {\n            ...storeSearchResult\n          }\n          fuel {\n            ...storeSearchResult\n          }\n          shouldShowFuelMessage\n        }\n      }\n      \n  fragment storeSearchResult on Store {\n    banner\n    vanityName\n    divisionNumber\n    storeNumber\n    phoneNumber\n    showWeeklyAd\n    showShopThisStoreAndPreferredStoreButtons\n    storeType\n    distance\n    latitude\n    longitude\n    tz\n    ungroupedFormattedHours {\n      displayName\n      displayHours\n      isToday\n    }\n    address {\n      addressLine1\n      addressLine2\n      city\n      countryCode\n      stateCode\n      zip\n    }\n    pharmacy {\n      phoneNumber\n    }\n    departments {\n      code\n    }\n    fulfillmentMethods{\n      hasPickup\n      hasDelivery\n    }\n  }\n","variables":{"searchText":"'+str(zip_code)+'","filters":[]},"operationName":"storeSearch"}'
+        log("zip_code: " + str(zip_code))
+        log("remaining zips: " + str(len(search.zipcodes)))
 
-        try:
-            r = session.post(
-                'https://www.frysfood.com/stores/api/graphql', headers=headers, data=data)
-        except RequestException as ex:
-            log("--- RequestException -->", ex, 'resetting session')
-            # reset the session and try again
-            session = SgRequests()
-            r = session.post(
-                'https://www.frysfood.com/stores/api/graphql', headers=headers, data=data)
+        r = do_search(zip_code)
+        log('status code for do_search(): ', r.status_code)
 
-        log('status code for search: ', r.status_code)
-
+        datas = []
         try:
             datas = r.json()['data']['storeSearch']['stores']
         except Exception as ex:
-            log(f'-------- exception parsing stores json for zip {zip_code} --------', ex, '---------')
+            log(
+                f'-------- exception parsing stores json for zip {zip_code} --------', ex, '---------')
             # pass
 
         for key in datas:
@@ -112,10 +195,12 @@ def fetch_data():
             yield store
 
         ###fuel store
+        datas1 = []
         try:
             datas1 = r.json()['data']['storeSearch']['fuel']
         except Exception as ex:
-            log(f'-------- exception parsing fuel stores for zip {zip_code} --------', ex, '---------')
+            log(
+                f'-------- exception parsing fuel stores for zip {zip_code} --------', ex, '---------')
             # pass
 
         for key1 in datas1:
@@ -183,4 +268,6 @@ def scrape():
     write_output(data)
 
 
+override_retries()
+session = SgRequests()
 scrape()
