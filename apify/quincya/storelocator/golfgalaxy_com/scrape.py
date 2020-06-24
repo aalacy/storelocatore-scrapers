f24112033e007e30a021592cde271dd4ca7bfd8b
Quincy <qcalexander@gmail.com>
2020-06-24 19:38:16 -0400

    golfgalaxy_com fixed

--- apify/quincya/storelocator/golfgalaxy_com/scrape.py
+++ apify/quincya/storelocator/golfgalaxy_com/scrape.py
@@ -1,31 +1,12 @@
+from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import csv
 import time
-import re
-import sgzip
-
 from random import randint
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-
-
-def get_driver():
-    options = Options() 
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--window-size=1920,1080')
-    return webdriver.Chrome('chromedriver', chrome_options=options)
-
+import re
 
 def write_output(data):
-	with open('data.csv', mode='w') as output_file:
+	with open('data.csv', mode='w', encoding="utf-8") as output_file:
 		writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
 		# Header
@@ -36,104 +17,109 @@ def write_output(data):
 
 def fetch_data():
 	
-	main_link = "https://www.golfgalaxy.com/s/stores"
-	base_link = "https://storelocator.golfgalaxy.com/index_single.html"
+	base_link = "https://stores.golfgalaxy.com/"
 
-	driver = get_driver()
-	time.sleep(2)
+	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+	HEADERS = {'User-Agent' : user_agent}
 
-	driver.get(base_link)
-	time.sleep(randint(8,10))
-
-	# # Clear ad
-	# try:
-	# 	ad_close = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "close")))
-	# 	driver.execute_script('arguments[0].click();', ad_close)
-	# except:
-	# 	pass
-
-	# element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "dsg_stores")))
-	# frame_link = driver.find_element_by_id("dsg_stores").get_attribute("src")
-	# driver.get(frame_link)
-
-	element = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.ID, "search_address")))
-
-	data = []
-	poi_done = []
-	zips = sgzip.for_radius(200)
-	total_zips = str(len(zips))
-	count = 1
-	for zip_code in zips[:3]:
-		print("Searching: " + str(zip_code))
-		print("# " + str(count) + " of " + total_zips)
-		print()
-		count = count + 1
-
-		search_element = driver.find_element_by_id("search_address")
-		search_element.clear()
-		time.sleep(randint(1,2))
-		search_element.send_keys(zip_code)
-		time.sleep(randint(1,2))
-		search_button = driver.find_element_by_id('search_button')
-		driver.execute_script('arguments[0].click();', search_button)
-		time.sleep(randint(8,10))
-
-		base = BeautifulSoup(driver.page_source,"lxml")
-
-		results = base.find_all(class_='location_row')[1:]
-
-		for result in results:
-			locator_domain = "golfgalaxy.com"
-			location_name = result.find(class_='store_name').text.strip()
-
-			shop_center = result.find(class_='shopping_center').text.strip()
-			address1 = result.find(class_='address1').text.strip()
-			street_address = (shop_center + " " + address1).strip()
-			
-
-			test_string = location_name + "_" + street_address
-			if test_string in poi_done:
-				continue
-
-			poi_done.append(test_string)
-
-			print(location_name)
-			city_line = result.find(class_='city_state_postalcode').text.strip()
-			city = city_line[:city_line.find(',')].strip()
-			state = city_line[city_line.find(',')+1:city_line.rfind(' ')].strip()
-			zip_code = city_line[city_line.rfind(' ')+1:].strip()
-			country_code = "US"
-
-			store_number = "<MISSING>"
-			try:
-				phone = result.find(class_='phone').text.strip()
-			except:
-				phone = "<MISSING>"
-
-			location_type = "<MISSING>"
-
-			raw_hours = result.find(class_="all_hours_table").find_all('tr')
-			hours = ""
-			hours_of_operation = ""
-
-			try:
-				for hour in raw_hours:
-					hours = hours + " " + hour.text.replace("\t","").replace("\n"," ").strip()
-				hours_of_operation = (re.sub(' +', ' ', hours)).strip()
-			except:
-				pass
-			if not hours_of_operation:
-				hours_of_operation = "<MISSING>"
-
-			latitude = "<MISSING>"
-			longitude = "<MISSING>"
-
-			data.append([locator_domain, main_link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
-	
+	session = SgRequests()
+	req = session.get(base_link, headers = HEADERS)
+	time.sleep(randint(1,2))
 	try:
-		driver.close()
-	except:
-		pass
+		base = BeautifulSoup(req.text,"lxml")
+		print("Got today page")
+	except (BaseException):
+		print('[!] Error Occured. ')
+		print('[?] Check whether system is Online.')
+
+	main_links = []
+	main_items = base.find(class_="contentlist").find_all('li')
+	for main_item in main_items:
+		main_link = main_item.a['href']
+		main_links.append(main_link)
+
+	final_links = []
+	for main_link in main_links:
+		print()
+		print("Processing State: " + main_link)
+		req = session.get(main_link, headers = HEADERS)
+		time.sleep(randint(1,2))
+		try:
+			base = BeautifulSoup(req.text,"lxml")
+		except (BaseException):
+			print('[!] Error Occured. ')
+			print('[?] Check whether system is Online.')
+
+		next_items = base.find(class_="contentlist").find_all('li')
+		for next_item in next_items:
+			next_link = next_item.a['href']
+			print("Processing City: " + next_link)
+
+			next_req = session.get(next_link, headers = HEADERS)
+			time.sleep(randint(1,2))
+			try:
+				next_base = BeautifulSoup(next_req.text,"lxml")
+			except (BaseException):
+				print('[!] Error Occured. ')
+				print('[?] Check whether system is Online.')
+
+			final_items = next_base.find_all('span', attrs={'itemprop': 'streetAddress'})
+			for final_item in final_items:
+				final_link = final_item.find('a', attrs={'linktrack': 'Landing page'})['href']
+				final_links.append(final_link)
+	
+	data = []
+	for final_link in final_links:
+		final_req = session.get(final_link, headers = HEADERS)
+		time.sleep(randint(1,2))
+		try:
+			item = BeautifulSoup(final_req.text,"lxml")
+		except (BaseException):
+			print('[!] Error Occured. ')
+			print('[?] Check whether system is Online.')
+
+		locator_domain = "getregionalcash.com"
+
+		location_name = item.find("h1").text.strip().title()
+		print(location_name)
+
+		street_address = item.find('span', attrs={'itemprop': 'containedIn'}).text.strip()
+		try:
+			street_address = street_address + " " + item.find('span', attrs={'itemprop': 'streetAddress'}).text.strip()
+			street_address = street_address.strip()
+		except:
+			pass
+		
+		city = item.find('span', attrs={'itemprop': 'addressLocality'}).text.strip()
+		state = item.find('span', attrs={'itemprop': 'addressRegion'}).text.strip()
+		zip_code = item.find('span', attrs={'itemprop': 'postalcode'}).text.strip()
+		country_code = "US"
+		store_number = "<MISSING>"
+		
+		location_type = "<MISSING>"
+
+		try:
+			phone = item.find(class_="phone").text.strip()
+		except:
+			phone = "<MISSING>"
+
+		latitude = item.find('meta', attrs={'property': 'place:location:latitude'})['content']
+		longitude = item.find('meta', attrs={'property': 'place:location:longitude'})['content']
+
+		raw_hours = item.find(id="all_hours_table").find_all('tr')
+		hours = ""
+		hours_of_operation = ""
+
+		try:
+			for hour in raw_hours:
+				hours = hours + " " + hour.text.replace("\t","").replace("\n"," ").strip()
+			hours_of_operation = (re.sub(' +', ' ', hours)).strip()
+		except:
+			pass
+		if not hours_of_operation:
+			hours_of_operation = "<MISSING>"
+
+		data.append([locator_domain, final_link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
 
 	return data
 
