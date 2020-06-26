6ae715c91bba0d9cebfcb0504997eb2437887e83
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-06-26 23:32:58 +0500

    Add files via upload

--- apify/ggrahambaker/storelocator/coyoteuglysaloon_com/scrape.py
+++ apify/ggrahambaker/storelocator/coyoteuglysaloon_com/scrape.py
@@ -1,93 +1,131 @@
+import requests
+from bs4 import BeautifulSoup
 import csv
-import os
-from sgselenium import SgSelenium
-import json
-import usaddress
+import string
+import re, time
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
-    locator_domain = 'https://www.coyoteuglysaloon.com/'
-    ext = 'map/'
+    # Your scraper here
+    data = []
+    p = 0
+    url = 'https://www.coyoteuglysaloon.com/'
+    r = session.get(url, headers=headers, verify=False)  
+    soup =BeautifulSoup(r.text, "html.parser")   
+    state_list = soup.find('select', {'name': 'site'})
+    state_list = state_list.findAll('option')
+    #print(len(state_list))
+    flag = 0 
+    for i in range(2,len(state_list)):
+        link = state_list[i]
+        #print(link.text)
+        if link.text.find('--') > -1 :
+            break
+        else:            
+            link = link['value']
+            r1 = session.get(link, headers=headers, verify=False)
+            soup1 =BeautifulSoup(r1.text, "html.parser")
+            maindiv = soup1.find('div',{'id':'mapinfo'})
+            title = maindiv.find('dt').text
 
-    driver = SgSelenium().chrome()
-    driver.get(locator_domain + ext)
+            address = maindiv.find('dd').text.splitlines()
+            m = 0
+            street = address[m]
+            m += 1
+            if address[m].find(',') == -1:
+                street = street + ' '  + address[m]
+                m += 1
+            #print(address[m])
+            city,state = address[m].split(', ')
+            state =state.lstrip()
+            state,pcode = state.split(' ')
+            maindiv = soup1.find('div',{'id':'detailinfo'})
+            maindiv = maindiv.findAll('dd')
+            phone = maindiv[0].text
+            hourd = maindiv[2].findAll('li')
+            hours = ''
+            for hr in hourd:
+                hours = hours + ' ' + hr.text
 
-    groups = driver.find_element_by_css_selector('section#newsarchive').find_elements_by_css_selector('dl')
+            if len(hours) < 2:
+                hours = '<MISSING>'
+            else:
+                hours = hours.replace('am',' am').replace('pm',' pm').replace('-',' - ').replace('  ',' ')
 
-    link_list = []
-    for group in groups[7:]:
-        links = group.find_elements_by_css_selector('a')
-        for link in links:
-            link_list.append(link.get_attribute('href'))
+            soup1 = str(soup1)
+            start = soup1.find('"id"')
+            if start == -1:
+                store = '<MISSING>'
+            else:
+                start = soup1.find(':',start)
+                start = soup1.find('"',start)+1
+                end = soup1.find('"',start)
+                store = soup1[start:end]
+            start = soup1.find('"lat"')
+            if start == -1:
+                lat = '<MISSING>'
+            else:
+                start = soup1.find(':',start)
+                start = soup1.find('"',start)+1
+                end = soup1.find('"',start)
+                lat = soup1[start:end]
+            start = soup1.find('"lng"')
+            if start == -1:
+                longt = '<MISSING>'
+            else:
+                start = soup1.find(':',start)
+                start = soup1.find('"',start)+1
+                end = soup1.find('"',start)
+                longt = soup1[start:end]
 
-    all_store_data = []
-    for link in link_list:
-        driver.get(link)
-        driver.implicitly_wait(10)
-        info = driver.find_element_by_css_selector('div#detailinfo').text.split('\n')
+            if phone.find('UGLY') > -1 and phone.find('(') == -1:                
+                phone = phone.replace('UGLY','8459')
+                
+            elif phone.find('UGLY') > -1 and phone.find('(') > -1:                
+                phone = phone.replace('UGLY ','')
+                
+            data.append([
+                        'https://www.coyoteuglysaloon.com/',
+                        link,                   
+                        title,
+                        street,
+                        city,
+                        state,
+                        pcode,
+                        'US',
+                        store,
+                        phone,
+                        '<MISSING>',
+                        lat,
+                        longt,
+                        hours
+                    ])
+            #print(p,data[p])
+            p += 1
+                
+        
+    return data
 
-        if 'daytona' in info[1]:
-            phone_number = '(386) 256-4954'
-        else:
-            phone_number = info[1].replace('PHONE', '').strip().split(' ')[0]
-            if 'UGLY' in phone_number:
-                phone_number = phone_number.replace('UGLY', '8459')
-        hours = ''
-        for h in info:
-            if 'pm-' in h or 'am-' in h:
-                hours += h + ' '
-
-        address = driver.find_element_by_css_selector('div#mapinfo').text.split('\n')
-        location_name = address[1]
-        addy = address[-7]
-
-        parsed_add = usaddress.tag(addy)[0]
-
-        street_address = ''
-        if 'AddressNumber' in parsed_add:
-            street_address += parsed_add['AddressNumber'] + ' '
-        if 'StreetNamePreDirectional' in parsed_add:
-            street_address += parsed_add['StreetNamePreDirectional'] + ' '
-        if 'StreetName' in parsed_add:
-            street_address += parsed_add['StreetName'] + ' '
-        if 'StreetNamePostType' in parsed_add:
-            street_address += parsed_add['StreetNamePostType'] + ' '
-        if 'OccupancyType' in parsed_add:
-            street_address += parsed_add['OccupancyType'] + ' '
-        if 'OccupancyIdentifier' in parsed_add:
-            street_address += parsed_add['OccupancyIdentifier'] + ' '
-        city = parsed_add['PlaceName']
-        state = parsed_add['StateName']
-        zip_code = parsed_add['ZipCode']
-
-        data = driver.find_element_by_css_selector('div.wpgmza_map').get_attribute('data-settings')
-        json_geo = json.loads(data)
-        # print(json_geo['map_start_location'].split(','))
-        coords = json_geo['map_start_location'].split(',')
-        lat = coords[0]
-        longit = coords[1]
-
-        country_code = 'US'
-        location_type = '<MISSING>'
-        store_number = '<MISSING>'
-        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
-                      store_number, phone_number, location_type, lat, longit, hours]
-        all_store_data.append(store_data)
-
-    driver.quit()
-    return all_store_data
 
 def scrape():
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
     data = fetch_data()
     write_output(data)
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
 
 scrape()
