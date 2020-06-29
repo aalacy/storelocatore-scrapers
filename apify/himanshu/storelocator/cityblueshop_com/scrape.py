654022f93668a1c3f3fad36a924ca44ce8700813
Tenzing Shaw <tenzing@safegraph.com>
2020-06-29 13:56:35 -0700

    fix and remove printing

--- apify/himanshu/storelocator/cityblueshop_com/scrape.py
+++ apify/himanshu/storelocator/cityblueshop_com/scrape.py
@@ -32,15 +32,17 @@ def fetch_data():
     soup = BeautifulSoup(r.text, "lxml")
     for parts in soup.find_all("div", {"class": "rte-content colored-links"}):
         for semi_parts in parts.find_all("h3"):
-            top_level_address = list(semi_parts.find_next_sibling().stripped_strings)[0]
+            siblings = list(semi_parts.find_next_sibling().stripped_strings)
+            top_level_address = ', '.join(siblings)
             tagged = usaddress.tag(top_level_address)[0]
             if 'StateName' not in tagged:
                 continue
             state = tagged['StateName']
             city = tagged['PlaceName']
-            street_address = top_level_address.split(city)[0].strip().strip(',')
+            street_address = top_level_address.split(city)[0].strip(',( ')
+            if street_address.lower().endswith("shopping center"):
+                street_address = street_address.rsplit(',', 1)[0].strip()
             zipcode = tagged['ZipCode']
-            print(zipcode)
             phone1 =''
             store_request = session.get(semi_parts.find("a")['href'])
             store_soup = BeautifulSoup(store_request.text, "lxml")
@@ -52,7 +54,6 @@ def fetch_data():
                 location_name = semi_parts.text
                 if len(temp_storeaddresss) ==7:
                     phone1 = temp_storeaddresss[2].replace("Ph: ","")
-                    street_address = temp_storeaddresss[0].replace("\xa0","")
                     hours = " ".join(temp_storeaddresss[-4:])
                 elif len(temp_storeaddresss) ==4:
                     phone1 = temp_storeaddresss[-3]
@@ -73,9 +74,9 @@ def fetch_data():
             return_object =[]
             return_object.append("https://www.cityblueshop.com")
             return_object.append(location_name.encode('ascii', 'ignore').decode('ascii').strip() if location_name else "<MISSING>")
-            return_object.append(street_address.encode('ascii', 'ignore').decode('ascii').strip().replace('333 Naamans Rd Claymont','333 Naamans Rd').replace("4601Northfield","4601 Northfield") if street_address else "<MISSING>")
+            return_object.append(street_address.encode('ascii', 'ignore').decode('ascii').strip() if street_address else "<MISSING>")
             return_object.append(city.encode('ascii', 'ignore').decode('ascii').strip() if city else "<MISSING>")
-            return_object.append(state.encode('ascii', 'ignore').decode('ascii').strip().replace("Philadelphia","PA").replace("Upper Darby","PA").replace("East Cleveland","OH").replace("North Randall","OH") if state.replace("Philadelphia","PA").replace("Upper Darby","PA").replace("Upper Darby","PA") else "<MISSING>")
+            return_object.append(state.encode('ascii', 'ignore').decode('ascii').strip() if state  else "<MISSING>")
             return_object.append(zipcode if zipcode else "<MISSING>")
             return_object.append("US")
             return_object.append("<MISSING>")
