948d5b52f904edc852eb5c35e9340b3eadfdaed9
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-06-30 01:09:27 +0500

    Add files via upload

---
+++ apify/aleenah/storelocator/bbtheatres_com/bbtheatres.py
@@ -0,0 +1,66 @@
+import csv
+from sgrequests import SgRequests
+import re
+import json
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation",
+                         "page_url"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+session = SgRequests()
+all=[]
+def fetch_data():
+    # Your scraper here
+
+    res=session.get("https://www.bbtheatres.com/theatres/") # not using because phone is not available in itt
+    #print(res.text)
+    #print(re.findall(r'"CinemaList":(\[.*\]),"experiences"',res.text.replace('\\',''),re.DOTALL)[0])
+    jso = json.loads(re.findall(r'"CinemaList":(\[.*\]),"experiences"',res.text.replace('\\',''))[0])
+    print(len(jso))
+
+    for js in jso:
+        zip = js["CinemaInfo"]["ZipCode"]
+        if zip == "":
+            zip="<MISSING>"
+        phone = js["CinemaInfo"]["Phone"]
+        if phone == "":
+            phone="<MISSING>"
+        lat = js["CinemaInfo"]["Latitude"]
+        if lat == "":
+            lat="<MISSING>"
+        long = js["CinemaInfo"]["Longitude"]
+        if long == "":
+            long="<MISSING>"
+        all.append([
+            "https://www.bbtheatres.com/theatres/",
+            js["Name"],
+            (js["CinemaInfo"]["Address1"]+" "+js["CinemaInfo"]["Address2"]).strip(),
+            js["CinemaInfo"]["City"],
+            js["CinemaInfo"]["StateCode"],
+            zip,
+            'US',
+            js["Id"],  # store #
+            phone,  # phone
+            "<MISSING>",  # type
+            lat,  # lat
+            long,  # long
+            "<MISSING>",  # timing
+            "https://www.bbtheatres.com/theatres/"])
+
+    return all
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
+
