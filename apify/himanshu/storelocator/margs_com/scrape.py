37cafb98adcc793e8568cd0755b7563f57abc22d
diapk <Hank4957!>
2020-06-30 17:49:38 +0530

    docker run and push

--- apify/himanshu/storelocator/margs_com/scrape.py
+++ apify/himanshu/storelocator/margs_com/scrape.py
@@ -24,7 +24,6 @@ def fetch_data():
     base_url = "https://www.margs.com"
     r = session.get("https://www.margs.com/locations",headers=headers)
     soup = BeautifulSoup(r.text,"lxml")
-    return_main_object = []
     for link in soup.find_all("a",{"class":"ca1link"}):
         if base_url == link["href"]:
             continue
@@ -35,8 +34,16 @@ def fetch_data():
             geo_locations.append(geo["href"])
         for location in state_soup.find_all("div",{"data-width":"247"}):
             location_link = location.find("a")["href"]
+            if "https://www.margs.com/ellsworth" == location_link:
+                continue
+           
             location_request = session.get(location_link,headers=headers)
             location_soup = BeautifulSoup(location_request.text,"lxml")
+            try:
+                phone = location_soup.find("a",{"data-type":"phone"})['data-content']
+            except:
+                phone = location_soup.find(lambda tag: (tag.name == "span") and "Phone:" in tag.text).text.replace("Phone:","").strip()
+
             if location_soup.find("span",text=re.compile("OPENING")):
                 continue
             if location_soup.find("span",text=re.compile("Coming Soon")):
@@ -45,10 +52,12 @@ def fetch_data():
             for details in location_soup.find_all("h2",{"class":"font_2"}):
                 location_details.extend(list(details.stripped_strings))
             for i in range(len(location_details)):
-                if location_details[i] == "LOCATION DETAILS":
+                if location_details == "ONLINE ORDERING" or location_details[i] == "LOCATION DETAILS":
                     location_details = location_details[:i]
                     break
             location_details[3] = location_details[3].replace("\xa0"," ")
+            if "We are\xa0open for" in location_details[2]:
+                del location_details[2:4]
             store = []
             store.append("https://www.margs.com")
             store.append(location_details[1])
@@ -58,22 +67,16 @@ def fetch_data():
             store.append(location_details[3].split(",")[1].split(" ")[-1])
             store.append("US")
             store.append("<MISSING>")
-            store.append(location_details[5])
-            if store[-1] == "":
-                #print(store)
-                phone = location_soup.find("span",text=re.compile(".?(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}).?"))
-                if phone:
-                    store[-1] = re.findall(re.compile(".?(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}).?"),phone)[0]
+            store.append(phone)
             store.append("<MISSING>")
             store.append(geo_locations[0].split("/@")[1].split(",")[0])
             store.append(geo_locations[0].split("/@")[1].split(",")[1])
             del geo_locations[0]
-            store.append(" ".join(location_details[7:]).replace("\xa0"," ").replace("–","-"))
+            store.append(" ".join(location_details[7:]).replace("\xa0"," ").replace("–","-").split("ONLINE ORDERING")[0].split("Kitchen hours")[0].split("ORDER TAKEOUT")[0].split("DELIVERY SERVICES")[0])
             if store[-1] == "":
                 store[-1] = "<MISSING>"
             store.append(location_link)
             yield store
-
 def scrape():
     data = fetch_data()
     write_output(data)
--- apify/himanshu/storelocator/oritani_com/scrape.py
+++ apify/himanshu/storelocator/oritani_com/scrape.py
@@ -1,59 +1,160 @@
 import csv
 from sgrequests import SgRequests
-from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup as bs
 import re
 import json
-
-
+import sgzip
+import time
 session = SgRequests()
 
+# def request_wrapper(url,method,headers,data=None):
+#    request_counter = 0
+#    if method == "get":
+#        while True:
+#            try:
+#                r = requests.get(url,headers=headers)
+#                return r
+#                break
+#            except:
+#                time.sleep(2)
+#                request_counter = request_counter + 1
+#                if request_counter > 10:
+#                    return None
+#                    break
+#    elif method == "post":
+#        while True:
+#            try:
+#                if data:
+#                    r = requests.post(url,headers=headers,data=data)
+#                else:
+#                    r = requests.post(url,headers=headers)
+#                return r
+#                break
+#            except:
+#                time.sleep(2)
+#                request_counter = request_counter + 1
+#                if request_counter > 10:
+#                    return None
+#                    break
+#    else:
+#        return None
+
+
 def write_output(data):
-    with open('data.csv', mode='w',encoding="utf-8") as output_file:
+    with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
         # Body
         for row in data:
             writer.writerow(row)
 
 def fetch_data():
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
-    }
+    MAX_RESULTS = 1000
+    MAX_DISTANCE = 100
+    search = sgzip.ClosestNSearch()
+    search.initialize(country_codes=['US'])
+    zip_code = search.next_zip()
+    current_results_len = 0
+    adressess = []
+    result_len=0
     base_url = "https://www.oritani.com"
-    r = session.get("https://www.oritani.com/Locations",headers=headers)
-    soup = BeautifulSoup(r.text,"lxml")
-    return_main_object = []
-    location_list = soup.find_all("div",{"class":"col-sm-12 col-md-6"})
-    for i in range(0,len(location_list),2):
-        if location_list[i+1].find("a") == None:
-            lat = "<MISSING>"
-            lng = "<MISSING>"
-        else:
-            lat = location_list[i+1].find("a")["href"].split("/@")[1].split(",")[0]
-            lng = location_list[i+1].find("a")["href"].split("/@")[1].split(",")[1]
-        location_details = list(location_list[i].stripped_strings)
-        if location_details[3] == "Phone":
-            location_details[3] = "".join(location_details[3:5])
-            del location_details[4]
-        store = []
-        store.append("https://www.oritani.com")
-        store.append(location_details[0])
-        store.append(location_details[1])
-        store.append(location_details[2].split(",")[0])
-        store.append(",".join(location_details[2].split(",")[1:]).split(" ")[-2].replace(",",""))
-        store.append(",".join(location_details[2].split(",")[1:]).split(" ")[-1].replace(",",""))
-        store.append("US")
-        store.append("<MISSING>")
-        store.append(location_details[3].split("Phone:")[1])
-        store.append("oritani bank")
-        store.append(lat)
-        store.append(lng)
-        store.append(" ".join(location_details[4:]))
-        return_main_object.append(store)
-    return return_main_object
+    
+    while zip_code:
+        result_coords =[]
+        print("remaining zipcodes: " + str(len(search.zipcodes)))
+        url = "https://www.valley.com/siteAPI/Branch/Branches"
 
+        payload = "{\"Location\":\"'"+str(zip_code)+"'\"}"
+        # print(payload)
+        headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Connection': 'keep-alive',
+        'Content-Length': '20',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Origin': 'https://www.valley.com',
+        'Referer': 'https://www.valley.com/find-a-branch',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+        }
+        try:
+            json_data = requests.request("POST", url, headers=headers, data = payload).json()
+        except:
+            continue
+        # if "Branches" in json_data:
+        #     json_data = len(json_data['Branches'])
+        # print(json_data)
+        if type(json_data)==str:
+            data = json.loads(json_data)
+            if 'Branches' in data:
+                result_len = len(data['Branches'])
+                for store in data['Branches']:
+                    location_name = store['Name']
+                    street_address = store['StreetAddress']
+                    city = store['City']
+                    state = store['State']
+                    zipp = store['Zipcode']
+                    phone = store['PhoneFormatted']
+                    lat = store['Lat']
+                    lng = store['Lng']
+                    store_number = store['ID']
+                    location_type = store['Type']
+                    page_url = "https://www.valley.com"+store['Path'].lower()
+                    hours = ""
+                    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',}
+                    try:    
+                        store_soup = bs(requests.get(page_url).text,'lxml')
+                    except:
+                        continue
+                    
+                    hours=''
+                    names_hours = (store_soup.find_all("a",{"href":re.compile("#panel")}))
+                    for index,i in enumerate(store_soup.find_all("div",{"class":"tabs-panel"})):
+                        if i['id']==names_hours[index]['href'].replace("#",''):
+                            hours = hours + ' '+names_hours[index].text.strip() + ' '+" ".join(list(i.stripped_strings))
+                    # print(hours)
+                    # hours = " ".join(list(store_soup.find("div",{"class":"tabs-content","class":"small-9"}).stripped_strings))
+                    # try:
+                    # store_soup = request_wrapper(page_url,"get",headers=headers)
+                    # except:
+                    #     pass
+                    result_coords.append((lat,lng))
+                    store = []
+                    store.append(base_url)
+                    store.append(location_name)
+                    store.append(street_address)
+                    store.append(city)
+                    store.append(state)
+                    store.append(zipp)   
+                    store.append("US")
+                    store.append(store_number)
+                    store.append(phone)
+                    store.append(location_type)
+                    store.append(lat)
+                    store.append(lng)
+                    store.append(hours if hours else "<MISSING>")
+                    store.append(page_url)     
+                    if store[2] in adressess:
+                        continue
+                    adressess.append(store[2]) 
+                    store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+                    yield store
+                    #print(store)
+             
+        if (result_len) < MAX_RESULTS:
+            # print("max distance update")
+            search.max_distance_update(MAX_DISTANCE)
+        elif (result_len) == MAX_RESULTS:
+            # print("max count update")
+            search.max_count_update(result_coords)
+        else:
+            raise Exception("expected at most " + str(MAX_RESULTS) + " results")
+        
+        zip_code = search.next_zip()
+    
+    
 def scrape():
     data = fetch_data()
     write_output(data)
