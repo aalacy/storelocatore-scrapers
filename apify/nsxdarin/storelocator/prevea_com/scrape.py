7d3babbe408f702ae5ced94ed227c05eb19d25b0
Tenzing Shaw <tenzing@safegraph.com>
2020-07-01 15:23:43 -0700

    rm logging

--- apify/nsxdarin/storelocator/prevea_com/scrape.py
+++ apify/nsxdarin/storelocator/prevea_com/scrape.py
@@ -110,7 +110,6 @@ def parse_locations(response, locs):
     for line in response.iter_lines(decode_unicode=True):
         if '<strong><a href=\\"/locations' in line:
             loc = 'https://www.prevea.com/locations' + line.split('<strong><a href=\\"/locations')[1].split('\\')[0]
-            raise Exception(loc)
             locs.append(loc)
 
 
