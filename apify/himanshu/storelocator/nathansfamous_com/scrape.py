51581ea5909c64b437a98e26b9c7f07f3a6d72ee
Tenzing Shaw <tenzing@safegraph.com>
2020-07-01 00:41:30 -0700

    safety check

--- apify/himanshu/storelocator/nathansfamous_com/scrape.py
+++ apify/himanshu/storelocator/nathansfamous_com/scrape.py
@@ -3,8 +3,8 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
+
 session = SgRequests()
-import requests
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
@@ -14,6 +14,7 @@ def write_output(data):
         # Body
         for row in data:
             writer.writerow(row)
+
 def fetch_data():
     return_main_object = []
     addresses = []
@@ -21,7 +22,7 @@ def fetch_data():
     base_url = "https://www.nathansfamous.com"
     headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"}
     output = []
-    r = requests.get("https://restaurants.nathansfamous.com/locations/",headers=headers)
+    r = session.get("https://restaurants.nathansfamous.com/locations/",headers=headers)
     soup = BeautifulSoup(r.text,"lxml")
     lat  =[]
     lng = []
@@ -30,80 +31,81 @@ def fetch_data():
         lng.append(lats['data-lng'])
   
     for index,row in enumerate(soup.find_all("div",{"id":"twentyninepalms"})):
-        if list(row.stripped_strings)[0]=="Dominican Republic":
-            break
-        else:
-            full = list(row.stripped_strings)
-            phone = list(row.stripped_strings)[-1].replace(" x 115",'')           
-            name = full[0]
-            city=''
-            zipp=''
-            if len(full[2:-2])==3:
-                if full[2:-2][-1]=="32413":
-                    del full[-1]
-                city = full[2:-2][-1].split(',')[0].replace("29 ",'')
-                # print()
-                state = full[2:-2][-1].split(",")[-1].split( )[0]
-                zipp = full[2:-2][-1].split(",")[-1].split( )[-1].replace("FL","32413")
-                if "801 Pier Park Drive" in full[2:-2][:-1] or "301 Mount Hope Avenue" in full[2:-2][:-1]:
-                    address =" ".join(full[2:-2][:-1])
-                else:
-                    name = full[2:-2][:-1][0]
-                    address = " ".join(full[2:-2][:-1][1:])
-                    # print(full[2:-2][:-1][1:])
-            elif len(full[2:-2])==2:
-                state_list = re.findall(r' ([A-Z]{2})', str(full[2:-2][-1]))
-                us_zip_list = re.findall(re.compile(r"\b[0-9]{5}(?:-[0-9]{4})?\b"), str(full[2:-2][-1]))
-                if us_zip_list:
-                    zipp = us_zip_list[-1]
-                else:
-                    zipp=''
-                if state_list:
-                    state = state_list[-1]
-                else:
-                    state=""
-                city = full[2:-2][-1].replace(zipp,'').replace(state,'').replace(",",'')
-                address = " ".join(full[2:-2][:-1])
-                
-            elif len(full[2:-2])==1:
-                state = full[2:-2][0].split(",")[-1].split( )[0]
-                zipp = full[2:-2][0].split(",")[-1].split( )[1]
-                city  = full[2:-2][0].split(",")[-2].replace("5770 W. Irlo Bronson Memorial Hwy ",'')
-                address = full[2:-2][0].split(",")[0].replace("Kissimmee",'')
-            if "12475" in zipp :
-                state = "New York"
-                city = city.replace("Ruby New York","Ruby")
-            if "10580" in zipp :
-                state = "New York"
-                city = city.replace("Rye New York","Rye")
-            if "11364" in zipp :
-                state = "New York"
-                city = city.replace("Bayside New York","Bayside")
-            if "10121" in zipp :
-                state = "New York"
-                city = city.replace("New York New York","New York")
-            if "JFK Memorial Hwy. MM 82" in address:
-                state = "NY"
-                city = "New York"
-                address = address.replace('JFK Memorial Hwy. MM 82',"JFK Memorial Hwy. MM 82 Vesey St. & West St.")
-            store = []
-            store.append("http://nathansfamous.com/")
-            store.append(name)
-            store.append(address.strip())
-            store.append(city)
-            store.append(state)
-            store.append(zipp)   
-            store.append("US")
-            store.append("<MISSING>")
-            store.append(phone)
-            store.append("<MISSING>")
-            store.append(lat[index])
-            store.append(lng[index])
-            store.append("<MISSING>")
-            store.append( "<MISSING>")     
-            store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
-            yield store
-            # print(state)
+        full = list(row.stripped_strings)
+        if full:
+            if full[0]=="Dominican Republic":
+                break
+            else:
+                phone = full[-1].replace(" x 115",'')           
+                name = full[0]
+                city=''
+                zipp=''
+                if len(full[2:-2])==3:
+                    if full[2:-2][-1]=="32413":
+                        del full[-1]
+                    city = full[2:-2][-1].split(',')[0].replace("29 ",'')
+                    # print()
+                    state = full[2:-2][-1].split(",")[-1].split( )[0]
+                    zipp = full[2:-2][-1].split(",")[-1].split( )[-1].replace("FL","32413")
+                    if "801 Pier Park Drive" in full[2:-2][:-1] or "301 Mount Hope Avenue" in full[2:-2][:-1]:
+                        address =" ".join(full[2:-2][:-1])
+                    else:
+                        name = full[2:-2][:-1][0]
+                        address = " ".join(full[2:-2][:-1][1:])
+                        # print(full[2:-2][:-1][1:])
+                elif len(full[2:-2])==2:
+                    state_list = re.findall(r' ([A-Z]{2})', str(full[2:-2][-1]))
+                    us_zip_list = re.findall(re.compile(r"\b[0-9]{5}(?:-[0-9]{4})?\b"), str(full[2:-2][-1]))
+                    if us_zip_list:
+                        zipp = us_zip_list[-1]
+                    else:
+                        zipp=''
+                    if state_list:
+                        state = state_list[-1]
+                    else:
+                        state=""
+                    city = full[2:-2][-1].replace(zipp,'').replace(state,'').replace(",",'')
+                    address = " ".join(full[2:-2][:-1])
+                    
+                elif len(full[2:-2])==1:
+                    state = full[2:-2][0].split(",")[-1].split( )[0]
+                    zipp = full[2:-2][0].split(",")[-1].split( )[1]
+                    city  = full[2:-2][0].split(",")[-2].replace("5770 W. Irlo Bronson Memorial Hwy ",'')
+                    address = full[2:-2][0].split(",")[0].replace("Kissimmee",'')
+                if "12475" in zipp :
+                    state = "New York"
+                    city = city.replace("Ruby New York","Ruby")
+                if "10580" in zipp :
+                    state = "New York"
+                    city = city.replace("Rye New York","Rye")
+                if "11364" in zipp :
+                    state = "New York"
+                    city = city.replace("Bayside New York","Bayside")
+                if "10121" in zipp :
+                    state = "New York"
+                    city = city.replace("New York New York","New York")
+                if "JFK Memorial Hwy. MM 82" in address:
+                    state = "NY"
+                    city = "New York"
+                    address = address.replace('JFK Memorial Hwy. MM 82',"JFK Memorial Hwy. MM 82 Vesey St. & West St.")
+                store = []
+                store.append("http://nathansfamous.com/")
+                store.append(name)
+                store.append(address.strip())
+                store.append(city)
+                store.append(state)
+                store.append(zipp)   
+                store.append("US")
+                store.append("<MISSING>")
+                store.append(phone)
+                store.append("<MISSING>")
+                store.append(lat[index])
+                store.append(lng[index])
+                store.append("<MISSING>")
+                store.append( "<MISSING>")     
+                store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+                yield store
+                # print(state)
 
 def scrape():
     data = fetch_data()
