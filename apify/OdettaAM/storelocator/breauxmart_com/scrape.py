b0b2d0c297e377abb8621d0088f8eda84d35b609
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-07-04 22:35:19 +0500

    Add files via upload

--- apify/OdettaAM/storelocator/breauxmart_com/scrape.py
+++ apify/OdettaAM/storelocator/breauxmart_com/scrape.py
@@ -1,24 +1,17 @@
 import time
 import csv
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import json
 import requests
 
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-#driver = webdriver.Chrome("C:\chromedriver.exe", options=options)
-driver = webdriver.Chrome("chromedriver", options=options)
+
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain",'page_url', "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -29,10 +22,14 @@ def fetch_data():
     # Your scraper here
     data=[]
     #response = requests.get('https://api.freshop.com/1/stores?app_key=breaux_mart&has_address=true&limit=-1&token=f448f7cbfbefb4b7eb6f24bd81be0705')
-    response = requests.get('https://api.freshop.com/1/stores?app_key=breaux_mart&has_address=true&limit=-1&token=40343be1840f5a5e915146d700a4f50b')
-    time.sleep(3)
+    #response = requests.get('https://api.freshop.com/1/stores?app_key=breaux_mart&has_address=true&limit=-1&token=40343be1840f5a5e915146d700a4f50b')
+    response = requests.get('https://api-2.freshop.com/1/stores?app_key=breaux_mart&has_address=true&limit=-1&token=0dc33c6f693b5797f6941e824ef0de6d')
+    
     responseJson = json.loads(response.text)
+    #print(responseJson)
     stores = responseJson.get("items")
+    
+    print(len(stores))
     for i in range(len(stores)):
         location_name = stores[i]['name']
         street_address = stores[i]['address_1']
@@ -46,6 +43,7 @@ def fetch_data():
         longitude = stores[i]['longitude']
         data.append([
              'https://www.breauxmart.com/',
+             'https://www.breauxmart.com/my-store/store-locator',
               location_name,
               street_address,
               city,
@@ -57,11 +55,12 @@ def fetch_data():
               '<MISSING>',
               latitude,
               longitude,
-              hours_of_op
+              hours_of_op.replace('\n',' ')
             ])
+        #print(data[i])
 
-    time.sleep(3)
-    driver.quit()
+    #time.sleep(3)
+    
     return data
 
 def scrape():
