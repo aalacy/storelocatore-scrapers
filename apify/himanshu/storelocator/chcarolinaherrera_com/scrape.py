fabec95be49724486bc6ff2fe1b8953c40f1b765
diapk <Hank4957!>
2020-07-06 10:46:34 +0530

    docker run and push

--- apify/himanshu/storelocator/chcarolinaherrera_com/scrape.py
+++ apify/himanshu/storelocator/chcarolinaherrera_com/scrape.py
@@ -12,7 +12,7 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -27,10 +27,14 @@ def fetch_data():
     soup = BeautifulSoup(r.text,"lxml")
     for location in soup.find("datalist",{"data-country-id":"US"}).find_all("option"):
         state_request = session.get("https://chcarolinaherrera.com/STLStoreLocatorJSONDisplayView?catalogId=3074457345616676668&langId=-1002&storeId=715838508&country=US&state=" + str(location["value"]) + "&searchTerm=" ,headers=headers)
+        # print(state_request)
         state_soup = BeautifulSoup(state_request.text,"lxml")
         file1 = open("myfile1.txt","w")
         file1.write(state_soup.find("script").text.split("STL.storeArray = ")[1].split("];")[0] + "]")
-        location_list = json.loads((state_soup.find("script").text.split("STL.storeArray = ")[1].split("];")[0] + "]").replace("'",'"'))
+        data = state_soup.find("script").text.split("STL.storeArray = ")[1].split("];")[0] + "]"
+        location_list1 = (data.replace("'",'"').replace('""','"').replace('"extCountry": ",','"extCountry": "",').replace('"info":",','"info":"",').replace('"emailTo" : ",','"emailTo" : "",'))
+        location_list = json.loads(location_list1)
+        # print(location_list)
         for i in range(len(location_list)):
             store_data = location_list[i]
             store = []
@@ -46,16 +50,15 @@ def fetch_data():
             store.append("Carolina Herrera")
             store.append(store_data["latitude"])
             store.append(store_data["longitude"])
-            store_hours = store_data["timetable"]
-            hours = ""
-            for k in range(len(store_hours)):
-                hours = hours + " " + store_hours[i]["day"] + " " + store_hours[i]["hours"]
-            store.append(hours if hours != "" else "<MISSING>")
+            store_hours = str(store_data["timetable"]).replace("'}, {'day': '",", ").replace("', 'hours': '"," - ").replace("[","").replace("]","").replace("'","").replace("{","").replace("}","").replace("day: ","")
+            # hours = ""
+            # for k in range(len(store_hours)):
+            #     hours = hours + " " + store_hours[i]["day"] + " " + store_hours[i]["hours"]
+            store.append(store_hours if store_hours != "" else "<MISSING>")
+            store.append("<MISSING>")
             return_main_object.append(store)
     return return_main_object
-
 def scrape():
     data = fetch_data()
     write_output(data)
-
 scrape()
