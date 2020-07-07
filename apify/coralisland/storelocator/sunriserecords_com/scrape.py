565f3d06a01a663ddf08eb8c81f8e67a1c2d3c2b
Quincy <qcalexander@gmail.com>
2020-07-07 18:32:44 -0400

    sunriserecords_com fixed

--- apify/coralisland/storelocator/sunriserecords_com/scrape.py
+++ apify/coralisland/storelocator/sunriserecords_com/scrape.py
@@ -1,99 +1,126 @@
+from sgrequests import SgRequests
+from bs4 import BeautifulSoup
 import csv
+import time
+from random import randint
 import re
-import pdb
-import requests
-from lxml import etree
-import json
 
-base_url = 'https://www.sunriserecords.com'
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-def validate(item):
-    if type(item) == list:
-        item = ' '.join(item)
-    while True:
-        if item[-1:] == ' ':
-            item = item[:-1]
-        else:
-            break
-    return item.encode('ascii', 'ignore').encode("utf8").strip()
 
-def get_value(item):
-    if item == None :
-        item = '<MISSING>'
-    item = validate(item)
-    if item == '':
-        item = '<MISSING>'    
-    return item
+def get_driver():
+    options = Options() 
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    return webdriver.Chrome('chromedriver', chrome_options=options)
 
-def eliminate_space(items):
-    rets = []
-    for item in items:
-        item = validate(item)
-        if item != '':
-            rets.append(item)
-    return rets
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
+
 
 def fetch_data():
     output_list = []
 
     url = "https://www.sunriserecords.com/locations/"
-    request = requests.get(url)
-    response = etree.HTML(request.text)
-    store_list = response.xpath('//div[@class="vc_tta-panel"]')
 
-    for store_block in store_list:
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+    HEADERS = {'User-Agent' : user_agent}
 
-        state = validate(store_block.xpath('.//span[@class="vc_tta-title-text"]//text()'))
-        store_data = store_block.xpath('.//tr')
+    session = SgRequests()
+    req = session.get(url, headers = HEADERS)
+    time.sleep(randint(1,2))
+    try:
+        base = BeautifulSoup(req.text,"lxml")
+        print("Got today page")
+    except (BaseException):
+        print('[!] Error Occured. ')
+        print('[?] Check whether system is Online.')
 
-        for store in store_data:
-            title = validate(store.xpath(".//td")[0].xpath(".//text()"))
-            if "NOW CLOSED" in title:
+    driver = get_driver()
+    time.sleep(2)
+
+    states = base.find_all(class_="loc-header")
+    store_tables = base.find(class_="locations-cont").find_all("table")
+    
+    for i, store_table in enumerate(store_tables):
+        raw_state = states[i].text.strip()
+        rows = store_table.find_all("tr")
+        stores = []
+        for row in rows:
+            stores.append(row)
+
+        for store in stores:
+            try:
+                location_name = store.find("td").text.strip()
+                print(location_name)
+            except:
                 continue
-            info = validate(store.xpath(".//td//text()")[1]).split(',')
-            street = info[0]
-            if len(info) < 2:
-                if '-' in info[0]:
-                    zipcode = "<MISSING>"
-                else:
-                    zipcode = info[0][-6:]
-            else:
-                zipcode = info.pop()
-                if len(zipcode.split(' ')) > 3:
-                    zipcode = validate(zipcode.split(' ')[2:])
 
-            
-            city = validate(store.xpath(".//td//text()")[2])
-            phone = validate(store.xpath(".//td//text()")[3])
-            geoinfo = validate(store.xpath(".//td/a/@href")).split('@')
-            if len(geoinfo) < 2:
-                geoinfo = ["<MISSING>", "<MISSING>"]
+            raw_address = store.find_all("td")[1].text.replace("J1L1K1", ", J1L 1K1").replace("SK S7K","S7K")
+            street_address = raw_address[:raw_address.rfind(",")].strip()
+            raw_city = store.find_all("td")[2].text.strip()
+            if raw_state == "Atlantic Canada":
+                state = raw_city[-3:].strip()
+                city = raw_city[:raw_city.find(",")]
             else:
-                geoinfo = geoinfo[1].split(',')[:2]
+                city = raw_city
+                state = raw_state
+                
+            try:
+                map_link = store.find_all("td")[4].a['href']
+                at_pos = map_link.rfind("@")
+                latitude = map_link[at_pos+1:map_link.find(",", at_pos)].strip()
+                longitude = map_link[map_link.find(",", at_pos)+1:map_link.find(",", at_pos+15)].strip()
+
+                if not latitude[:2].isnumeric():
+                    driver.get(map_link)
+                    time.sleep(7)
+
+                    try:
+                        map_link = driver.current_url
+                        at_pos = map_link.rfind("@")
+                        latitude = map_link[at_pos+1:map_link.find(",", at_pos)].strip()
+                        longitude = map_link[map_link.find(",", at_pos)+1:map_link.find(",", at_pos+15)].strip()
+                    except:
+                        latitude = "<INACCESSIBLE>"
+                        longitude = "<INACCESSIBLE>"
+            except:
+                latitude = "<MISSING>"
+                longitude = "<MISSING>"
+
+            if street_address == "2102 11th Ave":
+                latitude = 50.4506685
+                longitude = -104.6127555
 
             output = []
-            output.append(base_url) # url
-            output.append(title) #location name
-            output.append(street) #address
+            output.append("sunriserecords.com") # locator_domain
+            output.append(url) # url
+            output.append(location_name) #location name
+            output.append(street_address) #address
             output.append(city) #city
             output.append(state) #state
-            output.append(zipcode) #zipcode
+            output.append(raw_address[raw_address.rfind(",")+1:].strip()) #zipcode
             output.append('CA') #country code
             output.append("<MISSING>") #store_number
-            output.append(phone) #phone
-            output.append("Sunrise Records - Leading Canadian retailer of music, film, games, and pop culture items") #location type
-            output.append(geoinfo[0]) #latitude
-            output.append(geoinfo[1]) #longitude
+            output.append(store.find_all("td")[3].text.strip()) #phone
+            output.append("<MISSING>") #location type
+            output.append(latitude) #latitude
+            output.append(longitude) #longitude
             output.append("<MISSING>") #opening hours
-
             output_list.append(output)
+
+    try:
+        driver.close()
+    except:
+        pass
 
     return output_list
 
@@ -102,3 +129,4 @@ def scrape():
     write_output(data)
 
 scrape()
+
