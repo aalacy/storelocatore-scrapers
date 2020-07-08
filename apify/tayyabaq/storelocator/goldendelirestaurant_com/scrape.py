ac4497b1f2f0696441e4862b25503083e8a4f5cc
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-07-08 18:12:54 +0500

    Add files via upload

--- apify/tayyabaq/storelocator/goldendelirestaurant_com/scrape.py
+++ apify/tayyabaq/storelocator/goldendelirestaurant_com/scrape.py
@@ -1,69 +1,84 @@
+import requests
+from bs4 import BeautifulSoup
 import csv
-import os
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import string
 import re, time
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
-    with open('data.csv', mode='wb') as output_file:
+    with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
-            if row:
-                writer.writerow(row)
-def get_driver():
-    options = Options() 
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    return webdriver.Chrome('chromedriver', chrome_options=options)
+            writer.writerow(row)
+
 
 def fetch_data():
-    #Variables
-    data=[];address_stores=[]; city=[];street_address=[];zipcode=[]; state=[]; latitude=[]; longitude=[]; hours_of_operation=[]; phone=[]
-    #Driver
-    driver = get_driver()
-    #Get site
-    driver.get('http://www.goldendelirestaurant.com/locations/')
-    time.sleep(6)
-    # Fetch stores
-    hours = driver.find_elements_by_class_name("g1-list--empty")
-    hours_of_operation=[hours[i].text for i in range(0,len(hours))]
-    street = driver.find_elements_by_tag_name("h2")
-    street_address = [street[i].text for i in range(0,len(street))]
-    address =driver.find_elements_by_tag_name("h4")
-    for n in range(0,len(address),2):
-        city.append(address[n].text.split(",")[0])
-        zipcode.append(address[n].text.split(",")[1].strip().split()[-1])
-        state.append(address[n].text.split(",")[1].strip().split()[0])
-    for n in range(1,len(address),2):
-        phone.append(address[n].text)
-    for n in range(0,len(street)): 
-        data.append([
-            'https://www.goldendelirestaurant.com',
-            '<MISSING>',
-            street_address[n],
-            city[n],
-            state[n],
-            zipcode[n],
-            'US',
-            '<MISSING>',
-            phone[n],
-            '<MISSING>',
-            '<MISSING>',
-            '<MISSING>',
-            hours_of_operation[n]
-        ])
-    driver.quit()
+    # Your scraper here
+    data = []
+    
+    url = 'https://www.thegoldendeli.com/contacts/'
+    r = session.get(url, headers=headers, verify=False)
+  
+    soup =BeautifulSoup(r.text, "html.parser")
+   
+    divlist = soup.findAll('div', {'class': 'contact-icon'})
+   # print("states = ",len(state_list))
+    p = 0
+    cleanr = re.compile(r'<[^>]+>')
+    for div in divlist:
+        det = div.findAll('p')
+        det =det[1]
+        det = re.sub(cleanr,' ',str(det)).splitlines()
+        street = det[0]
+        city, state = det[1].split(', ',1)
+        state = state.lstrip()
+        state,pcode= state.split(' ',1)
+        phone = det[2]
+        hourlist = div.find('ul',{'class':'list'}).findAll('li')
+        hours = ''
+        for hr in hourlist:
+            hours = hours + hr.text + ' '
+        if len(hours) < 2:
+            hour = '<MISSING>'
+        lat = '<MISSING>'
+        longt  = '<MISSING>'
+        if p == 0:
+            lat = str(soup).split('"lat":')[1].split(',',1)[0]
+            lat = lat[0:9]           
+            longt = str(soup).split('"lng":')[1].split(',',1)[0]
+            longt = longt[0:9]    
+        data.append(['https://goldendelirestaurant.com/',url,city,street,city,state,pcode,'US',
+                        '<MISSING>',
+                        phone,
+                       '<MISSING>',
+                        lat,
+                        longt,
+                        hours
+                    ])
+        print(p,data[p])
+        p += 1
+                
+
+                
+                
+            
+        
     return data
 
+
 def scrape():
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
     data = fetch_data()
     write_output(data)
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
 
 scrape()
