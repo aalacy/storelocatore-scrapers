162b033efd450b36a5fed1b75ecbd864d8164bd7
Kevin Collins <nck@users.noreply.github.com>
2020-07-14 18:18:11 -0400

    scppool.com (SLC-3047): adjust use of sgzip to prevent timeout

--- apify/himanshu/storelocator/scppool_com/scrape.py
+++ apify/himanshu/storelocator/scppool_com/scrape.py
@@ -1,48 +1,15 @@
 import csv
 from sgrequests import SgRequests
-import requests
-from bs4 import BeautifulSoup as bs
-import re
 import json
 import sgzip
 import time
 session = SgRequests()
 
-def request_wrapper(url,method,headers,data=None):
-   request_counter = 0
-   if method == "get":
-       while True:
-           try:
-               r = requests.get(url,headers=headers)
-               return r
-               break
-           except:
-               time.sleep(2)
-               request_counter = request_counter + 1
-               if request_counter > 10:
-                   return None
-                   break
-   elif method == "post":
-       while True:
-           try:
-               if data:
-                   r = requests.post(url,headers=headers,data=data)
-               else:
-                   r = requests.post(url,headers=headers)
-               return r
-               break
-           except:
-               time.sleep(2)
-               request_counter = request_counter + 1
-               if request_counter > 10:
-                   return None
-                   break
-   else:
-       return None
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(output_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
@@ -51,51 +18,55 @@ def write_output(data):
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
-    MAX_RESULTS = 60
-    MAX_DISTANCE = 20
     search = sgzip.ClosestNSearch()
     search.initialize(country_codes=['US'])
     zip_code = search.next_zip()
     current_results_len = 0
     adressess = []
     base_url = "http://scppool.com"
+    headers = {
+        'accept': 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+    }
     while zip_code:
-        result_coords =[]
-        # r = request_wrapper
-        #print("remaining zipcodes: " + str(len(search.zipcodes)))
+        result_coords = []
+        # print(f"zip code: {zip_code}")
+        # print("remaining zipcodes: " + str(len(search.zipcodes)))
         try:
-            json_data = session.get("http://scppool.com/map_api/?z="+str(zip_code)).json()
-        except:
+            json_data = session.get(
+                "http://scppool.com/map_api/?z="+str(zip_code), headers=headers).json()
+        except Exception as ex:
+            print(f'Exception getting {zip_code}', ex)
             continue
-        for store in json_data['locations']:            
+
+        for store in json_data['locations']:
             location_name = store['sitename']+' '+store['sitenumber']
             street_address = store['address1']
-            city = store['city'].replace("Vereda La Isla, Cundinamarca","Vereda La Isla")
-            state = store['state'].replace("--",'Cundinamarca')
+            city = store['city'].replace(
+                "Vereda La Isla, Cundinamarca", "Vereda La Isla")
+            state = store['state'].replace("--", 'Cundinamarca')
             zipp = store['zip']
-            phone=''
+            phone = ''
             if "phone" in store:
                 phone = store['phone']
             lat = store['latlon'].split(",")[0]
             lng = store['latlon'].split(",")[1]
             store_number = store['sitenumber']
-            if store_number.isdigit()==True:
-                store_number = store_number
-            else:
-                store_number = "<MISSING>"
+            # print(f'store number: {store_number}')
             page_url = "<MISSING>"
-            hours = ""
-            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',}
             hours = "<MISSING>"
-            result_coords.append((lat,lng))
+            result_coords.append((lat, lng))
             store = []
             store.append(base_url)
             store.append(location_name)
             store.append(street_address)
             store.append(city)
             store.append(state)
-            store.append(zipp)   
+            store.append(zipp)
             store.append("US")
             store.append(store_number)
             store.append(phone)
@@ -103,28 +74,26 @@ def fetch_data():
             store.append(lat)
             store.append(lng)
             store.append(hours if hours else "<MISSING>")
-            store.append(page_url)     
+            store.append(page_url)
             if store[2] in adressess:
+                # print(f'>>> already have store at {store[2]} <<<')
                 continue
-            adressess.append(store[2]) 
-            store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+            adressess.append(store[2])
+            store = [str(x).encode('ascii', 'ignore').decode(
+                'ascii').strip() if x else "<MISSING>" for x in store]
             if "KM 7.5 Autopista a Medellin" in store:
                 pass
             else:
                 yield store
             # print(store)
-        if len(json_data) < MAX_RESULTS:
-            # print("max distance update")
-            search.max_distance_update(MAX_DISTANCE)
-        elif len(json_data) == MAX_RESULTS:
-            # print("max count update")
-            search.max_count_update(result_coords)
-        else:
-            raise Exception("expected at most " + str(MAX_RESULTS) + " results")
+
+        search.max_count_update(result_coords)
         zip_code = search.next_zip()
+
 
 def scrape():
     data = fetch_data()
     write_output(data)
+
 
 scrape()
