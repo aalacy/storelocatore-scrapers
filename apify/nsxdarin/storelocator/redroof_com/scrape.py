9d183e2e9b1bebf72b47c5d55f99b48a18653bc3
Kevin Collins <nck@users.noreply.github.com>
2020-07-15 17:43:17 -0400

    redroof.com (SLC-1108):   - upgrade Dockerfile to use ssl upgrade base image   - use JSON instead of iter_lines   - override sgrequests max retries   - try with new session on any exception

--- apify/nsxdarin/storelocator/redroof_com/scrape.py
+++ apify/nsxdarin/storelocator/redroof_com/scrape.py
@@ -1,30 +1,68 @@
 import csv
-import urllib2
 import requests
 import os
+import re
+import json
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from requests.exceptions import ConnectionError
 from sgrequests import SgRequests
-import collections 
+import collections
+
+
+def override_retries():
+    # monkey patch sgrequests in order to set max retries
+    def new_init(self):
+        requests.packages.urllib3.disable_warnings()
+        self.session = self.requests_retry_session(retries=0)
+
+    SgRequests.__init__ = new_init
+
+
+override_retries()
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
 }
 
+re_get_json = re.compile(
+    'Utilities\.SDL\.Add\("ServicePropertyDetails", (.+?)\);')
+
+
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer = csv.writer(output_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip",
+                         "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
 
+
+def get_json_data(html):
+    match = re.search(re_get_json, html)
+    json_text = match.group(1)
+    return json.loads(json_text)
+
+
+def get_sitemap(attempts=1):
+    if attempts > 10:
+        print("Couldn't get sitemap after 10 attempts")
+        raise SystemExit
+    try:
+        session = SgRequests()
+        url = 'https://www.redroof.com/sitemap.xml'
+        r = session.get(url, headers=headers)
+        return r
+    except (ConnectionError, Exception) as ex:
+        print("Exception getting sitemap", ex)
+        return get_sitemap(attempts=attempts+1)
+
+
 def fetch_data():
-    url = 'https://www.redroof.com/sitemap.xml'
     locs = []
-    session = SgRequests()
-    r = session.get(url, headers=headers)
-    for line in r.iter_lines():
+    r = get_sitemap()
+    for line in r.iter_lines(decode_unicode=True):
         if 'https://www.redroof.com/property/' in line:
             lurl = line.split('<loc>')[1].split('<')[0]
             locs.append(lurl)
@@ -44,17 +82,19 @@ def fetch_data():
         zc = ''
         phone = ''
         website = 'redroof.com'
-        typ = '<MISSING>'
-        lat = '<MISSING>'
-        lng = '<MISSING>'
+        typ = ''
+        lat = ''
+        lng = ''
         hours = '<MISSING>'
-        store = loc.rsplit('/',1)[1]
+        store = loc.rsplit('/', 1)[1]
         r2 = None
         try:
             session = SgRequests()
+            print(loc)
             r2 = session.get(loc, headers=headers)
-        except ConnectionError:
+        except (ConnectionError, Exception) as ex:
             print('Failed to connect to ' + loc)
+            print("Exception: ", ex)
             if attempts.get(loc, 0) >= 3:
                 print('giving up on ' + loc)
             else:
@@ -62,32 +102,32 @@ def fetch_data():
                 attempts[loc] = attempts.get(loc, 0) + 1
                 print('attempts: ' + str(attempts[loc]))
             continue
-        for line2 in r2.iter_lines():
-            if 'name="og:title" content="' in line2:
-                try:
-                    name = line2.split(' | ')[1].split('"')[0]
-                except:
-                    name = 'Red Roof Inn'
-            if 'property="s:streetAddress" content="' in line2:
-                add = line2.split('property="s:streetAddress" content="')[1].split('"')[0]
-            if 'property="s:addressLocality" content="' in line2:
-                city = line2.split('property="s:addressLocality" content="')[1].split('"')[0]
-            if 'property="s:addressRegion" content="' in line2:
-                state = line2.split('property="s:addressRegion" content="')[1].split('"')[0]
-            if 'property="s:postalCode" content="' in line2:
-                zc = line2.split('property="s:postalCode" content="')[1].split('"')[0]
-            if 'itemprop="telephone">' in line2:
-                phone = line2.split('itemprop="telephone">')[1].split('<')[0]
-        if 'Red Roof' not in name:
-            name = 'Red Roof Inn'
-        if 'PLUS+' in name:
-            typ = 'Red Room PLUS+'
-        else:
-            typ = 'Red Roof Inn'
-        yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
+        data = get_json_data(r2.text)
+
+        name = data["Description"]
+        add = data["Street1"] + (", " + data["Street2"]
+                                 if data["Street2"] else "")
+        city = data["City"]
+        state = data["State"]
+        zc = data["PostalCode"]
+        phone = data["PhoneNumber"]
+        typ = data["PropertyType"]
+        lat = data["Latitude"]
+        lng = data["Longitude"]
+
+        location = [website, loc, name, add, city, state,
+                    zc, country, store, phone, typ, lat, lng, hours]
+        location = [str(x).encode('ascii', 'ignore').decode(
+            'ascii').strip() if x else "<MISSING>" for x in location]
+        print(location)
+        print('---------')
+        yield location
+
 
 def scrape():
     data = fetch_data()
     write_output(data)
+
 
 scrape()
