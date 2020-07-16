1dd0c8d0a92c010c83c73d6061b9c83187c5775d
Quincy <qcalexander@gmail.com>
2020-07-16 17:04:24 -0400

    pizzacucinova_com fixed

--- apify/quincya/storelocator/pizzacucinova_com/scrape.py
+++ apify/quincya/storelocator/pizzacucinova_com/scrape.py
@@ -1,4 +1,4 @@
-import requests
+from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import csv
 import re
@@ -9,7 +9,7 @@ def write_output(data):
 		writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
 		# Header
-		writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+		writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
 		# Body
 		for row in data:
 			writer.writerow(row)
@@ -21,7 +21,9 @@ def fetch_data():
 	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
 	headers = {'User-Agent' : user_agent}
 
-	req = requests.get(base_link, headers=headers)
+	session = SgRequests()
+
+	req = session.get(base_link, headers=headers)
 
 	try:
 		base = BeautifulSoup(req.text,"lxml")
@@ -35,7 +37,7 @@ def fetch_data():
 	for item in items:
 		link = item.find('a')['href']
 
-		req = requests.get(link, headers=headers)
+		req = session.get(link, headers=headers)
 		try:
 			base = BeautifulSoup(req.text,"lxml")
 		except (BaseException):
@@ -58,9 +60,7 @@ def fetch_data():
 		location_type = "<MISSING>"
 		hours_of_operation = base.find('div', attrs={'class': 'normal-hours'}).get_text(separator=u' ').replace("\n"," ").replace("\xa0","").strip()
 		hours_of_operation = re.sub(' +', ' ', hours_of_operation)
-
-		req = requests.get(link, headers=headers)
-
+		
 		try:
 			new_base = BeautifulSoup(req.text,"lxml")
 		except (BaseException):
@@ -73,7 +73,7 @@ def fetch_data():
 		semi_point = raw_gps.find("longitude",start_point)
 		longitude = raw_gps[semi_point+12:raw_gps.find('}',semi_point)].strip()
 
-		data.append([locator_domain, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+		data.append([locator_domain, link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
 
 	return data
 
