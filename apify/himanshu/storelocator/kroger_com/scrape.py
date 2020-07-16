948d6a8a2781bc58dfd14fe39c71aac91bfb1129
diapk <Hank4957!>
2020-07-16 12:22:12 +0530

    docker run and push

--- apify/himanshu/storelocator/kroger_com/scrape.py
+++ apify/himanshu/storelocator/kroger_com/scrape.py
@@ -5,7 +5,6 @@ import re
 import sgzip
 import json
 import phonenumbers
-import requests
 session = SgRequests()
 def write_output(data):
     with open('data.csv', mode='w', newline='') as output_file:
@@ -35,7 +34,7 @@ def fetch_data():
         }
         data = r'{"query":"\n      query storeSearch($searchText: String!, $filters: [String]!) {\n        storeSearch(searchText: $searchText, filters: $filters) {\n          stores {\n            ...storeSearchResult\n          }\n          fuel {\n            ...storeSearchResult\n          }\n          shouldShowFuelMessage\n        }\n      }\n      \n  fragment storeSearchResult on Store {\n    banner\n    vanityName\n    divisionNumber\n    storeNumber\n    phoneNumber\n    showWeeklyAd\n    showShopThisStoreAndPreferredStoreButtons\n    storeType\n    distance\n    latitude\n    longitude\n    tz\n    ungroupedFormattedHours {\n      displayName\n      displayHours\n      isToday\n    }\n    address {\n      addressLine1\n      addressLine2\n      city\n      countryCode\n      stateCode\n      zip\n    }\n    pharmacy {\n      phoneNumber\n    }\n    departments {\n      code\n    }\n    fulfillmentMethods{\n      hasPickup\n      hasDelivery\n    }\n  }\n","variables":{"searchText":"'+str(zip_code)+'","filters":[]},"operationName":"storeSearch"}'
         # try:
-        r = requests.post('https://www.kroger.com/stores/api/graphql', headers=headers,data=data)
+        r = session.post('https://www.kroger.com/stores/api/graphql', headers=headers,data=data)
         # except:
         #     continue
         datas = r.json()['data']['storeSearch']['stores']        
