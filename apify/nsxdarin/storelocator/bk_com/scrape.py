bc2829ded732a660f2c3877d2300507a731934af
nsxdarin <nsxdarin@gmail.com>
2020-07-17 09:46:29 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/bk_com/scrape.py
+++ apify/nsxdarin/storelocator/bk_com/scrape.py
@@ -1,6 +1,5 @@
 import csv
 from sgrequests import SgRequests
-import json
 
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
