fd85d41b159ddca308fe11c1fe1e4459e285cd1b
diapk <Hank4957!>
2020-07-18 15:49:24 +0530

    docker run and push

--- apify/himanshu/storelocator/labcorp_com/scrape.py
+++ apify/himanshu/storelocator/labcorp_com/scrape.py
@@ -1,50 +1,17 @@
 # coding=UTF-8
 
 import csv
-from sgrequests import SgRequests
+from  sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
 import sgzip
-import time
-
-
 session = SgRequests()
 
-def request_wrapper(url,method,headers,data=None):
-    request_counter = 0
-    if method == "get":
-        while True:
-            try:
-                r = session.get(url,headers=headers)
-                return r
-                break
-            except:
-                time.sleep(2)
-                request_counter = request_counter + 1
-                if request_counter > 10:
-                    return None
-                    break
-    elif method == "post":
-        while True:
-            try:
-                if data:
-                    r = session.post(url,headers=headers,data=data)
-                else:
-                    r = session.post(url,headers=headers)
-                return r
-                break
-            except:
-                time.sleep(2)
-                request_counter = request_counter + 1
-                if request_counter > 10:
-                    return None
-                    break
-    else:
-        return None
+
 
 def write_output(data):
-    with open('data.csv', mode='w', encoding="utf-8") as output_file:
+    with open('data.csv', mode='w', encoding="utf-8", newline='') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
@@ -58,65 +25,73 @@ def fetch_data():
     addresses = []
     search = sgzip.ClosestNSearch()
     search.initialize()
-    MAX_RESULTS = 50
-    MAX_DISTANCE = 10
+    MAX_RESULTS = 100
+    MAX_DISTANCE = 50
     current_results_len = 0     # need to update with no of count.
-    zip_code = search.next_zip()
+    coord = search.next_coord()
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
     }
 
     base_url = "https://www.labcorp.com/"
 
-    while zip_code:
+    while coord:
         result_coords = []
-        # print("zip_code === "+zip_code)
-        location_url = "https://www.labcorp.com/labs-and-appointments/results?zip="+str(zip_code)+"&service=PEDIATRIC&radius=25"
+      #  print(search.current_zip)
+       # print("remaining zipcodes: " + str(len(search.zipcodes)))
+        location_url = "https://www.labcorp.com/labs-and-appointments/results?lat="+str(coord[0])+"&lon="+str(coord[1])+"&radius="+str(MAX_DISTANCE)
+        # print(location_url)
         try:
-            r = request_wrapper(location_url,'get',headers=headers)
+            r = session.get(location_url, headers=headers)
         except:
-            continue
+            pass
+        
         soup = BeautifulSoup(r.text, "lxml") 
-        data = soup.find("script",{"type":"application/json"})
-        if data!=None:
-            data = soup.find("script",{"type":"application/json"}).text
-            json_data = json.loads(data)
-            if "lc_psc_locator" in json_data:
-                if "psc_locator_app" in json_data['lc_psc_locator']:
-                    for i in json_data['lc_psc_locator']['psc_locator_app']['settings']['labs']:  
-                        #print(i)
-                        current_results_len = len(json_data['lc_psc_locator']['psc_locator_app']['settings']['labs']) 
-                        location_name = i['name']
-                        store_number = i['locatorId']
-                        street_address = i['address']['street']
-                        city = i['address']['city']
-                        state = i['address']['stateAbbr']
-                        zipp = i['address']['postalCode']
-                        phone = i['phone']
-                        latitude = i['address']['lat']
-                        longitude = i['address']['lng']
-                        hours_of_operation = i['hours']
+        data = soup.find("script",{"type":"application/json"}).text
+        json_data = json.loads(data)
+        if "lc_psc_locator" in json_data:
+            if "psc_locator_app" in json_data['lc_psc_locator']:
+                current_results_len = len(json_data['lc_psc_locator']['psc_locator_app']['settings']['labs'])
+                #print(current_results_len)
+                for i in json_data['lc_psc_locator']['psc_locator_app']['settings']['labs']:  
+                     
+                    location_name = i['name']
+                    store_number = i['locatorId']
+                    street_address = i['address']['street']
+                    city = i['address']['city']
+                    state = i['address']['stateAbbr']
+                    zipp = i['address']['postalCode']
+                    phone = i['phone']
+                    latitude = i['address']['lat']
+                    longitude = i['address']['lng']
+                    hours_of_operation = i['hours']
 
-                        result_coords.append((latitude, longitude))
-                        store = []
-                        store.append(base_url)
-                        store.append(location_name)
-                        store.append(street_address)
-                        store.append(city)
-                        store.append(state)
-                        store.append(zipp)
-                        store.append("US")
-                        store.append(store_number) 
-                        store.append(phone)
-                        store.append("<MISSING>")
-                        store.append(latitude)
-                        store.append(longitude)
-                        store.append(hours_of_operation)
-                        store.append("<MISSING>")
-                        if store[2] in addresses:
-                            continue
-                        addresses.append(store[2])
-                        yield store
+                    result_coords.append((latitude, longitude))
+                    store = []
+                    store.append(base_url)
+                    store.append(location_name)
+                    store.append(street_address)
+                    store.append(city)
+                    store.append(state)
+                    store.append(zipp)
+                    store.append("US")
+                    store.append(store_number) 
+                    store.append(phone)
+                    store.append("<MISSING>")
+                    store.append(latitude)
+                    store.append(longitude)
+                    store.append(hours_of_operation)
+                    store.append("<MISSING>")
+                    if store[2] in addresses:
+                        continue
+                    addresses.append(store[2])
+                    yield store
+            else:
+                current_results_len = 0
+                pass
+        else:
+            current_results_len = 0
+            pass
 
         if current_results_len < MAX_RESULTS:
             # print("max distance update")
@@ -126,7 +101,8 @@ def fetch_data():
             search.max_count_update(result_coords)
         else:
             raise Exception("expected at most " + str(MAX_RESULTS) + " results")
-        zip_code = search.next_zip()
+        coord = search.next_coord()
+
     
 
 def scrape():
