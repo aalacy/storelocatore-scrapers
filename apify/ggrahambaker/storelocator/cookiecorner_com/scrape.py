4a9b2cb3ff8807c48ef0ddf954d7ce7de8bc0b3a
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-07-20 20:39:24 +0500

    Add files via upload

--- apify/ggrahambaker/storelocator/cookiecorner_com/scrape.py
+++ apify/ggrahambaker/storelocator/cookiecorner_com/scrape.py
@@ -1,89 +1,125 @@
+import requests
+from bs4 import BeautifulSoup
 import csv
-import os
-from sgselenium import SgSelenium
+import string
+import re, time
 import usaddress
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
-    locator_domain = 'https://cookiecorner.com/'
-    ext = 'about/store_locations'
-
-    driver = SgSelenium().chrome()
-    driver.get(locator_domain + ext)
-
-    main = driver.find_element_by_css_selector('div#main')
-    content = main.text.split('\n')
-    cycle = 0
-    all_store_data = []
-    for i, cont in enumerate(content[1:]):
-        if cont == '':
-            cycle = -1
-
-            lat = '<MISSING>'
-            longit = '<MISSING>'
-
-            country_code = 'US'
-            location_type = '<MISSING>'
-            store_number = '<MISSING>'
-            store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code,
-                          store_number, phone_number, location_type, lat, longit, hours]
-            all_store_data.append(store_data)
-
-        elif cycle == 0:
-            location_name = cont
-        elif cycle == 1:
-            phone_number = cont
-        elif cycle == 2:
-            if 'Kapolei' in cont:
-                street_address = '91 - 590 Farrington Highway # 115'
-                city = 'Kapolei'
-                state = 'HI'
-                zip_code = '96707'
+    # Your scraper here
+    data = []
+    p = 0
+    cleanr = re.compile(r'<[^>]+>')
+    url = 'https://cookiecorner.com/about/store_locations'
+    r = session.get(url, headers=headers, verify=False)  
+    soup =BeautifulSoup(r.text, "html.parser")
+    maindiv = str(soup.find('div',{'id':'main'}))
+    maindiv = maindiv[maindiv.find('<h3>'):maindiv.find('<br></div>',maindiv.find('<h3>'))]
+    maindiv = re.sub(cleanr,'\n',str(maindiv)).splitlines()
+    m = 0
+    flag = 0
+    title = ''
+    address = ''
+    phone = ''
+    hours = ''
+    while m < len(maindiv):
+        #print(maindiv[i])
+        
+        if maindiv[m] == '':
+            pass
+        else:
+            if maindiv[p].islower() == False and (flag == 0 or flag == 4) and maindiv[m].find('Sunday') == -1:
+                #print(maindiv[m])
+                if flag == 4:
+                    #print(p,"Result=",title,address,phone,hours)
+                    address = usaddress.parse(address)
+                    i = 0
+                    street = ""
+                    city = ""
+                    state = ""
+                    pcode = ""
+                    while i < len(address):
+                        temp = address[i]
+                        if temp[1].find("Address") != -1 or temp[1].find("Street") != -1 or temp[1].find("Recipient") != -1 or temp[1].find("BuildingName") != -1 or temp[1].find("USPSBoxType") != -1 or temp[1].find(
+                            "USPSBoxID") != -1:
+                            street = street + " " + temp[0]
+                        if temp[1].find("PlaceName") != -1:
+                            city = city + " " + temp[0]
+                        if temp[1].find("StateName") != -1:
+                            state = state + " " + temp[0]
+                        if temp[1].find("ZipCode") != -1:
+                            pcode = pcode + " " + temp[0]
+                        i += 1
+                    if len(pcode) < 3:
+                        pcode = '<MISSING>'
+                        
+                    data.append([
+                        'https://cookiecorner.com/',
+                        'https://cookiecorner.com/about/store_locations',                   
+                        title,
+                        street.lstrip().replace(',',''),
+                        city.lstrip().replace(',',''),
+                        state.lstrip(),
+                        pcode.lstrip(),
+                        'US',
+                        '<MISSING>',
+                        phone,
+                        '<MISSING>',
+                        '<MISSING>',
+                        '<MISSING>',
+                        hours.rstrip()
+                    ])
+                    #print(p,data[p])
+                    p += 1
+                    title = ''
+                    address = ''
+                    phone = ''
+                    hours = ''
+                   
+                
+                flag = 1
+                title = maindiv[m]
+                #input(flag)
+                    
+            elif phone == '' and maindiv[m].find('-') > -1 and flag == 1:
+                phone = maindiv[m]
+                flag = 2
+            elif address == '' and flag == 2:
+                address = maindiv[m]
+                flag = 3
+            elif (flag == 3 or flag == 4) and (maindiv[m].find('p.m.')> -1 or maindiv[m].find('COVID-19') > -1) :               
+                hours =    hours + maindiv[m]+ ' '
+                flag = 4
             else:
-                parsed_add = usaddress.tag(cont)[0]
+                pass
+            
+     
+        m += 1
+     
+        
+    return data
 
-                street_address = ''
-                street_address += parsed_add['AddressNumber'] + ' '
-                if 'StreetNamePreDirectional' in parsed_add:
-                    street_address += parsed_add['StreetNamePreDirectional'] + ' '
-
-                street_address += parsed_add['StreetName'] + ' '
-                if 'StreetNamePostType' in parsed_add:
-                    street_address += parsed_add['StreetNamePostType'] + ' '
-                if 'OccupancyType' in parsed_add:
-                    street_address += parsed_add['OccupancyType'] + ' '
-                if 'OccupancyIdentifier' in parsed_add:
-                    street_address += parsed_add['OccupancyIdentifier'] + ' '
-
-                street_address = street_address.strip()
-                city = parsed_add['PlaceName']
-                state = parsed_add['StateName']
-                if 'ZipCode' in parsed_add:
-                    zip_code = parsed_add['ZipCode']
-                else:
-                    zip_code = '<MISSING>'
-
-        elif cycle == 4:
-            hours = cont
-        elif cycle == 5:
-            hours += ' ' + cont
-
-        cycle += 1
-
-    driver.quit()
-    return all_store_data
 
 def scrape():
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
     data = fetch_data()
     write_output(data)
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
 
 scrape()
