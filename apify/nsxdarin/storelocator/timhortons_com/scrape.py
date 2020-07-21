ce4dac332879092a9faa5405e5203a905ef32bf6
nsxdarin <nsxdarin@gmail.com>
2020-07-21 10:40:36 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/timhortons_com/scrape.py
+++ apify/nsxdarin/storelocator/timhortons_com/scrape.py
@@ -8,7 +8,7 @@ headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
 
@@ -21,6 +21,7 @@ def fetch_data():
             items = line.split('{"_id":"restaurant_')
             for item in items:
                 if '{"ms":' not in item:
+                    loc = '<MISSING>'
                     website = 'timhortons.com'
                     store = item.split('"number":"')[1].split('"')[0]
                     name = item.split('"name":"')[1].split('"')[0]
@@ -52,7 +53,7 @@ def fetch_data():
                         hours = hours + '; Sun: ' + days.split('"sunOpen":"')[1].split(' ')[1].split(':00"')[0] + '-' + days.split('"sunClose":"')[1].split(' ')[1].split(':00"')[0]
                     if hours == '':
                         hours = '<MISSING>'
-                    yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+                    yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
