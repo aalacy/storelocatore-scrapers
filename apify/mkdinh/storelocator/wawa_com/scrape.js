8bb6b6eb6fe9ec444a97dfd54bda5390c025ca13
Michael Dinh <mkdinh94@gmail.com>
2020-07-23 20:21:03 -0400

    SLC-2150/wawa_com: move code over to mkdinh

--- apify/mkdinh/storelocator/wawa_com/scrape.js
+++ apify/mkdinh/storelocator/wawa_com/scrape.js
@@ -1,45 +1,35 @@
 const Apify = require('apify');
+const BASE_URL = 'https://www.wawa.com';
+const SITEMAP = 'site-map';
+
+async function handlePageFunction({ request, page }) {
+  // Begin scraper
+  const siteMapLinks = await page.$('a.CMSSiteMapLink');
+  await setTimeout(() => {}, 2000);
+  siteMapLinks.each(async (link) => {
+    const href = await link.attr('href');
+    console.log(href);
+  });
+}
 
 Apify.main(async () => {
-	const requestQueue = await Apify.openRequestQueue();
-	await requestQueue.addRequest({ url: 'https://safegraph.com/' });
+  const url = `${BASE_URL}/${SITEMAP}`;
+  const requestQueue = await Apify.openRequestQueue();
+  requestQueue.addRequest({ url });
+  const useProxy = process.env.USE_PROXY;
 
-	const useProxy = process.env.USE_PROXY;
+  const crawler = new Apify.PuppeteerCrawler({
+    requestQueue,
+    handlePageFunction,
+    maxConcurrency: 1,
+    maxRequestsPerCrawl: 1,
+    launchPuppeteerOptions: {
+      headless: false,
+      stealth: true,
+      useChrome: true,
+      useApifyProxy: !!useProxy,
+    },
+  });
 
-	const crawler = new Apify.PuppeteerCrawler({
-		requestQueue,
-		handlePageFunction: async ({ request, page }) => {
-
-			// Begig scraper
-
-			const heading = await page.evaluate(() => document.querySelector('h1.heading-primary').innerText);
-
-			const poi = {
-				locator_domain: 'safegraph.com',
-				page_url: '<MISSING>',
-				location_name: heading,
-				street_address: '1543 mission st',
-				city: 'san francisco',
-				state: 'CA',
-				zip: '94107',
-				country_code: 'US',
-				store_number: '<MISSING>',
-				phone: '<MISSING>',
-				location_type: '<MISSING>',
-				latitude: 37.773500,
-				longitude: -122.417774,
-				hours_of_operation: '<MISSING>',
-			};
-
-			await Apify.pushData([poi]);
-
-			// End scraper
-
-		},
-		maxRequestsPerCrawl: 100,
-		maxConcurrency: 10,
-		launchPuppeteerOptions: {headless: true, stealth: true, useChrome: true, useApifyProxy: !!useProxy},
-	});
-
-	await crawler.run();
+  await crawler.run();
 });
