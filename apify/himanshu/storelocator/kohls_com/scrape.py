054519fb2334f78fcac9360142ca4235d1f4937f
Kevin Collins <nck@users.noreply.github.com>
2020-07-24 11:12:37 -0400

    kohls.com (SLC-1440): reset session on request exception

--- apify/himanshu/storelocator/kohls_com/scrape.py
+++ apify/himanshu/storelocator/kohls_com/scrape.py
@@ -5,7 +5,7 @@ import re
 import json
 import time
 session = SgRequests()
-import requests
+
 
 def write_output(data):
     with open('data.csv', mode='w', newline="") as output_file:
@@ -16,25 +16,43 @@ def write_output(data):
         # Body
         for row in data:
             writer.writerow(row)
+
+
+def get(url, headers, attempts=1): 
+    global session
+    if attempts == 10: 
+        print(f'could not get {url} after {attempts} tries... giving up')
+        raise SystemExit
+    try: 
+        # print(url)
+        r = session.get(url, headers=headers)
+        return r
+    except Exception as ex: 
+        print(f'>>> exception getting {url} : {ex}')
+        print(f'>>> reset session and try again')
+        session = SgRequests()
+        return get(url, headers, attempts+1)
+
+
 def fetch_data():
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
     }
     base_url= "https://www.kohls.com/stores.shtml"
-    r = requests.get(base_url,headers=headers)
+    r = get(base_url,headers=headers)
     address =[]
     soup= BeautifulSoup(r.text,"lxml")
     for state_link in soup.find("div",{"class":"tlsmap_list"}).find_all("a",{"class":"regionlist"}):
-        r1 = requests.get(state_link['href'],headers=headers)
+        r1 = get(state_link['href'], headers=headers)
         soup1 = BeautifulSoup(r1.text,"lxml")
         for city_link in soup1.find("div",{"class":"tlsmap_list"}).find_all("a",{"class":"citylist"}):
-            r2 = requests.get(city_link['href'],headers=headers)
+            r2 = get(city_link['href'], headers=headers)
             soup2 = BeautifulSoup(r2.text,"lxml")
             try:
                 for url in soup2.find("div",{"class":"tlsmap_list"}).find_all("span",{"class":"location-title"}):
                     page_url = url.find("a")['href']
                     store_number = page_url.split("-")[-1].split(".s")[0]
-                    r3 = requests.get(page_url ,headers=headers)
+                    r3 = get(page_url, headers=headers)
                     soup3 = BeautifulSoup(r3.text,"lxml")
                     json_data = json.loads(re.sub(r"\s+", " ",soup3.find("script",{"type":"application/ld+json"}).text.strip()))
                     if type(json_data)==list: 
