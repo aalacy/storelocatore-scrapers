3c4d8d6807b445999856f812e4827dff333e0caf
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-07-25 16:13:10 +0500

    Add files via upload

--- apify/ektasg/storelocator/youthlandacademy_com/scrape.py
+++ apify/ektasg/storelocator/youthlandacademy_com/scrape.py
@@ -1,98 +1,85 @@
-import time
+from bs4 import BeautifulSoup
 import csv
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import re
+import string
+import re, time
 import json
+from sgrequests import SgRequests
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-
-#driver = webdriver.Chrome("C:\chromedriver.exe", options=options)
-driver = webdriver.Chrome("chromedriver", options=options)
-#driver2 = webdriver.Chrome("C:\chromedriver.exe", options=options)
-driver2 = webdriver.Chrome("chromedriver", options=options)
-
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
 
-def parse_geo(url):
-    lon = re.findall(r'\,(--?[\d\.]*)', url)[0]
-    lat = re.findall(r'\@(-?[\d\.]*)', url)[0]
-    return lat, lon
-
-
 def fetch_data():
+    # Your scraper here
     data = []
-    driver.get("https://www.youthlandacademy.com/location")
-    time.sleep(10)
-    page_url = "https://www.youthlandacademy.com/location"
-    text = driver.find_element_by_xpath("/html/body/script[2]").get_attribute('innerText')
-    text_1 = text.split('MapifyPro.Google(center')[1].split(', zoom,')[1]
-    text_1 = text_1.split(', map_instance,')[0]
-    req_json = json.loads(text_1)
-    count =0
-    for i in range(len(req_json)):
-        name = req_json[i]['post_title']
-        street1 = req_json[i]['tooltip_content'].split('<br />')[1]
-        state = req_json[i]['tooltip_content'].split('<br />')[2].split('</p>')[0].split(',')[1].split(" ")[1]
-        zipcode = req_json[i]['pin_zip']
-        city = req_json[i]['pin_city']
-        lat = req_json[i]['google_coords'][0]
-        lng = req_json[i]['google_coords'][1]
-        country = req_json[i]['tooltip_content'].split('<br />')[2].split('</p>')[0].split(',')[2]
-        phone = req_json[i]['tooltip_content'].split('<br />')[2].split('</i>')[1]
-        store_id = req_json[i]['ID']
+    cleanr = re.compile(r'<[^>]+>')    
+    url = 'https://www.youthlandacademy.com/location'
+    p = 0
+    r = session.get(url, headers=headers, verify=False)
+    r = r.text.split('MapifyPro.Google(center')[1].split(', zoom,')[1]
+    r = r.split(', map_instance,')[0]    
+    loclist = json.loads(r)
+    for loc in loclist:
+        title = loc['post_title']
+        link = 'https://www.youthlandacademy.com/locations/'+title.lower().replace(' ','-')+'?location='+title
+        #print(link)
+        #input()
+        lat = loc['google_coords'][0]
+        longt = loc['google_coords'][1]
+        det = BeautifulSoup(loc['tooltip_content'],'html.parser')
+        det = det.text.splitlines()
+        street = det[0]
+        city,state = det[1].split(', ',1)
+        state = state.lstrip()
+        state,pcode = state.split(' ',1)
         try:
-            hour = req_json[i]['tooltip_content'].split('<br />')[4].replace('</p>', '').replace('\n', '').replace(
-                '<strong>', '').replace('</strong>', '')
+            pcode = pcode.split(', ',1)[0]
         except:
             pass
-
-        data.append([
-            'www.youthlandacademy.com',
-            page_url,
-            name,
-            street1,
-            city,
-            state,
-            zipcode,
-            country,
-            store_id,
-            phone,
-            '<MISSING>',
-            lat,
-            lng,
-            hour
-        ])
-        count+=1
-        print(count)
-
-    time.sleep(3)
-    driver.quit()
-    driver2.quit()
+        ccode = 'US'
+        phone = det[2]
+        hours = det[4].replace('am',' am ').replace('pm',' pm ')
+        if phone.find('Opening soon!') == -1:
+            data.append([
+                    'https://www.youthlandacademy.com/',
+                    link,                   
+                    title,
+                    street,
+                    city,
+                    state,
+                    pcode,
+                    ccode,
+                    '<MISSING>',
+                    phone,
+                    '<MISSING>',
+                    lat,
+                    longt,
+                    hours
+                ])
+            print(p,data[p])
+            p += 1
+        
+                    
+        
     return data
 
 
 def scrape():
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
     data = fetch_data()
     write_output(data)
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
 
 scrape()
-
-
-
-
 
