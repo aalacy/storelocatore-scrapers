9259caa2c5d45d54e16e9da489f85e88808f0f44
nsxdarin <nsxdarin@gmail.com>
2020-07-27 10:20:04 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/starbucks_co_uk/scrape.py
+++ apify/nsxdarin/storelocator/starbucks_co_uk/scrape.py
@@ -35,25 +35,25 @@ def fetch_data():
                 for item in array['stores']:
                     website = 'starbucks.co.uk'
                     store = item['storeNumber']
-                    name = item['name'].encode('utf-8')
+                    name = item['name']
                     phone = item['phoneNumber']
                     lat = item['coordinates']['latitude']
                     lng = item['coordinates']['longitude']
-                    add = item['address']['streetAddressLine1'].encode('utf-8')
+                    add = item['address']['streetAddressLine1']
                     try:
-                        add = add + ' ' + item['address']['streetAddressLine2'].encode('utf-8')
+                        add = add + ' ' + item['address']['streetAddressLine2']
                     except:
                         pass
                     try:
-                        add = add + ' ' + item['address']['streetAddressLine3'].encode('utf-8')
+                        add = add + ' ' + item['address']['streetAddressLine3']
                     except:
                         pass
                     add = add.strip()
-                    city = item['address']['city'].encode('utf-8')
+                    city = item['address']['city']
                     state = item['address']['countrySubdivisionCode']
                     country = item['address']['countryCode']
                     zc = item['address']['postalCode']
-                    typ = item['brandName'].encode('utf-8')
+                    typ = item['brandName']
                     hours = ''
                     weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
                     today = weekdays[weekday]
