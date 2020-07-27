f13a48242a5527d36e2fbfaae59ee362235adb92
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-07-27 16:43:14 +0500

    Add files via upload

--- apify/ektasg/storelocator/mexicaninncafe_com/scrape.py
+++ apify/ektasg/storelocator/mexicaninncafe_com/scrape.py
@@ -1,108 +1,122 @@
-import time
+from bs4 import BeautifulSoup
 import csv
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import re
+import string
+import re, time
 import json
+from sgrequests import SgRequests
 
-
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-#driver = webdriver.Chrome("C:\chromedriver.exe", options=options)
-driver = webdriver.Chrome("chromedriver", options=options)
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "page_url" , "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
 
-def parse_geo(url):
-    lon = re.findall(r'\,(--?[\d\.]*)', url)[0]
-    lat = re.findall(r'\@(-?[\d\.]*)', url)[0]
-    return lat, lon
-
-
 def fetch_data():
     # Your scraper here
-    count=0
-    data=[]
-    driver.get("https://www.mexicaninncafe.com/locations")
-    page_url = "https://www.mexicaninncafe.com/locations"
-    time.sleep(10)
-    columns = driver.find_elements_by_css_selector('div.col.sqs-col-4.span-4')
-    hours_of_op_all = driver.find_element_by_css_selector('div.sqs-block.html-block.sqs-block-html > div > p:nth-child(2)').text + \
-                    "     " + driver.find_element_by_css_selector('div.sqs-block.html-block.sqs-block-html > div > p:nth-child(4)').text
-    hours_of_op_all = hours_of_op_all.replace('All Locations (Except North Henderson)',"")
-    hours_northhenderson = driver.find_element_by_css_selector('div.sqs-block.html-block.sqs-block-html > div > p:nth-child(3)').text + \
-                    "     " + driver.find_element_by_css_selector('div.sqs-block.html-block.sqs-block-html > div > p:nth-child(4)').text
-    hours_northhenderson = hours_northhenderson.replace("North Henderson Location","")
-    stores = []; gmaps = []
-
-    for column in columns:
-        stores1 = column.find_elements_by_css_selector('div.sqs-block.html-block.sqs-block-html')
-        for i in range(0,len(stores1)):
-            stores.append(stores1[i])
-        gmaps1 = column.find_elements_by_css_selector('div.sqs-block.map-block.sqs-block-map')
-        for i in range(0,len(gmaps1)):
-            gmaps.append(gmaps1[i])
-
-    location_name = [] ; phone = [] ;  lat = [] ; lng =[] ; street_addr = []; zipcode = [] ; state = [] ; city = []
-    for store in stores:
-        location_name.append(store.find_element_by_css_selector('div > h2').text)
-        phone.append(store.find_element_by_css_selector('div > p > a:nth-child(3)').text)
-
-
-    for gmap in gmaps:
-        text = gmap.get_attribute('data-block-json')
-        json_data = json.loads(text)
-        req_json =json_data['location']
-        lat.append(req_json['mapLat'])
-        lng.append(req_json['mapLng'])
-        street_addr.append(req_json['addressLine1'])
-        state_city_zip = req_json['addressLine2']
-        zipcode.append(state_city_zip.split(" ")[-1])
-        state.append(state_city_zip.split(" ")[-2])
-        city.append(state_city_zip.split(",")[0])
-
-    for i in range(0,len(location_name)):
-        if location_name[i] =='NORTH HENDERSON':
-            hours_of_op = hours_northhenderson
-        else:
-            hours_of_op = hours_of_op_all
+    data = []
+    hourlist = []
+    pattern = re.compile(r'\s\s+')
+    p =0
+    url = 'https://www.mexicaninncafe.com/locations-3'
+    r = session.get(url, headers=headers, verify=False)
+    soup =BeautifulSoup(r.text, "html.parser")
+    maindiv = soup.find('section',{'class':'Main-content'})
+    divlist = maindiv.findAll('div',{'class':'col sqs-col-12 span-12'})
+    maplist = soup.findAll('div',{'class':'map-block'})
+    print(len(maplist))
+    for i in range(0,1):
+        mainlist = str(divlist[i])   
+        start =0
+        
+        while True:
+            start = mainlist.find('<h2 ',start)
+            if start == -1:            
+                break
+            else:
+                start = start + 3
+            end = mainlist.find('View Menu',start)
+            if end == -1:
+                end = len(mainlist)-1
+                
+            det = mainlist[start-3:end]            
+            hourlist.append(det)
+            start = end + 3
+            
+ 
+    print(len(hourlist))
+    flag = 0
+    for divn in maplist:        
+        div = json.loads(divn['data-block-json'])        
+        #title = div['location']['addressTitle'].replace('Mexican Inn Cafe ','').upper()
+        street = div['location']['addressLine1']
+        city,state= div['location']['addressLine2'].split(', ',1)
+        try:
+            state,pcode = state.lstrip().split(', ',1)
+        except:
+            state,pcode = state.lstrip().split(' ',1)
+        
+        lat = div['location']['mapLat']
+        longt = div['location']['mapLng']
+     
+        hours = ''
+        phone = '<MISSING>'
+        title= '<MISSING>'
+        for det in hourlist:
+            det = BeautifulSoup(det,'html.parser')
+            
+            if det.find('p').text.find(pcode) > -1:
+                if pcode == '76107':
+                    flag = flag + 1
+                #print(pcode)
+                title = det.find('h2').text
+                if flag == 2:
+                    title = 'HENDERSON STREET'
+                    hours= 'Temporarily Closed'
+                    phone = '(817) 336-2164'
+                    flag = 1
+                    break
+                hourm = det.findAll('h3')                
+                for hr in hourm:
+                    hours = hours + hr.text + ' '
+                    
+                phone = det.find('a').text
+                break            
         data.append([
-                'https://www.mexicaninncafe.com/',
-                page_url,
-                location_name[i],
-                street_addr[i],
-                city[i],
-                state[i].replace(',',""),
-                zipcode[i],
-                'US',
-                '<MISSING>',
-                phone[i],
-                '<MISSING>',
-                lat[i],
-                lng[i],
-                hours_of_op.replace("\n", " ")
-            ])
-        count = count + 1
-        print(count)
-
-
-    time.sleep(3)
-    driver.quit()
+                        'https://www.mexicaninncafe.com/',
+                        'https://www.mexicaninncafe.com/locations-3',                   
+                        title,
+                        street,
+                        city,
+                        state,
+                        pcode,
+                        'US',
+                         '<MISSING>',
+                        phone,
+                         '<MISSING>',
+                        lat,
+                        longt,
+                        re.sub(pattern,' ',hours)
+                    ])
+        #print(p,data[p])
+        p += 1
+   
+        
     return data
 
+
 def scrape():
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
     data = fetch_data()
     write_output(data)
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
 
 scrape()
