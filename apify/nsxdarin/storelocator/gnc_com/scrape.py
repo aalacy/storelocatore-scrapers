04d5b58891062cf572b0672987b330cd15f5101a
Kevin Collins <nck@users.noreply.github.com>
2020-07-29 19:33:55 -0400

    gnc.com (SLC-1938): try to fix process getting killed

--- apify/nsxdarin/storelocator/gnc_com/scrape.py
+++ apify/nsxdarin/storelocator/gnc_com/scrape.py
@@ -5,42 +5,40 @@ import re
 import time
 import random
 import threading
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed, wait, ALL_COMPLETED
 from requests.exceptions import ProxyError
 
 
 show_logs = False
-
 thread_local = threading.local()
 
 post_headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9,la;q=0.8',
-    'cache-control': 'max-age=0',
-    'origin': 'https://www.gnc.com',
-    'referer': 'https://www.gnc.com/on/demandware.store/Sites-GNC2-Site/default/Stores-FindStores',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-    'content-type': 'application/x-www-form-urlencoded'
+  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'accept-encoding': 'gzip, deflate, br',
+  'accept-language': 'en-US,en;q=0.9,la;q=0.8',
+  'cache-control': 'max-age=0',
+  'origin': 'https://www.gnc.com',
+  'referer': 'https://www.gnc.com/on/demandware.store/Sites-GNC2-Site/default/Stores-FindStores',
+  'sec-fetch-dest': 'document',
+  'sec-fetch-mode': 'navigate',
+  'sec-fetch-site': 'same-origin',
+  'sec-fetch-user': '?1',
+  'upgrade-insecure-requests': '1',
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+  'content-type': 'application/x-www-form-urlencoded' 
 }
 get_headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9,la;q=0.8',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'accept-encoding': 'gzip, deflate, br',
+  'accept-language': 'en-US,en;q=0.9,la;q=0.8',
+  'sec-fetch-dest': 'document',
+  'sec-fetch-mode': 'navigate',
+  'sec-fetch-site': 'none',
+  'upgrade-insecure-requests': '1',
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
 }
 
-re_get_json = re.compile(
-    'map\.data\.addGeoJson\(\s*JSON\.parse\(\s*eqfeed_callback\((.+?)\)\s*\)\s*\);')
+re_get_json = re.compile('map\.data\.addGeoJson\(\s*JSON\.parse\(\s*eqfeed_callback\((.+?)\)\s*\)\s*\);')
 re_get_phone = re.compile('<a href="tel:(.+?)" class="store-phone">')
 re_get_hours_section = re.compile(
     '<div class="storeLocatorHours">([\s\S]+?)<\/div>')
@@ -61,10 +59,8 @@ def log(*args, **kwargs):
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',',
-                            quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip",
-                         "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
 
@@ -72,13 +68,13 @@ def write_output(data):
 def get_session(reset=False):
   # give each thread its own session object.
   # when using proxy, each thread's session will have a unique IP, and we'll switch IPs every 10 requests
-  if (not hasattr(thread_local, "session")) or (hasattr(thread_local, "request_count") and thread_local.request_count == 10) or (reset == True):
+  if (not hasattr(thread_local, "session")) or (hasattr(thread_local, "request_count") and thread_local.request_count == 10) or (reset==True):
     thread_local.session = SgRequests()
     # print out what the new IP is ...
     if show_logs == True:
         r = thread_local.session.get('https://jsonip.com/')
         log(f"new IP for thread id {threading.current_thread().ident}: {r.json()['ip']}")
-
+    
   if hasattr(thread_local, "request_count") and thread_local.request_count == 10:
     reset_request_count()
 
@@ -97,23 +93,23 @@ def increment_request_count():
     thread_local.request_count += 1
 
 
-def get_json_data(html):
+def get_json_data(html): 
     match = re.search(re_get_json, html)
     json_text = match.group(1)
     return json.loads(json_text)
 
 
-def get_phone(html):
+def get_phone(html): 
     match = re.search(re_get_phone, html)
-    if match is None:
+    if match is None: 
         return '<MISSING>'
     else:
         return match.group(1)
 
 
-def get_hours(html):
+def get_hours(html): 
     match_section = re.search(re_get_hours_section, html)
-    if match_section is None:
+    if match_section is None: 
         return '<MISSING>'
 
     hours = ''
@@ -121,8 +117,8 @@ def get_hours(html):
     if len(match_days) == 0:
         return '<MISSING>'
 
-    for m in match_days:
-        if len(hours) > 0:
+    for m in match_days: 
+        if len(hours) > 0: 
             hours = hours + ', '
         hours = f'{hours}{m[0].strip()}: {m[1].strip()}'
 
@@ -130,26 +126,33 @@ def get_hours(html):
     return hours
 
 
-def search_zip(zip):
+def search_zip(zip, reset=False, attempts=1): 
+    if attempts == 5:
+        raise Exception(f'failed to search zip {zip} after {attempts} tries ... giving up')
     log('searching: ', zip)
     url = 'https://www.gnc.com/on/demandware.store/Sites-GNC2-Site/default/Stores-FindStores'
     payload = {'dwfrm_storelocator_countryCode': 'US',
-               'dwfrm_storelocator_distanceUnit': 'mi',
-               'dwfrm_storelocator_postalCode': zip,
-               'dwfrm_storelocator_maxdistance': '500',
-               'dwfrm_storelocator_findbyzip': 'Search'}
-    session = get_session()
+                'dwfrm_storelocator_distanceUnit': 'mi',
+                'dwfrm_storelocator_postalCode': zip,
+                'dwfrm_storelocator_maxdistance': '500',
+                'dwfrm_storelocator_findbyzip': 'Search'}
+    session = get_session(reset=reset)
     sleep()
-    r = session.post(url, headers=post_headers, data=payload)
-    r.raise_for_status()
-    increment_request_count()
-    return get_json_data(r.text)["features"]
+    try: 
+        r = session.post(url, headers=post_headers, data=payload)
+        r.raise_for_status()
+        increment_request_count()
+        return get_json_data(r.text)["features"]
+    except Exception as ex:
+        print(f'>>> exception searching zip {zip} >>> {ex}')
+        print(f'resetting session to try again')
+        return search_zip(zip, reset=True, attempts=attempts+1)
 
 
 def get_location(loc, reset_session=False, attempts=1):
     max_attempts = 5
-    if attempts > max_attempts:
-        raise SystemExit(f"Max attempts ({max_attempts}) exceeded getting location {loc['storenumber']}")
+    if attempts > max_attempts: 
+        raise Exception(f"Max attempts ({max_attempts}) exceeded getting location {loc['storenumber']}")
 
     props = loc["properties"]
     store_id = props["storenumber"]
@@ -171,16 +174,13 @@ def get_location(loc, reset_session=False, attempts=1):
     session = get_session(reset=reset_session)
     sleep()
 
-    try:
+    try: 
         r = session.get(url, headers=get_headers)
         r.raise_for_status()
-    except ProxyError as proxy_err:
-        # attempt to handle "cannot connect to proxy, timed out"
-        log(f'ProxyError: {proxy_err}')
-        log('resetting session')
-        session = get_session(reset=True)
-        r = session.get(url, headers=get_headers)
-        r.raise_for_status()
+    except Exception as ex:
+        print(f'>>> exception getting location {loc} >>> {ex}')
+        print(f'resetting session to try again')
+        return get_location(loc, reset_session=True, attempts=attempts+1)
 
     increment_request_count()
     log(f'Pulling Store {url}')
@@ -192,22 +192,23 @@ def get_location(loc, reset_session=False, attempts=1):
     phone = get_phone(r.text)
     hours = get_hours(r.text)
 
-    location = [website, url, name, addr, city, state, zc,
-                country, store_id, phone, typ, lat, lng, hours]
+    location = [website, url, name, addr, city, state, zc, country, store_id, phone, typ, lat, lng, hours]
     return [x if x else "<MISSING>" for x in location]
 
 
 def fetch_data():
     store_ids = []
-    zips = ['60007', '10002', '90210', '96795', '99515', '98115', '88901', '87101',
-            '59715', '80014', '75001', '32034', '70032', '37011', '63101', '58102', '55408']
+    zips = ['60007','10002','90210','96795','99515','98115','88901','87101','59715','80014','75001','32034','70032','37011','63101','58102','55408']
     search_results = []
 
-    with ThreadPoolExecutor(max_workers=8) as executor:
+    with ThreadPoolExecutor(max_workers=32) as executor:
         futures = [executor.submit(search_zip, zipcode) for zipcode in zips]
-        for result in as_completed(futures):
+        done, not_done = wait(futures, return_when=ALL_COMPLETED)
+        log(f'\nDone futures: {len(done)}')
+        log(f'\nNot Done futures: {len(not_done)}')
+        for result in futures:
             locations = result.result()
-            for loc in locations:
+            for loc in locations: 
                 store_id = loc['properties']['storenumber']
                 if store_id not in store_ids:
                     log(f'queuing store id: {store_id}')
@@ -216,19 +217,42 @@ def fetch_data():
 
     log(f'found {len(search_results)} locations')
 
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        futures = [executor.submit(get_location, result)
-                   for result in search_results]
-        for result in as_completed(futures):
+    with ThreadPoolExecutor(max_workers=32) as executor: 
+        futures = [executor.submit(get_location, result) for result in search_results]
+        wait(futures, return_when=ALL_COMPLETED)
+        for result in futures:
             loc = result.result()
             if loc is not None:
                 yield loc
-
+            
 
 def scrape():
-
+    
+    # ---- to test hours/phone for just one store detail ---
+    # search_result = {
+    #     'type': 'Feature',
+    #     'geometry': {
+    #         'type': 'Point', 
+    #         'coordinates': [-80.14893562, 26.20537681]
+    #     }, 
+    #     'properties': {
+    #         'title': 'GNC at Cypress Creek Station', 
+    #         'storenumber': '1814',
+    #         'url': '<a id="3735" href="/store-details?StoreID=3735" title="View Store Details">Store Details</a>',
+    #         'address1': '6305 North Andrews Ave', 
+    #         'address2': None, 
+    #         'postalCode': '33309', 
+    #         'city': 
+    #         'Ft. Lauderdale', 
+    #         'state': 'FL', 
+    #         'country': {}, 
+    #         'inventoryListID': ''
+    #     }
+    # }
+    # data = get_location(search_result)
+    # write_output([data])
+    
     data = fetch_data()
     write_output(data)
-
 
 scrape()
