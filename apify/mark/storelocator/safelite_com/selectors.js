b3fbada04e12075a14efcce3f3eaa0628199439e
Michael Dinh <mkdinh94@gmail.com>
2020-07-30 22:21:26 -0400

    SLC-117/safelite_com: remove empty line

--- apify/mark/storelocator/safelite_com/selectors.js
+++ apify/mark/storelocator/safelite_com/selectors.js
@@ -5,7 +5,6 @@ const addressSelector = 'div.content-block:nth-child(3) > p:nth-child(2)';
 const googleMapSelector = '#map-1';
 
 const getScheduleButtonSelector = hasAddress => `#MainContent_C001_Col00 > div:nth-child(3) > p:nth-child(${hasAddress ? 4 : 2}) > a`;
-
 const getHoursSelector = (hasAddress, hasScheduleButton) => `#MainContent_C001_Col00 > div:nth-child(3) > p:nth-child(${hasAddress && hasScheduleButton ? 6 : 4})`
 const getPhoneSelector = (hasAddress) => `div.content-block:nth-child(3) > p:nth-child(${hasAddress ? 7 : 5})`;
 
