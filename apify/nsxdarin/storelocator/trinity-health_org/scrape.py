33d385c1f5426b0effb9e7cd6ba1a7d6506700b0
nsxdarin <nsxdarin@gmail.com>
2020-07-31 08:33:39 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/trinity-health_org/scrape.py
+++ apify/nsxdarin/storelocator/trinity-health_org/scrape.py
@@ -1,5 +1,4 @@
 import csv
-import urllib2
 from sgrequests import SgRequests
 
 session = SgRequests()
@@ -14,10 +13,21 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
-    for x in range(0, 250):
+    locs = []
+    for x in range(0, 20):
         print(x)
-        loc = 'http://www.trinity-health.org/body.cfm?id=21&action=detail&ref=' + str(x)
+        loc = 'https://www.trinity-health.org/find-a-location/location-results?page=' + str(x) + '&count='
         r = session.get(loc, headers=headers)
+        for line in r.iter_lines():
+            line = str(line.decode('utf-8'))
+            if '"DirectUrl\\":\\"' in line:
+                items = line.split('"DirectUrl\\":\\"')
+                for item in items:
+                    if '"LocationName\\":\\"\\"' in item:
+                        lurl = 'https://www.trinity-health.org/' + item.split('\\')[0]
+                        locs.append(lurl)
+    for loc in locs:
+        print(loc)
         name = ''
         website = 'trinity-health.org'
         typ = '<MISSING>'
@@ -26,39 +36,36 @@ def fetch_data():
         city = ''
         zc = ''
         country = 'US'
-        store = str(x)
-        phone = ''
+        store = '<MISSING>'
+        phone = '<MISSING>'
         lat = '<MISSING>'
         lng = '<MISSING>'
         hours = '<MISSING>'
-        for line in r.iter_lines():
-            if '<dd id="locationName"><h2>' in line:
-                name = line.split('<dd id="locationName"><h2>')[1].split('<')[0]
-            if '<dd id="address">' in line:
-                count = line.count('<br>')
-                if count == 1:
-                    add = line.split('<dd id="address">')[1].split('<')[0]
-                    city = line.split('<br>')[1].split(',')[0]
-                    state = line.split('<br>')[1].split(',')[1].strip().split(' ')[0]
-                    zc = line.split('</dd>')[0].rsplit(' ',1)[1]
-                else:
-                    add = line.split('<dd id="address">')[1].split('<')[0]
-                    city = line.split('<br>')[2].split(',')[0]
-                    state = line.split('<br>')[2].split(',')[1].strip().split(' ')[0]
-                    zc = line.split('</dd>')[0].rsplit(' ',1)[1]
-            if '<dd id="phone"><a href="tel:' in line:
-                phone = line.split('<dd id="phone"><a href="tel:')[1].split('"')[0]
-        if name != '':
-            if zc == '':
-                zc = '<MISSING>'
-            if phone == '':
-                phone = '<MISSING>'
-            if ' Suite' in add:
-                add = add.split(' Suite')[0]
-            if ' #' in add:
-                add = add.split(' #')[0]
-            add = add.strip()
-            yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+        r2 = session.get(loc, headers=headers)
+        for line2 in r2.iter_lines():
+            line2 = str(line2.decode('utf-8'))
+            if '"Name\\\\\\":\\\\\\"' in line2:
+                name = line2.split('"Name\\\\\\":\\\\\\"')[1].split('\\')[0]
+            if '"Address1\\\\\\":\\\\\\"' in line2:
+                add = line2.split('"Address1\\\\\\":\\\\\\"')[1].split('\\')[0]
+            if '"Address2\\\\\\":\\\\\\"' in line2:
+                add = add + ' ' + line2.split('"Address2\\\\\\":\\\\\\"')[1].split('\\')[0]
+                add = add.strip()
+            if '"City\\\\\\":\\\\\\"' in line2:
+                city = line2.split('"City\\\\\\":\\\\\\"')[1].split('\\')[0]
+            if '"PostalCode\\\\\\":\\\\\\"' in line2:
+                zc = line2.split('"PostalCode\\\\\\":\\\\\\"')[1].split('\\')[0]
+            if '"Phone\\\\\\":\\\\\\"' in line2:
+                phone = line2.split('"Phone\\\\\\":\\\\\\"')[1].split('\\')[0]
+            if '"StateName\\\\\\":\\\\\\"' in line2:
+                state = line2.split('"StateName\\\\\\":\\\\\\"')[1].split('\\')[0]
+            if 'Latitude\\\\\\":' in line2:
+                lat = line2.split('Latitude\\\\\\":')[1].split(',')[0]
+            if 'Longitude\\\\\\":' in line2:
+                lng = line2.split('Longitude\\\\\\":')[1].split(',')[0]
+        if phone == '':
+            phone = '<MISSING>'
+        yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
