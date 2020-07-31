afc620df91e409e131697e51bf4694e240d3ba67
Michael Dinh <mkdinh94@gmail.com>
2020-07-31 13:38:56 -0400

    SLC-171/pollotropical_com: update scraper to fetch data from api endpoint

--- apify/mark/storelocator/pollotropical_com/scrape.js
+++ apify/mark/storelocator/pollotropical_com/scrape.js
@@ -1,135 +1,152 @@
 const Apify = require('apify');
-const {
-  submitButton,
-  viewAllLocationSelector,
-  locationNameSelector,
-  addressSelector,
-  phoneSelector,
-  detailLocationNameSelector,
-  detailhourSelector,
-  detailLatitudeSelector,
-  detailLongitudeSelector,
-} = require('./selectors');
+const axios = require('axios');
+const { Poi } = require('./Poi');
 
-const {
-  getDataKey,
-  getStoreNumber,
-  formatPhoneNumber,
-  formatAddress,
-  formatHours,
-} = require('./tools');
+// extract url to common.js to find client credentials
+function queryScriptFile($) {
+  const scripts = $('script[src]');
+  let srcUrl = null;
+  scripts.each(function (idx) {
+    const src = $(this).attr('src');
+    if (src.match(/commons/)) {
+      srcUrl = src;
+    }
+  });
+  return srcUrl;
+}
 
-const {
-  Poi,
-} = require('./Poi');
+// fetch js file and extract credentials from code
+async function queryCredentials(commonSrcUrl) {
+  const { data } = await axios.get(commonSrcUrl);
+  const matches = data.match(/\{(client.*?)\}/);
+  if (!matches || !matches[1]) {
+    throw new Error('Unable to extract client credentials from common file.');
+  }
+
+  // manual extraction since this is not in valid JSON format
+  const [idKvp, secretKvp] = matches[1].split(',');
+  const [idKey, idValue] = idKvp.split(':');
+  const [secretKey, secretValue] = secretKvp.split(':');
+
+  return {
+    [idKey.trim()]: idValue.replace(/\'|\"/g, '').trim(),
+    [secretKey.trim()]: secretValue.replace(/\'|\"/g, '').trim(),
+  };
+}
+
+// recursively loop through paginations and fetch data.
+async function queryAllStoreLocations(authToken, stores = [], nextUrl) {
+  const currentPageUrl =
+    nextUrl ||
+    'https://api.koala.fuzzhq.com/v1/ordering/store-locations/?include[0]=operating_hours&per_page=50';
+
+  const headers = {
+    Authorization: `Bearer ${authToken}`,
+  };
+  try {
+    const {
+      data: { data, meta },
+    } = await axios.get(currentPageUrl, { headers });
+
+    stores.push(...data);
+    const { next } = meta.pagination.links;
+
+    if (next) {
+      await queryAllStoreLocations(authToken, stores, next);
+    }
+
+    return stores;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// fetch authToken to make api query.
+async function queryAuthToken(credentials) {
+  const body = {
+    ...credentials,
+    grant_type: 'ordering_app_credentials',
+    scope: 'group:ordering_app',
+  };
+  try {
+    const { data } = await axios.post('https://api.koala.fuzzhq.com/oauth/access_token', body);
+    return data.access_token;
+  } catch (err) {
+    throw new Error(`unable to fetch auth token: ${JSON.stringify(err, null, 2)}`);
+  }
+}
+
+function formatPhone(phone) {
+  if (!phone) {
+    return null;
+  }
+
+  return phone.replace(/[\(|\)|-|+|\s]/g, '');
+}
+
+function formatHoursOfOperation(hours) {
+  if (!hours || (hours && !hours.length)) {
+    return null;
+  }
+
+  return hours.map((day) => ({
+    start: new Date(day.start).toLocaleTimeString(),
+    end: new Date(day.end).toLocaleTimeString(),
+    day_of_week: day.day_of_week,
+  }));
+}
 
 Apify.main(async () => {
-  const dataStorage = await Apify.openKeyValueStore('tempData');
   const requestQueue = await Apify.openRequestQueue();
+  const locator_domain = 'pollotropical.com';
+  const baseUrl = `https://www.${locator_domain}`;
+
   await requestQueue.addRequest({
-    url: 'https://www.pollotropical.com/locations',
-    userData: {
-      urlType: 'initial',
-    },
+    url: `${baseUrl}/locations`,
   });
 
-  const crawler = new Apify.PuppeteerCrawler({
+  const crawler = new Apify.CheerioCrawler({
     requestQueue,
-    launchPuppeteerOptions: {
-      headless: true,
-      useChrome: true,
-      stealth: true,
-    },
-    gotoFunction: async ({
-      request, page,
-    }) => {
-      await page.goto(request.url, {
-        timeout: 30000, waitUntil: 'networkidle0',
-      });
-    },
-    maxRequestsPerCrawl: 3000,
-    maxConcurrency: 10,
-    handlePageFunction: async ({ request, page }) => {
-      if (request.userData.urlType === 'initial') {
-        // Check if the page shows the view all location element
-        if (await page.$(viewAllLocationSelector) === null) {
-          await page.$eval('input[name=address]', el => el.value = 'United States');
-          await page.click(submitButton);
-          await page.waitFor(10000);
-        }
-        await page.click(viewAllLocationSelector);
-        await page.waitFor(10000);
-        const locationNameRaw = await page.$$eval(locationNameSelector, le => le
-          .map(l => l.innerText));
-        const addressRaw = await page.$$eval(addressSelector, ae => ae.map(a => a.innerText));
-        const phoneNumberRaw = await page.$$eval(phoneSelector, pe => pe.map(p => p.innerText));
-
-        // Temporarily Set Info to add to later
-        /* eslint-disable no-restricted-syntax */
-        for await (const [i, name] of locationNameRaw.entries()) {
-          const address = formatAddress(addressRaw[i]);
-          const phoneStringRaw = phoneNumberRaw[i];
-          const phoneArrayRaw = phoneStringRaw.replace(/\n/g, ',').split(',');
-          const phoneArrayNumber = phoneArrayRaw.filter(e => e.match(/[0-9]{3}-[0-9]{4}/));
-          const phoneNumber = phoneArrayNumber[0];
-          const key = getDataKey(name);
-          const poiData = {
-            locator_domain: 'pollotropical.com',
-            location_name: name,
-            store_number: getStoreNumber(name),
-            ...address,
-            phone: phoneNumber,
-          };
-          await dataStorage.setValue(key, poiData);
-        }
-        // Look deeper to find geolocation
-        await page.waitFor(5000);
-        await Apify.utils.enqueueLinks({
-          page,
-          requestQueue,
-          selectors: 'div.styles__StyledCardLinks-s1y7dfjk-2.jAGAES > a',
-          pseudoUrls: [
-            'https://olo.pollotropical.com/menu/[.*]',
-          ],
-          userData: {
-            urlType: 'detail',
-          },
+    async handlePageFunction({ request, $ }) {
+      const commonSrcUrl = queryScriptFile($);
+      const credentials = await queryCredentials(`${baseUrl}${commonSrcUrl}`);
+      const authToken = await queryAuthToken(credentials);
+      const stores = await queryAllStoreLocations(authToken);
+      const promises = stores.map((store) => {
+        const {
+          latitude,
+          longitude,
+          phone_number,
+          operating_hours,
+          street_address,
+          city,
+          zip_code: zip,
+          brand_id: store_number,
+          label: location_name,
+          country: country_code,
+          cached_data: { state },
+        } = store;
+        const poi = new Poi({
+          locator_domain,
+          city,
+          state,
+          zip,
+          street_address,
+          latitude,
+          longitude,
+          country_code,
+          location_name,
+          store_number,
+          phone: formatPhone(phone_number),
+          hours_of_operation: formatHoursOfOperation(operating_hours),
         });
-      }
-      if (request.userData.urlType === 'detail') {
-        // Some pages have empty information for stores -> if they do skip them
-        if (await page.$(detailLocationNameSelector) !== null) {
-          const detailLocationName = await page.$eval(detailLocationNameSelector, n => n.innerText);
-          const latitude = await page.$eval(detailLatitudeSelector, la => la.getAttribute('title'));
-          const longitude = await page.$eval(detailLongitudeSelector, lo => lo.getAttribute('title'));
-          const detailHourRaw = await page.$eval(detailhourSelector, h => h.innerText);
-          const key = getDataKey(detailLocationName);
-          /* eslint-disable camelcase */
-          const hours_of_operation = formatHours(detailHourRaw);
-          const currentStoreData = await dataStorage.getValue(key);
-          if (currentStoreData) {
-            const revisedStoreData = {
-              ...currentStoreData,
-              latitude,
-              longitude,
-              hours_of_operation,
-            };
-            // Upload new
-            await dataStorage.setValue(key, revisedStoreData);
-          }
-        }
-      }
+
+        return Apify.pushData(poi);
+      });
+
+      await Promise.all(promises);
     },
   });
 
   await crawler.run();
-  if (requestQueue.isFinished()) {
-    await dataStorage.forEachKey(async (key) => {
-      if (key) {
-        const locationInfo = await dataStorage.getValue(key);
-        const poi = new Poi(locationInfo);
-        await Apify.pushData(poi);
-      }
-    });
-  }
 });
