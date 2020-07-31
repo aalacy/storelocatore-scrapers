09e5c9e32f310299eb9eb419311b60b97db1017d
Tenzing Shaw <tenzing@safegraph.com>
2020-07-30 19:50:21 -0700

    armani use sgselenium

--- apify/himanshu/storelocator/armani_com/scrape.py
+++ apify/himanshu/storelocator/armani_com/scrape.py
@@ -1,26 +1,8 @@
 import csv
 import os
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from sgselenium import SgSelenium
 import re
 import json
-import platform
-system = platform.system()
-
-
-def get_driver():
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--window-size=1920,1080')
-    if "linux" in system.lower():
-        return webdriver.Firefox(executable_path='./geckodriver', options=options)
-    elif "darwin" in system.lower():
-        return webdriver.Firefox(executable_path='./geckodriver-mac', options=options)
-    else:
-        return webdriver.Firefox(executable_path='geckodriver.exe', options=options)
-
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
@@ -40,7 +22,7 @@ def rreplace(s, old, new, count):
 
 
 def fetch_data():
-    driver = get_driver()
+    driver = SgSelenium().firefox()
     driver.get(
         'https://www.armani.com/experience/us/?yoox_storelocator_action=true&action=yoox_storelocator_get_all_stores')
 
