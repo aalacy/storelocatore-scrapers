3253ac5d1ccb0d3230e724930926c020eb2040db
Tenzing Shaw <tenzing@safegraph.com>
2020-07-31 18:20:48 -0700

    rm commented code

--- apify/himanshu/storelocator/bakersribs_com/scrape.py
+++ apify/himanshu/storelocator/bakersribs_com/scrape.py
@@ -9,9 +9,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 session = SgRequests()
 
-
-
-
 def write_output(data):
     with open('data.csv', mode='w',encoding="utf-8") as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -68,50 +65,6 @@ def fetch_data():
             store.append(" ".join(hours1).encode('ascii', 'ignore').decode('ascii').strip())
             store.append("<MISSING>")
             yield store
-            # return_main_object.append(store)
-            # print("-----------------------",store)
-    # return return_main_object
-
-
-        # print(phone_list)
-
-        # print(list(location.stripped_strings)[0] )
-        # print(list(location.stripped_strings))
-        # .find_all("a")
-    #     location_request = session.get(location["href"],headers=headers)
-    #     location_soup = BeautifulSoup(location_request.text,"lxml")
-    #     location_details = list(location_soup.find("div",{'class':"et_pb_blurb_description"}).stripped_strings)
-    #     geo_request = session.get(location_soup.find("iframe",{'src':re.compile("google")})["src"],headers=headers)
-    #     geo_soup = BeautifulSoup(geo_request.text,"lxml")
-    #     for script in geo_soup.find_all("script"):
-    #         if "initEmbed" in script.text:
-    #             geo_data = json.loads(script.text.split("initEmbed(")[1].split(");")[0])[21][3][0][1]
-    #             lat = json.loads(script.text.split("initEmbed(")[1].split(");")[0])[21][3][0][2][0]
-    #             lng = json.loads(script.text.split("initEmbed(")[1].split(");")[0])[21][3][0][2][1]
-    #     for i in range(len(location_details)):
-    #         if "Phone:" == location_details[i]:
-    #             phone = location_details[i+1]
-    #         elif "Phone:" in location_details[i]:
-    #             phone = location_details[i].split("Phone:")[1]
-    #     for i in range(len(location_details)):
-    #         if "HOURS" == location_details[i]:
-    #             hours = " ".join(location_details[i+1:])
-    #     store = []
-    #     store.append("http://bakersribs.com")
-    #     store.append(location.text)
-    #     store.append(geo_data.split(",")[1])
-    #     store.append(geo_data.split(",")[2])
-    #     store.append(geo_data.split(",")[-1].split(" ")[-2])
-    #     store.append(geo_data.split(",")[-1].split(" ")[-1])
-    #     store.append("US")
-    #     store.append("<MISSING>")
-    #     store.append(phone.replace("\xa0",""))
-    #     store.append("baker's ribs")
-    #     store.append(lat)
-    #     store.append(lng)
-    #     store.append(hours)
-    #     return_main_object.append(store)
-    # return return_main_object
 
 def scrape():
     data = fetch_data()
