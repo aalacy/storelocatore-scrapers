df2a33b908d2db17b1ea4803a0a9d37a8e741e63
Quincy <qcalexander@gmail.com>
2020-08-02 16:24:22 -0400

    globalpetfoods_com fixed

--- apify/ggrahambaker/storelocator/globalpetfoods_com/scrape.py
+++ apify/ggrahambaker/storelocator/globalpetfoods_com/scrape.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 import json
 import re
 
+import time
+from random import randint
+
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -37,9 +40,10 @@ def fetch_data():
         if 'Opening' in location_name:
             continue
 
+        print(url)
         raw_json = soup.find('script', {'type': 'application/ld+json'}).text.replace('\r\n', '').replace("\\\\\\\'", "'")
-        formatted_json = raw_json.replace("\\", "")
-    
+        formatted_json = raw_json.replace("\\", "").replace('<a href="https://miramichi.globalpetfoods.com/" target="_blank">SHOP NOW!</a>',"")
+
         loc_json = json.loads(formatted_json)
         
         hours = ''
@@ -47,12 +51,14 @@ def fetch_data():
         for h in loc_json['openingHours']:
             hours += h + ' '
             
-        hours = hours.strip()    
+        hours = hours.strip()
         
         addy = loc_json['address']
         city = addy['addressLocality']
         state = addy['addressRegion']
         zip_code = addy['postalCode']
+        if zip_code == "HN8 1X7":
+            zip_code = "H8N 1X7"
         street_address = addy['streetAddress']
         phone_number = addy['telephone'].replace('=', '').replace('+1', '').strip()
         if phone_number == '':
@@ -63,8 +69,31 @@ def fetch_data():
         
         store_number = i
         location_type = '<MISSING>'
-        lat = '<MISSING>'
-        longit = '<MISSING>'
+
+        map_link = soup.find(class_="location_directions").a['href']
+        req = session.get(map_link, headers = HEADERS)
+        time.sleep(randint(1,2))
+        try:
+            maps = BeautifulSoup(req.text,"lxml")
+        except (BaseException):
+            print('[!] Error Occured. ')
+            print('[?] Check whether system is Online.')
+
+        try:
+            raw_gps = maps.find('meta', attrs={'itemprop': "image"})['content']
+            lat = raw_gps[raw_gps.find("=")+1:raw_gps.find("%")].strip()
+            longit = raw_gps[raw_gps.find("-"):raw_gps.find("&")].strip()
+        except:
+            lat = "<MISSING>"
+            longit = "<MISSING>"
+
+        if len(lat) > 20 or not longit:
+            lat = raw_gps[raw_gps.rfind("=")+1:raw_gps.find(",")].strip()
+            longit = raw_gps[raw_gps.rfind("-"):].strip()
+        if not lat[3:5].isnumeric() or not longit[5:7].isnumeric():
+            lat = "<MISSING>"
+            longit = "<MISSING>"
+
         page_url = url
         store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code, 
                     store_number, phone_number, location_type, lat, longit, hours, page_url]
