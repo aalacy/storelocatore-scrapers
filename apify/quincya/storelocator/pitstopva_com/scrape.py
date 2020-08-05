600b42efa983bd892b6cbc4986f26814334b8cab
Quincy <qcalexander@gmail.com>
2020-08-04 21:19:31 -0400

    pitstopva_com first commit

---
+++ apify/quincya/storelocator/pitstopva_com/scrape.py
@@ -0,0 +1,68 @@
+from sgrequests import SgRequests
+from bs4 import BeautifulSoup
+import csv
+import time
+from random import randint
+import re
+
+def write_output(data):
+	with open('data.csv', mode='w', encoding="utf-8") as output_file:
+		writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+		# Header
+		writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+		# Body
+		for row in data:
+			writer.writerow(row)
+
+def fetch_data():
+	
+	base_link = "http://pitstopva.com/location.htm"
+
+	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+	HEADERS = {'User-Agent' : user_agent}
+
+	session = SgRequests()
+	req = session.get(base_link, headers = HEADERS)
+	time.sleep(randint(1,2))
+	try:
+		base = BeautifulSoup(req.text,"lxml")
+	except (BaseException):
+		print('[!] Error Occured. ')
+		print('[?] Check whether system is Online.')
+
+	data = []
+
+	items = base.find_all("table")[2].find_all("tr")[2:]
+	locator_domain = "pitstopva.com"
+
+	for item in items:
+
+		raw_data = item.text.strip().split("\n")
+
+		location_name = raw_data[0].strip()
+		print(location_name)
+
+		street_address = raw_data[1].strip()
+		raw_address = raw_data[2].split(",")
+		city = raw_address[0].strip()
+		state = raw_address[1].split()[0]
+		zip_code = raw_address[1].split()[1]
+
+		country_code = "US"
+		store_number = "<MISSING>"
+		location_type = "<MISSING>"
+		phone = raw_data[3]
+		hours_of_operation = "<MISSING>"
+		latitude = "<MISSING>"
+		longitude = "<MISSING>"
+
+		data.append([locator_domain, base_link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+
+	return data
+
+def scrape():
+	data = fetch_data()
+	write_output(data)
+
+scrape()
