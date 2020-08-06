05a35e78abb4c3f0ef23e328f64c57ae5033c761
vivri <victor.ivri@gmail.com>
2020-08-06 10:19:21 -0400

    [SLC-3689] Windermere.com scraper complete, but wrongly fails on an un-ignorable validation

--- apify/vivri/storelocator/windermere_com/scrape.py
+++ apify/vivri/storelocator/windermere_com/scrape.py
@@ -1,21 +1,113 @@
-import csv
+import time
+from sgrequests import SgRequests
+import urllib.parse
+import simplejson as json
+import simple_scraper_pipeline as scrape_pipeline
 
-def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-        # Header
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
-        # Body
-        for row in data:
-            writer.writerow(row)
+### LIBRARY INVOCATION CODE ###
 
 def fetch_data():
-    # Your scraper here
-    return [["safegraph.com", "<MISSING>", "SafeGraph", "1543 Mission St.", "San Francisco", "CA", "94103", "US", "<MISSING>", "(415) 966-1152", "Office", 37.773500, -122.417831, "mon-fri 9am-5pm"]]
+    """
+    Fetches the data from an external source, conforming to the signature described in `scrape_pipeline(data_fetcher)`
+    :return:
+        A list of dicts, each of which is the raw data field.
+    """
+
+    locations_url = "https://svc.moxiworks.com/service/v1/profile/offices/search"
+    ms_since_epoch = int(time.time() * 1000)
+    params = urllib.parse.urlencode({
+        "center_lat": 34.014959683598164,
+        "center_lon": -118.4117215,
+        "order_by": "distance",
+        "company_uuid": "1234567",
+        "callback": "jQuery22407851157122881546_1596651612792",
+        "source": "agent % 20",
+        "website": "",
+        "source_display_name": "Windermere.com",
+        "_": ms_since_epoch
+    })
+
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'cache-control': 'max-age=0',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
+    }
+
+    session = SgRequests()
+    response = session.get(locations_url, data=params, headers=headers)
+
+    json_result = json.loads(urllib.parse.unquote(response.text), encoding="utf8")
+
+    result_status = str(json_result['status'])
+    if result_status != "success":
+        print("API call is not successful; result status: " + result_status)
+        print("Printing result:")
+        print(json.dumps(json_result, indent=2))
+        raise Exception("API call is not successful; result status: " + result_status)
+
+    return json_result['data']['result_list']
+
+def strip_extension(phone: str):
+    return phone.split("x")[0]
+
+def phone_number_extract(raw: str, which_index=0):
+    """
+    Attempts to extract a raw phone number from a string in which phone numbers are delimited by "/",
+    And extensions are present as e.g. "x12345"
+    :param raw:
+        The raw phone number.
+    :param which_index:
+        Which of the present phone numbers to take?
+        Defaults to 0 (first one.)
+    :return:
+        The phone number, or an empty string if none exist at the given index.
+    """
+    split_phones = raw.split("/")
+    try:
+        return strip_extension(split_phones[which_index])
+    except ValueError:
+        return ""
 
 def scrape():
-    data = fetch_data()
-    write_output(data)
+    """
+    The main entrypoint into the program; uses the above "library" to invoke the scraper functionality.
+    """
+
+    record_mapping = {
+        "location_name": [["name"]],
+        "street_address": [["location", "address"], ["location", "address2"]],
+        "city": [["location", "city"]],
+        "state": [["location", "state"]],
+        "zip": [["location", "zip"]],
+        "country_code": [["location", "country_code"]],
+        "store_number": [["external_id"]],
+        "phone": [["phone"]],
+        "location_type": [["legal"]],
+        "latitude": [["location", "latitude"]],
+        "longitude": [["location", "longitude"]],
+    }
+
+    constant_fields = {
+        "locator_domain": "https://windermere.com",
+        "page_url": "<MISSING>",
+        "hours_of_operation": "<MISSING>"
+    }
+
+    required_fields = [
+        "street_address",
+        "city",
+        "state"
+    ]
+
+    field_transformers = {
+        "phone": lambda s: phone_number_extract(s)
+    }
+
+    scrape_pipeline.define_and_run( data_fetcher= lambda: fetch_data(),
+                                    record_mapping=record_mapping,
+                                    constant_fields=constant_fields,
+                                    required_fields=required_fields,
+                                    field_transform=field_transformers,
+                                    fail_on_outlier=False)
 
 scrape()
---
+++ apify/vivri/storelocator/windermere_com/simple_scraper_pipeline.py
@@ -0,0 +1,221 @@
+"""
+This is a simple scraper library that uses a fetcher function, and some configuration to write data to a CSV file.
+
+The main entrypoint is `define_and_run`, but you can feel free to use any of the utilities therein.
+"""
+
+import csv
+import simplejson as json
+
+MISSING = "<MISSING>"
+
+def write_output(data: list, header: list):
+    """
+    Writes the header and data to a CSV.
+
+    :param data:
+        A list of fields, pre-sorted by their keys (matching the header keys)
+    :param header:
+        A list of pre-sorted column headers.
+    :return:
+        Nothing; it's a side-effecting method.
+    """
+
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(header)
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+def sorted_keys(the_dict: dict):
+    """
+    :returns
+        The keys of the dict as a sorted list.
+    """
+    keys = []
+    for k in the_dict.keys():
+        keys.append(k)
+    keys.sort()
+    return keys
+
+def sort_values_dict(the_dict: dict):
+    """
+    :returns
+        The values in the dict as a list, sorted by the dict keys.
+    """
+    values = []
+    for k in sorted_keys(the_dict):
+        values.append(the_dict[k])
+
+    return values
+
+def drill_down_into(record: dict, field_chain: list):
+    """
+    Follows a `field_path` in the `record` dictionary.
+    :returns
+        The value in the `record` after traversing the `field_path`.
+        If `field_path` == [], returns the record.
+        If some key in the chain isn't found, returns `None`.
+    """
+    result = record
+    for step in field_chain:
+        try:
+            result = result[step]
+        except KeyError:
+            result = None
+            break
+    return result
+
+def missing_if_empty(field: str):
+    """
+    Convenience function that defaults an empty field to `MISSING`
+    """
+    if field == "":
+        return MISSING
+    else:
+        return field
+
+def decode_record (record: dict, decode_map: dict, fail_on_outlier: bool, constant_fields: dict, required_fields: list, field_transform=None):
+    """
+    Decodes a record, and returns a list of decoded string values, sorted by the record's own keys
+
+    :param record:
+        The raw record as a dict.
+    :param decode_map:
+        List of lists, each representing a path to search for a value. Results concatenated via a single space " ".
+    :param constant_fields:
+        Dict of constant values, to be present in the result.
+    :param required_fields:
+        List of fields that are mandatory.
+    :param fail_on_outlier:
+        Should the function except on missing a mandatory field, or just record an absence and skip it?
+    :param field_transform:
+        A map keyed on the logical field name, of a string->string lambda that transforms the field
+    :return:
+    """
+
+    # instantiate with the constant fields
+    result = constant_fields
+    for field, fieldPaths in decode_map.items():
+
+        field_value = ""
+        for fieldPath in fieldPaths:
+            next_value = drill_down_into(record, fieldPath)
+            if next_value is not None:
+                field_value += " " + str(next_value)
+
+        field_value = field_value.strip() # remove prefixed space
+
+        if field_value == "":
+            try:
+                required_fields.index(field) # will only succeed if it's there
+                print("Required field: " + field + " was missing from record: " + json.dumps(record))
+                if fail_on_outlier:
+                    raise Exception("Required field: " + field + " was missing from record: " + json.dumps(record))
+                else:
+                    return None
+            except ValueError:
+                # it's not in the required_field list, ergo it's defaultable.
+                field_value = MISSING
+        else:
+            transformer = field_transform.get(field)
+            if transformer is not None:
+                field_value = transformer(field_value)
+
+        result[field] = missing_if_empty(field_value)
+
+    return sort_values_dict(result)
+
+
+def parse_data(locations: list, record_mapping: dict, constant_fields: dict, required_fields: list, fail_on_outlier: bool, field_transform: list):
+    """
+    Parses the raw location data
+
+    :param locations:
+        A list of raw location data (as dict).
+    :param record_mapping:
+        List of lists, each representing a path to search for a value. Results concatenated via a single space " ".
+    :param constant_fields:
+        Dict of constant values, to be present in the result.
+    :param required_fields:
+        List of fields that are mandatory.
+    :param field_transform:
+        A map keyed on the logical field name, of a string->string lambda that transforms the field
+    :param fail_on_outlier:
+        Should the function except on missing a mandatory field, or just record an absence and skip it?
+    :return:
+        A list of location data, each record being a list of parsed string values, sorted by key.
+    """
+
+    print("Number of locations in resultset: " + str(len(locations)))
+
+    result = []
+    for record in locations:
+        decoded = decode_record(
+            record=record,
+            decode_map=record_mapping,
+            constant_fields=constant_fields,
+            required_fields=required_fields,
+            field_transform=field_transform,
+            fail_on_outlier=fail_on_outlier)
+
+        if decoded is not None:
+            # only append successfully decoded fields.
+            result.append(decoded)
+
+    return result
+
+def define_and_run(data_fetcher: lambda: list, record_mapping: dict, constant_fields=None, required_fields=None, field_transform=None, fail_on_outlier=False):
+    """
+    Creates and executes the scraper pipeline.
+    Writes `data.csv` to the current dir, and logs out useful info/errors.
+
+    :param data_fetcher:
+        A lambda that fetches a raw list of location data, each record being a dict. Can be deeply nested or flat.
+    :param record_mapping:
+        List of lists, each representing a path/ to search for a value in the fetched data rows. Results concatenated via a single space " ".
+        Each key represents a field to be populated in the final results.
+
+        For example: { locations: { street: "123 Sesame St.", addr2: "Unit #3" }}
+        Could be parsed as: { street_address: [["locations", "street"], ["locations", "addr2"]] }
+    :param constant_fields:
+        Dict of constant values, to be present in the result.
+        For example: { locator_domain: "https://windermere.com" }
+        Defaults to empty dict.
+    :param required_fields:
+        List of fields that are mandatory.
+        Defaults to ["street_address", "city", "state"]
+    :param field_transform:
+        A map keyed on the logical field name, of a string->string lambda that transforms the field.
+        Defaults to empty dict.
+    :param fail_on_outlier:
+        Should the function except on missing a mandatory field, or just record an absence and skip it?
+        Default is False.
+    :return:
+        Nothing, as it simply writes to a file.
+    """
+
+    if constant_fields is None:
+        constant_fields = {}
+    if required_fields is None:
+        required_fields = ["street_address", "city", "state"]
+    if field_transform is None:
+        field_transform = {}
+
+
+    csv_header = sorted_keys(record_mapping)
+    csv_header.extend(sorted_keys(constant_fields))
+    csv_header.sort()
+
+    data = parse_data(
+        locations=data_fetcher(),
+        record_mapping=record_mapping,
+        constant_fields=constant_fields,
+        required_fields=required_fields,
+        field_transform=field_transform,
+        fail_on_outlier=fail_on_outlier)
+
+    write_output(data, csv_header)
