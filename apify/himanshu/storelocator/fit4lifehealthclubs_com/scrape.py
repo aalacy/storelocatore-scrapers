b068437adf2c4e54c20a7357c7a7de72c44f06cd
Quincy <qcalexander@gmail.com>
2020-08-07 06:00:53 -0400

    fit4lifehealthclubs_com fixed

--- apify/himanshu/storelocator/fit4lifehealthclubs_com/scrape.py
+++ apify/himanshu/storelocator/fit4lifehealthclubs_com/scrape.py
@@ -4,9 +4,19 @@ from bs4 import BeautifulSoup
 import re
 import json
 import unicodedata
+import time
+from random import randint
+
+from sgselenium import SgSelenium
 
 
-session = SgRequests()
+def addy_ext(addy):
+    addy = addy.split(',')
+    city = addy[0]
+    state_zip = addy[1].strip().split(' ')
+    state = state_zip[0]
+    zip_code = state_zip[1]
+    return city, state, zip_code
 
 def write_output(data):
     with open('data.csv', 'w') as output_file:
@@ -17,189 +27,69 @@ def write_output(data):
         for i in data or []:
             writer.writerow(i)
 
-
 def fetch_data():
+
+    base_link = "https://fit4lifehealthclubs.com/find-a-gym/"
+
+    driver = SgSelenium().chrome()
+    time.sleep(2)
+
+    driver.get(base_link)
+    time.sleep(randint(15,20))
+    soup = BeautifulSoup(driver.page_source, "lxml")
+
+    session = SgRequests()
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
     }
-    store_detail = []
-    store_name = []
-    return_main_object = []
-    address1 = []
-    phone = ''
+
     
-    r = session.get(
-    "https://fit4lifehealthclubs.com/", headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    k  = soup.find('li', {'id': 'menu-item-864'}).find_all('ul',{'class':'sub-menu'})
+    all_store_data = []
+
+    k = soup.find_all(class_='wpgmp_locations')
     for i in k:
+        store = []
+        if "coming soon" in i.text.lower():
+            continue
+        link = i.find_all("a")[-1]['href']
+        print(link)
+        location_name= i.find_all("a")[-2].text
+        raw_address = i.find(class_="wpgmp_locations_content").text.strip().split("\n")
+        try:
+            address = (raw_address[-5] + " " + raw_address[-4]).strip()
+        except:
+            address = raw_address[-4]
 
-        link1 = i.find_all('a')
-        
+        city, state, zip1 = addy_ext(raw_address[-3])
+        phone = raw_address[-2]
 
-        for i in link1: 
-                link = i['href']
-                location_name= i.text
-                 
-                
-                r1 = session.get(link, headers=headers)
-                soup1 = BeautifulSoup(r1.text, "lxml")
-                if soup1.find("span",text=re.compile(" OPENING ")):
-                        continue
-                address_tmp = soup1.find_all('div',{'class':'wpb_text_column'})
-                lng = soup1.find_all('iframe')[1]['src'].split('!2d')[1].split('!3d')[0]             
-                lat = soup1.find_all('iframe')[1]['src'].split('!2d')[1].split('!3d')[1].split('!')[0]
-               
-              
-                store =[]
-                if(len(address_tmp) ==11):
-                        address_tmp1= address_tmp[3]
-                        address_tmp2=list(address_tmp1.stripped_strings)                       
-                        hour = address_tmp[-1].text.strip().replace('\n','').strip().replace('\xa0','').strip().replace('        ','')
-                        if(len(address_tmp2)==2):
-                                address_tmp3= address_tmp[2]
-                                address_tmp4=list(address_tmp3.stripped_strings)
-                                address = address_tmp4[1]
-                                city_tmp = address_tmp4[2].split(',')
-                                city =city_tmp[0]
-                                state_tmp = city_tmp[1].split(' ')
-                                state = state_tmp[1]
-                                zip1 =state_tmp[2]
-                                phone_tmp = address_tmp[3]
-                                phone_tmp1= list(phone_tmp.stripped_strings)
-                                phone = phone_tmp1[1]
-                                
-                                
-                        elif(len(address_tmp2)==5):
-                                address_tmp3= address_tmp[3]
-                                address_tmp4=list(address_tmp3.stripped_strings)
-                                address = address_tmp4[1].replace('(Brinkley Commons – Next to new Walmart)','')
-                                city_tmp = address_tmp4[2].split(' ')
-                                phone_tmp = address_tmp[3]
-                                phone_tmp1= list(phone_tmp.stripped_strings)
-                                phone = phone_tmp1[4]
-                                
-                                
-                                if(len(city_tmp)==3):
-                                        city =city_tmp[0]
-                                        state = city_tmp[1].replace('\u200e','')
-                                        
-                                        zip1 = city_tmp[2].replace('\u200e','')
-                                        
-                                elif(len(city_tmp)==4):
-                                        city =city_tmp[0]+''+city_tmp[1]
-                                        state =city_tmp[2]
-                                        zip1= city_tmp[3]
-                                        
+        r1 = session.get(link, headers=headers)
+        soup1 = BeautifulSoup(r1.text, "lxml")
+        try:
+            lng = soup1.find_all('iframe')[1]['src'].split('!2d')[1].split('!3d')[0]
+            lat = soup1.find_all('iframe')[1]['src'].split('!2d')[1].split('!3d')[1].split('!')[0]
+        except:
+            continue
+        raw_hours = soup1.find_all('div', {'class': re.compile(r'wpb_text_column wpb_content_element vc_custom_[0-9]+')})[-2].p.text.strip()
+        hour = raw_hours.replace('\n','').strip().replace('\xa0','').replace("pm","pm ").strip()
+        store.append('https://fit4lifehealthclubs.com/')
+        store.append(location_name)
+        store.append(address)
+        store.append(city)
+        store.append(state) 
+        store.append(zip1)
+        store.append('US')
+        store.append("<MISSING>")
+        store.append(phone)
+        store.append("<MISSING>")
+        store.append(lat)
+        store.append(lng)
+        store.append(hour)
+        store.append(link)
+        all_store_data.append(store)
 
-                        elif(len(address_tmp2)==6):
-                                address_tmp3= address_tmp[3]
-                                address_tmp4=list(address_tmp3.stripped_strings)  
-                                address = address_tmp4[2]
-                                city_tmp =address_tmp4[3].split(',')
-                                city =city_tmp[0]
-                                state_tmp = city_tmp[1].split(' ')
-                                state =state_tmp[1]
-                                zip1 =state_tmp[2]
-                               
-                elif(len(address_tmp) ==10):
-                        address_tmp3= address_tmp[2]
-                        address_tmp4=list(address_tmp3.stripped_strings)
-                        hour = address_tmp[-1].text.strip().replace('\n','').strip().replace('\xa0','').strip().replace('        ','')
-                        
-                      
-                        if(len(address_tmp4)==6):
-                                address = address_tmp4[2]
-                                city_tmp = address_tmp4[3].split(',')
-                                city =city_tmp[0]
-                                state_tmp = city_tmp[1].split(' ')
-                                state = state_tmp[1]
-                                zip1= state_tmp[2].replace('xa','').strip()
-                                phone = address_tmp4[5]
-                                
-                               
-                               
-                        if(len(address_tmp4)==5):
-                                address =address_tmp4[1]
-                                city_tmp = address_tmp4[2].split(',')
-                                city =city_tmp[0]
-                                state_tmp = city_tmp[1].split(' ')
-                                state =state_tmp[1]
-                                zip1= state_tmp[2]
-                                phone = address_tmp4[4]
-                              
-                                
-                        if(len(address_tmp4)==8):
-                                address =address_tmp4[2]
-                                city_tmp = address_tmp4[3].split(',')
-                                city = city_tmp[0]
-                                state_tmp = city_tmp[1].split(' ')
-                                state =state_tmp[1]
-                                zip1= state_tmp[2]
-                                phone = address_tmp4[5]
-                                
-                          
-                elif(len(address_tmp) ==3):  
-                        address_tmp3= address_tmp[1]
-                        address_tmp4=list(address_tmp3.stripped_strings) 
-                        address = address_tmp4[1]
-                        city_tmp =address_tmp4[2].split(',')
-                        city =city_tmp[0]
-                        state_tmp = city_tmp[1].split(' ')
-                        state = state_tmp[1]
-                        zip1= state_tmp[2] 
-                        phone = '<MISSING>'
-                        hour =  '<MISSING>'
-                      
-                        
-                elif(len(address_tmp) ==9):
-                        address_tmp3= address_tmp[2]
-                        address_tmp4=list(address_tmp3.stripped_strings) 
-                        address =address_tmp4[2]
-                        city_tmp = address_tmp4[3].split(',')
-                        city =city_tmp[0]
-                        state_tmp = city_tmp[1].split(' ')
-                        state = state_tmp[1]
-                        zip1 = state_tmp[2]
-                        phone = address_tmp4[5]
-                        hour = address_tmp[-1].text.strip().replace('\n','').strip().replace('\xa0','').strip().replace('        ','')
-                      
-                         
-
-                elif(len(address_tmp) ==12):
-                        address_tmp3= address_tmp[3]
-                        address_tmp4=list(address_tmp3.stripped_strings)   
-                        address = address_tmp4[2]
-                        city_tmp = address_tmp4[3].split(',')
-                        city =city_tmp[0]
-                        state_tmp = city_tmp[1].split(' ')
-                        state = state_tmp[1].replace('xa','')
-                        zip1 = state_tmp[2]
-                        phone_tmp = address_tmp[4]
-                        phone_tmp1= list(phone_tmp.stripped_strings) 
-                        phone = phone_tmp1[1]
-                        hour = address_tmp[-1].text.strip().replace('\n','').strip().replace('\xa0','').strip().replace('        ','')
-                        
-                store.append('https://fit4lifehealthclubs.com/')
-                store.append(location_name)
-                store.append(address)
-                store.append(city)
-                store.append(state) 
-                store.append(zip1)
-                store.append('US')
-                store.append("<MISSING>")
-                store.append(phone)
-                store.append("<MISSING>")
-                store.append(lat)
-                store.append(lng)
-                store.append(hour)
-                store.append(link)
-                for i in range(len(store)):
-                        if type(store[i]) == str:
-                                store[i] = ''.join((c for c in unicodedata.normalize('NFD', store[i]) if unicodedata.category(c) != 'Mn'))
-                store = [x.replace("–","-") if type(x) == str else x for x in store]
-                store = [x.encode('ascii', 'ignore').decode('ascii').strip() if type(x) == str else x for x in store]
-                yield store
+    driver.close()
+    return all_store_data
 
 def scrape():
     data = fetch_data()
