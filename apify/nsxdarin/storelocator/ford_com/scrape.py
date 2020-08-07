413975793a273216dee62fdfa003f4accaac0ed2
Tenzing Shaw <tenzing@safegraph.com>
2020-08-06 17:42:48 -0700

    fix ford.com

--- apify/nsxdarin/storelocator/ford_com/scrape.py
+++ apify/nsxdarin/storelocator/ford_com/scrape.py
@@ -1,5 +1,4 @@
 import csv
-import urllib2
 from sgrequests import SgRequests
 import sgzip
 import json
@@ -17,57 +16,60 @@ def write_output(data):
 
 def fetch_data():
     ids = []
-    for code in sgzip.for_radius(100):
-        print('Pulling Zip Code %s...' % code)
+    search = sgzip.ClosestNSearch()
+    search.initialize()
+    code = search.next_zip()
+    while code:
+        #print('Pulling Zip Code %s...' % code)
+        #print("remaining zipcodes: " + str(len(search.zipcodes)))
+        result_coords = []
         url = 'https://www.ford.com/services/dealer/Dealers.json?make=Ford&radius=500&filter=&minDealers=1&maxDealers=100&postalCode=' + code
-        LFound = True
-        while LFound:
-            try:
-                LFound = False
-                r = session.get(url, headers=headers, timeout=10)
-                if '"Dealer":[{' in r.content:
-                    for item in json.loads(r.content)['Response']['Dealer']:
-                        lng = item['Longitude']
-                        lat = item['Latitude']
-                        name = item['Name']
-                        typ = item['dealerType']
-                        website = 'ford.com'
-                        purl = item['URL']
-                        hours = ''
-                        add = item['Address']['Street1'] + ' ' + item['Address']['Street2'] + ' ' + item['Address']['Street3']
-                        add = add.strip()
-                        city = item['Address']['City']
-                        state = item['Address']['State']
-                        country = item['Address']['Country'][:2]
-                        zc = item['Address']['PostalCode']
-                        store = item['SalesCode']
-                        phone = item['Phone']
-                        daytext = str(item['SalesHours'])
-                        daytext = daytext.replace("'",'"')
-                        daytext = daytext.replace('u"','"').replace(' {','{')
-                        days = daytext.split(',{')
-                        for day in days:
-                            if '"name": "' in day:
-                                dname = day.split('"name": "')[1].split('"')[0]
-                                if '"closed": "true"' in day:
-                                    hrs = 'Closed'
-                                else:
-                                    hrs = day.split('"open": "')[1].split('"')[0] + '-' + day.split('"close": "')[1].split('"')[0]
-                                if hours == '':
-                                    hours = dname + ': ' + hrs
-                                else:
-                                    hours = hours + '; ' + dname + ': ' + hrs
-                        if store not in ids:
-                            ids.append(store)
-                            if hours == '':
-                                hours = '<MISSING>'
-                            if purl == '':
-                                purl = '<MISSING>'
-                            if phone == '':
-                                phone = '<MISSING>'
-                            yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
-            except:
-                LFound = True
+        js = session.get(url, headers=headers, timeout=10).json()
+        if 'Dealer' in js['Response']:
+            dealers = js['Response']['Dealer'] if type(js['Response']['Dealer']) == type([]) else [js['Response']['Dealer']]
+            for item in dealers:
+                lng = item['Longitude']
+                lat = item['Latitude']
+                result_coords.append((lat, lng))
+                name = item['Name']
+                typ = item['dealerType']
+                website = 'ford.com'
+                purl = item['URL']
+                hours = ''
+                add = item['Address']['Street1'] + ' ' + item['Address']['Street2'] + ' ' + item['Address']['Street3']
+                add = add.strip()
+                city = item['Address']['City']
+                state = item['Address']['State']
+                country = item['Address']['Country'][:2]
+                zc = item['Address']['PostalCode']
+                store = item['SalesCode']
+                phone = item['Phone']
+                daytext = str(item['SalesHours'])
+                daytext = daytext.replace("'",'"')
+                daytext = daytext.replace('u"','"').replace(' {','{')
+                days = daytext.split(',{')
+                for day in days:
+                    if '"name": "' in day:
+                        dname = day.split('"name": "')[1].split('"')[0]
+                        if '"closed": "true"' in day:
+                            hrs = 'Closed'
+                        else:
+                            hrs = day.split('"open": "')[1].split('"')[0] + '-' + day.split('"close": "')[1].split('"')[0]
+                        if hours == '':
+                            hours = dname + ': ' + hrs
+                        else:
+                            hours = hours + '; ' + dname + ': ' + hrs
+                if store not in ids:
+                    ids.append(store)
+                    if hours == '':
+                        hours = '<MISSING>'
+                    if purl == '':
+                        purl = '<MISSING>'
+                    if phone == '':
+                        phone = '<MISSING>'
+                    yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+            search.max_count_update(result_coords)
+        code = search.next_zip()
 
 def scrape():
     data = fetch_data()
