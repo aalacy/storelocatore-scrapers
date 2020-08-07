d060d47c1396cd896d0934a8f7bafd64d5f7bab8
Tenzing Shaw <tenzing@safegraph.com>
2020-08-06 18:43:57 -0700

    fix

--- apify/himanshu/storelocator/walgreens_com/scrape.py
+++ apify/himanshu/storelocator/walgreens_com/scrape.py
@@ -1,11 +1,10 @@
-# coding=UTF-8
 import csv
-from bs4 import BeautifulSoup
-import re
 import json
 import sgzip
-import time
-import requests
+from sgrequests import SgRequests
+
+session = SgRequests()
+
 def write_output(data):
     with open('data.csv', mode='w', encoding="utf-8", newline="") as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -18,42 +17,21 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
-    return_main_object = []
-    addresses = []
+    keys = set()
     search = sgzip.ClosestNSearch()
     search.initialize()
-    MAX_RESULTS = 1000
     MAX_DISTANCE = 25
-    current_results_len = 0  # need to update with no of count.
     zip_code = search.next_zip()
-    headers = {
-                'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36",
-                'Accept': "application/json, text/plain, */*",
-                'Content-Type': "application/json; charset=UTF-8",
-                'Cache-Control': "no-cache",
-                'Postman-Token': "b15101fa-fad9-40bd-ae46-a16720e8afc8,59f80690-1b85-4654-adee-313ecbdfd7d6",
-                'Host': "www.walgreens.com",
-                'Accept-Encoding': "gzip, deflate",
-                'Content-Length': "101",
-                # 'Cookie': "dtCookie=6$A3425DF8397009D51DBAAA43E235903D; bm_sz=F124BF48E16D20D9D57CDBC52FD9C434~YAAQdzYauK+WoglyAQAAXQ9mJwesiiUOaw4YE4q30HDpCIiq2pitnMem0H9mz2D8rYmMd9rOQCtFHN8qj9AW9GjvNCqGEtnokcC2TnCNKqH/pe4wKyv5NFQuB1lWD/cdulHeRlH0IVHW9071vRsqNmFwAuaIHH18RSjUaQxdVMpNjJWTZptV+bCLGFRKDNc8LH0T; _abck=5A86A806EC7840014A3875C784EF84EA~-1~YAAQdzYauLCWoglyAQAAXQ9mJwMLS6oEE75LWvrjEqe00QAWRF+GNIGc+41AMs4q60iQMir0xCjU4thqnvSltFyOcVrGbsN+r94vzGlKwtQJ3AGyNhXSatWR4iS/OHFV6hoa+GULFQWaUyRoEYZXo7IsdL0VEtweS90nH/5kR3tB0GeGcPqdLHmlNNFx5jQAsvwGgLxP1YYrQGbNG9KVCXp6g9vsOzHsSJCG/IbMwcpxozAvfc25ObkMBQIN1+xJekV2ZWKCWF7pKHm5x1WVR9F6/8kQrT/CJJaoQ8xSd7+b0ZwlgZKuppUbyNxT~-1~-1~-1",
-                'Connection': "keep-alive",
-                'cache-control': "no-cache"
-                }
     base_url = "https://www.walgreens.com"
     while zip_code:
         result_coords = []
-        # print(zip_code)
-        # print("remaining zipcodes: " + str(len(search.zipcodes)))
-        page = 1
         url = "https://www.walgreens.com/locator/v1/stores/search?requestor=search"
         payload = "{\"r\":\"1000\",\"zip\":%s,\"requestType\":\"dotcom\",\"s\":\"1000\"}"%('"{}"'.format(str(zip_code)))
         headers = {
-        'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         }
-        r = requests.post( url, headers=headers, data = payload).json()
+        r = session.post( url, headers=headers, data = payload).json()
         if "results" in r:
-            current_results_len= len(r['results'])
-            # print(current_results_len)
             locator_domain = base_url
             location_name = ""
             street_address = ""
@@ -85,35 +63,24 @@ def fetch_data():
                 else:
                     phone = "<MISSING>"  
                 page_url = "https://www.walgreens.com"+location['storeSeoUrl']
-                try:
-                    r1 = requests.get(page_url ,headers=headers)
-                except:
-                    pass
-                soup = BeautifulSoup(r1.text,"lxml")
-                try:
-                    hours_of_operation = " ".join(list(soup.find("div",{"class":'service-section'}).find("li",{"class":'single-hours-lists'}).stripped_strings))
-                except:
-                    hours_of_operation = "<MISSING>"
-
+                days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+                start = location['store']['storeOpenTime']
+                end = location['store']['storeCloseTime']
+                hours_of_operation = ', '.join(['{}: {}-{}'.format(day, start, end) for day in days])
                 result_coords.append((latitude, longitude))
                 store = [locator_domain, location_name, location['store']['address']['street'].capitalize(), location['store']['address']['city'].capitalize(), location['store']['address']['state'], zipp, country_code,
                         storeNumber, phone.strip(), location_type, latitude, longitude, hours_of_operation,page_url]
 
-                if str(store[2]) in addresses:
+                if storeNumber in keys:
                     continue
-                addresses.append(str(store[2]) )
+                keys.add(storeNumber)
                 store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
-                # print("data =="+str(store))
-                # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`")
                 yield store
-        if current_results_len < MAX_RESULTS:
+        if len(result_coords) == 0:
             search.max_distance_update(MAX_DISTANCE)
-        elif current_results_len == MAX_RESULTS:
-            search.max_count_update(result_coords)
         else:
-            raise Exception("expected at most " + str(MAX_RESULTS) + " results")
+            search.max_count_update(result_coords)
         zip_code = search.next_zip()
-
         
 def scrape():
     data = fetch_data()
