34f161003295bba54a8773a6df8e1968d26d3903
nsxdarin <nsxdarin@gmail.com>
2020-08-19 10:56:32 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/tidedrycleaners_com/scrape.py
+++ apify/nsxdarin/storelocator/tidedrycleaners_com/scrape.py
@@ -1,5 +1,4 @@
 import csv
-import urllib2
 from sgrequests import SgRequests
 
 session = SgRequests()
@@ -15,43 +14,56 @@ def write_output(data):
 
 def fetch_data():
     locs = []
-    url = 'https://www.tidedrycleaners.com/locations'
+    weekdays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+    url = 'https://consumer.tidelaundry.com/v1/servicepoints?maxLatitude=64.93218366344629&maxLongitude=-63.19580074023439&minLatitude=14.87138826455165&minLongitude=-173.33038325976564&statuses=1&statuses=2&statuses=3'
     r = session.get(url, headers=headers)
     for line in r.iter_lines():
+        line = str(line.decode('utf-8'))
+        line = line.replace('"address":{"id":','"address":{"ID')
         if '{"id":' in line:
             items = line.split('{"id":')
             for item in items:
-                if '"title":"' in item:
-                    store = item.split(',')[0]
+                if ',"name":"' in item:
                     website = 'tidedrycleaners.com'
-                    name = item.split('"title":"')[1].split('"')[0]
-                    add = item.split('"street1":"')[1].split('"')[0]
-                    city = item.split('"city":"')[1].split('"')[0]
+                    country = 'US'
+                    store = item.split(',')[0]
+                    loc = 'https://tidecleaners.com/en-us/locations/' + store
+                    name = item.split(',"name":"')[1].split('"')[0]
+                    lat = item.split('"latitude":')[1].split(',')[0]
+                    lng = item.split('"longitude":')[1].split(',')[0]
+                    add = item.split('streetLine1":"')[1].split('"')[0]
+                    try:
+                        add = add + ' ' + item.split('streetLine2":"')[1].split('"')[0]
+                    except:
+                        pass
+                    try:
+                        city = item.split(',"city":"')[1].split('"')[0]
+                    except:
+                        city = '<MISSING>'
                     state = item.split('"state":"')[1].split('"')[0]
                     try:
-                        zc = item.split('"zip":"')[1].split('"')[0]
+                        zc = item.split('"zipCode":"')[1].split('"')[0]
                     except:
                         zc = '<MISSING>'
-                    country = 'US'
-                    hours = item.split('"hours":"')[1].split('"')[0].replace('<br \\/>','; ')
                     try:
-                        phone = item.split('"phone":"')[1].split('"')[0]
+                        phone = item.split('"phoneNumber":"')[1].split('"')[0]
                     except:
                         phone = '<MISSING>'
-                    if '"icon":"store' in item:
-                        typ = 'Store'
-                    else:
-                        typ = ''
-                    try:
-                        loc = 'https://www.tidedrycleaners.com/' + item.split('"url":"')[1].split('"')[0].replace('\\','')
-                    except:
-                        loc = '<MISSING>'
-                    if '"coordinates":[" ' in item:
-                        lat = item.split('"coordinates":[" ')[1].split(',"')[1].split('"')[0]
-                        lng = item.split('"coordinates":[" ')[1].split('"')[0]
+                    typ = 'Store'
+                    hours = ''
+                    days = item.split('hoursOfOperation":[')[1].split(']')[0].split('{"weekday":')
+                    for day in days:
+                        if '"opensLocal":"' in day:
+                            daynum = day.split(',')[0]
+                            dayname = weekdays[int(daynum)]
+                            hrs = dayname + ': ' + day.split('"opensLocal":"')[1].split('"')[0] + '-' + day.split('"closesLocal":"')[1].split('"')[0]
+                            if hours == '':
+                                hours = hrs
+                            else:
+                                hours = hours + '; ' + hrs
                     if hours == '':
                         hours = '<MISSING>'
-                    if typ == 'Store':
+                    if '"locationTypeId":1' in item:
                         yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
