559e8227054b008bae522d762062ec0d2c05a9a4
diapk <Hank4957!>
2020-08-19 18:11:48 +0530

    docker run and push

--- apify/himanshu/storelocator/budgettruck_ca/scrape.py
+++ apify/himanshu/storelocator/budgettruck_ca/scrape.py
@@ -18,33 +18,30 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
-    return_main_object = []
     addressess = []
 
     headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
     }
-    base_url = "https://www.budgettruck.com/"
-    r = session.get("https://www.budgettruck.ca/en/locations/ca",headers=headers)
-    soup = BeautifulSoup(r.text,"lxml")
+    base_url = "https://www.budgettruck.ca/"
 
-    for li in soup.find("div",{"class":"country-wrapper row"}).find("ul").find_all("li"):
-        state_link = "https://www.budgettruck.ca" + li.find("a")['href']
-        state_request = session.get(state_link)
-        state_soup = BeautifulSoup(state_request.text,"lxml")
-        for c in state_soup.find("div",{"class":"location-state kansas-section"}).find_all("div",{"class":"col-sm-12 col-xs-12 border-bottom padd-top0 padd-right0"}):
-            city_link = "https://www.budgettruck.ca" + c.find("a")['href']
-           # print(city_link)
-            city_request = session.get(city_link)
-            city_soup = BeautifulSoup(city_request.text,"lxml")
-            # script = city_soup.find(lambda tag: (tag.name == "script") and 'stringyStations' in tag.text)
-            if "var stringyStations = '" in str(city_soup):
-                script = str(city_soup).split("var stringyStations = '")[1].split("';")[0]
-                json_data = json.loads(script)
-            else:
-                continue
-
+    states = session.get("https://www.budgettruck.ca/en/bin/location-browse.html/ca.json").json()
+    for region in states:
+        city_list = session.get("https://www.budgettruck.ca/en/bin/location-browse.html/"+str(region['relPath'])+".json").json()
+    
+        for city_name in city_list:
             
+            payload = "{\"country\":\"CA\",\"state\":\""+str(region['code'].upper())+"\",\"city\":\""+str(city_name['name'])+"\",\"rqHeader\":{\"locale\":\"en_US\",\"domain\":\"ca\"}}"
+            headers = {
+                'content-type': "application/json",
+                'cache-control': "no-cache",
+
+                }
+            try:
+                json_data = session.post("https://www.budgettruck.ca/webapi/station/lat-long", data=payload, headers=headers).json()['stationInfoList']
+            except:
+                continue
+           
             for dealer in json_data:
                 locator_domain = base_url
                 location_name = dealer['description']
@@ -53,15 +50,16 @@ def fetch_data():
                 state = dealer['stateCode']
                 zipp = dealer['zipCode']
                 country_code = dealer['countyCode']
-                store_number = dealer['locationCode']
                 phone = dealer['phoneNumber']
                 location_type = dealer['licInd']
-                latitude = dealer['latitude']
-                longitude = dealer['longitude']
+                try:
+                    latitude = dealer['latitude']
+                    longitude = dealer['longitude']
+                except:
+                    latitude = "<MISSING>"
+                    longitude = "<MISSING>"
                 hours_of_operation = dealer['hoursOfOperation']
-                for page in city_soup.find_all("div",{"class":"locTitl"}):
-                    if store_number.lower() in page.find("a")['ng-href'].split("/")[-1]:
-                        page_url = "https://www.budgettruck.ca" + page.find("a")['ng-href']
+                page_url = "https://www.budgettruck.ca/en/locations/"+dealer['augmentDataMap']['REL_PATH']
 
                 
                 store = []
@@ -85,6 +83,7 @@ def fetch_data():
                     continue
                 addressess.append(store[2])
                 yield store
+           
 
 
 
