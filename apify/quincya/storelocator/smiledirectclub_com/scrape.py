2056229ac6e3ffd0a40c1f231130b0c5fea321f3
Quincy <qcalexander@gmail.com>
2020-08-20 02:46:26 -0400

    smiledirectclub_com fixed

--- apify/quincya/storelocator/smiledirectclub_com/scrape.py
+++ apify/quincya/storelocator/smiledirectclub_com/scrape.py
@@ -5,8 +5,6 @@ from random import randint
 import re
 from sgselenium import SgSelenium
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
