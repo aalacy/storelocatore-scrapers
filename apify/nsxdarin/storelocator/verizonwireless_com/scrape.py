71cea152a756e6fbddc11cb91753c7ac5c00ab70
vivri <victor.ivri@gmail.com>
2020-08-25 23:11:04 -0400

    [SLC-2179] verizonwireless.com: Fixed usage of ClosestNSearch, as well as multiple code issues

--- apify/nsxdarin/storelocator/verizonwireless_com/scrape.py
+++ apify/nsxdarin/storelocator/verizonwireless_com/scrape.py
@@ -2,12 +2,15 @@ import csv
 from sgrequests import SgRequests
 import sgzip
 from tenacity import retry, stop_after_attempt
+from typing import *
+import json
 
 search = sgzip.ClosestNSearch()
 search.initialize()
 
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
-           }
+MISSING = '<MISSING>'
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
 
 MAX_RESULTS = 250
 MAX_DISTANCE = 5
@@ -24,66 +27,90 @@ def request_with_retries(url):
     session = SgRequests()
     return session.get(url, headers=headers)
 
+def parse_store_locations(iter_lines: Iterator):
+    for b_line in iter_lines:
+        line = str(b_line)
+        if "var searchJSON" in line:
+            json_starts = line.find("[")
+            json_ends = line.rfind("]") + 1
+            json_str = line[json_starts:json_ends].strip().replace("\\","") # we don't care about escape chars
+
+            try:
+                return json.loads(json_str)
+            except json.decoder.JSONDecodeError:
+                print(f"Cannot parse string to json (most likely due to 'no results' javascript) : {json_str}")
+                return []
+
+    return []
+
+def get(record: Dict[str, str], key: str):
+    try:
+        value = record[key]
+        if isinstance(value, list):
+            v_strip = [item.strip() for item in value if item.strip]
+            return ", ".join(v_strip) if v_strip else MISSING
+        elif isinstance(value, dict):
+            v_strip = [f"{k.strip()}: {v.strip()}" for k, v in value.items() if k.strip() ]
+            return ", ".join(v_strip) if v_strip else MISSING
+        elif isinstance(value, str):
+            v_strip = value.strip()
+            return v_strip if v_strip else MISSING
+        else:
+            raise ValueError(f"Unknown record type: {key} in {record}")
+
+    except KeyError:
+        return MISSING
+
+
 def fetch_data():
     ids = set()
-    locations = []
-    alllocs = []
     coord = search.next_coord()
     while coord:
-        #print("remaining zipcodes: " + str(len(search.zipcodes)))
         x = coord[0]
         y = coord[1]
         website = 'verizonwireless.com'
-        #print('%s, %s...' % (x, y))
-        url = 'https://www.verizonwireless.com/stores/storesearchresults/?lat=' + str(x) + '&long=' + str(y)
-        r = request_with_retries(url) 
+        url = f'https://www.verizonwireless.com/stores/storesearchresults/?lat={x}&long={y}'
+        r = request_with_retries(url)
         result_coords = []
-        purl = '<MISSING>'
-        array = []
-        for line in r.iter_lines():
-            if 'var searchJSON' in line:
-                items = line.split('"storeName":"')
-                for item in items:
-                    if '"address":"' in item:
-                        purl = 'https://www.verizonwireless.com' + item.split('"storeUrl":"')[1].split('"')[0]
-                        typ = item.split('"typeOfStore":["')[1].split('"')[0]
-                        name = item.split('"')[0]
-                        add = item.split('"address":"')[1].split('"')[0]
-                        city = item.split('"city":"')[1].split('"')[0]
-                        state = item.split('"stateAbbr":"')[1].split('"')[0]
-                        country = 'US'
-                        zc = item.split('"zip":"')[1].split('"')[0]
-                        store = item.split('"storeNumber":"')[1].split('"')[0]
-                        lat = item.split('lat":"')[1].split('"')[0]
-                        lng = item.split('lng":"')[1].split('"')[0]
-                        try:
-                            phone = item.split('"phone":"')[1].split('"')[0]
-                        except:
-                            phone = '<MISSING>'
-                        try:
-                            hours = item.split('"openingHours":{')[1].split('}')[0].replace('":"',': ').replace('","','; ').replace('"','')
-                        except:
-                            hours = ''
-                        info = add + ';' + city + ';' + state
-                        if hours == '':
-                            hours = '<MISSING>'
-                        ids.add(info)
-                        array.append(info)
-                        if purl not in alllocs:
-                            alllocs.append(purl)
-                            yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
-        if len(array) <= MAX_RESULTS:
-            #print("max distance update")
+
+        json_results = parse_store_locations(r.iter_lines())
+
+        for j_result in json_results:
+            page_url = 'https://www.verizonwireless.com' + j_result['storeUrl']
+            if page_url in ids:
+                continue
+            else:
+                ids.add(page_url)
+
+            store_name = get(j_result, 'storeName')
+            address = get(j_result, 'address')
+            city = get(j_result, 'city')
+            state = get(j_result, 'state')
+            zipcode = get(j_result, 'zip')
+            country = 'US'
+            store_number = get(j_result, 'storeNumber')
+            phone = get(j_result, 'phone')
+            store_type = get(j_result, 'typeOfStore')
+            lat = get(j_result, 'lat')
+            lng = get(j_result, 'lng')
+            hours = get(j_result, 'openingHours')
+
+            result_coords.append((lat, lng))
+
+            yield [website, page_url, store_name, address, city, state, zipcode, country, store_number, phone, store_type, lat, lng, hours]
+
+        if len(result_coords) < MAX_RESULTS:
             search.max_distance_update(MAX_DISTANCE)
-##        elif len(array) == MAX_RESULTS:
-##            print("max count update")
-##            search.max_count_update(result_coords)
+        elif len(result_coords) == MAX_RESULTS:
+            search.max_count_update(result_coords)
         else:
             raise Exception("expected at most " + str(MAX_RESULTS) + " results")
+
         coord = search.next_coord()
 
 def scrape():
     data = fetch_data()
     write_output(data)
 
-scrape()
+if __name__ == "__main__":
+    scrape()
