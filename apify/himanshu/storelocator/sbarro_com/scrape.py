e65d56580931d628b4f26c9c378b55339a9ee6b0
diapk <Hank4957!>
2020-08-27 15:52:33 +0530

    docker run and push

--- apify/himanshu/storelocator/sbarro_com/scrape.py
+++ apify/himanshu/storelocator/sbarro_com/scrape.py
@@ -1,14 +1,69 @@
 import csv
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
-import re
-import json
 
-
+sta = {
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+}
+states_lp = sta.keys()
 session = SgRequests()
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
+    with open('data.csv', mode='w',encoding="utf-8") as output_file:
         writer = csv.writer(output_file, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_ALL)
 
@@ -19,73 +74,83 @@ def write_output(data):
         for row in data:
             writer.writerow(row)
 
-
 def fetch_data():
-    addresses =[]
-    addresses1 =[]
-    #### US location
+    addresses = []
     base_url = "https://sbarro.com"
-    
-    
-    r = session.get("https://sbarro.com/locations/?user_search=")
-    soup = BeautifulSoup(r.text, "lxml")
-    links = soup.find_all("h2",{"class":"location-name"})
-    coords = soup.find_all("section",{"class":"locations-result"})
-    latitude = []
-    longitude = []
-    for coord in coords:
-        latitude.append(coord['data-latitude'])
-        longitude.append(coord['data-longitude'])
-    
-    for index,link in enumerate(links):
-        page_url = base_url+link.find("a")['href']
-        r1 = session.get(page_url)
-        soup1 = BeautifulSoup(r1.text, "lxml")
-        location_name = soup1.find("h1",{"class":"location-name"}).text.strip()
-        street_address = " ".join(soup1.find("p",{"class":"location-address"}).text.replace(",,",',').split(",")[:-2]).strip()
-        city = soup1.find("p",{"class":"location-address"}).text.replace(",,",',').split(",")[-2].strip().capitalize()
-        temp_state_zip = soup1.find("p",{"class":"location-address"}).text.replace(",,",',').split(",")[-1].strip().split(" ")
-        if len(temp_state_zip) == 2:
-            state = temp_state_zip[0]
-            zipp = temp_state_zip[1]
-        else:
-            state = temp_state_zip[0]
-            zipp = "<MISSING>"
-        try:
-            phone = soup1.find("div",{"class":"location-phone location-cta"}).find("span",{"class":"btn-label"}).text.strip()
-        except:
-            phone = "<MISSING>"
-        hours_of_operation = " ".join(list(soup1.find("div",{"class":"location-hours"}).stripped_strings))
-        country_code = "US"
-        location_type = "Restaurant"
+    for st in states_lp:
+        # print(st)
+        location_url = "https://sbarro.com/locations/?user_search=" + str(st)
+        r = session.get(location_url)
+        soup = BeautifulSoup(r.text, "lxml")
+        links = soup.find_all("h2",{"class":"location-name"})
+        for index,link in enumerate(links):
+            coords = soup.find_all("section",{"class":"locations-result"})
+            latitude = []
+            longitude = []
+            for coord in coords:
+                latitude.append(coord['data-latitude'])
+                longitude.append(coord['data-longitude'])
+            page_url = base_url+link.find("a")['href']
+            r1 = session.get(page_url)
+            soup1 = BeautifulSoup(r1.text, "lxml")
+            try:
 
-        store = []
-        store.append(base_url)
-        store.append(location_name if location_name else "<MISSING>")
-        store.append(street_address if street_address else "<MISSING>")
-        store.append(city if city else "<MISSING>")
-        store.append(state if state else "<MISSING>")
-        store.append(zipp if zipp else "<MISSING>")
-        store.append(country_code if country_code else "<MISSING>")
-        store.append("<MISSING>") 
-        store.append(phone if phone else "<MISSING>") 
-        store.append(location_type if location_type else "<MISSING>")
-        store.append(latitude[index])
-        store.append(longitude[index])
-        store.append(hours_of_operation.replace("Hours of Operation","") if hours_of_operation.replace("Hours of Operation","") else "<MISSING>")
-        store.append(page_url)
-
-        if store[2] in addresses1:
+                try:
+                    location_name = soup1.find("h1",{"class":"location-name"}).text.strip()
+                except:
+                    location_name = soup1.find("h2",{"class":"location-name"}).text.strip()
+            except:
                 continue
-        addresses1.append(store[2])
-        # print("data===="+str(store))
-        # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`")
-        yield store
+                
+            
+            # print(location_name)
+            street_address = " ".join(soup1.find("p",{"class":"location-address"}).text.replace(",,",',').split(",")[:-2]).strip()
+            city = soup1.find("p",{"class":"location-address"}).text.replace(",,",',').split(",")[-2].strip().capitalize()
+            temp_state_zip = soup1.find("p",{"class":"location-address"}).text.replace(",,",',').split(",")[-1].strip().split(" ")
+            # print(temp_state_zip)
+            if len(temp_state_zip) == 2:
+                state = temp_state_zip[0]
+                zipp = temp_state_zip[1]
+            elif len(temp_state_zip) == 3:
+                state = temp_state_zip[0]
+                zipp = temp_state_zip[2]
+            else:
+                state = temp_state_zip[0]
+                zipp = "<MISSING>"
+            try:
+                phone = soup1.find("div",{"class":"location-phone location-cta"}).find("span",{"class":"btn-label"}).text.strip()
+            except:
+                phone = "<MISSING>"
+            try:
+                hours_of_operation = " ".join(list(soup1.find("div",{"class":"location-hours"}).stripped_strings))
+            except:
+                hours_of_operation = "<MISSING>"
 
+            country_code = "US"
+            location_type = "Restaurant"
+            if state == st:
 
+                store = []
+                store.append(base_url)
+                store.append(location_name if location_name else "<MISSING>")
+                store.append(street_address if street_address else "<MISSING>")
+                store.append(city if city else "<MISSING>")
+                store.append(state if state else "<MISSING>")
+                store.append(zipp if zipp else "<MISSING>")
+                store.append(country_code if country_code else "<MISSING>")
+                store.append("<MISSING>") 
+                store.append(phone if phone else "<MISSING>") 
+                store.append(location_type if location_type else "<MISSING>")
+                store.append(latitude[index])
+                store.append(longitude[index])
+                store.append(hours_of_operation.replace("Hours of Operation","") if hours_of_operation.replace("Hours of Operation","") else "<MISSING>")
+                store.append(page_url)
+                if store[2] in addresses:
+                        continue
+                addresses.append(store[2])
+                yield store
+                
 def scrape():
     data = fetch_data()
     write_output(data)
-
-
 scrape()
