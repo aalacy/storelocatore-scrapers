3ad1383c953873c306ca8083d9d97ecab6f8f00e
Michael Dinh <mkdinh94@gmail.com>
2020-08-27 22:19:34 -0400

    SLC-2714/zacksfashions_com: rewrite to mkdinh using cheerio crawler

---
+++ apify/mkdinh/storelocator/zacksfashions_com/run_scraper.sh
@@ -0,0 +1,8 @@
+base_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $base_path
+domain=${PWD##*/}
+scraper_name=${domain}-scraper
+docker build -t $scraper_name --no-cache .
+rm -rf apify_docker_storage
+docker rm $scraper_name
+docker run -e "GOOGLE_API_KEY" -e "APIFY_LOCAL_STORAGE_DIR"="apify_storage" -e "APIFY_TOKEN=''" -v "/${base_path}/apify_docker_storage":/apify_storage --name $scraper_name ${scraper_name}:latest
