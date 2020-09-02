53ab282561e323607c19c5c599f0df86c78d89a0
nsxdarin <nsxdarin@gmail.com>
2020-09-02 09:14:09 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/scs_co_uk/scrape.py
+++ apify/nsxdarin/storelocator/scs_co_uk/scrape.py
@@ -15,6 +15,51 @@ def write_output(data):
 
 def fetch_data():
     locs = []
+    x = 0
+    y = 0
+    url = 'https://www.scs.co.uk/on/demandware.store/Sites-SFRA_SCS-Site/en_GB/Stores-FindStores?showMap=true&radius=1000.0&lat=' + str(x) + '&long=' + str(y) + '&storeCount=5&searchKey='
+    r = session.get(url, headers=headers)
+    for item in json.loads(r.content)['stores']:
+        name = item['name']
+        website = 'scs.co.uk'
+        try:
+            add = item['address1'] + ' ' + item['address2']
+        except:
+            add = item['address1']
+        add = add.strip()
+        city = item['city']
+        try:
+            state = item['stateCode']
+        except:
+            state = '<MISSING>'
+        zc = item['postalCode']
+        lat = item['latitude']
+        lng = item['longitude']
+        phone = item['phone']
+        country = item['countryCode']
+        store = item['ID']
+        typ = '<MISSING>'
+        purl = 'https://www.scs.co.uk/stores/' + item['storeTag']
+        hours = ''
+        hrinfo = item['openingHoursTable']
+        hrinfo = hrinfo.replace('\\r','').replace('\\n','').replace('\r','').replace('\n','')
+        days = hrinfo.split('<td class=\"day\">')
+        for day in days:
+            if 'b-store-hours' not in day:
+                hrs = day.split('<')[0] + ': ' + day.split('class=\"hour\">')[1].split('<')[0]
+                if hours == '':
+                    hours = hrs
+                else:
+                    hours = hours + '; ' + hrs
+        phone = phone.replace('"',"'")
+        lat = '51.6570083'
+        lng = '-3.9030564'
+        if 'tel:' in phone:
+            phone = phone.split("tel:")[1].split("'")[0]
+        if add not in locs:
+            locs.append(add)
+            yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+
     for x in range(50, 59):
         for y in range(-8, 1):
             print('%s-%s...' % (str(x), str(y)))
@@ -53,6 +98,8 @@ def fetch_data():
                         else:
                             hours = hours + '; ' + hrs
                 phone = phone.replace('"',"'")
+                if 'tel:' in phone:
+                    phone = phone.split("tel:")[1].split("'")[0]
                 if add not in locs:
                     locs.append(add)
                     yield [website, purl, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
