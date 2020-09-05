894cf79fc5198bc84bf5f208e6e288772f45ab43
Tenzing Shaw <tenzing@safegraph.com>
2020-09-04 20:28:00 -0700

    fix hibbett.com

--- apify/nsxdarin/storelocator/hibbett_com/scrape.py
+++ apify/nsxdarin/storelocator/hibbett_com/scrape.py
@@ -2,8 +2,6 @@ import csv
 import urllib.request, urllib.error, urllib.parse
 from sgrequests import SgRequests
 
-requests.packages.urllib3.disable_warnings()
-
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
