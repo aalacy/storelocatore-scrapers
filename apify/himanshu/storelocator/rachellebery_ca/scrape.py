a191cbf0f6e4ad589d4b73e97d73fa92b7bdbcdd
Tenzing Shaw <tenzing@safegraph.com>
2020-09-04 20:17:38 -0700

    fix rachellebery_ca

--- apify/himanshu/storelocator/rachellebery_ca/scrape.py
+++ apify/himanshu/storelocator/rachellebery_ca/scrape.py
@@ -7,8 +7,6 @@ import unicodedata
 
 session = SgRequests()
 
-requests.packages.urllib3.disable_warnings()
-
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -19,22 +17,14 @@ def write_output(data):
         for row in data:
             writer.writerow(row)
 
-
 def fetch_data():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
     }
-
-    # print("soup ===  first")
-
     base_url = "https://www.rachellebery.ca"
-    r = session.get("https://www.rachellebery.ca/trouver-un-magasin/", headers=headers,verify=False)
+    r = session.get("https://www.rachellebery.ca/trouver-un-magasin/", headers=headers)
     soup = BeautifulSoup(r.text, "lxml")
     return_main_object = []
-    #   data = json.loads(soup.find("div",{"paging_container":re.compile('latlong.push')["paging_container"]}))
-    # for link in soup.find_all('ul',re.compile('content')):
-    #     print(link)
-
     # it will used in store data.
     locator_domain = base_url
     location_name = ""
@@ -53,8 +43,6 @@ def fetch_data():
     page_url = "<MISSING>"
 
     datapath_url = soup.text.split('datapath":"')[1].split('"')[0].replace("\\", "")
-
-    # print("datapath_url === " + datapath_url)
 
     r1 = session.get(datapath_url, headers=headers,verify=False)
 
@@ -87,25 +75,18 @@ def fetch_data():
             hours_of_operation += day_list[i] + " : " + hoursfrom_list[i] + "-" + hoursto_list[i] + ", "
 
         hours_of_operation = hours_of_operation[:-2]
-        # print(" === data ==== " + str(hours_of_operation))
 
         store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
                  store_number, phone, location_type, latitude, longitude, hours_of_operation,page_url]
-
-        # print("data = " + str(store))
-        # print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         for i in range(len(store)):
             if type(store[i]) == str:
                 store[i] = ''.join((c for c in unicodedata.normalize('NFD', store[i]) if unicodedata.category(c) != 'Mn'))
         store = [x.encode('ascii', 'ignore').decode('ascii').strip() if type(x) == str else x for x in store]
         return_main_object.append(store)
-
     return return_main_object
-
 
 def scrape():
     data = fetch_data()
     write_output(data)
-
 
 scrape()
