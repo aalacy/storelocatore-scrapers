cc141c112e6c4006a7f87f83fbbef58a553377ba
Tenzing Shaw <tenzing@safegraph.com>
2020-09-04 20:05:07 -0700

    rm requests reference

--- apify/nsxdarin/storelocator/bmoharris_com/scrape.py
+++ apify/nsxdarin/storelocator/bmoharris_com/scrape.py
@@ -5,8 +5,6 @@ from sgrequests import SgRequests
 import gzip
 import os
 
-requests.packages.urllib3.disable_warnings()
-
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
            }
