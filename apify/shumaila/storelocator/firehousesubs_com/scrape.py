b1ac448a238efc0a9e2fb48452086a042a1750f7
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-09-06 22:30:37 +0500

    Add files via upload

---
+++ apify/shumaila/storelocator/firehousesubs_com/scrape.py
@@ -0,0 +1,104 @@
+from bs4 import BeautifulSoup
+import csv
+import string
+import re, time
+import json
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    # Your scraper here
+    data = []
+    cleanr = re.compile(r'<[^>]+>')
+    states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    p = 0
+
+    for statenow in states:
+        url = 'https://www.firehousesubs.com/FindLocations/GetLocationsByState/?state='+statenow
+        loclist = session.get(url, headers=headers, verify=False).json()
+        #print(loclist)
+        for loc in loclist:            
+            if str(loc['isComingSoon']) == 'False':
+                try:
+                    street = loc['address'] + ' '+loc['address2']
+                except:
+                    street = loc['address']
+                    
+                city = loc['city']
+                state = loc['state']
+                lat = loc['latitude']
+                longt = loc['longitude']
+                phone = loc['phone'].replace('\u200b','').replace('\u202c','')
+                link = loc['moreInfoUrl']
+                pcode = loc['zip']
+                hours = loc['hoursOpen']
+                title = loc['title']
+                store = loc["siteId"]
+                ccode = 'US'
+                if phone.lower().find('available') > -1 or len(phone) < 3 :
+                    phone = '<MISSING>'
+                try:
+                    if len(hours) < 3:
+                        hours = '<MISSING>'
+                except:
+                    hours = '<MISSING>'
+
+                try:
+                    if len(store) < 1:
+                        store = '<MISSING>'
+                except:
+                    store = title.split('#')[1]
+                    
+                data.append([
+                'https://www.firehousesubs.com/',
+                link,                   
+                title,
+                street,
+                city,
+                state,
+                pcode,
+                ccode,
+                store,
+                phone,
+                '<MISSING>',
+                lat,
+                longt,
+                hours
+                ])
+                #print(p,data[p])
+                p += 1
+        
+
+
+    
+                
+        
+    return data
+
+
+def scrape():
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
+    data = fetch_data()
+    write_output(data)
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
+
+scrape()
+
