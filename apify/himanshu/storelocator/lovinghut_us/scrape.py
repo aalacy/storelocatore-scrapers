45c56815dbf7c8c376dcd0091c2e614a1e80bcb3
diapk <Hank4957!>
2020-09-07 12:58:59 +0530

    docker run and push

--- apify/himanshu/storelocator/lovinghut_us/scrape.py
+++ apify/himanshu/storelocator/lovinghut_us/scrape.py
@@ -26,72 +26,59 @@ def fetch_data():
     
     address = []
     
-    base_url= "https://lovinghut.us/locations/"
+    base_url= "https://lovinghut.us/"
     r= session.get(base_url, headers=headers)
     soup=BeautifulSoup(r.text, "lxml")
-    links=soup.find_all("td",{"class":"tg-citi"})
-   
-        
-    for link in links:
-        href = (link.a["href"])
-        
-        r1 = session.get(href, headers=headers)
-        soup1 = BeautifulSoup(r1.text, "lxml")
-        data = soup1.find("p",{"class":"section_text dark"})  
-        if data != None:
-            data1 = list(data.stripped_strings)
-            iframe = soup1.find("iframe")
-            if "1804 E Southern Ave #1" in data1:
-                pass
+    states=soup.find_all("ul",{"class":"sub-menu"})
+    for i in states:
+        for j in i.find_all("li"):
+            links = j.find("a")['href']
+            if links =="#":
+                continue
             else:
-                if iframe != None:
-                    del data1[-1] 
-                    phone = data1[-1]
-                    del data1[-1]
-                    city = data1[-1].split(',')[0]
-                    state = data1[-1].split(',')[1].split(' ')[1]
-                    zipp = data1[-1].split(',')[1].split(' ')[2].replace('1604','01604')
-                    del data1[-1]
-                    del data[-1]
-                    street_address = " ".join(data1)
-                    hours = soup1.find("table",{"class":"hours"})
-                    h = list(hours.stripped_strings)
-                    hours_of_operation=" ".join(h).encode('ascii', 'ignore').decode('ascii').replace('Kitchen is closed 20 minutes before closing time every lunch and dinner.','').replace('***','').replace('(buffet)','').replace('(menu)','').replace('(buffet + menu)','')
-                    name = list(link.stripped_strings)
-                    location_name = " ".join(name)
-                    src = iframe.attrs["src"]
-                    geo_request = session.get(src, headers=headers)
-                    geo_soup = BeautifulSoup(geo_request.text, "lxml")
+                r1 = session.get(links, headers=headers)
+                soup1 = BeautifulSoup(r1.text, "lxml")
+                location_name = soup1.find("h1",{"class":"page-title h1 intro-title"}).text
+                temp_add = list(soup1.find("address",{"class":"text-white"}).stripped_strings)
+                if location_name == "Loving Hut Claremont":
+                    street_address = " ".join(temp_add[0:2])
+                    city = temp_add[2].split(",")[0]
+                    state = temp_add[2].split(" ")[-2]
+                    zipp = temp_add[2].split(" ")[-1]
+                    phone = temp_add[3]
+                else:
+                    street_address = temp_add[0]
+                    city = temp_add[1].split(",")[0]
+                    state = temp_add[1].split(" ")[-2]
+                    zipp = temp_add[1].split(" ")[-1]
+                    phone = temp_add[2]
+                hoo = list(soup1.find("div",{"class":"textwidget openhours"}).stripped_strings)
+                hours_of_operation = " ".join(hoo)
 
-                    for script_geo in geo_soup.find_all("script"):
-                        if "initEmbed" in script_geo.text:
-                            geo_data = json.loads(script_geo.text.split("initEmbed(")[1].split(");")[0])[21][3][0][1]
-                            latitude = json.loads(script_geo.text.split("initEmbed(")[1].split(");")[0])[21][3][0][2][0]
-                            longitude = json.loads(script_geo.text.split("initEmbed(")[1].split(");")[0])[21][3][0][2][1]
-                                  
-        
-                    store = []
-                    store.append("https://lovinghut.us")
-                    store.append(location_name)
-                    store.append(street_address)
-                    store.append(city)
-                    store.append(state)
-                    store.append(zipp)
-                    store.append("US")
-                    store.append("<MISSING>")
-                    store.append(phone)
-                    store.append("<MISSING>")
-                    store.append(latitude)
-                    store.append(longitude)
-                    store.append(hours_of_operation)
-                    store.append(href)
-                    if store[2] in address:
-                        continue
-                    address.append(store[2])
-                    yield store
+                store = []
+                store.append(base_url)
+                store.append(location_name)
+                store.append(street_address)
+                store.append(city)
+                store.append(state)
+                store.append(zipp)
+                store.append("US")
+                store.append("<MISSING>")
+                store.append(phone)
+                store.append("<MISSING>")
+                store.append("<MISSING>")
+                store.append("<MISSING>")
+                store.append(hours_of_operation.replace('Kitchen is closed 20 minutes before closing time every lunch and dinner.','').replace('***','').replace('(buffet)','').replace('(menu)','').replace('(buffet + menu)',''))
+                store.append(links)
+                store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+                if store[2] in address:
+                    continue
+                address.append(store[2])
+                yield store
 
 
 def scrape():
+   
     data = fetch_data()
     write_output(data)
 
