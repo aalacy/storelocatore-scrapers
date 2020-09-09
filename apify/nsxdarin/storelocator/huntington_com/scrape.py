605e06e35ea75e97537171718e339507aee6a200
Tenzing Shaw <tenzing@safegraph.com>
2020-09-08 17:01:28 -0700

    fix huntington

--- apify/nsxdarin/storelocator/huntington_com/scrape.py
+++ apify/nsxdarin/storelocator/huntington_com/scrape.py
@@ -3,7 +3,6 @@ import urllib3
 from sgrequests import SgRequests
 import json
 
-session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
            'content-type': 'application/json',
            'X-Requested-With': 'XMLHttpRequest'
@@ -18,14 +17,16 @@ def write_output(data):
 
 def fetch_data():
     url = 'https://www.huntington.com/~/media/SEO_Files/sitemap'
-    r = session.get(url, headers=headers, verify=False)
+    session = SgRequests()
+    r = session.get(url, headers=headers)
     if r.encoding is None: r.encoding = 'utf-8'
     locs = []
     for line in r.iter_lines(decode_unicode=True):
         if '<loc>https://www.huntington.com/Community/branch-info?locationId=' in line:
             locs.append(line.split('>')[1].split('<')[0])
     for loc in locs:
-        r2 = session.get(loc, headers=headers, verify=False)
+        session = SgRequests()
+        r2 = session.get(loc, headers=headers)
         if r2.encoding is None: r2.encoding = 'utf-8'
         lines = r2.iter_lines(decode_unicode=True)
         hours = ''
@@ -33,18 +34,18 @@ def fetch_data():
         website = 'huntington.com'
         Found = False
         for line2 in lines:
-            if '<h1>' in line2:
-                name = line2.split('<h1>')[1].split('<')[0]
             if '"streetAddress":"' in line2:
-                store = line2.split('"@id":"https://www.huntington.com/?locationId=')[1].split('"')[0]
-                add = line2.split('"streetAddress":"')[1].split('"')[0]
-                city = line2.split('"addressLocality":"')[1].split('"')[0]
-                state = line2.split('"addressRegion":"')[1].split('"')[0]
-                country = line2.split('"addressCountry":"')[1].split('"')[0]
-                zc = line2.split('"postalCode":"')[1].split('"')[0]
-                phone = line2.split('"telephone":"')[1].split('"')[0]
-                lat = line2.split('"latitude":')[1].split(',')[0]
-                lng = line2.split('"longitude":')[1].split('}')[0]
+                data = json.loads(line2)
+                name = data['name']
+                store = data['@id'].rsplit('=')[1] 
+                add = data["address"]["streetAddress"]
+                city = data["address"]["addressLocality"]
+                state = data["address"]["addressRegion"]
+                country = data["address"]["addressCountry"]
+                zc = data["address"]["postalCode"]
+                phone = data["telephone"]
+                lat = data["geo"]["latitude"]
+                lng = data["geo"]["longitude"]
                 items = line2.split('"dayOfWeek":"http://schema.org/')
             if '<!-- Lobby section-->' in line2:
                 Found = True
