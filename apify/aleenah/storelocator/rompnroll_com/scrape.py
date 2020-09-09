c0162b5c0b49e58b048085b59c0eb4b03683265e
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-09-10 01:53:48 +0500

    Add files via upload

---
+++ apify/aleenah/storelocator/rompnroll_com/scrape.py
@@ -0,0 +1,68 @@
+import csv
+from sgrequests import SgRequests
+from bs4 import BeautifulSoup
+import re
+
+
+def write_output(data):
+    with open('data.csv', mode='w') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation",
+                         "page_url"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+session = SgRequests()
+all=[]
+def fetch_data():
+    # Your scraper here
+
+    res=session.get("https://rompnroll.com/locations")
+    soup = BeautifulSoup(res.text, 'html.parser')
+    divs = soup.find_all('div', {'class': 'card-body'})
+    print(len(divs))
+    del divs[4] #singapore
+
+    for div in divs:
+        lis=div.find_all('li')
+        print(len(lis))
+        for li in lis:
+
+            if'<span style="">' in str(li):
+                print('in')
+                continue
+
+            url=li.find('a').get('href')
+            print(url)
+            res = session.get(url)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            data=str(soup)
+
+            all.append([
+            "https://rompnroll.com",
+            re.findall(r'"name":"([^"]+)"',data)[0],
+            re.findall(r'"streetAddress":"([^"]+)"', data)[0],
+            re.findall(r'"addressLocality":"([^"]+)"', data)[0],
+            re.findall(r'"addressRegion":"([^"]+)"', data)[0],
+            re.findall(r'"postalCode":"([^"]+)"', data)[0],
+            "US",
+            "<MISSING>",  # store #
+            re.findall(r'"telephone":"([^"]+)"', data)[0],  # phone
+            re.findall(r'"@type":"([^"]+)"', data)[0],  # type
+            re.findall(r'"latitude":"([^"]+)"', data)[0],  # lat
+            re.findall(r'"longitude":"([^"]+)"', data)[0],  # long
+            "<MISSING>",  # timing
+            url])
+    return all
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+scrape()
