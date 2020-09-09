0247a0d25626be62014304b4f577c31d7b6f812e
Michael Dinh <mkdinh94@gmail.com>
2020-09-08 22:33:31 -0400

    SLC-3770i/cinearts_com: add latlng data

--- apify/saleem/storelocator/cinearts_com/scrape.js
+++ apify/saleem/storelocator/cinearts_com/scrape.js
@@ -1,72 +1,99 @@
 const Apify = require('apify');
-const request = require('request-promise');
-const cheerio = require('cheerio');
 
-(async () => {
-  const rootAddress = new URL('https://cinearts.com/full-theatre-list');
-  const storePageList = [];
-  const listPage = await request.get({
-    url: rootAddress,
-    rejectUnauthorized: false
+async function enqueueLocationLinks({ $, requestQueue }) {
+  return Apify.utils.enqueueLinks({
+    $,
+    requestQueue,
+    baseUrl: 'https://cinearts.com/theatres/',
+    selector: '.theatres-by-state a',
   });
-  let $ = cheerio.load(listPage);
-  $('.contentMain h3').each( (_, elem) => {
-    const state = $(elem).text().trim();
-    $(elem).next().children('a').each( (_, elem) => {
-      storePageList.push({
-        url: (new URL($(elem).attr('href'), rootAddress.origin).href),
-        userData: {state}
-      })
-    })
-  })
+}
 
-  const requestList = new Apify.RequestList({
-    sources: storePageList,
-  });
-  await requestList.initialize();
+function scrapeLatLng($) {
+  const src = $('.theatreMap img').attr('data-src');
+  const match = src.match(/pp=(.*),(.*)&/i);
+  if (match) {
+    const [_, latitude, longitude] = match;
+    return { latitude, longitude };
+  } else {
+    return {};
+  }
+}
 
-  const crawler = new Apify.CheerioCrawler({
-    requestList,
-    ignoreSslErrors: true,
-    handlePageFunction: async ({ request, response, html, $ }) => {
-
-      // Begin scraper
-      
-      const {
-        name: location_name,
-        address: [{
+async function scrape({ $, request }) {
+  const scripts = $("script[type='application/ld+json']");
+  const targetScript = scripts.length > 1 ? scripts[1] : scripts[0];
+  try {
+    const {
+      name: location_name,
+      address: [
+        {
           streetAddress: street_address,
           addressLocality: city,
           addressRegion: state,
           postalCode: zip,
           addressCountry: country_code,
-        }],
-        '@id': store_number,
-        telephone: phone,
-        '@type': location_type
-      } = JSON.parse($("script[type='application/ld+json']")[0].children[0].data);
-			const poi = {
-        locator_domain: 'cinearts.com',
-        location_name,
-        street_address,
-        city,
-        state,
-        zip,
-        country_code,
-				store_number,
-				phone: (phone.length > 0)? phone : '<MISSING>',
-				location_type,
-        latitude: '<MISSING>',
-        longitude: '<MISSING>',
-				hours_of_operation: '<MISSING>'
-			};
+        },
+      ],
+      email,
+      telephone: phone,
+      '@type': location_type,
+    } = JSON.parse(targetScript.children[0].data);
 
-			await Apify.pushData([poi]);
+    const { latitude, longitude } = scrapeLatLng($);
 
-			// End scraper
+    const store_number = parseInt(email.split('@')[0]);
 
+    return {
+      locator_domain: 'cinearts.com',
+      page_url: request.url,
+      location_name,
+      street_address,
+      city,
+      state,
+      zip,
+      country_code,
+      store_number,
+      phone: phone.length > 0 ? phone : '<MISSING>',
+      location_type,
+      latitude: latitude || '<MISSING>',
+      longitude: longitude || '<MISSING>',
+      hours_of_operation: '<MISSING>',
+    };
+  } catch (err) {
+    return null;
+  }
+}
+
+Apify.main(async () => {
+  const requestQueue = await Apify.openRequestQueue();
+  await requestQueue.addRequest({
+    url: 'http://cinearts.com/full-theatre-list',
+    userData: {
+      pageType: 'locations',
+    },
+  });
+
+  const crawler = new Apify.CheerioCrawler({
+    requestQueue,
+    ignoreSslErrors: true,
+    async handlePageFunction({ request, $ }) {
+      switch (request.userData.pageType) {
+        case 'locations':
+          await enqueueLocationLinks({ $, requestQueue });
+          break;
+        default:
+          const poi = await scrape({ $, request });
+          if (poi) await Apify.pushData(poi);
+      }
     },
   });
 
   await crawler.run();
-})();
+
+  Object.entries(counts).map(([key, value]) => {
+    if (value > 1) {
+      console.log(key);
+    }
+  });
+});
