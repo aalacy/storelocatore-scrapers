1480766bda96432d390b2c06b33a9559aec19673
Quincy <qcalexander@gmail.com>
2020-09-10 19:40:53 -0400

    porsche-design_us fixed

--- apify/quincya/storelocator/porsche-design_us/scrape.py
+++ apify/quincya/storelocator/porsche-design_us/scrape.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 import csv
 import time
 import re
-
 from random import randint
+from selenium.webdriver.support.ui import Select
+from sgselenium import SgSelenium
 
 def write_output(data):
 	with open('data.csv', mode='w') as output_file:
@@ -17,88 +18,100 @@ def write_output(data):
 			writer.writerow(row)
 
 def fetch_data():
+
+	driver = SgSelenium().chrome()
+	time.sleep(2)
 	
-	base_link = "https://www.porsche-design.us/store-locator/?cl=shops&fnc=search&scallback=radius&sCityZip=&fRadius=&sContinent=&sCountry=US&iShoptype=&sCategory="
+	base_link = "https://www.porsche-design.us/us/en/storelocator/"
 
 	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
 	HEADERS = {'User-Agent' : user_agent}
 
 	session = SgRequests()
+	driver.get(base_link)
+	time.sleep(6)
+
+	state = driver.find_element_by_id("ZipOrLocus")
+	state.clear()
+	state.send_keys('NY')
+	time.sleep(randint(1,2))
+	select = Select(driver.find_element_by_id('Distance'))
+	select.select_by_value('300')
+	time.sleep(randint(1,2))
+
+	search_button = driver.find_element_by_xpath("//*[(@value='Find stores')]")
+	driver.execute_script('arguments[0].click();', search_button)
+	time.sleep(randint(10,12))
 
 	all_links = []
 	data = []
 
-	req = session.get(base_link, headers = HEADERS)
-	time.sleep(randint(2,4))
+	base = BeautifulSoup(driver.page_source,"lxml")
+	items = items = base.find(class_="col-sm-12 col-md-6").find_all(class_="store-search-result-container d-flex")
 
-	try:
-		base = BeautifulSoup(req.text,"lxml")
-		print("Got main page")
-	except (BaseException):
-		print('[!] Error Occured. ')
-		print('[?] Check whether system is Online.')
-
-	items = base.find_all(class_="cell small-12")[1].find_all("div", class_=re.compile("^cell small-12"))[1:]
 	for item in items:
 		location_name = item.find(class_="h5").text
-		if "PORSCHE DESIGN DEALER" in location_name.upper():
-			break
-		link = item.find(class_="link link--arrow")['href']
+		link = "https://www.porsche-design.us" + item.find(class_="text-uppercase more-link")['href']
 		all_links.append(link)
 
 	total_links = len(all_links)
 	for i, link in enumerate(all_links):
-		print("Link %s of %s" %(i+1,total_links))
-
+		# print("Link %s of %s" %(i+1,total_links))
 		req = session.get(link, headers = HEADERS)
-		time.sleep(randint(1,2))
-		try:
-			base = BeautifulSoup(req.text,"lxml")
-		except (BaseException):
-			print('[!] Error Occured. ')
-			print('[?] Check whether system is Online.')
+		base = BeautifulSoup(req.text,"lxml")
 
 		locator_domain = "porsche-design.us"
+		location_type = "<MISSING>"
 
-		item = base.find(class_="storefinder__store")
+		item = base.find(class_="container mt-5")
 		location_name = item.find('h1').text.strip()
 		print(location_name)
 
 		raw_address = str(item.p)[3:].replace("</p>","").replace("\n","").strip().split("<br/>")
 
 		street_address = raw_address[0].strip()
-		if "," in street_address:
-			street_address = street_address[street_address.find(",")+1:].strip()
-		city_line = raw_address[1].strip()
-
+		if street_address[-1:] == ",":
+			street_address = street_address[:-1].strip()
 		try:
-			state = re.findall( r"[A-Z]{2} ",city_line)[0].strip()
+			city_line = raw_address[1].strip().split(",")
+			city = city_line[0].strip()
+			state = city_line[-1].strip().split()[0].strip()
+			zip_code = city_line[-1].strip().split()[1].strip()
 		except:
-			state = "<MISSING>"
-		try:
-			zip_code = re.findall( r"[0-9]{5}",city_line)[0]
-		except:
-			zip_code = "<MISSING>"
-
-		city = city_line.replace(zip_code,"").replace(state,"").replace(",","").strip()
+			street_address = "<MISSING>"
+			if location_name == "JFK Airport":
+				state = "NY"
+				city = "NY"
+			else:
+				state = "<MISSING>"
+				city = "<MISSING>"
+			data.append([locator_domain, link, location_name, street_address, city, state, "<MISSING>", country_code, "<MISSING>", "<MISSING>", location_type, "<MISSING>", "<MISSING>", "<MISSING>"])
+			continue
 
 		country_code = "US"
 		store_number = "<MISSING>"
-		phone = item.find(class_="list list--neutral").li.text.replace("Phone:","").strip()
-
-		location_type = "Design Store"
+		raw_phone = item.find_all("p")[1].text.replace("\n","")
+		phone = re.findall(r'\+.+ E', raw_phone)[0][:-1].strip()
 
 		hours_of_operation = ""
-		raw_hours = item.find_all("p")[1].text.replace("\n","").strip()
+		raw_hours = item.find_all("p")[3].text.replace("\n","").strip()
 		hours_of_operation = (re.sub(' +', ' ', raw_hours)).strip()
 		if not hours_of_operation:
 			hours_of_operation = "<MISSING>"
 
-		latitude = base.find(class_="window")['data-lat']
-		longitude = base.find(class_="window")['data-lng']
+		latitude = "<MISSING>"
+		longitude = "<MISSING>"
+		all_scripts = base.find_all('script')
+		for script in all_scripts:
+			if "lng" in str(script):
+				script = str(script)
+				coords = re.findall(r'\[-.+,.+\]},', script)[0]
+				latitude = coords.split(",")[1][:-2]
+				longitude = coords.split(",")[0][1:]
+				break
 
 		data.append([locator_domain, link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
-
+	driver.close()
 	return data
 
 def scrape():
