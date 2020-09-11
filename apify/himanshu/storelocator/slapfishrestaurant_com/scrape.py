8c5b3657c308afb30e447771527e0969bc7199a9
diapk <Hank4957!>
2020-09-11 11:47:00 +0530

    docker run and push

--- apify/himanshu/storelocator/slapfishrestaurant_com/scrape.py
+++ apify/himanshu/storelocator/slapfishrestaurant_com/scrape.py
@@ -3,65 +3,61 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
-
-
 session = SgRequests()
-
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
+    with open('data.csv', mode='w', newline= '') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
-
 def fetch_data():
-    base_url = "https://www.slapfishrestaurant.com"
-    return_main_object=[]
-    r = session.get(base_url+'/locations')
-    soup=BeautifulSoup(r.text,'lxml')
-    main=soup.find_all('script',type="text/javascript")
-    for script in main:
-        if "var jsonContent" in script.text:
-            main1=json.loads(script.text.split('var jsonContent = ')[1].split('};')[0]+'}',strict=False)
-            for loc in main1['data']:
-                address=loc['street_number']+' '+loc['address_route'].strip()
-                city=loc['city'].strip()
-                state=loc['state'].strip()
-                zip=loc['postal_code'].strip()
-                phone=loc['phone'].strip()
-                name=loc['title'].strip()
-                lat=loc['lat']
-                lng=loc['lng']
-                storeno=loc['id']
-                country=loc['country']
-                if country=="United States":
-                    country="US"
-                hour=loc['hours']
-                cleanr = re.compile('<.*?>')
-                hour = re.sub(cleanr, '', hour)
-                hour = re.sub(r'\s+', '', hour)
-                store=[]
-                store.append(base_url)
-                store.append(name if name else "<MISSING>")
-                store.append(address if address else "<MISSING>")
-                store.append(city if city else "<MISSING>")
-                store.append(state if state else "<MISSING>")
-                store.append(zip if zip else "<MISSING>")
-                store.append(country if country else "<MISSING>")
-                store.append(storeno if storeno else "<MISSING>")
-                store.append(phone if phone else "<MISSING>")
-                store.append("slapfishrestaurant")
-                store.append(lat if lat else "<MISSING>")
-                store.append(lng if lng else "<MISSING>")
-                store.append(hour if hour.strip() else "<MISSING>")
-                return_main_object.append(store)
-    return return_main_object
-
+    url = "https://www.slapfishrestaurant.com/locations/storelist"
+    payload = "latitude=33.5973469&longitude=-112.1072528&range=2500&search_by_state="
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9,pt;q=0.8',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': 'PHPSESSID=buikagjj7q4ugik0rb15ko84s5; _ga=GA1.2.1093626883.1599738803; _gid=GA1.2.1116822427.1599738803; _gat=1; _fbp=fb.1.1599738805287.642293938; PHPSESSID=1nmanf1bpl3sd9r7in8hh6cp04',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    response = session.post(url, headers=headers, data = payload).json()
+    data_main = (response['data'])
+    for i in data_main:
+        city = (i['location'].split("</br>")[1].strip().split(",")[0])
+        state = (i['location'].split("</br>")[1].strip().split(",")[1].strip().split(" ")[0])
+        zipp = (i['location'].split("</br>")[1].strip().split(",")[1].strip().split(" ")[1])
+        street_address = i['location'].split("</br>")[0].replace(",","")
+        phone = (i['phone'])
+        location_name = i['title']
+        lat = i['latitude']
+        lng = i['longitude']
+        hour = i['hours']
+        page_url = i['url']
+        base_url = "https://www.slapfishrestaurant.com"
+        store=[]
+        store.append(base_url)
+        store.append(location_name if location_name else "<MISSING>")
+        store.append(street_address if street_address else "<MISSING>")
+        store.append(city if city else "<MISSING>")
+        store.append(state if state else "<MISSING>")
+        store.append(zipp if zipp else "<MISSING>")
+        store.append("US")
+        store.append("<MISSING>")
+        store.append(phone if phone else "<MISSING>")
+        store.append("<MISSING>")
+        store.append(lat if lat else "<MISSING>")
+        store.append(lng if lng else "<MISSING>")
+        store.append(str(hour).replace("</li>"," ").replace("<li>","").replace('<li class="active">',"").strip() if hour else "<MISSING>")
+        store.append(page_url if page_url.strip() else "<MISSING>")
+        if "123 test test" in street_address:
+            continue
+        yield store
 def scrape():
     data = fetch_data()
     write_output(data)
-
 scrape()
