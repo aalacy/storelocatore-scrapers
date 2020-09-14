d5958916a1fc9f218e25451d7dfb9916e20ca0d9
AleenaHaider <51033033+AleenaHaider@users.noreply.github.com>
2020-09-14 23:54:58 +0500

    Update scrape.py

--- apify/aleenah/storelocator/spiritofmath_com/scrape.py
+++ apify/aleenah/storelocator/spiritofmath_com/scrape.py
@@ -1,62 +1,69 @@
 import csv
 from sgselenium import SgSelenium
 import re
-#import reverse_geocoder
-#from slimit.parser import Parser
+
+# import reverse_geocoder
+# from slimit.parser import Parser
 
 driver = SgSelenium().chrome()
+
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
+
 
 def parse_geo(url):
     lon = re.findall(r'll=[-?\d\.]*\,([-?\d\.]*)', url)[0]
     lat = re.findall(r'll=(-?[\d\.]*)', url)[0]
     return lat, lon
 
+
 def fetch_data():
     # Your scraper here
     locs = []
     street = []
-    states=[]
+    states = []
     cities = []
-    countries=[]
+    countries = []
     phones = []
     zips = []
     long = []
     lat = []
     timing = []
-    links=[]
+    links = []
 
     driver.get("https://www.spiritofmath.com/campuses/")
-    uls=driver.find_element_by_class_name('res-con').find_elements_by_tag_name('ul')
+    uls = driver.find_element_by_class_name('res-con').find_elements_by_tag_name('ul')
 
     for i in range(len(uls)):
         li = uls[i]
-        tex=li.text.split("\n")
+        tex = li.text.split("\n")
 
-        if tex[0]=="Head Office":
+        if tex[0] == "Head Office":
             continue
-        addr=tex[1]
-
+        addr = tex[1]
 
         if "Pakistan" in addr:
             continue
+        loc = tex[0]
+        locs.append(loc)
 
-        e = re.findall(r'( [ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z] [0-9][ABCEGHJ-NPRSTV-Z][0-9])', addr) #canadian zip
+        e = re.findall(r'( [ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z] [0-9][ABCEGHJ-NPRSTV-Z][0-9])',
+                       addr)  # canadian zip
         if e != []:
             addr = addr.replace(e[0], "")
             zips.append(e[0].strip())
             countries.append("CA")
         else:
-            e = re.findall(r'( [0-9]{5})', addr) #american zip
+            e = re.findall(r'( [0-9]{5})', addr)  # american zip
             if e != []:
                 addr = addr.replace(e[0], "")
                 zips.append(e[0].strip())
@@ -69,7 +76,7 @@ def fetch_data():
 
         if e != []:
             addr = addr.replace(e[0], "")
-            states.append( e[0].strip().strip(","))
+            states.append(e[0].strip().strip(","))
         else:
             e = re.findall(r'.*( Ontario[ \,]*)$', addr)
             if e != []:
@@ -78,64 +85,69 @@ def fetch_data():
             else:
                 states.append("<MISSING>")
         print(tex)
-        addr=addr.strip().strip(",")
+        addr = addr.strip().strip(",")
         print(addr)
         if ',' in addr:
 
-            o =addr.split(",")[-1].strip()
-            addr=addr.replace(o,"").strip().strip(",")
+            o = addr.split(",")[-1].strip()
+            if len(o.split(' ')) >2:
+                addr+=o
+                o=loc.replace('Campus','').replace('West','')
+
+            else:
+                addr = addr.replace(o, "").strip().strip(",")
+
         else:
 
-            o='<MISSING>'
+            o = '<MISSING>'
 
         if 'Phone' in addr:
             street.append('<MISSING>')
-            e=addr
+            e = addr
             cities.append('<MISSING>')
         else:
             street.append(addr)
             cities.append(o)
             e = re.findall(r'([0-9\-]+.*)', tex[2].strip())[0]
-        locs.append(tex[0])
+
 
         if e != []:
-            phones.append(e.replace("MATH ","").replace('or','').replace('Phone:','').strip())
+            phones.append(e.replace("MATH ", "").replace('or', '').replace('Phone:', '').strip())
         else:
             phones.append("<MISSING>")
         links.append(li.find_element_by_tag_name("a").get_attribute("href"))
-    
 
     for link in links:
         print(link)
         driver.get(link)
         try:
-            news=driver.find_element_by_xpath('//div[@class="news-text"]').text
+            news = driver.find_element_by_xpath('//div[@class="news-text"]').text
         except:
             try:
                 news = driver.find_element_by_class_name("news-text").text
             except:
 
-                h=driver.page_source
+                h = driver.page_source
                 if 'Mon' in h:
 
-                    news=re.findall(r'(Mon.*by appointment only)',h,re.DOTALL)[0].replace('<li style="text-align: left">',' ').replace('</li>','').replace('</b></span></p>','').replace('<ul>','')
+                    news = re.findall(r'(Mon.*by appointment only)', h, re.DOTALL)[0].replace(
+                        '<li style="text-align: left">', ' ').replace('</li>', '').replace('</b></span></p>','').replace('<ul>', '')
                 else:
-                    news=''
-        #print(news)
+                    news = ''
+        # print(news)
         try:
-            tim = re.findall(r'(Hours.*pm|HOURS.*pm|Hours.*PM|HOURS.*PM).*(Open)*(OPEN)*',news,re.DOTALL)[0][0].replace("\n"," ")
+            tim = re.findall(r'(Hours.*pm|HOURS.*pm|Hours.*PM|HOURS.*PM).*(Open)*(OPEN)*', news, re.DOTALL)[0][
+                0].replace("\n", " ")
             try:
-                tim=tim.replace(re.findall(r'pm|PM(.*Open.*)',tim,re.DOTALL)[0],"")
+                tim = tim.replace(re.findall(r'pm|PM(.*Open.*)', tim, re.DOTALL)[0], "")
             except:
-                tim=tim
-            print (tim)
+                tim = tim
+            print(tim)
             if 'Campus Hours:' in tim:
-                tim=tim.split('Campus Hours:')[1]
-            timing.append(tim.replace('Hours: ','').replace('HOURS OF OPERATION ','').replace('HOURS ','').split('Campus Hours:'))
+                tim = tim.split('Campus Hours:')[1]
+            timing.append(tim.replace('Hours: ', '').replace('HOURS OF OPERATION ', '').replace('HOURS ', '').replace('Campus Hours:','').replace(' ‚Äì',' -'))
         except:
             timing.append("<MISSING>")
-
-
 
     all = []
     for i in range(0, len(locs)):
@@ -152,13 +164,15 @@ def fetch_data():
         row.append("<INACCESSIBLE>")  # type
         row.append("<INACCESSIBLE>")  # lat
         row.append("<INACCESSIBLE>")  # long
-        row.append(timing[i])    #timing
+        row.append(timing[i])  # timing
 
         all.append(row)
     return all
 
+
 def scrape():
     data = fetch_data()
     write_output(data)
+
 
 scrape()
