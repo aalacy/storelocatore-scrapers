6dcfb2cc78485d885ace877288415a00d171c14c
Quincy <qcalexander@gmail.com>
2020-09-15 23:26:11 -0400

    muchasgraciasmexicanfood_com fixed

--- apify/himanshu/storelocator/muchasgraciasmexicanfood_com/scrape.py
+++ apify/himanshu/storelocator/muchasgraciasmexicanfood_com/scrape.py
@@ -1,13 +1,9 @@
 import csv
-from sgrequests import SgRequests
-from sgrequests import SgRequests
+from sgselenium import SgSelenium
 from bs4 import BeautifulSoup
 import re
 import json
-
-session = SgRequests()
-
-session = SgRequests()
+import time
 
 def write_output(data):
     with open('data.csv', mode='w', encoding="utf-8") as output_file:
@@ -23,61 +19,85 @@ def write_output(data):
 
 
 def fetch_data():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
-    }
-    base_url = "http://muchasgraciasmexicanfood.com"
-    r = session.get(
-        "http://muchasgraciasmexicanfood.com/our-locations/", headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
+
+    base_url = "https://www.muchasgraciasmexicanfood.com/locations/"
+
+    driver = SgSelenium().chrome()
+    time.sleep(2)
+
+    driver.get(base_url)
+    time.sleep(8)
+
+    base = BeautifulSoup(driver.page_source,"lxml")
+
     return_main_object = []
-    address = []
-    for city in soup.find_all("ul",{"class":"sub-menu"})[1].find_all("ul",{"class":"sub-menu"}):
-        for url in city.find_all("a"):
-            page_url = url['href']
-            location_r = session.get(page_url, headers=headers)
-            location_soup = BeautifulSoup(location_r.text, "lxml")
-            location_name = location_soup.find_all("h6",{"class":"avaris-contact-title"})[0].text
-            addr = location_soup.find_all("h6",{"class":"avaris-contact-title"})[1].text
-            if len(addr.split(",")) == 2:
-                street_address = " ".join(addr.split(",")[0].split(" ")[:-1]).replace("Grants","").strip()
-                city = addr.split(",")[0].split(" ")[-1].replace("Pass","Grants Pass")
-                state = addr.split(",")[1].split(" ")[1]
-                zipp = addr.split(",")[1].split(" ")[2]
-            else:
-                street_address = addr.split(",")[0]
-                city = addr.split(",")[1]
-                if len(addr.split(",")[-1].split(" ")) == 2:
-                    state = addr.split(",")[-1].split(" ")[1]
-                    zipp = "<MISSING>"
-                else:
-                    state = addr.split(",")[-1].split(" ")[1]
-                    zipp = addr.split(",")[-1].split(" ")[2]
-            phone = location_soup.find_all("h6",{"class":"avaris-contact-title"})[-1].text.strip()
-            geo_location = location_soup.find("a", {"href": re.compile("/@")})['href']
-            latitude = geo_location.split("/@")[1].split(",")[0]
-            longitude = geo_location.split("/@")[1].split(",")[1]
-            
-            store = []
-            store.append(base_url)
-            store.append(location_name)
-            store.append(street_address)
-            store.append(city)
-            store.append(state)
-            store.append(zipp)
-            store.append("US")
-            store.append("<MISSING>")
-            store.append(phone)
-            store.append("<MISSING>")
-            store.append(latitude)
-            store.append(longitude)
-            store.append("<MISSING>")
-            store.append(page_url)
-            yield store
+
+    items = base.find_all(class_="item")
+
+    for location_soup in items:
+        location_name = location_soup.find(class_="p-title").text.strip()
+        addr = location_soup.find(class_="p-area").text.replace("  ",",").strip().split(",")
+        street_address = addr[0].strip()
+        city = addr[1].strip()
+        state = addr[2].strip()
+        zipp = addr[3].strip()
+        # if len(addr.split(",")) == 2:
+        #     street_address = " ".join(addr.split(",")[0].split(" ")[:-1]).replace("Grants","").strip()
+        #     city = addr.split(",")[0].split(" ")[-1].replace("Pass","Grants Pass")
+        #     state = addr.split(",")[1].split(" ")[1]
+        #     zipp = addr.split(",")[1].split(" ")[2]
+        # else:
+        #     street_address = addr.split(",")[0]
+        #     city = addr.split(",")[1]
+        #     if len(addr.split(",")[-1].split(" ")) == 2:
+        #         state = addr.split(",")[-1].split(" ")[1]
+        #         zipp = "<MISSING>"
+        #     else:
+        #         state = addr.split(",")[-1].split(" ")[1]
+        #         zipp = addr.split(",")[-1].split(" ")[2]
+        
+        try:
+            phone = re.findall("\([\d]{3}\) [\d]{3}-[\d]{4}", location_soup.text)[0]
+        except:
+            phone = "<MISSING>"
+        latitude = "<MISSING>"
+        longitude = "<MISSING>"
+        
+        days = ""
+        hours = ""
+        ps = location_soup.find_all(class_="p-area")
+        for p in ps:
+            if "mon," in p.text.lower():
+                days = p.text
+            if ":00 " in p.text.lower():
+                hours = p.text.strip()
+
+        if days and hours:
+            hours_of_operation = hours + days
+        else:
+            hours_of_operation = "<MISSING>"
+
+        store = []
+        store.append("muchasgraciasmexicanfood.com")
+        store.append(location_name)
+        store.append(street_address)
+        store.append(city)
+        store.append(state)
+        store.append(zipp)
+        store.append("US")
+        store.append("<MISSING>")
+        store.append(phone)
+        store.append("<MISSING>")
+        store.append(latitude)
+        store.append(longitude)
+        store.append(hours_of_operation)
+        store.append(base_url)
+        return_main_object.append(store)
+    driver.close()
+    return return_main_object
 
 def scrape():
     data = fetch_data()
     write_output(data)
-
 
 scrape()
