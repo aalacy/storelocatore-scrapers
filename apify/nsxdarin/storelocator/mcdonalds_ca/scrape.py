e335b54da09b57da16722fa870040583b5537290
vivri <victor.ivri@gmail.com>
2020-09-17 13:27:32 -0400

    SLC-4252 mcdonalds.ca - fixed headers and extended extended range via query params.

--- apify/nsxdarin/storelocator/mcdonalds_ca/scrape.py
+++ apify/nsxdarin/storelocator/mcdonalds_ca/scrape.py
@@ -2,21 +2,30 @@ import csv
 from sgrequests import SgRequests
 import json
 
+MISSING = "<MISSING>"
+
 session = SgRequests()
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-           'content-type': 'application/json',
-           'X-Requested-With': 'XMLHttpRequest'
-           }
+headers = {
+        "Host": "www.mcdonalds.com",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-CA,en-US;q=0.7,en;q=0.3",
+        "Accept-Encoding": "gzip, deflate, br",
+        "X-Requested-With": "XMLHttpRequest",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Referer": "https://www.mcdonalds.com/us/en-us/restaurant-locator.html"
+    }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
 
 def fetch_data():
-    url = 'https://www.mcdonalds.com/googleapps/GoogleRestaurantLocAction.do?method=searchLocation&latitude=43.6424036&longitude=-79.3859716&radius=5000&maxResults=50000&country=ca&language=en-ca&showClosed=&hours24Text=Open%2024%20hr'
+    url = 'https://www.mcdonalds.com/googleapps/GoogleRestaurantLocAction.do?method=searchLocation&latitude=43.6424036&longitude=-79.3859716&radius=15000&maxResults=50000&country=ca&language=en-ca&showClosed=&hours24Text=Open%2024%20hr'
     r = session.get(url, headers=headers)
     array = json.loads(r.content)
     for item in array['features']:
@@ -24,13 +33,13 @@ def fetch_data():
         add = item['properties']['addressLine1']
         add = add.strip().replace('"',"'")
         city = item['properties']['addressLine3']
-        state = '<MISSING>'
+        state = MISSING
         country = 'CA'
         zc = item['properties']['postcode']
         try:
             phone = item['properties']['telephone']
         except:
-            phone = '<MISSING>'
+            phone = MISSING
         name = "McDonald's # " + store
         website = 'mcdonalds.com'
         typ = 'Restaurant'
@@ -45,11 +54,14 @@ def fetch_data():
             hours = hours + '; Sat: ' + item['properties']['restauranthours']['hoursSaturday']
             hours = hours + '; Sun: ' + item['properties']['restauranthours']['hoursSunday']
         except:
-            hours = '<MISSING>'
-        yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+            hours = MISSING
+
+        page_url = MISSING
+        yield [website, page_url, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
     write_output(data)
 
-scrape()
+if __name__ == "__main__":
+    scrape()
