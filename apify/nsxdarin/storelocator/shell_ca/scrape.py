07f7aacf4f35c07bf666b72dba04e8b3155b1f6e
Michael Dinh <mkdinh94@gmail.com>
2020-09-17 23:18:30 -0400

    SLC-4092/shell_ca: rewrite scraper to use api endpoints

--- apify/nsxdarin/storelocator/shell_ca/scrape.py
+++ apify/nsxdarin/storelocator/shell_ca/scrape.py
@@ -1,94 +1,99 @@
 import csv
+import json
 from sgrequests import SgRequests
 
+INACCESSIBLE = '<INACCESSIBLE>'
 session = SgRequests()
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
-           }
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+}
+
+canada = {
+    "country_code": "CA",
+    'bbox_sw': [41.6691, -141.0055],
+    'bbox_ne': [74.5559, -52.6166],
+}
+
 
 def write_output(data):
     with open('data.csv', mode='w', encoding='utf-8') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer = csv.writer(output_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip",
+                         "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
 
-def fetch_data():
-    ids = []
-    url = ''
-    for x in range(41, 64):
-        for y in range(-52, -141, -1):
-            lats = x
-            latn = x + 1
-            lnge = y
-            lngw = y - 1
-            print(str(x) + ',' + str(y))
-            url = 'https://shellgsllocator.geoapp.me/api/v1/locations/within_bounds?sw%5B%5D=' + str(lats) + '&sw%5B%5D=' + str(lngw) + '&ne%5B%5D=' + str(latn) + '&ne%5B%5D=' + str(lnge) + '&selected=&autoload=true&travel_mode=driving&avoid_tolls=false&avoid_highways=false&avoid_ferries=false&corridor_radius=5&driving_distances=false&format=json'
-            r = session.get(url, headers=headers)
-            for line in r.iter_lines():
-                line = str(line.decode('utf-8'))
-                if '{"id":"' in line:
-                    items = line.split('{"id":"')
-                    for item in items:
-                        if '"name":"' in item:
-                            name = item.split('"name":"')[1].split('"')[0].replace('\\u0026','&')
-                            lat = item.split('"lat":')[1].split(',')[0]
-                            lng = item.split('"lng":')[1].split(',')[0]
-                            add = item.split('"address":"')[1].split('"')[0].replace('\\u0026','&')
-                            city = item.split('"city":"')[1].split('"')[0]
-                            state = item.split('"state":"')[1].split('"')[0]
-                            zc = item.split('"postcode":"')[1].split('"')[0]
-                            country = item.split('"country_code":"')[1].split('"')[0]
-                            phone = item.split('"telephone":"')[1].split('"')[0]
-                            typ = item.split('"brand":"')[1].split('"')[0]
-                            website = 'shell.ca'
-                            try:
-                                loc = item.split('"website_url":"')[1].split('"')[0]
-                            except:
-                                loc = '<MISSING>'
-                            store = '<MISSING>'
-                            storeinfo = name + '|' + add + '|' + city + '|' + lat
-                            hours = ''
-                            if phone == '':
-                                phone = '<MISSING>'
-                            if storeinfo not in ids and country == 'CA' and '10053240-fariview-ave-st-thomas' not in loc:
-                                print('Pulling Store %s...' % name)
-                                days = []
-                                try:
-                                    if loc != '<MISSING>':
-                                        r2 = session.get(loc, headers=headers, timeout=5)
-                                        lines = r2.iter_lines()
-                                        rc = 0
-                                        dc = -1
-                                        for line in lines:
-                                            line = str(line.decode('utf-8'))
-                                            if '<div class="opening-times__cell">' in line:
-                                                rc = rc + 1
-                                                if rc <= 7:
-                                                    g = next(lines)
-                                                    g = str(g.decode('utf-8'))
-                                                    days.append(g.strip().replace('\r','').replace('\n','').replace('\t',''))
-                                                if rc >= 8:
-                                                    dc = dc + 1
-                                                    g = next(lines)
-                                                    g = str(g.decode('utf-8'))
-                                                    days[dc] = days[dc] + ': ' + g.strip().replace('\r','').replace('\n','').replace('\t','')
-                                        for day in days:
-                                            if hours == '':
-                                                hours = day
-                                            else:
-                                                hours = hours + '; ' + day
-                                except:
-                                    pass
-                                if hours == '':
-                                    hours = '<MISSING>'
-                                if phone == '':
-                                    phone = '<MISSING>'
-                                store = loc.rsplit('/',1)[1].split('-')[0]
-                                yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
-                                ids.append(storeinfo)
+
+def get_location_data(location):
+
+    if (location.get('country_code') == 'CA'):
+        return [
+            locator_domain,
+            location.get('website_url'),
+            location.get('name'),
+            location.get('address'),
+            location.get('city'),
+            location.get('state'),
+            location.get('postcode'),
+            location.get('country_code'),
+            location.get('id'),
+            location.get('telephone'),
+            location.get('type'),
+            location.get('lat'),
+            location.get('lng'),
+            INACCESSIBLE
+        ]
+
+
+locator_domain = 'shell.ca'
+
+
+def fetch_within_bounds(sw, ne):
+    params = {
+        'sw[]': sw,
+        'ne[]': ne,
+        'autoload': True,
+        'travel_mode': 'driving',
+        'avoid_tolls': False,
+        'avoid_highways': False,
+        'avoid_ferries': False,
+        'corridor_radius': 5,
+        'driving_distances': False,
+        'format': 'json'
+    }
+
+    data = session.get(f'https://shellgsllocator.geoapp.me/api/v1/locations/within_bounds',
+                       headers=headers, params=params).json()
+
+    for item in data:
+        bounds = item.get('bounds')
+        if bounds:
+            yield from fetch_within_bounds(bounds.get(
+                'sw'), bounds.get('ne'))
+        else:
+            if item.get('country_code') == 'CA':
+                yield [
+                    locator_domain,
+                    item.get('website_url'),
+                    item.get('name'),
+                    item.get('address'),
+                    item.get('city'),
+                    item.get('state'),
+                    item.get('postcode'),
+                    item.get('country_code'),
+                    item.get('id'),
+                    item.get('telephone'),
+                    item.get('type'),
+                    item.get('lat'),
+                    item.get('lng'),
+                    INACCESSIBLE
+                ]
+
 
 def scrape():
-    data = fetch_data()
+    data = fetch_within_bounds(canada['bbox_sw'], canada['bbox_ne'])
     write_output(data)
+
 
 scrape()
