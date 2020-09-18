037bd762e899d199cf11624521544519bfa47be4
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-09-18 17:55:44 +0500

    Add files via upload

--- apify/ggrahambaker/storelocator/mariaempanada_com/scrape.py
+++ apify/ggrahambaker/storelocator/mariaempanada_com/scrape.py
@@ -1,67 +1,116 @@
-import csv
-from sgrequests import SgRequests
 from bs4 import BeautifulSoup
+import csv
+import string
+import re, time, usaddress
+
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
-def addy_ext(addy):
-    addy = addy.split(',')
-    city = addy[0]
-    state_zip = addy[1].strip().split(' ')
-    state = state_zip[0]
-    zip_code = state_zip[1]
-    return city, state, zip_code
 
 def fetch_data():
-    session = SgRequests()
-    HEADERS = { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36' }
+    p = 0
+    data = []
+    pattern = re.compile(r'\s\s+')
+    cleanr = re.compile(r'<[^>]+>')
+    url = 'https://www.mariaempanada.com/find-us'
+    r = session.get(url, headers=headers, verify=False)
+  
+    soup =BeautifulSoup(r.text, "html.parser")
+   
+    divlist = soup.findAll('div', {'class': "sqs-block html-block sqs-block-html"})
+    print("states = ",len(divlist))
+    #input()
+    for div in divlist:
+        try:
+           
+            title = div.find('strong').text
+            #print(title)
+            if div.text.find('TEMPORARILY CLOSED') > -1:
+                title = title + '( TEMPORARILY CLOSED )'
+            if title.find('@mariaempanada') > -1:
+                continue
+            
+            det = div.findAll('p')
+            
+            address = det[1]
+            address =re.sub(cleanr,' ',str(address)).rstrip()
+            address = re.sub(pattern,' ',address)
+            phone = address.split(' ')[-1]
+            if len(phone) < 8:
+                phone = '<MISSING>'
+            else:
+                address = address.replace(phone,'')
+            
+            address = usaddress.parse(address)
+            i = 0
+            street = ""
+            city = ""
+            state = ""
+            pcode = ""
+            while i < len(address):
+                temp = address[i]
+                if temp[1].find("Address") != -1 or temp[1].find("Street") != -1 or temp[1].find('Occupancy') != -1 or temp[1].find("Recipient") != -1 or temp[1].find("BuildingName") != -1 or temp[1].find("USPSBoxType") != -1 or temp[1].find("USPSBoxID") != -1:
+                    street = street + " " + temp[0]
+                if temp[1].find("PlaceName") != -1:
+                    city = city + " " + temp[0]
+                if temp[1].find("StateName") != -1:
+                    state = state + " " + temp[0]
+                if temp[1].find("ZipCode") != -1:
+                    pcode = pcode + " " + temp[0]
+                i += 1
 
-    locator_domain = 'http://mariaempanada.com/' 
-    ext = 'contact/'
-    r = session.get(locator_domain + ext, headers = HEADERS)
+            street = street.lstrip().replace(',','')
+            city = city.lstrip().replace(',','')
+            state = state.lstrip().replace(',','')
+            pcode = pcode.lstrip().replace(',','')
+            hours = det[3].text.replace('AM',' AM ').replace('PM',' PM ')
+            data.append([
+                        'https://www.mariaempanada.com/',
+                        'https://www.mariaempanada.com/find-us',                   
+                        title,
+                        street,
+                        city,
+                        state,
+                        pcode,
+                        'US',
+                        '<MISSING>',
+                        phone,
+                        '<MISSING>',
+                        '<MISSING>',
+                        '<MISSING>',
+                        hours.replace('\xa0','')
+                    ])
+            #print(p,data[p])
+            p += 1
+                
+            
+            
+            
+        except Exception as e:
+            
+            pass
+    
+        
+    return data
 
-    soup = BeautifulSoup(r.content, 'html.parser')
-    maps = soup.find_all('div', {'class': 'google_map_holder'})
-
-    all_store_data = []
-    for map_div in maps:
-        main = map_div.parent
-        location_name = main.find('h4').text
-        
-        location_type = main.find('h6').text
-        more_info = main.find_all('div', {'class': 'wpb_text_column'})[1]
-        ps = more_info.find_all('p')
-        street_address = ps[0].text.strip().replace(',', '').strip()
-        city, state, zip_code = addy_ext(ps[1].text.strip())    
-        if len(ps) == 4:
-            phone_number = '<MISSING>'
-        else:
-            phone_number = ps[2].text.strip()
-        
-        hours = ps[-1].text.strip()
-        
-        country_code = 'US'
-        store_number = '<MISSING>'
-        lat = '<MISSING>'
-        longit = '<MISSING>'
-        page_url = '<MISSING>'
-        
-        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code, 
-                    store_number, phone_number, location_type, lat, longit, hours, page_url]
-        all_store_data.append(store_data)
-
-    return all_store_data
 
 def scrape():
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
     data = fetch_data()
     write_output(data)
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
 
 scrape()
