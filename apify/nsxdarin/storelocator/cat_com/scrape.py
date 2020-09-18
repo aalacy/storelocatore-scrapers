38c0a131704862b501d9c216c8a913c4ef1bf1b1
nsxdarin <nsxdarin@gmail.com>
2020-09-18 12:34:41 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/cat_com/scrape.py
+++ apify/nsxdarin/storelocator/cat_com/scrape.py
@@ -15,86 +15,89 @@ def write_output(data):
 def fetch_data():
     locs = []
     allstores = []
-    urls = ['https://cat-ms.esri.com/dls/cat/locations/en?f=json&forStorage=false&distanceUnit=mi&&searchType=location&maxResults=5000&searchDistance=700&productDivId=1%2C6%2C3%2C5%2C4%2C8%2C7%2C2&serviceId=1%2C2%2C3%2C4%2C8%2C9%2C10%2C5%2C6%2C7%2C12&appId=n6HDEnXnYRTDAxFr&searchValue=-115.8431267%2C35.2270869','https://cat-ms.esri.com/dls/cat/locations/en?f=json&forStorage=false&distanceUnit=mi&&searchType=location&maxResults=5000&searchDistance=10000&productDivId=1%2C6%2C3%2C5%2C4%2C8%2C7&serviceId=1%2C2&appId=n6HDEnXnYRTDAxFr&searchValue=-90%2C40','https://cat-ms.esri.com/dls/cat/locations/en?f=json&forStorage=false&distanceUnit=mi&&searchType=location&maxResults=5000&searchDistance=700&productDivId=1%2C6%2C3%2C5%2C4%2C8%2C7%2C2&serviceId=1%2C2%2C3%2C4%2C8%2C9%2C10%2C5%2C6%2C7%2C12&appId=n6HDEnXnYRTDAxFr&searchValue=-80.8431267%2C35.2270869']
-    for url in urls:
-        r = session.get(url, headers=headers)
-        for line in r.iter_lines():
-            line = str(line.decode('utf-8'))
-            if '"dealerId":' in line:
-                items = line.split('"dealerId":')
-                for item in items:
-                    if '"territoryId":' in item:
-                        store = item.split('"dealerLocationId":')[1].split(',')[0]
-                        website = 'cat.com'
-                        hours = ''
-                        name = item.split(',"dealerName":"')[1].split('"')[0].encode("ascii", errors="ignore").decode()
-                        lat = item.split('"latitude":')[1].split(',')[0]
-                        lng = item.split('"longitude":')[1].split(',')[0]
-                        try:
-                            country = item.split('"countryCode":"')[1].split('"')[0]
-                        except:
-                            country = ''
-                        add = item.split('"siteAddress":"')[1].split('"')[0] + ' ' + item.split('"siteAddress1":"')[1].split('"')[0]
-                        add = add.strip().encode("ascii", errors="ignore").decode()
-                        pnums = item.split('"phoneNumberTypeId":')
-                        phone = '<MISSING>'
-                        for pnum in pnums:
-                            if '"phoneNumberType":"GENERAL INFO' in pnum:
-                                phone = pnum.split('"phoneNumber":"')[1].split('"')[0].encode("ascii", errors="ignore").decode()
-                        city = item.split('"siteCity":"')[1].split('"')[0].encode("ascii", errors="ignore").decode()
-                        state = item.split('"siteState":"')[1].split('"')[0]
-                        zc = item.split(',"sitePostal":"')[1].split('"')[0]
-                        typ = ''
-                        snums = item.split('{"serviceId":')
-                        donetyps = []
-                        for snum in snums:
-                            if '"serviceDesc":"' in snum:
-                                styp = snum.split('"serviceDesc":"')[1].split('"')[0].encode("ascii", errors="ignore").decode()
-                                if typ == '':
-                                    typ = styp
+    #urls = ['https://cat-ms.esri.com/dls/cat/locations/en?f=json&forStorage=false&distanceUnit=mi&&searchType=location&maxResults=5000&searchDistance=700&productDivId=1%2C6%2C3%2C5%2C4%2C8%2C7%2C2&serviceId=1%2C2%2C3%2C4%2C8%2C9%2C10%2C5%2C6%2C7%2C12&appId=n6HDEnXnYRTDAxFr&searchValue=-115.8431267%2C35.2270869','https://cat-ms.esri.com/dls/cat/locations/en?f=json&forStorage=false&distanceUnit=mi&&searchType=location&maxResults=5000&searchDistance=10000&productDivId=1%2C6%2C3%2C5%2C4%2C8%2C7&serviceId=1%2C2&appId=n6HDEnXnYRTDAxFr&searchValue=-90%2C40','https://cat-ms.esri.com/dls/cat/locations/en?f=json&forStorage=false&distanceUnit=mi&&searchType=location&maxResults=5000&searchDistance=700&productDivId=1%2C6%2C3%2C5%2C4%2C8%2C7%2C2&serviceId=1%2C2%2C3%2C4%2C8%2C9%2C10%2C5%2C6%2C7%2C12&appId=n6HDEnXnYRTDAxFr&searchValue=-80.8431267%2C35.2270869']
+    for x in range(20, 70, 10):
+        for y in range(-60, -160, -10):
+            print(str(x) + ',' + str(y))
+            url = 'https://cat-ms.esri.com/dls/cat/locations/en?f=json&forStorage=false&distanceUnit=mi&&searchType=location&maxResults=1000&searchDistance=700&productDivId=1%2C6%2C3%2C5%2C4%2C8%2C7%2C2&serviceId=1%2C2%2C3%2C4%2C8%2C9%2C10%2C5%2C6%2C7%2C12&appId=n6HDEnXnYRTDAxFr&searchValue=' + str(y) + '%2C' + str(x)
+            r = session.get(url, headers=headers)
+            for line in r.iter_lines():
+                line = str(line.decode('utf-8'))
+                if '"dealerId":' in line:
+                    items = line.split('"dealerId":')
+                    for item in items:
+                        if '"territoryId":' in item:
+                            store = item.split('"dealerLocationId":')[1].split(',')[0]
+                            website = 'cat.com'
+                            hours = ''
+                            name = item.split(',"dealerName":"')[1].split('"')[0].encode("ascii", errors="ignore").decode()
+                            lat = item.split('"latitude":')[1].split(',')[0]
+                            lng = item.split('"longitude":')[1].split(',')[0]
+                            try:
+                                country = item.split('"countryCode":"')[1].split('"')[0]
+                            except:
+                                country = ''
+                            add = item.split('"siteAddress":"')[1].split('"')[0] + ' ' + item.split('"siteAddress1":"')[1].split('"')[0]
+                            add = add.strip().encode("ascii", errors="ignore").decode()
+                            pnums = item.split('"phoneNumberTypeId":')
+                            phone = '<MISSING>'
+                            for pnum in pnums:
+                                if '"phoneNumberType":"GENERAL INFO' in pnum:
+                                    phone = pnum.split('"phoneNumber":"')[1].split('"')[0].encode("ascii", errors="ignore").decode()
+                            city = item.split('"siteCity":"')[1].split('"')[0].encode("ascii", errors="ignore").decode()
+                            state = item.split('"siteState":"')[1].split('"')[0]
+                            zc = item.split(',"sitePostal":"')[1].split('"')[0]
+                            typ = ''
+                            snums = item.split('{"serviceId":')
+                            donetyps = []
+                            for snum in snums:
+                                if '"serviceDesc":"' in snum:
+                                    styp = snum.split('"serviceDesc":"')[1].split('"')[0].encode("ascii", errors="ignore").decode()
+                                    if typ == '':
+                                        typ = styp
+                                    else:
+                                        if styp not in donetyps:
+                                            donetyps.append(styp)
+                                            typ = typ + ', ' + styp
+                            try:
+                                if '"storeHoursMon":""' in item:
+                                    hours = 'Mon: Closed'
                                 else:
-                                    if styp not in donetyps:
-                                        donetyps.append(styp)
-                                        typ = typ + ', ' + styp
-                        try:
-                            if '"storeHoursMon":""' in item:
-                                hours = 'Mon: Closed'
-                            else:
-                                hours = 'Mon: ' + item.split('"storeHoursMon":"')[1].split('"')[0]
-                            if '"storeHoursTue":""' in item:
-                                hours = hours + '; Tue: Closed'
-                            else:
-                                hours = hours + '; Tue: ' + item.split('"storeHoursTue":"')[1].split('"')[0]
-                            if '"storeHoursWed":""' in item:
-                                hours = hours + '; Wed: Closed'
-                            else:
-                                hours = hours + '; Wed: ' + item.split('"storeHoursTue":"')[1].split('"')[0]
-                            if '"storeHoursThu":""' in item:
-                                hours = hours + '; Thu: Closed'
-                            else:
-                                hours = hours + '; Thu: ' + item.split('"storeHoursTue":"')[1].split('"')[0]
-                            if '"storeHoursFri":""' in item:
-                                hours = hours + '; Fri: Closed'
-                            else:
-                                hours = hours + '; Fri: ' + item.split('"storeHoursTue":"')[1].split('"')[0]
-                            if '"storeHoursSat":""' in item:
-                                hours = hours + '; Sat: Closed'
-                            else:
-                                hours = hours + '; Sat: ' + item.split('"storeHoursTue":"')[1].split('"')[0]
-                            if '"storeHoursSun":""' in item:
-                                hours = hours + '; Sun: Closed'
-                            else:
-                                hours = hours + '; Sun: ' + item.split('"storeHoursTue":"')[1].split('"')[0]
-                        except:
-                            hours = '<MISSING>'
-                        if country == 'CA' or country == 'US':
-                            if 'Sales"},' in item:
-                                loc = '<MISSING>'
-                                if state == '':
-                                    state = '<MISSING>'
-                                if store not in allstores:
-                                    allstores.append(store)
-                                    yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+                                    hours = 'Mon: ' + item.split('"storeHoursMon":"')[1].split('"')[0]
+                                if '"storeHoursTue":""' in item:
+                                    hours = hours + '; Tue: Closed'
+                                else:
+                                    hours = hours + '; Tue: ' + item.split('"storeHoursTue":"')[1].split('"')[0]
+                                if '"storeHoursWed":""' in item:
+                                    hours = hours + '; Wed: Closed'
+                                else:
+                                    hours = hours + '; Wed: ' + item.split('"storeHoursWed":"')[1].split('"')[0]
+                                if '"storeHoursThu":""' in item:
+                                    hours = hours + '; Thu: Closed'
+                                else:
+                                    hours = hours + '; Thu: ' + item.split('"storeHoursThu":"')[1].split('"')[0]
+                                if '"storeHoursFri":""' in item:
+                                    hours = hours + '; Fri: Closed'
+                                else:
+                                    hours = hours + '; Fri: ' + item.split('"storeHoursFri":"')[1].split('"')[0]
+                                if '"storeHoursSat":""' in item:
+                                    hours = hours + '; Sat: Closed'
+                                else:
+                                    hours = hours + '; Sat: ' + item.split('"storeHoursSat":"')[1].split('"')[0]
+                                if '"storeHoursSun":""' in item:
+                                    hours = hours + '; Sun: Closed'
+                                else:
+                                    hours = hours + '; Sun: ' + item.split('"storeHoursSun":"')[1].split('"')[0]
+                            except:
+                                hours = '<MISSING>'
+                            if country == 'CA' or country == 'US':
+                                if 'Sales"},' in item:
+                                    loc = '<MISSING>'
+                                    if state == '':
+                                        state = '<MISSING>'
+                                    if store not in allstores:
+                                        allstores.append(store)
+                                        yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
