5f276b48499693f84798801e482a7dcac4c7b0b8
diapk <Hank4957!>
2020-09-21 11:36:01 +0530

    docker run and push

--- apify/himanshu/storelocator/mymotomart_com/scrape.py
+++ apify/himanshu/storelocator/mymotomart_com/scrape.py
@@ -8,20 +8,20 @@ import json
 session = SgRequests()
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
+    with open('data.csv', mode='w', newline='') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
         # Body
         for row in data:
             writer.writerow(row)
 
 def fetch_data():
     base_url = "http://mymotomart.com"
-    r = session.get(base_url+"/Locations")
-    soup=BeautifulSoup(r.text,'lxml')
-    return_main_object = []
+
+    soup = BeautifulSoup(session.get(base_url+"/Locations").text,'lxml')
+   
     main=soup.find('div',{"id":"dnn_RightPane"}).find_all('div',{"class":"highrow"})
     for loc in main:
         name=loc.find('div',{"class":"storeNames"}).text.strip()
@@ -29,36 +29,37 @@ def fetch_data():
             madd=list(loc.find('div',{"class":"storeAddress"}).stripped_strings)
         else:
             madd=list(loc.find('div',{"class":"storAddress"}).stripped_strings)
-        print(madd)
-        address=madd[0].strip()
-        if len(madd)==4:
-            address+=' '+madd[1]
-            del madd[1]
-        ct=madd[1].strip().split(',')
-        city=ct[0].strip()
-        state=ct[1].replace('\n',' ').strip().split(' ')[0].strip()
-        zip=ct[1].replace('\n',' ').strip().split(' ')[1].strip()
-        phone=madd[2].strip()
+        address = " ".join(madd[:-2])
+
+        city = madd[-2].split(",")[0].strip()
+        state = madd[-2].split(",")[1].split()[0].strip()
+        zipp = madd[-2].split(",")[1].split()[1].strip()
+        phone = madd[-1].strip()
+
+        lat = loc.find("button", {"onclick": re.compile("&lat=")})['onclick'].split('lat=')[1].split('&')[0].strip()
+        lng = loc.find("button", {"onclick": re.compile("&lat=")})['onclick'].split('lon=')[1].split('&')[0].strip()
+    
         country="US"
         hour=''
-        lat=loc.find('a')['href'].split('lat=')[1].split('&')[0].strip()
-        lng=loc.find('a')['href'].split('lon=')[1].split('&')[0].strip()
+       
         store=[]
         store.append(base_url)
         store.append(name if name else "<MISSING>")
         store.append(address if address else "<MISSING>")
         store.append(city if city else "<MISSING>")
         store.append(state if state else "<MISSING>")
-        store.append(zip if zip else "<MISSING>")
+        store.append(zipp if zipp else "<MISSING>")
         store.append(country if country else "<MISSING>")
         store.append("<MISSING>")
         store.append(phone if phone else "<MISSING>")
-        store.append("mymotomart")
+        store.append("<MISSING>")
         store.append(lat if lat else "<MISSING>")
         store.append(lng if lng else "<MISSING>")
         store.append(hour if hour else "<MISSING>")
-        return_main_object.append(store)
-    return return_main_object
+        store.append("<MISSING>")
+        
+        yield store
+ 
 
 def scrape():
     data = fetch_data()
