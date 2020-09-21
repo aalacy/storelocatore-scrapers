78f8e4b3ea9c17f8d3904297b987136f7d69fb2d
diapk <Hank4957!>
2020-09-21 15:57:14 +0530

    docker run and push

--- apify/himanshu/storelocator/onewestbank_com/scrape.py
+++ apify/himanshu/storelocator/onewestbank_com/scrape.py
@@ -1,54 +1,26 @@
-import csv
-from sgrequests import SgRequests
+
 from bs4 import BeautifulSoup
-import re
-import json
-import sgzip
+import pandas as pd
 
 
-session = SgRequests()
+URL ='https://www.onewestbank.com/handlers/StoreLocationsHandler.ashx?BranchLocationsId=23622321295'
+df = pd.read_csv(URL,delimiter=",")
+df['locator_domain']="https://www.onewestbank.com"
+df['country_code']="US"
+df['page_url']="<MISSING>"
+df['HoursBiz']=df['HoursBiz'].str.split("Please note",n = 1,expand = True).replace("<p>","")
+rows = []
+for t in df['HoursBiz']:
+    soup = BeautifulSoup(t,"lxml")
+    rows.append(soup.get_text().replace("Sunset Park branch is closed",'').replace(";","").strip().lstrip())
+df['HoursBiz'] = rows
+df['HoursBiz']=df['HoursBiz'].replace("ATM Only","<MISSING>").str.encode('ascii', 'ignore').str.decode('ascii').replace("<p>",'').replace("\n"," ").replace(";","").replace("<p>Sunset Park branch is closed","")
+df['Phone']=df['Phone'].replace("ATM Only","<MISSING>")
+df['ATM']=df['ATM'].replace("yes","ATM").replace("no","branch")
+df['BranchID']=df['BranchID'].str.replace("T","")
+df=df.drop(['Safekeeping','DriveThru','Fax'], axis = 1) 
+df = df.rename(columns={'BranchName': 'location_name', 'Street': 'street_address',"City":"city","State":"state","Zip":"zip","Xcoord":"latitude","Ycoord":"longitude","HoursBiz":"hours_of_operation","ATM":"location_type","Phone":"phone","BranchID":"store_number"})
+df['street_address']=df['street_address'].str.replace("&amp;",'')
+df = df[["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"]]
+df.to_csv('data.csv',index=None)
 
-def write_output(data):
-    with open('data.csv', mode='w',encoding="utf-8") as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-        # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
-        # Body
-        for row in data:
-            writer.writerow(row)
-
-def fetch_data():
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
-    }
-    base_url = "https://www.onewestbank.com"
-    r = session.get("https://www.onewestbank.com/handlers/StoreLocationsHandler.ashx?BranchLocationsId=23622321295",headers=headers)
-    return_main_object = []
-    for location in r.text.split("\n")[1:]:
-        location_details = location.split('",')
-        for i in range(len(location_details)):
-            location_details[i] = location_details[i].replace('"',"")
-        store = []
-        store.append("https://www.onewestbank.com")
-        store.append(location_details[1])
-        store.append(location_details[2])
-        store.append(location_details[3])
-        store.append(location_details[4])
-        store.append(location_details[5])
-        store.append("US")
-        store.append(location_details[0])
-        store.append(location_details[9] if location_details[9] != "ATM Only" else "<MISSING>")
-        store.append("<MISSING>")
-        store.append(location_details[7])
-        store.append(location_details[8])
-        store.append(" ".join(list(BeautifulSoup(location_details[6],"lxml").stripped_strings)))
-        store.append('<MISSING>')
-        return_main_object.append(store)
-    return return_main_object
-
-def scrape():
-    data = fetch_data()
-    write_output(data)
-
-scrape()
