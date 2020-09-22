b9b7cac419cceb46fbe77e6b1266b46ed9ccc0aa
diapk <Hank4957!>
2020-09-22 18:59:06 +0530

    docker run and push

--- apify/himanshu/storelocator/onewestbank_com/scrape.py
+++ apify/himanshu/storelocator/onewestbank_com/scrape.py
@@ -16,11 +16,11 @@ for t in df['HoursBiz']:
 df['HoursBiz'] = rows
 df['HoursBiz']=df['HoursBiz'].replace("ATM Only","<MISSING>").str.encode('ascii', 'ignore').str.decode('ascii').replace("<p>",'').replace("\n"," ").replace(";","").replace("<p>Sunset Park branch is closed","")
 df['Phone']=df['Phone'].replace("ATM Only","<MISSING>")
-df['ATM']=df['ATM'].replace("yes","ATM").replace("no","branch")
+df['ATM']=df['ATM'].replace("yes","ATM & Branch").replace("no","Branch")
 df['BranchID']=df['BranchID'].str.replace("T","")
 df=df.drop(['Safekeeping','DriveThru','Fax'], axis = 1) 
 df = df.rename(columns={'BranchName': 'location_name', 'Street': 'street_address',"City":"city","State":"state","Zip":"zip","Xcoord":"latitude","Ycoord":"longitude","HoursBiz":"hours_of_operation","ATM":"location_type","Phone":"phone","BranchID":"store_number"})
 df['street_address']=df['street_address'].str.replace("&amp;",'')
 df = df[["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"]]
 df.to_csv('data.csv',index=None)
-
+# print(df)
