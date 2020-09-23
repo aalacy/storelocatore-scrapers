5b86a59f924de2faa62e6d7123626b4902bb497b
diapk <Hank4957!>
2020-09-23 17:03:31 +0530

    done

--- apify/himanshu/storelocator/papamurphys_ca/scrape.py
+++ apify/himanshu/storelocator/papamurphys_ca/scrape.py
@@ -6,14 +6,11 @@ from bs4 import BeautifulSoup
 import re
 import json
 import http.client
-
 session = SgRequests()
-
 http.client._MAXHEADERS = 1000
 
-
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
+    with open('data.csv', mode='w',newline="") as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
@@ -32,49 +29,46 @@ def fetch_data():
     }
 
     base_url = "http://papamurphys.ca/"
-    location_url = "https://papa-murphys-order-online-locations.securebrygid.com/zgrid/themes/13097/portal/index.jsp"   
+    location_url = "https://order.papamurphys.com/locations"   
     r = session.get(location_url, headers=headers)   
     soup = BeautifulSoup(r.text, "lxml")
-    data = soup.find_all("div",{"class":"restaurant"})
-    for i in data:
-        t = list(i.stripped_strings)
-        if "ONLINE ORDERING COMING SOON" in t:
-            continue
-        
-        if "Skip The Dishes - Delivery" in t[-1]:
-            del t[-1] 
-        if "Order Online" in t[-1]:
-            del t[-1]
-        location_name = t[0]
-        street_address = t[1]
-        city = t[2].split(',')[0]
-        state = t[2].split(',')[1].split(' ')[1]
-        zipp = ' '.join(t[2].split(',')[1].split(' ')[2:])
-        phone = t[3]
-        hours_of_operation=''
-        for index,dt in enumerate(t):
-            if "Get Directions" in dt:
-                hours_of_operation= " ".join(t[index+1:])
-        # hours_of_operation = ' '.join(t[5:8]).replace("*delivery available from 4pm to close.",'').replace("Delivery: 11am - Close",'')
-        store = []
-        store.append(base_url)
-        store.append(location_name)
-        store.append(street_address)
-        store.append(city)
-        store.append(state)
-        store.append(zipp)   
-        store.append("CA")
-        store.append("<MISSING>")
-        store.append(phone)
-        store.append("<MISSING>")
-        store.append("<MISSING>")
-        store.append("<MISSING>")
-        store.append(hours_of_operation)
-        store.append(location_url)
-        store = [x.encode('ascii', 'ignore').decode('ascii').strip() if type(x) == str else x for x in store]
-        # print(store)
-        yield store
-        
+    for href in soup.find("ul",{"id":"ParticipatingStates"}).find_all("a"):
+        r1 = session.get("https://order.papamurphys.com"+href['href'], headers=headers)   
+        soup1 = BeautifulSoup(r1.text, "lxml")
+        for h1 in soup1.find("ul",{"id":"ParticipatingRestaurants"}).find_all("a"):
+            page_url = h1['href']
+            r2 = session.get(page_url, headers=headers)   
+            soup2 = BeautifulSoup(r2.text, "lxml")
+            street_address = soup2.find("span",{"class":"street-address"}).text.strip()
+            city = soup2.find("span",{"class":"locality"}).text.strip()
+            location_name = soup2.find("div",{"class":"fn org"}).find("h1").text.strip()
+            state = soup2.find("span",{"class":"region"}).text.strip()
+            zipp = soup2.find("span",{"class":"postal-code"}).text.strip()
+            lat = soup2.find("span",{"class":"geo"}).find("span",{"class":"latitude"}).find("span",{"class":"value-title"})['title']
+            longs = soup2.find("span",{"class":"geo"}).find("span",{"class":"longitude"}).find("span",{"class":"value-title"})['title']
+            phone = soup2.find("span",{"class":re.compile("tel")}).text.strip()
+            hours_of_operation = ''
+            for q in soup2.find_all("dl",{"class":"available-hours"}):
+                hours_of_operation = hours_of_operation + ' '+ " ".join(list(q.stripped_strings))
+            store = []
+            store.append(base_url)
+            store.append(location_name)
+            store.append(street_address)
+            store.append(city)
+            store.append(state)
+            store.append(zipp)   
+            store.append("US")
+            store.append("<MISSING>")
+            store.append(phone)
+            store.append("<MISSING>")
+            store.append(lat)
+            store.append(longs)
+            store.append(hours_of_operation)
+            store.append(page_url)
+            store = [x.encode('ascii', 'ignore').decode('ascii').strip() if type(x) == str else x for x in store]
+            # print(store)
+            yield store
+            
 
 
 def scrape():
