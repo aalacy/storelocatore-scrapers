2c7369ab78305de80fb48c2841a59cc242dc6f5e
diapk <Hank4957!>
2020-09-25 12:11:58 +0530

    done

--- apify/himanshu/storelocator/malbeccuisine_com/scrape.py
+++ apify/himanshu/storelocator/malbeccuisine_com/scrape.py
@@ -3,20 +3,16 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
-
-
 session = SgRequests()
-
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
-
 def fetch_data():
     headers = {
         "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
@@ -25,29 +21,32 @@ def fetch_data():
     r = session.get("http://malbeccuisine.com",headers=headers)
     soup = BeautifulSoup(r.text,"lxml")
     return_main_object = []
-    for location in soup.find("ul",{'class':"location_modal"}).find_all("a"):
-            location_request = session.get(base_url + location["href"],headers=headers)
-            location_soup = BeautifulSoup(location_request.text,"lxml")
-            location_details = list(location_soup.find("section",{"class":"contact_info"}).find("ul").stripped_strings)
-            store = []
-            store.append("http://malbeccuisine.com")
-            store.append(location.text)
-            store.append(location_details[0].split("|")[0])
-            store.append(location_details[0].split("|")[1].split(",")[0])
-            store.append(location_details[0].split("|")[1].split(",")[1].split(" ")[-2])
-            store.append(location_details[0].split("|")[1].split(",")[1].split(" ")[-1])
-            store.append("US")
-            store.append("<MISSING>")
-            store.append(location_details[1])
-            store.append("malbec argentinean cuisine")
-            store.append("<MISSING>")
-            store.append("<MISSING>")
-            store.append(" ".join(location_soup.find("div",{'class':"hours"}).stripped_strings))
-            return_main_object.append(store)
-    return return_main_object
-
+    for location in soup.find_all("script",{'type':"application/json"})[-3]:
+        json_data = json.loads(location)
+        j =  json_data['preloadQueries']
+        for k in j :
+            try:
+                h = k['data']['restaurant']['locations']
+            except:
+                continue
+            for g in h:
+                store = []
+                store.append("http://malbeccuisine.com")
+                store.append(g['name'])
+                store.append(g['streetAddress'])
+                store.append(g['city'])
+                store.append(g['state'])
+                store.append(g['postalCode'])
+                store.append("US")
+                store.append("<MISSING>")
+                store.append(g['phone'])
+                store.append("malbec argentinean cuisine")
+                store.append(g['lat'])
+                store.append(g['lng'])
+                store.append(str(g['schemaHours']).replace("'","").replace("[","").replace("]","").replace("'",""))
+                store.append("https://www.malbeccuisine.com/")
+                yield store
 def scrape():
     data = fetch_data()
     write_output(data)
-
 scrape()
