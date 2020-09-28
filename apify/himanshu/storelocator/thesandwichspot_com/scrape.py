f7ee4ff1b39e481c0f8a8d27e40ca9a7dfb1f087
diapk <Hank4957!>
2020-09-28 15:34:14 +0530

    done

--- apify/himanshu/storelocator/thesandwichspot_com/scrape.py
+++ apify/himanshu/storelocator/thesandwichspot_com/scrape.py
@@ -3,7 +3,6 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup as BS
 import re
 import json
-import requests
 session = SgRequests()
 def write_output(data):
     with open('data.csv', mode='w',newline='') as output_file:
