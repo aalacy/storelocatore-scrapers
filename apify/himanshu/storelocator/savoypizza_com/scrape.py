0ce199abf588254f02ff41e8202abbae2807a033
Quincy <qcalexander@gmail.com>
2020-09-29 18:32:45 -0400

    savoypizza_com fixed

--- apify/himanshu/storelocator/savoypizza_com/scrape.py
+++ apify/himanshu/storelocator/savoypizza_com/scrape.py
@@ -12,7 +12,7 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -26,15 +26,20 @@ def fetch_data():
     soup = BeautifulSoup(r.text,"lxml")
     return_main_object = []
     for location in soup.find("nav",{"class":"locationsNav"}).find_all("a"):
-        location_request = session.get(location["href"],headers=headers)
+        page_url = location["href"]
+        # print(page_url)
+        location_request = session.get(page_url,headers=headers)
         location_soup = BeautifulSoup(location_request.text,"lxml")
         name = location_soup.find("div",{'class':"main"}).find("h1").text
         address = list(location_soup.find("div",{'class':"main"}).find_all("p")[0].stripped_strings)
-        hours = " ".join(list(location_soup.find("div",{'class':"main"}).find_all("p")[1].stripped_strings))
+        hours = " ".join(list(location_soup.find("div",{'class':"main"}).find_all("p")[1].stripped_strings)).replace("Hours:","").replace(" •",":").strip()
+        if "coming" in hours.lower():
+            continue
         lat = location_soup.find("div",{'class':"main"}).find("a",{'class':"map"})["data-lat"]
         lng = location_soup.find("div",{'class':"main"}).find("a",{'class':"map"})["data-lng"]
         store = []
         store.append("https://savoypizza.com")
+        store.append(page_url)
         store.append(name)
         store.append(address[0])
         store.append(address[1].split(",")[0])
@@ -43,7 +48,7 @@ def fetch_data():
         store.append("US")
         store.append("<MISSING>")
         store.append(address[-1].replace("\u202d","").replace("\u202c","") if len(address) != 2 else "<MISSING>")
-        store.append("red's savoy pizza")
+        store.append("<MISSING>")
         store.append(lat)
         store.append(lng)
         store.append(hours)
