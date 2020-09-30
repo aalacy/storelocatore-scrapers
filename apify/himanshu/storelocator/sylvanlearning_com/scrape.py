59388e9c67f57f22c50f601c670760d6acd11373
Quincy <qcalexander@gmail.com>
2020-09-30 03:49:49 -0400

    sylvanlearning_com fixed

--- apify/himanshu/storelocator/sylvanlearning_com/scrape.py
+++ apify/himanshu/storelocator/sylvanlearning_com/scrape.py
@@ -24,7 +24,7 @@ def write_output(data):
 
 def fetch_data():
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
     }
     base_url = "https://www.sylvanlearning.com/locations"
     r = session.get(base_url, headers=headers)
@@ -54,9 +54,12 @@ def fetch_data():
                 if y in vb:
                     country_code = vb[y][0]
 
+                print(base_url+y['href'])
                 r = session.get(base_url+y['href'])
                 soup = BeautifulSoup(r.text,"lxml")
                 for j in soup.find_all('div',{'class':'locationResults'}):
+                    if "satelliteIcon" in str(j):
+                        continue
                     locator_domain = base_url
                     location_name = j.find('h2',{'itemprop':'name'}).text
 
@@ -75,7 +78,7 @@ def fetch_data():
 
                     hours_of_operation = ''
                     if soup.find('div',{'class':'locationHoursContainer'}) != None:
-                        hours_of_operation = remove(soup.find('div',{'class':'locationHoursContainer'}).find('ul').text)
+                        hours_of_operation = remove(soup.find('div',{'class':'locationHoursContainer'}).find('ul').text).strip()
                     latitude = ''
                     longitude = ''
                     if soup.find('script') != None:
