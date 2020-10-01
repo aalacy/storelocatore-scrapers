de43324873a1c89133d226770ae5d0ee7f2330c7
Michael Dinh <mkdinh94@gmail.com>
2020-10-01 19:11:39 -0400

    SLC-2228/pauldavis_com: rewrite to mkdinh

--- apify/mkdinh/storelocator/pauldavis_com/scrape.py
+++ apify/mkdinh/storelocator/pauldavis_com/scrape.py
@@ -3,17 +3,21 @@ import time
 import datetime
 import re
 import threading
+import json
+import random
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
+from sglogging import sglog
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 MISSING = "<MISSING>"
-max_workers = 10
+max_workers = 8
 session = SgRequests()
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
 }
 thread_local = threading.local()
+log = sglog.SgLogSetup().get_logger(logger_name="pauldavis.com")
 
 
 def get_or_default(obj, key):
@@ -46,15 +50,9 @@ def write_output(data):
             ]
         )
 
-        # print("data::" + str(data))
-        for i in data or []:
-            writer.writerow(i)
-
-
-def get_session(reset=False):
-    if not hasattr(thread_local, "session") or (reset == True):
-        thread_local.session = SgRequests()
-    return thread_local.session
+        for i in data:
+            if i:
+                writer.writerow(i)
 
 
 def create_url(url):
@@ -96,7 +94,8 @@ def create_location(component):
     name = component.text
 
     href = component.get("href")
-    url = href if "https://" in href else f"https://{href}"
+    location_url = f"{href.rstrip('/')}/contact-us"
+    url = location_url if "https://" in location_url else f"https://{location_url}"
 
     return {"name": name, "url": url}
 
@@ -159,63 +158,65 @@ def fetch_location_data(location):
     page_url = location.get("url")
     location_name = location.get("name")
 
-    r = get_session().get(page_url)
+    r = session.get(page_url)
     bs = BeautifulSoup(r.text, "html.parser")
 
     info = {}
+    try:
+        address = bs.select("address .info")
+        city_state_zipcode = address[1].getText().split(", ")
 
-    address = bs.select("address .info")
-    city_state_zipcode = address[1].getText().split(", ")
+        city = city_state_zipcode.pop(0)
+        state_zipcode = city_state_zipcode.pop()
 
-    city = city_state_zipcode.pop(0)
-    state_zipcode = city_state_zipcode.pop()
+        state, zipcode = re.split("\.?\s", state_zipcode)
 
-    state, zipcode = re.split("\.?\s", state_zipcode)
+        info["street_address"] = address[0].getText()
+        info["city"] = city
+        info["state"] = state
+        info["zipcode"] = zipcode
+        info["country_code"] = "US"
 
-    info["street_address"] = address[0].getText()
-    info["city"] = city
-    info["state"] = state
-    info["zipcode"] = zipcode
-    info["country_code"] = "US"
+        if not info["street_address"]:
+            info["street_address"] = get_alt_info(bs)["street_address"]
 
-    if not info["street_address"]:
-        info["street_address"] = get_alt_info(bs)["street_address"]
+        if not info["city"]:
+            info["city"] = get_alt_info(bs)["city"]
 
-    if not info["city"]:
-        info["city"] = get_alt_info(bs)["city"]
+        if not info["state"]:
+            info["state"] = get_alt_info(bs)["state"]
 
-    if not info["state"]:
-        info["state"] = get_alt_info(bs)["state"]
+        if not info["zipcode"]:
+            info["zipcode"] = get_alt_info(bs)["zipcode"]
 
-    if not info["zipcode"]:
-        info["zipcode"] = get_alt_info(bs)["zipcode"]
+        phones = re.split("[\/|\|or]\s", bs.select_one(".phone-icon").getText())
+        cleaned_phone = [re.sub("\D", "", phone) for phone in phones]
+        info["phone"] = "|".join(cleaned_phone)
 
-    phones = re.split("[\/|\|or]\s", bs.select_one(".phone-icon").getText())
-    cleaned_phone = [re.sub("\D", "", phone) for phone in phones]
-    info["phone"] = "|".join(cleaned_phone)
+        store_number = MISSING
+        location_type = MISSING
+        latitude = MISSING
+        longitude = MISSING
+        hours_of_operation = MISSING
 
-    store_number = MISSING
-    location_type = MISSING
-    latitude = MISSING
-    longitude = MISSING
-    hours_of_operation = MISSING
-
-    return [
-        locator_domain,
-        page_url,
-        get_or_default(info, "location_name"),
-        store_number,
-        location_type,
-        get_or_default(info, "street_address"),
-        get_or_default(info, "city"),
-        get_or_default(info, "state"),
-        get_or_default(info, "zipcode"),
-        get_or_default(info, "country_code"),
-        latitude,
-        longitude,
-        get_or_default(info, "phone"),
-        hours_of_operation,
-    ]
+        return [
+            locator_domain,
+            page_url,
+            location_name,
+            store_number,
+            location_type,
+            get_or_default(info, "street_address"),
+            get_or_default(info, "city"),
+            get_or_default(info, "state"),
+            get_or_default(info, "zipcode"),
+            get_or_default(info, "country_code"),
+            latitude,
+            longitude,
+            get_or_default(info, "phone"),
+            hours_of_operation,
+        ]
+    except Exception as ex:
+        print(str(ex), page_url)
 
 
 def enqueue_locations_data(locations):
@@ -231,20 +232,18 @@ def enqueue_locations_data(locations):
 
 def fetch_data():
     state_urls = fetch_state_urls()
-    print(f"states count: {len(state_urls)}")
+    log.info(f"states count: {len(state_urls)}")
 
     city_urls = enqueue_cities(state_urls)
-    print(f"cities count: {len(city_urls)}")
+    log.info(f"cities count: {len(city_urls)}")
 
     location_urls = enqueue_locations(city_urls)
-    print(f"locations count: {len(location_urls)}")
+    log.info(f"locations count: {len(location_urls)}")
 
     yield from enqueue_locations_data(location_urls)
 
 
 def scrape():
-    print("-------------")
-
     start = time.time()
 
     data = fetch_data()
@@ -253,9 +252,7 @@ def scrape():
     end = time.time()
 
     elapsed = end - start
-    print("-------------")
-    print(f"duration: {datetime.timedelta(seconds=elapsed)}")
-    print("-------------")
+    log.info(f"duration: {datetime.timedelta(seconds=elapsed)}")
 
 
 scrape()
