a6cbcb8c9edf112b8156960b8926bbea97305054
Quincy <qcalexander@gmail.com>
2020-10-01 00:15:36 -0400

    champps_com fixed

--- apify/himanshu/storelocator/champps_com/scrape.py
+++ apify/himanshu/storelocator/champps_com/scrape.py
@@ -15,6 +15,7 @@ def write_output(data):
         # Body
         for row in data:
             writer.writerow(row)
+
 def fetch_data():
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
@@ -23,12 +24,18 @@ def fetch_data():
     base_url= "https://champps.com/location-search/"
     r = session.get(base_url)
     soup= BeautifulSoup(r.text,"lxml")
-    a1 = soup.find("script",{"type":"text/javascript"})
+    a1 = soup.find_all("script",{"type":"text/javascript"})
+    for a in a1:
+        if 'long"' in str(a):
+            a1 = a
+            break
+
     p1 = a1.text.replace("\/","").split('var mapdata = ')[1].split("var layout =")[0].replace(";","")
     p = json.loads(p1)
     for index,o in enumerate(p['markers']):
         if index > 0:
             location_name = p['markers'][o]['name']
+            # print(location_name)
             street_address = p['markers'][o]['address']+" "+p['markers'][o]['address2']
             city = p['markers'][o]['city']
             state = p['markers'][o]['state']
@@ -40,12 +47,15 @@ def fetch_data():
             link1 = p['markers'][o]['path']
             page_url = "https://champps.com/"+str(link1)
             r1 = session.get(page_url)
-            soup1 = BeautifulSoup(r1.text, "lxml")
-            hour = list(soup1.find("h4", text=re.compile(
-                "HOURS OF OPERATION")).parent.stripped_strings)
-            if len(hour) > 1:
-                hours_of_operation = " ".join(hour[1:])
-            else:
+            try:
+                soup1 = BeautifulSoup(r1.text, "lxml")
+                hour = list(soup1.find("h4", text=re.compile(
+                    "HOURS OF OPERATION")).parent.stripped_strings)
+                if len(hour) > 1:
+                    hours_of_operation = " ".join(hour[1:])
+                else:
+                    hours_of_operation = "<MISSING>"
+            except:
                 hours_of_operation = "<MISSING>"
             store = []
             store.append("https://champps.com/")
