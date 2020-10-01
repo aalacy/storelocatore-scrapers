9a704911a2bc364417460dd48147ac23985d32c3
Quincy <qcalexander@gmail.com>
2020-10-01 03:58:03 -0400

    drmarklynn_com fixed

--- apify/quincya/storelocator/drmarklynn_com/scrape.py
+++ apify/quincya/storelocator/drmarklynn_com/scrape.py
@@ -21,41 +21,35 @@ def fetch_data():
 
     locator_domain = "drmarklynn.com"
 
-    for num in range(1,4):
-        base_link = "https://drmarklynn.com/wp-json/wp/v2/doctor?page=%s&per_page=100&_embed" %num
+    base_link = "https://drmarklynn.com/wp-json/wp/v2/location?page=1&per_page=100&_embed"
 
-        session = SgRequests()
-        req = session.get(base_link, headers = HEADERS)
+    session = SgRequests()
+    req = session.get(base_link, headers = HEADERS)
 
-        base = BeautifulSoup(req.text,"lxml")
-        stores = stores = json.loads(base.text.strip())
+    base = BeautifulSoup(req.text,"lxml")
+    stores = json.loads(base.text.strip())
 
-        try:
-            if stores["data"]["status"] == 400:
-                break
-        except:
-            pass
+    for i in stores:
+        store = i["acf"]
+        location_name = store['location_address_map']['name']
+        raw_address = store["location_address_map"]["address"]
+        street_address = (raw_address["line_1"] + " " + raw_address["line_2"]).strip()
+        city = raw_address['city']
+        state = raw_address["state"]
+        zip_code = raw_address["zip"]
+        country_code = "US"
+        store_number = store["location_office_number"]
+        location_type = "<MISSING>"
+        phone = store['location_address_map']['info']['phone']
+        hours_of_operation = "<MISSING>"
+        latitude = store['location_address_map']['position']['lat']
+        longitude = store['location_address_map']['position']['lng']
 
-        for i in stores:
-            store = i["acf"]["locations_doctors"][0]["acf"]
-            location_name = store['location_address_map']['name']
-            raw_address = store["location_address_map"]["address"]
-            street_address = (raw_address["line_1"] + " " + raw_address["line_2"]).strip()
-            city = raw_address['city']
-            state = raw_address["state"]
-            zip_code = raw_address["zip"]
-            country_code = "US"
-            store_number = store["location_office_number"]
-            location_type = "<MISSING>"
-            phone = store['location_address_map']['info']['phone']
-            hours_of_operation = "<MISSING>"
-            latitude = store['location_address_map']['position']['lat']
-            longitude = store['location_address_map']['position']['lng']
+        if store_number not in found_poi:
+            # Store data
+            data.append([locator_domain, "<MISSING>", location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+            found_poi.append(store_number)
 
-            if street_address not in found_poi:
-                # Store data
-                data.append([locator_domain, "<MISSING>", location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
-                found_poi.append(street_address)
     return data
 
 def scrape():
