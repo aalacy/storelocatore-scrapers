36ac99735ca79244f9851a9c5e82cc09db6a716b
Quincy <qcalexander@gmail.com>
2020-10-03 16:01:59 -0400

    spunkfitness_com fixed

--- apify/aleksey/storelocator/spunkfitness_com/scrape.py
+++ apify/aleksey/storelocator/spunkfitness_com/scrape.py
@@ -1,62 +1,66 @@
+from sgrequests import SgRequests
+from bs4 import BeautifulSoup
 import csv
-import os
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
 import re
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
-def parse_extra(text):
-    return re.compile(r"(?<!^)\n([A-z\'\s\.]+)\,\s([\sA-z]+)\s([0-9]+)\n(\([0-9]{3}\)\s[0-9]{3}\-[0-9]{4})").split(text.replace('Come visit us at:\n', ''))
-def parse_hour(text):
-    result = '24' in text
-    return result
-def parse_geo(url):
-    lat_lon = re.findall(r'(?<=@)\S+', url)[0]
-    lat = lat_lon.split(',')[0]
-    lon = lat_lon.split(',')[1]    
-    return lat, lon
-
 def fetch_data():
     data = []
-    options = Options() 
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    # options.add_argument("--start-maximized")
 
-    # driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)
-    driver = webdriver.Chrome('chromedriver', options=options)
-    # driver = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
+    base_link = "https://www.spunkfitness.com/memberships/"
 
-    driver.get('https://www.spunkfitness.com/memberships/')
-    # Fetch store urls from location menu
-    store_els = driver.find_elements_by_class_name('cta2-heading-wrap')
-    store_hrefs = [store_el.find_element_by_xpath('.//a') for store_el in store_els] 
-    store_urls = [store_href.get_attribute('href') for store_href in store_hrefs]
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+    HEADERS = {'User-Agent' : user_agent}
+
+    session = SgRequests()
+    req = session.get(base_link, headers = HEADERS)
+    base = BeautifulSoup(req.text,"lxml")
+
+    store_urls = base.find_all(class_="grid-tab-4")
     # Fetch data for each store url
-    i = 0
+
     for store_url in store_urls:
-        i = i + 1
-        index = (i%2) + 1
-        driver.get(store_url)
+        link = store_url.a['href']
         # Fetch address/phone elements
-        location_name = driver.find_element_by_id('main-title').text
-        street_address, city, state, zipcode, phone, none = parse_extra(driver.find_element_by_css_selector('div.editor-content p:nth-of-type('+str(index)+')').text)
-        lat, lon = parse_geo(driver.find_element_by_css_selector('div.editor-content p a[target="_blank"]').get_attribute('href'))
-        hours_of_operation = '24 hours'
-        if(parse_hour(driver.find_element_by_css_selector('div.editor-content h2').text) == False):
-            hours_of_operation = driver.find_element_by_css_selector('div.editor-content p:nth-of-type(1)').text.replace("\n", ",")
+        location_name = store_url.h4.text
+        raw_address = store_url.p.text.split("\n")
+        street_address = raw_address[0]
+        city = raw_address[1].split(",")[0]
+        state = raw_address[1].split(",")[1].split()[0]
+        zipcode = raw_address[1].split(",")[1].split()[1]
+        phone = store_url.find_all("p")[1].text.strip()
+
+        map_link = store_url.find_all("a")[1]["href"]
+        if "@" in map_link:
+            at_pos = map_link.rfind("@")
+            lat = map_link[at_pos+1:map_link.find(",", at_pos)].strip()
+            lon = map_link[map_link.find(",", at_pos)+1:map_link.find(",", at_pos+15)].strip()
+        else:
+            lat_pos = map_link.rfind("!3d")
+            lat = map_link[lat_pos+3:map_link.find("!",lat_pos+5)].strip()
+            lng_pos = map_link.find("!4d")
+            lon = map_link[lng_pos+3:].strip()
+
+        req = session.get(link, headers = HEADERS)
+        base = BeautifulSoup(req.text,"lxml")
+
+        try:
+            hours_of_operation = base.table.text.replace("\xa0","").replace("\n"," ")
+        except:
+            hours_of_operation = base.find(style="font-size:20px").text.replace("Our regular hours of operation are:","").replace("\n"," ").strip()
+        hours_of_operation = (re.sub(' +', ' ', hours_of_operation)).strip()
+
         data.append([
             'https://www.spunkfitness.com/',
+            link,
             location_name,
             street_address,
             city,
@@ -68,10 +72,9 @@ def fetch_data():
             '<MISSING>',
             lat,
             lon,
-            hours_of_operation
+            hours_of_operation.encode("ascii", "replace").decode().replace("?","-")
         ])
 
-    driver.quit()
     return data
 
 def scrape():
