28fce6477ac43cc0056a0cf851e6cfeaa1b425b8
diapk <Hank4957!>
2020-10-06 15:58:19 +0530

    done

--- apify/himanshu/storelocator/avalon-hotel_com/scrape.py
+++ apify/himanshu/storelocator/avalon-hotel_com/scrape.py
@@ -2,76 +2,59 @@ import csv
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
-
-
+import json
 session = SgRequests()
-
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-        # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
-        # Body
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         for row in data:
             writer.writerow(row)
-
 def fetch_data():
+    addressess = []
     base_url = "https://www.avalon-hotel.com/"
     r = session.get(base_url)
     soup = BeautifulSoup(r.text,"lxml")
-    return_main_object = []
     store_listing = soup.find("li",{"id": "menu-item-1347"}).find('ul')
     alla = store_listing.find_all('li')
     for i in range(len(alla)):
-        store_no=alla[i]['id'].split('-')[-1]
         link=alla[i].find('a')['href']
         r1 = session.get(link)
         soup1 = BeautifulSoup(r1.text,"lxml")
-        link1 = soup1.find("ul",{"class":"topnav-menu"}).find('li').find("ul",{"class":"sub-menu"}).find('a')['href']
-        r2 = session.get(link1)
-        soup2 = BeautifulSoup(r2.text,"lxml")
-        store_name=soup2.find("h2",{"class":"feattitlebox-subtitle"}).text.strip()
-        mainaddress=soup2.find('div',{'class':"ftr-address left"}).text.split(",")
-        mlat=soup2.find('div',{'class':"ftr-address left"}).find('a')['href'].split('@')[-1]
-        lat=mlat.split(',')[0]
-        lng=mlat.split(',')[1]
-        state=mainaddress[-1].split(' ')[-2].strip()
-        zip=mainaddress[-1].split(' ')[-1].strip()
-        phone = soup2.find('div',{'class':"ftr-phone left"}).find('span').text
-        if len(mainaddress) == 3:
-            address=mainaddress[-3]
-            city=mainaddress[-2]
-        else:
-            address1=mainaddress[-2].split(' ')
-            city =address1[-2]+" "+address1[-1]
-            address =mainaddress[-2].split(city)[0]
+        jd = json.loads(str(soup1).split('<script type="application/ld+json">[')[1].split(']</script>')[0])
+        location_name = jd['name']
+        street_address = jd['address']['streetAddress']
+        city = jd['address']['addressLocality']
+        state = jd['address']['addressRegion']
+        zipp = jd['address']['postalCode']
+        country_code= jd['address']['addressCountry']
+        phone = jd['telephone']
+        location_type = jd['@type']
+        latitude = jd['geo']['latitude']
+        longitude = jd['geo']['longitude']
+        hours_of_operation = "Monday to Sunday - 24 hours"
+        page_url = jd['url']
         store = []
-        store.append("https://www.avalon-hotel.com")
-        store.append(store_name)
-        store.append(address)
-        store.append(city)
-        store.append(state)
-        store.append(zip)
-        store.append("US")
-        store.append(store_no)
-        try:
-            store.append(phone)
-        except:
-            store.append("<MISSING>")
-        store.append("Avalon Hotel")
-        try:
-            store.append(lat)
-            store.append(lng)
-        except:
-            store.append("<MISSING>")
-            store.append("<MISSING>")
-        store.append("<MISSING>")
-        return_main_object.append(store)
-    return return_main_object
-
+        store.append(base_url if base_url else '<MISSING>')
+        store.append(location_name if location_name else '<MISSING>')
+        store.append(street_address if street_address else '<MISSING>')
+        store.append(city if city else '<MISSING>')
+        store.append(state if state else '<MISSING>')
+        store.append(zipp if zipp else '<MISSING>')
+        store.append(country_code)
+        store.append('<MISSING>')
+        store.append(phone if phone else '<MISSING>')
+        store.append(location_type)
+        store.append(latitude if latitude else '<MISSING>')
+        store.append(longitude if longitude else '<MISSING>')
+        store.append(hours_of_operation if hours_of_operation else '<MISSING>')
+        store.append(page_url)
+        store = [x.encode('ascii', 'ignore').decode('ascii').strip() if type(x) == str else x for x in store]
+        if store[2] in addressess:
+            continue
+        addressess.append(store[2])
+        yield store
 def scrape():
     data = fetch_data()
     write_output(data)
-
 scrape()
--- apify/himanshu/storelocator/redroof_com__hometowne-studios/scrape.py
+++ apify/himanshu/storelocator/redroof_com__hometowne-studios/scrape.py
@@ -7,42 +7,35 @@ session = SgRequests()
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-        # Header
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
-        # Body
         for row in data:
             writer.writerow(row)
 def fetch_data():
     addressess = []
     headers = {
-
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
 	}
-    base_url = "https://www.redroof.com/hometowne-studios"
+    base_url = "https://www.redroof.com"
     location_url = "https://www.redroof.com/extendedstay/hometownestudios/all-locations"
     r = session.get(location_url,headers=headers)
     soup=BeautifulSoup(r.text,'lxml')
     link = soup.find("div",{"class":"rich-text-content"}).find_all("a")
     for i in link:
         page_url = "https://www.redroof.com/"+i['href']
-        r1 = session.get(page_url,headers=headers)
-        soup1 = BeautifulSoup(r1.text,'lxml')
-        jd = json.loads(str(soup1).split('Utilities.SDL.Add("ServicePropertyDetails", ')[1].split(');')[0])
+        property_id = page_url.split("/")[-1]
+        jd = session.get("https://www.redroof.com/api/GetPropertyDetail?PropertyId="+str(property_id),headers=headers).json()['PropertyDetail']['ServicePropertyDetails']
         location_name = jd['Description']
         street_address = jd['Street1']
         city = jd['City']
         state = jd['State']
         zipp = jd['PostalCode']
         country_code = jd['Country']
-        store_number = "<MISSING>"
+        store_number = jd['PropertyCode'].replace("HTS","").replace("hts","")
         phone = jd['PhoneNumber']
         location_type = jd['PropertyType']
-        latitude = jd['Latitude']
-        longitude = jd['Longitude']
-        hours_of_operation = "<MISSING>"
-
-
+        latitude = jd['MajorMetroArea_Latitude']
+        longitude = jd['MajorMetroArea_Longitude']
+        hours_of_operation = "Check in time-3:00 PM, Check out time-11:00 AM"
         store = []
         store.append(base_url if base_url else '<MISSING>')
         store.append(location_name if location_name else '<MISSING>')
@@ -63,7 +56,6 @@ def fetch_data():
             continue
         addressess.append(store[2])
         yield store
-
 def scrape():
     data = fetch_data()
     write_output(data)
