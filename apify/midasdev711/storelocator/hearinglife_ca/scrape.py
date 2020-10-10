73f1d7ec24e1f398f55163a92eaf79bed81e4cf1
Quincy <qcalexander@gmail.com>
2020-10-09 20:56:00 -0400

    hearinglife_ca fixed

--- apify/midasdev711/storelocator/hearinglife_ca/scrape.py
+++ apify/midasdev711/storelocator/hearinglife_ca/scrape.py
@@ -1,7 +1,6 @@
 import csv
 import re
-import pdb
-import requests
+from sgrequests import SgRequests
 from lxml import etree
 import json
 
@@ -13,14 +12,14 @@ def validate(items):
         if item is '<MISSING>':
             continue
         if type(item) is str:
-            item = item.encode('ascii','ignore').strip()
+            item = item.strip()
         rets.append(item)
     return rets
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
     return
@@ -28,38 +27,47 @@ def write_output(data):
 def fetch_data():
     output_list = []
     url = "https://www.hearinglife.ca/webservices/centerlocator.svc/GetCenters/%7B6B8D4C17-298F-47DA-82F8-0628E2C8C6C9%7D/null/null/en-ca/"
-    headers={
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Connection": "keep-alive",
-        "Cookie": "ASP.NET_SessionId=znfhse0rdzdnzu5xwqxkdj11; _ga=GA1.2.412036171.1565172381; _gid=GA1.2.293429706.1565172381; _gat_UA-42935243-1=1; _hjid=15667ba2-f38c-491a-8097-356e935c47e1; _hjIncludedInSample=1; _fbp=fb.1.1565172383943.299340127; SC_ANALYTICS_GLOBAL_COOKIE=21b7acba52a948d4af52131f296af25e|True; _gcl_au=1.1.546395052.1565172391; mTrackingPageViewCount=3; mTrackingTimeOnSite=43500",
-        "Host": "www.hearinglife.ca",
-        "Referer": "https://www.hearinglife.ca/centre-locator",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest"
-        }
-    request = requests.get(url, headers=headers)
+
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+    headers = {'User-Agent' : user_agent}
+
+    session = SgRequests()
+    request = session.get(url, headers=headers)
     store_list = json.loads(request.text)['Centers']
     for store in store_list:
         output = []
+
+        desc = re.findall(r'https://www\.hearinglife\.ca/book-a-consultation\?region=.+">', store["Description"])[0]
+        link = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', desc)[0].replace("&amp;","&").replace("%20","-")
+
+        link = "https://www.hearinglife.ca/book-a-consultation?region=" + store["RegionFolder"] + "&centername=" + store["ItemName"]
         hours = store.get('OpeningDayHours')
         store_hours = ""
         if len(hours) > 0:
             for x in hours:
                 store_hours += (x.get('Day') or ' ') + ' ' + (x.get('OpeningHours') or ' ') + ','
+        store_hours = store_hours.replace("<!-- ","(").replace("<!-- ","(").replace(" -->",")").replace("  ,",",").strip()
+        if store_hours[-1:] == ",":
+            store_hours = store_hours[:-1].strip()
         output.append(base_url)
+        output.append(link)
         output.append(store.get('Title'))
-        output.append(store.get('Address'))
+        street_address = store.get('Address').replace("Alpha Corporate Centre - ","")
+        if "|" in street_address:
+            street_address = street_address.split("|")[-1].strip()
+        if "(" in street_address:
+            street_address = street_address.split("(")[0].strip()
+        output.append(street_address)
         output.append(store.get('City'))
         output.append(store.get('Region'))
         output.append(store.get('PostalCode'))
         output.append('CA')
-        output.append(store.get('Id'))
+        store_num = store.get('Id')
+        if not store_num:
+            store_num = '<MISSING>'
+        output.append(store_num)
         output.append(store.get('Phonenumber'))
-        output.append(store.get('CenterType'))
+        output.append('<MISSING>')
         output.append(store.get('Latitude'))
         output.append(store.get('Longitude'))
         output.append(store_hours)
