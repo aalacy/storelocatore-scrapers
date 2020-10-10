4c73bbed22c7d2a30ac38079783c61cfe661ad77
Quincy <qcalexander@gmail.com>
2020-10-10 04:20:00 -0400

    elranchoinc_com first commit

---
+++ apify/quincya/storelocator/elranchoinc_com/scrape.py
@@ -0,0 +1,91 @@
+import csv
+import time
+import re
+from sgrequests import SgRequests
+from bs4 import BeautifulSoup
+import sgzip
+import json
+
+
+def write_output(data):
+	with open('data.csv', mode='w') as output_file:
+		writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+		# Header
+		writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+		# Body
+		for row in data:
+			writer.writerow(row)
+
+def fetch_data():
+
+	base_link = "https://elranchoinc.com/wp-admin/admin-ajax.php"
+	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+	HEADERS = {'User-Agent' : user_agent}
+
+	session = SgRequests()
+	
+	data = []
+	found_poi = []
+
+	coords = sgzip.coords_for_radius(200)
+	for coord in coords:
+		lat, lng = coord[0], coord[1]
+		payload = { 'lat': lat,
+					'lng': lng,
+					'store_locatore_search_radius': '200',
+					'action': 'make_search_request'}
+
+		base_link = "https://elranchoinc.com/wp-admin/admin-ajax.php"
+		response = session.post(base_link,headers=HEADERS,data=payload)
+		base = BeautifulSoup(response.text,"lxml")
+		# print(lat, lng)
+
+		fin_script = ""
+		all_scripts = base.find_all('script')
+		for script in all_scripts:
+			if "lng" in str(script):
+				fin_script = script.text.replace('\\', '').replace('"/>', '').replace('"http', '').replace('">','').replace('class="','class=').replace('" class',' class').replace("\n","").strip()
+				break
+		if not fin_script:
+			continue
+
+		js = fin_script.split('locations":')[-1].split("};  ")[0].strip()
+		stores = json.loads(js)
+		items = base.find_all(class_="store-locator-item")
+		
+		locator_domain = "elranchoinc.com"
+
+		for i in range(len(stores)):
+			store = stores[i]
+			item = items[i]
+			location_name = item.find(class_="wpsl-name").text.strip()
+			street_address = item.find(class_="wpsl-address").text.strip()
+			if street_address in found_poi:
+				continue
+			found_poi.append(street_address)
+			city_line = item.find(class_="wpsl-city").text.strip().split(",")
+			city = city_line[0]
+			state = city_line[1].split()[0].strip()
+			try:
+				zip_code = city_line[1].split()[1].strip()
+			except:
+				zip_code = "<MISSING>"
+			country_code = "US"
+			store_number = item["id"]
+			location_type = "<MISSING>"
+			phone = item.find(class_="wpsl-phone").text.strip()
+			hours_of_operation = "<MISSING>"
+			latitude = store['lat']
+			longitude = store['lng']
+			link = "https://elranchoinc.com/stores-2/"
+			
+			# Store data
+			data.append([locator_domain, link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+	return data
+
+def scrape():
+	data = fetch_data()
+	write_output(data)
+
+scrape()
