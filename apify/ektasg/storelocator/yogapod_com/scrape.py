9efa9d61a5ece20c92d3c5ff1609fbac4943c9bf
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-10-12 15:11:41 +0500

    Add files via upload

--- apify/ektasg/storelocator/yogapod_com/scrape.py
+++ apify/ektasg/storelocator/yogapod_com/scrape.py
@@ -1,19 +1,13 @@
-import time
+from bs4 import BeautifulSoup
 import csv
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import re
-import usaddress
+import string
+import re, time
 
+from sgrequests import SgRequests
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-#driver = webdriver.Chrome("C:\chromedriver.exe", options=options)
-driver = webdriver.Chrome("chromedriver", options=options)
-#driver2 = webdriver.Chrome("C:\chromedriver.exe", options=options)
-driver2 = webdriver.Chrome("chromedriver", options=options)
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
@@ -26,131 +20,75 @@ def write_output(data):
             writer.writerow(row)
 
 
-
-def parse_geo(url):
-    lon = re.findall(r'\,(--?[\d\.]*)', url)[0]
-    lat = re.findall(r'\@(-?[\d\.]*)', url)[0]
-    return lat, lon
-
-
 def fetch_data():
-    # Your scraper here
-    count=0
-    data=[]
-    driver.get("https://www.yogapod.com/studios/")
-    time.sleep(5)
-    stores = driver.find_elements_by_css_selector('div.textwidget > ul > li > a')
-    name = [stores[i].get_attribute('href') for i in range(0, len(stores))]
-    name = name[:-5]
-    for i in range(len(name)):
-            driver.get(name[i])
-            page_url = name[i]
-            print(page_url)
-            time.sleep(5)
-            location_name = page_url.split(".com/")[1].replace("/","")
-            address = driver.find_element_by_xpath("//span[contains(@itemprop,'address')]").text
-            tagged = usaddress.tag(address)[0]
-            try:
-                street_addr = tagged['BuildingName'].split('\n')[0] + " " + tagged['AddressNumber'] + " " + \
-                              tagged['StreetNamePreDirectional'] + " " + tagged['StreetName'] + " " + \
-                              tagged['OccupancyType'] + " " + tagged['OccupancyIdentifier'].split('\n')[0]
-            except:
-                try:
-                    street_addr = tagged['AddressNumber'] + " " + tagged['StreetNamePreDirectional'] + " " + tagged[
-                    'StreetName'] + " " + tagged['StreetNamePostType'].split('\n')[0]
-                except:
+    p = 0
+    data = []
+    pattern = re.compile(r'\s\s+')
+    cleanr = re.compile(r'<[^>]+>')
+    titles = []
+    url = ['https://www.yogapod.com/wp-admin/admin-ajax.php?action=store_search&max_results=50&search_radius=100&autoload=1']
+    for i in range(0,len(url)):
+        loclist = session.get(url[i], headers=headers, verify=False).json()
+        print(len(loclist))
+        for loc in loclist:
+            #print(loc['store'])
+            if loc['store'] in titles:
+                continue
+            else:
+                titles.append(loc['store'])
+                title = loc['store']
+                street = loc['address']+' ' +loc['address2']
+                state = loc['state']
+                city = loc['city']
+                pcode = loc['zip']
+                store = loc['id']
+                link =  loc['url']
+                lat =  loc['lat']
+                longt =  loc['lng']
+                hours = BeautifulSoup(loc['hours'],'html.parser').text.replace('day','day ').replace('PM','PM ').replace('Closed','Closed ')
+                phone= loc['phone']
+                page = session.get(link, headers=headers, verify=False)
+                plist = BeautifulSoup(page.text,'html.parser').findAll('a')
+                for ph in plist:
                     try:
-                        street_addr = tagged['AddressNumber'] + " " + tagged['StreetNamePreType'] + " " + \
-                                      tagged['StreetName'] + " " + tagged['OccupancyIdentifier'].split('\n')[0]
-                    except:
-                        try:
-                            street_addr = tagged['AddressNumber'] + " " + tagged['StreetName'] + " " + \
-                                          tagged['StreetNamePostType'].split('\n')[0] + " " + tagged[
-                                              'StreetNamePostDirectional'] + " " + tagged['OccupancyIdentifier'].split('\n')[0]
-                        except:
-                            try:
-                                street_addr = tagged['AddressNumber'] + " " + tagged['StreetNamePreDirectional'] + " " + tagged[
-                                    'StreetName'] + " " + tagged['StreetNamePostType'].split('\n')[0]
-                            except:
-                                try:
-                                    street_addr = tagged['AddressNumber'] + " " + tagged['StreetName'] + " " + \
-                                                  tagged['StreetNamePostDirectional'].split('\n')[0] + " " + \
-                                                  tagged['StreetNamePostType'].split('\n')[0]
-                                except:
-                                    try:
-                                        street_addr = tagged['AddressNumber'] + " " + tagged['StreetName'] + " " + \
-                                                      tagged['StreetNamePostType'].split('\n')[0] + " " + \
-                                                      tagged['OccupancyIdentifier'].split('\n')[0]
-                                    except:
-                                        try:
-                                            street_addr = tagged['AddressNumber'] + " " + tagged['StreetName'] + " " + \
-                                                          tagged['StreetNamePostType'].split('\n')[0]
-                                        except:
-                                            try:
-                                                street_addr = tagged['AddressNumber'] + " " + tagged[
-                                                    'StreetNamePreType'] + " " + tagged['StreetName'] + " " + tagged[
-                                                                  'StreetNamePostDirectional']
-                                            except:
-                                                try:
-                                                    street_addr = tagged['AddressNumber'] + " " + tagged['StreetName']
-                                                except:
-                                                    street_addr = '<MISSING>'
-
-            state = tagged['StateName']
-            city = tagged['PlaceName']
-            zipcode = tagged['ZipCode']
-            phone = driver.find_element_by_xpath("//a[contains(@href,'tel:')]").text
-            if phone == "" or phone == " ":
-                phone = '<MISSING>'
-            elements = driver.find_elements_by_css_selector('div.mk-text-block')
-            for elem in elements:
-                try:
-                    h4_elem = elem.find_element_by_css_selector('h4')
-                    if h4_elem.text.lower() == 'studio hours':
-                        hours = elem.find_elements_by_css_selector('h6')
-                        hours_of_op = ""
-                        for j in hours:
-                            hours_of_op = hours_of_op + j.text
-                        break
-                except:
-                    try:
-                        hours_elem = elem.find_element_by_css_selector('table.mabel-bhi-businesshours')
-                        hours_of_op = hours_elem.text
-                        break
+                        if ph['href'].find('tel:') > -1:
+                            phone = ph.text
+                            break
                     except:
                         pass
-            if hours_of_op == "" or hours_of_op == " ":
-                hours_of_op = '<MISSING>'
-
-            geomap = driver.find_element_by_xpath("//a[contains(@href,'https://goo.gl/maps/')]").get_attribute('href')
-            driver2.get(geomap)
-            time.sleep(5)
-            lat,lon = parse_geo(driver2.current_url)
-            data.append([
-                'https://www.yogapod.com/',
-                page_url,
-                location_name,
-                street_addr.replace("\n", " "),
-                city,
-                state,
-                zipcode,
-                'US',
-                '<MISSING>',
-                phone,
-                '<MISSING>',
-                lat,
-                lon,
-                hours_of_op.replace("\n", " ")
-            ])
-            count = count + 1
-            print(count)
-
-    time.sleep(3)
-    driver.quit()
+               # print(phone)
+                
+                data.append([
+                        'https://www.yogapod.com/',
+                        link,                   
+                        title.replace('\u202d',''),
+                        street.replace('\u202d',''),
+                        city.replace('\u202d',''),
+                        state.replace('\u202d',''),
+                        pcode.replace('\u202d',''),
+                        'US',
+                        store,
+                        phone.replace('\u202d',''),
+                        '<MISSING>',
+                        lat,
+                        longt,
+                        hours.replace('\u202d','')
+                    ])
+                #print(p,data[p])
+                p += 1
+                    
+            
+        
+        
+           
+        
     return data
 
+
 def scrape():
+    
     data = fetch_data()
     write_output(data)
+
 
 scrape()
