fa2d967741ecd6926daa2c8f537062182868b60c
Quincy <qcalexander@gmail.com>
2020-10-12 17:24:07 -0400

    graffitijunktion_com fixed

--- apify/himanshu/storelocator/graffitijunktion_com/scrape.py
+++ apify/himanshu/storelocator/graffitijunktion_com/scrape.py
@@ -31,17 +31,14 @@ def fetch_data():
     store_name=[]
     store_detail=[]
     return_main_object=[]
-    lat=[]
-    lng=[]
-    hours =[]
-    k  = soup.find_all("script")
-    for i in k:
-        if "var map" in i.text:
-            k1 = json.loads(i.text.split("maps(")[1].replace(').data("wpgmp_maps");});',''))
-            for i in k1['places']:
-                lat.append(i['location']['lat'])
-                lng.append(i['location']['lng'])
-                # print(i['location']['lng'])
+    # k  = soup.find_all("script")
+    # for i in k:
+    #     if "var map" in i.text:
+    #         k1 = json.loads(i.text.split("maps(")[1].replace(').data("wpgmp_maps");});',''))
+    #         for i in k1['places']:
+    #             lat.append(i['location']['lat'])
+    #             lng.append(i['location']['lng'])
+    #             # print(i['location']['lng'])
     # exit()
     data1 = soup.find_all("h2",{"style":"font-weight:bold;"})
     for d in data1:
@@ -56,17 +53,15 @@ def fetch_data():
             state = list(target_list.find('div',{'class':'wpb_wrapper'}).stripped_strings)[1].split(',')[2].split( )[0]
             zipcode =  list(target_list.find('div',{'class':'wpb_wrapper'}).stripped_strings)[1].split(',')[2].split( )[1]
             phone = list(target_list.find('div',{'class':'wpb_wrapper'}).stripped_strings)[3]
-            hours1= list(target_list.find('div',{'class':'wpb_wrapper'}).stripped_strings)[5]
 
             tem_var.append(street_address)
-            tem_var.append(city)
+            tem_var.append(city.strip())
             tem_var.append(state)
             tem_var.append(zipcode)
             tem_var.append("US")
             tem_var.append("<MISSING>")
             tem_var.append(phone)
-            tem_var.append("<MISSING>")            
-            hours.append(hours1)            
+            tem_var.append("<MISSING>")
             store_detail.append(tem_var)
             
         
@@ -75,9 +70,9 @@ def fetch_data():
         store.append("https://graffitijunktion.com")
         store.append(store_name[i])
         store.extend(store_detail[i])
-        store.append(lat[i])
-        store.append(lng[i])
-        store.append(hours[i])
+        store.append('<MISSING>')
+        store.append('<MISSING>')
+        store.append('<MISSING>')
         store.append('<MISSING>')
         return_main_object.append(store) 
     return return_main_object
