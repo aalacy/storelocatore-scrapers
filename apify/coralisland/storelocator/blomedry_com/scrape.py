0217f0c3eb49ca45141b1800a6bd61abc64ac60f
Quincy <qcalexander@gmail.com>
2020-10-12 23:29:44 -0400

    blomedry_com fixed

--- apify/coralisland/storelocator/blomedry_com/scrape.py
+++ apify/coralisland/storelocator/blomedry_com/scrape.py
@@ -7,6 +7,9 @@ from lxml import etree
 import json
 import time
 from random import randint
+from sglogging import sglog
+
+log = sglog.SgLogSetup().get_logger(logger_name="blomedry.com")
 
 base_url = 'https://blomedry.com'
 
@@ -25,7 +28,7 @@ def get_value(item):
         item = '<MISSING>'
     item = validate(item)
     if item == '':
-        item = '<MISSING>'    
+        item = '<MISSING>'
     return item
 
 def eliminate_space(items):
@@ -54,60 +57,68 @@ def fetch_data():
 
     session = SgRequests()
     output_list = []
-    url = "https://blomedry.com/locations/"
-    request = session.get(url, headers=headers)
-    response = etree.HTML(request.text)
-    store_list = response.xpath('//li[contains(@class, "article-location")]')
+    countries = ["US","CA"]
+    for country in countries:
+        url = "https://blomedry.com/locations/?country=" + country
+        request = session.get(url, headers=headers)
+        response = etree.HTML(request.text)
+        store_list = response.xpath('//li[contains(@class, "article-location")]')
+        log.info(url)
+        for i, store in enumerate(store_list):
+            # print("Link %s of %s" %(i+1,len(store_list)))
+            detail_url = store.xpath(".//h2/a/@href")[0]
+            # print(detail_url)
+            latitude = store.xpath("@data-lat")[0]
+            longitude = store.xpath("@data-lng")[0]
 
-    for i, store in enumerate(store_list):
-        print("Link %s of %s" %(i+1,len(store_list)))
-        detail_url = store.xpath(".//h2/a/@href")[0]
-        print(detail_url)
-        latitude = store.xpath("@data-lat")[0]
-        longitude = store.xpath("@data-lng")[0]
+            title = store.xpath(".//h2/a/text()")[0]
+            address = store.xpath(".//p/text()")
+            if not address[-1].strip():
+                address.pop(-1)
+            try:
+                street_address = get_value(address[-3]).replace(","," ").strip() + " " + get_value(address[-2]).replace(","," ").strip()
+            except:
+                street_address = get_value(address[-2]).replace(","," ").strip()
+            city_state = validate(address[-1])
+            city = city_state.split(",")[0]
+            if country == "US":                
+                state = city_state.split(",")[1][:-6].strip()
+            else:
+                state = city_state.split(",")[1][:-7].strip()
+            if country == "US":
+                zipcode = city_state.split(",")[1][-6:].strip()
+                if zipcode == "90595" and city == "Torrance":
+                    zipcode = "90505"
+            else:
+                zipcode = city_state.split(",")[1][-7:].strip()
+            try:
+                phone = store.xpath(".//p/a/text()")[0]
+            except:
+                continue
 
-        title = store.xpath(".//h2/a/text()")[0]
-        address = store.xpath(".//p/text()")
-        if not address[-1].strip():
-            address.pop(-1)
-        try:
-            street_address = get_value(address[-3]).replace(","," ").strip() + " " + get_value(address[-2]).replace(","," ").strip()
-        except:
-            street_address = get_value(address[-2]).replace(","," ").strip()
-        city_state = validate(address[-1])
-        city = city_state.split(",")[0]
-        state = city_state.split(",")[1][:-6].strip()
-        zipcode = city_state.split(",")[1][-6:].strip()
-        if zipcode == "90595" and city == "Torrance":
-            zipcode = "90505"
-        try:
-            phone = store.xpath(".//p/a/text()")[0]
-        except:
-            continue
+            try:
+                req = session.get(detail_url, headers = headers)
+                base = BeautifulSoup(req.text,"lxml")
+                hours = base.find(class_="schedule__body").ul.text.replace("\n"," ").strip()
+            except:
+                hours = "<INACCESSIBLE>"
 
-        try:
-            req = session.get(detail_url, headers = headers)
-            base = BeautifulSoup(req.text,"lxml")
-            hours = base.find(class_="schedule__body").ul.text.replace("\n"," ").strip()
-        except:
-            hours = "<INACCESSIBLE>"
-
-        output = []
-        output.append(base_url) # url
-        output.append(detail_url) # page_url
-        output.append(title) #location name
-        output.append(street_address) #address
-        output.append(city) #city
-        output.append(state) #state
-        output.append(zipcode) #zipcode
-        output.append("US") #country code
-        output.append("<MISSING>") #store_number
-        output.append(phone) #phone
-        output.append("<MISSING>") #location type
-        output.append(latitude) #latitude
-        output.append(longitude) #longitude
-        output.append(hours) #opening hours
-        output_list.append(output)
+            output = []
+            output.append(base_url) # url
+            output.append(detail_url) # page_url
+            output.append(title) #location name
+            output.append(street_address) #address
+            output.append(city) #city
+            output.append(state) #state
+            output.append(zipcode) #zipcode
+            output.append(country) #country code
+            output.append("<MISSING>") #store_number
+            output.append(phone) #phone
+            output.append("<MISSING>") #location type
+            output.append(latitude) #latitude
+            output.append(longitude) #longitude
+            output.append(hours) #opening hours
+            output_list.append(output)
         
     return output_list
 
