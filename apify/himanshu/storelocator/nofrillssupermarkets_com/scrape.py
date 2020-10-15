b8f8ae29b4d83c29fcf64c78a4ee2759bbd15224
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-10-15 14:00:37 +0500

    Add files via upload

--- apify/himanshu/storelocator/nofrillssupermarkets_com/scrape.py
+++ apify/himanshu/storelocator/nofrillssupermarkets_com/scrape.py
@@ -1,68 +1,81 @@
-import csv
-from sgrequests import SgRequests
 from bs4 import BeautifulSoup
-import re
-import json
+import csv
+import string
+import re, time
 
-
+from sgrequests import SgRequests
 
 session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
 
 def fetch_data():
-    base_url = "http://nofrillssupermarkets.com/store-location/store-detail"
-    r = session.get(base_url)
-    soup= BeautifulSoup(r.text,"lxml")
-    return_main_object =[]
-    store_detail =[]
-    store_name=[]
-    
+    # Your scraper here
+    data = []
+    pattern = re.compile(r'\s\s+')
+    cleanr = re.compile(r'<[^>]+>')
+    url = 'https://nofrillssupermarkets.com/'
+    r = session.get(url, headers=headers, verify=False)   
+    soup =BeautifulSoup(r.text, "html.parser")
+   
+    divlist = soup.find('ul', {'id': "avia-menu"}).findAll('li')[2].find('ul').findAll('a')
+    #print(len(divlist))
+    p = 0
+    for div in divlist:
+        title = div.text
+        link = 'https://nofrillssupermarkets.com/'+div.text.lower().replace(' ','-')
+        #link = div.find('a',{'itemprop':'url'})#['href']
+        #print(link)
+        r = session.get(link, headers=headers, verify=False)
+        soup = BeautifulSoup(r.text,'html.parser')
+        content = soup.find('section',{'class':'av_textblock_section'}).text.splitlines()
+        street = content[1]
+        city, state = content[2].split(', ')
+        state,pcode = state.lstrip().split(' ')
+        phone = content[4]
+        hours = ' '.join(content[6:])
+        lat,longt = soup.find('iframe')['src'].split('!2d',1)[1].split('!2m',1)[0].split('!3d',1)
+        
+        data.append([
+                        ':https://nofrillssupermarkets.com/',
+                        link,                   
+                        title,
+                        street,
+                        city,
+                        state,
+                        pcode,
+                        'US',
+                        '<MISSING>',
+                        phone,
+                        '<MISSING>',
+                        lat,
+                        longt,
+                        hours
+                    ])
+        #print(p,data[p])
+        p += 1
+                
+            
+       
+           
+        
+    return data
 
-    k =  soup.find_all("div",{"class":"location-box"})
 
-    for i in k:
-        tem_var =[]
-        store_no = list(i.stripped_strings)[0].split("#")[-1]
-        name = list(i.stripped_strings)[0].split("#")[0]
-        st = list(i.stripped_strings)[1].split(",")[0]
-        city = list(i.stripped_strings)[1].split(",")[1]
-        state = list(i.stripped_strings)[1].split(",")[2].split( )[0]
-        zip1 = list(i.stripped_strings)[1].split(",")[2].split( )[1]
-        phone = list(i.stripped_strings)[6]
-        time = " ".join(list(i.stripped_strings)[10:20]).replace("Services meat","")
-        tem_var.append("http://nofrillssupermarkets.com")
-        tem_var.append(name)
-        tem_var.append(st)
-        tem_var.append(city)
-        tem_var.append(state)
-        tem_var.append(zip1)
-        tem_var.append("US")
-        tem_var.append(store_no)
-        tem_var.append(phone)
-        tem_var.append("nofrillssupermarkets")
-        tem_var.append("<MISSING>")
-        tem_var.append("<MISSING>")
-        tem_var.append(time)
-        return_main_object.append(tem_var)
-
-
-    return return_main_object
-
-
-def scrape():
+def scrape():   
     data = fetch_data()
     write_output(data)
-
+   
 
 scrape()
