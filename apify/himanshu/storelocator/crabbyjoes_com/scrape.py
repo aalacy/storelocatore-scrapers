1cb5695907da7a6a9e1e84f14a0a9826b1becbcb
diapk <Hank4957!>
2020-10-19 15:44:40 +0530

    done

--- apify/himanshu/storelocator/crabbyjoes_com/scrape.py
+++ apify/himanshu/storelocator/crabbyjoes_com/scrape.py
@@ -3,43 +3,31 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
-
-
-
 session = SgRequests()
-
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
+    with open('data.csv', mode='w', newline="") as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
         # Header
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
-
-
 def fetch_data():
-  
     base_url= "https://www.crabbyjoes.com/locations/"
     r = session.get(base_url)
     soup= BeautifulSoup(r.text,"lxml")
-  
     hours = []
     name_store=[]
     store_detail=[]
     phone=[]
     return_main_object=[]
-    
-
-    
-
     k=(soup.find_all("div",{"id":"locations","class":"locations2"}))
-
     for i in k:
         p =i.find_all("li",{"class":"equal"})
         for j in p:
+            if "9155.29 KM" in list(j.stripped_strings)[1] :
+                continue
             latitude = (j.attrs['onclick'].split('(')[1].split(");")[0].split(',')[0])
             longitude  = (j.attrs['onclick'].split('(')[1].split(");")[0].split(',')[1])
             tem_var=[]
@@ -50,13 +38,10 @@ def fetch_data():
             zipcode =" ".join(list(j.stripped_strings)[3].split(',')[1].split( )[1:])
             phone =list(j.stripped_strings)[7]
             hours = " ".join(list(j.stripped_strings)[10:][:-1])
-         
-     
             if len(zipcode)!=5:
                 coutry = "CA"
             else:
                 coutry = "US"
-
             tem_var.append("https://www.crabbyjoes.com")
             tem_var.append(name)
             tem_var.append(st)
@@ -70,17 +55,13 @@ def fetch_data():
             tem_var.append(latitude)
             tem_var.append(longitude)
             tem_var.append(hours if hours else "<MISSING>")
-            return_main_object.append(tem_var)
-   
-    return return_main_object
-
-
+            tem_var.append("https://www.crabbyjoes.com/locations/")
+            yield tem_var
 def scrape():
     data = fetch_data()
     write_output(data)
-
-
 scrape()
+
 
 
 
