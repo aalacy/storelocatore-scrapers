07157814dca8fc7880e961c986e84d5f634a61bc
diapk <Hank4957!>
2020-10-19 17:36:02 +0530

    done

--- apify/himanshu/storelocator/rockler_com/scrape.py
+++ apify/himanshu/storelocator/rockler_com/scrape.py
@@ -8,7 +8,7 @@ import json
 session = SgRequests()
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
+    with open('data.csv', mode='w', newline='') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
@@ -18,88 +18,89 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
-    base_url ="https://www.rockler.com"
-    return_main_object=[]
-    r = session.get(base_url+'/retail/stores')
-    soup=BeautifulSoup(r.text,'lxml')
+    base_url = "https://www.rockler.com"
+    return_main_object = []
+    soup = BeautifulSoup(session.get(base_url+'/retail/stores').text,'lxml')
     output=[]
-    main=soup.find('ul',{'class':'cms-menu'}).find('ul').find_all('li')
-    for dt in main[:-1]:
+   
+    for dt in soup.find('ul',{'class':'cms-menu'}).find('ul').find_all('li')[:-1]:
         st = dt.find('a')['href']
-        r1 = session.get(st)
-        soup1=BeautifulSoup(r1.text,'lxml')
         
-        main1=soup1.find('ul',{"class":'cms-menu'}).find('ul').find_all('a')
+        soup1 = BeautifulSoup(session.get(st).text,'lxml')
+        
+        main1 = soup1.find('ul',{"class":'cms-menu'}).find('ul').find_all('a')
     
         for dt1 in main1:
             if dt1['href'] == "https://www.rockler.com/retail/stores/wi":
-                citi = "https://www.rockler.com/retail/stores/wi/brookfield-store"
+                page_url = "https://www.rockler.com/retail/stores/wi/brookfield-store"
             else:
-                citi = dt1['href']
+                page_url = dt1['href']
+           
+            soup2 = BeautifulSoup(session.get(page_url).text, 'lxml')
 
-            r2 = session.get(citi)
-            soup2 = BeautifulSoup(r2.text, 'lxml')
-            if citi == "https://www.rockler.com/retail/stores/wi/brookfield-store":
+            if page_url == "https://www.rockler.com/retail/stores/wi/brookfield-store":
                 main2 = soup2.find("div",{"class":"col-m-5"})
+            elif page_url == "https://www.rockler.com/retail/stores/wa/tukwila-store":
+                main2 = soup2.find_all("div",{"class":"col-m-6"})[1]
             else:
                 main2 = soup2.find("div",{"class":"col-m-6"})
 
-            loc=list(main2.stripped_strings)
+            loc = list(main2.stripped_strings)
             if '**New Address**' in loc:
                 loc.remove('**New Address**')
                 loc.remove('Store Location')
-                
-            hour=loc[-6]+' '+loc[-5]+' '+loc[-4]+' '+loc[-3]+' '+loc[-2]+' '+loc[-1]
-            name=loc[0].strip()
-            i=loc.index('Phone:')
+               
+            hours = " ".join(loc[loc.index("Store Hours"):])
+            
+            name = loc[0].strip()
+            i = loc.index('Phone:')
             if i>3:
                 del loc[1]
-            # if len(loc)>14:
-            #     address=loc[2].strip()
-            #     del loc[1]
-            address=loc[1].strip()
-            addr=loc[1].split(',')
-            if len(addr)==2 and i==2:
-                state=addr[-1].strip().split(' ')[0]
-                zip=addr[-1].strip().split(' ')[1]
-                adr=addr[0].strip().split(' ')
-                city=adr[-1].strip()
+
+            address = loc[1].strip()
+            addr = loc[1].split(',')
+            if len(addr) == 2 and i == 2:
+                state = addr[-1].strip().split(' ')[0]
+                zipp = addr[-1].strip().split(' ')[1]
+                adr = addr[0].strip().split(' ')
+                city = adr[-1].strip()
                 del adr[-1]
-                address=' '.join(adr)
+                address = ' '.join(adr)
                 phone = loc[3].strip()
             else:
-                ct=loc[2].split(',')
+                ct = loc[2].split(',')
                 
-                city=ct[0].strip()
-                state=ct[1].strip().split(' ')[0].strip()
-                zip=ct[1].strip().split(' ')[1].strip()
-                phone=loc[4].strip()
+                city = ct[0].strip()
+                state = ct[1].strip().split(' ')[0].strip()
+                zipp = ct[1].strip().split(' ')[1].strip()
+                phone = loc[4].strip()
 
             if "Suite 006-B" in address:
                 address = "2000 Park Manor Blvd" + " " + loc[1]
 
-            storeno=''
-            lat=soup2.find('h3', text="Store Hours").parent.parent.parent.find('iframe')['src'].split('!3d')[1].split('!')[0].strip()
-            lng=soup2.find('h3', text="Store Hours").parent.parent.parent.find('iframe')['src'].split('!2d')[1].split('!')[0].strip()
-            country="US"
-            page_url = citi
-            store=[]
+            storeno = ''
+            lat = soup2.find('h3', text="Store Hours").parent.parent.parent.find('iframe')['src'].split('!3d')[1].split('!')[0].strip()
+            lng = soup2.find('h3', text="Store Hours").parent.parent.parent.find('iframe')['src'].split('!2d')[1].split('!')[0].strip()
+            
+            country = "US"
+           
+            store = []
             store.append(base_url)
             store.append(name if name else "<MISSING>")
             store.append(address if address else "<MISSING>")
             store.append(city if city else "<MISSING>")
             store.append(state if state else "<MISSING>")
-            store.append(zip if zip else "<MISSING>")
+            store.append(zipp if zipp else "<MISSING>")
             store.append(country if country else "<MISSING>")
             store.append(storeno if storeno else "<MISSING>")
             store.append(phone if phone else "<MISSING>")
             store.append("rockler")
             store.append(lat if lat else "<MISSING>")
             store.append(lng if lng else "<MISSING>")
-            store.append(hour if hour.strip() else "<MISSING>")
+            store.append(hours if hours.strip() else "<MISSING>")
             store.append(page_url if page_url else "<MISSING>")
             store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
-            ads=address+' '+city+' '+state+' '+zip
+            ads = address + ' ' + city + ' ' + state + ' ' + zipp
             if ads not in output:
                 output.append(ads)
                 return_main_object.append(store)
@@ -110,3 +111,4 @@ def scrape():
     write_output(data)
 
 scrape()
+
