d00e615bc1e4ba28ae2c64730e33526dc90fa818
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-10-20 23:49:29 +0500

    Add files via upload

--- apify/himanshu/storelocator/kayndaves_com/scrape.py
+++ apify/himanshu/storelocator/kayndaves_com/scrape.py
@@ -1,53 +1,72 @@
-import csv
-from sgrequests import SgRequests
 from bs4 import BeautifulSoup
-import re
-import json
+import csv
+import string
+import re, time
 
+from sgrequests import SgRequests
 
 session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
-    headers = {
-        "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
-    }
-    base_url = "http://kayndaves.com"
-    r = session.get("http://kayndaves.com/locations/",headers=headers)
-    soup = BeautifulSoup(r.text,"lxml")
-    return_main_object = []
-    for location in soup.find_all("div",{'class':'vc_col-sm-6'}):
-        name = location.find("b").text
-        location_details = list(location.find("div",{'id':re.compile("text-block-")}).stripped_strings)
-        geo_location = location.find("iframe")["src"]
-        store = []
-        store.append("http://kayndaves.com")
-        store.append(name)
-        store.append(location_details[0])
-        store.append(location_details[1].split(",")[0])
-        store.append(location_details[1].split(",")[1].split(" ")[-2])
-        store.append(location_details[1].split(",")[1].split(" ")[-1])
-        store.append("US")
-        store.append("<MISSING>")
-        store.append(location_details[2])
-        store.append("<MISSING>")
-        store.append(geo_location.split("!3d")[1].split("!")[0])
-        store.append(geo_location.split("!2d")[1].split("!")[0])
-        store.append(" ".join(location_details[2:]).replace(store[8],""))
-        store.append("http://kayndaves.com/locations/")
-        yield store
+    # Your scraper here
+    data = []
+    pattern = re.compile(r'\s\s+')
+    cleanr = re.compile(r'<[^>]+>')
+    url = 'https://www.kayndaves.com/'
+    r = session.get(url, headers=headers, verify=False)    
+    soup =BeautifulSoup(r.text, "html.parser")
+   
+    divlist = soup.find('div', {'id': "comp-kczp38o9"}).findAll('div',{'data-testid':'richTextElement'})
+    
+   # print("states = ",len(state_list))
+    p = 0
+    for div in divlist:
+        content = div.text.strip().splitlines()
+        title = content[0]
+        street = div.findAll('a')[0].text
+        phone = div.findAll('a')[1].text.strip()
+        hours = content[4]        
+        lat,longt = div.find('a')['href'].split('@',1)[1].split('data',1)[0].split(',',1)
+        longt = longt.split(',',1)[0]
+        data.append([
+                        'https://www.kayndaves.com/',
+                        'https://www.kayndaves.com/',                   
+                        title,
+                        street,
+                        title,
+                        'CA',
+                        '<MISSING>',
+                        'US',
+                        '<MISSING>',
+                        phone,
+                        '<MISSING>',
+                        lat,
+                        longt,
+                        hours
+                    ])
+        #print(p,data[p])
+        p += 1
+    
+    return data
+
 
 def scrape():
+   
     data = fetch_data()
     write_output(data)
+  
 
 scrape()
