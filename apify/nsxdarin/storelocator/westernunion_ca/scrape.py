dc95cc6184b4bc2ddfef262bd5dc4147071d4ea9
nsxdarin <nsxdarin@gmail.com>
2020-10-22 10:45:38 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/westernunion_ca/scrape.py
+++ apify/nsxdarin/storelocator/westernunion_ca/scrape.py
@@ -17,7 +17,7 @@ def write_output(data):
 
 def fetch_data():
     locs = []
-    for x in range(1, 15):
+    for x in range(1, 5):
         print(('Pulling Sitemap %s...' % str(x)))
         smurl = 'http://locations.westernunion.com/sitemap-' + str(x) + '.xml.gz'
         with open('branches.xml.gz','wb') as f:
@@ -27,10 +27,10 @@ def fetch_data():
                 for line in f:
                     if '<loc>http://locations.westernunion.com/ca/' in line:
                         locs.append(line.split('<loc>')[1].split('<')[0])
-        print((str(len(locs)) + ' Locations Found...'))
+        #print((str(len(locs)) + ' Locations Found...'))
     for loc in locs:
         website = 'westernunion.ca'
-        typ = 'Location'
+        typ = '<MISSING>'
         store = '<MISSING>'
         hours = '<MISSING>'
         city = ''
@@ -50,33 +50,26 @@ def fetch_data():
         lines = r.iter_lines(decode_unicode=True)
         AFound = False
         for line in lines:
-            if '<h1 class="offscreen">' in line:
-                name = line.split('<h1 class="offscreen">')[1].split('<')[0]
+            if '<h1 class="wu_LocationCard' in line:
+                name = line.split('<h1 class="wu_LocationCard')[1].split('">')[1].split('<')[0]
                 if 'Western Union Agent Location' in name:
                     name = name.replace(name[:32], '')
                     name = name.strip()
-            if 'itemprop="streetAddress">' in line and AFound is False:
+            if '"streetAddress":"' in line and AFound is False:
                 AFound = True
-                add = line.split('itemprop="streetAddress">')[1].split('<')[0]
-            if 'itemprop="addressLocality">' in line:
-                city = line.split('itemprop="addressLocality">')[1].split('<')[0]
-            if 'itemprop="addressRegion">' in line:
-                state = line.split('itemprop="addressRegion">')[1].split('<')[0]
-            if 'itemprop="postalCode">' in line:
-                zc = line.split('itemprop="postalCode">')[1].split('<')[0]
-            if 'aria-label="Telephone">' in line:
-                phone = line.split('aria-label="Telephone">')[1].split('<')[0]
-            if 'itemprop="latitude" content="' in line:
-                lat = line.split('itemprop="latitude" content="')[1].split('"')[0]
-            if 'itemprop="longitude" content="' in line:
-                lng = line.split('itemprop="longitude" content="')[1].split('"')[0]
-            if '"id":"' in line:
-                store = line.split('"id":"')[1].split('"')[0]
-            if '<meta itemprop="openingHours" content="' in line:
-                if hours == '<MISSING>':
-                    hours = line.split('<meta itemprop="openingHours" content="')[1].split('"')[0]
-                else:
-                    hours = hours + '; ' + line.split('<meta itemprop="openingHours" content="')[1].split('"')[0]
+                add = line.split('"streetAddress":"')[1].split('"')[0]
+                state = line.split('"state":"')[1].split('"')[0]
+                city = line.split('"city":"')[1].split('"')[0]
+                zc = line.split('"postal":"')[1].split('"')[0]
+                lat = line.split('"latitude":')[1].split(',')[0]
+                lng = line.split('"longitude":')[1].split(',')[0]
+                if phone == '':
+                    phone = line.split('","phone":"')[1].split('"')[0]
+                if store == '<MISSING>':
+                    store = line.split('"id":"')[1].split('"')[0]
+            if '"desktopHours":{"desktopHours":{' in line:
+                hours = line.split('"desktopHours":{"desktopHours":{')[1].split('}}')[0]
+                hours = hours.replace('","','; ').replace('"','')
         yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
