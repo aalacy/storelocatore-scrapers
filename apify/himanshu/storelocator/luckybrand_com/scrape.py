e845b28b67b037b56c32c945cf3281cf20eeeb89
diapk <Hank4957!>
2020-10-22 11:18:43 +0530

    done

--- apify/himanshu/storelocator/luckybrand_com/scrape.py
+++ apify/himanshu/storelocator/luckybrand_com/scrape.py
@@ -2,11 +2,7 @@ import csv
 from bs4 import BeautifulSoup
 import re
 import json
-import time
 import sgzip
-from random import randint
-from time import sleep
-from datetime import datetime
 from sgrequests import SgRequests
 
 def write_output(data):
@@ -18,7 +14,6 @@ def write_output(data):
         # Body
         for row in data:
             writer.writerow(row)
-
 def get_hours(raw_hours):
     hours = []
     for day in raw_hours.keys():
@@ -27,7 +22,6 @@ def get_hours(raw_hours):
             end = raw_hours[day]['openIntervals'][0]['end']
             hours.append('{}: {}-{}'.format(day, start, end))
     return ', '.join(hours)
-
 def fetch_data():
     addresses = []
     search = sgzip.ClosestNSearch()
@@ -35,14 +29,12 @@ def fetch_data():
     MAX_RESULTS = 50
     MAX_DISTANCE = 500
     coord = search.next_coord()
-
     base_url = "https://www.luckybrand.com"
     coord = search.next_coord()
     while coord:
         result_coords = []
         lat = coord[0]
         lng = coord[1]
-        # print(lat,lng)
         session = SgRequests()
         json_data = session.get("https://liveapi.yext.com/v2/accounts/me/entities/geosearch?api_key=0700165de62eb1a445df7d02b02c7831&v=20181017&location="+str(lat)+",%20"+str(lng)+"&limit=50&radius=500&entityTypes=location,healthcareProfessional,restaurant,healthcareFacility,atm&fields=name,hours,address,websiteUrl&resolvePlaceholders=true&searchIds=").json()['response']['entities']
         current_result_len = len(json_data)
@@ -52,7 +44,10 @@ def fetch_data():
             country_code = data['address']['countryCode']
             if country_code not in ['US','CA']:
                 continue
-            page_url = data['websiteUrl']['url']
+            try:
+                page_url = data['websiteUrl']['url']
+            except KeyError:
+                continue
             street_address = data['address']['line1']
             if 'line2' in data['address']:
                 street_address += " "+ str(data['address']['line2']).strip()
@@ -72,7 +67,7 @@ def fetch_data():
             location_name = '<MISSING>'
             if page_url != base_url:
                 r = session.get(page_url)
-                soup = BeautifulSoup(r.text, "lxml")
+                soup = BeautifulSoup(r.text, "html5lib")
                 location_name = soup.find("title").text
                 info = json.loads(soup.find(lambda tag:(tag.name == "script") and "latitude" in tag.text).text)
                 latitude = info['geo']['latitude']
@@ -81,7 +76,9 @@ def fetch_data():
                 phone = info['telephone']
                 if hours == '<MISSING>' or not hours:
                     hours = " ".join(list(soup.find(class_="hours-body").stripped_strings))
-
+            if "Closed" in location_name:
+                continue
+            
             store = []
             store.append(base_url)            
             store.append(location_name)
@@ -102,15 +99,12 @@ def fetch_data():
                 continue
             addresses.append(store[2])
             yield store
-           
         if current_result_len == 0:
             search.max_distance_update(MAX_DISTANCE)
         else:
             search.max_count_update(result_coords)
         coord = search.next_coord()
-
 def scrape():
     data = fetch_data()
     write_output(data)
-
 scrape()
