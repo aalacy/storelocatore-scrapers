68a1a99355f75010881ff9f868a402fef51305ff
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-10-25 13:15:24 +0500

    Add files via upload

--- apify/Sammeth0/storelocator/zoomcare_com/scrape.py
+++ apify/Sammeth0/storelocator/zoomcare_com/scrape.py
@@ -1,110 +1,93 @@
-from sgrequests import SgRequests
 from bs4 import BeautifulSoup
-from lxml import html
 import csv
-import re
+import string
+import re, time
+
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
-    with open('data.csv', mode='w', encoding='utf-8') as output_file:
+    with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
-
-        locs = []
-        streets = []
-        states=[]
-        cities = []
-        types=[]
-        phones = []
-        zips = []
-        longs = []
-        lats = []
-        timing = []
-        ids=[]
-        pages=[]
+    # Your scraper here
+    data = []
+    pattern = re.compile(r'\s\s+')
+    cleanr = re.compile(r'<[^>]+>')
+    url = 'https://www.zoomcare.com/about'
+    r = session.get(url, headers=headers, verify=False)
+    linklist = []
+    soup =BeautifulSoup(r.text, "html.parser")  
+    divlist = r.text.split('\\u002Fclinic\\u002Fzoomcare')   
+    p = 0
+    for div in divlist:
+        link = 'https://www.zoomcare.com/clinic/zoomcare'+div.split('"')[0]  
         
-        locator_domain ="https://www.zoomcare.com"
-        country_code="US"
-
-        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
-        HEADERS = {'User-Agent' : user_agent}
-
-        session = SgRequests()
-                
-        r = session.get("https://www.zoomcare.com/about")
-        soup=str(BeautifulSoup(r.text,'lxml'))
-        soup = soup.replace("\\u002F","/")
-
-        links = re.findall(r'/clinic/.*?"', soup)
-        for link in links:
-            page_locator = locator_domain + link[:-1]
-            print(page_locator)
-            r_locator=session.get(page_locator)
-            soup_locator=BeautifulSoup(r_locator.text,'lxml')
-            if "Goodbye" in soup_locator.h1.text:
-                    continue
-            if "We're coming" in soup_locator.h1.text:
-                    continue
-            pages.append(page_locator)
+        if link.find('clinic/zoomcare') > -1 and link.find('html') == -1:           
+            if link in linklist:
+                continue
+            linklist.append(link)
+            #print(link)
+            r = session.get(link , headers=headers, verify=False)
+            soup = BeautifulSoup(r.text,'html.parser')
+            title = soup.find('title').text
             try:
-                    locs.append(soup_locator.find(class_="header-3").text)
+                title = title.split(' |')[0]
             except:
-                    locs.append(soup_locator.title.text.split("|")[0].strip())
-            streets.append(soup_locator.find(class_="clinic-address").contents[0].text)
-            cities.append(soup_locator.find(class_="clinic-address").contents[2].text.split(',')[0])
-            states.append(soup_locator.find(class_="clinic-address").contents[2].text.split(', ')[1])
+                pass
+            address = soup.find('div',{'class':'clinic-address'}).findAll('div')
+            street = address[0].text
+            city, state = address[1].text.split(', ')
             try:
-                    zips.append(soup_locator.find(class_="map-info-column").a.get('href').split('+')[-1])
+                state,pcode = state.lstrip().spliy(' ',1)
             except:
-                    try:
-                            zips.append(str(soup_locator.findAll("script")[-9]).split('postalCode="')[1].split('"')[0])
-                    except:
-                            try:
-                                    map_link = soup_locator.find('iframe')['src']
-                                    zip_code = re.findall(r'\+[0-9]{5}', map_link)[0][1:]
-                                    zips.append(zip_code)
-                            except:
-                                    zips.append("<MISSING>")
-            types_list=soup_locator.findAll(class_="col-8")
-            list=[]
-            for type in types_list:
-                    list.append(type.h2.text)
-            types.append(', '.join(list))
-            lats.append(str(soup_locator.findAll("script")[-9]).split('latitude="')[1].split('"')[0])
-            longs.append(str(soup_locator.findAll("script")[-9]).split('longitude="')[1].split('"')[0])
+                pcode = '<MISSING>'
+            hours = soup.find('div',{'class':'Clinic__body__hours'}).text
             try:
-                    timing.append(soup_locator.find(class_="Clinic__body__hours").text.strip())
+                coord = soup.find('iframe',{'class':'Clinic__body__maps-iframe'})['src']
+                lat,longt = coord.split('sll=',1)[1].split('&',1)[0].split(',')
             except:
-                    timing.append("<MISSING>")
+                lat = longt = '<MISSING>'
+            phone = '<MISSING>'
+            data.append([
+                        'https://www.zoomcare.com',
+                        link,                   
+                        title,
+                        street,
+                        city,
+                        state,
+                        pcode,
+                        'US',
+                        '<MISSING>',
+                        phone,
+                        '<MISSING>',
+                        lat,
+                        longt,
+                        hours.replace('\n','').strip()
+                    ])
+            #print(p,data[p])
+            p += 1
                 
-        return_main_object=[]
-        for i in range(len(locs)):
-                row = []
-                row.append(locator_domain)
-                row.append(locs[i] if locs[i] else "<MISSING>")
-                row.append(streets[i] if streets[i] else "<MISSING>")
-                row.append(cities[i] if cities[i] else "<MISSING>")
-                row.append(states[i] if states[i] else "<MISSING>")
-                row.append(zips[i] if zips[i] else "<MISSING>")
-                row.append(country_code)
-                row.append("<MISSING>")
-                row.append("<MISSING>")
-                row.append(types[i] if types[i] else "<MISSING>")
-                row.append(lats[i] if lats[i] else "<MISSING>")
-                row.append(longs[i] if longs[i] else "<MISSING>")
-                row.append(timing[i] if timing[i] else "<MISSING>")
-                row.append(pages[i] if pages[i] else "<MISSING>")
-                
-                return_main_object.append(row)
-        return return_main_object
+      
+           
+        
+    return data
+
 
 def scrape():
+  
     data = fetch_data()
     write_output(data)
+    
 scrape()
