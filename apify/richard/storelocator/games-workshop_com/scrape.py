9b86ac7ff01f3479361c139d73e86494dec34734
Quincy <qcalexander@gmail.com>
2020-10-26 19:00:56 -0400

    games-workshop_com fixed

--- apify/richard/storelocator/games-workshop_com/scrape.py
+++ apify/richard/storelocator/games-workshop_com/scrape.py
@@ -3,183 +3,202 @@ from bs4 import BeautifulSoup
 import time
 import json
 import re
+import csv
 
-from Scraper import Scrape
-
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-
-# def get_driver():
-#     options = Options() 
-#     options.add_argument('--headless')
-#     options.add_argument('--no-sandbox')
-#     options.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36")
-#     options.add_argument('--disable-dev-shm-usage')
-#     options.add_argument('--window-size=1920,1080')
-#     return webdriver.Chrome('chromedriver', chrome_options=options)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 URL = "https://www.games-workshop.com/"
 
 
-class Scraper(Scrape):
-    def __init__(self, url):
-        Scrape.__init__(self, url)
-        self.data = []
+def write_output(data):
+    with open('data.csv', mode='w', encoding="utf-8") as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-    def fetch_data(self):
-        # store data
-        locations_ids = []
-        locations_titles = []
-        street_addresses = []
-        cities = []
-        store_type_list = []
-        states = []
-        zip_codes = []
-        latitude_list = []
-        longitude_list = []
-        phone_numbers = []
-        hours = []
-        countries = []
-        stores = []
-        links = []
-        seen = []
+        # Header
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
 
-        driver = SgSelenium().chrome()
-        # driver = get_driver()
+
+def fetch_data():
+
+    # store data
+    locations_ids = []
+    locations_titles = []
+    street_addresses = []
+    cities = []
+    store_type_list = []
+    states = []
+    zip_codes = []
+    latitude_list = []
+    longitude_list = []
+    phone_numbers = []
+    hours = []
+    countries = []
+    stores = []
+    links = []
+
+    data = []
+    seen = []
+
+    base_link = "https://www.games-workshop.com/en-US/store/fragments/resultsJSON.jsp?latitude=40.2475923&radius=20000&longitude=-77.03341790000002"
+
+    user_agents = ["Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
+                   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36",
+                   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"]
+
+    for user_agent in user_agents:
+        driver = SgSelenium().chrome(user_agent=user_agent)
         time.sleep(2)
 
-        base_link = "https://www.games-workshop.com/en-US/store/fragments/resultsJSON.jsp?latitude=40.2475923&radius=20000&longitude=-77.03341790000002"
         driver.get(base_link)
-        time.sleep(15)
+
+        element = WebDriverWait(driver, 30).until(EC.presence_of_element_located(
+            (By.TAG_NAME, "body")))
+        time.sleep(5)
 
         base = BeautifulSoup(driver.page_source,"lxml")
-        stores = json.loads(base.text)['locations']
 
-        for store in stores:
-            if store['type'] == 'independentRetailer':
-                continue
+        try:
+            stores = json.loads(base.text)['locations']
+            break
+        except:
+            continue
 
-            # Country
-            country = store['country'] if 'country' in store.keys() else '<MISSING>'
-            if country not in ["CA","US"]:
-                continue
+    for store in stores:
+        if store['type'] == 'independentRetailer':
+            continue
 
-            # Store ID
-            location_id = store['storeId'] if 'storeId' in store.keys() else store['id'].split("-")[-1]
+        # Country
+        country = store['country'] if 'country' in store.keys() else '<MISSING>'
+        if country not in ["CA","US"]:
+            continue
 
-            # Name
-            location_title = store["name"].encode("ascii", "replace").decode().replace("?","-") if 'name' in store.keys() else '<MISSING>'
+        # Store ID
+        location_id = store['storeId'] if 'storeId' in store.keys() else store['id'].split("-")[-1]
 
-            # Street
-            try:
-                street_address = store['address1'] + " " + store['address2']
-            except:
-                street_address = store['address1']
+        # Name
+        location_title = store["name"].encode("ascii", "replace").decode().replace("?","-") if 'name' in store.keys() else '<MISSING>'
 
-            street_address = (re.sub(' +', ' ', street_address)).strip()
-            digit = re.search("\d", street_address).start(0)
-            if digit != 0:
-                street_address = street_address[digit:]
+        # Street
+        try:
+            street_address = store['address1'] + " " + store['address2']
+        except:
+            street_address = store['address1']
 
-            # State
-            state = store['state'] if 'state' in store.keys() else '<MISSING>'
+        street_address = (re.sub(' +', ' ', street_address)).strip()
+        digit = re.search("\d", street_address).start(0)
+        if digit != 0:
+            street_address = street_address[digit:]
 
-            # city
-            city = store['city'] if 'city' in store.keys() else '<MISSING>'
+        # State
+        state = store['state'] if 'state' in store.keys() else '<MISSING>'
 
-            # zip
-            zipcode = store['postalCode'].replace(" -","-").replace("L24R 3N1","L2R 3N1") if 'postalCode' in store.keys() else '<MISSING>'
-            if len(zipcode) == 4:
-                zipcode = "0" + zipcode
+        # city
+        city = store['city'] if 'city' in store.keys() else '<MISSING>'
 
-            # store type
-            store_type = store['type'] if 'type' in store.keys() else '<MISSING>'
+        # zip
+        zipcode = store['postalCode'].replace(" -","-").replace("L24R 3N1","L2R 3N1") if 'postalCode' in store.keys() else '<MISSING>'
+        if len(zipcode) == 4:
+            zipcode = "0" + zipcode
 
-            # Lat
-            lat = store['latitude'] if 'latitude' in store.keys() else '<MISSING>'
+        # store type
+        # store_type = store['type'] if 'type' in store.keys() else '<MISSING>'
+        store_type = '<MISSING>'
 
-            # Long
-            lon = store['longitude'] if 'longitude' in store.keys() else '<MISSING>'
-            if lon == -76593137.0:
-                lon = -76.593137
+        # Lat
+        lat = store['latitude'] if 'latitude' in store.keys() else '<MISSING>'
 
-            # Phone
-            phone = store['telephone'] if 'telephone' in store.keys() else '<MISSING>'
-            if phone[:2] == "00":
-                phone = phone[2:]
-            if len(phone) < 8:
-                phone = '<MISSING>'
-            # hour
-            hour = '<MISSING>'
+        # Long
+        lon = store['longitude'] if 'longitude' in store.keys() else '<MISSING>'
+        if lon == -76593137.0:
+            lon = -76.593137
 
-            link = "https://www.games-workshop.com/en-" + country + "/" + store['seoUrl']
+        # Phone
+        phone = store['telephone'] if 'telephone' in store.keys() else '<MISSING>'
+        if phone[:2] == "00":
+            phone = phone[2:]
+        if len(phone) < 8:
+            phone = '<MISSING>'
+        # hour
+        hour = '<MISSING>'
 
-            # Store data
-            locations_ids.append(location_id)
-            store_type_list.append(store_type)
-            locations_titles.append(location_title)
-            street_addresses.append(street_address)
-            states.append(state)
-            zip_codes.append(zipcode)
-            hours.append(hour)
-            latitude_list.append(lat)
-            longitude_list.append(lon)
-            phone_numbers.append(phone)
-            cities.append(city)
-            countries.append(country)
-            links.append(link)
+        link = "https://www.games-workshop.com/en-" + country + "/" + store['seoUrl']
 
-        for (   
-                link,
-                locations_title,
-                street_address,
-                city,
-                state,
-                zipcode,
-                phone_number,
-                latitude,
-                longitude,
-                hour,
-                location_id,
-                country,
-                store_type
-        ) in zip(
-            links,
-            locations_titles,
-            street_addresses,
-            cities,
-            states,
-            zip_codes,
-            phone_numbers,
-            latitude_list,
-            longitude_list,
-            hours,
-            locations_ids,
-            countries,
-            store_type_list
-        ):
-            if location_id not in seen:
-                self.data.append(
-                    [
-                        self.url,
-                        link,
-                        locations_title,
-                        street_address,
-                        city,
-                        state,
-                        zipcode,
-                        country,
-                        location_id,
-                        phone_number,
-                        store_type,
-                        latitude,
-                        longitude,
-                        hour,
-                    ]
-                )
-                seen.append(location_id)
+        # Store data
+        locations_ids.append(location_id)
+        store_type_list.append(store_type)
+        locations_titles.append(location_title)
+        street_addresses.append(street_address)
+        states.append(state)
+        zip_codes.append(zipcode)
+        hours.append(hour)
+        latitude_list.append(lat)
+        longitude_list.append(lon)
+        phone_numbers.append(phone)
+        cities.append(city)
+        countries.append(country)
+        links.append(link)
 
+    for (   
+            link,
+            locations_title,
+            street_address,
+            city,
+            state,
+            zipcode,
+            phone_number,
+            latitude,
+            longitude,
+            hour,
+            location_id,
+            country,
+            store_type
+    ) in zip(
+        links,
+        locations_titles,
+        street_addresses,
+        cities,
+        states,
+        zip_codes,
+        phone_numbers,
+        latitude_list,
+        longitude_list,
+        hours,
+        locations_ids,
+        countries,
+        store_type_list
+    ):
+        if location_id not in seen:
+            data.append(
+                [
+                    URL,
+                    link,
+                    locations_title,
+                    street_address,
+                    city,
+                    state,
+                    zipcode,
+                    country,
+                    location_id,
+                    phone_number,
+                    store_type,
+                    latitude,
+                    longitude,
+                    hour,
+                ]
+            )
+            seen.append(location_id)
 
+    driver.close()
+    return data
 
-scrape = Scraper(URL)
-scrape.scrape()
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+scrape()
