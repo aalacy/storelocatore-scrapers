1207d2d5bb1b6c0670de73dd1955ede0c5e6f8f8
Quincy <qcalexander@gmail.com>
2020-10-31 02:49:45 -0400

    mazdausa_com first commit

---
+++ apify/quincya/storelocator/mazdausa_com/scrape.py
@@ -0,0 +1,95 @@
+from sgrequests import SgRequests
+import csv
+import sgzip
+
+
+def write_output(data):
+	with open('data.csv', mode='w', encoding="utf-8") as output_file:
+		writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+		# Header
+		writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+		# Body
+		for row in data:
+			writer.writerow(row)
+
+def fetch_data():
+
+	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+	HEADERS = {'User-Agent' : user_agent}
+	session = SgRequests()
+
+	locator_domain = "mazdausa.com"
+
+	data = []
+	found_poi = []
+
+	search = sgzip.ClosestNSearch()
+	# Initialize the search for the US only
+	search.initialize(country_codes = ['us'])
+
+	coord = search.next_zip()
+
+	while coord:
+		
+		result_coords = []
+
+		base_link = "https://www.mazdausa.com/handlers/dealer.ajax?zip=" + coord + "&maxDistance=300"
+
+		stores = session.get(base_link,headers=HEADERS).json()["body"]["results"]
+
+		for store in stores:
+
+			id_ = store['id']
+			if id_ not in found_poi:
+				found_poi.append(id_)
+			else:
+				continue
+
+			location_name = store['name']
+			try:
+				street_address = store['address1'] + " " + store['address2']
+			except:
+				street_address = store['address1']
+
+			city = store['city']
+			state = store['state']
+			zip_code = store['zip']
+
+			if street_address == "ROUTE 30 EAST":
+				street_address = "5110 ROUTE 30 EAST"
+			if street_address == "U.S. 460 BUSINESS":
+				street_address = "125 Jennelle Rd".upper()
+				city = "Christiansburg".upper()
+				zip_code = "24073"
+
+			country_code = "US"
+			store_number = store['id']
+			location_type = "<MISSING>"
+			phone = store['dayPhone']
+
+			hours_of_operation = "<MISSING>"
+
+			latitude = store['lat']
+			longitude = store['long']
+			result_coords.append((latitude, longitude))
+
+			link = store["webUrl"]
+			if not link:
+				link = "<MISSING>"
+
+			data.append([locator_domain, link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+
+		if len(result_coords) > 0:
+			search.max_count_update(result_coords)
+		else:
+			search.max_distance_update(100)
+		coord = search.next_zip()
+
+	return data
+
+def scrape():
+	data = fetch_data()
+	write_output(data)
+
+scrape()
