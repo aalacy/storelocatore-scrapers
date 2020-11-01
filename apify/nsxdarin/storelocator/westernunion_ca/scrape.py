f0535b2a812b240c3d4615d1957adea5c31fcc2d
Kevin Collins <nck@users.noreply.github.com>
2020-11-01 15:51:04 -0500

    westernunion.ca (SLC-4491): reset session after request exception

--- apify/nsxdarin/storelocator/westernunion_ca/scrape.py
+++ apify/nsxdarin/storelocator/westernunion_ca/scrape.py
@@ -7,18 +7,46 @@ from sglogging import SgLogSetup
 
 logger = SgLogSetup().get_logger('westernunion_ca')
 
+def override_retries():
+    # monkey patch sgrequests in order to set max retries ...
+    # we will control retries in this script in order to reset the session and get a new IP each time
+    import requests  # ignore_check
+    def new_init(self):
+        requests.packages.urllib3.disable_warnings()
+        self.session = self.requests_retry_session(retries=0)
+    SgRequests.__init__ = new_init
 
 
+override_retries()
 session = SgRequests()
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
-           }
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         for row in data:
             writer.writerow(row)
+
+
+def get(url, headers, attempts=0):
+    global session
+
+    max_attempts = 10
+    if attempts > max_attempts:
+        print(f"Exceeded max attempts ({max_attempts}) for URL: {url}")
+
+    try:
+        r = session.get(url, headers=headers)
+        logger.info(f"Status {r.status_code} for URL: {url}")
+        r.raise_for_status()
+        return r
+    except Exception as ex:
+        logger.info(f"---  Resetting session after exception --> {ex} ")
+        session = SgRequests()
+        return get(url, headers, attempts + 1)
+
 
 def fetch_data():
     locs = []
@@ -35,6 +63,7 @@ def fetch_data():
         logger.info((str(len(locs)) + ' Locations Found...'))
     for loc in locs:
         logger.info(loc)
+        page_url = loc
         website = 'westernunion.ca'
         typ = '<MISSING>'
         store = '<MISSING>'
@@ -51,7 +80,7 @@ def fetch_data():
         phone = ''
         lat = ''
         lng = ''
-        r = session.get(loc, headers=headers)
+        r = get(loc, headers=headers)
         if r.encoding is None: r.encoding = 'utf-8'
         lines = r.iter_lines(decode_unicode=True)
         AFound = False
@@ -80,7 +109,7 @@ def fetch_data():
             hours = '<MISSING>'
         if phone == '':
             phone = '<MISSING>'
-        yield [website, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
+        yield [website, page_url, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
 
 def scrape():
     data = fetch_data()
