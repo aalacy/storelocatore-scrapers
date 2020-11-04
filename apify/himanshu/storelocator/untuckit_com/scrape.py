ef82227e4e225ffc8051ceca5935af2f1bb0ab47
Himanshu347 <hdhameliya@dhameliyaconsultingservice.com>
2020-11-04 12:38:54 +0530

    done

--- apify/himanshu/storelocator/untuckit_com/scrape.py
+++ apify/himanshu/storelocator/untuckit_com/scrape.py
@@ -12,7 +12,7 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
@@ -28,8 +28,15 @@ def fetch_data():
         store.append("https://www.untuckit.com")
         store.append(store_data['name'])
         location_request = session.get(store_data["url"])
+        # print(store_data["url"])
         location_soup = BeautifulSoup(location_request.text,"lxml")
         address = list(location_soup.find("p",{"class":"store_info--copy"}).stripped_strings)
+        if "Monday" in address:
+            continue
+        if "london-covent-garden" in store_data["url"] or "boca-raton" in store_data["url"] or "london" in store_data["url"]:
+            continue
+        # print(address)
+        # print(address[-1])
         ca_zip_split = re.findall(r'[A-Z]{1}[0-9]{1}[A-Z]{1}\s*[0-9]{1}[A-Z]{1}[0-9]{1}',address[-1])
         if ca_zip_split:
             ca_zip = ca_zip_split[-1]
@@ -40,7 +47,7 @@ def fetch_data():
         state_split = re.findall(r'[A-Z]{2}',address[-1])
         if state_split:
             state = state_split[-1]
-        store.append(address[0])
+        store.append(address[0].replace("International Plaza Mall",'').replace("Copley Place",'').replace(",",' ').strip())
         store.append(address[-1].replace(state,"").replace(ca_zip,"").replace(us_zip,"").replace(",",""))
         store.append(state)
         store.append(ca_zip if ca_zip else us_zip)
@@ -50,12 +57,19 @@ def fetch_data():
         store.append("untuck it " + store_data["category"])
         store.append(store_data['latitude'])
         store.append(store_data['longitude'])
-        store.append(store_data["description"].replace("\n"," ") if store_data["description"] != "" else "<MISSING>")
-        return_main_object.append(store)
-    return return_main_object
+        hours_of_operation=''
+        h = BeautifulSoup(store_data["description"],'lxml')
+        hours_of_operation =  (" ".join(list(h.stripped_strings)).replace("Open. Contactless ",''))
+        store.append(hours_of_operation if hours_of_operation != "" else "<MISSING>")
+        store.append(store_data["url"])
+        yield store
+
+    #     return_main_object.append(store)
+    # return return_main_object
 
 def scrape():
     data = fetch_data()
     write_output(data)
 
 scrape()
+
