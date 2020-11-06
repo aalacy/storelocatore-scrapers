1963ea1cba3a85283ee790c2fd4e08ca3debd906
Michael Dinh <mkdinh94@gmail.com>
2020-11-06 17:09:54 -0500

    SLC-3961/chevrolet_com: rewrite to retrieve data as json

--- apify/nsxdarin/storelocator/chevrolet_com/scrape.py
+++ apify/nsxdarin/storelocator/chevrolet_com/scrape.py
@@ -1,105 +1,170 @@
 import csv
-import sgzip
+from sgzip import DynamicGeoSearch, SearchableCountries
 from sgrequests import SgRequests
 from sglogging import SgLogSetup
+from datetime import datetime
 
-logger = SgLogSetup().get_logger('chevrolet_com')
-
-
-
-search = sgzip.ClosestNSearch()
-search.initialize()
+logger = SgLogSetup().get_logger("chevrolet_com")
 
 session = SgRequests()
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-           'authority': 'www.chevrolet.ca',
-           'accept': 'application/json, text/plain, */*',
-           'clientapplicationid': 'OCNATIVEAPP',
-           'loginin': 'mytest016@outlook.com',
-           'locale': 'en_US'
-           }
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+    "authority": "www.chevrolet.com",
+    "accept": "application/json, text/plain, */*",
+    "clientapplicationid": "OCNATIVEAPP",
+    "loginin": "mytest016@outlook.com",
+    "locale": "en_US",
+}
 
-MAX_RESULTS = 50
-MAX_DISTANCE = 10
+fields = [
+    "locator_domain",
+    "page_url",
+    "location_name",
+    "street_address",
+    "city",
+    "state",
+    "zip",
+    "country_code",
+    "store_number",
+    "phone",
+    "location_type",
+    "latitude",
+    "longitude",
+    "hours_of_operation",
+]
+
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+    with open("data.csv", mode="w") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
+        writer.writerow(fields)
         for row in data:
             writer.writerow(row)
 
+
+MISSING = "<MISSING>"
+
+
+def get(location, key):
+    return location.get(key, MISSING) or MISSING
+
+
+days = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday",
+}
+
+
+def get_hours(location):
+    hours = location.get("generalOpeningHour")
+    if not hours:
+        return MISSING
+
+    hours_of_operation = []
+
+    for hour in hours:
+        for day in hour.get("dayOfWeek"):
+            hours_of_operation.append(
+                f"{days[day]} {hour.get('openFrom')}-{hour.get('openTo')}"
+            )
+
+    return ",".join(hours_of_operation)
+
+
+def extract(location):
+    locator_domain = "chevrolet.com"
+    page_url = get(location, "dealerUrl")
+    store_number = get(location, "id")
+    location_name = get(location, "dealerName")
+    location_type = MISSING
+
+    contact = location.get("generalContact")
+    phone = get(contact, "phone1")
+
+    address = location.get("address")
+    street_address = get(address, "addressLine1")
+    city = get(address, "cityName")
+    state = get(address, "countrySubdivisionCode")
+    postal = get(address, "postalCode")
+    country_code = get(address, "countryIso")
+
+    geolocation = location.get("geolocation")
+    latitude = get(geolocation, "latitude")
+    longitude = get(geolocation, "longitude")
+
+    hours_of_operation = get_hours(location)
+
+    return {
+        "locator_domain": locator_domain,
+        "page_url": page_url,
+        "store_number": store_number,
+        "location_name": location_name,
+        "location_type": location_type,
+        "phone": phone,
+        "street_address": street_address,
+        "city": city,
+        "state": state,
+        "zip": postal,
+        "country_code": country_code,
+        "latitude": latitude,
+        "longitude": longitude,
+        "hours_of_operation": hours_of_operation,
+    }
+
+
 def fetch_data():
-    sids = []
-    ids = []
-    locations = []
-    coord = search.next_coord()
+    MAX_DISTANCE = 50
+    MAX_RESULTS = 50
+
+    search = DynamicGeoSearch(
+        country_codes=[SearchableCountries.USA],
+        max_radius_miles=MAX_DISTANCE,
+        max_search_results=MAX_RESULTS,
+    )
+    search.initialize()
+
+    locations = {}
+    coord = search.next()
+
     while coord:
-        llat = coord[0]
-        llng = coord[1]
-        logger.info('%s-%s...' % (llat, llng))
-        url = 'https://www.chevrolet.com/OCRestServices/dealer/latlong/v1/chevrolet/' + str(llat) + '/' + str(llng) + '/?distance=500&maxResults=50'
-        r = session.get(url, headers=headers)
-        result_coords = []
-        array = []
-        website = 'chevrolet.com'
-        for line in r.iter_lines():
-            line = str(line.decode('utf-8'))
-            if '{"id":' in line:
-                items = line.split('{"id":')
-                for item in items:
-                    if '"dealerName":"' in item:
-                        try:
-                            name = item.split('"dealerName":"')[1].split('"')[0]
-                            store = item.split(',')[0]
-                            typ = '<MISSING>'
-                            try:
-                                loc = item.split('"dealerUrl":"')[1].split('"')[0]
-                            except:
-                                loc = '<MISSING>'
-                            phone = item.split('"phone1":"')[1].split('"')[0]
-                            add = item.split('{"addressLine1":"')[1].split('"')[0]
-                            city = item.split('"cityName":"')[1].split('"')[0]
-                            state = item.split('"countrySubdivisionCode":"')[1].split('"')[0]
-                            zc = item.split(',"postalCode":"')[1].split('"')[0]
-                            country = 'US'
-                            lat = item.split('"latitude":')[1].split(',')[0]
-                            hours = ''
-                            lng = item.split('"longitude":')[1].split('}')[0]
-                            try:
-                                days = item.split('"generalOpeningHour":[{')[1].split('}],"serviceOpeningHour":')[0].split('"openFrom":"')
-                                for day in days:
-                                    if '"dayOfWeek":' in day:
-                                        dname = day.split('"dayOfWeek":[')[1].split(']')[0]
-                                        dname = dname.replace('1','Mon').replace('2','Tue').replace('3','Wed').replace('4','Thu').replace('5','Fri').replace('6','Sat').replace('7','Sun')
-                                        hrs = dname + ': ' + day.split('"')[0] + '-' + day.split(',"openTo":"')[1].split('"')[0]
-                                        if hours == '':
-                                            hours = hrs
-                                        else:
-                                            hours = hours + '; ' + hrs
-                            except:
-                                hours = '<MISSING>'
-                            if 'Mon' in hours and 'Sun' not in hours:
-                                hours = hours + '; Sun: Closed'
-                            array.append(store)
-                            zc = zc[:5]
-                            if store not in sids:
-                                sids.append(store)
-                                #logger.info(store)
-                                yield [website, loc, name, add, city, state, zc, country, store, phone, typ, lat, lng, hours]
-                        except:
-                            pass
-        if len(array) <= MAX_RESULTS:
-                    logger.info("max distance update")
-                    search.max_distance_update(MAX_DISTANCE)
-        ##        elif len(array) == MAX_RESULTS:
-        ##            logger.info("max count update")
-        ##            search.max_count_update(result_coords)
-        else:
-            raise Exception("expected at most " + str(MAX_RESULTS) + " results")
-        coord = search.next_coord()        
+        coords = []
+        lat, lng = coord
+
+        params = {"distance": MAX_DISTANCE, "maxResults": MAX_RESULTS}
+        url = f"https://www.chevrolet.com/OCRestServices/dealer/latlong/v1/chevrolet/{lat}/{lng}"
+        data = session.get(url, headers=headers, params=params).json()
+        dealers = data.get("payload").get("dealers") or []
+
+        for dealer in dealers:
+            store_number = dealer.get("id")
+
+            if store_number in locations:
+                continue
+
+            data = extract(dealer)
+            locations[store_number] = data
+            coords.append([data.get("latitude"), data.get("longitude")])
+
+            yield [data[field] for field in fields]
+
+        search.update_with(coords)
+        coord = search.next()
+        logger.info(f"remaining: {search.zipcodes_remaining()}")
+
+
 def scrape():
+    start = datetime.now()
     data = fetch_data()
     write_output(data)
+
+    logger.info(f"duration: ${datetime.now() - start}")
+
 
 scrape()
