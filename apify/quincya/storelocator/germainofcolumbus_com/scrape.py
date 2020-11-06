55f204982a3b7aab610138f1da846fd44f60970c
Quincy <qcalexander@gmail.com>
2020-11-05 21:54:17 -0400

    germainofcolumbus_com first commit

---
+++ apify/quincya/storelocator/germainofcolumbus_com/scrape.py
@@ -0,0 +1,76 @@
+from sgrequests import SgRequests
+from bs4 import BeautifulSoup
+import csv
+import json
+
+
+def write_output(data):
+	with open('data.csv', mode='w', encoding="utf-8") as output_file:
+		writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+		# Header
+		writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+		# Body
+		for row in data:
+			writer.writerow(row)
+
+def fetch_data():
+	
+	base_link = "https://www.germainofcolumbus.com/locations/index.htm"
+
+	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+	HEADERS = {'User-Agent' : user_agent}
+
+	session = SgRequests()
+	req = session.get(base_link, headers = HEADERS)
+	base = BeautifulSoup(req.text,"lxml")
+
+	data = []
+
+	items = base.find(id="proximity-dealer-list").find_all(class_="vcard")
+	locator_domain = "germainofcolumbus.com"
+
+	for item in items:
+
+		location_name = item.a.text.strip()
+
+		street_address = item.find(class_="street-address").text.strip()
+		city = item.find(class_="locality").text.strip()
+		state = item.find(class_="region").text.strip()
+		zip_code = item.find(class_="postal-code").text.strip()
+		country_code = "US"
+		store_number = "<MISSING>"
+		location_type = "<MISSING>"
+		phone = item.find(class_="tel phone1").text.split(":")[1].strip()
+		latitude = item.find(class_="latitude").text.strip()
+		longitude = item.find(class_="longitude").text.strip()
+
+		link = item.a["href"]
+		if link:
+			req = session.get(link, headers = HEADERS)
+			base = BeautifulSoup(req.text,"lxml")
+
+			fin_script = ""
+			all_scripts = base.find_all('script')
+			for script in all_scripts:
+					if "window.DDC.browserSunsetData" in str(script):
+							fin_script = str(script)
+							break
+
+			days = json.loads(fin_script.split('"hours":')[1].split(",\n")[0].strip())
+			hours_of_operation = ""
+			for day in days:
+				hours_of_operation = (hours_of_operation + " " + day.title() + " " + days[day]["value"]).strip()
+		else:
+			link = "<MISSING>"
+			hours_of_operation = "<MISSING>"
+
+		data.append([locator_domain, link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
+
+	return data
+
+def scrape():
+	data = fetch_data()
+	write_output(data)
+
+scrape()
