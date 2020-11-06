d0831f6c38eb78fcf2eb87860a363b102c5ea2ee
vivek <vivek@rentechdigital.com>
2020-11-06 14:35:42 +0530

    done

--- apify/himanshu/storelocator/newfoundlandgrocerystores_ca/scrape.py
+++ apify/himanshu/storelocator/newfoundlandgrocerystores_ca/scrape.py
@@ -18,20 +18,19 @@ def fetch_data():
     headers = {
         'site-banner': 'fortinos',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
-        'Cookie': 'ak_bmsc=18B950B5D2AF67482FD6D7D942F177E617417C34E17A0000649C9A5FBB2E1644~plQlKD7ApWOKJJDRFcfVclw0p/Ke7WRPaT4kDTJKFSVqjsjynXMXw5bL7XaiAI7UUYw4JnPHSaVRx1df4D+ziBqaWLn7nWjwmwN8DXqgZtpXCjoL0eaJtf5DjHye1TiGGzXGiKo2xBmuq2jJiUEN9CrfIRDVo2rXnatxP6WNXghL05r2EV9B89xgtbW1ee4T+CtopCgW99JmZWXYOMbC8mG0PP0Wi/eP9/eNdIYuH7WWU=; ADRUM_BTa=R:0|g:d3f2323f-44a8-4af5-9d1b-f0ee26aa6a34|n:lblw_afe7f4d6-4637-4e11-95bb-0a169ff97498; ADRUM_BT1=R:0|i:352462|e:99; bm_sv=EA63D9ED91118F2C13887BFF56449650~UbAdYJMOlOAEBRv2ajLNck5f2RbU1GQh5CNEYD5u/2BGavRlr0YgAwxglQftqvsd0tJFaf00ipTiUagAkjZfdIk95IMJnUlrCExJ9Wp1WWiGT2J5/dsDXVBL1aOOgVRjRd1egMz5IKWL/iAy3mbQRfy5jNGSb4v6IIZJ5lN7Kkw='
+        'Cookie': 'ak_bmsc=18B950B5D2AF67482FD6D7D942F177E617417C34E17A0000649C9A5FBB2E1644~plQlKD7ApWOKJJDRFcfVclw0p/Ke7WRPaT4kDTJKFSVqjsjynXMXw5bL7XaiAI7UUYw4JnPHSaVRx1df4D+ziBqaWLn7nWjwmwN8DXqgZtpXCjoL0eaJtf5DjHye1TiGGzXGiKo2xBmuq2jJiUEN9CrfIRDVo2rXnatxP6WNXghL05r2EV9B89xgtbW1ee4T+CtopCgW99JmZWXYOMbC8mG0PP0Wi/eP9/eNdIYuH7WWU=; ADRUM_BTa=R:0|g:d3f2323f-44a8-4af5-9d1b-f0ee26aa6a34|n:lblw_afe7f4d6-4637-4e11-95bb-0a169ff97498; ADRUM_BT1=R:0|i:352462|e:99; bm_sv=EA63D9ED91118F2C13887BFF56449650~UbAdYJMOlOAEBRv2ajLNck5f2RbU1GQh5CNEYD5u/2BGavRlr0YgAwxglQftqvsd0tJFaf00ipTiUagAkjZfdIk95IMJnUlrCExJ9Wp1WWiGT2J5/dsDXVBL1aOOgVRjRd1egMz5IKWL/iAy3mbQRfy5jNGSb4v6IIZJ5lN7Kkw=; ADRUM_BTa=R:0|g:0e4eab8c-ee43-4fc4-a647-ca6030c817be|n:lblw_afe7f4d6-4637-4e11-95bb-0a169ff97498; ADRUM_BT1=R:0|i:352462|e:63'
         }
     base_url = "https://www.newfoundlandgrocerystores.ca/"
     r = session.get("https://www.newfoundlandgrocerystores.ca/api/pickup-locations?bannerIds=dominion",headers=headers).json()
     for val in r:
         store_number = val['id'].replace("CT","").replace("0096SD1179","0096SD1268")
-        time.sleep(2)
+        time.sleep(5)
         jd = session.get("https://www.newfoundlandgrocerystores.ca/api/pickup-locations/"+store_number,headers=headers).json()
         location_name = jd['name']
         if val['address']['line2']!=None:
             street_address = jd['address']['line1']+" "+jd['address']['line2']
         else:
             street_address = jd['address']['line1']
-        
         city = jd['address']['town']
         state = jd['address']['region']
         if jd['address']['postalCode']!=None:
