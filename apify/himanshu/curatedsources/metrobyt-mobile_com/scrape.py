f8e7473c7b1398cc9a721d684d54dda949e6b0f5
vivek <vivek@rentechdigital.com>
2020-11-06 11:48:16 +0530

    done

--- apify/himanshu/curatedsources/metrobyt-mobile_com/scrape.py
+++ apify/himanshu/curatedsources/metrobyt-mobile_com/scrape.py
@@ -1,14 +1,27 @@
 import csv
-from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
 import sgzip
-import csv
-import re
-import json
-import unicodedata
+import ssl
+from sglogging import SgLogSetup
+from sgrequests import SgRequests
 session = SgRequests()
+logger = SgLogSetup().get_logger('metrobyt-mobile')
+
+
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    #logger.info("Error##################")
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
+
 def write_output(data):
     with open('data.csv', mode='w', newline="") as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -19,101 +32,167 @@ def write_output(data):
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
-    MAX_RESULTS = 100
-    MAX_DISTANCE = 100
-    search = sgzip.ClosestNSearch()
-    search.initialize(country_codes=['US'])
-    coord = search.next_coord()
-    current_results_len = 0
-    address = []
-    json_data=0
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
-        'Content-Type': 'application/json',
-        'Referer': 'https://www.bmwusa.com/?bmw=grp:BMWcom:header:nsc-flyout'
-    }
     addressess = []
+    addresses1 =[]
+    search = sgzip.ClosestNSearch()
+    search.initialize()
+    MAX_RESULTS = 100000
+    MAX_DISTANCE = 10
+    current_results_len = 0  # need to update with no of count.
+    coord = search.next_coord()
 
+
+    response1 =0
     while coord:
-        # print("remaining zipcodes: " + str(len(search.zipcodes)))
-        # print("remaining zipcodes: " + str(search.zipcodes_remaining()))
-        # print('Pulling Lat-Long %s,%s...' % (str(lat), str(lng)))
-        # print(search.current_zip)
-        result_coords =[]
+        # logger.info("remaining zipcodes: " + str(search.zipcodes_remaining()))
+        result_coords = []
+        lat = coord[0]
+        lng = coord[1]
+        base_url = "https://www.metrobyt-mobile.com/"
+        url = "https://www.metrobyt-mobile.com/api/v1/commerce/store-locator"
+        # logger.info(str(search.current_zip))
+        # data="https://www.metrobyt-mobile.com/api/v1/commerce/store-locator?address=10429%20n%2019th%20ave%20phoenix%20az&store-type=All&min-latitude=33.58853657681159&max-latitude=33.58853657681159&min-longitude=-112.10864146226419&max-longitude=-112.10864146226419"
+        querystring1 = {"address": ""+str(search.current_zip)+"", "store-type": "All", "min-latitude": "19.50139",
+                       "max-latitude": ""+str(lat)+"", "min-longitude": "-68.01197", "max-longitude": ""+str(lng)+""}
         
-        headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
-        }
-        locator_domain = "https://www.metrobyt-mobile.com/"
-        location_url = "https://www.metrobyt-mobile.com/api/v1/commerce/store-locator?address="+str(search.current_zip)+"&store-type=All&min-latitude="+str(coord[0])+"&max-latitude=37.0902&min-longitude="+str(coord[0])+"&max-longitude=-95.7129"
-        r = session.get(location_url, headers=headers).json()
+       
+        
 
-        for dt in r:
-            
-            # print(location_name)
-            street_address = dt['location']['address']['streetAddress']
-            zipp = dt['location']['address']['postalCode']
-            state = dt['location']['address']['addressRegion']
-            city = dt['location']['address']['addressLocality']
-            latitude = dt['location']['latitude']
-            longitude = dt['location']['longitude']
-            phone = dt['telephone']
-            hours_of_operation=''
+        headers = {
+            'Content-type': "*/*,multipart/form-data; boundary=--------------------------773008355060723902209115",
+            'User-Agent': "PostmanRuntime/7.20.1",
+            'Accept': "*/*",
+            'Cache-Control': "no-cache",
+            'Postman-Token': "4fa3632d-185c-4b7b-9811-f701eb949536,9ae7e127-2dcd-4ac3-9dff-c9f672d23431",
+            'Host': "www.metrobyt-mobile.com",
+            'Accept-Encoding': "gzip, deflate",
+            'Content-Length': "0",
+            'Connection': "keep-alive",
+            'cache-control': "no-cache"
+        }
+
+        try:
+            response1 = session.get(url, headers=headers, params=querystring1).json()
+            current_results_len = len(response1)
+        except:
+            continue
+
+        for loc in response1:
+            locator_domain = 'https://www.metrobyt-mobile.com/'
+            country_code = "US"
+            store_number = loc["id"]
+            location_type = loc["type"]
             location_name=''
-            for h in dt['openingHours']:
-                hours_of_operation = ", ".join(h['days']) + ' ' + h['time']
-            location_type=''
-            location_type=dt['type']
+            location_type=loc['type']
             if location_type:
                 if "Corporate Store" in location_type:
                     continue
             else:
                 location_type=''
-            page_url = ("https://www.metrobyt-mobile.com/storelocator/"+state.lower()+'/'+city.lower().replace(" ",'-')+'/'+street_address.lower().replace(" ",'-'))
-            if len(zipp)==4:
-                zipp = "0"+zipp
 
-            
+
+
+            # try:
+            #     location_name = loc["name"]
+            # except:
+            #     location_name = "<MISSING>"
+
+
             if location_type:
                 location_name = "Metro by T-Mobile "+location_type.strip()
             else:
                 location_name =  '<MISSING>'
+            try:
+                phone = loc["telephone"]
+            except:
+                phone = "<MISSING>"
+            try:
+                latitude = loc["location"]["latitude"]
+                longitude = loc["location"]["longitude"]
+            except:
+                latitude = "<MISSING>"
+                longitude = "<MISSING>"
+            # logger.info(loc["address"]["streetAddress"])
+            try:
+                street_address = loc["location"]["address"]["streetAddress"]
+            except:
+                street_address = "<MISSING>"
+            try:
+                city = loc["location"]["address"]["addressLocality"]
+            except:
+                city = "<MISSING>"
+            try:
+                state = loc["location"]["address"]["addressRegion"]
+            except:
+                state = "<MISSING>"
+            try:
+                zipp = loc["location"]["address"]["postalCode"]
+            except:
+                zipp = "<MISSING>"
+            if len(zipp.strip()) == 4:
+                zipp = "0" + zipp
+            #logger.info(zipp)
+            # try:
+            #     hours_of_operation = ""
+            #     for key, value in loc["openingHours"].items():
+            #         hours_of_operation += key + " " + value + " "
+            # except:
+            #     hours_of_operation = "<MISSING>"
+            hours_of_operation=''
+            for h in loc['openingHours']:
+                hours_of_operation = ", ".join(h['days']) + ' ' + h['time']
 
 
-            
-            store = []
-            store.append(locator_domain if locator_domain else '<MISSING>')
-            store.append(location_name if location_name else '<MISSING>')
-            store.append(street_address if street_address else '<MISSING>')
-            store.append(city if city else '<MISSING>')
-            store.append(state if state else '<MISSING>')
-            store.append(zipp if zipp else '<MISSING>')
-            store.append('US')
-            store.append('<MISSING>')
-            store.append(phone if phone else '<MISSING>')
-            store.append("<MISSING>")
-            store.append(latitude if latitude else '<MISSING>')
-            store.append(longitude if longitude else '<MISSING>')
-            store.append(hours_of_operation if hours_of_operation else '<MISSING>')
-            store.append(dt['name'] if dt['name'] else '<MISSING>')
-            store.append(page_url)
-            store = [x.replace("–","-") if type(x) == str else x for x in store]
-            store = [x.encode('ascii', 'ignore').decode('ascii').strip() if type(x) == str else x for x in store]
-            if str(store[2]+str(store[-7])+store[-1]) in addressess:
+
+            page_url = "<MISSING>"
+            if phone== None:
+                phone = "<MISSING>"
+
+            if phone==".":
+                phone = "<MISSING>"
+            page_url = ("https://www.metrobyt-mobile.com/storelocator/"+state.lower()+'/'+city.lower().replace(" ",'-')+'/'+street_address.lower().replace(" ",'-'))
+            store1 = []
+            result_coords.append((latitude, longitude))
+            store1.append(locator_domain if locator_domain else '<MISSING>')
+            store1.append(location_name if location_name else '<MISSING>')
+            store1.append(street_address if street_address else '<MISSING>')
+            store1.append(city if city else '<MISSING>')
+            store1.append(state if state else '<MISSING>')
+            store1.append(zipp if zipp else '<MISSING>')
+            store1.append(country_code if country_code else '<MISSING>')
+            store1.append(store_number if store_number else '<MISSING>')
+            store1.append(phone.encode('ascii', 'ignore').decode('ascii').strip() if phone.encode('ascii', 'ignore').decode('ascii').strip() else '<MISSING>')
+            store1.append('<MISSING>')
+            store1.append(latitude if latitude else '<MISSING>')
+            store1.append(longitude if longitude else '<MISSING>')
+            store1.append(hours_of_operation if hours_of_operation else '<MISSING>')
+            store1.append(loc['name'].capitalize()  if loc['name'] else '<MISSING>')
+            store1.append(page_url)
+            if str(store1[2]+str(store1[-7])+store1[-1]) in addressess:
                 continue
-            addressess.append(str(store[2]+str(store[-7])+store[-1]))
-            yield store
-        
-        if json_data < MAX_RESULTS:
+            addressess.append(str(store1[2]+str(store1[-7])+store1[-1]))
+            # print("store1=======", store1)
+            yield store1
+
+       
+
+        if current_results_len < MAX_RESULTS:
+            # logger.info("max distance update")
             search.max_distance_update(MAX_DISTANCE)
-        elif json_data== MAX_RESULTS:
+        elif current_results_len == MAX_RESULTS:
+            # logger.info("max count update")
             search.max_count_update(result_coords)
         else:
-            raise Exception("expected at most " + str(MAX_RESULTS) + " results")
+            raise Exception("expected at most " +
+                            str(MAX_RESULTS) + " results")
         coord = search.next_coord()
+
+
 def scrape():
     data = fetch_data()
     write_output(data)
-scrape()
 
+
+scrape()
