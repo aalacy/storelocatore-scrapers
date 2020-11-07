a1f976dcdeb97cbbc566068dacce66336c4f51f5
Matt Wittmann <mcwitt@gmail.com>
2020-11-06 17:29:10 -0800

    CI fixes & improvements (#98)

--- .circleci/lib.sh
+++ .circleci/lib.sh
@@ -2,14 +2,18 @@
 
 crawler_subdir_regex='apify(\/[^/]+){3}'
 
-list_updated_files() {
+list_diffs() {
 	current_branch=$(git rev-parse --abbrev-ref HEAD)
 	target_branch="${TARGET_BRANCH:-origin/master}"
-	git --no-pager diff --name-only "$(git merge-base "$current_branch" "$target_branch")"
+	git --no-pager diff ${1:+"$1"} --name-only "$(git merge-base "$current_branch" "$target_branch")"
+}
+
+list_updated_files() {
+	list_diffs --diff-filter=d
 }
 
 list_updated_crawlers() {
-	list_updated_files |
+	list_diffs "" |
 		xargs -I{} dirname {} |
 		sort |
 		uniq |
@@ -26,31 +30,43 @@ filter_files_outside_crawler_subdir() {
 }
 
 filter_python_files() {
-	grep -E '[^.]+\.py$' || true
+	grep -E '\.py$' || true
 }
 
 filter_node_files() {
-	grep -E '[^.]+\.jsx?$' || true
+	grep -E '\.jsx?$' || true
 }
 
-check_update_scope() {
+ensure_no_linter_config() {
+	if [ -e "$1" ]; then
+		echo "FAIL: Crawler-specific linting configuration is not allowed. Please remove '$1'"
+		return 1
+	fi
+}
+
+check_diffs() {
 	mapfile -t updated_subdirs < <(list_updated_crawlers)
-	updated_files_outside_crawler_subdir=$(list_updated_files | filter_files_outside_crawler_subdir)
+	diffs_outside_crawler_subdir=$(list_diffs "" | filter_files_outside_crawler_subdir)
 
 	exit_status=0
-	if [ -n "$updated_files_outside_crawler_subdir" ]; then
-		echo "FAIL: Changes should be confined to a crawler subdirectory, but found changes to the following files:"
-		echo "$updated_files_outside_crawler_subdir"
+	if [ -n "$diffs_outside_crawler_subdir" ]; then
+		echo "FAIL: Diffs should be limited to a crawler subdirectory, but found changes to the following files:"
+		echo "$diffs_outside_crawler_subdir"
 		echo
 		exit_status=$((exit_status + 1))
 	fi
 
 	if [ "${#updated_subdirs[@]}" -gt 1 ]; then
-		echo "FAIL: Changes should be contained in a single crawler subdirectory, but found changes in the following crawler subdirectories:"
+		echo "FAIL: Diffs should be limited to a single crawler subdirectory, but found changes in the following crawler subdirectories:"
 		printf '%s\n' "${updated_subdirs[@]}"
 		echo
 		exit_status=$((exit_status + 2))
 	fi
+
+	updated_crawler="${updated_subdirs[0]}"
+
+	ensure_no_linter_config "${updated_crawler}/.eslintrc.json" || exit_status=$((exit_status + 4))
+	ensure_no_linter_config "${updated_crawler}/.flake8" || exit_status=$((exit_status + 8))
 
 	return $exit_status
 }
