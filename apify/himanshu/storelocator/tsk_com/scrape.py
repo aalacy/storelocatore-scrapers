ad50dfbe9397ec32ab5fd6be7587461e48e32e1e
vivek <vivek@rentechdigital.com>
2020-11-09 14:20:56 +0530

    done

--- apify/himanshu/storelocator/tsk_com/scrape.py
+++ apify/himanshu/storelocator/tsk_com/scrape.py
@@ -3,72 +3,58 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
-
-
 session = SgRequests()
-
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
         # Body
         for row in data:
             writer.writerow(row)
-
 def fetch_data():
-    base_url = "https://tsk.com"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+    }
+    base_url = "https://tsk.com/locations/"
     return_main_object = []
-    r = session.get(base_url+'/locations/')
-    soup=BeautifulSoup(r.text,'lxml')
-    main=soup.find('div',{"class":"loc-full-st"}).find_all('div',{"class":"loc-fullbox"})
-    for atag in main:
-        link=atag.find('a')['href']
-        r1 = session.get(link)
-        soup1=BeautifulSoup(r1.text,'lxml')
-        phone=''
-        address=''
-        city=''
-        state=''
-        zip=''
-        lat=''
-        lng=''
-        country="US"
-        name=soup1.find('h2',{'itemprop':'name'}).text.strip()
-        if soup1.find('div',{"class":"address-info"}).find('span',{"itemprop":"streetAddress"})!=None:
-            address=soup1.find('div',{"class":"address-info"}).find('span',{"itemprop":"streetAddress"}).text.replace(',',' ').strip()
-        if soup1.find('div',{"class":"address-info"}).find('span',{"itemprop":"addressLocality"})!=None:
-            city=soup1.find('div',{"class":"address-info"}).find('span',{"itemprop":"addressLocality"}).text.replace(',',' ').strip()
-        if soup1.find('div',{"class":"address-info"}).find('span',{"itemprop":"addressRegion"})!=None:
-            state=soup1.find('div',{"class":"address-info"}).find('span',{"itemprop":"addressRegion"}).text.strip()
-        if soup1.find('div',{"class":"address-info"}).find('span',{"itemprop":"postalCode"})!=None:
-            zip=soup1.find('div',{"class":"address-info"}).find('span',{"itemprop":"postalCode"}).text.strip()
-        if soup1.find('a',{"itemprop":"telephone"})!=None:
-            phone=soup1.find('a',{"itemprop":"telephone"}).text.replace('Phone: ','').strip()
-        hour=''
-        if soup1.find('time',{"itemprop":"openingHours"})!=None:
-            for hr in soup1.find_all('time',{"itemprop":"openingHours"}):
-                hour+=' '.join(list(hr.stripped_strings))
-        store=[]
-        store.append(base_url)
-        store.append(name if name else "<MISSING>")
-        store.append(address if address.strip() else "<MISSING>")
-        store.append(city if city.strip() else "<MISSING>")
-        store.append(state if state.strip() else "<MISSING>")
-        store.append(zip if zip.strip() else "<MISSING>")
-        store.append(country if country.strip() else "<MISSING>")
-        store.append("<MISSING>")
-        store.append(phone if phone.strip() else "<MISSING>")
-        store.append("tsk")
-        store.append(lat if lat else "<MISSING>")
-        store.append(lng if lng else "<MISSING>")
-        store.append(hour if hour.strip() else "<MISSING>")
-        return_main_object.append(store)                
-    return return_main_object
-
+    r = session.get(base_url)
+    soup = BeautifulSoup(r.text,'html5lib')
+    main = soup.find_all('script')[15].text.split("var locations = ")[1].split("for (var i=0; i < locations.length; i++)")[0].replace('twitter.com\/TSMMAFeaster"}]];','twitter.com\/TSMMAFeaster"}]]')
+    list_1 = []
+    data_json = json.loads(main)
+    for i in data_json:
+        for j in i:
+            list_1.append(j)
+    try:
+        i = 4
+        while True:
+            # r = session.get("https://tsk.com/locations/"+list_1[i]['url'],headers=headers)
+            # soup = BeautifulSoup(r.text, "html5lib")
+            # data = soup.find("div",{"class":"qigLe4VzIR2OBRVx0QbLM dS6uS5vUPuwi9woFSKW5A _1gETrmJP4-nKwWKbJliZCW _1TdCsy72ploof1Rx0WfhEv foajPYZEN0yB_-OGHR4Ol merce-column"}).text
+            # print(data)
+            store=[]
+            store.append("https://tsk.com/")
+            store.append(list_1[i]['name'] if list_1[i]['name'] else "<MISSING>")
+            store.append(list_1[i]['street'] if list_1[i]['street'] else "<MISSING>")
+            store.append(list_1[i]['city'] if list_1[i]['city'] else "<MISSING>")
+            store.append(list_1[i]['state'] if list_1[i]['state'] else "<MISSING>")
+            store.append(list_1[i]['zip5'] if list_1[i]['zip5'] else "<MISSING>")
+            store.append("US")
+            store.append(list_1[i]['crmSchoolId'])
+            store.append(list_1[i]['phone'] if list_1[i]['phone'] else "<MISSING>")
+            store.append("tsk")
+            store.append(list_1[i]['lat'] if list_1[i]['lat'] else "<MISSING>")
+            store.append(list_1[i]['lon'] if list_1[i]['lon'] else "<MISSING>")
+            store.append("<INACCESSIBLE>")
+            # inaccessible
+            store.append("https://tsk.com/locations"+list_1[i]['url'])
+            yield store
+            i = i + 5
+    except:
+        pass
 def scrape():
     data = fetch_data()
     write_output(data)
-
 scrape()
