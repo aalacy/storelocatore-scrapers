ea2babddb98ba7e31e51f31baad302f55afb3d8e
vivek <vivek@rentechdigital.com>
2020-11-10 15:43:28 +0530

    done

--- apify/himanshu/storelocator/untuckit_com/scrape.py
+++ apify/himanshu/storelocator/untuckit_com/scrape.py
@@ -17,6 +17,7 @@ def write_output(data):
             writer.writerow(row)
 
 def fetch_data():
+    addressess =[]
     base_url = "https://www.untuckit.com"
     r = session.get("https://storemapper-herokuapp-com.global.ssl.fastly.net/api/users/5048/stores.js")
     data = r.json()["stores"]
@@ -27,28 +28,36 @@ def fetch_data():
         store.append("https://www.untuckit.com")
         store.append(store_data['name'])
         location_request = session.get(store_data["url"])
-        # print(store_data["url"])
         location_soup = BeautifulSoup(location_request.text,"lxml")
         address = list(location_soup.find("p",{"class":"store_info--copy"}).stripped_strings)
-        if "Monday" in address:
+        st = address[0].replace("International Plaza Mall",'').replace("Copley Place",'').replace(",",' ').strip()
+ 
+     
+        if "london-covent-garden" in store_data["url"] or "london" in store_data["url"]:
             continue
-        if "london-covent-garden" in store_data["url"] or "boca-raton" in store_data["url"] or "london" in store_data["url"]:
-            continue
-        # print(address)
-        # print(address[-1])
-        ca_zip_split = re.findall(r'[A-Z]{1}[0-9]{1}[A-Z]{1}\s*[0-9]{1}[A-Z]{1}[0-9]{1}',address[-1])
-        if ca_zip_split:
-            ca_zip = ca_zip_split[-1]
+        if "boca-raton" in store_data["url"]:
+            st = store_data['address'].split(",")[0]
+            city = store_data['address'].split(",")[1]
+            state = store_data['address'].split(",")[-1].strip().split( )[0]
+            us_zip = store_data['address'].split(",")[-1].strip().split( )[1]
         else:
-            ca_zip = ''
-            us_zip_split = re.findall(r'[0-9]{5}',address[-1])
-            us_zip = us_zip_split[0]
-        state_split = re.findall(r'[A-Z]{2}',address[-1])
-        if state_split:
-            state = state_split[-1]
-        store.append(address[0].replace("International Plaza Mall",'').replace("Copley Place",'').replace(",",' ').strip())
-        store.append(address[-1].replace(state,"").replace(ca_zip,"").replace(us_zip,"").replace(",",""))
-        store.append(state)
+            ca_zip_split = re.findall(r'[A-Z]{1}[0-9]{1}[A-Z]{1}\s*[0-9]{1}[A-Z]{1}[0-9]{1}',address[-1])
+            if ca_zip_split:
+                ca_zip = ca_zip_split[-1]
+            else:
+                ca_zip = ''
+                us_zip_split = re.findall(r'[0-9]{5}',address[-1])
+                us_zip = us_zip_split[0]
+            state_split = re.findall(r'[A-Z]{2}',address[-1])
+            if state_split:
+                state = state_split[-1]
+            city = address[-1].replace(state,"").replace(ca_zip,"").replace(us_zip,"").replace(",","")
+
+
+        
+        store.append(st.strip())
+        store.append(city.strip())
+        store.append(state.strip())
         store.append(ca_zip if ca_zip else us_zip)
         store.append("CA" if ca_zip else "US")
         store.append(store_data["id"])
@@ -61,6 +70,9 @@ def fetch_data():
         hours_of_operation =  " ".join(list(h.stripped_strings)).replace("Open. Contactless ",'').replace("Pick Up & Same-Day Delivery also available.",'').replace("Open for Contactless Pickup & Same-Day Delivery also available.",'').split(".")[-1].strip()
         store.append(hours_of_operation if hours_of_operation != "" else "<MISSING>")
         store.append(store_data["url"])
+        if str(store[2]+store[-1]) in addressess:
+            continue
+        addressess.append(str(store[2]+store[-1]))
         yield store
 
 
@@ -69,4 +81,3 @@ def scrape():
     write_output(data)
 
 scrape()
-
