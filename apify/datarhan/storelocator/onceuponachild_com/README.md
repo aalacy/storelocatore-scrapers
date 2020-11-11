da3da63844888e363d3bf8c4760d91259553127b
datarhan <datarhan@github.com>
2020-11-11 13:08:26 +0100

    [onceuponachild.com] One request to get all locations

--- apify/datarhan/storelocator/onceuponachild_com/README.md
+++ apify/datarhan/storelocator/onceuponachild_com/README.md
@@ -2,4 +2,5 @@
 1. --ignore GeoConsistencyValidator, ZIPCODE_NOT_IN_STATE (30671)
 2. --ignore CountValidator, the spider makes serach requests with state code to get results. I added all states for USA and Canada. Total adresses for crawl is 348, expected 390
 3. Fixed one postal code, input value "CAN8T 3K7", in results "N8T 3K7".
+4. --ignore StreetAddressHasNumber. row 397, ADDR_HAS_NO_NUMBER
 
--- apify/datarhan/storelocator/onceuponachild_com/scrape.py
+++ apify/datarhan/storelocator/onceuponachild_com/scrape.py
@@ -24,83 +24,73 @@ def fetch_data():
     session = SgRequests()
 
     items = []
-    gathered_ids = []
+    gathered_items = []
     
-    states = [
-        "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
-        "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
-        "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
-        "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
-        "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
-    ]
+    state_response = session.get('https://wmrk-api.treefort.com/corp/ouac/locations/search/?maxDistance=500000')
+    state_data = json.loads(state_response.text)
+    
+    for store_data_countries in state_data[0]['countries']:
+        for store_data_regions in store_data_countries['regions']:
+            for store_data in store_data_regions['stores']:
+                if store_data['statusText'] == 'Coming Soon':
+                    continue
+                store_url = 'https://onceuponachild.com/locations?query={}'.format(store_data['region'])
+                location_name = store_data['storeName']
+                street_address = store_data['addressLine1']
+                if street_address:
+                    if store_data['addressLine2']:
+                        street_address += ' ' + store_data['addressLine2']
+                if not street_address:
+                    street_address = store_data['addressLine2']
+                street_address = street_address if street_address else '<MISSING>'
+                city = store_data['city']
+                city = city if city else '<MISSING>'
+                state = store_data['region']
+                state = state if state else '<MISSING>'
+                zip_code = store_data['postalCode']
+                zip_code = zip_code if zip_code else '<MISSING>'
+                if zip_code.startswith('CA'):
+                    zip_code = zip_code[2:]
+                country_code = store_data['countryCode']
+                country_code = country_code if country_code else '<MISSING>'
+                store_number = store_data['storeNumber']
+                phone = store_data['phoneNumber']
+                phone = phone if phone else '<MISSING>'
+                location_type = '<MISSING>'
+                latitude = store_data['latitude']
+                latitude = latitude if latitude else '<MISSING>'
+                longitude = store_data['longitude']
+                longitude = longitude if longitude else '<MISSING>'
+                hours_of_operation = store_data['storeHours']
+                if hours_of_operation:
+                    hours_of_operation_list = []
+                    for elem in hours_of_operation:
+                        hours_of_operation_list.append('{}: {}-{}'.format(elem['day'], elem['open'], elem['close']))
+                    hours_of_operation = ', '.join(hours_of_operation_list)
+                hours_of_operation = hours_of_operation if hours_of_operation else '<MISSING>'
+                
+                item = [
+                    DOMAIN,
+                    store_url,
+                    location_name,
+                    street_address,
+                    city,
+                    state,
+                    zip_code,
+                    country_code,
+                    store_number,
+                    phone,
+                    location_type,
+                    latitude,
+                    longitude,
+                    hours_of_operation
+                ]
 
-    canada_states = [
-        'AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT'
-    ]
-
-    states += canada_states
-    for state in states:
-        state_url = 'https://wmrk-api.treefort.com/corp/ouac/locations/search/{}?maxDistance=500'.format(state)
-        state_response = session.get(state_url)
-        state_data = json.loads(state_response.text)
-        if not state_data:
-            continue
+                check = location_name.strip().lower() + ' ' + street_address.strip().lower()
+                if check not in gathered_items:
+                    gathered_items.append(check)
+                    items.append(item)
         
-        for store_data_countries in state_data[0]['countries']:
-            for store_data_regions in store_data_countries['regions']:
-                for store_data in store_data_regions['stores']:
-                    if store_data['statusText'] == 'Coming Soon':
-                        continue
-                    store_url = 'https://onceuponachild.com/locations?query={}'.format(state)
-                    location_name = store_data['storeName']
-                    street_address = store_data['addressLine1']
-                    if street_address:
-                        if store_data['addressLine2']:
-                            street_address += ' ' + store_data['addressLine2']
-                    if not street_address:
-                        street_address = store_data['addressLine2']
-                    street_address = street_address if street_address else '<MISSING>'
-                    city = store_data['city']
-                    city = city if city else '<MISSING>'
-                    state = store_data['region']
-                    state = state if state else '<MISSING>'
-                    zip_code = store_data['postalCode']
-                    zip_code = zip_code if zip_code else '<MISSING>'
-                    if zip_code.startswith('CA'):
-                        zip_code = zip_code[2:]
-                    country_code = store_data['countryCode']
-                    country_code = country_code if country_code else '<MISSING>'
-                    store_number = store_data['storeNumber']
-                    phone = store_data['phoneNumber']
-                    phone = phone if phone else '<MISSING>'
-                    location_type = '<MISSING>'
-                    latitude = store_data['latitude']
-                    latitude = latitude if latitude else '<MISSING>'
-                    longitude = store_data['longitude']
-                    longitude = longitude if longitude else '<MISSING>'
-                    hours_of_operation = store_data['storeHours']
-                    hours_of_operation = hours_of_operation if hours_of_operation else '<MISSING>'
-                    
-                    item = [
-                        DOMAIN,
-                        store_url,
-                        location_name,
-                        street_address,
-                        city,
-                        state,
-                        zip_code,
-                        country_code,
-                        store_number,
-                        phone,
-                        location_type,
-                        latitude,
-                        longitude,
-                        hours_of_operation
-                    ]
-                    if store_number not in gathered_ids:
-                        gathered_ids.append(store_number)
-                        items.append(item)
-            
     return items
 
 
