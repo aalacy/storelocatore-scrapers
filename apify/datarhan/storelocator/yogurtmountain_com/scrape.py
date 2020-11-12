e49a1cf226358c1cd055cfc3f036c8bca5f992f8
datarhan <datarhan@github.com>
2020-11-12 22:01:34 +0100

    [yogurtmountain.com] update after QA

--- apify/datarhan/storelocator/yogurtmountain_com/scrape.py
+++ apify/datarhan/storelocator/yogurtmountain_com/scrape.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import usaddress
 from lxml import etree
 
 from sgrequests import SgRequests
@@ -32,26 +34,36 @@ def fetch_data():
         adr_list = location.xpath('.//div[@class="loca-address loca-text list-column"]/p[1]//text()')
         if not adr_list:
             continue
-        adr_list = [elem.strip() for elem in adr_list]
-        street_address = ' '.join(adr_list[:-1])
-        street_address = street_address if street_address else '<MISSING>'
-        city = adr_list[-1].split(',')[0]
+        adr_list = ', '.join([elem.strip() for elem in adr_list]).split('(located')[0].strip()
+        try:
+            location_dict = json.loads(json.dumps(usaddress.tag(adr_list)))[0]
+            street_address_list = []
+            for key, value in location_dict.items():
+                if key == 'PlaceName':
+                    break
+                street_address_list.append(value)   
+            street_address = ' '.join(street_address_list)
+            city = location_dict.get('PlaceName')
+            state = location_dict.get('StateName')
+            zip_code = location_dict.get('ZipCode')
+        except:
+            street_address = ', '.join(adr_list.split(',')[:-2])
+            city = adr_list.split(',')[-2].strip()
+            state = adr_list.split(',')[-1].split()[0].strip()
+            zip_code = adr_list.split(',')[-1].split()[1].strip()
+
+        street_address = street_address if street_address else '<MISSING>'    
         city = city if city else '<MISSING>'
-        state = adr_list[-1].split(',')[0].split()[0]
         state = state if state else '<MISSING>'
-        zip_code = adr_list[-1].split(',')[0].split()[-1]
         zip_code = zip_code if zip_code else '<MISSING>'
         country_code = '<MISSING>'
-        country_code = country_code if country_code else '<MISSING>'
         store_number = '<MISSING>'
         phone = location.xpath('.//p[@class="phone"]/text()')
         phone = phone[0] if phone else ''
         phone = phone if phone else '<MISSING>'
         location_type = '<MISSING>'
         latitude = '<MISSING>'
-        latitude = latitude if latitude else '<MISSING>'
         longitude = '<MISSING>'
-        longitude = longitude if longitude else '<MISSING>'
         hours_of_operation = location.xpath('.//div[@class="loca-hours loca-text list-column"]/p[2]//text()')
         hours_of_operation = ', '.join([elem.strip() for elem in hours_of_operation]) if hours_of_operation else '<MISSING>'
 
