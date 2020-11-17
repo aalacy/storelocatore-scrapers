7f7272cb44eb8f28bb761e1d0291ca9af6a034dc
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2020-11-17 11:24:23 +0100

    Update scrape.py

--- apify/ggrahambaker/storelocator/philsbbq_net/scrape.py
+++ apify/ggrahambaker/storelocator/philsbbq_net/scrape.py
@@ -81,7 +81,10 @@ def fetch_data():
         all_store_data.append(store_data)
 
     #weird format of html, gotta dive in here
-    hours = soup.find('h2', text='San Diego International Airport').nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text
+    try:
+        hours = soup.find('h2', text='San Diego International Airport').nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text
+    except:
+        hours = "<MISSING>"
     store_data = [locator_domain, soup.find('h2', text='San Diego International Airport').text, '<MISSING>', 'San Diego', 'CA', '<MISSING>', 'US',
                      '<MISSING>', '<MISSING>', 'airport', '<MISSING>', '<MISSING>', hours ]
     all_store_data.append(store_data)
