f1304edf7e52b3a612410930398978f475cab236
Michael Dinh <mkdinh94@gmail.com>
2020-11-17 10:17:09 -0500

    SLC-4095/maxmara_com: fix api endpoints

--- apify/ggrahambaker/storelocator/maxmara_com/scrape.py
+++ apify/ggrahambaker/storelocator/maxmara_com/scrape.py
@@ -1,89 +1,135 @@
 import csv
-from sgselenium import SgChrome
 from bs4 import BeautifulSoup
-import json
+from sgrequests import SgRequests
+
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", mode="w") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+                "page_url",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
 
+
+MISSING = "<MISSING>"
+
+
+def get(location, key):
+    return location.get(key, MISSING) or MISSING
+
+
+def get_state(components):
+    state = components[1]
+    for d in components[2]:
+        if d.isdigit():
+            state = MISSING
+            break
+
+    return state
+
+
+def get_country_code(properties):
+    country = get(properties, "country")
+    if country == "United States":
+        return "US"
+    if country == "Canada":
+        return "CA"
+
+    return country
+
+
+def get_hours(properties):
+    days = properties.get("openingHours")
+    if not days:
+        return MISSING
+
+    hours = [f"{day}: {val[0]}" for day, val in days.items()]
+    return ",".join(hours).strip()
+
+
 def fetch_data():
+    session = SgRequests()
+    HEADERS = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
+    }
 
-    driver = SgChrome().chrome()
+    us_url = "https://ca.maxmara.com/store-locator?south=-8.658678440878985&west=-141.73433772563934&north=67.44481391760164&east=-49.595662274360656&name=&listJson=true&withoutRadius=false&country=US"
+    can_url = "https://ca.maxmara.com/store-locator?south=-27.105693815228516&west=171.8613245487213&north=84.5028074010682&east=-3.861324548721332&name=&listJson=true&withoutRadius=false&country=CA"
 
-    locator_domain = 'https://maxmara.com/' 
-    us_url = 'https://us.maxmara.com/store-locator?south=-37.85012759632715&west=-149.62984375&north=77.04151444711106&east=-41.70015625&name=&listJson=true&withoutRadius=false&country=US'
-    can_url = 'https://ca.maxmara.com/store-locator?south=-52.08464201527304&west=-170.443359375&north=86.91057206489693&east=-21.556640625000018&name=&listJson=true&withoutRadius=false&country=CA'
     urls = [us_url, can_url]
-    all_store_data = []
     for url in urls:
-        driver.get(url)
-        driver.implicitly_wait(5)
-        
-        base = BeautifulSoup(driver.page_source,"lxml")
-        locs = json.loads(base.text)['features']
+        data = session.get(url, headers=HEADERS, timeout=1).json()
+        locations = data.get("features")
+        for location in locations:
 
-        for loc in locs:
-            props = loc['properties']
-            zip_code = props['zip']
-            if 'United States' in props['country']:
-                country_code = 'US'
-            else:
-                country_code = 'CA'
-                
-            lat = props['lat']
-            longit = props['lng']
-            city = props['city'].strip()
-            addy_split = props['formattedAddress'].split(',')
-            state = addy_split[1].strip().replace("Washington","WA")
-            
-            for d in addy_split[2]:
-                if d.isdigit():
-                    state = '<MISSING>'
-                    break
+            properties = location.get("properties", {})
 
-            if "Lexington" in state:
-                city = "Lexington Ave"
-                state = "New York"
+            locator_domain = "https://maxmara.com/"
+            location_name = get(properties, "displayName")
+            store_number = get(properties, "name")
 
-            location_name = props['displayName']
-            
-            phone_number = props['phone1']
-            
-            street_address = " ".join(props['formattedAddress'].split(',')[:-3]).replace("Street And","Street")
-            if not street_address:
-                street_address = props['formattedAddress'].split(',')[0]
-      
-            hours = ''
-                
-            for day, val in props['openingHours'].items():
-                
-                hours += day + ' ' + val[0] +  ' '
-            
-            hours = hours.strip()
+            address = get(properties, "formattedAddress")
+            components = address.split(",")
 
-            if not hours:
-                hours = '<MISSING>'
+            street_address = components[0]
+            city = get(properties, "city")
+            state = get_state(components)
+            postal = get(properties, "zip")
+            country_code = get_country_code(properties)
+            lat = get(properties, "lat")
+            lng = get(properties, "lng")
 
-            store_number = str(props['name'])
-            page_url = 'https://world.maxmara.com/store/' + store_number
-            location_type = '<MISSING>'
+            phone_number = get(properties, "phone1")
+            hours = get_hours(properties)
+            page_url = f"https://world.maxmara.com/store/{get(properties, 'name')}"
 
-            store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code, 
-                     store_number, phone_number, location_type, lat, longit, hours, page_url]
+            location_type = MISSING
 
-            all_store_data.append(store_data)
-    driver.close()
-    return all_store_data
+            store_data = [
+                locator_domain,
+                location_name,
+                street_address,
+                city,
+                state,
+                postal,
+                country_code,
+                store_number,
+                phone_number,
+                location_type,
+                lat,
+                lng,
+                hours,
+                page_url,
+            ]
+
+            yield store_data
+
 
 def scrape():
     data = fetch_data()
     write_output(data)
+
 
 scrape()
