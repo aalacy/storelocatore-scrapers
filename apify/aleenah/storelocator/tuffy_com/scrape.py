dd2910b88d91e6a76fb710b5ee9bc2c2fd02b4c1
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-11-17 19:10:39 +0500

    Add files via upload

--- apify/aleenah/storelocator/tuffy_com/scrape.py
+++ apify/aleenah/storelocator/tuffy_com/scrape.py
@@ -1,123 +1,130 @@
+from bs4 import BeautifulSoup
 import csv
-from sgselenium import SgSelenium
-import re
-from pyzipcode import ZipCodeDatabase
-import time
+import string
+import re, time, usaddress
 
-driver = SgSelenium().chrome()
+from sgrequests import SgRequests
+
+session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
-def parse_geo(url):
-    lon = re.findall(r'll=[-?\d\.]*\,([-?\d\.]*)', url)[0]
-    lat = re.findall(r'll=(-?[\d\.]*)', url)[0]
-    return lat, lon
 
 def fetch_data():
-    # Your scraper here
-    locs = []
-    street = []
-    states=[]
-    cities = []
-    countries=[]
-    phones = []
-    zips = []
-    long = []
-    lat = []
-    timing = []
-    page_url=[]
-    US_states=["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New+Hampshire","New+Jersey","New+Mexico","New+York","North+Carolina","North+Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode+Island","South+Carolina","South+Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West+Virginia","Wisconsin","Wyoming"]
+    pattern = re.compile(r'\s\s+') 
+    data = []
+    titlelist = []
+    p = 0
+    states = ["AL", "AK", "AZ", "AR","CT", "DC", "DE", "FL", "GA", 
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "ME", "MD", "MI", "MN", "MS", "MO", "NE", "NH", "NJ", 
+          "NM", "NY", "NC", "ND", "OH", "OK", "PA", "SC", 
+          "SD", "TN", "TX", "UT", "VT", "VA", "WV", "WI"]
+    for statenow in states:
+        coordlist =[]
+        url = 'https://www.tuffy.com/location_search?zip_code='+statenow
+        print(url)
+        print('...............')
+        r = session.get(url, headers=headers, verify=False)       
+        soup =BeautifulSoup(r.text, "html.parser")        
+        divlist = soup.findAll('div', {'class': 'contact-info'})
+        if len(divlist) == 0:
+            continue
+        latlnglist = r.text.split('var locations = [',1)[1].split('];',1)[0]
+        while True:
+            try:
+                temp,latlnglist = latlnglist.split('(',1)[1].split('),',1)
+                coordlist.append(temp)
+            except:
+                break
+        print(len(coordlist))
+        
+        for j in range(0,len(divlist)):
+            div = divlist[j]
+            title = div.find('h2').text.strip().split('T',1)[1]
+            title = 'T'+title
+            #print(title)
+           
+            address= div.find('address').text
+            address = re.sub(pattern,' ',address).strip()
+            
+            try:
+                address = address.split('MANAGER',1)[0]
+            except:
+                pass
+            address = usaddress.parse(address)
+            
+            i = 0
+            street = ""
+            city = ""
+            state = ""
+            pcode = ""
+            while i < len(address):
+                temp = address[i]
+                if temp[1].find("Address") != -1 or temp[1].find("Street") != -1 or temp[1].find('Occupancy') != -1 or temp[1].find("Recipient") != -1 or temp[1].find("BuildingName") != -1 or temp[1].find("USPSBoxType") != -1 or temp[1].find("USPSBoxID") != -1:
+                    street = street + " " + temp[0]
+                if temp[1].find("PlaceName") != -1:
+                    city = city + " " + temp[0]
+                if temp[1].find("StateName") != -1:
+                    state = state + " " + temp[0]
+                if temp[1].find("ZipCode") != -1:
+                    pcode = pcode + " " + temp[0]
+                i += 1
 
-    for us in US_states:
-        url="https://www.tuffy.com/location_search?zip_code="+us
+            street = street.lstrip().replace(',','')
+            if street in titlelist:
+                continue
+            titlelist.append(street)
+            city = city.lstrip().replace(',','')
+            state = state.lstrip().replace(',','')
+            pcode = pcode.lstrip().replace(',','')
+            
+            phone =div.find('span',{'class':'tel'}).text
+            hours = div.find('div',{'class':'schedule-holder'}).text
+            hours = re.sub(pattern,'\n',hours).strip().replace('\n',' ')
+            
+            lat,longt = coordlist[j].split(', ')
+            
+            data.append([
+                            'https://www.tuffy.com/',
+                            url,                   
+                            title,
+                            street,
+                            city,
+                            state,
+                            pcode,
+                            'US',
+                            '<MISSING>',
+                            phone,
+                            '<MISSING>',
+                            lat,
+                            longt,
+                            hours
+                        ])
+            print(p,data[p])
+            p += 1
+                    
 
-        driver.get(url)
-        inp=driver.find_element_by_xpath('/html/body/div[3]/main/section[1]/div/form/fieldset/input')
-        inp.click()
-        time.sleep(5)
+                    
+                    
+            
+        
+    return data
 
-        try:
-            divs=driver.find_element_by_xpath('//div[@class="col contact-info-hlder"]')
-            divs=divs.find_elements_by_class_name("contact-info")
-            for div in divs:
-
-                loc=div.find_element_by_tag_name("h2").get_attribute("innerText")
-                loc=re.sub(r'\d+',"",loc).strip()
-
-                if loc in locs:
-                    continue
-                locs.append(loc)
-
-                addr=div.find_element_by_tag_name("address").get_attribute("innerText")
-
-                #addr=add.text
-                p=div.find_element_by_class_name("tel").get_attribute("innerText")
-                tim=div.find_element_by_class_name("schedule-holder").get_attribute("innerText")
-                if p!="":
-                    phones.append(p.strip())
-                else:
-                    phones.append("<MISSING>")
-                if tim!="":
-                    timing.append(tim.replace("\n"," ").strip().replace("  "," "))
-                else:
-                    timing.append("<MISSING>")
-
-                addr=addr.split("\n")[0]
-                addr=addr.split(",")
-                sz=addr[-1].strip().split(" ")
-                states.append(sz[0])
-                z=sz[1]
-                addr=addr[0].strip(",")
-                zips.append(z)
-                zcdb = ZipCodeDatabase()
-                z=zcdb[int(re.findall(r'[0-9]{5}',z)[0])]
-                c= z.place
-                if c in addr:
-                    addr=addr.replace(c,"").strip()
-                    cities.append(c)
-                    street.append(addr)
-                else:
-                    c=addr.strip().split(" ")[-1]
-                    cities.append(c)
-                    street.append(addr.replace(c,"").strip())
-                lat.append(z.latitude)
-                long.append(z.longitude)
-                page_url.append(url)
-
-        except:
-           continue
-
-    all = []
-    for i in range(0, len(locs)):
-        row = []
-        row.append("https://www.tuffy.com")
-        row.append(locs[i])
-        row.append(street[i])
-        row.append(cities[i])
-        row.append(states[i])
-        row.append(zips[i])
-        row.append("US")
-        row.append("<MISSING>")  # store #
-        row.append(phones[i])  # phone
-        row.append("<MISSING>")  # type
-        row.append(lat[i])  # lat
-        row.append(long[i])  # long
-        row.append(timing[i])  # timing
-        row.append(page_url[i])  # page_url
-
-        all.append(row)
-    return all
 
 def scrape():
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
     data = fetch_data()
     write_output(data)
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
 
 scrape()
