f5ffdd4acf2fbaa2cc20a89711c7a3a6d310ad5d
Quincy <qcalexander@gmail.com>
2020-11-17 23:38:49 -0400

    regissalons_com fixed

--- apify/ggrahambaker/storelocator/regissalons_com/scrape.py
+++ apify/ggrahambaker/storelocator/regissalons_com/scrape.py
@@ -2,12 +2,8 @@ import csv
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import json
-import sgzip 
-from sglogging import SgLogSetup
-
-logger = SgLogSetup().get_logger('regissalons_com')
-
-
+import sgzip
+from sgzip import DynamicGeoSearch, SearchableCountries
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
@@ -23,36 +19,39 @@ def fetch_data():
     session = SgRequests()
     HEADERS = { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36' }
     locator_domain = 'https://www.regissalons.com/'
-    search = sgzip.ClosestNSearch()
-    search.initialize(country_codes = ['ca', 'us'])
 
     MAX_RESULTS = 25
     MAX_DISTANCE = 500
 
-    coord = search.next_coord()
     all_store_data = []
 
     dup_tracker = []
 
+    search = sgzip.DynamicGeoSearch(country_codes=[SearchableCountries.USA, SearchableCountries.CANADA],max_radius_miles=MAX_DISTANCE, max_search_results=MAX_RESULTS)
+
+    search.initialize()
+
+    coord = search.next()
+
     while coord:
-        #logger.info("remaining zipcodes: " + str(search.zipcodes_remaining()))
         x = coord[0]
         y = coord[1]
-        #logger.info('Pulling Lat-Long %s,%s...' % (str(x), str(y)))
+
         url = 'https://www.regissalons.com/wp-admin/admin-ajax.php?action=store_search&lat=' + str(x) + '&lng=' + str(y) + '&max_results=' + str(MAX_RESULTS) + '&search_radius=' + str(MAX_DISTANCE) 
         r = session.get(url, headers=HEADERS)
         
         res_json = json.loads(r.content)
-
+        
         result_coords = []
-        result_coords.append((x, y))
-  
+        result_coords.append([x, y])
+
         for loc in res_json:
             lat = loc['lat']
             longit = loc['lng']
-            result_coords.append((lat, longit))
+            result_coords.append([lat, longit])
+            search.update_with(result_coords)
             
-            location_name = loc['store'].replace('&#8211;', '').replace('&#8217;', "'").replace('&#038;', '&')
+            location_name = loc['store'].replace('&#8211;', '-').replace('&#8217;', "'").replace('&#038;', '&').split("|")[0].strip()
            
             phone_number = loc['phone']
             if phone_number not in dup_tracker:
@@ -64,6 +63,8 @@ def fetch_data():
             city = loc['city']
             state = loc['state']
             zip_code = loc['zip']
+            if len(zip_code) < 5:
+                zip_code = "0" + zip_code
 
             if len(zip_code.split(' ')) == 2:
                 country_code = 'CA'
@@ -72,16 +73,19 @@ def fetch_data():
             
             store_number = loc['id']
             
-            hours_obj = loc['hours']
-            
-            soup = BeautifulSoup(hours_obj, 'html.parser')
-            
-            hours_table = soup.find_all('tr')
-            hours = ''
-            for row in hours_table:
-                tds = row.find_all('td')
-                for td in tds:
-                    hours += td.text + ' '
+            try:
+                hours_obj = loc['hours']
+                
+                soup = BeautifulSoup(hours_obj, 'html.parser')
+                
+                hours_table = soup.find_all('tr')
+                hours = ''
+                for row in hours_table:
+                    tds = row.find_all('td')
+                    for td in tds:
+                        hours += td.text + ' '
+            except:
+                hours = '<MISSING>'
 
             page_url = loc['permalink']
             
@@ -93,10 +97,8 @@ def fetch_data():
             all_store_data.append(store_data)
             
         if len(res_json) == 0:
-            search.max_distance_update(MAX_DISTANCE)
-        else:
-            search.max_count_update(result_coords)
-        coord = search.next_coord()  
+            search.update_with(result_coords)
+        coord = search.next()
 
     return all_store_data
 
