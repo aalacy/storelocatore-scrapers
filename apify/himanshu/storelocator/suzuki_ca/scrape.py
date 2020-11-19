9391bef8f8e3821a9785fbfc93891016e892db93
vivek <vivek@rentechdigital.com>
2020-11-19 18:38:11 +0530

    done

--- apify/himanshu/storelocator/suzuki_ca/scrape.py
+++ apify/himanshu/storelocator/suzuki_ca/scrape.py
@@ -9,7 +9,7 @@ def write_output(data):
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","raw_address"])
+        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
