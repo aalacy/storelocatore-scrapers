6250bacb2e19a20d6ac8171982bf4c654d75bd04
datarhan <datarhan@github.com>
2020-11-19 18:01:42 +0100

    Added new spider for dollartree.com

---
+++ apify/datarhan/storelocator/dollartree_com/scrape.py
@@ -0,0 +1,118 @@
+import csv
+import json
+import sgzip
+import urllib.parse
+from lxml import etree
+from sgzip import SearchableCountries
+
+from sgrequests import SgRequests
+
+
+def write_output(data):
+    with open('data.csv', mode='w', encoding='utf-8') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    # Your scraper here
+    session = SgRequests()
+
+    items = []
+    scraped_items = []
+
+    DOMAIN = 'dollartree.com'
+
+    start_url = 'https://hosted.where2getit.com/dollartree/rest/locatorsearch?lang=en_US'
+    hdr = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+
+    all_codes = []
+    us_zips = sgzip.for_radius(radius=200, country_code=SearchableCountries.USA)
+    for zip_code in us_zips:
+        all_codes.append(zip_code)
+    ca_zips = sgzip.for_radius(radius=200, country_code=SearchableCountries.CANADA)
+    for zip_code in ca_zips:
+        all_codes.append(zip_code)
+
+    for code in all_codes:
+        body = '{"request":{"appkey":"134E9A7A-AB8F-11E3-80DE-744E58203F82","formdata":{"geoip":false,"dataview":"store_default","limit":1000,"order":"_DISTANCE","geolocs":{"geoloc":[{"addressline":"%s","country":"","latitude":"","longitude":""}]},"searchradius":"500","radiusuom":"","where":{"icon":{"eq":""},"ebt":{"eq":""},"freezers":{"eq":""},"crafters_square":{"eq":""},"snack_zone":{"eq":""},"distributioncenter":{"distinctfrom":"1"}},"false":"0"}}}'
+        response = session.post(start_url, data=body % code, headers=hdr)
+        
+        data = json.loads(response.text)
+    
+        if not data['response'].get('collection'):
+            continue
+
+        for poi in data['response']['collection']:
+            store_url = '<MISSING>'
+            location_name = poi['name']
+            location_name = location_name if location_name else '<MISSING>'
+            street_address = poi['address1']
+            if poi['address2']:
+                street_address += ', ' + poi['address2']
+            if poi['address3']:
+                street_address += ', ' + poi['address3']
+            street_address = street_address if street_address else '<MISSING>'
+            city = poi['city']
+            city = city if city else '<MISSING>'
+            state = poi['state']
+            state = state if state else '<MISSING>'
+            zip_code = poi['postalcode']
+            zip_code = zip_code if zip_code else '<MISSING>'
+            country_code = poi['country']
+            country_code = country_code if country_code else '<MISSING>'
+            store_number = poi['uid']
+            store_number = store_number if store_number else '<MISSING>'
+            phone = poi['phone']
+            phone = phone if phone else '<MISSING>'
+            location_type = '<MISSING>'
+            location_type = location_type if location_type else '<MISSING>'
+            latitude = poi['latitude']
+            latitude = latitude if latitude else '<MISSING>'
+            longitude = poi['longitude']
+            longitude = longitude if longitude else '<MISSING>'
+            hours_of_operation = poi['hours'] 
+            if poi['hours2']:
+                hours_of_operation += ', ' + poi['hours2']
+            hours_of_operation = hours_of_operation if hours_of_operation else '<MISSING>'
+            
+            item = [
+                DOMAIN,
+                store_url,
+                location_name,
+                street_address,
+                city,
+                state,
+                zip_code,
+                country_code,
+                store_number,
+                phone,
+                location_type,
+                latitude,
+                longitude,
+                hours_of_operation
+            ]
+
+            if store_number not in scraped_items:
+                scraped_items.append(store_number)
+                items.append(item)
+        
+    return items
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+if __name__ == "__main__":
+    scrape()
