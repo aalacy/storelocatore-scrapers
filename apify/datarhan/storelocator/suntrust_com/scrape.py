80b0c3a76e4a16b91abf7cb8161a939c71afb643
datarhan <datarhan@github.com>
2020-11-20 23:05:50 +0100

    Added new spider for suntrust.com

---
+++ apify/datarhan/storelocator/suntrust_com/scrape.py
@@ -0,0 +1,103 @@
+import csv
+import json
+import sgzip
+import urllib.parse
+from lxml import etree
+from sgzip import SearchableCountries
+
+from sgrequests import SgRequests
+
+
+def write_output(data):
+    with open('data.csv', mode='w', encoding='utf-8') as output_file:
+        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Header
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    # Your scraper here
+    session = SgRequests()
+
+    items = []
+    scraped_items = []
+
+    DOMAIN = 'suntrust.com'
+    
+    all_codes = []
+    us_zips = sgzip.for_radius(radius=200, country_code=SearchableCountries.USA)
+    for zip_code in us_zips:
+        all_codes.append(zip_code)
+
+    start_url = 'https://www.mapquestapi.com/search/v2/radius?origin={}&radius=3000&maxMatches=500&ambiguities=ignore&hostedData=mqap.32547_SunTrust_Branch_Loc&outFormat=json&key=Gmjtd|lu6zn1ua2d,70=o5-l0850'
+    for code in all_codes:
+        response = session.get(start_url.format(code))
+        data = json.loads(response.text)
+        if not data.get('searchResults'):
+            continue
+
+        for poi in data['searchResults']:
+            location_name = poi['name']
+            location_name = location_name if location_name else '<MISSING>'
+            street_address = poi['fields']['address']
+            street_address = street_address if street_address else '<MISSING>'
+            city = poi['fields']['city']
+            city = city if city else '<MISSING>'
+            state = poi['fields']['state']
+            state = state if state else '<MISSING>'
+            zip_code = poi['fields']['postal']
+            zip_code = zip_code if zip_code else '<MISSING>'
+            country_code = poi['fields']['country']
+            country_code = country_code if country_code else '<MISSING>'
+            store_number = poi['fields']['RecordId']
+            store_number = store_number if store_number else '<MISSING>'
+            phone = poi['fields']['Phone_Published']
+            phone = phone if phone else '<MISSING>'
+            location_type = ''
+            location_type = location_type[0] if location_type else '<MISSING>'
+            latitude = poi['fields']['mqap_geography']['latLng']['lat']
+            latitude = latitude if latitude else '<MISSING>'
+            longitude = poi['fields']['mqap_geography']['latLng']['lng']
+            longitude = longitude if longitude else '<MISSING>'
+            hours_of_operation = poi['fields']['Hours_Lobby_For_VRU']
+            hours_of_operation = hours_of_operation if hours_of_operation else '<MISSING>'
+
+            store_url = 'https://www.suntrust.com/branch/{}/{}/{}/{}?location={}'
+            location = '{} {} {}'.format(city, poi['fields']['region_code'], zip_code)
+            store_url = store_url.format(state, city.replace(' ', '-'), zip_code, poi['fields']['N'], location)
+            
+            item = [
+                DOMAIN,
+                store_url,
+                location_name,
+                street_address,
+                city,
+                state,
+                zip_code,
+                country_code,
+                store_number,
+                phone,
+                location_type,
+                latitude,
+                longitude,
+                hours_of_operation
+            ]
+
+            if location_name not in scraped_items:
+                scraped_items.append(location_name)
+                items.append(item)
+        
+    return items
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+if __name__ == "__main__":
+    scrape()
