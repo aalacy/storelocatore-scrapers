6be26caed57e955762b6178a9dd4b014ebe3a046
Quincy <qcalexander@gmail.com>
2020-11-21 05:29:16 -0400

    pakmail_com fixed

--- apify/quincya/storelocator/pakmail_com/scrape.py
+++ apify/quincya/storelocator/pakmail_com/scrape.py
@@ -16,7 +16,7 @@ def write_output(data):
 
 def fetch_data():
 	
-	base_link = "https://www.pakmail.com/store-locator?origin=NY&lat=40.7127753&long=-74.0059728&distance[search_distance]=5000&Submit=Find+Now"
+	base_link = 'https://www.pakmail.com/store-locator'
 
 	user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
 	HEADERS = {'User-Agent' : user_agent}
@@ -25,51 +25,55 @@ def fetch_data():
 	req = session.get(base_link, headers = HEADERS)
 	base = BeautifulSoup(req.text,"lxml")
 
-	all_scripts = base.find_all('script')
-	for script in all_scripts:
-		if '"markers":' in str(script):
-			script = str(script)
-			break
+	items = base.find_all(class_="layout-3col blockish address-list")
 	
-	all_links = []	
-	ids = re.findall(r'us[0-9]+', script)
-	
-	for id_ in ids:
-		link = "https://www.pakmail.com/" + id_
-		if link not in all_links:
-			all_links.append(link)
-
 	data = []
 	locator_domain = "pakmail.com"
 
-	for link in all_links:
+	for item in items:
+		link = "https://www.pakmail.com" + item.a["href"]
 		req = session.get(link, headers = HEADERS)
 		base = BeautifulSoup(req.text,"lxml")
 
 		try:
 			script = base.find('script', attrs={'type': "application/ld+json"}).text.replace('\n', '').strip()
-		except:
-			continue
-		store = json.loads(script)
+			store = json.loads(script)
 
-		location_name = store['name']
-		street_address = store['address']['streetAddress'].replace("Shopping Center","").split("(")[0].strip()
-		city = store['address']['addressLocality']
-		state = "<MISSING>"
-		zip_code = store['address']['postalCode']
-		if len(zip_code) < 5:
-			zip_code = "0" + zip_code
+			location_name = store['name']
+			street_address = store['address']['streetAddress'].replace("Shopping Center","").split("(")[0].strip()
+			city = store['address']['addressLocality']
+			state = store['address']['addressRegion']
+			zip_code = store['address']['postalCode']
+			if len(zip_code) < 5:
+				zip_code = "0" + zip_code
+			phone = store['telephone']
+
+			latitude = store['geo']['latitude']
+			longitude = store['geo']['longitude']
+
+			hours_of_operation = " ".join(list(base.find(class_="hours").stripped_strings))
+			final_link = store['url']
+		except:
+			location_name = "Pak Mail " + item.h2.text.strip()
+			street_address = list(item.find(class_="layout-3col__col-2").stripped_strings)[0].strip()
+			city = item.h2.text.strip().split(",")[0]
+			state = item.h2.text.strip().split(",")[1].strip()
+			zip_code = list(item.find(class_="layout-3col__col-2").stripped_strings)[1][-5:].strip()
+			phone = item.find(class_="layout-3col__col-3").text.split("[")[0].strip()
+			final_link = req.url
+			try:
+				latitude = base.find('meta', attrs={'name': "geo.position"})['content'].split(";")[0]
+				longitude = base.find('meta', attrs={'name': "geo.position"})['content'].split(";")[1]
+				hours_of_operation = " ".join(list(base.find(id="block-views-store-hours-block-block").find(class_="view-content").stripped_strings))
+			except:
+				latitude = "<MISSING>"
+				longitude = "<MISSING>"
+				hours_of_operation = "<MISSING>"
+				final_link = base_link
 
 		country_code = "US"
-		store_number = link.split("/")[-1].split("us")[1]
+		store_number = link.split("/")[-1].split("US")[1]
 		location_type = "<MISSING>"
-		phone = store['telephone']
-
-		latitude = store['geo']['latitude']
-		longitude = store['geo']['longitude']
-
-		hours_of_operation = " ".join(list(base.find(class_="hours").stripped_strings))
-		final_link = store['url']
 
 		data.append([locator_domain, final_link, location_name, street_address, city, state, zip_code, country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation])
 
