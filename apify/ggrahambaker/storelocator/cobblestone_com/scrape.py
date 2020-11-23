003e31df4fe51906f7a54d6029427576996387a8
Michael Dinh <mkdinh94@gmail.com>
2020-11-23 11:48:27 -0500

    SLC-3960/cobblestone_com: rewrite to use api endpoint

--- apify/ggrahambaker/storelocator/cobblestone_com/scrape.py
+++ apify/ggrahambaker/storelocator/cobblestone_com/scrape.py
@@ -1,18 +1,22 @@
 import csv
-from sgselenium import SgChrome
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
+
+fields = ["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"]
+session = SgRequests()
+
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(fields)
         # Body
         for row in data:
             writer.writerow(row)
+
 
 def addy_ext(addy):
     address = addy.split(',')
@@ -22,52 +26,89 @@ def addy_ext(addy):
     zip_code = state_zip[1]
     return city, state, zip_code
 
+
+MISSING = '<MISSING>'
+
+
+def get(data, key):
+    return data.get(key) or MISSING
+
+
+def extract(location):
+    page_url = get(location, 'url')
+    location_name = get(location, 'post_title')
+    location_type = MISSING
+    store_number = get(location, 'ID')
+    phone = get(location, 'phone')
+
+    street_address = get(location, 'address')
+    city = get(location, 'city')
+    state = get(location, 'state')
+    postal = get(location, 'zip')
+    country_code = get(location, 'country')
+    latitude = get(location, 'lat')
+    longitude = get(location, 'lng')
+
+    hours_of_operation = get_hours(page_url)
+
+    return {
+        'locator_domain': 'cobblestone.com',
+        'page_url': page_url,
+        'location_name': location_name,
+        'location_type': location_type,
+        'store_number': store_number,
+        'phone': phone,
+        'street_address': street_address,
+        'city': city,
+        'state': state,
+        'zip': postal,
+        'country_code': country_code,
+        'latitude': latitude,
+        'longitude': longitude,
+        'hours_of_operation': hours_of_operation
+    }
+
+
+def get_hours(page_url):
+    choices = ['Full Service', 'Express Service', 'Oil And Lube', 'Covenience Store', 'Gas Station']
+    page = session.get(page_url).text
+    soup = BeautifulSoup(page)
+
+    hours = soup.select_one('.location__hours')
+
+    for choice in choices:
+        nav_string = hours.find(text=re.compile(choice, re.IGNORECASE))
+        if nav_string:
+            hour = nav_string.parent.findNext('dd')
+            if hour:
+                open_close = hour.get_text().strip()
+                if open_close:
+                    return re.sub('\n', ',', open_close)
+
+    return MISSING
+
+
 def fetch_data():
     locator_domain = 'https://cobblestone.com/'
     ext = 'locations/'
 
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36'
-    HEADERS = {'User-Agent' : user_agent}
+    headers = {'User-Agent': user_agent}
+    params = {
+        'sm-xml-search': 1,
+        'query_type': 'all'
+    }
 
-    session = SgRequests()
+    locations = session.get('https://cobblestone.com/', params=params, headers=headers).json()
 
-    driver = SgChrome().chrome()
-    driver.get(locator_domain + ext)
+    for location in locations:
+        data = extract(location)
+        yield [data[field] for field in fields]
 
-    all_store_data = []
-    div = driver.find_element_by_css_selector('div#results')
-    locations = div.find_elements_by_css_selector('div.result')
-    for loc in locations:
-        link = loc.find_element_by_css_selector('.result_website>a').get_attribute('href')
-        line_split = loc.text.split('\n')
-        location_name = line_split[0][:-10]
-        street_address = line_split[1]
-        city, state, zip_code = addy_ext(line_split[2])        
-        href_link = loc.find_element_by_css_selector('a.result_directions').get_attribute('href')
-        start_idx = href_link.find('&daddr')
-        end_idx = href_link.find('(')
-        coords = href_link[start_idx + 7:end_idx].split(',')
-        lat = coords[0]
-        longit = coords[1]
-        country_code = 'US'
-        store_number = '<MISSING>'
-        location_type = '<MISSING>'
-        hours = '<INACCESSIBLE>'
-        phone_number = line_split[3].replace('Phone:', '').strip()
-        if "-" not in phone_number:
-            req = session.get(link, headers = HEADERS)
-            base = BeautifulSoup(req.text,"lxml")
-            phone_number = base.find(class_="location__description").find_all("a")[-1].text
-
-        store_data = [locator_domain, link, location_name, street_address, city, state, zip_code, country_code,
-                      store_number, phone_number, location_type, lat, longit, hours]
-        all_store_data.append(store_data)
-
-    driver.quit()
-    return all_store_data
 
 def scrape():
     data = fetch_data()
     write_output(data)
+
 
 scrape()
