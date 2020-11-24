603961adfe37ec48d91e3e3670dc0fad0d8804e5
vivek <vivek@rentechdigital.com>
2020-11-24 14:10:58 +0530

    done

--- apify/himanshu/storelocator/amerisbank_com/scrape.py
+++ apify/himanshu/storelocator/amerisbank_com/scrape.py
@@ -4,6 +4,7 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
+import html5lib
 session = SgRequests()
 
 def write_output(data):
@@ -28,9 +29,11 @@ def fetch_data():
 
             for branch_link in BeautifulSoup(session.get(bank_city_link.get('href')).content, 'lxml').find_all("a",{"linktrack":"Landing page"}):
                 page_url = branch_link.get('href')
-                soup = BeautifulSoup(session.get(page_url).content, 'lxml')
-                
-                json_data = json.loads(soup.find(lambda tag:(tag.name == "script") and '"streetAddress"' in tag.text).text.replace("//if applied, use the tmpl_var to retrieve the database value",""))
+                soup = BeautifulSoup(session.get(page_url).content, 'html5lib')
+                data = (soup.find_all("script",{"type":"application/ld+json"})[-1]).text
+                data1 = (data.replace("//if applied, use the tmpl_var to retrieve the database value",""))
+
+                json_data = json.loads(data1)
                 
                 location_name = soup.find("div",{"id":"branchName"}).text.strip()
                 street_address = json_data['address']['streetAddress']
@@ -46,12 +49,14 @@ def fetch_data():
                 location_type = json_data['@type']
                 lat = json_data['geo']['latitude']
                 lng = json_data['geo']['longitude']
-
-                hours = "Branch Hours "+ " ".join(list(soup.find("table",{"id":"hoursTable"}).stripped_strings))
-                if soup.find("p",{"id":"driveDesc"}):
-                    hours = hours
-                else:
-                    hours+= " Drive-Thru Hours "+" ".join(list(soup.find("table",{"id":"hoursTableDT"}).stripped_strings))
+                try:
+                    hours = " ".join(list(soup.find("div",{"class":"flexRight hideATMNo"}).stripped_strings))
+                    
+                except:
+                    try:
+                        hours = " ".join(list(soup.find("div",{"class":"flexRight hideATM"}).stripped_strings))
+                    except:
+                        hours = " ".join(list(soup.find("div",{"class":"flexRight"}).stripped_strings))
                 if "n/a" in hours or "N/A" in hours:
                     hours = "<MISSING>"
                 store = []
@@ -67,21 +72,14 @@ def fetch_data():
                 store.append(location_type)
                 store.append(lat)
                 store.append(lng)
-                store.append(hours)
+                store.append(hours.replace("Drive-Thru Hours Monday: Drive-Thru Service Not Available Tuesday: Drive-Thru Service Not Available Wednesday: Drive-Thru Service Not Available Thursday: Drive-Thru Service Not Available Friday: Drive-Thru Service Not Available Saturday: Drive-Thru Service Not Available Sunday: Drive-Thru Service Not Available ","").replace("HOURS ","").replace(" Drive-Thru Hours",", Drive-Thru Hours"))
                 store.append(page_url)
                 if store[2] in addresses:
                     continue
-                addresses.append(store[2])     
-            
-                store = [str(x).strip() if x else "<MISSING>" for x in store]
-                yield store
-                
-                
-
-    
-
+                addresses.append(store[2])   
+                store = [str(x).encode('ascii', 'ignore').decode('ascii').strip() if x else "<MISSING>" for x in store]
+                yield store   
 def scrape():
     data = fetch_data()
     write_output(data)
-
 scrape()
