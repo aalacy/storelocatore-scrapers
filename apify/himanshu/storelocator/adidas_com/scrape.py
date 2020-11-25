9ace94407bbe88dab37933d8c3b0c1566fd31081
vivek <vivek@rentechdigital.com>
2020-11-25 18:05:26 +0530

    done

--- apify/himanshu/storelocator/adidas_com/scrape.py
+++ apify/himanshu/storelocator/adidas_com/scrape.py
@@ -20,7 +20,7 @@ def fetch_data():
         "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
     }
     base_url = "https://www.adidas.com"
-    r = session.get("https://placesws.adidas-group.com/API/search?brand=adidas&geoengine=google&method=get&category=store&latlng=32.13574945925331%2C-154.41568025677128%2C15000&page=1&pagesize=3000&fields=name%2Cstreet1%2Cstreet2%2Caddressline%2Cbuildingname%2Cpostal_code%2Ccity%2Cstate%2Cstore_o+wner%2Ccountry%2Cstoretype%2Clongitude_google%2Clatitude_google%2Cstore_owner%2Cstate%2Cperformance%2Cbrand_store%2Cfactory_outlet%2Coriginals%2Cneo_label%2Cy3%2Cslvr%2Cchildren%2Cwoman%2Cfootwear%2Cfootball%2Cbasketball%2Coutdoor%2Cporsche_design%2Cmiadidas%2Cmiteam%2Cstella_mccartney%2Ceyewear%2Cmicoach%2Copening_ceremony%2Coperational_status%2Cfrom_date%2Cto_date%2Cdont_show_country&format=json&storetype=",headers=headers)
+    r = session.get("https://placesws.adidas-group.com/API/search?brand=adidas&geoengine=google&method=get&category=store&latlng=39.491751305282705%2C-471.94216860348604%2C15000&page=1&pagesize=50000&fields=name%2Cstreet1%2Cstreet2%2Caddressline%2Cbuildingname%2Cpostal_code%2Ccity%2Cstate%2Cstore_o+wner%2Ccountry%2Cstoretype%2Clongitude_google%2Clatitude_google%2Cstore_owner%2Cstate%2Cperformance%2Cbrand_store%2Cfactory_outlet%2Coriginals%2Cneo_label%2Cy3%2Cslvr%2Cchildren%2Cwoman%2Cfootwear%2Cfootball%2Cbasketball%2Coutdoor%2Cporsche_design%2Cmiadidas%2Cmiteam%2Cstella_mccartney%2Ceyewear%2Cmicoach%2Copening_ceremony%2Coperational_status%2Cfrom_date%2Cto_date%2Cdont_show_country&format=json&storetype=",headers=headers)
     soup = BeautifulSoup(r.text,'html5lib')
     location_data = json.loads(str(soup).split("<body>")[1].split("</body>")[0].replace("<br>","").replace("\n",""))['wsResponse']["result"]
     for store_data in location_data:
@@ -37,14 +37,21 @@ def fetch_data():
                 address = address + " " + store_data["addressline"] 
             store.append(address.replace("&amp;","&").replace(", San Antonio Tx, 78209","").replace("RICHFIELD, UTAH 84701","<MISSING>"))
             store.append(store_data["city"])
-            store.append(store_data["state"].replace("British Colombia","BC") if "state" in store_data else "<MISSING>")
+            st_list = ['British','Colombia','BRITISH','COLUMBIA','BRITISH COLUMBIA','British Colombia']
+            try:
+                state = store_data["state"]
+            except:
+                state = "<MISSING>"
+            if state in st_list:
+                state = "BC"
+            store.append(state if state else "<MISSING>")
             if 'postal_code' in store_data:
                 if 'US' in store_data["country"]:
                     try:
-                        temp_zipp = store_data["postal_code"].split(",")[1].strip()
+                        temp_zipp = store_data["postal_code"].split(",")[1].strip().replace("VA","")
                     except:
-                        temp_zipp = store_data["postal_code"]
-                    zipp = temp_zipp[:5].replace("TX 77","77056").replace("CA 90","90045").replace("CA 92","92626")
+                        temp_zipp = store_data["postal_code"].split(" ")[-1].replace("VA","")
+                    zipp = temp_zipp[:5].replace("TX 77","77056").replace("CA 90","90045").replace("CA 92","92626").replace("MI 48","48242").replace("NC 28","28204").replace("1224","98944")
                 else:
                     zipp = store_data["postal_code"].upper().replace("BC ","")
             else:
@@ -65,8 +72,25 @@ def fetch_data():
                 phone = "<MISSING>"
             store.append(phone if phone else "<MISSING>")
             store.append(location_data['storetype'] if 'storetype' in location_data else "<MISSING>")
-            store.append(location_data["latitude_google"])
-            store.append(location_data["longitude_google"])
+            try:
+                try:
+                    lat = location_data["latitude_bing"]
+                    lng = location_data["longitude_bing"]
+                except KeyError:
+                    lat = location_data["latitude_google"]
+                    lng = location_data["longitude_google"]
+            except:
+                lat = "<MISSING>"
+                lng = "<MISSING>"
+            if lat == "0":
+                lat = "<MISSING>"
+            if lng == "0":
+                lng = "<MISSING>"
+                
+            if lat=="38":
+                lat = "38.001954"
+            store.append(lat)
+            store.append(lng)
             hours = ""
             for key in location_data:
                 if "openinghours" in key:
@@ -74,7 +98,7 @@ def fetch_data():
             store.append(hours if hours != "" else "<MISSING>")
             store.append("<MISSING>")
             store = [x.replace("–","-") if type(x) == str else x for x in store]
-            store = [x.strip() if type(x) == str else x for x in store]
+            store = [x.encode('ascii', 'ignore').decode('ascii').strip() if type(x) == str else x for x in store]
             if store[2] in addressess:
                 continue
             addressess.append(store[2])
@@ -83,4 +107,3 @@ def scrape():
     data = fetch_data()
     write_output(data)
 scrape()
-
