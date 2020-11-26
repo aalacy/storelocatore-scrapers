f8449ecaa2b79fa074a699f35ce4796ba2159781
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2020-11-26 14:16:39 +0500

    Add files via upload

--- apify/himanshu/storelocator/justtires_com/scrape.py
+++ apify/himanshu/storelocator/justtires_com/scrape.py
@@ -1,77 +1,83 @@
-import csv
-from sgrequests import SgRequests
 from bs4 import BeautifulSoup
-import re
-import json
-from sglogging import SgLogSetup
+import csv
+import string
+import re, time, json
 
-logger = SgLogSetup().get_logger('justtires_com')
-
-
-
+from sgrequests import SgRequests
 
 session = SgRequests()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+           }
 
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
+        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
         # Body
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-    }
-    return_main_object = []
-    base_url = "https://www.justtires.com"
-    try:
-        r = session.get("https://www.justtires.com/en-US/service-center-near-me",headers=headers)
-        soup = BeautifulSoup(r.text,"lxml")
-        for state in soup.find("div",{'class':"browse-by-state-wrapper__list"}).find_all("a"):
-            # logger.info(base_url + state["href"])
-            state_request = session.get(base_url + state["href"],headers=headers)
-            state_soup = BeautifulSoup(state_request.text,"lxml")
-            for city in state_soup.find("div",{'class':"browse-by-state-wrapper__list"}).find_all("a"):
-                # logger.info(base_url + city["href"])
-                city_request = session.get(base_url + city["href"],headers=headers)
-                city_soup = BeautifulSoup(city_request.text,"lxml")
-                for location in city_soup.find_all("li",{"class":'store-results__results__item'}):
-                    page_url = base_url + location.find("a")["href"]
-                    #logger.info(page_url)
-                    if location.find("p",{"class":"my-store__direction"}) == None:
-                        continue
-                    address = json.loads(location.find("p",{"class":"my-store__direction"})["data-location"])
-                    location_request = session.get(page_url,headers=headers)
-                    location_soup = BeautifulSoup(location_request.text,"lxml")
-                    name = location_soup.find("span",{'itemprop':"name"}).text.strip()
-                    store_id = location_soup.find("div",{"data-mobile-store":"store"})["data-store-id"]
-                    phone = location_soup.find("span",{'itemprop':"telephone"}).text.strip()
-                    store = []
-                    store.append("https://www.justtires.com")
-                    store.append(name)
-                    store.append(address["street"])
-                    store.append(address["city"])
-                    store.append(address["state"])
-                    store.append(address["zipCode"])
-                    store.append(address["country"])
-                    store.append(store_id)
-                    store.append(phone if phone else "<MISSING>")
-                    store.append("<MISSING>")
-                    store.append(address["latitude"])
-                    store.append(address["longitude"])
-                    hours = " ".join(list(location_soup.find("div",{"class":"store-page-masthead__store__wrapper"}).find("div",{"class":"right-column"}).stripped_strings))
-                    store.append(hours.replace('Closed Now ','') if hours else "<MISSING>")
-                    store.append(page_url)
-                    # logger.info(store)
-                    yield store
-    except:
-        pass
-def scrape():
+    data = []
+    p = 0
+    pattern = re.compile(r'\s\s+')
+    cleanr = re.compile(r'<[^>]+>')
+    page = 0
+    while True:
+        url = 'https://www.justtires.com/en-US/shop/_jcr_content/content/store_results.content?currentPage='+str(page)+'&latitude=32.3182314&longitude=-86.902298'
+        r = session.get(url, headers=headers, verify=False)
+        soup =BeautifulSoup(r.text, "html.parser")
+        try:
+            divlist = soup.find('ul', {'class': "store-results__results"}).findAll('li')        
+        except:
+            break
+        for div in divlist:
+            title = div.find('h4').text.replace('\n','')
+            link = 'https://www.justtires.com'+div.find('h4').find('a')['href']
+            content = div.find('div',{'class':'nav-my-store__information-area'}).find('p')['data-location']
+            content = json.loads(content)
+            street = content['street']
+            city = content['city']
+            state = content['state']
+            pcode = content['zipCode']
+            ccode = content['country']
+            lat = content['latitude']
+            longt = content['longitude']            
+            hours = 'Mon - Sun ' + div.find('p',{'class':"nav-my-store__schedule"}).text
+            phone = div.find('p',{'class':'telephone'}).text.replace('\n','')
+            store = div.find('input',{'name':'storeId'})['value']
+            data.append([
+                        'https://www.justtires.com/',
+                        link,                   
+                        title,
+                        street,
+                        city,
+                        state,
+                        pcode,
+                        'US',
+                        store,
+                        phone,
+                        '<MISSING>',
+                        lat,
+                        longt,
+                        hours
+                    ])
+            #print(p,data[p])
+            p += 1
+         #   input()
+           
+        page = page + 1
+        
+    
+
+    return data
+
+
+def scrape(): 
     data = fetch_data()
     write_output(data)
-
+  
 scrape()
