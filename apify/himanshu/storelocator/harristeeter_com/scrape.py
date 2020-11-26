269e0071de1b2c22adada64af8f035f85a7dd825
vivek <vivek@rentechdigital.com>
2020-11-26 17:59:30 +0530

    done

--- apify/himanshu/storelocator/harristeeter_com/scrape.py
+++ apify/himanshu/storelocator/harristeeter_com/scrape.py
@@ -4,28 +4,27 @@ from bs4 import BeautifulSoup as bs
 import re
 import json
 import unicodedata
+import html5lib
 session = SgRequests()
 def write_output(data):
     with open('data.csv', mode='w',newline='') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
         # Header
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
         # Body
         for row in data:
             writer.writerow(row)
 def fetch_data():
-
     addressess =[]
     url = "https://locations.harristeeter.com/"
-    sup = bs(session.get(url).text,'lxml')
+    sup = bs(session.get(url).text,'html5lib')
     for td in sup.find("div",{"id":"state_list"}).find_all("li"):
-        link = bs(session.get(td.find("a")['href']).text,'lxml')
+        link = bs(session.get(td.find("a")['href']).text,'html5lib')
         for href in link.find_all("div",{"class":"city_item"}):
-            sub_page = bs(session.get(href.find("a")['href']).text,'lxml')
+            sub_page = bs(session.get(href.find("a")['href']).text,'html5lib')
             for pag_url in sub_page.find("div",{"id":"locations"}).find_all("a",{"linktrack":re.compile("Location page")}):
                 page_urls = pag_url['href']
-                store_sup = bs(session.get(page_urls).text,'lxml')
+                store_sup = bs(session.get(page_urls).text,'html5lib')
                 data = json.loads(store_sup.find_all("script",{"type":"application/ld+json"})[-1].text.strip().replace("// if the location file does not have the hours separated into open/close for each day, remove the below section",''))
                 location_name = data['name']
                 street_address = data['address']['streetAddress']
@@ -62,10 +61,7 @@ def fetch_data():
                     continue
                 addressess.append(str(store[2]+store[-1]))
                 yield store
-
 def scrape():
     data = fetch_data()
     write_output(data)
 scrape()
-
-
