6287e2769c184c3b206189074744f233d43e6851
Quincy <qcalexander@gmail.com>
2020-11-27 03:42:22 -0400

    thecamptc_com fixed

--- apify/coralisland/storelocator/thecamptc_com/scrape.py
+++ apify/coralisland/storelocator/thecamptc_com/scrape.py
@@ -1,12 +1,8 @@
 from bs4 import BeautifulSoup
 import csv
-import time
-from random import randint
 import re
 from sgrequests import SgRequests
-from lxml import etree
 
-from sgselenium import SgSelenium
 from sglogging import SgLogSetup
 
 logger = SgLogSetup().get_logger('thecamptc_com')
@@ -56,25 +52,32 @@ def fetch_data():
 
     output_list = []
     url = "https://www.thecamptc.com/locations.php"
-    source = session.get(url).text
-    response = etree.HTML(source)
-    store_list = response.xpath('//a[@class="location-btn"]')
+
+    req = session.get(url, headers = HEADERS)
+    base = BeautifulSoup(req.text,"lxml")
+
+    store_list = base.find_all(class_="location-btn")
+    
     for store_link in store_list:
-        city_state = eliminate_space(validate(store_link.xpath('.//text()')).split(' '))
-        store_link = validate(store_link.xpath('./@href'))
+        store_link = validate(store_link["href"])
         if "mexicali" in store_link:
             continue
         logger.info(store_link)
-        store = etree.HTML(session.get(store_link).text)
+        store = BeautifulSoup(session.get(store_link, headers = HEADERS).text,"lxml")
         output = []
-        raw_address = eliminate_space(store.xpath('.//div[@class="location-details"][1]//text()'))
+        try:
+            raw_address = list(store.find(class_='location-details').stripped_strings)
+        except:
+            continue
         if raw_address:
-            raw_hours = eliminate_space(store.xpath('.//div[@class="location-details"][2]//text()'))[1:]
-            phone = get_value(re.findall("\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}", ', '.join(raw_address))[0])                
+            raw_hours = eliminate_space(list(store.find_all(class_='location-details')[1].stripped_strings))[1:]
+            if "opening soon" in str(raw_hours).lower():
+                continue
+            phone = get_value(re.findall("[(\d)]{5} [\d]{3}-[\d]{4}", ', '.join(raw_address))[0])
             zipcode = get_value(re.findall("\d{5}", ', '.join(raw_address))[-1])
             output.append(base_url) # url
             output.append(store_link) # page_url
-            output.append(', '.join(city_state)) #location name
+            output.append(store.h1.text) #location name
             output.append(raw_address[1]) #address
             output.append(raw_address[2].split('\n')[0].replace(",","")) # city
             output.append(raw_address[2].split('\n')[1].strip()) # state
@@ -83,47 +86,13 @@ def fetch_data():
             output.append("<MISSING>") #store_number
             output.append(phone) #phone
             output.append("<MISSING>") #location type
-            geo_loc = validate(store.xpath('.//iframe/@src')).split('!2d')[1].split('!2m')[0].split('!3d')
+            geo_loc = validate(store.iframe["src"]).split('!2d')[1].split('!2m')[0].split('!3d')
             lat = geo_loc[1]
             if len(lat) > 30:
                 lat = lat[:lat.find("!")]
             output.append(lat) #latitude
             output.append(geo_loc[0]) #longitude
-            output.append(validate(' '.join(raw_hours))) #opening hours            
-        else:
-            if store_link == "https://thecamptc.com/locations/cool-springs-tennessee":
-                driver = SgSelenium().chrome()
-                time.sleep(2)
-
-                driver.get(store_link)
-                time.sleep(randint(6,8))
-
-                base = BeautifulSoup(driver.page_source,"lxml")
-                raw_address = base.footer.find(id="text-2").text.split("\n")
-                phone = raw_address[0].replace("CONTACT US","").strip()
-                raw_hours = base.footer.find(id="text-3").text.replace("CLASS HOURS","").replace("\n"," ").replace("\xa0","")
-                if "Pandemic" in raw_hours:
-                    raw_hours = raw_hours[raw_hours.find(":")+1:].strip()
-                zipcode = get_value(re.findall("\d{5}", ', '.join(raw_address))[-1])
-                output.append(base_url) # url
-                output.append(store_link) # page_url
-                output.append(', '.join(city_state)) #location name
-                output.append(raw_address[1].strip()) #address
-                output.append(raw_address[2].split(',')[0].replace(",","")) # city
-                output.append(raw_address[2].split(' ')[1].strip()) # state
-                output.append(zipcode) #zipcode
-                output.append('US') #country code
-                output.append("<MISSING>") #store_number
-                output.append(phone) #phone
-                output.append("<MISSING>") #location type
-                geo_loc = base.find("iframe")['src'].split('!2d')[1].split('!2m')[0].split('!3d')
-                lat = geo_loc[1]
-                if len(lat) > 30:
-                    lat = lat[:lat.find("!")]
-                output.append(lat) #latitude
-                output.append(geo_loc[0]) #longitude
-                output.append(raw_hours) #opening hours
-                driver.close()
+            output.append(validate(' '.join(raw_hours))) #opening hours 
         output_list.append(output)
 
     return output_list
