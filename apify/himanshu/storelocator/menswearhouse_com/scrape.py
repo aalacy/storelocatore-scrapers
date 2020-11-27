8ee24f3eb53f36ab432c787173f4edb5c307d776
vivek <vivek@rentechdigital.com>
2020-11-27 15:43:06 +0530

    done

--- apify/himanshu/storelocator/menswearhouse_com/scrape.py
+++ apify/himanshu/storelocator/menswearhouse_com/scrape.py
@@ -3,10 +3,8 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
-import sgzip
 import unicodedata
-from sglogging import SgLogSetup
-logger = SgLogSetup().get_logger('menswearhouse_com')
+
 session = SgRequests()
 def write_output(data):
     with open('data.csv', mode='w') as output_file:
@@ -16,14 +14,7 @@ def write_output(data):
         for row in data:
             writer.writerow(row)
 def fetch_data():
-    return_main_object = []
     addresses = []
-    search = sgzip.ClosestNSearch() # TODO: OLD VERSION [sgzip==0.0.55]. UPGRADE IF WORKING ON SCRAPER!
-    search.initialize()
-    MAX_RESULTS = 50
-    MAX_DISTANCE = 50
-    current_results_len = 0
-    zip_code = search.next_zip()
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
         "X-Requested-With": "XMLHttpRequest",
@@ -32,21 +23,13 @@ def fetch_data():
     location_url = "https://www.menswearhouse.com/sr/search/resources/store/12751/storelocator/byProximity?radius=50000000&zip=21216&city=&state=&brand=TMW&profileName=X_findStoreLocatorWithExtraFields"
     loc = session.get(location_url,headers=headers).json()
     locator_domain = base_url
-    location_name = ""
     street_address = ""
-    city = ""
-    state = ""
-    zipp = ""
     country_code = "US"
     store_number = ""
     phone = ""
     location_type = "Men's Wearhouse"
-    latitude = ""
-    longitude = ""
-    raw_address = ""
     hours_of_operation = ""
     street_address1 = ''
-    street_address2 = ''
     store_name=''
     if "DocumentList" in loc:
         current_results_len = len(loc['DocumentList']) 
@@ -67,15 +50,14 @@ def fetch_data():
                 phone = "<MISSING>"
             store = [locator_domain, store_name.capitalize(), street_address.replace(data['state_ntk'],"").lower(), data['city_ntk'].lower(), data['state_ntk'], data['zipcode_ntk'], country_code,
                     store_number, phone, location_type, data['latlong'].split(",")[0], data['latlong'].split(",")[1], hours_of_operation.replace("   ",","),page_url]
+            if store[2] + store[-3] in addresses:
+                continue
+            addresses.append(store[2] + store[-3])
             for i in range(len(store)):
                 if type(store[i]) == str:
                     store[i] = ''.join((c for c in unicodedata.normalize('NFD', store[i]) if unicodedata.category(c) != 'Mn'))
             store = [x.replace("–","-") if type(x) == str else x for x in store]
             store = [x.encode('ascii', 'ignore').decode('ascii').strip() if type(x) == str else x for x in store]
-            if store[2] + store[-3] in addresses:
-                continue
-            addresses.append(store[2] + store[-3])
-            
             yield store
 def scrape():
     data = fetch_data()
