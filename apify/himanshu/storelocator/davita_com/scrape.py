42fffc3e5cefbcf88a7d114eb7afd0d36863dc82
vivek <vivek@rentechdigital.com>
2020-11-28 12:26:17 +0530

    done

--- apify/himanshu/storelocator/davita_com/scrape.py
+++ apify/himanshu/storelocator/davita_com/scrape.py
@@ -1,56 +1,34 @@
 # coding=UTF-8
-
 import csv
-import requests
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
 import sgzip
-from sglogging import SgLogSetup
-
-logger = SgLogSetup().get_logger('davita_com')
 
 
 session = SgRequests()
-
-
 def write_output(data):
-    with open('data.csv', mode='w', encoding="utf-8") as output_file:
+    with open('data.csv', mode='w', encoding="utf-8",newline='') as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
         # Header
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
                          "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
         # Body
         for row in data:
             writer.writerow(row)
-
-
 def fetch_data():
     addresses = []
-    search = sgzip.ClosestNSearch() # TODO: OLD VERSION [sgzip==0.0.55]. UPGRADE IF WORKING ON SCRAPER!
-    search.initialize()
-    MAX_RESULTS = 100
-    MAX_DISTANCE = 25
-    current_results_len = 0  # need to update with no of count.
-    coord = search.next_coord()
-
+    coords = sgzip.coords_for_radius(25)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
         'Accept': 'application/json, text/plain, */*',
     }
-
     base_url = "https://www.davita.com/"
-
-    while coord:
+    for coord in coords:
         result_coords = []
-
         lat = coord[0]
         lng = coord[1]
-        # logger.info(search.current_zip)
-        # logger.info("remaining zipcodes: " + str(search.zipcodes_remaining()))
-        # logger.info('Pulling Lat-Long %s,%s...' % (str(lat), str(lng)))
         locator_domain = base_url
         location_name = ""
         street_address = ""
@@ -64,11 +42,9 @@ def fetch_data():
         latitude = ""
         longitude = ""
         hours_of_operation = ""
-        location_url = "https://www.davita.com/api/find-a-center?location="+str(search.current_zip)+"&lat="+str(lat)+"&lng="+str(lng)+"&modalities=0&p=0"
-
+        location_url = "https://www.davita.com/api/find-a-center?lat="+str(lat)+"&lng="+str(lng)+"&modalities=0&p=0"
         r = session.get(location_url, headers=headers, verify=False).json()
         if r['locations'] != None:
-            current_results_len = (len(r['locations'])) 
             for data in r['locations']:
                 location_name = data['facilityname']
                 street_address = (data['address']['address1'] +" "+ str(data['address']['adress2'])).strip()
@@ -79,40 +55,18 @@ def fetch_data():
                 longitude = data['address']['longitude']
                 store_number = data['facilityid']
                 phone = data['phone']
-                
                 page_url = "https://www.davita.com/locations/"+str(state.lower())+"/"+str(city.lower())+"/"+str(data['address']['address1'].replace(" ","-").lower())+"--"+str(store_number)
-                # logger.info(page_url)       
-    
                 result_coords.append((latitude, longitude))
                 store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
                         store_number, phone, location_type, latitude, longitude, hours_of_operation, page_url]
-
-                if str(store[2]) in addresses:
+                if str(store[1]) in addresses:
                     continue
-                addresses.append(str(store[2]))
-
+                addresses.append(str(store[1]))
                 store = [x.strip() if x else "<MISSING>" for x in store]
-
-                # logger.info("data = " + str(store))
-                # logger.info('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
                 yield store
         else:
             pass
-
-        if current_results_len < MAX_RESULTS:
-            # logger.info("max distance update")
-            search.max_distance_update(MAX_DISTANCE)
-        elif current_results_len == MAX_RESULTS:
-            # logger.info("max count update")
-            search.max_count_update(result_coords)
-        else:
-            raise Exception("expected at most " + str(MAX_RESULTS) + " results")
-        coord = search.next_coord()
-
-
 def scrape():
     data = fetch_data()
     write_output(data)
-
-
 scrape()
