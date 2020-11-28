454f41a4b6b4ed3addafefb427e7d44f41524e24
vivek <vivek@rentechdigital.com>
2020-11-28 16:22:14 +0530

    done

--- apify/himanshu/storelocator/adidas_com/scrape.py
+++ apify/himanshu/storelocator/adidas_com/scrape.py
@@ -3,10 +3,11 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
+import unicodedata
 session = SgRequests()
 
 def write_output(data):
-    with open('data.csv', mode='w',encoding="utf-8") as output_file:
+    with open('data.csv', mode='w', newline='', encoding="utf-8") as output_file:
         writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         # Header
         writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
@@ -19,7 +20,6 @@ def fetch_data():
     headers = {
         "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
     }
-    base_url = "https://www.adidas.com"
     r = session.get("https://placesws.adidas-group.com/API/search?brand=adidas&geoengine=google&method=get&category=store&latlng=39.491751305282705%2C-471.94216860348604%2C15000&page=1&pagesize=50000&fields=name%2Cstreet1%2Cstreet2%2Caddressline%2Cbuildingname%2Cpostal_code%2Ccity%2Cstate%2Cstore_o+wner%2Ccountry%2Cstoretype%2Clongitude_google%2Clatitude_google%2Cstore_owner%2Cstate%2Cperformance%2Cbrand_store%2Cfactory_outlet%2Coriginals%2Cneo_label%2Cy3%2Cslvr%2Cchildren%2Cwoman%2Cfootwear%2Cfootball%2Cbasketball%2Coutdoor%2Cporsche_design%2Cmiadidas%2Cmiteam%2Cstella_mccartney%2Ceyewear%2Cmicoach%2Copening_ceremony%2Coperational_status%2Cfrom_date%2Cto_date%2Cdont_show_country&format=json&storetype=",headers=headers)
     soup = BeautifulSoup(r.text,'html5lib')
     location_data = json.loads(str(soup).split("<body>")[1].split("</body>")[0].replace("<br>","").replace("\n",""))['wsResponse']["result"]
@@ -97,6 +97,9 @@ def fetch_data():
                     hours = hours + " " + key.split("_")[-1] + " " + location_data[key]
             store.append(hours if hours != "" else "<MISSING>")
             store.append("<MISSING>")
+            for i in range(len(store)):
+                if type(store[i]) == str:
+                    store[i] = ''.join((c for c in unicodedata.normalize('NFD', store[i]) if unicodedata.category(c) != 'Mn'))
             store = [x.replace("–","-") if type(x) == str else x for x in store]
             if store[2] in addressess:
                 continue
