1cfa41d154a378995bd1899e1bb313462f165f07
datarhan <datarhan@github.com>
2020-11-29 11:12:45 +0100

    [xpo.com] update after QA

--- apify/datarhan/storelocator/xpo_com/scrape.py
+++ apify/datarhan/storelocator/xpo_com/scrape.py
@@ -100,9 +100,8 @@ def fetch_data():
             longitude,
             hours_of_operation,
         ]
-        if location_name not in scraped_items:
-            scraped_items.append(location_name)
-            items.append(item)
+        
+        items.append(item)
 
     return items
 
