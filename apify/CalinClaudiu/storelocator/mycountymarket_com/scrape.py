259a7db859bca5f3085421969d06ed7425db4fff
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2020-12-01 16:27:39 +0100

    Update scrape.py

--- apify/CalinClaudiu/storelocator/mycountymarket_com/scrape.py
+++ apify/CalinClaudiu/storelocator/mycountymarket_com/scrape.py
@@ -1,7 +1,6 @@
 from sgscrape.simple_scraper_pipeline import SimpleScraperPipeline
 from sgscrape.simple_scraper_pipeline import ConstantField
 from sgscrape.simple_scraper_pipeline import MappingField
-from sgscrape.simple_scraper_pipeline import MultiMappingField
 from sgscrape.simple_scraper_pipeline import MissingField
 from sgscrape import simple_utils as utils
 from sgrequests import SgRequests
