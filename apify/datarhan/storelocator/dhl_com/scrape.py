88c999dc2f15bff795c7e32932bdcb3c478d4777
datarhan <datarhan@github.com>
2020-12-01 08:58:13 +0100

    [dhl.com] update after QA

--- apify/datarhan/storelocator/dhl_com/scrape.py
+++ apify/datarhan/storelocator/dhl_com/scrape.py
@@ -4,7 +4,6 @@ import sgzip
 from sgzip import SearchableCountries
 
 from sgrequests import SgRequests
-from sgselenium import SgFirefox
 
 
 def write_output(data):
@@ -47,14 +46,16 @@ def fetch_data():
     DOMAIN = "dhl.com"
     start_url_us = "https://wsbexpress.dhl.com/ServicePointLocator/restV3/servicepoints?servicePointResults=50&address=&countryCode=US&capability=80&weightUom=lb&dimensionsUom=in&latitude={}&longitude={}&languageScriptCode=Latn&language=eng&languageCountryCode=GB&resultUom=mi&key=963d867f-48b8-4f36-823d-88f311d9f6ef"
     start_url_ca = "https://wsbexpress.dhl.com/ServicePointLocator/restV3/servicepoints?servicePointResults=50&address=&countryCode=CA&capability=80&weightUom=lb&dimensionsUom=in&latitude={}&longitude={}&languageScriptCode=Latn&language=eng&languageCountryCode=GB&resultUom=mi&key=963d867f-48b8-4f36-823d-88f311d9f6ef"
-    session.get('https://locator.dhl.com/ServicePointLocator/restV3/appConfig')
-    
+    session.get("https://locator.dhl.com/ServicePointLocator/restV3/appConfig")
+
     all_requests = []
     us_coords = sgzip.coords_for_radius(radius=50, country_code=SearchableCountries.USA)
     for coord in us_coords:
         lat, lng = coord
         all_requests.append(start_url_us.format(lat, lng))
-    ca_coords = sgzip.coords_for_radius(radius=50, country_code=SearchableCountries.CANADA)
+    ca_coords = sgzip.coords_for_radius(
+        radius=50, country_code=SearchableCountries.CANADA
+    )
     for coord in ca_coords:
         lat, lng = coord
         all_requests.append(start_url_ca.format(lat, lng))
@@ -70,7 +71,7 @@ def fetch_data():
             continue
 
         for poi in data["servicePoints"]:
-            store_url = ''
+            store_url = ""
             store_url = store_url if store_url else "<MISSING>"
             location_name = poi["servicePointName"]
             location_name = location_name if location_name else "<MISSING>"
@@ -124,7 +125,7 @@ def fetch_data():
                 longitude,
                 hours_of_operation,
             ]
-            check = '{} {}'.format(store_number, street_address)
+            check = "{} {}".format(store_number, street_address)
             if check not in scrape_items:
                 scrape_items.append(check)
                 items.append(item)
