58e27d9c7f3821c7bb1d30db91316a609d0b573e
Quincy <qcalexander@gmail.com>
2020-12-02 00:30:20 -0400

    black reformat

--- apify/quincya/storelocator/gologas_com/scrape.py
+++ apify/quincya/storelocator/gologas_com/scrape.py
@@ -19,7 +19,7 @@ from sgrequests import SgRequests
 from sgselenium import SgChrome
 
 
-log = sglog.SgLogSetup().get_logger(logger_name='gologas_com')
+log = sglog.SgLogSetup().get_logger(logger_name="gologas_com")
 
 
 def write_output(data):
@@ -55,10 +55,10 @@ def write_output(data):
 def fetch_data():
     base_link = "http://www.gologas.com/oak-park-il"
 
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,\
-    like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,\
+    like Gecko) Chrome/72.0.3626.119 Safari/537.36"
 
-    headers = {'User-Agent': user_agent}
+    headers = {"User-Agent": user_agent}
 
     session = SgRequests()
     req = session.get(base_link, headers=headers)
@@ -75,19 +75,23 @@ def fetch_data():
     driver = SgChrome().chrome()
 
     for item in stores:
-        link = locator_domain + item['map_title'].replace(", Michigan", ", MI")
-        link = link.replace("Niles", "elgin").lower().replace(", ", "-").replace(" ", "-")
+        link = locator_domain + item["map_title"].replace(", Michigan", ", MI")
+        link = (
+            link.replace("Niles", "elgin").lower().replace(", ", "-").replace(" ", "-")
+        )
         log.info(link)
         driver.get(link)
 
         try:
-            WebDriverWait(driver, 50).until(ec.presence_of_element_located(
-                (By.CLASS_NAME, "wpgmza-address")))
+            WebDriverWait(driver, 50).until(
+                ec.presence_of_element_located((By.CLASS_NAME, "wpgmza-address"))
+            )
         except TimeoutException:
             try:
                 driver.get(link)
-                WebDriverWait(driver, 90).until(ec.presence_of_element_located(
-                    (By.CLASS_NAME, "wpgmza-address")))
+                WebDriverWait(driver, 90).until(
+                    ec.presence_of_element_located((By.CLASS_NAME, "wpgmza-address"))
+                )
             except TimeoutException:
                 continue
 
@@ -95,13 +99,15 @@ def fetch_data():
 
         base = BeautifulSoup(driver.page_source, "lxml")
 
-        locs = base.find_all('div', {'class': re.compile(
-            r'wpgmaps_mlist_row wpgmza_basic_row wpgmaps_.+')})
+        locs = base.find_all(
+            "div",
+            {"class": re.compile(r"wpgmaps_mlist_row wpgmza_basic_row wpgmaps_.+")},
+        )
 
         for loc in locs:
             location_name = base.h2.text.strip()
             if "," not in location_name:
-                location_name = item['map_title']
+                location_name = item["map_title"]
             raw_address = loc.find(class_="wpgmza-address").text
             if "," in location_name:
                 city = location_name.split(",")[0].strip()
@@ -110,11 +116,11 @@ def fetch_data():
                 city = location_name.split("|")[0].strip()
                 state = raw_address.split(",")[-1][:3].strip()
 
-            street_address = raw_address[:raw_address.rfind(city)].strip()
+            street_address = raw_address[: raw_address.rfind(city)].strip()
             if street_address[-1:] == ",":
                 street_address = street_address[:-1]
             try:
-                zip_code = re.findall(r'[0-9]{5}', raw_address[-20:])[0]
+                zip_code = re.findall(r"[0-9]{5}", raw_address[-20:])[0]
             except:
                 zip_code = "<MISSING>"
 
@@ -131,23 +137,27 @@ def fetch_data():
                 location_type = "<MISSING>"
             phone = "<MISSING>"
             hours_of_operation = "<MISSING>"
-            latitude = loc['data-latlng'].split(",")[0].strip()
-            longitude = loc['data-latlng'].split(",")[1].strip()
+            latitude = loc["data-latlng"].split(",")[0].strip()
+            longitude = loc["data-latlng"].split(",")[1].strip()
 
-            data.append([locator_domain,
-                         link,
-                         location_name,
-                         street_address,
-                         city,
-                         state,
-                         zip_code,
-                         country_code,
-                         store_number,
-                         phone,
-                         location_type,
-                         latitude,
-                         longitude,
-                         hours_of_operation])
+            data.append(
+                [
+                    locator_domain,
+                    link,
+                    location_name,
+                    street_address,
+                    city,
+                    state,
+                    zip_code,
+                    country_code,
+                    store_number,
+                    phone,
+                    location_type,
+                    latitude,
+                    longitude,
+                    hours_of_operation,
+                ]
+            )
     driver.close()
     return data
 
