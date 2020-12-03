17db0412efcb04117dccdd69694700a66fb61212
nomihd <nouman.awais.nu@gmail.com>
2020-12-03 17:41:37 +0500

    remove unused imports

--- apify/nomihd/storelocator/sweetfrog_com/scrape.py
+++ apify/nomihd/storelocator/sweetfrog_com/scrape.py
@@ -3,8 +3,6 @@ import csv
 from sgrequests import SgRequests
 from sglogging import sglog
 import json
-import lxml.html
-import us
 
 website = "sweetfrog.com"
 log = sglog.SgLogSetup().get_logger(logger_name=website)
