d28a1f2b2d1ee11749632f2527ac952b76c41981
Michael Dinh <mkdinh94@gmail.com>
2020-12-04 17:36:34 -0500

    SLC-5300/gnc_com: workarounds for captcha

--- apify/nsxdarin/storelocator/gnc_com/scrape.py
+++ apify/nsxdarin/storelocator/gnc_com/scrape.py
@@ -1,6 +1,6 @@
 import csv
 from sgrequests import SgRequests
-import requests_random_user_agent # ignore_check
+import requests_random_user_agent  # ignore_check
 import json
 import re
 import time
@@ -9,39 +9,20 @@ import threading
 from concurrent.futures import ThreadPoolExecutor, as_completed, wait, ALL_COMPLETED
 from requests.exceptions import ProxyError
 from sglogging import SgLogSetup
+import requests
 
 logger = SgLogSetup().get_logger('gnc_com')
-
-
-
 
 show_logs = False
 thread_local = threading.local()
 
 post_headers = {
-  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-  'accept-encoding': 'gzip, deflate, br',
-  'accept-language': 'en-US,en;q=0.9,la;q=0.8',
-  'cache-control': 'max-age=0',
-  'origin': 'https://www.gnc.com',
-  'referer': 'https://www.gnc.com/on/demandware.store/Sites-GNC2-Site/default/Stores-FindStores',
-  'sec-fetch-dest': 'document',
-  'sec-fetch-mode': 'navigate',
-  'sec-fetch-site': 'same-origin',
-  'sec-fetch-user': '?1',
-  'upgrade-insecure-requests': '1',
-#   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-  'content-type': 'application/x-www-form-urlencoded' 
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
 }
 get_headers = {
-  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-  'accept-encoding': 'gzip, deflate, br',
-  'accept-language': 'en-US,en;q=0.9,la;q=0.8',
-  'sec-fetch-dest': 'document',
-  'sec-fetch-mode': 'navigate',
-  'sec-fetch-site': 'none',
-  'upgrade-insecure-requests': '1',
-#   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
 }
 
 re_get_json = re.compile('map\.data\.addGeoJson\(\s*JSON\.parse\(\s*eqfeed_callback\((.+?)\)\s*\)\s*\);')
@@ -59,8 +40,8 @@ def sleep(min=2, max=10):
 
 
 def log(*args, **kwargs):
-  if (show_logs == True):
-    logger.info(" ".join(map(str, args)), **kwargs)
+    if (show_logs == True):
+        logger.info(" ".join(map(str, args)), **kwargs)
 
 
 def write_output(data):
@@ -72,50 +53,51 @@ def write_output(data):
 
 
 def get_session(reset=False):
-  # give each thread its own session object.
-  # when using proxy, each thread's session will have a unique IP, and we'll switch IPs every 10 requests
-  if (not hasattr(thread_local, "session")) or (hasattr(thread_local, "request_count") and thread_local.request_count == 10) or (reset==True):
-    thread_local.session = SgRequests()
-    # print out what the new IP is ...
-    if show_logs == True:
-        r = thread_local.session.get('https://jsonip.com/')
-        log(f"new IP for thread id {threading.current_thread().ident}: {r.json()['ip']}")
-    
-  if hasattr(thread_local, "request_count") and thread_local.request_count == 10:
-    reset_request_count()
+    # give each thread its own session object.
+    # when using proxy, each thread's session will have a unique IP, and we'll switch IPs every 10 requests
+    if (not hasattr(thread_local, "session")) or (hasattr(thread_local, "request_count") and thread_local.request_count == 10) or (reset == True):
+        thread_local.session = SgRequests()
+        # print out what the new IP is ...
+        if show_logs == True:
+            r = thread_local.session.get('https://jsonip.com/')
+            log(f"new IP for thread id {threading.current_thread().ident}: {r.json()['ip']}")
 
-  return thread_local.session
+    if hasattr(thread_local, "request_count") and thread_local.request_count == 10:
+        reset_request_count()
+
+    return thread_local.session
+    return requests
 
 
 def reset_request_count():
-  if hasattr(thread_local, "request_count"):
-    thread_local.request_count = 0
+    if hasattr(thread_local, "request_count"):
+        thread_local.request_count = 0
 
 
 def increment_request_count():
-  if not hasattr(thread_local, "request_count"):
-    thread_local.request_count = 1
-  else:
-    thread_local.request_count += 1
+    if not hasattr(thread_local, "request_count"):
+        thread_local.request_count = 1
+    else:
+        thread_local.request_count += 1
 
 
-def get_json_data(html): 
+def get_json_data(html):
     match = re.search(re_get_json, html)
     json_text = match.group(1)
     return json.loads(json_text)
 
 
-def get_phone(html): 
+def get_phone(html):
     match = re.search(re_get_phone, html)
-    if match is None: 
+    if match is None:
         return '<MISSING>'
     else:
         return match.group(1)
 
 
-def get_hours(html): 
+def get_hours(html):
     match_section = re.search(re_get_hours_section, html)
-    if match_section is None: 
+    if match_section is None:
         return '<MISSING>'
 
     hours = ''
@@ -123,8 +105,8 @@ def get_hours(html):
     if len(match_days) == 0:
         return '<MISSING>'
 
-    for m in match_days: 
-        if len(hours) > 0: 
+    for m in match_days:
+        if len(hours) > 0:
             hours = hours + ', '
         hours = f'{hours}{m[0].strip()}: {m[1].strip()}'
 
@@ -132,34 +114,36 @@ def get_hours(html):
     return hours
 
 
-def search_zip(zip, reset=False, attempts=1): 
+def search_zip(postal, reset=False, attempts=1):
     if attempts == 5:
-        raise Exception(f'failed to search zip {zip} after {attempts} tries ... giving up')
-    log('searching: ', zip)
+        raise Exception(f'failed to search zip {postal} after {attempts} tries ... giving up')
+    log('searching: ', postal)
     url = 'https://www.gnc.com/on/demandware.store/Sites-GNC2-Site/default/Stores-FindStores'
-    payload = {'dwfrm_storelocator_countryCode': 'US',
-                'dwfrm_storelocator_distanceUnit': 'mi',
-                'dwfrm_storelocator_postalCode': zip,
-                'dwfrm_storelocator_maxdistance': '500',
-                'dwfrm_storelocator_findbyzip': 'Search'}
+    payload = {
+        "dwfrm_storelocator_countryCode": "US",
+        "dwfrm_storelocator_distanceUnit": "mi",
+        "dwfrm_storelocator_postalCode": postal,
+        "dwfrm_storelocator_maxdistance": "15",
+        "dwfrm_storelocator_findbyzip": "Search"
+    }
     session = get_session(reset=reset)
     # get the home page before each search to avoid captcha
     session.get('https://www.gnc.com/', headers=get_headers)
     sleep()
-    try: 
-        r = session.post(url, headers=post_headers, data=payload)
+    try:
+        r = requests.post(url, headers=post_headers, data=payload)
         r.raise_for_status()
         increment_request_count()
         return get_json_data(r.text)["features"]
     except Exception as ex:
-        logger.info(f'>>> exception searching zip {zip} >>> {ex}')
+        logger.info(f'>>> exception searching zip {postal} >>> {ex}')
         logger.info(f'resetting session to try again')
-        return search_zip(zip, reset=True, attempts=attempts+1)
+        return search_zip(postal, reset=True, attempts=attempts + 1)
 
 
 def get_location(loc, reset_session=False, attempts=1):
     max_attempts = 5
-    if attempts > max_attempts: 
+    if attempts > max_attempts:
         raise Exception(f"Max attempts ({max_attempts}) exceeded getting location {loc['storenumber']}")
 
     props = loc["properties"]
@@ -178,24 +162,22 @@ def get_location(loc, reset_session=False, attempts=1):
     lat = loc["geometry"]["coordinates"][1]
     lng = loc["geometry"]["coordinates"][0]
     country = 'US'
-
     session = get_session(reset=reset_session)
     sleep()
-
-    try: 
+    try:
         r = session.get(url, headers=get_headers)
         r.raise_for_status()
     except Exception as ex:
         logger.info(f'>>> exception getting location {loc} >>> {ex}')
         logger.info(f'resetting session to try again')
-        return get_location(loc, reset_session=True, attempts=attempts+1)
+        return get_location(loc, reset_session=True, attempts=attempts + 1)
 
     increment_request_count()
     log(f'Pulling Store {url}')
 
     if 'px-captcha' in r.text:
         log('CAPTCHA, resetting and trying again')
-        return get_location(loc, reset_session=True, attempts=attempts+1)
+        return get_location(loc, reset_session=True, attempts=attempts + 1)
 
     phone = get_phone(r.text)
     hours = get_hours(r.text)
@@ -206,62 +188,41 @@ def get_location(loc, reset_session=False, attempts=1):
 
 def fetch_data():
     store_ids = []
-    zips = ['60007','10002','90210','96795','99515','98115','88901','87101','59715','80014','75001','32034','70032','37011','63101','58102','55408']
+    zips = ['60007', '10002', '90210', '96795', '99515', '98115', '88901', '87101', '59715', '80014', '75001', '32034', '70032', '37011', '63101', '58102', '55408']
     search_results = []
 
-    with ThreadPoolExecutor(max_workers=32) as executor:
+    with ThreadPoolExecutor(max_workers=5) as executor:
         futures = [executor.submit(search_zip, zipcode) for zipcode in zips]
         done, not_done = wait(futures, return_when=ALL_COMPLETED)
         log(f'Done futures: {len(done)}')
         log(f'Not Done futures: {len(not_done)}')
         for result in futures:
             locations = result.result()
-            for loc in locations: 
+            for loc in locations:
                 store_id = loc['properties']['storenumber']
                 if store_id not in store_ids:
                     log(f'queuing store id: {store_id}')
                     search_results.append(loc)
                     store_ids.append(store_id)
 
-    log(f'found {len(search_results)} locations')
+    # log(f'found {len(search_results)} locations')
+    # # with open("search_results.json", 'w') as f:
+    # #     json.dump(search_results, f)
+    # with open("search_results.json", 'r') as f:
+    #     search_results = json.load(f)
 
-    with ThreadPoolExecutor(max_workers=32) as executor: 
+    with ThreadPoolExecutor(max_workers=10) as executor:
         futures = [executor.submit(get_location, result) for result in search_results]
         wait(futures, return_when=ALL_COMPLETED)
         for result in futures:
             loc = result.result()
             if loc is not None:
                 yield loc
-            
+
 
 def scrape():
-    
-    # ---- to test hours/phone for just one store detail ---
-    # search_result = {
-    #     'type': 'Feature',
-    #     'geometry': {
-    #         'type': 'Point', 
-    #         'coordinates': [-80.14893562, 26.20537681]
-    #     }, 
-    #     'properties': {
-    #         'title': 'GNC at Cypress Creek Station', 
-    #         'storenumber': '1814',
-    #         'url': '<a id="3735" href="/store-details?StoreID=3735" title="View Store Details">Store Details</a>',
-    #         'address1': '6305 North Andrews Ave', 
-    #         'address2': None, 
-    #         'postalCode': '33309', 
-    #         'city': 
-    #         'Ft. Lauderdale', 
-    #         'state': 'FL', 
-    #         'country': {}, 
-    #         'inventoryListID': ''
-    #     }
-    # }
-    # data = get_location(search_result)
-    # write_output([data])
-    # raise SystemExit
-    
     data = fetch_data()
     write_output(data)
+
 
 scrape()
