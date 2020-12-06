b1a2cf8c7123bb0231c3197d056354da103bbda2
Quincy <qcalexander@gmail.com>
2020-12-06 18:21:50 -0400

    landrover_co_uk fixed

--- apify/quincya/storelocator/landrover_co_uk/scrape.py
+++ apify/quincya/storelocator/landrover_co_uk/scrape.py
@@ -136,7 +136,7 @@ def fetch_data():
                 location_type = ", ".join(
                     list(item.find(class_="services").stripped_strings)
                 ).replace(
-                    "LAND ROVER TO YOU,Too busy to come to us?,Try Land Rover to You",
+                    "LAND ROVER TO YOU, Too busy to come to us?, Try Land Rover to You",
                     "LAND ROVER TO YOU",
                 )
             except:
