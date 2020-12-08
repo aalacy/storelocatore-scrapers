d77bf5ff0727f5ab69074eef6e52d82529937dfb
datarhan <datarhan@github.com>
2020-12-08 20:59:45 +0100

    [txfb-ins.com] update aftyer QA

--- apify/datarhan/storelocator/txfb-ins_com/scrape.py
+++ apify/datarhan/storelocator/txfb-ins_com/scrape.py
@@ -55,6 +55,7 @@ def fetch_data():
         )
         store_response = session.get(store_url)
         poi = json.loads(store_response.text)
+        store_url = 'https://www.txfb-ins.com/county/details/{}'.format(county)
         if not poi["_countyOffice"]:
             continue
         location_name = poi["_countyOffice"][0]["_agencyName"]
