377bda9d7c898f944b7c94a524334c07efc53651
nsxdarin <nsxdarin@gmail.com>
2020-12-09 10:17:42 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/libertytravel_com/scrape.py
+++ apify/nsxdarin/storelocator/libertytravel_com/scrape.py
@@ -1,11 +1,11 @@
 import csv
-import requests
+from sgrequests import SgRequests
 from sglogging import SgLogSetup
 
 logger = SgLogSetup().get_logger("libertytravel_com")
 
 
-session = requests.Session()
+session = SgRequests()
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
