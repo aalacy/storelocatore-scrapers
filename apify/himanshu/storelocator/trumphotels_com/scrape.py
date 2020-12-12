42f1905397f06613c15132fa19b614bddf326785
vivek <vivek@rentechdigital.com>
2020-12-12 15:12:02 +0530

    Done

--- apify/himanshu/storelocator/trumphotels_com/scrape.py
+++ apify/himanshu/storelocator/trumphotels_com/scrape.py
@@ -33,11 +33,6 @@ def write_output(data):
 
 def fetch_data():
     driver = webdriver.Chrome(executable_path="chromedriver")
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
-    }
-    addresses = []
     base_url = "https://www.trumphotels.com"
     driver.get("https://www.trumphotels.com/")
     soup = BeautifulSoup(driver.page_source, "lxml")
