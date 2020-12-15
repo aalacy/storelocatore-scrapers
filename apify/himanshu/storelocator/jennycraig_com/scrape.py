311d78195718f19eb50b8995cc470c498393ad60
vivek <vivek@rentechdigital.com>
2020-12-15 14:39:12 +0530

    Done

--- apify/himanshu/storelocator/jennycraig_com/scrape.py
+++ apify/himanshu/storelocator/jennycraig_com/scrape.py
@@ -90,10 +90,8 @@ def fetch_data():
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
     }
     addresses = []
-    base_url = "https://jennycraig.com"
     r = session.get("https://locations.jennycraig.com", headers=headers)
     soup = BeautifulSoup(r.text, "lxml")
-    return_main_object = []
     for states in soup.find_all("a", {"class": "ga-link"}):
         state_request = session.get(states["href"], headers=headers)
         state_soup = BeautifulSoup(state_request.text, "lxml")
