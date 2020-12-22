0f235020a35fe3d82095d0584b2875516d0160a3
datarhan <datarhan@github.com>
2020-12-22 14:20:51 +0100

    Added new spider for flyersenergy.com

--- apify/datarhan/storelocator/flyersenergy_com/scrape.py
+++ apify/datarhan/storelocator/flyersenergy_com/scrape.py
@@ -1,8 +1,9 @@
 import re
+import json
 import csv
 from lxml import etree
 
-from sgrequests import SgRequests
+from sgselenium import SgChrome
 
 
 def write_output(data):
@@ -37,71 +38,47 @@ def write_output(data):
 
 def fetch_data():
     # Your scraper here
-    session = SgRequests().requests_retry_session(retries=0, backoff_factor=0.3)
 
     items = []
 
     DOMAIN = "flyersenergy.com"
     start_url = "https://www.flyersenergy.com/locations/"
-    headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36"
-    }
 
-    response = session.get(start_url, headers=headers)
-    dom = etree.HTML(response.text)
-    all_locations = dom.xpath('//div[@class="wpb_wrapper"]/p/a/@href')
-
-    for store_url in all_locations:
-        store_response = session.get(store_url)
-        store_dom = etree.HTML(store_response.text)
-        location_name = store_dom.xpath(
-            '//div[@class="title_subtitle_holder"]//span/text()'
-        )
-        location_name = location_name[0] if location_name else "<MISSING>"
-        address_raw = store_dom.xpath(
-            '//div[contains(@class, "wpb_text_column wpb_content_element")]/div[h3]/p[1]/text()'
-        )
-        if address_raw:
-            if len(address_raw) == 3:
-                address_raw = [
-                    ", ".join(address_raw[:2]).replace("\n", "")
-                ] + address_raw[2:]
-            street_address = address_raw[0]
-            street_address = street_address if street_address else "<MISSING>"
-            city = address_raw[1].split(",")[0].strip()
-            state = address_raw[1].split(",")[-1].split()[0]
-            zip_code = address_raw[1].split(",")[-1].split()[-1]
-            geo = re.findall("2d(.+)!2m", store_dom.xpath("//iframe/@src")[-1])
-            if not geo:
-                geo = (
-                    re.findall("2d(.+)!3m", store_dom.xpath("//iframe/@src")[-1])[0]
-                    .split("!3m2")[0]
-                    .split("!3d")
-                )
+    with SgChrome() as driver:
+        passed = False
+        while not passed:
+            driver.get(start_url)
+            if driver.page_source == "<html><head></head><body></body></html>":
+                passed = False
+                continue
             else:
-                geo = geo[0].split("!3d")
-            latitude = geo[1]
-            longitude = geo[0]
-        else:
-            street_address = "<MISSING>"
-            city = "<MISSING>"
-            state = "<MISSING>"
-            zip_code = "<MISSING>"
-            latitude = "<MISSING>"
-            longitude = "<MISSING>"
+                passed = True
+
+            element = driver.find_element_by_xpath(
+                '//button[@class="mgbutton moove-gdpr-infobar-allow-all"]'
+            )
+            driver.execute_script("arguments[0].click();", element)
+            driver.find_element_by_xpath(
+                '//button[@class="wpgmza-api-consent"]'
+            ).click()
+            dom = etree.HTML(driver.page_source)
+            data = dom.xpath('//script[contains(text(), "marker_data")]/text()')[0]
+            data = json.loads(re.findall("marker_data = (.+?);", data)[0])
+
+    for poi in data["1"].values():
+        store_url = "<MISSING>"
+        location_name = poi["title"]
+        street_address = poi["address"].split(", ")[0]
+        city = poi["address"].split(", ")[1]
+        state = poi["address"].split(", ")[-1].split()[0]
+        zip_code = poi["address"].split(", ")[-1].split()[-1]
+        latitude = poi["lat"]
+        longitude = poi["lng"]
         country_code = "<MISSING>"
-        store_number = "<MISSING>"
-        phone = store_dom.xpath(
-            '//div[contains(@class, "wpb_text_column wpb_content_element")]/div[h3]/p[3]/text()'
-        )
-        phone = phone[0].split(":")[-1].strip() if phone else "<MISSING>"
+        store_number = re.findall("(\d+) -", poi["title"])[0]
+        phone = "<MISSING>"
         location_type = "<MISSING>"
-        hours_of_operation = store_dom.xpath(
-            '//div[contains(@class, "wpb_text_column wpb_content_element")]/div[h3]/p[2]/text()'
-        )
-        hours_of_operation = (
-            hours_of_operation[-1].strip() if hours_of_operation else "<MISSING>"
-        )
+        hours_of_operation = "<MISSING>"
 
         item = [
             DOMAIN,
