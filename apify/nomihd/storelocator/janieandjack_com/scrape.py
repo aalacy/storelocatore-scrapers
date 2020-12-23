e827449fb65d027233de418a15cc3983aebe91e3
nomihd <nouman.awais.nu@gmail.com>
2020-12-23 12:41:59 +0500

    debug janieandjack

--- apify/nomihd/storelocator/janieandjack_com/scrape.py
+++ apify/nomihd/storelocator/janieandjack_com/scrape.py
@@ -90,6 +90,7 @@ def fetch_data():
     if count == 5:
         raise Exception("This should never happen")
 
+    print(stores_req.text)
     stores = json.loads(stores_req.text.strip())["stores"]
     for store in stores.keys():
         locator_domain = website
