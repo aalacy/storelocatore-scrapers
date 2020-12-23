e82b981d0ccb263395a45e09d217c9aec7c2011d
idev-a <ideveloper003@protonmail.com>
2020-12-23 10:58:06 -0800

    run black for util.py

--- apify/idev-a/storelocator/citymd_net/util.py
+++ apify/idev-a/storelocator/citymd_net/util.py
@@ -1,28 +1,165 @@
-us_states = ['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'new hampshire', 'new jersey', 'new mexico', 'new york', 'north carolina', 'north dakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhode island', 'south carolina', 'south dakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'west virginia', 'wisconsin', 'wyoming']
-us_states_codes = {'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MH', 'MA', 'MI', 'FM', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'VI', 'WA', 'WV', 'WI', 'WY'}
-ca_provinces = ['alberta', 'british columbia', 'manitoba', 'new brunswick', 'newfoundland and labrador', 'nova scotia', 'ontario', 'prince edward island', 'quebec', 'saskatchewan']
-ca_provinces_codes = {'AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT'}
+us_states = [
+    "alabama",
+    "alaska",
+    "arizona",
+    "arkansas",
+    "california",
+    "colorado",
+    "connecticut",
+    "delaware",
+    "florida",
+    "georgia",
+    "hawaii",
+    "idaho",
+    "illinois",
+    "indiana",
+    "iowa",
+    "kansas",
+    "kentucky",
+    "louisiana",
+    "maine",
+    "maryland",
+    "massachusetts",
+    "michigan",
+    "minnesota",
+    "mississippi",
+    "missouri",
+    "montana",
+    "nebraska",
+    "nevada",
+    "new hampshire",
+    "new jersey",
+    "new mexico",
+    "new york",
+    "north carolina",
+    "north dakota",
+    "ohio",
+    "oklahoma",
+    "oregon",
+    "pennsylvania",
+    "rhode island",
+    "south carolina",
+    "south dakota",
+    "tennessee",
+    "texas",
+    "utah",
+    "vermont",
+    "virginia",
+    "washington",
+    "west virginia",
+    "wisconsin",
+    "wyoming",
+]
+us_states_codes = {
+    "AL",
+    "AK",
+    "AS",
+    "AZ",
+    "AR",
+    "CA",
+    "CO",
+    "CT",
+    "DE",
+    "DC",
+    "FL",
+    "GA",
+    "GU",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MD",
+    "MH",
+    "MA",
+    "MI",
+    "FM",
+    "MN",
+    "MS",
+    "MO",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "MP",
+    "OH",
+    "OK",
+    "OR",
+    "PW",
+    "PA",
+    "PR",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VA",
+    "VI",
+    "WA",
+    "WV",
+    "WI",
+    "WY",
+}
+ca_provinces = [
+    "alberta",
+    "british columbia",
+    "manitoba",
+    "new brunswick",
+    "newfoundland and labrador",
+    "nova scotia",
+    "ontario",
+    "prince edward island",
+    "quebec",
+    "saskatchewan",
+]
+ca_provinces_codes = {
+    "AB",
+    "BC",
+    "MB",
+    "NB",
+    "NL",
+    "NS",
+    "NT",
+    "NU",
+    "ON",
+    "PE",
+    "QC",
+    "SK",
+    "YT",
+}
+
 
 class Util:
-	def get_country_by_code(self, code=''):
-		if code in us_states_codes:
-			return "US"
-		elif code in ca_provinces_codes:
-			return "CA"
-		else:
-			return "<MISSING>"
+    def get_country_by_code(self, code=""):
+        if code in us_states_codes:
+            return "US"
+        elif code in ca_provinces_codes:
+            return "CA"
+        else:
+            return "<MISSING>"
 
-	def parse_address_simple(self, address):
-		address = address.split(',')
-		street_address = self._valid(''.join(address[:-2]))
-		city = self._valid(address[-2])
-		state = self._valid(address[-1].strip().split(' ')[0])
-		zip = self._valid(address[-1].strip().split(' ')[1])
-		country_code = self.get_country_by_code(state)
-		return street_address, city, state, zip, country_code
+    def parse_address_simple(self, address):
+        address = address.split(",")
+        street_address = self._valid("".join(address[:-2]))
+        city = self._valid(address[-2])
+        state = self._valid(address[-1].strip().split(" ")[0])
+        zip = self._valid(address[-1].strip().split(" ")[1])
+        country_code = self.get_country_by_code(state)
+        return street_address, city, state, zip, country_code
 
-	def _valid(self, val):
-		if val:
-			return val.strip()
-		else:
-			return "<MISSING>"
+    def _valid(self, val):
+        if val:
+            return val.strip()
+        else:
+            return "<MISSING>"
