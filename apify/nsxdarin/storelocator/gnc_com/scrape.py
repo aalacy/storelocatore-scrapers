a186eb42d9b29beeade7912a66e1b3d12ddbeec4
Michael Dinh <mkdinh94@gmail.com>
2020-12-23 14:44:03 -0500

    SLC-5300/gnc_com: handle retry

--- apify/nsxdarin/storelocator/gnc_com/scrape.py
+++ apify/nsxdarin/storelocator/gnc_com/scrape.py
@@ -8,7 +8,9 @@ import requests_random_user_agent  # noqa
 from bs4 import BeautifulSoup
 from sglogging import SgLogSetup
 from sgrequests import SgRequests
-from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from sgzip.static import static_zipcode_list
+from sgzip.dynamic import SearchableCountries
 
 logger = SgLogSetup().get_logger("gnc_com")
 
@@ -98,6 +100,8 @@ def increment_request_count():
 
 def get_json_data(html):
     match = re.search(re_get_json, html)
+    if not match:
+        return None
     json_text = match.group(1)
     return json.loads(json_text)
 
@@ -121,30 +125,36 @@ def get_hours(soup):
     return ",".join(extract_hours(day) for day in openingHours)
 
 
-def search_zip(postal, reset=False):
+def search_zip(postal, reset=False, retry_count=0):
     log("searching: ", postal)
     url = "https://www.gnc.com/on/demandware.store/Sites-GNC2-Site/default/Stores-FindStores"
     payload = {
         "dwfrm_storelocator_countryCode": "US",
         "dwfrm_storelocator_distanceUnit": "mi",
         "dwfrm_storelocator_postalCode": postal,
-        "dwfrm_storelocator_maxdistance": "15",
+        "dwfrm_storelocator_maxdistance": "100",
         "dwfrm_storelocator_findbyzip": "Search",
     }
     session = get_session(reset=reset)
     # get the home page before each search to avoid captcha
-    session.get("https://www.gnc.com/", headers=headers)
+    session.get("https://www.gnc.com/", headers=headers, timeout=1)
     sleep()
     try:
         r = session.post(url, headers=headers, data=payload)
         r.raise_for_status()
         increment_request_count()
-        return get_json_data(r.text)["features"]
+        data = get_json_data(r.text)
+        if data:
+            return data.get('features', [])
+        else:
+            return []
     except Exception as ex:
+        if retry_count < 3:
+            return search_zip(postal, True, retry_count + 1)
         logger.info(f">>> exception searching zip {postal} >>> {ex}")
 
 
-def get_location(loc, reset_session=False):
+def get_location(loc, reset_session=False, retry_count=0):
     props = loc["properties"]
     store_id = props["storenumber"]
     website = "gnc.com"
@@ -161,9 +171,9 @@ def get_location(loc, reset_session=False):
     lat = loc["geometry"]["coordinates"][1]
     lng = loc["geometry"]["coordinates"][0]
     country = "US"
-    session = get_session(reset=reset_session)
-    sleep()
     try:
+        session = get_session(reset=reset_session)
+        sleep()
         r = session.get(url, headers=headers)
         r.raise_for_status()
         increment_request_count()
@@ -191,50 +201,32 @@ def get_location(loc, reset_session=False):
         return [x if x else "<MISSING>" for x in location]
 
     except Exception as ex:
+        if retry_count < 3:
+            return get_location(loc, True, retry_count + 1)
         logger.info(f">>> exception getting location {loc} >>> {ex}")
 
 
 def fetch_data():
     store_ids = []
-    zips = [
-        "60007",
-        "10002",
-        "90210",
-        "96795",
-        "99515",
-        "98115",
-        "88901",
-        "87101",
-        "59715",
-        "80014",
-        "75001",
-        "32034",
-        "70032",
-        "37011",
-        "63101",
-        "58102",
-        "55408",
-    ]
+    zip_searched = 0
+    zips = static_zipcode_list(50, SearchableCountries.USA)
     search_results = []
 
-    with ThreadPoolExecutor(max_workers=5) as executor:
+    with ThreadPoolExecutor(max_workers=10) as executor:
         futures = [executor.submit(search_zip, zipcode) for zipcode in zips]
-        done, not_done = wait(futures, return_when=ALL_COMPLETED)
-        log(f"Done futures: {len(done)}")
-        log(f"Not Done futures: {len(not_done)}")
-        for result in futures:
+        for result in as_completed(futures):
             locations = result.result()
+            zip_searched += 1
+            logger.info(f'pulling: {len(locations)} | found: {len(search_results)} | zipcodes: {zip_searched}/{len(zips)}')
             for loc in locations:
                 store_id = loc["properties"]["storenumber"]
                 if store_id not in store_ids:
-                    log(f"queuing store id: {store_id}")
                     search_results.append(loc)
                     store_ids.append(store_id)
 
     with ThreadPoolExecutor(max_workers=10) as executor:
         futures = [executor.submit(get_location, result) for result in search_results]
-        wait(futures, return_when=ALL_COMPLETED)
-        for result in futures:
+        for result in as_completed(futures):
             loc = result.result()
             if loc is not None:
                 yield loc
