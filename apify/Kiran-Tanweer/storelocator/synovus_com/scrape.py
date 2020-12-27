003b6988638d5de116da14443cce8457164ae3c4
Kiran Tanweer <44836465+Kiran-Tanweer@users.noreply.github.com>
2020-12-27 13:59:28 +0500

    Update scrape.py

--- apify/Kiran-Tanweer/storelocator/synovus_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/synovus_com/scrape.py
@@ -4,22 +4,21 @@ import time
 import csv
 
 
-
 logger = SgLogSetup().get_logger("synovus_com")
 
 session = SgRequests()
 headers = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Connection': 'keep-alive',
-    'Host': 'www.mapquestapi.com',
-    'Referer': 'https://www.synovus.com/',
-    'Sec-Fetch-Dest': 'script',
-    'Sec-Fetch-Mode': 'no-cors',
-    'Sec-Fetch-Site': 'cross-site',
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Host": "www.mapquestapi.com",
+    "Referer": "https://www.synovus.com/",
+    "Sec-Fetch-Dest": "script",
+    "Sec-Fetch-Mode": "no-cors",
+    "Sec-Fetch-Site": "cross-site",
     "method": "GET",
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
 }
 
 
@@ -68,20 +67,24 @@ def write_output(data1):
 
 def fetch_data():
     data = []
-    state_list = ['Alabama', 'Florida', 'Georgia', 'South Carolina', 'Tenessee']
+    state_list = ["Alabama", "Florida", "Georgia", "South Carolina", "Tenessee"]
     for state in state_list:
-        url = 'https://www.mapquestapi.com/search/v2/radius?origin=' + state + '&radius=100&distanceUnit=dm&hostedData=mqap.36969_Synovus&ambiguities=ignore&key=Gmjtd%7Cluu7n961n1%2Crw%3Do5-ly10h&callback=window.MapManager.processSearchResults&_=1606874485792'
+        url = (
+            "https://www.mapquestapi.com/search/v2/radius?origin="
+            + state
+            + "&radius=100&distanceUnit=dm&hostedData=mqap.36969_Synovus&ambiguities=ignore&key=Gmjtd%7Cluu7n961n1%2Crw%3Do5-ly10h&callback=window.MapManager.processSearchResults&_=1606874485792"
+        )
         r = session.get(url, headers=headers, verify=False)
         response = r.text
         data2 = response.split("fields")
         data2.pop(0)
 
         for loc in data2:
-            location = "\"fields" + loc
+            location = '"fields' + loc
             location = location.rstrip(',"')
-            title = location.split('"Name":"',1)[1].split('"')[0]
-            latlng = location.split('"latLng":{', 1)[1].split('}')[0]
-            lngt = latlng.split('"lng":', 1)[1].split(',')[0]
+            title = location.split('"Name":"', 1)[1].split('"')[0]
+            latlng = location.split('"latLng":{', 1)[1].split("}")[0]
+            lngt = latlng.split('"lng":', 1)[1].split(",")[0]
             lat = latlng.split('"lat":', 1)[1]
             Phone = location.split('"Phone":"', 1)[1].split('"')[0]
             if Phone == "":
@@ -90,17 +93,33 @@ def fetch_data():
             city = location.split('"city":"', 1)[1].split('"')[0]
             state = location.split('"state":"', 1)[1].split('"')[0]
             postal = location.split('"postal":"', 1)[1].split('"')[0]
-            Mon = 'Monday: ' + location.split('"Monday":"', 1)[1].split('"')[0]
-            Tues = 'Tuesday: ' + location.split('"Tuesday":"', 1)[1].split('"')[0]
-            Wed = 'Wednesday: ' + location.split('"Wednesday":"', 1)[1].split('"')[0]
-            Thurs = 'Thursday: ' + location.split('"Thursday":"', 1)[1].split('"')[0]
-            fri = 'Friday: ' + location.split('"Friday":"', 1)[1].split('"')[0]
-            sat = 'Saturday: ' + location.split('"Saturday":"', 1)[1].split('"')[0]
-            sun = 'Sunday: ' + location.split('"Sunday":"', 1)[1].split('"')[0]
-            Hours = Mon + ', ' + Tues + ', ' + Wed + ', ' + Thurs + ', ' + fri + ', ' + sat + ', ' + sun
-            if Hours == 'Monday: , Tuesday: , Wednesday: , Thursday: , Friday: , Saturday: , Sunday: ':
+            Mon = "Monday: " + location.split('"Monday":"', 1)[1].split('"')[0]
+            Tues = "Tuesday: " + location.split('"Tuesday":"', 1)[1].split('"')[0]
+            Wed = "Wednesday: " + location.split('"Wednesday":"', 1)[1].split('"')[0]
+            Thurs = "Thursday: " + location.split('"Thursday":"', 1)[1].split('"')[0]
+            fri = "Friday: " + location.split('"Friday":"', 1)[1].split('"')[0]
+            sat = "Saturday: " + location.split('"Saturday":"', 1)[1].split('"')[0]
+            sun = "Sunday: " + location.split('"Sunday":"', 1)[1].split('"')[0]
+            Hours = (
+                Mon
+                + ", "
+                + Tues
+                + ", "
+                + Wed
+                + ", "
+                + Thurs
+                + ", "
+                + fri
+                + ", "
+                + sat
+                + ", "
+                + sun
+            )
+            if (
+                Hours
+                == "Monday: , Tuesday: , Wednesday: , Thursday: , Friday: , Saturday: , Sunday: "
+            ):
                 Hours = "<MISSING>"
-            
 
             data.append(
                 [
@@ -131,7 +150,3 @@ def scrape():
 
 
 scrape()
-
-# Wheeler Road Office and ATM
-# Washington Road Branch and ATM
-# python validate.py data.csv --ignore LatLngDuplicationValidator --ignore StreetAddressHasNumber
