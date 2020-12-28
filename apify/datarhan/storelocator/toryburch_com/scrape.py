22517ebc53bdf3048146288b235f95c6f1a74575
datarhan <datarhan@github.com>
2020-12-28 10:29:11 +0100

    [toryburch.com] update after QA

--- apify/datarhan/storelocator/toryburch_com/scrape.py
+++ apify/datarhan/storelocator/toryburch_com/scrape.py
@@ -38,22 +38,29 @@ def fetch_data():
     # Your scraper here
     items = []
 
-    session = SgRequests()
+    session = SgRequests().requests_retry_session(retries=2, backoff_factor=0.3)
 
     DOMAIN = "toryburch.com"
-    start_url = "https://www.toryburch.com/stores?country=US&format=ajax"
-    response = session.get(start_url)
+    start_url = "https://www.toryburch.com/stores?country=US"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+    }
+    response = session.get(start_url, headers=headers)
+
     dom = etree.HTML(response.text)
     all_locations = dom.xpath('//div[@itemtype="http://schema.org/ClothingStore"]')
 
     for poi_html in all_locations:
         store_url = poi_html.xpath('.//meta[@itemprop="url"]/@content')
         store_url = store_url[0] if store_url else "<MISSING>"
+        print(store_url)
         location_name = poi_html.xpath('.//p[@class="store-name"]/a/text()')
         location_name = location_name[0] if location_name else "<MISSING>"
         street_address = poi_html.xpath('.//meta[@itemprop="streetAddress"]/@content')[
             0
         ]
+        if len(street_address.split(",")) == 3:
+            street_address = ", ".join(street_address.split(",")[1:])
         city = poi_html.xpath('.//meta[@itemprop="addressLocality"]/@content')
         city = city[0] if city else "<MISSING>"
         if "null" in city:
@@ -68,7 +75,7 @@ def fetch_data():
         phone = phone[0] if phone else "<MISSING>"
         location_type = poi_html.xpath("@itemtype")[0].split("/")[-1]
 
-        loc_response = session.get(store_url)
+        loc_response = session.get(store_url, headers=headers)
         loc_dom = etree.HTML(loc_response.text)
         latitude = loc_dom.xpath('//meta[@itemprop="latitude"]/@content')
         latitude = latitude[0] if latitude else "<MISSING>"
@@ -76,7 +83,7 @@ def fetch_data():
         longitude = longitude[0] if longitude else "<MISSING>"
         hours_of_operation = poi_html.xpath('.//div[@class="store-hours-info"]//text()')
         hours_of_operation = (
-            hours_of_operation[0].replace("\n", " ")
+            " ".join(hours_of_operation).replace("\n", " ")
             if hours_of_operation
             else "<MISSING>"
         )
