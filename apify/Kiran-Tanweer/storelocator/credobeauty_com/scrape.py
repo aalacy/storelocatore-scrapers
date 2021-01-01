efaf2a3e3f30dfdf7b3c69d751791abcea9040c6
Kiran Tanweer <44836465+Kiran-Tanweer@users.noreply.github.com>
2021-01-01 23:24:44 +0500

    Update scrape.py

--- apify/Kiran-Tanweer/storelocator/credobeauty_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/credobeauty_com/scrape.py
@@ -11,8 +11,7 @@ logger = SgLogSetup().get_logger("credobeauty_com")
 
 session = SgRequests()
 headers = {
-'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
 }
 
 
@@ -58,75 +57,79 @@ def write_output(data):
                 writer.writerow(row)
         logger.info(f"No of records being processed: {len(temp_list)}")
 
+
 def fetch_data():
     data = []
-    HOO = ''
-    pattern = re.compile(r'\s\s+')
-    url = 'https://credobeauty.com/blogs/credo-stores'
+    HOO = ""
+    pattern = re.compile(r"\s\s+")
+    url = "https://credobeauty.com/blogs/credo-stores"
     r = session.get(url, headers=headers, verify=False)
     soup = BeautifulSoup(r.text, "html.parser")
     loc_div = soup.find("div", {"class": "sixteen columns store-row"})
-    loc = loc_div.findAll("div", {"class":"one-third"})
+    loc = loc_div.findAll("div", {"class": "one-third"})
     for info in loc:
-        address = info.find("div", {"class":"address-store"})
+        address = info.find("div", {"class": "address-store"})
         addr = address.text
-        ptag = address.findAll('p')
+        ptag = address.findAll("p")
         if len(ptag) > 1:
-            lat = '<MISSING>'
-            longt = '<MISSING>'
-            coord_link = '<MISSING>'
+            lat = "<MISSING>"
+            longt = "<MISSING>"
+            coord_link = "<MISSING>"
         else:
-            coord = info.find("a", {"class":"sign_up_btn"})
+            coord = info.find("a", {"class": "sign_up_btn"})
             if coord is not None:
-                coord_link = coord['href']
-                points = coord_link.split('/@')[1].split(',')
+                coord_link = coord["href"]
+                points = coord_link.split("/@")[1].split(",")
                 lat = points[0]
                 longt = points[1]
             else:
                 s = ptag
-                if addr ==' 552 Hayes StreetSan Francisco, CA 94102':
-                    lat = '<MISSING>'
-                    longt = '<MISSING>'
-                    coord_link = '<MISSING>'
-                if addr == ' 1659 N Damen AveChicago, IL 60647' or addr == ' 9 Prince Street New York, NY 10012':
-                    coord_link = ptag[0].find('a')['href']
-                    points = coord_link.split('/@')[1].split(',')
+                if addr == " 552 Hayes StreetSan Francisco, CA 94102":
+                    lat = "<MISSING>"
+                    longt = "<MISSING>"
+                    coord_link = "<MISSING>"
+                if (
+                    addr == " 1659 N Damen AveChicago, IL 60647"
+                    or addr == " 9 Prince Street New York, NY 10012"
+                ):
+                    coord_link = ptag[0].find("a")["href"]
+                    points = coord_link.split("/@")[1].split(",")
                     lat = points[0]
                     longt = points[1]
-                if addr == ' 99 N. 6th St.Brooklyn, NY 11249':
-                    coord_link = ptag[0].findAll('a')[0]
-                    coord_link = coord_link['href']
-                    points = coord_link.split('/@')[1].split(',')
+                if addr == " 99 N. 6th St.Brooklyn, NY 11249":
+                    coord_link = ptag[0].findAll("a")[0]
+                    coord_link = coord_link["href"]
+                    points = coord_link.split("/@")[1].split(",")
                     lat = points[0]
                     longt = points[1]
-                    
-        atags= info.findAll('a')
+
+        atags = info.findAll("a")
         tag1 = atags[0]
-        loclink = tag1['href']
+        loclink = tag1["href"]
         phone = atags[-1].text
-        loclink = 'https://credobeauty.com' + loclink
+        loclink = "https://credobeauty.com" + loclink
         p = session.get(loclink, headers=headers, verify=False)
         soup = BeautifulSoup(p.text, "html.parser")
-        location = soup.find("div", {"class":"location__content"})
-        title = location.find('h2').text.strip()
+        location = soup.find("div", {"class": "location__content"})
+        title = location.find("h2").text.strip()
         addr = addr.strip()
-        addr = addr.replace('\n', ' ')
-        addr = addr.lstrip('The Shops At Legacy West')
-        if addr.find('Street') != -1:
-            addr = addr.replace('Street', 'Street ')
-        if addr.find('Damen Ave') != -1:
-            addr = addr.replace('Ave', 'Ave ')
-        if addr.find('G160') != -1:
-            addr = addr.replace('G160', 'G160 ')
-        if addr.find('Street  New York') != -1:
-            addr = addr.replace('Street  New York', 'Street New York')
-        addr = addr.replace(',', '')
+        addr = addr.replace("\n", " ")
+        addr = addr.lstrip("The Shops At Legacy West")
+        if addr.find("Street") != -1:
+            addr = addr.replace("Street", "Street ")
+        if addr.find("Damen Ave") != -1:
+            addr = addr.replace("Ave", "Ave ")
+        if addr.find("G160") != -1:
+            addr = addr.replace("G160", "G160 ")
+        if addr.find("Street  New York") != -1:
+            addr = addr.replace("Street  New York", "Street New York")
+        addr = addr.replace(",", "")
         address = usaddress.parse(addr)
-        i=0
-        street = ''
-        city = ''
-        state = ''
-        pcode = ''
+        i = 0
+        street = ""
+        city = ""
+        state = ""
+        pcode = ""
         while i < len(address):
             temp = address[i]
             if (
@@ -155,9 +158,9 @@ def fetch_data():
         pcode = pcode.lstrip()
         pcode = pcode.replace(",", "")
 
-        hours = soup.find("div", {"class":"store-hours"}).text
-        hours = re.sub(pattern, ' ', hours).strip()
-             
+        hours = soup.find("div", {"class": "store-hours"}).text
+        hours = re.sub(pattern, " ", hours).strip()
+
         data.append(
             [
                 "https://credobeauty.com/",
@@ -187,5 +190,3 @@ def scrape():
 
 
 scrape()
-
-
