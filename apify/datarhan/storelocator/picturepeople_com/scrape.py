e9e687e718eb3b68b728b5612529bcbc5e461820
datarhan <datarhan@github.com>
2021-01-08 16:15:29 +0100

    circleci update

--- apify/datarhan/storelocator/picturepeople_com/scrape.py
+++ apify/datarhan/storelocator/picturepeople_com/scrape.py
@@ -44,7 +44,6 @@ def fetch_data():
     scraped_items = []
 
     DOMAIN = "picturepeople.com"
-    start_url = "https://www.picturepeople.com/locations/"
     headers = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36"
     }
