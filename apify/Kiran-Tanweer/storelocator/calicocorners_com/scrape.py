960645a2341a91bde95a08a3e2516c9f1d7d9e6a
Kiran Tanweer <44836465+Kiran-Tanweer@users.noreply.github.com>
2021-01-12 15:37:29 +0500

    Update scrape.py

--- apify/Kiran-Tanweer/storelocator/calicocorners_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/calicocorners_com/scrape.py
@@ -9,8 +9,9 @@ logger = SgLogSetup().get_logger("calicocorners_com")
 
 session = SgRequests()
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 }
+
 
 def write_output(data):
     with open("data.csv", mode="w", newline="", encoding="utf8") as output_file:
@@ -58,13 +59,13 @@ def write_output(data):
 def fetch_data():
     data = []
     store = []
-    store_id =[]
+    store_id = []
     title = []
     adr = []
     hours = []
     Phone = []
-    i=0
-    url = 'https://www.calicocorners.com/storelocator.aspx'
+    i = 0
+    url = "https://www.calicocorners.com/storelocator.aspx"
     r = session.get(url, headers=headers, verify=False)
     soup = BeautifulSoup(r.text, "html.parser")
     locs = soup.find("div", {"id": "ctl00_PageContent_pnlStores"})
@@ -72,12 +73,12 @@ def fetch_data():
     info2 = locs.findAll("div", {"class": "storehours storehr-popup"})
     info3 = locs.findAll("div", {"class": "calicoalinkdiv"})
     for i in range(len(info)):
-        if i%2 == 0:
+        if i % 2 == 0:
             tit = info[i].text.strip()
             title.append(tit)
         else:
             address = info[i].text
-            ph = address.split('Phone:')[1].split('Email:')[0].strip()
+            ph = address.split("Phone:")[1].split("Email:")[0].strip()
             Phone.append(ph)
 
     for i in info2:
@@ -87,25 +88,30 @@ def fetch_data():
         hour = hour.replace("PM", "PM ").strip()
         hours.append(hour)
 
-    for i in range(len(info3)): 
-        if (i % 3 == 0):
-            locator = info3[i].find('a')['onclick'] #address
-            locator = locator.split('trackgetdirections(')[1].split(')')[0]
+    for i in range(len(info3)):
+        if i % 3 == 0:
+            locator = info3[i].find("a")["onclick"]  # address
+            locator = locator.split("trackgetdirections(")[1].split(")")[0]
             locator = locator.replace("'", "").strip()
             adr.append(locator)
         else:
-            locator = info3[i].find('a')['onclick'].strip() #storeid and city
+            locator = info3[i].find("a")["onclick"].strip()  # storeid and city
             store.append(locator)
-    for i in range(len(store)): # separating store_id from city
-        if (i % 2 == 0):
-            storeid = store[i].split("trackOutboundLink('storepage.aspx?storeid=")[1].split(" '")[0].strip()
+    for i in range(len(store)):  # separating store_id from city
+        if i % 2 == 0:
+            storeid = (
+                store[i]
+                .split("trackOutboundLink('storepage.aspx?storeid=")[1]
+                .split(" '")[0]
+                .strip()
+            )
             store_id.append(storeid)
 
-    for i,j,k,l,m in zip(adr, hours, title, Phone, store_id):
+    for i, j, k, l, m in zip(adr, hours, title, Phone, store_id):
         title = k
         storeid = m
         address = i
-        address = address.split(',')
+        address = address.split(",")
         street = address[0].strip()
         city = address[1].strip()
         state = address[2].strip()
@@ -113,19 +119,18 @@ def fetch_data():
         phone = l
         HOO = j
 
-        if street == 'Strawberry Village Shopping Center Suite 121':
-            street = '800 Redwood Highway Frontage Road, Strawberry Village Shopping Center Suite 121'
+        if street == "Strawberry Village Shopping Center Suite 121":
+            street = "800 Redwood Highway Frontage Road, Strawberry Village Shopping Center Suite 121"
 
-        if street == 'Suite 184':
-            street = '2355 Vanderbilt Beach Rd, Suite 184'
+        if street == "Suite 184":
+            street = "2355 Vanderbilt Beach Rd, Suite 184"
 
-        if street == 'ParkTowne Village':
-            street = '1630 E. Woodlawn Road, ParkTowne Village'
+        if street == "ParkTowne Village":
+            street = "1630 E. Woodlawn Road, ParkTowne Village"
 
-        if street == 'Suite 126/128':
-            street = '1860 Laskin Road, Suite 126/128'
-            
-    
+        if street == "Suite 126/128":
+            street = "1860 Laskin Road, Suite 126/128"
+
         data.append(
             [
                 "https://www.calicocorners.com/",
@@ -146,7 +151,7 @@ def fetch_data():
         )
     return data
 
-        
+
 def scrape():
     logger.info(time.strftime("%H:%M:%S", time.localtime(time.time())))
     data = fetch_data()
