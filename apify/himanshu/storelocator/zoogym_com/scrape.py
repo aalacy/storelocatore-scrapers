897146530a23221f999c60827f497ea747e8c450
Sajjal Malik <40670001+sajjalmalik@users.noreply.github.com>
2021-01-13 23:49:07 +0500

    Add files via upload

--- apify/himanshu/storelocator/zoogym_com/scrape.py
+++ apify/himanshu/storelocator/zoogym_com/scrape.py
@@ -3,68 +3,100 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 import re
 import json
-from urllib.request import urlopen
 
 
 session = SgRequests()
 
+
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", mode="w") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
 
 
-
-
 def fetch_data():
-    base_url = "https://www.zoogym.com/wp-admin/admin-ajax.php?action=store_search&lat=26.101509&lng=-80.26195999999999&max_results=25&search_radius=50&autoload=1"
+    base_url = "http://www.batterysource.com/find_store.php"
     r = session.get(base_url)
-    soup = json.loads(r.text)
-    store_name = []
-    store_detail =[]
-    return_main_object =[]
-    for i in soup:
-        tem_var =[]
-        street_address = i['address']
-        city = i['city']
-        state = i['state']
-        zipcode  = i['zip']
-        country_code = "US"
-        phone  = i["phone"]
-        location_type = 'zoogym'   
-        latitude = i['lat'] 
-        longitude = i['lng']
-        hours_of_operation = "<MISSING>"
-        name = i['store'].replace("<br>"," ").replace("<br />","").replace("(Visit Our Site)","").replace("</a>","")
-        store_name.append(name.rstrip())
+    soup = BeautifulSoup(r.text, "lxml")
+    return_main_object = []
+    data = soup.find_all("div", {"class": "address_block"})
 
-        tem_var.append(street_address)
+    for d in data:
+        tem_var = []
+        # print(list(d.stripped_strings))
+        name = list(d.stripped_strings)[0]
+        st = list(d.stripped_strings)[1]
+        if st == "Coming Soon" or st == "New Store":
+            st = list(d.stripped_strings)[2]
+            city = list(d.stripped_strings)[3].split(",")[0]
+            if list(d.stripped_strings)[3].find(",") != -1:
+                state = list(d.stripped_strings)[3].split(",")[1].split(" ")[1]
+            else:
+                state = "<MISSING>"
+            if list(d.stripped_strings)[3].find(",") != -1:
+                zip1 = list(d.stripped_strings)[3].split(",")[1].split(" ")[2]
+            else:
+                zip1 = "<MISSING>"
+            phone = list(d.stripped_strings)[4].replace("Now Open!", "")
+            hours = list(d.stripped_strings)[5]
+
+            if phone.find("Mon") != -1:
+                hours = phone
+                phone = "<MISSING>"
+        else:
+            city = list(d.stripped_strings)[2].split(",")[0]
+            if list(d.stripped_strings)[2].find(",") != -1:
+                state = list(d.stripped_strings)[2].split(",")[1].split(" ")[1]
+            else:
+                state = "<MISSING>"
+            if list(d.stripped_strings)[2].find(",") != -1:
+                zip1 = list(d.stripped_strings)[2].split(",")[1].split(" ")[2]
+            else:
+                zip1 = "<MISSING>"
+            phone = list(d.stripped_strings)[3].replace("Now Open!", "")
+            hours = list(d.stripped_strings)[4]
+            if phone.find("Mon") != -1:
+                hours = phone
+                phone = "<MISSING>"
+        tem_var.append("https://www.batterysource.com")
+        tem_var.append("https://www.batterysource.com/find_store.php")
+        tem_var.append(name)
+        tem_var.append(st)
         tem_var.append(city)
         tem_var.append(state)
-        tem_var.append(zipcode)
-        tem_var.append(country_code)
+        tem_var.append(zip1)
+        tem_var.append("US")
         tem_var.append("<MISSING>")
         tem_var.append(phone)
-        tem_var.append(location_type)
-        tem_var.append(latitude)
-        tem_var.append(longitude)
-        tem_var.append(hours_of_operation)
-        store_detail.append(tem_var)
-   
-    return_main_object= []
-    for i in range(len(store_name)):
-           store = list()
-           store.append("https://www.zoogym.com")
-           store.append(store_name[i])
-           store.extend(store_detail[i])
-           return_main_object.append(store)
-   
+        tem_var.append("batterysource")
+        tem_var.append("<MISSING>")
+        tem_var.append("<MISSING>")
+        tem_var.append(hours)
+        return_main_object.append(tem_var)
     return return_main_object
 
 
@@ -74,5 +106,3 @@ def scrape():
 
 
 scrape()
-
-
