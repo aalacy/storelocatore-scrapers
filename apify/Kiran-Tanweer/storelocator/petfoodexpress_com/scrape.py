e43435c4be14aa5f005bff8ffb2d53e7fad970b6
Kiran Tanweer <44836465+Kiran-Tanweer@users.noreply.github.com>
2021-01-13 05:48:23 +0500

    Update scrape.py

--- apify/Kiran-Tanweer/storelocator/petfoodexpress_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/petfoodexpress_com/scrape.py
@@ -9,24 +9,23 @@ logger = SgLogSetup().get_logger("petfoodexpress_com")
 
 session = SgRequests()
 headers = {
-    'authority': 'api.petfood.express',
-    'method': 'GET',
-    'path': '/store/pickup',
-    'scheme': 'https',
-    'accept': 'text/plain, */*; q=0.01',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9',
-    'content-type': 'text/html',
-    'if-modified-since': 'Mon, 11 Jan 2021 18:03:11 GMT',
-    'origin': 'https://www.petfood.express',
-    'referer': 'https://www.petfood.express/',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-site',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-    'x-api-key': '{"key":"ZmQ5OWU4NGMtYTY5YS00MWExLTg1NzktNGMzYWVlMDMzYjRk","fibonacciKey":"931295","jwt":null}',
+    "authority": "api.petfood.express",
+    "method": "GET",
+    "path": "/store/pickup",
+    "scheme": "https",
+    "accept": "text/plain, */*; q=0.01",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "text/html",
+    "if-modified-since": "Mon, 11 Jan 2021 18:03:11 GMT",
+    "origin": "https://www.petfood.express",
+    "referer": "https://www.petfood.express/",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "x-api-key": '{"key":"ZmQ5OWU4NGMtYTY5YS00MWExLTg1NzktNGMzYWVlMDMzYjRk","fibonacciKey":"931295","jwt":null}',
 }
-
 
 
 def write_output(data):
@@ -71,20 +70,21 @@ def write_output(data):
                 writer.writerow(row)
         logger.info(f"No of records being processed: {len(temp_list)}")
 
+
 def fetch_data():
     data = []
-    url = 'https://api.petfood.express/store/map/'
+    url = "https://api.petfood.express/store/map/"
     response = session.get(url, headers=headers, verify=False).json()
     for r in response:
-        lat = r['latitude']
-        lng = r['longitude']
-        phone = r['storePhone']
-        title = r['storeName']
-        storeid = r['storeNumber']
-        street = r['storeAddress']
-        city = r['storeCity']
-        state = 'CA'
-        pcode = r['storeZip']
+        lat = r["latitude"]
+        lng = r["longitude"]
+        phone = r["storePhone"]
+        title = r["storeName"]
+        storeid = r["storeNumber"]
+        street = r["storeAddress"]
+        city = r["storeCity"]
+        state = "CA"
+        pcode = r["storeZip"]
         url2 = "https://api.petfood.express/store/details/" + storeid
         p = session.post(url2, headers=headers, verify=False)
         soup = BeautifulSoup(p.text, "html.parser")
