a011383ff0c1d5735f22ee7948568d5cb583b644
Tenzing Shaw <tenzing@safegraph.com>
2021-01-12 19:37:23 -0800

    force build

--- apify/quincya/storelocator/mtb_com/scrape.py
+++ apify/quincya/storelocator/mtb_com/scrape.py
@@ -38,7 +38,6 @@ def write_output(data):
 
 
 def fetch_data():
-
     base_link = "https://locations.mtb.com/"
 
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"
