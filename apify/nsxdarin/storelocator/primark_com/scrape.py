432bc9b9b072654cf7fb88b1d15861e35193b284
nsxdarin <nsxdarin@gmail.com>
2021-01-18 10:08:19 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/primark_com/scrape.py
+++ apify/nsxdarin/storelocator/primark_com/scrape.py
@@ -39,6 +39,7 @@ def write_output(data):
 
 def fetch_data():
     locs = []
+    infos = []
     for x in range(0, 7):
         url = "https://stores.primark.com/sitemap.xml." + str(x)
         r = session.get(url, headers=headers)
@@ -77,60 +78,67 @@ def fetch_data():
         country = "US"
         if "-kingdom/" in loc:
             country = "GB"
-        r2 = session.get(loc, headers=headers)
-        for line2 in r2.iter_lines():
-            line2 = str(line2.decode("utf-8"))
-            if "<title>" in line2:
-                name = line2.split("<title>")[1].split("|")[0].strip()
-            if '"line1":"' in line2:
-                add = line2.split('"line1":"')[1].split('"')[0]
-                try:
-                    add = add + " " + line2.split('"line2":"')[1].split('"')[0]
-                except:
-                    pass
-                try:
-                    add = add + " " + line2.split('"line3":"')[1].split('"')[0]
-                except:
-                    pass
-                add = add.strip()
-                zc = line2.split('"postalCode":"')[1].split('"')[0]
-                city = line2.split('"city":"')[1].split('"')[0]
-                try:
-                    state = line2.split('"region":"')[1].split('"')[0]
-                except:
-                    state = "<MISSING>"
-                phone = (
-                    line2.split('"mainPhone":')[1].split('"display":"')[1].split('"')[0]
-                )
-            if 'itemprop="latitude" content="' in line2:
-                lat = line2.split('itemprop="latitude" content="')[1].split('"')[0]
-                lng = line2.split('itemprop="longitude" content="')[1].split('"')[0]
-            if '"openingHours" content="' in line2:
-                days = line2.split('"openingHours" content="')
-                for day in days:
-                    if '<td class="c-hours-details-row-day"' in day:
-                        hrs = day.split('"')[0]
-                        if hours == "":
-                            hours = hrs
-                        else:
-                            hours = hours + "; " + hrs
-        if add != "":
-            yield [
-                website,
-                loc,
-                name,
-                add,
-                city,
-                state,
-                zc,
-                country,
-                store,
-                phone,
-                typ,
-                lat,
-                lng,
-                hours,
-            ]
+            r2 = session.get(loc, headers=headers)
+            for line2 in r2.iter_lines():
+                line2 = str(line2.decode("utf-8"))
+                if "<title>" in line2:
+                    name = line2.split("<title>")[1].split("|")[0].strip()
+                if '"line1":"' in line2:
+                    add = line2.split('"line1":"')[1].split('"')[0]
+                    try:
+                        add = add + " " + line2.split('"line2":"')[1].split('"')[0]
+                    except:
+                        pass
+                    try:
+                        add = add + " " + line2.split('"line3":"')[1].split('"')[0]
+                    except:
+                        pass
+                    add = add.strip()
+                    zc = line2.split('"postalCode":"')[1].split('"')[0]
+                    city = line2.split('"city":"')[1].split('"')[0]
+                    try:
+                        state = line2.split('"region":"')[1].split('"')[0]
+                    except:
+                        state = "<MISSING>"
+                    phone = (
+                        line2.split('"mainPhone":')[1]
+                        .split('"display":"')[1]
+                        .split('"')[0]
+                    )
+                if 'itemprop="latitude" content="' in line2:
+                    lat = line2.split('itemprop="latitude" content="')[1].split('"')[0]
+                    lng = line2.split('itemprop="longitude" content="')[1].split('"')[0]
+                if '"openingHours" content="' in line2:
+                    days = line2.split('"openingHours" content="')
+                    for day in days:
+                        if '<td class="c-hours-details-row-day"' in day:
+                            hrs = day.split('"')[0]
+                            if hours == "":
+                                hours = hrs
+                            else:
+                                hours = hours + "; " + hrs
+            if add != "":
+                if "1 Chequers Mall" in add:
+                    hours = "Mon-Sat: 8:00-20:00; Sun: 11:00-17:00"
+                addinfo = add + "|" + city + "|" + zc
+                if addinfo not in infos:
+                    infos.append(addinfo)
+                    yield [
+                        website,
+                        loc,
+                        name,
+                        add,
+                        city,
+                        state,
+                        zc,
+                        country,
+                        store,
+                        phone,
+                        typ,
+                        lat,
+                        lng,
+                        hours,
+                    ]
 
 
 def scrape():
