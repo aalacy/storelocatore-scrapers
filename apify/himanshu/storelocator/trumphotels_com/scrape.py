b1a2a7a6242adacdb019983e8f4cfa27ad6e24d2
Michael Dinh <mkdinh94@gmail.com>
2021-01-17 21:55:52 -0500

    SLSC-3624: update ssl

--- apify/himanshu/storelocator/trumphotels_com/scrape.py
+++ apify/himanshu/storelocator/trumphotels_com/scrape.py
@@ -1,8 +1,11 @@
 import csv
 from bs4 import BeautifulSoup
-from sgselenium import SgSelenium
+from sglogging import SgLogSetup
+from sgselenium import SgFirefox, SgChrome
 import os
 import json
+
+logger = SgLogSetup().get_logger("trumphotels.com")
 
 
 def write_output(data):
@@ -33,7 +36,7 @@ def write_output(data):
 
 
 def fetch_data():
-    driver = SgSelenium().firefox(executable_path=os.path.abspath("geckodriver"))
+    driver = SgChrome().driver()
     base_url = "https://www.trumphotels.com"
     driver.get("https://www.trumphotels.com/")
     soup = BeautifulSoup(driver.page_source, "lxml")
@@ -42,14 +45,15 @@ def fetch_data():
         "div", {"class": "filterlist"}
     ):
         for location in country.find_all("a"):
+            logger.info(f" scraping: {location}")
             driver.get(base_url + location["href"])
             page_url = base_url + location["href"]
-            location_soup = BeautifulSoup(driver.page_source, "lxml")
+            location_soup = BeautifulSoup(driver.page_source, "html.parser")
             for script in location_soup.find_all(
                 "script", {"type": "application/ld+json"}
             ):
-                if "address" in json.loads(script.text):
-                    store_data = json.loads(script.text)
+                store_data = json.loads(script.string)
+                if "address" in store_data:
                     if store_data["address"]["addressCountry"] not in ("USA", "Canada"):
                         continue
                     name = location.text.strip()
