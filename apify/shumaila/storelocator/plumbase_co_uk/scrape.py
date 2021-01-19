55da65f76351af3ccb45469f4b9d964daa2c83c5
Sajjal Malik <40670001+sajjalmalik@users.noreply.github.com>
2021-01-19 22:57:37 +0500

    Update scrape.py

--- apify/shumaila/storelocator/plumbase_co_uk/scrape.py
+++ apify/shumaila/storelocator/plumbase_co_uk/scrape.py
@@ -3,6 +3,7 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup
 from sglogging import SgLogSetup
 import json
+
 logger = SgLogSetup().get_logger("plumbase_com")
 
 
@@ -47,17 +48,16 @@ def fetch_data():
     base_url = "https://www.plumbase.co.uk/storefinder/stores"
     r = session.get(base_url, headers=headers)
     s = json.loads(r.text)
-    s = s['all']
+    s = s["all"]
     for loc in s:
-        
+
         title = loc["LocationName"]
         phone = loc["Phone"]
-        # street = loc["address"]
-        if (loc["Address4"]):
-            street = loc["Address1"] +", "+loc["Address2"] +", "+ loc["Address3"]
+        if loc["Address4"]:
+            street = loc["Address1"] + ", " + loc["Address2"] + ", " + loc["Address3"]
             city = loc["Address4"]
-        else: 
-            street = loc["Address1"] +", "+loc["Address2"]
+        else:
+            street = loc["Address1"] + ", " + loc["Address2"]
             city = loc["Address3"]
         state = "<MISSING>"
         zip = loc["postcode"]
@@ -69,15 +69,32 @@ def fetch_data():
             store_type = "<MISSING>"
         storenum = loc["BranchID"]
         if loc["SmallTimes"]:
-            weekday =  loc["SmallTimes"][0]['DayOfWeek'] +": "+ loc["SmallTimes"][0]['OpeningAt'] +" - "+ loc["SmallTimes"][0]['ClosingAt'] 
-            sat = loc["SmallTimes"][1]['DayOfWeek'] + ": "+ loc["SmallTimes"][1]['OpeningAt'] +" - "+ loc["SmallTimes"][1]['ClosingAt'] 
-            sun = loc["SmallTimes"][2]['DayOfWeek'] + ": "+ loc["SmallTimes"][2]['OpeningAt'] +" - "+ loc["SmallTimes"][2]['ClosingAt']
+            weekday = (
+                loc["SmallTimes"][0]["DayOfWeek"]
+                + ": "
+                + loc["SmallTimes"][0]["OpeningAt"]
+                + " - "
+                + loc["SmallTimes"][0]["ClosingAt"]
+            )
+            sat = (
+                loc["SmallTimes"][1]["DayOfWeek"]
+                + ": "
+                + loc["SmallTimes"][1]["OpeningAt"]
+                + " - "
+                + loc["SmallTimes"][1]["ClosingAt"]
+            )
+            sun = (
+                loc["SmallTimes"][2]["DayOfWeek"]
+                + ": "
+                + loc["SmallTimes"][2]["OpeningAt"]
+                + " - "
+                + loc["SmallTimes"][2]["ClosingAt"]
+            )
             if sun.find("Closed") != -1:
-                sun = sun.replace("-","").rstrip() 
-            hours_of_operation = weekday+"\n"+sat+"\n"+sun
-        else: 
+                sun = sun.replace("-", "").rstrip()
+            hours_of_operation = weekday + "\n" + sat + "\n" + sun
+        else:
             hours_of_operation = "<MISSING>"
-        print(title)
         data.append(
             [
                 "https://www.plumbase.co.uk",
