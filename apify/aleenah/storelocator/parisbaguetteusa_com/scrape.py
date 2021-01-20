a88de26a77eb54a5079a8899f1d4edea745a8b74
Michael Dinh <mkdinh94@gmail.com>
2021-01-19 21:04:03 -0500

    linting

--- apify/aleenah/storelocator/parisbaguetteusa_com/scrape.py
+++ apify/aleenah/storelocator/parisbaguetteusa_com/scrape.py
@@ -35,14 +35,17 @@ def write_output(data):
         # Body
         for row in data:
             writer.writerow(row)
-            
+
+
 def get_json_data(html):
-    re_get_json = "var location_data[\s\S]*?=[\s\S]*?(.+?);$"
+    re_get_json = r"var location_data[\s\S]*?=[\s\S]*?(.+?);$"
     match = re.search(re_get_json, html, re.MULTILINE)
     json_text = match.group(1)
     return json.loads(json_text)
 
+
 driver = SgSelenium().chrome()
+
 
 def fetch_data():
 
