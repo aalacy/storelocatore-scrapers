9967b3379fac078e6cb665b5d74f4753555b67b0
Tenzing Shaw <tenzing@safegraph.com>
2021-01-20 17:10:26 -0800

    motel 6 add retries (#2839)
    * motel 6 add retries
    
    * rotate proxy
    
    * flake8 issue

--- apify/nsxdarin/storelocator/motel6_com/scrape.py
+++ apify/nsxdarin/storelocator/motel6_com/scrape.py
@@ -2,11 +2,11 @@ import csv
 from sgrequests import SgRequests
 import json
 from sglogging import SgLogSetup
+from tenacity import retry, stop_after_attempt
 
 logger = SgLogSetup().get_logger("motel6_com")
 
 
-session = SgRequests()
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
@@ -39,10 +39,19 @@ def write_output(data):
             writer.writerow(row)
 
 
+@retry(stop=stop_after_attempt(3))
+def get_url(lurl):
+    session = SgRequests()
+    r = session.get(lurl, headers=headers)
+    if r.encoding is None:
+        r.encoding = "utf-8"
+
+
 def fetch_data():
     locs = []
     ids = []
     url = "https://www.motel6.com/content/g6-cache/property-summary.1.json"
+    session = SgRequests()
     r = session.get(url, headers=headers)
     if r.encoding is None:
         r.encoding = "utf-8"
@@ -59,9 +68,7 @@ def fetch_data():
         typ = ""
         hours = "<MISSING>"
         lurl = "https://www.motel6.com/bin/g6/propertydata." + loc + ".json?lng=en"
-        r2 = session.get(lurl, headers=headers)
-        if r2.encoding is None:
-            r2.encoding = "utf-8"
+        r2 = get_url(lurl)
         try:
             array = json.loads(r2.content)
             lat = array["latitude"]
