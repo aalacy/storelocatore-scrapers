9e4a867448f406e77b220b11292a862fabc282ca
vivek <vivek@rentechdigital.com>
2021-01-23 11:00:28 +0530

    Done

--- apify/himanshu/storelocator/westin_marriott_com/scrape.py
+++ apify/himanshu/storelocator/westin_marriott_com/scrape.py
@@ -67,6 +67,7 @@ def request_wrapper(url, method, headers, data=None):
 
 
 def fetch_data():
+    address = []
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
     }
