3f05c052c329cd6739a9339e10701237c0a54cee
Kiran Tanweer <44836465+Kiran-Tanweer@users.noreply.github.com>
2021-01-24 05:51:53 +0500

    Update scrape.py

--- apify/Kiran-Tanweer/storelocator/sutherlands_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/sutherlands_com/scrape.py
@@ -12,19 +12,19 @@ logger = SgLogSetup().get_logger("sutherlands_com")
 session = SgRequests()
 
 headers = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Connection': 'keep-alive',
-    'Cookie': 'storenum=0; _ALGOLIA=anonymous-8722c4dc-e3c4-4db6-a1b3-2d6ef1f8bb67; _ga=GA1.2.960820933.1610610759; _fbp=fb.1.1610610759998.1510881136; laravel_session=ZpRJb3zRSCGuCLnd6lAelk7Pdzbng00RGvG23Uig; 302e79545f8dc1468f3d3ba795b88f1e1d9307b0=ZpRJb3zRSCGuCLnd6lAelk7Pdzbng00RGvG23Uig; _gid=GA1.2.783065270.1611164438; _gat_gtag_UA_7521516_1=1; LTC=eyJpdiI6IkozODBBSzdSMmVSWW5TYTdDRHdMVGc9PSIsInZhbHVlIjoiVDF0TVFZbFhuZ1wvMGFVT3NxRmt2OGN1UzlGZjh2MU1kRDFiTk9FbWdCcXI0WUJGa1wvNlA3YmlRTVhXS1k3XC95cTRGWE1rU2FEcWo5RFAxN1U1bURVXC81MHk1VXgwVTNpaEpNcEFGcHg5QmxVPSIsIm1hYyI6ImEzMGEwYTc0M2ZmZjJjZDMwNWNiMzIzYTJhYjE3ZGEwYjg3MWNhN2YyMzMyNWY0ODk2Y2FhZjU0ZGZmOTFmNzAifQ%3D%3D; XSRF-TOKEN=eyJpdiI6ImdsbU96WEdEXC9WUGY4WCt5ZFwvOWxaUT09IiwidmFsdWUiOiJ5VXQzWkJLMFh4Z2w0Vjg2MVFNaGR3clUwYkd5MDhTcUg3bFljNFRwTHVKUjQ4RnhoYk83akRnU1Uzd1hqSjFIOHU1eVlWek9Mb0pFcWdmYk5WYlRSWjkzUzNzWFN2M1MzUE5zSGx0dm9mMmZLaVBCbytLTEtpbldXYU1UZW1udCIsIm1hYyI6ImQ5N2RiMWEyZjFkMjAzMDdiYjQ0MWJkYzM0M2QxMjU0ZDFjNTQ0NzE1OTM2ZDBiZTUyYTUzNGM1MWJlZWRhN2YifQ%3D%3D',
-    'Host': 'sutherlands.com',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
-    'X-CSRF-TOKEN': 'XPpILMzHJ0U6NJRG8V7PTp8qcgGoussTlpvHLwtw',
-    'X-Requested-With': 'XMLHttpRequest',
-    }
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Cookie": "storenum=0; _ALGOLIA=anonymous-8722c4dc-e3c4-4db6-a1b3-2d6ef1f8bb67; _ga=GA1.2.960820933.1610610759; _fbp=fb.1.1610610759998.1510881136; laravel_session=ZpRJb3zRSCGuCLnd6lAelk7Pdzbng00RGvG23Uig; 302e79545f8dc1468f3d3ba795b88f1e1d9307b0=ZpRJb3zRSCGuCLnd6lAelk7Pdzbng00RGvG23Uig; _gid=GA1.2.783065270.1611164438; _gat_gtag_UA_7521516_1=1; LTC=eyJpdiI6IkozODBBSzdSMmVSWW5TYTdDRHdMVGc9PSIsInZhbHVlIjoiVDF0TVFZbFhuZ1wvMGFVT3NxRmt2OGN1UzlGZjh2MU1kRDFiTk9FbWdCcXI0WUJGa1wvNlA3YmlRTVhXS1k3XC95cTRGWE1rU2FEcWo5RFAxN1U1bURVXC81MHk1VXgwVTNpaEpNcEFGcHg5QmxVPSIsIm1hYyI6ImEzMGEwYTc0M2ZmZjJjZDMwNWNiMzIzYTJhYjE3ZGEwYjg3MWNhN2YyMzMyNWY0ODk2Y2FhZjU0ZGZmOTFmNzAifQ%3D%3D; XSRF-TOKEN=eyJpdiI6ImdsbU96WEdEXC9WUGY4WCt5ZFwvOWxaUT09IiwidmFsdWUiOiJ5VXQzWkJLMFh4Z2w0Vjg2MVFNaGR3clUwYkd5MDhTcUg3bFljNFRwTHVKUjQ4RnhoYk83akRnU1Uzd1hqSjFIOHU1eVlWek9Mb0pFcWdmYk5WYlRSWjkzUzNzWFN2M1MzUE5zSGx0dm9mMmZLaVBCbytLTEtpbldXYU1UZW1udCIsIm1hYyI6ImQ5N2RiMWEyZjFkMjAzMDdiYjQ0MWJkYzM0M2QxMjU0ZDFjNTQ0NzE1OTM2ZDBiZTUyYTUzNGM1MWJlZWRhN2YifQ%3D%3D",
+    "Host": "sutherlands.com",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    "X-CSRF-TOKEN": "XPpILMzHJ0U6NJRG8V7PTp8qcgGoussTlpvHLwtw",
+    "X-Requested-With": "XMLHttpRequest",
+}
 
 
 def write_output(data):
@@ -52,7 +52,7 @@ def write_output(data):
             ]
         )
 
-        temp_list = []  
+        temp_list = []
         for row in data:
             comp_list = [
                 row[2].strip(),
@@ -68,55 +68,53 @@ def write_output(data):
                 writer.writerow(row)
         logger.info(f"No of records being processed: {len(temp_list)}")
 
-        
-
 
 def fetch_data():
     data = []
-    pattern = re.compile(r'\s\s+')
-    cleanr = re.compile(r'<[^>]+>')
+    pattern = re.compile(r"\s\s+")
+    cleanr = re.compile(r"<[^>]+>")
     zips = static_zipcode_list(radius=50, country_code=SearchableCountries.USA)
     for zip_code in zips:
-        search_url = "https://sutherlands.com/locations/"+ zip_code + "?distance=150"
+        search_url = "https://sutherlands.com/locations/" + zip_code + "?distance=150"
         stores_req = session.get(search_url, headers=headers)
         soup = BeautifulSoup(stores_req.text, "html.parser")
         if len(soup) > 2:
-            loc_block = soup.findAll("div", {"class" : "card-block"})
+            loc_block = soup.findAll("div", {"class": "card-block"})
             for loc in loc_block:
-                linkdiv = loc.findAll("div", {"class":"col-md-4"})[2].findAll("p")
+                linkdiv = loc.findAll("div", {"class": "col-md-4"})[2].findAll("p")
                 if len(linkdiv) == 2:
-                    title = loc.find('h3').text.strip()
-                    street = loc.find("span", {"itemprop" : "streetAddress"}).text
-                    city = loc.find("span", {"itemprop" : "addressLocality"}).text
-                    state = loc.find("span", {"itemprop" : "addressRegion"}).text
-                    pcode = loc.find("span", {"itemprop" : "postalCode"}).text
-                    phone = loc.find("p", {"class" : "phone"}).text.strip()
-                    link = 'https://sutherlands.com/locations/' + pcode
-                    lat = '<MISSING>'
-                    lng = '<MISSING>'
-                    hours = loc.findAll("div", {"class":"col-md-4"})[1].text
-                    hours = hours.replace('\n', ' ')
+                    title = loc.find("h3").text.strip()
+                    street = loc.find("span", {"itemprop": "streetAddress"}).text
+                    city = loc.find("span", {"itemprop": "addressLocality"}).text
+                    state = loc.find("span", {"itemprop": "addressRegion"}).text
+                    pcode = loc.find("span", {"itemprop": "postalCode"}).text
+                    phone = loc.find("p", {"class": "phone"}).text.strip()
+                    link = "https://sutherlands.com/locations/" + pcode
+                    lat = "<MISSING>"
+                    lng = "<MISSING>"
+                    hours = loc.findAll("div", {"class": "col-md-4"})[1].text
+                    hours = hours.replace("\n", " ")
                     hours = hours.lstrip("Operating Hours: ")
                     hours = hours.replace("M-F:                     ", "Mon-Fri: ")
-                    hours = re.sub(pattern, ' ', hours)
-                    hours = re.sub(cleanr, ' ', hours)
-                    storeid = '<MISSING>'
+                    hours = re.sub(pattern, " ", hours)
+                    hours = re.sub(cleanr, " ", hours)
+                    storeid = "<MISSING>"
                 else:
-                    link = linkdiv[2].find('a')['href']
-                    street = loc.find("span", {"itemprop" : "streetAddress"}).text
-                    city = loc.find("span", {"itemprop" : "addressLocality"}).text
-                    state = loc.find("span", {"itemprop" : "addressRegion"}).text
-                    pcode = loc.find("span", {"itemprop" : "postalCode"}).text
-                    phone = loc.find("p", {"class" : "phone"}).text.strip()
-                    hours = loc.findAll("div", {"class":"col-md-4"})[1].text
-                    hours = hours.replace('\n', ' ')
+                    link = linkdiv[2].find("a")["href"]
+                    street = loc.find("span", {"itemprop": "streetAddress"}).text
+                    city = loc.find("span", {"itemprop": "addressLocality"}).text
+                    state = loc.find("span", {"itemprop": "addressRegion"}).text
+                    pcode = loc.find("span", {"itemprop": "postalCode"}).text
+                    phone = loc.find("p", {"class": "phone"}).text.strip()
+                    hours = loc.findAll("div", {"class": "col-md-4"})[1].text
+                    hours = hours.replace("\n", " ")
                     hours = hours.lstrip("Operating Hours: ")
                     hours = hours.replace("M-F:                     ", "Mon-Fri: ")
-                    hours = re.sub(pattern, ' ', hours)
-                    hours = re.sub(cleanr, ' ', hours)
+                    hours = re.sub(pattern, " ", hours)
+                    hours = re.sub(cleanr, " ", hours)
                     req = session.get(link, headers=headers)
                     soup = BeautifulSoup(req.text, "html.parser")
-                    title = soup.find('h1', {"class" : "h2"}).text.strip()
+                    title = soup.find("h1", {"class": "h2"}).text.strip()
                     storeid = link.split("/")[-1]
                     coodiv = soup.findAll("script")
                     scriptcoord = str(coodiv[12])
