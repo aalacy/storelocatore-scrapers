8e322fa89fb906c07cc4172cd6c87e45513405e9
datarhan <datarhan@github.com>
2021-01-26 12:04:06 +0100

    requirements update

--- apify/datarhan/storelocator/covance_com/scrape.py
+++ apify/datarhan/storelocator/covance_com/scrape.py
@@ -45,13 +45,6 @@ def fetch_data():
     DOMAIN = "covance.com"
     start_url = "https://www.covance.com/locations.html"
 
-    hdr = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,pt;q=0.6",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
-    }
-
     response = session.get(start_url)
     dom = etree.HTML(response.text)
     all_locations = dom.xpath('//div[@class="locations-results-row"]')
