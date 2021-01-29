2c22d0001d0ff1894784dc72c671d817d5a89dc2
idev-a <ideveloper003@protonmail.com>
2021-01-29 06:33:14 -0800

    merge

--- apify/idev-a/storelocator/equinix_com/README.md
+++ apify/idev-a/storelocator/equinix_com/README.md
@@ -1,4 +1,7 @@
 There are some potential issues when running the validate.py
 
-# --ignore CountryCodeFillRateChecker
-This is not US or CA data.
+# --ignore LatLngDuplicationValidator 
+
+# --ignore StreetAddressHasStateName 
+
+# --ignore CountryValidator
--- apify/idev-a/storelocator/equinix_com/scrape.py
+++ apify/idev-a/storelocator/equinix_com/scrape.py
@@ -1,5 +1,4 @@
 import csv
-from bs4 import BeautifulSoup as bs
 from sgrequests import SgRequests
 from urllib.parse import urljoin
 import json
@@ -44,83 +43,96 @@ def write_output(data):
 
 
 def fetch_data():
-    locator_domain = 'https://www.equinix.com/data-centers/americas-colocation/'
+    locator_domain = "https://www.equinix.com/data-centers/americas-colocation/"
     base_url = "https://www.equinix.com/api/marketo/buildjson?_=1611881724493"
     r = session.get(base_url)
-    links = json.loads(r.text)['mapping-data']
+    links = json.loads(r.text)["mapping-data"]
     data = []
     for key, value in links.items():
-        if key.startswith('/locations/americas-colocation/united-states-colocation/') or key.startswith('/locations/americas-colocation/canada-colocation/'):
+        if key.startswith(
+            "/locations/americas-colocation/united-states-colocation/"
+        ) or key.startswith("/locations/americas-colocation/canada-colocation/"):
             page_url = urljoin("https://www.equinix.com", key)
-            if len( key.split('/')) == 6:
+            if len(key.split("/")) == 6:
                 continue
-            _id = key.split('/')[-2]
-            link = f'https://www.equinix.com/api/content/render/false/type/json/query/+contentType:Location%2520+Location.title:{_id}%2520+languageId:1%2520deleted:false%2520+working:true/orderby/modDate%2520desc'
+            _id = key.split("/")[-2]
+            link = f"https://www.equinix.com/api/content/render/false/type/json/query/+contentType:Location%2520+Location.title:{_id}%2520+languageId:1%2520deleted:false%2520+working:true/orderby/modDate%2520desc"
             r1 = session.get(link)
             _location = json.loads(r1.text)
-            if 'contentlets' in _location and _location['contentlets']:
-                location = _location['contentlets'][0]
+            if "contentlets" in _location and _location["contentlets"]:
+                location = _location["contentlets"][0]
                 location_name = location["popupLinkText"]
-                street_address = ''
-                city = ''
-                state = ''
-                zip = ''
-                # if page_url == 'https://www.equinix.com/locations/americas-colocation/canada-colocation/calgary-data-centers/cl2/':
-                #     import pdb; pdb.set_trace()
-                _split = location["address"].split(',')
-                if 'canada' in location["address"].lower():
-                    street_address = _split[0]
-                    country_code = 'CA'
+                street_address = ""
+                city = ""
+                state = ""
+                zip = ""
+                _split = location["address"].split(",")
+                if "canada" in location["address"].lower():
+                    street_address = "".join(_split[:-3])
+                    country_code = "CA"
                     city = _split[-3].strip()
-                    state = ''.join(_split[-2].strip().split(' ')[:-2])
-                    zip = ' '.join(_split[-2].strip().split(' ')[-2:])
-                elif 'canada' in location.get("description", "").lower():
-                    street_address = _split[0]
-                    country_code = 'CA'
+                    _state = myutil._strip_list(_split[-2].strip().split(" "))
+                    state = _state[0].strip()
+                    zip = " ".join(_state[1:]).strip()
+                elif "canada" in location.get("description", "").lower():
+                    street_address = "".join(_split[:-2]).strip()
+                    country_code = "CA"
                     city = _split[-2].strip()
-                    state = ''.join(_split[-1].strip().split(' ')[:-2])
-                    zip = ' '.join(_split[-1].strip().split(' ')[-2:])
+                    _state = myutil._strip_list(_split[-1].strip().split(" "))
+                    state = _state[0].strip()
+                    zip = " ".join(_state[1:]).strip()
                 else:
-
                     if len(_split) == 1:
-                        street_address = _split[0].split('\n')[0].strip()
-                        city = ''.join(_split[0].split('\n')[-1].split(' ')[:-2]).strip()
+                        street_address = _split[0].split("\n")[0].strip()
+                        city = "".join(
+                            _split[0].split("\n")[-1].split(" ")[:-2]
+                        ).strip()
                     elif len(_split) == 2:
-                        street_address = _split[0].split('\n')[0].replace('<br>', '').strip()
-                        if len(_split[0].split('\n')) > 1:
-                            city = _split[0].split('\n')[-1].strip().split(' ')[-1].strip()
-                        if len(_split[0].split('\n')) > 2:
-                            street_address += ' ' + _split[0].split('\n')[-2]
+                        street_address = (
+                            _split[0].split("\n")[0].replace("<br>", "").strip()
+                        )
+                        if len(_split[0].split("\n")) > 1:
+                            city = (
+                                _split[0].split("\n")[-1].strip().split(" ")[-1].strip()
+                            )
+                        if len(_split[0].split("\n")) > 2:
+                            street_address += " " + _split[0].split("\n")[-2]
                     else:
-                        street_address = ''.join(_split[:-1]).strip()
-                        city = ''.join(_split[-1].strip().split(' ')[:-2]).strip()
+                        street_address = "".join(_split[:-1]).strip()
+                        city = "".join(_split[-1].strip().split(" ")[:-2]).strip()
                         if not city:
-                            if len(_split[1].split('\n')) > 1:
-                                city = _split[1].split('\n')[1]
-                                street_address = _split[0] +  _split[1].split('\n')[0].replace('<br>', '').strip()
+                            if len(_split[1].split("\n")) > 1:
+                                city = _split[1].split("\n")[1]
+                                street_address = (
+                                    _split[0]
+                                    + _split[1]
+                                    .split("\n")[0]
+                                    .replace("<br>", "")
+                                    .strip()
+                                )
                             else:
                                 city = _split[1].strip()
                     try:
-                        _split1 = _split[-1].strip().split(' ')
+                        _split1 = _split[-1].strip().split(" ")
                         state = _split1[-2].strip()
                         zip = _split1[-1].strip()
-                        if len(_split) > 1 and len(_split1) >= 3: # has city
-                            city = ' '.join(_split1[:-2])
+                        if len(_split) > 1 and len(_split1) >= 3:  # has city
+                            city = " ".join(_split1[:-2])
                     except:
                         zip = _split[-1].strip()
                         state = _split[-2].strip()
-                        street_address = _split[0].split('\n')[0].strip()
-                        city = _split[0].split('\n')[1].strip()
-                        
+                        street_address = _split[0].split("\n")[0].strip()
+                        city = _split[0].split("\n")[1].strip()
+
                     country_code = myutil.get_country_by_code(state)
                 phone = location.get("localSupport")
                 if not phone:
                     phone = location.get("internationalSupport")
-                store_number = '<MISSING>'
-                location_type = '<MISSING>'
+                store_number = "<MISSING>"
+                location_type = "<MISSING>"
                 latitude = location["latitude"]
                 longitude = location["longitude"]
-                hours_of_operation = '<INACCESSIBLE>'
+                hours_of_operation = "<INACCESSIBLE>"
 
                 data.append(
                     [
@@ -140,8 +152,6 @@ def fetch_data():
                         hours_of_operation,
                     ]
                 )
-            else:
-                pass
 
     return data
 
