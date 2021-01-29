e31d681890e786d78294599af3b9f06dc47df0dc
idev-a <ideveloper003@protonmail.com>
2021-01-29 02:56:26 -0800

    update equinix.com" " ;

--- apify/idev-a/storelocator/equinix_com/scrape.py
+++ apify/idev-a/storelocator/equinix_com/scrape.py
@@ -2,6 +2,8 @@ import csv
 from bs4 import BeautifulSoup as bs
 from sgrequests import SgRequests
 from urllib.parse import urljoin
+import json
+import re
 
 from util import Util  # noqa: I900
 
@@ -42,59 +44,104 @@ def write_output(data):
 
 
 def fetch_data():
-    locator_domain = "https://www.equinix.com/data-centers/americas-colocation/"
-    r = session.get(locator_domain)
-    soup = bs(r.text, "lxml")
-    sections = soup.select("div.eq-region")
+    locator_domain = 'https://www.equinix.com/data-centers/americas-colocation/'
+    base_url = "https://www.equinix.com/api/marketo/buildjson?_=1611881724493"
+    r = session.get(base_url)
+    links = json.loads(r.text)['mapping-data']
     data = []
-    import pdb
+    for key, value in links.items():
+        if key.startswith('/locations/americas-colocation/united-states-colocation/') or key.startswith('/locations/americas-colocation/canada-colocation/'):
+            page_url = urljoin("https://www.equinix.com", key)
+            if len( key.split('/')) == 6:
+                continue
+            _id = key.split('/')[-2]
+            link = f'https://www.equinix.com/api/content/render/false/type/json/query/+contentType:Location%2520+Location.title:{_id}%2520+languageId:1%2520deleted:false%2520+working:true/orderby/modDate%2520desc'
+            r1 = session.get(link)
+            _location = json.loads(r1.text)
+            if 'contentlets' in _location and _location['contentlets']:
+                location = _location['contentlets'][0]
+                location_name = location["popupLinkText"]
+                street_address = ''
+                city = ''
+                state = ''
+                zip = ''
+                # if page_url == 'https://www.equinix.com/locations/americas-colocation/canada-colocation/calgary-data-centers/cl2/':
+                #     import pdb; pdb.set_trace()
+                _split = location["address"].split(',')
+                if 'canada' in location["address"].lower():
+                    street_address = _split[0]
+                    country_code = 'CA'
+                    city = _split[-3].strip()
+                    state = ''.join(_split[-2].strip().split(' ')[:-2])
+                    zip = ' '.join(_split[-2].strip().split(' ')[-2:])
+                elif 'canada' in location.get("description", "").lower():
+                    street_address = _split[0]
+                    country_code = 'CA'
+                    city = _split[-2].strip()
+                    state = ''.join(_split[-1].strip().split(' ')[:-2])
+                    zip = ' '.join(_split[-1].strip().split(' ')[-2:])
+                else:
 
-    pdb.set_trace()
-    for section in sections:
-        if section.h2.id == "united-states" or section.h2.id == "canada":
-            links = section.select("a")
-            for link in links:
-                page_url = urljoin("https://www.equinix.com", link["href"])
-                r1 = session.get(page_url)
-                soup1 = bs(r1.text, "lxml")
-                lead = soup1.select_one("div.eq-ibx-contact p").text
-                store_number = myutil._valid(location["id"])
-                location_name = location["store"]
-                street_address = myutil._valid(
-                    f"{location.get('address')} {location.get('address2')}"
+                    if len(_split) == 1:
+                        street_address = _split[0].split('\n')[0].strip()
+                        city = ''.join(_split[0].split('\n')[-1].split(' ')[:-2]).strip()
+                    elif len(_split) == 2:
+                        street_address = _split[0].split('\n')[0].replace('<br>', '').strip()
+                        if len(_split[0].split('\n')) > 1:
+                            city = _split[0].split('\n')[-1].strip().split(' ')[-1].strip()
+                        if len(_split[0].split('\n')) > 2:
+                            street_address += ' ' + _split[0].split('\n')[-2]
+                    else:
+                        street_address = ''.join(_split[:-1]).strip()
+                        city = ''.join(_split[-1].strip().split(' ')[:-2]).strip()
+                        if not city:
+                            if len(_split[1].split('\n')) > 1:
+                                city = _split[1].split('\n')[1]
+                                street_address = _split[0] +  _split[1].split('\n')[0].replace('<br>', '').strip()
+                            else:
+                                city = _split[1].strip()
+                    try:
+                        _split1 = _split[-1].strip().split(' ')
+                        state = _split1[-2].strip()
+                        zip = _split1[-1].strip()
+                        if len(_split) > 1 and len(_split1) >= 3: # has city
+                            city = ' '.join(_split1[:-2])
+                    except:
+                        zip = _split[-1].strip()
+                        state = _split[-2].strip()
+                        street_address = _split[0].split('\n')[0].strip()
+                        city = _split[0].split('\n')[1].strip()
+                        
+                    country_code = myutil.get_country_by_code(state)
+                phone = location.get("localSupport")
+                if not phone:
+                    phone = location.get("internationalSupport")
+                store_number = '<MISSING>'
+                location_type = '<MISSING>'
+                latitude = location["latitude"]
+                longitude = location["longitude"]
+                hours_of_operation = '<INACCESSIBLE>'
+
+                data.append(
+                    [
+                        locator_domain,
+                        page_url,
+                        location_name,
+                        street_address,
+                        city,
+                        state,
+                        zip,
+                        country_code,
+                        store_number,
+                        phone,
+                        location_type,
+                        latitude,
+                        longitude,
+                        hours_of_operation,
+                    ]
                 )
-                city = myutil._valid(location.get("city"))
-                state = myutil._valid(location.get("state"))
-                zip = myutil._valid(location.get("zip"))
-                country_code = myutil.get_country_by_code(state)
-                phone = myutil._valid(location.get("phone"))
-                location_type = "<MISSING>"
-                latitude = myutil._valid(location.get("lat"))
-                longitude = myutil._valid(location.get("lng"))
-                tags = bs(location["hours"], "lxml")
-                hours = []
-                for tag in tags.select("tr"):
-                    hours.append(f"{tag.td.text}: {tag.select_one('td time').text}")
-                hours_of_operation = "; ".join(hours)
-
-                _item = [
-                    locator_domain,
-                    page_url,
-                    location_name,
-                    street_address,
-                    city,
-                    state,
-                    zip,
-                    country_code,
-                    store_number,
-                    phone,
-                    location_type,
-                    latitude,
-                    longitude,
-                    hours_of_operation,
-                ]
-
-                myutil._check_duplicate_by_loc(data, _item)
+            else:
+                pass
 
     return data
 
