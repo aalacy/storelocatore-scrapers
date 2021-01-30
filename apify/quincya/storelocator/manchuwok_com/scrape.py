c2264440874ef2ab7f605544255b05f0b65db81c
Quincy <qcalexander@gmail.com>
2021-01-30 19:13:52 -0400

    manchuwok_com user-agent

--- apify/quincya/storelocator/manchuwok_com/scrape.py
+++ apify/quincya/storelocator/manchuwok_com/scrape.py
@@ -43,8 +43,8 @@ def write_output(data):
 def fetch_data():
 
     base_link = "https://manchuwok.com/locations/"
-
-    driver = SgChrome().chrome()
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36"
+    driver = SgChrome(user_agent=user_agent).chrome()
     time.sleep(2)
 
     driver.get(base_link)
