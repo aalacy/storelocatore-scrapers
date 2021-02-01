482c38e9492fba2f3e986939c79bd9a36423827b
Kiran Tanweer <44836465+Kiran-Tanweer@users.noreply.github.com>
2021-02-01 20:08:16 +0500

    Update scrape.py

--- apify/Kiran-Tanweer/storelocator/shopbedmart_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/shopbedmart_com/scrape.py
@@ -11,8 +11,8 @@ logger = SgLogSetup().get_logger("shopbedmart_com")
 session = SgRequests()
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
-    }
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+}
 
 
 def write_output(data):
@@ -40,7 +40,7 @@ def write_output(data):
             ]
         )
 
-        temp_list = []  
+        temp_list = []
         for row in data:
             comp_list = [
                 row[2].strip(),
@@ -55,27 +55,32 @@ def write_output(data):
                 temp_list.append(comp_list)
                 writer.writerow(row)
         logger.info(f"No of records being processed: {len(temp_list)}")
-        
-locations = ['https://www.shopbedmart.com/hi/locations/', 'https://www.shopbedmart.com/nw/locations/']
+
+
+locations = [
+    "https://www.shopbedmart.com/hi/locations/",
+    "https://www.shopbedmart.com/nw/locations/",
+]
+
 
 def fetch_data():
     data = []
-    pattern = re.compile(r'\s\s+')
-    cleanr = re.compile(r'<[^>]+>')
+    pattern = re.compile(r"\s\s+")
+    cleanr = re.compile(r"<[^>]+>")
     for link in locations:
         url = link
         r = session.get(url, headers=headers, verify=False)
         soup = BeautifulSoup(r.text, "html.parser")
-        divloc = soup.find("div", {"class":"row location-list"})
-        locs = divloc.findAll("div", {"class":"location-card"})
+        divloc = soup.find("div", {"class": "row location-list"})
+        locs = divloc.findAll("div", {"class": "location-card"})
         for loc in locs:
-            title = loc.find("h4", {"class":"location-card-title"}).find('a')
-            link = title['href'].strip()
+            title = loc.find("h4", {"class": "location-card-title"}).find("a")
+            link = title["href"].strip()
             title = title.text.strip()
-            info = loc.find("p", {"class":"location-card-content"}).text
-            info = re.sub(pattern, ' ', info)
-            info = re.sub(cleanr, ' ', info)
-            info = info.split('\n')
+            info = loc.find("p", {"class": "location-card-content"}).text
+            info = re.sub(pattern, " ", info)
+            info = re.sub(cleanr, " ", info)
+            info = info.split("\n")
             if len(info) == 5:
                 address = info[0]
                 phone = info[1]
@@ -83,48 +88,48 @@ def fetch_data():
             if len(info) == 4:
                 address = info[0]
                 phone = info[1]
-                hours = info[2] + ' ' + info[3]
+                hours = info[2] + " " + info[3]
             if len(info) == 7:
                 address = info[0]
                 phone = info[1]
-                hours = info[2] + ' ' + info[3] + ' ' + info[4]
+                hours = info[2] + " " + info[3] + " " + info[4]
             if len(info) == 6:
                 address = info[0]
                 phone = info[1]
                 hours = info[3]
 
-            if address == ' 1825 Haleukana St Unit C, Lihue 808-600-3934':
-                address = ' 1825 Haleukana St Unit C, Lihue'
-                phone = '808-600-3934'
+            if address == " 1825 Haleukana St Unit C, Lihue 808-600-3934":
+                address = " 1825 Haleukana St Unit C, Lihue"
+                phone = "808-600-3934"
 
-            if address == ' 380 Dairy Rd Kahului, HI 808.376.2740':
-                address = ' 380 Dairy Rd Kahului, HI'
-                phone = '808.376.2740'
-                hours = info[1] + ' ' + info[2] + ' ' + info[3]
+            if address == " 380 Dairy Rd Kahului, HI 808.376.2740":
+                address = " 380 Dairy Rd Kahului, HI"
+                phone = "808.376.2740"
+                hours = info[1] + " " + info[2] + " " + info[3]
 
-            if address == ' 1039 NW Glisan St Portland, OR 97209':
-                hours = info[2] + ' ' + info[3]
+            if address == " 1039 NW Glisan St Portland, OR 97209":
+                hours = info[2] + " " + info[3]
 
-            if address == ' 15387 bangy road lake oswego, or 503.639.9750':
-                address = ' 15387 bangy road lake oswego, or'
-                phone = '503.639.9750'
-                hours = info[1] + ' ' + info[2] + ' ' + info[3]
+            if address == " 15387 bangy road lake oswego, or 503.639.9750":
+                address = " 15387 bangy road lake oswego, or"
+                phone = "503.639.9750"
+                hours = info[1] + " " + info[2] + " " + info[3]
 
             address = address.strip()
             phone = phone.strip()
             hours = hours.strip()
 
-            title = title.rstrip(' – Pickup Only')
-            hours = hours.rstrip(' Directions')
+            title = title.rstrip(" – Pickup Only")
+            hours = hours.rstrip(" Directions")
 
             address = address.replace(",", " ")
             address = usaddress.parse(address)
-     
-            i=0
-            street = ''
-            city = ''
-            state = ''
-            pcode = ''
+
+            i = 0
+            street = ""
+            city = ""
+            state = ""
+            pcode = ""
             while i < len(address):
                 temp = address[i]
                 if (
@@ -154,32 +159,31 @@ def fetch_data():
             pcode = pcode.replace(",", "")
 
             if pcode == "":
-                pcode = '<MISSING>'
+                pcode = "<MISSING>"
 
-            if street == '1825 Haleukana St Unit C Lihue':
-                street = '1825 Haleukana St Unit C'
-                city = 'Lihue'
-                state = 'HI'
-                pcode = '<MISSING>'
+            if street == "1825 Haleukana St Unit C Lihue":
+                street = "1825 Haleukana St Unit C"
+                city = "Lihue"
+                state = "HI"
+                pcode = "<MISSING>"
 
-            if state == 'or':
-                state = 'OR'
-                
-            if state == 'hi':
-                state = 'HI'
+            if state == "or":
+                state = "OR"
+
+            if state == "hi":
+                state = "HI"
 
             p = session.get(link, headers=headers, verify=False)
             soup = BeautifulSoup(p.text, "html.parser")
 
-            scripts = soup.findAll('script')
+            scripts = soup.findAll("script")
             script = str(scripts[29])
-            coords = script.split('center: {')[1].split('},')[0]
-            coords= coords.split(',')
+            coords = script.split("center: {")[1].split("},")[0]
+            coords = coords.split(",")
             lat = coords[0].strip()
-            lat = lat.split('lat: ')[1]
+            lat = lat.split("lat: ")[1]
             lng = coords[1].strip()
-            lng = lng.split('lng: ')[1]
-
+            lng = lng.split("lng: ")[1]
 
             data.append(
                 [
@@ -210,4 +214,3 @@ def scrape():
 
 
 scrape()
-
