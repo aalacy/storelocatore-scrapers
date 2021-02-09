125b5c14758ae66dfcb81a14cd9ef31924dd2bfe
shumailaalid <55207354+shumailaalid@users.noreply.github.com>
2021-02-09 19:05:42 +0500

    Add files via upload

--- apify/shumaila/storelocator/judesbarbershop_com/scrape.py
+++ apify/shumaila/storelocator/judesbarbershop_com/scrape.py
@@ -4,11 +4,6 @@ import string
 import re, time, json
 
 from sgrequests import SgRequests
-from sglogging import SgLogSetup
-
-logger = SgLogSetup().get_logger('judesbarbershop_com')
-
-
 
 session = SgRequests()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
@@ -31,88 +26,70 @@ def fetch_data():
     cleanr = re.compile(r'<[^>]+>')
     url = 'https://www.judesbarbershop.com/location/'
     r = session.get(url, headers=headers, verify=False)    
-    soup =BeautifulSoup(r.text, "html.parser")
-   
-    divlist = soup.findAll('a', {'class': "elementor-button-link"})
-    #logger.info(divlist)
+    soup =BeautifulSoup(r.text, "html.parser")   
+    divlist = soup.find('ul',{'id':'nv-primary-navigation-main'}).findAll('ul',{'class':'sub-menu'})[1].findAll('li', {'class': "menu-item"})   
     p = 0
     for div in divlist:
+        link = div.find('a')['href']
+        print(link)
+        r = session.get(link, headers=headers, verify=False)
         try:
-            div = div['href']
-            #logger.info(div)
-            if div.find('tel') > -1:
-                break
+            loclist = r.text.split('"@context": "https://schema.org",',1)[1].split('</script>',1)[0]
+        except:
+            continue
+        loclist ='{'+loclist
+        loc = re.sub(pattern,'',loclist)
+        loc = json.loads(loc)
+        print(loc)
+        street = loc['address']["streetAddress"]
+        city = loc['address']["addressLocality"]
+        state = loc['address']["addressRegion"]
+        pcode = loc['address']["postalCode"]
+        ccode = loc['address']["addressCountry"]
+        lat = loc['geo']['latitude']
+        longt = loc['geo']['longitude']
+        phone = loc['telephone']
+        title = loc['name']
+        hourlist = loc['openingHoursSpecification']
+        hours = ''
+        for hour in hourlist:
+            start = (int)(hour['opens'].split(':')[0])
+            if start > 12:
+                start = end -12
+            end = (int)(hour['closes'].split(':')[0])
+            if end > 12:
+                end = end -12
+            hours = hours + hour['dayOfWeek'] +' ' + str(start) +":"+hour['opens'].split(':')[1] + ' AM - ' +str(end) +":"+hour['closes'].split(':')[1] + ' PM  '
             
-            r = session.get(div, headers=headers, verify=False)
-            soup =BeautifulSoup(r.text, "html.parser")
-            linklist = soup.findAll('h2',{'class':'elementor-heading-title'})
-            for link in linklist:
-                try:
-                    link = link.find('a')['href']
-                    #logger.info(link)
-                    r = session.get(link, headers=headers, verify=False)
-                except:
-                    link = div
-                loc = r.text.split('<script type="application/ld+json">',1)[1].split('</script>',1)[0]
-                #logger.info(":::",loc)
-                loc = loc.replace('\n','')
-                loc = re.sub(pattern,'',loc)                
-                loc = json.loads(loc)                
-                street = loc['address']["streetAddress"]
-                city = loc['address']["addressLocality"]
-                state = loc['address']["addressRegion"]
-                pcode = loc['address']["postalCode"]
-                ccode = loc['address']["addressCountry"]
-                lat = loc['geo']['latitude']
-                longt = loc['geo']['longitude']
-                phone = loc['telephone']
-                title = loc['name']
-                hourlist = loc['openingHoursSpecification']
-                hours = ''
-                for hour in hourlist:
-                    start = (int)(hour['opens'].split(':')[0])
-                    if start > 12:
-                        start = end -12
-                    end = (int)(hour['closes'].split(':')[0])
-                    if end > 12:
-                        end = end -12
-                    hours = hours + hour['dayOfWeek'] +' ' + str(start) +":"+hour['opens'].split(':')[1] + ' AM - ' +str(end) +":"+hour['closes'].split(':')[1] + ' PM  '
-                    
-                
-            
-                data.append([
-                            'https://www.judesbarbershop.com',
-                            link,                   
-                            title,
-                            street,
-                            city,
-                            state,
-                            pcode,
-                            'US',
-                            '<MISSING>',
-                            phone,
-                            '<MISSING>',
-                            lat,
-                            longt,
-                            hours
-                        ])
-                #logger.info(p,data[p])
-                p += 1
-                    
-            
-            
-        except Exception as e:            
-            pass
         
-           
+    
+        data.append([
+                    'https://www.judesbarbershop.com',
+                    link,                   
+                    title,
+                    street,
+                    city,
+                    state,
+                    pcode,
+                    'US',
+                    '<MISSING>',
+                    phone,
+                    '<MISSING>',
+                    lat,
+                    longt,
+                    hours
+                ])
+        print(p,data[p])
+        p += 1
+
         
     return data
 
 
 def scrape():
-   
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
     data = fetch_data()
     write_output(data)
-
+    print(time.strftime("%H:%M:%S", time.localtime(time.time())))
 
 scrape()
