8efc4971e7027210edb1c1a3f4dc482e29448627
nomihd <nouman.awais.nu@gmail.com>
2021-02-20 21:51:51 +0500

    remove unused varialbe

--- apify/himanshu/storelocator/footlocker_com/scrape.py
+++ apify/himanshu/storelocator/footlocker_com/scrape.py
@@ -104,7 +104,6 @@ def fetch_data():
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
     }
-    base_url = "https://stores.footlocker.com"
     r = session.get("https://stores.footlocker.com/", headers=headers)
     soup = BeautifulSoup(r.text, "lxml")
     for states in soup.find_all("a", {"class": "Directory-listLink"}):
