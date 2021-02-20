09bdae3d80e069f5b8eae284a9781e227e611dc2
nomihd <nouman.awais.nu@gmail.com>
2021-02-20 21:40:22 +0500

    remove unused variable

--- apify/himanshu/storelocator/footlocker_ca/scrape.py
+++ apify/himanshu/storelocator/footlocker_ca/scrape.py
@@ -106,7 +106,6 @@ def fetch_data():
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
     }
-    base_url = "https://stores.footlocker.ca/"
     r = session.get("https://stores.footlocker.ca/index.html", headers=headers)
     soup = BeautifulSoup(r.text, "lxml")
     for states in soup.find_all("a", {"class": "Directory-listLink"}):
