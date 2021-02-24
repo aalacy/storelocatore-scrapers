5a61a05fa87b92642aa258735e95781596bf4f67
aalacy <aaronlacywhite387@gmail.com>
2021-02-24 04:55:46 -0500

    [SLC-9009] fix lint error

--- apify/idev-a/storelocator/featherpetro_com/scrape.py
+++ apify/idev-a/storelocator/featherpetro_com/scrape.py
@@ -11,18 +11,26 @@ locator_domain = "https://www.featherpetro.com"
 def fetch_data():
     with SgRequests() as session:
         res = session.get("https://www.featherpetro.com/locations")
-        store_links = bs(res.text, "lxml").select("a[data-testid='gallery-item-click-action-link']")
+        store_links = bs(res.text, "lxml").select(
+            "a[data-testid='gallery-item-click-action-link']"
+        )
         for store_link in store_links:
             page_url = store_link["href"]
             res = session.get(page_url)
             store = bs(res.text, "lxml")
-            address = parse_address_intl(store.select('div[data-testid="mesh-container-content"] div[data-testid="richTextElement"]')[1].text)
+            address = parse_address_intl(
+                store.select(
+                    'div[data-testid="mesh-container-content"] div[data-testid="richTextElement"]'
+                )[1].text
+            )
             location_name = store_link.text
             zip = address.postcode
             state = address.state
             city = address.city
             street_address = address.street_address_1.split("|")[0].strip()
-            phone = store.select('div[data-testid="mesh-container-content"] div[data-testid="richTextElement"]')[2].text
+            phone = store.select(
+                'div[data-testid="mesh-container-content"] div[data-testid="richTextElement"]'
+            )[2].text
             record = SgRecord(
                 page_url=page_url,
                 location_name=location_name,
