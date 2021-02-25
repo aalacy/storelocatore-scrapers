ae714e3fec390967c310f888a80297979dc0740b
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-02-25 11:51:04 +0100

    Update scrape.py

--- apify/CalinClaudiu/storelocator/davidstea_com/scrape.py
+++ apify/CalinClaudiu/storelocator/davidstea_com/scrape.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup as b4
 
 import json
 
+
 def form_url(url):
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
@@ -15,19 +16,21 @@ def form_url(url):
 
     api_url = "https://sls-api-service.sweetiq-sls-production-west.sweetiq.com/"
 
-    scripts = soup.find_all('script')
+    scripts = soup.find_all("script")
     theScript = ""
     for i in scripts:
-        if 'REDUX_STATE' in i.text:
+        if "REDUX_STATE" in i.text:
             theScript = i.text
-    theScript = json.loads(str(theScript.split('REDUX_STATE__ = ',1)[1].rsplit(';',1)[0]))
+    theScript = json.loads(
+        str(theScript.split("REDUX_STATE__ = ", 1)[1].rsplit(";", 1)[0])
+    )
     ids = []
     for i in theScript["dataLocations"]["collection"]["features"]:
         ids.append(str(i["properties"]["id"]))
 
     api_url = api_url + theScript["dataSettings"]["storeLocatorId"]
     api_url = api_url + "/locations-details?locale=en_US&ids="
-    api_url = api_url + '%2C'.join(ids) +"&clientId="
+    api_url = api_url + "%2C".join(ids) + "&clientId="
     api_url = api_url + theScript["dataSettings"]["clientId"] + "&cname="
     api_url = api_url + theScript["dataSettings"]["cname"]
 
@@ -47,76 +50,77 @@ def fetch_data():
             yield i
     logzilla.info(f"Finished grabbing data!!")  # noqa
 
+
 def fix_comma(x):
-    x = x.replace("None","")
+    x = x.replace("None", "")
     h = []
     try:
-        for i in x.split(','):
-            if len(i.strip())>=1:
+        for i in x.split(","):
+            if len(i.strip()) >= 1:
                 h.append(i)
-        return ', '.join(h)
+        return ", ".join(h)
     except Exception:
         return x
+
 
 def human_hours(x):
     week = []
     for i in list(x):
         day = []
         for j in x[i]:
-            day.append('-'.join(j))
-        if len(day)==0:
+            day.append("-".join(j))
+        if len(day) == 0:
             day = "Closed"
         else:
-            day = ' & '.join(day)
-        week.append(i+": "+day)
-    return '; '.join(week)
+            day = " & ".join(day)
+        week.append(i + ": " + day)
+    return "; ".join(week)
+
+
 def scrape():
     url = "https://www.davidstea.com/"
     field_defs = sp.SimpleScraperPipeline.field_definitions(
         locator_domain=sp.ConstantField(url),
         page_url=sp.MappingField(
-            mapping=["properties","slug"],
-            value_transform = lambda x : url+x,
+            mapping=["properties", "slug"],
+            value_transform=lambda x: url + x,
         ),
-        location_name=sp.MappingField(
-            mapping=["properties","name"]
-        ),
+        location_name=sp.MappingField(mapping=["properties", "name"]),
         latitude=sp.MappingField(
-            mapping=["geometry","coordinates",1],
+            mapping=["geometry", "coordinates", 1],
         ),
         longitude=sp.MappingField(
-            mapping=["geometry","coordinates",0],
+            mapping=["geometry", "coordinates", 0],
         ),
         street_address=sp.MultiMappingField(
-            mapping=[["properties","addressLine1"], ["properties","addressLine2"]],
-            multi_mapping_concat_with = ", ",
-            value_transform = fix_comma,
+            mapping=[["properties", "addressLine1"], ["properties", "addressLine2"]],
+            multi_mapping_concat_with=", ",
+            value_transform=fix_comma,
         ),
         city=sp.MappingField(
-            mapping=["properties","city"],
+            mapping=["properties", "city"],
         ),
         state=sp.MappingField(
-            mapping=["properties","province"],
+            mapping=["properties", "province"],
         ),
         zipcode=sp.MappingField(
-            mapping=["properties","postalCode"],
+            mapping=["properties", "postalCode"],
         ),
         country_code=sp.MappingField(
-            mapping=["properties","country"],
+            mapping=["properties", "country"],
         ),
         phone=sp.MappingField(
-            mapping=["properties","phoneNumber"],
+            mapping=["properties", "phoneNumber"],
         ),
         store_number=sp.MappingField(
-            mapping=["properties","id"],
+            mapping=["properties", "id"],
         ),
         hours_of_operation=sp.MappingField(
-            mapping=["properties","hoursOfOperation"],
-            raw_value_transform = human_hours
+            mapping=["properties", "hoursOfOperation"], raw_value_transform=human_hours
         ),
         location_type=sp.MappingField(
-            mapping=["properties","categories"],
-            raw_value_transform = lambda x : ', '.join(x)
+            mapping=["properties", "categories"],
+            raw_value_transform=lambda x: ", ".join(x),
         ),
         raw_address=sp.MissingField(),
     )
