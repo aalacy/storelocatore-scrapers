519fad28ee640d9d38b81e06eeb026f80928b805
nomihd <nouman.awais.nu@gmail.com>
2021-02-26 17:03:45 +0500

    remove unused variable

--- apify/himanshu/storelocator/americangirl_com/scrape.py
+++ apify/himanshu/storelocator/americangirl_com/scrape.py
@@ -92,8 +92,8 @@ def fetch_data():
             store = [x.replace("—", "-") if type(x) == str else x for x in store]
             store = [x.strip() if type(x) == str else x for x in store]
             return_main_object.append(store)
-        except Exception as e:
-            raise
+        except:
+            pass
     return return_main_object
 
 
