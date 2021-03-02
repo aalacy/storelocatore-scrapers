9b1e2962f56b215033013cb4e92a66e033030a6b
datarhan <datarhan@github.com>
2021-03-02 19:08:44 +0100

    [SLC-9273] Add crawler for healthtrax.com

---
+++ apify/datarhan/storelocator/healthtrax_com/scrape.py
@@ -0,0 +1,110 @@
+import re
+import csv
+import json
+from lxml import etree
+
+from sgrequests import SgRequests
+
+
+def write_output(data):
+    with open("data.csv", mode="w", encoding="utf-8") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
+
+        # Header
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    # Your scraper here
+    session = SgRequests().requests_retry_session(retries=2, backoff_factor=0.3)
+
+    items = []
+
+    start_url = "https://www.healthtrax.com/locations"
+    domain = re.findall("://(.+?)/", start_url)[0].replace('www.', '')
+    hdr = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:86.0) Gecko/20100101 Firefox/86.0"
+    }
+    response = session.get(start_url, headers=hdr)
+    dom = etree.HTML(response.text)
+
+    all_locations = dom.xpath(
+        '//a[contains(@href, "https://www.healthtrax.com/locations")]/@href'
+    )
+    for store_url in all_locations:
+        loc_response = session.get(store_url, headers=hdr)
+        loc_dom = etree.HTML(loc_response.text)
+        geo = loc_dom.xpath("//div/@data-block-json")[1]
+        geo = json.loads(geo)
+
+        location_name = loc_dom.xpath("//h1/strong/text()")
+        location_name = location_name[0] if location_name else "<MISSING>"
+        raw_address = loc_dom.xpath('//p[strong[contains(text(), "Location:")]]/text()')
+        raw_address = [e.strip() for e in raw_address if e.strip()]
+        street_address = raw_address[0]
+        city = raw_address[1].split(", ")[0]
+        state = raw_address[1].split(", ")[-1].split()[0]
+        zip_code = raw_address[1].split(", ")[-1].split()[-1]
+        country_code = geo["location"]["addressCountry"]
+        store_number = "<MISSING>"
+        phone = loc_dom.xpath('//a[contains(@href, "tel")]/text()')
+        phone = phone[0] if phone else "<MISSING>"
+        location_type = "<MISSING>"
+        latitude = geo["location"]["mapLat"]
+        longitude = geo["location"]["mapLng"]
+        hoo = loc_dom.xpath(
+            '//p[strong[contains(text(), "Hours of Operation: ")]]/text()'
+        )
+        hours_of_operation = " ".join(hoo) if hoo else "<MISSING>"
+
+        item = [
+            domain,
+            store_url,
+            location_name,
+            street_address,
+            city,
+            state,
+            zip_code,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
+
+        items.append(item)
+
+    return items
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+if __name__ == "__main__":
+    scrape()
