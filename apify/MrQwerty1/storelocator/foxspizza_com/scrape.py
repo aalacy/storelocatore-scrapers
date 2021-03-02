7a78a17ab21741b282be73558a8da8314333877d
Ihor <rzhdgqrr@gmail.com>
2021-03-03 00:24:49 +0200

    fix

--- apify/MrQwerty1/storelocator/foxspizza_com/scrape.py
+++ apify/MrQwerty1/storelocator/foxspizza_com/scrape.py
@@ -7,12 +7,29 @@ from sgrequests import SgRequests
 
 
 def write_output(data):
-    with open('data.csv', mode='w', encoding='utf8', newline='') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
         writer.writerow(
-            ["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code",
-             "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
 
         for row in data:
             writer.writerow(row)
@@ -20,54 +37,88 @@ def write_output(data):
 
 def get_urls():
     session = SgRequests()
-    r = session.get('https://www.foxspizza.com/store-sitemap.xml')
+    r = session.get("https://www.foxspizza.com/store-sitemap.xml")
     tree = html.fromstring(r.content)
     return tree.xpath("//loc/text()")
 
 
 def get_data(page_url):
-    locator_domain = 'https://foxspizza.com/'
+    locator_domain = "https://foxspizza.com/"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
     }
 
-    tag = {'Recipient': 'recipient', 'AddressNumber': 'address1', 'AddressNumberPrefix': 'address1',
-           'AddressNumberSuffix': 'address1', 'StreetName': 'address1', 'StreetNamePreDirectional': 'address1',
-           'StreetNamePreModifier': 'address1', 'StreetNamePreType': 'address1',
-           'StreetNamePostDirectional': 'address1',
-           'StreetNamePostModifier': 'address1', 'StreetNamePostType': 'address1', 'CornerOf': 'address1',
-           'IntersectionSeparator': 'address1', 'LandmarkName': 'address1', 'USPSBoxGroupID': 'address1',
-           'USPSBoxGroupType': 'address1', 'USPSBoxID': 'address1', 'USPSBoxType': 'address1',
-           'BuildingName': 'address2', 'OccupancyType': 'address2', 'OccupancyIdentifier': 'address2',
-           'SubaddressIdentifier': 'address2', 'SubaddressType': 'address2', 'PlaceName': 'city', 'StateName': 'state',
-           'ZipCode': 'postal'}
+    tag = {
+        "Recipient": "recipient",
+        "AddressNumber": "address1",
+        "AddressNumberPrefix": "address1",
+        "AddressNumberSuffix": "address1",
+        "StreetName": "address1",
+        "StreetNamePreDirectional": "address1",
+        "StreetNamePreModifier": "address1",
+        "StreetNamePreType": "address1",
+        "StreetNamePostDirectional": "address1",
+        "StreetNamePostModifier": "address1",
+        "StreetNamePostType": "address1",
+        "CornerOf": "address1",
+        "IntersectionSeparator": "address1",
+        "LandmarkName": "address1",
+        "USPSBoxGroupID": "address1",
+        "USPSBoxGroupType": "address1",
+        "USPSBoxID": "address1",
+        "USPSBoxType": "address1",
+        "BuildingName": "address2",
+        "OccupancyType": "address2",
+        "OccupancyIdentifier": "address2",
+        "SubaddressIdentifier": "address2",
+        "SubaddressType": "address2",
+        "PlaceName": "city",
+        "StateName": "state",
+        "ZipCode": "postal",
+    }
 
     session = SgRequests()
     r = session.get(page_url, headers=headers)
     tree = html.fromstring(r.text)
 
-    location_name = ''.join(tree.xpath("//h5[@class='blog_title']//text()")).strip()
-    line = ''.join(tree.xpath("//h6[@class='loc_address']/text()")).strip()
+    location_name = "".join(tree.xpath("//h5[@class='blog_title']//text()")).strip()
+    line = "".join(tree.xpath("//h6[@class='loc_address']/text()")).strip()
     if not line:
         return
     a = usaddress.tag(line, tag_mapping=tag)[0]
     street_address = f"{a.get('address1')} {a.get('address2') or ''}".strip()
-    if street_address == 'None':
-        street_address = '<MISSING>'
-    city = a.get('city') or '<MISSING>'
-    state = a.get('state') or '<MISSING>'
-    postal = a.get('postal') or '<MISSING>'
-    country_code = 'US'
-    store_number = '<MISSING>'
-    phone = ''.join(tree.xpath("//span[@class='phone_no']/a/text()")).strip()
-    location_type = '<MISSING>'
-    latitude = '<MISSING>'
-    longitude = '<MISSING>'
-    hours_of_operation = ';'.join(tree.xpath("//span[@class='ad1']/text()")) or '<MISSING>'
+    if street_address == "None":
+        street_address = "<MISSING>"
+    city = a.get("city") or "<MISSING>"
+    state = a.get("state") or "<MISSING>"
+    postal = a.get("postal") or "<MISSING>"
+    country_code = "US"
+    store_number = "<MISSING>"
+    phone = "".join(tree.xpath("//span[@class='phone_no']/a/text()")).strip()
+    location_type = "<MISSING>"
+    latitude = "<MISSING>"
+    longitude = "<MISSING>"
+    hours_of_operation = (
+        ";".join(tree.xpath("//span[@class='ad1']/text()")) or "<MISSING>"
+    )
 
-    row = [locator_domain, page_url, location_name, street_address, city, state, postal,
-           country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation]
+    row = [
+        locator_domain,
+        page_url,
+        location_name,
+        street_address,
+        city,
+        state,
+        postal,
+        country_code,
+        store_number,
+        phone,
+        location_type,
+        latitude,
+        longitude,
+        hours_of_operation,
+    ]
     return row
 
 
