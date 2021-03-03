13257823f2d88a9bc7a9087c1ffcfb5625cd770e
datarhan <datarhan@github.com>
2021-03-03 17:00:35 +0100

    [SLC-9228] black update

--- apify/datarhan/storelocator/bulkbarn_ca/apify_to_csv.py
+++ apify/datarhan/storelocator/bulkbarn_ca/apify_to_csv.py
@@ -4,10 +4,11 @@ import sys
 import csv
 from glob import glob
 import pandas
+
 data_location = sys.argv[1]
 data = []
-for f_name in glob(os.path.join(data_location, 'datasets/default', '*.json')):
-    with open(f_name, encoding='utf-8') as json_file:
+for f_name in glob(os.path.join(data_location, "datasets/default", "*.json")):
+    with open(f_name, encoding="utf-8") as json_file:
         data.append(json.load(json_file))
 df = pandas.DataFrame.from_records(data)
-df.to_csv('data.csv', index=False)
+df.to_csv("data.csv", index=False)
