85b67c31f9a25a38de7ea98e257681e3ad1aa9af
datarhan <datarhan@github.com>
2021-03-03 17:28:15 +0100

    [SLC-9260] black update

--- apify/datarhan/storelocator/saucepizzaandwine_com/scrape.py
+++ apify/datarhan/storelocator/saucepizzaandwine_com/scrape.py
@@ -43,36 +43,40 @@ def fetch_data():
     items = []
 
     start_url = "https://www.saucepizzaandwine.com/locations/"
-    domain = re.findall("://(.+?)/", start_url)[0].replace('www.', '')
+    domain = re.findall("://(.+?)/", start_url)[0].replace("www.", "")
     response = session.get(start_url)
     dom = etree.HTML(response.text)
 
-    all_locations = dom.xpath('//a[contains(@href, "https://www.saucepizzaandwine.com/locations/")]/@href')
+    all_locations = dom.xpath(
+        '//a[contains(@href, "https://www.saucepizzaandwine.com/locations/")]/@href'
+    )
     for store_url in list(set(all_locations)):
         print(store_url)
-        if store_url == 'https://www.saucepizzaandwine.com/locations/':
+        if store_url == "https://www.saucepizzaandwine.com/locations/":
             continue
         loc_response = session.get(store_url)
         loc_dom = etree.HTML(loc_response.text)
-        poi = loc_dom.xpath('//script[@type="application/ld+json" and contains(text(), "address")]/text()')[0]
+        poi = loc_dom.xpath(
+            '//script[@type="application/ld+json" and contains(text(), "address")]/text()'
+        )[0]
         poi = json.loads(poi)
 
-        location_name = poi['name']
+        location_name = poi["name"]
         location_name = location_name if location_name else "<MISSING>"
-        street_address = poi['address']['streetAddress']
+        street_address = poi["address"]["streetAddress"]
         street_address = street_address if street_address else "<MISSING>"
-        city = poi['address']['addressLocality']
+        city = poi["address"]["addressLocality"]
         city = city if city else "<MISSING>"
-        state = poi['address']['addressRegion']
-        zip_code = poi['address']['postalCode']
+        state = poi["address"]["addressRegion"]
+        zip_code = poi["address"]["postalCode"]
         zip_code = zip_code if zip_code else "<MISSING>"
-        country_code = poi['address']['addressCountry']
+        country_code = poi["address"]["addressCountry"]
         store_number = "<MISSING>"
-        phone = poi['telephone']
-        location_type = poi['@type']
-        latitude = re.findall('lat: (.+?),', loc_response.text)[0]
-        longitude = re.findall('lng: (.+?)}', loc_response.text)[0]
-        hoo = poi['openingHours']
+        phone = poi["telephone"]
+        location_type = poi["@type"]
+        latitude = re.findall("lat: (.+?),", loc_response.text)[0]
+        longitude = re.findall("lng: (.+?)}", loc_response.text)[0]
+        hoo = poi["openingHours"]
         hours_of_operation = hoo[0] if hoo else "<MISSING>"
 
         item = [
