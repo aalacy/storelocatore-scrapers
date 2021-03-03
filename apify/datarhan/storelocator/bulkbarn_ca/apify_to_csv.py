c2d109c4381cfa569c5ea8ed4e798c83c2d8533f
datarhan <datarhan@github.com>
2021-03-03 16:58:11 +0100

    [SLC-9228] Add crawler for bulkbarn.ca

---
+++ apify/datarhan/storelocator/bulkbarn_ca/apify_to_csv.py
@@ -0,0 +1,13 @@
+import json
+import os
+import sys
+import csv
+from glob import glob
+import pandas
+data_location = sys.argv[1]
+data = []
+for f_name in glob(os.path.join(data_location, 'datasets/default', '*.json')):
+    with open(f_name, encoding='utf-8') as json_file:
+        data.append(json.load(json_file))
+df = pandas.DataFrame.from_records(data)
+df.to_csv('data.csv', index=False)
