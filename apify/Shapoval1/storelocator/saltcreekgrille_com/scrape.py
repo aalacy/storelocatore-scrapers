86aa0a14462dd851ee60a037bc545ae6a92cae37
Shapoval1 <shshipovald@mail.ru>
2021-03-04 10:05:24 +0200

    add saltcreekgrille_com

--- apify/Shapoval1/storelocator/saltcreekgrille_com/scrape.py
+++ apify/Shapoval1/storelocator/saltcreekgrille_com/scrape.py
@@ -46,34 +46,7 @@ def get_data(url):
     locator_domain = "https://saltcreekgrille.com/"
     page_url = url
     session = SgRequests()
-    tag = {
-        "Recipient": "recipient",
-        "AddressNumber": "address1",
-        "AddressNumberPrefix": "address1",
-        "AddressNumberSuffix": "address1",
-        "StreetName": "address1",
-        "StreetNamePreDirectional": "address1",
-        "StreetNamePreModifier": "address1",
-        "StreetNamePreType": "address1",
-        "StreetNamePostDirectional": "address1",
-        "StreetNamePostModifier": "address1",
-        "StreetNamePostType": "address1",
-        "CornerOf": "address1",
-        "IntersectionSeparator": "address1",
-        "LandmarkName": "address1",
-        "USPSBoxGroupID": "address1",
-        "USPSBoxGroupType": "address1",
-        "USPSBoxID": "address1",
-        "USPSBoxType": "address1",
-        "BuildingName": "address2",
-        "OccupancyType": "address2",
-        "OccupancyIdentifier": "address2",
-        "SubaddressIdentifier": "address2",
-        "SubaddressType": "address2",
-        "PlaceName": "city",
-        "StateName": "state",
-        "ZipCode": "postal",
-    }
+
     r = session.get(page_url)
     tree = html.fromstring(r.text)
 
