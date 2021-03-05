710cfadf7747a5b769feeecd7d506f00ee399d39
datarhan <slutchenko@gmail.com>
2021-03-05 17:19:55 +0100

    [SLC-9519] black update

--- apify/datarhan/storelocator/factorymattresstexas_com/scrape.py
+++ apify/datarhan/storelocator/factorymattresstexas_com/scrape.py
@@ -42,42 +42,44 @@ def fetch_data():
     items = []
 
     start_url = "https://factorymattresstexas.com/locations/"
-    domain = re.findall("://(.+?)/", start_url)[0].replace('www.', '')
+    domain = re.findall("://(.+?)/", start_url)[0].replace("www.", "")
     with SgFirefox() as driver:
         driver.get(start_url)
         dom = etree.HTML(driver.page_source)
     data = dom.xpath('//script[contains(text(), "maplistScriptParamsKo")]//text()')[0]
-    data = re.findall('maplistScriptParamsKo =(.+);', data)[0]
+    data = re.findall("maplistScriptParamsKo =(.+);", data)[0]
     data = json.loads(data)
 
-    for poi in data['KOObject'][0]['locations']:
-        store_url = poi['locationUrl']
+    for poi in data["KOObject"][0]["locations"]:
+        store_url = poi["locationUrl"]
         with SgFirefox() as driver:
             driver.get(store_url)
             loc_dom = etree.HTML(driver.page_source)
-        location_name = poi['title']
+        location_name = poi["title"]
         location_name = location_name if location_name else "<MISSING>"
-        raw_address = etree.HTML(poi['address'])
-        raw_address = raw_address.xpath('//text()')
+        raw_address = etree.HTML(poi["address"])
+        raw_address = raw_address.xpath("//text()")
         raw_address = [e.strip() for e in raw_address if e.strip()]
-        addr = parse_address_intl(' '.join(raw_address))
+        addr = parse_address_intl(" ".join(raw_address))
         street_address = addr.street_address_1
         if addr.street_address_2:
-            street_address += ' ' + addr.street_address_2
+            street_address += " " + addr.street_address_2
         city = addr.city
-        city = city if city else '<MISSING>'
+        city = city if city else "<MISSING>"
         state = addr.state
-        state = state if state else '<MISSING>'
+        state = state if state else "<MISSING>"
         zip_code = addr.postcode
-        zip_code = zip_code if zip_code else '<MISSING>'
+        zip_code = zip_code if zip_code else "<MISSING>"
         country_code = addr.country
-        country_code = country_code if country_code else '<MISSING>' 
-        store_number = '<MISSING>'
-        phone = etree.HTML(poi['description']).xpath('//div[@class="custom_phone"]/a/text()')
-        phone = phone[0] if phone else '<MISSING>'
-        location_type = '<MISSING>'
-        latitude = poi['latitude']
-        longitude = poi['longitude']
+        country_code = country_code if country_code else "<MISSING>"
+        store_number = "<MISSING>"
+        phone = etree.HTML(poi["description"]).xpath(
+            '//div[@class="custom_phone"]/a/text()'
+        )
+        phone = phone[0] if phone else "<MISSING>"
+        location_type = "<MISSING>"
+        latitude = poi["latitude"]
+        longitude = poi["longitude"]
         hoo = loc_dom.xpath('//span[@itemprop="hours"]/text()')
         hoo = [e.strip() for e in hoo if e.strip()]
         hours_of_operation = " ".join(hoo) if hoo else "<MISSING>"
