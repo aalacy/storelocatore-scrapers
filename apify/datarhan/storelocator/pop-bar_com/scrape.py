ecb169dae09c93a2d52768b30e6c581ba42a922f
datarhan <slutchenko@gmail.com>
2021-03-06 18:27:47 +0100

    [SLC-9521] country_code update

--- apify/datarhan/storelocator/pop-bar_com/scrape.py
+++ apify/datarhan/storelocator/pop-bar_com/scrape.py
@@ -54,63 +54,68 @@ def fetch_data():
     for start_url in start_urls:
         response = session.get(start_url, headers=hdr)
         dom = etree.HTML(response.text)
-        all_locations += dom.xpath('//div[@class="one-third-column"]/a/@href')
+        all_locations = dom.xpath('//div[@class="one-third-column"]/a/@href')
         all_locations += dom.xpath('//div[@class="one-column"]/a/@href')
 
-    for url in list(set(all_locations)):
-        store_url = urljoin(start_url, url)
-        if store_url == "https://www.pop-bar.com/pages/canada":
-            continue
-        loc_response = session.get(store_url)
-        loc_dom = etree.HTML(loc_response.text)
+        for url in list(set(all_locations)):
+            if not url:
+                continue
+            store_url = urljoin(start_url, url)
+            loc_response = session.get(store_url)
+            loc_dom = etree.HTML(loc_response.text)
 
-        location_name = loc_dom.xpath('//h1[@class="Heading u-h1"]/text()')
-        location_name = location_name[0] if location_name else "<MISSING>"
-        raw_address = loc_dom.xpath(
-            '//div[p[contains(text(), "Address")]]/following-sibling::div[1]/text()'
-        )
-        raw_address = [e.strip() for e in raw_address if e.strip()]
-        addr = parse_address_intl(" ".join(raw_address))
-        street_address = addr.street_address_1
-        if addr.street_address_2:
-            street_address += " " + addr.street_address_2
-        street_address = street_address if street_address else "<MISSING>"
-        street_address = street_address.split("(")[0].strip().replace("*", "")
-        city = addr.city
-        city = city if city else "<MISSING>"
-        state = addr.state
-        state = state if state else "<MISSING>"
-        zip_code = addr.postcode
-        zip_code = zip_code if zip_code else "<MISSING>"
-        country_code = addr.country
-        country_code = country_code if country_code else "<MISSING>"
-        store_number = "<MISSING>"
-        phone = "<MISSING>"
-        location_type = "<MISSING>"
-        latitude = "<MISSING>"
-        longitude = "<MISSING>"
-        hoo = loc_dom.xpath('//div[@class="store-hours"]/div[@class="paragraf"]/text()')
-        hoo = [e.strip() for e in hoo if e.strip() if "am -" in e.lower()]
-        hours_of_operation = " ".join(hoo) if hoo else "<MISSING>"
+            location_name = loc_dom.xpath('//h1[@class="Heading u-h1"]/text()')
+            location_name = location_name[0] if location_name else "<MISSING>"
+            raw_address = loc_dom.xpath(
+                '//div[p[contains(text(), "Address")]]/following-sibling::div[1]/text()'
+            )
+            raw_address = [e.strip() for e in raw_address if e.strip()]
+            addr = parse_address_intl(" ".join(raw_address))
+            street_address = addr.street_address_1
+            if addr.street_address_2:
+                street_address += " " + addr.street_address_2
+            street_address = street_address if street_address else "<MISSING>"
+            street_address = street_address.split("(")[0].strip().replace("*", "")
+            city = addr.city
+            city = city if city else "<MISSING>"
+            state = addr.state
+            state = state if state else "<MISSING>"
+            zip_code = addr.postcode
+            zip_code = zip_code if zip_code else "<MISSING>"
+            if "united-states" in start_url:
+                country_code = "US"
+            else:
+                country_code = "CA"
+            country_code = country_code if country_code else "<MISSING>"
+            store_number = "<MISSING>"
+            phone = "<MISSING>"
+            location_type = "<MISSING>"
+            latitude = "<MISSING>"
+            longitude = "<MISSING>"
+            hoo = loc_dom.xpath(
+                '//div[@class="store-hours"]/div[@class="paragraf"]/text()'
+            )
+            hoo = [e.strip() for e in hoo if e.strip() if "am -" in e.lower()]
+            hours_of_operation = " ".join(hoo) if hoo else "<MISSING>"
 
-        item = [
-            domain,
-            store_url,
-            location_name,
-            street_address,
-            city,
-            state,
-            zip_code,
-            country_code,
-            store_number,
-            phone,
-            location_type,
-            latitude,
-            longitude,
-            hours_of_operation,
-        ]
+            item = [
+                domain,
+                store_url,
+                location_name,
+                street_address,
+                city,
+                state,
+                zip_code,
+                country_code,
+                store_number,
+                phone,
+                location_type,
+                latitude,
+                longitude,
+                hours_of_operation,
+            ]
 
-        items.append(item)
+            items.append(item)
 
     return items
 
