83b51fc2523f7f41f5f57afa60ff23362124404d
Shapoval1 <shshipovald@mail.ru>
2021-03-09 00:53:42 +0200

    add qwenchjuice_com

---
+++ apify/Shapoval1/storelocator/qwenchjuice_com/scrape.py
@@ -0,0 +1,133 @@
+import csv
+import usaddress
+from lxml import html
+from sgrequests import SgRequests
+
+
+def write_output(data):
+    with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
+
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
+
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    out = []
+    locator_domain = "https://qwenchjuice.com/"
+    page_url = "https://qwenchjuice.com/locations"
+    session = SgRequests()
+    tag = {
+        "Recipient": "recipient",
+        "AddressNumber": "address1",
+        "AddressNumberPrefix": "address1",
+        "AddressNumberSuffix": "address1",
+        "StreetName": "address1",
+        "StreetNamePreDirectional": "address1",
+        "StreetNamePreModifier": "address1",
+        "StreetNamePreType": "address1",
+        "StreetNamePostDirectional": "address1",
+        "StreetNamePostModifier": "address1",
+        "StreetNamePostType": "address1",
+        "CornerOf": "address1",
+        "IntersectionSeparator": "address1",
+        "LandmarkName": "address1",
+        "USPSBoxGroupID": "address1",
+        "USPSBoxGroupType": "address1",
+        "USPSBoxID": "address1",
+        "USPSBoxType": "address1",
+        "BuildingName": "address2",
+        "OccupancyType": "address2",
+        "OccupancyIdentifier": "address2",
+        "SubaddressIdentifier": "address2",
+        "SubaddressType": "address2",
+        "PlaceName": "city",
+        "StateName": "state",
+        "ZipCode": "postal",
+    }
+    r = session.get(page_url)
+    tree = html.fromstring(r.text)
+    block = tree.xpath('//div[contains(@class,"grid4-12")]')
+    for i in block:
+        location_name = "".join(
+            i.xpath('.//div[@class="row location_heading"]/h4/text()')
+        )
+        line = " ".join(
+            i.xpath(
+                './/address/text() | .//div[@class="row location_address"]/p/text()'
+            )
+        )
+        if line.find("1915") != -1:
+            line = line.replace("L6H 0N1", "")
+
+        a = usaddress.tag(line, tag_mapping=tag)[0]
+
+        street_address = f"{a.get('address1')} {a.get('address2')}".replace(
+            "None", ""
+        ).strip()
+        phone = "".join(i.xpath('.//a[@class="contact_call"]/text()')) or "<MISSING>"
+        city = a.get("city")
+        state = a.get("state") or "<MISSING>"
+        if state.find("Canada") != -1:
+            state = state.split()[0].strip()
+        country_code = "US"
+        store_number = "<MISSING>"
+        latitude = "<MISSING>"
+        longitude = "<MISSING>"
+        location_type = "<MISSING>"
+        hours_of_operation = "<MISSING>"
+        postal = a.get("postal")
+        if street_address.find("1915") != -1:
+            postal = "L6H 0N1"
+        if phone == "<MISSING>" and street_address.find("2210") == -1:
+            hours_of_operation = "Coming Soon"
+
+        row = [
+            locator_domain,
+            page_url,
+            location_name,
+            street_address,
+            city,
+            state,
+            postal,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
+        out.append(row)
+
+    return out
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+if __name__ == "__main__":
+    scrape()
