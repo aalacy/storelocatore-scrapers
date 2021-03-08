14395aa2272d03bc55e1812844e65c12e062d9ba
nomihd <nouman.awais.nu@gmail.com>
2021-03-09 01:52:48 +0500

    fix broken script

--- apify/himanshu/storelocator/brothersbar_com/scrape.py
+++ apify/himanshu/storelocator/brothersbar_com/scrape.py
@@ -1,24 +1,38 @@
 import csv
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
-import re
-import json
 from sglogging import SgLogSetup
+import lxml.html
 
-logger = SgLogSetup().get_logger('brothersbar_com')
-
-
-
-
+logger = SgLogSetup().get_logger("brothersbar_com")
 
 session = SgRequests()
 
+
 def write_output(data):
-    with open('data.csv', mode='w', encoding="utf-8") as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", newline="", mode="w", encoding="utf-8") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+                "page_url",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
@@ -26,20 +40,12 @@ def write_output(data):
 
 def fetch_data():
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
     }
-
-    # logger.info("soup ===  first")
 
     base_url = "https://www.brothersbar.com"
     r = session.get("https://www.brothersbar.com/", headers=headers)
     soup = BeautifulSoup(r.text, "lxml")
-    return_main_object = []
-    #   data = json.loads(soup.find("div",{"paging_container":re.compile('latlong.push')["paging_container"]}))
-    # for link in soup.find_all('ul',re.compile('content')):
-    #     logger.info(link)
-
-    # it will used in store data.
     locator_domain = base_url
     location_name = ""
     street_address = "<MISSING>"
@@ -55,47 +61,59 @@ def fetch_data():
     raw_address = ""
     hours_of_operation = "<MISSING>"
 
-    json_uri_list = soup.text.split('"pageList":')[1].split('}]')[0] + '}]}'
-    json_uri_list = json.loads(json_uri_list)
+    stores_sel = lxml.html.fromstring(r.text)
+    stores = stores_sel.xpath('//li[@id="comp-ikmnfi1c1"]/ul/li/a/@href')
+    for location_url in stores:
+        logger.info(location_url)
+        r_location = session.get(location_url, headers=headers)
+        soup_location = BeautifulSoup(r_location.text, "lxml")
 
-    for link_data in json_uri_list['pages']:
-        if '-' not in link_data['pageUriSEO']:
-            location_url = base_url + '/' + link_data['pageUriSEO']
-            # logger.info('location_url === ' + str(location_url))
+        tag_hours = soup_location.find(
+            lambda tag: tag.name == "span" and "hours:" in tag.text.lower()
+        )
+        if tag_hours is None:
+            continue
+        tag_hours = tag_hours.parent.parent
+        hours_of_operation = (
+            ",".join(list(tag_hours.stripped_strings))
+            .replace(",CLOSING HOURS SUBJECT TO CHANGE", "")
+            .replace("HOURS:,", "")
+            .replace(",:,", ":")
+            .strip()
+        )
 
-            r_location = session.get(location_url, headers=headers)
-            soup_location = BeautifulSoup(r_location.text, "lxml")
+        tag_address = soup_location.find(
+            lambda tag: tag.name == "span"
+            and "brothers bar & grill" in tag.text.lower()
+        )
+        tag_address = tag_address.parent.parent
+        list_address = list(tag_address.stripped_strings)
 
-            tag_hours = soup_location.find(lambda tag: tag.name == "span" and "hours:" in tag.text.lower())
-            if tag_hours is None:
-                continue
-            tag_hours = tag_hours.parent.parent
-            hours_of_operation = ','.join(list(tag_hours.stripped_strings))
+        street_address = list_address[1]
+        city = list_address[2].split(",")[0]
+        state = list_address[2].split(",")[1].strip().split(" ")[0]
+        zipp = list_address[2].split(",")[1].strip().split(" ")[-1]
+        phone = list_address[-1]
 
-            tag_address = soup_location.find(
-                lambda tag: tag.name == "span" and "brothers bar & grill" in tag.text.lower())
-            tag_address = tag_address.parent.parent
-            list_address = list(tag_address.stripped_strings)
+        location_name = city
 
-            street_address = list_address[1]
-            city = list_address[2].split(',')[0]
-            state = list_address[2].split(',')[1].strip().split(' ')[0]
-            zipp = list_address[2].split(',')[1].strip().split(' ')[-1]
-            phone = list_address[-1]
-
-            location_name = city
-
-            # logger.info('script_hours === ' + str(list_address))
-
-            store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
-                     store_number, phone, location_type, latitude, longitude, hours_of_operation]
-
-            # logger.info("data = " + str(store))
-            # logger.info('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
-            return_main_object.append(store)
-
-    return return_main_object
+        store = [
+            locator_domain,
+            location_name,
+            street_address,
+            city,
+            state,
+            zipp,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+            location_url,
+        ]
+        yield store
 
 
 def scrape():
