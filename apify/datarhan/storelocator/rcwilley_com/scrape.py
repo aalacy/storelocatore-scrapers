92167a9e123016606799e9d9cc06052df2262fe8
datarhan <slutchenko@gmail.com>
2021-03-08 15:20:02 +0100

    [SLC-9591] black update

--- apify/datarhan/storelocator/rcwilley_com/scrape.py
+++ apify/datarhan/storelocator/rcwilley_com/scrape.py
@@ -44,42 +44,39 @@ def fetch_data():
     items = []
 
     start_url = "https://rcwilley.com/Google-Map-Action"
-    domain = re.findall("://(.+?)/", start_url)[0].replace('www.', '')
+    domain = re.findall("://(.+?)/", start_url)[0].replace("www.", "")
     hdr = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,pt;q=0.6',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,pt;q=0.6",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     }
-    formdata = {
-        'query': '',
-        'json': 'true'
-    }
+    formdata = {"query": "", "json": "true"}
     response = session.post(start_url, headers=hdr, data=formdata)
     data = json.loads(response.text)
 
-    for poi in data['result']:
-        store_url = urljoin(start_url, poi['url'])
-        location_name = poi['pageName']
+    for poi in data["result"]:
+        store_url = urljoin(start_url, poi["url"])
+        location_name = poi["pageName"]
         location_name = location_name if location_name else "<MISSING>"
-        street_address = poi['street']
+        street_address = poi["street"]
         street_address = street_address if street_address else "<MISSING>"
-        city = poi['city']
+        city = poi["city"]
         city = city if city else "<MISSING>"
-        state = poi['state']
-        state = state if state else '<MISSING>'
-        zip_code = poi['zip']
+        state = poi["state"]
+        state = state if state else "<MISSING>"
+        zip_code = poi["zip"]
         zip_code = zip_code if zip_code else "<MISSING>"
-        country_code = '<MISSING>' 
-        store_number = poi['id']
-        phone = poi['phone']
-        phone = phone if phone else '<MISSING>'
-        location_type = '<MISSING>'
-        latitude = poi['latitude']
-        longitude = poi['longitude']
-        hours_of_operation = poi['hours']
+        country_code = "<MISSING>"
+        store_number = poi["id"]
+        phone = poi["phone"]
+        phone = phone if phone else "<MISSING>"
+        location_type = "<MISSING>"
+        latitude = poi["latitude"]
+        longitude = poi["longitude"]
+        hours_of_operation = poi["hours"]
 
         item = [
             domain,
