81119560867f7e79918c397ec8fd526696c87b75
datarhan <slutchenko@gmail.com>
2021-03-09 15:21:48 +0100

    [SLC-9470] qa updates

--- apify/datarhan/storelocator/tommyjohn_com/scrape.py
+++ apify/datarhan/storelocator/tommyjohn_com/scrape.py
@@ -41,7 +41,7 @@ def fetch_data():
 
     items = []
 
-    start_url = "https://tommyjohn.com/pages/store-locator"
+    start_url = "https://tommyjohn.com/pages/store-directory"
     domain = re.findall("://(.+?)/", start_url)[0].replace("www.", "")
     hdr = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
@@ -49,28 +49,43 @@ def fetch_data():
     response = session.get(start_url, headers=hdr)
     dom = etree.HTML(response.text)
 
-    all_locations = dom.xpath('//div[@class="stores__container"]/div')
+    all_locations = dom.xpath('//div[contains(@class, "stores-section city")]')
     for poi_html in all_locations:
         store_url = start_url
-        location_name = poi_html.xpath(
-            './/h1[@class="stores__individual-title"]/text()'
-        )
+        location_name = poi_html.xpath('.//h2[@class="stores-section__heading"]/text()')
         location_name = location_name[0].strip() if location_name else "<MISSING>"
         raw_address = poi_html.xpath(
-            './/h6[@class="stores__individual-content"]/text()'
+            './/div[@class="stores-section__text-container-sub"]/p[1]/text()'
         )
-        raw_address = [e.strip() for e in raw_address if e.strip()]
+        raw_address = [
+            " ".join([i for i in e.split() if i.strip()])
+            for e in raw_address
+            if e.strip()
+        ]
+        location_type = "<MISSING>"
+        if "#ComfortSoon!" in raw_address[0]:
+            location_type = "coming soon"
+            raw_address = raw_address[1:]
         street_address = raw_address[0]
-        city = raw_address[-1].split(",")[0].strip()
-        state = raw_address[-1].split(",")[-1].split()[0]
-        zip_code = raw_address[-1].split(",")[-1].split()[-1]
+        city = poi_html.xpath(
+            './/p[@class="stores-section__content-section stores-section__content-city"]/text()'
+        )
+        city = city[0] if city else "<MISSING>"
+        state = raw_address[1].split(",")[1].strip()
+        zip_code = raw_address[1].split(",")[-1].strip()
         country_code = "United States"
         store_number = "<MISSING>"
-        phone = "<MISSING>"
-        location_type = "<MISSING>"
+        phone = poi_html.xpath(
+            './/p[@class="stores-section__content-section stores-section__content-phone"]/a/text()'
+        )
+        phone = phone[0] if phone else "<MISSING>"
         latitude = "<MISSING>"
         longitude = "<MISSING>"
-        hours_of_operation = "<MISSING>"
+        hoo = poi_html.xpath(
+            './/p[@class="stores-section__content-section stores-section__content-hours"]//text()'
+        )
+        hoo = [e.strip() for e in hoo if e.strip()]
+        hours_of_operation = " ".join(hoo) if hoo else "<MISSING>"
 
         item = [
             domain,
