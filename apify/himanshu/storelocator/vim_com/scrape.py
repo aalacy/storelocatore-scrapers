d4e4ed98ac4e9fe4cfb1959381588c8bc6d5a477
nomihd <nouman.awais.nu@gmail.com>
2021-03-09 16:01:14 +0500

    fix broken script

--- apify/himanshu/storelocator/vim_com/scrape.py
+++ apify/himanshu/storelocator/vim_com/scrape.py
@@ -1,24 +1,39 @@
 import csv
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
-import re
-import json
 from sglogging import SgLogSetup
+import lxml.html
+import json
 
-logger = SgLogSetup().get_logger('vim_com')
-
-
-
-
+logger = SgLogSetup().get_logger("vim_com")
 
 session = SgRequests()
 
+
 def write_output(data):
-    with open('data.csv', mode='w', encoding="utf-8") as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", newline="", mode="w", encoding="utf-8") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+                "page_url",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
@@ -26,20 +41,11 @@ def write_output(data):
 
 def fetch_data():
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
     }
-
-    logger.info("soup ===  first")
 
     base_url = "https://www.vim.com"
     r = session.get("https://www.vim.com/apps/store-locator", headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    return_main_object = []
-    #   data = json.loads(soup.find("div",{"paging_container":re.compile('latlong.push')["paging_container"]}))
-    # for link in soup.find_all('ul',re.compile('content')):
-    #     logger.info(link)
-
-    # it will used in store data.
     locator_domain = base_url
     location_name = ""
     street_address = "<MISSING>"
@@ -52,35 +58,41 @@ def fetch_data():
     location_type = "vim"
     latitude = "<MISSING>"
     longitude = "<MISSING>"
-    raw_address = ""
     hours_of_operation = "<MISSING>"
-
-    list_locations = soup.text.split("markersCoords.push({")
-
+    page_url = "https://www.vim.com/apps/store-locator"
+    list_locations = r.text.split("markersCoords.push({")
     for script in list_locations[1:-2]:
         soup_location = script.split("});")[0]
 
         list_location = soup_location.split(",")
-        # logger.info(str(len(list_location)) + " == list_locations ====== " + str(list_location))
-
         latitude = list_location[0].replace("lat:", "").strip()
         longitude = list_location[1].replace("lng:", "").strip()
         store_number = list_location[2].replace("id:", "").strip()
 
-        address_str = list_location[5].replace("address:'", "").strip().replace("&#039;", "\"").replace("&lt;",
-                                                                                                        "<").replace(
-            "&gt;", ">")
+        address_str = (
+            list_location[5]
+            .replace("address:'", "")
+            .strip()
+            .replace("&#039;", '"')
+            .replace("&lt;", "<")
+            .replace("&gt;", ">")
+        )
         address_soup = BeautifulSoup(address_str, "lxml")
         address_list = list(address_soup.stripped_strings)
         location_name = address_list[0]
         street_address = address_list[1]
         city = address_list[2]
 
-        sz_str = list_location[6].replace("address:'", "").strip().replace("&#039;", "\"").replace("&lt;", "<").replace(
-            "&gt;", ">")
+        sz_str = (
+            list_location[6]
+            .replace("address:'", "")
+            .strip()
+            .replace("&#039;", '"')
+            .replace("&lt;", "<")
+            .replace("&gt;", ">")
+        )
         sz_str_soup = BeautifulSoup(sz_str, "lxml")
         sz_str_list = list(sz_str_soup.stripped_strings)
-        # logger.info("address == " + str(sz_str_list))
         if len(sz_str_list) > 1:
             state = sz_str_list[0]
             zipp = sz_str_list[1]
@@ -90,24 +102,49 @@ def fetch_data():
 
         country_code = "US"
 
-        r_phone_hour = session.get("https://stores.boldapps.net/front-end/get_store_info.php?shop=vimstores.myshopify.com&data=detailed&store_id="+store_number, headers=headers)
-        soup_phone_hour = BeautifulSoup(r_phone_hour.text, "lxml")
+        logger.info(f"pulling info for store ID: {store_number}")
+        r_phone_hour = session.get(
+            "https://stores.boldapps.net/front-end/get_store_info.php?shop=vimstores.myshopify.com&data=detailed&store_id="
+            + store_number,
+            headers=headers,
+        )
+        store_sel = lxml.html.fromstring(json.loads(r_phone_hour.text)["data"])
 
-        phone = soup_phone_hour.find('span',{"class":"phone"}).text.split("Fax:")[0]
-        hours_of_operation = soup_phone_hour.find('span',{"class":"phone"}).text.split("M-F:")[1].replace("\"}","")
-        # logger.info("soup_location ===== "+str(soup_phone_hour))
-        # logger.info("phone ===== "+phone)
-        # logger.info("hours_of_operation ===== "+hours_of_operation)
+        phone = "".join(store_sel.xpath('//span[@class="phone"]/text()')).strip()
+        hours_of_operation = ""
+        hours = store_sel.xpath('//span[@class="hours"]/text()')
+        hours_list = []
+        for hour in hours:
+            if len("".join(hour).strip()) > 0:
+                day = "".join(hour).strip().split(" ", 1)[0].strip()
+                time = "".join(hour).strip().split(" ", 1)[1].strip()
+                hours_list.append(day + ":" + time)
 
-        store = [locator_domain, location_name, street_address, city, state, zipp, country_code,
-                 store_number, phone, location_type, latitude, longitude, hours_of_operation]
-
-        # logger.info("data = " + str(store))
-        # logger.info('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
-        return_main_object.append(store)
-
-    return return_main_object
+        hours_of_operation = (
+            "; ".join(hours_list)
+            .strip()
+            .encode("ascii", "replace")
+            .decode("utf-8")
+            .replace("?", "-")
+            .strip()
+        )
+        store = [
+            locator_domain,
+            location_name,
+            street_address,
+            city,
+            state,
+            zipp,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+            page_url,
+        ]
+        yield store
 
 
 def scrape():
@@ -116,8 +153,3 @@ def scrape():
 
 
 scrape()
- 
-
-
-
-
