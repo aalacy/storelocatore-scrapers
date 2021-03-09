a13ccc33b80b1ccaa8d53316092802dcbef5fe31
datarhan <slutchenko@gmail.com>
2021-03-09 17:34:31 +0100

    [SLC8982] maintenance

--- apify/himanshu/storelocator/onewestbank_com/scrape.py
+++ apify/himanshu/storelocator/onewestbank_com/scrape.py
@@ -1,31 +1,99 @@
+import re
+import csv
+from lxml import etree
 
-from bs4 import BeautifulSoup
-import pandas as pd
-from sglogging import SgLogSetup
-
-logger = SgLogSetup().get_logger('onewestbank_com')
-
+from sgrequests import SgRequests
 
 
+def write_output(data):
+    with open("data.csv", mode="w", encoding="utf-8") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
-URL ='https://www.onewestbank.com/handlers/StoreLocationsHandler.ashx?BranchLocationsId=23622321295'
-df = pd.read_csv(URL,delimiter=",")
-df['locator_domain']="https://www.onewestbank.com"
-df['country_code']="US"
-df['page_url']="<MISSING>"
-df['HoursBiz']=df['HoursBiz'].str.split("Please note",n = 1,expand = True).replace("<p>","")
-rows = []
-for t in df['HoursBiz']:
-    soup = BeautifulSoup(t,"lxml")
-    rows.append(soup.get_text().replace("Sunset Park branch is closed",'').replace(";","").strip().lstrip())
-df['HoursBiz'] = rows
-df['HoursBiz']=df['HoursBiz'].replace("ATM Only","<MISSING>").str.encode('ascii', 'ignore').str.decode('ascii').replace("<p>",'').replace("\n"," ").replace(";","").replace("<p>Sunset Park branch is closed","")
-df['Phone']=df['Phone'].replace("ATM Only","<MISSING>")
-df['ATM']=df['ATM'].replace("yes","ATM & Branch").replace("no","Branch")
-df['BranchID']=df['BranchID'].str.replace("T","")
-df=df.drop(['Safekeeping','DriveThru','Fax'], axis = 1) 
-df = df.rename(columns={'BranchName': 'location_name', 'Street': 'street_address',"City":"city","State":"state","Zip":"zip","Xcoord":"latitude","Ycoord":"longitude","HoursBiz":"hours_of_operation","ATM":"location_type","Phone":"phone","BranchID":"store_number"})
-df['street_address']=df['street_address'].str.replace("&amp;",'')
-df = df[["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"]]
-df.to_csv('data.csv',index=None)
-# logger.info(df)
+        # Header
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    # Your scraper here
+    session = SgRequests().requests_retry_session(retries=2, backoff_factor=0.3)
+
+    items = []
+
+    start_url = "https://www.onewestbank.com/assets/csv/branchlocations.csv"
+    domain = re.findall("://(.+?)/", start_url)[0].replace("www.", "")
+    hdr = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
+    }
+    response = session.get(start_url, headers=hdr)
+    with open("out.csv", "w") as f:
+        writer = csv.writer(f)
+        for line in response.iter_lines():
+            writer.writerow(line.decode("utf-8").split(","))
+
+    all_locations = csv.DictReader(open("out.csv"))
+    for poi in all_locations:
+        store_url = "https://www.onewestbank.com/branch-locator"
+        location_name = poi['"BranchName"'][1:-1]
+        street_address = poi['"Street"'][1:-1]
+        city = poi['"City"'][1:-1]
+        state = poi['"State"'][1:-1]
+        zip_code = poi['"Zipcode"'][1:-1]
+        country_code = "<MISSING>"
+        store_number = poi['"BranchID"'][1:-1]
+        phone = poi['"Phone"'][1:-1]
+        location_type = "<MISSING>"
+        latitude = poi['"Xcoord"'][1:-1]
+        longitude = poi['"Ycoord"'][1:-1]
+        hours_of_operation = poi['"HoursBiz"'][1:-1]
+
+        item = [
+            domain,
+            store_url,
+            location_name,
+            street_address,
+            city,
+            state,
+            zip_code,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
+
+        items.append(item)
+
+    return items
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+if __name__ == "__main__":
+    scrape()
