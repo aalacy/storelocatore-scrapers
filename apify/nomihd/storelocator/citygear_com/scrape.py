26336851304b1ecbdac3d9f8194267bd9812cbf6
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-03-10 23:30:31 +0100

    captcha

--- apify/nomihd/storelocator/citygear_com/scrape.py
+++ apify/nomihd/storelocator/citygear_com/scrape.py
@@ -2,23 +2,12 @@
 import csv
 from sgrequests import SgRequests
 from sglogging import sglog
+from sgselenium import SgChrome
 import json
-
+import time
 website = "citygear.com"
 log = sglog.SgLogSetup().get_logger(logger_name=website)
-session = SgRequests()
-headers = {
-    "authority": "www.hibbett.com",
-    "cache-control": "max-age=0",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "sec-fetch-site": "none",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-user": "?1",
-    "sec-fetch-dest": "document",
-    "accept-language": "en-US,en-GB;q=0.9,en;q=0.8",
-}
+
 
 
 def write_output(data):
@@ -64,7 +53,46 @@ def write_output(data):
 
         log.info(f"No of records being processed: {len(temp_list)}")
 
+def to_json(dData):
+    log.info(f"Length of data: {len(text)}")
+    dData = dData.split('{',1)[1].rsplit('}',1)[0]
+    return json.loads(str('{'+dData+'}'))
+    
+def get_data(url,attempts,maxRetries):
+    session = SgRequests()
+    headers = {
+    "authority": "www.hibbett.com",
+    "cache-control": "max-age=0",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "sec-fetch-site": "none",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-user": "?1",
+    "sec-fetch-dest": "document",
+    "accept-language": "en-US,en-GB;q=0.9,en;q=0.8",
+}
+    
+    #dData = session.get(url,headers=headers).text
+    
+    with SgChrome(is_headless=False) as driver:
+        driver.get(url)
+        dData = driver.page_source
+        time.sleep(900)
+        
+        
+    with open('captcha.txt', mode='w') as file:
+        file.write(dData)
+    try:
+        return to_json(dData)["stores"]
+    except Exception as pJsonError:
+        if attempts < maxRetries:
+            return get_data(url,int(attempts+1),maxRetries)
+        else:
+            tooManyRetries = str(pJsonError)+'\n\n Retried too many times\nRetries:'+str(maxRetries)+'\n\nWith URL:\n'+str(url)
+            raise pJsonError(f'Too many retries')
 
+    
 def fetch_data():
     # Your scraper here
     loc_list = []
@@ -74,11 +102,8 @@ def fetch_data():
         "default/Stores-GetNearestStores?latitude=42.4685771&"
         "longitude=-83.42107759999999&countryCode=US&distanceUnit=mi&maxdistance=2500000"
     )
-    stores_req = session.get(
-        url,
-        headers=headers,
-    )
-    stores = json.loads(stores_req.text.strip())["stores"]
+    url="https://citygear.com/"
+    stores = get_data(url,0,15)
     for store in stores.keys():
         locator_domain = website
         location_name = stores[store]["name"]
