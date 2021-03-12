27c023eaaf6c00c55aca1dfd7964d07254432fc2
Michael Dinh <mkdinh94@gmail.com>
2021-03-12 16:54:02 -0500

    [SLC-8114] update yield

--- apify/himanshu/storelocator/pretzelmaker_com/scrape.py
+++ apify/himanshu/storelocator/pretzelmaker_com/scrape.py
@@ -45,7 +45,6 @@ def fetch_data():
     data = "locateStore=true&country=USA"
     r = session.post("https://pretzelmaker.com/locations/", headers=headers, data=data)
     soup = BeautifulSoup(r.text, "lxml")
-    return_main_object = []
     for script in soup.find_all("script"):
         content = script.string
 
@@ -98,7 +97,7 @@ def fetch_data():
                 for i in store:
                     if not i:
                         i = "<MISSING>"
-                return_main_object.append(store)
+                yield store
 
     data = "locateStore=true&country=Canada"
     r = session.post("https://pretzelmaker.com/locations/", headers=headers, data=data)
