20e2cb4dc2651bb5e8af56749b86a38b8071e9a4
nomihd <nouman.awais.nu@gmail.com>
2021-03-13 17:23:01 +0500

    remove selenium and tag missing lat/lng for 1 location

--- apify/himanshu/storelocator/annashouseus_com/scrape.py
+++ apify/himanshu/storelocator/annashouseus_com/scrape.py
@@ -1,58 +1,87 @@
 import csv
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
-import re
-from sgselenium import SgSelenium
-import json
-import time
-from selenium.webdriver.support.wait import WebDriverWait
+import lxml.html
 
 session = SgRequests()
 
+
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", newline="", mode="w", encoding="utf-8") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+                "page_url",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
-    driver = SgSelenium().firefox() 
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
     }
     base_url = "https://annashouseus.com"
-    r = session.get("https://annashouseus.com/locations/",headers=headers)
-    soup = BeautifulSoup(r.text,"lxml")
-    return_main_object = []
+    r = session.get("https://annashouseus.com/locations/", headers=headers)
+    soup = BeautifulSoup(r.text, "lxml")
+    stores_sel = lxml.html.fromstring(r.text)
     location_object = {}
-    for location in soup.find_all("div",{'class':"vc-location-content"}):
+    for location in soup.find_all("div", {"class": "vc-location-content"}):
         location_details = list(location.stripped_strings)
         for i in range(len(location_details)):
             if location_details[i] == "Phone:":
-                phone = location_details[i+1]
+                phone = location_details[i + 1]
                 break
         geo_location = location.find_all("iframe")[-1]["src"]
         if len(geo_location.split("!3d")) != 1:
-            location_object[location_details[0]] = [phone,geo_location.split("!3d")[1].split("!")[0],geo_location.split("!2d")[1].split("!")[0]]
+            location_object[location_details[0]] = [
+                phone,
+                geo_location.split("!3d")[1].split("!")[0],
+                geo_location.split("!2d")[1].split("!")[0],
+            ]
         else:
-            driver.get("https://annashouseus.com/locations/")
-            time.sleep(5)
-            driver.find_element_by_xpath("//li[@data-target='" + str(location["id"]) + "']").click()
-            driver.switch_to.frame(driver.find_element_by_xpath("//div[@id='" + str(location["id"]) + "']//iframe[@scrolling='no']"))
-            time.sleep(3)
-            geo_soup = BeautifulSoup(driver.page_source,"lxml")
-            for script in geo_soup.find_all("script"):
-                if "initEmbed" in script.text:
-                    lat = json.loads(script.text.split("initEmbed(")[1].split(");")[0])[21][0][0][2]
-                    lng = json.loads(script.text.split("initEmbed(")[1].split(");")[0])[21][0][0][1]
-            location_object[location_details[0]] = [phone,lat,lng]
-    for location in soup.find_all("div",{"class":"vc-locationContainer"}):
+            location_object[location_details[0]] = [phone, "<MISSING>", "<MISSING>"]
+
+    hours = stores_sel.xpath(
+        '//div[@class="et_pb_row et_pb_row_1"]//div[@class="et_pb_text_inner"]/p'
+    )
+    for location in soup.find_all("div", {"class": "vc-locationContainer"}):
         location_details = list(location.stripped_strings)
-        address = location_details[-1].replace("\r","").replace("\n","").replace("\u200e","")
+        address = (
+            location_details[-1]
+            .replace("\r", "")
+            .replace("\n", "")
+            .replace("\u200e", "")
+        )
+        hours_of_operation = "<MISSING>"
+        for hour in hours:
+            if location_details[0] in "".join(hour.xpath("text()")).strip():
+                if "open " in "".join(hour.xpath("text()")).strip():
+                    hours_of_operation = (
+                        "".join(hour.xpath("text()")).strip().split("open ")[1].strip()
+                    )
+                elif "temporarily closed" in "".join(hour.xpath("text()")).strip():
+                    hours_of_operation = "temporarily closed"
+
         store = []
         store.append("https://annashouseus.com")
         store.append(location_details[0])
@@ -72,12 +101,19 @@ def fetch_data():
         store.append("<MISSING>")
         store.append(location_object[location_details[0]][1])
         store.append(location_object[location_details[0]][2])
-        store.append(soup.find("div",{'class':"et_pb_row et_pb_row_1"}).find("p").text.replace("–","-"))
+        store.append(
+            hours_of_operation.encode("ascii", "replace")
+            .decode("utf-8")
+            .replace("?", "-")
+            .strip()
+        )
         store.append("https://annashouseus.com/locations/")
         yield store
+
 
 def scrape():
     data = fetch_data()
     write_output(data)
+
 
 scrape()
