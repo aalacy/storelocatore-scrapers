ff68a245b5108601497a4e1b220bb69c91b64d8d
Shapoval1 <shshipovald@mail.ru>
2021-03-17 23:01:43 +0200

    add texassteakhouse_com

---
+++ apify/Shapoval1/storelocator/texassteakhouse_com/scrape.py
@@ -0,0 +1,121 @@
+import csv
+import usaddress
+from lxml import html
+from sgrequests import SgRequests
+
+
+def write_output(data):
+    with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
+
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
+
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    out = []
+    locator_domain = "https://texassteakhouse.com"
+    page_url = "https://texassteakhouse.com/locations/"
+    session = SgRequests()
+    tag = {
+        "Recipient": "recipient",
+        "AddressNumber": "address1",
+        "AddressNumberPrefix": "address1",
+        "AddressNumberSuffix": "address1",
+        "StreetName": "address1",
+        "StreetNamePreDirectional": "address1",
+        "StreetNamePreModifier": "address1",
+        "StreetNamePreType": "address1",
+        "StreetNamePostDirectional": "address1",
+        "StreetNamePostModifier": "address1",
+        "StreetNamePostType": "address1",
+        "CornerOf": "address1",
+        "IntersectionSeparator": "address1",
+        "LandmarkName": "address1",
+        "USPSBoxGroupID": "address1",
+        "USPSBoxGroupType": "address1",
+        "USPSBoxID": "address1",
+        "USPSBoxType": "address1",
+        "BuildingName": "address2",
+        "OccupancyType": "address2",
+        "OccupancyIdentifier": "address2",
+        "SubaddressIdentifier": "address2",
+        "SubaddressType": "address2",
+        "PlaceName": "city",
+        "StateName": "state",
+        "ZipCode": "postal",
+    }
+    r = session.get(page_url)
+    tree = html.fromstring(r.text)
+    block = tree.xpath('//div[./h4]')
+    for b in block:
+        location_name = ''.join(b.xpath('.//h4/text()'))
+        line = b.xpath('.//p[1]/text()')
+        ad = ''.join(line[:2])
+        a = usaddress.tag(ad, tag_mapping=tag)[0]
+
+        street_address = f"{a.get('address1')} {a.get('address2')}".replace('None','').strip()
+        phone = ''.join(line[2]).strip()
+        city = a.get('city')
+        state = a.get('state')
+        country_code = "US"
+        store_number = "<MISSING>"
+        ll = ''.join(b.xpath('.//a[contains(@href, "google")]/@href'))
+        latitude = "<MISSING>"
+        longitude = "<MISSING>"
+        if ll.find('z/') != -1:
+            latitude = ''.join(ll.split('/@')[1].split(',')[:1])
+            longitude = ll.split('/@')[1].split(',')[1]
+        location_type = "<MISSING>"
+        hours_of_operation = ' '.join(b.xpath('.//p[2]//text()')).replace('\n','').strip()
+        postal = a.get('postal')
+
+        row = [
+            locator_domain,
+            page_url,
+            location_name,
+            street_address,
+            city,
+            state,
+            postal,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
+        out.append(row)
+
+    return out
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+if __name__ == "__main__":
+    scrape()
