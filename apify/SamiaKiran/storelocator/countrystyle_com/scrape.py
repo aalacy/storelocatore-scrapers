a189f2cd7fd2ae1a50aefda8a0714c88234d0bd1
Samia Kiran <samiakiran17@gmail.com>
2021-03-18 01:19:35 +0500

    SLC-9363 Add crawl for countrystyle_com
    https://safegraph-crawl.atlassian.net/browse/SLC-9363

--- apify/SamiaKiran/storelocator/countrystyle_com/scrape.py
+++ apify/SamiaKiran/storelocator/countrystyle_com/scrape.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import csv
 from sgrequests import SgRequests
 from sglogging import sglog
@@ -79,12 +80,88 @@ def fetch_data():
                 ]
             )
         return final_data
+=======
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
+from sgrequests import SgRequests
+from sglogging import sglog
+from sgscrape import sgpostal as parser
+from bs4 import BeautifulSoup
+
+website = "countrystyle_com"
+log = sglog.SgLogSetup().get_logger(logger_name=website)
+session = SgRequests()
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
+    "Accept": "application/json",
+}
+
+
+def fetch_data():
+    if True:
+        url = "https://www.countrystyle.com/wp-content/plugins/superstorefinder-wp/ssf-wp-xml.php?wpml_lang=&t=1615704674870"
+        r = session.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, "html.parser")
+        loclist = soup.find("store").findAll("item")
+        for loc in loclist:
+            location_name = loc.find("location").text
+            store = loc.find("sortord").text
+            raw_address = loc.find("address").text.replace(",", "")
+            formatted_addr = parser.parse_address_intl(raw_address)
+            street_address = formatted_addr.street_address_1
+            street_address = (
+                street_address + ", " + formatted_addr.street_address_2
+                if formatted_addr.street_address_2
+                else street_address
+            )
+            if formatted_addr.street_address_2:
+                street_address = street_address + ", " + formatted_addr.street_address_2
+            city = formatted_addr.city
+            zip_postal = loc.find("telephone").text
+            if not zip_postal:
+                zip_postal = "<MISSING>"
+            state = loc.find("country").text
+            latitude = loc.find("latitude").text
+            longitude = loc.find("longitude").text
+            phone = loc.find("fax").text
+            if not phone:
+                phone = "<MISSING>"
+
+            yield SgRecord(
+                locator_domain="https://www.countrystyle.com/",
+                page_url="https://www.countrystyle.com/stores/",
+                location_name=location_name,
+                street_address=street_address,
+                city=city,
+                state=state,
+                zip_postal=zip_postal,
+                country_code="CA",
+                store_number="<MISSING>",
+                phone=phone,
+                location_type="<MISSING>",
+                latitude="<MISSING>",
+                longitude="<MISSING>",
+                hours_of_operation="<MISSING>",
+                raw_address=raw_address,
+            )
+>>>>>>> Stashed changes
 
 
 def scrape():
     log.info("Started")
+<<<<<<< Updated upstream
     data = fetch_data()
     write_output(data)
+=======
+    count = 0
+    with SgWriter() as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
+
+    log.info(f"No of records being processed: {count}")
+>>>>>>> Stashed changes
     log.info("Finished")
 
 
