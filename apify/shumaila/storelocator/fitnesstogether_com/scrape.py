44b3b8ba4117e9a9ed844abe9449949172c0b074
Sajjal Malik <40670001+sajjalmalik@users.noreply.github.com>
2021-03-17 23:05:14 +0500

    Update scrape.py

--- apify/shumaila/storelocator/fitnesstogether_com/scrape.py
+++ apify/shumaila/storelocator/fitnesstogether_com/scrape.py
@@ -1,107 +1,137 @@
 from bs4 import BeautifulSoup
 import csv
-import string
-import re, time
 import json
 from sgrequests import SgRequests
 
 session = SgRequests()
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
-           }
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
+}
+
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", mode="w") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
         # Header
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
 
 
 def fetch_data():
-    # Your scraper here
     data = []
-    cleanr = re.compile(r'<[^>]+>')    
-    url = 'https://fitnesstogether.com/personal-trainers-near-me'
+    url = "https://fitnesstogether.com/personal-trainers-near-me"
     p = 0
     r = session.get(url, headers=headers, verify=False)
-    soup = BeautifulSoup(r.text,'html.parser')
-    divlist = soup.select("a[href*=find-a-]")    
-    hourdict = {1:'mon',2:'tue',3:'wed',4:'thu',5:'fri',6:'sat',7:'sun'}
+    soup = BeautifulSoup(r.text, "html.parser")
+    divlist = soup.findAll("li", {"class": "list-title"})
+    hourdict = {1: "mon", 2: "tue", 3: "wed", 4: "thu", 5: "fri", 6: "sat", 7: "sun"}
     for div in divlist:
         search = div.text
-        gurl = 'https://maps.googleapis.com/maps/api/geocode/json?address='+search+'&key=AIzaSyCT4uvUVAv4U6-Lgeg94CIuxUg-iM2aA4s&components=country%3AUS'
+        gurl = (
+            "https://maps.googleapis.com/maps/api/geocode/json?address="
+            + search
+            + "&key=AIzaSyCT4uvUVAv4U6-Lgeg94CIuxUg-iM2aA4s&components=country%3AUS"
+        )
         r = session.get(gurl, headers=headers, verify=False).json()
-        if r['status'] == 'REQUEST_DENIED':
+        if r["status"] == "REQUEST_DENIED":
             pass
         else:
-            coord = r['results'][0]["geometry"]['location']
-            latnow = coord['lat']
-            lngnow = coord['lng']
-        url = 'https://fitnesstogether.com/locator?q='+search+'%2C%20United%20States&lat='+str(latnow)+'&lng='+str(lngnow)
+            coord = r["results"][0]["geometry"]["location"]
+            latnow = coord["lat"]
+            lngnow = coord["lng"]
+        url = (
+            "https://fitnesstogether.com/locator?q="
+            + search
+            + "%2C%20United%20States&lat="
+            + str(latnow)
+            + "&lng="
+            + str(lngnow)
+        )
         loclist = session.get(url, headers=headers, verify=False).json()["locations"]
         for loc in loclist:
             if loc["status"] == "soon":
                 continue
             title = loc["name"]
             store = loc["id"]
-            link = 'https://fitnesstogether.com/'+loc['slug']
-            street = loc["address"]+' '+loc["address_2"]
-            city = loc['city']
-            state = loc['state']
-            pcode =loc['zip_code']
-            phone = loc['phone_number']
+            link = "https://fitnesstogether.com/" + loc["slug"]
+            street = loc["address"] + " " + loc["address_2"]
+            city = loc["city"]
+            state = loc["state"]
+            pcode = loc["zip_code"]
+            phone = loc["phone_number"]
             if len(phone) < 3:
-                phone = '<MISSING>'
-            lat = loc['latitude']
-            longt = loc['longitude']
+                phone = "<MISSING>"
+            lat = loc["latitude"]
+            longt = loc["longitude"]
             hourslist = json.loads(loc["business_hours"])
-            hours = ''
+            hours = ""
             for hr in hourslist:
                 day = hr
                 hr = hourslist[day]
-                start = ':'.join(hr['start'].split(':')[0:2])
-                end = hr['end'].split(':')[0:2]
-                endtime =(int)(end[0])
+                start = ":".join(hr["start"].split(":")[0:2])
+                end = hr["end"].split(":")[0:2]
+                endtime = (int)(end[0])
                 if endtime > 12:
                     endtime = endtime - 12
+                hours = (
+                    hours
+                    + day
+                    + " "
+                    + start
+                    + " AM - "
+                    + str(endtime)
+                    + ":"
+                    + end[1]
+                    + " PM "
+                )
+            data.append(
+                [
+                    "https://fitnesstogether.com/",
+                    link,
+                    title,
+                    street,
+                    city,
+                    state,
+                    pcode,
+                    "US",
+                    store,
+                    phone,
+                    "<MISSING>",
+                    lat,
+                    longt,
+                    hours,
+                ]
+            )
 
-                hours = hours + day + ' '+start + ' AM - '+str(endtime) + ':'+end[1]+' PM '
-                
-            
-    
-            data.append([
-                'https://fitnesstogether.com/',
-                link,                   
-                title,
-                street,
-                city,
-                state,
-                pcode,
-                'US',
-                store,
-                phone,
-                '<MISSING>',
-                lat,
-                longt,
-                hours
-            ])
-            #print(p,data[p])
             p += 1
-        
-
-
-    
-                
-        
     return data
 
 
-def scrape():   
+def scrape():
     data = fetch_data()
-    write_output(data)   
+    write_output(data)
+
 
 scrape()
-
