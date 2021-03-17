582f3aba53c0fcfd887621ae7bed00ae8029083e
Samia Kiran <samiakiran17@gmail.com>
2021-03-18 01:22:22 +0500

    SLC-9363 Add crawl for countrystyle_com
    https://safegraph-crawl.atlassian.net/browse/SLC-9363

--- apify/SamiaKiran/storelocator/countrystyle_com/scrape.py
+++ apify/SamiaKiran/storelocator/countrystyle_com/scrape.py
@@ -1,86 +1,3 @@
-<<<<<<< Updated upstream
-import csv
-from sgrequests import SgRequests
-from sglogging import sglog
-
-website = "dibellas_com"
-log = sglog.SgLogSetup().get_logger(logger_name=website)
-session = SgRequests()
-
-session = SgRequests()
-headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
-}
-
-
-def write_output(data):
-    with open("data.csv", mode="w", newline="", encoding="utf8") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        for row in data:
-            writer.writerow(row)
-        log.info(f"No of records being processed: {len(data)}")
-
-
-def fetch_data():
-    # Your scraper here
-    final_data = []
-    if True:
-        url = "https://api.chepri.io/dibellas/custom/dineengine/vendor/olo/restaurants?includePrivate=false"
-        loclist = session.get(url, headers=headers).json()["restaurants"]
-        for loc in loclist:
-            link = loc["url"]
-            title = loc["storename"]
-            store = loc["id"]
-            street = loc["streetaddress"]
-            city = loc["city"]
-            state = loc["state"]
-            pcode = loc["zip"]
-            lat = loc["latitude"]
-            longt = loc["longitude"]
-            phone = loc["telephone"]
-            hours = "<INACCESSIBLE>"
-            final_data.append(
-                [
-                    "https://www.dibellas.com/",
-                    link,
-                    title,
-                    street,
-                    city,
-                    state,
-                    pcode,
-                    "USA",
-                    store,
-                    phone,
-                    "<MISSING>",
-                    lat,
-                    longt,
-                    hours.strip(),
-                ]
-            )
-        return final_data
-=======
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
@@ -144,15 +61,10 @@ def fetch_data():
                 hours_of_operation="<MISSING>",
                 raw_address=raw_address,
             )
->>>>>>> Stashed changes
 
 
 def scrape():
     log.info("Started")
-<<<<<<< Updated upstream
-    data = fetch_data()
-    write_output(data)
-=======
     count = 0
     with SgWriter() as writer:
         results = fetch_data()
@@ -161,7 +73,6 @@ def scrape():
             count = count + 1
 
     log.info(f"No of records being processed: {count}")
->>>>>>> Stashed changes
     log.info("Finished")
 
 
