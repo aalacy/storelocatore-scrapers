dd0f58e8891cd296549ede29b201e01580f5ebe8
Samia Kiran <samiakiran17@gmail.com>
2021-03-17 23:40:05 +0500

    Update scrape.py

--- apify/SamiaKiran/storelocator/opticalexpress_co_uk/scrape.py
+++ apify/SamiaKiran/storelocator/opticalexpress_co_uk/scrape.py
@@ -15,39 +15,7 @@ headers = {
 }
 
 
-def write_output(data):
-    with open("data.csv", mode="w", newline="", encoding="utf8") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        for row in data:
-            writer.writerow(row)
-        log.info(f"No of records being processed: {len(data)}")
-
-
 def fetch_data():
-    # Your scraper here
-    final_data = []
     temp = []
     if True:
         locator_domain = "https://www.opticalexpress.co.uk/clinic-finder"
