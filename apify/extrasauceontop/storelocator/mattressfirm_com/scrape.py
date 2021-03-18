55c6d7b875bbc758b0eed86ecb338baa3b59ded7
extrasauceontop <msuchemba@gmail.com>
2021-03-18 15:34:56 -0500

    scrape

--- apify/extrasauceontop/storelocator/mattressfirm_com/scrape.py
+++ apify/extrasauceontop/storelocator/mattressfirm_com/scrape.py
@@ -62,126 +62,131 @@ headers = {
 
 session = SgRequests(retry_behavior=None)
 
-hour_urls = []
 
-for zipcode in search:
-    search_url = (
-        "https://maps.mattressfirm.com/api/getAsyncLocations?template=search&level=search&radius=100&search="
-        + zipcode
-    )
-    response = session.get(search_url, headers=headers).json()
+def getdata():
+    for zipcode in search:
+        search_url = (
+            "https://maps.mattressfirm.com/api/getAsyncLocations?template=search&level=search&radius=100&search="
+            + zipcode
+        )
+        response = session.get(search_url, headers=headers).json()
 
-    coords = []
+        coords = []
 
-    if response["markers"] is None:
+        if response["markers"] is None:
+            search.mark_found(coords)
+            continue
+        for location in response["markers"]:
+
+            soup = bs(location["info"], "html.parser")
+
+            locator_domain = "mattressfirm.com"
+            page_url = search_url
+            location_name = soup.find("div", attrs={"class": "store-name"}).text.strip()
+            address = soup.find(
+                "span", attrs={"class": "block address-1 bold fc-gray"}
+            ).text.strip()
+            city = (
+                soup.find("span", attrs={"class": "block address-zip bold fc-gray"})
+                .text.strip()
+                .split(",")[0]
+                .strip()
+            )
+            state = (
+                soup.find("span", attrs={"class": "block address-zip bold fc-gray"})
+                .text.strip()
+                .split(",")[1]
+                .strip()
+            )
+            zipp = (
+                soup.find("span", attrs={"class": "block address-zip bold fc-gray"})
+                .text.strip()
+                .split(",")[2]
+                .strip()
+            )
+            country_code = "US"
+
+            phone = soup.find("a", attrs={"class": "phone"}).text.strip()
+            location_type = (
+                soup.find("div", attrs={"class": "popup map-list-item-wrap"})[
+                    "data-particles"
+                ]
+                .replace("Is", "")
+                .strip()
+            )
+
+            latitude = location["lat"]
+            longitude = location["lng"]
+
+            locator_domains.append(locator_domain)
+            page_urls.append(page_url)
+            location_names.append(location_name)
+            street_addresses.append(address)
+            citys.append(city)
+            states.append(state)
+            zips.append(zipp)
+            country_codes.append(country_code)
+            phones.append(phone)
+            location_types.append(location_type)
+            latitudes.append(latitude)
+            longitudes.append(longitude)
+            coords.append([latitude, longitude])
+
+        json_objects = extract_json(response["maplist"])
+        for y in range(len(json_objects)):
+
+            if y % 3 == 0:
+                store_number = json_objects[y]["store-id"]
+                store_numbers.append(store_number)
+
+            elif y % 3 == 2:
+                try:
+                    day_hours = json_objects[y]["days"]
+                    days = day_hours.keys()
+                    hours = ""
+                    for day in days:
+                        hours = (
+                            hours
+                            + day
+                            + " "
+                            + day_hours[day][0]["open"]
+                            + "-"
+                            + day_hours[day][0]["close"]
+                            + ", "
+                        )
+
+                    hours = hours[:-2]
+                    hours_of_operations.append(hours)
+
+                except Exception:
+                    hours = "<MISSING>"
+                    hours_of_operations.append(hours)
+
         search.mark_found(coords)
-        continue
-    for location in response["markers"]:
 
-        soup = bs(location["info"], "html.parser")
+    df = pd.DataFrame(
+        {
+            "locator_domain": locator_domains,
+            "page_url": page_urls,
+            "location_name": location_names,
+            "street_address": street_addresses,
+            "city": citys,
+            "state": states,
+            "zip": zips,
+            "store_number": store_numbers,
+            "phone": phones,
+            "latitude": latitudes,
+            "longitude": longitudes,
+            "country_code": country_codes,
+            "location_type": location_types,
+            "hours_of_operation": hours_of_operations,
+        }
+    )
 
-        locator_domain = "mattressfirm.com"
-        page_url = search_url
-        location_name = soup.find("div", attrs={"class": "store-name"}).text.strip()
-        address = soup.find(
-            "span", attrs={"class": "block address-1 bold fc-gray"}
-        ).text.strip()
-        city = (
-            soup.find("span", attrs={"class": "block address-zip bold fc-gray"})
-            .text.strip()
-            .split(",")[0]
-            .strip()
-        )
-        state = (
-            soup.find("span", attrs={"class": "block address-zip bold fc-gray"})
-            .text.strip()
-            .split(",")[1]
-            .strip()
-        )
-        zipp = (
-            soup.find("span", attrs={"class": "block address-zip bold fc-gray"})
-            .text.strip()
-            .split(",")[2]
-            .strip()
-        )
-        country_code = "US"
+    return df
 
-        phone = soup.find("a", attrs={"class": "phone"}).text.strip()
-        location_type = (
-            soup.find("div", attrs={"class": "popup map-list-item-wrap"})[
-                "data-particles"
-            ]
-            .replace("Is", "")
-            .strip()
-        )
 
-        latitude = location["lat"]
-        longitude = location["lng"]
-
-        locator_domains.append(locator_domain)
-        page_urls.append(page_url)
-        location_names.append(location_name)
-        street_addresses.append(address)
-        citys.append(city)
-        states.append(state)
-        zips.append(zipp)
-        country_codes.append(country_code)
-        phones.append(phone)
-        location_types.append(location_type)
-        latitudes.append(latitude)
-        longitudes.append(longitude)
-        coords.append([latitude, longitude])
-
-    json_objects = extract_json(response["maplist"])
-    for y in range(len(json_objects)):
-
-        if y % 3 == 0:
-            store_number = json_objects[y]["store-id"]
-            store_numbers.append(store_number)
-
-        elif y % 3 == 2:
-            try:
-                day_hours = json_objects[y]["days"]
-                days = day_hours.keys()
-                hours = ""
-                for day in days:
-                    hours = (
-                        hours
-                        + day
-                        + " "
-                        + day_hours[day][0]["open"]
-                        + "-"
-                        + day_hours[day][0]["close"]
-                        + ", "
-                    )
-
-                hours = hours[:-2]
-                hours_of_operations.append(hours)
-
-            except Exception:
-                hours = "<MISSING>"
-                hours_of_operations.append(hours)
-
-    search.mark_found(coords)
-
-df = pd.DataFrame(
-    {
-        "locator_domain": locator_domains,
-        "page_url": page_urls,
-        "location_name": location_names,
-        "street_address": street_addresses,
-        "city": citys,
-        "state": states,
-        "zip": zips,
-        "store_number": store_numbers,
-        "phone": phones,
-        "latitude": latitudes,
-        "longitude": longitudes,
-        "country_code": country_codes,
-        "location_type": location_types,
-        "hours_of_operation": hours_of_operations,
-    }
-)
+df = getdata()
 
 df = df.fillna("<MISSING>")
 df = df.replace(r"^\s*$", "<MISSING>", regex=True)
