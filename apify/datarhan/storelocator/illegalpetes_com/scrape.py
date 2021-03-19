b1380553b92a81369eba1067d34cd046c9694e7c
datarhan <slutchenko@gmail.com>
2021-03-19 16:31:08 +0100

    [SLC-10310] black update

--- apify/datarhan/storelocator/illegalpetes_com/scrape.py
+++ apify/datarhan/storelocator/illegalpetes_com/scrape.py
@@ -43,41 +43,41 @@ def fetch_data():
     items = []
 
     start_url = "https://www.illegalpetes.com/hours-locations/"
-    domain = re.findall("://(.+?)/", start_url)[0].replace('www.', '')
+    domain = re.findall("://(.+?)/", start_url)[0].replace("www.", "")
     hdr = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
     response = session.get(start_url, headers=hdr)
     dom = etree.HTML(response.text)
     data = dom.xpath('//script[@type="application/ld+json"]/text()')[0]
     data = json.loads(data)
 
-    for poi in data['subOrganization']:
-        store_url = poi['url']
+    for poi in data["subOrganization"]:
+        store_url = poi["url"]
         loc_response = session.get(store_url)
         loc_dom = etree.HTML(loc_response.text)
 
-        location_name = poi['name']
+        location_name = poi["name"]
         location_name = location_name if location_name else "<MISSING>"
-        street_address = poi['address']['streetAddress']
+        street_address = poi["address"]["streetAddress"]
         street_address = street_address if street_address else "<MISSING>"
-        city = poi['address']['addressLocality']
+        city = poi["address"]["addressLocality"]
         city = city if city else "<MISSING>"
-        state = poi['address']['addressRegion']
-        state = state if state else '<MISSING>'
-        zip_code = poi['address']['postalCode']
+        state = poi["address"]["addressRegion"]
+        state = state if state else "<MISSING>"
+        zip_code = poi["address"]["postalCode"]
         zip_code = zip_code if zip_code else "<MISSING>"
-        country_code = '<MISSING>'
-        store_number = '<MISSING>'
-        phone = poi['telephone']
-        phone = phone if phone else '<MISSING>'
-        location_type = poi['@type']
-        latitude = loc_dom.xpath('//@data-gmaps-lat')[0]
-        longitude = loc_dom.xpath('//@data-gmaps-lng')[0]
+        country_code = "<MISSING>"
+        store_number = "<MISSING>"
+        phone = poi["telephone"]
+        phone = phone if phone else "<MISSING>"
+        location_type = poi["@type"]
+        latitude = loc_dom.xpath("//@data-gmaps-lat")[0]
+        longitude = loc_dom.xpath("//@data-gmaps-lng")[0]
         hoo = loc_dom.xpath('//div[h2[contains(text(), "Hours & Location")]]/h3/text()')
         hoo = [e.strip() for e in hoo if e.strip()]
         if not hoo:
-            location_type = 'temporarily closed'
+            location_type = "temporarily closed"
         hours_of_operation = " ".join(hoo) if hoo else "<MISSING>"
 
         item = [
@@ -94,7 +94,7 @@ def fetch_data():
             location_type,
             latitude,
             longitude,
-            hours_of_operation
+            hours_of_operation,
         ]
 
         items.append(item)
