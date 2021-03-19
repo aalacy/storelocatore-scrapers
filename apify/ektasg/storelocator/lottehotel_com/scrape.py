ef986e98cf187e0308184ff434874934470dc321
Samia Kiran <samiakiran17@gmail.com>
2021-03-19 11:56:25 +0500

    SLC-7662 Add maintenance for lottehotel.com
    https://safegraph-crawl.atlassian.net/browse/SLC-7662

--- apify/ektasg/storelocator/lottehotel_com/scrape.py
+++ apify/ektasg/storelocator/lottehotel_com/scrape.py
@@ -1,25 +1,44 @@
 from bs4 import BeautifulSoup
 import csv
-import string
-import re, time
+import time
 import usaddress
 from sgrequests import SgRequests
 from sglogging import SgLogSetup
 
-logger = SgLogSetup().get_logger('lottehotel_com')
-
+logger = SgLogSetup().get_logger("lottehotel_com")
 
 
 session = SgRequests()
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
-           }
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
+}
+
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", mode="w") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
         # Header
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
@@ -29,21 +48,28 @@ def fetch_data():
     # Your scraper here
     data = []
     p = 0
-    
-    url = 'https://www.lottehotel.com/global/en/hotel-finder.html'
-    r = session.get(url, headers=headers, verify=False)  
-    soup =BeautifulSoup(r.text, "html.parser")   
-    maindiv = soup.findAll('ul', {'class': 'd096-city'})[1].find('ul',{'class':'d096-hotel'})
-    
-            
-    divlist = maindiv.findAll('li')
+
+    url = "https://www.lottehotel.com/global/en/hotel-finder.html"
+    r = session.get(url, headers=headers, verify=False)
+    soup = BeautifulSoup(r.text, "html.parser")
+    maindiv = (
+        soup.find("ul", {"class": "d596-nation"})
+        .findAll("li", {"class": "d596-nation__item"})[1]
+        .find("ul")
+    )
+    divlist = maindiv.findAll("li", {"class": "d596-city__item d596-accordion__item"})[
+        0
+    ].findAll("li")
     for div in divlist:
-        link = div.find('p',{'class':'d096-hotel__title'}).find('a')['href']
-        if link.find('http') == -1:
-            link = 'https://www.lottehotel.com' + link
-        title = div.find('p',{'class':'d096-hotel__title'}).find('strong').text.replace('\n','')
-        address = div.find('p',{'class':'d096-hotel__address'}).text.replace('\n','')
-        phone = div.find('p',{'class':'d096-hotel__tel'}).text.lstrip().split('\n',1)[0]
+        temp = div.find("p", {"class": "d596-hotel__title"}).find("a")
+        link = temp["href"]
+        if link.find("http") == -1:
+            link = "https://www.lottehotel.com" + link
+        title = temp.find("strong", {"class": "d596-hotel__name"}).text
+        address = div.find("p", {"class": "d596-hotel__address"}).text
+        phone = (
+            div.find("p", {"class": "d596-hotel__tel"}).text.lstrip().split("\n", 1)[0]
+        )
         address = usaddress.parse(address)
         i = 0
         street = ""
@@ -52,7 +78,14 @@ def fetch_data():
         pcode = ""
         while i < len(address):
             temp = address[i]
-            if temp[1].find("Address") != -1 or temp[1].find("Street") != -1 or temp[1].find("Recipient") != -1 or temp[1].find("BuildingName") != -1 or temp[1].find("USPSBoxType") != -1 or temp[1].find("USPSBoxID") != -1:
+            if (
+                temp[1].find("Address") != -1
+                or temp[1].find("Street") != -1
+                or temp[1].find("Recipient") != -1
+                or temp[1].find("BuildingName") != -1
+                or temp[1].find("USPSBoxType") != -1
+                or temp[1].find("USPSBoxID") != -1
+            ):
                 street = street + " " + temp[0]
             if temp[1].find("PlaceName") != -1:
                 city = city + " " + temp[0]
@@ -62,56 +95,72 @@ def fetch_data():
                 pcode = pcode + " " + temp[0]
 
             i += 1
-        r = session.get(link, headers=headers, verify=False)  
-        soup = str(BeautifulSoup(r.text, "html.parser"))
-        start = soup.find('data-latitude=')
+        r = session.get(link, headers=headers)
+        temp = BeautifulSoup(r.text, "html.parser")
+        try:
+            hour_list = temp.find("div", {"class": "d088-info__time"}).findAll("p")
+            hours = ""
+            for hour in hour_list:
+                day = hour.find("strong").text
+                time = hour.find("span").text
+                hours = hours + " " + day + " " + time
+        except:
+            hours = "<MISSING>"
+        soup = str(temp)
+        start = soup.find("data-latitude=")
         if start == -1:
             start = soup.find("latitude")
             if start == -1:
-                lat = '<MISSING>'
-                longt = '<MISSING>'
+                lat = "<MISSING>"
+                longt = "<MISSING>"
             else:
-                lat,longt = soup.split('"latitude":')[1].split(',',1)
-                longt = longt.split('"longitude":')[1].split('}',1)[0].replace('"','').lstrip().replace('\n','').rstrip()
-                lat = lat.replace('"','').lstrip()
+                lat, longt = soup.split('"latitude":')[1].split(",", 1)
+                longt = (
+                    longt.split('"longitude":')[1]
+                    .split("}", 1)[0]
+                    .replace('"', "")
+                    .lstrip()
+                    .replace("\n", "")
+                    .rstrip()
+                )
+                lat = lat.replace('"', "").lstrip()
         else:
-            lat, longt = soup.split('data-latitude="')[1].split('"',1)
-            longt = soup.split('data-longitude="')[1].split('"',1)[0]
+            lat, longt = soup.split('data-latitude="')[1].split('"', 1)
+            longt = soup.split('data-longitude="')[1].split('"', 1)[0]
 
-        if pcode == '':
-            pcode = '<MISSING>'
+        if pcode == "":
+            pcode = "<MISSING>"
         try:
-            state = state.split(',')[0]
+            state = state.split(",")[0]
         except:
             pass
         try:
-            city = city.split('LOTTE HOTEL')[0]
+            city = city.split("LOTTE HOTEL")[0]
         except:
-            city = '<MISSING>'
-            
-        data.append([
-                        'https://www.lottehotel.com/',
-                        link,                   
-                        title,
-                        street.lstrip(),
-                        city.lstrip().replace(',',''),
-                        state.lstrip().replace(',',''),
-                        pcode.lstrip().replace(',',''),
-                        'US',
-                        '<MISSING>',
-                        phone.replace('+1-',''),
-                        '<MISSING>',
-                        lat,
-                        longt,
-                        '<MISSING>'
-                    ])
-        #logger.info(p,data[p])
+            city = "<MISSING>"
+
+        data.append(
+            [
+                "https://www.lottehotel.com/",
+                link,
+                title,
+                street.lstrip().replace(",", ""),
+                city.lstrip().replace(",", ""),
+                state.lstrip().replace(",", ""),
+                pcode.lstrip().replace(",", ""),
+                "US",
+                "<MISSING>",
+                phone.replace("+1-", ""),
+                "<MISSING>",
+                lat,
+                longt,
+                hours.strip(),
+            ]
+        )
+        # logger.info(p,data[p])
         p += 1
-        #input("NEXT")
-   
+        # input("NEXT")
 
-
-           
     return data
 
 
@@ -120,5 +169,6 @@ def scrape():
     data = fetch_data()
     write_output(data)
     logger.info(time.strftime("%H:%M:%S", time.localtime(time.time())))
+
 
 scrape()
