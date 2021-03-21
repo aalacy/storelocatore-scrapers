f5a801f7ea952553269f83d03d89b6d02ec9fd31
Samia Kiran <samiakiran17@gmail.com>
2021-03-21 18:32:32 +0500

    SLC-10221 Add maintenance for emagine-entertainment
    https://safegraph-crawl.atlassian.net/browse/SLC-10221

--- apify/SamiaKiran/storelocator/emagine-entertainment_com/scrape.py
+++ apify/SamiaKiran/storelocator/emagine-entertainment_com/scrape.py
@@ -1,4 +1,5 @@
-import csv
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
 from sglogging import sglog
 from bs4 import BeautifulSoup
@@ -13,39 +14,7 @@ headers = {
 }
 
 
-def write_output(data):
-    with open("data.csv", mode="w", newline="", encoding="utf8") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        for row in data:
-            writer.writerow(row)
-        log.info(f"No of records being processed: {len(data)}")
-
-
 def fetch_data():
-    # Your scraper here
-    final_data = []
     if True:
         url = "https://www.emagine-entertainment.com/theatres/"
         r = session.get(url, headers=headers, verify=False)
@@ -54,54 +23,68 @@ def fetch_data():
             "div", {"class": "theatre-listings__theatre"}
         )
         for loc in loclist:
-            link = loc.find("a")["href"]
-            r = session.get(link, headers=headers, verify=False)
+            page_url = loc.find("a")["href"]
+            r = session.get(page_url, headers=headers)
+            log.info(page_url)
             soup = BeautifulSoup(r.text, "html.parser")
-            store = r.text.split("full_address :")[1].split("title :", 1)[0]
-            store = store.split('id : "')[1].split('",', 1)[0]
-            title = soup.find("h1", {"class": "headline headline--1"}).text
-            address = soup.findAll(
-                "p", {"class": "theatre-details__sidebar-group-item--address"}
-            )
-            street = address[0].text
-            temp = address[1].text.split(",")
-            city = temp[0]
-            state = temp[1]
             try:
-                phone = (
-                    soup.find(
-                        "div", {"class": "theatre-details__sidebar-group sbgroup1"}
-                    )
-                    .find("a")
-                    .text.strip()
+                store_number = r.text.split("full_address :")[1].split("title :", 1)[0]
+                store_number = store_number.split('id : "')[1].split('",', 1)[0]
+                location_name = soup.find("h1", {"class": "headline headline--1"}).text
+                address = soup.findAll(
+                    "p", {"class": "theatre-details__sidebar-group-item--address"}
                 )
+                street_address = address[0].text
+                temp = address[1].text.split(",")
+                city = temp[0]
+                state = temp[1]
+                zip_postal = "<MISSING>"
+                try:
+                    phone = (
+                        soup.find(
+                            "div", {"class": "theatre-details__sidebar-group sbgroup1"}
+                        )
+                        .find("a")
+                        .text.strip()
+                    )
+                except:
+                    phone = "<MISSING>"
             except:
+                address = loc.find("div").get_text(separator="|", strip=True).split("|")
+                street_address = address[0]
+                address = address[1].split(",")
+                city = address[0]
+                state = address[1]
                 phone = "<MISSING>"
-            final_data.append(
-                [
-                    "https://www.emagine-entertainment.com/",
-                    link,
-                    title,
-                    street,
-                    city,
-                    state.strip(),
-                    "<MISSING>",
-                    "US",
-                    store,
-                    phone,
-                    "<MISSING>",
-                    "<MISSING>",
-                    "<MISSING>",
-                    "<MISSING>",
-                ]
+                store_number = "<MISSING>"
+            yield SgRecord(
+                locator_domain="https://www.emagine-entertainment.com/",
+                page_url=page_url,
+                location_name=location_name.strip(),
+                street_address=street_address.strip(),
+                city=city.strip(),
+                state=state.strip(),
+                zip_postal="<MISSING>",
+                country_code="US",
+                store_number=store_number.strip(),
+                phone=phone.strip(),
+                location_type="<MISSING>",
+                latitude="<MISSING>",
+                longitude="<MISSING>",
+                hours_of_operation="<MISSING>",
             )
-        return final_data
 
 
 def scrape():
     log.info("Started")
-    data = fetch_data()
-    write_output(data)
+    count = 0
+    with SgWriter() as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
+
+    log.info(f"No of records being processed: {count}")
     log.info("Finished")
 
 
