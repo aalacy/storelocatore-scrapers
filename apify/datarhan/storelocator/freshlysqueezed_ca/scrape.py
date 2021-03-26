82c81b9e7795dd5520e7ecc333ad6a460df09fbe
datarhan <slutchenko@gmail.com>
2021-03-26 21:46:35 +0100

    [SLC-10985] Add crawler for freshlysqueezed.ca

---
+++ apify/datarhan/storelocator/freshlysqueezed_ca/scrape.py
@@ -0,0 +1,120 @@
+import re
+import csv
+from lxml import etree
+
+from sgrequests import SgRequests
+from sgscrape.sgpostal import parse_address_intl
+
+
+def write_output(data):
+    with open("data.csv", mode="w", encoding="utf-8") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
+
+        # Header
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    # Your scraper here
+    session = SgRequests().requests_retry_session(retries=2, backoff_factor=0.3)
+
+    items = []
+
+    start_url = "http://www.freshlysqueezed.ca/stores/"
+    domain = re.findall("://(.+?)/", start_url)[0].replace("www.", "")
+    hdr = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
+    }
+    response = session.get(start_url, headers=hdr)
+    all_ids = re.findall(r"cspm_new_pin_object\(.+'(\d+)',", response.text)
+    post_url = "http://www.freshlysqueezed.ca/dev/wp-admin/admin-ajax.php"
+    hdr = {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+    }
+    for store_number in all_ids:
+        formdata = {
+            "action": "cspm_load_carousel_item",
+            "post_id": store_number,
+            "items_view": "listview",
+        }
+        loc_response = session.post(post_url, data=formdata, headers=hdr)
+        loc_dom = etree.HTML(loc_response.text)
+
+        store_url = start_url
+        location_name = loc_dom.xpath("//a/text()")
+        location_name = location_name[0] if location_name else "<MISSING>"
+        addr = parse_address_intl(
+            loc_dom.xpath('//div[@class="details_infos"]/text()')[0]
+        )
+        street_address = addr.street_address_1
+        if addr.street_address_2:
+            street_address += " " + addr.street_address_2
+        street_address = street_address if street_address else "<MISSING>"
+        if "…" in street_address:
+            street_address = "<MISSING>"
+        city = addr.city
+        city = city if city else "<MISSING>"
+        state = addr.state
+        state = state if state else "<MISSING>"
+        zip_code = addr.postcode
+        zip_code = zip_code.replace("…", "") if zip_code else "<MISSING>"
+        country_code = "CA"
+        phone = "<MISSING>"
+        location_type = "<MISSING>"
+        latitude = "<MISSING>"
+        longitude = "<MISSING>"
+        hours_of_operation = "<MISSING>"
+
+        item = [
+            domain,
+            store_url,
+            location_name,
+            street_address,
+            city,
+            state,
+            zip_code,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
+
+        items.append(item)
+
+    return items
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+if __name__ == "__main__":
+    scrape()
