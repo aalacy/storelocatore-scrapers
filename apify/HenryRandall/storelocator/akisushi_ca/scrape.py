63b8e3fec473dfe7755a2cf88fe001fb2ca3e5dc
HenryRandall <henrywrandall@gmail.com>
2021-04-01 16:27:07 -0400

    Update scrape.py

--- apify/HenryRandall/storelocator/akisushi_ca/scrape.py
+++ apify/HenryRandall/storelocator/akisushi_ca/scrape.py
@@ -14,7 +14,7 @@ def fetch_data():
     output = []
     for row in locations:
         currentrow = []
-        currentrow.append(loocator_domain)
+        currentrow.append(locator_domain)
         currentrow.append(url)
         currentrow.append(row[0])
         currentrow.append(row[4])
