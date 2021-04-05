e37b3379ac029ac211129439c5a8ec39a9a76633
Kiran Tanweer <44836465+Kiran-Tanweer@users.noreply.github.com>
2021-04-06 04:13:32 +0500

    Add files via upload

--- apify/Kiran-Tanweer/storelocator/zoomcare_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/zoomcare_com/scrape.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import csv
 import time
-import usaddress
+from sgscrape import sgpostal as parser
 from sgrequests import SgRequests
 from sglogging import SgLogSetup
 
@@ -78,69 +78,48 @@ def fetch_data():
             )
             if maps is not None:
                 maps = str(maps)
-                maps = maps.split("center%3D")[1].split("%26key")[0]
-                if maps.find("iframe") != -1:
-                    lat = "<MISSING>"
-                    lng = "<MISSING>"
-                else:
+                if maps.find("frameborder") != -1:
+                    maps = maps.split("center%3D")[1].split("%26key")[0]
                     lat, lng = maps.split("%252C")
+                else:
+                    maps = maps.split("!2d")[1].split("!2m")[0]
+                    lng, lat = maps.split("!3d")
             else:
                 lat = "<MISSING>"
                 lng = "<MISSING>"
             hours = info.find("h5").text.strip()
             hours = hours.replace(" |", ",")
             address = address.strip()
-            address = address + " 12345"
+            parsed = parser.parse_address_usa(address)
+            street1 = (
+                parsed.street_address_1 if parsed.street_address_1 else "<MISSING>"
+            )
+            street = (
+                (street1 + ", " + parsed.street_address_2)
+                if parsed.street_address_2
+                else street1
+            )
+            city = parsed.city if parsed.city else "<MISSING>"
+            state = parsed.state if parsed.state else "<MISSING>"
+            pcode = parsed.postcode if parsed.postcode else "<MISSING>"
 
-            address = address.replace(",", "")
-            address = usaddress.parse(address)
-            i = 0
-            street = ""
-            city = ""
-            state = ""
-            pcode = ""
-            while i < len(address):
-                temp = address[i]
-                if (
-                    temp[1].find("Address") != -1
-                    or temp[1].find("Street") != -1
-                    or temp[1].find("Recipient") != -1
-                    or temp[1].find("Occupancy") != -1
-                    or temp[1].find("BuildingName") != -1
-                    or temp[1].find("USPSBoxType") != -1
-                    or temp[1].find("USPSBoxID") != -1
-                ):
-                    street = street + " " + temp[0]
-                if temp[1].find("PlaceName") != -1:
-                    city = city + " " + temp[0]
-                if temp[1].find("StateName") != -1:
-                    state = state + " " + temp[0]
-                if temp[1].find("ZipCode") != -1:
-                    pcode = pcode + " " + temp[0]
-                i += 1
-            street = street.lstrip()
-            street = street.replace(",", "")
-            city = city.lstrip()
-            city = city.replace(",", "")
-            state = state.lstrip()
-            state = state.replace(",", "")
-            pcode = pcode.lstrip()
-            pcode = pcode.replace(",", "")
-
-            pcode = "<MISSING>"
             street = street.strip()
 
             if city.find("null") != -1:
                 city = city.lstrip("null").strip()
+
             if link == "https://www.zoomcare.com/clinic/zoomcare-downtown-portland":
                 street = "900 SW 5th Ave (enter on 4th) null"
                 city = "Portland"
                 state = "OR"
+
             if street.find("null") != -1:
                 street = street.replace("null", "")
+
             if link == "https://www.zoomcare.com/clinic/zoomcare-bellevue":
                 street = "10425 NE 8th St #4 Lincoln Square"
                 city = "Bellevue"
+
             street = street.strip()
             city = city.strip()
             state = state.strip()
