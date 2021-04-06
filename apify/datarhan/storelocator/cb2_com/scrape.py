5e052272f2a8684cb74ff346a93150fe42ff3c80
extrasauceontop <msuchemba@gmail.com>
2021-04-06 17:37:49 -0400

    scrape

--- apify/datarhan/storelocator/cb2_com/scrape.py
+++ apify/datarhan/storelocator/cb2_com/scrape.py
@@ -57,7 +57,6 @@ def fetch_data():
 
         driver.get(start_url)
         sleep(10)
-        page_sour = driver.page_source
         dom = etree.HTML(driver.page_source)
 
     all_locations = dom.xpath('//script[@type="application/ld+json"]/text()')
