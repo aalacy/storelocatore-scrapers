2d8d8fbb1b06b044b3b6b0bda1c7820d1d844694
datarhan <slutchenko@gmail.com>
2021-04-07 13:50:35 +0200

    [SLC-11742] black update

--- apify/datarhan/storelocator/eatchinakitchen_com/scrape.py
+++ apify/datarhan/storelocator/eatchinakitchen_com/scrape.py
@@ -42,30 +42,30 @@ def fetch_data():
     items = []
 
     start_url = "http://eatchinakitchen.com/locations/"
-    domain = re.findall(r"://(.+?)/", start_url)[0].replace('www.', '')
+    domain = re.findall(r"://(.+?)/", start_url)[0].replace("www.", "")
     hdr = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
     response = session.get(start_url, headers=hdr)
     dom = etree.HTML(response.text)
 
     all_locations = dom.xpath('//table[@id="tablepress-1"]/tbody/tr')
     for poi_html in all_locations:
-        raw_data = poi_html.xpath('.//text()')
+        raw_data = poi_html.xpath(".//text()")
         raw_data = [e.strip() for e in raw_data if e.strip()]
 
         store_url = start_url
-        location_name = '<MISSING>'
+        location_name = "<MISSING>"
         street_address = raw_data[1]
         city = raw_data[0]
         state = raw_data[-2]
-        zip_code = raw_data[2].replace('#', '').strip()
-        country_code = '<MISSING>'
-        store_number = '<MISSING>'
+        zip_code = raw_data[2].replace("#", "").strip()
+        country_code = "<MISSING>"
+        store_number = "<MISSING>"
         phone = raw_data[-1]
-        location_type = '<MISSING>'
-        latitude = '<MISSING>'
-        longitude = '<MISSING>'
+        location_type = "<MISSING>"
+        latitude = "<MISSING>"
+        longitude = "<MISSING>"
         hours_of_operation = "<MISSING>"
 
         item = [
@@ -82,7 +82,7 @@ def fetch_data():
             location_type,
             latitude,
             longitude,
-            hours_of_operation
+            hours_of_operation,
         ]
 
         items.append(item)
