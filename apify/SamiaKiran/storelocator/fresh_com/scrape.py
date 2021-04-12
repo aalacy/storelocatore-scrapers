1a50dba797684e6acb6396805f21a656b0c835be
Samia Kiran <samiakiran17@gmail.com>
2021-04-12 23:36:20 +0500

    Update scrape.py

--- apify/SamiaKiran/storelocator/fresh_com/scrape.py
+++ apify/SamiaKiran/storelocator/fresh_com/scrape.py
@@ -10,22 +10,22 @@ log = sglog.SgLogSetup().get_logger(logger_name=website)
 session = SgRequests()
 session = SgRequests()
 headers = {
-"authority": "www.fresh.com",
-"method": "GET",
-"path": "/us/customer-service/USShops.html",
-"scheme": "https",
-"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-"accept-encoding": "gzip, deflate, br",
-"accept-language": "en-US,en;q=0.9",
-"cache-control": "max-age=0",
-"sec-ch-ua": '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-"sec-ch-ua-mobile":"?0",
-"sec-fetch-dest": "document",
-"sec-fetch-mode": "navigate",
-"sec-fetch-site": "none",
-"sec-fetch-user": "?1",
-"upgrade-insecure-requests": "1",
-"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+    "authority": "www.fresh.com",
+    "method": "GET",
+    "path": "/us/customer-service/USShops.html",
+    "scheme": "https",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "max-age=0",
+    "sec-ch-ua": '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
 }
 
 
@@ -39,7 +39,9 @@ def fetch_data():
             if len(loc) < 5:
                 continue
             else:
-                location_name= loc.find("p", {"class": "subheader1 privacy-info-question"})
+                location_name = loc.find(
+                    "p", {"class": "subheader1 privacy-info-question"}
+                )
                 page_url = location_name.find("a")["href"]
                 log.info(page_url)
                 location_name = location_name.text
