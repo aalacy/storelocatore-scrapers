506aac64dfedec3b0bf2cbf067ff199346732416
idev-a <ideveloper003@protonmail.com>
2021-04-15 15:52:41 +0000

    fix

--- apify/idev-a/storelocator/resultspt_com/scrape.py
+++ apify/idev-a/storelocator/resultspt_com/scrape.py
@@ -1,7 +1,6 @@
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
-from bs4 import BeautifulSoup as bs
 from sglogging import SgLogSetup
 
 logger = SgLogSetup().get_logger("resultspt")
