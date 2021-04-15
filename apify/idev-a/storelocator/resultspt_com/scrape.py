12fe8dcd7e730171996aff231b0c496be70fe0be
idev-a <ideveloper003@protonmail.com>
2021-04-15 15:51:09 +0000

    fix

--- apify/idev-a/storelocator/resultspt_com/scrape.py
+++ apify/idev-a/storelocator/resultspt_com/scrape.py
@@ -13,71 +13,41 @@ _headers = {
 locator_domain = "https://www.resultspt.com"
 
 
-def _valid(val):
-    val = val.strip()
-    if val.endswith(","):
-        val = val[:-1]
-
-    return val
-
-
-def parse(page_url, sp1):
-    hours = []
-    for hh in sp1.select("div.lmlw-hours-normal-wrap > div"):
-        day = hh.select_one("span.lmlw-normal-hours__day").text
-        time = ""
-        if hh.select_one("span.lmlw-normal-hours-closed"):
-            time = hh.select_one("span.lmlw-normal-hours-closed").text.strip()
-        else:
-            time = f"{hh.select_one('span.lmlw-normal-hours__openat').text.strip()}-{hh.select_one('span.lmlw-normal-hours__closeat').text.strip()}"
-        hours.append(f"{day} {time}")
-
-    return SgRecord(
-        page_url=page_url,
-        location_name=_valid(sp1.h1.text),
-        street_address=_valid(sp1.select_one("span.lmlw-addr-wrap__address").text),
-        city=_valid(sp1.select_one("span.lmlw-addr-wrap__city").text),
-        state=_valid(sp1.select_one("span.lmlw-addr-wrap__state").text),
-        zip_postal=_valid(sp1.select_one("span.lmlw-addr-wrap__zip").text),
-        country_code="US",
-        phone=_valid(sp1.select_one("span.lmlw-contact-phone__number").text),
-        locator_domain=locator_domain,
-        hours_of_operation="; ".join(hours),
-    )
+days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 
 def fetch_data():
+    locator_domain = "https://www.resultspt.com/"
     base_url = "https://www.resultspt.com/locations"
     with SgRequests() as session:
-        sp = bs(session.get(base_url, headers=_headers).text, "lxml")
-        links = sp.select("div.location-column li a")
-        logger.info(f"{len(links)} locations found")
-        for link in links:
-            if not link.has_attr("href"):
-                continue
-            page_url = locator_domain + link["href"]
-            logger.info(page_url)
-            res = session.get(page_url, headers=_headers)
-            if res.status_code != 200:
-                continue
-            if res.url != page_url:
-                continue
-            sp1 = bs(res.text, "lxml")
-            sub_locations = sp1.select("div.location-meta ul li a")
-            if sub_locations:
-                for sub in sub_locations:
-                    if not sub.has_attr("href"):
-                        continue
-                    page_url = locator_domain + sub["href"]
-                    res = session.get(page_url, headers=_headers)
-                    if res.status_code != 200:
-                        continue
-                    if res.url != page_url:
-                        continue
-                    sp1 = bs(res.text, "lxml")
-                    yield parse(page_url, sp1)
-            else:
-                yield parse(page_url, sp1)
+        res = session.get(base_url, headers=_headers).text
+        api_key = res.split("apiKey:")[1].split("shopKitApiUrl:")[0].strip()[1:-2]
+        json_url = f"https://www.resultspt.com/swx/api/v2/locations/271799.json?api_key={api_key}"
+        locations = session.get(json_url, headers=_headers).json()
+        for _ in locations["Places"]:
+            hours = []
+            for hh in _["Hours"]:
+                time = (
+                    "Closed"
+                    if not hh["OpenAt"] and not hh["CloseAt"]
+                    else f"{hh['OpenAt']}-{hh['CloseAt']}"
+                )
+                hours.append(f"{days[hh['Day']]}: {time}")
+            yield SgRecord(
+                page_url=_["Url"],
+                store_number=_["PlaceId"],
+                location_name=_["Name"],
+                street_address=_["Address"],
+                city=_["City"],
+                state=_["State"],
+                latitude=_["CenterPointLat"],
+                longitude=_["CenterPointLong"],
+                zip_postal=_["Zipcode"],
+                country_code=_["Country"],
+                phone=_["Phone"],
+                locator_domain=locator_domain,
+                hours_of_operation="; ".join(hours),
+            )
 
 
 if __name__ == "__main__":
