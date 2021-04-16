b4e93155e39b9e92911aa7a1979143af0f3159ee
datarhan <slutchenko@gmail.com>
2021-04-16 21:05:38 +0200

    [SLC-11810] unused deleted

--- apify/datarhan/storelocator/doitbest_com/scrape.py
+++ apify/datarhan/storelocator/doitbest_com/scrape.py
@@ -1,7 +1,6 @@
 import csv
 import json
 from lxml import etree
-from urllib.parse import urljoin
 
 from sgzip.dynamic import DynamicZipSearch, SearchableCountries
 from sgrequests import SgRequests
