040fef034b1c0a040f1de392ce3e991873c8cf08
Ihor <rzhdgqrr@gmail.com>
2021-04-20 04:22:57 +0300

    add buddyspizza_com

---
+++ apify/MrQwerty1/storelocator/buddyspizza_com/scrape.py
@@ -0,0 +1,169 @@
+import csv
+import usaddress
+
+from concurrent import futures
+from lxml import html
+from sgrequests import SgRequests
+
+
+def write_output(data):
+    with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
+
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
+
+        for row in data:
+            writer.writerow(row)
+
+
+def get_address(line):
+    tag = {
+        "Recipient": "recipient",
+        "AddressNumber": "address1",
+        "AddressNumberPrefix": "address1",
+        "AddressNumberSuffix": "address1",
+        "StreetName": "address1",
+        "StreetNamePreDirectional": "address1",
+        "StreetNamePreModifier": "address1",
+        "StreetNamePreType": "address1",
+        "StreetNamePostDirectional": "address1",
+        "StreetNamePostModifier": "address1",
+        "StreetNamePostType": "address1",
+        "CornerOf": "address1",
+        "IntersectionSeparator": "address1",
+        "LandmarkName": "address1",
+        "USPSBoxGroupID": "address1",
+        "USPSBoxGroupType": "address1",
+        "USPSBoxID": "address1",
+        "USPSBoxType": "address1",
+        "OccupancyType": "address2",
+        "OccupancyIdentifier": "address2",
+        "SubaddressIdentifier": "address2",
+        "SubaddressType": "address2",
+        "PlaceName": "city",
+        "StateName": "state",
+        "ZipCode": "postal",
+    }
+
+    a = usaddress.tag(line, tag_mapping=tag)[0]
+    street_address = f"{a.get('address1')} {a.get('address2') or ''}".strip()
+    if street_address == "None":
+        street_address = "<MISSING>"
+    city = a.get("city") or "<MISSING>"
+    state = a.get("state") or "<MISSING>"
+    postal = a.get("postal") or "<MISSING>"
+
+    return street_address, city, state, postal
+
+
+def get_coords_from_embed(text):
+    try:
+        latitude = text.split("!3d")[1].strip().split("!")[0].strip()
+        longitude = text.split("!2d")[1].strip().split("!")[0].strip()
+    except IndexError:
+        latitude, longitude = "<MISSING>", "<MISSING>"
+
+    return latitude, longitude
+
+
+def get_urls():
+    session = SgRequests()
+    r = session.get("https://www.buddyspizza.com/locations")
+    tree = html.fromstring(r.text)
+
+    return tree.xpath("//div[@class='image-slide-title']/preceding-sibling::a/@href")
+
+
+def get_data(url):
+    locator_domain = "https://www.buddyspizza.com/"
+    page_url = f"https://www.buddyspizza.com{url}"
+
+    session = SgRequests()
+    r = session.get(page_url)
+    tree = html.fromstring(r.text)
+
+    location_name = "".join(tree.xpath("//meta[@itemprop='name']/@content"))
+    line = tree.xpath("//p[./strong[contains(text(), 'Address')]]/text()")
+    line = ", ".join(list(filter(None, [l.strip() for l in line])))
+
+    street_address, city, state, postal = get_address(line)
+    country_code = "US"
+    store_number = "<MISSING>"
+    phone = (
+        "".join(tree.xpath("//p[./strong[contains(text(), 'Phone')]]/a/text()")).strip()
+        or "<MISSING>"
+    )
+    text = "".join(tree.xpath("//iframe/@src"))
+    latitude, longitude = get_coords_from_embed(text)
+    location_type = "<MISSING>"
+
+    hours = tree.xpath(
+        "//h3[text()='Hours']/following-sibling::p/text()|//h3[./strong[contains(text(), 'Hours')]]/following-sibling::p/text()"
+    )
+    hours = list(filter(None, [h.strip() for h in hours]))
+    if len(hours) > 7:
+        hours = hours[:7]
+    hours_of_operation = ";".join(hours).replace("*", "") or "<MISSING>"
+    if ";Dine-in" in hours_of_operation:
+        hours_of_operation = hours_of_operation.split(";Dine-in")[0]
+
+    row = [
+        locator_domain,
+        page_url,
+        location_name,
+        street_address,
+        city,
+        state,
+        postal,
+        country_code,
+        store_number,
+        phone,
+        location_type,
+        latitude,
+        longitude,
+        hours_of_operation,
+    ]
+
+    return row
+
+
+def fetch_data():
+    out = []
+    urls = get_urls()
+
+    with futures.ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_url = {executor.submit(get_data, url): url for url in urls}
+        for future in futures.as_completed(future_to_url):
+            row = future.result()
+            if row:
+                out.append(row)
+
+    return out
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+if __name__ == "__main__":
+    scrape()
