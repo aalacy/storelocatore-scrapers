19a4faa572a08ac5ea7a2a423d6c274dd75be90e
extrasauceontop <msuchemba@gmail.com>
2021-04-21 17:17:09 -0400

    update scrape

--- apify/nsxdarin/storelocator/kfc_co_uk/scrape.py
+++ apify/nsxdarin/storelocator/kfc_co_uk/scrape.py
@@ -88,7 +88,6 @@ def fetch_data():
         website = "kfc.co.uk"
         typ = "<MISSING>"
         store = item["storeid"]
-        print(store)
         hours = ""
         country = "GB"
         try:
