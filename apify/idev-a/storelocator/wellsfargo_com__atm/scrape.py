89592aadd92b0b65eacbe43099a93aed8741a6d1
idev-a <ideveloper003@protonmail.com>
2021-04-21 12:07:29 -0700

    fix

--- apify/idev-a/storelocator/wellsfargo_com__atm/scrape.py
+++ apify/idev-a/storelocator/wellsfargo_com__atm/scrape.py
@@ -1,13 +1,17 @@
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
-from sgselenium import SgChrome
 from bs4 import BeautifulSoup as bs
 from sglogging import SgLogSetup
-import json
-import time
+import re
 
 logger = SgLogSetup().get_logger("wellsfargo")
+
+locator_domain = "https://www.wellsfargo.com/"
+base_url = "https://www.wellsfargo.com/locator/"
+payload_url = "https://www.wellsfargo.com/locator/as/getpayload"
+sitemap1 = "https://www.wellsfargo.com/locator/sitemap1"
+sitemap2 = "https://www.wellsfargo.com/locator/sitemap2"
 
 _headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -18,64 +22,103 @@ _headers = {
 
 
 def fetch_data():
-    locator_domain = "https://www.wellsfargo.com/"
-    base_url = "https://www.wellsfargo.com/locator/"
-    payload_url = "https://www.wellsfargo.com/locator/as/getpayload"
-    with SgChrome(executable_path=r"/mnt/g/work/mia/chromedriver.exe") as driver:
-        driver.get(base_url)
-        soup = bs(driver.page_source, "lxml")
-        states = soup.select("map#theImageMap area")
-        logger.info(f"[states] {len(states)} states")
+    with SgRequests() as session:
         total = 0
-        for state in states:
-            _state = state["href"].replace("#", "")
-            state_url = f"https://www.wellsfargo.com/locator/as/getCities/{_state}"
-            with SgRequests() as session:
-                cities = json.loads(
-                    session.get(state_url, headers=_headers)
-                    .text.split("/*WellsFargoProprietary%")[1]
-                    .split("%WellsFargoProprietary*/")[0]
-                    .strip()
-                )["allCities"]
-                logger.info(f"[{_state}] {len(cities)} cities")
-                for city in cities:
-                    city_url = f"https://www.wellsfargo.com/locator/search/?searchTxt={city}%2C+{_state}&mlflg=N&sgindex=99&chflg=Y&_bo=on&_wl=on&_os=on&bdu=1&_bdu=on&adu=1&_adu=on&ah=1&_ah=on&_sdb=on&_aa=on&_nt=on&_fe=on"
-                    driver.get(city_url)
-                    exist = False
-                    locations = []
-                    while not exist:
-                        time.sleep(1)
-                        logger.info(f"[{city}] waiting ....")
-                        for rr in driver.requests[::-1]:
-                            if rr.url == payload_url and rr.response:
-                                exist = True
-                                locations = json.loads(rr.response.body)[
-                                    "searchResults"
-                                ]
-                                break
+        streets = []
+        # sitemap1
+        links = bs(session.get(sitemap1).text, "lxml").text.strip().split("\n")
+        for link in links:
+            session.get(link, headers=_headers)
+            locations = session.post(payload_url, headers=_headers).json()[
+                "searchResults"
+            ]
+            total += len(locations)
+            logger.info(f"[total {total}] {len(locations)} locations")
+            for _ in locations:
+                if _["locationLine1Address"] in streets:
+                    continue
+                streets.append(_["locationLine1Address"])
 
-                    total += len(locations)
-                    logger.info(
-                        f"[total {total}] [{_state}] [{city}] {len(locations)} locations"
-                    )
-                    for _ in locations:
-                        page_url = f"https://www.wellsfargo.com/locator/bank/?slindex={_['index']}"
-                        yield SgRecord(
-                            page_url=page_url,
-                            store_number=_["index"],
-                            location_name=_["branchName"],
-                            street_address=_["locationLine1Address"],
-                            city=_["city"],
-                            state=_["state"],
-                            zip_postal=_["postalcode"],
-                            country_code="US",
-                            latitude=_["latitude"],
-                            location_type=_["locationType"],
-                            longitude=_["longitude"],
-                            phone=_["phone"],
-                            locator_domain=locator_domain,
-                            hours_of_operation="; ".join(_.get("arrDriveUpHours", [])),
-                        )
+                page_url = (
+                    f"https://www.wellsfargo.com/locator/bank/?slindex={_['index']}"
+                )
+                hours_of_operation = "; ".join(_.get("arrDailyEvents", []))
+                if (
+                    "incidentMessage" in _
+                    and _["incidentMessage"].get("incidentDesc", "").lower()
+                    == "temporary closure"
+                    and _["incidentMessage"].get("outletStatusDesc", "").lower()
+                    == "closed"
+                ):
+                    hours_of_operation = "Temporary closed"
+                yield SgRecord(
+                    page_url=page_url,
+                    location_name=_["branchName"],
+                    street_address=_["locationLine1Address"],
+                    city=_["city"],
+                    state=_["state"],
+                    zip_postal=_["postalcode"],
+                    country_code="US",
+                    latitude=_["latitude"],
+                    location_type=_["locationType"],
+                    longitude=_["longitude"],
+                    phone=_["phone"].strip(),
+                    locator_domain=locator_domain,
+                    hours_of_operation=hours_of_operation,
+                )
+
+        # sitemap2
+        links = (
+            bs(session.get(sitemap2, headers=_headers).text, "lxml")
+            .text.strip()
+            .split("\n")
+        )
+        for link in links:
+            res = session.get(link, headers=_headers)
+            if "error.html" in res.url:
+                continue
+            sp1 = bs(res.text, "lxml")
+            location_type = sp1.select_one("div.fn.heading").text.strip()
+            if "ATM" not in location_type:
+                continue
+            if sp1.find("", string=re.compile(r"could not find")):
+                continue
+            logger.info(link)
+            try:
+                coord = (
+                    sp1.select_one("div.mapView img")["src"]
+                    .split("Road/")[1]
+                    .split("/")[0]
+                    .split(",")
+                )
+            except:
+                coord = ["", ""]
+            hours = []
+            _hr = sp1.find("h2", string=re.compile(r"Lobby Hours", re.IGNORECASE))
+            if _hr:
+                hours = list(_hr.find_next_sibling().stripped_strings)
+            addr = [
+                aa for aa in list(sp1.address.stripped_strings) if aa.strip() != ","
+            ]
+            street_address = " ".join(addr[1].split(",")[:-1])
+            if street_address in streets:
+                continue
+            streets.append(street_address)
+            yield SgRecord(
+                page_url=link,
+                location_name=addr[0],
+                street_address=street_address,
+                city=addr[1].split(",")[-1],
+                state=addr[2],
+                zip_postal=addr[3],
+                country_code="US",
+                latitude=coord[0],
+                longitude=coord[1],
+                location_type=location_type,
+                phone=sp1.find("a", href=re.compile(r"tel:")).text.strip(),
+                locator_domain=locator_domain,
+                hours_of_operation="; ".join(hours),
+            )
 
 
 if __name__ == "__main__":
