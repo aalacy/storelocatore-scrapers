09081c2971de5d066154079e088b93e79f2ed5a6
idev-a <ideveloper003@protonmail.com>
2021-04-22 11:40:28 -0700

    rewrite

--- apify/idev-a/storelocator/sleepoutfitters_com/scrape.py
+++ apify/idev-a/storelocator/sleepoutfitters_com/scrape.py
@@ -1,122 +1,49 @@
-from sgscrape.simple_scraper_pipeline import SimpleScraperPipeline
-from sgscrape.simple_scraper_pipeline import ConstantField
-from sgscrape.simple_scraper_pipeline import MappingField
-from sgscrape.simple_scraper_pipeline import MultiMappingField
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
+from sgrequests import SgRequests
 import json
-from sglogging import sglog
-from sgselenium import SgFirefox
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import time
 
-
-logzilla = sglog.SgLogSetup().get_logger(logger_name="sleepoutfitter")
+_headers = {
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
+}
 
 
 def fetch_data():
-    url = "https://stores.sleepoutfitters.com/"
-    son = []
-    with SgFirefox() as driver:
-        logzilla.info(f"Opening {url}")  # noqa
-        driver.get(url)
-        logzilla.info(f"Waiting for requests to load")  # noqa
-        time.sleep(5)
-        footer = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located(
-                (By.XPATH, '//*[@id="sls-locations-list-scroller"]/footer/nav')
-            )
+    locator_domain = "https://www.sleepoutfitters.com/"
+    base_url = "https://www.sleepoutfitters.com/store-locator"
+    with SgRequests() as session:
+        locations = json.loads(
+            session.get(base_url, headers=_headers)
+            .text.split("jsonLocations:")[1]
+            .split("imageLocations:")[0]
+            .strip()[:-1]
         )
-        # https://stackoverflow.com/questions/53571352/how-to-scroll-down-on-my-page-through-selenium-and-python
-        driver.execute_script("return arguments[0].scrollIntoView(true);", footer)
-        time.sleep(3)
-        driver.execute_script("return arguments[0].scrollIntoView(true);", footer)
-        time.sleep(3)
-        driver.execute_script("return arguments[0].scrollIntoView(true);", footer)
-        time.sleep(3)
-        driver.execute_script("return arguments[0].scrollIntoView(true);", footer)
-        time.sleep(3)
-        driver.execute_script("return arguments[0].scrollIntoView(true);", footer)
-        time.sleep(5)
-        logzilla.info(f"Looking for API calls")  # noqa
-        for r in driver.requests:
-            if "locations-details" in r.path and r.response:
-                try:
-                    son.append(json.loads(r.response.body))
-                except Exception:
-                    continue
-
-    for i in son:
-        for j in i["features"]:
-            yield j
-
-    logzilla.info(f"Finished grabbing data!!")  # noqa
-
-
-def pretty_hours(x):
-    x = (
-        x.replace("]]", "; ")
-        .replace("'", "")
-        .replace("[", "")
-        .replace("]", "")
-        .replace(",", "")
-        .replace("}", "")
-        .replace("{", "")
-    )
-    return x
-
-
-def fix_address(x):
-    x = x.replace("None", "")
-    h = []
-    for i in x.split(","):
-        if len(i) > 2:
-            h.append(i)
-    return ", ".join(h)
-
-
-def scrape():
-    url = "https://stores.sleepoutfitters.com/"
-    field_defs = SimpleScraperPipeline.field_definitions(
-        locator_domain=ConstantField(url),
-        page_url=MappingField(
-            mapping=["properties", "slug"], value_transform=lambda x: url + x
-        ),
-        location_name=MappingField(mapping=["properties", "name"]),
-        latitude=MappingField(mapping=["geometry", "coordinates", 1]),
-        longitude=MappingField(mapping=["geometry", "coordinates", 0]),
-        street_address=MultiMappingField(
-            mapping=[["properties", "addressLine1"], ["properties", "addressLine2"]],
-            multi_mapping_concat_with=", ",
-            value_transform=fix_address,
-        ),
-        city=MappingField(mapping=["properties", "city"]),
-        state=MappingField(mapping=["properties", "province"]),
-        zipcode=MappingField(mapping=["properties", "postalCode"]),
-        country_code=MappingField(mapping=["properties", "country"]),
-        phone=MappingField(mapping=["properties", "phoneNumber"]),
-        store_number=MappingField(
-            mapping=["properties", "id"], part_of_record_identity=True
-        ),
-        hours_of_operation=MappingField(
-            mapping=["properties", "hoursOfOperation"], value_transform=pretty_hours
-        ),
-        location_type=MappingField(
-            mapping=["properties", "isPermanentlyClosed"],
-            value_transform=lambda x: "<MISSING>"
-            if x == "False"
-            else "isPermanentlyClosed",
-        ),
-    )
-
-    pipeline = SimpleScraperPipeline(
-        scraper_name="sleepoutfitters.com",
-        data_fetcher=fetch_data,
-        field_definitions=field_defs,
-        log_stats_interval=15,
-    )
-    pipeline.run()
+        for _ in locations["items"]:
+            hours = []
+            for day, hh in json.loads(_["schedule_string"]).items():
+                times = "closed"
+                if hh[f"{day}_status"]:
+                    times = f"{hh['from']['hours']}:{hh['from']['minutes']}-{hh['to']['hours']}:{hh['to']['minutes']}"
+                hours.append(f"{day}: {times}")
+            yield SgRecord(
+                page_url=_["seo_url"],
+                store_number=_["id"],
+                location_name=_["name"],
+                street_address=_["address"],
+                city=_["city"],
+                state=_["region_code"],
+                zip_postal=_["zip"],
+                latitude=_["lat"],
+                longitude=_["lng"],
+                country_code=_["country"],
+                phone=_["phone"],
+                locator_domain=locator_domain,
+                hours_of_operation="; ".join(hours),
+            )
 
 
 if __name__ == "__main__":
-    scrape()
+    with SgWriter() as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
