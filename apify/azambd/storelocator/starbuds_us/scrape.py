4505d0615fa4ca684a2e885be547563de0ff2b61
Azam <azam@wscraper.com>
2021-04-23 08:02:25 +0600

    SLC-12459: update location_type

--- apify/azambd/storelocator/starbuds_us/scrape.py
+++ apify/azambd/storelocator/starbuds_us/scrape.py
@@ -7,9 +7,10 @@ from sgscrape.sgrecord import SgRecord
 from sgrequests import SgRequests
 from sglogging import sglog
 
-# F
-MISSING = "<MISSING>"
-website = "https://www.starbuds.us"
+#FA
+
+MISSING = '<MISSING>'
+website = 'https://www.starbuds.us'
 session = SgRequests().requests_retry_session()
 log = sglog.SgLogSetup().get_logger(logger_name=website)
 
@@ -17,7 +18,7 @@ log = sglog.SgLogSetup().get_logger(logger_name=website)
 def fetchNodeText(node):
     results = []
     for i, text_line in enumerate(node):
-        span_text = [x.strip() for x in text_line.xpath(".//text()")]
+        span_text = [x.strip() for x in text_line.xpath('.//text()')]
         results.append(span_text)
     return results
 
@@ -26,33 +27,28 @@ def getFeatureData(url):
     response = session.get(url)
     body = html.fromstring(response.text, "lxml")
     featuredJsonLink = body.xpath(
-        '//link[contains(@id, "features_") and @id != "features_masterPage"]/@href'
-    )[0]
+        '//link[contains(@id, "features_") and @id != "features_masterPage"]/@href')[0]
     response = session.get(featuredJsonLink)
     return {"data": json.loads(response.text), "body": body}
 
 
 def fetchLocationStores(url):
-    compProps = getFeatureData(url)["data"]["props"]["render"]["compProps"]
+    compProps = getFeatureData(url)["data"]['props']['render']['compProps']
     storeLinks = []
     for key in compProps.keys():
-        if (
-            "label" in compProps[key]
-            and compProps[key]["label"] == "VIEW HOURS, SPECIALS AND MENU"
-        ):
-            storeLinks.append(compProps[key]["link"]["href"])
+        if 'label' in compProps[key] and compProps[key]['label'] == 'VIEW HOURS, SPECIALS AND MENU':
+            storeLinks.append(compProps[key]['link']['href'])
     return storeLinks
 
 
 def fetchStoreLinks():
-    compProps = getFeatureData(website + "/locations")["data"]["props"]["render"][
-        "compProps"
-    ]
+    compProps = getFeatureData(
+        website + '/locations')["data"]['props']['render']['compProps']
     storeLinks = []
     for key in compProps.keys():
-        if "skin" in compProps[key] and compProps[key]["skin"] == "BasicButton":
-            url = compProps[key]["link"]["href"]
-            if "LOCATIONS" in compProps[key]["label"]:
+        if 'skin' in compProps[key] and compProps[key]['skin'] == 'BasicButton':
+            url = compProps[key]['link']['href']
+            if 'LOCATIONS' in compProps[key]['label']:
                 for link in fetchLocationStores(url):
                     if link not in storeLinks:
                         storeLinks.append(link)
@@ -67,24 +63,19 @@ def fetchStoreDetails(link):
     body = featuredData["body"]
     data = featuredData["data"]
 
-    title = (
-        (" ".join(body.xpath("//h1/span/span/text()")))
-        .replace("Coming Soon!", "")
-        .replace("&nbsp;", " ")
-        .strip()
-    )
+    title = (' '.join(body.xpath('//h1/span/span/text()'))
+             ).replace('Coming Soon!', '').replace('&nbsp;', ' ').strip()
 
     phone = body.xpath('//a[contains(@href, "tel:")]/span/span/text()')
     if len(phone) == 0:
         phone = body.xpath('//a[contains(@href, "tel:")]/span/text()')
         if len(phone) == 0:
             phone = body.xpath(
-                '//span[contains(text(), "(") and contains(text(), ")")]/text()'
-            )
+                '//span[contains(text(), "(") and contains(text(), ")")]/text()')
             if len(phone) == 0:
                 phone = [MISSING]
 
-    phone = phone[0].replace("tel:", "").strip()
+    phone = phone[0].replace('tel:', '').strip()
 
     operations = []
     hoursP = body.xpath("//span[text()='HOURS']")
@@ -93,23 +84,20 @@ def fetchStoreDetails(link):
         dayParts = fetchNodeText(parentDiv)
         for dayPart in dayParts[1:]:
             operations.append("".join(dayPart))
+
+    location_type = MISSING
     if len(operations) == 0:
         phone = MISSING
-
+    if phone == MISSING:
+        location_type = 'coming soon'
     location = {}
 
-    compProps = data["props"]["render"]["compProps"]
+    compProps = data['props']['render']['compProps']
     for key in compProps.keys():
-        if "mapData" in compProps[key]:
-            location = compProps[key]["mapData"]["locations"][0]
-    store_number = data["props"]["seo"]["pageId"]
-    return {
-        "title": title,
-        "phone": phone,
-        "operations": ", ".join(operations),
-        "location": location,
-        "store_number": store_number,
-    }
+        if 'mapData' in compProps[key]:
+            location = compProps[key]['mapData']['locations'][0]
+    store_number = data['props']['seo']['pageId']
+    return {'title': title, 'phone': phone, 'operations': ', '.join(operations), 'location': location, 'store_number': store_number, 'location_type': location_type}
 
 
 def fetchData():
@@ -119,22 +107,23 @@ def fetchData():
     for link in storeLinks:
         log.info(f"Scrapping {link} ...")
         details = fetchStoreDetails(link)
-        title = details["title"]
-        phone = details["phone"]
-        operations = details["operations"]
-        store_number = details["store_number"]
-        location = details["location"]
+        title = details['title']
+        phone = details['phone']
+        operations = details['operations']
+        store_number = details['store_number']
+        location = details['location']
+        location_type = details['location_type']
 
-        locationParts = location["address"].split(", ")
-        if " " not in locationParts[2]:
+        locationParts = location['address'].split(", ")
+        if ' ' not in locationParts[2]:
             locationParts[2] = locationParts[2] + " " + MISSING
-        country_code = "US"
-        if locationParts[3] != "USA":
+        country_code = 'US'
+        if locationParts[3] != 'USA':
             continue
 
         city = locationParts[1]
-        state = locationParts[2].split(" ")[0]
-        zip_postal = str(locationParts[2].split(" ")[1])
+        state = locationParts[2].split(' ')[0]
+        zip_postal = str(locationParts[2].split(' ')[1])
         street_address = locationParts[0]
         yield SgRecord(
             locator_domain=website,
@@ -147,9 +136,10 @@ def fetchData():
             country_code=country_code,
             store_number=store_number,
             phone=phone,
-            latitude=str(location["latitude"]),
-            longitude=str(location["longitude"]),
+            latitude=str(location['latitude']),
+            longitude=str(location['longitude']),
             hours_of_operation=operations,
+            location_type=location_type,
         )
 
 
@@ -157,8 +147,8 @@ def scrape():
     log.info("Crawling Started")
     count = 0
     start = time.time()
+    results = fetchData()
     with SgWriter() as writer:
-        results = fetchData()
         for rec in results:
             writer.write_row(rec)
             count = count + 1
