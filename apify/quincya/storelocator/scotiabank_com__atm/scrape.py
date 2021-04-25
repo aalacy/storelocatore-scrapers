fcaa45f225baed791cb4e6119207d5105bd3fb87
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-04-25 21:00:40 +0200

    SLC-12162
    https://safegraph-crawl.atlassian.net/browse/SLC-12162

--- apify/quincya/storelocator/scotiabank_com__atm/scrape.py
+++ apify/quincya/storelocator/scotiabank_com__atm/scrape.py
@@ -2,6 +2,7 @@ import csv
 from sgrequests import SgRequests
 from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
 from sglogging import SgLogSetup
+import json
 
 logger = SgLogSetup().get_logger("scotiabank_com")
 
@@ -40,7 +41,7 @@ def fetch_data():
 
     session = SgRequests()
 
-    max_results = 20
+    max_results = 50
     max_distance = None
     dup_tracker = []
     total = 0
@@ -51,28 +52,35 @@ def fetch_data():
         max_radius_miles=max_distance,
         max_search_results=max_results,
     )
-
     for lat, lng in search:
         base_link = (
-            "https://mapsms.scotiabank.com/branches?1=1&latitude=%s&longitude=%s&recordlimit=%s&locationtypes=3"
-            % (lat, lng, max_results)
+            "https://mapsms.scotiabank.com/branches?1=1&latitude=%s&longitude=%s&recordlimit=%s&locationtypes=1,2,3"
+            % (lat, lng, "50")
         )
         try:
             stores = session.get(base_link, headers=headers).json()["branchInfo"][
                 "marker"
             ]
-        except:
+        except Exception as e:
+            print(session.get(base_link, headers=headers).text)
+            print(e)
             continue
         logger.info(f"Pulling the data from: {base_link}")
         found = 0
+        print(len(stores))
         for store in stores:
             store_number = store["@attributes"]["id"]
+            latitude = store["@attributes"]["lat"]
+            longitude = store["@attributes"]["lng"]
+
+            search.found_location_at(latitude, longitude)
             if store_number in dup_tracker:
                 continue
             dup_tracker.append(store_number)
 
             location_name = store["name"]
-            location_type = "<MISSING>"
+            location_type = "Branch" if store["@attributes"]["type"] == "1" else "ABM" if store["@attributes"]["type"] == "3" else store["@attributes"]["type"]
+            print(location_type)
             street_address = " ".join(store["address"].split(",")[:-3])
             city = store["address"].split(",")[-3].strip()
             if len(store["address"].split(",")[-2].split()) > 1:
@@ -100,10 +108,6 @@ def fetch_data():
                 hours_of_operation = "<MISSING>"
             if hours_of_operation.count("Closed") == 7:
                 hours_of_operation = "<MISSING>"
-            latitude = store["@attributes"]["lat"]
-            longitude = store["@attributes"]["lng"]
-
-            search.found_location_at(latitude, longitude)
 
             # Store data
             yield [
