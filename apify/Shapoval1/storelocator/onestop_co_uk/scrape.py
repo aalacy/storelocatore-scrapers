e9a1c5604c5119ddcc32b5dfb01b9ac867cd9a45
Shapoval1 <shshipovald@mail.ru>
2021-04-25 20:51:40 +0300

    add onestop_co_uk

--- apify/Shapoval1/storelocator/onestop_co_uk/scrape.py
+++ apify/Shapoval1/storelocator/onestop_co_uk/scrape.py
@@ -4,6 +4,7 @@ from concurrent import futures
 from sgrequests import SgRequests
 from sgzip.static import static_zipcode_list, SearchableCountries
 
+
 def write_output(data):
     with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
         writer = csv.writer(
@@ -42,35 +43,55 @@ def get_data(_zip):
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
     }
 
-    zip_bytes = _zip.encode('ascii')
+    zip_bytes = _zip.encode("ascii")
     base64_bytes = base64.b64encode(zip_bytes)
-    base64_zip = base64_bytes.decode('ascii')
+    base64_zip = base64_bytes.decode("ascii")
 
     data = {
         "action": "findstockists",
         "searchfor": str(base64_zip),
     }
     session = SgRequests()
-    r = session.post('https://www.onestop.co.uk/wp-admin/admin-ajax.php', headers=headers, data=data)
+    r = session.post(
+        "https://www.onestop.co.uk/wp-admin/admin-ajax.php", headers=headers, data=data
+    )
     js = r.json()
-    if 'locations' in js['message']:
-        for loc in js['message']['locations']:
+    if "locations" in js["message"]:
+        for loc in js["message"]["locations"]:
 
-            location_name = loc.get('location').get('name') or '<MISSING>'
-            print(location_name)
+            location_name = loc.get("location").get("name") or "<MISSING>"
             store_number = "<MISSING>"
             country_code = "GB"
-            state = '<MISSING>'
-            street_address = loc.get('location').get('address').get('details').get('lines')[0]
-            if len(loc.get('location').get('address').get('details').get('lines')) == 2:
-                street_address = street_address + ' ' + loc.get('location').get('address').get('details').get('lines')[1]
-            city = loc.get('location').get('address').get('details').get('postTown') or '<MISSING>'
+            state = "<MISSING>"
+            street_address = (
+                loc.get("location").get("address").get("details").get("lines")[0]
+            )
+            if len(loc.get("location").get("address").get("details").get("lines")) == 2:
+                street_address = (
+                    street_address
+                    + " "
+                    + loc.get("location").get("address").get("details").get("lines")[1]
+                )
+            city = (
+                loc.get("location").get("address").get("details").get("postTown")
+                or "<MISSING>"
+            )
 
-            postal = loc.get('location').get('address').get('details').get('postcode') or '<MISSING>'
-            phone = loc.get('location').get('contact').get('phoneNumbers').get('main') or '<MISSING>'
-            latitude = loc.get('location').get('coordinates').get('latitude') or '<MISSING>'
-            longitude = loc.get('location').get('coordinates').get('longitude') or '<MISSING>'
-            location_type = loc.get('location').get('types')[0] or '<MISSING>'
+            postal = (
+                loc.get("location").get("address").get("details").get("postcode")
+                or "<MISSING>"
+            )
+            phone = (
+                loc.get("location").get("contact").get("phoneNumbers").get("main")
+                or "<MISSING>"
+            )
+            latitude = (
+                loc.get("location").get("coordinates").get("latitude") or "<MISSING>"
+            )
+            longitude = (
+                loc.get("location").get("coordinates").get("longitude") or "<MISSING>"
+            )
+            location_type = loc.get("location").get("types")[0] or "<MISSING>"
             try:
                 monday_hrs = f"Monday: {loc.get('location').get('openingHours').get('standard').get('monday').get('intervals')[0].get('start')} - {loc.get('location').get('openingHours').get('standard').get('monday').get('intervals')[0].get('end')}"
                 tuesday_hrs = f"Tuesday: {loc.get('location').get('openingHours').get('standard').get('tuesday').get('intervals')[0].get('start')} - {loc.get('location').get('openingHours').get('standard').get('tuesday').get('intervals')[0].get('end')}"
@@ -80,27 +101,53 @@ def get_data(_zip):
                 saturday_hrs = f"Saturday: {loc.get('location').get('openingHours').get('standard').get('saturday').get('intervals')[0].get('start')} - {loc.get('location').get('openingHours').get('standard').get('saturday').get('intervals')[0].get('end')}"
                 sunday_hrs = f"Sunday: {loc.get('location').get('openingHours').get('standard').get('sunday').get('intervals')[0].get('start')} - {loc.get('location').get('openingHours').get('standard').get('sunday').get('intervals')[0].get('end')}"
             except AttributeError:
-                monday_hrs = '<MISSING>'
-                tuesday_hrs = '<MISSING>'
-                wednesday_hrs = '<MISSING>'
-                thursday_hrs = '<MISSING>'
-                friday_hrs = '<MISSING>'
-                saturday_hrs = '<MISSING>'
-                sunday_hrs = '<MISSING>'
+                monday_hrs = "<MISSING>"
+                tuesday_hrs = "<MISSING>"
+                wednesday_hrs = "<MISSING>"
+                thursday_hrs = "<MISSING>"
+                friday_hrs = "<MISSING>"
+                saturday_hrs = "<MISSING>"
+                sunday_hrs = "<MISSING>"
 
-            hours_of_operation = monday_hrs + ' ' + tuesday_hrs + ' ' + wednesday_hrs + ' ' + thursday_hrs + ' ' + friday_hrs + ' ' + saturday_hrs + ' ' + sunday_hrs or '<MISSING>'
-            if hours_of_operation.count('<MISSING>') == 7:
-                hours_of_operation = '<MISSING>'
+            hours_of_operation = (
+                monday_hrs
+                + " "
+                + tuesday_hrs
+                + " "
+                + wednesday_hrs
+                + " "
+                + thursday_hrs
+                + " "
+                + friday_hrs
+                + " "
+                + saturday_hrs
+                + " "
+                + sunday_hrs
+                or "<MISSING>"
+            )
+            if hours_of_operation.count("<MISSING>") == 7:
+                hours_of_operation = "<MISSING>"
             page_url = f"https://www.onestop.co.uk/store/?store={loc.get('location').get('id')}"
 
-
-
-            row = [locator_domain, page_url, location_name, street_address, city, state, postal,
-                   country_code, store_number, phone, location_type, latitude, longitude, hours_of_operation]
+            row = [
+                locator_domain,
+                page_url,
+                location_name,
+                street_address,
+                city,
+                state,
+                postal,
+                country_code,
+                store_number,
+                phone,
+                location_type,
+                latitude,
+                longitude,
+                hours_of_operation,
+            ]
             rows.append(row)
 
     return rows
-
 
 
 def fetch_data():
@@ -109,7 +156,9 @@ def fetch_data():
     postals = static_zipcode_list(radius=20, country_code=SearchableCountries.BRITAIN)
 
     with futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_url = {executor.submit(get_data, postal): postal for postal in postals}
+        future_to_url = {
+            executor.submit(get_data, postal): postal for postal in postals
+        }
         for future in futures.as_completed(future_to_url):
             rows = future.result()
             try:
