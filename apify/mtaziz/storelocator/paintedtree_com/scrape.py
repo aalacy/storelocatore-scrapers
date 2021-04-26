ce85817f5e1f87c0a9e92e44f02058088abe32f5
mtaziz <mdtariqaziz@gmail.com>
2021-04-26 19:12:42 +0600

    scraper updated

--- apify/mtaziz/storelocator/paintedtree_com/scrape.py
+++ apify/mtaziz/storelocator/paintedtree_com/scrape.py
@@ -177,117 +177,124 @@ def get_latlng_and_other_fields():
 
 def fetch_data():
     latlng = get_latlng_and_other_fields()
-    driver = SgChrome(is_headless=True).driver()
-    driver.get(base_url)
-    iframe = driver.find_element_by_xpath('//iframe[@title="Located Map"]')
-    driver.switch_to.frame(iframe)
-    time.sleep(40)
-    data_iframe = html.fromstring(driver.page_source)
-    xpath_stores = '//div[contains(@id, "card") and contains(@class, "card")]'
-    data_stores = data_iframe.xpath(xpath_stores)
-    for idx, pd in enumerate(data_stores):
-        locator_domain = DOMAIN
+    logger.info("Pulling the data from iframe")
+    with SgChrome(
+        is_headless=True,
+    ) as driver:
+        driver.get(base_url)
+        time.sleep(60)
+        logger.info("Trying to get the data from SgChrome response")
+        iframe = driver.find_element_by_xpath('//iframe[@title="Located Map"]')
+        driver.switch_to.frame(iframe)
+        logger.info("Switched to iframe with SUCCESS ")
+        time.sleep(30)
+        data_iframe = html.fromstring(driver.page_source)
+        xpath_stores = '//div[contains(@id, "card") and contains(@class, "card")]'
+        data_stores = data_iframe.xpath(xpath_stores)
+        logger.info(f"Data object from iframe: {data_stores}")
+        for idx, pd in enumerate(data_stores):
+            locator_domain = DOMAIN
 
-        # Page URL
-        page_url = MISSING
-        logger.info(
-            f"{idx} out of {len(data_stores)} Stores][Pulling the data from: {page_url} "
-        )
-
-        # Location Name
-        locname = "".join(pd.xpath(".//div/h5/text()")).strip()
-
-        location_name = locname.split(",")[0].strip()
-        location_name = location_name if location_name else MISSING
-        logger.info(f"[Location Name: {location_name}]")
-
-        address_from_map = "".join(pd.xpath(".//div[1]/div/text()")).strip()
-        logger.info(f"Raw Address: {address_from_map}")
-
-        # Street Address
-        ra_scp = address_from_map.split(",")
-        street_address = ra_scp[0].strip()
-        street_address = street_address if street_address else MISSING
-        logger.info(f"[Street Address: {street_address}]")
-
-        # City
-        try:
-            city = ra_scp[1].strip()
-        except Exception:
-            city = MISSING
-        logger.info(f"[City: {city}]")
-
-        # State
-        state = ra_scp[2].strip()
-        state = state if city else MISSING
-        logger.info(f"[State: {state}]")
-
-        # Zipcode
-        zip_postal = ra_scp[3].strip()
-        zip_postal = zip_postal if zip_postal else MISSING
-        logger.info(f"[Zip: {zip_postal}]")
-
-        # Country Code
-        country_code = "US"
-
-        # Store Number
-        store_number = ""
-        store_number = store_number if store_number else MISSING
-        logger.info(f"[Store Number: {store_number}]")
-
-        # Phone
-        phone = "".join(pd.xpath('.//a[contains(@href, "tel:")]//text()')).strip()
-        phone = phone if phone else MISSING
-
-        # Location Type
-        location_type = MISSING
-
-        # Latitude
-        for ll in latlng:
-            if location_name.lower() in ll["location_name"].lower():
-                lat = ll["latitude"]
-                lng = ll["longitude"]
-
-            else:
-                continue
-        latitude = lat
-        logger.info(f"[Latitude: {latitude}]")
-
-        # Longitude
-        longitude = lng
-        logger.info(f"[Longitude: {longitude}]")
-
-        # Hours of operation
-        hoo = "".join(
-            pd.xpath(
-                './/div[div[contains(@class, "far fa-clock col-1 mt-1")]]//div/text()'
+            # Page URL
+            page_url = MISSING
+            logger.info(
+                f"{idx} out of {len(data_stores)} Stores][Pulling the data from: {page_url} "
             )
-        ).strip()
-        hours_of_operation = hoo if hoo else MISSING
-        logger.info(f"[Hours of Operation: {hours_of_operation}]")
 
-        # Raw Address
-        raw_address = address_from_map
-        raw_address = raw_address if raw_address else MISSING
-        logger.info(f"[Raw Address: {raw_address}]")
+            # Location Name
+            locname = "".join(pd.xpath(".//div/h5/text()")).strip()
 
-        yield SgRecord(
-            locator_domain=locator_domain,
-            page_url=page_url,
-            location_name=location_name,
-            street_address=street_address,
-            city=city,
-            state=state,
-            zip_postal=zip_postal,
-            country_code=country_code,
-            store_number=store_number,
-            phone=phone,
-            location_type=location_type,
-            latitude=latitude,
-            longitude=longitude,
-            hours_of_operation=hours_of_operation,
-            raw_address=raw_address,
-        )
+            location_name = locname.split(",")[0].strip()
+            location_name = location_name if location_name else MISSING
+            logger.info(f"[Location Name: {location_name}]")
+
+            address_from_map = "".join(pd.xpath(".//div[1]/div/text()")).strip()
+            logger.info(f"Raw Address: {address_from_map}")
+
+            # Street Address
+            ra_scp = address_from_map.split(",")
+            street_address = ra_scp[0].strip()
+            street_address = street_address if street_address else MISSING
+            logger.info(f"[Street Address: {street_address}]")
+
+            # City
+            try:
+                city = ra_scp[1].strip()
+            except Exception:
+                city = MISSING
+            logger.info(f"[City: {city}]")
+
+            # State
+            state = ra_scp[2].strip()
+            state = state if city else MISSING
+            logger.info(f"[State: {state}]")
+
+            # Zipcode
+            zip_postal = ra_scp[3].strip()
+            zip_postal = zip_postal if zip_postal else MISSING
+            logger.info(f"[Zip: {zip_postal}]")
+
+            # Country Code
+            country_code = "US"
+
+            # Store Number
+            store_number = ""
+            store_number = store_number if store_number else MISSING
+            logger.info(f"[Store Number: {store_number}]")
+
+            # Phone
+            phone = "".join(pd.xpath('.//a[contains(@href, "tel:")]//text()')).strip()
+            phone = phone if phone else MISSING
+
+            # Location Type
+            location_type = MISSING
+
+            # Latitude
+            for ll in latlng:
+                if location_name.lower() in ll["location_name"].lower():
+                    lat = ll["latitude"]
+                    lng = ll["longitude"]
+
+                else:
+                    continue
+            latitude = lat
+            logger.info(f"[Latitude: {latitude}]")
+
+            # Longitude
+            longitude = lng
+            logger.info(f"[Longitude: {longitude}]")
+
+            # Hours of operation
+            hoo = "".join(
+                pd.xpath(
+                    './/div[div[contains(@class, "far fa-clock col-1 mt-1")]]//div/text()'
+                )
+            ).strip()
+            hours_of_operation = hoo if hoo else MISSING
+            logger.info(f"[Hours of Operation: {hours_of_operation}]")
+
+            # Raw Address
+            raw_address = address_from_map
+            raw_address = raw_address if raw_address else MISSING
+            logger.info(f"[Raw Address: {raw_address}]")
+
+            yield SgRecord(
+                locator_domain=locator_domain,
+                page_url=page_url,
+                location_name=location_name,
+                street_address=street_address,
+                city=city,
+                state=state,
+                zip_postal=zip_postal,
+                country_code=country_code,
+                store_number=store_number,
+                phone=phone,
+                location_type=location_type,
+                latitude=latitude,
+                longitude=longitude,
+                hours_of_operation=hours_of_operation,
+                raw_address=raw_address,
+            )
 
 
 def scrape():
