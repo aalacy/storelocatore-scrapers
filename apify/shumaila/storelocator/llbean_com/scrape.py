ee4d65552f7b36ce6b67489ef094dfd7c1e3840f
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2021-05-01 21:05:10 +0500

    Update scrape.py

--- apify/shumaila/storelocator/llbean_com/scrape.py
+++ apify/shumaila/storelocator/llbean_com/scrape.py
@@ -1,109 +1,101 @@
-# https://zoeskitchen.com/locations/search?location=WI
-# https://www.llbean.com/llb/shop/1000001703?nav=gn-hp
-
-
-import requests
 from bs4 import BeautifulSoup
-import csv
-import string
-import re, time
-from sglogging import SgLogSetup
+import csv, re
+from sgrequests import SgRequests
 
-logger = SgLogSetup().get_logger('llbean_com')
-
-
+session = SgRequests()
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
+}
 
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", mode="w") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
         # Header
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
 
 
 def fetch_data():
-    # Your scraper here
-
     data = []
-    p = 1
-    pattern = re.compile(r'\s\s+')
-    url = 'https://www.llbean.com/llb/shop/1000001703?nav=gn-hp'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
-    maindiv = soup.find('div', {'id':'storeLocatorZone'})
-    link_list = maindiv.findAll('a')
-    logger.info(len(link_list))
+    p = 0
+    pattern = re.compile(r"\s\s+")
+    url = "https://www.llbean.com/llb/shop/1000001703?nav=gn-"
+    r = session.get(url, headers=headers, verify=False)
+    soup = BeautifulSoup(r.text, "html.parser")
+    link_list = soup.find("div", {"id": "storeLocatorZone"}).findAll("a")
     for alink in link_list:
         if alink.text.find(":") == -1 or alink.find("Freeport") == -1:
-            link = "https://www.llbean.com" + alink['href']
-            logger.info(link)
-            page1 = requests.get(link)
-            soup1 = BeautifulSoup(page1.text, "html.parser")
-            title = soup1.find('h1').text
-            phone = soup1.find('li',{'class','phone'}).text
-            phone = re.sub(pattern,"",phone)
-            street = soup1.find('span',{'class':'street-address'}).text
-            city = soup1.find('em', {'class': 'locality'}).text
-            state = soup1.find('abbr', {'class': 'region'}).text
-            pcode = soup1.find('em', {'class': 'postal-code'}).text
-            start = link.find("shop")
-            start = link.find("/", start) + 1
-            store = link[start:len(link)]
-            hours = soup1.find('div',{'class': 'row item-holder'}).text
-            hours = re.sub(pattern," ",hours)
-            soup1 = str(soup1)
-            start = soup1.find("var latitude")
-            start = soup1.find("=", start) + 3
-            end = soup1.find(';', start)
-            lat = soup1[start:end]
-            start = soup1.find("var longitude")
-            start = soup1.find("=", start) + 3
-            end = soup1.find(';', start)
-            longt = soup1[start:end]
-            hours = hours.replace("\n", "")
+            link = "https://www.llbean.com" + alink["href"]
+            r = session.get(link, headers=headers, verify=False)
+            soup = BeautifulSoup(r.text, "html.parser")
+            title = soup.find("h1").text
+            phone = soup.find("li", {"class", "phone"}).text.strip()
+            street = soup.find("span", {"class": "street-address"}).text
+            city = soup.find("em", {"class": "locality"}).text
+            state = soup.find("abbr", {"class": "region"}).text
+            pcode = soup.find("em", {"class": "postal-code"}).text
+            store = link.split("/")[-1]
+            hours = (
+                soup.find("ul", {"class": "hoursActive"}).text.replace("\n", "").strip()
+            )
+            hours = re.sub(pattern, " ", hours).strip()
+
+            lat = r.text.split("var latitude", 1)[1].split("=", 1)[1].split(";", 1)[0]
+
+            longt = (
+                r.text.split("var longitude", 1)[1].split("=", 1)[1].split(";", 1)[0]
+            )
+
             if len(hours) < 3:
                 hours = "<MISSING>"
-
-            #logger.info(title)
-            #logger.info(store)
-            #logger.info(street)
-            #logger.info(city)
-            #logger.info(state)
-            #logger.info(pcode)
-            #logger.info(phone)
-            #logger.info(lat)
-            #logger.info(longt)
-            #logger.info(hours)
-            logger.info(p)
+            data.append(
+                [
+                    "https://www.llbean.com",
+                    link,
+                    title,
+                    street,
+                    city,
+                    state,
+                    pcode,
+                    "US",
+                    store,
+                    phone,
+                    "flagship, Bike, Boat & Ski, hunting and fishing",
+                    lat,
+                    longt,
+                    hours,
+                ]
+            )
             p += 1
-            data.append([
-                'https://www.llbean.com',
-                link,
-                title,
-                street,
-                city,
-                state,
-                pcode,
-                'US',
-                store,
-                phone,
-                "flagship, Bike, Boat & Ski, hunting and fishing",
-                lat,
-                longt,
-                hours
-            ])
-
     return data
-
 
 
 def scrape():
     data = fetch_data()
     write_output(data)
 
-scrape()
 
+scrape()
