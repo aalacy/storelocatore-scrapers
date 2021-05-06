5f337e7f39c9c9bd5deab457d9343553b13d2207
datarhan <slutchenko@gmail.com>
2021-05-06 15:21:21 +0200

    [SLC-13178] Add crawler for chimasteakhouse.com

---
+++ apify/datarhan/storelocator/chimasteakhouse_com/scrape.py
@@ -0,0 +1,119 @@
+import re
+import csv
+import json
+from lxml import etree
+
+from sgrequests import SgRequests
+
+
+def write_output(data):
+    with open("data.csv", mode="w", encoding="utf-8") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
+
+        # Header
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
+        # Body
+        for row in data:
+            writer.writerow(row)
+
+
+def fetch_data():
+    # Your scraper here
+    session = SgRequests().requests_retry_session(retries=2, backoff_factor=0.3)
+
+    items = []
+
+    start_url = "https://www.chimasteakhouse.com/"
+    domain = re.findall(r"://(.+?)/", start_url)[0].replace("www.", "")
+    hdr = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
+    }
+    response = session.get(start_url, headers=hdr)
+    dom = etree.HTML(response.text)
+
+    all_locations = dom.xpath('//script[contains(text(), "PostalAddress")]/text()')
+    for poi in all_locations:
+        poi = json.loads(poi)
+
+        location_name = poi["name"]
+        location_name = location_name if location_name else "<MISSING>"
+        street_address = poi["address"]["streetAddress"]
+        street_address = street_address if street_address else "<MISSING>"
+        city = poi["address"]["addressLocality"]
+        city = city if city else "<MISSING>"
+        if "Corner-Vienna" in city:
+            city = "Tyson's Corner-Vienna"
+        state = poi["address"]["addressRegion"]
+        state = state if state else "<MISSING>"
+        zip_code = poi["address"]["postalCode"]
+        zip_code = zip_code if zip_code else "<MISSING>"
+        country_code = "<MISSING>"
+        store_number = "<MISSING>"
+        phone = poi["telephone"]
+        phone = phone if phone else "<MISSING>"
+        location_type = poi["@type"]
+
+        store_url = f'https://www.chimasteakhouse.com/{city.lower().replace(" ", "-")}-{state.lower()}/'
+        if "corner-vienna-va" in store_url:
+            store_url = "https://www.chimasteakhouse.com/tysons-corner-va/"
+        print(store_url)
+        loc_response = session.get(store_url, headers=hdr)
+        loc_dom = etree.HTML(loc_response.text)
+        geo = (
+            loc_dom.xpath("//iframe/@src")[-1]
+            .split("!1d")[-1]
+            .split("!3f")[0]
+            .split("!2d")
+        )
+        latitude = geo[0]
+        longitude = geo[1]
+        hours_of_operation = "<MISSING>"
+
+        item = [
+            domain,
+            store_url,
+            location_name,
+            street_address,
+            city,
+            state,
+            zip_code,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
+
+        items.append(item)
+
+    return items
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
+
+
+if __name__ == "__main__":
+    scrape()
