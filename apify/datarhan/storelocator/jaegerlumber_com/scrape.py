43f3b0e83a675d479428cb05980c070acb8929be
mtaziz <mdtariqaziz@gmail.com>
2021-05-08 20:31:54 +0600

    ssl certficate verify fixed

--- apify/datarhan/storelocator/jaegerlumber_com/scrape.py
+++ apify/datarhan/storelocator/jaegerlumber_com/scrape.py
@@ -4,6 +4,16 @@ from sgselenium import SgChrome
 import time
 import re
 import csv
+import ssl
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 
 logger = SgLogSetup().get_logger("jaegerlumber_com")
