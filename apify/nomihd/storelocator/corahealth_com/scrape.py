d023b922e2e75f82d715ad7e90a3227840cdb673
nomihd <nouman.awais.nu@gmail.com>
2021-05-09 02:39:46 +0500

    remove unused variable

--- apify/nomihd/storelocator/corahealth_com/scrape.py
+++ apify/nomihd/storelocator/corahealth_com/scrape.py
@@ -60,8 +60,6 @@ def write_output(data):
 
 def fetch_data():
     # Your scraper here
-    loc_list = []
-
     search_url = "https://www.coraphysicaltherapy.com/wpsl_stores-sitemap.xml"
     stores_req = session.get(search_url, headers=headers)
     stores = stores_req.text.split("<loc>")
