febe920f7c06a9eb1ddd451f62edd98af44f2506
Shapoval1 <shshipovald@mail.ru>
2021-05-12 14:05:33 +0300

    fix towerhealthurgentcare_org

--- apify/Shapoval1/storelocator/towerhealthurgentcare_org/scrape.py
+++ apify/Shapoval1/storelocator/towerhealthurgentcare_org/scrape.py
@@ -35,7 +35,7 @@ def write_output(data):
 def fetch_data():
     out = []
 
-    locator_domain = "https://towerhealth.org"
+    locator_domain = "https://towehealthurgentcare.org"
 
     for i in range(3):
 
