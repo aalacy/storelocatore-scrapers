143646165dec4583ece05838fff398138d3babb9
Shapoval1 <shshipovald@mail.ru>
2021-05-18 12:14:24 +0300

    fix dicksfreshmarket_com

--- apify/Shapoval1/storelocator/dicksfreshmarket_com/scrape.py
+++ apify/Shapoval1/storelocator/dicksfreshmarket_com/scrape.py
@@ -38,32 +38,32 @@ def fetch_data():
     api_url = "https://dicksfreshmarket.com/ajax/index.php"
     session = SgRequests()
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
-        'Referer': 'https://dicksfreshmarket.com/contact',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
+        "Referer": "https://dicksfreshmarket.com/contact",
     }
 
     data = {
-        "method": 'POST',
-        "apiurl": 'https://dicksfreshmarket.rsaamerica.com/Services/SSWebRestApi.svc/GetClientStores/1'
+        "method": "POST",
+        "apiurl": "https://dicksfreshmarket.rsaamerica.com/Services/SSWebRestApi.svc/GetClientStores/1",
     }
 
     r = session.post(api_url, headers=headers, data=data)
     js = r.json()
-    for j in js['GetClientStores']:
+    for j in js["GetClientStores"]:
 
         page_url = "https://dicksfreshmarket.com/contact"
-        location_name = j.get('ClientStoreName')
+        location_name = j.get("ClientStoreName")
         location_type = "<MISSING>"
-        street_address = j.get('AddressLine1')
-        phone = j.get('StorePhoneNumber')
-        state = j.get('StateName')
-        postal = j.get('ZipCode')
+        street_address = j.get("AddressLine1")
+        phone = j.get("StorePhoneNumber")
+        state = j.get("StateName")
+        postal = j.get("ZipCode")
         country_code = "US"
-        city = j.get('City')
-        store_number = j.get('StoreNumber')
-        latitude = j.get('Latitude')
-        longitude = j.get('Longitude')
-        hours_of_operation = j.get('StoreTimings')
+        city = j.get("City")
+        store_number = j.get("StoreNumber")
+        latitude = j.get("Latitude")
+        longitude = j.get("Longitude")
+        hours_of_operation = j.get("StoreTimings")
 
         row = [
             locator_domain,
