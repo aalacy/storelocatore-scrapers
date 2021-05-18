318ca24e8042c62024ff6e4d10ffb8d31985a9ed
datarhan <slutchenko@gmail.com>
2021-05-18 18:30:54 +0200

    [SLC-13643] maintenance

--- apify/datarhan/storelocator/healthtrax_com/scrape.py
+++ apify/datarhan/storelocator/healthtrax_com/scrape.py
@@ -56,7 +56,10 @@ def fetch_data():
     )
     for store_url in all_locations:
         store_url = urljoin(start_url, store_url)
+        print(store_url)
         loc_response = session.get(store_url, headers=hdr)
+        if loc_response.status_code != 200:
+            continue
         loc_dom = etree.HTML(loc_response.text)
         geo = loc_dom.xpath("//div/@data-block-json")
         if geo:
