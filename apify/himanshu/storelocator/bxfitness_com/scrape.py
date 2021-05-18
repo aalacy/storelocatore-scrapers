21717f9e8df971e422e99be6fe72a7cd49e1845d
Shapoval1 <shshipovald@mail.ru>
2021-05-18 09:34:33 +0300

    fix himanshu's bxfitness.com

--- apify/himanshu/storelocator/bxfitness_com/scrape.py
+++ apify/himanshu/storelocator/bxfitness_com/scrape.py
@@ -1,51 +1,70 @@
 import csv
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup
-import re
-import json
 
 
 session = SgRequests()
 
-def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-        # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
-        # Body
+def write_output(data):
+    with open("data.csv", mode="w", newline="") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
+
+        writer.writerow(
+            [
+                "locator_domain",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+                "page_url",
+            ]
+        )
+
         for row in data:
             writer.writerow(row)
 
+
 def fetch_data():
     headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
     }
-    base_url = "https://bxfitness.com"
-    r = session.get("https://bxfitness.com",headers=headers)
-    soup = BeautifulSoup(r.text,"lxml")
-    
-    for location in soup.find_all("ul",{"class":"sub-menu"})[2].find_all("a"):
-        
-        location_request = session.get(location["href"],headers=headers)
-        location_soup = BeautifulSoup(location_request.text,"lxml")
-        
-        hours = " ".join(list(location_soup.find("div",{'class':"hours-data"}).stripped_strings))
-        address = list(location_soup.find("div",{"class":'club-address'}).stripped_strings)
-        
-        
-        geo_location = location_soup.find_all("iframe")[0]["src"]
-       
+
+    r = session.get("https://bxfitness.com", headers=headers)
+    soup = BeautifulSoup(r.text, "lxml")
+
+    for location in soup.find_all("ul", {"class": "sub-menu"})[2].find_all("a"):
+        location_request = session.get(location["href"], headers=headers)
+        location_soup = BeautifulSoup(location_request.text, "lxml")
+
+        hours = " ".join(
+            list(location_soup.find("div", {"class": "hours-data"}).stripped_strings)
+        )
+        address = list(
+            location_soup.find("div", {"class": "club-address"}).stripped_strings
+        )
+
+        geo_location = location_soup.find_all("iframe")[1]["src"]
         name = location_soup.find("h1").text
-        
+
         store = []
         store.append("https://bxfitness.com")
         store.append(name)
         store.append(address[1])
         store.append(address[2].split(",")[0])
-        store.append(address[2].split(",")[1].replace("\xa0"," ").split(" ")[-2])
-        store.append(address[2].split(",")[1].replace("\xa0"," ").split(" ")[-1])
+        store.append(address[2].split(",")[1].replace("\xa0", " ").split(" ")[-2])
+        store.append(address[2].split(",")[1].replace("\xa0", " ").split(" ")[-1])
         store.append("US")
         store.append("<MISSING>")
         store.append(address[-1])
@@ -56,8 +75,10 @@ def fetch_data():
         store.append(location["href"])
         yield store
 
+
 def scrape():
     data = fetch_data()
     write_output(data)
+
 
 scrape()
