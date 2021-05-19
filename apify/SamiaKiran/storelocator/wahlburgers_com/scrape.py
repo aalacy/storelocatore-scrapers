aef75d01ceb8755009716f31c1d14ec28198af53
Samia Kiran <samiakiran17@gmail.com>
2021-05-19 16:59:03 +0500

    [SLC-13296] Add QA requested changes.
    https://safegraph-crawl.atlassian.net/browse/SLC-13296

--- apify/SamiaKiran/storelocator/wahlburgers_com/scrape.py
+++ apify/SamiaKiran/storelocator/wahlburgers_com/scrape.py
@@ -1,3 +1,4 @@
+import usaddress
 from sglogging import sglog
 from bs4 import BeautifulSoup
 from sgrequests import SgRequests
@@ -59,22 +60,49 @@ def fetch_data():
                     del temp[-1]
                 if len(temp) > 3:
                     temp = temp[1:]
-                phone = temp[0].find("a")["href"].replace("tel:", "")
-                address = temp[1].get_text(separator="|", strip=True).split("|")[1:]
+                phone = temp[0].find("a")["href"].replace("tel:", "").replace("%20", "")
                 hours_of_operation = (
                     temp[2].get_text(separator="|", strip=True).replace("|", " ")
+                ).replace("Opening hours:", "")
+                address = temp[1].get_text(separator="|", strip=True).split("|")[1:]
+                address = " ".join(
+                    x.replace("\n", "").replace("    ", " ") for x in address
                 )
-                street_address = address[0]
-                address = address[1].split("\n")
-                city = address[0].replace(",", "")
-                state = address[1]
-                zip_postal = address[2]
+                raw_address = address
+                address = usaddress.parse(address)
+
+                i = 0
+                street_address = ""
+                city = ""
+                state = ""
+                zip_postal = ""
+                while i < len(address):
+                    temp = address[i]
+                    if (
+                        temp[1].find("Address") != -1
+                        or temp[1].find("Street") != -1
+                        or temp[1].find("Recipient") != -1
+                        or temp[1].find("Occupancy") != -1
+                        or temp[1].find("BuildingName") != -1
+                        or temp[1].find("USPSBoxType") != -1
+                        or temp[1].find("USPSBoxID") != -1
+                    ):
+                        street_address = street_address + " " + temp[0]
+                    if temp[1].find("PlaceName") != -1:
+                        city = city + " " + temp[0]
+                    if temp[1].find("StateName") != -1:
+                        state = state + " " + temp[0]
+                    if temp[1].find("ZipCode") != -1:
+                        zip_postal = zip_postal + " " + temp[0]
+                    i += 1
                 longitude, latitude = (
                     soup.select_one("iframe[src*=maps]")["src"]
                     .split("!2d", 1)[1]
                     .split("!3m", 1)[0]
                     .split("!3d")
                 )
+                if "!2m" in latitude:
+                    latitude = latitude.split("!2m")[0]
                 yield SgRecord(
                     locator_domain="https://wahlburgers.com/",
                     page_url=page_url,
