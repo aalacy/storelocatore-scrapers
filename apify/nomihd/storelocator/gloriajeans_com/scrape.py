6c58e5cd2b77b7db25972109b5ea4e5997424744
nomihd <nouman.awais.nu@gmail.com>
2021-05-19 19:33:23 +0500

    add ssl for sgselenium

--- apify/nomihd/storelocator/gloriajeans_com/scrape.py
+++ apify/nomihd/storelocator/gloriajeans_com/scrape.py
@@ -6,6 +6,16 @@ import us
 import lxml.html
 from sgselenium import SgChrome
 import time
+import ssl
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 website = "gloriajeans.com"
 log = sglog.SgLogSetup().get_logger(logger_name=website)
