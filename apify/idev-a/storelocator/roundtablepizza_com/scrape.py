1998cc768d4d4457fdec2eae3702f60c59552067
idev-a <ideveloper003@protonmail.com>
2021-05-21 02:26:06 -0700

    fix

--- apify/idev-a/storelocator/roundtablepizza_com/scrape.py
+++ apify/idev-a/storelocator/roundtablepizza_com/scrape.py
@@ -4,6 +4,16 @@ from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
 from sglogging import SgLogSetup
 from bs4 import BeautifulSoup as bs
 from sgselenium import SgChrome
+import ssl
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 logger = SgLogSetup().get_logger("roundtablepizza")
 
