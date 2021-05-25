8b8e45c8f9a30afcab5b255affd32731a0430976
idev-a <ideveloper003@protonmail.com>
2021-05-24 23:57:13 -0700

    fix

--- apify/idev-a/storelocator/newleafcannabis_ca/scrape.py
+++ apify/idev-a/storelocator/newleafcannabis_ca/scrape.py
@@ -2,8 +2,6 @@ from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup as bs
-from sgselenium import SgFirefox
-import time
 import json
 from sglogging import SgLogSetup
 
@@ -13,7 +11,28 @@ _headers = {
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
 }
 
-days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+states = {
+    "A": "NL",
+    "B": "NS",
+    "C": "PE",
+    "E": "NB",
+    "G": "QC",
+    "H": "QC",
+    "J": "QC",
+    "K": "ON",
+    "L": "ON",
+    "M": "ON",
+    "N": "ON",
+    "P": "ON",
+    "R": "MB",
+    "S": "SK",
+    "T": "AB",
+    "V": "BC",
+    "X": "NU/NT",
+    "Y": "YT",
+}
 
 
 def fetch_data():
@@ -27,6 +46,7 @@ def fetch_data():
             .split("var coming_soon_store_data =")[0]
             .strip()[1:-2]
         )
+        logger.info(f"{len(locations)} found")
         with SgFirefox() as driver:
             for _ in locations:
                 address = list(bs(_["address"], "lxml").stripped_strings)
@@ -37,44 +57,22 @@ def fetch_data():
                         times = "closed"
                     hours.append(f"{days[int(x)]}: {times}")
 
-                page_url = _["shop_now_button_url"]
-                if not page_url.endswith("/"):
-                    page_url += "/"
-
-                driver.get(page_url)
-                logger.info(page_url)
-                exist = False
-                while not exist:
-                    time.sleep(1)
-                    for rr in driver.requests[::-1]:
-                        if rr.url.startswith(json_url) and rr.response:
-                            exist = True
-                            logger.info("found =======")
-                            locations = session.get(rr.url).json()
-                            addr = locations["data"]["filteredDispensaries"][0][
-                                "location"
-                            ]
-                            street_address = addr["ln1"]
-                            if addr["ln2"]:
-                                street_address += ", " + addr["ln2"]
-                            del rr
-                            yield SgRecord(
-                                page_url=_["shop_now_button_url"],
-                                store_number=_["id"],
-                                location_name=_["name"],
-                                street_address=street_address,
-                                city=_["city"],
-                                state=addr["state"],
-                                latitude=_["latitude"],
-                                longitude=_["longitude"],
-                                zip_postal=" ".join(address[1].split(" ")[1:]),
-                                country_code="CA",
-                                phone=_["phone_number"],
-                                locator_domain=locator_domain,
-                                hours_of_operation="; ".join(hours),
-                            )
-
-                            break
+                zip_postal = " ".join(address[1].split(" ")[1:])
+                yield SgRecord(
+                    page_url=_["shop_now_button_url"],
+                    store_number=_["id"],
+                    location_name=_["name"],
+                    street_address=address[0].replace("–", "-"),
+                    city=_["city"],
+                    state=states[zip_postal[0].upper()],
+                    latitude=_["latitude"],
+                    longitude=_["longitude"],
+                    zip_postal=zip_postal,
+                    country_code="CA",
+                    phone=_["phone_number"],
+                    locator_domain=locator_domain,
+                    hours_of_operation="; ".join(hours).replace("–", "-"),
+                )
 
 
 if __name__ == "__main__":
