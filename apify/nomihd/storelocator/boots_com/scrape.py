a5c0285e8bd2d2c77abab092a38fa6831ff94042
extrasauceontop <msuchemba@gmail.com>
2021-05-26 15:27:56 -0400

    scrape

--- apify/nomihd/storelocator/boots_com/scrape.py
+++ apify/nomihd/storelocator/boots_com/scrape.py
@@ -4,6 +4,7 @@ from sgrequests import SgRequests
 from sglogging import sglog
 import lxml.html
 from tenacity import retry, stop_after_attempt
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 website = "boots.com"
 log = sglog.SgLogSetup().get_logger(logger_name=website)
@@ -71,125 +72,134 @@ def fetch_data():
     stores_req = session.get(search_url, headers=headers)
     stores_sel = lxml.html.fromstring(stores_req.text)
     stores = stores_sel.xpath('//div[@class="brand_list_viewer"]//ul/li/a/@href')
+    print(len(stores))
 
-    for store_url in stores:
-        page_url = "https://www.boots.com" + store_url
-        log.info(page_url)
-        locator_domain = website
+    loc_list = scrape_loc_urls(stores)
+    return loc_list
 
-        store_req = get_page(page_url)
-        store_sel = lxml.html.fromstring(store_req.text)
-        location_name = "".join(
-            store_sel.xpath('//h2[@class="store_name"]/text()')
-        ).strip()
-        if location_name == "":
-            location_name = "<MISSING>"
-            continue
 
-        sections = store_sel.xpath('//dl[@class="store_info_list"]')
-        for sec in sections:
-            if (
-                "Address"
-                in "".join(
-                    sec.xpath('dt[@class="store_info_list_label"]/text()')
-                ).strip()
-            ):
+def scrape_loc_urls(stores):
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(parallel_run, store_url) for store_url in stores]
+        for future in as_completed(futures):
+            try:
+                record = future.result()
+                if record:
+                    yield record
+            except Exception:
+                continue
 
-                street_address = "".join(
-                    sec.xpath('dd[@class="store_info_list_item"][1]/text()')
+
+def parallel_run(store_url):
+    page_url = "https://www.boots.com" + store_url
+    log.info(page_url)
+    locator_domain = website
+
+    store_req = get_page(page_url)
+    store_sel = lxml.html.fromstring(store_req.text)
+    location_name = "".join(store_sel.xpath('//h2[@class="store_name"]/text()')).strip()
+    if location_name == "":
+        location_name = "<MISSING>"
+        return None
+
+    sections = store_sel.xpath('//dl[@class="store_info_list"]')
+    for sec in sections:
+        if (
+            "Address"
+            in "".join(sec.xpath('dt[@class="store_info_list_label"]/text()')).strip()
+        ):
+
+            street_address = "".join(
+                sec.xpath('dd[@class="store_info_list_item"][1]/text()')
+            ).strip()
+            city = "".join(
+                sec.xpath('dd[@class="store_info_list_item"][2]/text()')
+            ).strip()
+            state = "".join(
+                sec.xpath('dd[@class="store_info_list_item"][3]/text()')
+            ).strip()
+            zip = "".join(
+                sec.xpath('dd[@class="store_info_list_item"][4]/text()')
+            ).strip()
+        break
+
+    country_code = "".join(
+        store_sel.xpath('//input[@id="storeCountryCode"]/@value')
+    ).strip()
+
+    if street_address == "":
+        street_address = "<MISSING>"
+
+    if city == "":
+        city = "<MISSING>"
+
+    if state == "":
+        state = "<MISSING>"
+
+    if zip == "":
+        zip = "<MISSING>"
+
+    store_number = "".join(
+        store_sel.xpath('//input[@name="bootsStoreId"]/@value')
+    ).strip()
+    phone = "".join(
+        store_sel.xpath('//a[@name="Store telephone number"]/text()')
+    ).strip()
+
+    location_type = "<MISSING>"
+    temp_hours = store_sel.xpath('//table[@class="store_opening_hours "]')
+    hours_of_operation = ""
+    hours_list = []
+    for temp in temp_hours:
+        if (
+            "Store:"
+            in "".join(
+                temp.xpath('thead/tr/th[@class="store_hours_heading"]/text()')
+            ).strip()
+        ):
+            hours = temp.xpath("tbody/tr")
+            for hour in hours:
+                day = "".join(hour.xpath('td[@class="store_hours_day"]/text()')).strip()
+                time = "".join(
+                    hour.xpath('td[@class="store_hours_time"]/text()')
                 ).strip()
-                city = "".join(
-                    sec.xpath('dd[@class="store_info_list_item"][2]/text()')
-                ).strip()
-                state = "".join(
-                    sec.xpath('dd[@class="store_info_list_item"][3]/text()')
-                ).strip()
-                zip = "".join(
-                    sec.xpath('dd[@class="store_info_list_item"][4]/text()')
-                ).strip()
+                hours_list.append(day + ":" + time)
             break
 
-        country_code = "".join(
-            store_sel.xpath('//input[@id="storeCountryCode"]/@value')
-        ).strip()
+    hours_of_operation = ";".join(hours_list).strip()
 
-        if street_address == "":
-            street_address = "<MISSING>"
+    latitude = "".join(store_sel.xpath('//input[@id="lat"]/@value')).strip()
+    longitude = "".join(store_sel.xpath('//input[@id="lon"]/@value')).strip()
 
-        if city == "":
-            city = "<MISSING>"
+    if latitude == "":
+        latitude = "<MISSING>"
+    if longitude == "":
+        longitude = "<MISSING>"
 
-        if state == "":
-            state = "<MISSING>"
+    if hours_of_operation == "":
+        hours_of_operation = "<MISSING>"
+    if phone == "":
+        phone = "<MISSING>"
 
-        if zip == "":
-            zip = "<MISSING>"
-
-        store_number = "".join(
-            store_sel.xpath('//input[@name="bootsStoreId"]/@value')
-        ).strip()
-        phone = "".join(
-            store_sel.xpath('//a[@name="Store telephone number"]/text()')
-        ).strip()
-
-        location_type = "<MISSING>"
-        temp_hours = store_sel.xpath('//table[@class="store_opening_hours "]')
-        hours_of_operation = ""
-        hours_list = []
-        for temp in temp_hours:
-            if (
-                "Store:"
-                in "".join(
-                    temp.xpath('thead/tr/th[@class="store_hours_heading"]/text()')
-                ).strip()
-            ):
-                hours = temp.xpath("tbody/tr")
-                for hour in hours:
-                    day = "".join(
-                        hour.xpath('td[@class="store_hours_day"]/text()')
-                    ).strip()
-                    time = "".join(
-                        hour.xpath('td[@class="store_hours_time"]/text()')
-                    ).strip()
-                    hours_list.append(day + ":" + time)
-                break
-
-        hours_of_operation = ";".join(hours_list).strip()
-
-        latitude = "".join(store_sel.xpath('//input[@id="lat"]/@value')).strip()
-        longitude = "".join(store_sel.xpath('//input[@id="lon"]/@value')).strip()
-
-        if latitude == "":
-            latitude = "<MISSING>"
-        if longitude == "":
-            longitude = "<MISSING>"
-
-        if hours_of_operation == "":
-            hours_of_operation = "<MISSING>"
-        if phone == "":
-            phone = "<MISSING>"
-
-        if store_number != "":
-            curr_list = [
-                locator_domain,
-                page_url,
-                location_name,
-                street_address,
-                city,
-                state,
-                zip,
-                country_code,
-                store_number,
-                phone,
-                location_type,
-                latitude,
-                longitude,
-                hours_of_operation,
-            ]
-            loc_list.append(curr_list)
-        # break
-
-    return loc_list
+    if store_number != "":
+        curr_list = [
+            locator_domain,
+            page_url,
+            location_name,
+            street_address,
+            city,
+            state,
+            zip,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
+        print("SUCCESS")
+        return curr_list
 
 
 def scrape():
