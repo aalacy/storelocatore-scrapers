b174acd592401f38111753ca57b88b718036adfa
Ihor <rzhdgqrr@gmail.com>
2021-05-31 13:48:09 +0300

    fix

--- apify/MrQwerty1/storelocator/kristoil_com/scrape.py
+++ apify/MrQwerty1/storelocator/kristoil_com/scrape.py
@@ -39,13 +39,13 @@ def fetch_data():
     page_url = "https://kristoil.com/locations/"
     api_url = "https://kristoil.com/wp-content/themes/krist-2020/ajax/map.php"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Referer': 'https://kristoil.com/locations/',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Connection': 'keep-alive',
-        'TE': 'Trailers',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Referer": "https://kristoil.com/locations/",
+        "X-Requested-With": "XMLHttpRequest",
+        "Connection": "keep-alive",
+        "TE": "Trailers",
     }
 
     session = SgRequests()
@@ -61,13 +61,23 @@ def fetch_data():
         _id = phone[-4:]
         a = j.get("location")
 
-        li = tree.xpath(f"//li[@class='grid grid--locations grid--one-col-mobile locations-list__list-item' and .//a[contains(text(), '-{_id}')]]")[0]
-        city = ''.join(li.xpath("./text()")).replace('.', '').strip()
-        line = ''.join(li.xpath(".//li[@class='locations-list__phone-number']/preceding-sibling::li[1]/text()")).replace('.', '').strip()
-        street_address = line.rsplit(city, 1)[0].replace(',', '').strip()
-        sz = line.rsplit(city, 1)[-1].replace(',', '').strip()
+        li = tree.xpath(
+            f"//li[@class='grid grid--locations grid--one-col-mobile locations-list__list-item' and .//a[contains(text(), '-{_id}')]]"
+        )[0]
+        city = "".join(li.xpath("./text()")).replace(".", "").strip()
+        line = (
+            "".join(
+                li.xpath(
+                    ".//li[@class='locations-list__phone-number']/preceding-sibling::li[1]/text()"
+                )
+            )
+            .replace(".", "")
+            .strip()
+        )
+        street_address = line.rsplit(city, 1)[0].replace(",", "").strip()
+        sz = line.rsplit(city, 1)[-1].replace(",", "").strip()
         state = sz.split()[0]
-        postal = sz.replace(state, '') or '<MISSING>'
+        postal = sz.replace(state, "") or "<MISSING>"
         country_code = "US"
         latitude = a.get("lat") or "<MISSING>"
         longitude = a.get("lng") or "<MISSING>"
