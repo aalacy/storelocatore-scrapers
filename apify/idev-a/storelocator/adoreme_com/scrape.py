835851d101b00acd63f382cd295809932f37dc8d
idev-a <ideveloper003@protonmail.com>
2021-05-31 15:15:15 -0700

    fix

--- apify/idev-a/storelocator/adoreme_com/scrape.py
+++ apify/idev-a/storelocator/adoreme_com/scrape.py
@@ -6,6 +6,16 @@ from sglogging import SgLogSetup
 import re
 from datetime import datetime
 from sgselenium import SgChrome
+import ssl
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 logger = SgLogSetup().get_logger("adoreme")
 
