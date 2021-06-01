b72ff18291831431685767fabfb7c49bf37a1715
idev-a <ideveloper003@protonmail.com>
2021-05-31 23:13:54 -0700

    fix

--- apify/idev-a/storelocator/orrstown_com/scrape.py
+++ apify/idev-a/storelocator/orrstown_com/scrape.py
@@ -45,7 +45,13 @@ def fetch_data():
                 if hours == ["By appointment only"]:
                     hours = []
             try:
-                coord = link.iframe["src"].split("!2d")[1].split("!3m")[0].split("!3d")
+                coord = (
+                    link.iframe["src"]
+                    .split("!2d")[1]
+                    .split("!3m")[0]
+                    .split("!2m")[0]
+                    .split("!3d")
+                )
             except:
                 coord = ["", ""]
             location_type = "branch"
