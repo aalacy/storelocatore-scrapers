bf8fa4efd0c32146d2171bca94e56bab558fadbc
idev-a <ideveloper003@protonmail.com>
2021-06-09 11:14:05 -0700

    fix

--- apify/idev-a/storelocator/mkmbs_co_uk/scrape.py
+++ apify/idev-a/storelocator/mkmbs_co_uk/scrape.py
@@ -1,6 +1,5 @@
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
-from sgrequests import SgRequests
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
 import json
