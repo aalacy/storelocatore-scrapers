a3f00f33477144640c2827e0e03c59b37fcea5a6
datarhan <slutchenko@gmail.com>
2021-06-18 11:24:35 +0200

    [SLC-15049] re type

--- apify/datarhan/storelocator/eastlink_ca/scrape.py
+++ apify/datarhan/storelocator/eastlink_ca/scrape.py
@@ -47,7 +47,7 @@ def fetch_data():
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
     response = session.get(start_url, headers=hdr)
-    data = re.findall("locationsJson = (.+)\['public_locations", response.text)[0]
+    data = re.findall(r"locationsJson = (.+)\['public_locations", response.text)[0]
     data = json.loads(data)
 
     all_locations = data["public_locations"]
