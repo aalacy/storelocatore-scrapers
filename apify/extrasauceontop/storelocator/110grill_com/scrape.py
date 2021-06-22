6b290ceeda95d845120cb2e376b60c5f35659743
extrasauceontop <msuchemba@gmail.com>
2021-06-22 12:29:53 -0400

    scrape

--- apify/extrasauceontop/storelocator/110grill_com/scrape.py
+++ apify/extrasauceontop/storelocator/110grill_com/scrape.py
@@ -1,6 +1,3 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from sgselenium.sgselenium import SgChrome
 from webdriver_manager.chrome import ChromeDriverManager
 import json
