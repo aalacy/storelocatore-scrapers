a16f666fc79a238e8ae0d8e64aa3ef02c0a7e06b
datarhan <slutchenko@gmail.com>
2021-06-25 12:34:33 +0200

    [SLC-15396] Add spider for zara.com

--- apify/coralisland/storelocator/zara_com/scrape.py
+++ apify/coralisland/storelocator/zara_com/scrape.py
@@ -1,142 +1,109 @@
 import csv
-import re
-from sgrequests import SgRequests
 import json
-import usaddress
-import sgzip
-from sgzip import DynamicGeoSearch, SearchableCountries
+
+from sgrequests import SgRequests
+from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
+
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", mode="w", encoding="utf-8") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
         # Header
-        writer.writerow(["locator_domain", "page_url", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
 
 
-def validate(item):
-    if item == None:
-        item = ''
-    if type(item) == int or type(item) == float:
-        item = str(item)
-    if type(item) == list:
-        item = ' '.join(item)
-    return item.replace('\u2013', '-').strip()
-
-def get_value(item):
-    if item == None :
-        item = '<MISSING>'
-    item = validate(item)
-    if item == '':
-        item = '<MISSING>'    
-    return item
-
-def eliminate_space(items):
-    rets = []
-    for item in items:
-        item = validate(item)
-        if item != '':
-            rets.append(item)
-    return rets
-
-def parse_address(address):
-    address = usaddress.parse(address)
-    street = ''
-    city = ''
-    state = ''
-    zipcode = ''
-    for addr in address:
-        if addr[1] == 'PlaceName':
-            city += addr[0].replace(',', '') + ' '
-        elif addr[1] == 'ZipCode':
-            zipcode = addr[0].replace(',', '')
-        elif addr[1] == 'StateName':
-            state = addr[0].replace(',', '') + ' '
-        else:
-            street += addr[0].replace(',', '') + ' '
-    return { 
-        'street': get_value(street), 
-        'city' : get_value(city), 
-        'state' : get_value(state), 
-        'zipcode' : get_value(zipcode)
-    }
-
 def fetch_data():
-    output_list = []
-    history = []
+    # Your scraper here
+    items = []
+    scraped_items = []
 
     session = SgRequests()
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36'
-    headers = {'User-Agent' : user_agent}
 
-    search = sgzip.DynamicGeoSearch(country_codes=[SearchableCountries.USA, SearchableCountries.CANADA])
+    DOMAIN = "zara.com"
 
-    search.initialize()
+    hdr = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+    }
+    all_locations = []
+    all_coordinates = DynamicGeoSearch(
+        country_codes=SearchableCountries.WITH_COORDS_ONLY, max_radius_miles=30
+    )
+    for lat, lng in all_coordinates:
+        url = f"https://www.zara.com/uk/en/stores-locator/search?lat={lat}&lng={lng}&isGlobalSearch=true&showOnlyPickup=false&isDonationOnly=false&ajax=true"
+        response = session.get(url, headers=hdr)
+        data = json.loads(response.text)
+        all_locations += data
 
-    coord = search.next()
+    for poi in all_locations:
+        store_url = "<MISSING>"
+        location_name = poi["addressLines"][0]
+        street_address = poi["addressLines"][0]
+        city = poi["city"]
+        city = city if city else "<MISSING>"
+        state = poi["state"]
+        zip_code = poi["zipCode"]
+        zip_code = zip_code if zip_code else "<MISSING>"
+        country_code = poi["countryCode"]
+        store_number = poi["id"]
+        phone = poi["phones"]
+        phone = phone[0] if phone else "<MISSING>"
+        location_type = poi["datatype"]
+        latitude = poi["latitude"]
+        latitude = latitude if latitude else "<MISSING>"
+        longitude = poi["longitude"]
+        longitude = longitude if longitude else "<MISSING>"
+        hours_of_operation = "<MISSING>"
 
-    while coord:
-        x = coord[0]
-        y = coord[1]
+        item = [
+            DOMAIN,
+            store_url,
+            location_name,
+            street_address,
+            city,
+            state,
+            zip_code,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
+        if store_number not in scraped_items:
+            scraped_items.append(store_number)
+            items.append(item)
 
-        url = "https://www.zara.com/us/en/stores-locator/search?lat="+str(x)+"&lng="+str(y)+"&isGlobalSearch=true&showOnlyPickup=false&isDonationOnly=false&ajax=true"
-        
-        result_coords = []
-        result_coords.append([x, y])
-    
-        request = session.get(url, headers=headers)
-        store_list = json.loads(request.text)
-        for store in store_list:
-            store_id = validate(store.get('id'))
-            if store_id not in history:
-                history.append(store_id)
-                output = []
-                output.append("zara.com") # url
-                output.append("<MISSING>") # page url
-                output.append(get_value(store.get('name'))) #location name
-                output.append(get_value(store.get('addressLines'))) #address
-                output.append(get_value(store.get('city'))) #city
-                output.append(get_value(store.get('state'))) #state
-                output.append(get_value(store.get('zipCode'))) #zipcode
-                country = get_value(store.get('countryCode'))
-                if country not in ["US","CA"]:
-                    continue
-                output.append(country) #country code
-                output.append(get_value(store_id)) #store_number
-                output.append(get_value(store.get('phones')).replace('+', '')) #phone
-                output.append("<MISSING>") #location type
+    return items
 
-                lat = get_value(store.get('latitude'))
-                longit = get_value(store.get('longitude'))
-                output.append(lat) #latitude
-                output.append(longit) #longitude
-
-                result_coords.append([lat, longit])
-                search.update_with(result_coords)
-
-                days_of_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-                store_hours = []
-                if len(store.get(('openingHours'))) > 0:
-                    for hour in store.get('openingHours'):
-                        if hour['openingHoursInterval']:
-                            store_hours.append(days_of_week[hour['weekDay']-1] + ' ' + hour['openingHoursInterval'][0]['openTime'] + '-' + hour['openingHoursInterval'][0]['closeTime'])    
-                        else:
-                            store_hours.append(days_of_week[hour['weekDay']-1] + ' closed')
-
-                output.append(get_value(store_hours)) #opening hours
-                output_list.append(output)
-            
-        if len(store_list) == 0:
-            search.update_with(result_coords)
-        coord = search.next()
-
-    return output_list
 
 def scrape():
     data = fetch_data()
     write_output(data)
 
-scrape()
+
+if __name__ == "__main__":
+    scrape()
