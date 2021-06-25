acda2d4f82d4e2f61f85e056c93f6ea88e5e3606
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-06-25 13:06:08 +0200

    SLC-15396
    https://safegraph-crawl.atlassian.net/browse/SLC-15396

--- apify/coralisland/storelocator/zara_com/scrape.py
+++ apify/coralisland/storelocator/zara_com/scrape.py
@@ -1,9 +1,9 @@
 import csv
-import json
-
+from sglogging import sglog
 from sgrequests import SgRequests
 from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
-
+logzilla = sglog.SgLogSetup().get_logger(logger_name="Scraper")
+identities = set()
 
 def write_output(data):
     with open("data.csv", mode="w", encoding="utf-8") as output_file:
@@ -33,51 +33,29 @@ def write_output(data):
         # Body
         for row in data:
             writer.writerow(row)
-
-
-def fetch_data():
-    # Your scraper here
-    items = []
-    scraped_items = []
-
-    session = SgRequests()
-
+            
+def record_transformer(poi):
     DOMAIN = "zara.com"
+    store_url = "<MISSING>"
+    location_name = poi["addressLines"][0]
+    street_address = poi["addressLines"][0]
+    city = poi["city"]
+    city = city if city else "<MISSING>"
+    state = poi["state"]
+    zip_code = poi["zipCode"]
+    zip_code = zip_code if zip_code else "<MISSING>"
+    country_code = poi["countryCode"]
+    store_number = poi["id"]
+    phone = poi["phones"]
+    phone = phone[0] if phone else "<MISSING>"
+    location_type = poi["datatype"]
+    latitude = poi["latitude"]
+    latitude = latitude if latitude else "<MISSING>"
+    longitude = poi["longitude"]
+    longitude = longitude if longitude else "<MISSING>"
+    hours_of_operation = "<MISSING>"
 
-    hdr = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
-    }
-    all_locations = []
-    all_coordinates = DynamicGeoSearch(
-        country_codes=SearchableCountries.WITH_COORDS_ONLY, max_radius_miles=30
-    )
-    for lat, lng in all_coordinates:
-        url = f"https://www.zara.com/uk/en/stores-locator/search?lat={lat}&lng={lng}&isGlobalSearch=true&showOnlyPickup=false&isDonationOnly=false&ajax=true"
-        response = session.get(url, headers=hdr)
-        data = json.loads(response.text)
-        all_locations += data
-
-    for poi in all_locations:
-        store_url = "<MISSING>"
-        location_name = poi["addressLines"][0]
-        street_address = poi["addressLines"][0]
-        city = poi["city"]
-        city = city if city else "<MISSING>"
-        state = poi["state"]
-        zip_code = poi["zipCode"]
-        zip_code = zip_code if zip_code else "<MISSING>"
-        country_code = poi["countryCode"]
-        store_number = poi["id"]
-        phone = poi["phones"]
-        phone = phone[0] if phone else "<MISSING>"
-        location_type = poi["datatype"]
-        latitude = poi["latitude"]
-        latitude = latitude if latitude else "<MISSING>"
-        longitude = poi["longitude"]
-        longitude = longitude if longitude else "<MISSING>"
-        hours_of_operation = "<MISSING>"
-
-        item = [
+    item = [
             DOMAIN,
             store_url,
             location_name,
@@ -93,15 +71,56 @@ def fetch_data():
             longitude,
             hours_of_operation,
         ]
-        if store_number not in scraped_items:
-            scraped_items.append(store_number)
-            items.append(item)
-
-    return items
+    return (item,latitude,longitude)
+    
+def search_country(session,search,hdr,SearchableCountry):
+    total = 0
+    maxZ = search.items_remaining()
+    def getPoint(point,session,hdr):
+        url = "https://www.zara.com/uk/en/stores-locator/search?lat={}&lng={}&isGlobalSearch=true&showOnlyPickup=false&isDonationOnly=false&ajax=true".format(*point)
+        data = session.get(url,headers=hdr)
+        print(data.text)
+        return data.json()
+    for Point in search:
+        found = 0
+        for poi in getPoint(Point, session, hdr):
+            record,foundLat,foundLng = record_transformer(poi)
+            search.found_location_at(
+                foundLat, foundLng
+            )
+            if str(record) not in identities:
+                identities.add(str(record))
+                found +=1
+                yield record
+        progress = str(round(100 - (search.items_remaining() / maxZ * 100), 2)) + "%"
+        total += found
+        logzilla.info(
+            f"{str(Point).replace('(','').replace(')','')}|found: {found}|total: {total}|prog: {progress}|\nRemaining: {search.items_remaining()} Searchable: {SearchableCountry}"
+        )
+        
+def fetch_data():
+    # Your scraper here
+    hdr = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+    }
+    with SgRequests() as session:
+        for SearchableCountry in SearchableCountries.WITH_COORDS_ONLY:
+            search = None
+            try:
+                search = DynamicGeoSearch(
+        country_codes=[SearchableCountry], max_radius_miles=30
+    )
+            except KeyError:
+                continue
+            if search:
+                for item in search_country(session,search,hdr,SearchableCountry):
+                    yield item
 
 
 def scrape():
-    data = fetch_data()
+    data = []
+    for item in fetch_data():
+        data.append(item)
     write_output(data)
 
 
