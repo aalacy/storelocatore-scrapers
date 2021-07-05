5cd6b2f2d697f9a9f09e02c04941ce7f6b0f5cc2
Shapoval1 <shshipovald@mail.ru>
2021-07-05 10:07:56 +0300

    fix odwlogistics_com

--- apify/Shapoval1/storelocator/odwlogistics_com/scrape.py
+++ apify/Shapoval1/storelocator/odwlogistics_com/scrape.py
@@ -1,9 +1,6 @@
 import csv
-import time
-import usaddress
 from lxml import html
 from sgrequests import SgRequests
-from sgselenium import SgSelenium
 
 
 def write_output(data):
@@ -41,91 +38,39 @@ def fetch_data():
     locator_domain = "https://www.odwlogistics.com"
     api_url = "https://www.odwlogistics.com/locations-and-reach"
     session = SgRequests()
-    tag = {
-        "Recipient": "recipient",
-        "AddressNumber": "address1",
-        "AddressNumberPrefix": "address1",
-        "AddressNumberSuffix": "address1",
-        "StreetName": "address1",
-        "StreetNamePreDirectional": "address1",
-        "StreetNamePreModifier": "address1",
-        "StreetNamePreType": "address1",
-        "StreetNamePostDirectional": "address1",
-        "StreetNamePostModifier": "address1",
-        "StreetNamePostType": "address1",
-        "CornerOf": "address1",
-        "IntersectionSeparator": "address1",
-        "LandmarkName": "address1",
-        "USPSBoxGroupID": "address1",
-        "USPSBoxGroupType": "address1",
-        "USPSBoxID": "address1",
-        "USPSBoxType": "address1",
-        "BuildingName": "address2",
-        "OccupancyType": "address2",
-        "OccupancyIdentifier": "address2",
-        "SubaddressIdentifier": "address2",
-        "SubaddressType": "address2",
-        "PlaceName": "city",
-        "StateName": "state",
-        "ZipCode": "postal",
-    }
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
     }
     r = session.get(api_url, headers=headers)
     tree = html.fromstring(r.text)
-    div = tree.xpath(
-        '//div[contains(@class, "location--teaser__facility location--teaser")]'
-    )
+    div = tree.xpath('//a[text()="Learn More"]')
     for d in div:
-        slug = "".join(d.xpath(".//a[@class='location--teaser__url link']/@href"))
-        location_name = "".join(
-            d.xpath('.//div[@class="location--teaser__name"]//text()')
-        )
-        street_address = (
-            "".join(d.xpath('.//ul[@class="location--teaser__specs"]/li[4]/text()'))
-            or "<MISSING>"
-        )
-        if street_address == "<MISSING>":
-            street_address = "".join(
-                d.xpath('.//ul[@class="location--teaser__specs"]/li[3]/text()')
-            )
-        phone = (
-            "".join(d.xpath('.//ul[@class="location--teaser__specs"]/li[5]/a/text()'))
-            or "<MISSING>"
-        )
-        if phone == "<MISSING>":
-            phone = "".join(
-                d.xpath('.//ul[@class="location--teaser__specs"]/li[4]/a/text()')
-            )
-        page_url = f"{locator_domain}{slug}"
-        driver = SgSelenium().firefox()
-        time.sleep(10)
-        driver.get(page_url)
 
-        iframe = driver.find_element_by_xpath('//iframe[contains(@src, "maps")]')
-        driver.switch_to.frame(iframe)
-        time.sleep(10)
-        ad = driver.find_element_by_xpath("//div[@class='address']").text
-        location_type = "<MISSING>"
-        a = usaddress.tag(ad, tag_mapping=tag)[0]
-        state = a.get("state") or "<MISSING>"
-        postal = a.get("postal") or "<MISSING>"
+        page_url = 'https://www.odwlogistics.com' + ''.join(d.xpath('.//@href'))
+
+        session = SgRequests()
+        r = session.get(page_url, headers=headers)
+        tree = html.fromstring(r.text)
+
+        location_name = ''.join(tree.xpath('//h2[@class="hero__title title"]/text()'))
+
+        location_type = ''.join(tree.xpath('//ul[@class="location__specs"]/li[1]/text()')).strip()
+        street_address = ''.join(tree.xpath('//ul[@class="location__specs"]/li[4]/text()')) or '<MISSING>'
+        if street_address == '<MISSING>':
+            street_address = ''.join(tree.xpath('//ul[@class="location__specs"]/li[2]/text()'))
+        city_state = location_name
+        if city_state.find('|') != -1:
+            city_state = city_state.split('|')[0].strip()
+
+        state = city_state.split(',')[1].strip()
+        postal = "<MISSING>"
         country_code = "USA"
-        city = a.get("city") or "<MISSING>"
+        city = city_state.split(',')[0].strip()
         store_number = "<MISSING>"
-        latlon = driver.find_element_by_xpath(
-            '//a[text()="View larger map"]'
-        ).get_attribute("href")
-        try:
-            latitude = "".join(latlon).split("ll=")[1].split(",")[0].strip()
-            longitude = (
-                "".join(latlon).split("ll=")[1].split(",")[1].split("&")[0].strip()
-            )
-        except:
-            latitude, longitude = "<MISSING>", "<MISSING>"
+        latitude = "<MISSING>"
+        longitude = "<MISSING>"
+        phone = ''.join(tree.xpath('//ul[@class="location__specs"]//a[contains(@href, "tel")]/text()'))
         hours_of_operation = "<MISSING>"
-        driver.switch_to.default_content()
 
         row = [
             locator_domain,
