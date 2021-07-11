b83bc8b7df48f9b531fda3c4d9c0d1e95644284d
datarhan <slutchenko@gmail.com>
2021-07-11 18:20:24 +0200

    [SLC-16203] maintenance

--- apify/datarhan/storelocator/oldtimepottery_com/scrape.py
+++ apify/datarhan/storelocator/oldtimepottery_com/scrape.py
@@ -1,3 +1,4 @@
+import re
 import csv
 import json
 from lxml import etree
@@ -42,43 +43,33 @@ def fetch_data():
     items = []
 
     DOMAIN = "oldtimepottery.com"
-    start_url = "https://storerocket.global.ssl.fastly.net/api/user/j9X8R92JAE/locations?lat=40.6331249&lng=-89.3985283&radius=2500"
+    start_url = "https://storemapper-herokuapp-com.global.ssl.fastly.net/api/users/6818/stores.js?callback=SMcallback2"
     response = session.get(start_url)
-    data = json.loads(response.text)
+    data = re.findall("back2\((.+)\)", response.text)[0]
+    data = json.loads(data)
 
-    for poi in data["results"]["locations"]:
+    for poi in data["stores"]:
         store_url = poi["url"]
         location_name = poi["name"]
         location_name = location_name if location_name else "<MISSING>"
-        street_address = poi["address"].split("<br>")[0].strip()
-        street_address = street_address if street_address else "<MISSING>"
-        city = poi["city"]
-        city = city if city else "<MISSING>"
-        state = poi["state"]
-        state = state if state else "<MISSING>"
-        zip_code = poi["postcode"]
-        zip_code = zip_code if zip_code else "<MISSING>"
-        country_code = poi["country"]
-        country_code = country_code if country_code else "<MISSING>"
-        store_number = poi["marker_id"]
+        raw_address = poi["address"].split(", ")
+        street_address = raw_address[0]
+        city = raw_address[1]
+        state = raw_address[2]
+        zip_code = raw_address[3]
+        country_code = raw_address[4]
+        store_number = poi["id"]
         store_number = store_number if store_number else "<MISSING>"
         phone = poi["phone"]
         phone = phone if phone else "<MISSING>"
-        location_type = poi["location_type_name"]
-        latitude = poi["lat"]
-        longitude = poi["lng"]
+        location_type = "<MISSING>"
+        latitude = poi["latitude"]
+        longitude = poi["longitude"]
 
         loc_response = session.get(store_url)
         loc_dom = etree.HTML(loc_response.text)
-        hours_of_operation = loc_dom.xpath(
-            '//div[@class="hours"]/div[@class="footer"]/p[1]/span/text()'
-        )
-        hours_of_operation += loc_dom.xpath(
-            '//div[@class="hours"]/div[@class="footer"]/p[2]/span/text()'
-        )
-        hours_of_operation = (
-            " ".join(hours_of_operation) if hours_of_operation else "<MISSING>"
-        )
+        hoo = loc_dom.xpath('//div[@class="hours"]//p/span/text()')
+        hours_of_operation = " ".join(hoo) if hoo else "<MISSING>"
 
         item = [
             DOMAIN,
