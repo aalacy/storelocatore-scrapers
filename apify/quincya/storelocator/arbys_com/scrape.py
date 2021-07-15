ecf6e9b6efa0fd82b9adb99c0cdcc4ff8d752639
Quincy <qcalexander@gmail.com>
2021-07-14 22:37:18 -0400

    SLC-16370 fixed

--- apify/quincya/storelocator/arbys_com/scrape.py
+++ apify/quincya/storelocator/arbys_com/scrape.py
@@ -1,15 +1,22 @@
 import csv
+import json
 
 from bs4 import BeautifulSoup
 
+from sglogging import SgLogSetup
+
 from sgrequests import SgRequests
+
+logger = SgLogSetup().get_logger("arbys.com")
 
 
 def write_output(data):
-    with open("data.csv", mode="w") as output_file:
+    with open("data.csv", mode="w", encoding="utf-8") as output_file:
         writer = csv.writer(
             output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
         )
+
+        # Header
         writer.writerow(
             [
                 "locator_domain",
@@ -28,120 +35,106 @@ def write_output(data):
                 "hours_of_operation",
             ]
         )
+        # Body
         for row in data:
             writer.writerow(row)
 
 
 def fetch_data():
 
-    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36"
+    base_link = "https://locations.arbys.com/?_ga=2.162119932.1183880869.1626249434-1748791237.1623814424"
+
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"
     headers = {"User-Agent": user_agent}
 
-    base_link = "https://api.arbys.com/web-exp-api/v1/location?latitude=31.9685988&longitude=-99.9018131&radius=10000&limit=5000&page=1"
-
     session = SgRequests()
-    stores = session.get(base_link, headers=headers).json()
+    req = session.get(base_link, headers=headers)
+    base = BeautifulSoup(req.text, "lxml")
 
-    data = []
-    locator_domain = "arbys.com"
+    final_links = []
 
-    for store in stores:
-        location_name = store["displayName"]
-        raw_address = store["contactDetails"]["address"]
-        street_address = (
-            (
-                raw_address["line1"]
-                + " "
-                + raw_address["line2"].title()
-                + " "
-                + raw_address["line3"].title()
-            )
-            .replace("Building H 4512", "")
-            .strip()
-        )
-        city = raw_address["city"]
-        state = raw_address["stateProvinceCode"]
-        zip_code = raw_address["postalCode"]
-        if "1299 Oxford Street" in street_address:
-            zip_code = "N5Y 4W5"
-        if "26361 Fraser Hwy" in street_address:
-            zip_code = "V4W 2Z7"
-        country_code = raw_address["countryCode"]
-        if country_code not in ["US", "CA"]:
-            continue
-        store_number = store["storeId"]
+    main_items = base.find(class_="border-container-top").find_all(class_="ga-link")
+    for main_item in main_items:
+        main_link = main_item["href"]
+
+        state_count = []
+        logger.info("Processing: " + main_link)
+        req = session.get(main_link, headers=headers)
+        base = BeautifulSoup(req.text, "lxml")
+        next_items = base.find(class_="border-container-top").find_all(class_="ga-link")
+        for next_item in next_items:
+            link = next_item["href"]
+            next_req = session.get(link, headers=headers)
+            next_base = BeautifulSoup(next_req.text, "lxml")
+
+            other_links = next_base.find_all(class_="location-name ga-link")
+            for other_link in other_links:
+                link = other_link["href"]
+                final_links.append(link)
+                state_count.append(state_count)
+        logger.info(len(state_count))
+
+    logger.info("Processing %s links .." % (len(final_links)))
+    for final_link in final_links:
+        final_req = session.get(final_link, headers=headers)
+        item = BeautifulSoup(final_req.text, "lxml")
+
+        locator_domain = "arbys.com"
+
+        script = item.find("script", attrs={"type": "application/ld+json"}).contents[0]
         try:
-            location_type = store["status"]
+            store = json.loads(script)[0]
+        except:
+            store = json.loads(script.split('"additiona')[0].strip()[:-1] + "}]")[0]
+        try:
+            location_name = item.find(class_="h2").text.strip()
+        except:
+            location_name = store["name"]
+        street_address = store["address"]["streetAddress"].strip()
+        city = store["address"]["addressLocality"]
+        state = store["address"]["addressRegion"]
+        zip_code = store["address"]["postalCode"]
+        if not zip_code:
+            zip_code = "<MISSING>"
+        country_code = "US"
+        try:
+            location_type = ", ".join(
+                list(item.find(class_="location-features-wrap mb-0").stripped_strings)
+            )
         except:
             location_type = "<MISSING>"
-        hours = store["hours"]
-        hours_of_operation = ""
-        for h in hours:
-            day = h["dayOfWeek"]
-            open_time = h["openTime"]
-            close_time = h["closeTime"]
-            if open_time:
-                clean_hours = open_time + "-" + close_time
-            else:
-                clean_hours = "Closed"
-            hours_of_operation = (
-                hours_of_operation + " " + day + " " + clean_hours
-            ).strip()
-        if not hours_of_operation:
-            continue
-        if hours_of_operation.count("Closed") == 7:
-            hours_of_operation = "<MISSING>"
-        latitude = store["details"]["latitude"]
-        longitude = store["details"]["longitude"]
-        if str(latitude) == "-1":
-            continue
-        link = (
-            "https://locations.arbys.com/"
-            + state.lower()
-            + "/"
-            + city.lower()
-            .replace(" ", "")
-            .replace(".", "-")
-            .replace("lewismcchord", "lewis-mcchord")
-            + "/"
-            + raw_address["line1"].strip().lower().replace(" ", "-").replace(",", "")
-            + ".html"
-        )
-
-        if "store-#31a" in link and location_type == "<MISSING>":
-            continue
-
-        location_type = "<MISSING>"
-        phone = store["contactDetails"]["phone"]
+        phone = store["address"]["telephone"]
         if not phone:
-            req = session.get(link, headers=headers)
-            base = BeautifulSoup(req.text, "lxml")
-            phone = base.find(class_="phone ga-link").text
-
+            phone = "<MISSING>"
         if not location_name:
-            location_name = "Arby's – " + city
-
-        # Store data
-        data.append(
-            [
-                locator_domain,
-                link,
-                location_name,
-                street_address,
-                city,
-                state,
-                zip_code,
-                country_code,
-                store_number,
-                phone,
-                location_type,
-                latitude,
-                longitude,
-                hours_of_operation,
-            ]
+            location_name = "<MISSING>"
+        store_number = (
+            item.find(class_="store-id").text.replace("Store ID:", "").strip()
         )
+        latitude = store["geo"]["latitude"]
+        longitude = store["geo"]["longitude"]
 
-    return data
+        try:
+            hours_of_operation = " ".join(list(item.find(class_="hours").stripped_strings))
+        except:
+            hours_of_operation = "<MISSING>"
+
+        yield [
+            locator_domain,
+            final_link,
+            location_name,
+            street_address,
+            city,
+            state,
+            zip_code,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
 
 
 def scrape():
