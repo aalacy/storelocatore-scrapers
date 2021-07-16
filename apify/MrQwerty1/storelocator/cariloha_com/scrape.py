92e731d818df5c8bad80a766621f511c50ddb9de
MrQwerty1 <ihor.shan@gmail.com>
2021-07-16 08:23:44 +0300

    fix

--- apify/MrQwerty1/storelocator/cariloha_com/scrape.py
+++ apify/MrQwerty1/storelocator/cariloha_com/scrape.py
@@ -1,9 +1,9 @@
 import csv
-import usaddress
 
 from concurrent import futures
 from lxml import html
 from sgrequests import SgRequests
+from sgscrape.sgpostal import parse_address, International_Parser
 
 
 def write_output(data):
@@ -35,56 +35,12 @@ def write_output(data):
             writer.writerow(row)
 
 
-def get_address(line):
-    tag = {
-        "Recipient": "recipient",
-        "AddressNumber": "address1",
-        "AddressNumberPrefix": "address1",
-        "AddressNumberSuffix": "address1",
-        "StreetName": "address1",
-        "StreetNamePreDirectional": "address1",
-        "StreetNamePreModifier": "address1",
-        "StreetNamePreType": "address1",
-        "StreetNamePostDirectional": "address1",
-        "StreetNamePostModifier": "address1",
-        "StreetNamePostType": "address1",
-        "CornerOf": "address1",
-        "IntersectionSeparator": "address1",
-        "LandmarkName": "address1",
-        "USPSBoxGroupID": "address1",
-        "USPSBoxGroupType": "address1",
-        "USPSBoxID": "address1",
-        "USPSBoxType": "address1",
-        "OccupancyType": "address2",
-        "OccupancyIdentifier": "address2",
-        "SubaddressIdentifier": "address2",
-        "SubaddressType": "address2",
-        "PlaceName": "city",
-        "StateName": "state",
-        "ZipCode": "postal",
-    }
-
-    a = usaddress.tag(line, tag_mapping=tag)[0]
-    street_address = f"{a.get('address1')} {a.get('address2') or ''}".strip()
-    if street_address == "None":
-        street_address = "<MISSING>"
-    city = a.get("city") or "<MISSING>"
-    state = a.get("state") or "<MISSING>"
-    if "." in state:
-        state = state.replace(".", "")
-    postal = a.get("postal") or "<MISSING>"
-
-    return street_address, city, state, postal
-
-
 def get_urls():
     session = SgRequests()
-    r = session.get("https://www.cariloha.com/stores.html")
+    r = session.get("https://www.cariloha.com/stores")
     tree = html.fromstring(r.text)
 
-    return tree.xpath(
-        "//h3[@class='store-region' and ./span[not(contains(text(), 'MEXICO'))] and ./span[not(contains(text(), 'CARIBBEAN'))]]/following-sibling::p/a/@href"
-    )
+    return tree.xpath("//h3[@class='store-region']/following-sibling::p//a/@href")
 
 
 def get_data(page_url):
@@ -92,16 +48,27 @@ def get_data(page_url):
 
     session = SgRequests()
     r = session.get(page_url)
-    tree = html.fromstring(r.text)
+    if page_url != r.url:
+        return
 
+    tree = html.fromstring(r.text)
     location_name = "".join(tree.xpath("//h1/text()")).strip()
     line = tree.xpath("//h3[contains(text(), 'Address')]/following-sibling::p//text()")
-    line = ", ".join(list(filter(None, [l.strip() for l in line]))).replace(
-        ", Poipu Shopping Village", ""
+    line = ", ".join(list(filter(None, [l.strip() for l in line])))
+    print(page_url, ":", line)
+
+    adr = parse_address(International_Parser(), line)
+    street_address = (
+        f"{adr.street_address_1} {adr.street_address_2 or ''}".replace(
+            "None", ""
+        ).strip()
+        or "<MISSING>"
     )
 
-    street_address, city, state, postal = get_address(line)
-    country_code = "US"
+    city = adr.city or "<MISSING>"
+    state = adr.state or "<MISSING>"
+    postal = adr.postcode or "<MISSING>"
+    country_code = adr.country or "<MISSING>"
     store_number = "<MISSING>"
     phone = (
         "".join(
