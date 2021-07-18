e379a4e9dd012f6ddd5932a67ad0f7f5b058e657
Samia Kiran <samiakiran17@gmail.com>
2021-07-18 12:25:47 +0500

    Update scrape.py

--- apify/Kiran-Tanweer/storelocator/coreevolutionpb_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/coreevolutionpb_com/scrape.py
@@ -10,16 +10,16 @@ log = sglog.SgLogSetup().get_logger(logger_name=website)
 session = SgRequests()
 headers = {
     "authority": "coreevolutionpb.com",
-"method": "GET",
-"path": "/contact-us/",
-"scheme": "https",
-"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-"accept-encoding": "gzip, deflate, br",
-"accept-language": "en-US,en;q=0.9",
-"cache-control": "max-age=0",
-"cookie": "humans_21909=1",
-"referer": "https://coreevolutionpb.com/contact-us/",
-"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "method": "GET",
+    "path": "/contact-us/",
+    "scheme": "https",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "max-age=0",
+    "cookie": "humans_21909=1",
+    "referer": "https://coreevolutionpb.com/contact-us/",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
 }
 
 DOMAIN = "https://coreevolutionpb.com/"
@@ -36,7 +36,7 @@ def fetch_data():
             loc = loc.findAll("p")
             if not loc:
                 continue
-            if len(loc)<2:
+            if len(loc) < 2:
                 continue
             phone = loc[0].text
             address = loc[2].get_text(separator="|", strip=True).split("|")
