e3ff59c8faf2f2807c9b77a0510e8f8f6589bb0b
Samia Kiran <samiakiran17@gmail.com>
2021-07-18 16:47:38 +0500

    [SLC-16097] Add Maintenance for kloecknermetals_com
    https://safegraph-crawl.atlassian.net/browse/SLC-16097

--- apify/aleenah/storelocator/kloecknermetals_com/scrape.py
+++ apify/aleenah/storelocator/kloecknermetals_com/scrape.py
@@ -5,6 +5,17 @@ from sgrequests import SgRequests
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgscrape import sgpostal as parser
+import ssl
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
+
 
 session = SgRequests()
 website = "kloecknermetals_com"
