b337c97a81abf833d42d8662d6f1a4b8dded8bf2
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2021-07-19 15:23:01 +0500

    Update scrape.py

--- apify/aleenah/storelocator/amazinglashstudio_com/scrape.py
+++ apify/aleenah/storelocator/amazinglashstudio_com/scrape.py
@@ -5,19 +5,34 @@ import json
 import re
 from sglogging import SgLogSetup
 
-logger = SgLogSetup().get_logger('amazinglashstudio_com')
-
-
+logger = SgLogSetup().get_logger("amazinglashstudio_com")
 
 
 def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    with open("data.csv", mode="w") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
 
         # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code",
-                         "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation",
-                         "page_url"])
+        writer.writerow(
+            [
+                "locator_domain",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+                "page_url",
+            ]
+        )
         # Body
         for row in data:
             writer.writerow(row)
@@ -30,8 +45,8 @@ def fetch_data():
     # Your scraper here
 
     res = session.get("https://www.amazinglashstudio.com/find-a-studio?searchVal=54000")
-    soup = BeautifulSoup(res.text, 'html.parser')
-    #logger.info(soup)
+    soup = BeautifulSoup(res.text, "html.parser")
+    # logger.info(soup)
 
     data = re.findall('(studioArray.*)"};', str(soup), re.DOTALL)[0]
     urls = data.split('"};')
@@ -41,17 +56,19 @@ def fetch_data():
     all = []
     for yrl in urls:
 
-        url = "https://www.amazinglashstudio.com" + re.findall('(/studios/.*)', yrl)[0]
+        url = "https://www.amazinglashstudio.com" + re.findall("(/studios/.*)", yrl)[0]
         logger.info(url)
-        if url == 'https://www.amazinglashstudio.com/studios/tx/beaumont/beaumont': #redirects
+        if (
+            url == "https://www.amazinglashstudio.com/studios/tx/beaumont/beaumont"
+        ):  # redirects
             continue
         res = session.get(url)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        if 'coming soon' in str(soup).lower():
-            logger.info('coming soon')
+        soup = BeautifulSoup(res.text, "html.parser")
+        if "coming soon" in str(soup).lower():
+            logger.info("coming soon")
             continue
-        
-        jss = soup.find_all('script', {"type": "application/ld+json"})
+
+        jss = soup.find_all("script", {"type": "application/ld+json"})
         if len(jss) == 1:
             jss = jss[0]
         elif len(jss) == 2:
@@ -59,7 +76,10 @@ def fetch_data():
         else:
             logger.info("lllllllllllllllllllllllllllllllllllllllllllllllll")
         # break
-        js = jss.contents
+        try:
+            js = jss.contents
+        except:
+            continue
         js = json.loads("".join(js), strict=False)
         addr = js["address"]
         p = js["telephone"]
@@ -75,24 +95,27 @@ def fetch_data():
         if s == "TBD":
             s = "<MISSING>"
         if "openingHours" in js:
-            tim = ' '.join(js["openingHours"])
+            tim = " ".join(js["openingHours"])
         else:
             tim = "<MISSING>"
-        all.append([
-            "https://www.amazinglashstudio.com",
-            js["name"],
-            s,
-            addr["addressLocality"],
-            addr["addressRegion"],
-            addr["postalCode"].split("-")[0],
-            'US',
-            ids[urls.index(yrl)],  # store #
-            p,  # phone
-            js["@type"],  # type
-            lat,  # lat
-            long,  # long
-            tim,  # timing
-            url])
+        all.append(
+            [
+                "https://www.amazinglashstudio.com",
+                js["name"],
+                s,
+                addr["addressLocality"],
+                addr["addressRegion"],
+                addr["postalCode"].split("-")[0],
+                "US",
+                ids[urls.index(yrl)],  # store #
+                p,  # phone
+                js["@type"],  # type
+                lat,  # lat
+                long,  # long
+                tim,  # timing
+                url,
+            ]
+        )
 
     return all
 
