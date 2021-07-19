bf4c292e6ecaad9a1a48897c8f71f6af75d75e90
nsxdarin <nsxdarin@gmail.com>
2021-07-19 10:40:31 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/timberland_co_uk/scrape.py
+++ apify/nsxdarin/storelocator/timberland_co_uk/scrape.py
@@ -129,7 +129,7 @@ def fetch_data():
                 typ = "Timberland Store"
             if "TBC" in phone:
                 phone = "<MISSING>"
-            add = add.replace('None','').strip()
+            add = add.replace("None", "").strip()
             yield [
                 website,
                 loc,
