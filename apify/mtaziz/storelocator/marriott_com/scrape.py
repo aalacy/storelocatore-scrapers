abef0e1d8701565f2244b7459c63ca79e34ba075
mtaziz <mdtariqaziz@gmail.com>
2021-07-20 23:14:47 +0600

    Fix 233 missing records and scrape update 2

--- apify/mtaziz/storelocator/marriott_com/scrape.py
+++ apify/mtaziz/storelocator/marriott_com/scrape.py
@@ -32,8 +32,6 @@ headers = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
 }
 
-# session = SgRequests()
-
 
 def removekey(d, key):
     """
