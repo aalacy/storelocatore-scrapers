5b97ccc77ef9798086fa28a504721b659380209b
nsxdarin <nsxdarin@gmail.com>
2021-07-21 11:12:57 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/kfchk_com/scrape.py
+++ apify/nsxdarin/storelocator/kfchk_com/scrape.py
@@ -2,7 +2,6 @@ from sgrequests import SgRequests
 from sglogging import SgLogSetup
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord import SgRecord
-from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 session = SgRequests()
@@ -95,9 +94,7 @@ def fetch_data():
 
 def scrape():
     results = fetch_data()
-    with SgWriter(
-        deduper=SgRecordDeduper(SgRecordID({SgRecord.Headers.STREET_ADDRESS}))
-    ) as writer:
+    with SgWriter(deduper=SgRecordDeduper({SgRecord.Headers.STREET_ADDRESS})) as writer:
         for rec in results:
             writer.write_row(rec)
 
