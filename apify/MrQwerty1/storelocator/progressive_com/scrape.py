9cb4c0ee030cf2bbd5a4cd4be65ca8d278fd36e3
Azam <azam@wscraper.com>
2021-07-22 14:49:59 +0600

    SLC-15053: reduced max_workers

--- apify/MrQwerty1/storelocator/progressive_com/scrape.py
+++ apify/MrQwerty1/storelocator/progressive_com/scrape.py
@@ -19,7 +19,7 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
 
-session = SgRequests(retry_behavior=False, proxy_rotation_failure_threshold=10)
+session = SgRequests(proxy_rotation_failure_threshold=10)
 log = sglog.SgLogSetup().get_logger(logger_name=website)
 
 
