0468bbe5a012fced7a5fb8d543eab9f6673d2964
datarhan <slutchenko@gmail.com>
2021-07-22 13:17:14 +0200

    [SLC-166668] new template applied

--- apify/datarhan/storelocator/myxperiencefitness_com/scrape.py
+++ apify/datarhan/storelocator/myxperiencefitness_com/scrape.py
@@ -1,9 +1,12 @@
 import re
-import csv
 import ssl
 from lxml import etree
 
 from sgselenium.sgselenium import SgChrome
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgwriter import SgWriter
 
 try:
     _create_unverified_https_context = (
@@ -15,40 +18,8 @@ else:
     ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 
-def write_output(data):
-    with open("data.csv", mode="w", encoding="utf-8") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        # Body
-        for row in data:
-            writer.writerow(row)
-
-
 def fetch_data():
     # Your scraper here
-    items = []
-
     start_url = "https://myxperiencefitness.com/"
     domain = re.findall("://(.+?)/", start_url)[0].replace("www.", "")
     with SgChrome() as driver:
@@ -71,11 +42,7 @@ def fetch_data():
                 state = "mn"
                 zip_code = "<MISSING>"
             country_code = "US"
-            store_number = "<MISSING>"
             phone = loc_dom.xpath('//a[contains(@href, "tel")]/text()')[0]
-            location_type = "<MISSING>"
-            latitude = "<MISSING>"
-            longitude = "<MISSING>"
             hoo = loc_dom.xpath(
                 '//th[i[@class="fas fa-clock"]]/following-sibling::td/text()'
             )
@@ -88,31 +55,36 @@ def fetch_data():
                 else "<MISSING>"
             )
 
-            item = [
-                domain,
-                store_url,
-                location_name,
-                street_address,
-                city,
-                state,
-                zip_code,
-                country_code,
-                store_number,
-                phone,
-                location_type,
-                latitude,
-                longitude,
-                hours_of_operation,
-            ]
+            item = SgRecord(
+                locator_domain=domain,
+                page_url=store_url,
+                location_name=location_name,
+                street_address=street_address,
+                city=city,
+                state=state,
+                zip_postal=zip_code,
+                country_code=country_code,
+                store_number=SgRecord.MISSING,
+                phone=phone,
+                location_type=SgRecord.MISSING,
+                latitude=SgRecord.MISSING,
+                longitude=SgRecord.MISSING,
+                hours_of_operation=SgRecord.MISSING,
+            )
 
-            items.append(item)
-
-    return items
+        yield item
 
 
 def scrape():
-    data = fetch_data()
-    write_output(data)
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID(
+                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
+            )
+        )
+    ) as writer:
+        for item in fetch_data():
+            writer.write_row(item)
 
 
 if __name__ == "__main__":
