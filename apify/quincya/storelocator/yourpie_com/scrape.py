94c075001546bf03c5eda9a93546d26e8b0e4a5c
Quincy <qcalexander@gmail.com>
2021-07-23 04:12:40 -0400

    SLC-16799 fixed

--- apify/quincya/storelocator/yourpie_com/scrape.py
+++ apify/quincya/storelocator/yourpie_com/scrape.py
@@ -16,36 +16,6 @@ from sgzip.dynamic import DynamicZipSearch, SearchableCountries
 log = sglog.SgLogSetup().get_logger(logger_name="yourpie.com")
 
 
-def write_output(data):
-    with open("data.csv", mode="w") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        # Body
-        for row in data:
-            writer.writerow(row)
-
-
 def fetch_data(sgw: SgWriter):
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36"
     headers = {"User-Agent": user_agent}
