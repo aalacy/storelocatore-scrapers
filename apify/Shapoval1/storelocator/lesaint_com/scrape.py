ddeb7307d5974963a09892d6beec30637d8d1447
dima <shshipovald@mail.ru>
2021-07-25 13:42:41 +0300

    fix lesaint.com

--- apify/Shapoval1/storelocator/lesaint_com/scrape.py
+++ apify/Shapoval1/storelocator/lesaint_com/scrape.py
@@ -46,39 +46,70 @@ def fetch_data(sgw: SgWriter):
     }
     r = session.get(api_url, headers=headers)
     tree = html.fromstring(r.text)
-    div = tree.xpath('//section[@class="zn_section eluid8c9d7270     section-sidemargins    section--no "]//div[./p/a]')
+    div = tree.xpath(
+        '//section[@class="zn_section eluid8c9d7270     section-sidemargins    section--no "]//div[./p/a]'
+    )
 
     for d in div:
-        ad = d.xpath('.//p//text()')
+        ad = d.xpath(".//p//text()")
         ad = list(filter(None, [a.strip() for a in ad]))
-        ad = " ".join(ad).replace("Get Directions", "").replace("OH,", "OH").replace("Boulevard,", "Boulevard").replace("6044 0", "60440")
+        ad = (
+            " ".join(ad)
+            .replace("Get Directions", "")
+            .replace("OH,", "OH")
+            .replace("Boulevard,", "Boulevard")
+            .replace("6044 0", "60440")
+        )
         if ad.find("TAGG Logistics") != -1:
             ad = ad.split("TAGG Logistics")[1].strip()
         if ad.count(",") == 2:
             ad = " ".join(ad.split(",")[1:]).split()[1:]
             ad = " ".join(ad)
         a = usaddress.tag(ad, tag_mapping=tag)[0]
-        location_name = "".join(d.xpath('.//span[@class="location-name"]/text() | .//span[./strong]/strong/text() | .//span[./a]/a/text() | .//a[./span]/span/text()'))
+        location_name = "".join(
+            d.xpath(
+                './/span[@class="location-name"]/text() | .//span[./strong]/strong/text() | .//span[./a]/a/text() | .//a[./span]/span/text()'
+            )
+        )
         if ad.find("13204 ") != -1:
             location_name = "Los Angeles, CA"
-        street_address = f"{a.get('address1')} {a.get('address2')}".replace('None','').strip()
-        state = a.get('state') or '<MISSING>'
-        postal = a.get('postal') or '<MISSING>'
+        street_address = f"{a.get('address1')} {a.get('address2')}".replace(
+            "None", ""
+        ).strip()
+        state = a.get("state") or "<MISSING>"
+        postal = a.get("postal") or "<MISSING>"
         country_code = "US"
-        city = a.get('city') or '<MISSING>'
-        page_url = ''.join(d.xpath('.//a[contains(@href, "tagglogistics")]/@href')) or '<MISSING>'
-        if page_url.count('https') == 2:
-            page_url = ''.join(d.xpath('./p/a[contains(@href, "tagglogistics")][1]/@href')) or '<MISSING>'
-        if page_url == '<MISSING>':
-            page_url = ''.join(d.xpath('.//a[contains(@href, "order-fulfillment")]/@href')).replace('/order','order').replace('..','') or '<MISSING>'
-        if page_url.find('https') == -1 and page_url != '<MISSING>':
-            page_url = f'https://www.lesaint.com/{page_url}'
-        if page_url == '<MISSING>':
-            page_url = ''.join(d.xpath('.//p/span[1]/a/@href')).replace('..','') or '<MISSING>'
-        if page_url.find('https') == -1 and page_url != '<MISSING>':
+        city = a.get("city") or "<MISSING>"
+        page_url = (
+            "".join(d.xpath('.//a[contains(@href, "tagglogistics")]/@href'))
+            or "<MISSING>"
+        )
+        if page_url.count("https") == 2:
+            page_url = (
+                "".join(d.xpath('./p/a[contains(@href, "tagglogistics")][1]/@href'))
+                or "<MISSING>"
+            )
+        if page_url == "<MISSING>":
+            page_url = (
+                "".join(d.xpath('.//a[contains(@href, "order-fulfillment")]/@href'))
+                .replace("/order", "order")
+                .replace("..", "")
+                or "<MISSING>"
+            )
+        if page_url.find("https") == -1 and page_url != "<MISSING>":
+            page_url = f"https://www.lesaint.com/{page_url}"
+        if page_url == "<MISSING>":
+            page_url = (
+                "".join(d.xpath(".//p/span[1]/a/@href")).replace("..", "")
+                or "<MISSING>"
+            )
+        if page_url.find("https") == -1 and page_url != "<MISSING>":
             page_url = f"https://www.lesaint.com{page_url}"
-        page_url = page_url.replace('<MISSING>','') or 'https://www.lesaint.com/warehouse-ecommerce-fulfillment-centers-locations/'
-        text = ''.join(d.xpath('.//a[contains(text(), "Get")]/@href'))
+        page_url = (
+            page_url.replace("<MISSING>", "")
+            or "https://www.lesaint.com/warehouse-ecommerce-fulfillment-centers-locations/"
+        )
+        text = "".join(d.xpath('.//a[contains(text(), "Get")]/@href'))
         try:
             if text.find("ll=") != -1:
                 latitude = text.split("ll=")[1].split(",")[0]
