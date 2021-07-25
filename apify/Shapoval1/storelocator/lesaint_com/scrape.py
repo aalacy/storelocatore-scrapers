679b5facba956fba3fea7d98d9da7017d7908018
dima <shshipovald@mail.ru>
2021-07-25 13:41:22 +0300

    fix lesaint.com

--- apify/Shapoval1/storelocator/lesaint_com/scrape.py
+++ apify/Shapoval1/storelocator/lesaint_com/scrape.py
@@ -1,42 +1,14 @@
-import csv
 import usaddress
 from lxml import html
+from sgscrape.sgrecord import SgRecord
 from sgrequests import SgRequests
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 
-def write_output(data):
-    with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
+def fetch_data(sgw: SgWriter):
 
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-
-        for row in data:
-            writer.writerow(row)
-
-
-def fetch_data():
-    out = []
-
-    locator_domain = "https://www.lesaint.com/"
     api_url = (
         "https://www.lesaint.com/warehouse-ecommerce-fulfillment-centers-locations/"
     )
@@ -74,57 +46,39 @@ def fetch_data():
     }
     r = session.get(api_url, headers=headers)
     tree = html.fromstring(r.text)
-    div = tree.xpath(
-        '//div[./p//a[text()="Get Directions"]] | //div[./p//a[text()="Get Direction"]]'
-    )
-    s = set()
+    div = tree.xpath('//section[@class="zn_section eluid8c9d7270     section-sidemargins    section--no "]//div[./p/a]')
+
     for d in div:
-        ad = d.xpath(".//p//text()")
-
+        ad = d.xpath('.//p//text()')
         ad = list(filter(None, [a.strip() for a in ad]))
-
-        ad = (
-            " ".join(ad)
-            .replace("Get Directions", "")
-            .replace("OH,", "OH")
-            .replace("Boulevard,", "Boulevard")
-            .replace("6044 0", "60440")
-        )
-
+        ad = " ".join(ad).replace("Get Directions", "").replace("OH,", "OH").replace("Boulevard,", "Boulevard").replace("6044 0", "60440")
         if ad.find("TAGG Logistics") != -1:
             ad = ad.split("TAGG Logistics")[1].strip()
         if ad.count(",") == 2:
             ad = " ".join(ad.split(",")[1:]).split()[1:]
             ad = " ".join(ad)
-
         a = usaddress.tag(ad, tag_mapping=tag)[0]
-        page_url = (
-            "".join(d.xpath('.//a[contains(@href, "www.tagglogistics.com")]/@href'))
-            or "<MISSING>"
-        )
-        if page_url == "<MISSING>":
-            page_url = "https://www.lesaint.com/warehouse-ecommerce-fulfillment-centers-locations/"
-        if ad.find("2850 Marquis Drive") != -1:
-            page_url = "https://www.tagglogistics.com/dallas-tx-order-fulfillment-center-garland-texas/"
-        location_name = "".join(
-            d.xpath(
-                './/span[@class="location-name"]/text() | .//span[./strong]/strong/text() | .//span[./a]/a/text() | .//a[./span]/span/text()'
-            )
-        )
+        location_name = "".join(d.xpath('.//span[@class="location-name"]/text() | .//span[./strong]/strong/text() | .//span[./a]/a/text() | .//a[./span]/span/text()'))
         if ad.find("13204 ") != -1:
             location_name = "Los Angeles, CA"
-
-        location_type = "<MISSING>"
-        street_address = f"{a.get('address1')} {a.get('address2')}".replace(
-            "None", ""
-        ).strip()
-        state = a.get("state") or "<MISSING>"
-        postal = a.get("postal") or "<MISSING>"
+        street_address = f"{a.get('address1')} {a.get('address2')}".replace('None','').strip()
+        state = a.get('state') or '<MISSING>'
+        postal = a.get('postal') or '<MISSING>'
         country_code = "US"
-        city = a.get("city") or "<MISSING>"
-
-        store_number = "<MISSING>"
-        text = "".join(d.xpath('.//a[text()="Get Directions"]/@href'))
+        city = a.get('city') or '<MISSING>'
+        page_url = ''.join(d.xpath('.//a[contains(@href, "tagglogistics")]/@href')) or '<MISSING>'
+        if page_url.count('https') == 2:
+            page_url = ''.join(d.xpath('./p/a[contains(@href, "tagglogistics")][1]/@href')) or '<MISSING>'
+        if page_url == '<MISSING>':
+            page_url = ''.join(d.xpath('.//a[contains(@href, "order-fulfillment")]/@href')).replace('/order','order').replace('..','') or '<MISSING>'
+        if page_url.find('https') == -1 and page_url != '<MISSING>':
+            page_url = f'https://www.lesaint.com/{page_url}'
+        if page_url == '<MISSING>':
+            page_url = ''.join(d.xpath('.//p/span[1]/a/@href')).replace('..','') or '<MISSING>'
+        if page_url.find('https') == -1 and page_url != '<MISSING>':
+            page_url = f"https://www.lesaint.com{page_url}"
+        page_url = page_url.replace('<MISSING>','') or 'https://www.lesaint.com/warehouse-ecommerce-fulfillment-centers-locations/'
+        text = ''.join(d.xpath('.//a[contains(text(), "Get")]/@href'))
         try:
             if text.find("ll=") != -1:
                 latitude = text.split("ll=")[1].split(",")[0]
@@ -135,44 +89,36 @@ def fetch_data():
         except IndexError:
             latitude, longitude = "<MISSING>", "<MISSING>"
         phone = "<MISSING>"
-        hours_of_operation = "<MISSING>"
         if page_url.find("tagglogistics") != -1:
             session = SgRequests()
             r = session.get(page_url, headers=headers)
             tree = html.fromstring(r.text)
-
             phone = " ".join(tree.xpath("//*//strong/a/text()")).split()[0].strip()
 
-        line = street_address
-        if line in s:
-            continue
-        s.add(line)
+        row = SgRecord(
+            locator_domain=locator_domain,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=postal,
+            country_code=country_code,
+            store_number=SgRecord.MISSING,
+            phone=phone,
+            location_type=SgRecord.MISSING,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=SgRecord.MISSING,
+        )
 
-        row = [
-            locator_domain,
-            page_url,
-            location_name,
-            street_address,
-            city,
-            state,
-            postal,
-            country_code,
-            store_number,
-            phone,
-            location_type,
-            latitude,
-            longitude,
-            hours_of_operation,
-        ]
-        out.append(row)
-
-    return out
-
-
-def scrape():
-    data = fetch_data()
-    write_output(data)
+        sgw.write_row(row)
 
 
 if __name__ == "__main__":
-    scrape()
+    session = SgRequests()
+    locator_domain = "https://www.lesaint.com/"
+    with SgWriter(
+        SgRecordDeduper(SgRecordID({SgRecord.Headers.STREET_ADDRESS}))
+    ) as writer:
+        fetch_data(writer)
