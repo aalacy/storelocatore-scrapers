f151ab3cea1e805b4095ebfc997799e18508ce9d
idev-a <ideveloper003@protonmail.com>
2021-07-26 11:30:16 -0700

    f

--- apify/idev-a/storelocator/kungfutea_com/scrape.py
+++ apify/idev-a/storelocator/kungfutea_com/scrape.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup as bs
 from sglogging import SgLogSetup
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-import re
 import time
 import json
 from sgscrape.sgpostal import parse_address_intl
