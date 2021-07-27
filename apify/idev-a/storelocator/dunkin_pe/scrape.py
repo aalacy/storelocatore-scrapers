4fe3edeccb7d8726e65c9d1a864ffe8c6c68a71e
idev-a <ideveloper003@protonmail.com>
2021-07-27 11:00:53 -0700

    f

--- apify/idev-a/storelocator/dunkin_pe/scrape.py
+++ apify/idev-a/storelocator/dunkin_pe/scrape.py
@@ -3,7 +3,7 @@ from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup as bs
 from sglogging import SgLogSetup
-from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 import json
 import re
