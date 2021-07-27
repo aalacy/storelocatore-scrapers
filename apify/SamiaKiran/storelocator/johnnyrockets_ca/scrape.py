8b1a3ff3f33c3515714a1439b25305691e4da01a
Samia Kiran <samiakiran17@gmail.com>
2021-07-27 13:24:11 +0500

    Update scrape.py

--- apify/SamiaKiran/storelocator/johnnyrockets_ca/scrape.py
+++ apify/SamiaKiran/storelocator/johnnyrockets_ca/scrape.py
@@ -1,4 +1,3 @@
-import json
 from sglogging import sglog
 from bs4 import BeautifulSoup
 from sgrequests import SgRequests
@@ -66,7 +65,9 @@ def fetch_data():
 def scrape():
     log.info("Started")
     count = 0
-    with SgWriter() as writer:
+    with SgWriter(
+        deduper=SgRecordDeduper(record_id=RecommendedRecordIds.PhoneNumberId)
+    ) as writer:
         results = fetch_data()
         for rec in results:
             writer.write_row(rec)
