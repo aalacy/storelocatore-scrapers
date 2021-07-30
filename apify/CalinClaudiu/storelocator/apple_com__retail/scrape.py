f32bf998c01bfdd054dd5adfd3ee08fea9b48235
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-07-30 13:32:34 +0200

    Update scrape.py

--- apify/CalinClaudiu/storelocator/apple_com__retail/scrape.py
+++ apify/CalinClaudiu/storelocator/apple_com__retail/scrape.py
@@ -218,6 +218,7 @@ def get_country(search, country, session, headers, SearchableCountry, state):
             f"Found a total of 0 results for country {country}\n this is unacceptable and possibly a country/search space mismatch\n Matched to: {SearchableCountry}"
         )
         if SearchableCountry not in known_empties:
+            errorzCopy = []
             if errorz:
                 errorz.append(
                     str(
@@ -324,7 +325,7 @@ def fetch_data():
                     try:
                         search = DynamicGeoSearch(
                             country_codes=[SearchableCountry],
-                            expected_search_radius_miles=50,
+                            expected_search_radius_miles=100,  # Must turn it back down to 50 after testing
                             max_search_results=None,
                             granularity=Grain_8(),
                         )
@@ -461,9 +462,9 @@ def scrape():
                 state.set_misc_value("errorz", errorzCopy)
                 state.save(override=True)
                 errorz = errorzCopy
-    for i in errorz:
-        logzilla.warning(i)
-    raise
+    with open("data.csv", mode="a", encoding="utf-8") as file:
+        for i in errorz:
+            file.write(str(i))
 
 
 if __name__ == "__main__":
