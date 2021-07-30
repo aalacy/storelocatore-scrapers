9ef1d898af994eaf562af08a4b0431d0c086ada9
mtaziz <mdtariqaziz@gmail.com>
2021-07-31 05:02:51 +0600

    ssl certficate verification fix

--- apify/mtaziz/storelocator/cashamerica_com/scrape.py
+++ apify/mtaziz/storelocator/cashamerica_com/scrape.py
@@ -5,6 +5,18 @@ from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 import pgeocode
+import ssl
+
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
+
 
 logger = SgLogSetup().get_logger("cashamerica_com")
 MISSING = SgRecord.MISSING
