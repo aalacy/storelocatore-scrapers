d949b1144ed548f9ff145f361068bc1bed875585
mtaziz <mdtariqaziz@gmail.com>
2021-07-31 13:29:22 +0600

    scrape fix and updated

--- apify/mtaziz/storelocator/dollartree_com/scrape.py
+++ apify/mtaziz/storelocator/dollartree_com/scrape.py
@@ -5,7 +5,6 @@ from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-import pgeocode
 import json
 import ssl
 
