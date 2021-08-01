0ebd109fb16c822d02aecad30f505ecaf2add3a0
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2021-08-01 16:36:12 +0500

    Update scrape.py

--- apify/shumaila/storelocator/corelifeeatery_com/scrape.py
+++ apify/shumaila/storelocator/corelifeeatery_com/scrape.py
@@ -1,7 +1,5 @@
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord import SgRecord
-from sgscrape.sgrecord_id import RecommendedRecordIds
-from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 from sgrequests import SgRequests
 
