7db2215da01f412ffff6754696c73f9aa37fc558
MrQwerty1 <ihor.shan@gmail.com>
2021-08-03 12:20:16 +0300

    re-written

--- apify/MrQwerty1/storelocator/jonesbootmaker_com/scrape.py
+++ apify/MrQwerty1/storelocator/jonesbootmaker_com/scrape.py
@@ -1,122 +1,93 @@
-import csv
-import yaml
-
-from lxml import html
+from sgcrawler.sgcrawler_fun import SgCrawlerUsingHttpFun
+from sgcrawler.helper_definitions import (
+    DeclarativeTransformerAndFilter,
+    DeclarativePipeline,
+)
 from sgrequests import SgRequests
+from sgscrape.simple_scraper_pipeline import (
+    SSPFieldDefinitions,
+    ConstantField,
+    MappingField,
+    MissingField,
+)
+from sgscrape.sgwriter import SgWriter
 
 
-def write_output(data):
-    with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-
-        for row in data:
-            writer.writerow(row)
+def fetch_data(_, http: SgRequests):
+    res = http.request(
+        url="https://liveapi.yext.com/v2/accounts/me/answers/vertical/query?experienceKey=answers-jonesbootmaker&api_key=9d200ab7c8620cc20297f7dbfd870b45&v=20190101&version=PRODUCTION&locale=en_GB&input=&verticalKey=location&limit=50&offset=0&facetFilters={}&queryTrigger=initialize&sessionTrackingEnabled=true&sortBys=[]&referrerPageUrl=&source=STANDARD&jsLibVersion=v1.8.6",
+        headers={
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+        },
+    ).json()["response"]["results"]
+    for x in res:
+        yield x
 
 
-def get_root(_id):
-    session = SgRequests()
-    r = session.get(
-        f"https://stores.boldapps.net/front-end/get_store_info.php?shop=jonesbootmaker.myshopify.com&data=detailed&store_id={_id}"
-    )
-    source = r.json()["data"]
+def get_hours(intervals):
+    _tmp = []
+    for day, interval in intervals.items():
+        if interval.get("isClosed"):
+            _tmp.append(f"{day.capitalize()}: Closed")
+            continue
 
-    return html.fromstring(source)
+        start = interval["openIntervals"][0]["start"]
+        end = interval["openIntervals"][0]["end"]
+        _tmp.append(f"{day.capitalize()}: {start} - {end}")
 
-
-def remove_comma(text):
-    if text.endswith(","):
-        return text[:-1]
-    return text
-
-
-def fetch_data():
-    out = []
-    locator_domain = "https://www.jonesbootmaker.com/"
-    page_url = "https://www.jonesbootmaker.com/apps/store-locator"
-
-    session = SgRequests()
-    r = session.get(page_url)
-    tree = html.fromstring(r.text)
-    text = "".join(
-        tree.xpath("//script[contains(text(), 'markersCoords.push(')]/text()")
-    )
-    text = text.split("markersCoords.push(")[1:]
-    for t in text:
-        _id = t.split("id:")[1].split(",")[0].strip()
-        if _id == "0":
-            break
-
-        j = yaml.load(t.split(", id")[0] + "}", Loader=yaml.Loader)
-        root = get_root(_id)
-
-        street_address = "".join(root.xpath("//span[@class='address']/text()")).strip()
-        city = remove_comma("".join(root.xpath("//span[@class='city']/text()")).strip())
-        state = "".join(root.xpath("//span[@class='prov_state']/text()")).strip()
-        postal = remove_comma(
-            "".join(root.xpath("//span[@class='postal_zip']/text()")).strip()
-        )
-        country_code = "GB"
-        store_number = "<MISSING>"
-        location_name = "".join(root.xpath("//span[@class='name']/text()")).strip()
-        if location_name.find("(") != -1:
-            location_name = location_name.split("(")[0].strip()
-
-        phone = "".join(root.xpath("//span[@class='phone']/text()")).strip()
-        latitude = j.get("lat") or "<MISSING>"
-        longitude = j.get("lng") or "<MISSING>"
-        location_type = "<MISSING>"
-        hours_of_operation = (
-            "".join(root.xpath("//span[@class='hours']/text()")).strip() or "<MISSING>"
-        )
-        if hours_of_operation.lower().find("closed") != -1:
-            hours_of_operation = "Temporarily Closed"
-
-        row = [
-            locator_domain,
-            page_url,
-            location_name,
-            street_address,
-            city,
-            state,
-            postal,
-            country_code,
-            store_number,
-            phone,
-            location_type,
-            latitude,
-            longitude,
-            hours_of_operation,
-        ]
-        out.append(row)
-
-    return out
-
-
-def scrape():
-    data = fetch_data()
-    write_output(data)
+    return ";".join(_tmp)
 
 
 if __name__ == "__main__":
-    scrape()
+    crawler_domain = "jonesbootmaker.com"
+    SgCrawlerUsingHttpFun(
+        crawler_domain=crawler_domain,
+        transformer=DeclarativeTransformerAndFilter(
+            pipeline=DeclarativePipeline(
+                crawler_domain=crawler_domain,
+                field_definitions=SSPFieldDefinitions(
+                    locator_domain=ConstantField("https://jonesbootmaker.com"),
+                    page_url=MappingField(mapping=["data", "website"]),
+                    location_name=MappingField(
+                        mapping=["data", "name"], is_required=False
+                    ),
+                    street_address=MappingField(mapping=["data", "address", "line1"]),
+                    city=MappingField(mapping=["data", "address", "city"]),
+                    state=MissingField(),
+                    zipcode=MappingField(
+                        mapping=["data", "address", "postalCode"], is_required=False
+                    ),
+                    country_code=MappingField(
+                        mapping=["data", "address", "countryCode"], is_required=False
+                    ),
+                    store_number=MappingField(
+                        mapping=["data", "id"], part_of_record_identity=True
+                    ),
+                    phone=MappingField(
+                        mapping=["data", "mainPhone"], is_required=False
+                    ),
+                    location_type=MissingField(),
+                    latitude=MappingField(
+                        mapping=["data", "yextDisplayCoordinate", "latitude"],
+                        is_required=False,
+                    ),
+                    longitude=MappingField(
+                        mapping=["data", "yextDisplayCoordinate", "longitude"],
+                        is_required=False,
+                    ),
+                    hours_of_operation=MappingField(
+                        mapping=["data", "hours"], raw_value_transform=get_hours
+                    ),
+                    raw_address=MissingField(),
+                ),
+                fail_on_outlier=False,
+            )
+        ),
+        fetch_raw_using=fetch_data,
+        make_http=lambda _: SgRequests(),
+        data_writer=SgWriter(),
+    ).run()
