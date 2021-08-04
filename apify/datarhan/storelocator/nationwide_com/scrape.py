e5840c0b20c9183a9b631b272b1d17acd465388d
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-08-04 17:10:24 +0200

    Update scrape.py

--- apify/datarhan/storelocator/nationwide_com/scrape.py
+++ apify/datarhan/storelocator/nationwide_com/scrape.py
@@ -9,42 +9,9 @@ from sgrequests import SgRequests
 from sgzip.dynamic import DynamicZipSearch, SearchableCountries
 
 
-def write_output(data):
-    with open("data.csv", mode="w", encoding="utf-8") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        # Body
-        for row in data:
-            writer.writerow(row)
-
-
 def fetch_data():
     # Your scraper here
     session = SgRequests()
-
-    items = []
-    scraped_items = []
 
     DOMAIN = "nationwide.com"
 
