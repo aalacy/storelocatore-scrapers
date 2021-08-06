1c850df68f7fee8e0113ebeda15697d48762629f
nomihd <nouman.awais.nu@gmail.com>
2021-08-06 21:13:56 +0500

    add dedup

--- apify/nomihd/storelocator/longchamp_com/scrape.py
+++ apify/nomihd/storelocator/longchamp_com/scrape.py
@@ -7,6 +7,8 @@ from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgzip.dynamic import SearchableCountries
 from sgzip.dynamic import DynamicGeoSearch
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 website = "longchamp.com"
 log = sglog.SgLogSetup().get_logger(logger_name=website)
@@ -125,7 +127,9 @@ def process_record(raw_results_from_one_coordinate):
 def scrape():
     log.info("Started")
     count = 0
-    with SgWriter() as writer:
+    with SgWriter(
+        deduper=SgRecordDeduper(record_id=RecommendedRecordIds.StoreNumberId)
+    ) as writer:
         countries = (
             SearchableCountries.WITH_ZIPCODE_AND_COORDS
             + SearchableCountries.WITH_COORDS_ONLY
