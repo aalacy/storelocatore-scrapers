70f6aff473a779cb8249ce54806d83b08827c0b3
nomihd <nouman.awais.nu@gmail.com>
2021-08-06 21:23:57 +0500

    [slc-17359] add crawl for rentokil.co.uk

---
+++ apify/nomihd/storelocator/rentokil_co_uk/scrape.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+from sgrequests import SgRequests
+from sglogging import sglog
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
+import lxml.html
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+
+website = "rentokil.co.uk"
+log = sglog.SgLogSetup().get_logger(logger_name=website)
+session = SgRequests()
+
+headers = {
+    "authority": "www.rentokil.co.uk",
+    "cache-control": "max-age=0",
+    "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    "sec-ch-ua-mobile": "?0",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "sec-fetch-site": "none",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-user": "?1",
+    "sec-fetch-dest": "document",
+    "accept-language": "en-US,en-GB;q=0.9,en;q=0.8",
+}
+
+
+def fetch_data():
+    # Your scraper here
+    search_url = "https://www.rentokil.co.uk/property-care/branches/"
+    stores_req = session.get(search_url, headers=headers)
+    log.info("first_requst successfull")
+    stores_sel = lxml.html.fromstring(driver.page_source)
+    stores = stores_sel.xpath('//a[@class="link-list-module_link"]/@href')
+    for store_url in stores:
+        page_url = "https://www.rentokil.co.uk/property-care" + store_url
+
+        log.info(page_url)
+        store_req = session.get(page_url, headers=headers)
+        store_sel = lxml.html.fromstring(driver.page_source)
+
+        locator_domain = website
+        location_name = "".join(
+            store_sel.xpath('//p[@class="section-title_header alpha"]/text()')
+        ).strip()
+        street_address = ", ".join(
+            store_sel.xpath('//span[@itemprop="streetAddress"]/text()')
+        ).strip()
+        city = location_name
+        state = "<MISSING>"
+        zip = "".join(store_sel.xpath('//span[@itemprop="postalCode"]/text()')).strip()
+
+        country_code = "GB"
+
+        phone = "".join(
+            store_sel.xpath('//span[@class="call-cta_number"]//text()')
+        ).strip()
+        location_type = "<MISSING>"
+        store_number = "<MISSING>"
+
+        latitude = "<MISSING>"
+        longitude = "<MISSING>"
+
+        hours_of_operation = "<MISSING>"
+
+        yield SgRecord(
+            locator_domain=locator_domain,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=zip,
+            country_code=country_code,
+            store_number=store_number,
+            phone=phone,
+            location_type=location_type,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+            raw_address=raw_address,
+        )
+
+
+def scrape():
+    log.info("Started")
+    count = 0
+    with SgWriter(
+        deduper=SgRecordDeduper(record_id=RecommendedRecordIds.PageUrlId)
+    ) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
+
+    log.info(f"No of records being processed: {count}")
+    log.info("Finished")
+
+
+if __name__ == "__main__":
+    scrape()
