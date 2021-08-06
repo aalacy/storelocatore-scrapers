b187ead481a77db1d410a74215372d560dd8294b
Azam <azam@wscraper.com>
2021-08-06 20:27:58 +0600

    SLC-16574: updated

--- apify/azambd/storelocator/spring-market_com/scrape.py
+++ apify/azambd/storelocator/spring-market_com/scrape.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from sgselenium.sgselenium import SgChrome
-from webdriver_manager.chrome import ChromeDriverManager
+
 import pandas as pd
 
 import ssl
