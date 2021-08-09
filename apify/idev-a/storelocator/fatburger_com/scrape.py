5203d87b88eebb1dadae31e02e6e552e9a1dc53c
idev-a <ideveloper003@protonmail.com>
2021-08-09 02:37:07 -0700

    f

--- apify/idev-a/storelocator/fatburger_com/scrape.py
+++ apify/idev-a/storelocator/fatburger_com/scrape.py
@@ -13,17 +13,16 @@ _headers = {
 }
 
 locator_domain = "https://fatburger.com/"
-base_url = "https://api.momentfeed.com/v1/analytics/api/llp.json?auth_token=BBOAPSVZOXCPKFUV&center=33.6611,-117.673&coordinates=32.8572717922566,-116.29421582031239,34.45748789640378,-119.05178417968746&multi_account=true&page={page}&pageSize=1000"
-days = [
-    "",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-]
+base_url = "https://api.momentfeed.com/v1/analytics/api/llp.json?auth_token=BBOAPSVZOXCPKFUV&center=33.6611,-117.673&coordinates=32.8572717922566,-116.29421582031239,34.45748789640378,-119.05178417968746&multi_account=true&page={}&pageSize=1000"
+hr_obj = {
+    "1": "Monday",
+    "2": "Tuesday",
+    "3": "Wednesday",
+    "4": "Thursday",
+    "5": "Friday",
+    "6": "Saturday",
+    "7": "Sunday",
+}
 
 
 def _time(val):
@@ -51,8 +50,10 @@ def fetch_data():
                 hours = []
                 if _.get("store_hours"):
                     for hh in _["store_hours"].split(";"):
+                        if not hh:
+                            continue
                         hr = hh.split(",")
-                        hours.append(f"{days[hr]}: {_time(hr[1])}-{_time(hr[2])}")
+                        hours.append(f"{hr_obj[hr[0]]}: {_time(hr[1])}-{_time(hr[2])}")
                 yield SgRecord(
                     page_url=_["website"],
                     location_name=_["name"],
@@ -64,7 +65,7 @@ def fetch_data():
                     longitude=_["longitude"],
                     country_code=_["country"],
                     phone=_["phone"],
-                    location_type=["brand_name"],
+                    location_type=_["brand_name"],
                     locator_domain=locator_domain,
                     hours_of_operation="; ".join(hours),
                 )
