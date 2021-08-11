22eb34f944dcf821912891aafff91d6450092bcf
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2021-08-11 23:02:16 +0500

    Update scrape.py

--- apify/shumaila/storelocator/extremepizza_com/scrape.py
+++ apify/shumaila/storelocator/extremepizza_com/scrape.py
@@ -2,41 +2,15 @@ from bs4 import BeautifulSoup
 import csv
 import re
 from sgrequests import SgRequests
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 session = SgRequests()
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
-
-
-def write_output(data):
-    with open("data.csv", mode="w") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        # Body
-        for row in data:
-            writer.writerow(row)
 
 
 def fetch_data():
@@ -61,11 +35,20 @@ def fetch_data():
             address = soup.find("section", {"id": "intro"}).findAll("a")[0].text.strip()
         except:
             continue
+        if (
+            "Coming Soon!" in soup.find("section", {"id": "intro"}).text
+            or "temporarily closed"
+            in soup.find("section", {"id": "intro"}).text.lower()
+            or "soon!" in soup.find("section", {"id": "intro"}).text
+        ):
+            continue
         try:
             phone = soup.find("section", {"id": "intro"}).findAll("a")[1].text
         except:
             if (
                 "Coming Soon!" in soup.find("section", {"id": "intro"}).text
+                or "temporarily closed"
+                in soup.find("section", {"id": "intro"}).text.lower()
                 or "soon!" in soup.find("section", {"id": "intro"}).text
             ):
                 continue
@@ -75,11 +58,12 @@ def fetch_data():
         hours = ""
         for hr in hourlist:
             if (
-                "AM" in hr.text
-                or "day" in hr.text
-                or "am -" in hr.text
-                or "pm -" in hr.text
+                "am " in hr.text.lower()
+                or "day" in hr.text.lower()
+                or "am -" in hr.text.lower()
+                or "pm -" in hr.text.lower()
             ):
+
                 hrnow = re.sub(cleanr, " ", str(hr)).strip()
                 hours = hours + hrnow + " "
         address = address.split(", ")
@@ -109,36 +93,35 @@ def fetch_data():
         if "Order Online" in phone:
             phone = "<MISSING>"
         check = hours.split("Sun")
-        if len(check) > 1:
+        if len(check) > 1 and "Closed on Sundays" not in hours:
             hours = hours.split("Sun")[1]
-            hours = "Sun " + hours
-        data.append(
-            [
-                "https://www.extremepizza.com/",
-                link,
-                title,
-                street,
-                city,
-                state,
-                pcode,
-                "US",
-                "<MISSING>",
-                phone,
-                "<MISSING>",
-                lat,
-                longt.replace("\n", "").strip(),
-                hours.strip(),
-            ]
+            hours = "Sun" + hours
+        yield SgRecord(
+            locator_domain="https://www.extremepizza.com/",
+            page_url=link,
+            location_name=title,
+            street_address=street.strip(),
+            city=city.strip(),
+            state=state.strip(),
+            zip_postal=pcode.strip(),
+            country_code="US",
+            store_number="<MISSING>",
+            phone=phone.strip(),
+            location_type="<MISSING>",
+            latitude=lat,
+            longitude=longt.replace("\n", "").strip(),
+            hours_of_operation=hours.strip(),
         )
-
-        p += 1
-    return data
 
 
 def scrape():
 
-    data = fetch_data()
-    write_output(data)
+    with SgWriter(
+        deduper=SgRecordDeduper(record_id=RecommendedRecordIds.PageUrlId)
+    ) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
 
 
 scrape()
