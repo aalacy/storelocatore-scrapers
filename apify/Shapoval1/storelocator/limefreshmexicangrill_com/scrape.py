205b90e21c4e4cb10532a3a1f23a2c52410b8632
dima <shshipovald@mail.ru>
2021-08-13 22:18:13 +0300

    fix limefreshmexicangrill.com

--- apify/Shapoval1/storelocator/limefreshmexicangrill_com/scrape.py
+++ apify/Shapoval1/storelocator/limefreshmexicangrill_com/scrape.py
@@ -1,40 +1,13 @@
-import csv
 from lxml import html
+from sgscrape.sgrecord import SgRecord
 from sgrequests import SgRequests
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 
-def write_output(data):
-    with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
+def fetch_data(sgw: SgWriter):
 
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-
-        for row in data:
-            writer.writerow(row)
-
-
-def fetch_data():
-    out = []
-    locator_domain = "https://limefreshmexicangrill.com"
     api_url = "https://limefreshmexicangrill.com/wp-admin/admin-ajax.php?action=store_search&lat=26.3693717&lng=-80.2025238&max_results=25&search_radius=50&autoload=1"
 
     session = SgRequests()
@@ -62,17 +35,15 @@ def fetch_data():
         state = "<MISSING>"
         phone = "".join(j.get("phone"))
         country_code = "".join(j.get("country"))
-        store_number = "<MISSING>"
         location_name = "".join(j.get("store")).replace(" &#8211;", "–").strip()
         latitude = "".join(j.get("lat"))
         longitude = "".join(j.get("lng"))
-        location_type = "<MISSING>"
         page_url = "https://limefreshmexicangrill.com/locations/"
         hours = "".join(j.get("description"))
         hours = html.fromstring(hours)
         hours_of_operation = (
-            " ".join(hours.xpath('//p/span[contains(text(), "AM")]/text()')).replace(
-                "\n", ""
+            " ".join(hours.xpath('//*[contains(text(), "AM")]/text()')).replace(
+                "\r\n", ""
             )
             or "<MISSING>"
         )
@@ -83,31 +54,31 @@ def fetch_data():
             city = "".join(add[1]).split(",")[0]
             state = "".join(add[1]).split(",")[1].strip().split()[0]
             hours_of_operation = " ".join(line.xpath("//p[2]//text()"))
-        row = [
-            locator_domain,
-            page_url,
-            location_name,
-            street_address,
-            city,
-            state,
-            postal,
-            country_code,
-            store_number,
-            phone,
-            location_type,
-            latitude,
-            longitude,
-            hours_of_operation,
-        ]
-        out.append(row)
 
-    return out
+        row = SgRecord(
+            locator_domain=locator_domain,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=postal,
+            country_code=country_code,
+            store_number=SgRecord.MISSING,
+            phone=phone,
+            location_type=SgRecord.MISSING,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+        )
 
-
-def scrape():
-    data = fetch_data()
-    write_output(data)
+        sgw.write_row(row)
 
 
 if __name__ == "__main__":
-    scrape()
+    session = SgRequests()
+    locator_domain = "https://limefreshmexicangrill.com"
+    with SgWriter(
+        SgRecordDeduper(SgRecordID({SgRecord.Headers.STREET_ADDRESS}))
+    ) as writer:
+        fetch_data(writer)
