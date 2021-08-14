88bcd3b2697b63bc8c412efe5a9fe8a9de22244f
dima <shshipovald@mail.ru>
2021-08-14 13:10:47 +0300

    add wildbillstobacco_com

---
+++ apify/Shapoval1/storelocator/wildbillstobacco_com/scrape.py
@@ -0,0 +1,67 @@
+from lxml import html
+from sgscrape.sgrecord import SgRecord
+from sgrequests import SgRequests
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+
+
+def fetch_data(sgw: SgWriter):
+
+    locator_domain = "https://www.wildbillstobacco.com"
+    api_url = "https://www.wildbillstobacco.com/wp-admin/admin-ajax.php?action=store_search&lat=42.50534&lng=-83.10592&max_results=150&search_radius=500&search=48071&statistics%5Bcity%5D=Madison%20Heights&statistics%5Bregion%5D=Michigan&statistics%5Bcountry%5D=United%20States"
+    session = SgRequests()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
+    }
+    r = session.get(api_url, headers=headers)
+    js = r.json()
+    for j in js:
+
+        page_url = j.get("permalink")
+        location_name = (
+            "".join(j.get("store"))
+            .replace("&#8217;", "`")
+            .replace("&#038;", "&")
+            .strip()
+        )
+        location_type = "<MISSING>"
+        street_address = f"{j.get('address')} {j.get('address2')}".strip()
+        state = j.get("state")
+        postal = j.get("zip")
+        country_code = "USA"
+        city = j.get("city")
+        store_number = j.get("id")
+        latitude = j.get("lat")
+        longitude = j.get("lng")
+        phone = j.get("phone")
+        hours_of_operation = j.get("hours")
+        a = html.fromstring(hours_of_operation)
+        hours_of_operation = " ".join(a.xpath("//*//text()")).replace("\n", "").strip()
+
+        row = SgRecord(
+            locator_domain=locator_domain,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=postal,
+            country_code=country_code,
+            store_number=store_number,
+            phone=phone,
+            location_type=SgRecord.MISSING,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+        )
+
+        sgw.write_row(row)
+
+
+if __name__ == "__main__":
+    session = SgRequests()
+    with SgWriter(
+        SgRecordDeduper(SgRecordID({SgRecord.Headers.STORE_NUMBER}))
+    ) as writer:
+        fetch_data(writer)
