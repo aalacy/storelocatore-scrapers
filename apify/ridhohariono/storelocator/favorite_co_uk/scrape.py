98af718f81dbc605f9e40e65e2f0e70625325285
ridhohariono <ridhohariono1904@gmail.com>
2021-08-14 19:04:26 +0700

    [SLC-16728] fix favorite.com

--- apify/ridhohariono/storelocator/favorite_co_uk/scrape.py
+++ apify/ridhohariono/storelocator/favorite_co_uk/scrape.py
@@ -1,15 +1,21 @@
-import csv
 from bs4 import BeautifulSoup as bs
 from sgrequests import SgRequests
-from sglogging import sglog
-from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
 import re
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from sgselenium import SgSelenium
+from sglogging import sglog
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import SgRecordID
 import time
+from sgscrape.sgpostal import parse_address_intl
 
 DOMAIN = "favorite.com"
 BASE_URL = "https://stores.favorite.com/"
 LOCATION_URL = "https://favorite.co.uk/store-finder?delivery=0&lat={}&lng={}"
-AJAX_URL = "https://favorite.co.uk/ajax/storefinder"
 HEADERS = {
     "Accept": "application/json, text/plain, */*",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
@@ -23,64 +29,135 @@ log = sglog.SgLogSetup().get_logger(logger_name=DOMAIN)
 
 session = SgRequests()
 
+MISSING = "<MISSING>"
+CITIES = [
+    "Aberdeen",
+    "Armagh",
+    "Bangor",
+    "Bath",
+    "Belfast",
+    "Birmingham",
+    "Bradford",
+    "Brighton & Hove",
+    "Bristol",
+    "Cambridge",
+    "Canterbury",
+    "Cardiff",
+    "Carlisle",
+    "Chelmsford",
+    "Chester",
+    "Chichester",
+    "Coventry",
+    "Derby",
+    "Derry",
+    "Dundee",
+    "Durham",
+    "Edinburgh",
+    "Ely",
+    "Exeter",
+    "Glasgow",
+    "Gloucester",
+    "Hereford",
+    "Inverness",
+    "Kingston upon Hull",
+    "Lancaster",
+    "Leeds",
+    "Leicester",
+    "Lichfield",
+    "Lincoln",
+    "Lisburn",
+    "Liverpool",
+    "London",
+    "Manchester",
+    "Newcastle upon Tyne",
+    "Newport",
+    "Newry",
+    "Norwich",
+    "Nottingham",
+    "Oxford",
+    "Perth",
+    "Peterborough",
+    "Plymouth",
+    "Portsmouth",
+    "Preston",
+    "Ripon",
+    "St Albans",
+    "St Asaph",
+    "St Davids",
+    "Salford",
+    "Salisbury",
+    "Sheffield",
+    "Southampton",
+    "Stirling",
+    "Stoke-on-Trent",
+    "Sunderland",
+    "Swansea",
+    "Truro",
+    "Wakefield",
+    "Wells",
+    "Westminster",
+    "Winchester",
+    "Wolverhampton",
+    "Worcester",
+    "York",
+    "Sheerness",
+    "Kilburn",
+    "Yeovil",
+    "Bethnal Green",
+    "Corby",
+    "Rayleigh",
+    "Berkshire",
+    "Sussex",
+    "Buckinghamshire",
+    "Essex",
+    "Middlesex",
+    "Heston",
+    "Ipswich",
+    "Croydon",
+    "Greenleys",
+    "Witham",
+    "Crawley",
+    "Stevenage",
+    "Irthlingborough",
+    "Hoddesdon",
+    "Gravesend",
+    "Felixstowe",
+    "Dunstable",
+    "Rainham",
+    "Snodland",
+]
 
-def write_output(data):
-    log.info("Write Output of " + DOMAIN)
-    with open("data.csv", mode="w") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        # Body
-        for row in data:
-            writer.writerow(row)
 
+def getAddress(raw_address):
+    try:
+        if raw_address is not None and raw_address != MISSING:
+            data = parse_address_intl(raw_address)
+            street_address = data.street_address_1
+            if data.street_address_2 is not None:
+                street_address = street_address + " " + data.street_address_2
+            city = data.city
+            state = data.state
+            zip_postal = data.postcode
 
-def pull_content(url, payload):
-    log.info("Pull content => {}: {},{}".format(url, payload["lat"], payload["lng"]))
-    i = 0
-    while True:
-        req = session.post(url, headers=HEADERS, data=payload, allow_redirects=False)
-        if req.status_code == 200:
-            soup = bs(req.json()["html"], "lxml")
-            return soup
-        else:
-            i = i + 1
-            if i > 2:
-                return False
-            time.sleep(2)
-            continue
+            if street_address is None or len(street_address) == 0:
+                street_address = MISSING
+            if city is None or len(city) == 0:
+                city = MISSING
+            if state is None or len(state) == 0:
+                state = MISSING
+            if zip_postal is None or len(zip_postal) == 0:
+                zip_postal = MISSING
+            return street_address, city, state, zip_postal
+    except Exception as e:
+        log.info(f"No valid address {e}")
+        pass
+    return MISSING, MISSING, MISSING, MISSING
 
 
 def handle_missing(field):
     if field is None or (isinstance(field, str) and len(field.strip()) == 0):
         return "<MISSING>"
     return field
-
-
-def is_duplicate(list, filter):
-    for x in list:
-        if filter in x:
-            return True
-    return False
 
 
 def parse_hours(table):
@@ -94,57 +171,53 @@ def parse_hours(table):
     return ", ".join(hoo)
 
 
-def generate_payload():
-    log.info("Fetching store URL")
-    payloads = []
-    search = DynamicGeoSearch(
-        country_codes=[SearchableCountries.BRITAIN],
-        max_radius_miles=20,
-        max_search_results=200,
-    )
-    for lat, long in search:
-        payload = {
-            "action": "init",
-            "is_reload": True,
-            "delivery": 0,
-            "lat": lat,
-            "lng": long,
-        }
-        payloads.append(payload)
-    log.info("Found {} URL ".format(len(payloads)))
-    return payloads
+def get_latlong(url):
+    latlong = re.search(r"lat=(-?[\d]*\.[\d]*)\&lng=(-[\d]*\.[\d]*)", url)
+    if not latlong:
+        return "<MISSING>", "<MISSING>"
+    return latlong.group(1), latlong.group(2)
+
+
+def wait_load(driver):
+    try:
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="findstore-postcode"]'))
+        )
+    except:
+        driver.refresh()
+    return driver
 
 
 def fetch_data():
     log.info("Fetching store_locator data")
-    page_urls = generate_payload()
-    locations = []
-    for payload in page_urls:
-        soup = pull_content(AJAX_URL, payload)
-        if not soup:
-            continue
+    driver = SgSelenium().chrome()
+    driver.get(
+        "https://favorite.co.uk/store-finder?postcode=london&delivery=0&lat=51.5073509&lng=-0.1277583"
+    )
+    for city in CITIES:
+        driver = wait_load(driver)
+        driver.find_element_by_xpath('//*[@id="findstore-postcode"]').clear()
+        driver.find_element_by_xpath('//*[@id="findstore-postcode"]').send_keys(city)
+        driver.find_element_by_xpath('//*[@id="findstore-submit"]').click()
+        time.sleep(1)
+        soup = bs(driver.page_source, "lxml")
         main = soup.find("div", {"class": "row row-store mb0"})
         if not main or len(main) > 50:
             continue
         for row in main:
+            page_url = driver.current_url
             content = main.find("div", {"class": "col-12 mb0"})
             location_name = (
                 content.find("div", {"class": "store-name"})
                 .get_text(strip=True, separator=",")
                 .split(",")[0]
             )
-            locator_domain = DOMAIN
-            address = (
+            raw_address = ", ".join(
                 content.find("div", {"class": "store-name"})
                 .get_text(strip=True, separator=",")
                 .split(",")[2:]
             )
-            street_address = (
-                handle_missing(address[0]).replace("\n", " ").replace("\t", "").strip()
-            )
-            city = handle_missing(address[1]).strip()
-            state = handle_missing(address[2]).strip()
-            zip_code = handle_missing(address[3]).strip()
+            street_address, city, state, zip_postal = getAddress(raw_address)
             country_code = "UK"
             store_number = "<MISSING>"
             phone = soup.find(
@@ -174,44 +247,43 @@ def fetch_data():
                 .replace("Delivery, ", "")
                 .strip()
             )
-            latitude = payload["lat"]
-            longitude = payload["lng"]
-            page_url = LOCATION_URL.format(latitude, longitude)
-        if is_duplicate(locations, street_address):
-            log.info(
-                "Found duplicate => {}:{} => {}".format(
-                    latitude, longitude, street_address
-                )
+            latitude, longitude = get_latlong(page_url)
+            log.info("Found Location{} => {}".format(location_name, street_address))
+            yield SgRecord(
+                locator_domain=DOMAIN,
+                page_url=page_url,
+                location_name=location_name,
+                street_address=street_address.strip(),
+                city=city.strip(),
+                state=state.strip(),
+                zip_postal=zip_postal.strip(),
+                country_code=country_code,
+                store_number=store_number,
+                phone=phone.strip(),
+                location_type=location_type,
+                latitude=latitude,
+                longitude=longitude,
+                hours_of_operation=hours_of_operation,
+                raw_address=raw_address,
             )
-            continue
-        log.info("Append {} => {}".format(location_name, street_address))
-        locations.append(
-            [
-                locator_domain,
-                page_url,
-                location_name,
-                street_address,
-                city,
-                state,
-                zip_code,
-                country_code,
-                store_number,
-                phone,
-                location_type,
-                latitude,
-                longitude,
-                hours_of_operation,
-            ]
-        )
-    return locations
+            time.sleep(1)
 
 
 def scrape():
-    log.info("Start {} Scraper".format(DOMAIN))
-    data = fetch_data()
-    log.info("Found {} locations".format(len(data)))
-    write_output(data)
-    log.info("Finish processed " + str(len(data)))
+    log.info("start {} Scraper".format(DOMAIN))
+    count = 0
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID({SgRecord.Headers.PAGE_URL, SgRecord.Headers.STREET_ADDRESS})
+        )
+    ) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
+
+    log.info(f"No of records being processed: {count}")
+    log.info("Finished")
 
 
 scrape()
