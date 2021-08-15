709579bd72ee9caf4bfd34b2381e74daff050f9f
extrasauceontop <msuchemba@gmail.com>
2021-08-15 15:34:21 -0500

    scrape

--- apify/extrasauceontop/storelocator/webster_edu/scrape.py
+++ apify/extrasauceontop/storelocator/webster_edu/scrape.py
@@ -3,10 +3,13 @@ from bs4 import BeautifulSoup as bs
 import re
 from sgscrape import simple_scraper_pipeline as sp
 
+
 def get_data():
     session = SgRequests()
 
-    response = session.get("https://webster.edu/catalog/current/graduate-catalog/campus-locations.html#.YRbMxIhKi3A").text
+    response = session.get(
+        "https://webster.edu/catalog/current/graduate-catalog/campus-locations.html#.YRbMxIhKi3A"
+    ).text
 
     response = response.split("Palais Wenkheim, 23 Praterstrasse")[0]
     soup = bs(response, "html.parser")
@@ -22,10 +25,15 @@ def get_data():
 
     name_s_list = []
     for tag in name_tags:
-        if "^" in tag.text.strip() or "*" in tag.text.strip() or "Webster University at Southwestern Illinois College" in tag.text.strip() or "University Center of Lake County" in tag.text.strip():
+        if (
+            "^" in tag.text.strip()
+            or "*" in tag.text.strip()
+            or "Webster University at Southwestern Illinois College" in tag.text.strip()
+            or "University Center of Lake County" in tag.text.strip()
+        ):
             tag_check = tag.text.strip().replace("^", "")
             if tag_check == "*":
-                tag = name_tags[name_tags.index(tag)-1]
+                tag = name_tags[name_tags.index(tag) - 1]
             name_s_list.append(tag)
 
     x = 0
@@ -38,27 +46,27 @@ def get_data():
         address_pieces = []
         begin = "False"
         for part in address_parts:
-            if bool(re.search(r'\d', part)) is True:
+            if bool(re.search(r"\d", part)) is True:
                 begin = "True"
-            
+
             if begin == "True":
                 address_pieces.append(part)
-        
+
         for piece in address_pieces:
             if "Ph:" in piece:
                 phone_index = address_pieces.index(piece)
-        
+
         address = ""
-        for y in range(phone_index+1):
-            if y <= phone_index-2:
+        for y in range(phone_index + 1):
+            if y <= phone_index - 2:
                 address = address + address_pieces[y].replace("</strong>", "") + " "
-            
-            elif y == phone_index-1:
+
+            elif y == phone_index - 1:
                 city_state = address_pieces[y]
-            
+
             else:
                 phone = address_pieces[y]
-        
+
         address = address.strip()
         phone = phone.replace("Ph: ", "").split(",")[0]
         city = city_state.split(",")[0]
@@ -71,7 +79,6 @@ def get_data():
         location_type = ""
         hours = ""
         country_code = "US"
-
 
         yield {
             "locator_domain": locator_domain,
@@ -90,16 +97,21 @@ def get_data():
             "country_code": country_code,
         }
 
-        x = x+1
+        x = x + 1
+
 
 def scrape():
     field_defs = sp.SimpleScraperPipeline.field_definitions(
         locator_domain=sp.MappingField(mapping=["locator_domain"], is_required=False),
         page_url=sp.MappingField(mapping=["page_url"], is_required=False),
-        location_name=sp.MappingField(mapping=["location_name"], part_of_record_identity=True),
+        location_name=sp.MappingField(
+            mapping=["location_name"], part_of_record_identity=True
+        ),
         latitude=sp.MappingField(mapping=["latitude"], is_required=False),
         longitude=sp.MappingField(mapping=["longitude"], is_required=False),
-        street_address=sp.MultiMappingField(mapping=["street_address"], is_required=False),
+        street_address=sp.MultiMappingField(
+            mapping=["street_address"], is_required=False
+        ),
         city=sp.MappingField(mapping=["city"], part_of_record_identity=True),
         state=sp.MappingField(mapping=["state"], is_required=False),
         zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
