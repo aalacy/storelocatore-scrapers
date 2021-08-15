51cba0b211524443f1c4908273efa8527c14ee13
mtaziz <mdtariqaziz@gmail.com>
2021-08-15 14:49:06 +0600

    scrape code blacked

--- apify/mtaziz/storelocator/bobcat_com/scrape.py
+++ apify/mtaziz/storelocator/bobcat_com/scrape.py
@@ -32,7 +32,6 @@ search = DynamicZipSearch(
 )
 
 
-
 headers2 = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
