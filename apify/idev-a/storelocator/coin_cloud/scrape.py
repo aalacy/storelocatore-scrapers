f601a98d9168099b942fac657da57eb837b77150
idev-a <ideveloper003@protonmail.com>
2021-08-16 23:39:37 -0700

    f

--- apify/idev-a/storelocator/coin_cloud/scrape.py
+++ apify/idev-a/storelocator/coin_cloud/scrape.py
@@ -39,7 +39,7 @@ def fetch_data():
                 phone=_["phone"],
                 locator_domain=locator_domain,
                 hours_of_operation="; ".join(hours),
-                raw_address=_['address']
+                raw_address=_["address"],
             )
 
 
