b901cbdb783971007cbceb72bc8a1d4d185fd13a
datarhan <slutchenko@gmail.com>
2021-08-17 12:19:20 +0200

    Revert "[SLC-17937] maintenance"
    This reverts commit 142ad659d095071c86584a1f3527786487c7c9a7.

--- apify/datarhan/storelocator/seacoastbank_com___hash_atm/scrape.py
+++ apify/datarhan/storelocator/seacoastbank_com___hash_atm/scrape.py
@@ -1,17 +1,51 @@
 import re
+import csv
 import json
 from lxml import etree
 
 from sgrequests import SgRequests
-from sgscrape.sgrecord import SgRecord
-from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgrecord_id import SgRecordID
-from sgscrape.sgwriter import SgWriter
+
+
+def write_output(data):
+    with open("data.csv", mode="w", encoding="utf-8") as output_file:
+        writer = csv.writer(
+            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
+
+        # Header
+        writer.writerow(
+            [
+                "locator_domain",
+                "page_url",
+                "location_name",
+                "street_address",
+                "city",
+                "state",
+                "zip",
+                "country_code",
+                "store_number",
+                "phone",
+                "location_type",
+                "latitude",
+                "longitude",
+                "hours_of_operation",
+            ]
+        )
+        # Body
+        unique_rows = []
+        for row in data:
+            if row not in unique_rows:
+                writer.writerow(row)
+                unique_rows.append(row)
 
 
 def fetch_data():
+    # Your scraper here
     session = SgRequests().requests_retry_session(retries=2, backoff_factor=0.3)
-    domain = "seacoastbank.com"
+
+    items = []
+
+    DOMAIN = "seacoastbank.com"
     start_url = "https://www.seacoastbank.com/locations"
 
     response = session.get(start_url)
@@ -55,36 +89,31 @@ def fetch_data():
             hoo = [e.strip() for e in hoo if e.strip()]
         hours_of_operation = " ".join(hoo) if hoo else "<MISSING>"
 
-        item = SgRecord(
-            locator_domain=domain,
-            page_url=store_url,
-            location_name=location_name,
-            street_address=street_address,
-            city=city,
-            state=state,
-            zip_postal=zip_code,
-            country_code=country_code,
-            store_number=store_number,
-            phone=phone,
-            location_type=location_type,
-            latitude=latitude,
-            longitude=longitude,
-            hours_of_operation=hours_of_operation,
-        )
+        item = [
+            DOMAIN,
+            store_url,
+            location_name,
+            street_address,
+            city,
+            state,
+            zip_code,
+            country_code,
+            store_number,
+            phone,
+            location_type,
+            latitude,
+            longitude,
+            hours_of_operation,
+        ]
 
-        yield item
+        items.append(item)
+
+    return items
 
 
 def scrape():
-    with SgWriter(
-        SgRecordDeduper(
-            SgRecordID(
-                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
-            )
-        )
-    ) as writer:
-        for item in fetch_data():
-            writer.write_row(item)
+    data = fetch_data()
+    write_output(data)
 
 
 if __name__ == "__main__":
