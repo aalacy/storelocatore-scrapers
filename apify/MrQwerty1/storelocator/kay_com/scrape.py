0106a4931386fdfdf22c8066cc709049ba021471
MrQwerty1 <ihor.shan@gmail.com>
2021-08-18 21:10:56 +0300

    fix

--- apify/MrQwerty1/storelocator/kay_com/scrape.py
+++ apify/MrQwerty1/storelocator/kay_com/scrape.py
@@ -11,48 +11,48 @@ def fetch_data(_zip, sgw: SgWriter):
         api = f"https://www.kay.com/store-finder/find?q={_zip}&page={i}&includeKayStores=true&includeKayOutletStores=true"
 
         r = session.get(api, headers=headers)
-        js = r.json()['data']
+        js = r.json()["data"]
 
         for j in js:
-            location_name = 'Kay Jewelers'
-            location_type = 'Kay Jewelers'
-            slug = j.get('url')
+            location_name = "Kay Jewelers"
+            location_type = "Kay Jewelers"
+            slug = j.get("url")
             page_url = f"https://www.kay.com{slug}"
             street_address = f'{j.get("line1")} {j.get("line2") or ""}'.strip()
-            city = j.get('town')
-            state = j.get('regionIsoCodeShort')
-            postal = j.get('postalCode')
+            city = j.get("town")
+            state = j.get("regionIsoCodeShort")
+            postal = j.get("postalCode")
             country_code = "US"
-            phone = j.get('phone')
-            latitude = j.get('latitude')
-            longitude = j.get('longitude')
-            store_number = j.get('name')
-            _type = j.get('baseStore') or ''
-            if _type == 'kayoutlet':
-                location_name = 'Kay Jewelers Outlet'
-                location_type = 'Kay Jewelers Outlet'
+            phone = j.get("phone")
+            latitude = j.get("latitude")
+            longitude = j.get("longitude")
+            store_number = j.get("name")
+            _type = j.get("baseStore") or ""
+            if _type == "kayoutlet":
+                location_name = "Kay Jewelers Outlet"
+                location_type = "Kay Jewelers Outlet"
 
             _tmp = []
-            items = j.get('openings') or {}
+            items = j.get("openings") or {}
             for k, v in items.items():
                 _tmp.append(f"{k}: {v}")
 
             hours_of_operation = ";".join(_tmp)
             row = SgRecord(
-                            page_url=page_url,
-                            location_name=location_name,
-                            street_address=street_address,
-                            city=city,
-                            state=state,
-                            zip_postal=postal,
-                            country_code=country_code,
-                            store_number=store_number,
-                            phone=phone,
-                            location_type=location_type,
-                            latitude=latitude,
-                            longitude=longitude,
-                            locator_domain=locator_domain,
-                            hours_of_operation=hours_of_operation
+                page_url=page_url,
+                location_name=location_name,
+                street_address=street_address,
+                city=city,
+                state=state,
+                zip_postal=postal,
+                country_code=country_code,
+                store_number=store_number,
+                phone=phone,
+                location_type=location_type,
+                latitude=latitude,
+                longitude=longitude,
+                locator_domain=locator_domain,
+                hours_of_operation=hours_of_operation,
             )
 
             sgw.write_row(row)
@@ -61,12 +61,14 @@ def fetch_data(_zip, sgw: SgWriter):
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     session = SgRequests()
     locator_domain = "https://www.kay.com/"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"
     }
     with SgWriter(SgRecordDeduper(RecommendedRecordIds.StoreNumberId)) as writer:
-        for _z in DynamicZipSearch(country_codes=SearchableCountries.USA, max_search_distance_miles=100):
+        for _z in DynamicZipSearch(
+            country_codes=SearchableCountries.USA, max_search_distance_miles=100
+        ):
             fetch_data(_z, writer)
