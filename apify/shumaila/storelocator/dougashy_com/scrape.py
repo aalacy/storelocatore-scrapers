47ecbfcfe0d87e66d0ad3fceaee9ed3717ebaada
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2021-08-19 15:37:02 +0500

    Update scrape.py

--- apify/shumaila/storelocator/dougashy_com/scrape.py
+++ apify/shumaila/storelocator/dougashy_com/scrape.py
@@ -3,7 +3,7 @@ import re
 from sgrequests import SgRequests
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord import SgRecord
-from sgscrape.sgrecord_id import RecommendedRecordIds, SgRecordID
+from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 session = SgRequests()
