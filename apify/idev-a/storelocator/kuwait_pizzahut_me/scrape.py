e04bd75d4c7c4863a50e4e9d228050fd195e9300
idev-a <ideveloper003@protonmail.com>
2021-08-21 03:26:25 -0700

    f

--- apify/idev-a/storelocator/kuwait_pizzahut_me/scrape.py
+++ apify/idev-a/storelocator/kuwait_pizzahut_me/scrape.py
@@ -1,8 +1,10 @@
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
-from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
+from typing import Iterable
+from sgscrape.pause_resume import SerializableRequest, CrawlState, CrawlStateSingleton
 from sglogging import SgLogSetup
 
 logger = SgLogSetup().get_logger("pizzahut")
@@ -19,37 +21,45 @@ _headers = {
     "if-none-match": 'W/"c67-RfvFOzR4Li2deW0/nRL5kFgbo0I"',
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
 }
-
 locator_domain = "https://kuwait.pizzahut.me"
 base_url = "https://apimes1.phdvasia.com/v2/product-hut-fe/localization/getListArea"
 
 
-def fetch_data():
-    with SgRequests() as session:
-        import pdb
+def record_initial_requests(http: SgRequests, state: CrawlState) -> bool:
+    areas = http.get(base_url, headers=_headers).json()["data"]["items"]
+    for area in areas:
+        area_url = f"https://apimes1.phdvasia.com/v2/product-hut-fe/localization/getBlockByArea?area={area['area'].replace(' ','%20')}"
+        logger.info(area_url)
+        state.push_request(SerializableRequest(url=area_url))
 
-        pdb.set_trace()
-        areas = session.get(base_url, headers=_headers).json()["data"]["items"]
-        for area in areas:
-            area_url = f"https://apimes1.phdvasia.com/v2/product-hut-fe/localization/getBlockByArea?area={area['area'].replace(' ','%20')}"
-            logger.info(area_url)
-            locations = session.get(area_url, headers=_headers).json()["data"]["item"][
-                "block"
-            ]
-            for _ in locations:
-                yield SgRecord(
-                    store_number=_["id"],
-                    location_name=_["name"],
-                    street_address=_["name"],
-                    city=_["customize"]["CityName"],
-                    state=_["customize"]["ProvinceName"],
-                    country_code="Kuwait",
-                    locator_domain=locator_domain,
-                )
+    return True
+
+
+def fetch_records(http: SgRequests, state: CrawlState) -> Iterable[SgRecord]:
+    for next_r in state.request_stack_iter():
+        locations = http.get(next_r.url, headers=_headers).json()["data"]["item"][
+            "block"
+        ]
+        for _ in locations:
+            yield SgRecord(
+                store_number=_["id"],
+                location_name=_["name"],
+                street_address=_["name"],
+                city=_["customize"]["CityName"],
+                state=_["customize"]["ProvinceName"],
+                country_code="Kuwait",
+                locator_domain=locator_domain,
+            )
 
 
 if __name__ == "__main__":
-    with SgWriter() as writer:
-        results = fetch_data()
-        for rec in results:
-            writer.write_row(rec)
+    state = CrawlStateSingleton.get_instance()
+    with SgWriter(
+        deduper=SgRecordDeduper(RecommendedRecordIds.StoreNumberId)
+    ) as writer:
+        with SgRequests() as http:
+            state.get_misc_value(
+                "init", default_factory=lambda: record_initial_requests(http, state)
+            )
+            for rec in fetch_records(http, state):
+                writer.write_row(rec)
