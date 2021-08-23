df81e39e2b4b486cfd66de0c42c53a22734a9d72
Michael Dinh <mkdinh94@gmail.com>
2021-08-23 12:01:19 -0400

    [SLC-18204] set headless to True

--- apify/mkdinh/storelocator/napaonline_com/scrape.py
+++ apify/mkdinh/storelocator/napaonline_com/scrape.py
@@ -25,7 +25,7 @@ local = threading.local()
 def get_driver():
     if not hasattr(local, "driver"):
         local.driver = SgChrome(
-            seleniumwire_auto_config=False, is_headless=False
+            seleniumwire_auto_config=False, is_headless=True
         ).driver()
         local.driver.set_script_timeout(120)
         load_initial_page(local.driver)
