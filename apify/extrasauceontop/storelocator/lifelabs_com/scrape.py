a7823debc3bd60b1c06045f96ae204710f2fede0
vivri <me@victorivri.com>
2021-08-23 16:36:58 -0400

    ran black

--- apify/extrasauceontop/storelocator/lifelabs_com/scrape.py
+++ apify/extrasauceontop/storelocator/lifelabs_com/scrape.py
@@ -107,13 +107,21 @@ def scrape():
     field_defs = sp.SimpleScraperPipeline.field_definitions(
         locator_domain=sp.MappingField(mapping=["locator_domain"]),
         page_url=sp.MappingField(mapping=["page_url"], part_of_record_identity=True),
-        location_name=sp.MappingField(mapping=["location_name"],),
-        latitude=sp.MappingField(mapping=["latitude"],),
-        longitude=sp.MappingField(mapping=["longitude"],),
+        location_name=sp.MappingField(
+            mapping=["location_name"],
+        ),
+        latitude=sp.MappingField(
+            mapping=["latitude"],
+        ),
+        longitude=sp.MappingField(
+            mapping=["longitude"],
+        ),
         street_address=sp.MultiMappingField(
             mapping=["street_address"], is_required=False
         ),
-        city=sp.MappingField(mapping=["city"],),
+        city=sp.MappingField(
+            mapping=["city"],
+        ),
         state=sp.MappingField(mapping=["state"], is_required=False),
         zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
         country_code=sp.MappingField(mapping=["country_code"]),
