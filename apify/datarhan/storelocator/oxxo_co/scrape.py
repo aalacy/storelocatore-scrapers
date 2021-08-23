3cad14c162ef96eae0c2a54951ae0869f2cb424a
datarhan <slutchenko@gmail.com>
2021-08-23 11:58:05 +0200

    [SLC-18298] unused deleted

--- apify/datarhan/storelocator/oxxo_co/scrape.py
+++ apify/datarhan/storelocator/oxxo_co/scrape.py
@@ -16,8 +16,6 @@ def fetch_data():
     all_locations = session.get(start_url, headers=hdr).json()
 
     for poi in all_locations:
-        if poi["numero"]:
-            print(poi)
         city = poi["colonia"]
         if city == "CALLE 52":
             city = SgRecord.MISSING
