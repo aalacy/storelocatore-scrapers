5f8d40a27241abad336ca12289b7a6811ad20273
extrasauceontop <msuchemba@gmail.com>
2021-08-23 17:20:23 -0500

    scrape

--- apify/extrasauceontop/storelocator/premierrents_net/scrape.py
+++ apify/extrasauceontop/storelocator/premierrents_net/scrape.py
@@ -143,55 +143,22 @@ def get_data():
 
 def scrape():
     field_defs = sp.SimpleScraperPipeline.field_definitions(
-        locator_domain=sp.MappingField(
-            mapping=["locator_domain"]
-        ),
-        page_url=sp.MappingField(
-            mapping=["page_url"]
-        ),
-        location_name=sp.MappingField(
-            mapping=["location_name"]
-        ),
-        latitude=sp.MappingField(
-            mapping=["latitude"],
-            part_of_record_identity=True
-        ),
-        longitude=sp.MappingField(
-            mapping=["longitude"],
-            part_of_record_identity=True
-        ),
+        locator_domain=sp.MappingField(mapping=["locator_domain"]),
+        page_url=sp.MappingField(mapping=["page_url"]),
+        location_name=sp.MappingField(mapping=["location_name"]),
+        latitude=sp.MappingField(mapping=["latitude"], part_of_record_identity=True),
+        longitude=sp.MappingField(mapping=["longitude"], part_of_record_identity=True),
         street_address=sp.MultiMappingField(
             mapping=["street_address"], is_required=False
         ),
-        city=sp.MappingField(
-            mapping=["city"]
-        ),
-        state=sp.MappingField(
-            mapping=["state"],
-            is_required=False
-        ),
-        zipcode=sp.MultiMappingField(
-            mapping=["zip"],
-            is_required=False
-        ),
-        country_code=sp.MappingField(
-            mapping=["country_code"]
-        ),
-        phone=sp.MappingField(
-            mapping=["phone"],
-            is_required=False
-        ),
-        store_number=sp.MappingField(
-            mapping=["store_number"]
-        ),
-        hours_of_operation=sp.MappingField(
-            mapping=["hours"],
-            is_required=False
-        ),
-        location_type=sp.MappingField(
-            mapping=["location_type"],
-            is_required=False
-        ),
+        city=sp.MappingField(mapping=["city"]),
+        state=sp.MappingField(mapping=["state"], is_required=False),
+        zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
+        country_code=sp.MappingField(mapping=["country_code"]),
+        phone=sp.MappingField(mapping=["phone"], is_required=False),
+        store_number=sp.MappingField(mapping=["store_number"]),
+        hours_of_operation=sp.MappingField(mapping=["hours"], is_required=False),
+        location_type=sp.MappingField(mapping=["location_type"], is_required=False),
     )
 
     pipeline = sp.SimpleScraperPipeline(
