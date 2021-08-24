ef78b1c9a528e16b62ebd53d8b3fab7e620f20fd
Kiran Tanweer <44836465+Kiran-Tanweer@users.noreply.github.com>
2021-08-24 20:35:23 +0500

    Update scrape.py

--- apify/Kiran-Tanweer/storelocator/ritecheck_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/ritecheck_com/scrape.py
@@ -1,108 +1,75 @@
+from sglogging import sglog
 from bs4 import BeautifulSoup
-import csv
-import re
-import time
 from sgrequests import SgRequests
-from sglogging import SgLogSetup
-
-logger = SgLogSetup().get_logger("ritecheck_com")
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord import SgRecord
+import re
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 session = SgRequests()
-
+website = "ritecheck_com"
+log = sglog.SgLogSetup().get_logger(logger_name=website)
+session = SgRequests()
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
 }
 
-
-def write_output(data):
-    with open("data.csv", mode="w", newline="", encoding="utf8") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-
-        temp_list = []
-        for row in data:
-            comp_list = [
-                row[2].strip(),
-                row[3].strip(),
-                row[4].strip(),
-                row[5].strip(),
-                row[6].strip(),
-                row[8].strip(),
-                row[10].strip(),
-            ]
-            if comp_list not in temp_list:
-                temp_list.append(comp_list)
-                writer.writerow(row)
-        logger.info(f"No of records being processed: {len(temp_list)}")
+DOMAIN = "https://ritecheck.com/"
+MISSING = "<MISSING>"
 
 
 def fetch_data():
-    data = []
-    pattern = re.compile(r"\s\s+")
-    cleanr = re.compile(r"<[^>]+>")
-    search_url = "https://ritecheck.com/locations.html"
-    stores_req = session.get(search_url, headers=headers)
-    soup = BeautifulSoup(stores_req.text, "html.parser")
-    locations = soup.find("ul", {"class": "locations-list"}).findAll("li")
-    for loc in locations:
-        addresses = loc.find("p", {"class": "locations-list-title"}).findAll("a")
-        hours = loc.findAll("p")[1].text
-        for addr in addresses:
-            address = addr.text
-            hours = hours
-            hours = hours.replace("â", "-")
-            address = re.sub(pattern, " ", address)
-            address = re.sub(cleanr, " ", address)
+    if True:
+        pattern = re.compile(r"\s\s+")
+        cleanr = re.compile(r"<[^>]+>")
+        search_url = "https://ritecheck.com/locations.html"
+        stores_req = session.get(search_url, headers=headers)
+        soup = BeautifulSoup(stores_req.text, "html.parser")
+        locations = soup.find("ul", {"class": "locations-list"}).findAll("li")
+        for loc in locations:
+            addresses = loc.find("p", {"class": "locations-list-title"}).findAll("a")
+            hours = loc.findAll("p")[1].text
+            for addr in addresses:
+                address = addr.text
+                hours = hours
+                hours = hours.replace("â", "-")
+                address = re.sub(pattern, " ", address)
+                address = re.sub(cleanr, " ", address)
 
-            hours = hours.replace(",", "")
-            hours = hours.strip()
+                hours = hours.replace(",", "")
+                hours = hours.strip()
 
-            data.append(
-                [
-                    "https://ritecheck.com/",
-                    "https://ritecheck.com/",
-                    "<MISSING>",
-                    address,
-                    "<MISSING>",
-                    "<MISSING>",
-                    "<MISSING>",
-                    "US",
-                    "<MISSING>",
-                    "<MISSING>",
-                    "<MISSING>",
-                    "<MISSING>",
-                    "<MISSING>",
-                    hours,
-                ]
-            )
-    return data
+                yield SgRecord(
+                    locator_domain=DOMAIN,
+                    page_url=DOMAIN,
+                    location_name=MISSING,
+                    street_address=MISSING,
+                    city=MISSING,
+                    state=MISSING,
+                    zip_postal=MISSING,
+                    country_code=MISSING,
+                    store_number=MISSING,
+                    phone=MISSING,
+                    location_type=MISSING,
+                    latitude=MISSING,
+                    longitude=MISSING,
+                    hours_of_operation=hours.strip(),
+                    raw_address=address.strip(),
+                )
 
 
 def scrape():
-    logger.info(time.strftime("%H:%M:%S", time.localtime(time.time())))
-    data = fetch_data()
-    write_output(data)
-    logger.info(time.strftime("%H:%M:%S", time.localtime(time.time())))
+    log.info("Started")
+    count = 0
+    with SgWriter() as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
+    log.info(f"No of records being processed: {count}")
+    log.info("Finished")
 
 
-scrape()
+if __name__ == "__main__":
+    scrape()
