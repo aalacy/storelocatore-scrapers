72683ccf33655026d6d4ff2a5a59e4cdf004aa69
idev-a <ideveloper003@protonmail.com>
2021-08-31 01:38:59 +0800

    f

--- apify/idev-a/storelocator/7eleven_com_my/scrape.py
+++ apify/idev-a/storelocator/7eleven_com_my/scrape.py
@@ -3,8 +3,6 @@ from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
 import json
 from sgscrape.sgpostal import parse_address_intl
-from sgselenium import SgChrome
-from bs4 import BeautifulSoup as bs
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
@@ -32,40 +30,39 @@ def _phone(val):
 
 
 def fetch_data():
-    with SgChrome() as driver:
-        with SgRequests() as session:
-            res = session.get(base_url, headers=_headers())
-            cleaned = (
-                res.text.replace("\\t", " ")
-                .replace("\\\\u003d", "=")
-                .replace("\\\\u0026", "&")
-                .replace("\xa0", " ")
-                .replace('\\"', '"')
+    with SgRequests() as session:
+        res = session.get(base_url, headers=_headers())
+        cleaned = (
+            res.text.replace("\\t", " ")
+            .replace("\\\\u003d", "=")
+            .replace("\\\\u0026", "&")
+            .replace("\xa0", " ")
+            .replace('\\"', '"')
+        )
+        locations = json.loads(
+            cleaned.split('var _pageData = "')[1].split('";</script>')[0]
+        )
+        for _ in locations[1][6][0][12][0][13][0]:
+            location_name = _[5][0][1][0]
+            latitude = _[1][0][0][0]
+            longitude = _[1][0][0][1]
+            raw_address = _[5][3][1][1][0]
+            addr = parse_address_intl(raw_address)
+            street_address = addr.street_address_1
+            if addr.street_address_2:
+                street_address += ", " + addr.street_address_2
+            yield SgRecord(
+                location_name=location_name,
+                street_address=street_address,
+                city=addr.city,
+                state=addr.state,
+                zip_postal=addr.postcode,
+                country_code="Malaysia",
+                latitude=latitude,
+                longitude=longitude,
+                locator_domain=locator_domain,
+                raw_address=raw_address,
             )
-            locations = json.loads(
-                cleaned.split('var _pageData = "')[1].split('";</script>')[0]
-            )
-            for _ in locations[1][6][0][12][0][13][0]:
-                location_name = _[5][0][1][0]
-                latitude = _[1][0][0][0]
-                longitude = _[1][0][0][1]
-                raw_address = _[5][3][1][1][0]
-                addr = parse_address_intl(raw_address)
-                street_address = addr.street_address_1
-                if addr.street_address_2:
-                    street_address += ", " + addr.street_address_2
-                yield SgRecord(
-                    location_name=location_name,
-                    street_address=street_address,
-                    city=addr.city,
-                    state=addr.state,
-                    zip_postal=addr.postcode,
-                    country_code="Malaysia",
-                    latitude=latitude,
-                    longitude=longitude,
-                    locator_domain=locator_domain,
-                    raw_address=raw_address,
-                )
 
 
 if __name__ == "__main__":
