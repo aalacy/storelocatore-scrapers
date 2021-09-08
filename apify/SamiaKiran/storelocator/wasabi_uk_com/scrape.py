a4e7e26056a71f244b7f3da5f93ccc217c9232b4
Samia Kiran <samiakiran17@gmail.com>
2021-09-08 11:24:01 +0500

    Update scrape.py

--- apify/SamiaKiran/storelocator/wasabi_uk_com/scrape.py
+++ apify/SamiaKiran/storelocator/wasabi_uk_com/scrape.py
@@ -62,7 +62,7 @@ def fetch_data():
             phone = temp["phone_number"]
             address = temp["address"]
             address = BeautifulSoup(address, "html.parser")
-            raw_address = address.get_text(separator='|', strip=True).replace('|'," ")
+            raw_address = address.get_text(separator="|", strip=True).replace("|", " ")
             print(raw_address)
             hour_list = temp["opening_hours_schema"]
             hours_of_operation = ""
