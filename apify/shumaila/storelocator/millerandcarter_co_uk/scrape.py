9c54081cd20cee09f727e422090e2d2245c0de49
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2021-09-18 18:48:01 +0500

    Update scrape.py

--- apify/shumaila/storelocator/millerandcarter_co_uk/scrape.py
+++ apify/shumaila/storelocator/millerandcarter_co_uk/scrape.py
@@ -12,98 +12,91 @@ headers = {
 }
 
 
-
 def fetch_data():
 
-    
-    url = "https://www.millerandcarter.co.uk/sitemap.xml"   
+    url = "https://www.millerandcarter.co.uk/sitemap.xml"
     r = session.get(url, headers=headers, verify=False)
     soup = BeautifulSoup(r.text, "html.parser")
-    linklist = soup.findAll("loc")    
+    linklist = soup.findAll("loc")
     for link in linklist:
-        
+
         link = link.text
-        
+
         try:
-            check = link.split('/restaurants',1)[1].split('/')
-           
+            check = link.split("/restaurants", 1)[1].split("/")
+
             if len(check) == 3:
                 pass
             else:
                 continue
         except:
             continue
-
-        print('link=',link)
-    
-        
+        print("link=", link)
 
         try:
             r = session.get(link, headers=headers, verify=False)
         except:
             continue
-        loc = r.text.split('<script type="application/ld+json">',1)[1].split('</script>',1)[0]
-        loc = json.loads(loc.replace('\n',''))       
-        title = loc['name']
-        street=loc["address"]['streetAddress']
+        loc = r.text.split('<script type="application/ld+json">', 1)[1].split(
+            "</script>", 1
+        )[0]
+        loc = json.loads(loc.replace("\n", ""))
+        title = loc["name"]
+        street = loc["address"]["streetAddress"]
         try:
-            city=loc["address"]["addressLocality"]
+            city = loc["address"]["addressLocality"]
         except:
-            city='<MISSING>'
+            city = "<MISSING>"
         try:
-            state = loc["address"]['addressRegion']
+            state = loc["address"]["addressRegion"]
         except:
-            state ='<MISSING>'
+            state = "<MISSING>"
         ccode = "GB"
-        pcode = loc["address"]['postalCode']
+        pcode = loc["address"]["postalCode"]
         phone = loc["telephone"]
-        lat = loc["geo"]['latitude']
-        longt = loc["geo"]['longitude']
-        hourlist = loc['openingHoursSpecification']
-        hours = ''
+        lat = loc["geo"]["latitude"]
+        longt = loc["geo"]["longitude"]
+        hourlist = loc["openingHoursSpecification"]
+        hours = ""
         for hr in hourlist:
             day = hr["dayOfWeek"][0]
-            openstr = ''
-            closestr = ''
-            if int(hr["opens"].split(':',1)[0]) == 12:
-                openstr = 'Midday - '
+            openstr = ""
+            closestr = ""
+            if int(hr["opens"].split(":", 1)[0]) == 12:
+                openstr = "Midday - "
             else:
-                openstr = hr["opens"].split(':',1)[0]+' AM - '
-            if int(hr["closes"].split(':',1)[0]) > 12:
-                closestr = str(int(hr["closes"].split(':',1)[0]) - 12)+' PM '
+                openstr = hr["opens"].split(":", 1)[0] + " AM - "
+            if int(hr["closes"].split(":", 1)[0]) > 12:
+                closestr = str(int(hr["closes"].split(":", 1)[0]) - 12) + " PM "
             else:
-                closestr = str(int(hr["closes"].split(':',1)[0]))+' PM '
-            
-            hours =hours + day+' '+ openstr+closestr
-                               
-        
-        
+                closestr = str(int(hr["closes"].split(":", 1)[0])) + " PM "
+            hours = hours + day + " " + openstr + closestr
         yield SgRecord(
-                locator_domain="https://www.millerandcarter.co.uk",
-                page_url=link,
-                location_name=title,
-                street_address=street.strip(),
-                city=city.strip(),
-                state=state.strip(),
-                zip_postal=pcode.strip(),
-                country_code="US",
-                store_number=SgRecord.MISSING,
-                phone=phone.strip(),
-                location_type=SgRecord.MISSING,
-                latitude=str(lat),
-                longitude=str(longt),
-                hours_of_operation=hours,
-            )
-        
+            locator_domain="https://www.millerandcarter.co.uk",
+            page_url=link,
+            location_name=title,
+            street_address=street.strip(),
+            city=city.strip(),
+            state=state.strip(),
+            zip_postal=pcode.strip(),
+            country_code="US",
+            store_number=SgRecord.MISSING,
+            phone=phone.strip(),
+            location_type=SgRecord.MISSING,
+            latitude=str(lat),
+            longitude=str(longt),
+            hours_of_operation=hours,
+        )
+
 
 def scrape():
-    
-    with SgWriter( deduper=SgRecordDeduper(record_id=RecommendedRecordIds.PageUrlId)) as writer:
+
+    with SgWriter(
+        deduper=SgRecordDeduper(record_id=RecommendedRecordIds.PageUrlId)
+    ) as writer:
         results = fetch_data()
         for rec in results:
             writer.write_row(rec)
 
 
 scrape()
-
-
