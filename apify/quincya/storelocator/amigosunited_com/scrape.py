2cc37949b2505dacf1424e4cedf7bd78432de25a
Quincy <qcalexander@gmail.com>
2021-09-17 23:59:00 -0400

    SLC-19584 fixed

--- apify/quincya/storelocator/amigosunited_com/scrape.py
+++ apify/quincya/storelocator/amigosunited_com/scrape.py
@@ -1,4 +1,5 @@
 import json
+import ssl
 import time
 
 from bs4 import BeautifulSoup
@@ -9,6 +10,15 @@ from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 from sgselenium import SgChrome
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 
 def fetch_data(sgw: SgWriter):
