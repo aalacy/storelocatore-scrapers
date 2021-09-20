426280e877be858d50e04bdf9648cc5c20c0d050
idev-a <ideveloper003@protonmail.com>
2021-09-20 13:53:25 -0700

    f

--- apify/idev-a/storelocator/themelt_com/scrape.py
+++ apify/idev-a/storelocator/themelt_com/scrape.py
@@ -14,34 +14,29 @@ def fetch_data():
         locations = json.loads(rr.response.body)
         for _ in locations["result"]["items"]:
             hours = []
-            try:
-                if _["days_01"]:
-                    hours.append(f"{_['days_01']}: {_['hours_01']}")
-                if _.get("days_02", "").strip():
-                    hours.append(f"{_['days_02']}: {_['hours_02']}")
-                if _.get("days_03", "").strip():
-                    hours.append(f"{_['days_03']}: {_['hours_03']}")
-                street_address = _["address"]
-                cross_street = _["cross_streets"].split("\n")[-1]
-                if "St." not in street_address and "St." in cross_street:
-                    street_address = cross_street
-                if cross_street.split(" ")[0].strip().isdigit():
-                    street_address = cross_street
-                yield SgRecord(
-                    page_url=base_url,
-                    location_name=_["title"],
-                    street_address=street_address,
-                    city=_["city"],
-                    state=_["state"],
-                    country_code="US",
-                    phone=_["phone_number"],
-                    locator_domain=locator_domain,
-                    hours_of_operation="; ".join(hours),
-                )
-            except:
-                import pdb
-
-                pdb.set_trace()
+            if _["days_01"]:
+                hours.append(f"{_['days_01']}: {_['hours_01']}")
+            if _.get("days_02", "").strip():
+                hours.append(f"{_['days_02']}: {_['hours_02']}")
+            if _.get("days_03", "").strip():
+                hours.append(f"{_['days_03']}: {_['hours_03']}")
+            street_address = _["address"]
+            cross_street = _["cross_streets"].split("\n")[-1]
+            if "St." not in street_address and "St." in cross_street:
+                street_address = cross_street
+            if cross_street.split(" ")[0].strip().isdigit():
+                street_address = cross_street
+            yield SgRecord(
+                page_url=base_url,
+                location_name=_["title"],
+                street_address=street_address,
+                city=_["city"],
+                state=_["state"],
+                country_code="US",
+                phone=_["phone_number"],
+                locator_domain=locator_domain,
+                hours_of_operation="; ".join(hours),
+            )
 
 
 if __name__ == "__main__":
