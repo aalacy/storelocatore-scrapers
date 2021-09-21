1356f62137cfdd28fd53db68e0a03fca3c5d76d7
extrasauceontop <msuchemba@gmail.com>
2021-09-21 14:31:26 -0500

    scrape

--- apify/extrasauceontop/curatedsources/findachurch_co_uk/scrape.py
+++ apify/extrasauceontop/curatedsources/findachurch_co_uk/scrape.py
@@ -6,13 +6,12 @@ from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from bs4 import BeautifulSoup as bs
 import re
-import json
 
 log = sglog.SgLogSetup().get_logger(logger_name="findchurch")
 headers = {"User-Agent": "PostmanRuntime/7.19.0"}
 session = SgRequests()
 crawl_state = CrawlStateSingleton.get_instance()
-x = 0
+
 
 def get_urls():
     search = DynamicGeoSearch(country_codes=[SearchableCountries.BRITAIN])
@@ -66,8 +65,6 @@ def get_urls():
 
 
 def get_location(request_url):
-    x = x+1
-    print("x is " + str(x))
     session = SgRequests()
     url = request_url.url
     log.info(url)
@@ -232,29 +229,20 @@ def get_location(request_url):
 
 
 def scrape_loc_urls():
-    url_list = [loc for loc in crawl_state.request_stack_iter()]
+    url_list = [loc for loc in crawl_state.request_stack_iter()][:10000]
     if len(url_list) < 50:
         log.info("no records, did it run with a proxy?")
         raise Exception
-    results = []
+
     with ThreadPoolExecutor() as executor:
         futures = [executor.submit(get_location, loc) for loc in url_list]
         for future in as_completed(futures):
             try:
                 record = future.result()
                 if record:
-                    results.append(record)
+                    yield record
             except Exception as e:
                 log.error(str(e))
-        
-    return results
-
-
-def get_data():
-    results = scrape_loc_urls()
-
-    for result in results:
-        yield result
 
 
 def scrape():
@@ -292,7 +280,7 @@ def scrape():
 
     pipeline = sp.SimpleScraperPipeline(
         scraper_name="Crawler",
-        data_fetcher=get_data,
+        data_fetcher=scrape_loc_urls,
         field_definitions=field_defs,
         log_stats_interval=15,
     )
