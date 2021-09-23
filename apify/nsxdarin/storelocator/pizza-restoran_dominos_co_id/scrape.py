d667724041b465900d4289984bb7428e9b1f8912
nsxdarin <nsxdarin@gmail.com>
2021-09-23 10:09:59 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/pizza-restoran_dominos_co_id/scrape.py
+++ apify/nsxdarin/storelocator/pizza-restoran_dominos_co_id/scrape.py
@@ -10,7 +10,8 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
 
-logger = SgLogSetup().get_logger("dominos_co_id")
+logger = SgLogSetup().get_logger("pizza-restoran_dominos_co_id")
+
 
 def fetch_data():
     url = "https://pizza-restoran.dominos.co.id/robots.txt"
@@ -19,37 +20,44 @@ def fetch_data():
     r = session.get(url, headers=headers)
     for line in r.iter_lines():
         line = str(line.decode("utf-8"))
-        if 'Sitemap:' in line and '/index.xml' not in line:
-            sms.append(line.split('Sitemap:')[1].strip().replace('\r','').replace('\n',''))
-    website = "dominos.co.id"
+        if "Sitemap:" in line and "/index.xml" not in line:
+            sms.append(
+                line.split("Sitemap:")[1].strip().replace("\r", "").replace("\n", "")
+            )
+    website = "pizza-restoran.dominos.co.id"
     typ = "<MISSING>"
     country = "ID"
     for sm in sms:
         logger.info(sm)
         r2 = session.get(sm, headers=headers)
         for line2 in r2.iter_lines():
-            line2 = str(line2.decode('utf-8'))
-            if '/Home</loc>' in line2:
-                lurl = line2.split('<loc>')[1].split('<')[0]
+            line2 = str(line2.decode("utf-8"))
+            if "/Home</loc>" in line2:
+                lurl = line2.split("<loc>")[1].split("<")[0]
                 if lurl not in locs:
                     locs.append(lurl)
     for loc in locs:
         logger.info(loc)
-        name = ''
-        store = loc.rsplit('-',1)[1].split('/')[0]
-        city = ''
-        add = ''
-        state = ''
-        zc = ''
-        phone = ''
-        lat = ''
-        lng = ''
-        hours = ''
+        name = ""
+        store = loc.rsplit("-", 1)[1].split("/")[0]
+        city = ""
+        add = ""
+        state = ""
+        zc = ""
+        phone = ""
+        lat = ""
+        lng = ""
+        hours = ""
         r2 = session.get(loc, headers=headers)
         for line2 in r2.iter_lines():
-            line2 = str(line2.decode('utf-8'))
-            if '<title>' in line2:
-                name = line2.split('<title>')[1].split('|')[0].strip().replace('&#039;',"'")
+            line2 = str(line2.decode("utf-8"))
+            if "<title>" in line2:
+                name = (
+                    line2.split("<title>")[1]
+                    .split("|")[0]
+                    .strip()
+                    .replace("&#039;", "'")
+                )
             if '"streetAddress":"' in line2:
                 add = line2.split('"streetAddress":"')[1].split('"')[0]
                 city = line2.split('"addressLocality":"')[1].split('"')[0]
@@ -62,11 +70,17 @@ def fetch_data():
                 days = line2.split('"dayOfWeek":"')
                 for day in days:
                     if ',"opens":"' in day:
-                        hrs = day.split('"')[0] + ': ' + day.split(',"opens":"')[1].split('"')[0] + '-' + day.split('"closes":"')[1].split('"')[0]
-                        if hours == '':
+                        hrs = (
+                            day.split('"')[0]
+                            + ": "
+                            + day.split(',"opens":"')[1].split('"')[0]
+                            + "-"
+                            + day.split('"closes":"')[1].split('"')[0]
+                        )
+                        if hours == "":
                             hours = hrs
                         else:
-                            hours = hours + '; ' + hrs
+                            hours = hours + "; " + hrs
         yield SgRecord(
             locator_domain=website,
             page_url=loc,
@@ -90,5 +104,6 @@ def scrape():
     with SgWriter(deduper=SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
         for rec in results:
             writer.write_row(rec)
+
 
 scrape()
