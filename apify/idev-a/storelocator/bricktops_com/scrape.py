c1d51f28943e418948a2039bf3f8d013e3327645
idev-a <ideveloper003@protonmail.com>
2021-09-24 10:40:18 -0700

    f

--- apify/idev-a/storelocator/bricktops_com/scrape.py
+++ apify/idev-a/storelocator/bricktops_com/scrape.py
@@ -6,6 +6,8 @@ from sglogging import SgLogSetup
 import re
 import json
 from sgscrape.sgpostal import parse_address_intl
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 logger = SgLogSetup().get_logger("bricktops")
 
@@ -80,7 +82,7 @@ def fetch_data():
 
 
 if __name__ == "__main__":
-    with SgWriter() as writer:
+    with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
         results = fetch_data()
         for rec in results:
             writer.write_row(rec)
