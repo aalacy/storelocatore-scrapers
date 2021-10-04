1a81161c7187585bd9ce52f7a594951acccf853f
datarhan <slutchenko@gmail.com>
2021-10-04 10:20:50 +0200

    [SLC-20195] reqs update

--- apify/datarhan/storelocator/caraluzzis_com/scrape.py
+++ apify/datarhan/storelocator/caraluzzis_com/scrape.py
@@ -5,7 +5,7 @@ from time import sleep
 from urllib.parse import urljoin
 
 from sgselenium import SgChrome
-from sgscrape.sgpostal import parse_address_intl
+from sgpostal.sgpostal import parse_address_intl
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import SgRecordID
