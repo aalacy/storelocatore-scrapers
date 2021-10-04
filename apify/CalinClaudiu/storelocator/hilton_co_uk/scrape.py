4528fdc08f08e384ab7c9a9113ac49c9aa5a9fc8
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-10-04 10:52:19 +0200

    Update scrape.py

--- apify/CalinClaudiu/storelocator/hilton_co_uk/scrape.py
+++ apify/CalinClaudiu/storelocator/hilton_co_uk/scrape.py
@@ -18,7 +18,7 @@ logzilla = sglog.SgLogSetup().get_logger(logger_name="Scraper")
 
 def cleanup_json(x, url):
     try:
-        z = x.split('"description"')[0] + str('"opening') + x.split('"opening', 1)[1]
+        z = x.split('"description"')[0]+str('"opening')+x.split('"opening',1)[1]
     except Exception as e:
         logzilla.error(f"{x}\n{str(e)}\n{str(url)}")
         z = x
@@ -120,7 +120,7 @@ def fetch_data():
                     yield record
                 country["complete"] = True
                 state.set_misc_value("countries", countries)
-
+    raise
 
 def data_fetcher(country, state):
     url = country["link"]
@@ -162,20 +162,20 @@ def data_fetcher(country, state):
             total = total + len(i["data"]["hotelSummaryOptions"]["hotels"])
             for j in i["data"]["hotelSummaryOptions"]["hotels"]:
                 allhotels.append(j)
-        except KeyError:
-            pass
+        except KeyError as e:
+            logzilla.error(f"{i}\n{str(e)}\n\n")
 
     logzilla.info(f"Found a total of {total} hotels for country {country}")  # noqa
-    lize = utils.parallelize(
-        search_space=allhotels,
-        fetch_results_for_rec=para,
-        max_threads=10,
-        print_stats_interval=10,
-    )
+    #lize = utils.parallelize(# noqa
+    #    search_space=allhotels[0:2],# noqa
+    #    fetch_results_for_rec=para,# noqa
+    #    max_threads=10,# noqa
+    #    print_stats_interval=10,# noqa
+    #)# noqa
 
-    for j in lize:
-        yield j
-
+    #for j in lize:# noqa
+    #    yield j # noqa
+    yield para(allhotels[0])
     logzilla.info(f"Finished grabbing data!!")  # noqa
 
 
