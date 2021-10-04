82c43fca8933aec69ea42f8032d3d816a229f083
datarhan <slutchenko@gmail.com>
2021-10-04 14:23:06 +0200

    [SLC-20029] ssl added

--- apify/datarhan/storelocator/monumentparking_com/scrape.py
+++ apify/datarhan/storelocator/monumentparking_com/scrape.py
@@ -1,3 +1,4 @@
+import ssl
 from lxml import etree
 from time import sleep
 
@@ -7,6 +8,15 @@ from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgwriter import SgWriter
 from sgselenium.sgselenium import SgChrome
 from sgzip.dynamic import DynamicZipSearch, SearchableCountries
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 
 def fetch_data():
