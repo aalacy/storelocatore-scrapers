a38ef9d7c67d8654c0df7bbbcae3ec936af4a2e7
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-10-07 13:41:12 +0200

    SLC-16975
    https://safegraph-crawl.atlassian.net/browse/SLC-16975

--- apify/CalinClaudiu/storelocator/tjmaxx_com/scrape.py
+++ apify/CalinClaudiu/storelocator/tjmaxx_com/scrape.py
@@ -1,81 +1,107 @@
-from sgscrape.simple_scraper_pipeline import *
-from sgscrape import simple_network_utils as net_utils
-from sgscrape import simple_utils as utils
-from sgrequests import SgRequests
+from typing import Iterable, Tuple, Callable
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
+from sgscrape.pause_resume import CrawlStateSingleton
+from sgrequests.sgrequests import SgRequests
+from sgzip.dynamic import SearchableCountries, Grain_8
+from sgzip.parallel import DynamicSearchMaker, ParallelDynamicSearch, SearchIteration
 from sglogging import sglog
-import sgzip
-from sgzip import DynamicGeoSearch, SearchableCountries
-import json
 
-def parse_store(k):
-    k['error'] = False
-    k['page_url'] = 'https://tjmaxx.tjx.com/store/stores/' + str(k['City'].replace(' ','+'))+'-'+str(k['State'])+'-'+str(k['Zip'])+'/'+str(k['StoreID'])+'/aboutstore'
-    return k    
+logzilla = sglog.SgLogSetup().get_logger(logger_name="Scraper")
+
+def nice_locs(resp):
     
-def fetch_data():
-    logzilla = sglog.SgLogSetup().get_logger(logger_name='Scraper')
-    url = "https://mktsvc.tjx.com/storelocator/GetSearchResults?geolat=37.09024&geolong=-95.712891&chain=8&maxstores=10000&radius=10000"
-    headers = {
-                'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'        }
-    session = SgRequests()
-    son = session.get(url, headers = headers).json()
-    search = sgzip.DynamicGeoSearch(country_codes=[SearchableCountries.USA])
-    search.initialize()
-    coord = search.next()
-    identities = set()
-    while coord:
-        lat, long = coord
-        son = session.get('https://mktsvc.tjx.com/storelocator/GetSearchResults?geolat='+str(lat)+'&geolong='+str(long)+'&chain=8&maxstores=10000&radius=10000', headers = headers).json()
-        result_lats = []
-        result_longs = []
-        result_coords = []
-        topop = 0
-        stores = son['Stores']
-        if len(stores)!=0:
-            for j in stores:
-                k = {}
-                k = parse_store(j)
-                if k['error'] != True:
-                    result_lats.append(k['Latitude'])
-                    result_longs.append(k['Longitude'])
-                    if str(str(k['Latitude'])+str(k['Longitude'])) not in identities:
-                        identities.add(str(str(k['Latitude'])+str(k['Longitude'])))
-                        yield k
-                    else:
-                        topop += 1
-        result_coords = list(zip(result_lats,result_longs))
-        logzilla.info(f'Zipcodes remaining: {search.zipcodes_remaining()}; Last request yields {len(result_coords)-topop} stores.')
-        search.update_with(result_coords)
-        coord = search.next()
-    logzilla.info(f'Finished grabbing data!!')
+def fix_comma(x):
+    h = []
+    try:
+        for i in x.split(","):
+            if len(i.strip()) >= 1:
+                h.append(i)
+        return ", ".join(h)
+    except Exception:
+        return x
 
 
+class ExampleSearchIteration(SearchIteration):
+    """
+    Here, you define what happens with each iteration of the search.
+    The `do(...)` method is what you'd do inside of the `for location in search:` loop
+    It provides you with all the data you could get from the search instance, as well as
+    a method to register found locations.
+    """
 
-def scrape():
-    url="https://www.tjmaxx.tjx.com/"
-    field_defs = SimpleScraperPipeline.field_definitions(
-        locator_domain = ConstantField(url),
-        page_url=MappingField(mapping=['page_url']),
-        location_name=MappingField(mapping=['Name']),
-        latitude=MappingField(mapping=['Latitude']),
-        longitude=MappingField(mapping=['Longitude']),
-        street_address=MultiMappingField(mapping=[['Address'],['Address2']], multi_mapping_concat_with = ', ', value_transform = lambda x : x.replace(   'None','')),
-        city=MappingField(mapping=['City'], value_transform = lambda x : x.replace('None','<MISSING>')),
-        state=MappingField(mapping=['State'], value_transform = lambda x : x.replace('None','<MISSING>')),
-        zipcode=MappingField(mapping=['Zip'], value_transform = lambda x : x.replace('None','<MISSING>'), is_required = False),
-        country_code=MappingField(mapping=['Country'], value_transform = lambda x : x.replace('None','<MISSING>')),
-        phone=MappingField(mapping=['Phone'], value_transform = lambda x : x.replace('None','<MISSING>') , is_required = False),
-        store_number=MappingField(mapping=['StoreID']),
-        hours_of_operation=MappingField(mapping=['Hours']),
-        location_type=MissingField()
-    )
+    def __init__(self, http: SgRequests):
+        self.__http = http
+        self.__state = CrawlStateSingleton.get_instance()
 
-    pipeline = SimpleScraperPipeline(scraper_name='nofrills.ca',
-                                     data_fetcher=fetch_data,
-                                     field_definitions=field_defs,
-                                     log_stats_interval=15)
+    def do(
+        self,
+        coord: Tuple[float, float],
+        zipcode: str,
+        current_country: str,
+        items_remaining: int,
+        found_location_at: Callable[[float, float], None],
+    ) -> Iterable[SgRecord]:
+        """
+        This method gets called on each iteration of the search.
+        It provides you with all the data you could get from the search instance, as well as
+        a method to register found locations.
 
-    pipeline.run()
+        :param coord: The current coordinate (lat, long)
+        :param zipcode: The current zipcode (In DynamicGeoSearch instances, please ignore!)
+        :param current_country: The current country (don't assume continuity between calls - it's meant to be parallelized)
+        :param items_remaining: Items remaining in the search - per country, if `ParallelDynamicSearch` is used.
+        :param found_location_at: The equivalent of `search.found_location_at(lat, long)`
+        """
+
+        # here you'd use self.__http, and call `found_location_at(lat, long)` for all records you find.
+        lat, lng = coord
+        # just some clever accounting of locations/country:
+        numbers = "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99"
+        url = str(f"https://marketingsl.tjx.com/storelocator/GetSearchResults?geolat={lat}&geolong={lng}&chain={numbers}&maxstores=9999&radius=1000")
+        headers = {}
+        headers[
+            "accept"
+        ] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        headers["accept-encoding"] = "gzip, deflate, br"
+        headers["accept-language"] = "en-US,en;q=0.9"
+        headers["cache-control"] = "no-cache"
+        headers["pragma"] = "no-cache"
+        headers[
+            "user-agent"
+        ] = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        locations = None
+        try:
+            locations = SgRequests.raise_on_err(http.get(url, headers=headers)).json()
+            errorName = None
+        except Exception as e:
+            logzilla.error(f"{e}")
+
+        if locations:
+            for rec in nice_locs(locations):
+                yield rec
+
 
 if __name__ == "__main__":
-    scrape()
+    # additionally to 'search_type', 'DynamicSearchMaker' has all options that all `DynamicXSearch` classes have.
+    search_maker = DynamicSearchMaker(
+        search_type="DynamicGeoSearch",
+        granularity=Grain_8(),
+        expected_search_radius_miles=None,
+    )
+
+    with SgWriter(
+        deduper=SgRecordDeduper(RecommendedRecordIds.StoreNumAndPageUrlId)
+    ) as writer:
+        with SgRequests() as http:
+            search_iter = ExampleSearchIteration(http=http)
+            par_search = ParallelDynamicSearch(
+                search_maker=search_maker,
+                search_iteration=search_iter,
+                country_codes=SearchableCountries.ALL,
+            )
+
+            for rec in par_search.run():
+                writer.write_row(rec)
