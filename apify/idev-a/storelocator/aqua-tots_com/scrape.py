0c5867c2376d9d2c985caaa79d2c856ed6600475
idev-a <ideveloper003@protonmail.com>
2021-10-07 10:19:59 -0700

    f

--- apify/idev-a/storelocator/aqua-tots_com/scrape.py
+++ apify/idev-a/storelocator/aqua-tots_com/scrape.py
@@ -3,12 +3,14 @@ from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
 import json
 from sglogging import SgLogSetup
+from bs4 import BeautifulSoup as bs
 
 logger = SgLogSetup().get_logger("aqua-tots")
 
 _headers = {
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
 }
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 
 def fetch_data():
@@ -20,6 +22,8 @@ def fetch_data():
             res.split("var GTU_L_Locations =")[1].split("/* ]]> */")[0].strip()[:-1]
         )
         for _ in locations:
+            if not _["ACF"]["street"]:
+                continue
             if _["ACF"].get("coming_soon") == "yes":
                 continue
             if (_["ACF"].get("country") or "US") not in ["US", "CA"]:
@@ -27,50 +31,10 @@ def fetch_data():
                 continue
             page_url = f"{locator_domain}{_['post_name']}"
             logger.info(page_url)
-            hours = []
-            time = (
-                "closed"
-                if _["ACF"]["gtu_l_hours_mon"]["mon_business_open"] != "1"
-                else f"{_['ACF']['gtu_l_hours_mon']['mon_open']}-{_['ACF']['gtu_l_hours_mon']['mon_close']}"
-            )
-            hours += [f"Mon: {time}"]
-            time = (
-                "closed"
-                if _["ACF"]["gtu_l_hours_tue"]["tue_business_open"] != "1"
-                else f"{_['ACF']['gtu_l_hours_tue']['tue_open']}-{_['ACF']['gtu_l_hours_tue']['tue_close']}"
-            )
-            hours += [f"Tue: {time}"]
-            time = (
-                "closed"
-                if _["ACF"]["gtu_l_hours_wed"]["wed_business_open"] != "1"
-                else f"{_['ACF']['gtu_l_hours_wed']['wed_open']}-{_['ACF']['gtu_l_hours_wed']['wed_close']}"
-            )
-            hours += [f"Wed: {time}"]
-            time = (
-                "closed"
-                if _["ACF"]["gtu_l_hours_thu"]["thu_business_open"] != "1"
-                else f"{_['ACF']['gtu_l_hours_thu']['thu_open']}-{_['ACF']['gtu_l_hours_thu']['thu_close']}"
-            )
-            hours += [f"Thu: {time}"]
-            time = (
-                "closed"
-                if _["ACF"]["gtu_l_hours_fri"]["fri_business_open"] != "1"
-                else f"{_['ACF']['gtu_l_hours_fri']['fri_open']}-{_['ACF']['gtu_l_hours_fri']['fri_close']}"
-            )
-            hours += [f"Fri: {time}"]
-            time = (
-                "closed"
-                if _["ACF"]["gtu_l_hours_sat"]["sat_business_open"] != "1"
-                else f"{_['ACF']['gtu_l_hours_sat']['sat_open']}-{_['ACF']['gtu_l_hours_sat']['sat_close']}"
-            )
-            hours += [f"Sat: {time}"]
-            time = (
-                "closed"
-                if _["ACF"]["gtu_l_hours_sun"]["sun_business_open"] != "1"
-                else f"{_['ACF']['gtu_l_hours_sun']['sun_open']}-{_['ACF']['gtu_l_hours_sun']['sun_close']}"
-            )
-            hours += [f"Sun: {time}"]
-
+            sp1 = bs(session.get(page_url, headers=_headers).text, "lxml")
+            hours = [
+                ": ".join(hh.stripped_strings) for hh in sp1.select("div#hours div.row")
+            ]
             yield SgRecord(
                 page_url=page_url,
                 store_number=_["ID"],
