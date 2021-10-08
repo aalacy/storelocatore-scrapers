71adb22a1492204c54ce803faac9ce65b3203e46
idev-a <ideveloper003@protonmail.com>
2021-10-07 23:16:45 -0700

    f

--- apify/idev-a/storelocator/pandaexpress_ae/scrape.py
+++ apify/idev-a/storelocator/pandaexpress_ae/scrape.py
@@ -3,6 +3,7 @@ from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgpostal import parse_address_intl
 
 _headers = {
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
@@ -10,6 +11,8 @@ _headers = {
 
 locator_domain = "https://www.pandaexpress.ae"
 base_url = "https://www.pandaexpress.ae/en/userlocation/searchbycoordinates?lat=34.0667&lng=-118.0833&desiredDistance=25&maxDistance=50&page=0&limit=25&hours=true&filters=%7B%7D"
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 
 def fetch_data():
@@ -21,23 +24,45 @@ def fetch_data():
                 _.get("OperationalHours", {}).get("Normal", {}).get("Hours", {}).items()
             ):
                 if not hh["StartTime"]:
-                    break
-                hours.append(f"{day}: {hh['StartTime']} - {hh['EndTime']}")
+                    times = "Closed"
+                else:
+                    times = f"{hh['StartTime']} - {hh['EndTime']}"
+                hours.append(f"{day}: {times}")
+            if not hours and _.get("OperationalHours", {}).get("Normal", {}).get(
+                "HasHours"
+            ):
+                for day in days:
+                    hours.append(f"{day}: closed")
             location_type = ""
             if _["TemporarilyClosed"]:
                 location_type = "Temporarily Closed"
             page_url = f"https://www.pandaexpress.ae/userlocation/{_['Id']}/{_['City']}/{_['Address'].replace(' ', '-')}"
+            addr = parse_address_intl(_["Address"])
+            street_address = addr.street_address_1
+            if addr.street_address_2:
+                street_address += " " + addr.street_address_2
+            if not street_address:
+                street_address = _["Address"]
+            state = _["State"]
+            city = _["City"]
+            if addr.city:
+                city = addr.city
+            if addr.state:
+                state = addr.state
+            country_code = _["Country"]
+            if addr.country:
+                country_code = addr.country
             yield SgRecord(
                 page_url=page_url,
                 store_number=_["Id"],
                 location_name=_["Name"],
-                street_address=_["Address"],
-                city=_["City"],
-                state=_["State"],
+                street_address=street_address,
+                city=city,
+                state=state,
                 zip_postal=_["Zip"],
                 latitude=_["Latitude"],
                 longitude=_["Longitude"],
-                country_code="UAE",
+                country_code=country_code,
                 phone=_["Phone"],
                 locator_domain=locator_domain,
                 location_type=location_type,
