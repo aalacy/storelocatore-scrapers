0eae2014c53f21035ac7d1622d97856b4cdc448c
dima <shshipovald@mail.ru>
2021-10-18 13:56:35 +0300

    fix chilis_hn

--- apify/Shapoval1/storelocator/chilis_hn/scrape.py
+++ apify/Shapoval1/storelocator/chilis_hn/scrape.py
@@ -1,8 +1,18 @@
+import ssl
 from sgscrape.sgrecord import SgRecord
 from sgrequests import SgRequests
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgrecord_deduper import SgRecordDeduper
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 
 def fetch_data(sgw: SgWriter):
