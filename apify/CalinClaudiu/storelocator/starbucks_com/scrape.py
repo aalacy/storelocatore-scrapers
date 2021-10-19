76f581ccb9fabe8038f4bdbbe3825ceec36b94b1
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2021-10-19 22:14:18 +0200

    Update scrape.py

--- apify/CalinClaudiu/storelocator/starbucks_com/scrape.py
+++ apify/CalinClaudiu/storelocator/starbucks_com/scrape.py
@@ -256,7 +256,7 @@ if __name__ == "__main__":
     with SgWriter(
         deduper=SgRecordDeduper(RecommendedRecordIds.StoreNumAndPageUrlId)
     ) as writer:
-        search_iter = ExampleSearchIteration()
+        search_iter = ExampleSearchIteration(http=None)
         par_search = ParallelDynamicSearch(
             search_maker=search_maker,
             search_iteration=search_iter,
