874dd994c34d80e2d745b1d347fa52af73f2e0eb
nsxdarin <nsxdarin@gmail.com>
2021-10-21 12:40:16 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/coach_com/scrape.py
+++ apify/nsxdarin/storelocator/coach_com/scrape.py
@@ -20,9 +20,7 @@ def fetch_data():
     cities = []
     countries = []
     r = session.get(main_url, headers=headers)
-    if r.encoding is None:
-        r.encoding = "utf-8"
-    for line in r.iter_lines(decode_unicode=True):
+    for line in r.iter_lines():
         if 'name="dwfrm_storelocator_address_international" >' in line:
             info = (
                 line.split('name="dwfrm_storelocator_address_international" >')[1]
@@ -53,7 +51,6 @@ def fetch_data():
             r2 = session.get(url, headers=headers)
             allinfo = ""
             for line2 in r2.iter_lines():
-                line2 = str(line2.decode("utf-8"))
                 allinfo = allinfo + line2.replace("\r", "").replace("\n", "").replace(
                     "\t", ""
                 )
@@ -105,8 +102,10 @@ def fetch_data():
                         phone = phone.replace("&#40;", "(").replace("&#41;", ")")
                         if "outlet" in name.lower():
                             typ = "Coach Outlet"
+                            name = "Coach Outlet"
                         else:
                             typ = "Coach"
+                            name = "Coach"
                         if "popup" not in name.lower() and "pop-up" not in name.lower():
                             yield SgRecord(
                                 locator_domain=website,
@@ -128,7 +127,6 @@ def fetch_data():
     locs = []
     r = session.get(main_url, headers=headers)
     for line in r.iter_lines():
-        line = str(line.decode("utf-8"))
         if '<a class="Directory-listLink" href="' in line:
             items = line.split('<a class="Directory-listLink" href="')
             for item in items:
@@ -143,7 +141,6 @@ def fetch_data():
         logger.info(state)
         r2 = session.get(state, headers=headers)
         for line2 in r2.iter_lines():
-            line2 = str(line2.decode("utf-8"))
             if 'class="Directory-listLink" href="' in line2:
                 items = line2.split('class="Directory-listLink" href="')
                 for item in items:
@@ -161,7 +158,6 @@ def fetch_data():
         logger.info(city)
         r2 = session.get(city, headers=headers)
         for line2 in r2.iter_lines():
-            line2 = str(line2.decode("utf-8"))
             if '"visitpage" href="../' in line2:
                 items = line2.split('"visitpage" href="../')
                 for item in items:
@@ -187,7 +183,6 @@ def fetch_data():
         store = "<MISSING>"
         r2 = session.get(loc, headers=headers)
         for line2 in r2.iter_lines():
-            line2 = str(line2.decode("utf-8"))
             if 'emprop="openingHours" content="' in line2:
                 days = line2.split('emprop="openingHours" content="')
                 for day in days:
@@ -245,7 +240,6 @@ def fetch_data():
     locs = []
     r = session.get(main_url, headers=headers)
     for line in r.iter_lines():
-        line = str(line.decode("utf-8"))
         if '<a class="Directory-listLink" href="' in line:
             items = line.split('<a class="Directory-listLink" href="')
             for item in items:
@@ -260,7 +254,6 @@ def fetch_data():
         logger.info(state)
         r2 = session.get(state, headers=headers)
         for line2 in r2.iter_lines():
-            line2 = str(line2.decode("utf-8"))
             if 'class="Directory-listLink" href="' in line2:
                 items = line2.split('class="Directory-listLink" href="')
                 for item in items:
@@ -278,7 +271,6 @@ def fetch_data():
         logger.info(city)
         r2 = session.get(city, headers=headers)
         for line2 in r2.iter_lines():
-            line2 = str(line2.decode("utf-8"))
             if '"visitpage" href="../' in line2:
                 items = line2.split('"visitpage" href="../')
                 for item in items:
@@ -304,7 +296,6 @@ def fetch_data():
         store = "<MISSING>"
         r2 = session.get(loc, headers=headers)
         for line2 in r2.iter_lines():
-            line2 = str(line2.decode("utf-8"))
             if 'emprop="openingHours" content="' in line2:
                 days = line2.split('emprop="openingHours" content="')
                 for day in days:
@@ -367,7 +358,6 @@ def fetch_data():
         country = "GB"
         logger.info("Pulling Stores")
         for line in r.iter_lines():
-            line = str(line.decode("utf-8"))
             if 'meta itemprop="name" content="' in line:
                 items = line.split('meta itemprop="name" content="')
                 for item in items:
@@ -415,6 +405,7 @@ def fetch_data():
                             or "coach williams &" in name.lower()
                         ):
                             typ = "Coach Department & Specialty Store"
+                        name = typ
                         if "popup" not in name.lower() and "pop-up" not in name.lower():
                             yield SgRecord(
                                 locator_domain=website,
