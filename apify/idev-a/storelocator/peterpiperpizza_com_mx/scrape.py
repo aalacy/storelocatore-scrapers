8b7f988708dfdb58a1554b15aa43bc2b5167b3c2
idev-a <ideveloper003@protonmail.com>
2021-10-21 23:27:25 -0700

    f

--- apify/idev-a/storelocator/peterpiperpizza_com_mx/scrape.py
+++ apify/idev-a/storelocator/peterpiperpizza_com_mx/scrape.py
@@ -4,7 +4,7 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup as bs
 from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgpostal import parse_address_intl
+from sgpostal.sgpostal import parse_address_intl
 
 _headers = {
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
