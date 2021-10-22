e920d4d21a2a78f665831aa8e6fd38309242e081
mtaziz <mdtariqaziz@gmail.com>
2021-10-23 02:59:17 +0600

    scrape updated

--- apify/mtaziz/storelocator/dominos_com/scrape.py
+++ apify/mtaziz/storelocator/dominos_com/scrape.py
@@ -7,7 +7,6 @@ from sgscrape.sgrecord_deduper import SgRecordDeduper
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from lxml import html
-import datetime
 import time
 from tenacity import retry, stop_after_attempt
 import tenacity
