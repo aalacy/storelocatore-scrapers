aff29dbce3bf5bf3a9a084f33a82f236f6fa5f3f
idev-a <ideveloper003@protonmail.com>
2021-10-25 15:45:02 -0700

    f

--- apify/idev-a/storelocator/theuppercrustpizzeria_com/scrape.py
+++ apify/idev-a/storelocator/theuppercrustpizzeria_com/scrape.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup as bs
 from sglogging import SgLogSetup
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-import re
 
 logger = SgLogSetup().get_logger("theuppercrustpizzeria")
 
