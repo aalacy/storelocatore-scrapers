a36e63187a90c13014088b994c5b7875b06b7ea3
ihor <rzhdgqrr@gmail.com>
2021-10-28 15:28:40 +0300

    fix

--- apify/MrQwerty1/storelocator/fiatcanada_com/scrape.py
+++ apify/MrQwerty1/storelocator/fiatcanada_com/scrape.py
@@ -82,19 +82,25 @@ def fetch_data(la, ln, sgw: SgWriter):
 if __name__ == "__main__":
     session = SgRequests()
     headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'ru,en-US;q=0.7,en;q=0.3',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'cross-site',
-        'Cache-Control': 'max-age=0',
-        'TE': 'trailers',
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "ru,en-US;q=0.7,en;q=0.3",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "cross-site",
+        "Cache-Control": "max-age=0",
+        "TE": "trailers",
     }
     locator_domain = "https://www.fiatcanada.com/"
-    search = DynamicGeoSearch(country_codes=[SearchableCountries.CANADA], expected_search_radius_miles=100)
-    with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId, duplicate_streak_failure_factor=-1)) as writer:
+    search = DynamicGeoSearch(
+        country_codes=[SearchableCountries.CANADA], expected_search_radius_miles=100
+    )
+    with SgWriter(
+        SgRecordDeduper(
+            RecommendedRecordIds.PageUrlId, duplicate_streak_failure_factor=-1
+        )
+    ) as writer:
         for lat, lng in search:
             fetch_data(lat, lng, writer)
