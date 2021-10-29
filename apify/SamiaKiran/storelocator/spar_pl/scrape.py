b821d0063887e5c38ea22da4c029c95c4be9b9b4
Samia Kiran <samiakiran17@gmail.com>
2021-10-29 15:48:07 +0500

    Update scrape.py

--- apify/SamiaKiran/storelocator/spar_pl/scrape.py
+++ apify/SamiaKiran/storelocator/spar_pl/scrape.py
@@ -20,15 +20,14 @@ MISSING = SgRecord.MISSING
 
 payload = "action=shopsincity&lat=52.1799062&lng=20.9478161&distance=10000"
 headers = {
-  'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
 }
 
+
 def strip_accents(text):
-    
-    text = unicodedata.normalize('NFD', text)\
-           .encode('ascii', 'ignore')\
-           .decode("utf-8")
+
+    text = unicodedata.normalize("NFD", text).encode("ascii", "ignore").decode("utf-8")
 
     return str(text)
 
@@ -36,12 +35,12 @@ def strip_accents(text):
 def fetch_data():
     if True:
         url = "https://spar.pl/wp-admin/admin-ajax.php"
-        loclist = session.post(url, headers=headers, data= payload).json()['locations']
+        loclist = session.post(url, headers=headers, data=payload).json()["locations"]
         for loc in loclist:
             temp = loc
-            page_url = temp['permalink']
+            page_url = temp["permalink"]
             log.info(page_url)
-            store_number = temp['id']
+            store_number = temp["id"]
             street_address = strip_accents(temp["adres"])
             city = strip_accents(temp["miasto"])
             state = MISSING
@@ -52,32 +51,54 @@ def fetch_data():
             longitude = temp["lng"]
             r = session.get(page_url, headers=headers)
             soup = BeautifulSoup(r.text, "html.parser")
-            mon = temp['poniedzialek']
-            if not mon :
+            mon = temp["poniedzialek"]
+            if not mon:
                 mon = MISSING
-            tue = temp['poniedzialek']
-            if not tue :
+            tue = temp["poniedzialek"]
+            if not tue:
                 tue = MISSING
-            wed = temp['poniedzialek']
-            if not wed :
+            wed = temp["poniedzialek"]
+            if not wed:
                 wed = MISSING
-            thu = temp['poniedzialek']
-            if not thu :
+            thu = temp["poniedzialek"]
+            if not thu:
                 thu = MISSING
-            fri = temp['poniedzialek']
-            if not fri :
+            fri = temp["poniedzialek"]
+            if not fri:
                 fri = MISSING
-            sat = temp['poniedzialek']
-            if not sat :
+            sat = temp["poniedzialek"]
+            if not sat:
                 sat = MISSING
-            sun = temp['poniedzialek']
-            if not sun :
+            sun = temp["poniedzialek"]
+            if not sun:
                 sun = MISSING
-            hours_of_operation = ' mon '+mon+' tue '+tue+' wed ' +wed+' thu '+thu+' fri '+fri+' sat '+sat+' sun '+sun
-            if hours_of_operation=='mon <MISSING> tue <MISSING> wed <MISSING> thu <MISSING> fri <MISSING> sat <MISSING> sun <MISSING>':
+            hours_of_operation = (
+                " mon "
+                + mon
+                + " tue "
+                + tue
+                + " wed "
+                + wed
+                + " thu "
+                + thu
+                + " fri "
+                + fri
+                + " sat "
+                + sat
+                + " sun "
+                + sun
+            )
+            if (
+                hours_of_operation
+                == "mon <MISSING> tue <MISSING> wed <MISSING> thu <MISSING> fri <MISSING> sat <MISSING> sun <MISSING>"
+            ):
                 hours_of_operation = MISSING
-            phone = soup.find("div", {"class": "flex-x white"}).get_text(separator='|', strip=True).split('|')[-1]
-            if 'sprawdź jak dojechać' in phone:
+            phone = (
+                soup.find("div", {"class": "flex-x white"})
+                .get_text(separator="|", strip=True)
+                .split("|")[-1]
+            )
+            if "sprawdź jak dojechać" in phone:
                 phone = MISSING
             yield SgRecord(
                 locator_domain=DOMAIN,
