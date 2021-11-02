892b94c9213a0d79fde8edda50b84e659adefbcc
ihor <rzhdgqrr@gmail.com>
2021-11-02 17:20:53 +0200

    fix

--- apify/MrQwerty1/storelocator/blazingonion_com/scrape.py
+++ apify/MrQwerty1/storelocator/blazingonion_com/scrape.py
@@ -43,7 +43,7 @@ def get_data(page_url, sgw: SgWriter):
         .strip()
     )
 
-    _tmp = []  # type: List[str]
+    _tmp = []  # type: list[str]
     hours = tree.xpath("//div[@id='ctl01_pSpanDesc']//text()")
     hours = list(filter(None, [h.strip() for h in hours]))
 
