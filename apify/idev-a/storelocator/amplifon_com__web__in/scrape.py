feae6894f8372c09c7024a5c662b6c049ebe31ae
idev-a <ideveloper003@protonmail.com>
2021-11-06 13:16:50 -0700

    f

--- apify/idev-a/storelocator/amplifon_com__web__in/scrape.py
+++ apify/idev-a/storelocator/amplifon_com__web__in/scrape.py
@@ -9,7 +9,7 @@ _headers = {
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
 }
 
-locator_domain = ""
+locator_domain = "https://www.amplifon.com"
 urls = {
     "IN": "https://www.amplifon.com/web/in/store-locator",
     "HU": "https://www.amplifon.com/web/hu/hallaskozpont-kereso",
