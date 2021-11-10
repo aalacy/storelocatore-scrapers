986504a6796f65a2c8f007d447f17deccef01312
idev-a <ideveloper003@protonmail.com>
2021-11-10 13:25:40 -0800

    f

--- apify/idev-a/storelocator/amplifonusa_com/scrape.py
+++ apify/idev-a/storelocator/amplifonusa_com/scrape.py
@@ -8,7 +8,17 @@ from sglogging import SgLogSetup
 import time
 from bs4 import BeautifulSoup as bs
 from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
+import ssl
 
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
+    
 logger = SgLogSetup().get_logger("amplifonusa")
 
 _headers = {
