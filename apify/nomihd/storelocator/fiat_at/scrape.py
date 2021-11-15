8d958ee9629920559cde34d21137e7de519ffc4d
nomihd <nouman.awais.nu@gmail.com>
2021-11-15 17:42:34 +0500

    remove unused import

--- apify/nomihd/storelocator/fiat_at/scrape.py
+++ apify/nomihd/storelocator/fiat_at/scrape.py
@@ -8,7 +8,6 @@ from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.pause_resume import CrawlStateSingleton
-from sgzip.dynamic import SearchableCountries
 from sgzip.parallel import DynamicSearchMaker, ParallelDynamicSearch, SearchIteration
 
 
