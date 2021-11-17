5bc96ab28ccbdbb75c8170b997533b0c73e03d5f
ihor <rzhdgqrr@gmail.com>
2021-11-17 20:37:19 +0200

    add vodafone_nl

--- apify/MrQwerty1/storelocator/vodafone_nl/scrape.py
+++ apify/MrQwerty1/storelocator/vodafone_nl/scrape.py
@@ -6,29 +6,28 @@ from sgscrape.sgrecord_id import RecommendedRecordIds
 
 
 def fetch_data(sgw: SgWriter):
-    api = "https://www.vodafone.gr/service/?type=getStores"
+    api = "https://localistico-production.s3.eu-west-1.amazonaws.com/exports/locations/json/business-694cbc6c-9274-48b0-b509-f989f7febed2-locations-data-20211117085037.json"
+    page_url = "https://www.vodafone.nl/support/contact/vind-een-winkel"
     r = session.get(api, headers=headers)
     js = r.json()
 
     for j in js:
-        location_name = j.get("name")
-        store_number = j.get("id")
-        latitude = j.get("lat")
-        longitude = j.get("lng")
-        slug = j.get("path") or ""
-        page_url = f"https://www.vodafone.gr{slug}"
-        phone = j.get("telephone")
-        street_address = j.get("address_name")
-        city = j.get("city")
-        state = j.get("area")
-        postal = j.get("postal_code")
+        location_name = j.get("location_name")
+        store_number = j.get("location_code")
+        latitude = j.get("location_lat")
+        longitude = j.get("location_lng")
+        phone = j.get("phone")
+        street_address = f'{j.get("street_address")} {j.get("street_address_second_line") or ""}'.strip()
+        city = j.get("locality")
+        state = j.get("region")
+        postal = j.get("postcode")
+        hours = j.get("hours") or ""
 
         _tmp = []
-        hours = j.get("workingHours") or []
+        hours = hours.split("|")
         for h in hours:
-            day = h.get("day")
-            inter = h.get("hours")
-            _tmp.append(f"{day}: {inter}")
+            if not h.endswith(":"):
+                _tmp.append(h)
 
         hours_of_operation = ";".join(_tmp)
 
@@ -39,7 +38,7 @@ def fetch_data(sgw: SgWriter):
             city=city,
             state=state,
             zip_postal=postal,
-            country_code="GR",
+            country_code="NL",
             phone=phone,
             store_number=store_number,
             latitude=latitude,
@@ -52,7 +51,7 @@ def fetch_data(sgw: SgWriter):
 
 
 if __name__ == "__main__":
-    locator_domain = "https://www.vodafone.gr/"
+    locator_domain = "https://www.vodafone.nl/"
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0",
         "Accept": "application/json, text/javascript, */*; q=0.01",
