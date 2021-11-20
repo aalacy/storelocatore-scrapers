a346c64b00aac1fcfc66462789d7775337082735
idev-a <ideveloper003@protonmail.com>
2021-11-20 04:09:19 -0800

    f

--- apify/idev-a/storelocator/namasteplaza_com/scrape.py
+++ apify/idev-a/storelocator/namasteplaza_com/scrape.py
@@ -1,5 +1,7 @@
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup as bs
 from sglogging import SgLogSetup
@@ -26,39 +28,38 @@ def fetch_data():
             if res.status_code != 200:
                 continue
             sp1 = bs(res.text, "lxml")
-            addr = parse_address_intl(
+            raw_address = (
                 sp1.select_one("div.footer-cms div.footer-contacts")
                 .find_next_sibling()
                 .find_next_sibling()
                 .text.strip()
             )
+            addr = parse_address_intl(raw_address)
             street_address = addr.street_address_1
             if addr.street_address_2:
                 street_address += " " + addr.street_address_2
-            try:
-                yield SgRecord(
-                    page_url=page_url,
-                    location_name=option.text.strip(),
-                    street_address=street_address,
-                    city=addr.city,
-                    state=addr.state,
-                    zip_postal=addr.postcode,
-                    country_code="US",
-                    phone=sp1.select_one("div.footer-cms div.footer-contacts")
-                    .find_next_sibling()
-                    .find_next_sibling()
-                    .find_next_sibling()
-                    .text.strip(),
-                    locator_domain=locator_domain,
-                )
-            except:
-                import pdb
-
-                pdb.set_trace()
+            yield SgRecord(
+                page_url=page_url,
+                location_name=option.text.strip(),
+                street_address=street_address,
+                city=addr.city,
+                state=addr.state,
+                zip_postal=addr.postcode,
+                country_code="US",
+                phone=sp1.select_one("div.footer-cms div.footer-contacts")
+                .find_next_sibling()
+                .find_next_sibling()
+                .find_next_sibling()
+                .text.strip(),
+                locator_domain=locator_domain,
+                raw_address=raw_address,
+            )
 
 
 if __name__ == "__main__":
-    with SgWriter() as writer:
+    with SgWriter(
+        SgRecordDeduper(SgRecordID({SgRecord.Headers.RAW_ADDRESS}))
+    ) as writer:
         results = fetch_data()
         for rec in results:
             writer.write_row(rec)
