2ffb8c67f18ec8f0fd0cce4b14b875d7f1240740
nomihd <nouman.awais.nu@gmail.com>
2021-11-30 17:34:41 +0500

    remove csv

--- apify/nsxdarin/storelocator/banfield_com/scrape.py
+++ apify/nsxdarin/storelocator/banfield_com/scrape.py
@@ -25,33 +25,6 @@ headers = {
 }
 
 
-def write_output(data):
-    with open("data.csv", mode="w") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        for row in data:
-            writer.writerow(row)
-
-
 def fetch_data():
     stores_req = session.get(
         "https://www.banfield.com/locations/hospitals-by-state", headers=headers
