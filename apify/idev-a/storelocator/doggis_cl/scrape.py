7188ba1a26ca152a9923f975f5bd8407d1174f78
idev-a <ideveloper003@protonmail.com>
2021-12-08 01:11:10 -0800

    f

---
+++ apify/idev-a/storelocator/doggis_cl/scrape.py
@@ -0,0 +1,64 @@
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
+from sgselenium import SgChrome
+from bs4 import BeautifulSoup as bs
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from sgpostal.sgpostal import parse_address_intl
+
+locator_domain = "https://www.doggis.cl"
+base_url = "https://www.doggis.cl/locales"
+graph_url = "https://api.getjusto.com/graphql?operationName=getPlaceDetails_cached"
+
+
+def fetch_data():
+    with SgChrome() as driver:
+        driver.get(base_url)
+        # driver.wait_for_request(graph_url)
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(
+                (
+                    By.CSS_SELECTOR,
+                    "div.ct-stores",
+                )
+            )
+        )
+        soup = bs(driver.page_source, "lxml")
+        locations = soup.select("div.ct-stores div.col-xs-12.col-sm-6.col-md-3")
+        for loc in locations:
+            info = (
+                loc.findChildren("div", recursive=False)[0]
+                .findChildren("div", recursive=False)[0]
+                .findChildren("div", recursive=False)
+            )
+            raw_address = list(info[1].stripped_strings)[0]
+            addr = parse_address_intl(raw_address)
+            street_address = addr.street_address_1
+            if addr.street_address_2:
+                street_address += " " + addr.street_address_2
+            yield SgRecord(
+                page_url=base_url,
+                location_name=info[0].text.strip(),
+                street_address=street_address,
+                city=addr.city,
+                state=addr.state,
+                zip_postal=addr.postcode,
+                country_code="Chile",
+                locator_domain=locator_domain,
+                raw_address=raw_address,
+            )
+
+
+if __name__ == "__main__":
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID({SgRecord.Headers.RAW_ADDRESS, SgRecord.Headers.LOCATION_NAME})
+        )
+    ) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
