4e1f048b04f333e18ba994193ee74a796a6c31d3
idev-a <ideveloper003@protonmail.com>
2021-12-09 11:24:52 -0800

    f

--- apify/idev-a/storelocator/ey_com/scrape.py
+++ apify/idev-a/storelocator/ey_com/scrape.py
@@ -1,7 +1,6 @@
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
-from sgpostal.sgpostal import parse_address_intl
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
