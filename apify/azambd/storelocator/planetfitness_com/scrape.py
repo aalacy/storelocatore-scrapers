7f0c87affbe611328aaa9d5e34e4fdb6b412eab8
Faruque A <engrfazam@gmail.com>
2021-12-10 20:23:48 +0600

    SLC-23362: fixed

--- apify/azambd/storelocator/planetfitness_com/scrape.py
+++ apify/azambd/storelocator/planetfitness_com/scrape.py
@@ -4,7 +4,6 @@ from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import RecommendedRecordIds
-
 from sgrequests import SgRequests
 from sglogging import sglog
 
