7c2a5bb53dfa3350aaf6af9fc0046db39018b4f3
mtaziz <mdtariqaziz@gmail.com>
2021-12-11 20:05:48 +0600

    scrape redirect fix 3

--- apify/CalinClaudiu/storelocator/hilton_co_uk/scrape.py
+++ apify/CalinClaudiu/storelocator/hilton_co_uk/scrape.py
@@ -114,7 +114,9 @@ def get_response_redirect(urlnum, country, url_to_redirect):
                                 f"Redirect POST HTTP Status Code: {r_redir_post.status_code}"
                             )
                             return r_redir_post
-                        raise Exception(f"{urlnum} : {redir_url} >> Temporary Error: {hot2}")
+                        raise Exception(
+                            f"{urlnum} : {redir_url} >> Temporary Error: {hot2}"
+                        )
                     raise Exception(
                         f"{urlnum} : {redir_url} >> Temporary Error: {hot_redir}"
                     )
