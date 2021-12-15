d0fa9f0e7fbd9b500af4a34d2e044e87479ab98b
idev-a <ideveloper003@protonmail.com>
2021-12-15 00:44:17 -0800

    f

--- apify/idev-a/storelocator/leedsbuildingsociety_co_uk/scrape.py
+++ apify/idev-a/storelocator/leedsbuildingsociety_co_uk/scrape.py
@@ -5,7 +5,16 @@ from bs4 import BeautifulSoup as bs
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sglogging import SgLogSetup
-import dirtyjson as json
+import ssl
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 logger = SgLogSetup().get_logger("uk")
 
