dd637d074475956594fd921e84580b72ab55163d
datarhan <slutchenko@gmail.com>
2021-12-15 10:18:43 +0100

    [SLC-23833] Add crawler for telepizza.pl

---
+++ apify/datarhan/storelocator/telepizza_pl/scrape.py
@@ -0,0 +1,71 @@
+import re
+from lxml import etree
+from urllib.parse import urljoin
+
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgwriter import SgWriter
+from sgselenium.sgselenium import SgFirefox
+
+
+def fetch_data():
+    start_url = "https://www.telepizza.pl/lokale"
+    domain = "telepizza.pl"
+
+    with SgFirefox() as driver:
+        driver.get(start_url)
+        dom = etree.HTML(driver.page_source)
+
+        all_locations = dom.xpath('//div[@id="local_list_body"]//a/@href')
+        for url in list(set(all_locations)):
+            if url == "#":
+                continue
+            page_url = urljoin(start_url, url)
+            driver.get(page_url)
+            loc_dom = etree.HTML(driver.page_source)
+            location_name = loc_dom.xpath("//section/h2/text()")[0].split(", ")[0]
+            city = " ".join(location_name.split()[1:])
+            street_address = loc_dom.xpath("//section/h2/text()")[0].split(", ")[-1]
+            phone = loc_dom.xpath('//span[contains(@class, "phone")]/text()')[0]
+            latitude = re.findall(r"lat: (.+?),", driver.page_source)[0]
+            longitude = re.findall(r"lng: (.+?) \}", driver.page_source)[0]
+            hoo = loc_dom.xpath(
+                '//span[contains(text(), "Godziny otwarcia")]/following-sibling::span/text()'
+            )
+            hoo = ", ".join([e.strip() for e in hoo]).replace("LOCAL:,", "")
+
+            item = SgRecord(
+                locator_domain=domain,
+                page_url=page_url,
+                location_name=location_name,
+                street_address=street_address,
+                city=city,
+                state="",
+                zip_postal="",
+                country_code="PL",
+                store_number="",
+                phone=phone,
+                location_type="",
+                latitude=latitude,
+                longitude=longitude,
+                hours_of_operation=hoo,
+            )
+
+            yield item
+
+
+def scrape():
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID(
+                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
+            )
+        )
+    ) as writer:
+        for item in fetch_data():
+            writer.write_row(item)
+
+
+if __name__ == "__main__":
+    scrape()
