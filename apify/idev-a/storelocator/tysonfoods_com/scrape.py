4a5721af5916640c790deb7bc8a717c2b7f93b83
idev-a <ideveloper003@protonmail.com>
2021-12-18 03:31:45 -0800

    f

--- apify/idev-a/storelocator/tysonfoods_com/scrape.py
+++ apify/idev-a/storelocator/tysonfoods_com/scrape.py
@@ -10,6 +10,26 @@ locator_domain = "https://www.tysonfoods.com/"
 page_url = "https://www.tysonfoods.com/contact-us"
 
 
+def _p(val):
+    if (
+        val
+        and val.replace("Phone:", "")
+        .replace("TYSON", "")
+        .replace("(", "")
+        .replace(")", "")
+        .replace("+", "")
+        .replace("-", "")
+        .replace(".", " ")
+        .replace("to", "")
+        .replace(" ", "")
+        .strip()
+        .isdigit()
+    ):
+        return val
+    else:
+        return ""
+
+
 def fetch_data(sgw, session):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"
@@ -36,31 +56,41 @@ def fetch_data(sgw, session):
             lines = list(filter(None, [line.strip() for line in lines]))
             line = lines[-1]
 
-            if line.startswith("+") or line[0].isdigit() or "Phone" in line:
+            if _p(line):
                 phone = "".join(
                     lines.pop().replace("Phone:", "").replace("TYSON", "").split()
                 )
 
             raw_address = ", ".join(lines).replace(",,", ",")
-            addr = parse_address_intl(raw_address)
-            street_address = addr.street_address_1
-            if addr.street_address_2:
-                street_address += " " + addr.street_address_2
+            street_address = city = state = zip_postal = country_code = ""
+            if "Netherlands" in raw_address:
+                addr = raw_address.split(",")
+                zip_postal = " ".join(addr[-2].strip().split()[:-1])
+                city = addr[-2].strip().split()[-1]
+                street_address = ", ".join(addr[:-2])
+                country_code = "Netherlands"
+            else:
+                addr = parse_address_intl(raw_address)
+                street_address = addr.street_address_1
+                if addr.street_address_2:
+                    street_address += " " + addr.street_address_2
+                city = addr.city
+                state = addr.state
+                zip_postal = addr.postcode
+                country_code = addr.country if addr.country else country
 
-            if country_code == SgRecord.MISSING:
-                country_code = country
-            if postal == SgRecord.MISSING and country_code == "Mexico":
-                postal = raw_address.split(",")[-2].strip()
+            if not zip_postal and country_code == "Mexico":
+                zip_postal = raw_address.split(",")[-2].strip()
 
             sgw.write_row(
                 SgRecord(
                     page_url=page_url,
                     location_name=location_name,
                     street_address=street_address,
-                    city=addr.city,
-                    state=addr.state,
-                    zip_postal=addr.postcode,
-                    country_code=addr.ubuntu,
+                    city=city,
+                    state=state,
+                    zip_postal=zip_postal,
+                    country_code=country_code,
                     phone=phone,
                     locator_domain=locator_domain,
                     raw_address=raw_address,
@@ -70,7 +100,6 @@ def fetch_data(sgw, session):
 
 if __name__ == "__main__":
     with SgRequests() as session:
-
         with SgWriter(
             SgRecordDeduper(SgRecordID({SgRecord.Headers.RAW_ADDRESS}))
         ) as writer:
