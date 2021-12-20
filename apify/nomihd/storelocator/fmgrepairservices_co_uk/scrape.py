348c67536058e19f230b7ab74f9b8dc3b5e05209
nomihd <nouman.awais.nu@gmail.com>
2021-12-21 01:18:15 +0500

    fix

--- apify/nomihd/storelocator/fmgrepairservices_co_uk/scrape.py
+++ apify/nomihd/storelocator/fmgrepairservices_co_uk/scrape.py
@@ -5,7 +5,7 @@ from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgpostal import sgpostal as parser
 import lxml.html
-from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 website = "fmgrepairservices.co.uk"
