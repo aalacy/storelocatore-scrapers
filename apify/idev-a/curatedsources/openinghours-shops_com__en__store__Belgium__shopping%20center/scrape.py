b6a1b5415aaf2c66fb41f14b0d82f186577bff82
idev-a <ideveloper003@protonmail.com>
2021-12-24 02:35:39 -0800

    f

--- apify/idev-a/curatedsources/openinghours-shops_com__en__store__Belgium__shopping%20center/scrape.py
+++ apify/idev-a/curatedsources/openinghours-shops_com__en__store__Belgium__shopping%20center/scrape.py
@@ -32,55 +32,47 @@ def fetch_data():
                     page_url = locator_domain + _.a["href"]
                 except:
                     continue
-                try:
-                    logger.info(page_url)
-                    sp1 = bs(
-                        session.get(page_url.format(page), headers=_headers).text,
-                        "lxml",
-                    )
-                    addr = list(
-                        _.select_one("div.vestiging-info-adres").stripped_strings
-                    )
-                    # street_address = addr.street_address_1
-                    # if addr.street_address_2:
-                    #     street_address += ' ' + addr.street_address_2
-                    hours = []
-                    day = ""
-                    for hh in sp1.select("table.openingsuren tr"):
-                        td = hh.select("td")
-                        if hh.select_one("td.day"):
-                            day = list(hh.select_one("td.day").stripped_strings)[0]
+                logger.info(page_url)
+                sp1 = bs(
+                    session.get(page_url.format(page), headers=_headers).text,
+                    "lxml",
+                )
+                addr = list(
+                    _.select_one("div.vestiging-info-adres").stripped_strings
+                )
+                hours = []
+                day = ""
+                for hh in sp1.select("table.openingsuren tr"):
+                    td = hh.select("td")
+                    if hh.select_one("td.day"):
+                        day = list(hh.select_one("td.day").stripped_strings)[0]
 
-                        hours.append(f"{day}: {hh.select_one('td.open').text.strip()}")
+                    hours.append(f"{day}: {hh.select_one('td.open').text.strip()}")
 
-                    phone = ""
-                    if sp1.select_one("table.details").find(
-                        "a", href=re.compile(r"tel:")
-                    ):
-                        phone = (
-                            sp1.select_one("table.details")
-                            .find("a", href=re.compile(r"tel:"))
-                            .text.strip()
-                        )
-                    yield SgRecord(
-                        page_url=page_url,
-                        location_name=_.h3.text.strip(),
-                        street_address=addr[0],
-                        city=" ".join(addr[1].split()[1:]),
-                        zip_postal=addr[1].split()[0],
-                        country_code="Belgium",
-                        phone=phone,
-                        location_type=_.select_one(
-                            "div.vestiging-info-categorie"
-                        ).text.strip(),
-                        locator_domain=locator_domain,
-                        hours_of_operation="; ".join(hours),
-                        raw_address=" ".join(addr),
+                phone = ""
+                if sp1.select_one("table.details").find(
+                    "a", href=re.compile(r"tel:")
+                ):
+                    phone = (
+                        sp1.select_one("table.details")
+                        .find("a", href=re.compile(r"tel:"))
+                        .text.strip()
                     )
-                except:
-                    import pdb
-
-                    pdb.set_trace()
+                yield SgRecord(
+                    page_url=page_url,
+                    location_name=_.h3.text.strip(),
+                    street_address=addr[0],
+                    city=" ".join(addr[1].split()[1:]),
+                    zip_postal=addr[1].split()[0],
+                    country_code="Belgium",
+                    phone=phone,
+                    location_type=_.select_one(
+                        "div.vestiging-info-categorie"
+                    ).text.strip(),
+                    locator_domain=locator_domain,
+                    hours_of_operation="; ".join(hours),
+                    raw_address=" ".join(addr),
+                )
 
 
 if __name__ == "__main__":
