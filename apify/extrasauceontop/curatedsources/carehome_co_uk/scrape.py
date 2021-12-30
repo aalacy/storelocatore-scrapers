b43fb3eca17b0b95558ffe913bc22fc3f05059a2
extrasauceontop <msuchemba@gmail.com>
2021-12-29 18:22:57 -0600

    scrape

--- apify/extrasauceontop/curatedsources/carehome_co_uk/scrape.py
+++ apify/extrasauceontop/curatedsources/carehome_co_uk/scrape.py
@@ -120,7 +120,7 @@ def get_data():
         soup = bs(response_text, "html.parser")
 
         strong_tags = soup.find(
-            "div", attrs={"class": "row", "style": "margin-bottom:30px"}
+            "div", attrs={"class": "seo_links seo_links_country"}
         ).find_all("strong")
         country_urls = []
         location_urls = []
@@ -129,7 +129,7 @@ def get_data():
             url = a_tag["href"]
 
             if "searchcountry" in url:
-                country_urls.append(url)
+                country_urls.append("https://www.carehome.co.uk" + url)
 
         for country_url in country_urls:
 
@@ -279,16 +279,31 @@ def get_data():
         address_parts = soup.find("meta", attrs={"property": "og:title"})[
             "content"
         ].split(",")
-        address = address_parts[1].strip()
-        city = address_parts[-2].strip()
-        state_zipp_parts = address_parts[-1].split(" |")[0].split(" ")
-        state_parts = state_zipp_parts[:-2]
-        state = ""
-        for part in state_parts:
-            state = state + part + " "
-        state = state.strip().replace("County ", "")
 
-        zipp = state_zipp_parts[-2] + " " + state_zipp_parts[-1]
+        try:
+            address = address_parts[1].strip()
+            city = address_parts[-2].strip()
+            state_zipp_parts = address_parts[-1].split(" |")[0].split(" ")
+            state_parts = state_zipp_parts[:-2]
+            state = ""
+            for part in state_parts:
+                state = state + part + " "
+            state = state.strip().replace("County ", "")
+
+            zipp = state_zipp_parts[-2] + " " + state_zipp_parts[-1]
+        except Exception:
+            if len(address_parts) == 1:
+                address = "<MISSING>"
+                city = "<MISSING>"
+                state = "".join(part for part in address_parts[0].split(" ")[:-2])
+                zipp = (
+                    address_parts[0].split(" ")[1]
+                    + " "
+                    + address_parts[0].split(" ")[2]
+                )
+
+            else:
+                raise Exception
 
         country_code = "UK"
         store_number = location_url.split("/")[-1]
