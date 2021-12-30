2afd6cf104f621a236499926f7bca33bfbc54ad7
nsxdarin <nsxdarin@gmail.com>
2021-12-30 10:33:19 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/libertytax_com/scrape.py
+++ apify/nsxdarin/storelocator/libertytax_com/scrape.py
@@ -4,7 +4,6 @@ from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import RecommendedRecordIds
-import json
 
 logger = SgLogSetup().get_logger("libertytax_com")
 
