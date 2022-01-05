ccd995eef5bc4eba49d7ab199c84d448f19904e3
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2022-01-05 19:47:21 +0100

    SLC-23120
    https://safegraph-crawl.atlassian.net/browse/SLC-23120

--- apify/CalinClaudiu/storelocator/loblaws_com/scrape.py
+++ apify/CalinClaudiu/storelocator/loblaws_com/scrape.py
@@ -22,6 +22,7 @@ from fuzzywuzzy import process
 
 import json  # noqa
 
+import time
 
 def return_last4(fullId):
     last4 = list(fullId[-4:])
@@ -459,7 +460,7 @@ def url_fix(url):
 
 
 def get_api_call(url):
-    with SgFirefox() as driver:
+    with SgFirefox(is_headless = False) as driver:
         driver.get(url)
         to_click = WebDriverWait(driver, 40).until(
             EC.visibility_of_element_located(
@@ -472,13 +473,13 @@ def get_api_call(url):
             EC.visibility_of_element_located(
                 (
                     By.XPATH,
-                    "/html/body/div[6]/div[3]/div[2]/section/div/div[1]/div[2]/div/div[2]/form/div[1]/div[2]/input",
+                    "/html/body/div[6]/div[3]/div[2]/section/div/div[1]/div[2]/div/div[3]/form/div/div[2]/div/input",
                 )
             )
         )
         input_field.send_keys("B3L 4T2")
         input_field.send_keys(Keys.RETURN)
-
+        time.sleep(10)
         wait_for_loc = WebDriverWait(driver, 30).until(  # noqa
             EC.visibility_of_element_located(
                 (
@@ -491,7 +492,6 @@ def get_api_call(url):
             if "DoSearch2" in r.path:
                 url = r.url
                 headers = r.headers
-
     return url, headers
 
 
@@ -580,7 +580,7 @@ def fetch_data():
     url = "https://www.joefresh.com/ca/store-locator"
     # url entrypoint to get all loblaws data
     logzilla.info(f"Figuring out bullseye url and headers with selenium")  # noqa
-
+    url, headers = get_api_call(url)
     def retry_starting():
         try:
             return get_api_call(url)
