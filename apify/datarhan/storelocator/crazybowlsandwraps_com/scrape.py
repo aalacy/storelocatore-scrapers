c401c544eb4db64fe7435bdfc7e6c28053234e09
datarhan <slutchenko@gmail.com>
2022-01-07 19:10:04 +0100

    [SLC-24633] update

--- apify/datarhan/storelocator/crazybowlsandwraps_com/scrape.py
+++ apify/datarhan/storelocator/crazybowlsandwraps_com/scrape.py
@@ -12,26 +12,26 @@ def fetch_data():
     start_url = "https://crazybowlsandwraps.com/?sm-xml-search=1&lat=38.6268404&lng=-90.1993389&radius=0&namequery=38.6270025, -90.1994042&query_type=all&limit=0&hours=&sm_category=&sm_tag=&locname=&address=&city=&state=&zip=&pid=37"
     domain = "crazybowlsandwraps.com"
     hdr = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
     all_locations = session.get(start_url, headers=hdr).json()
     for poi in all_locations:
 
         item = SgRecord(
             locator_domain=domain,
-            page_url=poi['url'],
-            location_name=poi['name'],
-            street_address=poi['address'],
-            city=poi['city'],
-            state=poi['state'],
-            zip_postal=poi['zip'],
-            country_code=poi['country'],
-            store_number=poi['ID'],
-            phone=poi['phone'],
+            page_url=poi["url"],
+            location_name=poi["name"],
+            street_address=poi["address"],
+            city=poi["city"],
+            state=poi["state"],
+            zip_postal=poi["zip"],
+            country_code=poi["country"],
+            store_number=poi["ID"],
+            phone=poi["phone"],
             location_type="",
-            latitude=poi['lat'],
-            longitude=poi['lng'],
-            hours_of_operation=poi['taxes']['hours'],
+            latitude=poi["lat"],
+            longitude=poi["lng"],
+            hours_of_operation=poi["taxes"]["hours"],
         )
 
         yield item
