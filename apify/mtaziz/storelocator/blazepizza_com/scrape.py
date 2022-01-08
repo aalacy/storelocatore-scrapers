20504c6604799e3113e6c02442de580d50b01283
mtaziz <mdtariqaziz@gmail.com>
2022-01-08 19:45:55 +0600

    scrape completed as old one not crawling all the states and cities

--- apify/mtaziz/storelocator/blazepizza_com/scrape.py
+++ apify/mtaziz/storelocator/blazepizza_com/scrape.py
@@ -7,9 +7,18 @@ from sgrequests import SgRequests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tenacity import retry, stop_after_attempt
 import tenacity
-import ssl
 from datetime import datetime
 from datetime import timedelta
+import ssl
+
+try:
+    _create_unverified_https_context = (
+        ssl._create_unverified_context
+    )  # Legacy Python that doesn't verify HTTPS certificates by default
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
 
 
 logger = SgLogSetup().get_logger("blazepizza_com")
