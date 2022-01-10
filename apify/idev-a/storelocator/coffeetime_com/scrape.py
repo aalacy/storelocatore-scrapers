829ca767d171d76c8d953810d2a6391035a0394c
idev-a <ideveloper003@protonmail.com>
2022-01-10 02:13:48 -0800

    f

--- apify/idev-a/storelocator/coffeetime_com/scrape.py
+++ apify/idev-a/storelocator/coffeetime_com/scrape.py
@@ -3,6 +3,8 @@ from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup as bs
 from sglogging import SgLogSetup
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 logger = SgLogSetup().get_logger("coffeetime")
 
@@ -11,9 +13,8 @@ _headers = {
 }
 
 locator_domain = "http://www.coffeetime.com"
-base_url = (
-    "http://www.coffeetime.com/locations.aspx?address=&city=&Countryui=CA&pageNumber={}"
-)
+base_url = "https://api.momentfeed.com/v1/analytics/api/llp.json?auth_token=YDGUJSNDOUAFKPRL&center=43.696891,-79.371536&coordinates=43.06810520271273,-78.26878331445297,44.31915170663851,-80.47428868554653&multi_account=false&page={}&pageSize=300"
+days = ["", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
 
 
 def _p(val):
@@ -34,42 +35,51 @@ def fetch_data():
     with SgRequests() as session:
         page = 1
         while True:
-            soup = bs(session.get(base_url.format(page), headers=_headers).text, "lxml")
-            links = soup.select("div.location-box")
+            links = session.get(base_url.format(page), headers=_headers).json()
             if not links:
                 break
             page += 1
             logger.info(f"{len(links)} found")
 
-            for link in links:
-                addr = link.select_one("p.fontpro").text.strip().split(",")
-                coord = link.img["src"].split("|")[1].split("&")[0].split(",")
-                phone = link.select("ul.md-li li")[0].p.text.strip()
-                if not _p(phone):
-                    phone = ""
-                street_address = link.select_one("p.fontXXL").text.strip()
-                hours = ""
-                if street_address.startswith("MON"):
-                    hours = street_address.split(",")[0]
-                    street_address = ", ".join(street_address.split(",")[1:])
+            for _ in links:
+                page_url = "https://locations.coffeetime.com" + _["llp_url"]
+                if _["open_or_closed"] == "coming soon":
+                    continue
+                hours = []
+                for hh in _["store_info"]["hours"].split(";"):
+                    if not hh:
+                        continue
+                    time1 = hh.split(",")[1][:2] + ":" + hh.split(",")[1][2:]
+                    time2 = hh.split(",")[2][:2] + ":" + hh.split(",")[2][2:]
+                    hours.append(f"{days[int(hh.split(',')[0])]}: {time1}-{time2}")
+
+                if not hours:
+                    hours = ["Mon-Sun: Closed"]
+
+                info = _["store_info"]
+                location_name = ""
+                for nn in _["custom_fields"]:
+                    if nn["name"] == "StoreName":
+                        location_name = nn["data"]
+                        break
                 yield SgRecord(
-                    page_url=base_url,
-                    location_name=street_address,
-                    street_address=street_address,
-                    city=addr[0].strip(),
-                    state=addr[1].strip(),
-                    zip_postal=addr[2].strip(),
-                    country_code="CA",
-                    phone=phone,
+                    page_url=page_url,
+                    location_name=location_name,
+                    street_address=f"{info['address']} {info.get('address_extended', '')} {info.get('address3', '')}".strip(),
+                    city=info["locality"],
+                    state=info["region"],
+                    zip_postal=info["postcode"],
+                    country_code=info["country"],
+                    phone=info["phone"],
+                    latitude=info["latitude"],
+                    longitude=info["longitude"],
                     locator_domain=locator_domain,
-                    latitude=coord[0],
-                    longitude=coord[1],
-                    hours_of_operation=hours,
+                    hours_of_operation="; ".join(hours),
                 )
 
 
 if __name__ == "__main__":
-    with SgWriter() as writer:
+    with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
         results = fetch_data()
         for rec in results:
             writer.write_row(rec)
