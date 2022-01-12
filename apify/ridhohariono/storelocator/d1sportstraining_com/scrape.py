a470414fe16c1c6abef61ddfe2a3c2df40157313
ridho <ridhohariono1904@gmail.com>
2022-01-12 18:03:06 +0700

    fix

--- apify/ridhohariono/storelocator/d1sportstraining_com/scrape.py
+++ apify/ridhohariono/storelocator/d1sportstraining_com/scrape.py
@@ -10,7 +10,6 @@ DOMAIN = "d1sportstraining.com"
 BASE_URL = "https://www.d1training.com"
 API_URL = "https://www.d1training.com/locations/?CallAjax=GetLocations"
 HEADERS = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
 }
 MISSING = "<MISSING>"
