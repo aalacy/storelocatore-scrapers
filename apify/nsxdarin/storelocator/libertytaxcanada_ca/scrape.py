1a31e80c21a29a38400eb6382c1d2e834efc3042
nsxdarin <nsxdarin@gmail.com>
2022-01-13 14:00:42 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/libertytaxcanada_ca/scrape.py
+++ apify/nsxdarin/storelocator/libertytaxcanada_ca/scrape.py
@@ -13,6 +13,7 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
 
+
 def fetch_data():
     locs = []
     url = "https://www.libertytaxcanada.ca/api/franchise/getAllFranchise/get-all-franchise"
