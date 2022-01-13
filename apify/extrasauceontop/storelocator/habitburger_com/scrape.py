497a269e51cf5bfd9fe31aa4f0507c8328f64c23
extrasauceontop <msuchemba@gmail.com>
2022-01-13 15:21:54 -0600

    scrape

--- apify/extrasauceontop/storelocator/habitburger_com/scrape.py
+++ apify/extrasauceontop/storelocator/habitburger_com/scrape.py
@@ -37,10 +37,6 @@ def extract_json(html_string):
 def get_data():
     session = SgRequests()
 
-    headers = {
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-    }
-
     url = "https://www.habitburger.com/locations/all/"
     all_stores = session.get(url).text
 
