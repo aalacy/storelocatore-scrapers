af72dca22ffecc6c1ecb6b143f9bbb4a72b1d3f7
nsxdarin <nsxdarin@gmail.com>
2022-01-13 13:59:15 -0600

    Update scrape.py

--- apify/nsxdarin/storelocator/libertytaxcanada_ca/scrape.py
+++ apify/nsxdarin/storelocator/libertytaxcanada_ca/scrape.py
@@ -13,34 +13,6 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
 
-
-def write_output(data):
-    with open("data.csv", mode="w") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        for row in data:
-            writer.writerow(row)
-
-
 def fetch_data():
     locs = []
     url = "https://www.libertytaxcanada.ca/api/franchise/getAllFranchise/get-all-franchise"
