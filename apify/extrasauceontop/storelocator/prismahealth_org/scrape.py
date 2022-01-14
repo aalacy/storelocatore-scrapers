9e1a7a0b73f2e5f3bab66e3a77e90c6e590f6da3
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2022-01-14 08:49:15 +0100

    Update scrape.py

--- apify/extrasauceontop/storelocator/prismahealth_org/scrape.py
+++ apify/extrasauceontop/storelocator/prismahealth_org/scrape.py
@@ -5,7 +5,7 @@ from sgzip.dynamic import DynamicZipSearch, SearchableCountries
 
 search = DynamicZipSearch(
     country_codes=[SearchableCountries.USA],
-    max_radius_miles=100,
+    expected_search_radius_miles =100,
     max_search_results=100,
 )
 
@@ -77,14 +77,16 @@ base_url = "https://prismahealth.org"
 s = SgRequests()
 
 driver = SgChrome().driver()
-driver.get(base_url)
+driver.get("https://www.joefresh.com/ca/store-locator")
 
 incap_str = "/_Incapsula_Resource?SWJIYLWA=719d34d31c8e3a6e6fffd425f7e032f3"
-incap_url = base_url + incap_str
+incap_url = "https://www.joefresh.com/ca/store-locator"
 
-s.get(incap_url)
+#s.get(incap_url)
 
 for request in driver.requests:
+    print("gothere")
+    print(driver.requests)
     initial_search_url = "https://prismahealth.org/api/search/search?pageSize=100&pageNumber=1&searchType=location"
 
     headers = request.headers
