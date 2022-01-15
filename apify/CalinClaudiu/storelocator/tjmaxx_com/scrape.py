abcad1bc4750788aa123637dbfd003a877e374c4
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2022-01-15 10:39:56 +0100

    Update scrape.py

--- apify/CalinClaudiu/storelocator/tjmaxx_com/scrape.py
+++ apify/CalinClaudiu/storelocator/tjmaxx_com/scrape.py
@@ -184,7 +184,11 @@ class ExampleSearchIteration(SearchIteration):
 
 
 if __name__ == "__main__":
-    tocrawl = [SearchableCountries.CANADA]
+    tocrawl = []
+    tocrawl.append(SearchableCountries.USA)
+    tocrawl.append(SearchableCountries.CANADA)
+    tocrawl.append(SearchableCountries.AUSTRALIA)
+    tocrawl = tocrawl + SearchableCountries.ByGeography["CONTINENTAL_EUROPE"]
     search_maker = DynamicSearchMaker(
         search_type="DynamicGeoSearch",
         granularity=Grain_2(),
