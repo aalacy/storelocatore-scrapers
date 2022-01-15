eb0366da6087ecd29ae9d601e2df5d16fd4c4dc7
datarhan <slutchenko@gmail.com>
2022-01-15 10:59:08 +0100

    [SLC-10561] update

--- apify/datarhan/storelocator/moleskine_com/scrape.py
+++ apify/datarhan/storelocator/moleskine_com/scrape.py
@@ -56,7 +56,7 @@ def scrape():
             SgRecordID(
                 {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
             ),
-            duplicate_streak_failure_factor=-1
+            duplicate_streak_failure_factor=-1,
         )
     ) as writer:
         for item in fetch_data():
