664851d0c5cfc79d1e5b3a8159a823bd1374e94c
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2022-01-18 19:04:50 +0500

    Update scrape.py

--- apify/shumaila/storelocator/maisonbirks_com/scrape.py
+++ apify/shumaila/storelocator/maisonbirks_com/scrape.py
@@ -5,6 +5,8 @@ from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
+session = SgRequests()
+
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
     "X-Requested-With": "XMLHttpRequest",
