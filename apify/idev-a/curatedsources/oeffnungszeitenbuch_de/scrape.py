e617c0891e23056188d4202ca107901b78f4e9c0
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2022-01-19 18:18:11 +0100

    publish

--- apify/idev-a/curatedsources/oeffnungszeitenbuch_de/scrape.py
+++ apify/idev-a/curatedsources/oeffnungszeitenbuch_de/scrape.py
@@ -6,6 +6,10 @@ from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sglogging import SgLogSetup
 
+from sgscrape.pause_resume import SerializableRequest, CrawlStateSingleton
+from sgrequests.sgrequests import SgRequests, SgRequestsAsync
+import asyncio
+
 logger = SgLogSetup().get_logger("")
 
 _headers = {
@@ -16,55 +20,79 @@ locator_domain = "https://www.oeffnungszeitenbuch.de"
 base_url = "https://www.oeffnungszeitenbuch.de/"
 
 
-def fetch_data():
-    with SgRequests() as session:
-        soup = bs(session.get(base_url, headers=_headers).text, "lxml")
-        cities = soup.select("div#textbereich > table a")
-        for city in cities:
-            city_base = city["href"]
-            logger.info(city_base)
-            pages = bs(session.get(city_base, headers=_headers).text, "lxml").select(
-                "select#seitennr option"
+def record_initial_requests(http, state):
+    soup = bs(http.get(base_url, headers=_headers).text, "lxml")
+    cities = soup.select("div#textbereich > table a")
+    for city in cities[-5:-4]:  # Sliced for testing
+        city_base = city["href"]
+        logger.info(city_base)
+        pages = bs(http.get(city_base, headers=_headers).text, "lxml").select(
+            "select#seitennr option"
+        )
+        for page in pages[:1]:  # Sliced for testing
+            section_url = city_base.split("-")[0] + "-" + page.text + ".html"
+            logger.info(section_url)
+            links = bs(http.get(section_url, headers=_headers).text, "lxml").select(
+                "div.cboxserp"
             )
-            for page in pages:
-                section_url = city_base.split("-")[0] + "-" + page.text + ".html"
-                logger.info(section_url)
-                links = bs(
-                    session.get(section_url, headers=_headers).text, "lxml"
-                ).select("div.cboxserp")
-                for link in links:
-                    page_url = link.a["href"]
-                    logger.info(page_url)
-                    sp2 = bs(session.get(page_url, headers=_headers).text, "lxml")
-                    addr = list(
-                        sp2.select("span.entryAdrFnt > div")[1].stripped_strings
-                    )[:3]
-                    phone = ""
-                    if sp2.select_one("span.telClk"):
-                        phone = sp2.select_one("span.telClk").text.strip()
-                    hours = []
-                    for hh in sp2.select("table.zeitenTbl tr"):
-                        td = hh.select("td")
-                        if len(td) == 1:
-                            break
-                        hours.append(" ".join(hh.stripped_strings))
-
-                    yield SgRecord(
-                        page_url=page_url,
-                        location_name=link.a.text.strip(),
-                        street_address=addr[0],
-                        city=addr[-1],
-                        zip_postal=addr[1],
-                        country_code="DE",
-                        phone=phone,
-                        locator_domain=locator_domain,
-                        hours_of_operation="; ".join(hours),
-                        raw_address=" ".join(addr),
+            for link in links:
+                page_url = link.a["href"]
+                state.push_request(
+                    SerializableRequest(
+                        url=page_url, context={"link": link.a.text.strip()}
                     )
+                )
+    return True
+
+
+async def producer(state):
+    async with SgRequestsAsync() as httpx:
+        for next_r in state.request_stack_iter():
+            yield {
+                "page_url": next_r.url,
+                "link": next_r.context.get("link") if next_r.context else "<MISSING>",
+                "text": await httpx.get(next_r.url),
+            }
+
+
+async def consumer(resp_generator, writer):
+    async for response in resp_generator:
+        sp2 = bs(response["text"].text, "lxml")
+        addr = list(sp2.select("span.entryAdrFnt > div")[1].stripped_strings)[:3]
+        phone = ""
+        if sp2.select_one("span.telClk"):
+            phone = sp2.select_one("span.telClk").text.strip()
+        hours = []
+        for hh in sp2.select("table.zeitenTbl tr"):
+            td = hh.select("td")
+            if len(td) == 1:
+                break
+            hours.append(" ".join(hh.stripped_strings))
+        writer.write_row(
+            SgRecord(
+                page_url=response["page_url"],
+                location_name=response["link"],
+                street_address=addr[0],
+                city=addr[-1],
+                zip_postal=addr[1],
+                country_code="DE",
+                phone=phone,
+                locator_domain=locator_domain,
+                hours_of_operation="; ".join(hours),
+                raw_address=" ".join(addr),
+            )
+        )
+
+
+def fetch_data(state, writer):
+    asyncio.get_event_loop().run_until_complete(consumer(producer(state), writer))
 
 
 if __name__ == "__main__":
+    state = CrawlStateSingleton.get_instance()
     with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
-        results = fetch_data()
-        for rec in results:
-            writer.write_row(rec)
+        with SgRequests() as http:
+            state.get_misc_value(
+                "init", default_factory=lambda: record_initial_requests(http, state)
+            )
+            fetch_data(state, writer)
