d131212910ea1a0e6e2552052c2b6d4756b5afc6
Michael Dinh <mkdinh94@gmail.com>
2022-01-22 09:21:00 -0500

    [SLC-21360] use logger

--- apify/nomihd/storelocator/canadagoose_com/scrape.py
+++ apify/nomihd/storelocator/canadagoose_com/scrape.py
@@ -108,7 +108,7 @@ def fetch_data():
         user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
     ) as session:
         session.get(search_url)
-        print(session.page_source)
+        log.info(session.page_source)
         search_sel = lxml.html.fromstring(session.page_source, "lxml")
         store_list = search_sel.xpath('//div[@class="store"]')
         log.info(f"Total Locations to crawl: {len(store_list)}")
