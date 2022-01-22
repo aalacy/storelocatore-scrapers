349ac6bb3b337dbb25c610cfa0b4533fd88b070c
idev-a <ideveloper003@protonmail.com>
2022-01-22 12:52:11 -0800

    f

--- apify/idev-a/storelocator/colectivocoffee_com/scrape.py
+++ apify/idev-a/storelocator/colectivocoffee_com/scrape.py
@@ -3,7 +3,7 @@ from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
 from bs4 import BeautifulSoup as bs
 from sglogging import SgLogSetup
-from sgscrape.sgpostal import parse_address_intl
+from sgpostal.sgpostal import parse_address_intl
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
