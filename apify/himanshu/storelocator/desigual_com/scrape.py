a1c23d88202341f66ac557e7b75f5d51358a51dc
Samia Kiran <samiakiran17@gmail.com>
2022-01-23 16:40:09 +0500

    [SLC-25598] Add Maintenance
    https://safegraph-crawl.atlassian.net/browse/SLC-25598

--- apify/himanshu/storelocator/desigual_com/scrape.py
+++ apify/himanshu/storelocator/desigual_com/scrape.py
@@ -1,120 +1,109 @@
-import csv
-from sgrequests import SgRequests
-from bs4 import BeautifulSoup
-import re
 import json
-import sgzip
-session = SgRequests()
-def write_output(data):
-    with open('data.csv', mode='w',encoding="utf-8") as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation","page_url"])
-        for row in data:
-            writer.writerow(row)
-def fetch_data():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
-    }
-    address = []
-    search = sgzip.ClosestNSearch() # TODO: OLD VERSION [sgzip==0.0.55]. UPGRADE IF WORKING ON SCRAPER!
-    search.initialize()
-    result_coords = []
-    data_len = 0
-    MAX_RESULTS = 100
-    MAX_DISTANCE = 100
-    coords = search.next_coord()
-    while coords:
-        try:
-            r = session.get("https://www.desigual.com/on/demandware.store/Sites-dsglcom_prod_us-Site/en_US/Address-SearchStoreAddress?longitude="+str(coords[1])+"&latitude="+str(coords[0])+"&deliveryPoint=STORE&radius=7000&showOfficialStores=false&showOutlets=false&showAuthorized=false&showOnlyAllowDevosStores=false")
-        except:
-            continue
-        data = r.json()
-        mp = data['shippingAddresses']
-        for i in data['shippingAddresses']:
-            if i["countryCode"] not in ["US","CA"]:
-                continue
-            if 'address' in i:
-                street_address = i['address'].replace("Orlando Florida Mall,","").replace("McArthurGlen Designer Outlet,","").replace("Miromar Outlets,","").replace("Shopping Center Dolphin Mall,","").replace("Miracle Mile Shops,","").strip()
-            else:
-                street_address = "<MISSING>"
-            if 'name' in i:
-                loacation_name = i['name']
-            else:
-                loacation_name = "<MISSING>"
-            if "Desigual" not in loacation_name:
-                continue
-            if 'city' in i:
-                city = i['city']
-            else:
-                city = "<MISSING>"
-            if 'region' in i:
-                state = i['regionSapId']
-            else:
-                state = "<MISSING>"
-            if 'postalCode' in i:
-                zipp =i['postalCode']
-            else:
-                zipp = "<MISSING>"
-            if 'latitude' in i:
-                latitude = i['latitude']
-            else:
-                latitude = "<MISSING>"
-            if 'countryCode' in i:
-                country_code = i['countryCode']
-            else:
-                country_code = "<MISSING>"
-            if 'longitude' in i:
-                longitude = i['longitude']
-            else:
-                longitude = "<MISSING>"
-            result_coords.append((latitude, longitude))
-            if 'storeId' in i:
-                store_number = i['storeId'].replace("R","")
-            else:
-                store_number = "<MISSING>"
-            if 'phone' in i:
-                temp_phone = i['phone'].replace("+1","").replace(" ","").strip()
-                phone = "("+temp_phone[:3]+")"+temp_phone[3:6]+"-"+temp_phone[6:]
-            else:
-                phone = "<MISSING>"
-            hours = ""
-            days = {1:"Sunday",2:"Monday",3:"Tuesday",4:"Wednesday",5:"Thursday",6:"Friday",7:"Saturday"}
-            if "schedule" in mp and mp["schedule"]:
-                store_hours = mp["schedule"]
-                for hour in store_hours:
-                    hours = hours + " " + days[hour["dayNumber"]] + " " + hour["value"]
-                store.append(hours)
-            if "Desigual" in loacation_name:
-                if i['isOpen'] == True:
-                    hours = "Temporary closed"
-                store = []
-                store.append("https://www.desigual.com/")
-                store.append(loacation_name if loacation_name else "<MISSING>") 
-                store.append(street_address if street_address else "<MISSING>")
-                store.append(city if city else "<MISSING>")
-                store.append(state if state else "<MISSING>")
-                store.append(zipp if zipp else "<MISSING>")
-                store.append(country_code if country_code else "<MISSING>")
-                store.append(store_number if store_number else"<MISSING>") 
-                store.append(phone if phone else "<MISSING>")
-                store.append("<MISSING>")
-                store.append(latitude if latitude else "<MISSING>")
-                store.append(longitude if longitude else "<MISSING>")
-                store.append(hours if hours else "<MISSING>")
-                store.append("<MISSING>")
-                if store[2] in address :
-                    continue
-                address.append(store[2])
-                yield store
-        if data_len < MAX_RESULTS:
-            search.max_distance_update(MAX_DISTANCE)
-        elif data_len == MAX_RESULTS:
-            search.max_count_update(result_coords)
-        else:
-            raise Exception("expected at most " + str(MAX_RESULTS) + " results")
-        coords = search.next_coord()
-def scrape():
-    data = fetch_data()
-    write_output(data)
+from sglogging import sglog
+from bs4 import BeautifulSoup
+from sgrequests import SgRequests
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
-scrape()
+session = SgRequests()
+website = "desigual_com"
+log = sglog.SgLogSetup().get_logger(logger_name=website)
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+}
+
+DOMAIN = "https://desigual.com"
+MISSING = SgRecord.MISSING
+
+
+def fetch_data():
+    if True:
+        country_list = ["en_US", "en_CA"]
+        for country in country_list:
+            country_url = (
+                "https://www.desigual.com/"
+                + country
+                + "/shops/?showMap=true&horizontalView=true&isForm=true"
+            )
+            r = session.get(country_url, headers=headers)
+            soup = BeautifulSoup(r.text, "html.parser")
+            loclist = soup.find("ul", {"id": "collapseExample"}).findAll("li")
+            for loc in loclist:
+                page_url = loc.find("a")["href"]
+                log.info(page_url)
+                r = session.get(page_url, headers=headers)
+                temp = (
+                    r.text.split('initial-stores="')[1]
+                    .split('">')[0]
+                    .replace("&quot;", " ")
+                )
+                store_number = temp.split("id :")[1].split(",")[0]
+                location_name = temp.split("name :")[1].split(",")[0]
+                street_address = temp.split("address :")[1].split(", city :")[0]
+                city = temp.split(", city :")[1].split(",")[0]
+                state = temp.split("regionSapId :")[1].split(",")[0]
+                zip_postal = temp.split("postalCode :")[1].split(",")[0]
+                country_code = temp.split("countryCode :")[1].split(",")[0]
+                latitude = temp.split("latitude :")[1].split(",")[0]
+                longitude = temp.split("longitude :")[1].split(",")[0]
+                phone = temp.split("phone : ")[1].split(",")[0]
+                mon = "Mon " + temp.split("Monday , value :")[1].split(",")[0]
+                tue = "Tue " + temp.split("Tuesday , value :")[1].split(",")[0]
+                wed = "Wed " + temp.split("Wednesday , value :")[1].split(",")[0]
+                thu = "Thu " + temp.split("Thursday , value :")[1].split(",")[0]
+                fri = "Fri " + temp.split("Friday , value :")[1].split(",")[0]
+                sat = "Sat " + temp.split("Saturday , value :")[1].split(",")[0]
+                sun = "Sun " + temp.split("Sunday , value :")[1].split(",")[0]
+                hours_of_operation = (
+                    mon
+                    + ", "
+                    + tue
+                    + ", "
+                    + wed
+                    + ", "
+                    + thu
+                    + ", "
+                    + fri
+                    + ", "
+                    + sat
+                    + ", "
+                    + sun
+                )
+                yield SgRecord(
+                    locator_domain=DOMAIN,
+                    page_url=page_url,
+                    location_name=location_name,
+                    street_address=street_address.strip(),
+                    city=city.strip(),
+                    state=state.strip(),
+                    zip_postal=zip_postal.strip(),
+                    country_code=country_code,
+                    store_number=store_number,
+                    phone=phone.strip(),
+                    location_type=MISSING,
+                    latitude=latitude,
+                    longitude=longitude,
+                    hours_of_operation=hours_of_operation,
+                )
+
+
+def scrape():
+    log.info("Started")
+    count = 0
+    with SgWriter(
+        deduper=SgRecordDeduper(record_id=RecommendedRecordIds.PageUrlId)
+    ) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
+
+    log.info(f"No of records being processed: {count}")
+    log.info("Finished")
+
+
+if __name__ == "__main__":
+    scrape()
