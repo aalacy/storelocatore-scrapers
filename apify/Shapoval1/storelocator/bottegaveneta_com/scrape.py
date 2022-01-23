1754c1d7ac1bd2b3a3de87a5ea1ef299c8d9e942
dima <shshipovald@mail.ru>
2022-01-23 21:12:51 +0200

    fix bottegaveneta.com

--- apify/Shapoval1/storelocator/bottegaveneta_com/scrape.py
+++ apify/Shapoval1/storelocator/bottegaveneta_com/scrape.py
@@ -1,63 +1,25 @@
-import csv
-import json
-from lxml import html
+from sgscrape.sgrecord import SgRecord
 from sgrequests import SgRequests
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 
-def write_output(data):
-    with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-
-        for row in data:
-            writer.writerow(row)
-
-
-def fetch_data():
-    out = []
+def fetch_data(sgw: SgWriter):
 
     locator_domain = "https://www.bottegaveneta.com/"
-    countries = ["us", "ca"]
+    countries = ["US", "CA"]
     for c in countries:
-        api_url = f"https://www.bottegaveneta.com/en-{c}/storelocator"
+        api_url = f"https://www.bottegaveneta.com/on/demandware.store/Sites-BV-INTL-Site/en_ZW/Stores-FindStoresData?countryCode={c}"
         session = SgRequests()
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
         }
         r = session.get(api_url, headers=headers)
-        tree = html.fromstring(r.text)
-        jsblock = (
-            "".join(
-                tree.xpath('//script[contains(text(), "window.exposedData = ")]/text()')
-            )
-            .split("window.exposedData = ")[1]
-            .split(";")[0]
-        )
-        js = json.loads(jsblock)
+        js = r.json()
         for j in js["storesData"]["stores"]:
             page_url = j.get("detailsUrl")
             location_name = j.get("name")
-            location_type = "<MISSING>"
             street_address = f"{j.get('address1')} {j.get('address2') or ''}".replace(
                 "None", ""
             ).strip()
@@ -80,31 +42,27 @@ def fetch_data():
             if hours_of_operation.count("<MISSING>") == 7:
                 hours_of_operation = "<MISSING>"
 
-            row = [
-                locator_domain,
-                page_url,
-                location_name,
-                street_address,
-                city,
-                state,
-                postal,
-                country_code,
-                store_number,
-                phone,
-                location_type,
-                latitude,
-                longitude,
-                hours_of_operation,
-            ]
-            out.append(row)
+            row = SgRecord(
+                locator_domain=locator_domain,
+                page_url=page_url,
+                location_name=location_name,
+                street_address=street_address,
+                city=city,
+                state=state,
+                zip_postal=postal,
+                country_code=country_code,
+                store_number=store_number,
+                phone=phone,
+                location_type=SgRecord.MISSING,
+                latitude=latitude,
+                longitude=longitude,
+                hours_of_operation=hours_of_operation,
+            )
 
-    return out
-
-
-def scrape():
-    data = fetch_data()
-    write_output(data)
+            sgw.write_row(row)
 
 
 if __name__ == "__main__":
-    scrape()
+    session = SgRequests()
+    with SgWriter(SgRecordDeduper(SgRecordID({SgRecord.Headers.PAGE_URL}))) as writer:
+        fetch_data(writer)
