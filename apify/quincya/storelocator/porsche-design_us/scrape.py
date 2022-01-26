c4b8ff9fd3bf47507d3cc70367fbdd377950b046
Quincy <qcalexander@gmail.com>
2022-01-26 00:53:28 -0400

    SLC-25720 lint

--- apify/quincya/storelocator/porsche-design_us/scrape.py
+++ apify/quincya/storelocator/porsche-design_us/scrape.py
@@ -242,9 +242,10 @@ def fetch_data(sgw: SgWriter):
             street_address = street_address[:-1]
 
         if "Porsche" not in street_address:
-            digit = re.search(r"\d", street_address).start(0)
-            if digit != 0:
-                street_address = street_address[digit:]
+            if re.search(r"\d", street_address):
+                digit = str(re.search(r"\d", street_address))
+                start = int(digit.split("(")[1].split(",")[0])
+                street_address = street_address[start:]
 
         if state == "Las":
             state = "NV"
