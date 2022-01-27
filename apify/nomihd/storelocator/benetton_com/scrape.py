261763e3978c84cb69df523ce3e8c93922397f69
nomihd <nouman.awais.nu@gmail.com>
2022-01-28 00:31:01 +0500

    remove grain_8

--- apify/nomihd/storelocator/benetton_com/scrape.py
+++ apify/nomihd/storelocator/benetton_com/scrape.py
@@ -8,7 +8,7 @@ from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.pause_resume import CrawlStateSingleton
-from sgzip.dynamic import Grain_8, SearchableCountries
+from sgzip.dynamic import SearchableCountries
 from sgzip.parallel import DynamicSearchMaker, ParallelDynamicSearch, SearchIteration
 
 website = "benetton.com"
