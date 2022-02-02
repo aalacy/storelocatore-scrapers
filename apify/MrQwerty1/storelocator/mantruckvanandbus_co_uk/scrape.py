49db43961af8c4e322e996592b54fa4e5a0adbed
Michael Dinh <mkdinh94@gmail.com>
2022-02-02 12:54:13 -0500

    [SLC-24721] use selenium

--- apify/MrQwerty1/storelocator/mantruckvanandbus_co_uk/scrape.py
+++ apify/MrQwerty1/storelocator/mantruckvanandbus_co_uk/scrape.py
@@ -1,26 +1,29 @@
 import json
+from time import sleep
 from lxml import html
 from sgscrape.sgrecord import SgRecord
-from sgrequests import SgRequests
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from concurrent import futures
+from sgselenium import SgChrome
+
+agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
 
 
-def get_urls():
-    r = session.get(
-        "https://www.mantruckvanandbus.co.uk/dealer-locator/", headers=headers
-    )
-    tree = html.fromstring(r.text)
+def get_urls(driver):
+    driver.get("https://www.mantruckvanandbus.co.uk/dealer-locator/")
+    tree = html.fromstring(driver.page_source)
 
     return tree.xpath("//a[contains(text(), 'Details')]/@href")
 
 
-def get_data(slug, sgw: SgWriter):
+def get_data(slug, sgw: SgWriter, driver: SgChrome):
+    locator_domain = "https://www.mantruckvanandbus.co.uk/"
     page_url = f"https://www.mantruckvanandbus.co.uk{slug}"
-    r = session.get(page_url, headers=headers)
-    tree = html.fromstring(r.text)
+    sleep(15)
+    driver.get(page_url)
+    tree = html.fromstring(driver.page_source)
     text = "".join(tree.xpath("//script[contains(text(), 'AutoDealer')]/text()"))
     j = json.loads(text)
 
@@ -70,21 +73,19 @@ def get_data(slug, sgw: SgWriter):
     sgw.write_row(row)
 
 
-def fetch_data(sgw: SgWriter):
-    urls = get_urls()
+def fetch_data(sgw: SgWriter, driver: SgChrome):
+    urls = get_urls(driver)
 
-    with futures.ThreadPoolExecutor(max_workers=3) as executor:
-        future_to_url = {executor.submit(get_data, url, sgw): url for url in urls}
+    with futures.ThreadPoolExecutor(max_workers=1) as executor:
+        future_to_url = {
+            executor.submit(get_data, url, sgw, driver): url for url in urls
+        }
         for future in futures.as_completed(future_to_url):
             future.result()
 
 
 if __name__ == "__main__":
-    locator_domain = "https://www.mantruckvanandbus.co.uk/"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    }
-    session = SgRequests()
-    with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
-        fetch_data(writer)
+    with SgChrome(is_headless=False, user_agent=agent) as driver, SgWriter(
+        SgRecordDeduper(RecommendedRecordIds.PageUrlId)
+    ) as writer:
+        fetch_data(writer, driver)
