511016683e1c91ede311e0a4fe456f5fd6af7fa4
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2022-02-02 20:41:53 +0100

    Update scrape.py

--- apify/CalinClaudiu/storelocator/tjmaxx_com/scrape.py
+++ apify/CalinClaudiu/storelocator/tjmaxx_com/scrape.py
@@ -8,7 +8,8 @@ from sgrequests.sgrequests import SgRequests
 from sgzip.dynamic import SearchableCountries, Grain_2
 from sgzip.parallel import DynamicSearchMaker, ParallelDynamicSearch, SearchIteration
 from sglogging import sglog
-#This is a code change
+
+# This is a code change
 
 logzilla = sglog.SgLogSetup().get_logger(logger_name="Scraper")
 
