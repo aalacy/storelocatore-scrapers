f68ba9995f01617baa7b98a003ca3593cbff5799
datarhan <slutchenko@gmail.com>
2022-02-14 11:01:33 +0100

    [SLC26611] maintenance

--- apify/datarhan/storelocator/actiongatortire_com/scrape.py
+++ apify/datarhan/storelocator/actiongatortire_com/scrape.py
@@ -1,5 +1,6 @@
-import demjson
+import re
 from lxml import etree
+from urllib.parse import urljoin
 
 from sgrequests import SgRequests
 from sgscrape.sgrecord import SgRecord
@@ -9,8 +10,8 @@ from sgscrape.sgwriter import SgWriter
 
 
 def fetch_data():
-    session = SgRequests().requests_retry_session(retries=2, backoff_factor=0.3)
-    start_url = "https://www.actiongatortire.com/locations/"
+    session = SgRequests()
+    start_url = "https://www.mavistire.com/locations/"
     domain = "actiongatortire.com"
     hdr = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
@@ -18,34 +19,40 @@ def fetch_data():
     response = session.get(start_url, headers=hdr)
     dom = etree.HTML(response.text)
 
-    all_locations = dom.xpath(
-        '//div[@class="row collapse locations-search-grid"]//a[contains(text(), "View Location")]/@href'
-    )
+    all_locations = list(set(dom.xpath('//a[@class="mavis clr-mint"]/@href')[1:]))
     for store_url in all_locations:
+        store_url = urljoin(start_url, store_url)
         loc_response = session.get(store_url)
         loc_dom = etree.HTML(loc_response.text)
-        poi = loc_dom.xpath('//script[contains(text(), "streetAddress")]/text()')[0]
-        poi = demjson.decode(poi)
-        location_name = " ".join(loc_dom.xpath("//h1//text()"))
-        location_type = poi["@type"]
-        if loc_dom.xpath('//p[contains(text(), "s corporate office")]'):
-            location_type = "Corporate Office"
+
+        location_name = loc_dom.xpath('//span[@itemprop="name"]/text()')[0]
+        street_address = loc_dom.xpath('//span[@itemprop="streetAddress"]/text()')[
+            0
+        ].strip()
+        city = loc_dom.xpath('//span[@itemprop="addressLocality"]/text()')[0]
+        state = loc_dom.xpath('//span[@itemprop="addressRegion"]/text()')[0]
+        zip_code = loc_dom.xpath('//span[@itemprop="postalCode"]/text()')[0]
+        phone = loc_dom.xpath("//a/@data-tel-number")[0]
+        longitude = re.findall(r",Lng:(.+?),", loc_response.text)[0]
+        latitude = re.findall(r",Lat:(.+?),", loc_response.text)[0]
+        hoo = loc_dom.xpath('//div[@class="store-hours"]/div[1]//text()')
+        hoo = " ".join(hoo)
 
         item = SgRecord(
             locator_domain=domain,
             page_url=store_url,
             location_name=location_name,
-            street_address=poi["address"]["streetAddress"],
-            city=poi["address"]["addressLocality"],
-            state=poi["address"]["addressRegion"],
-            zip_postal=poi["address"]["postalCode"],
-            country_code=SgRecord.MISSING,
-            store_number=SgRecord.MISSING,
-            phone=poi["telephone"].strip(),
-            location_type=location_type,
-            latitude=poi["geo"]["latitude"],
-            longitude=poi["geo"]["longitude"],
-            hours_of_operation=" ".join(poi["openingHours"]),
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=zip_code,
+            country_code="",
+            store_number="",
+            phone=phone,
+            location_type="",
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hoo,
         )
 
         yield item
