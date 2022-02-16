aa31fa2ac942de78f00f1d1d4a11f2aaa0e03d16
nomihd <nouman.awais.nu@gmail.com>
2022-02-16 14:42:05 +0500

    fix street_address

--- apify/nomihd/storelocator/paydens_com/scrape.py
+++ apify/nomihd/storelocator/paydens_com/scrape.py
@@ -67,6 +67,9 @@ def fetch_data():
             if street_address is not None:
                 street_address = street_address.replace("Ste", "Suite")
 
+            if street_address and street_address.replace("-", "").isdigit():
+                street_address = raw_address.split(",")[0].strip()
+
             city = formatted_addr.city
 
             state = formatted_addr.state
