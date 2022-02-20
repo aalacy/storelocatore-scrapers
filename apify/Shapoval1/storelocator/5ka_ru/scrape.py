530e60d2576d022abbcc68ca0c010e8f9b8a94dc
dima <shshipovald@mail.ru>
2022-02-20 21:34:31 +0200

    fix 5ka.ru

--- apify/Shapoval1/storelocator/5ka_ru/scrape.py
+++ apify/Shapoval1/storelocator/5ka_ru/scrape.py
@@ -74,6 +74,8 @@ def fetch_data(sgw: SgWriter):
 if __name__ == "__main__":
     session = SgRequests()
     with SgWriter(
-        SgRecordDeduper(SgRecordID({SgRecord.Headers.STORE_NUMBER}))
+        SgRecordDeduper(
+            SgRecordID({SgRecord.Headers.LATITUDE, SgRecord.Headers.STORE_NUMBER})
+        )
     ) as writer:
         fetch_data(writer)
