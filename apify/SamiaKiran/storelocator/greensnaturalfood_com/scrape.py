dd76a2a22cb26c7f69ae98bda843746db2c87f4e
Samia Kiran <samiakiran17@gmail.com>
2022-02-23 12:23:52 +0500

    Update scrape.py
    https://safegraph-crawl.atlassian.net/browse/SLC-26804

--- apify/SamiaKiran/storelocator/greensnaturalfood_com/scrape.py
+++ apify/SamiaKiran/storelocator/greensnaturalfood_com/scrape.py
@@ -81,6 +81,12 @@ def fetch_data():
                 if temp[1].find("ZipCode") != -1:
                     zip_postal = zip_postal + " " + temp[0]
                 i += 1
+            if not state:
+                address = r.text.split(street_address)[1].split('"/>')[0].split(",")
+                city = address[1]
+                address = address[-1].split()
+                state = address[0]
+                zip_postal = address[1]
             country_code = "US"
             yield SgRecord(
                 locator_domain=DOMAIN,
