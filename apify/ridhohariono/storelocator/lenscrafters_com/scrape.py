5eb6a2edc2b1eeea8cd04d73678cbe609b4424d7
ridho <ridhohariono1904@gmail.com>
2022-02-24 21:16:00 +0700

    fix

--- apify/ridhohariono/storelocator/lenscrafters_com/scrape.py
+++ apify/ridhohariono/storelocator/lenscrafters_com/scrape.py
@@ -44,10 +44,16 @@ def fetch_data():
                 "content"
             ]
             city = store.find("span", {"class": "c-address-city"}).text.strip()
-            state = store.find("abbr", {"class": "c-address-state"}).text.strip()
-            zip_postal = store.find(
-                "span", {"class": "c-address-postal-code"}
-            ).text.strip()
+            try:
+                state = store.find("abbr", {"class": "c-address-state"}).text.strip()
+            except:
+                state = MISSING
+            try:
+                zip_postal = store.find(
+                    "span", {"class": "c-address-postal-code"}
+                ).text.strip()
+            except:
+                zip_postal = MISSING
             country_code = store.find("address", id="address")["data-country"]
             try:
                 phone = store.find("div", id="phone-main").text.strip()
