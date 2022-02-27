e476217e83294a7f4f4452875286aaaebf54ba66
ridho <ridhohariono1904@gmail.com>
2022-02-28 00:27:45 +0700

    [SLC-28197] Add crawler for littlesheep.com

---
+++ apify/ridhohariono/storelocator/littlesheep_com/scrape.py
@@ -0,0 +1,126 @@
+from bs4 import BeautifulSoup as bs
+from sgrequests import SgRequests
+from sglogging import sglog
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgpostal import parse_address_intl
+
+DOMAIN = "littlesheep.com"
+LOCATION_URL = "http://www.littlesheep.com/ResTuarant"
+DATA_URL = "http://www.littlesheep.com/ResTuarant/getEndList"
+HEADERS = {
+    "Accept": "application/json, text/plain, */*",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "x-requested-with": "XMLHttpRequest",
+}
+MISSING = "<MISSING>"
+log = sglog.SgLogSetup().get_logger(logger_name=DOMAIN)
+
+session = SgRequests()
+
+
+def getAddress(raw_address):
+    try:
+        if raw_address is not None and raw_address != MISSING:
+            data = parse_address_intl(raw_address)
+            street_address = data.street_address_1
+            if data.street_address_2 is not None:
+                street_address = street_address + " " + data.street_address_2
+            city = data.city
+            state = data.state
+            zip_postal = data.postcode
+            if street_address is None or len(street_address) == 0:
+                street_address = MISSING
+            if city is None or len(city) == 0:
+                city = MISSING
+            if state is None or len(state) == 0:
+                state = MISSING
+            if zip_postal is None or len(zip_postal) == 0:
+                zip_postal = MISSING
+            return street_address, city, state, zip_postal
+    except Exception as e:
+        log.info(f"No valid address {e}")
+        pass
+    return MISSING, MISSING, MISSING, MISSING
+
+
+def get_city(_city):
+    try:
+        if _city is not None and _city != MISSING:
+            data = parse_address_intl(_city)
+            city = data.city or MISSING
+            return city
+    except Exception as e:
+        log.info(f"No valid address {e}")
+        pass
+    return MISSING
+
+
+def pull_content(url):
+    log.info("Pull content => " + url)
+    soup = bs(session.get(url, headers=HEADERS).content, "lxml")
+    return soup
+
+
+def fetch_data():
+    log.info("Fetching store_locator data")
+    soup = pull_content(DATA_URL)
+    stores = soup.select("ul li")
+    for row in stores:
+        info = (
+            row["onclick"]
+            .replace("ClickStore('", "")
+            .replace("')", "")
+            .replace("'", "")
+            .split("|")
+        )
+        location_name = info[1]
+        raw_address = info[2]
+        street_address, city, state, zip_postal = getAddress(raw_address)
+        phone = info[3].split(",")[0].split("\n")[0]
+        country_code = "CN"
+        hours_of_operation = MISSING
+        store_number = info[3].split(",")[1]
+        location_type = MISSING
+        coord = info[0].split(",")
+        if len(coord[0].split(".")[0]) < 3 or float(coord[0]) < 80:
+            latitude = coord[0]
+            longitude = coord[1]
+        else:
+            latitude = coord[1]
+            longitude = coord[0]
+        log.info("Append {} => {}".format(location_name, street_address))
+        yield SgRecord(
+            locator_domain=DOMAIN,
+            page_url=LOCATION_URL,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=zip_postal,
+            country_code=country_code,
+            store_number=store_number,
+            phone=phone,
+            location_type=location_type,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+            raw_address=raw_address,
+        )
+
+
+def scrape():
+    log.info("start {} Scraper".format(DOMAIN))
+    count = 0
+    with SgWriter(SgRecordDeduper(RecommendedRecordIds.StoreNumberId)) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
+    log.info(f"No of records being processed: {count}")
+    log.info("Finished")
+
+
+scrape()
