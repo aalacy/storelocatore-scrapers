57a0c9eab738b933bd322c705893d57d8d6affc4
Michael Dinh <mkdinh94@gmail.com>
2022-02-28 13:33:12 -0500

    [SLC-23057] fix incorrect city value

--- apify/CalinClaudiu/storelocator/mg_co_uk/scrape.py
+++ apify/CalinClaudiu/storelocator/mg_co_uk/scrape.py
@@ -1,8 +1,11 @@
+from ast import parse
+import re
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgzip.static import static_zipcode_list, SearchableCountries
+from sgscrape.sgpostal import parse_address, International_Parser
 from sgrequests import SgRequests
 
 
@@ -40,6 +43,7 @@ def fetch_with_retry(url, retry=0):
 
 
 def fetch_data():
+    page_url = 'https://www.mg.co.uk/dealers'
     search = static_zipcode_list(10, country_code=SearchableCountries.BRITAIN)
 
     for zipcode in search:
@@ -58,7 +62,11 @@ def fetch_data():
 
             details = store.get("sales_details") or store.get("after_sales")
             street_address = get(details, "address1")
-            city = get(details, "town")
+
+            city =  get(details, "town")
+            if re.search(r'^MG', city):
+                city = get(details,'address2')
+
             state = get(store, "state")
             postal = get(details, "postcode")
 
@@ -70,6 +78,7 @@ def fetch_data():
             )
 
             yield SgRecord(
+                page_url=page_url,
                 location_name=location_name,
                 street_address=street_address,
                 city=city,
