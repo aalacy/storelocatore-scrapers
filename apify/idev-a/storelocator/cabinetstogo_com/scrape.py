3d5484159a313f93e280a574315a4cf99bd466d9
idev-a <ideveloper003@protonmail.com>
2022-02-28 02:12:18 -0800

    f

--- apify/idev-a/storelocator/cabinetstogo_com/scrape.py
+++ apify/idev-a/storelocator/cabinetstogo_com/scrape.py
@@ -35,7 +35,9 @@ def fetch_data():
             hours = list(bs(_["hours"], "lxml").stripped_strings)
             if "Please contact store" in hours[0]:
                 del hours[0]
-            page_url = locator_domain + _["storelink"]
+            if "Due to" in hours[0]:
+                del hours[0]
+
             phone = _["phone"]
             if "not available" in phone:
                 phone = ""
@@ -46,7 +48,7 @@ def fetch_data():
             ):
                 hours_of_operation = ""
             yield SgRecord(
-                page_url=page_url,
+                page_url=locator_domain + _["storelink"],
                 location_name=_["showroom"],
                 street_address=_["street"],
                 city=_["city"],
