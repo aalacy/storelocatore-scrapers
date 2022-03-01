4507dda05bfd752727e92630168fd504b4df3213
Samia Kiran <samiakiran17@gmail.com>
2022-03-01 20:25:12 +0500

    Update scrape.py

--- apify/SamiaKiran/storelocator/carryout_ie/scrape.py
+++ apify/SamiaKiran/storelocator/carryout_ie/scrape.py
@@ -65,7 +65,7 @@ def fetch_data():
                 state=state.strip(),
                 zip_postal=zip_postal.strip(),
                 country_code=country_code,
-                store_number=MISSING,
+                store_number=store_number,
                 phone=phone,
                 location_type=MISSING,
                 latitude=latitude,
