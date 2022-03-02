ce0ce496aae7b24d075d545a866c413cac6ef4b5
datarhan <slutchenko@gmail.com>
2022-03-02 12:50:52 +0100

    [SLC-31060] maintenance

--- apify/datarhan/storelocator/buildwithbmc_com/scrape.py
+++ apify/datarhan/storelocator/buildwithbmc_com/scrape.py
@@ -1,4 +1,3 @@
-import json
 from lxml import etree
 from urllib.parse import urljoin
 
@@ -23,83 +22,50 @@ def fetch_data():
         )
         response = session.get(state_url)
         dom = etree.HTML(response.text)
-        all_locations = dom.xpath(
-            '//div[@class="section2 search-result-item"]//a[contains(text(), "More Details")]/@href'
-        )
-
-        for url in list(set(all_locations)):
-            store_url = urljoin(start_url, url)
-            store_response = session.get(store_url)
-            if store_response.status_code != 200:
-                continue
-            dom = etree.HTML(store_response.text)
-            store_data = dom.xpath('//div[@id="map_canvas"]/@data-stores')
-            if not store_data:
-                continue
-            store_data = json.loads(store_data[0])
-
-            address_raw = etree.HTML(store_data["name"])
-            address_raw = address_raw.xpath("//text()")
-            if len(address_raw) == 6:
-                address_raw = [
-                    address_raw[0],
-                    ", ".join(address_raw[1:3]),
-                ] + address_raw[3:]
-            location_name = address_raw[0]
-            street_address = address_raw[1]
-            city = address_raw[2]
-            address_raw_2 = dom.xpath('//div[@class="detailSection section2"]/text()')
-            address_raw_2 = [elem.strip() for elem in address_raw_2 if elem.strip()]
-            state = address_raw_2[1].split(",")[-1].split()[0]
-            zip_code = address_raw[3]
-            country_code = address_raw[-1]
-            store_number = dom.xpath('//input[@id="storename"]/@value')[0]
-            phone = dom.xpath(
-                '//a[@class="phone-number phone-format bold-font"]/text()'
-            )
-            phone = phone[0] if phone else "<MISSING>"
-            location_type = dom.xpath(
-                '//h3[contains(text(), "Facility Type")]/following-sibling::div//label/text()'
-            )
-            location_type = ", ".join(location_type) if location_type else "<MISSING>"
-            latitude = store_data["latitude"]
-            latitude = latitude if latitude else "<MISSING>"
-            longitude = store_data["longitude"]
-            longitude = longitude if longitude else "<MISSING>"
-            hours_of_operation = dom.xpath(
-                '//div[@class="section1 puckup-hours section2"]/div//text()'
-            )
-            hours_of_operation = [
-                elem.strip()
-                for elem in hours_of_operation
-                if elem.strip() and "/" not in elem
+        all_locations = dom.xpath('//div[@class="section2 search-result-item"]')
+        for poi_html in all_locations:
+            url = poi_html.xpath('.//a[contains(text(), "More")]/@href')[0]
+            page_url = urljoin(start_url, url)
+            location_name = poi_html.xpath(".//h2/text()")[0][3:]
+            store_number = poi_html.xpath(
+                './/div[@class="pull-right white-font"]/text()'
+            )[0].split("#")[-1]
+            raw_address = poi_html.xpath(".//p/text()")
+            raw_address = [
+                e.replace("\xa0", "").strip() for e in raw_address if e.strip()
             ]
-            hours_of_operation = [
-                " ".join([e.strip() for e in elem.split()])
-                for elem in hours_of_operation
-            ]
-            hours_of_operation = " ".join(hours_of_operation)
-            hours_of_operation = (
-                hours_of_operation.replace(": - Monday", "Monday")
-                if hours_of_operation
-                else "<MISSING>"
+            phone = poi_html.xpath('.//a[contains(@href, "tel")]/text()')[0]
+            location_type = poi_html.xpath(
+                './/h3[contains(text(), "Facility Type")]/following-sibling::div//text()'
+            )
+            location_type = [e.strip() for e in location_type if e.strip()]
+            location_type = ", ".join(location_type)
+            latitude = poi_html.xpath(".//@data-latitude")[0]
+            longitude = poi_html.xpath(".//@data-longitude")[0]
+            hoo = poi_html.xpath(
+                './/div[h3[contains(text(), "Hours")]]/following-sibling::div//text()'
+            )
+            hoo = " ".join(
+                " ".join(
+                    [e.replace("\n", "").strip() for e in hoo if e.strip()]
+                ).split()
             )
 
             item = SgRecord(
                 locator_domain=domain,
-                page_url=store_url,
+                page_url=page_url,
                 location_name=location_name,
-                street_address=street_address,
-                city=city,
-                state=state,
-                zip_postal=zip_code,
-                country_code=country_code,
+                street_address=raw_address[0],
+                city=raw_address[1].split(",")[0],
+                state=raw_address[1].split(",")[-1].split()[0],
+                zip_postal=raw_address[1].split(",")[-1].split()[-1],
+                country_code="",
                 store_number=store_number,
                 phone=phone,
                 location_type=location_type,
                 latitude=latitude,
                 longitude=longitude,
-                hours_of_operation=hours_of_operation,
+                hours_of_operation=hoo,
             )
 
             yield item
