97b1fd4227ca02aed701d86fdcbc82bd7ea4abeb
extrasauceontop <msuchemba@gmail.com>
2022-03-04 15:12:38 -0600

    scrape

--- apify/extrasauceontop/curatedsources/headquartersoffice_com/scrape.py
+++ apify/extrasauceontop/curatedsources/headquartersoffice_com/scrape.py
@@ -1,4 +1,3 @@
-from pydoc import stripid
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -34,7 +33,7 @@ def get_page_urls():
             response = session.get(url, headers=headers).text
 
         except Exception:
-            print(url)
+            log.info(url)
             break
         if "looks like nothing was found at this location" in response:
             break
