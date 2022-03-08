432e1572c2b755200578bf08431fec29b75a32de
idev-a <ideveloper003@protonmail.com>
2022-03-08 15:57:03 -0800

    f

--- apify/idev-a/storelocator/schoolofrock_com/scrape.py
+++ apify/idev-a/storelocator/schoolofrock_com/scrape.py
@@ -13,44 +13,53 @@ _headers = {
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
 }
 locator_domain = "https://www.schoolofrock.com"
-base_url = "https://locations.schoolofrock.com/"
+base_url = "https://www.schoolofrock.com/ajax/get-book-class-form-locations"
 
 
 def fetch_records(http):
-    locs = json.loads(
-        http.get(base_url, headers=_headers)
-        .text.split("var closestLocations =")[1]
-        .split("</script>")[0]
-        .strip()[:-1]
-    )
+    locs = http.get(base_url, headers=_headers).json()["data"]["locations"]
     for _ in locs:
-        page_url = _["url"]
+        page_url = _.get("url")
+        if not page_url:
+            continue
         logger.info(page_url)
         sp1 = bs(http.get(page_url, headers=_headers).text, "lxml")
+        if (
+            sp1.select_one("div.wrapper")
+            and "Coming Soon" in sp1.select_one("div.wrapper").text
+        ):
+            continue
         bar = sp1.select_one("div.promo-bar")
         if (
             bar
             and "coming soon" in bar.text.lower()
-            and "black friday" not in bar.text.lower()
-            and "summer camp" not in bar.text.lower()
+            and "black friday" in bar.text.lower()
+            and "summer camp" in bar.text.lower()
         ):
             continue
         hours = [
             hh.text.replace("\n", "").replace("   ", "").strip()
             for hh in sp1.select("ul.open-hours li")
         ]
+
+        street_address = _["address"]
+        if _["suburb"]:
+            street_address += " " + _["suburb"]
+        phone = _.get("phone")
+        if phone:
+            phone = phone.split("/")[0].strip()
         yield SgRecord(
             page_url=page_url,
             store_number=_["id"],
             location_name=_["name"],
-            street_address=_["address"],
+            street_address=street_address,
             city=_["city"],
             state=_.get("state"),
             zip_postal=_.get("zip"),
             country_code=_["country"],
             latitude=_["display_latitude"],
             longitude=_["display_longitude"],
-            phone=_.get("phone"),
+            phone=phone,
             locator_domain=locator_domain,
             hours_of_operation="; ".join(hours),
         )
