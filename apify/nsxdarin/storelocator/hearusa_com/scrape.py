18be3e2a1275cd92ed366e91aa2590563e17d400
nsxdarin <nsxdarin@gmail.com>
2022-03-10 09:57:38 -0800

    Update scrape.py

--- apify/nsxdarin/storelocator/hearusa_com/scrape.py
+++ apify/nsxdarin/storelocator/hearusa_com/scrape.py
@@ -14,7 +14,7 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
 
-search = DynamicZipSearch(
+search = DynamicGeoSearch(
     country_codes=[SearchableCountries.USA],
     max_search_distance_miles=100,
     max_search_results=25,
