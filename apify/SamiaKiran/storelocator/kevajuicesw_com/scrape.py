a45e6789f406f7259f5fae0cadb5604c96b9787e
Samia Kiran <samiakiran17@gmail.com>
2022-03-21 18:27:08 +0500

    Update scrape.py

--- apify/SamiaKiran/storelocator/kevajuicesw_com/scrape.py
+++ apify/SamiaKiran/storelocator/kevajuicesw_com/scrape.py
@@ -34,7 +34,7 @@ def fetch_data():
         loclist = session.get(url, headers=headers).json()["data"]
         for loc in loclist:
             location_name = loc["display_name"]
-            if "Keva Juice Southwest, LLC # 10" in location_name:
+            if "Keva Juice Southwest, LLC  # 10" in location_name:
                 continue
             store_number = loc["id"]
             temp = loc["address"]["data"]
