34dc765c711cda66ecc01a88f09c7f646c1fa1d0
dima <shshipovald@mail.ru>
2022-03-30 11:43:40 +0300

    fix daltile.com

--- apify/himanshu/storelocator/daltile_com/scrape.py
+++ apify/himanshu/storelocator/daltile_com/scrape.py
@@ -12,8 +12,6 @@ session = SgRequests()
 
 def get_data(zipps, sgw: SgWriter):
 
-    adressess = []
-
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
