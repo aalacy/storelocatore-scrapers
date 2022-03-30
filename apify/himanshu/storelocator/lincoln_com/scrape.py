b1dbaf4723ae65e3ac3c46f42fb9e3b1bd2f6a82
nomihd <nouman.awais.nu@gmail.com>
2022-03-30 16:43:48 +0500

    fix

--- apify/himanshu/storelocator/lincoln_com/scrape.py
+++ apify/himanshu/storelocator/lincoln_com/scrape.py
@@ -1,5 +1,5 @@
 from sgrequests import SgRequests
-from sgzip.dynamic import DynamicZipSearch, SearchableCountries, Grain_1_KM
+from sgzip.dynamic import DynamicZipSearch, SearchableCountries
 from sglogging import SgLogSetup
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
@@ -7,7 +7,7 @@ from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 logger = SgLogSetup().get_logger("lincoln_com")
-session = SgRequests()
+session = SgRequests(dont_retry_status_codes=([404]))
 headers = {
     "authority": "www.lincoln.com",
     "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
@@ -31,8 +31,7 @@ def fetch_data():
     base_url = "https://www.lincoln.com"
     zipcodes = DynamicZipSearch(
         country_codes=[SearchableCountries.USA],
-        expected_search_radius_miles=20,
-        granularity=Grain_1_KM(),
+        
     )
     for zip_code in zipcodes:
         str_zip = str(zip_code)
