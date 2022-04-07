bde7edc45837d3f308be1a44c0676c19e85546d8
Michael Dinh <mkdinh94@gmail.com>
2022-04-07 12:06:04 -0400

    [SLC-23825] linting

--- apify/mkdinh/storelocator/auchan_ro/scrape.py
+++ apify/mkdinh/storelocator/auchan_ro/scrape.py
@@ -12,40 +12,49 @@ logger = SgLogSetup().get_logger("questdiagonstics.com")
 
 session = SgRequests()
 
-def get_csrf_token():
-    html = session.get('https://www.auchan.ro/store/home')
-    soup = BeautifulSoup(html, 'html.parser')
-    return soup.find('input', {"name":'CSRFToken'}).attrs['value']
-    
-def fetch_locations(token, query=""):
-    data = {
-        "locationQuery": re.sub(r'\(.*\)', '', query),
-        "CSRFToken": token
-    }
-    html = session.post('https://www.auchan.ro/store/store-pickup/pointOfServices', data=data).text
-    soup = BeautifulSoup(html, 'html.parser')
 
-    cities = [item.text.strip() for item in soup.find('div', id='pos-list-localities').find_all('div', id='pos-list')]
-    locations = soup.find_all('li', class_='searchPOSResult')
+def get_csrf_token():
+    html = session.get("https://www.auchan.ro/store/home")
+    soup = BeautifulSoup(html, "html.parser")
+    return soup.find("input", {"name": "CSRFToken"}).attrs["value"]
+
+
+def fetch_locations(token, query=""):
+    data = {"locationQuery": re.sub(r"\(.*\)", "", query), "CSRFToken": token}
+    html = session.post(
+        "https://www.auchan.ro/store/store-pickup/pointOfServices", data=data
+    ).text
+    soup = BeautifulSoup(html, "html.parser")
+
+    cities = [
+        item.text.strip()
+        for item in soup.find("div", id="pos-list-localities").find_all(
+            "div", id="pos-list"
+        )
+    ]
+    locations = soup.find_all("li", class_="searchPOSResult")
 
     return locations, cities
+
 
 def fetch_data():
     token = get_csrf_token()
     locations, cities = fetch_locations(token)
 
-    locator_domain = 'auchan.ro'
-    page_url = 'https://www.auchan.ro/store/home'
+    locator_domain = "auchan.ro"
+    page_url = "https://www.auchan.ro/store/home"
     for city in cities:
         locations, _ = fetch_locations(token, city)
 
         for location in locations:
-            store_number = location.find('button', id='changePickupStoreButton').attrs['data-store']
-            latitude = location.attrs['data-latitude']
-            longitude = location.attrs['data-longitude']
+            store_number = location.find("button", id="changePickupStoreButton").attrs[
+                "data-store"
+            ]
+            latitude = location.attrs["data-latitude"]
+            longitude = location.attrs["data-longitude"]
 
-            location_name = location.find('span', class_='resultName').text.strip()
-            address = location.find('div', class_='resultLine1').text
+            location_name = location.find("span", class_="resultName").text.strip()
+            address = location.find("div", class_="resultLine1").text
             parsed = parse_address(International_Parser(), address)
 
             street_address = parsed.street_address_1
@@ -62,12 +71,11 @@ def fetch_data():
                 latitude=latitude,
                 longitude=longitude,
                 street_address=street_address,
-                city = city,
+                city=city,
                 zip_postal=postal,
                 state=state,
                 country_code=country_code,
             )
-
 
 
 if __name__ == "__main__":
