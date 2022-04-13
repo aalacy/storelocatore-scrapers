ba9bbc79894157b5bf38ece87bd6bba5e7709a5c
Faruque A <azam@wscraper.com>
2022-04-13 21:38:01 +0600

    SLC-26629: fixed

--- apify/MrQwerty1/storelocator/davidjones_com/scrape.py
+++ apify/MrQwerty1/storelocator/davidjones_com/scrape.py
@@ -106,7 +106,7 @@ def scrape():
         scraper_name="Crawler",
         data_fetcher=fetch_data,
         field_definitions=field_defs,
-        log_stats_interval=1,
+        log_stats_interval=5,
     )
 
     pipeline.run()
