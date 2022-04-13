e82932b0e8b192c338c7ab2b450b69bf0e86e904
datarhan <slutchenko@gmail.com>
2022-04-13 14:22:46 +0200

    [SLC-33424] maintenance

--- apify/datarhan/storelocator/toyota_ro/scrape.py
+++ apify/datarhan/storelocator/toyota_ro/scrape.py
@@ -17,6 +17,10 @@ def fetch_data():
         "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/toyota/se/sv/drive/13.167381/55.701493?count=500&extraCountries=&limitSearchDistance=0&isCurrentLocation=false&services=",
         "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/toyota/sk/sk/drive/17.135763/48.156086?count=500&extraCountries=&limitSearchDistance=60&isCurrentLocation=false",
         "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/toyota/dk/da/drive/12.079135/55.648779?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/toyota/gb/en/drive/-0.081018/51.652085?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/toyota/es/es/drive/-3.703583/40.416705?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/toyota/fi/fi/drive/22.75/60.5?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-prox-aws.toyota-europe.com/dxp/dealers/api/toyota/fr/fr/drive/0.428551/44.064432?count=500&extraCountries=MC&isCurrentLocation=false",
     ]
     domain = "toyota.ro"
     hdr = {
@@ -25,21 +29,26 @@ def fetch_data():
     for url in start_urls:
         data = session.get(url, headers=hdr).json()
         for poi in data["dealers"]:
-            page_url = poi["url"]
-            loc_response = session.get(page_url, headers=hdr)
+            page_url = poi.get("url")
             hoo = ""
-            if loc_response.status_code == 200:
-                loc_dom = etree.HTML(loc_response.text)
+            if page_url:
+                loc_response = session.get(page_url, headers=hdr)
+                if loc_response.status_code == 200:
+                    loc_dom = etree.HTML(loc_response.text)
 
-                hoo = loc_dom.xpath(
-                    '//div[h3[i[@class="fa fa-clock-o fa-fw"]]]/following-sibling::div[1]//text()'
-                )
-                hoo = " ".join(
-                    [" ".join([l for l in e.strip().split()]) for e in hoo if e.strip()]
-                )
-                if hoo == "-":
-                    hoo = ""
-                hoo = hoo.replace("<o:p></o:p>", "")
+                    hoo = loc_dom.xpath(
+                        '//div[h3[i[@class="fa fa-clock-o fa-fw"]]]/following-sibling::div[1]//text()'
+                    )
+                    hoo = " ".join(
+                        [
+                            " ".join([l for l in e.strip().split()])
+                            for e in hoo
+                            if e.strip()
+                        ]
+                    )
+                    if hoo == "-":
+                        hoo = ""
+                    hoo = hoo.replace("<o:p></o:p>", "")
             street_address = poi["address"]["address1"]
             city = poi["address"]["city"]
             zip_code = poi["address"]["zip"]
