7a6db17030fa56ce3a32cafa4d754941e188c3c7
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2022-04-15 04:12:10 +0500

    Update scrape.py

--- apify/ggrahambaker/storelocator/deciem_com/scrape.py
+++ apify/ggrahambaker/storelocator/deciem_com/scrape.py
@@ -11,7 +11,7 @@ from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
 def fetch_data():
 
     session = SgRequests()
-    headers = {
+    hdr = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
     }
 
