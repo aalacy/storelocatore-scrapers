3c68649dead3fc8f7b73a34dfb155ccff2a299a0
Quincy <qcalexander@gmail.com>
2022-04-20 05:42:44 -0400

    SLC-33886 fixed

--- apify/quincya/storelocator/spacenk_com/scrape.py
+++ apify/quincya/storelocator/spacenk_com/scrape.py
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
 from sgselenium.sgselenium import SgChrome
 
