e9d8ac2d19e76bef3d15630ed6b49dae4e5b7420
nomihd <nouman.awais.nu@gmail.com>
2022-04-22 03:02:54 +0500

    this is rewrite

--- apify/nomihd/storelocator/bathandbodyworks_in/scrape.py
+++ apify/nomihd/storelocator/bathandbodyworks_in/scrape.py
@@ -3,171 +3,97 @@ from sgrequests import SgRequests
 from sglogging import sglog
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
+import lxml.html
+from sgpostal import sgpostal as parser
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 website = "bathandbodyworks.in"
 log = sglog.SgLogSetup().get_logger(logger_name=website)
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Language": "en-US,en;q=0.9,ar;q=0.8",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+}
 
 
 def fetch_data():
     # Your scraper here
-
-    search_url = "https://www.bathandbodyworks.in/store-locator"
-    headers = {
-        "authority": "www.bathandbodyworks.in",
-        "sec-ch-ua": '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-        "accept": "*/*",
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "x-requested-with": "XMLHttpRequest",
-        "sec-ch-ua-mobile": "?0",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
-        "sec-ch-ua-platform": '"Windows"',
-        "origin": "https://www.bathandbodyworks.in",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-dest": "empty",
-        "referer": "https://www.bathandbodyworks.in/store-locator",
-        "accept-language": "en-US,en-GB;q=0.9,en;q=0.8",
-    }
-
-    params = (("route", "extension/module/wk_store_locater/setter"),)
-
-    data = {
-        "longitude": "0",
-        "latitude": "0",
-        "city": "all",
-        "location": "Name of Town or City",
-    }
+    search_url = "https://www.bathandbodyworks.in/bbw-store-locator.html"
 
     with SgRequests() as session:
-        search_res = session.post(
-            "https://www.bathandbodyworks.in/index.php",
-            headers=headers,
-            params=params,
-            data=data,
-        )
-        log.info(search_res)
-        stores = search_res.text.split("~")
-        for index in range(0, len(stores) - 1):
-            store_info = stores[index].split("!")
-            page_url = search_url
+        search_res = session.get(search_url, headers=headers)
+
+        search_sel = lxml.html.fromstring(search_res.text)
+
+        stores = search_sel.xpath('//div[@class="store_address"]/..')
+
+        for no, store in enumerate(stores, 1):
 
             locator_domain = website
 
-            location_name = store_info[3].strip()
+            page_url = search_url + str(no)
 
-            raw_address = (
-                stores[index]
-                .split("!!!")[-1]
-                .strip()
-                .replace("\\/", "/")
-                .strip()
-                .split("PH -")[0]
-                .strip()
-                .split("; Contact-")[0]
-                .strip()
-                .replace("\\u2013", "-")
-                .strip()
-                .split("; contact -")[0]
-                .strip()
-                .replace("\\n", "")
-                .strip()
-                .split("Email:")[0]
-                .strip()
-                .split(", INDIA")[0]
-                .strip()
-            )
-            if raw_address[-1] == ",":
-                raw_address = "".join(raw_address[:-1]).strip()
-
-            street_address = ", ".join(raw_address.split(",")[:-1]).strip()
-            city = (
-                raw_address.split(",")[-1]
-                .strip()
-                .replace(
-                    "Chandigarh Industrial Area (Near centra mall) Phase", "Chandigarh"
-                )
-                .strip()
-            )
-            try:
-                city = city.split("-")[0].strip()
-            except:
-                pass
-            state = "<MISSING>"
-            zip = "<MISSING>"
-            try:
-                if "Pincode" in raw_address:
-                    city = city.split("Pincode")[0].strip()
-                    zip = raw_address.split("Pincode")[1].strip()
-                else:
-                    zip = raw_address.rsplit("-", 1)[-1].strip()
-            except:
-                pass
-
-            if not zip.isdigit():
-                zip = "<MISSING>"
-
-            if city == "Shop No":
-                street_address = (
-                    "Shop No - G 14 15 L2 Lower ground floor Hi-tech city opp to I Labs"
-                )
-                city = "Hyderabad"
-
-            if city == "Infinity 2 Link RD Malad(W) Mumbai":
-                street_address = "F-118, Infinity 2 Link RD Malad(W)"
-                city = "Mumbai"
-
-            try:
-                if city.split(" ")[-1].isdigit():
-                    zip = city.split(" ")[-1]
-                    city = " ".join(city.split(" ")[:-1]).strip()
-            except:
-                pass
-            country_code = "IN"
-
-            store_number = store_info[0].strip().replace('"', "").strip()
-            phone = "<MISSING>"
-            try:
-                phone = (
-                    stores[index]
-                    .split("!!!")[-1]
-                    .strip()
-                    .replace("\\/", "/")
-                    .strip()
-                    .split("PH -")[1]
-                    .strip()
-                )
-            except:
-                try:
-                    phone = (
-                        stores[index]
-                        .split("!!!")[-1]
-                        .strip()
-                        .replace("\\/", "/")
-                        .strip()
-                        .split("; Contact-")[1]
-                        .strip()
-                    )
-                except:
-                    try:
-                        phone = (
-                            stores[index]
-                            .split("!!!")[-1]
-                            .strip()
-                            .replace("\\/", "/")
-                            .strip()
-                            .split("; contact -")[1]
-                            .strip()
-                        )
-                    except:
-                        pass
+            location_name = "".join(
+                store.xpath('.//div[@class="store_title"]/text()')
+            ).strip()
 
             location_type = "<MISSING>"
 
-            hours_of_operation = "<MISSING>"
+            store_info = list(
+                filter(
+                    str,
+                    [
+                        x.strip()
+                        for x in store.xpath('.//p[@class="store_addressline"]//text()')
+                    ],
+                )
+            )
 
-            latitude, longitude = store_info[1].strip(), store_info[2].strip()
+            raw_address = (
+                " ".join(store_info)
+                .strip('" ')
+                .replace("MAJOR BRANDS INDIA PVT LTD, BATH & BODY WORKS,", "")
+                .strip()
+            )
+
+            formatted_addr = parser.parse_address_intl(raw_address)
+            street_address = formatted_addr.street_address_1
+            if formatted_addr.street_address_2:
+                street_address = street_address + ", " + formatted_addr.street_address_2
+
+            if street_address is not None:
+                street_address = street_address.replace("Ste", "Suite")
+
+            city = formatted_addr.city
+
+            state = formatted_addr.state
+            zip = formatted_addr.postcode
+
+            if not state:
+                if zip and "-" in zip:
+                    state = zip.split("-")[0].strip()
+                    zip = zip.split("-")[1].strip()
+
+            if not zip:
+                if city and "-" in city:
+                    zip = city.split("-")[1].strip()
+                    city = city.split("-")[0].strip()
+
+            country_code = formatted_addr.country
+
+            store_number = "".join(
+                store.xpath('.//span[@class="store-number"]/text()')
+            ).strip(" .")
+            phone = "<MISSING>"
+
+            hours_of_operation = "<MISSING>"
+            map_info = "".join(store.xpath("./@str_lati/@value"))
+
+            latitude, longitude = (
+                "".join(store.xpath("./@str_lati/@value")).strip(),
+                "".join(store.xpath("./@str_longi/@value")).strip(),
+            )
 
             yield SgRecord(
                 locator_domain=locator_domain,
