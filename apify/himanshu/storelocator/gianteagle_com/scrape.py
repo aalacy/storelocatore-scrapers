04c0bacde17835f62441044b543953e960b822bf
datarhan <slutchenko@gmail.com>
2022-04-22 13:57:58 +0200

    [SLC-33814] update

--- apify/himanshu/storelocator/gianteagle_com/scrape.py
+++ apify/himanshu/storelocator/gianteagle_com/scrape.py
@@ -12,7 +12,7 @@ logger = SgLogSetup().get_logger("gianteagle_com")
 
 
 def fetch_data():
-    session = SgRequests()
+    session = SgRequests(verify_ssl=False)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
