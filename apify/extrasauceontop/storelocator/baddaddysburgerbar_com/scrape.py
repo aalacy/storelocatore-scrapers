0aa3ae3d1a89ea7d680b0b28b4b822e54df1b1be
extrasauceontop <msuchemba@gmail.com>
2022-04-29 16:36:35 -0500

    scrape

--- apify/extrasauceontop/storelocator/baddaddysburgerbar_com/scrape.py
+++ apify/extrasauceontop/storelocator/baddaddysburgerbar_com/scrape.py
@@ -39,7 +39,11 @@ def get_data():
         "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
     )
 
-    with SgChrome(executable_path=ChromeDriverManager().install(), user_agent=user_agent, is_headless=True).driver() as driver:
+    with SgChrome(
+        executable_path=ChromeDriverManager().install(),
+        user_agent=user_agent,
+        is_headless=True,
+    ).driver() as driver:
         driver.get(
             "https://api.dineengine.io/baddaddys/items/custom_pages?fields%5B0%5D=%2A.%2A.%2A.%2A&single=false&limit=-1"
         )
