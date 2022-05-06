862c63bcae9b6a52f1d72e9f62b8de351c37b3f1
dima <shshipovald@mail.ru>
2022-05-06 11:58:09 +0300

    add mein-immergruen_de

---
+++ apify/Shapoval1/storelocator/mein-immergruen_de/scrape.py
@@ -0,0 +1,62 @@
+from lxml import html
+from sgscrape.sgrecord import SgRecord
+from sgrequests import SgRequests
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgpostal.sgpostal import International_Parser, parse_address
+
+
+def fetch_data(sgw: SgWriter):
+
+    locator_domain = "https://mein-immergruen.de/"
+    api_url = "https://mein-immergruen.de/standorte/"
+    session = SgRequests()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
+    }
+    r = session.get(api_url, headers=headers)
+    tree = html.fromstring(r.text)
+    div = tree.xpath('//div[@class="marker"]')
+    for d in div:
+
+        page_url = "".join(d.xpath('.//p[@class="link"]//a//@href'))
+        ad = "".join(d.xpath('.//p[@class="address"]//text()'))
+        location_name = "".join(d.xpath(".//h4//text()"))
+        a = parse_address(International_Parser(), ad)
+        street_address = (
+            f"{a.street_address_1} {a.street_address_2}".replace("None", "").strip()
+            or "<MISSING>"
+        )
+        state = a.state or "<MISSING>"
+        postal = a.postcode or "<MISSING>"
+        country_code = "DE"
+        city = a.city or "<MISSING>"
+        latitude = "".join(d.xpath(".//@data-lat"))
+        longitude = "".join(d.xpath(".//@data-lng"))
+
+        row = SgRecord(
+            locator_domain=locator_domain,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=postal,
+            country_code=country_code,
+            store_number=SgRecord.MISSING,
+            phone=SgRecord.MISSING,
+            location_type=SgRecord.MISSING,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=SgRecord.MISSING,
+            raw_address=ad,
+        )
+
+        sgw.write_row(row)
+
+
+if __name__ == "__main__":
+    session = SgRequests()
+    with SgWriter(SgRecordDeduper(SgRecordID({SgRecord.Headers.PAGE_URL}))) as writer:
+        fetch_data(writer)
