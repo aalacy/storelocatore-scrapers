2ee0b937d9aa800d9242c7c12a85ef39ab95c596
datarhan <slutchenko@gmail.com>
2022-05-06 18:49:14 +0200

    [SLC-34676] update

--- apify/datarhan/storelocator/adecco_no/scrape.py
+++ apify/datarhan/storelocator/adecco_no/scrape.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# --extra-index-url https://dl.cloudsmith.io/KVaWma76J5VNwrOm/crawl/crawl/python/simple/
-from ctypes import addressof
 from lxml import etree
 from urllib.parse import urljoin
 
