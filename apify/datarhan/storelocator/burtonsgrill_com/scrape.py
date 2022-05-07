5aa8e45bb9fd7c7d03fbee1aa7dcba8798472c8d
extrasauceontop <msuchemba@gmail.com>
2022-05-07 15:46:18 -0500

    scrape

--- apify/datarhan/storelocator/burtonsgrill_com/scrape.py
+++ apify/datarhan/storelocator/burtonsgrill_com/scrape.py
@@ -13,7 +13,7 @@ def fetch_data():
     domain = "burtonsgrill.com"
     hdr = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     }
     response = session.get(start_url, headers=hdr)
     dom = etree.HTML(response.text)
