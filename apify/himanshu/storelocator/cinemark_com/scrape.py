9fda21a3aec9aadb57e9552d330a9dc8c4234f11
Quincy <qcalexander@gmail.com>
2022-05-07 05:34:25 -0400

    SLC-34740 fixed

--- apify/himanshu/storelocator/cinemark_com/scrape.py
+++ apify/himanshu/storelocator/cinemark_com/scrape.py
@@ -1,46 +1,23 @@
-import csv
 from sgrequests import SgRequests
 import json
 from sglogging import SgLogSetup
 import lxml.html
 
-logger = SgLogSetup().get_logger("cinemark_com")
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
+logger = SgLogSetup().get_logger("cinemark_com")
 
 session = SgRequests()
 
 
-def write_output(data):
-    with open("data.csv", newline="", mode="w") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
+def fetch_data(sgw: SgWriter):
 
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-                "page_url",
-            ]
-        )
-        # Body
-        for row in data:
-            writer.writerow(row)
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36"
+    headers = {"User-Agent": user_agent}
 
-
-def fetch_data():
     base_url = "https://cinemark.com/"
 
     r = session.get("https://cinemark.com/full-theatre-list")
@@ -48,63 +25,81 @@ def fetch_data():
     stores_sel = lxml.html.fromstring(r.text)
     links = stores_sel.xpath('//div[@class="columnList wide"]//a/@href')
     for link in links:
-        page_url = "https://cinemark.com/" + link
+        page_url = "https://cinemark.com" + link
+        if "now-closed" in page_url:
+            continue
         logger.info(page_url)
-        r1 = session.get(page_url)
+        r1 = session.get(page_url, headers=headers)
         store_sel = lxml.html.fromstring(r1.text)
         info = "".join(
             store_sel.xpath('//script[@type="application/ld+json"]/text()')
         ).strip()
 
         try:
-            data = json.loads(info)
-            for address in data["address"]:
-                street_address = address["streetAddress"]
-                city = address["addressLocality"]
-                state = address["addressRegion"]
-                zipp = address["postalCode"]
-                country_code = address["addressCountry"]
-            phone = data["telephone"]
-            location_name = data["name"]
-            if "NOW CLOSED".lower() in location_name.lower():
+            if (
+                "permanently closed"
+                in store_sel.xpath('//div[@class="theatre-status-label"]/text()')[
+                    0
+                ].lower()
+            ):
                 continue
-            location_type = data["@type"]
-            latitude = (
-                "".join(store_sel.xpath('//div[@class="theatreMap"]/a/img/@data-src'))
-                .split("pp=")[1]
-                .split(",")[0]
-            )
-            longitude = (
-                "".join(store_sel.xpath('//div[@class="theatreMap"]/a/img/@data-src'))
-                .split("pp=")[1]
-                .split(",")[1]
-                .split("&")[0]
-            )
-
-            store = []
-            store.append(base_url)
-            store.append(location_name)
-            store.append(street_address)
-            store.append(city)
-            store.append(state)
-            store.append(zipp)
-            store.append(country_code)
-            store.append("<MISSING>")
-            store.append(phone if phone else "<MISSING>")
-            store.append(location_type)
-            store.append(latitude if latitude else "<MISSING>")
-            store.append(longitude if longitude else "<MISSING>")
-            store.append("<MISSING>")
-            store.append(page_url)
-
-            yield store
         except:
             pass
 
+        try:
+            if (
+                "coming soon"
+                in str(store_sel.xpath('//div[@class="clearfix"]//text()')[:5]).lower()
+            ):
+                continue
+        except:
+            pass
 
-def scrape():
-    data = fetch_data()
-    write_output(data)
+        data = json.loads(info)
+        for address in data["address"]:
+            street_address = (
+                address["streetAddress"].split("(")[0].split(", The Common")[0].strip()
+            )
+            city = address["addressLocality"]
+            state = address["addressRegion"]
+            zipp = address["postalCode"]
+            country_code = address["addressCountry"]
+        phone = data["telephone"]
+        location_name = data["name"].strip()
+        if "NOW CLOSED".lower() in location_name.lower():
+            continue
+        location_type = ""
+        latitude = (
+            "".join(store_sel.xpath('//div[@class="theatreMap"]/a/img/@data-src'))
+            .split("pp=")[1]
+            .split(",")[0]
+        )
+        longitude = (
+            "".join(store_sel.xpath('//div[@class="theatreMap"]/a/img/@data-src'))
+            .split("pp=")[1]
+            .split(",")[1]
+            .split("&")[0]
+        )
+
+        sgw.write_row(
+            SgRecord(
+                locator_domain=base_url,
+                page_url=page_url,
+                location_name=location_name,
+                street_address=street_address,
+                city=city,
+                state=state,
+                zip_postal=zipp,
+                country_code=country_code,
+                store_number="",
+                phone=phone,
+                location_type=location_type,
+                latitude=latitude,
+                longitude=longitude,
+                hours_of_operation="",
+            )
+        )
 
 
-scrape()
+with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
+    fetch_data(writer)
