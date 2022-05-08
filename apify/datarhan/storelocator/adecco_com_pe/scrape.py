d516bc7168a029bca3c6ff8cebe779a6d4317402
datarhan <slutchenko@gmail.com>
2022-05-08 15:45:43 +0200

    [SLC-34682] Add crawler for adecco.com.pe

---
+++ apify/datarhan/storelocator/adecco_com_pe/scrape.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# --extra-index-url https://dl.cloudsmith.io/KVaWma76J5VNwrOm/crawl/crawl/python/simple/
+from lxml import etree
+
+from sgrequests import SgRequests
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgwriter import SgWriter
+from sgpostal.sgpostal import parse_address_intl
+
+
+def fetch_data():
+    session = SgRequests()
+
+    start_url = "https://www.adecco.com.pe/sucursales/"
+    domain = "adecco.com.pe"
+    hdr = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
+    }
+    response = session.get(start_url, headers=hdr)
+    dom = etree.HTML(response.text)
+
+    all_locations = dom.xpath('//div[@class="addc_maps_Sucursales"]')
+    for poi_html in all_locations:
+        location_name = poi_html.xpath('.//strong/text()')[0]
+        raw_address = poi_html.xpath('.//div[@class="addc_maps_Description"]/p/text()')[0]
+        addr = parse_address_intl(raw_address)
+        street_address = addr.street_address_1
+        if addr.street_address_2:
+            street_address += ' ' + addr.street_address_2
+        geo = poi_html.xpath('.//iframe/@src')[0].split('!2d')[-1].split('!2m')[0].split('!3d')
+
+        item = SgRecord(
+            locator_domain=domain,
+            page_url=start_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=addr.city,
+            state=addr.state,
+            zip_postal=addr.postcode,
+            country_code="PE",
+            store_number="",
+            phone="",
+            location_type="",
+            latitude=geo[0],
+            longitude=geo[1],
+            hours_of_operation="",
+        )
+
+        yield item
+
+
+def scrape():
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID(
+                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
+            )
+        )
+    ) as writer:
+        for item in fetch_data():
+            writer.write_row(item)
+
+
+if __name__ == "__main__":
+    scrape()
