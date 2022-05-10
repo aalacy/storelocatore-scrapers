2a0fea02f3d49576a852b5aa852eb07b1ef18aa2
datarhan <slutchenko@gmail.com>
2022-05-10 14:14:25 +0200

    [SLC-34846] maintenance

--- apify/datarhan/storelocator/fm_bank/scrape.py
+++ apify/datarhan/storelocator/fm_bank/scrape.py
@@ -1,72 +1,38 @@
-import re
-import csv
-
 from sgrequests import SgRequests
-
-
-def write_output(data):
-    with open("data.csv", mode="w", encoding="utf-8") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
-
-        # Header
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-        # Body
-        for row in data:
-            writer.writerow(row)
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgwriter import SgWriter
 
 
 def fetch_data():
-    # Your scraper here
-    session = SgRequests().requests_retry_session(retries=2, backoff_factor=0.3)
+    session = SgRequests()
+    start_url = "https://www.fm.bank/wp-admin/admin-ajax.php?action=store_search&lat=41.52144&lng=-84.30717&max_results=10&search_radius=200&autoload=1"
+    domain = "fm.bank"
 
-    items = []
-
-    start_url = "https://www.fm.bank/wp-admin/admin-ajax.php?action=store_search&lat=41.52144&lng=-84.30717&max_results=50&search_radius=100&autoload=1"
-    domain = re.findall(r"://(.+?)/", start_url)[0].replace("www.", "")
-
-    all_locations = session.get(start_url).json()
+    hdr = {
+        'accept': '*/*',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    all_locations = session.get(start_url, headers=hdr).json()
     for poi in all_locations:
-        store_url = "https://www.fm.bank/locations/"
+        page_url = "https://www.fm.bank/locations/"
         location_name = poi["store"]
         location_name = (
             location_name.replace("&#8211;", "-")
             .replace("#038;", "")
             .replace("&#8217;", "'")
             if location_name
-            else "<MISSING>"
+            else ""
         )
         street_address = poi["address"]
-        street_address = street_address if street_address else "<MISSING>"
         city = poi["city"]
-        city = city if city else "<MISSING>"
         state = poi["state"]
-        state = state if state else "<MISSING>"
         zip_code = poi["zip"]
-        zip_code = zip_code if zip_code else "<MISSING>"
         country_code = poi["country"]
-        country_code = country_code if country_code else "<MISSING>"
         store_number = poi["id"]
         phone = poi["phone"]
-        phone = phone if phone else "<MISSING>"
         location_type = poi["cat_name"]
         latitude = poi["lat"]
         longitude = poi["lng"]
@@ -81,33 +47,38 @@ def fetch_data():
             poi["lobby_hours_v4"],
         ]
         hoo = [e.strip() for e in hoo if e.strip()]
-        hours_of_operation = " ".join(hoo) if hoo else "<MISSING>"
+        hours_of_operation = " ".join(hoo) if hoo else ""
 
-        item = [
-            domain,
-            store_url,
-            location_name,
-            street_address,
-            city,
-            state,
-            zip_code,
-            country_code,
-            store_number,
-            phone,
-            location_type,
-            latitude,
-            longitude,
-            hours_of_operation,
-        ]
+        item = SgRecord(
+            locator_domain=domain,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=zip_code,
+            country_code=country_code,
+            store_number=store_number,
+            phone=phone,
+            location_type=location_type,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+        )
 
-        items.append(item)
-
-    return items
+        yield item
 
 
 def scrape():
-    data = fetch_data()
-    write_output(data)
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID(
+                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
+            )
+        )
+    ) as writer:
+        for item in fetch_data():
+            writer.write_row(item)
 
 
 if __name__ == "__main__":
