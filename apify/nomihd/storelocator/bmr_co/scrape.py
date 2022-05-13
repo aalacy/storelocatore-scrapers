d0c18cb5d972d82753ad798f2e2df21c354fb7c5
nomihd <nouman.awais.nu@gmail.com>
2022-05-13 22:48:02 +0500

    fix

--- apify/nomihd/storelocator/bmr_co/scrape.py
+++ apify/nomihd/storelocator/bmr_co/scrape.py
@@ -11,7 +11,6 @@ import lxml.html
 
 website = "bmr.co"
 log = sglog.SgLogSetup().get_logger(logger_name=website)
-session = SgRequests()
 headers = {
     "authority": "www.bmr.ca",
     "content-length": "0",
@@ -32,82 +31,83 @@ headers = {
 def fetch_data():
     # Your scraper here
     search_url = "https://www.bmr.ca/fr/storelocator/index/loadstore/"
-    stores_req = session.get(search_url, headers=headers)
-    stores = stores_req.json()["storesjson"]
-    openingTimes = json.loads(stores_req.text)["openingTimes"]
-    for store in stores:
-        page_url = "https://www.bmr.co/en/" + store["rewrite_request_path"]
-        locator_domain = website
-        location_name = store["store_name"]
-        street_address = store["address"]
-        city = store["city"]
-        state = store["state"]
-        zip = store["zipcode"]
+    with SgRequests(dont_retry_status_codes=([404])) as session:
+        stores_req = session.get(search_url, headers=headers)
+        stores = stores_req.json()["storesjson"]
+        openingTimes = json.loads(stores_req.text)["openingTimes"]
+        for store in stores:
+            page_url = "https://www.bmr.co/en/" + store["rewrite_request_path"]
+            locator_domain = website
+            location_name = store["store_name"]
+            street_address = store["address"]
+            city = store["city"]
+            state = store["state"]
+            zip = store["zipcode"]
 
-        country_code = "CA"
-        if state is not None and us.states.lookup(state):
-            country_code = "US"
+            country_code = "CA"
+            if state is not None and us.states.lookup(state):
+                country_code = "US"
 
-        store_number = store["storelocator_id"]
-        phone = store["phone"]
-        if "," in phone:
-            phone = phone.split(",")[0].strip()
+            store_number = store["storelocator_id"]
+            phone = store["phone"]
+            if "," in phone:
+                phone = phone.split(",")[0].strip()
 
-        location_type = "<MISSING>"
-
-        hours_list = []
-        hours_found = True
-        for time in openingTimes:
-            if store_number == time["storelocator_id"]:
-                hours = time["store_hours"]
-                for day in hours.keys():
-                    time = ""
-                    if isinstance(hours[day], dict):
-                        if "open" in hours[day]:
-                            time = hours[day]["open"] + " - " + hours[day]["close"]
-                        else:
-                            hours_found = False
-                            break
-                    else:
-                        time = hours[day]
-                    if len(time) > 0:
-                        hours_list.append(day + ":" + time)
-
-                break
-
-        if hours_found is False:
-            log.info(page_url)
-            store_req = session.get(page_url, headers=headers)
-            store_sel = lxml.html.fromstring(store_req.text)
+            location_type = "<MISSING>"
 
             hours_list = []
-            hours = store_sel.xpath('//div[@class="opening-hours"]/li')
-            for hour in hours:
-                day = "".join(hour.xpath('div[@class="content1"]/text()')).strip()
-                time = "".join(hour.xpath('div[@class="content2"]//text()')).strip()
-                hours_list.append(day + ":" + time)
+            hours_found = True
+            for time in openingTimes:
+                if store_number == time["storelocator_id"]:
+                    hours = time["store_hours"]
+                    for day in hours.keys():
+                        time = ""
+                        if isinstance(hours[day], dict):
+                            if "open" in hours[day]:
+                                time = hours[day]["open"] + " - " + hours[day]["close"]
+                            else:
+                                hours_found = False
+                                break
+                        else:
+                            time = hours[day]
+                        if len(time) > 0:
+                            hours_list.append(day + ":" + time)
 
-        hours_of_operation = "; ".join(hours_list).strip()
+                    break
 
-        latitude = store["latitude"]
-        longitude = store["longitude"]
+            if hours_found is False:
+                log.info(page_url)
+                store_req = session.get(page_url, headers=headers)
+                store_sel = lxml.html.fromstring(store_req.text)
 
-        yield SgRecord(
-            locator_domain=locator_domain,
-            page_url=page_url,
-            location_name=location_name,
-            street_address=street_address,
-            city=city,
-            state=state,
-            zip_postal=zip,
-            country_code=country_code,
-            store_number=store_number,
-            phone=phone,
-            location_type=location_type,
-            latitude=latitude,
-            longitude=longitude,
-            hours_of_operation=hours_of_operation,
-        )
+                hours_list = []
+                hours = store_sel.xpath('//div[@class="opening-hours"]/li')
+                for hour in hours:
+                    day = "".join(hour.xpath('div[@class="content1"]/text()')).strip()
+                    time = "".join(hour.xpath('div[@class="content2"]//text()')).strip()
+                    hours_list.append(day + ":" + time)
+
+            hours_of_operation = "; ".join(hours_list).strip()
+
+            latitude = store["latitude"]
+            longitude = store["longitude"]
+
+            yield SgRecord(
+                locator_domain=locator_domain,
+                page_url=page_url,
+                location_name=location_name,
+                street_address=street_address,
+                city=city,
+                state=state,
+                zip_postal=zip,
+                country_code=country_code,
+                store_number=store_number,
+                phone=phone,
+                location_type=location_type,
+                latitude=latitude,
+                longitude=longitude,
+                hours_of_operation=hours_of_operation,
+            )
 
 
 def scrape():
