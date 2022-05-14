f31ea8edbce004ba43da689476e036790de4eca7
extrasauceontop <msuchemba@gmail.com>
2022-05-14 17:08:37 -0500

    Scrape

--- apify/extrasauceontop/storelocator/centrehifi_com/scrape.py
+++ apify/extrasauceontop/storelocator/centrehifi_com/scrape.py
@@ -12,46 +12,50 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def get_driver(url, class_name, driver=None):
-    if driver is not None:
-        driver.quit()
+# def get_driver(url, class_name, driver=None):
+#     if driver is not None:
+#         driver.quit()
 
-    user_agent = (
-        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
-    )
-    x = 0
-    while True:
-        x = x + 1
-        try:
-            driver = SgChrome(
-                executable_path=ChromeDriverManager().install(),
-                user_agent=user_agent,
-                is_headless=True,
-            ).driver()
-            driver.get(url)
+#     user_agent = (
+#         "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
+#     )
+#     x = 0
+#     while True:
+#         x = x + 1
+#         try:
+#             driver = SgChrome(
+#                 executable_path=ChromeDriverManager().install(),
+#                 user_agent=user_agent,
+#                 is_headless=True,
+#             ).driver()
+#             driver.get(url)
 
-            WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.CLASS_NAME, class_name))
-            )
-            break
-        except Exception:
-            driver.quit()
-            if x == 10:
-                raise Exception(
-                    "Make sure this ran with a Proxy, will fail without one"
-                )
-            continue
-    return driver
+#             WebDriverWait(driver, 20).until(
+#                 EC.presence_of_element_located((By.CLASS_NAME, class_name))
+#             )
+#             break
+#         except Exception:
+#             driver.quit()
+#             if x == 10:
+#                 raise Exception(
+#                     "Make sure this ran with a Proxy, will fail without one"
+#                 )
+#             continue
+#     return driver
 
 
 def get_data():
     url = "https://www.centrehifi.com/en/store-locator/"
     class_name = "popup-language-header"
-    driver = get_driver(url, class_name)
-    response = driver.page_source
+
+    with SgChrome() as driver:
+        driver.get(url)
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, class_name))
+        )
+        response = driver.page_source
 
     soup = bs(response, "html.parser")
-    driver.quit()
 
     grids = soup.find_all("div", attrs={"class": "js-single-store-info"})
 
@@ -122,12 +126,21 @@ def get_data():
                     + address_parts.split(", ")[-1].strip().split(" ")[-1]
                 )
                 state = "<MISSING>"
-
+        address = address.strip()
         store_number = grid["data-store-id"]
         phone = ""
         location_type = ""
         hours = ""
         country_code = "CA"
+
+        try:
+            phone = "(" + location_name.split("(")[1]
+            location_name = location_name.replace(phone, "").strip()
+        except Exception:
+            pass
+
+        if "," == address[-1]:
+            address = address[:-1]
 
         yield {
             "locator_domain": locator_domain,
