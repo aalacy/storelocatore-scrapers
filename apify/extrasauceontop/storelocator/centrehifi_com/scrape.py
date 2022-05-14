4d66aa471d6e42bd888fbe4fea5175c7a6cd129d
extrasauceontop <msuchemba@gmail.com>
2022-05-14 17:10:14 -0500

    Scrape

--- apify/extrasauceontop/storelocator/centrehifi_com/scrape.py
+++ apify/extrasauceontop/storelocator/centrehifi_com/scrape.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from sgselenium.sgselenium import SgChrome
-from webdriver_manager.chrome import ChromeDriverManager
 from sgscrape import simple_scraper_pipeline as sp
 from bs4 import BeautifulSoup as bs
 import re
@@ -10,38 +9,6 @@ import unidecode
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
-
-
-# def get_driver(url, class_name, driver=None):
-#     if driver is not None:
-#         driver.quit()
-
-#     user_agent = (
-#         "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
-#     )
-#     x = 0
-#     while True:
-#         x = x + 1
-#         try:
-#             driver = SgChrome(
-#                 executable_path=ChromeDriverManager().install(),
-#                 user_agent=user_agent,
-#                 is_headless=True,
-#             ).driver()
-#             driver.get(url)
-
-#             WebDriverWait(driver, 20).until(
-#                 EC.presence_of_element_located((By.CLASS_NAME, class_name))
-#             )
-#             break
-#         except Exception:
-#             driver.quit()
-#             if x == 10:
-#                 raise Exception(
-#                     "Make sure this ran with a Proxy, will fail without one"
-#                 )
-#             continue
-#     return driver
 
 
 def get_data():
