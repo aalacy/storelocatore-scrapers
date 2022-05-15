8a647f3e411761c3682aea1cf0b5ac9b303a0fcf
Michael Dinh <mkdinh94@gmail.com>
2022-05-14 21:59:53 -0400

    [SL-33806] use latest sgselenium package

--- apify/madhumitha/storelocator/mastercuts_com/scrape.py
+++ apify/madhumitha/storelocator/mastercuts_com/scrape.py
@@ -6,7 +6,7 @@ from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgwriter import SgWriter
-from sgselenium.sgselenium import SgFirefox
+from sgselenium import SgFirefox
 
 start_url = "https://www.signaturestyle.com/salon-directory.html"
 domain = "mastercuts.com"
@@ -14,18 +14,18 @@ hdr = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36"
 }
 
-driver = SgFirefox(is_headless=True).driver()
+driver = None
 
 
 def get_driver(retry=0):
     global driver
-    if retry:
-        driver = SgFirefox().driver()
+    if not driver or retry:
+        driver = SgFirefox()
 
     return driver
 
 
-def fetch_location(poi_html, retry=0):
+def fetch_location(poi_html, driver, retry=0):
     try:
         url = poi_html.xpath("@href")[0]
         raw_address = poi_html.xpath("text()")[0]
@@ -86,21 +86,30 @@ def fetch_location(poi_html, retry=0):
         return item
     except:
         if retry < 3:
-            return fetch_location(poi_html, retry + 1)
+            return fetch_location(poi_html, driver, retry + 1)
+
+
+def get_state(url, session, retry=0):
+    try:
+        driver.get()
+        dom = etree.HTML(driver.page_source)
+        return dom.xpath("//td/a")
+    except:
+        if retry < 3:
+            return get_state(url, session, retry + 1)
 
 
 def fetch_data():
-    driver.get(start_url)
-    dom = etree.HTML(driver.page_source)
-    all_states = dom.xpath('//a[@class="btn btn-primary"]/@href')
-    for url in all_states:
-        driver.get(urljoin(start_url, url))
+    with SgFirefox() as driver:
+        driver.get(start_url)
         dom = etree.HTML(driver.page_source)
-        all_locations = dom.xpath("//td/a")
-        for poi_html in all_locations:
-            poi = fetch_location(poi_html)
-            if poi:
-                yield poi
+        all_states = dom.xpath('//a[@class="btn btn-primary"]/@href')
+        for url in all_states:
+            all_locations = urljoin(start_url, url)
+            for poi_html in all_locations:
+                poi = fetch_location(poi_html, driver)
+                if poi:
+                    yield poi
 
 
 def scrape():
