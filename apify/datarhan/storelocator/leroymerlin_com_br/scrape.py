5faf25da7b1c5ee2749457fae570c8334d586538
datarhan <slutchenko@gmail.com>
2022-05-20 12:46:27 +0200

    [SLC-35273] qa update

--- apify/datarhan/storelocator/leroymerlin_com_br/scrape.py
+++ apify/datarhan/storelocator/leroymerlin_com_br/scrape.py
@@ -44,7 +44,9 @@ def fetch_data():
             state = ""
             if len(raw_address.split(" - ")[-1]) == 2:
                 state = raw_address.split(" - ")[-1]
-            zip_code = loc_dom.xpath('//dd[@itemprop="postalCode"]/text()')[0]
+            zip_code = loc_dom.xpath('//dd[@itemprop="postalCode"]/text()')[0].replace(
+                ".", ""
+            )
             phone = loc_dom.xpath('//dd[@itemprop="telephone"]/text()')
             phone = phone[0].split("/")[0].split("(")[0] if phone else ""
             geo = (
