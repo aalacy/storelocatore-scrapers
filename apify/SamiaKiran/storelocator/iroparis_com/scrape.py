5636c7750dbb2a892b416840037405fd3862dc5c
Samia Kiran <samiakiran17@gmail.com>
2022-05-20 11:25:41 +0500

    Update scrape.py

--- apify/SamiaKiran/storelocator/iroparis_com/scrape.py
+++ apify/SamiaKiran/storelocator/iroparis_com/scrape.py
@@ -47,8 +47,20 @@ def fetch_data():
                     )
                 except:
                     hours_of_operation = MISSING
+
                 if "Here are the store hours" in hours_of_operation:
                     hours_of_operation = MISSING
+                if (
+                    "Monday Tuesday Wednesday Thursday Friday Saturday Sunday"
+                    in hours_of_operation
+                ):
+                    hours_of_operation = hours_of_operation.replace("Sunday", "Sunday#")
+                    hour_list = hours_of_operation.split("#")
+                    day_list = hour_list[0]
+                    time_list = hour_list[1]
+                    hours_of_operation = ""
+                    for day, time in zip(day_list, time_list):
+                        hours_of_operation = hours_of_operation + " " + day + " " + time
                 phone = loc["phone"]
                 street_address = loc["address1"]
                 log.info(street_address)
@@ -59,6 +71,9 @@ def fetch_data():
                     state = MISSING
                 zip_postal = loc["postalCode"]
                 country_code = loc["countryCode"]
+                if "Los Angeles" in state:
+                    if "CA" in city.split()[-1]:
+                        state = "CA"
                 yield SgRecord(
                     locator_domain=DOMAIN,
                     page_url="https://www.iroparis.com/us/stores",
