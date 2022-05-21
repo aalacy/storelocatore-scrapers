10613844b0061a8f2cabd9be96082e05aa429da2
Samia Kiran <samiakiran17@gmail.com>
2022-05-21 13:37:49 +0500

    [SLC-35429] Add Maintenance
    https://safegraph-crawl.atlassian.net/browse/SLC-35429

--- apify/SamiaKiran/storelocator/blacks_co_uk/scrape.py
+++ apify/SamiaKiran/storelocator/blacks_co_uk/scrape.py
@@ -1,16 +1,19 @@
-import json
+import ssl
 from sglogging import sglog
 from bs4 import BeautifulSoup
 from sgrequests import SgRequests
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord import SgRecord
+from sgselenium.sgselenium import SgChrome
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 session = SgRequests()
 website = "blacks_co_uk"
 log = sglog.SgLogSetup().get_logger(logger_name=website)
-session = SgRequests()
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
 }
@@ -30,47 +33,48 @@ def fetch_data():
             location_name = loc.text
             page_url = "https://www.blacks.co.uk" + loc["href"]
             log.info(page_url)
-            r = session.get(page_url, headers=headers)
-            try:
-                loc = r.text.split('</script><script type="application/ld+json">')[
-                    1
-                ].split("</script>")[0]
-            except:
-                continue
-            loc = json.loads(loc)
-            address = loc["address"]
-            street_address = address["streetAddress"]
-            city = address["addressLocality"]
-            state = MISSING
-            zip_postal = address["postalCode"]
-            country_code = address["addressCountry"]
-            coords = loc["hasmap"].split("@")[1].split(",")
-            latitude = coords[0]
-            longitude = coords[1]
-            phone = loc["telephone"]
-            hours_of_operation = str(loc["openingHours"])
-            hours_of_operation = (
-                hours_of_operation.replace("]", "")
-                .replace("[", "")
-                .replace(",", " ")
-                .replace("'", "")
-            )
-            yield SgRecord(
-                locator_domain=DOMAIN,
-                page_url=page_url,
-                location_name=location_name,
-                street_address=street_address.strip(),
-                city=city.strip(),
-                state=state.strip(),
-                zip_postal=zip_postal.strip(),
-                country_code=country_code,
-                store_number=MISSING,
-                phone=phone.strip(),
-                location_type=MISSING,
-                latitude=latitude,
-                longitude=longitude,
-                hours_of_operation=hours_of_operation.strip(),
-            )
+            with SgChrome() as driver:
+                driver.get(page_url)
+                response_text = driver.page_source
+                soup = BeautifulSoup(response_text, "html.parser")
+                try:
+                    street_address = (
+                        soup.find("span", {"data-yext-field": "address1"}).text
+                        + " "
+                        + soup.find("span", {"data-yext-field": "address2"}).text
+                    )
+                except:
+                    street_address = soup.find(
+                        "span", {"data-yext-field": "address1"}
+                    ).text
+                city = soup.find("span", {"data-yext-field": "city"}).text
+                state = soup.find("span", {"data-yext-field": "state"}).text
+                zip_postal = soup.find("span", {"data-yext-field": "zip"}).text
+                country_code = "UK"
+                phone = soup.find("a", {"data-yext-field": "phone"}).text
+                hours_of_operation = (
+                    soup.find("ul", {"id": "opening_times"})
+                    .get_text(separator="|", strip=True)
+                    .replace("|", " ")
+                )
+                latitude = str(soup).split('"latitude":')[1].split(",")[0]
+                longitude = str(soup).split('"longitude":')[1].split(",")[0]
+                yield SgRecord(
+                    locator_domain=DOMAIN,
+                    page_url=page_url,
+                    location_name=location_name,
+                    street_address=street_address.strip(),
+                    city=city.strip(),
+                    state=state.strip(),
+                    zip_postal=zip_postal.strip(),
+                    country_code=country_code,
+                    store_number=MISSING,
+                    phone=phone.strip(),
+                    location_type=MISSING,
+                    latitude=latitude,
+                    longitude=longitude,
+                    hours_of_operation=hours_of_operation.strip(),
+                )
 
 
 def scrape():
