4e8fd6c8e46abc898d77ac260bf062ba240f3f87
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2022-05-25 12:55:57 +0200

    SLC-22633
    https://safegraph-crawl.atlassian.net/browse/SLC-22633

--- apify/CalinClaudiu/storelocator/walmart_com__vision/scrape.py
+++ apify/CalinClaudiu/storelocator/walmart_com__vision/scrape.py
@@ -65,6 +65,8 @@ def grab_json(soup):
 def other_source(session, state):
     url = "https://www.walmart.com/store/directory"
     main = SgRequests.raise_on_err(session.get(url, headers=headers))
+    logger.info(f"{str(main.text.split('store-directory-container')[1]))}") # noqa
+    logger.info(f"{str(main.text)}") # noqa
     soup = b4(main.text, "lxml")
     allstates = (
         soup.find("div", {"class": "store-directory-container"})
