a839235c831ee45e5fab500e984e5fabf581c622
CalinClaudiu <54081267+CalinClaudiu@users.noreply.github.com>
2022-05-25 12:58:44 +0200

    Update scrape.py

--- apify/CalinClaudiu/storelocator/walmart_com__vision/scrape.py
+++ apify/CalinClaudiu/storelocator/walmart_com__vision/scrape.py
@@ -65,8 +65,9 @@ def grab_json(soup):
 def other_source(session, state):
     url = "https://www.walmart.com/store/directory"
     main = SgRequests.raise_on_err(session.get(url, headers=headers))
-    logger.info(f"{str(main.text.split('store-directory-container')[1]))}") # noqa
-    logger.info(f"{str(main.text)}") # noqa
+    toprint = str(main.text.split("store-directory-container")[1])
+    logger.info(f"{toprint}")  # noqa
+    logger.info(f"{str(main.text)}")  # noqa
     soup = b4(main.text, "lxml")
     allstates = (
         soup.find("div", {"class": "store-directory-container"})
