1c9b15cc180ca3139c4ec05f17882520785658d0
datarhan <slutchenko@gmail.com>
2022-05-28 12:09:46 +0200

    [SLC-35640] update

--- apify/datarhan/storelocator/tommybahama_com/scrape.py
+++ apify/datarhan/storelocator/tommybahama_com/scrape.py
@@ -70,7 +70,7 @@ def fetch_data():
             street_address=street_address,
             city=city,
             state=raw_data[1].split(", ")[1].split()[0],
-            zip_postal=raw_data[1].split(", ")[1].split()[1],
+            zip_postal=" ".join(raw_data[1].split(", ")[1].split()[1:]),
             country_code=country_code,
             store_number="",
             phone=phone,
