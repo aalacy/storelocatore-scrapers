455bebf98927ccaa41de43274027fb55e12c000d
datarhan <slutchenko@gmail.com>
2022-06-01 17:57:16 +0200

    [SLC-35764] maintenance

--- apify/datarhan/storelocator/adecco_co_nz/scrape.py
+++ apify/datarhan/storelocator/adecco_co_nz/scrape.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# --extra-index-url https://dl.cloudsmith.io/KVaWma76J5VNwrOm/crawl/crawl/python/simple/
 from time import sleep
 from lxml import etree
 from urllib.parse import urljoin
