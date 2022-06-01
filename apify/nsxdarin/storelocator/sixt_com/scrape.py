790137480ddd4eaea160e79698a95750b9a70295
nsxdarin <nsxdarin@gmail.com>
2022-06-01 12:42:48 -0500

    Update scrape.py

--- apify/nsxdarin/storelocator/sixt_com/scrape.py
+++ apify/nsxdarin/storelocator/sixt_com/scrape.py
@@ -15,89 +15,96 @@ headers = {
 
 
 def fetch_data():
-    ##    locs = []
-    ##    states = []
-    ##    url = "https://www.sixt.com/car-rental/#/"
-    ##    r = session.get(url, headers=headers)
-    ##    website = "sixt.com"
-    ##    typ = "<MISSING>"
-    ##    country = "US"
-    ##    logger.info("Pulling Stores")
-    ##    Found = False
-    ##    for line in r.iter_lines():
-    ##        if 'States</span>' in line:
-    ##            Found = True
-    ##        if Found and 'Europe</span>' in line:
-    ##            Found = False
-    ##        if Found and '<a href="/car-rental/usa/' in line:
-    ##            surl = 'https://www.sixt.com/car-rental/usa/' + line.split('<a href="/car-rental/usa/')[1].split('"')[0]
-    ##            states.append(surl)
-    ##    for state in states:
-    ##        logger.info(state)
-    ##        r2 = session.get(state, headers=headers)
-    ##        for line2 in r2.iter_lines():
-    ##            if 'locationLink:  "' in line2:
-    ##                locs.append("https://www.sixt.com" + line2.split('locationLink:  "')[1].split('"')[0])
-    ##    for loc in locs:
-    ##        logger.info(loc)
-    ##        name = ""
-    ##        add = ""
-    ##        city = ""
-    ##        state = ""
-    ##        zc = ""
-    ##        store = "<MISSING>"
-    ##        phone = "<MISSING>"
-    ##        lat = ""
-    ##        lng = ""
-    ##        hours = ""
-    ##        r2 = session.get(loc, headers=headers)
-    ##        for line2 in r2.iter_lines():
-    ##            if "<h1>" in line2:
-    ##                name = line2.split("<h1>")[1].split("<")[0].strip()
-    ##            if "orlando-lake-buena-vista" in loc:
-    ##                add = "12205 S Apopka Vineland Rd"
-    ##                city = "Orlando"
-    ##                state = "FL"
-    ##                zc = "32836-6804"
-    ##                lat = "28.386945724487"
-    ##                lng = "-81.504974365234"
-    ##                hours = "MO - FR: 07:00 - 18:00; SA - SU: 09:00 - 17:00"
-    ##            if '"streetAddress":"' in line2:
-    ##                add = line2.split('"streetAddress":"')[1].split('"')[0]
-    ##                zc = line2.split('"postalCode":"')[1].split('"')[0]
-    ##                city = line2.split('"addressLocality":"')[1].split('"')[0]
-    ##                add = line2.split('"streetAddress":"')[1].split('"')[0]
-    ##                lat = line2.split('"latitude":')[1].split(',')[0]
-    ##                lng = line2.split('"longitude":')[1].split('}')[0]
-    ##            if '<h2>Sixt Services' in line2:
-    ##                htext = line2.split('<h2>Sixt Services')[1].split('<')[0]
-    ##                if ',' in htext:
-    ##                    state = htext.split(',')[1].strip()
-    ##                else:
-    ##                    state = "<MISSING>"
-    ##            if '"openingHours":[' in line2:
-    ##                hours = line2.split('"openingHours":[')[1].split('"]')[0]
-    ##                hours = hours.replace('"24-hour return","','')
-    ##                if '","HOLIDAYS' in hours:
-    ##                    hours = hours.split('","HOLIDAYS')[0]
-    ##                hours = hours.replace('","','; ')
-    ##                hours = hours.replace('"','')
-    ##        yield SgRecord(
-    ##            locator_domain=website,
-    ##            page_url=loc,
-    ##            location_name=name,
-    ##            street_address=add,
-    ##            city=city,
-    ##            state=state,
-    ##            zip_postal=zc,
-    ##            country_code=country,
-    ##            phone=phone,
-    ##            location_type=typ,
-    ##            store_number=store,
-    ##            latitude=lat,
-    ##            longitude=lng,
-    ##            hours_of_operation=hours,
-    ##        )
+    locs = []
+    states = []
+    url = "https://www.sixt.com/car-rental/#/"
+    r = session.get(url, headers=headers)
+    website = "sixt.com"
+    typ = "<MISSING>"
+    country = "US"
+    logger.info("Pulling Stores")
+    Found = False
+    for line in r.iter_lines():
+        if "States</span>" in line:
+            Found = True
+        if Found and "Europe</span>" in line:
+            Found = False
+        if Found and '<a href="/car-rental/usa/' in line:
+            surl = (
+                "https://www.sixt.com/car-rental/usa/"
+                + line.split('<a href="/car-rental/usa/')[1].split('"')[0]
+            )
+            states.append(surl)
+    for state in states:
+        logger.info(state)
+        r2 = session.get(state, headers=headers)
+        for line2 in r2.iter_lines():
+            if 'locationLink:  "' in line2:
+                locs.append(
+                    "https://www.sixt.com"
+                    + line2.split('locationLink:  "')[1].split('"')[0]
+                )
+    for loc in locs:
+        logger.info(loc)
+        name = ""
+        add = ""
+        city = ""
+        state = ""
+        zc = ""
+        store = "<MISSING>"
+        phone = "<MISSING>"
+        lat = ""
+        lng = ""
+        hours = ""
+        r2 = session.get(loc, headers=headers)
+        for line2 in r2.iter_lines():
+            if "<h1>" in line2:
+                name = line2.split("<h1>")[1].split("<")[0].strip()
+            if "orlando-lake-buena-vista" in loc:
+                add = "12205 S Apopka Vineland Rd"
+                city = "Orlando"
+                state = "FL"
+                zc = "32836-6804"
+                lat = "28.386945724487"
+                lng = "-81.504974365234"
+                hours = "MO - FR: 07:00 - 18:00; SA - SU: 09:00 - 17:00"
+            if '"streetAddress":"' in line2:
+                add = line2.split('"streetAddress":"')[1].split('"')[0]
+                zc = line2.split('"postalCode":"')[1].split('"')[0]
+                city = line2.split('"addressLocality":"')[1].split('"')[0]
+                add = line2.split('"streetAddress":"')[1].split('"')[0]
+                lat = line2.split('"latitude":')[1].split(",")[0]
+                lng = line2.split('"longitude":')[1].split("}")[0]
+            if "<h2>Sixt Services" in line2:
+                htext = line2.split("<h2>Sixt Services")[1].split("<")[0]
+                if "," in htext:
+                    state = htext.split(",")[1].strip()
+                else:
+                    state = "<MISSING>"
+            if '"openingHours":[' in line2:
+                hours = line2.split('"openingHours":[')[1].split('"]')[0]
+                hours = hours.replace('"24-hour return","', "")
+                if '","HOLIDAYS' in hours:
+                    hours = hours.split('","HOLIDAYS')[0]
+                hours = hours.replace('","', "; ")
+                hours = hours.replace('"', "")
+        yield SgRecord(
+            locator_domain=website,
+            page_url=loc,
+            location_name=name,
+            street_address=add,
+            city=city,
+            state=state,
+            zip_postal=zc,
+            country_code=country,
+            phone=phone,
+            location_type=typ,
+            store_number=store,
+            latitude=lat,
+            longitude=lng,
+            hours_of_operation=hours,
+        )
+
     locs = []
     states = []
     url = "https://www.sixt.com/car-rental/united-kingdom/#/"
