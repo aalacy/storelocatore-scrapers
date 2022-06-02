628e9ad2a53c47479f80427d6081293c011c016c
Michael Dinh <mkdinh94@gmail.com>
2022-06-02 14:07:06 -0400

    [SLC-35510] remove bidirectional character from phone

--- apify/Shapoval1/storelocator/docbsrestaurant_com/scrape.py
+++ apify/Shapoval1/storelocator/docbsrestaurant_com/scrape.py
@@ -1,3 +1,4 @@
+import re
 import json
 from lxml import html
 from sgscrape.sgrecord import SgRecord
@@ -53,8 +54,9 @@ def fetch_data(sgw: SgWriter):
             "".join(tree.xpath('//a[contains(@href, "tel")]/text()')).strip()
             or "<MISSING>"
         )
-        if phone.find(">") != -1:
-            phone = phone.split(">")[1].split("<")[0].strip()
+
+        phone = re.search(r"(\d{3}-\d{3}-\d{4})", phone).group(1)
+
         hours_of_operation = (
             " ".join(
                 tree.xpath(
