907aaca911fd8140ecd67dc6c37cb86a3fea662d
aalacy <aaronlacywhite387@gmail.com>
2022-06-07 09:43:17 -0700

    fix

--- apify/idev-a/curatedsources/tiendeo_com/scrape.py
+++ apify/idev-a/curatedsources/tiendeo_com/scrape.py
@@ -328,7 +328,6 @@ def _d(loc, domain, country):
 
 def fetch_data():
     with SgChrome(
-        executable_path=ChromeDriverManager().install(),
         user_agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
         is_headless=True,
     ) as driver:
