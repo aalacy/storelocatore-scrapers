4567e1e6046c54eb348902072344ebc9b711c23e
datarhan <slutchenko@gmail.com>
2022-06-10 17:40:56 +0200

    [SLC-36291] maintenance

--- apify/datarhan/storelocator/lexus_co_uk/scrape.py
+++ apify/datarhan/storelocator/lexus_co_uk/scrape.py
@@ -8,26 +8,22 @@ from sgscrape.sgwriter import SgWriter
 def fetch_data():
     session = SgRequests()
 
-    start_urls = {
-        "Austria": "https://www.lexus.at/api/dealers/all",
-        "Hungary": "https://www.lexus.hu/api/dealers/all",
-        "Latvia": "https://lv.lexus.lv/api/dealers/all",
-        "Lithuania": "https://lt.lexus.lt/api/dealers/all",
-        "Poland": "https://www.lexus-polska.pl/api/dealers/all",
-    }
+    start_urls = [
+        "https://www.lexus.hu/api/dealers/all",
+        "https://lv.lexus.lv/api/dealers/all",
+        "https://www.lexus-polska.pl/api/dealers/all"
+    ]
 
     loc_url = {
-        "at": "https://www.lexus.at/forms/find-a-retailer",
         "hu": "https://www.lexus.hu/",
         "lv": "https://lv.lexus.lv/",
-        "lt": "https://lt.lexus.lt",
         "pl": "https://www.lexus-polska.pl/",
     }
 
     hdr = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
-    for country, start_url in start_urls.items():
+    for start_url in start_urls:
         data = session.get(start_url, headers=hdr).json()
         for poi in data["dealers"]:
             location_type = ", ".join(poi["services"])
