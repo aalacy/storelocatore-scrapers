733d7762412d1771556df5e6e120d0533964bf87
datarhan <slutchenko@gmail.com>
2022-06-10 18:00:58 +0200

    [SLC-36291] update

--- apify/datarhan/storelocator/lexus_hr/scrape.py
+++ apify/datarhan/storelocator/lexus_hr/scrape.py
@@ -10,19 +10,21 @@ from sgscrape.sgwriter import SgWriter
 def fetch_data():
     session = SgRequests()
 
-    start_urls = {
-        "Croatia": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/hr/hr/drive/16.059354/45.732163?count=500&extraCountries=UK&limitSearchDistance=0&isCurrentLocation=false&services=",
-        "Bulgaria": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/bg/bg/drive/23.350282/42.652363?count=500&extraCountries=UK&limitSearchDistance=0&isCurrentLocation=false&services=",
-        "Switzerland": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/ch/de/drive/8.53989/47.378084?count=500&extraCountries=&isCurrentLocation=false",
-        "Spain": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/es/es/drive/-3.703583/40.416705?count=500&extraCountries=&isCurrentLocation=false",
-        "France": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/fr/fr/drive/0.428551/44.064432?count=500&extraCountries=AD|MC&isCurrentLocation=false",
-        "Estonia": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/ee/et/drive/24.745369/59.437216?count=500&extraCountries=&isCurrentLocation=false",
-        "Germany": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/de/de/drive/13.38333/52.51667?count=500&extraCountries=&isCurrentLocation=false",
-        "Sweden": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/se/sv/drive/18.071093/59.325117?count=500&extraCountries=&isCurrentLocation=false",
-        "Netherlands": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/nl/nl/drive/4.893604/52.37276?count=500&extraCountries=&isCurrentLocation=false",
-        "Portugal": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/pt/pt/drive/-7.4333/39.2911?count=500&extraCountries=&isCurrentLocation=false",
-        "Italy": "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/it/it/drive/15.436903/38.051059?count=500&extraCountries=&isCurrentLocation=false",
-    }
+    start_urls = [
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/hr/hr/drive/16.059354/45.732163?count=500&extraCountries=UK&limitSearchDistance=0&isCurrentLocation=false&services=",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/bg/bg/drive/23.350282/42.652363?count=500&extraCountries=UK&limitSearchDistance=0&isCurrentLocation=false&services=",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/ch/de/drive/8.53989/47.378084?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/es/es/drive/-3.703583/40.416705?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/fr/fr/drive/0.428551/44.064432?count=500&extraCountries=AD|MC&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/ee/et/drive/24.745369/59.437216?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/de/de/drive/13.38333/52.51667?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/se/sv/drive/18.071093/59.325117?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/nl/nl/drive/4.893604/52.37276?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/pt/pt/drive/-7.4333/39.2911?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/it/it/drive/15.436903/38.051059?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/lt/lt/drive/25.282911/54.687046?count=500&extraCountries=&isCurrentLocation=false",
+        "https://kong-proxy-aws.toyota-europe.com/dxp/dealers/api/lexus/at/de/drive/15.75/48.33333?count=500&extraCountries=&isCurrentLocation=false",
+    ]
     l_url = {
         "hr": "https://www.lexus.hr/contact/dealers",
         "bg": "https://www.lexus.bg/contact/dealers",
@@ -35,13 +37,15 @@ def fetch_data():
         "nl": "https://www.lexus.nl/#/publish/my_lexus_my_dealers",
         "it": "https://www.lexus.it/#/publish/my_lexus_my_dealers",
         "pt": "https://www.lexus.pt/#publish/my_lexus_my_dealers",
+        "at": "https://www.lexus.at/#publish/my_lexus_my_dealers",
+        "lt": "https://www.lexus.lt/#publish/my_lexus_my_dealers",
     }
 
     domain = "lexus.hr"
     hdr = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
-    for country_code, start_url in start_urls.items():
+    for start_url in start_urls:
         data = session.get(start_url, headers=hdr).json()
         for poi in data["dealers"]:
             page_url = urljoin(l_url[poi["country"]], poi["t1Url"])
