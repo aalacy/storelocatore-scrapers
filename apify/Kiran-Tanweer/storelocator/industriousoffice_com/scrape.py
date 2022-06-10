d4b73878e553b2fe5a1b2e561eb08e6b533b0f7e
Kiran Tanweer <44836465+Kiran-Tanweer@users.noreply.github.com>
2022-06-11 00:07:28 +0500

    Add files via upload

--- apify/Kiran-Tanweer/storelocator/industriousoffice_com/scrape.py
+++ apify/Kiran-Tanweer/storelocator/industriousoffice_com/scrape.py
@@ -13,7 +13,9 @@ from sgscrape import sgpostal as parser
 session = SgRequests()
 website = "industriousoffice_com"
 log = sglog.SgLogSetup().get_logger(logger_name=website)
-headers = {}
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+}
 
 DOMAIN = "https://www.industriousoffice.com/"
 MISSING = SgRecord.MISSING
