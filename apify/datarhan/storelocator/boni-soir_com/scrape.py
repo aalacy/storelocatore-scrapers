d070eaa271e643e1f389cc43e17b4bb8320f0226
datarhan <slutchenko@gmail.com>
2022-06-12 14:18:16 +0200

    [SLC-4612] update

--- apify/datarhan/storelocator/boni-soir_com/scrape.py
+++ apify/datarhan/storelocator/boni-soir_com/scrape.py
@@ -17,13 +17,15 @@ def fetch_data():
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
 
-    all_locations = all_locations = session.get(start_url, headers=hdr).json()
+    all_locations = session.get(start_url, headers=hdr).json()
     for poi in all_locations:
         store_number = poi["ID"]
         poi_data_url = f"https://www.boni-soir.com/wp-json/wp/v2/store/{store_number}"
         poi_data = session.get(poi_data_url, headers=hdr)
-        if poi_data.status_code != 200:
-            continue
+        while poi_data.status_code != 200:
+            session = SgRequests()
+            poi_data = session.get(poi_data_url, headers=hdr)
+            print("!!!!", poi)
         poi_data = poi_data.json()
         street_address = poi_data["store_details"]["location_address_address_1"]
         street_address_2 = poi_data["store_details"]["location_address_address_2"]
@@ -36,10 +38,12 @@ def fetch_data():
             hoo.append(f"{day}: {hours}")
         hoo = " ".join(hoo)
 
+        phone = ""
         loc_response = session.get(poi_data["link"], headers=hdr)
-        loc_dom = etree.HTML(loc_response.text)
-        phone = loc_dom.xpath('//span[@class="phone"]/a/text()')
-        phone = phone[0] if phone else ""
+        if loc_response.status_code == 200:
+            loc_dom = etree.HTML(loc_response.text)
+            phone = loc_dom.xpath('//span[@class="phone"]/a/text()')
+            phone = phone[0] if phone else ""
 
         item = SgRecord(
             locator_domain=domain,
@@ -64,9 +68,7 @@ def fetch_data():
 def scrape():
     with SgWriter(
         SgRecordDeduper(
-            SgRecordID(
-                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
-            )
+            SgRecordID({SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STORE_NUMBER})
         )
     ) as writer:
         for item in fetch_data():
