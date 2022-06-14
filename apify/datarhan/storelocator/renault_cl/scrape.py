fc17c75bd200b3260108f2d52800a75fc4d2cd58
datarhan <slutchenko@gmail.com>
2022-06-14 13:43:06 +0200

    [SLC-36248] update

--- apify/datarhan/storelocator/renault_cl/scrape.py
+++ apify/datarhan/storelocator/renault_cl/scrape.py
@@ -14,7 +14,7 @@ def fetch_data():
 
     start_url = "https://www.renault.cl/concesionarios/"
     domain = "renault.cl"
-    
+
     hdr = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
