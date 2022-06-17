6c95bb1ef32bdef418833ea2530cb1bfe6505af3
dima <shshipovald@mail.ru>
2022-06-17 14:57:43 +0300

    fix bajafishtacos.com

--- apify/Shapoval1/storelocator/bajafishtacos_com/scrape.py
+++ apify/Shapoval1/storelocator/bajafishtacos_com/scrape.py
@@ -1,135 +1,87 @@
-import csv
 from lxml import html
+from sgscrape.sgrecord import SgRecord
 from sgrequests import SgRequests
-from concurrent import futures
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
 
-def write_output(data):
-    with open("data.csv", mode="w", encoding="utf8", newline="") as output_file:
-        writer = csv.writer(
-            output_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
-        )
+def fetch_data(sgw: SgWriter):
 
-        writer.writerow(
-            [
-                "locator_domain",
-                "page_url",
-                "location_name",
-                "street_address",
-                "city",
-                "state",
-                "zip",
-                "country_code",
-                "store_number",
-                "phone",
-                "location_type",
-                "latitude",
-                "longitude",
-                "hours_of_operation",
-            ]
-        )
-
-        for row in data:
-            writer.writerow(row)
-
-
-def get_urls():
+    locator_domain = "https://www.bajafishtacos.com/"
+    page_url = "https://www.bajafishtacos.com/locations"
     session = SgRequests()
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0",
-    }
-    r = session.get("http://bajafishtacos.com/locations/", headers=headers)
-    tree = html.fromstring(r.text)
-    return tree.xpath('//a[text()="View location"]/@href')
-
-
-def get_data(url):
-    locator_domain = "http://bajafishtacos.com"
-    page_url = url
-    session = SgRequests()
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
     }
     r = session.get(page_url, headers=headers)
     tree = html.fromstring(r.text)
+    div = tree.xpath('//div[@class="contact-card"]')
+    for d in div:
 
-    location_name = (
-        "".join(tree.xpath('//h2[@class="headline__secondary"]//text()'))
-        .replace("\n", "")
-        .strip()
-    )
-    street_address = (
-        "".join(tree.xpath('//h3[text()="Address"]/following-sibling::p/text()[1]'))
-        .replace(",", "")
-        .strip()
-    )
-    ad = (
-        "".join(tree.xpath('//h3[text()="Address"]/following-sibling::p/text()[2]'))
-        .replace("\n", "")
-        .replace(",", "")
-        .strip()
-    )
-    city = " ".join(ad.split()[:-2]).strip()
-    state = ad.split()[-2].strip()
-    postal = ad.split()[-1].strip()
-    country_code = "US"
-    store_number = "<MISSING>"
-    phone = "".join(tree.xpath('//h1[@class="headline__primary"]/text()'))
-    location_type = "Baja Fish Tacos"
-    hours_of_operation = (
-        " ".join(tree.xpath('//h3[text()="Hours"]/following-sibling::p/text()'))
-        .replace("\n", "")
-        .strip()
-    )
-    text = "".join(tree.xpath("//a[contains(@href, 'maps')]/@href"))
-    try:
-        if text.find("ll=") != -1:
-            latitude = text.split("ll=")[1].split(",")[0]
-            longitude = text.split("ll=")[1].split(",")[1].split("&")[0]
-        else:
-            latitude = text.split("@")[1].split(",")[0]
-            longitude = text.split("@")[1].split(",")[1]
-    except IndexError:
-        latitude, longitude = "<MISSING>", "<MISSING>"
+        location_name = "".join(d.xpath(".//preceding::h1[1]//text()"))
+        location_type = "<MISSING>"
+        street_address = (
+            "".join(
+                d.xpath('.//h3[text()="ADDRESS"]/following-sibling::p[1]/text()[1]')
+            )
+            .replace("\n", "")
+            .strip()
+        )
+        ad = (
+            "".join(
+                d.xpath('.//h3[text()="ADDRESS"]/following-sibling::p[1]/text()[2]')
+            )
+            .replace("\n", "")
+            .strip()
+        )
+        state = ad.split(",")[1].split()[0].strip()
+        postal = ad.split(",")[1].split()[1].strip()
+        country_code = "US"
+        city = ad.split(",")[0].strip()
+        store_number = "<MISSING>"
+        text = "".join(d.xpath('.//preceding::a[contains(@href, "maps")][1]/@href'))
+        try:
+            if text.find("ll=") != -1:
+                latitude = text.split("ll=")[1].split(",")[0]
+                longitude = text.split("ll=")[1].split(",")[1].split("&")[0]
+            else:
+                latitude = text.split("@")[1].split(",")[0]
+                longitude = text.split("@")[1].split(",")[1]
+        except IndexError:
+            latitude, longitude = "<MISSING>", "<MISSING>"
+        phone = "".join(d.xpath('.//a[contains(@href, "tel")]//text()'))
+        hours_of_operation = (
+            " ".join(d.xpath('.//h3[text()="HOURS"]/following-sibling::p//text()'))
+            .replace("\n", "")
+            .strip()
+        )
+        hours_of_operation = " ".join(hours_of_operation.split())
 
-    row = [
-        locator_domain,
-        page_url,
-        location_name,
-        street_address,
-        city,
-        state,
-        postal,
-        country_code,
-        store_number,
-        phone,
-        location_type,
-        latitude,
-        longitude,
-        hours_of_operation,
-    ]
+        row = SgRecord(
+            locator_domain=locator_domain,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=postal,
+            country_code=country_code,
+            store_number=SgRecord.MISSING,
+            phone=phone,
+            location_type=SgRecord.MISSING,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+            raw_address=f"{street_address} {ad}",
+        )
 
-    return row
-
-
-def fetch_data():
-    out = []
-    urls = get_urls()
-    with futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_url = {executor.submit(get_data, url): url for url in urls}
-        for future in futures.as_completed(future_to_url):
-            row = future.result()
-            if row:
-                out.append(row)
-
-    return out
-
-
-def scrape():
-    data = fetch_data()
-    write_output(data)
+        sgw.write_row(row)
 
 
 if __name__ == "__main__":
-    scrape()
+    session = SgRequests()
+    with SgWriter(
+        SgRecordDeduper(SgRecordID({SgRecord.Headers.LOCATION_NAME}))
+    ) as writer:
+        fetch_data(writer)
