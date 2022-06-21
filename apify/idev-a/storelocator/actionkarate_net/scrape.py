eda7b5dad3aaba0b7592978fd06bd05998c5c466
aalacy <aaronlacywhite387@gmail.com>
2022-06-20 23:40:13 -0700

    f

--- apify/idev-a/storelocator/actionkarate_net/scrape.py
+++ apify/idev-a/storelocator/actionkarate_net/scrape.py
@@ -1,6 +1,5 @@
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
-from sgrequests import SgRequests
 from sgselenium import SgChrome
 from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
@@ -10,27 +9,26 @@ base_url = "https://actionkarate.net/locations/"
 
 
 def fetch_data():
-    with SgRequests() as session:
-        with SgChrome() as driver:
-            driver.get(base_url)
-            driver.wait_for_request("state.js")
-            script = "return __NUXT__.state.locations"
-            locations = driver.execute_script(script)
-            for _ in locations:
-                yield SgRecord(
-                    page_url=base_url,
-                    store_number=_["id"],
-                    location_name=_["city"],
-                    street_address=_["street"],
-                    city=_["city"],
-                    state=_["state"]["name"],
-                    zip_postal=_["zip_code"],
-                    latitude=_["latitude"],
-                    longitude=_["longitude"],
-                    country_code="US",
-                    phone=_["phone"],
-                    locator_domain=locator_domain,
-                )
+    with SgChrome() as driver:
+        driver.get(base_url)
+        driver.wait_for_request("state.js")
+        script = "return __NUXT__.state.locations"
+        locations = driver.execute_script(script)
+        for _ in locations:
+            yield SgRecord(
+                page_url=base_url,
+                store_number=_["id"],
+                location_name=_["city"],
+                street_address=_["street"],
+                city=_["city"],
+                state=_["state"]["name"],
+                zip_postal=_["zip_code"],
+                latitude=_["latitude"],
+                longitude=_["longitude"],
+                country_code="US",
+                phone=_["phone"],
+                locator_domain=locator_domain,
+            )
 
 
 if __name__ == "__main__":
