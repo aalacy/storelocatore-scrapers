608ecf0677fa473e9cce4d4acc89718934c9470d
Samia Kiran <samiakiran17@gmail.com>
2022-06-22 08:52:54 -0700

    [SLC-36619] Add maintenance
    https://safegraph-crawl.atlassian.net/browse/SLC-36619

--- apify/ggrahambaker/storelocator/memorialcare_org/scrape.py
+++ apify/ggrahambaker/storelocator/memorialcare_org/scrape.py
@@ -1,82 +1,96 @@
-import csv
-from sgrequests import SgRequests
-from bs4 import BeautifulSoup
 import json
+from sglogging import sglog
+from bs4 import BeautifulSoup
+from sgrequests import SgRequests
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 
-def write_output(data):
-    with open('data.csv', mode='w') as output_file:
-        writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+session = SgRequests()
+website = "memorialcare.org"
+log = sglog.SgLogSetup().get_logger(logger_name=website)
 
-        # Header
-        writer.writerow(["locator_domain", "location_name", "street_address", "city", "state", "zip", "country_code", "store_number", "phone", "location_type", "latitude", "longitude", "hours_of_operation", "page_url"])
-        # Body
-        for row in data:
-            writer.writerow(row)
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+}
+
+DOMAIN = "https://www.memorialcare.org/"
+MISSING = SgRecord.MISSING
+
 
 def fetch_data():
-    session = SgRequests()
-    HEADERS = { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36' }
-
-    locator_domain = 'https://www.memorialcare.org/' 
-    ext = 'api/location-search-lookup?&proximity[value]=25'
-    r = session.get(locator_domain + ext, headers = HEADERS)
-
-    locs = json.loads(r.content)
-    all_store_data = []
+    api_url = (
+        "https://www.memorialcare.org/api/location-search-lookup?&proximity[value]=75"
+    )
+    locs = session.get(api_url, headers=headers).json()
     for loc in locs:
-        store_number = loc['id']
-        page_url = locator_domain[:-1] + loc['url']
-
-        location_type = loc['location_type'].replace('&amp;', '&').strip()
-        if location_type == '':
-            location_type = '<MISSING>'
-        lat = loc['lat']
-        longit = loc['lon']
-        
-        location_name = loc['title']
-        
-        street_address = loc['address_line_1']
-        
-        street_address = street_address.strip()
-        city = loc['city']
-        state = loc['state']
-        zip_code = loc['zip']
-        
-        country_code = 'US'
-        
-        hours = '<MISSING>'
-        
-        r = session.get(page_url, headers = HEADERS)
-        soup = BeautifulSoup(r.content, 'html.parser')
-        
-        ps = soup.find_all('p', {'class': 'location-marquee__info'}) #location-marquee__info--has-image
-        for p in ps:
-            if 'Phone' in p.text:
-                phone_number = p.text.replace('Phone:', '').strip()
-
+        store_number = loc["id"]
+        page_url = DOMAIN[:-1] + loc["url"]
+        log.info(page_url)
+        location_type = loc["location_type"].replace("&amp;", "&")
+        latitude = loc["lat"]
+        longitude = loc["lon"]
+        location_name = loc["title"]
+        street_address = loc["address_line_1"]
+        city = loc["city"]
+        state = loc["state"]
+        zip_postal = loc["zip"]
+        country_code = "US"
+        r = session.get(page_url, headers=headers)
+        soup = BeautifulSoup(r.content, "html.parser")
         try:
-            hours = ''
-            hours_p = soup.find('div', {'class': 'open-hours__description'}).find_all('p')
-            for h in hours_p:
-                hours += h.text + ' '
-                
-            hours = hours.split('Lab Hours')[0]
-            hours = hours.split('Holiday Hours')[0]
-            
-            hours = ' '.join(hours.split())
-            
+            phone = (
+                soup.find("div", {"class": "location-marquee__contact-info"})
+                .get_text(separator="|", strip=True)
+                .replace("|", "")
+                .replace("Phone:", "")
+            )
         except:
-            hours = '<MISSING>'
+            phone = MISSING
+        hour_list = soup.find("div", {"class": "open-hours__description"})
+        if hour_list:
+            hour_list = hour_list.findAll("p")
+            hours_of_operation = ""
+            for hour in hour_list:
+                hour = hour.text
+                if "(" in hour:
+                    hour = hour.split("(")[0]
+                hours_of_operation = hours_of_operation + " " + hour
+        else:
+            hours_of_operation = MISSING
+        yield SgRecord(
+            locator_domain=DOMAIN,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=zip_postal,
+            country_code=country_code,
+            store_number=MISSING,
+            phone=phone,
+            location_type=MISSING,
+            latitude=MISSING,
+            longitude=MISSING,
+            hours_of_operation=hours_of_operation,
+        )
 
-        store_data = [locator_domain, location_name, street_address, city, state, zip_code, country_code, 
-                    store_number, phone_number, location_type, lat, longit, hours, page_url]
-
-        all_store_data.append(store_data)
-        
-    return all_store_data
 
 def scrape():
-    data = fetch_data()
-    write_output(data)
+    log.info("Started")
+    count = 0
+    with SgWriter(
+        deduper=SgRecordDeduper(record_id=RecommendedRecordIds.PageUrlId)
+    ) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
 
-scrape()
+    log.info(f"No of records being processed: {count}")
+    log.info("Finished")
+
+
+if __name__ == "__main__":
+    scrape()
