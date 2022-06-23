191b235b17e4d9590e8b8f1c52d00aea7a7d0a3c
Tenzing Shaw <tenzingshaw@ip-192-168-0-16.us-west-2.compute.internal>
2022-06-23 12:05:55 -0700

    try with shared http session

--- apify/nsxdarin/storelocator/walmart_ca___question_fltr_equals_WALMART___VISION___CENTRE/scrape.py
+++ apify/nsxdarin/storelocator/walmart_ca___question_fltr_equals_WALMART___VISION___CENTRE/scrape.py
@@ -17,90 +17,90 @@ headers = {
 search = static_zipcode_list(1, SearchableCountries.CANADA)
 
 
-@tenacity.retry(wait=tenacity.wait_fixed(5))
-def fetch_stores(code):
+@retry(stop=stop_after_attempt(7), wait=tenacity.wait_fixed(7))
+def fetch_stores(http, code):
+    logger.info(f"Pulling Zip Code: {code}...")
+    formatted_code = code.replace(" ", "")
+    url = f"https://www.walmart.ca/en/stores-near-me/api/searchStores?singleLineAddr={formatted_code}&serviceTypes=WALMART_VISION_CENTRE"
+    return http.get(url, headers=headers).json()["payload"]["stores"]
+
+
+def fetch_data():
     with SgRequests(
         proxy_escalation_order=[
             "http://groups-RESIDENTIAL,country-{}:{}@proxy.apify.com:8000/"
         ]
     ) as http:
-        logger.info(f"Pulling Zip Code: {code}...")
-        formatted_code = code.replace(" ", "")
-        url = f"https://www.walmart.ca/en/stores-near-me/api/searchStores?singleLineAddr={formatted_code}&serviceTypes=WALMART_VISION_CENTRE"
-        return http.get(url, headers=headers).json()["payload"]["stores"]
-
-
-def fetch_data():
-    website = "walmart.ca/?fltr_equals_WALMART_VISION_CENTRE"
-    typ = "Walmart"
-    country = "CA"
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(fetch_stores, code) for code in search]
-    for future in as_completed(futures):
-        stores = future.result()
-        for item in stores:
-            Fuel = False
-            try:
-                name = item["displayName"]
-            except:
-                name = item["address"]["city"]
-            store = item["id"]
-            add = item["address"]["address1"]
-            try:
-                add = add + " " + item["address"]["address2"]
-            except:
-                pass
-            city = item["address"]["city"]
-            state = item["address"]["state"]
-            zc = item["address"]["postalCode"]
-            phone = item["phone"]
-            lat = item["geoPoint"]["latitude"]
-            lng = item["geoPoint"]["longitude"]
-            hours = ""
-            for svc in item["servicesMap"]:
-                svcname = svc["service"]["name"]
-                if "WALMART_VISION_CENTRE" in svcname:
-                    Fuel = True
-                    for day in svc["regularHours"]:
-                        try:
-                            hrs = day["day"] + ": " + day["start"] + "-" + day["end"]
-                        except:
-                            hrs = day["day"] + ": Closed"
-                        if hours == "":
-                            hours = hrs
-                        else:
-                            hours = hours + "; " + hrs
-            loc = (
-                "https://www.walmart.ca/en/stores-near-me/"
-                + name.replace(" ", "-").lower()
-                + "-"
-                + str(store)
-            )
-            if "0" not in hours:
-                hours = "<MISSING>"
-            if "Supercentre" in name:
-                typ = "Supercenter"
-            if "Neighborhood Market" in name:
-                typ = "Neighborhood Market"
-            if hours == "":
-                hours = "<MISSING>"
-            if add != "" and Fuel:
-                yield SgRecord(
-                    locator_domain=website,
-                    page_url=loc,
-                    location_name=name,
-                    street_address=add,
-                    city=city,
-                    state=state,
-                    zip_postal=zc,
-                    country_code=country,
-                    phone=phone,
-                    location_type=typ,
-                    store_number=store,
-                    latitude=lat,
-                    longitude=lng,
-                    hours_of_operation=hours,
+        website = "walmart.ca/?fltr_equals_WALMART_VISION_CENTRE"
+        typ = "Walmart"
+        country = "CA"
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(fetch_stores, http, code) for code in search]
+        for future in as_completed(futures):
+            stores = future.result()
+            for item in stores:
+                Fuel = False
+                try:
+                    name = item["displayName"]
+                except:
+                    name = item["address"]["city"]
+                store = item["id"]
+                add = item["address"]["address1"]
+                try:
+                    add = add + " " + item["address"]["address2"]
+                except:
+                    pass
+                city = item["address"]["city"]
+                state = item["address"]["state"]
+                zc = item["address"]["postalCode"]
+                phone = item["phone"]
+                lat = item["geoPoint"]["latitude"]
+                lng = item["geoPoint"]["longitude"]
+                hours = ""
+                for svc in item["servicesMap"]:
+                    svcname = svc["service"]["name"]
+                    if "WALMART_VISION_CENTRE" in svcname:
+                        Fuel = True
+                        for day in svc["regularHours"]:
+                            try:
+                                hrs = day["day"] + ": " + day["start"] + "-" + day["end"]
+                            except:
+                                hrs = day["day"] + ": Closed"
+                            if hours == "":
+                                hours = hrs
+                            else:
+                                hours = hours + "; " + hrs
+                loc = (
+                    "https://www.walmart.ca/en/stores-near-me/"
+                    + name.replace(" ", "-").lower()
+                    + "-"
+                    + str(store)
                 )
+                if "0" not in hours:
+                    hours = "<MISSING>"
+                if "Supercentre" in name:
+                    typ = "Supercenter"
+                if "Neighborhood Market" in name:
+                    typ = "Neighborhood Market"
+                if hours == "":
+                    hours = "<MISSING>"
+                if add != "" and Fuel:
+                    yield SgRecord(
+                        locator_domain=website,
+                        page_url=loc,
+                        location_name=name,
+                        street_address=add,
+                        city=city,
+                        state=state,
+                        zip_postal=zc,
+                        country_code=country,
+                        phone=phone,
+                        location_type=typ,
+                        store_number=store,
+                        latitude=lat,
+                        longitude=lng,
+                        hours_of_operation=hours,
+                    )
 
 
 def scrape():
