bd140baa4b683fc6b247fefca7abe09208cb82d6
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2022-06-24 23:13:31 +0500

    Update scrape.py

--- apify/shumaila/storelocator/peoplesjewellers_com/scrape.py
+++ apify/shumaila/storelocator/peoplesjewellers_com/scrape.py
@@ -1,5 +1,4 @@
-from bs4 import BeautifulSoup
-import re
+import json
 from sgrequests import SgRequests
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord import SgRecord
@@ -13,82 +12,70 @@ headers = {
 
 
 def fetch_data():
-    pattern = re.compile(r"\s\s+")
-    url = "https://www.peoplesjewellers.com/store-finder/view-all-states"
+
+    url = "https://www.peoplesjewellers.com/sitemap-20.xml"
     r = session.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "html.parser")
-    state_list = soup.select("a[href*=view-stores]")
-
-    for slink in state_list:
-        slink = "https://www.peoplesjewellers.com/store-finder/" + slink["href"]
-
-        r = session.get(slink, headers=headers)
-        soup = BeautifulSoup(r.text, "html.parser")
-        branchlist = soup.find("div", {"class": "view-all-stores"}).findAll(
-            "div", {"class": "col-lg-3"}
-        )
-
-        for branch in branchlist:
-            try:
-                link = branch.find("a")["href"]
-                title = branch.find("a").text
-            except:
-                continue
-            if branch.find("a")["href"].find("/null") == -1:
-
-                link = "https://www.peoplesjewellers.com" + branch.find("a")["href"]
-
-                r = session.get(link, headers=headers)
-                soup = BeautifulSoup(r.text, "html.parser")
-                title = soup.find("h1", {"itemprop": "name"}).text
-                street = soup.find("span", {"itemprop": "streetAddress"}).text
-                city = soup.find("span", {"itemprop": "addressLocality"}).text
-                state = soup.find("span", {"itemprop": "addressRegion"}).text
-                pcode = soup.find("span", {"itemprop": "postalCode"}).text
-                ccode = soup.find("span", {"itemprop": "addressCountry"}).text
-                phone = soup.find("span", {"itemprop": "telephone"}).text
-                coord = soup.find("a", {"class": "link-directions"})["href"]
-                lat, longt = coord.split("Location/")[1].split(",", 1)
-                store = link.split("-peo")[1]
-                hours = (
-                    str(soup)
-                    .split('"openings":', 1)[1]
-                    .split("{", 1)[1]
-                    .split("}", 1)[0]
-                    .replace('"', "")
-                    .replace(":", " ")
-                )
-                hours = re.sub(pattern, " ", hours).strip()
-            else:
-
-                street = branch.find("span", {"itemprop": "streetAddress"}).text
-                city = branch.find("span", {"itemprop": "addressLocality"}).text
-                state = branch.find("span", {"itemprop": "addressRegion"}).text
-                pcode = branch.find("span", {"itemprop": "postalCode"}).text
-                phone = branch.find("span", {"itemprop": "telephone"}).text
-
-                store = "<MISSING>"
-                ccode = "CA"
-                lat = "<MISSING>"
-                longt = "<MISSING>"
-                hours = "<MISSING>"
-                link = "<MISSING>"
-            yield SgRecord(
-                locator_domain="https://www.peoplesjewellers.com/",
-                page_url=link,
-                location_name=title,
-                street_address=street.strip(),
-                city=city.strip(),
-                state=state.strip(),
-                zip_postal=pcode.strip(),
-                country_code=ccode,
-                store_number=store,
-                phone=phone.strip(),
-                location_type="<MISSING>",
-                latitude=lat,
-                longitude=longt,
-                hours_of_operation=hours,
+    soup = BeautifulSoup(r.text, "xml")
+    linklist = soup.findAll("loc")
+    for link in linklist:
+        link = link.text
+        r = session.get(link, headers=headers)
+        try:
+            loc = (
+                r.text.split('<script type="application/ld+json">', 1)[1]
+                .split("</script", 1)[0]
+                .replace("\n", "")
+                .strip()
             )
+        except:
+            continue
+        loc = json.loads(loc)
+        title = loc["name"]
+        link = loc["@id"]
+        phone = loc["telephone"]
+        street = loc["address"]["streetAddress"]
+        city = loc["address"]["addressLocality"]
+        state = loc["address"]["addressRegion"]
+        pcode = loc["address"]["postalCode"]
+        ccode = loc["address"]["addressCountry"]
+        lat = loc["geo"]["latitude"]
+        longt = loc["geo"]["longitude"]
+        hourslist = loc["openingHoursSpecification"]
+        hours = ""
+        for hr in hourslist:
+            day = hr["dayOfWeek"]
+            openstr = hr["opens"] + " am - "
+            closestr = hr["closes"]
+            close = int(closestr.split(":", 1)[0])
+            if close > 12:
+                close = close - 12
+            hours = (
+                hours
+                + day
+                + " "
+                + openstr
+                + str(close)
+                + ":"
+                + closestr.split(":", 1)[1]
+                + " pm "
+            )
+        store = link.split("/")[-1]
+        yield SgRecord(
+            locator_domain="https://www.peoplesjewellers.com/",
+            page_url=link,
+            location_name=title,
+            street_address=street.strip(),
+            city=city.strip(),
+            state=state.strip(),
+            zip_postal=pcode.strip(),
+            country_code=ccode,
+            store_number=store,
+            phone=phone.strip(),
+            location_type="<MISSING>",
+            latitude=lat,
+            longitude=longt,
+            hours_of_operation=hours,
+        )
 
 
 def scrape():
