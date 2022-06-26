5d8b612a1e5a1ed894b1bde478a2b7a32f0e5644
ihor <ihor.shan@gmail.com>
2022-06-26 12:06:02 +0200

    re-written

--- apify/MrQwerty1/storelocator/goorin_com/scrape.py
+++ apify/MrQwerty1/storelocator/goorin_com/scrape.py
@@ -1,45 +1,115 @@
-from lxml import html
+import usaddress
+
 from sgscrape.sgrecord import SgRecord
 from sgrequests import SgRequests
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from concurrent import futures
+
+
+def get_address(line):
+    tag = {
+        "Recipient": "recipient",
+        "AddressNumber": "address1",
+        "AddressNumberPrefix": "address1",
+        "AddressNumberSuffix": "address1",
+        "StreetName": "address1",
+        "StreetNamePreDirectional": "address1",
+        "StreetNamePreModifier": "address1",
+        "StreetNamePreType": "address1",
+        "StreetNamePostDirectional": "address1",
+        "StreetNamePostModifier": "address1",
+        "StreetNamePostType": "address1",
+        "CornerOf": "address1",
+        "IntersectionSeparator": "address1",
+        "LandmarkName": "address1",
+        "USPSBoxGroupID": "address1",
+        "USPSBoxGroupType": "address1",
+        "USPSBoxID": "address1",
+        "USPSBoxType": "address1",
+        "OccupancyType": "address2",
+        "OccupancyIdentifier": "address2",
+        "SubaddressIdentifier": "address2",
+        "SubaddressType": "address2",
+        "PlaceName": "city",
+        "StateName": "state",
+        "ZipCode": "postal",
+    }
+
+    a = usaddress.tag(line, tag_mapping=tag)[0]
+    adr1 = a.get("address1") or ""
+    adr2 = a.get("address2") or ""
+    street_address = f"{adr1} {adr2}".strip()
+    city = a.get("city")
+    state = a.get("state")
+    postal = a.get("postal")
+
+    return street_address, city, state, postal
+
+
+def get_ids():
+    ids = []
+    api = "https://www.closeby.co/embed/49f236a73f3493bdaf88e7d524254274/locations"
+    r = session.get(api, headers=headers)
+    js = r.json()["locations"]
+
+    for j in js:
+        ids.append(j.get("id"))
+
+    return ids
+
+
+def get_data(store_number, sgw: SgWriter):
+    api = f"https://www.closeby.co/locations/{store_number}"
+    r = session.get(api, headers=headers)
+    j = r.json()["location"]
+
+    raw_address = j.get("address_full")
+    street_address, city, state, postal = get_address(raw_address)
+    country_code = "US"
+    location_name = j.get("title")
+    phone = j.get("phone_number")
+    latitude = j.get("latitude")
+    longitude = j.get("longitude")
+
+    _tmp = []
+    hours = j.get("location_hours") or []
+    for h in hours:
+        day = h.get("day_short_name")
+        start = h.get("time_open")
+        end = h.get("time_close")
+        _tmp.append(f"{day}: {start}-{end}")
+
+    hours_of_operation = ";".join(_tmp)
+
+    row = SgRecord(
+        page_url=page_url,
+        location_name=location_name,
+        street_address=street_address,
+        city=city,
+        state=state,
+        zip_postal=postal,
+        country_code=country_code,
+        latitude=latitude,
+        longitude=longitude,
+        phone=phone,
+        store_number=store_number,
+        raw_address=raw_address,
+        hours_of_operation=hours_of_operation,
+        locator_domain=locator_domain,
+    )
+
+    sgw.write_row(row)
 
 
 def fetch_data(sgw: SgWriter):
-    r = session.get(page_url, headers=headers)
-    tree = html.fromstring(r.text)
-    divs = tree.xpath("//div[@class='find-shop-item']")
+    ids = get_ids()
 
-    for d in divs:
-        location_name = d.xpath(".//div[@class='address-blog']/h4/text()")[0].strip()
-        line = d.xpath(".//div[@class='address-blog'][1]/p/text()")
-        line = list(filter(None, [l.strip() for l in line]))
-
-        phone = line.pop()
-        csz = line.pop().replace(" CA ", ", CA ").split(", ")
-        postal = csz[1].split()[-1]
-        state = csz[1].replace(postal, "").strip()
-        city = csz.pop(0)
-        street_address = ", ".join(line)
-        hours_of_operation = ";".join(
-            d.xpath(".//h4[text()='HOURS']/following-sibling::p/text()")
-        )
-
-        row = SgRecord(
-            page_url=page_url,
-            location_name=location_name,
-            street_address=street_address,
-            city=city,
-            state=state,
-            zip_postal=postal,
-            country_code="US",
-            phone=phone,
-            locator_domain=locator_domain,
-            hours_of_operation=hours_of_operation,
-        )
-
-        sgw.write_row(row)
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        future_to_url = {executor.submit(get_data, _id, sgw): _id for _id in ids}
+        for future in futures.as_completed(future_to_url):
+            future.result()
 
 
 if __name__ == "__main__":
@@ -48,8 +118,6 @@ if __name__ == "__main__":
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"
     }
-    session = SgRequests()
-    with SgWriter(
-        SgRecordDeduper(SgRecordID({SgRecord.Headers.LOCATION_NAME}))
-    ) as writer:
-        fetch_data(writer)
+    with SgRequests() as session:
+        with SgWriter(SgRecordDeduper(RecommendedRecordIds.StoreNumberId)) as writer:
+            fetch_data(writer)
