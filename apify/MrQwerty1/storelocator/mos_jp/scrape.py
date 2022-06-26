2f4f2de7d62107e3f0f8c77df8febcd7b3b7ce5a
ihor <ihor.shan@gmail.com>
2022-06-26 10:44:27 +0200

    re-written

--- apify/MrQwerty1/storelocator/mos_jp/scrape.py
+++ apify/MrQwerty1/storelocator/mos_jp/scrape.py
@@ -1,97 +1,60 @@
-from lxml import html
 from sgscrape.sgrecord import SgRecord
 from sgrequests import SgRequests
 from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import RecommendedRecordIds
-from concurrent import futures
-from sgscrape.sgpostal import parse_address, International_Parser
-
-
-def get_international(line):
-    adr = parse_address(International_Parser(), line)
-    street_address = f"{adr.street_address_1} {adr.street_address_2 or ''}".replace(
-        "None", ""
-    ).strip()
-    city = adr.city or ""
-    state = adr.state
-    postal = adr.postcode
-
-    return street_address, city, state, postal
-
-
-def get_urls():
-    urls = []
-    for i in range(1, 48):
-        r = session.get(f"https://www.mos.jp/shop/{str(i).zfill(2)}/", headers=headers)
-        tree = html.fromstring(r.text)
-        urls += tree.xpath("//div[@class='frame-green clickableFrame']/a/@href")
-
-    return urls
-
-
-def get_data(slug, sgw: SgWriter):
-    page_url = f"https://www.mos.jp{slug}"
-    r = session.get(page_url, headers=headers)
-    tree = html.fromstring(r.text)
-
-    location_name = "".join(tree.xpath("//span[@class='hps-s2']/text()")).strip()
-    raw_address = "".join(
-        tree.xpath("//th[contains(text(), '住所')]/following-sibling::td/text()")
-    ).strip()
-    street_address, city, state, postal = get_international(raw_address)
-    phone = "".join(
-        tree.xpath("//th[contains(text(), 'TEL')]/following-sibling::td//text()")
-    ).strip()
-    text = "".join(tree.xpath("//iframe/@src"))
-    try:
-        latitude, longitude = text.split("q=")[1].split("&")[0].split(",")
-    except:
-        latitude, longitude = SgRecord.MISSING, SgRecord.MISSING
-    hours_of_operation = " ".join(
-        ";".join(
-            tree.xpath("//th[contains(text(), '営業時間')]/following-sibling::td//text()")
-        ).split()
-    )
-    if hours_of_operation.startswith(";"):
-        hours_of_operation = hours_of_operation[1:]
-    if hours_of_operation.endswith(";"):
-        hours_of_operation = hours_of_operation[:-1]
-
-    row = SgRecord(
-        page_url=page_url,
-        location_name=location_name,
-        street_address=street_address,
-        city=city,
-        state=state,
-        zip_postal=postal,
-        country_code="JP",
-        latitude=latitude,
-        longitude=longitude,
-        phone=phone,
-        locator_domain=locator_domain,
-        hours_of_operation=hours_of_operation,
-        raw_address=raw_address,
-    )
-
-    sgw.write_row(row)
 
 
 def fetch_data(sgw: SgWriter):
-    urls = get_urls()
+    api = "https://www.mos.jp/data/shop/shop.json"
+    r = session.get(api, headers=headers)
+    js = r.json()["shops"]
 
-    with futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_url = {executor.submit(get_data, url, sgw): url for url in urls}
-        for future in futures.as_completed(future_to_url):
-            future.result()
+    for j in js:
+        raw_address = j.get("addr_all")
+        street_address = j.get("addr")
+        city = j.get("city_name")
+        state = j.get("pref_name")
+        postal = f'{j.get("zip1") or ""}-{j.get("zip2") or ""}'.strip()
+        if postal == "-":
+            postal = SgRecord.MISSING
+        country_code = "JP"
+        store_number = j.get("shop_cd")
+        location_name = j.get("name")
+        page_url = f"https://netorder.mos.co.jp/pc/shop_detail/{store_number}/"
+        phone = (
+            f'{j.get("tel1") or ""}-{j.get("tel2") or ""}-{j.get("tel3") or ""}'.strip()
+        )
+        if phone == "--":
+            phone = SgRecord.MISSING
+        latitude = j.get("lat")
+        longitude = j.get("lon")
+
+        row = SgRecord(
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=postal,
+            country_code=country_code,
+            latitude=latitude,
+            longitude=longitude,
+            phone=phone,
+            store_number=store_number,
+            raw_address=raw_address,
+            hours_of_operation="<INACCESSIBLE>",
+            locator_domain=locator_domain,
+        )
+
+        sgw.write_row(row)
 
 
 if __name__ == "__main__":
     locator_domain = "https://www.mos.jp/"
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0",
     }
-    session = SgRequests()
-    with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
-        fetch_data(writer)
+    with SgRequests() as session:
+        with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
+            fetch_data(writer)
