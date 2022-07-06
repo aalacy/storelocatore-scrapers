81b3551d8bb1fa44fd6c96d2d3351656a5f1518c
aalacy <aaronlacywhite387@gmail.com>
2022-07-06 00:58:31 -0700

    f

--- apify/idev-a/storelocator/modernbeauty_ca/scrape.py
+++ apify/idev-a/storelocator/modernbeauty_ca/scrape.py
@@ -1,7 +1,10 @@
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
 from bs4 import BeautifulSoup as bs
+from sgscrape.simple_utils import parallelize
 import json
 from sglogging import SgLogSetup
 
@@ -14,15 +17,28 @@ locator_domain = "https://www.modernbeauty.com"
 base_url = "https://www.modernbeauty.com/locations.html"
 
 
+def reqs(r, http):
+    logger.info(r)
+    res = http.get(r, headers=_headers).text
+    return res, r
+
+
 def fetch_data():
-    with SgRequests() as session:
-        soup = bs(session.get(base_url, headers=_headers).text, "lxml")
-        links = soup.select("div.location_details")
+    with SgRequests() as http:
+        soup = bs(http.get(base_url, headers=_headers).text, "lxml")
+        links = [
+            locator_domain + link["data-loc"]
+            for link in soup.select("div.location_details")
+        ]
         logger.info(f"{len(links)} found")
-        for link in links:
-            page_url = locator_domain + link["data-loc"]
-            logger.info(page_url)
-            res = session.get(page_url, headers=_headers).text
+        results = list(
+            parallelize(
+                search_space=links,
+                fetch_results_for_rec=lambda r: reqs(r, http),
+                max_threads=4,
+            )
+        )
+        for res, page_url in results:
             state = (
                 bs(res, "lxml")
                 .select("div.font_size_8")[1]
@@ -56,7 +72,7 @@ def fetch_data():
 
 
 if __name__ == "__main__":
-    with SgWriter() as writer:
+    with SgWriter(SgRecordDeduper(RecommendedRecordIds.StoreNumberId)) as writer:
         results = fetch_data()
         for rec in results:
             writer.write_row(rec)
