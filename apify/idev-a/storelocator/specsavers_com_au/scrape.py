4beb942e3c4ae3663a7d8e1f81463dee8e3913bf
aalacy <aaronlacywhite387@gmail.com>
2022-07-05 23:06:28 -0700

    f

--- apify/idev-a/storelocator/specsavers_com_au/scrape.py
+++ apify/idev-a/storelocator/specsavers_com_au/scrape.py
@@ -39,9 +39,7 @@ days = [
 
 
 def fetch_data():
-    with SgChrome(
-        user_agent="Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1"
-    ) as driver:
+    with SgChrome() as driver:
         with SgRequests() as session:
             driver.get(base_url)
             cookies = []
